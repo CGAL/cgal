d5598b6db355af0bff407d008389f072b4ab6f85
Sven Schönherr <sven@inf.ethz.ch>
1997-12-02 18:12:53 +0000

    Initial version

---
+++ Packages/Min_circle_2/demo/Min_circle_2/demo_Min_circle_2.C
@@ -0,0 +1,88 @@
+// ============================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       : $CGAL_Revision: CGAL-wip $
+// release_date  : $CGAL_Date$
+//
+// file          : demo/Optimisation/demo_Min_circle_2.C
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Sven Schönherr <sven@inf.fu-berlin.de>
+//
+// coordinator   : ETH Zurich (Bernd Gärtner <gaertner@inf.ethz.ch>)
+//
+// demo program  : 2D Smallest Enclosing Circle
+// ============================================================================
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// NOTE: In this release correct results are only guaranteed if exact
+// arithmetic is used, so this demo (using inexact floating-point
+// arithmetic) is only intended to illustrate the techniques. However,
+// the program will terminate, but the computed circle may neither
+// contain all points nor be the smallest one.
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// includes
+#include <CGAL/Homogeneous.h>
+#include <CGAL/Point_2.h>
+#include <CGAL/Optimisation_traits_2.h>
+#include <CGAL/Min_circle_2.h>
+#include <CGAL/IO/Window_stream.h>
+
+// typedefs
+typedef  CGAL_Homogeneous< double >         Rep;
+typedef  CGAL_Point_2< Rep >                Point;
+typedef  CGAL_Optimisation_traits_2< Rep >  Traits;
+typedef  CGAL_Min_circle_2< Traits >        Min_circle;
+
+// main
+int
+main( int, char**)
+{
+    cerr << "  left button: insert point" << endl;
+    cerr << "middle button: clear points" << endl;
+    cerr << " right button: exit"         << endl;
+
+    // create empty min_circle;
+    Min_circle  mc;
+
+    // open window
+    CGAL_Window_stream ws( 500, 500, 300, 200);
+    ws.set_frame_label( "CGAL Demo: Smallest Enclosing Circle in 2D");
+    ws.init( -100.0, 100.0, -100.0);
+
+    // main loop
+    double  x, y;
+    int     button, i;
+    do {
+	// get mouse click
+	button = ws.read_mouse( x, y);
+
+	switch ( button) {
+
+	  case MOUSE_BUTTON( 1):                        // left button
+	    ws << CGAL_WHITE << mc.circle();
+	    mc.insert( ::Point( x, y));
+	    ws << CGAL_BLACK << mc;
+	    ws << CGAL_BLUE  << mc.circle();
+	    ws << CGAL_RED;
+	    for ( i = 0; i < mc.number_of_support_points(); ++i)
+		ws << mc.support_point( i);
+	    break;
+
+	  case MOUSE_BUTTON( 2):                        // middle button
+	    ws << CGAL_WHITE << mc;
+	    mc.clear();
+	    break; } }
+
+    while ( button != MOUSE_BUTTON( 3));                // right button
+}
+
+// ===== EOF ==================================================================
---
+++ Packages/Min_ellipse_2/examples/Min_ellipse_2/example_Min_ellipse_2.C
@@ -0,0 +1,59 @@
+// ============================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       : $CGAL_Revision: CGAL-wip $
+// release_date  : $CGAL_Date$
+//
+// file          : examples/Optimisation/example_Min_ellipse_2.C
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Sven Schönherr <sven@inf.fu-berlin.de>
+//                 Bernd Gärtner
+//
+// coordinator   : ETH Zurich (Bernd Gärtner <gaertner@inf.ethz.ch>)
+//
+// example progr.: 2D Smallest Enclosing Ellipse
+// ============================================================================
+
+// includes
+#include <CGAL/Gmpz.h>
+#include <CGAL/Homogeneous.h>
+#include <CGAL/Point_2.h>
+#include <CGAL/Optimisation_traits_2.h>
+#include <CGAL/Min_ellipse_2.h>
+
+// typedefs
+typedef  CGAL_Gmpz                      NT;
+typedef  CGAL_Homogeneous<NT>           R;
+typedef  CGAL_Point_2<R>                Point;
+typedef  CGAL_Optimisation_traits_2<R>  Traits;
+typedef  CGAL_Min_ellipse_2<Traits>     Min_ellipse;
+
+// main
+int
+main( int, char**)
+{
+    int     n = 100;
+    Point*  P = new Point[ n];
+
+    for ( int i = 0; i < n; ++i)
+	P[ i] = Point( (i%2 == 0 ? i : -i), 0);
+    // (0,0), (-1,0), (2,0), (-3,0), ...
+
+    Min_ellipse  me1( P, P+n);           // very slow
+    Min_ellipse  me2( P, P+n, true);     // fast
+
+    CGAL_set_pretty_mode( cout);
+    cout << me2;
+
+    delete[] P;
+}
+
+// ===== EOF ==================================================================
