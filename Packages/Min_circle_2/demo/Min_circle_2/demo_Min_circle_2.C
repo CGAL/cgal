68362940aae02027994c02de2fcc26efa30dac68
Sven Schönherr <sven@inf.ethz.ch>
1998-06-26 15:25:04 +0000

    updated: Copyright notice fro 1998

--- Packages/Min_circle_2/demo/Min_circle_2/demo_Min_circle_2.C
+++ Packages/Min_circle_2/demo/Min_circle_2/demo_Min_circle_2.C
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (c) 1997 The CGAL Consortium
+// Copyright (c) 1997,1998 The CGAL Consortium
 //
 // This software and related documentation is part of an INTERNAL release
 // of the Computational Geometry Algorithms Library (CGAL). It is not
@@ -14,7 +14,7 @@
 // file          : demo/Optimisation/demo_Min_circle_2.C
 // chapter       : $CGAL_Chapter: Geometric Optimisation $
 // package       : $CGAL_Package: Min_circle_2 WIP $
-
+//
 // revision      : $Revision$
 // revision_date : $Date$
 // author(s)     : Sven Schönherr <sven@inf.fu-berlin.de>
