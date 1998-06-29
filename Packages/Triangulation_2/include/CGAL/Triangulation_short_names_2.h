3ef92c1a76074c4ef0727b2d6d9b4bce229e1f09
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
1998-06-29 15:28:22 +0000

    Initial revision

---
+++ Packages/Triangulation_2/include/CGAL/Triangulation_short_names_2.h
@@ -0,0 +1,28 @@
+#ifndef CGAL_TRIANGULATION_SHORT_NAMES_2_H
+#define CGAL_TRIANGULATION_SHORT_NAMES_2_H
+
+
+
+
+// Define shorter names to please linker (g++/egcs)
+   #define CGAL_Triangulation_default_data_structure_2 CGAL_Tdds
+   #define CGAL_Triangulation_vertex_base_2  CGAL_Trvb
+   #define CGAL_Triangulation_face_base_2  CGAL_Trfb
+   #define CGAL_Triangulation_euclidean_traits_2 CGAL_Et2
+   #define CGAL_Triangulation_euclidean_traits_xy_3 GGAL_Etxy3
+   #define CGAL_Triangulation_euclidean_traits_xz_3 CGAL_Etxz3
+   #define CGAL_Triangulation_euclidean_traits_yz_3 CGAL_Etyz3
+
+// #define CGAL_Triangulation_vertex_2 CGAL_Trv
+// #define CGAL_Triangulation_face_2 CGAL_Trf
+// #define CGAL_Triangulation_vertex_handle_2 CGAL_Trvh
+// #define CGAL_Triangulation_face_handle_2 CGAL_Trfh
+// #define CGAL_Triangulation_vertex_iterator_2 CGAL_Trvi
+// #define CGAL_Triangulation_face_iterator_2 CGAL_Trfi
+// #define CGAL_Triangulation_edge_iterator_2 CGAL_Trei
+// #define CGAL_Triangulation_vertex_circulator_2 CGAL_Trvc
+// #define CGAL_Triangulation_face_circulator_2 CGAL_Trfc
+// #define CGAL_Triangulation_edge_circulator_2 CGAL_Trec
+
+
+#endif CGAL_TRIANGULATION_SHORT_NAMES_2_H
