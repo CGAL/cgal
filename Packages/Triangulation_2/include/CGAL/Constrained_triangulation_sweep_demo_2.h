4055c816bd01d131ea4e1b4a17fead0d87797f9e
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
1998-07-20 09:13:45 +0000

    Initial revision

---
+++ Packages/Triangulation_2/include/CGAL/Constrained_triangulation_sweep_demo_2.h
@@ -0,0 +1,191 @@
+#ifndef CGAL_CONSTRAINED_TRIANGULATION_SWEEP_DEMO_H
+#define CGAL_CONSTRAINED_TRIANGULATION_SWEEP_DEMO_H
+
+// #include <CGAL/Constrained_triangulation_traits_2.h>
+#include <CGAL/Constrained_triangulation_sweep_2.h>
+#include <CGAL/IO/Window_stream.h>
+
+template < class Gt, class Tds>
+class CGAL_Constrained_triangulation_sweep_demo_2
+  : public CGAL_Constrained_triangulation_sweep_2<Gt,Tds>
+{
+public:
+  typedef typename Gt::Segment Segment;
+  typedef CGAL_Window_stream Window_stream;
+
+  typedef CGAL_Constrained_triangulation_sweep_2<Gt,Tds> Sweep;
+  typedef typename Sweep::Out_edges Out_edges;
+  typedef typename Sweep::Event_queue Event_queue;
+  typedef typename Sweep::Sweep_status Sweep_status;
+
+
+CGAL_Constrained_triangulation_sweep_demo_2() {}
+
+CGAL_Constrained_triangulation_sweep_demo_2(Window_stream& W,
+					 list<Constraint>& lc, const Gt& t=Gt())
+  : CGAL_Constrained_triangulation_sweep_2<Gt,Tds>()
+  {
+    _t = t ; 
+    _lc = &lc;
+    event_less= Event_less();
+    queue = Event_queue(event_less);
+    status_comp = Status_comp();
+    status= Sweep_status(status_comp);
+    upper_chain=Chain();
+    make_event_queue();
+    demo_build_triangulation(W);
+  }
+
+
+
+private:
+void 
+demo_build_triangulation(Window_stream& W);
+
+void 
+draw_face(Face_handle f, CGAL_Window_stream& W)
+  {
+    for(int i =0; i<3; i++){
+      W << Segment((f->vertex(i))->point(), (f->vertex(f->cw(i)))->point());
+    }
+  return;
+  }
+
+void 
+draw_new_faces(Vertex_handle v, CGAL_Window_stream& W)
+{
+   drawing_mode dm=W.set_mode(leda_src_mode);
+   W << CGAL_BLUE;
+   Face_handle f = v->face();
+   if (f == NULL) { return;} //first point, no face
+   draw_face(f,W);
+   while(f->neighbor(1) != NULL) {
+     f = f->neighbor(1);
+     draw_face(f,W);
+   }
+}
+
+void 
+draw_chain(Chain * pc,CGAL_Window_stream& W)
+{
+   Neighbor_list::iterator nit;
+   Face_handle f;
+   int i;
+   nit = (pc->down_list())->begin();
+   while (nit != (pc-> down_list())->end()) {
+     f = (*nit).first;
+     i = (*nit).second;
+     W << Segment(f->vertex(f->cw(i))->point(), 
+		  f->vertex(f->ccw(i))->point());
+     nit++;
+   }
+   nit = (pc->up_list())->begin();	 
+   while (nit != (pc->up_list())->end()) {
+     f = (*nit).first;
+     i = (*nit).second;
+     W << Segment(f->vertex(f->cw(i))->point(), 
+		    f->vertex(f->ccw(i))->point());
+     nit++;
+   }
+   return;
+}
+
+void 
+draw_status_chains(CGAL_Window_stream& W)
+{
+  drawing_mode dm=W.set_mode(leda_src_mode);
+  W << CGAL_GREEN;
+
+  Chain * pc;
+  typename Sweep_status::iterator status_it=status.begin();
+  while ( status_it != status.end()) {
+    pc = (Chain *) ((*status_it).second);
+    draw_chain(pc,W);
+    status_it ++;
+  }
+  pc = & upper_chain;
+  draw_chain(pc,W);
+  return;
+}
+  
+void 
+draw_constraints(CGAL_Window_stream& W)
+{
+  drawing_mode dm=W.set_mode(leda_src_mode);
+  W << CGAL_RED;
+
+  list<Constraint>::iterator cit=_lc->begin();
+  while(cit != _lc->end()){
+    W << Segment((*cit).first,(*cit).second);
+    cit++;
+  }
+  return;
+}
+
+
+void 
+draw_next_event(CGAL_Window_stream& W)
+{
+  return;
+}
+
+void 
+any_button(CGAL_Window_stream& W)
+{
+  double x, y;
+  cerr << "Press any button to continue" << endl;
+  W.read_mouse(x,y);
+}
+
+};
+
+
+template<class Gt, class Tds>
+void
+CGAL_Constrained_triangulation_sweep_demo_2<Gt,Tds>::
+demo_build_triangulation(CGAL_Window_stream& W)
+{
+  Point p; 
+  Vertex_handle v;
+  Out_edges * out;
+  typename Event_queue::iterator event;
+  int count =0;
+
+  while (! queue.empty()) {
+    count++;
+    event = queue.begin();
+             
+    // locate (p,p) dans status
+    p = (*event).first;
+    out = (*event).second;
+    typename Sweep_status::iterator loc=status.lower_bound(Constraint(p,p));
+    // deal with the contraints finishing at p
+    v = treat_in_edges(event,loc);
+    // insert constraint beginning at p
+    treat_out_edges(event,loc);
+
+    draw_new_faces(v,W);
+    draw_constraints(W);
+    draw_status_chains(W);
+    draw_next_event(W);
+    any_button(W);
+	
+	
+    // delete event from event_queue
+    out= (*event).second;
+    assert( (*out).empty());
+    delete out;
+    queue.erase(event);
+    //delete la liste out
+  }
+  // make inifinite vertex, infinite faces 
+  // at this stage status is empty
+  // and the lists of upper_chain correspond to the convex hull
+  assert( status.empty());
+  the_vertex = set_infinite_faces();
+
+  return;
+}
+
+
+#endif //CGAL_CONSTRAINED_TRIANGULATION_SWEEP_DEMO_2_H
