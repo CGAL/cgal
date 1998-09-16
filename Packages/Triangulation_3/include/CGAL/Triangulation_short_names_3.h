76daf28038ed319cde7d9e998ddeaee51280137e
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
1998-09-16 11:10:27 +0000

    First non empty version

---
+++ Packages/Triangulation_3/include/CGAL/Triangulation_short_names_3.h
@@ -0,0 +1,39 @@
+// ============================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       :
+// release_date  :
+//
+// file          : include/CGAL/Triangulation_short_names.h
+// revision      : $Revision$
+// author(s)     : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
+//
+// coordinator   : Mariette Yvinec  <Mariette.Yvinec@sophia.inria.fr>
+//
+// ============================================================================
+
+#ifndef CGAL_TRIANGULATION_SHORT_NAMES_H
+#define CGAL_TRIANGULATION_SHORT_NAMES_H
+
+// Define shorter names to please linker (g++/egcs)
+#define CGAL_Triangulation_base_vertex CGAL_Tbv
+#define CGAL_Triangulation_base_cell CGAL_Tbc
+#define CGAL_Triangulation_geom_traits CGAL_Gt
+
+#define CGAL_Triangulation_ds_vertex CGAL_Tdsv
+#define CGAL_Triangulation_ds_cell CGAL_Tdsc
+#define CGAL_Triangulation_data_structure CGAL_Tds
+
+#define CGAL_Triangulation_ds_cell_iterator CGAL_Tdscit
+#define CGAL_Triangulation_ds_vertex_iterator CGAL_Tdsvit
+#define CGAL_Triangulation_ds_facet_iterator CGAL_Tdsfit
+#define CGAL_Triangulation_ds_cell_circulator CGAL_Tdsccir
+
+#endif CGAL_TRIANGULATION_SHORT_NAMES_H
