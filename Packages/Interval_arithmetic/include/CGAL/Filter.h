9cdc072750dfbccacc0e05867ce7c75f81659029
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1998-10-30 16:51:45 +0000

    First release.  The wrapper works at least on a few cases.

---
+++ Packages/Interval_arithmetic/include/CGAL/Filter.h
@@ -0,0 +1,96 @@
+// ============================================================================
+//
+// Copyright (c) 1998 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       :
+// release_date  :
+//
+// file          : include/CGAL/Filter.h
+// revision      : $Revision$
+// revision_date : $Date$
+// package       : Interval Arithmetic
+// author(s)     : Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
+//
+// coordinator   : INRIA Sophia-Antipolis (<Mariette.Yvinec@sophia.inria.fr>)
+//
+// ============================================================================
+
+// This file contains a wrapper type for number types, that helps for
+// specializing template predicates, to use interval arithmetic as a filter.
+
+#ifndef CGAL_FILTER_H
+#define CGAL_FILTER_H
+
+// CT = construction type (filtered)
+// ET = exact type, used for exact predicate evaluation
+// (Interval_nt_advanced) is used for filtering.
+//
+// 2 functions must be provided for the whole thing to work for a particular
+// instantiation:
+// - CGAL_to_interval_nt_advanced CGAL_to_interval_nt(const CT &);
+//     which gives an interval surely containing the CT value.
+// - ET CGAL_to_exact_type<ET>(const CT &)
+//     which converts _exactly_ the CT value to ET.
+
+template <class CT, class ET>
+class CGAL_Filtering
+{
+public:
+  CT value;
+
+  CGAL_Filtering ()      : value()   {}
+  CGAL_Filtering (int i) : value(i)  {}
+  CGAL_Filtering (CT ct) : value(ct) {}
+
+  typedef CGAL_Filtering<CT,ET> Fil;
+
+  Fil operator-()               const { return Fil(-value); }
+
+  Fil operator+(const Fil& fil) const { return Fil(value + fil.value); }
+  Fil operator-(const Fil& fil) const { return Fil(value - fil.value); }
+  Fil operator*(const Fil& fil) const { return Fil(value * fil.value); }
+  Fil operator/(const Fil& fil) const { return Fil(value / fil.value); }
+
+  Fil& operator+=(const Fil& fil) { value += fil.value; return *this; }
+  Fil& operator-=(const Fil& fil) { value -= fil.value; return *this; }
+  Fil& operator*=(const Fil& fil) { value *= fil.value; return *this; }
+  Fil& operator/=(const Fil& fil) { value /= fil.value; return *this; }
+
+  bool operator< (const Fil& fil) const { return value <  fil.value; }
+  bool operator> (const Fil& fil) const { return value >  fil.value; }
+  bool operator<=(const Fil& fil) const { return value <= fil.value; }
+  bool operator>=(const Fil& fil) const { return value >= fil.value; }
+  bool operator==(const Fil& fil) const { return value == fil.value; }
+  bool operator!=(const Fil& fil) const { return value != fil.value; }
+};
+
+template <class CT, class ET>
+inline bool CGAL_is_valid    (const CGAL_Filtering<CT,ET>& fil)
+{ return CGAL_is_valid(fil.value); }
+
+template <class CT, class ET>
+inline bool CGAL_is_finite   (const CGAL_Filtering<CT,ET>& fil)
+{ return CGAL_is_finite(fil.value); }
+
+template <class CT, class ET>
+inline double CGAL_to_double (const CGAL_Filtering<CT,ET>& fil)
+{ return CGAL_to_double(fil.value); }
+
+// template <class ET>
+  // template <class CT>
+    // ET CGAL_to_exact_type(const CT &);
+
+// All exact types should reasonnably have a built-in exact conversion
+// from doubles ?  If not, it will fail, and you have to provide it.
+
+template <class ET>
+inline ET CGAL_to_exact_type (const double & d)
+{ return ET(d); }
+
+#endif // CGAL_FILTER_H
