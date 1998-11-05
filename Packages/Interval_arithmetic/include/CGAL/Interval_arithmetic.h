b1b92487d7e24f298d262f4367c2310b0bdb0844
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1998-11-05 14:28:18 +0000

    - Added specializations for the functions CGAL_{sign,compare,abs,min,max}. - overlap_action() returns an int(0), to shut up compiler, and provide   a default answer, when we don't want the exception. - The test operators now "return overlap_action();". - One test less in operator==().

--- Packages/Interval_arithmetic/include/CGAL/Interval_arithmetic.h
+++ Packages/Interval_arithmetic/include/CGAL/Interval_arithmetic.h
@@ -33,17 +33,17 @@
 #include <iostream.h>
 #include <CGAL/assertions.h>
 #include <CGAL/double.h>	// For CGAL_is_valid() and CGAL_is_finite().
+#include <CGAL/number_utils.h>  // For CGAL_{sign,compare,abs,min,max...}
 #include <CGAL/Interval_arithmetic/_FPU.h>	// FPU rounding mode functions.
 
 struct CGAL_Interval_nt_advanced
 {
+  struct unsafe_comparison{};  // Exception class.
 protected:
   double inf, sup;	// "inf" stores the OPPOSITE of the lower bound.
 			// "sup" stores the upper bound of the interval.
-  struct unsafe_comparison{};  // Exception class.
-
 private:
-  void overlap_action() const throw (unsafe_comparison)
+  int overlap_action() const throw (unsafe_comparison)
   {
 #if !defined(CGAL_IA_NO_WARNINGS) && !defined(CGAL_NO_WARNINGS)
     CGAL_warning_msg(false, " Comparison between overlapping intervals");
@@ -51,15 +51,24 @@ private:
 #ifndef CGAL_IA_DONT_THROW_EXCEPTION
     throw unsafe_comparison();
 #endif
+    return 0; // Return a value to prevent compiler warnings.
   }
 
 public:
-  friend CGAL_Interval_nt_advanced sqrt (const CGAL_Interval_nt_advanced&);
+  friend CGAL_Interval_nt_advanced sqrt     (const CGAL_Interval_nt_advanced&);
+  friend CGAL_Interval_nt_advanced CGAL_abs (const CGAL_Interval_nt_advanced&);
+  friend CGAL_Interval_nt_advanced CGAL_min (const CGAL_Interval_nt_advanced&,
+	  				     const CGAL_Interval_nt_advanced&);
+  friend CGAL_Interval_nt_advanced CGAL_max (const CGAL_Interval_nt_advanced&,
+					     const CGAL_Interval_nt_advanced&);
+  friend CGAL_Interval_nt_advanced operator* (const double,
+					      const CGAL_Interval_nt_advanced&);
   friend double CGAL_to_double (const CGAL_Interval_nt_advanced&);
   friend bool   CGAL_is_valid  (const CGAL_Interval_nt_advanced&);
   friend bool   CGAL_is_finite (const CGAL_Interval_nt_advanced&);
-  friend CGAL_Interval_nt_advanced operator*
-      (const double, const CGAL_Interval_nt_advanced&);
+  friend CGAL_Sign CGAL_sign   (const CGAL_Interval_nt_advanced&);
+  friend CGAL_Comparison_result CGAL_compare (const CGAL_Interval_nt_advanced&,
+					      const CGAL_Interval_nt_advanced&);
 
   // The constructors.
   CGAL_Interval_nt_advanced() {}
@@ -112,8 +121,7 @@ public:
   {
     if (sup  < -d.inf) return true;
     if (-inf >= d.sup) return false;
-    overlap_action();
-    return false;  // Hum...
+    return overlap_action();
   }
 
   bool operator>(const CGAL_Interval_nt_advanced& d) const
@@ -123,8 +131,7 @@ public:
   {
     if (sup <= -d.inf) return true;
     if (-inf >  d.sup) return false;
-    overlap_action();
-    return false;  // Hum...
+    return overlap_action();
   }
 
   bool operator>=(const CGAL_Interval_nt_advanced& d) const
@@ -132,10 +139,9 @@ public:
 
   bool operator==(const CGAL_Interval_nt_advanced& d) const
   {
-    if ((d > *this) || (d < *this)) return false;
-    if ((-d.inf == d.sup) && (-inf == sup) && (sup == d.sup)) return true;
-    overlap_action();
-    return false;  // Hum...
+    if ((-d.inf >  sup) || (d.sup  < -inf)) return false;
+    if ((-d.inf == sup) && (d.sup == -inf)) return true;
+    return overlap_action();
   }
 
   bool operator!=(const CGAL_Interval_nt_advanced& d) const
@@ -260,14 +266,53 @@ inline CGAL_Interval_nt_advanced sqrt(const CGAL_Interval_nt_advanced& d)
   return tmp;
 }
 
-inline double CGAL_to_double(const CGAL_Interval_nt_advanced& d)
+inline double CGAL_to_double (const CGAL_Interval_nt_advanced& d)
 { return (d.sup-d.inf)*.5; }
 
-inline bool CGAL_is_valid(const CGAL_Interval_nt_advanced& d)
+inline bool CGAL_is_valid  (const CGAL_Interval_nt_advanced& d)
 { return CGAL_is_valid(d.inf) && CGAL_is_valid(d.sup) && (d.sup >= -d.inf); }
 
-inline bool CGAL_is_finite(const CGAL_Interval_nt_advanced& d)
+inline bool CGAL_is_finite (const CGAL_Interval_nt_advanced& d)
 { return CGAL_is_finite(d.inf) && CGAL_is_finite(d.sup); }
+
+inline CGAL_Sign CGAL_sign (const CGAL_Interval_nt_advanced& d)
+{
+    // Benchmark it, and compare with CGAL_compare(d,0).
+  if (d.inf < 0) return CGAL_POSITIVE;
+  if (d.sup < 0) return CGAL_NEGATIVE;
+  if ( (d.inf == 0) && (d.sup == 0) ) return CGAL_ZERO;
+  return (CGAL_Sign) d.overlap_action();
+}
+
+inline CGAL_Comparison_result CGAL_compare(const CGAL_Interval_nt_advanced& d,
+					   const CGAL_Interval_nt_advanced& e)
+{
+  if (e.sup < -d.inf) return CGAL_LARGER;
+  if (-e.inf > d.sup) return CGAL_SMALLER;
+  if ( (-d.inf==e.sup) && (-e.inf==d.sup) ) return CGAL_EQUAL;
+  return (CGAL_Comparison_result) d.overlap_action();
+}
+
+inline CGAL_Interval_nt_advanced CGAL_abs (const CGAL_Interval_nt_advanced& d)
+{
+  if (d.inf <= 0) return d;
+  if (d.sup <= 0) return -d;
+  return CGAL_Interval_nt_advanced(0, CGAL_max(d.inf,d.sup));
+}
+
+inline CGAL_Interval_nt_advanced CGAL_min (const CGAL_Interval_nt_advanced& d,
+					   const CGAL_Interval_nt_advanced& e)
+{
+  return CGAL_Interval_nt_advanced(-CGAL_max(d.inf, e.inf),
+	  			    CGAL_min(d.sup, e.sup));
+}
+
+inline CGAL_Interval_nt_advanced CGAL_max (const CGAL_Interval_nt_advanced& d,
+					   const CGAL_Interval_nt_advanced& e)
+{
+  return CGAL_Interval_nt_advanced(-CGAL_min(d.inf, e.inf),
+	  			    CGAL_max(d.sup, e.sup));
+}
 
 ostream& operator<<(ostream& os, const CGAL_Interval_nt_advanced& d)
 { return os << "[" << d.lower_bound() << ";" << d.upper_bound() << "]"; }
