2ab02387b6cf8ca130eb4ebfda7c7c3f309487f4
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1998-11-12 16:03:38 +0000

    Cleaned up for it's first test-suite !!!

--- Packages/Interval_arithmetic/test/Interval_arithmetic/tst5.C
+++ Packages/Interval_arithmetic/test/Interval_arithmetic/tst5.C
@@ -8,21 +8,23 @@
 #define CGAL_IA_NO_WARNINGS
 #include <CGAL/Interval_arithmetic.h>
 
-typedef CGAL_Filtering<double, CGAL_Gmpz> NT;
-// typedef CGAL_Filtering<double, leda_real> NT;
+// Don't be stupid, CGAL_Gmpz can only store integers !!!
+// typedef CGAL_Filtering<double, CGAL_Gmpz> NT;
+typedef CGAL_Filtering<double, leda_real> NT;
 // typedef double NT;
 
 // The predicate code is 100% generic.
-#include <CGAL/filtered_predicates_on_ftC2.h>
+// The #inclusion should be done automatically.
+// #include <CGAL/Filter/predicates_on_ftC2.h>
 
-// The program code is 100% generic/templated.
+// The program code is 100% generic/templated, as usual.
 int main()
 {
   NT px, py, la, lb, lc;
   px=1; py=2; la=3; lb=4; lc=5;
-  cout << "Result 1st test: " << CGAL_compare_y_at_xC2(px, py, la, lb, lc);
-  cout << endl;
+  cout << "Result 1st test: " << (int)CGAL_compare_y_at_xC2(px, py, la, lb, lc);
+  cout << " ( == 1 )\n";
   px=1.1; py=1.7; la=1.3; lb=1.5; lc=-3.98;
   cout << "Result 2nd test: " << (int)CGAL_compare_y_at_xC2(px, py, la, lb, lc);
-  cout << endl;
+  cout << " ( == 0 ) (not sure, it depends of the first approx)\n";
 }
