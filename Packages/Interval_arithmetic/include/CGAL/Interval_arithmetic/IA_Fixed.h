861307904b8af65a6527cf99cc9187568eca6f1c
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1998-12-01 13:03:02 +0000

    Cast function for Olivier's CGAL_Fixed_precision_nt.

---
+++ Packages/Interval_arithmetic/include/CGAL/Interval_arithmetic/IA_Fixed.h
@@ -0,0 +1,35 @@
+// ============================================================================
+//
+// Copyright (c) 1998 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       :
+// release_date  :
+//
+// file          : include/CGAL/Interval_arithmetic/IA_Fixed.h
+// revision      : $Revision$
+// revision_date : $Date$
+// package       : Interval Arithmetic
+// author(s)     : Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
+//
+// coordinator   : INRIA Sophia-Antipolis (<Mariette.Yvinec@sophia.inria.fr>)
+//
+// ============================================================================
+
+#ifndef CGAL_IA_FIXED_H
+#define CGAL_IA_FIXED_H
+
+// The Fixed is in fact a float => exact conversion.
+
+inline CGAL_Interval_nt_advanced CGAL_to_Interval_nt_advanced
+	(const CGAL_Fixed_precision_nt &z)
+{
+    return CGAL_Interval_nt_advanced (z.to_double());
+}
+
+#endif	 // CGAL_IA_FIXED_H
