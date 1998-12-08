4b2bce06b40c928a221d71cdc9610caf47a2c86f
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1998-12-08 13:03:30 +0000

    - Big renaming:   - /Filter/ 		-> /Arithmetic_filter/   - Filter.h		-> Arithmetic_filter.h   - CGAL_to_exact_type 	-> CGAL_convert_to   - CGAL_FILTER_H 	-> CGAL_ARITHMETIC_FILTER_H   - CGAL_Filtering 	-> CGAL_Filtered_exact

--- Packages/Interval_arithmetic/include/CGAL/Arithmetic_filter.h
+++ Packages/Interval_arithmetic/include/CGAL/Arithmetic_filter.h
@@ -11,7 +11,7 @@
 // release       :
 // release_date  :
 //
-// file          : include/CGAL/Filter.h
+// file          : include/CGAL/Arithmetic_filter.h
 // revision      : $Revision$
 // revision_date : $Date$
 // package       : Interval Arithmetic
@@ -24,8 +24,8 @@
 // This file contains a wrapper type for number types, that helps for
 // specializing template predicates, to use interval arithmetic as a filter.
 
-#ifndef CGAL_FILTER_H
-#define CGAL_FILTER_H
+#ifndef CGAL_ARITHMETIC_FILTER_H
+#define CGAL_ARITHMETIC_FILTER_H
 
 #include <iostream.h>	// Because we declare operator<< and >>.
 #include <CGAL/enum.h>  // Because we overload CGAL_{sign,compare,abs,min,max}
@@ -41,23 +41,23 @@
 // instantiation:
 // - CGAL_to_interval_nt_advanced CGAL_to_interval_nt(const CT &);
 //     which gives an interval surely containing the CT value.
-// - ET CGAL_to_exact_type<ET>(const CT &)
+// - ET CGAL_convert_to<ET>(const CT &)
 //     which converts _exactly_ the CT value to ET.
 
 template <class CT, class ET>
-struct CGAL_Filtering
+struct CGAL_Filtered_exact
 {
   CT value;
 
-  CGAL_Filtering () {}
-  CGAL_Filtering (const CGAL_Filtering<CT,ET> & fil)
+  CGAL_Filtered_exact () {}
+  CGAL_Filtered_exact (const CGAL_Filtered_exact<CT,ET> & fil)
       : value(fil.value)  {}
   template <class NT>
-  CGAL_Filtering (const NT & nt) : value(nt)  {}
-  // CGAL_Filtering (const double & d)	: value(d)  {}
-  // CGAL_Filtering (const CT & ct)	: value(ct) {}
+  CGAL_Filtered_exact (const NT & nt) : value(nt)  {}
+  // CGAL_Filtered_exact (const double & d)	: value(d)  {}
+  // CGAL_Filtered_exact (const CT & ct)	: value(ct) {}
 
-  typedef CGAL_Filtering<CT,ET> Fil;
+  typedef CGAL_Filtered_exact<CT,ET> Fil;
 
   // Check Stroustrup if it's ok for assignment/ctors.
   Fil& operator= (const Fil& fil) { value = fil.value; return *this; }
@@ -90,66 +90,70 @@ struct CGAL_Filtering
 // operator>>, operator<<.
 
 template <class CT, class ET>
-inline bool CGAL_is_valid    (const CGAL_Filtering<CT,ET>& fil)
+inline bool CGAL_is_valid    (const CGAL_Filtered_exact<CT,ET>& fil)
 { return CGAL_is_valid(fil.value); }
 
 template <class CT, class ET>
-inline bool CGAL_is_finite   (const CGAL_Filtering<CT,ET>& fil)
+inline bool CGAL_is_finite   (const CGAL_Filtered_exact<CT,ET>& fil)
 { return CGAL_is_finite(fil.value); }
 
 template <class CT, class ET>
-inline double CGAL_to_double (const CGAL_Filtering<CT,ET>& fil)
+inline double CGAL_to_double (const CGAL_Filtered_exact<CT,ET>& fil)
 { return CGAL_to_double(fil.value); }
 
 template <class CT, class ET>
-inline CGAL_Sign CGAL_sign (const CGAL_Filtering<CT,ET>& fil)
+inline CGAL_Sign CGAL_sign (const CGAL_Filtered_exact<CT,ET>& fil)
 { return CGAL_Sign(fil.value); }
 
 template <class CT, class ET>
-inline CGAL_Comparison_result CGAL_compare (const CGAL_Filtering<CT,ET>& fil,
-					    const CGAL_Filtering<CT,ET>& fil2)
+inline
+CGAL_Comparison_result CGAL_compare (	const CGAL_Filtered_exact<CT,ET>& fil,
+					const CGAL_Filtered_exact<CT,ET>& fil2)
 { return CGAL_compare(fil.value, fil2.value); }
 
 template <class CT, class ET>
-inline CGAL_Filtering<CT,ET> CGAL_abs (const CGAL_Filtering<CT,ET>& fil)
+inline
+CGAL_Filtered_exact<CT,ET> CGAL_abs (const CGAL_Filtered_exact<CT,ET>& fil)
 { return CGAL_abs(fil.value); }
 
 template <class CT, class ET>
-inline CGAL_Filtering<CT,ET> CGAL_min (const CGAL_Filtering<CT,ET>& fil,
-				       const CGAL_Filtering<CT,ET>& fil2)
+inline
+CGAL_Filtered_exact<CT,ET> CGAL_min (	const CGAL_Filtered_exact<CT,ET>& fil,
+					const CGAL_Filtered_exact<CT,ET>& fil2)
 { return CGAL_min(fil.value, fil2.value); }
 
 template <class CT, class ET>
-inline CGAL_Filtering<CT,ET> CGAL_max (const CGAL_Filtering<CT,ET>& fil,
-				       const CGAL_Filtering<CT,ET>& fil2)
+inline
+CGAL_Filtered_exact<CT,ET> CGAL_max (	const CGAL_Filtered_exact<CT,ET>& fil,
+					const CGAL_Filtered_exact<CT,ET>& fil2)
 { return CGAL_max(fil.value, fil2.value); }
 
 template <class CT, class ET>
-inline CGAL_io_Operator CGAL_io_tag(CGAL_Filtering<CT,ET> &fil)
+inline CGAL_io_Operator CGAL_io_tag(CGAL_Filtered_exact<CT,ET> &fil)
 { return CGAL_io_tag(fil.value); }
 
 template <class CT, class ET>
-inline CGAL_Number_tag CGAL_number_type_tag(CGAL_Filtering<CT,ET> &fil)
+inline CGAL_Number_tag CGAL_number_type_tag(CGAL_Filtered_exact<CT,ET> &fil)
 { return CGAL_number_type_tag(fil.value); }
 
 #ifndef CGAL_DENY_INEXACT_OPERATIONS_ON_FILTER
 template <class CT, class ET>
-inline CGAL_Filtering<CT,ET> sqrt (const CGAL_Filtering<CT,ET>& fil)
+inline CGAL_Filtered_exact<CT,ET> sqrt (const CGAL_Filtered_exact<CT,ET>& fil)
 { return sqrt(fil.value); }
 #endif // CGAL_DENY_INEXACT_OPERATIONS_ON_FILTER
 
 template <class CT, class ET>
-inline ostream& operator<<(ostream& os, const CGAL_Filtering<CT,ET>& d)
+inline ostream& operator<<(ostream& os, const CGAL_Filtered_exact<CT,ET>& d)
 { return os << d.value; }
 
 template <class CT, class ET>
-inline istream &operator>>(istream &is, const CGAL_Filtering<CT,ET>& d)
+inline istream &operator>>(istream &is, const CGAL_Filtered_exact<CT,ET>& d)
 { return is >> d.value; }
 
 
 // template <class ET>
   // template <class CT>
-    // ET CGAL_to_exact_type(const CT &);
+    // ET CGAL_convert_to(const CT &);
 
 // All exact types should reasonnably have a built-in exact conversion
 // from doubles ?  If not, it will fail, and you have to provide it.
@@ -158,33 +162,33 @@ inline istream &operator>>(istream &is, const CGAL_Filtering<CT,ET>& d)
 // ex: CGAL_Gmpz accepts it, but it's false !!!
 
 template <class ET, class CT>
-inline ET CGAL_to_exact_type (const CT & ct)
+inline ET CGAL_convert_to (const CT & ct)
 { return ET(ct); }
 
 // template <class ET>
-// inline ET CGAL_to_exact_type (const double & d)
+// inline ET CGAL_convert_to (const double & d)
 // { return ET(d); }
 
 // When CT == ET.
 template <class ET>
-inline ET CGAL_to_exact_type (const ET & e)
+inline ET CGAL_convert_to (const ET & e)
 { return e; }
 
 
 #ifdef CGAL_PREDICATES_ON_FTC2_H
-#include <CGAL/Filter/predicates_on_ftC2.h>
+#include <CGAL/Arithmetic_filter/predicates_on_ftC2.h>
 #endif
 
 #ifdef CGAL_PREDICATES_ON_FTC3_H
-#include <CGAL/Filter/predicates_on_ftC3.h>
+#include <CGAL/Arithmetic_filter/predicates_on_ftC3.h>
 #endif
 
 #ifdef CGAL_PREDICATES_ON_RTH2_H
-#include <CGAL/Filter/predicates_on_rtH2.h>
+#include <CGAL/Arithmetic_filter/predicates_on_rtH2.h>
 #endif
 
 #ifdef CGAL_PREDICATES_ON_RTH3_H
-#include <CGAL/Filter/predicates_on_rtH3.h>
+#include <CGAL/Arithmetic_filter/predicates_on_rtH3.h>
 #endif
 
-#endif // CGAL_FILTER_H
+#endif // CGAL_ARITHMETIC_FILTER_H
