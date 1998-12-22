6f0cf1d2f75e2d4fd5459f8316e44867031b500c
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1998-12-22 12:51:17 +0000

    - CGAL_square(double) doesn't exist yet.

--- Packages/Interval_arithmetic/include/CGAL/Interval_arithmetic.h
+++ Packages/Interval_arithmetic/include/CGAL/Interval_arithmetic.h
@@ -345,7 +345,10 @@ CGAL_square (const CGAL_Interval_nt_advanced& d)
 {
   if (d.inf<=0) return CGAL_Interval_nt_advanced(-(-d.inf*d.inf), d.sup*d.sup);
   if (d.sup<=0) return CGAL_Interval_nt_advanced(-(-d.sup*d.sup), d.inf*d.inf);
-  return CGAL_Interval_nt_advanced(0, CGAL_square(CGAL_max(d.inf, d.sup)));
+// Till CGAL_square(double) is not defined, this fails.
+// return CGAL_Interval_nt_advanced(0.0, CGAL_square(CGAL_max(d.inf, d.sup)));
+  return CGAL_Interval_nt_advanced(0.0, CGAL_max(d.inf, d.sup)
+	  				* CGAL_max(d.inf, d.sup));
 }
 
 inline
