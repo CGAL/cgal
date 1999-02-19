094ec0f8b0e6cb7d265092bad9b1935269518640
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1999-02-19 14:34:39 +0000

    - Replaced CGAL_NO_PRECONDITIONS by CGAL_NO_ASSERTIONS and commented out. - Added benchs for the new operators having one double as parameter. - Added CGAL_orientationC2 benchmark. - Added test of alignment (bad alignment produces unreliable benchmark,   especially on x86).

--- Packages/Interval_arithmetic/test/Interval_arithmetic/include/bench_generic.C
+++ Packages/Interval_arithmetic/test/Interval_arithmetic/include/bench_generic.C
@@ -1,7 +1,7 @@
 // Generic bench file for the IA package. include/bench_generic.C
 //  $Revision$
 //  $Date$
-// Written by Sylvain Pion, 1997/1998.
+// Written by Sylvain Pion, 1997-1999.
 
 // This file is included from tst[34].C, that do just a #define:
 // #define TESTED_TYPE CGAL_Interval_nt_advanced // For tst3.C
@@ -9,9 +9,10 @@
 
 #define CGAL_IA_NO_EXCEPTION
 #define CGAL_IA_NO_WARNINGS
-#define CGAL_NO_PRECONDITIONS
+// #define CGAL_NO_ASSERTIONS
 #include <CGAL/Timer.h>
 #include <CGAL/Interval_arithmetic.h>
+#include <CGAL/predicates_on_ftC2.h>
 
 typedef TESTED_TYPE IA;
 
@@ -23,11 +24,11 @@ void bench()
   int i;
   CGAL_Timer t;
   double dt;
-  const double dd = 1.0;
+  const double dd = 1.0000001;
   const IA a(0.12);
   // const IA b(2.1);
   const IA b(IA(21)/10);
-  IA c(1);
+  IA c(1), d(-5.0/3), e(-6.0/7), f(7.0/9);
 
 // egcs-1.1 + -O + not advanced n'affiche pas pareil....
 // De même que le snapshot du 19 octobre...  inquiétant.
@@ -84,6 +85,46 @@ void bench()
   for (i=0; i<loops; i++) { c = dd * c; }
   t.stop();
   cout << c << "\tdbl*ia\t" << t.time()-dt << endl;
+
+  dt = t.time(); t.start();
+  for (i=0; i<loops; i++) { c = c + dd; }
+  t.stop();
+  cout << c << "\tia+dbl\t" << t.time()-dt << endl;
+
+  dt = t.time(); t.start();
+  for (i=0; i<loops; i++) { c = dd + c; }
+  t.stop();
+  cout << c << "\tdbl+ia\t" << t.time()-dt << endl;
+
+  dt = t.time(); t.start();
+  for (i=0; i<loops; i++) { c = c - dd; }
+  t.stop();
+  cout << c << "\tia-dbl\t" << t.time()-dt << endl;
+
+  dt = t.time(); t.start();
+  for (i=0; i<loops; i++) { c = dd - c; }
+  t.stop();
+  cout << c << "\tdbl-ia\t" << t.time()-dt << endl;
+
+  dt = t.time(); t.start();
+  for (i=0; i<loops; i++) { c = dd/c; }
+  t.stop();
+  cout << c << "\td/ia\t" << t.time()-dt << endl;
+
+  dt = t.time(); t.start();
+  for (i=0; i<loops; i++) { c = c/dd; }
+  t.stop();
+  cout << c << "\tia/d\t" << t.time()-dt << endl;
+
+#if 1
+  cout << a<<b<<c<<d<<endl;
+  CGAL_Orientation o;
+  dt = t.time(); t.start();
+  for (i=0; i<loops; i++)
+    o = CGAL_orientationC2(a,b,c,d,e,f);
+  t.stop();
+  cout << (int)o << "\tori2\t" << t.time()-dt << endl;
+#endif
 }
 
 
@@ -95,9 +136,22 @@ int main()
 #else
   cout << "Benching the class CGAL_Interval_nt.\n";
 #endif
+  double d;
+  if ((((int) &d) & 7) != 0)
+    cout << "Benchmark might not be meaningful due to bad alignment" << endl;
 
   cout.precision(20);
   bench();
+
+  IA a=1, b=2;
+  (int) CGAL_sign(a);
+  (int) CGAL_compare(a,b);
+#if 0
+  // It's nice if it crashed, because c is not initialized.
+  IA c;
+  a = c+c;
+  cout << c << a << endl;
+#endif
 
 #ifdef ADVANCED
   CGAL_FPU_set_rounding_to_nearest();
