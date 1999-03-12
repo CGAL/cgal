167af57186fc0e9f7925bd6dff959cfe4634bb84
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1999-03-12 17:16:47 +0000

    - Initial version: contains the static data member "number_of_failures".

---
+++ Packages/Interval_arithmetic/src/Interval_arithmetic.C
@@ -0,0 +1,28 @@
+// ======================================================================
+//
+// Copyright (c) 1999 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+// 
+// release       : $CGAL_Revision: $
+// release_date  : $CGAL_Date: $
+// 
+// file          : src/Interval_arithmetic.C
+// revision      : $Revision$
+// revision_date : $Date$
+// package       : Interval Arithmetic
+// author(s)     : Sylvain Pion
+//
+// coordinator   : INRIA Sophia-Antipolis (<Mariette.Yvinec@sophia.inria.fr>)
+//
+// ======================================================================
+ 
+
+#include <CGAL/Interval_arithmetic.h>
+
+unsigned CGAL::Interval_nt_advanced::number_of_failures=0;
+
