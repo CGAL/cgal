765b30fa790e326214613182281d810273e196d5
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
1999-03-31 16:22:23 +0000

    reestablish #defined for test classes to make egcs compile

--- Packages/Triangulation_2/test/Triangulation_2/include/CGAL/_test_short_names_2.h
+++ Packages/Triangulation_2/test/Triangulation_2/include/CGAL/_test_short_names_2.h
@@ -3,12 +3,12 @@
 
 
 // Define shorter names to please linker (g++/egcs)
-//    #define Triangulation_test_point Ttp
-//    #define Triangulation_test_segment Tts
-//    #define Triangulation_test_line Ttl
-//    #define Triangulation_test_direction Ttd
-//    #define Triangulation_test_ray Ttr
-//    #define Triangulation_test_distance Ttdst
-//    #define _Triangulation_test_traits _Tttr
+   #define Triangulation_test_point Ttp
+   #define Triangulation_test_segment Tts
+   #define Triangulation_test_line Ttl
+   #define Triangulation_test_direction Ttd
+   #define Triangulation_test_ray Ttr
+   #define Triangulation_test_distance Ttdst
+   #define _Triangulation_test_traits _Tttr
 
 #endif //CGAL_TRIANGULATION_TEST_SHORT_NAMES_2_H
