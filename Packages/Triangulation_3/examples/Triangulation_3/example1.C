e824b1f3f6fb659383f534faf36fc8e0b8e15d16
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
1999-06-14 13:04:59 +0000

    *** empty log message ***

---
+++ Packages/Triangulation_3/examples/Triangulation_3/example1.C
@@ -0,0 +1,122 @@
+// ============================================================================
+//
+// Copyright (c) 1998-1999 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       :
+// release_date  :
+//
+// file          : example/Triangulation3/example1.C
+// revision      : $Revision$
+// author(s)     : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
+//
+// coordinator   : INRIA Sophia Antipolis (Mariette Yvinec)
+//
+// ============================================================================
+#include <CGAL/basic.h>
+
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <strstream.h>
+
+#include <list>
+#include <vector>
+
+#include <CGAL/triangulation_assertions.h>
+
+#include <CGAL/Cartesian.h>
+#include <CGAL/Point_3.h>
+
+#include <CGAL/Triangulation_iterators_3.h>
+#include <CGAL/Triangulation_circulators_3.h>
+#include <CGAL/Triangulation_cell_base_3.h>
+#include <CGAL/Triangulation_vertex_base_3.h>
+#include <CGAL/Triangulation_data_structure_3.h>
+#include <CGAL/Triangulation_geom_traits_3.h>
+#include <CGAL/Triangulation_3.h>
+//#include <CGAL/Delaunay_triangulation_3.h>
+
+#include <CGAL/IO/Geomview_stream.h>
+
+typedef CGAL::Cartesian<double>  Rep;
+
+typedef CGAL::Triangulation_geom_traits_3<Rep> Gt;
+typedef CGAL::Triangulation_vertex_base_3<Gt> Vb;
+typedef CGAL::Triangulation_cell_base_3<Gt>  Cb;
+
+typedef CGAL::Triangulation_data_structure_3<Vb,Cb> TDS;
+typedef CGAL::Triangulation_3<Gt,TDS> Triangulation;
+//typedef CGAL::Delaunay_triangulation_3<Gt,TDS> Delaunay;
+
+//typedef CGAL::Triangulation_vertex_iterator_3<Gt,TDS> Vertex_iterator;
+//typedef CGAL::Triangulation_edge_iterator_3<Gt,TDS> Edge_iterator;
+//typedef CGAL::Triangulation_cell_iterator_3<Gt,TDS> Cell_iterator;
+//typedef CGAL::Triangulation_facet_iterator_3<Gt,TDS> Facet_iterator;
+//typedef CGAL::Triangulation_cell_circulator_3<Gt,TDS> Cell_circulator;
+
+//typedef typename Triangulation::Cell Cell;
+//typedef typename Triangulation::Vertex Vertex;
+typedef typename Triangulation::Cell_handle Cell_handle;
+typedef typename Triangulation::Vertex_handle Vertex_handle;
+typedef typename Triangulation::Locate_type Locate_type;
+
+typedef Gt::Point Point;
+//typedef CGAL::Point_3<Rep>  Point;
+
+int main(int argc, char* argv[])
+{
+
+  Triangulation T;
+
+  // insertion from a list :
+  std::list<Point> L;
+  L.push_front(Point(0,0,0));
+  L.push_front(Point(1,0,0));
+  L.push_front(Point(0,1,0));
+
+  int n = T.insert(L.begin(), L.end());
+
+  // isertion from a vector :
+  std::vector<Point> V(3);
+  V[0] = Point(0,0,1);
+  V[1] = Point(1,1,1);
+  V[2] = Point(2,2,2);
+
+  n = n + T.insert(V.begin(), V.end());
+
+  // 6 points have been inserted :
+  //  CGAL::CGAL_triangulation_assertion( n == 6 );
+  assert( n == 6 );
+
+  // checking validity of T :
+  assert( T.is_valid(false) );
+
+  Locate_type lt;
+  int li, lj;
+  Point p(0,0,0);
+  Cell_handle c = T.locate(p, lt, li, lj);
+  // p is the vertex of c of index li :
+  assert( lt == Triangulation::VERTEX );
+  assert(  c->vertex(li)->point() == p );
+
+  Vertex_handle v = c->vertex( (li+1)&3 );
+  // v is another vertex of c
+  Cell_handle nc = c->neighbor(li);
+  // nc = neighbor of c opposite to the vertex associated with p
+  // nc must have vertex v :
+  int nli;
+  assert(  nc->has_vertex( v, nli ) );
+  // nli is the index of v in nc
+
+  std::ofstream oFileT("data/output",ios::out);
+  // writing file data/output; 
+  oFileT << T; 
+
+  return 1;
+}
