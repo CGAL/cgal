e001c0ae045277dc46f824884fd6015fad92b81b
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1999-06-27 15:06:52 +0000

    - Big clean up. - No more *ASSEMBLY* macros. - FPU_[sg]et_cw only use the CGAL_IA_[SG]ETCW() macros.

--- Packages/Interval_arithmetic/include/CGAL/Interval_arithmetic/_FPU.h
+++ Packages/Interval_arithmetic/include/CGAL/Interval_arithmetic/_FPU.h
@@ -27,43 +27,46 @@
 // This file specifies some platform dependant functions, regarding the FPU
 // directed rounding modes.  There is only support for double precision.
 
-
 // Some useful constants
 
 #define CGAL_IA_MIN_DOUBLE (5e-324) // subnormal
 #define CGAL_IA_MAX_DOUBLE (1.7976931348623157081e+308)
 
-
-#if defined(__osf__)
-#undef __osf
-#define __osf
-#endif
+#ifdef __i386__
+// This is compulsory to have this to force the Intel processor to output a
+// real double, and not Intel's doubles, because even when precision is
+// explicitely set to "double", the exponent still has 15 bits, and it bugs
+// when some values are in this range.
+// The other possible workaround is to use intervals of "long doubles"
+// directly, but I think it would be much slower.
+#define CGAL_IA_FORCE_TO_DOUBLE(x) ({ volatile double y = (x); y; })
+#else
+#define CGAL_IA_FORCE_TO_DOUBLE(x) (x)
+#endif __i386__
 
 #if (	!defined(__i386__)     && \
+	!defined(__mips__)     && \
 	!defined(__sparc__)    && \
 	!defined(__alpha__)    && \
-	!defined(__mips__)     && \
 	!defined(__powerpc__)  && \
+	!defined(__SUNPRO_CC)  && \
 	!defined(__sgi)        && \
 	!defined(_MSC_VER) )
 #error "Architecture not supported."
-#endif
-
-#if defined (__GNUC__) && ! defined (CGAL_IA_DONT_USE_ASSEMBLY)
-#define CGAL_IA_USE_ASSEMBLY
 #endif
 
 #ifdef __linux__
 #include <fpu_control.h>
 #endif
 
-#ifndef CGAL_IA_USE_ASSEMBLY
-#ifdef __sun__
+#ifdef __SUNPRO_CC
 #include <ieeefp.h>
-#endif // __sun__
-#ifdef __osf
+#endif
+
+#if defined(__osf) || defined (__osf__)
 #include <float.h>
-#endif // __osf
+#endif
+
 #ifdef __sgi
     // The 3 C functions do not work on IRIX 6.5 !!!!!
     // So we use precompiled (by gcc) binaries linked into libCGAL.
@@ -73,27 +76,13 @@ extern "C" {
   int  CGAL_workaround_IRIX_get_FPU_cw (void);
 }
 #endif // __sgi
-#endif // CGAL_IA_USE_ASSEMBLY
 
 CGAL_BEGIN_NAMESPACE
 
-#define CGAL_IA_FORCE_TO_DOUBLE(x) (x)
-
 #ifdef __i386__
-
-// This is compulsory to have this to force the Intel processor to output a
-// real double, and not Intel's doubles, because even when precision is
-// explicitely set to "double", the exponent still has 15 bits, and it bugs
-// when some values are in this range.
-// The other possible workaround is to use intervals of "long doubles"
-// directly, but I think it would be much slower.
-#undef CGAL_IA_FORCE_TO_DOUBLE
-#define CGAL_IA_FORCE_TO_DOUBLE(x) ({ volatile double y = (x); y; })
-
-#ifdef CGAL_IA_USE_ASSEMBLY
+// The GNU libc version (cf powerpc) is nicer, but doesn't work on libc 5 :(
 #define CGAL_IA_SETFPCW(CW) asm volatile ("fldcw %0" : :"m" (CW))
 #define CGAL_IA_GETFPCW(CW) asm volatile ("fstcw %0" : "=m" (CW))
-#endif
 typedef unsigned short FPU_CW_t;
 enum {  //               rounding | def. mask
     FPU_cw_near = _FPU_RC_NEAREST | 0x127f,
@@ -103,8 +92,19 @@ enum {  //               rounding | def. mask
 };
 #endif // __i386__
 
+#ifdef __powerpc__
+#define CGAL_IA_SETFPCW(CW) _FPU_SETCW(CW)
+#define CGAL_IA_GETFPCW(CW) _FPU_GETCW(CW)
+typedef fpu_control_t FPU_CW_t;
+enum {         // rounding        | def.mask
+    FPU_cw_zero = _FPU_RC_ZERO    | _FPU_DEFAULT,
+    FPU_cw_near = _FPU_RC_NEAREST | _FPU_DEFAULT,
+    FPU_cw_up   = _FPU_RC_UP      | _FPU_DEFAULT,
+    FPU_cw_down = _FPU_RC_DOWN    | _FPU_DEFAULT
+};
+#endif // __powerpc__
+
 #ifdef __sparc__
-#ifdef CGAL_IA_USE_ASSEMBLY
 #define CGAL_IA_SETFPCW(CW) asm volatile ("ld %0,%%fsr" : :"m" (CW))
 #define CGAL_IA_GETFPCW(CW) asm volatile ("st %%fsr,%0" : "=m" (CW))
 typedef unsigned int FPU_CW_t;
@@ -114,22 +114,23 @@ enum {  //        rounding   | precision  | def.mask
     FPU_cw_up   = 0x80000000 | 0x20000000 | 0x1f,
     FPU_cw_down = 0xc0000000 | 0x20000000 | 0x1f
 };
-#else
-typedef unsigned int FPU_CW_t; // fp_rnd
+#endif // __sparc__
+
+#ifdef __SUNPRO_CC
+#define CGAL_IA_GETFPCW(CW) CW = fpgetround()
+#define CGAL_IA_SETFPCW(CW) fpsetround(fp_rnd(CW))
+typedef unsigned int FPU_CW_t;
 enum {
     FPU_cw_near = FP_RN,
     FPU_cw_zero = FP_RZ,
     FPU_cw_up   = FP_RP,
     FPU_cw_down = FP_RM
 };
-#endif
-#endif // __sparc__
+#endif // __SUNPRO_CC
 
-#if defined(__mips__) || defined(__sgi)
-#ifdef CGAL_IA_USE_ASSEMBLY
-#define CGAL_IA_SETFPCW(CW) asm volatile ("ctc1 %0,$31" : :"r" (CW))
-#define CGAL_IA_GETFPCW(CW) asm volatile ("cfc1 %0,$31" : "=r" (CW))
-#endif // CGAL_IA_USE_ASSEMBLY
+#ifdef __sgi
+#define CGAL_IA_GETFPCW(CW) CW = CGAL_workaround_IRIX_get_FPU_cw()
+#define CGAL_IA_SETFPCW(CW) CGAL_workaround_IRIX_set_FPU_cw(CW)
 typedef unsigned int FPU_CW_t;
 enum {
     FPU_cw_near = 0x0,
@@ -137,10 +138,21 @@ enum {
     FPU_cw_up   = 0x2,
     FPU_cw_down = 0x3
 };
-#endif // __mips__ || __sgi
+#endif // __sgi
+
+#ifdef __mips__
+#define CGAL_IA_SETFPCW(CW) asm volatile ("ctc1 %0,$31" : :"r" (CW))
+#define CGAL_IA_GETFPCW(CW) asm volatile ("cfc1 %0,$31" : "=r" (CW))
+typedef unsigned int FPU_CW_t;
+enum {
+    FPU_cw_near = 0x0,
+    FPU_cw_zero = 0x1,
+    FPU_cw_up   = 0x2,
+    FPU_cw_down = 0x3
+};
+#endif // __mips__
 
 #ifdef __alpha__ // This one is not really supported [yet].
-#ifdef CGAL_IA_USE_ASSEMBLY
 #define CGAL_IA_SETFPCW(CW) asm volatile ("mt_fpcr %0; excb" : :"f" (CW))
 #define CGAL_IA_GETFPCW(CW) asm volatile ("excb; mf_fpcr %0" : "=f" (CW))
 typedef unsigned long FPU_CW_t;
@@ -151,7 +163,11 @@ enum { //         rounding
     FPU_cw_up   = 0x0c00000000000000UL,
     FPU_cw_down = 0x0400000000000000UL
 };
-#else
+#endif // __alpha__
+
+#if defined(__osf) || defined(__osf__)  // Not yet supported.
+#define CGAL_IA_GETFPCW(CW) CW = read_rnd()
+#define CGAL_IA_SETFPCW(CW) write_rnd(CW)
 typedef unsigned int FPU_CW_t;
 enum {
     FPU_cw_zero = FP_RND_RZ,
@@ -159,10 +175,11 @@ enum {
     FPU_cw_up   = FP_RND_RP,
     FPU_cw_down = FP_RND_RM
 };
-#endif
-#endif // __alpha__
+#endif // __osf || __osf__
 
-#ifdef _MSC_VER
+#ifdef _MSC_VER // Not yet supported.
+// Which header file ?
+// define the GET/SET macros.
 enum float_round_style {
   round_indeterminate = -1,
   round_toward_zero = 0,
@@ -190,74 +207,18 @@ enum {
 #endif //_MSC_VER
 
 
-// This one should be easily portable to any platform using GNU libc.
-#ifdef __powerpc__
-#ifdef CGAL_IA_USE_ASSEMBLY
-#define CGAL_IA_SETFPCW(CW) _FPU_SETCW(CW)
-#define CGAL_IA_GETFPCW(CW) _FPU_GETCW(CW)
-#endif
-typedef fpu_control_t FPU_CW_t;
-enum {         // rounding        | def.mask
-    FPU_cw_zero = _FPU_RC_ZERO    | _FPU_DEFAULT,
-    FPU_cw_near = _FPU_RC_NEAREST | _FPU_DEFAULT,
-    FPU_cw_up   = _FPU_RC_UP      | _FPU_DEFAULT,
-    FPU_cw_down = _FPU_RC_DOWN    | _FPU_DEFAULT
-};
-#endif // __powerpc__
-
-
-// Main functions: FPU_get_cw() and FPU_set_cw();
+// User interface:
 
 inline FPU_CW_t FPU_get_cw (void)
 {
     FPU_CW_t cw;
-#ifdef CGAL_IA_USE_ASSEMBLY
     CGAL_IA_GETFPCW(cw);
-#else
-
-#ifdef __linux__
-#error "It seems there's no C function in libc5 !!!"
-#endif
-
-#ifdef __sun__
-    cw = fpgetround();
-#endif
-
-#ifdef __sgi
-    cw = CGAL_workaround_IRIX_get_FPU_cw();
-#endif
-
-#ifdef __osf
-    cw = read_rnd();
-#endif
-
-#endif // CGAL_IA_USE_ASSEMBLY
     return cw;
 }
 
 inline void FPU_set_cw (FPU_CW_t cw)
 {
-#ifdef CGAL_IA_USE_ASSEMBLY
     CGAL_IA_SETFPCW(cw);
-#else
-
-#ifdef __linux__
-    __setfpucw(cw);
-#endif
-
-#ifdef __sun__
-    fpsetround(fp_rnd(cw));
-#endif
-
-#ifdef __sgi
-    CGAL_workaround_IRIX_set_FPU_cw(cw);
-#endif
-
-#ifdef __osf
-    write_rnd(cw);
-#endif
-
-#endif // CGAL_IA_USE_ASSEMBLY
 }
 
 CGAL_END_NAMESPACE
