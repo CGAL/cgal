ca6b5096b338715205b7da4d30eab1f680453924
Geert-Jan Giezeman <geert@cs.uu.nl>
1999-07-01 08:18:34 +0000

    *** empty log message ***

--- Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
+++ Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
@@ -1,21 +1,14 @@
-#ifndef CGAL_MINMAX_FIX_H
-#define CGAL_MINMAX_FIX_H
+#ifndef CGAL_MSVC_STANDARD_HEADER_FIXES_H
+#define CGAL_MSVC_STANDARD_HEADER_FIXES_H
 
 #include <cmath>
-#include <cstdlib>
-#include <cstddef>
-
-#ifdef atoi
-#undef atoi
-#endif
-
 namespace std {
+	using ::fabs;
+	using ::sqrt;
+	using ::log;
+};
 
-using ::fabs;
-using ::atoi;
-using ::size_t;
-using ::sqrt;
-
+namespace std{
 template <class T>
 inline const T& min(const T& a, const T&b)
 { return (a<b) ? a : b;}
@@ -30,5 +23,32 @@ inline const T& max(const T& a, const T&b)
 
 }
 
-#endif
+};
 
+#include <cstddef>
+namespace std{
+using ::size_t;
+using ::ptrdiff_t;
+};
+
+#include <cstdlib>
+namespace std{
+using ::atoi;
+};
+
+
+namespace std{
+template < class T, class Dist> struct input_iterator: public iterator<input_iterator_tag,T,Dist> {};
+template <class T, class Dist> struct forward_iterator : public iterator<forward_iterator_tag,T,Dist> {};
+template <class T, class Dist> struct bidirectional_iterator : public iterator<bidirectional_iterator_tag,T,Dist> {};
+template <class T, class Dist> struct random_access_iterator : public iterator<random_access_iterator_tag,T,Dist> {};
+struct output_iterator : public iterator<output_iterator_tag,void,void> {};
+};
+
+#include <cstring>
+namespace std{
+using ::strcat;
+using ::strcpy;
+};
+
+#endif // CGAL_MSVC_STANDARD_HEADER_FIXES_H
