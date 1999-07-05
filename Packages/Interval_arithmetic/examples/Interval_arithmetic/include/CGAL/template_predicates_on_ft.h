1ed68e4e6e74f5fa61e1e540d3a929d309746b4d
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1999-07-05 14:24:34 +0000

    - Removed CGAL stuff, do as if the predicate comes from outside CGAL.

--- Packages/Interval_arithmetic/examples/Interval_arithmetic/include/CGAL/template_predicates_on_ft.h
+++ Packages/Interval_arithmetic/examples/Interval_arithmetic/include/CGAL/template_predicates_on_ft.h
@@ -1,30 +1,23 @@
-// This is an extract from include/CGAL/predicates_on_ftC2.h
-// It's a template predicate.
+// It's a template predicate, given as an example to make your own predicates.
 
-#ifndef CGAL_TEMPLATE_PREDICATES_ON_FT_H
-#define CGAL_TEMPLATE_PREDICATES_ON_FT_H
-
-CGAL_BEGIN_NAMESPACE
+#ifndef TEMPLATE_PREDICATES_ON_FT_H
+#define TEMPLATE_PREDICATES_ON_FT_H
 
 template < class FT >
-CGAL_KERNEL_LARGE_INLINE
-Comparison_result
-compare_xC2(const FT &px,  const FT &py,
+CGAL::Comparison_result
+compare_xC2(const FT &px,
             const FT &l1a, const FT &l1b, const FT &l1c,
             const FT &l2a, const FT &l2b, const FT &l2c)
 {
-  Sign sign1 = sign(det2x2_by_formula(l1a, l1b, l2a, l2b));
-  Sign sign2 = sign(det3x3_by_formula(l1a, l1b, l1c,
-                                      l2a, l2b, l2c,
-                                      -FT(1), FT(0), px));
-  CGAL_kernel_assertion( sign1 != 0 );
-  return Comparison_result (sign1 * sign2);
+    CGAL::Sign sign1 = CGAL::sign(CGAL::det2x2_by_formula(l1a, l1b, l2a, l2b));
+    CGAL::Sign sign2 = CGAL::sign(CGAL::det3x3_by_formula(l1a, l1b, l1c,
+                                                          l2a, l2b, l2c,
+                                                          -FT(1), FT(0), px));
+    return CGAL::Comparison_result (sign1 * sign2);
 }
 
-CGAL_END_NAMESPACE
-
 #ifdef CGAL_ARITHMETIC_FILTER_H
-#include <CGAL/Arithmetic_filter/template_predicates_on_ft.h>
+#include <Arithmetic_filter/template_predicates_on_ft.h>
 #endif
 
-#endif // CGAL_TEMPLATE_PREDICATES_ON_FT_H
+#endif // TEMPLATE_PREDICATES_ON_FT_H
