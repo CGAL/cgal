a38d5799d66045854acf8dcc9ae1118f37bea80e
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1999-07-07 12:52:49 +0000

    - Example of how to use cl_I.

---
+++ Old_Packages/CLN/examples/CLN/example_cl_I.C
@@ -0,0 +1,23 @@
+
+#ifndef CGAL_USE_CLN
+#  error You need to have CLN installed in order to run this example
+#endif
+
+#include <CGAL/basic.h>
+#include <iostream>
+
+#include <CGAL/CLN/cl_integer.h>
+
+#include <cl_io.h>
+#include <cl_integer_io.h>
+
+int
+main ()
+{
+    cl_I b = "31";
+    cl_I p = b * 75;
+    bool ok = (p==2325) && (CGAL::compare(p,cl_I(31*75))==CGAL::EQUAL);
+    std::cout << (ok ? "ok" : "ERROR") << std::endl;
+    std::cout << p << " == " << 31*75 << endl;
+    return (ok) ? 0 : -1;
+}
