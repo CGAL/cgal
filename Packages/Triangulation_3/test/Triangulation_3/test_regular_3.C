361b6ea686f3467ad7a5df93509b1ece7fe3ecf9
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
1999-07-22 13:35:16 +0000

    cleaner version + dim 1

--- Packages/Triangulation_3/test/Triangulation_3/test_regular_3.C
+++ Packages/Triangulation_3/test/Triangulation_3/test_regular_3.C
@@ -26,17 +26,9 @@
 #include <list>
 #include <vector>
 
-#include <CGAL/_test_types.h>
 #include <CGAL/triple.h>
 #include <CGAL/Triangulation_cell_base_3.h>
 #include <CGAL/Triangulation_vertex_base_3.h>
-
-#include <CGAL/Regular_triangulation_euclidean_traits_3.h>
-
-#include <CGAL/Triangulation_data_structure_3.h>
-
-#include <CGAL/Triangulation_3.h>
-#include <CGAL/Regular_triangulation_3.h>
 
 #ifdef CGAL_USE_LEDA
 #include <CGAL/leda_integer.h>
@@ -50,9 +42,24 @@ typedef CGAL::Gmpz my_NT;
 typedef double my_NT;
 #endif
 #endif
-// #endif
 
+#ifndef CGAL_CARTESIAN_H
 #include <CGAL/Cartesian.h>
+#endif // CGAL_CARTESIAN_H
+#ifndef CGAL_HOMOGENEOUS_H
+#include <CGAL/Homogeneous.h>
+#endif // CGAL_HOMOGENEOUS_H
+
+#include <CGAL/Regular_triangulation_euclidean_traits_3.h>
+
+#include <CGAL/Triangulation_data_structure_3.h>
+
+#include <CGAL/Triangulation_3.h>
+#include <CGAL/Regular_triangulation_3.h>
+
+#include <CGAL/_test_types.C>
+#include <CGAL/_test_cls_regular_3.C>
+
 
 typedef CGAL::Cartesian<my_NT> Test_rep_cartesian;
 typedef CGAL::Homogeneous<my_NT> Test_rep_homogeneous;
@@ -72,123 +79,7 @@ int main()
   typedef CGAL::Triangulation_data_structure_3<Vb,Fb>               Tds;
   typedef CGAL::Regular_triangulation_3<traits,Tds>                Cls;
 
-  typedef traits::Bare_point Point;
-  typedef traits::Weighted_point Weighted_point;
-
-  typedef list<Weighted_point>                        list_point;
-
-  //  _test_cls_triangulation_3( Cls() );
-
-  // temporary version
-
-  std::cout << " test dimension 2 " << std::endl;
-  std::cout << " number of inserted points : " ;
-  Cls T2;
-  // 3 particular first points 
-  T2.insert( Weighted_point(Point(0,0,0),1) );
-  T2.insert( Weighted_point(Point(1,0,0),1) );
-  T2.insert( Weighted_point(Point(0,1,0),1) );
-  int count = 3 ;
-  int n,m;
-  int px=1, py=1;
-  int qx=-1, qy=2;
-  for (m=0; m<10; m++)
-    for (n=0; n<10; n++) {
-      T2.insert( Weighted_point( Point(m*px+n*qx, m*py+n*qy, 0), 1 ) );
-      count++;
-      if (count <10)
-	std::cout << count << '\b' ;
-      else
-	if (count < 100)
-	  std::cout << count << '\b' << '\b' ;
-	else
-	  std::cout << count << '\b' << '\b' << '\b' ;
-      std::cout.flush();
-    }
-  for (m=10; m<20; m++)
-    for (n=0; n<10; n++) {
-      T2.insert( Weighted_point( Point(m*px+n*qx, m*py+n*qy, 0), -1 ) );
-      count++;
-      if (count <10)
-	std::cout << count << '\b' ;
-      else
-	if (count < 100)
-	  std::cout << count << '\b' << '\b' ;
-	else
-	  std::cout << count << '\b' << '\b' << '\b' ;
-      std::cout.flush();
-    }
-  for (m=0; m<10; m++)
-    for (n=10; n<20; n++) {
-      T2.insert( Weighted_point( Point(m*px+n*qx, m*py+n*qy, 0), -2 ) );
-      count++;
-      if (count <10)
-	std::cout << count << '\b' ;
-      else
-	if (count < 100)
-	  std::cout << count << '\b' << '\b' ;
-	else
-	  std::cout << count << '\b' << '\b' << '\b' ;
-      std::cout.flush();
-    }
-  for (m=10; m<20; m++)
-    for (n=10; n<20; n++) {
-      T2.insert( Weighted_point( Point(m*px+n*qx, m*py+n*qy, 0), 5 ) );
-      count++;
-      if (count <10)
-	std::cout << count << '\b' ;
-      else
-	if (count < 100)
-	  std::cout << count << '\b' << '\b' ;
-	else
-	  std::cout << count << '\b' << '\b' << '\b' ;
-      std::cout.flush();
-    }
- 
-  std::cout << std::endl << " number of vertices : " 
-	    << T2.number_of_vertices() << std::endl;
-  assert( T2.dimension()==2 );
-  assert( T2.is_valid() );
-
- // dimension 3
-  std::cout << " test dimension 3" << std::endl;
-  Cls T;
-  // 3 particular first points 
-  T.insert( Weighted_point(Point(0,0,0),1) );
-  T.insert( Weighted_point(Point(1,0,0),1) );
-  T.insert( Weighted_point(Point(0,1,0),1) );
-
-  list_point lp;
-  int a, b, d;
-  for (a=0;a!=10;a++)
-    for (b=0;b!=10;b++)
-      for (d=0;d!=10;d++)
-	lp.push_back(Weighted_point( Point(a*b-d*a + (a-b)*10 +a ,
-					   a-b+d +5*b,
-					   a*a-d*d+b),
-				     a*b-a*d) );
-  list_point::iterator it;
-  count = 0 ;
-  std::cout << " number of inserted points : " ;
-  for (it=lp.begin(); it!=lp.end();it++){
-    count++;
-    T.insert(*it);
-    if (count <10)
-      std::cout << count << '\b' ;
-    else
-      if (count < 100)
-        std::cout << count << '\b' << '\b' ;
-      else 
-        if (count < 1000)
-          std::cout << count << '\b' << '\b' << '\b' ;
-        else
-	  std::cout << count << std::endl;
-    std::cout.flush();
-  }
-
-  std::cout << " number of vertices : " << T.number_of_vertices() << std::endl;
-  assert(T.is_valid(true));
-  assert(T.dimension()==3);
+  _test_cls_regular_3( Cls() );
 
   return 0;
 }
