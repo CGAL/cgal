aba08a3e70c7b20b28446b1116f3697b4dea9772
Geert-Jan Giezeman <geert@cs.uu.nl>
1999-08-03 10:59:49 +0000

    formatting

--- Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
+++ Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
@@ -57,11 +57,24 @@ using ::atoi;
 
 #include <iterator>
 namespace std{
-template < class T, class Dist> struct input_iterator: public iterator<input_iterator_tag,T,Dist> {};
-template <class T, class Dist> struct forward_iterator : public iterator<forward_iterator_tag,T,Dist> {};
-template <class T, class Dist> struct bidirectional_iterator : public iterator<bidirectional_iterator_tag,T,Dist> {};
-template <class T, class Dist> struct random_access_iterator : public iterator<random_access_iterator_tag,T,Dist> {};
-struct output_iterator : public iterator<output_iterator_tag,void,void> {};
+template < class T, class Dist>
+struct input_iterator
+: public iterator<input_iterator_tag,T,Dist> {};
+
+template <class T, class Dist>
+struct forward_iterator
+: public iterator<forward_iterator_tag,T,Dist> {};
+
+template <class T, class Dist>
+struct bidirectional_iterator
+: public iterator<bidirectional_iterator_tag,T,Dist> {};
+
+template <class T, class Dist>
+struct random_access_iterator
+: public iterator<random_access_iterator_tag,T,Dist> {};
+
+struct output_iterator
+: public iterator<output_iterator_tag,void,void> {};
 }
 
 #include <cstring>
