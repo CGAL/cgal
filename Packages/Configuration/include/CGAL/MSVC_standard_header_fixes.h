c5a15e1019a0852fccf7544488819ce9116ae5b9
Geert-Jan Giezeman <geert@cs.uu.nl>
1999-08-03 09:43:10 +0000

    Added header

--- Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
+++ Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
@@ -1,3 +1,20 @@
+// ============================================================================
+//
+// Copyright (c) 1999 The CGAL Consortium
+//
+// ----------------------------------------------------------------------------
+//
+// release       :
+// release_date  :
+//
+// file          : include/CGAL/MSVC_standard_header_fixes.h
+// chapter       : $CGAL_Chapter: Configuration $
+//
+// author(s)     : Geert-Jan Giezeman <geert@cs.uu.nl>
+//
+// coordinator   : Utrecht University
+// ============================================================================
+
 #ifndef CGAL_MSVC_STANDARD_HEADER_FIXES_H
 #define CGAL_MSVC_STANDARD_HEADER_FIXES_H
 
