0b752883923e2aa22688df8064afc1b360cfa120
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
1999-08-04 06:47:57 +0000

    code of _test_fct_delaunay_duality.C incorporated in _test_cls_delaunay_triangulation_2.C

--- Packages/Triangulation_2/test/Triangulation_2/include/CGAL/_test_cls_delaunay_triangulation_2.C
+++ Packages/Triangulation_2/test/Triangulation_2/include/CGAL/_test_cls_delaunay_triangulation_2.C
@@ -21,14 +21,9 @@
 // ============================================================================
 
 #include <iostream>
-#include <fstream>
-
-#include <utility>
-#include <list>
 #include <vector>
 
-//#include <CGAL/_test_fct_delaunay_duality.C>
-//#include <CGAL/_test_cls_triangulation_2.C>
+#include <CGAL/_test_cls_triangulation_2.C>
 
 template <class Del>
 void
@@ -39,7 +34,7 @@ _test_cls_delaunay_triangulation_2( const Del & )
 
   /***********************/
   /***** SUBCLASSES ******/
-     //_test_cls_triangulation_2( Delaunay() );
+   _test_cls_triangulation_2( Delaunay() );
 
   // Constructors
   cout << "    constructors(3)" << endl;
