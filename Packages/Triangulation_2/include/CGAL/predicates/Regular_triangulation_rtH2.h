1bbc7378cef1ec2d24af0e16078adcd488ecf521
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
1999-08-16 15:56:52 +0000

    retablish the file which had been scrtched with Regular_triangulation_ftC2.h

--- Packages/Triangulation_2/include/CGAL/predicates/Regular_triangulation_rtH2.h
+++ Packages/Triangulation_2/include/CGAL/predicates/Regular_triangulation_rtH2.h
@@ -11,8 +11,7 @@
 // release       :
 // release_date  :
 //
-// file          : include/CGAL/predicates/Regular_triangulation_ftC2.h
-// package       : Triangulation
+// file          : include/CGAL/predicates/Regular_triangulation_rtH2.h
 // revision      : $Revision$
 // revision_date : $Date$
 // author(s)     : Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
@@ -22,70 +21,92 @@
 //
 // ============================================================================
 
-#ifndef CGAL_REGULAR_TRIANGULATION_FTC2_H
-#define CGAL_REGULAR_TRIANGULATION_FTC2_H
+#ifndef CGAL_REGULAR_TRIANGULATION_RTH2_H
+#define CGAL_REGULAR_TRIANGULATION_RTH2_H
 
-// This file contains the low level cartesian predicates
+// This file contains the low level homogeneous predicates
 // used by the 2D regular triangulation.
 
 CGAL_BEGIN_NAMESPACE
 
-template <class FT>
+template <class RT>
 Oriented_side
-power_testC2( const FT &px, const FT &py, const FT &pwt,
-              const FT &qx, const FT &qy, const FT &qwt,
-              const FT &rx, const FT &ry, const FT &rwt,
-              const FT &tx, const FT &ty, const FT &twt)
+power_testH2( const RT &phx, const RT &phy, const RT &phw, const RT &pwt,
+              const RT &qhx, const RT &qhy, const RT &qhw, const RT &qwt,
+              const RT &rhx, const RT &rhy, const RT &rhw, const RT &rwt,
+              const RT &thx, const RT &thy, const RT &thw, const RT &twt)
 {
-    // Note: maybe this can be further optimized like the usual in_circle() ?
+    RT dphx = phx*phw;
+    RT dphy = phy*phw;
+    RT dphw = square(phw);
+    RT dpz = square(phx) + square(phy) - pwt*dphw;
 
-    // We translate the 4 points so that T becomes the origin.
-    FT dpx = px - tx;
-    FT dpy = py - ty;
-    FT dpz = square(dpx) + square(dpy) - pwt + twt;
-    FT dqx = qx - tx;
-    FT dqy = qy - ty;
-    FT dqz = square(dqx) + square(dqy) - qwt + twt;
-    FT drx = rx - tx;
-    FT dry = ry - ty;
-    FT drz = square(drx) + square(dry) - rwt + twt;
+    RT dqhx = qhx*qhw;
+    RT dqhy = qhy*qhw;
+    RT dqhw = square(qhw);
+    RT dqz = square(qhx) + square(qhy) - qwt*dqhw;
 
-    return Oriented_side(sign_of_determinant3x3(dpx, dpy, dpz,
-                                                dqx, dqy, dqz,
-                                                drx, dry, drz));
+    RT drhx = rhx*rhw;
+    RT drhy = rhy*rhw;
+    RT drhw = square(rhw);
+    RT drz = square(rhx) + square(rhy) - rwt*drhw;
+
+    RT dthx = thx*thw;
+    RT dthy = thy*thw;
+    RT dthw = square(thw);
+    RT dtz = square(thx) + square(thy) - twt*dthw;
+
+    return Oriented_side(sign_of_determinant4x4(dphx, dphy, dpz, dphw,
+	                                        dqhx, dqhy, dqz, dqhw,
+	                                        drhx, drhy, drz, drhw,
+	                                        dthx, dthy, dtz, dthw));
 }
 
 
-template <class FT>
+template <class RT>
 Oriented_side
-power_testC2( const FT &px, const FT &py, const FT &pwt,
-	      const FT &qx, const FT &qy, const FT &qwt,
-	      const FT &tx, const FT &ty, const FT &twt)
+power_testH2( const RT &phx, const RT &phy, const RT &phw, const RT &pwt,
+              const RT &qhx, const RT &qhy, const RT &qhw, const RT &qwt,
+              const RT &thx, const RT &thy, const RT &thw, const RT &twt)
 {
-    // Same translation as above.
-    FT dpx = px - tx;
-    FT dpy = py - ty;
-    FT dpz = square(dpx) + square(dpy) - pwt + twt;
-    FT dqx = qx - tx;
-    FT dqy = qy - ty;
-    FT dqz = square(dqx) + square(dqy) - qwt + twt;
+    // Test if we can project on the (x) axis.  If not, then on the
+    // (y) axis
+    RT pa, qa, ta;
 
-    // We do an orthogonal projection on the (x) axis, if possible.
-    Comparison_result cmpx = CGAL::compare(px, qx);
-    if (cmpx != EQUAL)
-	return Oriented_side(cmpx * sign_of_determinant2x2(dpx, dpz, dqx, dqz));
+    if (phx * qhw != qhx * phw )
+    {
+	pa = phx*phw;
+	qa = qhx*qhw;
+	ta = thx*thw;
+    }
+    else
+    {   
+	pa = phy*phw;
+	qa = qhy*qhw;
+	ta = thy*thw;
+    }
 
-    // If not possible, then on the (y) axis.
-    Comparison_result cmpy = CGAL::compare(py, qy);
-    return Oriented_side(cmpy * sign_of_determinant2x2(dpy, dpz, dqy, dqz));
+    RT dphw = square(phw);
+    RT dpz = square(phx) + square(phy) - pwt*dphw;
+
+    RT dqhw = square(qhw);
+    RT dqz = square(qhx) + square(qhy) - qwt*dqhw;
+
+    RT dthw = square(thw);
+    RT dtz = square(thx) + square(thy) - twt*dthw;
+
+    return Oriented_side(CGAL::compare(pa, qa) *
+	                 sign_of_determinant3x3(pa, dpz, dphw,
+				                qa, dqz, dqhw,
+				                ta, dtz, dthw));
 }
 
 CGAL_END_NAMESPACE
 
 #ifdef CGAL_ARITHMETIC_FILTER_H
-#ifndef CGAL_ARITHMETIC_FILTER_REGULAR_TRIANGULATION_FTC2_H
-#include <CGAL/Arithmetic_filter/predicates/Regular_triangulation_ftC2.h>
-#endif // CGAL_ARITHMETIC_FILTER_REGULAR_TRIANGULATION_FTC2_H
+#ifndef CGAL_ARITHMETIC_FILTER_REGULAR_TRIANGULATION_RTH2_H
+#include <CGAL/Arithmetic_filter/predicates/Regular_triangulation_rtH2.h>
+#endif // CGAL_ARITHMETIC_FILTER_REGULAR_TRIANGULATION_RTH2_H
 #endif
 
-#endif // CGAL_REGULAR_TRIANGULATION_FTC2_H
+#endif // CGAL_REGULAR_TRIANGULATION_RTH2_H
