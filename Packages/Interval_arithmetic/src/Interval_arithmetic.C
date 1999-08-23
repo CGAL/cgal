56a16013e9ca714b0d194243e3a75881419c0435
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1999-08-23 12:58:54 +0000

    - Removed "want_exceptions".

--- Packages/Interval_arithmetic/src/Interval_arithmetic.C
+++ Packages/Interval_arithmetic/src/Interval_arithmetic.C
@@ -39,7 +39,6 @@ CGAL_BEGIN_NAMESPACE
 #include <CGAL/Arithmetic_filter/static_infos/dispatch.h>
 
 unsigned Interval_nt_advanced::number_of_failures = 0;
-bool     Interval_nt_advanced::want_exceptions    = true;
 
 std::ostream &
 operator<< (std::ostream & os, const Interval_nt_advanced & I)
@@ -60,22 +59,23 @@ void force_ieee_double_precision()
 {
 #ifdef __i386__
     FPU_set_cw(FPU_cw_near);
-#endif // __i386__
+#endif
 }
 
 // The results of 1-epsilon and -1+epsilon are enough
-// to detect exactly the rounding mode.
-// ----------------------------------------------------
-// rounding mode:        +inf    -inf    0       nearest
-// ----------------------------------------------------
-//  1-MIN_DOUBLE         1       1-ulp   1-ulp   1
-// -1+MIN_DOUBLE        -1+ulp  -1      -1+ulp  -1
-// ----------------------------------------------------
+// to detect exactly the current rounding mode.
+//                          1-MIN_DOUBLE
+//                        +------+-------+
+//                        |  1   | 1-ulp |
+//               +--------+------+-------+
+// -1+MIN_DOUBLE | -1     | near |  -inf |
+//               | -1+ulp | +inf |  zero |
+//               +--------+------+-------+
 
 FPU_CW_t FPU_empiric_test()
 {
     // If not marked "volatile", the result is false when optimizing
-    // because the constants are pre-computed at compile time !!!
+    // because the constants are propagated at compile time !!!
     volatile const double m = CGAL_IA_MIN_DOUBLE;
     const double y = 1.0, z = -1.0;
     double ye, ze;
