17fe61181612952eca0ad3491d2fe9bafc83dedb
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
1999-08-24 07:09:36 +0000

    readded for backward compatibility just includes the new named file Triangulation_window_stream.h

---
+++ Packages/Triangulation_2/include/CGAL/IO/triangulation_Window_stream.h
@@ -0,0 +1,26 @@
+// ============================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       :
+// release_date  :
+//
+// file          : include/CGAL/IO/triangulation_Window_stream.h
+// source        : $RCSfile$
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Mariette Yvinec
+//
+// coordinator   : Mariette Yvinec  <Mariette Yvinec@sophia.inria.fr>
+//
+// ============================================================================
+
+
+//file included for backward compatibility with previous version
+#include <CGAL/IO/Triangulation_window_stream.h>
