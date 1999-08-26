1fad5e2ef6cb9273a86598f5f50bd1bff3363f6f
Michael Hoffmann <hoffmann@inf.ethz.ch>
1999-08-26 09:31:16 +0000

    Converted to namespace CGAL.

--- Packages/Installation/examples/example.C
+++ Packages/Installation/examples/example.C
@@ -7,13 +7,13 @@
 #include <CGAL/Point_2.h>
 #include <iostream>
 
-typedef CGAL_Cartesian< double >  R;
-typedef CGAL_Point_2< R >         Point;
+typedef CGAL::Cartesian< double >  R;
+typedef CGAL::Point_2< R >         Point;
 
 int main()
 {
   Point p(0, 0);
-  CGAL_set_ascii_mode(std::cout);
+  CGAL::set_ascii_mode(std::cout);
   std::cout << "p = " << p << std::endl;
 }
 
