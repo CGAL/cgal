17e2da1e8735114165fcc3370a8b0b73e4afca7f
Geert-Jan Giezeman <geert@cs.uu.nl>
1999-09-01 19:07:52 +0000

    Changes for STLport

--- Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
+++ Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
@@ -25,24 +25,12 @@ namespace std {
 	using ::fabs;
 	using ::sqrt;
 	using ::log;
+	using ::cos;
+	using ::sin;
+	using ::ceil;
+	using ::floor;
 }
-
-namespace std{
-template <class T>
-inline const T& min(const T& a, const T&b)
-{ return (a<b) ? a : b;}
-
-template <class T, class Cmp>
-inline const T& min(const T& a, const T&b, Cmp cmp)
-{ return (cmp(b,a)) ? a : b;}
-
-template <class T>
-inline const T& max(const T& a, const T&b)
-{ return (a<b) ? b : a;}
-
-}
-
-
+#define M_PI 3.14159
 #include <cstddef>
 namespace std{
 using ::size_t;
@@ -54,29 +42,6 @@ namespace std{
 using ::abort;
 using ::atoi;
 }
-
-#include <iterator>
-namespace std{
-template < class T, class Dist>
-struct input_iterator
-: public iterator<input_iterator_tag,T,Dist> {};
-
-template <class T, class Dist>
-struct forward_iterator
-: public iterator<forward_iterator_tag,T,Dist> {};
-
-template <class T, class Dist>
-struct bidirectional_iterator
-: public iterator<bidirectional_iterator_tag,T,Dist> {};
-
-template <class T, class Dist>
-struct random_access_iterator
-: public iterator<random_access_iterator_tag,T,Dist> {};
-
-struct output_iterator
-: public iterator<output_iterator_tag,void,void> {};
-}
-
 #include <cstring>
 namespace std{
 using ::strcat;
