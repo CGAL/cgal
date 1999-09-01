0397894d2b5cf3c6ed1f58cdca8cc9062d2ed5b5
Geert-Jan Giezeman <geert@cs.uu.nl>
1999-09-01 16:06:49 +0000

    *** empty log message ***

--- Packages/Configuration/test/Configuration/CGAL_CFG_NO_LONGNAME_PROBLEM.C
+++ Packages/Configuration/test/Configuration/CGAL_CFG_NO_LONGNAME_PROBLEM.C
@@ -1,3 +1,28 @@
+// ======================================================================
+//
+// Copyright (c) 1999 The CGAL Consortium
+//
+// ----------------------------------------------------------------------
+//
+// release       : 
+// release_date  : 
+//
+// file          : config/testfiles/CGAL_CFG_NO_LONGNAME_PROBLEM.C
+// author(s)     : various
+//
+// coordinator   : Utrecht University
+//
+// ======================================================================
+
+// CGAL_CFG_NO_LONGNAME_PROBLEM.C
+// ---------------------------------------------------------------------
+// A short test program to evaluate a C++ compiler.
+// This program is used by cgal_configure.
+// The following documentation will be pasted in the generated configfile.
+// ---------------------------------------------------------------------
+
+//| If a compiler (or assembler or linker) has problems with long names
+//| CGAL_CFG_NO_LONGNAME_PROBLEM is set.
 
 #define LONG_NAME \
 Wwwwwwwwwooooooooo_vvvvvvveeeeeerrrryyyy_llllooooonnnnnnggggg_nnnnnaaaammmmmeeeeWwwwwwwwwooooooooo_vvvvvvveeeeeerrrryyyy_llllooooonnnnnnggggg_nnnnnaaaammmmmeeee
