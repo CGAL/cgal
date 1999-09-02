3eeebb9b5ff00dcba6eeaab2c3d8d2fca143179a
Hervé Brönnimann <Herve.Bronnimann@sophia.inria.fr>
1999-09-02 15:17:23 +0000

    Version 3.2.6 (September 2 1999) - removed #warning which is not standard-compliant and crashes on M$VC++

--- Old_Packages/C3/include/CGAL/Cartesian/basic_constructions_3.h
+++ Old_Packages/C3/include/CGAL/Cartesian/basic_constructions_3.h
@@ -26,8 +26,8 @@
 #ifndef CGAL_CARTESIAN_BASIC_CONSTRUCTIONS_3_H
 #define CGAL_CARTESIAN_BASIC_CONSTRUCTIONS_3_H
 
-#warning "basic_construction_3.h: this file will disappear"
-#warning "  please include <CGAL/Cartesian/constructions_on_....h>"
+// #warning "basic_construction_3.h: this file will disappear"
+// #warning "  please include <CGAL/Cartesian/constructions_on_....h>"
 
 #include <CGAL/Cartesian/constructions_on_points_3.h>
 #include <CGAL/Cartesian/constructions_on_vectors_3.h>
