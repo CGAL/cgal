50c65dfe1123bc032e3368984e5a8302e01819b2
Hervé Brönnimann <Herve.Bronnimann@sophia.inria.fr>
1999-09-03 18:28:30 +0000

    Added header to avoid complaints by Utrecht

--- Old_Packages/C2/include/CGAL/Cartesian/Rotation_rep_2.h
+++ Old_Packages/C2/include/CGAL/Cartesian/Rotation_rep_2.h
@@ -1,3 +1,28 @@
+// ============================================================================
+//
+// Copyright (c) 1998 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       :
+// release_date  :
+//
+// file          : include/CGAL/Cartesian/Rotation_rep_2.h
+// source        : include/CGAL/Cartesian/Rotation_rep_2.h
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Andreas.Fabri@sophia.inria.fr, Herve.Bronnimann@sophia.inria.fr
+//
+// coordinator   : INRIA Sophia-Antipolis (Herve.Bronnimann@sophia.inria.fr)
+//
+// ============================================================================
+
+
+#ifndef CGAL_CARTESIAN_ROTATION_REP_2_H
 #define CGAL_CARTESIAN_ROTATION_REP_2_H
 
 #ifndef CGAL_RATIONAL_ROTATION_H
