7927849c0b9437c16df54b59a893287eeafca4a1
Geert-Jan Giezeman <geert@cs.uu.nl>
1999-09-08 08:12:22 +0000

    *** empty log message ***

--- Packages/Configuration/config/testfiles/CGAL_CFG_NO_TEMPLATE_FRIEND_DISTINCTION.C
+++ Packages/Configuration/config/testfiles/CGAL_CFG_NO_TEMPLATE_FRIEND_DISTINCTION.C
@@ -1,6 +1,6 @@
 // ======================================================================
 //
-// Copyright (c) 1997 The CGAL Consortium
+// Copyright (c) 1999 The CGAL Consortium
 //
 // ----------------------------------------------------------------------
 //
@@ -19,30 +19,30 @@
 
 // CGAL_CFG_NO_TEMPLATE_FRIEND_DISTINCTION.C
 // ---------------------------------------------------------------------
-// A short test program to evaluate a C++ compiler whether it distinguishes
-// a template instantiation and a non-templated function with the same
-// signature in a friend declaration.
+// A short test program to evaluate a C++ compiler.
 // This program is used by cgal_configure.
 // The following documentation will be pasted in the generated configfile.
-// Perhaps this file should be called CGAL_CFG_NO_EMPTY_TEMPLATE_ARGUMENT_LIST
 // ---------------------------------------------------------------------
 
-//| If a compiler doesn't distinguish a template instantiation and 
-//| a non-templated function with the same signature, 
-//| CGAL_CFG_NO_TEMPLATE_FRIEND_DISTINCTION is set.
-
-int y(int i) { return i + 1; }
+//| Checks whether the compiler wants to have a <> in friend declarations
+//| of template functions.
 
 template < class T >
-int y(T t) { return t - 1; }
+inline int y(T t);
+
+struct A {
+  A(int i_) : i(i_) {}
+  friend int y<>(A);
+private:
+  int i;
+};
+
+template < class T >
+int y(T t) { return t.i - 1; }
 
 int main()
 {
-  int i = 3;
-  if (y(i) == 4 && y<>(i) == 2)
-    return 0;
-  return 1;
+  A a(3);
+  return (y(a) == 2)?0:1;
 }
-
-// EOF //
 
