79160c96e006bf8998ebc60e7b42e63a32f789bf
Michael Hoffmann <hoffmann@inf.ethz.ch>
1999-09-10 13:52:06 +0000

    *** empty log message ***

--- Packages/Matrix_search/doc_tex/Optimisation/Optimisation_ref/rectangular_p_center_2_example_noheader.C
+++ Packages/Matrix_search/doc_tex/Optimisation/Optimisation_ref/rectangular_p_center_2_example_noheader.C
@@ -3,7 +3,7 @@
 #include <CGAL/point_generators_2.h>
 #include <CGAL/rectangular_p_center_2.h>
 #include <CGAL/IO/Ostream_iterator.h>
-#include <CGAL/copy_n.h>
+#include <CGAL/algorithm.h>
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -18,33 +18,34 @@ typedef CGAL::Point_2< R >                       Point;
 typedef std::vector< Point >                     Cont;
 typedef CGAL::Creator_uniform_2< FT, Point >     Creator;
 typedef CGAL::Random_points_in_square_2< Point, Creator >
-                                                 Point_generator;
+  Point_generator;
 typedef CGAL::Ostream_iterator< Point, ostream > Ostream_iterator_point;
 
 int main() {
 
-  int number_of_points( 10);
-  int p( 2);
-  Ostream_iterator_point cout_ip( cout);
-  set_pretty_mode( cout);
+  int number_of_points(10);
+  int p(2);
+  Ostream_iterator_point cout_ip(cout);
+  set_pretty_mode(cout);
 
   Cont points;
-  CGAL::copy_n( Point_generator( 1),
-                number_of_points,
-                back_inserter( points));
+  CGAL::copy_n(Point_generator(1),
+               number_of_points,
+               back_inserter(points));
   cout << "Generated Point Set:" << endl;
-  copy( points.begin(), points.end(), cout_ip);
+  copy(points.begin(), points.end(), cout_ip);
 
   Cont centers;
   FT p_radius;
   rectangular_p_center_2(
     points.begin(),
     points.end(),
-    back_inserter( centers),
+    back_inserter(centers),
     p_radius,
-    p);
+    3);
+
   cout << "\n\n" << p << "-centers:" << endl;
-  copy( centers.begin(), centers.end(), cout_ip);
+  copy(centers.begin(), centers.end(), cout_ip);
   cout << "\n\n" << p << "-radius = " << p_radius << endl;
 
 } 
--- Packages/Matrix_search/examples/Matrix_search/rectangular_p_center_2_example.C
+++ Packages/Matrix_search/examples/Matrix_search/rectangular_p_center_2_example.C
@@ -39,9 +39,9 @@
 #ifndef CGAL_IO_OSTREAM_ITERATOR_H
 #include <CGAL/IO/Ostream_iterator.h>
 #endif // CGAL_IO_OSTREAM_ITERATOR_H
-#ifndef CGAL_COPY_N_H
-#include <CGAL/copy_n.h>
-#endif // CGAL_COPY_N_H
+#ifndef CGAL_ALGORITHM_H
+#include <CGAL/algorithm.h>
+#endif // CGAL_ALGORITHM_H
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -56,33 +56,34 @@ typedef CGAL::Point_2< R >                       Point;
 typedef std::vector< Point >                     Cont;
 typedef CGAL::Creator_uniform_2< FT, Point >     Creator;
 typedef CGAL::Random_points_in_square_2< Point, Creator >
-                                                 Point_generator;
+  Point_generator;
 typedef CGAL::Ostream_iterator< Point, ostream > Ostream_iterator_point;
 
 int main() {
 
-  int number_of_points( 10);
-  int p( 2);
-  Ostream_iterator_point cout_ip( cout);
-  set_pretty_mode( cout);
+  int number_of_points(10);
+  int p(2);
+  Ostream_iterator_point cout_ip(cout);
+  set_pretty_mode(cout);
 
   Cont points;
-  CGAL::copy_n( Point_generator( 1),
-                number_of_points,
-                back_inserter( points));
+  CGAL::copy_n(Point_generator(1),
+               number_of_points,
+               back_inserter(points));
   cout << "Generated Point Set:" << endl;
-  copy( points.begin(), points.end(), cout_ip);
+  copy(points.begin(), points.end(), cout_ip);
 
   Cont centers;
   FT p_radius;
   rectangular_p_center_2(
     points.begin(),
     points.end(),
-    back_inserter( centers),
+    back_inserter(centers),
     p_radius,
-    p);
+    3);
+
   cout << "\n\n" << p << "-centers:" << endl;
-  copy( centers.begin(), centers.end(), cout_ip);
+  copy(centers.begin(), centers.end(), cout_ip);
   cout << "\n\n" << p << "-radius = " << p_radius << endl;
 
 } 
