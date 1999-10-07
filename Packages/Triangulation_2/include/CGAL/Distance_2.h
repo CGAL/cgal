03bd74295d78f4c0aef8a74c7c78198945028e93
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
1999-10-07 14:16:03 +0000

    Change the Distance subtypes in Triangulation_euclidean_traits_??_3.h

--- Packages/Triangulation_2/include/CGAL/Distance_2.h
+++ Packages/Triangulation_2/include/CGAL/Distance_2.h
@@ -68,6 +68,38 @@ private:
   Point p[3];
 };
 
+template <class Traits>
+struct Distance_xy_3  : public Distance_2<Traits> 
+{
+  typedef typename Distance_2<Traits>::Point Point;
+
+public:
+  Distance_xy_3(const Point& p0,
+	   const Traits* traits = NULL)
+    : Distance_2<Traits>(p0, traits) { }
+    
+    
+  Distance_xy_3(const Point& p0,
+	   const Point& p1,
+	   const Traits* traits = NULL)
+    : Distance_2<Traits>(p0,p1,traits) { }
+    
+  Distance_xy_3(const Point& p0,
+	   const Point& p1,
+	   const Point& p2,
+	   const Traits* traits = NULL)
+    : Distance_2<Traits>(p0,p1,p2,traits) { }
+    
+  Comparison_result
+  compare() const
+    {
+      Point p0 = get_point(0);
+      Point p1 = get_point(1);
+      Point p2 = get_point(2);
+      return cmp_dist_to_pointC2(x(p0),y(p0),x(p1),y(p1),x(p2),y(p2));
+    }
+};
+
 CGAL_END_NAMESPACE
 
 #endif // CGAL_DISTANCE_2_H
--- Packages/Triangulation_2/include/CGAL/Triangulation_euclidean_traits_xz_3.h
+++ Packages/Triangulation_2/include/CGAL/Triangulation_euclidean_traits_xz_3.h
@@ -48,6 +48,7 @@ public:
     typedef Line_3<R>   Line;
     typedef Ray_3<R>    Ray;
     typedef Direction_3<R> Direction;
+    typedef Distance_xy_3<Traits> Distance;
     
     
   Triangulation_euclidean_traits_xz_3(){}
@@ -92,38 +93,6 @@ public:
 				       x(s), y(s));
     }
         
-    
-    class Distance : public Distance_2<Traits> 
-    {
-      typedef typename Distance_2<Traits>::Point Point;
-
-    public:
-        Distance(const Point& p0,
-                 const Traits* traits = NULL)
-            : Distance_2<Traits>(p0, traits) { }
-    
-    
-        Distance(const Point& p0,
-                 const Point& p1,
-                 const Traits* traits = NULL)
-            : Distance_2<Traits>(p0,p1,traits) { }
-    
-        Distance(const Point& p0,
-                 const Point& p1,
-                 const Point& p2,
-                 const Traits* traits = NULL)
-            : Distance_2<Traits>(p0,p1,p2,traits) { }
-    
-        Comparison_result
-        compare() const
-        {
-          Point p0 = get_point(0);
-          Point p1 = get_point(1);
-          Point p2 = get_point(2);
-          return cmp_dist_to_pointC2(x(p0),y(p0),x(p1),y(p1),x(p2),y(p2));
-        }
-    };
-    
 };
 
 CGAL_END_NAMESPACE
