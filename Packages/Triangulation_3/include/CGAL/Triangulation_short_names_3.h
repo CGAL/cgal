9096576196ef6ea13a6f269a205a5e14e75d3af2
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
1999-10-14 07:25:04 +0000

    short names re-added for MSVC

---
+++ Packages/Triangulation_3/include/CGAL/Triangulation_short_names_3.h
@@ -0,0 +1,73 @@
+// ============================================================================
+//
+// Copyright (c) 1999 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       :
+// release_date  :
+//
+// file          : include/CGAL/Triangulation_short_names_3.h
+// revision      : $Revision$
+//
+// author(s)     : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
+//
+// coordinator   : INRIA Sophia Antipolis 
+//                 (Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>)
+//
+// ============================================================================
+
+#ifndef CGAL_TRIANGULATION_SHORT_NAMES_3_H
+#define CGAL_TRIANGULATION_SHORT_NAMES_3_H
+
+// for MSVC
+
+// ======
+#define Triangulation_vertex_base_3                 T_vb
+#define Triangulation_cell_base_3                   T_cb
+
+#define Triangulation_geom_traits_3                 T_gt
+
+// ======
+#define Triangulation_data_structure_3              Tds
+
+#define Triangulation_ds_vertex_3                   Tds_v
+#define Triangulation_ds_cell_3                     Tds_c
+
+#define Triangulation_ds_vertex_iterator_3          Tds_vit
+#define Triangulation_ds_edge_iterator_3            Tds_eit
+#define Triangulation_ds_facet_iterator_3           Tds_fit
+#define Triangulation_ds_cell_iterator_3            Tds_cit
+
+#define Triangulation_ds_cell_circulator_3          Tds_ccir
+#define Triangulation_ds_facet_circulator_3         Tds_fcir
+
+// ======
+#define Triangulation_3                             Tr
+
+#define Triangulation_vertex_3                      T_v
+#define Triangulation_cell_3                        T_c
+
+#define Triangulation_vertex_handle_3               T_vh
+#define Triangulation_cell_handle_3                 T_ch
+
+#define Triangulation_vertex_iterator_3             T_vit
+#define Triangulation_edge_iterator_3               T_eit
+#define Triangulation_facet_iterator_3              T_fit
+#define Triangulation_cell_iterator_3               T_cit
+
+#define Triangulation_cell_circulator_3             T_ccir
+#define Triangulation_facet_circulator_3            T_fcir
+
+// ======
+#define Delaunay_triangulation_3                    Dt
+
+#define Regular_triangulation_euclidean_traits_3    R_gt
+#define Regular_triangulation_3                     Rt
+
+
+#endif //CGAL_TRIANGULATION_SHORT_NAMES_3_H
