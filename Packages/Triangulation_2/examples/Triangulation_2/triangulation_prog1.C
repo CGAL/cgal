307ebf04b76c27fca910ac6006a29656f27f062e
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
1999-10-19 13:47:52 +0000

    added the first example in doc

---
+++ Packages/Triangulation_2/examples/Triangulation_2/triangulation_prog1.C
@@ -0,0 +1,34 @@
+#include <CGAL/basic.h>
+#include <iostream>
+#include <CGAL/Cartesian.h>
+#include <CGAL/Point_2.h>
+#include <CGAL/Triangulation_2.h>
+#include <CGAL/Triangulation_euclidean_traits_2.h>
+
+using namespace CGAL;
+
+typedef Cartesian<double> Rp;
+typedef Triangulation_euclidean_traits_2<Rp> Gt;
+typedef Triangulation_vertex_base_2<Gt> Vb;
+typedef Triangulation_face_base_2<Gt>  Fb;
+typedef Triangulation_default_data_structure_2<Gt,Vb,Fb> Tds;
+typedef Triangulation_2<Gt, Tds> Triangulation;
+typedef Triangulation::Vertex_circulator Vertex_circulator;
+typedef Gt::Point   Point;
+
+int main() {
+  Triangulation t;
+    
+  Point p;
+  while (std::cin >> p){
+    t.insert(p);
+  }
+  
+  Vertex_circulator vc = t.incident_vertices(t.infinite_vertex()),
+    done(vc);
+  if (vc != 0) {
+    do{
+      std::cout << vc->point() << std::endl;
+    }while(++vc != done);
+  }
+}
