71719a98e06b506daf7a9b40dd433579ea9effb6
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
1999-10-19 17:21:27 +0000

    new example corresponding to an example of the doc

---
+++ Packages/Triangulation_2/examples/Triangulation_2/terrain.C
@@ -0,0 +1,28 @@
+#include <CGAL/basic.h>
+#include <iostream>
+#include <CGAL/leda_integer.h>
+#include <CGAL/Homogeneous.h>
+#include <CGAL/Triangulation_euclidean_traits_xy_3.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/IO/Geomview_stream.h>
+
+using namespace CGAL;
+
+typedef Homogeneous<leda_integer>  Rp;
+typedef Triangulation_euclidean_traits_xy_3<Rp>  Gt;
+typedef Triangulation_vertex_base_2<Gt> Vb;
+typedef Triangulation_face_base_2<Gt> Fb;
+typedef Triangulation_default_data_structure_2<Gt,Vb,Fb > Tds;
+typedef Delaunay_triangulation_2<Gt, Tds> Delaunay;
+
+int main()
+{
+    Delaunay dt;
+
+    Point_3<Rp> p;
+    while(cin >> p){
+        dt.insert(p);
+    }
+    
+}
+
