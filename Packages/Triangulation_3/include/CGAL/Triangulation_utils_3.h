61b3d8546eab0927c3177b7a9834958242b3bcb2
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1999-10-22 14:33:15 +0000

    - Put the array outside the function so that the function can be inlined. - Changed it to an array of "char". - Simplified the preconditions.

--- Packages/Triangulation_3/include/CGAL/Triangulation_utils_3.h
+++ Packages/Triangulation_3/include/CGAL/Triangulation_utils_3.h
@@ -24,57 +24,42 @@
 #define CGAL_TRIANGULATION_UTILS_3_H
 
 #include <CGAL/triangulation_assertions.h>
-
 #include <CGAL/Triangulation_short_names_3.h>
 
 CGAL_BEGIN_NAMESPACE
 
-class Triangulation_utils_3
+struct Triangulation_utils_3
 {
-public:
-//   inline unsigned int ccw(const unsigned int i) const
-//     {
-//       CGAL_precondition( i < 3 );
-//       static const unsigned int tab_ccw[] = {1,2,0};
-//       return tab_ccw[i];
-//     }
-  
-//   inline unsigned int cw(const unsigned int i) const
-//     {
-//       CGAL_precondition( i < 3 );
-//       static const unsigned int tab_cw[] = {2,0,1};
-//       return tab_cw[i];
-//     }
+  static const char tab_next_around_edge_ij[4][4];
 
-  inline int ccw(const int i) const
+  int ccw(const int i) const
     {
-      CGAL_triangulation_precondition( (0 <= i) && (i < 3) );
+      CGAL_triangulation_precondition( 3 > (unsigned) i );
       return (i==2) ? 0 : i+1;
     }
   
-  inline int cw(const int i) const
+  int cw(const int i) const
     {
-      CGAL_triangulation_precondition( (0 <= i) || (i < 3) );
+      CGAL_triangulation_precondition( 3 > (unsigned) i );
       return (i==0) ? 2 : i-1;
     }
 
-  inline int next_around_edge(const int i, 
-			   const int j) const
-{
-  // index of the next cell when turning around the
-  // oriented edge vertex(i) vertex(j) in 3d
-  CGAL_precondition( (0 <= i) && ( i < 4 ) && 
-		     (0 <= j) && ( j < 4 ) && 
-		     ( i != j ) );
-  static const int tab_next_around_edge_ij[4][4] = {
-    {5, 2, 3, 1},
-    {3, 5, 0, 2},
-    {1, 3, 5, 0},
-    {2, 0, 1, 5} }; // the diagonal has no meaning
-  return tab_next_around_edge_ij[i][j];
-}
-
+  int next_around_edge(const int i, const int j) const
+  {
+    // index of the next cell when turning around the
+    // oriented edge vertex(i) vertex(j) in 3d
+    CGAL_triangulation_precondition( ( 4 > (unsigned) i) &&
+		                     ( 4 > (unsigned) j) &&
+		                     ( i != j ) );
+    return tab_next_around_edge_ij[i][j];
+  }
 };
+
+const char Triangulation_utils_3::tab_next_around_edge_ij[4][4] = {
+      {5, 2, 3, 1},
+      {3, 5, 0, 2},
+      {1, 3, 5, 0},
+      {2, 0, 1, 5} };
 
 CGAL_END_NAMESPACE
 
