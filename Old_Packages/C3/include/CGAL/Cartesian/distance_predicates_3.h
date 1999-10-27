630e4b46421924331a119c92aeddc40e9ca072c9
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1999-10-27 19:02:00 +0000

    - Various cleanup.

--- Old_Packages/C3/include/CGAL/Cartesian/distance_predicates_3.h
+++ Old_Packages/C3/include/CGAL/Cartesian/distance_predicates_3.h
@@ -1,6 +1,6 @@
 // ==========================================================================
 //
-// Copyright (c) 1998 The CGAL Consortium
+// Copyright (c) 1998, 1999 The CGAL Consortium
 //
 // This software and related documentation is part of an INTERNAL release
 // of the Computational Geometry Algorithms Library (CGAL). It is not
@@ -8,12 +8,10 @@
 //
 // --------------------------------------------------------------------------
 //
-
 // release       :
 // release_date  :
 //
 // file          : include/CGAL/Cartesian/distance_predicates_3.h
-// source        : include/CGAL/Cartesian/distance_predicates_3.h
 // revision      : $Revision$
 // revision_date : $Date$
 // author(s)     : Herve.Bronnimann@sophia.inria.fr
@@ -22,23 +20,13 @@
 //
 // ==========================================================================
 
+#ifndef CGAL_CARTESIAN_DISTANCE_PREDICATES_3_H
+#define CGAL_CARTESIAN_DISTANCE_PREDICATES_3_H
 
-#ifndef DISTANCE_PREDICATES_3_H
-#define DISTANCE_PREDICATES_3_H
-
-#ifndef CGAL_CARTESIAN_REDEFINE_NAMES_3_H
 #include <CGAL/Cartesian/redefine_names_3.h>
-#endif
-
-#ifndef CGAL_CARTESIAN_POINT_3_H
 #include <CGAL/Cartesian/Point_3.h>
-#endif
-#ifndef CGAL_CARTESIAN_PLANE_3_H
 #include <CGAL/Cartesian/Plane_3.h>
-#endif
-#ifndef CGAL_PREDICATES_KERNEL_FTC3_H
 #include <CGAL/predicates/kernel_ftC3.h>
-#endif
 
 CGAL_BEGIN_NAMESPACE
 
@@ -77,6 +65,7 @@ has_smaller_dist_to_point(const PointC3<R CGAL_CTAG> &p,
                                      q.x(),q.y(),q.z(),
                                      r.x(),r.y(),r.z());
 }
+
 template < class R >
 CGAL_KERNEL_LARGE_INLINE
 Comparison_result
@@ -163,4 +152,4 @@ has_smaller_signed_dist_to_plane(const PointC3<R CGAL_CTAG> &hp,
 
 CGAL_END_NAMESPACE
 
-#endif // DISTANCE_PREDICATES_3_H
+#endif // CGAL_CARTESIAN_DISTANCE_PREDICATES_3_H
