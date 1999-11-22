fb16ecd4954a771685bdf5b740bdd0e9c1e6dadc
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
1999-11-22 14:07:45 +0000

    Initial revision

---
+++ Packages/Modifier/test/Modifier/test_modifier.C
@@ -0,0 +1,59 @@
+// ============================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       : $CGAL_Revision: $
+// release_date  : $CGAL_Date: $
+//
+// file          : test_modifier.C
+// chapter       : $CGAL_Chapter: Protected access to the internal repr.$
+// package       : $CGAL_Package: Modifier 1.2 (07 Apr 1999) $
+// source        : modifier.fw
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Lutz Kettner  <kettner@inf.ethz.ch>
+//
+// coordinator   : INRIA, Sophia Antipolis
+//
+// Protected access to the internal representation.
+// ============================================================================
+
+
+#ifndef CGAL_BASIC_H
+#include <CGAL/basic.h>
+#endif // CGAL_BASIC_H
+#ifndef CGAL_MODIFIER_BASE_H
+#include <CGAL/Modifier_base.h>
+#endif // CGAL_MODIFIER_BASE_H
+
+using CGAL::Modifier_base;
+
+class A {
+    int i;  // protected internal representation
+public:
+    void delegate( Modifier_base<int>& modifier) {
+        modifier(i);
+        // check validity
+        CGAL_postcondition( i > 0);
+    }
+    int get_i() const { return i;}  // read access
+};
+
+struct Modifier : public Modifier_base<int> {
+    void operator()( int& rep) { rep = 42;}
+};
+
+int main() {
+    A a;
+    Modifier m;
+    a.delegate(m);
+    CGAL_assertion( a.get_i() == 42);
+    return 0;
+}
+// EOF //
