175c340b18071fb64beca8f26c9553605b797a6c
Geert-Jan Giezeman <geert@cs.uu.nl>
1999-12-02 14:53:44 +0000

    Removed lots of using declarations. These are now covered by the use of the CGAL_CLIB_STD macro.

--- Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
+++ Packages/Configuration/include/CGAL/MSVC_standard_header_fixes.h
@@ -23,54 +23,14 @@
 
 #include <cmath>
 namespace std {
-	using ::fabs;
 	using ::sqrt;
-	using ::log;
-	using ::cos;
-	using ::sin;
-	using ::ceil;
-	using ::floor;
 }
-#define M_PI 3.14159
+#define M_PI 3.14159265358979323846
+
 #include <cstddef>
 namespace std{
   using ::size_t;
   using ::ptrdiff_t;
-}
-
-#include <cstdlib>
-namespace std{
-  using ::abort;
-  using ::atoi;
-}
-
-#include <cstdio>
-namespace std{
-  using ::sscanf;
-  using ::sprintf;
-}
-
-#include <cstring>
-namespace std{
-  using ::strcat;
-  using ::strcpy;
-  using ::strlen;
-  using ::strcmp;
-}
-
-#include <ctime>
-
-namespace std{
-  using ::clock;
-  using ::clock_t;
-}
-
-#include <cctype>  // this should go away as soon as CGAL is made <locale>
-                   // compliant.
-namespace std{     // to provide replacements...
-  using ::isalnum;
-  using ::isdigit;
-  using ::isspace;
 }
 
 #endif // CGAL_MSVC_STANDARD_HEADER_FIXES_H
