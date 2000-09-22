8dee7534e7f0dc66d98161c1300d73578107fdbc
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2000-09-22 11:42:15 +0000

    removed a part which makes i686_CYGWINNT-5.0-1.1.4-0.26-3-2_CL.EXE-12.00.8804 crash

--- Packages/Triangulation_2/test/Triangulation_2/include/CGAL/_test_cls_delaunay_hierarchy_2.C
+++ Packages/Triangulation_2/test/Triangulation_2/include/CGAL/_test_cls_delaunay_hierarchy_2.C
@@ -32,19 +32,21 @@ _test_cls_delaunay_hierarchy_2( const Dh & )
   typedef typename Delaunay_hierarchy::Point   Point;
   typedef CGAL::Creator_uniform_2<double,Point>  Creator;
 
-  _test_cls_delaunay_triangulation_2( Delaunay_hierarchy());
 
-  // temporary commented 
-//   std::cout << "    insertion removal of 1000 points" << std::endl;
-//   Delaunay_hierarchy dh;
-//   CGAL::Random_points_in_square_2<Point,Creator> g(1.);
-//   CGAL::copy_n( g, 1000, std::back_inserter(dh));
+  // makes i686_CYGWINNT-5.0-1.1.4-0.26-3-2_CL.EXE-12.00.8804 crash
+  //_test_cls_delaunay_triangulation_2( Delaunay_hierarchy());
 
-//   dh.locate(Point(0.,0.));
+  
+  std::cout << "    insertion removal of 1000 points" << std::endl;
+  Delaunay_hierarchy dh;
+  CGAL::Random_points_in_square_2<Point,Creator> g(1.);
+  CGAL::copy_n( g, 1000, std::back_inserter(dh));
 
-//   while( dh.number_of_vertices() >0) {
-//     dh.remove(dh.finite_vertices_begin());
-//   }
+  dh.locate(Point(0.,0.));
+
+  while( dh.number_of_vertices() >0) {
+    dh.remove(dh.finite_vertices_begin());
+  }
 
   return;
 }
