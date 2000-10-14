6ac71217ce3d01e4c5c8ff4543c6dba6ea385558
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2000-10-14 14:05:43 +0000

    - Got rid of Cartesian_[23].

--- Packages/Cartesian_kernel/include/CGAL/Cartesian.h
+++ Packages/Cartesian_kernel/include/CGAL/Cartesian.h
@@ -24,76 +24,143 @@
 
 #include <CGAL/basic.h>
 #include <CGAL/cartesian_classes.h>
-#include <CGAL/Cartesian_2.h>
-#include <CGAL/Cartesian_3.h>
 #include <CGAL/Cartesian_dynamic_d.h>
+
+#ifdef CGAL_CFG_NO_ADVANCED_KERNEL
+  // Because we cannot use Michael's scheme, we need the wrapper classes
+  // We include them (they are common to Cartesian and Homogeneous)
+#include <CGAL/user_classes.h>
+#endif // CGAL_CFG_NO_ADVANCED_KERNEL
 
 #define CGAL_REP_CLASS_DEFINED
 #define CGAL_CARTESIAN_CLASS_DEFINED
+
+#include <CGAL/Cartesian/Point_2.h>
+#include <CGAL/Cartesian/Vector_2.h>
+#include <CGAL/Cartesian/Direction_2.h>
+#include <CGAL/Cartesian/Line_2.h>
+#include <CGAL/Cartesian/Ray_2.h>
+#include <CGAL/Cartesian/Segment_2.h>
+#include <CGAL/Cartesian/Triangle_2.h>
+#include <CGAL/Cartesian/Circle_2.h>
+#include <CGAL/Cartesian/Iso_rectangle_2.h>
+#include <CGAL/Cartesian/Aff_transformation_2.h>
+#include <CGAL/Cartesian/Data_accessor_2.h>
+
+#include <CGAL/Cartesian/global_operators_2.h>
+#include <CGAL/Cartesian/predicates_on_points_2.h>
+#include <CGAL/Cartesian/predicates_on_directions_2.h>
+#include <CGAL/Cartesian/predicates_on_lines_2.h>
+#include <CGAL/Cartesian/distance_predicates_2.h>
+
+#include <CGAL/Cartesian/basic_constructions_2.h>
+
+#include <CGAL/Cartesian/Aff_transformation_2.C>
+
+// #include <CGAL/Kernel/Construction_objects_2.h>
+// #include <CGAL/Kernel/Predicate_objects_2.h>
+#include <CGAL/predicate_objects_on_points_2.h>
+#include <CGAL/Kernel/function_objects.h>
+
+#include <CGAL/Cartesian/Point_3.h>
+#include <CGAL/Cartesian/Vector_3.h>
+#include <CGAL/Cartesian/Direction_3.h>
+#include <CGAL/Cartesian/Line_3.h>
+#include <CGAL/Cartesian/Plane_3.h>
+#include <CGAL/Cartesian/Ray_3.h>
+#include <CGAL/Cartesian/Segment_3.h>
+#include <CGAL/Cartesian/Triangle_3.h>
+#include <CGAL/Cartesian/Tetrahedron_3.h>
+#include <CGAL/Cartesian/Iso_cuboid_3.h>
+#include <CGAL/Cartesian/Sphere_3.h>
+#include <CGAL/Cartesian/Aff_transformation_3.h>
+
+#include <CGAL/Cartesian/global_operators_3.h>
+#include <CGAL/Cartesian/predicates_on_points_3.h>
+#include <CGAL/Cartesian/predicates_on_planes_3.h>
+#include <CGAL/Cartesian/distance_predicates_3.h>
+
+#include <CGAL/Cartesian/basic_constructions_3.h>
+
+#include <CGAL/Cartesian/Aff_transformation_3.C>
+ 
+// #include <CGAL/Kernel/Construction_objects_3.h>
+// #include <CGAL/Kernel/Predicate_objects_3.h>
 
 CGAL_BEGIN_NAMESPACE
 
 template< class R, class FT_ >
 struct Cartesian_base :
-    public Cartesian_base_2<R,FT_>
-    , public Cartesian_base_3<R,FT_>
-    , public Cartesian_base_dynamic_d<R,FT_>
+     public Cartesian_base_dynamic_d<R,FT_>
 {
-    // Number types and representation tag (to avoid ambiguity in
-    // inheritance tree)
-    typedef FT_                                           RT;
-    typedef FT_                                           FT;
-    typedef Cartesian_tag                                 Rep_tag;
-    typedef Cartesian_tag                                 Kernel_tag;
+    typedef FT_                                         RT;
+    typedef FT_                                         FT;
+    typedef Cartesian_tag                               Rep_tag;
+    typedef Cartesian_tag                               Kernel_tag;
 
-    // All the classes are inherited, but because we inherit from a
-    // template parameter, we need to explicitly write the inheritance
-    // (see mail from Michael Hoffmann of July 28th 1999 in cgal-develop)
+    // Maybe Cd should be merged like C2 and C3 ?
+    typedef Cartesian_base_dynamic_d<R,FT_>             Kernel_base_d;
 
-    typedef Cartesian_base_2<R,FT_>                       Kernel_base_2;
-    typedef Cartesian_base_3<R,FT_>                       Kernel_base_3;
-    typedef Cartesian_base_dynamic_d<R,FT_>               Kernel_base_d;
+    typedef CGAL::Object                                Object_2;
+    typedef CGAL::Object                                Object_3;
 
-    typedef typename Kernel_base_2::Object_2              Object_2;
-    typedef typename Kernel_base_3::Object_3              Object_3;          
+#ifndef CGAL_CFG_NO_ADVANCED_KERNEL
+    typedef CGAL::Point_2<R,Rep_tag>                    Point_2;
+    typedef CGAL::Vector_2<R,Rep_tag>                   Vector_2;
+    typedef CGAL::Direction_2<R,Rep_tag>                Direction_2;
+    typedef CGAL::Segment_2<R,Rep_tag>                  Segment_2;
+    typedef CGAL::Line_2<R,Rep_tag>                     Line_2;
+    typedef CGAL::Ray_2<R,Rep_tag>                      Ray_2;
+    typedef CGAL::Triangle_2<R,Rep_tag>                 Triangle_2;
+    typedef CGAL::Circle_2<R,Rep_tag>                   Circle_2;
+    typedef CGAL::Iso_rectangle_2<R,Rep_tag>            Iso_rectangle_2;
+    typedef CGAL::Aff_transformation_2<R,Rep_tag>       Aff_transformation_2;
+    typedef CGAL::Data_accessor_2<R,Rep_tag>            Data_accessor_2;
+    typedef CGAL::ConicCPA2<Point_2,Data_accessor_2>    Conic_2;
+
+    typedef CGAL::Point_3<R,Rep_tag>                    Point_3;
+    typedef CGAL::Vector_3<R,Rep_tag>                   Vector_3;
+    typedef CGAL::Direction_3<R,Rep_tag>                Direction_3;
+    typedef CGAL::Line_3<R,Rep_tag>                     Line_3;
+    typedef CGAL::Plane_3<R,Rep_tag>                    Plane_3;
+    typedef CGAL::Ray_3<R,Rep_tag>                      Ray_3;
+    typedef CGAL::Segment_3<R,Rep_tag>                  Segment_3;
+    typedef CGAL::Triangle_3<R,Rep_tag>                 Triangle_3;
+    typedef CGAL::Tetrahedron_3<R,Rep_tag>              Tetrahedron_3;
+    typedef CGAL::Iso_cuboid_3<R,Rep_tag>               Iso_cuboid_3;
+    typedef CGAL::Sphere_3<R,Rep_tag>                   Sphere_3;
+    typedef CGAL::Aff_transformation_3<R,Rep_tag>       Aff_transformation_3;
+#else
+    typedef PointC2<R>                                  Point_2;
+    typedef VectorC2<R>                                 Vector_2;
+    typedef DirectionC2<R>                              Direction_2;
+    typedef SegmentC2<R>                                Segment_2;
+    typedef LineC2<R>                                   Line_2;
+    typedef RayC2<R>                                    Ray_2;
+    typedef TriangleC2<R>                               Triangle_2;
+    typedef CircleC2<R>                                 Circle_2;
+    typedef Iso_rectangleC2<R>                          Iso_rectangle_2;
+    typedef Aff_transformationC2<R>                     Aff_transformation_2;
+    typedef Data_accessorC2<R>                          Data_accessor_2;
+    typedef ConicCPA2<Point_2,Data_accessor_2>          Conic_2;
+
+    typedef PointC3<R>                                  Point_3;
+    typedef VectorC3<R>                                 Vector_3;
+    typedef DirectionC3<R>                              Direction_3;
+    typedef LineC3<R>                                   Line_3;
+    typedef PlaneC3<R>                                  Plane_3;
+    typedef RayC3<R>                                    Ray_3;
+    typedef SegmentC3<R>                                Segment_3;
+    typedef TriangleC3<R>                               Triangle_3;
+    typedef TetrahedronC3<R>                            Tetrahedron_3;
+    typedef Iso_cuboidC3<R>                             Iso_cuboid_3;
+    typedef SphereC3<R>                                 Sphere_3;
+    typedef Aff_transformationC3<R>                     Aff_transformation_3;
+#endif // CGAL_CFG_NO_ADVANCED_KERNEL
   
-    typedef typename Kernel_base_2::Point_2               Point_2;
-    typedef typename Kernel_base_2::Vector_2              Vector_2;
-    typedef typename Kernel_base_2::Direction_2           Direction_2;
-    typedef typename Kernel_base_2::Segment_2             Segment_2;
-    typedef typename Kernel_base_2::Line_2                Line_2;
-    typedef typename Kernel_base_2::Ray_2                 Ray_2;
-    typedef typename Kernel_base_2::Triangle_2            Triangle_2;
-    typedef typename Kernel_base_2::Circle_2              Circle_2;
-    typedef typename Kernel_base_2::Iso_rectangle_2       Iso_rectangle_2;
-    typedef typename Kernel_base_2::Aff_transformation_2  Aff_transformation_2;
-
-    typedef typename Kernel_base_2::Data_accessor_2       Data_accessor_2;
-    typedef typename Kernel_base_2::Conic_2               Conic_2;
-
-    typedef typename Kernel_base_3::Point_3               Point_3;
-    typedef typename Kernel_base_3::Vector_3              Vector_3;
-    typedef typename Kernel_base_3::Direction_3           Direction_3;
-    typedef typename Kernel_base_3::Line_3                Line_3;
-    typedef typename Kernel_base_3::Plane_3               Plane_3;
-    typedef typename Kernel_base_3::Ray_3                 Ray_3;
-    typedef typename Kernel_base_3::Segment_3             Segment_3;
-    typedef typename Kernel_base_3::Triangle_3            Triangle_3;
-    typedef typename Kernel_base_3::Tetrahedron_3         Tetrahedron_3;
-    typedef typename Kernel_base_3::Sphere_3              Sphere_3;
-    typedef typename Kernel_base_3::Iso_cuboid_3          Iso_cuboid_3;
-    typedef typename Kernel_base_3::Aff_transformation_3  Aff_transformation_3;
-
-    typedef typename Kernel_base_d::Point_d               Point_d;
+    typedef typename Kernel_base_d::Point_d             Point_d;
 };
 
-CGAL_END_NAMESPACE
-
-// #include <CGAL/Kernel/Construction_objects_2.h>
-// #include <CGAL/Kernel/Predicate_objects_2.h>
-#include <CGAL/Kernel/function_objects.h>
-
-CGAL_BEGIN_NAMESPACE
 
 template< class FT_ >
 struct Cartesian : public Cartesian_base< Cartesian<FT_>, FT_ >
@@ -114,7 +181,10 @@ struct Cartesian : public Cartesian_base< Cartesian<FT_>, FT_ >
 #ifndef CGAL_CFG_NO_ADVANCED_KERNEL
     // The other classes are inherited and because of partial specialization,
     // Cartesian<FT>::Point_2 is exactly CGAL::Point_2< Cartesian<FT> >
-    // As above, we still need to write down the inheritance explicitly
+
+    // All the classes are inherited, but because we inherit from a
+    // template parameter, we need to explicitly write the inheritance
+    // (see mail from Michael Hoffmann of July 28th 1999 in cgal-develop)
 
     typedef typename Kernel_base::Point_2                 Point_2;
     typedef typename Kernel_base::Vector_2                Vector_2;
