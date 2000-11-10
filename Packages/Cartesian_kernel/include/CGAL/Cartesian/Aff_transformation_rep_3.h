62b0701108a032452911eca39f445179d7a1ce1a
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2000-11-10 15:53:44 +0000

    - Merged .C into .h.

--- Packages/Cartesian_kernel/include/CGAL/Cartesian/Aff_transformation_rep_3.h
+++ Packages/Cartesian_kernel/include/CGAL/Cartesian/Aff_transformation_rep_3.h
@@ -23,6 +23,7 @@
 #define CGAL_CARTESIAN_AFF_TRANSFORMATION_REP_3_H
 
 #include <CGAL/Cartesian/redefine_names_3.h>
+#include <CGAL/determinant.h>
 
 CGAL_BEGIN_NAMESPACE
 
@@ -206,6 +207,125 @@ private:
   FT   t21, t22, t23, t24;
   FT   t31, t32, t33, t34;
 };
+
+
+#ifdef CGAL_CFG_TYPENAME_BUG
+#define typename
+#endif
+
+template < class R >
+CGAL_KERNEL_LARGE_INLINE
+typename Aff_transformation_repC3<R>::Aff_transformation_3
+Aff_transformation_repC3<R>::inverse() const
+{
+  return Aff_transformation_3(
+      det2x2_by_formula( t22, t23, t32, t33),         // i 11
+     -det2x2_by_formula( t12, t13, t32, t33),         // i 12
+      det2x2_by_formula( t12, t13, t22, t23),         // i 13
+     -det3x3_by_formula( t12, t13, t14, t22, t23, t24, t32, t33, t34 ),
+
+     -det2x2_by_formula( t21, t23, t31, t33),         // i 21 
+      det2x2_by_formula( t11, t13, t31, t33),         // i 22
+     -det2x2_by_formula( t11, t13, t21, t23),         // i 23
+      det3x3_by_formula( t11, t13, t14, t21, t23, t24, t31, t33, t34 ),
+
+      det2x2_by_formula( t21, t22, t31, t32),         // i 31
+     -det2x2_by_formula( t11, t12, t31, t32),         // i 32
+      det2x2_by_formula( t11, t12, t21, t22),         // i 33
+     -det3x3_by_formula( t11, t12, t14, t21, t22, t24, t31, t32, t34 ),
+
+      det3x3_by_formula( t11, t12, t13, t21, t22, t23, t31, t32, t33 ));
+}
+
+template < class R >
+CGAL_KERNEL_INLINE
+typename Aff_transformation_repC3<R>::Aff_transformation_3
+Aff_transformation_repC3<R>::
+operator*(const Aff_transformation_rep_baseC3<R> &t) const
+{
+  return t.compose(*this);
+}
+
+template < class R >
+CGAL_KERNEL_LARGE_INLINE
+typename Aff_transformation_repC3<R>::Aff_transformation_3
+Aff_transformation_repC3<R>::
+compose(const Aff_transformation_repC3<R> &t) const
+{
+  return Aff_transformation_3(t.t11*t11 + t.t12*t21 + t.t13*t31,
+                              t.t11*t12 + t.t12*t22 + t.t13*t32,
+                              t.t11*t13 + t.t12*t23 + t.t13*t33,
+                              t.t11*t14 + t.t12*t24 + t.t13*t34 + t.t14,
+
+                              t.t21*t11 + t.t22*t21 + t.t23*t31,
+                              t.t21*t12 + t.t22*t22 + t.t23*t32,
+                              t.t21*t13 + t.t22*t23 + t.t23*t33,
+                              t.t21*t14 + t.t22*t24 + t.t23*t34 + t.t24,
+
+                              t.t31*t11 + t.t32*t21 + t.t33*t31,
+                              t.t31*t12 + t.t32*t22 + t.t33*t32,
+                              t.t31*t13 + t.t32*t23 + t.t33*t33,
+                              t.t31*t14 + t.t32*t24 + t.t33*t34 + t.t34);
+}
+
+template < class R >
+CGAL_KERNEL_LARGE_INLINE
+typename Aff_transformation_repC3<R>::Aff_transformation_3
+Aff_transformation_repC3<R>::
+compose(const Translation_repC3<R> &t) const
+{
+  return Aff_transformation_3(t11,
+                              t12,
+                              t13,
+                              t14 + t._translationvector.x(),
+
+                              t21,
+                              t22,
+                              t23,
+                              t24 + t._translationvector.y(),
+
+                              t31,
+                              t32,
+                              t33,
+                              t34 + t._translationvector.z());
+}
+
+template < class R >
+CGAL_KERNEL_LARGE_INLINE
+typename Aff_transformation_repC3<R>::Aff_transformation_3
+Aff_transformation_repC3<R>::
+compose(const Scaling_repC3<R> &t) const
+{
+  return Aff_transformation_3(t._scalefactor * t11,
+                              t._scalefactor * t12,
+                              t._scalefactor * t13,
+                              t._scalefactor * t14,
+			      
+                              t._scalefactor * t21,
+                              t._scalefactor * t22,
+                              t._scalefactor * t23,
+                              t._scalefactor * t24,
+                              
+			      t._scalefactor * t31,
+                              t._scalefactor * t32,
+                              t._scalefactor * t33,
+                              t._scalefactor * t34);
+}
+
+template < class R >
+CGAL_KERNEL_LARGE_INLINE
+typename Aff_transformation_repC3<R>::Aff_transformation_3
+Aff_transformation_repC3<R>::transpose() const
+{
+  FT ft0(0);
+  return Aff_transformation_3( t11, t21, t31, t14,
+                               t12, t22, t32, t24,
+                               t13, t23, t33, t34);
+}
+
+#ifdef CGAL_CFG_TYPENAME_BUG
+#undef typename
+#endif
 
 CGAL_END_NAMESPACE
 
