7a42e9106d65abaeb340ad440c19174b64e8da8d
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2000-11-24 14:30:37 +0000

    - Added more kernel objects.

--- Packages/Geomview/test/Geomview/kernel_objects.C
+++ Packages/Geomview/test/Geomview/kernel_objects.C
@@ -1,5 +1,5 @@
 // Test program for Geomview_stream with kernel objects.  It's mostly a
-// compilation test, I do not verify the output.
+// compilation test, I do not verify the output automatically.
 //
 //  Sylvain Pion, 2000.
 
@@ -18,19 +18,40 @@
 
 #include <CGAL/IO/Geomview_stream.h>
 
-typedef CGAL::Cartesian<double> Kernel;
+typedef CGAL::Cartesian<double> K;
 
 int main()
 {
   CGAL::Geomview_stream gv(CGAL::Bbox_3(0, 0, 0, 350, 350, 350));
 
-  Kernel::Point_3 P3(200, 100, 100);
-  Kernel::Sphere_3 S3(Kernel::Point_3(100, 100, 100), 1000);
+  K::Point_3 P3(200, 100, 100);
+  K::Point_2 P2(200, 100);
+  K::Segment_2 S2(K::Point_2(200, 100),
+                  K::Point_2(300, 100));
+  K::Segment_3 S3(K::Point_3(200, 100, 100),
+                  K::Point_3(300, 100, 200));
+  K::Sphere_3 Sph3(K::Point_3(100, 100, 100), 1000);
+  K::Triangle_2 Tr2(K::Point_2(200, 200),
+                    K::Point_2(220, 220),
+                    K::Point_2(180, 220));
+  K::Triangle_3 Tr3(K::Point_3(200, 200, 50),
+                    K::Point_3(220, 220, 80),
+                    K::Point_3(180, 220, 100));
+  K::Tetrahedron_3 T3(K::Point_3(100, 100, 180),
+                      K::Point_3(120,  70, 220),
+                      K::Point_3(100, 100, 220),
+                      K::Point_3(120, 150, 250));
 
-  gv << S3;
+  gv << Sph3;
   gv << P3;
+  gv << P2;
+  gv << S2;
+  gv << S3;
+  gv << Tr2;
+  gv << Tr3;
+  gv << T3;
 
-  sleep(10);
+  sleep(30);
 
   return 0;
 }
