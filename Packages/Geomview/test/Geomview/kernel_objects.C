e52575163a7fe76010b7f8dd7cc05828987fed94
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2000-11-24 14:52:00 +0000

    - Added BBox_[23]. - Added call to clear() and look_recenter().

--- Packages/Geomview/test/Geomview/kernel_objects.C
+++ Packages/Geomview/test/Geomview/kernel_objects.C
@@ -24,33 +24,28 @@ int main()
 {
   CGAL::Geomview_stream gv(CGAL::Bbox_3(0, 0, 0, 350, 350, 350));
 
-  K::Point_3 P3(200, 100, 100);
-  K::Point_2 P2(200, 100);
-  K::Segment_2 S2(K::Point_2(200, 100),
-                  K::Point_2(300, 100));
-  K::Segment_3 S3(K::Point_3(200, 100, 100),
-                  K::Point_3(300, 100, 200));
-  K::Sphere_3 Sph3(K::Point_3(100, 100, 100), 1000);
-  K::Triangle_2 Tr2(K::Point_2(200, 200),
-                    K::Point_2(220, 220),
-                    K::Point_2(180, 220));
-  K::Triangle_3 Tr3(K::Point_3(200, 200, 50),
-                    K::Point_3(220, 220, 80),
-                    K::Point_3(180, 220, 100));
-  K::Tetrahedron_3 T3(K::Point_3(100, 100, 180),
-                      K::Point_3(120,  70, 220),
-                      K::Point_3(100, 100, 220),
-                      K::Point_3(120, 150, 250));
+  gv.clear(); // remove pickplane.
+  gv << K::Point_2 (200, 100);
+  gv << K::Point_3 (200, 100, 100);
+  gv << K::Segment_2 (K::Point_2(200, 100),
+                      K::Point_2(300, 100));
+  gv << K::Segment_3 (K::Point_3(200, 100, 100),
+                      K::Point_3(300, 100, 200));
+  gv << K::Sphere_3 (K::Point_3(100, 100, 100), 1000);
+  gv << K::Triangle_2 (K::Point_2(200, 200),
+                       K::Point_2(220, 220),
+                       K::Point_2(180, 220));
+  gv << K::Triangle_3 (K::Point_3(200, 200, 50),
+                       K::Point_3(220, 220, 80),
+                       K::Point_3(180, 220, 100));
+  gv << K::Tetrahedron_3 (K::Point_3(100, 100, 180),
+                          K::Point_3(120,  70, 220),
+                          K::Point_3(100, 100, 220),
+                          K::Point_3(120, 150, 250));
+  gv << CGAL::Bbox_2(10, 10, 30, 30);
+  gv << CGAL::Bbox_3(10, 10, 10, 30, 30, 30);
 
-  gv << Sph3;
-  gv << P3;
-  gv << P2;
-  gv << S2;
-  gv << S3;
-  gv << Tr2;
-  gv << Tr3;
-  gv << T3;
-
+  gv.look_recenter();
   sleep(30);
 
   return 0;
