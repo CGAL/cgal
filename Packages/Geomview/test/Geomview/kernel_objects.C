0d6f11913750a53261455e56248cab6a3aabd5df
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2000-11-29 12:59:24 +0000

    - Remove stuff that is in demo now.

--- Packages/Geomview/test/Geomview/kernel_objects.C
+++ Packages/Geomview/test/Geomview/kernel_objects.C
@@ -1,7 +1,7 @@
-// Test program for Geomview_stream with kernel objects.  It's mostly a
-// compilation test, I do not verify the output automatically.
+// Simple test program for Geomview_stream.
+// See the demo directory for more extensive use.
 //
-//  Sylvain Pion, 2000.
+// Sylvain Pion, 2000.
 
 #include <CGAL/basic.h>
 
@@ -18,8 +18,6 @@
 
 #include <CGAL/IO/Geomview_stream.h>
 
-#include <unistd.h> // for sleep()
-
 typedef CGAL::Cartesian<double> K;
 
 void test_parse_point()
@@ -33,33 +31,5 @@ void test_parse_point()
 int main()
 {
   test_parse_point();
-
-  CGAL::Geomview_stream gv(CGAL::Bbox_3(0, 0, 0, 350, 350, 350));
-  gv.set_trace(true);
-
-  gv.clear(); // remove pickplane.
-  gv << K::Point_2 (200, 100);
-  gv << K::Point_3 (200, 100, 100);
-  gv << K::Segment_2 (K::Point_2(200, 100),
-                      K::Point_2(300, 100));
-  gv << K::Segment_3 (K::Point_3(200, 100, 100),
-                      K::Point_3(300, 100, 200));
-  gv << K::Sphere_3 (K::Point_3(100, 100, 100), 1000);
-  gv << K::Triangle_2 (K::Point_2(200, 200),
-                       K::Point_2(220, 220),
-                       K::Point_2(180, 220));
-  gv << K::Triangle_3 (K::Point_3(200, 200, 50),
-                       K::Point_3(220, 220, 80),
-                       K::Point_3(180, 220, 100));
-  gv << K::Tetrahedron_3 (K::Point_3(100, 100, 180),
-                          K::Point_3(120,  70, 220),
-                          K::Point_3(100, 100, 220),
-                          K::Point_3(120, 150, 250));
-  gv << CGAL::Bbox_2(10, 10, 30, 30);
-  gv << CGAL::Bbox_3(10, 10, 10, 30, 30, 30);
-
-  gv.look_recenter();
-  sleep(10);
-
   return 0;
 }
