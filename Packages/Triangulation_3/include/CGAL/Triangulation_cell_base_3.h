c1e1a3b9e83e58b674de66f13409f6f398961562
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2000-12-12 15:45:54 +0000

    - cleanup.

--- Packages/Triangulation_3/include/CGAL/Triangulation_cell_base_3.h
+++ Packages/Triangulation_3/include/CGAL/Triangulation_cell_base_3.h
@@ -19,49 +19,36 @@
 //                 (Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>)
 //
 // ============================================================================
-//
+
 // cell of a triangulation of any dimension <=3
-//
-// ============================================================================
 
 #ifndef CGAL_TRIANGULATION_CELL_BASE_3_H
 #define CGAL_TRIANGULATION_CELL_BASE_3_H
 
 #include <CGAL/triangulation_assertions.h>
-
 #include <CGAL/Triangulation_short_names_3.h>
 
 CGAL_BEGIN_NAMESPACE
 
-template < class Gt > class Triangulation_cell_base_3;
-template < class Gt > 
-std::istream& operator >> (std::istream&, Triangulation_cell_base_3<Gt>&);
-
 template < class Gt >
 class Triangulation_cell_base_3
 {
-  friend std::istream& operator >> CGAL_NULL_TMPL_ARGS
-  (std::istream&, Triangulation_cell_base_3<Gt>&);
-
 public:
 
   // CONSTRUCTORS
 
-  inline
   Triangulation_cell_base_3()
   {
     set_vertices();
     set_neighbors();
   }
 
-  inline
   Triangulation_cell_base_3(void* v0, void* v1, void* v2, void* v3)
   {
     set_vertices(v0, v1, v2, v3);
     set_neighbors();
   }
 
-  inline
   Triangulation_cell_base_3(void* v0, void* v1, void* v2, void* v3,
 			    void* n0, void* n1, void* n2, void* n3)
   {
@@ -71,20 +58,17 @@ public:
 
   // ACCESS FUNCTIONS
 
-  inline 
   const void* vertex(int i) const
   {
-    CGAL_triangulation_precondition( i == 0 || i == 1 || i == 2 || i==3 );
+    CGAL_triangulation_precondition( i >= 0 && i <= 3 );
     return V[i];
   } 
 
-  inline 
   bool has_vertex(const void* v) const
   {
     return (V[0] == v) || (V[1] == v) || (V[2]== v) || (V[3]== v);
   }
     
-  inline 
   bool has_vertex(const void* v, int & i) const
     {
       if (v == V[0]) { i = 0; return true; }
@@ -94,7 +78,6 @@ public:
       return false;
     }
     
-  inline 
   int vertex_index(const void* v) const
   {
     if (v == V[0]) { return 0; }
@@ -105,20 +88,17 @@ public:
     return -1;
   }
 
-  inline 
   void* neighbor(int i) const
   {
-    CGAL_triangulation_precondition( i == 0 || i == 1 || i == 2 || i == 3);
+    CGAL_triangulation_precondition( i >= 0 && i <= 3);
     return N[i];
   }
     
-  inline 
   bool has_neighbor(const void* n) const
   {
     return (N[0] == n) || (N[1] == n) || (N[2] == n) || (N[3] == n);
   }
     
-  inline 
   bool has_neighbor(const void* n, int & i) const
   {
     if(n == N[0]){ i = 0; return true; }
@@ -128,7 +108,6 @@ public:
     return false;
   }
     
-  inline 
   int cell_index(const void* n) const
   {
     if (n == N[0]) return 0;
@@ -140,27 +119,23 @@ public:
  
   // SETTING
 
-  inline 
   void set_vertex(int i, void* v)
   {
-    CGAL_triangulation_precondition( i == 0 || i == 1 || i == 2 || i == 3);
+    CGAL_triangulation_precondition( i >= 0 && i <= 3);
     V[i] = v;
   }
     
-  inline 
   void set_neighbor(int i, void* n)
   {
-    CGAL_triangulation_precondition( i == 0 || i == 1 || i == 2 || i == 3);
+    CGAL_triangulation_precondition( i >= 0 && i <= 3);
     N[i] = n;
   }
 
-  inline 
   void set_vertices()
   {
     V[0] = V[1] = V[2] = V[3] = NULL;
   }
     
-  inline 
   void set_vertices(void* v0, void* v1, void* v2, void* v3)
   {
     V[0] = v0;
@@ -169,7 +144,6 @@ public:
     V[3] = v3;
   }
     
-  inline 
   void set_neighbors()
   {
     N[0] = NULL;
@@ -178,7 +152,6 @@ public:
     N[3] = NULL;
   }
     
-  inline
   void set_neighbors(void* n0, void* n1, void* n2, void* n3)
   {
     N[0] = n0;
@@ -192,9 +165,8 @@ public:
   // the following trivial is_valid allows
   // the user of derived cell base classes 
   // to add their own purpose checking
-  bool is_valid(bool , int ) const
+  bool is_valid(bool, int ) const
     {return true;}
-
 
 private:
   void* V[4];
@@ -214,10 +186,6 @@ std::ostream& operator<<
 (std::ostream& os, const Triangulation_cell_base_3<GT> & )
   // non combinatorial information. Default = nothing
 {
-  // ADD THE FOLLOWING WHEN ADDING INFORMATION : 
-//   if(is_ascii(os)) {
-//     os << endl;
-//   }
   return os;
 }
 
