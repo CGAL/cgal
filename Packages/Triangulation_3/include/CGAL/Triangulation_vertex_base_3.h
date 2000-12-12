443a710fc7c30d32d2a19affe4422a1603cd3682
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2000-12-12 15:36:03 +0000

    - cleanup.

--- Packages/Triangulation_3/include/CGAL/Triangulation_vertex_base_3.h
+++ Packages/Triangulation_3/include/CGAL/Triangulation_vertex_base_3.h
@@ -27,58 +27,43 @@
 
 CGAL_BEGIN_NAMESPACE
 
-template < class GT > class Triangulation_vertex_base_3;
-template < class GT >
-std::istream& operator >> (std::istream&, Triangulation_vertex_base_3<GT>&);
-  
 template < class GT >
 class Triangulation_vertex_base_3
 {
-  friend std::istream& operator >> CGAL_NULL_TMPL_ARGS
-  (std::istream&, Triangulation_vertex_base_3<GT>&);
-
 public:
   typedef typename GT::Point_3 Point;
   
   // CONSTRUCTORS
   
-  inline 
   Triangulation_vertex_base_3()
     : _p(), _c(NULL)
   {}
   
-  inline 
   Triangulation_vertex_base_3(const Point & p)
     :  _p(p), _c(NULL)
   {}
     
-  inline 
   Triangulation_vertex_base_3(const Point & p, void* c)
     :  _p(p), _c(c)
   {}
 
-  inline 
   Triangulation_vertex_base_3(void* c)
     :  _p(), _c(c)
   {}
 
   // ACCES 
 
-  inline 
   const Point & point() const
   { return _p; }
     
-  inline 
   void* cell() const
   { return _c; }
 
   // SETTING
 
-  inline 
   void set_point(const Point & p)
   { _p = p; }
     
-  inline 
   void set_cell(void* c)
   { _c = c; }
 
@@ -89,15 +74,12 @@ public:
   // to add their own purpose checking
   bool is_valid(bool, int ) const
   { 
-    //return true; 
-    return( cell() != NULL );
+    return cell() != NULL;
   }
-
 
 private:
   Point _p;
   void * _c;
-  
 };
 
 template < class GT >
@@ -105,93 +87,60 @@ std::istream& operator >>
 (std::istream& is, Triangulation_vertex_base_3<GT> & v)
   // non combinatorial information. Default = point
 {
-  typedef typename GT::Point_3 Point;
-  //  Point p;
-  //  is >> p;
-  is >> v._p;
-  //  v.set_point(p);
+  typename GT::Point_3 p;
+  is >> p;
+  v.set_point(p);
   return is;
 }
+
 template < class GT >
 std::ostream& operator<<
 (std::ostream& os, const Triangulation_vertex_base_3<GT> & v)
   // non combinatorial information. Default = point
 {
   os << v.point();
-  //  if(is_ascii(os)){
-  //    os << endl;
-  //  }
   return os;
 }
 
 template < class GT >
-class Triangulation_vertex_base_pointer_3;
-template < class GT >
-std::istream& operator >> 
-(std::istream&, Triangulation_vertex_base_pointer_3<GT>&);
-
-template < class GT >
 class Triangulation_vertex_base_pointer_3
 {
-  friend std::istream& operator >> CGAL_NULL_TMPL_ARGS
-  (std::istream&, Triangulation_vertex_base_pointer_3<GT>&);
-
 public:
   typedef typename GT::Point_3 Point;
-  //typedef Point Point_3;
   
   // CONSTRUCTORS
   
-  inline 
   Triangulation_vertex_base_pointer_3()
     : _p(NULL), _c(NULL)
   {}
   
-  inline 
   Triangulation_vertex_base_pointer_3(const Point & p)
-    :  _p(&p), _c(NULL)
-  {
-    //    const Point * q =  &p;
-    //    _p = q;
-  }
+    : _p(&p), _c(NULL)
+  {}
     
-  inline 
   Triangulation_vertex_base_pointer_3(const Point & p, void* c)
-    :  _p(p), _c(c)
+    : _p(p), _c(c)
   {}
 
-  inline 
   Triangulation_vertex_base_pointer_3(void* c)
-    :  _p(NULL), _c(c)
+    : _p(NULL), _c(c)
   {}
 
   // ACCES 
 
-  inline 
   const Point & point() const
   { return *_p; }
     
-  inline 
   void* cell() const
   { return _c; }
 
   // SETTING
 
-//   inline 
-//   void set_point(Point * p)
-//   {     
-//     const Point * q =  &p;
-//     p = q; 
-//   }
-    
-  inline 
   void set_point(const Point & p)
   {     
-    //    const Point * q =  &p;
     _p = &p; 
   }
     
-  inline 
   void set_cell(void* c)
   { _c = c; }
 
@@ -202,15 +151,12 @@ public:
   // to add their own purpose checking
   bool is_valid(bool, int ) const
   { 
-    //return true; 
-    return( cell() != NULL );
+    return cell() != NULL;
   }
-
 
 private:
   const Point * _p;
   void * _c;
-  
 };
 
 template < class GT >
@@ -219,21 +165,18 @@ std::istream& operator >>
   // non combinatorial information. Default = point
 { 
   typedef typename GT::Point_3 Point;
-  Point q;
-  is >> q;
-  const Point * p = new Point(q);
-  v._p = p;
+  Point * q = new Point();
+  is >> *q;
+  v.set_point(*q);
   return is;
 }
+
 template < class GT >
 std::ostream& operator<<
 (std::ostream& os, const Triangulation_vertex_base_pointer_3<GT> & v)
   // non combinatorial information. Default = point
 {
   os << v.point();
-  //  if(is_ascii(os)){
-  //    os << endl;
-  //  }
   return os;
 }
 
