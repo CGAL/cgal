152c9d35975471564db85d4058312b7451ee73e6
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2001-02-13 18:12:50 +0000

    - Triangulation_hierarchy<>.  Initial version that "only" compiles.

---
+++ Packages/Triangulation_3/test/Triangulation_3/include/CGAL/_test_cls_delaunay_hierarchy_3.C
@@ -0,0 +1,52 @@
+// ============================================================================
+//
+// Copyright (c) 1998, 2001 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+// 
+// release       :
+// release_date  :
+// 
+// file          : test/Triangulation3/include/CGAL/
+//                 _test_cls_delaunay_hierarchy_3.C
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Mariette Yvinec, Sylvain Pion
+//
+// coordinator   : INRIA Sophia-Antipolis <Mariette Yvinec@sophia.inria.fr>
+// ============================================================================
+
+#include <CGAL/_test_cls_delaunay_3.C>
+#include <CGAL/point_generators_3.h>
+#include <CGAL/algorithm.h>
+
+template <class Dh>
+void
+_test_cls_delaunay_hierarchy_3( const Dh & )
+{
+  typedef Dh  Delaunay_hierarchy;
+  typedef typename Delaunay_hierarchy::Point   Point;
+  typedef CGAL::Creator_uniform_3<double,Point>  Creator;
+
+
+  // makes i686_CYGWINNT-5.0-1.1.4-0.26-3-2_CL.EXE-12.00.8804 crash
+  //_test_cls_delaunay_triangulation_3( Delaunay_hierarchy());
+
+  
+  std::cout << "    insertion removal of 1000 points" << std::endl;
+  Delaunay_hierarchy dh;
+  CGAL::Random_points_in_cube_3<Point,Creator> g(1.);
+  CGAL::copy_n( g, 1000, std::back_inserter(dh));
+
+  dh.locate(Point(0.,0.,0.));
+
+  while( dh.number_of_vertices() >0) {
+    dh.remove(dh.finite_vertices_begin());
+  }
+
+  return;
+}
---
+++ Packages/Triangulation_3/test/Triangulation_3/test_delaunay_hierarchy_3.C
@@ -0,0 +1,60 @@
+// ============================================================================
+//
+// Copyright (c) 1998,2001 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+// 
+// release       :
+// release_date  :
+// 
+// file          : test/Triangulation3/test_delaunay_hierarchy_3.C
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Mariette Yvinec, Sylvain Pion
+//
+// coordinator   : INRIA Sophia-Antipolis
+// ============================================================================
+
+#include <CGAL/basic.h>
+#include <iostream>
+
+#include <CGAL/_test_types.h>
+#include <CGAL/Cartesian.h>
+
+#include <CGAL/Point_3.h>
+#include <CGAL/Segment_3.h>
+#include <CGAL/Triangle_3.h>
+#include <CGAL/Line_3.h>
+#include <CGAL/Direction_3.h>
+#include <CGAL/Ray_3.h>
+
+#include <CGAL/Triangulation_geom_traits_3.h>
+#include <CGAL/Triangulation_data_structure_3.h>
+#include <CGAL/Delaunay_triangulation_3.h>
+#include <CGAL/Triangulation_hierarchy_3.h>
+
+#include <CGAL/_test_cls_delaunay_hierarchy_3.C>
+
+int main()
+{
+  std::cout << "Testing Delaunay_hierarchy_3 " << std::endl; 
+  std::cout << " with Cartesian<double> points "<<  std::endl;
+
+  typedef double                      Coord_type;
+  typedef CGAL::Cartesian<Coord_type> Rep;
+  typedef CGAL::Triangulation_geom_traits_3<Rep> Gt;
+  typedef CGAL::Triangulation_vertex_base_3<Gt> Vbb;
+  typedef CGAL::Triangulation_hierarchy_vertex_base_3<Vbb> Vb;
+  typedef CGAL::Triangulation_cell_base_3<Gt>  Ce;
+  typedef CGAL::Triangulation_data_structure_3<Vb,Ce>  Tdsul;
+  typedef CGAL::Delaunay_triangulation_3<Gt,Tdsul>  Dt;
+  typedef CGAL::Triangulation_hierarchy_3<Dt>  Dh;
+
+  _test_cls_delaunay_hierarchy_3( Dh() );
+
+   return 0;
+}
