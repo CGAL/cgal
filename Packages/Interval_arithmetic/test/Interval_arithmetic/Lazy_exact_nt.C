8a40ad6c783dd4eba3cfc1d3041a9f0f7feeff2e
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2001-03-07 13:15:44 +0000

    - Remove Filtered_exact<>.

--- Packages/Interval_arithmetic/test/Interval_arithmetic/Lazy_exact_nt.C
+++ Packages/Interval_arithmetic/test/Interval_arithmetic/Lazy_exact_nt.C
@@ -5,27 +5,22 @@
 #include <CGAL/Cartesian.h>
 #include <CGAL/Point_2.h>
 #include <CGAL/predicates_on_points_2.h>
-#include <CGAL/Arithmetic_filter.h>
 #include <CGAL/Lazy_exact_nt.h>
+#include <CGAL/MP_Float.h>
 
 #ifdef CGAL_USE_LEDA
 #  include <CGAL/leda_real.h>
 typedef leda_real        Exact_NT;
 #else
-#  include <CGAL/MP_Float.h>
 typedef CGAL::MP_Float   Exact_NT; // doesn't do exact sqrt() though
 #endif
 
 // typedef CGAL::Lazy_exact_nt<int> NT;
-// typedef CGAL::Lazy_exact_nt<Exact_NT> NT;
-typedef CGAL::Filtered_exact<CGAL::Lazy_exact_nt<Exact_NT>, Exact_NT> NT;
+typedef CGAL::Lazy_exact_nt<Exact_NT> NT;
 // typedef Exact_NT NT;
 
 typedef CGAL::Cartesian<NT> Kernel;
 typedef Kernel::Point_2     Point;
-
-using std::cout;
-using std::endl;
 
 void predicates()
 {
@@ -33,15 +28,15 @@ void predicates()
   Point B(NT(2.0)/NT(3),NT(3.0)/NT(3));
   Point C(NT(3.0)/NT(3),NT(4.0)/NT(3));
   Point D(NT(4.0)/NT(3),NT(3.0)/NT(3));
-  cout << "A : " << A << endl;
-  cout << "B : " << B << endl;
-  cout << "C : " << C << endl;
-  cout << (int) CGAL::orientation(A,B,C) << endl;
+  std::cout << "A : " << A << std::endl;
+  std::cout << "B : " << B << std::endl;
+  std::cout << "C : " << C << std::endl;
+  std::cout << (int) CGAL::orientation(A,B,C) << std::endl;
 }
 
 int main ()
 {
-  cout.precision(20);
+  std::cout.precision(20);
   // NT a;
   // NT b(a);
   // NT c = b;
@@ -49,19 +44,27 @@ int main ()
   NT e = d + d;
   NT z = min(e,d);
   (void) d; (void) e; (void) z; // Shut up warnings.
-  cout << e/NT(3) << endl;
+  std::cout << e/NT(3) << std::endl;
   // NT f = abs(NT(0));
-  cout << "sign(3*(1/3)-1) = " << CGAL_NTS sign(NT(3)*(NT(1)/NT(3))-NT(1)) << endl;
-  cout << "sign(sqrt(2)^2-2) = " << CGAL_NTS sign(CGAL_NTS square(CGAL_NTS sqrt(NT(2)))-NT(2)) << endl;
-  // cout << "sign(sqrt(2)) = " << CGAL_NTS sign(CGAL_NTS sqrt(NT(2))) << endl;
-  // cout << "sign(square(2)) = " << CGAL_NTS sign(CGAL_NTS square(NT(2))) << endl;
+  std::cout << "sign(3*(1/3)-1) = " << CGAL_NTS sign(NT(3)*(NT(1)/NT(3))-NT(1))
+            << std::endl;
+  std::cout << "sign(sqrt(2)^2-2) = "
+            << CGAL_NTS sign(CGAL_NTS square(CGAL_NTS sqrt(NT(2)))-NT(2))
+            << std::endl;
+  // std::cout << "sign(sqrt(2)) = " << CGAL_NTS sign(CGAL_NTS sqrt(NT(2))) << std::endl;
+  // std::cout << "sign(square(2)) = " << CGAL_NTS sign(CGAL_NTS square(NT(2))) << std::endl;
   // bool pipo = e < d;
-  cout << "sizeof(rep) = " << sizeof(CGAL::Lazy_exact_rep<int>) << endl;
-  cout << "sizeof(cst) = " << sizeof(CGAL::Lazy_exact_Cst<int>) << endl;
-  cout << "sizeof(abs) = " << sizeof(CGAL::Lazy_exact_Abs<int>) << endl;
-  cout << "sizeof(add) = " << sizeof(CGAL::Lazy_exact_Add<int>) << endl;
-  cout << "sizeof(Exat_NT) = " << sizeof(Exact_NT) << endl;
-  cout << "sizeof(Lazy_exact_nt) = " << sizeof(CGAL::Lazy_exact_nt<int>) << endl;
+  std::cout << "sizeof(rep) = " << sizeof(CGAL::Lazy_exact_rep<int>)
+            << std::endl;
+  std::cout << "sizeof(cst) = " << sizeof(CGAL::Lazy_exact_Cst<int>)
+            << std::endl;
+  std::cout << "sizeof(abs) = " << sizeof(CGAL::Lazy_exact_Abs<int>)
+            << std::endl;
+  std::cout << "sizeof(add) = " << sizeof(CGAL::Lazy_exact_Add<int>)
+            << std::endl;
+  std::cout << "sizeof(Exat_NT) = " << sizeof(Exact_NT) << std::endl;
+  std::cout << "sizeof(Lazy_exact_nt) = " << sizeof(CGAL::Lazy_exact_nt<int>)
+            << std::endl;
   predicates();
   return 0;
 }
