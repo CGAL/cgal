0c862165bf725c7cb8084239377bb8cf59465191
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2001-04-19 14:45:00 +0000

    - Update for the new simple kernels.

--- Packages/Triangulation_2/include/CGAL/Regular_triangulation_euclidean_traits_2.h
+++ Packages/Triangulation_2/include/CGAL/Regular_triangulation_euclidean_traits_2.h
@@ -32,13 +32,12 @@
 #error  no representation class defined
 #endif  // CGAL_REP_CLASS_DEFINED
 
-#if defined CGAL_CARTESIAN_H || defined CGAL_SIMPLE_CARTESIAN_V2_H || \
-    defined CGAL_SIMPLE_CARTESIAN_H
+#if defined CGAL_CARTESIAN_H || defined CGAL_SIMPLE_CARTESIAN_H
 #include <CGAL/predicates/Regular_triangulation_ftC2.h>
 #include <CGAL/constructions_on_weighted_points_cartesian_2.h>
 #endif
 
-#ifdef CGAL_HOMOGENEOUS_H
+#if defined CGAL_HOMOGENEOUS_H || defined CGAL_SIMPLE_HOMOGENEOUS_H
 #include <CGAL/predicates/Regular_triangulation_rtH2.h>
 #include <CGAL/constructions_on_weighted_points_homogeneous_2.h>
 #endif
