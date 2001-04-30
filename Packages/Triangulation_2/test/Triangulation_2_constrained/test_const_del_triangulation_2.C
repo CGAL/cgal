674248c8d28d0bdc1b1ed7bde322750cf8840a21
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2001-04-30 14:30:44 +0000

    fixed the test of Constrained Delaunay triangulation

--- Packages/Triangulation_2/test/Triangulation_2_constrained/test_const_del_triangulation_2.C
+++ Packages/Triangulation_2/test/Triangulation_2_constrained/test_const_del_triangulation_2.C
@@ -31,39 +31,39 @@
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
 
 #include <CGAL/_test_types.C>
-#include <CGAL/_test_cls_constrained_triangulation_2.C>
+#include <CGAL/_test_cls_const_Del_triangulation_2.C>
 
 int main()
 {
-  std::cout << "Testing constrained_Delaunay_triangulation "<< std::endl;
-  std::cout << " with Triangulation_euclidean_traits_2 : " << std::endl;
-  std::cout << " and double coordinates " << std::endl;
+//   std::cout << "Testing constrained_Delaunay_triangulation "<< std::endl;
+//   std::cout << " with Triangulation_euclidean_traits_2 : " << std::endl;
+//   std::cout << " and double coordinates " << std::endl;
 
-  typedef double coord_type;
-  typedef CGAL::Cartesian<coord_type>  Rep;
-  typedef CGAL::Triangulation_euclidean_traits_2<Rep>            Gt;
-  typedef CGAL::Triangulation_vertex_base_2<Gt>                  Vb;
-  typedef CGAL::Constrained_triangulation_face_base_2<Gt>        CFb;
-  typedef CGAL::Triangulation_default_data_structure_2<Gt,Vb,CFb> Tds;
-  typedef CGAL::Constrained_Delaunay_triangulation_2<Gt,Tds>         CDt;
+//   typedef double coord_type;
+//   typedef CGAL::Cartesian<coord_type>  Rep;
+//   typedef CGAL::Triangulation_euclidean_traits_2<Rep>            Gt;
+//   typedef CGAL::Triangulation_vertex_base_2<Gt>                  Vb;
+//   typedef CGAL::Constrained_triangulation_face_base_2<Gt>        CFb;
+//   typedef CGAL::Triangulation_default_data_structure_2<Gt,Vb,CFb> Tds;
+//   typedef CGAL::Constrained_Delaunay_triangulation_2<Gt,Tds>         CDt;
 
-  _test_cls_constrained_triangulation(CDt());
+//   _test_cls_cons_Del_triangulation(CDt());
 
-  std::cout << "Testing constrained_Delaunay_triangulation "<< std::endl;
-  std::cout << " with Triangulation_data_structure_using_list : " << std::endl;
-  typedef CGAL::Triangulation_data_structure_using_list_2<Vb,CFb> Tds1;
-  typedef CGAL::Constrained_Delaunay_triangulation_2<Gt, Tds1>         CDt1;
+//   std::cout << "Testing constrained_Delaunay_triangulation "<< std::endl;
+//   std::cout << " with Triangulation_data_structure_using_list : " << std::endl;
+//   typedef CGAL::Triangulation_data_structure_using_list_2<Vb,CFb> Tds1;
+//   typedef CGAL::Constrained_Delaunay_triangulation_2<Gt, Tds1>         CDt1;
 
-  _test_cls_constrained_triangulation(CDt1());
+//   _test_cls_const_Del__triangulation(CDt1());
 
   std::cout << "Testing constrained_triangulation "<< std::endl;
   std::cout << " with Kernel traits Cartesian<double> : " << std::endl;
   typedef CGAL::Cartesian<double>                                    Gt2;
   typedef CGAL::Triangulation_vertex_base_2<Gt2>                     Vb2;
   typedef CGAL::Constrained_triangulation_face_base_2<Gt2>           CFb2;
-  typedef CGAL::Triangulation_default_data_structure_2<Gt2,Vb2,CFb2> Tds2;
-  typedef CGAL::Constrained_Delaunay_triangulation_2<Gt,Tds2>          CDt2;
+  typedef CGAL::Triangulation_data_structure_using_list_2<Vb2,CFb2>  Tds2;
+  typedef CGAL::Constrained_Delaunay_triangulation_2<Gt2,Tds2>        CDt2;
 
-  _test_cls_constrained_triangulation(CDt2());
+  _test_cls_const_Del_triangulation(CDt2());
  return 0;
 }
