ffb0c243a09bfd687dcd0f64c22b0931c4c4d852
Shai Hirsch <shaihi@post.tau.ac.il>
2001-05-02 14:51:08 +0000

    Changed leda_rational to Quotient<int> and did some cosmetic changes.

--- Packages/Planar_map/examples/Planar_map/example10.C
+++ Packages/Planar_map/examples/Planar_map/example10.C
@@ -1,8 +1,8 @@
-#include <CGAL/basic.h> //CGAL definitions that need to be before anything
-#include <iostream>
-#include<string>
+// example10
 
-#include <CGAL/leda_rational.h>
+#include <CGAL/basic.h> //CGAL definitions that need to come before anything
+
+#include <CGAL/Quotient.h>
 #include <CGAL/Cartesian.h>
 
 #include <CGAL/Pm_default_dcel.h>
@@ -10,8 +10,8 @@
 #include <CGAL/Pm_segment_exact_traits.h>
 
 #include <CGAL/IO/Pm_iostream.h>
-
-
+#include <iostream>
+#include<string>
 
 CGAL_BEGIN_NAMESPACE
 
@@ -31,15 +31,16 @@ private:
 
 // building new dcel with my vertex base.
 template <class Traits>
-class Pm_my_dcel : public Pm_dcel<Pm_my_vertex<typename Traits::Point>,
-                                       Pm_halfedge_base<typename Traits::X_curve>, 
-                                       Pm_face_base> 
+class Pm_my_dcel : 
+  public Pm_dcel<Pm_my_vertex<typename Traits::Point>,
+                 Pm_halfedge_base<typename Traits::X_curve>, 
+                 Pm_face_base> 
 {
-public:  // CREATION
+public:  // Creation
   Pm_my_dcel() {}
 };
 
-// extend the drarwer for printing the color in addition. 
+// extend the drawer to print the color as well. 
 template <class PM>
 class Pm_my_file_writer:  public Pm_file_writer<PM> {
 public:
@@ -49,8 +50,8 @@ public:
   typedef typename PM::Vertex_iterator           Vertex_iterator;
   typedef typename PM::Vertex_const_iterator     Vertex_const_iterator;
 
-  Pm_my_file_writer(std::ostream& o, const PM& pm, bool verbose = false) : Pm_file_writer<PM>(o, pm, verbose){}
-
+  Pm_my_file_writer(std::ostream& o, const PM& pm, bool verbose = false) : 
+    Pm_file_writer<PM>(o, pm, verbose) {}
   
   void write_vertex(Vertex_handle v) {
     out() << v->point() <<"  ";
@@ -62,39 +63,44 @@ public:
     out() << v->get_color()<< std::endl;
   }
    
-  void write_vertices(Vertex_iterator Vertices_begin, Vertex_iterator Vertices_end) {
-    for (Vertex_iterator v_iter = Vertices_begin; v_iter !=  Vertices_end; v_iter++)
+  void write_vertices(Vertex_iterator Vertices_begin, 
+		      Vertex_iterator Vertices_end) {
+    for (Vertex_iterator v_iter = Vertices_begin; 
+	 v_iter != Vertices_end;
+	 v_iter++)
       write_vertex(v_iter);
   }
   
-  void write_vertices(Vertex_const_iterator Vertices_begin, Vertex_const_iterator Vertices_end) {
-    for (Vertex_const_iterator v_iter = Vertices_begin; v_iter !=  Vertices_end; v_iter++)
+  void write_vertices(Vertex_const_iterator Vertices_begin, 
+		      Vertex_const_iterator Vertices_end) {
+    for (Vertex_const_iterator v_iter = Vertices_begin;
+	 v_iter !=  Vertices_end; 
+	 v_iter++)
       write_vertex(v_iter);
   }
 };
 
 CGAL_END_NAMESPACE
 
-typedef leda_rational                      NT;
+typedef CGAL::Quotient<int>                NT;
 typedef CGAL::Cartesian<NT>                R;
 typedef CGAL::Pm_segment_exact_traits<R>   Traits;
-
 
 typedef CGAL::Pm_my_dcel<Traits>           Dcel;
 typedef CGAL::Planar_map_2<Dcel,Traits>    PM;
 
-
 typedef PM::Vertex_iterator                Vertex_iterator;
 //typedef PM::Vertex_const_iterator          Vertex_const_iterator;
 
-
 int main(int argc, char* argv[])
 {
-  PM    pm;
-  cin>>pm;
+  PM pm;
+  cin >> pm;
  
   // updating the colors for halfedge and vertex.
-  for (Vertex_iterator v_iter = pm.vertices_begin(); v_iter != pm.vertices_end(); v_iter++)
+  for (Vertex_iterator v_iter = pm.vertices_begin(); 
+       v_iter != pm.vertices_end(); 
+       v_iter++)
     v_iter->set_color("BLUE");
 
  //printing pm to output stream with the user attributes.
@@ -103,4 +109,3 @@ int main(int argc, char* argv[])
 
   return 0;
 }
-
