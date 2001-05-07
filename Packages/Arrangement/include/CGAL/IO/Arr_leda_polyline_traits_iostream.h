9f7aab867cb8d471ea165dbfa465844a91f9e78e
Ester Ezra <estere@post.tau.ac.il>
2001-05-07 16:34:17 +0000

    Defines an output operator for writing a polyline to the standard output.

---
+++ Packages/Arrangement/include/CGAL/IO/Arr_leda_polyline_traits_iostream.h
@@ -0,0 +1,38 @@
+// ======================================================================
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+//
+// release       : $CGAL_Revision: CGAL-2.3-I-44 $
+// release_date  : $CGAL_Date: 2001/03/09 $
+//
+// file          : include/CGAL/IO/Arr_leda_polyline_traits_iostream.h 
+// package       : Arrangement (1.81)
+// maintainer    : Eyal Flato <flato@math.tau.ac.il>
+// source        : 
+// revision      : 
+// revision_date : 
+// author(s)     : Eti Ezra <estere@post.tau.ac.il>
+//
+//
+// coordinator   : Tel-Aviv University (Dan Halperin <halperin@math.tau.ac.il>)
+//
+// Chapter       : 
+// ======================================================================
+#ifdef CGAL_ARR_LEDA_POLYLINE_TRAITS_H
+#ifndef CGAL_ARR_LEDA_POLYLINE_TRAITS_IOSTREAM_H   
+#define CGAL_ARR_LEDA_POLYLINE_TRAITS_IOSTREAM_H  
+
+#include <CGAL/IO/Arr_polyline_traits_iostream.h>
+
+#endif
+#endif 
+
+
+
+
+
+
