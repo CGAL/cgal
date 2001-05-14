7214ca5d39127dccb879c0eea37905567d149089
Ester Ezra <estere@post.tau.ac.il>
2001-05-14 16:48:22 +0000

    Adding the missting '_' in the including of the Arrangement.

--- Packages/Arrangement/include/CGAL/IO/Arr_Geomview_stream.h
+++ Packages/Arrangement/include/CGAL/IO/Arr_Geomview_stream.h
@@ -1,5 +1,7 @@
 // ======================================================================
 //
+// Copyright (c) 1997 The CGAL Consortium
+//
 // This software and related documentation is part of an INTERNAL release
 // of the Computational Geometry Algorithms Library (CGAL). It is not
 // intended for general use.
@@ -10,7 +12,7 @@
 // release_date  : $CGAL_Date: 2001/03/09 $
 //
 // file          : include/CGAL/IO/Arr_Geomview_stream.h
-// package       : Arrangement (1.81)
+// package       : pm (1.81)
 // maintainer    : Eyal Flato <flato@math.tau.ac.il>
 // source        : 
 // revision      : 
@@ -27,7 +29,7 @@
 #define CGAL_IO_ARR_GEOMVIEW_STREAM_H
 
 #ifndef CGAL_ARRANGEMENT_2_H
-#include <CGAL/Arrangement2.h>
+#include <CGAL/Arrangement_2.h>
 #endif
 
 #ifndef CGAL_GEOMVIEW_STREAM_H
