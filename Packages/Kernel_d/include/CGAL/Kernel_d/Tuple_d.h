7ac2aaceccc2dae4b88c71ef5620c1857e70a5a5
Michael Seel <seel@mpi-sb.mpg.de>
2001-05-16 11:42:57 +0000

    added diverse header files

--- Packages/Kernel_d/include/CGAL/Kernel_d/Tuple_d.h
+++ Packages/Kernel_d/include/CGAL/Kernel_d/Tuple_d.h
@@ -1,3 +1,25 @@
+// ======================================================================
+//
+// Copyright (c) 2000,2001 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+//
+// release       : $CGAL_Revision$
+// release_date  : $CGAL_Date$
+//
+// file          : include/CGAL/Kernel_d/Tuple_d.h
+// package       : Kernel_d
+// maintainer    : Michael Seel <Michael.Seel@mpi-sb.mpg.de>
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Michael Seel
+// coordinator   : MPI Saarbruecken (Susan.Hert@mpi-sb.mpg.de)
+//
+// ======================================================================
 #ifndef CGAL_TUPLE_D_H
 #define CGAL_TUPLE_D_H
 
--- Packages/Kernel_d/include/CGAL/Kernel_d/VectorHd.h
+++ Packages/Kernel_d/include/CGAL/Kernel_d/VectorHd.h
@@ -1,3 +1,25 @@
+// ======================================================================
+//
+// Copyright (c) 2000,2001 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+//
+// release       : $CGAL_Revision$
+// release_date  : $CGAL_Date$
+//
+// file          : include/CGAL/Kernel_d/VectorHd.h
+// package       : Kernel_d
+// maintainer    : Michael Seel <Michael.Seel@mpi-sb.mpg.de>
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Michael Seel
+// coordinator   : MPI Saarbruecken (Susan.Hert@mpi-sb.mpg.de)
+//
+// ======================================================================
 #ifndef CGAL_VECTORHD_H
 #define CGAL_VECTORHD_H 
 
