bb0019fe95a11b3f1191f427bbfcc243e9c7dfdd
Ester Ezra <estere@post.tau.ac.il>
2001-05-24 18:35:35 +0000

    Adding header.

--- Packages/Arrangement/include/CGAL/sweep_to_produce_planar_map_subcurves.h
+++ Packages/Arrangement/include/CGAL/sweep_to_produce_planar_map_subcurves.h
@@ -1,3 +1,30 @@
+// ======================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+//
+// release       : $CGAL_Revision: CGAL-2.3-I-44 $
+// release_date  : $CGAL_Date: 2001/03/09 $
+//
+// file          : include/CGAL/sweep_to_produce_planar_map_subcurves.h
+// package       : arr (1.87)
+// maintainer    : Eyal Flato <flato@math.tau.ac.il>
+// source        : 
+// revision      : 
+// revision_date : 
+// author(s)     : Eti Ezra <estere@post.tau.ac.il>
+//
+//
+// coordinator   : Tel-Aviv University (Dan Halperin <halperin@math.tau.ac.il>)
+//
+// Chapter       : 
+// ======================================================================
+
 #ifndef SWEEP_TO_PRODUCE_PLANAR_MAP_SUBCURVES_H
 #define SWEEP_TO_PRODUCE_PLANAR_MAP_SUBCURVES_H
 
