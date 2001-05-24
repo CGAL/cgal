ea4d8a3ddf52c0b502e1f9f5e174f82481167153
Susan Hert <hert@mpi-sb.mpg.de>
2001-05-24 11:10:55 +0000

    added testing include files

---
+++ Packages/Convex_hull_2/examples/Convex_hull_2/include/CGAL/ch_timing_2.h
@@ -0,0 +1,43 @@
+// ============================================================================
+//
+// Copyright (c) 1999 The CGAL Consortium
+//
+// 
+// 
+// 
+//
+// ----------------------------------------------------------------------------
+// release       :
+// release_date  :
+//
+// file          : examples/Convex_hull_2/include/CGAL/ch_timing_2.h
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Stefan Schirra
+//
+// coordinator   : MPI, Saarbruecken
+// ============================================================================
+
+
+#ifndef CGAL_CH_TIMING_2_H
+#define CGAL_CH_TIMING_2_H
+
+#include <CGAL/Timer.h>
+
+
+CGAL_BEGIN_NAMESPACE
+template <class ForwardIterator, class OutputIterator, class Traits>
+void
+ch_timing( ForwardIterator first, ForwardIterator last,
+           OutputIterator result,
+           int iterations, 
+           const Traits& ch_traits);
+
+CGAL_END_NAMESPACE
+
+#ifdef CGAL_CFG_NO_AUTOMATIC_TEMPLATE_INCLUSION
+#include <CGAL/ch_timing_2.C>
+#endif // CGAL_CFG_NO_AUTOMATIC_TEMPLATE_INCLUSION
+
+#endif // CGAL_CH_TIMING_2_H
+
