7dd9d8df30e7b86a723653e808ac0b6fcf64b158
Shai Hirsch <shaihi@post.tau.ac.il>
2001-05-30 12:39:13 +0000

    Eyal introduces intersect_base to speed up traits class. do_intersect_to_left and nearest_intersection_to_left are introduced under a special flag.

--- Packages/Arrangement/include/CGAL/Arr_leda_segment_exact_traits.h
+++ Packages/Arrangement/include/CGAL/Arr_leda_segment_exact_traits.h
@@ -13,14 +13,15 @@
 //
 // file          : include/CGAL/Arr_leda_segment_exact_traits.h
 // package       : arr (1.39)
-// maintainer    : Sigal Raab <raab@math.tau.ac.il>
+// maintainer    : Eyal Flato <flato@post.tau.ac.il>
 // author(s)     : Iddo Hanniel
+//                 Eyal Flato <flato@post.tau.ac.il>
+//
 // coordinator   : Tel-Aviv University (Dan Halperin <halperin@math.tau.ac.il>)
 //
 // ======================================================================
 #ifndef CGAL_ARR_LEDA_SEGMENT_EXACT_TRAITS
 #define CGAL_ARR_LEDA_SEGMENT_EXACT_TRAITS
-
 
 #include <list>
 
@@ -28,6 +29,8 @@
 
 CGAL_BEGIN_NAMESPACE
 
+#define CGAL_XT_SINGLE_POINT 1
+#define	CGAL_XT_ORIGINAL_POINT 2
 
 class Arr_leda_segment_exact_traits 
         : public Pm_leda_segment_exact_traits
@@ -50,7 +53,7 @@ public:
   void make_x_monotone(const Curve& cv, std::list<Curve>& l) {} 
 
   X_curve curve_flip(const X_curve& cv) const {
-    return X_curve(curve_target(cv), curve_source(cv));
+	return cv.reversal();
   }
 
   void curve_split(const X_curve& cv, X_curve& c1, X_curve& c2, 
@@ -62,34 +65,51 @@ public:
 	//CGAL_precondition(curve_source(cv)!=split_pt);
     //CGAL_precondition(curve_target(cv)!=split_pt);
     
-    c1=X_curve(curve_source(cv),split_pt);
-    c2=X_curve(split_pt,curve_target(cv));
+    c1=X_curve(cv.source(),split_pt);
+    c2=X_curve(split_pt,cv.target());
   }
 
+
+public:
 
   //returns true iff the intersection is strictly right of pt
   bool do_intersect_to_right(const X_curve& c1, const X_curve& c2,
                              const Point& pt) const 
   {
+    return intersection_base(c1, c2, pt, true, false, dummy_pnt1, dummy_pnt2, 
+			     dummy_int);
 
-    X_curve xcv;
-    bool res = c1.intersection(c2, xcv);
-    if (!res) return false;
+    // Following implementation was commented out during to the 
+    // introduction of intersection_base by Eyal to speed up the traits class.
+    /*	if (!c1.intersection(c2))
+	return false;
+	X_curve xcv;
+	bool res = c1.intersection(c2, xcv);
+	if (!res) return false;
     
-    if (lexicographically_xy_larger(xcv.source(),pt) || 
+	if (lexicographically_xy_larger(xcv.source(),pt) || 
         lexicographically_xy_larger(xcv.target(),pt))
-      return true;
+	return true;
     
-    return false;
+	return false;
+    */
   }
 
-
+  
   bool nearest_intersection_to_right(const X_curve& c1,
                                      const X_curve& c2,
                                      const Point& pt,
                                      Point& p1,
-                                     Point& p2) const {
-    X_curve xcv;
+                                     Point& p2) const 
+  {
+    bool res = intersection_base(c1, c2, pt, true, true, p1, p2, dummy_int);
+	if ((res) && (dummy_int & CGAL_XT_SINGLE_POINT))
+		p2 = p1;
+	return res;
+
+    // Following implementation was commented out during to the 
+    // introduction of intersection_base by Eyal to speed up the traits class.
+/*    X_curve xcv;
     bool res = c1.intersection(c2, xcv);
     if (!res) return false;
 
@@ -105,9 +125,10 @@ public:
       return true;
     }
 
-    return false;
+    return false; */
   }
 
+#ifndef CGAL_PMWX_TRAITS_HAVE_INTERSECT_TO_LEFT
 
   X_curve curve_reflect_in_x_and_y (const X_curve& cv) const
   {
@@ -123,6 +144,55 @@ public:
     return reflected_pt;
   }
       
+#else
+  bool do_intersect_to_left(const X_curve& c1, const X_curve& c2,
+			    const Point& pt) const 
+  {
+    return intersection_base(c1, c2, pt, false, false, dummy_pnt1, dummy_pnt2,
+			     dummy_int);
+    /*	if (!c1.intersection(c2))
+	return false;
+	X_curve xcv;
+	bool res = c1.intersection(c2, xcv);
+	if (!res) return false;
+		
+	if (lexicographically_xy_smaller(xcv.source(),pt) || 
+	lexicographically_xy_smaller(xcv.target(),pt))
+	return true;
+		
+	return false;*/
+  }
+
+  bool nearest_intersection_to_left(const X_curve& c1,
+                                     const X_curve& c2,
+                                     const Point& pt,
+                                     Point& p1,
+                                     Point& p2) const 
+  {
+    bool res = intersection_base(c1, c2, pt, false, true, p1, p2, dummy_int);
+	if ((res) && (dummy_int & CGAL_XT_SINGLE_POINT))
+		p2 = p1;
+	return res;
+    /*X_curve xcv;
+    bool res = c1.intersection(c2, xcv);
+    if (!res) return false;
+
+    if (lexicographically_xy_smaller(xcv.source(),xcv.target()))
+      xcv=curve_flip(xcv);
+    if (lexicographically_xy_smaller(xcv.target(),pt)) {
+      p2=point_normalize(xcv.target());
+      if (lexicographically_xy_smaller(xcv.source(),pt))
+        p1=point_normalize(xcv.source());
+      else
+        p1=pt;
+      
+      return true;
+    }
+
+    return false;*/
+  }
+
+#endif
 
   bool curves_overlap(const X_curve& ca, const X_curve& cb) const {
     X_curve xcv;
@@ -130,6 +200,211 @@ public:
     ca.intersection(cb, xcv);
     return !(xcv.is_trivial());
   }
+
+
+  // returns values in p1 and p2 only if return_intersection is true
+  // if (xsect_type | CGAL_XT_SINGLE_POINT) then only p1 is returned
+  bool intersection_base(const X_curve& c1, const X_curve& c2,
+			 const Point& pt, bool right, bool return_intersection,
+			 Point &p1, Point &p2, int &xsect_type) const 
+  {
+    xsect_type = 0;
+    if ( c1.is_trivial() )
+      { 
+	if ( c2.contains(c1.source()) )
+	  { 
+	    if (right)
+	      {
+		if ( lexicographically_xy_larger(c1.source(),pt) )
+		  {
+		    // intersection is c1.source()
+		    xsect_type = CGAL_XT_SINGLE_POINT | CGAL_XT_ORIGINAL_POINT;
+		    if (return_intersection)
+		      {
+			p1 = c1.source();
+			//p2 = p1;
+		      }	
+		    return true; 
+		  }
+	      }
+	    else
+	      {
+		if ( lexicographically_xy_smaller(c1.source(),pt) )
+		  {
+		    // intersection is c1.source()
+		    xsect_type = CGAL_XT_SINGLE_POINT | CGAL_XT_ORIGINAL_POINT;
+		    if (return_intersection)
+		      {
+			p1 = c1.source();
+			//p2 = p1;
+		      }	
+		    return true; 
+		  }
+	      }
+	  }
+	else
+	  {
+	    return false;
+	  }
+      }
+	  
+    if ( c2.is_trivial() )
+      { 
+	if ( c1.contains(c2.source()) )
+	  { 
+	    if (right)
+	      {
+		if ( lexicographically_xy_larger(c2.source(),pt) )
+		  {
+		    // intersection is c2.source()
+		    xsect_type = CGAL_XT_SINGLE_POINT | CGAL_XT_ORIGINAL_POINT;
+		    if (return_intersection)
+		      {
+			p1 = c2.source();
+			//p2 = p1;
+		      }	
+		    return true; 
+		  }
+	      }
+	    else
+	      {
+		if ( lexicographically_xy_smaller(c2.source(),pt) )
+		  {
+		    // intersection is c2.source()
+		    xsect_type = CGAL_XT_SINGLE_POINT | CGAL_XT_ORIGINAL_POINT;
+		    if (return_intersection)
+		      {
+			p1 = c2.source();
+			//p2 = p1;
+		      }	
+		    return true; 
+		  }
+	      }
+	  }
+	else
+	  {
+	    return false;
+	  }
+      }
+	  
+    int o1 = ::orientation(c1,c2.start()); 
+    int o2 = ::orientation(c1,c2.end());
+	  
+    if ( o1 == 0 && o2 == 0 )
+      { 
+	leda_rat_point sa = c1.source(); 
+	leda_rat_point sb = c1.target();
+	if ( ::compare (sa,sb) > 0 )
+	  { 
+	    leda_rat_point h = sa; 
+	    sa = sb; 
+	    sb = h; 
+	  }
+		  
+	leda_rat_point ta = c2.source(); 
+	leda_rat_point tb = c2.target();
+		  
+	if ( ::compare (ta,tb) > 0 )
+	  { 
+	    leda_rat_point h = ta; 
+	    ta = tb; 
+	    tb = h; 
+	  }
+		  
+	leda_rat_point a = sa;
+	if (::compare(sa,ta) < 0) 
+	  a = ta;
+		  
+	leda_rat_point b = tb; 
+	if (::compare(sb,tb) < 0) 
+	  b = sb;
+		  
+	if ( ::compare(a,b) <= 0 )
+	  { 
+	    // a is left-low to b
+	    if (right)
+	      {
+		//intersection (not to the right) is rat_segment(a,b);
+		if (lexicographically_xy_larger(b,pt))
+		  {
+		    xsect_type = 0;
+		    if (return_intersection)
+		      {
+			//if (b_right) 
+			p2 = point_normalize(b);
+			if (lexicographically_xy_larger(a,pt))
+			  p1 = point_normalize(a);
+			else
+			  p1 = pt;
+		      }	
+		    return true;
+		  }
+	      }
+	    else
+	      {
+		//intersection (not to the right) is rat_segment(a,b);
+		if (lexicographically_xy_smaller(a,pt))
+		  {
+		    xsect_type = 0;
+		    if (return_intersection)
+		      {
+			p2 = point_normalize(a);
+			if (lexicographically_xy_smaller(b,pt))
+			  p1 = point_normalize(b);
+			else
+			  p1 = pt;
+		      }	
+		    return true;
+		  }
+	      }
+	  }
+	return false;
+      }
+
+    int o3 = ::orientation(c2,c1.start());
+    int o4 = ::orientation(c2,c1.end());
+	  
+    if ( o1 != o2 && o3 != o4 )
+      { 
+	leda_integer w  = c1.dy()*c2.dx() - c2.dy()*c1.dx();
+	leda_integer m1 = c1.X2()*c1.Y1() - c1.X1()*c1.Y2();
+	leda_integer m2 = c2.X2()*c2.Y1() - c2.X1()*c2.Y2();
+		  
+	leda_rat_point p(m2*c1.dx()-m1*c2.dx(), m2*c1.dy()-m1*c2.dy(), w);
+	if (right)
+	  {
+	    if (lexicographically_xy_larger(p,pt) )
+	      {
+		//intersection is rat_segment(p,p);
+		if (return_intersection)
+		  {
+		    xsect_type = CGAL_XT_SINGLE_POINT;
+		    p1 = point_normalize(p);
+		    //p2 = p1;
+		  }	
+		return true;
+	      }
+	  }
+	else
+	  {
+	    if (lexicographically_xy_smaller(p,pt) )
+	      {
+		//intersection is rat_segment(p,p);
+		if (return_intersection)
+		  {
+		    xsect_type = CGAL_XT_SINGLE_POINT;
+		    p1 = point_normalize(p);
+		    //p2 = p1;
+		  }	
+		return true;
+	      }
+	  }
+      }
+
+    return false;
+  }
+
+
 
 
 private:
@@ -151,11 +426,14 @@ private:
     }
 
   }
-  
+
+  // Dummies  
+mutable leda_rat_point dummy_pnt1, dummy_pnt2;
+mutable int dummy_int;
 
 public:
 
-  //in future versions of LEDA these operators should be defined
+//in future versions of LEDA these operators should be defined
 // friend inline
 // Window_stream& operator<<(Window_stream& os, const Point& p){
 //     return os << leda_point(p.xcoordD(),p.ycoordD());
@@ -172,8 +450,3 @@ public:
 CGAL_END_NAMESPACE
 
 #endif
-
-
-
-
-
