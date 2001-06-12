635810777d9ad8cfc3cbb342a2a198a1af670190
Matthias Bäsken <Matthias.Baesken@mpi-sb.mpg.de>
2001-06-12 15:23:58 +0000

    *** empty log message ***

--- Packages/cgal_window/include/CGAL/LEDA/pixmaps/texture.h
+++ Packages/cgal_window/include/CGAL/LEDA/pixmaps/texture.h
@@ -9,14 +9,16 @@
 // ----------------------------------------------------------------------
 //
 // release       : 
-// release_date  : 2001, May 23
+// release_date  : 2001, June 12
 //
-// file          : ?/include/CGAL/LEDA/pixmaps/texture.h
-// package       : cgal_window (0.9.7)
+// file          : include/CGAL/LEDA/pixmaps/texture.h
+// package       : cgal_window (0.9.8)
 // maintainer    : Matthias Baesken <baesken@informatik.uni-trier.de>
-// author(s)     : ?
-// coordinator   : ?
+// revision      : 0.9.8
+// revision_date : 12 June 2001
+// author(s)     : Matthias Baesken, Algorithmic Solutions
 //
+// coordinator   : Matthias Baesken, Trier  (<baesken@informatik.uni-trier.de>)
 // ======================================================================
 #include <CGAL/LEDA/pixmaps/texture/blue.xpm>
 #include <CGAL/LEDA/pixmaps/texture/clouds.xpm>
