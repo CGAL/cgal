67b44e4ab0e9342ac033b844dc930c52d205af93
Matthias Bäsken <Matthias.Baesken@mpi-sb.mpg.de>
2001-06-19 15:51:40 +0000

    *** empty log message ***

---
+++ Packages/cgal_window/test/cgal_window/bitmap.C
@@ -0,0 +1,54 @@
+#if defined(CGAL_USE_CGAL_WINDOW)
+
+#include <CGAL/LEDA/window.h>
+#include <CGAL/LEDA/bitmaps/button32/voro.xbm>
+#include <list>
+
+using namespace std;
+
+/*
+#if defined(__KCC)
+list<CGAL::window_point> L1;
+list<std::string>        L2;
+#endif
+*/
+
+int main()
+{ 
+  CGAL::window W(400,400);
+  W.set_bg_color(CGAL::yellow);
+  W.display();
+
+  // construct bitmap from the bitmap data in 
+  // <CGAL/LEDA/bitmaps/button32/voro.xbm>
+
+  char* bm = W.create_bitmap(32,
+                             32, 
+                             voro_bits);
+
+  // copy copies of bm into the window
+
+  CGAL::window_point p;
+  while (W.read_mouse(p) != MOUSE_BUTTON(3)) {
+    W.put_bitmap(p.xcoord(),p.ycoord(),bm,CGAL::blue);
+  }
+
+  W.del_bitmap(bm);
+
+  //W.screenshot("bitmap");
+  return 0;
+}
+
+#else
+#include <iostream>
+
+int main()
+{
+ std::cout << "CGAL::window is not available !\n";
+
+ return 0;
+}
+
+#endif
+
+
