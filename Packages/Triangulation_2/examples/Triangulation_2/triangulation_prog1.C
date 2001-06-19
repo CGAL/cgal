4e896bb5e710c1cd56761b4227cc7f68521e25f4
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2001-06-19 17:25:40 +0000

    improve triangulation_prog1.C

--- Packages/Triangulation_2/examples/Triangulation_2/triangulation_prog1.C
+++ Packages/Triangulation_2/examples/Triangulation_2/triangulation_prog1.C
@@ -4,25 +4,23 @@
 #include <CGAL/Triangulation_2.h>
 #include <CGAL/Triangulation_euclidean_traits_2.h>
 
-using namespace CGAL;
-
-typedef Cartesian<double> Rp;
-typedef Triangulation_euclidean_traits_2<Rp> Gt;
-typedef Triangulation_vertex_base_2<Gt> Vb;
-typedef Triangulation_face_base_2<Gt>  Fb;
-typedef Triangulation_default_data_structure_2<Gt,Vb,Fb> Tds;
-typedef Triangulation_2<Gt, Tds> Triangulation;
+typedef CGAL::Cartesian<double> Rp;
+typedef CGAL::Triangulation_euclidean_traits_2<Rp> Gt;
+typedef CGAL::Triangulation_vertex_base_2<Gt> Vb;
+typedef CGAL::Triangulation_face_base_2<Gt>  Fb;
+typedef CGAL::Triangulation_default_data_structure_2<Gt,Vb,Fb> Tds;
+typedef CGAL::Triangulation_2<Gt, Tds> Triangulation;
 typedef Triangulation::Vertex_circulator Vertex_circulator;
 typedef Gt::Point_2   Point;
 
 int main() {
+  std::ifstream in("data/triangulation_prog1.cin");
+  std::istream_iterator<Point> begin(in);
+  std::istream_iterator<Point> end;
+
   Triangulation t;
+  t.insert(begin, end);
     
-  Point p;
-  while (std::cin >> p){
-    t.insert(p);
-  }
-  
   Vertex_circulator vc = t.incident_vertices(t.infinite_vertex()),
     done(vc);
   if (vc != 0) {
