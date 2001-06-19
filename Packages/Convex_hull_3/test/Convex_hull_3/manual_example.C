844018c19dc8c290c8b147193771c48ae436f337
Susan Hert <hert@mpi-sb.mpg.de>
2001-06-19 14:16:15 +0000

    removed stuff based on dd_geo; condense manual_example file

--- Packages/Convex_hull_3/test/Convex_hull_3/manual_example.C
+++ Packages/Convex_hull_3/test/Convex_hull_3/manual_example.C
@@ -19,7 +19,6 @@
 // ============================================================================
 
 #include <CGAL/Cartesian.h>
-#include <CGAL/Point_3.h>
 #include <CGAL/point_generators_3.h>
 #include <CGAL/copy_n.h>
 #include <CGAL/Halfedge_data_structure_polyhedron_default_3.h>
@@ -28,33 +27,24 @@
 #include <CGAL/convex_hull_3.h>
 #include <vector>
 
-/* representation class */
-typedef CGAL::Cartesian<double>                                     RepCls;
-
-/* define polyhedron type */
-typedef CGAL::Halfedge_data_structure_polyhedron_default_3<RepCls>  HDS;
-typedef CGAL::Polyhedron_default_traits_3<RepCls>                   PolyTraits;
-typedef CGAL::Polyhedron_3< PolyTraits, HDS>                        Polyhedron;
-
-/* define point creator */
-typedef CGAL::Point_3<RepCls>                                       Point;
-typedef CGAL::Creator_uniform_3<double,Point>                       PointCreator;
+typedef CGAL::Cartesian<double>                                R;
+typedef CGAL::Halfedge_data_structure_polyhedron_default_3<R>  HDS;
+typedef CGAL::Polyhedron_default_traits_3<R>                   PolyTraits;
+typedef CGAL::Polyhedron_3< PolyTraits, HDS>                   Polyhedron;
+typedef R::Point_3                                       Point_3;
 
 int 
 main()
 {
   /* generate 250 points randomly on a sphere of radius 100.0 */
-  CGAL::Random_points_in_sphere_3< Point, PointCreator> gen(100.0);
+  CGAL::Random_points_in_sphere_3< Point_3 > gen(100.0);
 
-  /* and copy them to a vector */
-  std::vector<Point> V;
-  CGAL::copy_n( gen, 250, std::back_inserter(V) );
+  std::vector<Point_3> V;
+  CGAL::copy_n( gen, 250, std::back_inserter(V) ); /* copy them to a vector */
   
-  /* define polyhedron to hold convex hull */
-  Polyhedron P;
+  Polyhedron P; /* define polyhedron to hold convex hull */
 
-  /* compute convex hull */
-  CGAL::convex_hull_3( V.begin(), V.end(), P);
+  CGAL::convex_hull_3( V.begin(), V.end(), P);    /* compute convex hull */
 
   return 0;
 }
