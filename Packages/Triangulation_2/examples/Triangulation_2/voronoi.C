28b7adb727c369b90b9c6e666e33bdb1cb3d267d
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2001-06-20 13:40:45 +0000

    changed and cleaned up

--- Packages/Triangulation_2/examples/Triangulation_2/voronoi.C
+++ Packages/Triangulation_2/examples/Triangulation_2/voronoi.C
@@ -1,75 +1,31 @@
-#include <CGAL/basic.h>
-#include <fstream>
-
-
-// Define shorter names to please linker (g++/egcs)
-#define Cartesian Cart
-
 #include <CGAL/Cartesian.h>
-
-#include <CGAL/squared_distance_2.h>   // to avoid a g++ problem
-#include <CGAL/Point_2.h>
-#include <CGAL/predicates_on_points_2.h>
-#include <CGAL/Triangle_2.h>
-#include <CGAL/Segment_2.h>
-
-#include <CGAL/Triangulation_euclidean_traits_2.h>
 #include <CGAL/Delaunay_triangulation_2.h>
 
 typedef double coord_type;
-typedef CGAL::Cartesian<coord_type>  Rpst;
-
-typedef CGAL::Triangulation_euclidean_traits_2<Rpst> Gt;
-typedef Gt::Point_2       Point;
-typedef Gt::Segment_2     Segment;
-typedef Gt::Ray_2         Ray;
-typedef Gt::Line_2        Line;
-typedef Gt::Triangle_2    Triangle;
-
-typedef CGAL::Triangulation_vertex_base_2<Gt> Vb;
-typedef CGAL::Triangulation_face_base_2<Gt>  Fb;
-typedef CGAL::Triangulation_default_data_structure_2<Gt,Vb,Fb> Tds;
-typedef CGAL::Delaunay_triangulation_2<Gt,Tds>  Triangulation;
-
-typedef Triangulation::Face_iterator  Face_iterator;
+typedef CGAL::Cartesian<coord_type>  Gt;
+typedef CGAL::Delaunay_triangulation_2<Gt>  Triangulation;
 typedef Triangulation::Edge_iterator  Edge_iterator;
 
-int
-main( )
+int main( )
 {
-  //    Options opt;
-  //  parse(argc, argv, opt);
-
   std::ifstream in("data/voronoi.cin");
-  std::istream_iterator<Point> begin(in);
-  std::istream_iterator<Point> end;
+  std::istream_iterator<Gt::Point_2> begin(in);
+  std::istream_iterator<Gt::Point_2> end;
   Triangulation T;
   T.insert(begin, end);
-    
-  std::cout << "Vertices :" << std::endl << "=============" << std::endl;
-  Face_iterator fit =T.faces_begin()  ; 
-  for (  ; fit != T.faces_end(); ++fit) 
-    std::cout << T.dual(fit) << std::endl;
 
-  std::cout << "Segment :" << std::endl << "=============" << std::endl;
-  {
-    Edge_iterator eit =T.edges_begin();
-    for ( ; eit !=T.edges_end(); ++eit) {
- 	CGAL::Object o = T.dual(eit);
-	Triangulation::Segment s;
-	if (CGAL::assign(s,o)) std::cout << s << std::endl;
-    }
+  int ns = 0;
+  int nr = 0;  
+  Edge_iterator eit =T.edges_begin();
+  for ( ; eit !=T.edges_end(); ++eit) {
+    CGAL::Object o = T.dual(eit);
+    Gt::Segment_2 s;
+    Gt::Ray_2     r;
+    if (CGAL::assign(s,o)) {++ns;}
+    if (CGAL::assign(r,o)) {++nr;}
   }
-
-  std::cout << "Rays :" << std::endl << "=============" << std::endl;
-  {
-     Edge_iterator eit =T.edges_begin();
-    for ( ; eit !=T.edges_end(); ++eit) {
-      CGAL::Object o = T.dual(eit);
-      Ray r;
-      if (CGAL::assign(r,o)) std::cout << r << std::endl;
-    }
-  }
-
-    return 0;
+  std::cout << "The voronoi diagram as " << ns << "finite edges " 
+	    << " and " << nr << " rays" << std::endl;
+  return 0;
 }
+ 
