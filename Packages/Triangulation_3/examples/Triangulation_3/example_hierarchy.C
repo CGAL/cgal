45505455a618767c65b75c910e07c6be86e460b0
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
2001-06-20 12:18:13 +0000

    new example for hierarchy

---
+++ Packages/Triangulation_3/examples/Triangulation_3/example_hierarchy.C
@@ -0,0 +1,74 @@
+#include <CGAL/Cartesian.h>
+
+#include <CGAL/Triangulation_data_structure_3.h>
+#include <CGAL/Delaunay_triangulation_3.h>
+#include <CGAL/Triangulation_hierarchy_3.h>
+
+#include <cassert>
+#include <vector>
+
+////////////////////// 
+// chosing the number type
+////////////////////// 
+
+// for this simple example, using doubles is ok though the points
+// are in degenerate configutations, since their coordinates
+// are very small integers
+// for examples with data stored on a large number of bits,
+// using Filtered_exact number type is advised :
+// 
+// #include <CGAL/Arithmetic_filter.h>
+// #include <CGAL/MP_Float.h>
+// 
+// typedef CGAL::Filtered_exact<double, CGAL::MP_Float> NT;
+
+typedef double NT;
+
+////////////////////// 
+// chosing the representation (cartesian or homogeneous)
+////////////////////// 
+
+typedef CGAL::Cartesian<NT> Gt;
+
+////////////////////// 
+
+typedef CGAL::Triangulation_vertex_base_3<Gt>            Vb;
+typedef CGAL::Triangulation_hierarchy_vertex_base_3<Vb>  Vbh;
+typedef CGAL::Triangulation_cell_base_3<void>            Cb;
+typedef CGAL::Triangulation_data_structure_3<Vbh,Cb>     Tds;
+typedef CGAL::Delaunay_triangulation_3<Gt,Tds>           Dt;
+typedef CGAL::Triangulation_hierarchy_3<Dt>              Dh;
+
+typedef Dh::Vertex_iterator Vertex_iterator;
+typedef Dh::Vertex_handle Vertex_handle;
+typedef Gt::Point_3 Point;
+
+int main()
+{
+  Dh T;
+
+  // insertion of points on a 3D grid
+  int x,y,z;
+  std::vector<Vertex_handle> V(125);
+  int i=0;
+  
+  for (z=0 ; z<5 ; z++)
+    for (y=0 ; y<5 ; y++)
+      for (x=0 ; x<5 ; x++) 
+	  V[i++] = T.insert(Point(x,y,z));
+
+  assert( T.is_valid() );
+  assert( T.number_of_vertices() == 125 );
+  assert( T.dimension() == 3 );
+
+  // removal of the vertices in random order
+  std::random_shuffle(V.begin(), V.end());
+
+  for (i=0; i<125; ++i)
+    assert( T.remove(V[i]) );
+
+  assert( T.is_valid() );
+  assert( T.number_of_vertices() == 0 );
+
+  return 0;
+}
