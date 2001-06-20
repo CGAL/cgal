b2b6301c28cd03fee93b13ef379d58eee69b56b5
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2001-06-20 20:33:54 +0000

    added an example for constrained triangulation

--- Packages/Triangulation_2/examples/Triangulation_2/points.C
+++ Packages/Triangulation_2/examples/Triangulation_2/points.C
@@ -15,35 +15,22 @@ typedef CGAL::Triangulation_default_data_structure_2<Gt,Vb,Fb> Tds;
 typedef CGAL::Triangulation_2<Gt,Tds>  Triangulation;
 typedef CGAL::Delaunay_triangulation_2<Gt,Tds>  Delaunay_triangulation;
 
-int main(int, char* argv[])
+int main()
 {
-
   Triangulation T;
-
   PVector V;
-
-  std::ifstream data(argv[1]);
-  
+  std::ifstream data("data/points.cin");
   if(data.bad()){
-    std::cout << "Problem with file " << argv[1] << std::endl;
+    std::cout << "Problem with file " << "data/points.cin" << std::endl;
   }
   data >> V;
 
-  std::cout << V.size() << std::endl;
-
-  int count = 0;
+  std::cout << V.size() << " points read " << std::endl;
   std::cout << "Start insertion" << std::endl;
   for(int i = 0; i<V.size();i++){
-    
     T.insert(V[i]);
-
-    if(++count == 100){
-      std::cout << ".";
-      count = 0;
-    }
   }
   std::cout << std::endl << "done" << std::endl;
   T.is_valid();
-
   return 0;
 }
