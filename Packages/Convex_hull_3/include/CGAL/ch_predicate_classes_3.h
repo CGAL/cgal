6edadbb7435814c7cfcb4755b0230c73e1cecdf7
Susan Hert <hert@mpi-sb.mpg.de>
2001-06-21 09:41:04 +0000

    removed predicates that are not in kernel traits

--- Packages/Convex_hull_3/include/CGAL/ch_predicate_classes_3.h
+++ Packages/Convex_hull_3/include/CGAL/ch_predicate_classes_3.h
@@ -34,59 +34,6 @@
 
 CGAL_BEGIN_NAMESPACE
 
-/*
-template <class R_>
-class Construct_centroid_3
-{
- public:
-  typedef R_           R;
-  typedef typename R::Point_3   Point_3;
-
-  Point_3 operator()(const typename R::Point_3& p, 
-                     const typename R::Point_3& q, 
-                     const typename R::Point_3& r,
-                     const typename R::Point_3& s)
-  { 
-    typedef typename R::FT  FT;
-    return Point_3((p.hx() + q.hx() + r.hx() + s.hx())/FT(4),
-                   (p.hy() + q.hy() + r.hy() + s.hy())/FT(4),
-                   (p.hz() + q.hz() + r.hz() + s.hz())/FT(4));
-  }
-
-  Point_3 operator()(const typename R::Point_3& p, 
-                     const typename R::Point_3& q, 
-                     const typename R::Point_3& r)
-  { 
-    typedef typename R::FT  FT;
-    return Point_3((p.hx() + q.hx() + r.hx())/FT(3),
-                   (p.hy() + q.hy() + r.hy())/FT(3),
-                   (p.hz() + q.hz() + r.hz())/FT(3));
-  }
-};
-*/
-
-template <class Plane_3, class Vector_3>
-class Construct_orthogonal_vector_3
-{
- public:
-  Vector_3 operator()(const Plane_3& p)
-  { return p.orthogonal_vector(); }
-};
-
-
-/*
-template <class Plane, class Point>
-class Greater_signed_dist_to_plane_3
-{
- public:
-  Greater_signed_dist_to_plane_3( const Plane& p) : _p(p) {}
-  bool operator()( const Point& q, const Point& r)
-       { return has_larger_signed_dist_to_plane( _p,q,r); }
- private:
-  Plane _p;
-};
-*/
-
 template <class Plane, class Point>
 class Less_signed_dist_to_plane_3
 {
