19bfe7211751a318867768280e5161d1dc806283
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2001-06-22 09:40:17 +0000

    - Update test/Kernel_checker.C for VC++ and new Delaunay.

--- Packages/Interval_arithmetic/test/Interval_arithmetic/Filtered_kernel.C
+++ Packages/Interval_arithmetic/test/Interval_arithmetic/Filtered_kernel.C
@@ -1,8 +1,5 @@
-
 // Test program for the new filtering wrapper for kernel traits.
 // Currently, it's only a compilation test.
-
-#include <CGAL/basic.h>
 
 #include <CGAL/Cartesian.h>
 #include <CGAL/Homogeneous.h>
@@ -13,8 +10,6 @@
 #include <CGAL/Random.h>
 #include <CGAL/MP_Float.h>
 
-#include <CGAL/Triangulation_data_structure_3.h>
-#include <CGAL/Triangulation_geom_traits_3.h>
 #include <CGAL/Delaunay_triangulation_3.h>
 
 #if 0
@@ -31,12 +26,7 @@ typedef CGAL::Filtered_kernel<K1, K2, K3,
 #endif
 
 typedef Rep::RT  NT;
-typedef CGAL::Triangulation_geom_traits_3<Rep> Gt3d;
-typedef CGAL::Triangulation_vertex_base_3<Gt3d> Vb3d;
-// typedef CGAL::Triangulation_vertex_base_pointer_3<Gt3d> Vb3d;
-typedef CGAL::Triangulation_cell_base_3<Gt3d> Ce3d;
-typedef CGAL::Triangulation_data_structure_3<Vb3d,Ce3d> Tds3d;
-typedef CGAL::Delaunay_triangulation_3<Gt3d, Tds3d> Delaunay3d;
+typedef CGAL::Delaunay_triangulation_3<Rep> Delaunay3d;
 
 int my_rand()
 {
@@ -46,13 +36,9 @@ int my_rand()
 int main()
 {
   Delaunay3d D;
-  Rep K;
-  int loops = 100;
-  for (int i=0; i<loops; i++)
-    D.insert(K.construct_point_3_object()(NT(my_rand()),
-                                          NT(my_rand()),
-                                          NT(my_rand()),
-                                          NT(my_rand()) // for homogeneous
-					  ));
+  for (int i=0; i<100; i++)
+    D.insert(Rep::Point_3(NT(my_rand()), NT(my_rand()), NT(my_rand()),
+                          NT(my_rand()) // for homogeneous
+			));
   return 0;
 }
--- Packages/Interval_arithmetic/test/Interval_arithmetic/Kernel_checker.C
+++ Packages/Interval_arithmetic/test/Interval_arithmetic/Kernel_checker.C
@@ -1,19 +1,22 @@
 
 // Test program for the kernel checker.
 
-#include <CGAL/basic.h>
+// Workaround for buggy compilers.
+#ifdef CGAL_CFG_MATCHING_BUG_2
+#  define CGAL_IA_CT double
+#  define CGAL_IA_PROTECTED true
+#  define CGAL_IA_CACHE No_Filter_Cache
+#  define CGAL_IA_ET CGAL::MP_Float
+#endif
 
 #include <CGAL/Cartesian.h>
 #include <CGAL/Kernel_checker.h>
 #include <CGAL/Cartesian_converter.h>
 
-#include <CGAL/Point_3.h>
-
 #include <CGAL/MP_Float.h>
 #include <CGAL/Filtered_exact.h>
 #include <CGAL/Random.h>
 
-#include <CGAL/Triangulation_data_structure_3.h>
 #include <CGAL/Delaunay_triangulation_3.h>
 
 typedef CGAL::Filtered_exact<double, CGAL::MP_Float> NT1;
@@ -34,11 +37,7 @@ typedef CGAL::Kernel_checker<K1, K2, CGAL::Cartesian_converter<K1, K2, conv> >
         Rep;
 
 typedef Rep::RT  NT;
-typedef Rep Gt3d;
-typedef CGAL::Triangulation_vertex_base_3<Gt3d> Vb3d;
-typedef CGAL::Triangulation_cell_base_3<Gt3d> Ce3d;
-typedef CGAL::Triangulation_data_structure_3<Vb3d,Ce3d> Tds3d;
-typedef CGAL::Delaunay_triangulation_3<Gt3d, Tds3d> Delaunay3d;
+typedef CGAL::Delaunay_triangulation_3<Rep> Delaunay3d;
 
 int my_rand()
 {
