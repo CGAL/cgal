b5258a8d140493ce8eba4f6f4798f3842725df98
Susan Hert <hert@mpi-sb.mpg.de>
2001-06-27 16:27:04 +0000

    renamed example files and created new dynamic convex hull example

--- Packages/Convex_hull_3/examples/Convex_hull_3/quickhull_3_ex.C
+++ Packages/Convex_hull_3/examples/Convex_hull_3/quickhull_3_ex.C
@@ -10,31 +10,32 @@
 
 // NOTE: the choice of double here for a number type may cause problems 
 //       for degenerate point sets
-typedef CGAL::Cartesian<double>                   R;
-typedef CGAL::Convex_hull_traits_3<R>             Traits;
+typedef CGAL::Cartesian<double>                   K;
+typedef CGAL::Convex_hull_traits_3<K>             Traits;
 typedef Traits::Polyhedron_3                      Polyhedron_3;
-typedef CGAL::Segment_3<R>                        Segment_3;
+typedef K::Segment_3                              Segment_3;
 
-/* define point creator */
-typedef CGAL::Point_3<R>                          Point_3;
+// define point creator 
+typedef K::Point_3                                Point_3;
 typedef CGAL::Creator_uniform_3<double, Point_3>  PointCreator;
 
 
 int main()
 {
-  /* generate 250 points randomly on a sphere of radius 100.0 */
   CGAL::Random_points_in_sphere_3<Point_3, PointCreator> gen(100.0);
 
-  /* and copy them to a vector */
+  // generate 250 points randomly on a sphere of radius 100.0 
+  // and copy them to a vector 
   std::vector<Point_3> points;
   CGAL::copy_n( gen, 250, std::back_inserter(points) );
   
-  /* define polyhedron to hold convex hull */
+  // define object to hold convex hull 
   CGAL::Object ch_object;
 
-  /* compute convex hull */
+  // compute convex hull 
   CGAL::convex_hull_3(points.begin(), points.end(), ch_object);
 
+  // determine what kind of object it is
   Segment_3 segment;
   Polyhedron_3 polyhedron;
   if ( assign(segment, ch_object) )
