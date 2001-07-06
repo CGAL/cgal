6d37daba75f60d1870df50bf7b8b1d82bcaf1d46
Susan Hert <hert@mpi-sb.mpg.de>
2001-07-06 17:04:43 +0000

    removed types and functions no longer needed by the polygon_2 functions (converted to kernel traits)

--- Packages/Partition_2/include/CGAL/Partition_traits_2_base.h
+++ Packages/Partition_2/include/CGAL/Partition_traits_2_base.h
@@ -51,8 +51,6 @@ class Partition_traits_2_base
     typedef typename R::Compare_y_2                 Compare_y_2;
     typedef typename R::Compare_x_2                 Compare_x_2;
 
-    // needed below for do_intersect
-    typedef typename R::Segment_2                   Segment_2;
 
     Less_yx_2
     less_yx_2_object() const
@@ -77,72 +75,6 @@ class Partition_traits_2_base
     Compare_x_2
     compare_x_2_object() const
     {  return Compare_x_2(); }
-
-
-    // Needed for Polygon_algorithms_2 without the _2
-    typedef typename R::Less_xy_2            Less_xy;
-
-    // Needed for simplicity and CCW order precondition checking
-    // ----------
-    typedef typename Poly_Traits::Vector_2   Vector_2;
-    typedef typename R::FT                   FT;
-
-    typedef typename R::Direction_2          Direction_2;
-
-    Comparison_result 
-    compare_x(const Point_2 &p, const Point_2 &q) const
-    {
-       return Compare_x_2()(p, q);
-    }
-
-    Comparison_result 
-    compare_y(const Point_2 &p, const Point_2 &q) const
-    {
-       return Compare_y_2()(p, q);
-    }
-
-
-    FT 
-    cross_product_2(const Vector_2& p, const Vector_2& q) const
-    {
-        return p.x() * q.y() - q.x() * p.y();
-    }
-
-    bool 
-    do_intersect(const Point_2& p1, const Point_2& q1, const Point_2& p2,
-                 const Point_2& q2) const
-    {
-       return ::CGAL::do_intersect(Segment_2(p1,q1), Segment_2(p2,q2));
-    }
-
-    bool 
-    have_equal_direction(const Vector_2& v1, const Vector_2& v2) const
-    {
-       return Direction_2(v1) == Direction_2(v2);
-    }
-
-    bool 
-    is_negative(const FT& x) const
-    {
-       return CGAL_NTS is_negative(x);
-    }
-
-    // necessary for precondition checking
-    bool 
-    lexicographically_yx_smaller_or_equal(const Point_2& p, 
-                                          const Point_2& q) const
-    {
-       return ::CGAL::lexicographically_yx_smaller_or_equal(p,q);
-    }
-    // ----------
-
-    // necessary for postcondition and precondition checking
-    ::CGAL::Orientation 
-    orientation(const Point_2& p, const Point_2& q, const Point_2& r) const
-    {
-       return Orientation_2()(p,q,r);
-    }
-
 
 };
 
