8ece3ba898e7991fa7aa4393b7e3f2ec2c750ab1
Susan Hert <hert@mpi-sb.mpg.de>
2001-07-12 15:03:04 +0000

    changed tests so they can use the default cgal_test

--- Packages/Partition_2/test/Partition_2/test_defaults.h
+++ Packages/Partition_2/test/Partition_2/test_defaults.h
@@ -1,44 +1,3 @@
-// ============================================================================
-//
-// Copyright (c) 2000 The CGAL Consortium
-//
-// This software and related documentation is part of an INTERNAL release
-// of the Computational Geometry Algorithms Library (CGAL). It is not
-// intended for general use.
-//
-// ----------------------------------------------------------------------------
-//
-// release       : $CGAL_Revision $
-// release_date  : $CGAL_Date $
-//
-// file          : include/CGAL/partition_2_default_test.C
-// package       : $CGAL_Package: Partition_2 1.0 (27 Jul 2000) $
-// chapter       : Planar Polygon Partitioning
-//
-// revision      : $Revision$
-// revision_date : $Date$
-//
-// author(s)     : Susan Hert <hert@mpi-sb.mpg.de>
-// maintainer    : Susan Hert <hert@mpi-sb.mpg.de>
-//
-// coordinator   : MPI (Susan Hert <hert@mpi-sb.mpg.de>)
-//
-// implementation: testing of use of default traits classes for partitioning
-// ============================================================================
-
-#include "kernel_include.h"
-#include <CGAL/Partition_traits_2.h>
-#include <CGAL/partition_2.h>
-#include <list>
-#include <cassert>
-
-#include "kernel_def.h"
-
-typedef CGAL::Partition_traits_2<K>                       Traits;
-typedef Traits::Point_2                                   Point_2;
-typedef Traits::Polygon_2                                 Polygon_2;
-typedef std::list<Polygon_2>                              Polygon_list;
-
 
 void make_polygon(Polygon_2& polygon)
 {
@@ -56,8 +15,7 @@ void make_polygon(Polygon_2& polygon)
    polygon.push_back(Point_2(196,326));
 }
 
-
-int main( int argc, char** argv )
+void test_defaults()
 {
    Polygon_2    polygon;
    Polygon_list partition_polys;
@@ -115,6 +73,4 @@ int main( int argc, char** argv )
                                      polygon.vertices_end(),
                                      partition_polys.begin(),
                                      partition_polys.end());
-
-   return 0;
 }
