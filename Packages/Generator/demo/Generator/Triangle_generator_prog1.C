d1e0db43d22c8fc7ebb7b76265b9e09a93aec598
Susan Hert <hert@mpi-sb.mpg.de>
2001-07-16 17:15:43 +0000

    removed using namespace CGAL; converted to K::P instead of P<K>

--- Packages/Generator/demo/Generator/Triangle_generator_prog1.C
+++ Packages/Generator/demo/Generator/Triangle_generator_prog1.C
@@ -2,26 +2,21 @@
 // -------------------------------------------------
 // CGAL example program generating random triangles.
 
-#include <CGAL/basic.h>
-#include <cassert>
-#include <vector>
-#include <algorithm>
 #include <CGAL/Cartesian.h>
-#include <CGAL/Point_2.h>
-#include <CGAL/Triangle_2.h>
 #include <CGAL/point_generators_2.h>
 #include <CGAL/function_objects.h>
 #include <CGAL/Join_input_iterator.h>
 #include <CGAL/copy_n.h>
-#include <CGAL/IO/Window_stream.h>  // used for visualization
+#include <CGAL/IO/Window_stream.h>
+#include <cassert>
+#include <vector>
+#include <algorithm>
 
-using namespace CGAL;
-
-typedef Cartesian<double>                R;
-typedef Point_2<R>                       Point;
-typedef Creator_uniform_2<double,Point>  Pt_creator;
-typedef Triangle_2<R>                    Triangle;
-typedef std::vector<Triangle>            Vector;
+typedef CGAL::Cartesian<double>                K;
+typedef K::Point_2                             Point;
+typedef CGAL::Creator_uniform_2<double,Point>  Pt_creator;
+typedef K::Triangle_2                          Triangle;
+typedef std::vector<Triangle>                  Vector;
 
 int main() {
     // Create test triangle set. Prepare a vector for 20 triangles.
@@ -29,19 +24,19 @@ int main() {
     triang.reserve(20);
 
     // Prepare point generator for random points in a disc.
-    typedef  Random_points_in_disc_2<Point,Pt_creator>  RP;
+    typedef  CGAL::Random_points_in_disc_2<Point,Pt_creator>  RP;
     RP p1( 1.0);
     RP p2( 1.0);
     RP p3( 1.0);
 
     // Create 20 triangles.
-    typedef Creator_uniform_3< Point, Triangle> T_creator;
-    typedef Join_input_iterator_3< RP, RP, RP, T_creator> Triang_iterator;
+    typedef CGAL::Creator_uniform_3< Point, Triangle> T_creator;
+    typedef CGAL::Join_input_iterator_3< RP, RP, RP, T_creator> Triang_iterator;
     Triang_iterator ti( p1, p2, p3);
     CGAL::copy_n( ti, 20, std::back_inserter(triang));
 
     // Visualize triangles.
-    Window_stream* window = create_and_display_demo_window();
+    CGAL::Window_stream* window = CGAL::create_and_display_demo_window();
     for( Vector::iterator i = triang.begin(); i != triang.end(); i++)
         *window << *i;
 
--- Packages/Generator/demo/Generator/Triangle_generator_prog2.C
+++ Packages/Generator/demo/Generator/Triangle_generator_prog2.C
@@ -1,28 +1,23 @@
 //  demo/Generator/Triangle_generator_prog2.C
-//  -------------------------------
+//  -----------------------------------------
 //  CGAL example program generating a regular triangle pattern.
 
-#include <CGAL/basic.h>
-#include <algorithm>
 #include <CGAL/Cartesian.h>
-#include <CGAL/Point_2.h>
-#include <CGAL/Triangle_2.h>
 #include <CGAL/point_generators_2.h>
 #include <CGAL/function_objects.h>
 #include <CGAL/Join_input_iterator.h>
 #include <CGAL/Counting_iterator.h>
 #include <CGAL/IO/Ostream_iterator.h>
-#include <CGAL/IO/Window_stream.h>  // only used for visualization
+#include <CGAL/IO/Window_stream.h>  
+#include <algorithm>
 
-using namespace CGAL;
-
-typedef Cartesian<double>                            R;
-typedef Point_2<R>                                   Point;
-typedef Triangle_2<R>                                Triangle;
-typedef Points_on_segment_2<Point>                   PG;
-typedef Creator_uniform_3< Point, Triangle>          Creator;
-typedef Join_input_iterator_3< PG, PG, PG, Creator>  Triang_iterator;
-typedef Counting_iterator<Triang_iterator,Triangle>  Count_iterator;
+typedef CGAL::Cartesian<double>                            K;
+typedef K::Point_2                                         Point;
+typedef K::Triangle_2                                      Triangle;
+typedef CGAL::Points_on_segment_2<Point>                   PG;
+typedef CGAL::Creator_uniform_3< Point, Triangle>          Creator;
+typedef CGAL::Join_input_iterator_3< PG, PG, PG, Creator>  Triang_iterator;
+typedef CGAL::Counting_iterator<Triang_iterator,Triangle>  Count_iterator;
 
 int main() {
     // Prepare point generator for three segments.
@@ -36,9 +31,9 @@ int main() {
     Count_iterator  t_end( 50);
 
     // Open window and copy 50 triangles into window.
-    Window_stream* window = create_and_display_demo_window();
+    CGAL::Window_stream* window = CGAL::create_and_display_demo_window();
     std::copy( t_begin, t_end,
-               Ostream_iterator<Triangle,Window_stream>(*window));
+               CGAL::Ostream_iterator<Triangle,CGAL::Window_stream>(*window));
 
     // Wait for mouse click in window.
     (*window).read_mouse();
