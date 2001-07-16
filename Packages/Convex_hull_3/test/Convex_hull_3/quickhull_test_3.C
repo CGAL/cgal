1377fbc2db510594cd97268ac3cb1f604dd3ac81
Susan Hert <hert@mpi-sb.mpg.de>
2001-07-16 15:41:53 +0000

    fixed number type problems

--- Packages/Convex_hull_3/test/Convex_hull_3/quickhull_test_3.C
+++ Packages/Convex_hull_3/test/Convex_hull_3/quickhull_test_3.C
@@ -1,21 +1,36 @@
-#include <CGAL/basic.h>
 #include <CGAL/Cartesian.h>
 
-#include <vector>
 #include <CGAL/Convex_hull_traits_3.h>
 #include <CGAL/convex_hull_3.h>
 #include <CGAL/Object.h>
 #include <CGAL/point_generators_3.h>
+#include <CGAL/Filtered_exact.h>
+#include <vector>
+#include <cassert>
+#ifdef CGAL_USE_LEDA
+#  include <CGAL/leda_rational.h>
+typedef leda_rational                   Precise_rational;
+#elif defined CGAL_USE_GMP
+#  include <CGAL/Gmpz.h>
+#  include <CGAL/Quotient.h>
+typedef CGAL::Quotient<CGAL::Gmpz>      Precise_rational;
+#else
+#  include <CGAL/MP_Float.h>
+#  include <CGAL/Quotient.h>
+typedef CGAL::Quotient<CGAL::MP_Float>  Precise_rational;
+#endif
 
-typedef CGAL::Cartesian<double>                 R;
-typedef CGAL::Convex_hull_traits_3<R>           Traits;
-typedef Traits::Polyhedron_3                    Polyhedron_3;
 
-typedef R::Point_3                              Point_3;
-typedef R::Segment_3                            Segment_3;
+typedef Precise_rational                              NT;
+typedef CGAL::Cartesian<NT>                           K;
+typedef CGAL::Convex_hull_traits_3<K>                 Traits;
+typedef Traits::Polyhedron_3                          Polyhedron_3;
 
-typedef CGAL::Creator_uniform_3<double,Point_3>               Creator;
-typedef CGAL::Random_points_in_sphere_3<Point_3,Creator>      Generator;
+typedef K::Point_3                                        Point_3;
+typedef K::Segment_3                                      Segment_3;
+
+typedef CGAL::Creator_uniform_3<NT,Point_3>               Creator;
+typedef CGAL::Random_points_in_sphere_3<Point_3,Creator>  Generator;
 
 const unsigned int num = 40;
 
@@ -23,12 +38,12 @@ void test_small_hull()
 {
   std::vector<Point_3> points;
   points.push_back(Point_3(0,0,0));
-  points.push_back(Point_3(1,0,0));
-  points.push_back(Point_3(0,1,0));
-  points.push_back(Point_3(0,0,1));
-  points.push_back(Point_3(0.5,0.5,0.5));
-  points.push_back(Point_3(0.2,0.5,0.3));
-  points.push_back(Point_3(0.1,0.3,0.2));
+  points.push_back(Point_3(10,0,0));
+  points.push_back(Point_3(0,10,0));
+  points.push_back(Point_3(0,0,10));
+  points.push_back(Point_3(5,5,5));
+  points.push_back(Point_3(2,5,3));
+  points.push_back(Point_3(1,3,2));
 
   Polyhedron_3 polyhedron1;
   CGAL::convex_hull_3(points.begin(), points.end(), polyhedron1, Traits());
@@ -45,9 +60,8 @@ void test_small_hull()
 int main()
 {
   test_small_hull();
-
   std::vector<Point_3> points;
-  Generator g(5000.0);
+  Generator g(5000);
   CGAL::copy_n( g, num, std::back_inserter(points));
 
   assert(points.size() == num);
