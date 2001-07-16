18d4012bf08c6324882d2a9b628c6d6e751b77f4
Susan Hert <hert@mpi-sb.mpg.de>
2001-07-16 17:23:07 +0000

    removed using statements; added missing std::s

--- Packages/Generator/demo/Generator/rcs_demo.C
+++ Packages/Generator/demo/Generator/rcs_demo.C
@@ -1,27 +1,6 @@
-// ============================================================================
-//
-// Copyright (c) 1998 The CGAL Consortium
-//
-// This software and related documentation is part of an INTERNAL release
-// of the Computational Geometry Algorithms Library (CGAL). It is not
-// intended for general use.
-//
-// ----------------------------------------------------------------------------
-//
-// release       : $CGAL_Revision $
-// release_date  : $CGAL_Date $
-//
-// file          : demo/Generator/rcs_demo.C
-// package       : $CGAL_Package: Generator 2.12 (28 Jul 1999) $
-// source        : src/rcs/rcs.aw
-// revision      : $Revision$
-// revision_date : $Date$
-// author(s)     : Michael Hoffmann <hoffmann@inf.ethz.ch>
-//
-// coordinator   : ETH Zurich (Bernd Gaertner <gaertner@inf.ethz.ch>)
-//
-// Random Convex Point Sets: Demo Program
-// ============================================================================
+// file : demo/Generator/rcs_demo.C
+// --------------------------------
+// program that generates a random convex set
 
 #include <CGAL/Cartesian.h>
 #include <CGAL/Polygon_2.h>
@@ -32,76 +11,55 @@
 #include <cstdlib>
 #include <CGAL/IO/Window_stream.h>
 
-#if defined(CGAL_USE_CGAL_WINDOW)
-#define leda_window CGAL::window
-#endif
+typedef CGAL::Cartesian< double >                          K;
+typedef K::Point_2                                         Point;
+typedef CGAL::Polygon_traits_2< K >                        P_traits;
+typedef std::vector< Point >                               Cont;
+typedef CGAL::Polygon_2< P_traits, Cont >                  Polygon_2;
+typedef CGAL::Creator_uniform_2< double, Point >           Creator;
+typedef CGAL::Random_points_in_square_2< Point, Creator >  Point_generator;
 
-using CGAL::cgalize;
-
-using std::cout;
-using std::cerr;
-using std::endl;
-using std::flush;
-using std::vector;
-using std::back_inserter;
-using CGAL::Cartesian;
-using CGAL::Polygon_traits_2;
-using CGAL::Creator_uniform_2;
-using CGAL::Random_points_in_square_2;
-using CGAL::set_pretty_mode;
-using CGAL::random_convex_set_2;
-
-typedef Cartesian< double >                          R;
-typedef CGAL::Point_2< R >                           Point;
-typedef Polygon_traits_2< R >                        P_traits;
-typedef vector< Point >                              Cont;
-typedef CGAL::Polygon_2< P_traits, Cont >            Polygon_2;
-typedef Creator_uniform_2< double, Point >           Creator;
-typedef Random_points_in_square_2< Point, Creator >  Point_generator;
-
-int
-main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-  // this is not initialized on MIPSPRO:
-  set_pretty_mode( cout);
-  set_pretty_mode( cerr);
+  // this is not initialized on MIPSPRO
+  CGAL::set_pretty_mode( std::cout);
+  CGAL::set_pretty_mode( std::cerr);
 
   // take #points from command line:
   int n;
   if ( argc < 2 || (n = atoi( argv[1])) < 3) {
-    cerr << "usage: " << argv[0] << " \"#points\" (>= 3)" << endl;
+    std::cerr << "usage: " << argv[0] << " \"#points\" (>= 3)" << std::endl;
     return 1;
   }
 
-  cout << "Test random_convex_set_2:\n" << endl;
+  std::cout << "Test random_convex_set_2:\n" << std::endl;
 
   // build random n-gon:
-  cout << "constructing random " << n << "-gon ..." << flush;
+  std::cout << "constructing random " << n << "-gon ..." << std::flush;
   Polygon_2 p;
-  random_convex_set_2( n, back_inserter( p), Point_generator( 1));
-  cout << " done." << endl;
+  CGAL::random_convex_set_2( n, std::back_inserter( p), Point_generator( 1));
+  std::cout << " done." << std::endl;
 
   // output polygon:
-  cout << "\nHere is the result:" << endl;
+  std::cout << "\nHere is the result:" << std::endl;
 
-  leda_window W;
-  cgalize( W);
-  W.display();
+  CGAL::Window_stream W;
   W.init( -1.05, 1.05, -1.05);
+  W.display();
+  CGAL::cgalize( W);
   W << p;
 
-  // wait for mouse-click:
-  W.read_mouse();
+  W.read_mouse();        // wait for mouse-click
 
   // check convexity:
   if ( ! p.is_convex()) {
-    cerr << "ERROR: polygon is not convex." << endl;
+    std::cerr << "ERROR: polygon is not convex." << std::endl;
     return 1;
   }
 
-  cout << "done." << endl;
+  std::cout << "done." << std::endl;
   return 0;
-} // int main( argc, argv)
+} 
 
 // ----------------------------------------------------------------------------
 // ** EOF
