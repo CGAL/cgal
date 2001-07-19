dca6a59b52e87c953e9890c75e014932998559b4
Susan Hert <hert@mpi-sb.mpg.de>
2001-07-19 12:19:08 +0000

    added more output statements and changed to Homogeneous kernel

--- Packages/Generator/examples/Generator/random_polygon_ex.C
+++ Packages/Generator/examples/Generator/random_polygon_ex.C
@@ -1,23 +1,36 @@
 //
 // file : examples/Generator/random_polygon_ex.C
 //
-#include <CGAL/Cartesian.h>
+#include <CGAL/Homogeneous.h>
 #include <CGAL/Polygon_2.h>
-#include <fstream>
-
 #include <CGAL/point_generators_2.h>
 #include <CGAL/random_polygon_2.h>
 #include <CGAL/Random.h>
 #include <CGAL/copy_n.h>
+#ifdef CGAL_USE_LEDA
+#include <CGAL/leda_integer.h>
+typedef leda_integer RT;
+#else
+#ifdef CGAL_USE_GMP
+#include <CGAL/Gmpz.h>
+typedef CGAL::Gmpz RT;
+#else
+// NOTE: the choice of double here for a number type may cause problems
+//       for degenerate point sets
+#include <CGAL/double.h>
+typedef double RT;
+#endif
+#endif
+
+#include <fstream>
 #include <list>
 
-typedef double                                            NT;
-typedef CGAL::Cartesian<NT>                               K;
+typedef CGAL::Homogeneous<RT>                             K;
 typedef CGAL::Polygon_traits_2<K>                         Traits;
 typedef Traits::Point_2                                   Point_2;
 typedef std::list<Point_2>                                Container;
 typedef CGAL::Polygon_2<Traits, Container>                Polygon_2;
-typedef CGAL::Creator_uniform_2<int, Point_2>             Creator;
+typedef CGAL::Creator_uniform_2<int, Point_2>              Creator;
 typedef CGAL::Random_points_in_square_2<Point_2, Creator> Point_generator;
 
 const double RADIUS = 100;
@@ -35,8 +48,16 @@ int main( )
    // polygon will have <= size vertices
    CGAL::copy_n_unique(Point_generator(RADIUS), size,
                        std::back_inserter(point_set));
+
+   std::ostream_iterator< Point_2 >  out( std::cout, " " );
+   std::cout << "From the following " << point_set.size() << " points "
+             << std::endl;
+   std::copy(point_set.begin(), point_set.end(), out);
+   std::cout << std::endl;
+
    CGAL::random_polygon_2(point_set.size(), std::back_inserter(polygon), 
                           point_set.begin());
+   std::cout << "The following simple polygon was made: " << endl;
    std::cout << polygon << std::endl;
    return 0;
 }
