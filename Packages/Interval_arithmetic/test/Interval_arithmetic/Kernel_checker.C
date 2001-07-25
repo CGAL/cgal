573d2f5f6609cee4c549c118900244d275c9b551
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2001-07-25 11:46:51 +0000

    - Use Filtered_kernel in place of Filtered_exact.

--- Packages/Interval_arithmetic/test/Interval_arithmetic/Kernel_checker.C
+++ Packages/Interval_arithmetic/test/Interval_arithmetic/Kernel_checker.C
@@ -5,43 +5,21 @@
 #define Cartesian CA
 #define Cartesian_converter CC
 #define Kernel_checker KC
+#define Filtered_kernel Fk
 
 #include <CGAL/Cartesian.h>
-
-// Workaround for buggy compilers.
-#ifdef CGAL_CFG_MATCHING_BUG_2
-#  define CGAL_IA_CT double
-#  define CGAL_IA_ET CGAL::MP_Float
-#  define CGAL_IA_PROTECTED true
-#  define CGAL_IA_CACHE No_Filter_Cache
-#endif
-
+#include <CGAL/Filtered_kernel.h>
 #include <CGAL/Kernel_checker.h>
-#include <CGAL/Cartesian_converter.h>
 
-#include <CGAL/MP_Float.h>
-#include <CGAL/Filtered_exact.h>
 #include <CGAL/Random.h>
 
 #include <CGAL/Delaunay_triangulation_3.h>
 
-typedef CGAL::Filtered_exact<double, CGAL::MP_Float> NT1;
-// typedef double NT1;
-
-class conv
-{
-public:
-  CGAL::MP_Float operator()(const NT1 &n) const
-  {
-    return CGAL::to_double(n);
-  }
-};
-
-typedef CGAL::Cartesian<NT1>            K1;
+typedef CGAL::Filtered_kernel<CGAL::Cartesian<double> > K1;
 typedef CGAL::Cartesian<CGAL::MP_Float> K2;
-typedef CGAL::Kernel_checker<K1, K2, CGAL::Cartesian_converter<K1, K2, conv> >
-        Rep;
+typedef CGAL::Kernel_checker<K1, K2, CGAL::Cartesian_converter<K1, K2> > K;
 
+struct Rep : public  K {};
 typedef Rep::RT  NT;
 typedef CGAL::Delaunay_triangulation_3<Rep> Delaunay3d;
 
@@ -53,10 +31,7 @@ int my_rand()
 int main()
 {
   Delaunay3d D;
-  Rep K;
   for (int i=0; i<100; i++)
-    D.insert(K.construct_point_3_object()(NT(my_rand()),
-                                          NT(my_rand()),
-                                          NT(my_rand())));
+    D.insert(Rep::Point_3(NT(my_rand()), NT(my_rand()), NT(my_rand())));
   return 0;
 }
