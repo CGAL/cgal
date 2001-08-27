4ec99785447bbdaf4e5ee78bb615c79d228ea828
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2001-08-27 17:59:45 +0000

    Initial revision

---
+++ Old_Packages/Manual_tools.orig/src/test_html/extended/examples/example1.C
@@ -0,0 +1,8 @@
+//
+// File example1.C
+//
+int main(void)
+{
+   std::cout << "This is the first example program." << std::endl;
+   return 0;
+}
---
+++ Packages/Manual_tools/src/test_html/extended/examples/example2.C
@@ -0,0 +1,8 @@
+//
+// File: example2.C
+//
+int main(void)
+{
+   std::cout << "This is the second example program." << std::endl;
+   return 0;
+}
