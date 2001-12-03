e154b407b879f87aadb4067c8a5e1709f7b481d5
Matthias Bäsken <Matthias.Baesken@mpi-sb.mpg.de>
2001-12-03 07:44:12 +0000

    *** empty log message ***

--- Packages/GeoWin/demo/GeoWin/CGAL_delaunay_triang_3.C
+++ Packages/GeoWin/demo/GeoWin/CGAL_delaunay_triang_3.C
@@ -56,23 +56,23 @@ int main(int argc, char *argv[])
 #include <CGAL/Triangulation_geom_traits_3.h>
 #include <CGAL/Delaunay_triangulation_3.h>
 
-typedef double NT;
-typedef CGAL::Cartesian<NT> rep3_t;
-typedef CGAL::Point_3<rep3_t> point_3;
-typedef CGAL::Triangulation_geom_traits_3<rep3_t>  traits_3;
-typedef CGAL::Triangulation_vertex_base_3<traits_3>     Vb ;
-typedef CGAL::Triangulation_cell_base_3<traits_3>       Fb;
-typedef CGAL::Triangulation_data_structure_3<Vb,Fb> TDS3 ;
-typedef CGAL::Triangulation_3< traits_3 , TDS3> Triangulation_3;
-typedef CGAL::Delaunay_triangulation_3<traits_3,TDS3> Delaunay_3;
+typedef double                                               NT;
+typedef CGAL::Cartesian<NT>                                  K;
+typedef K::Point_3                                           Point_3;
+typedef CGAL::Triangulation_geom_traits_3<K>                 Traits_3;
+typedef CGAL::Triangulation_vertex_base_3<Traits_3>          Vb ;
+typedef CGAL::Triangulation_cell_base_3<Traits_3>            Fb;
+typedef CGAL::Triangulation_data_structure_3<Vb,Fb>          TDS3 ;
+typedef CGAL::Delaunay_triangulation_3<Traits_3,TDS3>        Delaunay_3;
+typedef std::list<Point_3>                                   Point_3_list;
 
-static void show_d3_points(geo_scene sc, leda_d3_window& W, GRAPH<leda_d3_point,int>& H)
+void show_d3_points(geo_scene sc, leda_d3_window& W, GRAPH<leda_d3_point,int>& H)
 {
- GeoEditScene< CGALPoint_3_list >* esc = (GeoEditScene< CGALPoint_3_list > *) sc;
- CGALPoint_3_list& L= esc->get_objref();
+ GeoEditScene<Point_3_list>* esc = (GeoEditScene<Point_3_list> *) sc;
+ Point_3_list& L= esc->get_objref();
 
- CGALPoint_3 p;
- CGALPoint_3_list::const_iterator it = L.begin();
+ Point_3 p;
+ Point_3_list::const_iterator it = L.begin();
 
  // input points ...
  for(; it != L.end();++it) { p=*it; H.new_node(convert_to_leda(p)); }
@@ -85,22 +85,18 @@ static void show_d3_points(geo_scene sc, leda_d3_window& W, GRAPH<leda_d3_point,
  // construct graph for visualization ...
  typedef Delaunay_3::Vertex_handle Vertex_handle;
  typedef Delaunay_3::Cell_handle Cell_handle;
- typedef Delaunay_3::Vertex Vertex;
- typedef Delaunay_3::Cell Cell;
- typedef Delaunay_3::Edge_iterator Edge_iterator;
- typedef Delaunay_3::Cell_iterator Cell_iterator;
- typedef Delaunay_3::Triangle  Triangle; 
+ typedef Delaunay_3::Finite_edges_iterator Finite_edges_iterator;
 
  Vertex_handle v1, v2;
  Cell_handle f;
  int n1, n2;
 
- Edge_iterator eit = G_delaunay.finite_edges_begin();
- Edge_iterator beyond = G_delaunay.edges_end();
+ Finite_edges_iterator eit = G_delaunay.finite_edges_begin();
+ Finite_edges_iterator beyond = G_delaunay.finite_edges_end();
 
  // edges ...
  for ( ;eit != beyond; ++eit) {
-	f = (*eit).first;
+	f  = (*eit).first;
 	n1 = (*eit).second;
        	n2 = (*eit).third;
 	v1 = f->vertex(n1);
