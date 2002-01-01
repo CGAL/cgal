21afef354a9046c793bdc237ca686c34cb6e7c6e
Ester Ezra <estere@post.tau.ac.il>
2002-01-01 15:28:35 +0000

    Initialize the Planar map notifier file.

---
+++ Packages/Planar_map/include/CGAL/Planar_map_2/Pm_change_notification.h
@@ -0,0 +1,100 @@
+// ======================================================================
+//
+// Copyright (c) 2000 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+//
+// release       : $CGAL_Revision: CGAL-2.2-I-17 $
+// release_date  : $CGAL_Date: 2000/05/12 $
+//
+// file          : include/CGAL/Pm_with_intersections_misc.h
+// package       : arr (1.27)
+// maintainer    : Sigal Raab <raab@math.tau.ac.il>
+// author(s)     : Eyal flato <flato@math.tau.ac.il>
+// coordinator   : Tel-Aviv University (Dan Halperin <halperin@math.tau.ac.il>)
+//
+// ======================================================================
+#ifndef CGAL_PM_CHANGE_NOTIFICATION_H
+#define CGAL_PM_CHANGE_NOTIFICATION_H
+
+//#ifndef CGAL_PLANAR_MAP_MISC_H
+//#include <CGAL/Planar_map_2/Planar_map_misc.h>
+//#endif
+
+CGAL_BEGIN_NAMESPACE
+
+template<class Planar_map_>
+class Pm_change_notification
+{
+public:
+  typedef Planar_map_ Planar_map;
+  typedef typename Planar_map::Traits Traits;
+  
+  virtual void add_edge(
+			const typename Traits::X_curve& cv, 
+			typename Planar_map::Halfedge_handle e, 
+			bool original_direction, bool overlap=false)
+  {
+  }
+
+  virtual void split_edge(
+			  typename Planar_map::Halfedge_handle orig_edge, 
+			  typename Planar_map::Halfedge_handle new_edge,
+			  const typename Traits::X_curve& c1,
+			  const typename Traits::X_curve& c2)
+  {
+  }
+
+  //  virtual void merge_edge(
+  //  			  typename Planar_map::Halfedge_handle orig_edge, 
+  //  			  typename Planar_map::Halfedge_handle new_edge,
+  //  			  const typename Traits::X_curve& c)
+  //    {
+  //    }
+
+  //   virtual void remove_edge(typename Planar_map::Halfedge_handle orig_edge)
+  //    {
+  //    }
+
+  virtual void split_face(
+			  typename Planar_map::Face_handle orig_face, 
+			  typename Planar_map::Face_handle new_face)
+  {
+  }
+	
+  virtual void add_hole(
+			typename Planar_map::Face_handle in_face, 
+			typename Planar_map::Halfedge_handle new_hole)
+  {
+  }
+
+  virtual const typename Traits::X_curve&
+  edge_support_curve(typename Planar_map::Halfedge_handle edge)
+  {
+    return edge->curve();
+  }
+
+  virtual bool have_support_curve()
+  {
+    return false;
+  }
+
+};
+
+CGAL_END_NAMESPACE
+
+#endif  // PM_CHANGE_NOTIFICATION
+
+
+
+
+
+
+
+
+
+
