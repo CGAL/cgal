9ce346cc6c3085f943e3f46b256f4b737837e2e4
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2002-01-23 08:15:50 +0000

    readded files : Triangulation_data_structure_using_list_2.h Triangulation_default_data_structure_2.h for backwards compatibility

---
+++ Packages/Triangulation_2/include/CGAL/Triangulation_data_structure_using_list_2.h
@@ -0,0 +1,31 @@
+// ======================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+//
+// release       : $CGAL_Revision$
+// release_date  : $CGAL_Date$
+//
+// file          : include/CGAL/Triangulation_data_structure_using_list_2.h
+// source        : $RCSfile$
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Mariette Yvinec
+//
+// coordinator   : Mariette Yvinec  <Mariette Yvinec@sophia.inria.fr>
+//
+// ============================================================================
+
+
+#ifndef CGAL_TRIANGULATION_DATA_STRUCTURE_USING_LIST_2_H
+#define CGAL_TRIANGULATION_DATA_STRUCTURE_USING_LIST_2_H
+
+// no longer needed 
+// maintained for backwards compatibility
+
+#endif //CGAL_TRIANGULATION_DATA_STRUCTURE_USING_LIST_2_H
---
+++ Packages/Triangulation_2/include/CGAL/Triangulation_default_data_structure_2.h
@@ -0,0 +1,31 @@
+// ======================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+//
+// release       : $CGAL_Revision: CGAL-2.0-I-12 $
+// release_date  : $CGAL_Date: 1999/04/28 $
+//
+// file          : include/CGAL/Triangulation_default_data_structure_2.h
+// source        : $RCSfile$
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Mariette Yvinec
+//
+// coordinator   : Mariette Yvinec  <Mariette Yvinec@sophia.inria.fr>
+//
+// ============================================================================
+
+
+#ifndef CGAL_TRIANGULATION_DEFAULT_DATA_STRUCTURE_2_H
+#define CGAL_TRIANGULATION_DEFAULT_DATA_STRUCTURE_2_H
+
+// no longer needed
+// for backwards compatibility only
+
+#endif //CGAL_TRIANGULATION_DEFAULT_DATA_STRUCTURE_2_H
