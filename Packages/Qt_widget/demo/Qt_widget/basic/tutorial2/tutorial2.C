8308106511c7b88dd829473efbdfd5af71a36f8d
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2002-01-25 07:58:23 +0000

    Radu Ursu 25.01.2002

---
+++ Packages/Qt_widget/demo/Qt_widget/basic/tutorial2/tutorial2.C
@@ -0,0 +1,41 @@
+#include <CGAL/Cartesian.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+
+#include <qapplication.h>
+#include <CGAL/IO/Qt_widget.h>
+
+typedef CGAL::Cartesian<double>		    Rep;
+typedef CGAL::Point_2<Rep>		    Point;
+typedef CGAL::Delaunay_triangulation_2<Rep> Delaunay;
+
+Delaunay dt;
+
+class My_Window : public CGAL::Qt_widget {
+public:
+  My_Window(int x, int y){ resize(x,y); };
+private:
+  
+  void redraw()
+  {
+    Qt_widget::redraw();
+    *this << dt;
+  }
+  //this event is called only when the user presses the mouse
+  void mousePressEvent(QMouseEvent *e)
+  {
+    Qt_widget::mousePressEvent(e);
+    dt.insert(Point(x_real(e->x()), y_real(e->y())));
+    redraw();
+  }
+};
+
+int main( int argc, char **argv )
+{
+    QApplication app( argc, argv );
+    My_Window W(600,600);
+    app.setMainWidget( &W );
+    W.set_window(0, 600, 0, 600);
+    W.show();
+    return app.exec();
+}
