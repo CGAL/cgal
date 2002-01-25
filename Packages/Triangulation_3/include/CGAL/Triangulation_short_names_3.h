647660461d787bdac7a4098c42999b2f7246a478
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2002-01-25 10:44:40 +0000

    - Add suffix "3" to avoid clashes with T2D.

--- Packages/Triangulation_3/include/CGAL/Triangulation_short_names_3.h
+++ Packages/Triangulation_3/include/CGAL/Triangulation_short_names_3.h
@@ -23,29 +23,29 @@
 #ifndef CGAL_TRIANGULATION_SHORT_NAMES_3_H
 #define CGAL_TRIANGULATION_SHORT_NAMES_3_H
 
-#define Triangulation_vertex_base_3                 T_vb
-#define Triangulation_cell_base_3                   T_cb
+#define Triangulation_vertex_base_3                 T_vb3
+#define Triangulation_cell_base_3                   T_cb3
 
-#define Triangulation_data_structure_3              TDS
+#define Triangulation_data_structure_3              TDS3
 
-#define Triangulation_ds_vertex_3                   TDS_v
-#define Triangulation_ds_cell_3                     TDS_c
+#define Triangulation_ds_vertex_3                   TDS_v3
+#define Triangulation_ds_cell_3                     TDS_c3
 
-#define Triangulation_ds_edge_iterator_3            TDS_eit
-#define Triangulation_ds_facet_iterator_3           TDS_fit
+#define Triangulation_ds_edge_iterator_3            TDS_eit3
+#define Triangulation_ds_facet_iterator_3           TDS_fit3
 
-#define Triangulation_ds_cell_circulator_3          TDS_ccir
-#define Triangulation_ds_facet_circulator_3         TDS_fcir
+#define Triangulation_ds_cell_circulator_3          TDS_ccir3
+#define Triangulation_ds_facet_circulator_3         TDS_fcir3
 
-#define Triangulation_3                             TR
+#define Triangulation_3                             TR3
 
-#define Delaunay_triangulation_3                    DT
+#define Delaunay_triangulation_3                    DT3
 #define Delaunay_remove_tds_face_3_2                DRf32
 #define Delaunay_remove_tds_vertex_3_2              DRv32
 #define Delaunay_remove_tds_halfedge_compare_3_2    DRthc32
 
-#define Regular_triangulation_euclidean_traits_3    R_gt
-#define Regular_triangulation_3                     RT
+#define Regular_triangulation_euclidean_traits_3    R_gt3
+#define Regular_triangulation_3                     RT3
 
 #define Triangulation_hierarchy_3                   TH_3
 #define Triangulation_hierarchy_vertex_base_3       TH_vb3
