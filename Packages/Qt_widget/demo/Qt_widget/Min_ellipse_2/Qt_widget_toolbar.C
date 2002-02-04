cfa1648ef769d7efe2b1d679ed0ce71d31008f27
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2002-02-04 15:07:55 +0000

    The min_ellipse_2 demo

---
+++ Packages/Qt_widget/demo/Qt_widget/Min_ellipse_2/Qt_widget_toolbar.C
@@ -0,0 +1,114 @@
+// ============================================================================
+//
+// Copyright (c) 1997-2000 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// file          : src/Qt_Window_toolbar.C
+// package       : QT_window
+// author(s)     : Radu Ursu
+// release       : 
+// release_date  : 
+//
+// coordinator   : Laurent Rineau <rineau@clipper.ens.fr>
+//
+// ============================================================================
+
+#ifdef CGAL_USE_QT
+
+#include <CGAL/IO/Qt_widget.h>
+#include "Qt_widget_toolbar.h"
+
+// icons
+#include <CGAL/IO/pixmaps/movepoint.xpm>
+#include <CGAL/IO/pixmaps/point.xpm>
+#include <CGAL/IO/pixmaps/arrow.xpm>
+
+
+namespace CGAL {
+  Tools_toolbar::Tools_toolbar(Qt_widget *w, QMainWindow *mw, std::list<Point> &l1) : 
+  my_toolbar_list(l1)
+  {
+    //when it is created, the toolbar has 0 buttons
+    nr_of_buttons = 0;
+    //set the widget
+    widget = w;
+    is_active = FALSE;
+
+#if QT_VERSION < 300
+  // for Qt 2.3 and before
+  maintoolbar = new QToolBar("tools", mw, QMainWindow::Top, TRUE, "Tools");
+#else
+  // from Qt 3.0
+  maintoolbar = new QToolBar(mw, "Tools");
+  mw->addDockWindow (maintoolbar, "tools", DockTop, TRUE );
+#endif
+		
+
+  but[0] = new QToolButton(QPixmap( (const char**)arrow_xpm ),
+			     "Detach current tool", 
+			     0, 
+			     this, 
+			     SLOT(notool()), 
+			     maintoolbar, 
+			     "Detach current tool");
+
+  but[1] = new QToolButton(QPixmap( (const char**)point_xpm ),
+			     "Point Tool", 
+			     0, 
+			     this, 
+			     SLOT(pointtool()), 
+			     maintoolbar, 
+			     "Point Tool");
+
+  
+  but[1]->setToggleButton(TRUE);
+
+  nr_of_buttons = 2;
+
+  connect(w, SIGNAL(detached_tool()), this, SLOT(toggle_button()));
+};
+
+      
+  //the definition of the slots
+  void Tools_toolbar::toggle_button ()
+  {
+    if(is_active) {
+      but[activebutton]->toggle();
+      is_active = false;
+    }
+  }	
+  
+  void Tools_toolbar::pointtool()
+  {
+    if (but[1]->isOn())
+    {
+      widget->attach(pointbut);
+      activebutton = 1;
+      is_active = true;
+    }
+    else
+    {
+      is_active = false;
+      widget->detach_current_tool();
+    }
+  }
+
+  void Tools_toolbar::notool()
+  {
+    if(is_active) {
+      widget->detach_current_tool();
+      is_active = false;
+    }
+  }
+  
+
+}//end namespace
+
+#include "Qt_widget_toolbar.moc"
+
+#endif
---
+++ Packages/Qt_widget/demo/Qt_widget/Min_ellipse_2/Qt_widget_toolbar.h
@@ -0,0 +1,82 @@
+// ============================================================================
+//
+// Copyright (c) 1997-2000 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// file          : include/CGAL/IO/Qt_Window_toolbar.h
+// package       : QT_window
+// author(s)     : Ursu Radu
+// release       : 
+// release_date  : 
+//
+// coordinator   : Laurent Rineau <rineau@clipper.ens.fr>
+//
+// ============================================================================
+
+
+#ifndef CGAL_QT_WINDOW_TOOLBAR_H
+#define CGAL_QT_WINDOW_TOOLBAR_H
+
+#include <CGAL/basic.h>
+#include <CGAL/Cartesian.h>
+
+
+// TODO: check if some of those includes shouldn't be in the .C file
+#include <CGAL/IO/Qt_widget.h>
+#include <CGAL/IO/Qt_widget_get_point.h>
+
+#include <qobject.h>
+#include <qtoolbutton.h>
+#include <qtoolbar.h>
+#include <qmainwindow.h>
+
+typedef CGAL::Cartesian<double>	  Rp;
+typedef Rp::Point_2		  Point;
+
+namespace CGAL {
+
+class Tools_toolbar : public QObject
+{
+	Q_OBJECT
+public:
+  Tools_toolbar(Qt_widget *w, QMainWindow *mw, std::list<Point> &l1);
+  ~Tools_toolbar()
+  {
+    delete maintoolbar;
+  };
+  QToolBar*	toolbar(){return maintoolbar;}
+
+signals:
+  void new_object(CGAL::Object);
+
+
+private slots:
+  void get_new_object(CGAL::Object obj) { emit(new_object(obj)); }
+
+  void pointtool();
+  void notool();
+  
+  void toggle_button();
+
+private:
+  QToolBar		*maintoolbar;
+  QToolButton		*but[10];
+  Qt_widget		*widget;
+  int			activebutton;
+  bool			is_active;
+  void			setActiveButton(int i);
+  int			nr_of_buttons;
+
+  std::list<Point>	my_toolbar_list;
+
+  CGAL::Qt_widget_get_point<Rp>	    pointbut;
+};//end class
+
+};//end namespace
+
+#endif
