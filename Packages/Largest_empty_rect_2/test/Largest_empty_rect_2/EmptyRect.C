0e26b9724a958a81540cabb74f734073235ea081
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2002-02-04 11:10:42 +0000

    *** empty log message ***

---
+++ Packages/Largest_empty_rect_2/test/Largest_empty_rect_2/EmptyRect.C
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <CGAL/Cartesian.h>
+#include <CGAL/Segment_2.h>
+#include <CGAL/Iso_rectangle_2.h>
+#include <CGAL/Largest_empty_iso_rectangle_2.h>
+
+
+typedef double                                 Number_Type;
+
+typedef CGAL::Cartesian<Number_Type>           K;
+typedef K::Point_2                             Point; 
+typedef K::Iso_rectangle_2                     Iso_rectangle_2;
+typedef CGAL::Largest_empty_iso_rectangle_2<K> Largest_empty_rect;
+
+
+
+
+
+int main(int /*argc*/,char */*argv[]*/)
+{
+  Point bl(1,1);
+  Point tr(10, 10);
+  Iso_rectangle_2 b(bl, tr);
+
+  Largest_empty_rect empty_rectangle(b);
+
+  std::vector<Point> V;
+  V[0] = Point(2,2);
+  
+  empty_rectangle.insert(V[0]);
+  empty_rectangle.insert(V.begin(), V.end());
+  for(Largest_empty_rect::const_iterator it = empty_rectangle.begin();
+      it != empty_rectangle.end();
+      ++it){
+    const Point& p = *it;
+    std::cout << p;
+  }
+  
+  Iso_rectangle_2 br = empty_rectangle.get_largest_empty_iso_rectangle();
+
+  empty_rectangle.get_bounding_box();
+  empty_rectangle.clear();
+
+  return(0);
+}
