0a8ad24e08320ff6c5852fd883251495e7b2bcee
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2002-03-01 08:08:06 +0000

    Added more tests

--- Packages/Largest_empty_rect_2/test/Largest_empty_rect_2/EmptyRect.C
+++ Packages/Largest_empty_rect_2/test/Largest_empty_rect_2/EmptyRect.C
@@ -1,9 +1,10 @@
-#include <fstream>
+
 #include <CGAL/Cartesian.h>
 #include <CGAL/Segment_2.h>
 #include <CGAL/Iso_rectangle_2.h>
 #include <CGAL/Largest_empty_iso_rectangle_2.h>
-
+#include <cassert>
+#include <fstream>
 
 typedef double                                 Number_Type;
 
@@ -22,28 +23,49 @@ int main(int /*argc*/,char */*argv[]*/)
   Point tr(10, 10);
   Iso_rectangle_2 b(bl, tr);
 
+  // missing default constructor
+  //  Largest_empty_rect dc;  // af: does not compile
+  // by the way what is the bounding box ((0,0),(1,1)) ??  
   Largest_empty_rect empty_rectangle(b);
+  assert(b == empty_rectangle.get_largest_empty_iso_rectangle());
 
   Largest_empty_rect er(empty_rectangle);
+  assert(b == er.get_largest_empty_iso_rectangle());
 
   Largest_empty_rect er2 = er;
+  assert(b == er2.get_largest_empty_iso_rectangle());
 
-  std::vector<Point> V;
+  std::vector<Point> V(2);
   V[0] = Point(2,2);
-  
-  empty_rectangle.insert(V[0]);
-  empty_rectangle.insert(V.begin(), V.end());
+  V[1] = Point(4,4);
+  std::cout <<"4"  << std::endl;
+  bool b1 = empty_rectangle.insert(V[0]);
+  assert(b1 == true);
+  bool b2 = empty_rectangle.insert(V[0]);
+  //assert(b2 == false); // af: assertion fails
+
+  std::cout << empty_rectangle.get_largest_empty_iso_rectangle()  << std::endl;
+
+  int n = empty_rectangle.insert(V.begin(), V.end());
+  std::cout << "n = " << n << std::endl;
+  // assert(n == 1); // af: assertion fails
   for(Largest_empty_rect::const_iterator it = empty_rectangle.begin();
       it != empty_rectangle.end();
       ++it){
     const Point& p = *it;
-    std::cout << p;
+    std::cout << p << std::endl;
   }
   
   Iso_rectangle_2 br = empty_rectangle.get_largest_empty_iso_rectangle();
 
   empty_rectangle.get_bounding_box();
+
   empty_rectangle.clear();
+  assert(empty_rectangle.begin() == empty_rectangle.end());
+  Point pp(6,6);
+  assert(empty_rectangle.insert(pp));
+  //  assert(empty_rectangle.remove(pp)); // af does not compile
+  assert(empty_rectangle.begin() == empty_rectangle.end());
 
   return(0);
 }
