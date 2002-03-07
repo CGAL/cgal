cc237e6addeafa787429370e2570fd69cd62747e
Susan Hert <hert@mpi-sb.mpg.de>
2002-03-07 11:25:52 +0000

    added header

--- Packages/Nef_2/include/CGAL/Nef_2/iterator_tools.h
+++ Packages/Nef_2/include/CGAL/Nef_2/iterator_tools.h
@@ -1,3 +1,28 @@
+// ============================================================================
+//
+// Copyright (c) 1997-2002 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// release       : $CGAL_Revision$
+// release_date  : $CGAL_Date$
+//
+// file          : include/CGAL/Nef_2/iterator_tools.h
+// package       : Nef_2
+// chapter       : Nef Polyhedra
+//
+// revision      : $Revision$
+// revision_date : $Date$
+//
+// author(s)     : Michael Seel <seel@mpi-sb.mpg.de>
+// maintainer    : Michael Seel <seel@mpi-sb.mpg.de>
+// coordinator   : Michael Seel <seel@mpi-sb.mpg.de>
+//
+// ============================================================================
 #ifndef CGAL_ITERATORTOOLS_H
 #define CGAL_ITERATORTOOLS_H
 
