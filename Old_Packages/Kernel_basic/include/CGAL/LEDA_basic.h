4c2abdb396163e713ea4af5a4ed83ab339503389
Susan Hert <hert@mpi-sb.mpg.de>
2002-03-18 15:59:32 +0000

    basic file for LEDA configuration.

---
+++ Old_Packages/Kernel_basic/include/CGAL/LEDA_basic.h
@@ -0,0 +1,41 @@
+// ======================================================================
+//
+// Copyright (c) 1999 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+// 
+// release       : 
+// release_date  : 
+// 
+// file          : include/CGAL/LEDA_basic.h
+// package       : 
+// maintainer    : 
+// revision      : 
+// revision_date : 
+// author(s)     : 
+//                
+//
+// coordinator   : 
+// ======================================================================
+
+#ifndef CGAL_LEDA_BASIC_H
+#define CGAL_LEDA_BASIC_H
+
+#ifdef CGAL_USE_LEDA
+#include <LEDA/basic.h>
+
+#ifdef LEDA_NAMESPACE
+#  define CGAL_LEDA_SCOPE  leda
+#else
+#  define CGAL_LEDA_SCOPE 
+#endif
+
+
+#endif
+
+
+#endif
