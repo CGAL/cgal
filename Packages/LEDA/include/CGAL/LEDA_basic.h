4ec8a1ec0eb16c39bf78659588e623feccae16c2
Matthias Bäsken <Matthias.Baesken@mpi-sb.mpg.de>
2002-03-19 14:23:19 +0000

    *** empty log message ***

---
+++ Packages/LEDA/include/CGAL/LEDA_basic.h
@@ -0,0 +1,41 @@
+// ======================================================================
+//
+// Copyright (c) 1999 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+// 
+// release       : 
+// release_date  : 
+// 
+// file          : include/CGAL/LEDA_basic.h
+// package       : LEDA (1.0)
+// maintainer    : Matthias Baesken <baesken@informatik.uni-trier.de>
+// revision      : 1.0
+// revision_date : 19 March 2002
+// author(s)     : Matthias Baesken
+//                
+//
+// coordinator   :  Matthias Baesken <baesken@informatik.uni-trier.de>
+// ======================================================================
+
+#ifndef CGAL_LEDA_BASIC_H
+#define CGAL_LEDA_BASIC_H
+
+#ifdef CGAL_USE_LEDA
+#include <LEDA/basic.h>
+
+#ifdef LEDA_NAMESPACE
+#  define CGAL_LEDA_SCOPE  leda
+#else
+#  define CGAL_LEDA_SCOPE 
+#endif
+
+
+#endif
+
+
+#endif
