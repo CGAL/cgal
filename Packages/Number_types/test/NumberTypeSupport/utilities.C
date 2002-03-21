85ba54338b948886558b08a35ee35ccd7f9c7234
Michael Hoffmann <hoffmann@inf.ethz.ch>
2002-03-21 10:37:19 +0000

    Include functors and various Filtered_exact instances.

--- Packages/Number_types/test/NumberTypeSupport/utilities.C
+++ Packages/Number_types/test/NumberTypeSupport/utilities.C
@@ -16,10 +16,20 @@
 #include <CGAL/leda_real.h>
 #endif // CGAL_USE_LEDA
 
+typedef CGAL::Quotient<CGAL::MP_Float>            QMPF;
+typedef CGAL::Filtered_exact<int, CGAL::Gmpz>     FEIG;
+typedef CGAL::Filtered_exact<double, CGAL::Gmpz>  FEDG;
+typedef CGAL::Filtered_exact<double, QMPF>        FEDQ;
+
+#ifdef CGAL_USE_LEDA
+typedef CGAL::Filtered_exact<double, leda_real>   FEDR;
+#endif // CGAL_USE_LEDA
+
 template < class NT >
 bool
 test_sqrt(NT, CGAL::Tag_true)
 {
+  // sqrt
   NT sixteen(16);
   NT four(4);
   std::cerr << "  sqrt()" << std::endl;
@@ -41,14 +51,21 @@ template < class NT >
 bool
 test_gcd(NT z, CGAL::Tag_true)
 {
+  // div
   NT eleven(11);
   NT three(3);
   std::cerr << "  div()" << std::endl;
   if (CGAL_NTS div(eleven, three) != three) return false;
+  CGAL::Div<NT> d;
+  if (d(eleven, three) != three) return false;
+  
+  // gcd
   NT x(2737);
   NT y(5083);
   std::cerr << "  gcd()" << std::endl;
+  CGAL::Gcd<NT> gc;
   if (CGAL_NTS gcd(x, y) != NT(391)) return false;
+  if (gc(x, y) != NT(391)) return false;
 
   typedef typename CGAL::Number_type_traits<NT>::Has_sqrt Has_sqrt;
   Has_sqrt has_sqrt = Has_sqrt();
@@ -70,28 +87,68 @@ test_basic(NT x)
 {
   NT zero(0);
   NT one(1);
+  NT mone(-one);
+
+  // is_zero
   std::cerr << "  is_zero()" << std::endl;
   if (!CGAL_NTS is_zero(zero)) return false;
+  CGAL::Is_zero<NT> iz;
+  if (!iz(zero)) return false;
+
+  // is_one
   std::cerr << "  is_one()" << std::endl;
   if (!CGAL_NTS is_one(one)) return false;
+  CGAL::Is_one<NT> io;
+  if (!io(one)) return false;
+
+  // is_positive
   std::cerr << "  is_positive()" << std::endl;
   if (!CGAL_NTS is_positive(one)) return false;
+  CGAL::Is_positive<NT> ip;
+  if (!ip(one)) return false;
+
+  // is_negative
   std::cerr << "  is_negative()" << std::endl;
   if (CGAL_NTS is_negative(one)) return false;
+  CGAL::Is_negative<NT> in;
+  if (in(one)) return false;
+
+  // sign
   std::cerr << "  sign()" << std::endl;
+  CGAL::Sgn<NT> sg;
   if (CGAL_NTS sign(one) != CGAL::POSITIVE) return false;
+  if (sg(one) != CGAL::POSITIVE) return false;
   if (CGAL_NTS sign(zero) != CGAL::ZERO) return false;
+  if (sg(zero) != CGAL::ZERO) return false;
+
+  // abs
   std::cerr << "  abs()" << std::endl;
-  if (CGAL_NTS abs(-one) != one &&
-      CGAL_NTS abs(-one) != -one) // unsigned types :-)
+  CGAL::Abs<NT> ab;
+  if (CGAL_NTS abs(mone) != one &&
+      CGAL_NTS abs(mone) != mone) // unsigned types :-)
     return false;
+  if (ab(mone) != one && ab(mone) != mone) 
+    return false;
+  if (ab(mone) != CGAL_NTS abs(mone) || ab(one) != CGAL_NTS abs(one))
+    return false;
+
+  // compare
   std::cerr << "  compare()" << std::endl;
+  CGAL::Compare<NT> co;
   if (CGAL_NTS compare(one, zero) != CGAL::LARGER) return false;
   if (CGAL_NTS compare(one, one) != CGAL::EQUAL) return false;
   if (CGAL_NTS compare(zero, one) != CGAL::SMALLER) return false;
+  if (co(one, zero) != CGAL::LARGER) return false;
+  if (co(one, one) != CGAL::EQUAL) return false;
+  if (co(zero, one) != CGAL::SMALLER) return false;
+
+  // square
   std::cerr << "  square()" << std::endl;
   NT two(2);
-  NT four = CGAL_NTS square(two);
+  NT four(4);
+  CGAL::Square<NT> sq;
+  if (CGAL_NTS square(two) != four) return false;
+  if (sq(two) != four) return false;
 
   typedef typename CGAL::Number_type_traits<NT>::Has_gcd Has_gcd;
   Has_gcd has_gcd = Has_gcd();
@@ -123,10 +180,14 @@ int main()
   TESTIT(double, "double")
 
   // CGAL number types
-  TESTIT(CGAL::MP_Float, "CGAL::MP_Float")
-  TESTIT(CGAL::Quotient<int>, "CGAL::Quotient<int>")
-    //  TESTIT(CGAL::Lazy_exact_nt<CGAL::Quotient<CGAL::MP_Float> >,
-    //	 "CGAL::Lazy_exact_nt<CGAL::Quotient<CGAL::MP_Float> >")
+  TESTIT(CGAL::Fixed_precision_nt, "Fixed_precision_nt")
+  TESTIT(CGAL::MP_Float, "MP_Float")
+  TESTIT(CGAL::Quotient<int>, "Quotient<int>")
+  TESTIT(QMPF, "Quotient<MP_Float>")
+  TESTIT(CGAL::Lazy_exact_nt<QMPF>, "Lazy_exact_nt<Quotient<MP_Float> >")
+  TESTIT(FEIG, "Filtered_exact<int, Gmpz>");
+  TESTIT(FEDG, "Filtered_exact<double, Gmpz>");
+  TESTIT(FEDQ, "Filtered_exact<double, Quotient<MP_Float> >");
 
   // GMP based NTs
 #ifdef CGAL_USE_GMP
@@ -139,6 +200,7 @@ int main()
   TESTIT(leda_rational, "leda_rational")
   TESTIT(leda_bigfloat, "leda_bigfloat")
   TESTIT(leda_real, "leda_real")
+  TESTIT(FEDR, "Filtered_exact<double, leda_real");
 #endif // CGAL_USE_LEDA
 
 
