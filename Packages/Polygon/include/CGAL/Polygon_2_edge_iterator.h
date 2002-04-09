eb0011c9bc7fa996d9e11e3825337b95b5cd20d1
Geert-Jan Giezeman <geert@cs.uu.nl>
2002-04-09 08:48:45 +0000

    Added operator-> for Edge_const_iterator

--- Packages/Polygon/include/CGAL/Polygon_2_edge_iterator.h
+++ Packages/Polygon/include/CGAL/Polygon_2_edge_iterator.h
@@ -15,7 +15,7 @@
 // source        : 
 // revision      : 1.8a
 // revision_date : 13 Mar 1998
-// author(s)     : Wieger Wesselink <wieger@cs.ruu.nl>
+// author(s)     : Wieger Wesselink, Geert-Jan Giezeman <geert@cs.uu.nl>
 //
 // coordinator   : Utrecht University
 //
@@ -36,32 +36,50 @@ CGAL_BEGIN_NAMESPACE
 // Polygon_2, but this leads to compiler problems with SGI C++ 4.0
 // with the iterator_category() function
 
-template <class _Traits, class _Container> class Polygon_2;
+template <class Traits_, class Container_> class Polygon_2;
 
-template <class _Traits, class _Container>
+template <class Segment_>
+class Polygon_2__Segment_ptr
+{
+public:
+    typedef Segment_ Segment;
+    Polygon_2__Segment_ptr(Segment const &seg) :m_seg(seg){}
+    Segment* operator->() {return &m_seg;}
+private:
+    Segment m_seg;
+};
+
+template <class Traits_, class Container_>
 class Polygon_2_edge_iterator {
   public:
-    typedef _Traits Traits;
-    typedef typename _Traits::Segment_2 Segment_2;
-    typedef _Container Container;
-    typedef typename _Container::const_iterator const_iterator;
-    typedef typename _Container::difference_type difference_type;
+#ifdef CGAL_CFG_NO_LAZY_INSTANTIATION
+    typedef std::forward_iterator_tag iterator_category;
+#else
+    typedef std::random_access_iterator_tag iterator_category;
+#endif
+    typedef typename Traits_::Segment_2 Segment_2;
+    typedef typename Traits_::Segment_2 value_type;
+    typedef Container_ Container;
+    typedef typename Container_::const_iterator const_iterator;
+    typedef typename Container_::difference_type difference_type;
+    typedef Segment_2*           pointer;
+    typedef Segment_2&           reference;
   private:
-    const _Container* container;   // needed for dereferencing the last edge
+    const Container_* container;   // needed for dereferencing the last edge
     const_iterator first_vertex;   // points to the first vertex of the edge
   public:
     Polygon_2_edge_iterator() {}
-    Polygon_2_edge_iterator(const _Container* c, const_iterator f)
+    Polygon_2_edge_iterator(const Container_* c, const_iterator f)
       : container(c), first_vertex(f) {}
 
     bool operator==(
-      const Polygon_2_edge_iterator<_Traits, _Container>& x) const
+      const Polygon_2_edge_iterator<Traits_, Container_>& x) const
     {
       return first_vertex == x.first_vertex;
     }
     
     bool operator!=(
-      const Polygon_2_edge_iterator<_Traits, _Container>& x) const
+      const Polygon_2_edge_iterator<Traits_, Container_>& x) const
     {
       return !(first_vertex == x.first_vertex);
     }
@@ -75,80 +93,83 @@ class Polygon_2_edge_iterator {
             Traits().construct_segment_2_object();
       return construct_segment_2(*first_vertex, *second_vertex);
     }
+    
+    Polygon_2__Segment_ptr<Segment_2> operator->()
+        {return Polygon_2__Segment_ptr<Segment_2>(operator*());}
 
-    Polygon_2_edge_iterator<_Traits, _Container>& operator++() {
+    Polygon_2_edge_iterator<Traits_, Container_>& operator++() {
       ++first_vertex;
       return *this;
     }
 
-    Polygon_2_edge_iterator<_Traits, _Container> operator++(int) {
-      Polygon_2_edge_iterator<_Traits, _Container> tmp = *this;
+    Polygon_2_edge_iterator<Traits_, Container_> operator++(int) {
+      Polygon_2_edge_iterator<Traits_, Container_> tmp = *this;
       ++*this;
       return tmp;
     }
 
-    Polygon_2_edge_iterator<_Traits, _Container>& operator--() {
+    Polygon_2_edge_iterator<Traits_, Container_>& operator--() {
       --first_vertex;
       return *this;
     }
 
-    Polygon_2_edge_iterator<_Traits, _Container> operator--(int) {
-      Polygon_2_edge_iterator<_Traits, _Container> tmp = *this;
+    Polygon_2_edge_iterator<Traits_, Container_> operator--(int) {
+      Polygon_2_edge_iterator<Traits_, Container_> tmp = *this;
       --*this;
       return tmp;
     }
 
 #ifndef CGAL_CFG_NO_LAZY_INSTANTIATION
 // random access iterator requirements
-    Polygon_2_edge_iterator<_Traits, _Container>&
+    Polygon_2_edge_iterator<Traits_, Container_>&
     operator+=(difference_type n) {
       first_vertex += n;
       return *this;
     }
 
-    Polygon_2_edge_iterator<_Traits, _Container>
+    Polygon_2_edge_iterator<Traits_, Container_>
     operator+(difference_type n) {
-      return Polygon_2_edge_iterator<_Traits, _Container>(
+      return Polygon_2_edge_iterator<Traits_, Container_>(
         container, first_vertex + n);
     }
 
-    Polygon_2_edge_iterator<_Traits, _Container>&
+    Polygon_2_edge_iterator<Traits_, Container_>&
     operator-=(difference_type n) {
       return (*this) -= n;
     }
 
-    Polygon_2_edge_iterator<_Traits, _Container>
+    Polygon_2_edge_iterator<Traits_, Container_>
     operator-(difference_type n) {
-      return Polygon_2_edge_iterator<_Traits, _Container>(
+      return Polygon_2_edge_iterator<Traits_, Container_>(
         container, first_vertex - n);
     }
 
     difference_type
-    operator-(const Polygon_2_edge_iterator<_Traits, _Container>& a) {
+    operator-(const Polygon_2_edge_iterator<Traits_, Container_>& a) {
       return first_vertex - a.first_vertex;
     }
 
     Segment_2 operator[](int n) {
-      return *Polygon_2_edge_iterator<_Traits, _Container>(
+      return *Polygon_2_edge_iterator<Traits_, Container_>(
         container, first_vertex+n);
     }
 
-    bool operator<(const Polygon_2_edge_iterator<_Traits, _Container>& a)
+    bool operator<(const Polygon_2_edge_iterator<Traits_, Container_>& a)
     {
       return first_vertex < a.first_vertex;
     }
 
-    bool operator>(const Polygon_2_edge_iterator<_Traits, _Container>& a)
+    bool operator>(const Polygon_2_edge_iterator<Traits_, Container_>& a)
     {
       return first_vertex > a.first_vertex;
     }
 
-    bool operator<=(const Polygon_2_edge_iterator<_Traits, _Container>& a)
+    bool operator<=(const Polygon_2_edge_iterator<Traits_, Container_>& a)
     {
       return first_vertex <= a.first_vertex;
     }
 
-    bool operator>=(const Polygon_2_edge_iterator<_Traits, _Container>& a)
+    bool operator>=(const Polygon_2_edge_iterator<Traits_, Container_>& a)
     {
       return first_vertex >= a.first_vertex;
     }
@@ -157,15 +178,15 @@ class Polygon_2_edge_iterator {
 };
 
 
-template <class _Traits,  class _Container>
-typename _Container::difference_type
-distance_type(const Polygon_2_edge_iterator<_Traits,_Container>&)
-{ return _Container::difference_type(); }
+template <class Traits_,  class Container_>
+typename Container_::difference_type
+distance_type(const Polygon_2_edge_iterator<Traits_,Container_>&)
+{ return Container_::difference_type(); }
 
-template <class _Traits,  class _Container>
-typename _Traits::Segment_2*
-value_type(const Polygon_2_edge_iterator<_Traits,_Container>&)
-{ return (typename _Traits::Segment_2 *)(0); }
+template <class Traits_,  class Container_>
+typename Traits_::Segment_2*
+value_type(const Polygon_2_edge_iterator<Traits_,Container_>&)
+{ return (typename Traits_::Segment_2 *)(0); }
 
 
 //-----------------------------------------------------------------------//
@@ -179,10 +200,10 @@ value_type(const Polygon_2_edge_iterator<_Traits,_Container>&)
 //--------------------------------------------------------------------//
 
 #ifdef CGAL_CFG_NO_ITERATOR_TRAITS 
-template <class _Traits, class _Container>
+template <class Traits_, class Container_>
 inline
 bidirectional_iterator_tag
-iterator_category(const Polygon_2_edge_iterator<_Traits, _Container>&)
+iterator_category(const Polygon_2_edge_iterator<Traits_, Container_>&)
 {
   return bidirectional_iterator_tag();
 }
@@ -192,11 +213,11 @@ iterator_category(const Polygon_2_edge_iterator<_Traits, _Container>&)
 //--------------------------------------------------------------------//
 // I don't know how to implement the following function:
 //
-// template <class _Traits, class _Container>
+// template <class Traits_, class Container_>
 // inline
-// Polygon_2_edge_iterator<_Traits, _Container>
-// operator+(_Container::difference_type n,
-//           Polygon_2_edge_iterator<_Traits, _Container>& a)
+// Polygon_2_edge_iterator<Traits_, Container_>
+// operator+(Container_::difference_type n,
+//           Polygon_2_edge_iterator<Traits_, Container_>& a)
 // { return a+n; }
 //--------------------------------------------------------------------//
 
