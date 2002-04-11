7e79d9573ac3af52d152548cd7be1f52c307a12e
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2002-04-11 13:24:51 +0000

    just try to undo previous changes

--- Packages/Triangulation_2/test/Triangulation_2_constrained/test_const_triang_plus_2.C
+++ Packages/Triangulation_2/test/Triangulation_2_constrained/test_const_triang_plus_2.C
@@ -36,25 +36,25 @@ int main()
   std::cout << "Testing constraint hierarchy" << std::endl;
   _test_cls_hierarchy_2();
 
-//   std::cout << "Testing constrained_triangulation_plus_2 "<< std::endl;
-//   std::cout << " with Exact_predicates_tag : " << std::endl;
-//   typedef CGAL::Triangulation_vertex_base_2<TestK>              Vb;
-//   typedef CGAL::Constrained_triangulation_face_base_2<TestK>    Fb;
-//   typedef CGAL::Triangulation_data_structure_2<Vb,Fb>           TDS;
-//   typedef CGAL::Exact_predicates_tag                            Itag;
-//   typedef CGAL::Constrained_Delaunay_triangulation_2<TestK,TDS,Itag>   CDt;
-//   typedef CGAL::Constrained_triangulation_plus_2<CDt>   CDtplus;
-//   _test_cls_const_triang_plus_2(CDtplus());
+  std::cout << "Testing constrained_triangulation_plus_2 "<< std::endl;
+  std::cout << " with Exact_predicates_tag : " << std::endl;
+  typedef CGAL::Triangulation_vertex_base_2<TestK>              Vb;
+  typedef CGAL::Constrained_triangulation_face_base_2<TestK>    Fb;
+  typedef CGAL::Triangulation_data_structure_2<Vb,Fb>           TDS;
+  typedef CGAL::Exact_predicates_tag                            Itag;
+  typedef CGAL::Constrained_Delaunay_triangulation_2<TestK,TDS,Itag>   CDt;
+  typedef CGAL::Constrained_triangulation_plus_2<CDt>   CDtplus;
+  _test_cls_const_triang_plus_2(CDtplus());
   
-//   std::cout << "Testing constrained_triangulation_plus_2 "<<   std::endl;
-//   std::cout << " with Exact_intersections_tag : " << std::endl;
-//   typedef CGAL::Triangulation_vertex_base_2<EK>                 Vbb;
-//   typedef CGAL::Constrained_triangulation_face_base_2<EK>       Fbb;
-//   typedef CGAL::Triangulation_data_structure_2<Vbb,Fbb>         TDSS;
-//   typedef CGAL::Exact_intersections_tag                         EItag;
-//   typedef CGAL::Constrained_Delaunay_triangulation_2<EK,TDSS,EItag>  CDtei;
-//   typedef CGAL::Constrained_triangulation_plus_2<CDtei>   CDteiplus;
-//   _test_cls_const_triang_plus_2(CDteiplus());
+  std::cout << "Testing constrained_triangulation_plus_2 "<<   std::endl;
+  std::cout << " with Exact_intersections_tag : " << std::endl;
+  typedef CGAL::Triangulation_vertex_base_2<EK>                 Vbb;
+  typedef CGAL::Constrained_triangulation_face_base_2<EK>       Fbb;
+  typedef CGAL::Triangulation_data_structure_2<Vbb,Fbb>         TDSS;
+  typedef CGAL::Exact_intersections_tag                         EItag;
+  typedef CGAL::Constrained_Delaunay_triangulation_2<EK,TDSS,EItag>  CDtei;
+  typedef CGAL::Constrained_triangulation_plus_2<CDtei>   CDteiplus;
+  _test_cls_const_triang_plus_2(CDteiplus());
 
   return 0;
 }
