c450323b9ab6270f53e4c788baae7e58a67e992e
Lutz Kettner <kettner@mpi-sb.mpg.de>
2002-04-19 14:34:47 +0000

    - Added missing std:: before size_t in hds_prog_compact[2].C.

--- Old_Packages/Halfedge_DS/examples/Halfedge_DS/hds_prog_compact.C
+++ Old_Packages/Halfedge_DS/examples/Halfedge_DS/hds_prog_compact.C
@@ -9,9 +9,9 @@ using namespace CGAL;
 // Define a new halfedge class.
 class My_halfedge {
 protected:
-    size_t  nxt;
-    void*   v;
-    void*   f;
+    std::size_t  nxt;
+    void*        v;
+    void*        f;
 public:
     typedef Tag_false Supports_halfedge_prev;
     typedef Tag_true  Supports_halfedge_vertex;
@@ -20,19 +20,19 @@ public:
     My_halfedge() : nxt(0), f(NULL) {}
 
     void*       opposite()       {
-        const size_t SIZE = sizeof( My_halfedge);
+        const std::size_t SIZE = sizeof( My_halfedge);
         if ( nxt & 1)
             return (char*)this + SIZE;
         return (char*)this - SIZE;
     }
     const void* opposite() const {
-        const size_t SIZE = sizeof( My_halfedge);
+        const std::size_t SIZE = sizeof( My_halfedge);
         if ( nxt & 1)
             return (const char*)this + SIZE;
         return (const char*)this - SIZE;
     }
-    void*       next()           { return (void*)(nxt & (~ size_t(1)));}
-    const void* next() const     { return (void*)(nxt & (~ size_t(1)));}
+    void*       next()           { return (void*)(nxt & (~ std::size_t(1)));}
+    const void* next() const     { return (void*)(nxt & (~ std::size_t(1)));}
 
     void*       vertex()         { return v;}
     const void* vertex() const   { return v;}
@@ -44,15 +44,16 @@ public:
 
     void  set_opposite( void* g) {
         char* h = (char*)g;
-        CGAL_assertion( size_t( abs( h - (char*)this)) == sizeof(My_halfedge));
+        CGAL_assertion( std::size_t( abs( h - (char*)this)) 
+                        == sizeof(My_halfedge));
         if ( h > (char*)this)
             nxt |= 1;
         else
-            nxt &= (~ size_t(1));
+            nxt &= (~ std::size_t(1));
     }
     void  set_next( void* h)     {
-        CGAL_assertion( ((size_t)h & 1) == 0);
-        nxt = ((size_t)(h)) | (nxt & 1);
+        CGAL_assertion( ((std::size_t)h & 1) == 0);
+        nxt = ((std::size_t)(h)) | (nxt & 1);
     }
     void  set_vertex( void* _v)  { v = _v;}
     void  set_facet( void* _f)   { f = _f;}
