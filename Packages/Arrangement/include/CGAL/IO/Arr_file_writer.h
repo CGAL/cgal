f0900f14b85085f34258865e9066fde39c052e6b
Efi Fogel <efif@post.tau.ac.il>
2002-04-20 15:02:07 +0000

    cleanup

--- Packages/Arrangement/include/CGAL/IO/Arr_file_writer.h
+++ Packages/Arrangement/include/CGAL/IO/Arr_file_writer.h
@@ -28,50 +28,33 @@
 #ifndef CGAL_IO_ARR_FILE_WRITER_H
 #define CGAL_IO_ARR_FILE_WRITER_H 1
 
-#ifndef CGAL_IO_BINARY_FILE_IO_H
 #include <CGAL/IO/binary_file_io.h>
-#endif // CGAL_IO_BINARY_FILE_IO_H
-
-#ifndef CGAL_IO_PM_FILE_WRITER_H
 #include <CGAL/IO/Pm_file_writer.h>
-#endif // CGAL_IO_PM_FILE_HEADER_H
-
-#ifndef CGAL_PROTECT_IOSTREAM
 #include <iostream>
-#define CGAL_PROTECT_IOSTREAM
-#endif
-
-#ifndef CGAL_PROTECT_CSTDDEF
 #include <cstddef>
-#define CGAL_PROTECT_CSTDDEF
-#endif
 
 CGAL_BEGIN_NAMESPACE
-
 
 template <class Arrangement>
 class Arr_file_writer : public  Pm_file_writer<Arrangement> {
 
 public:
-  //typedef Arrangement_                                           Arrangement;
-  typedef typename Arrangement::Curve_iterator            Curve_iterator;
-  typedef typename Arrangement::Subcurve_iterator         Subcurve_iterator;
-  typedef typename Arrangement::Edge_iterator             Edge_iterator;
-  typedef typename Arrangement::Curve_const_iterator      Curve_const_iterator;
+  typedef typename Arrangement::Curve_iterator          Curve_iterator;
+  typedef typename Arrangement::Subcurve_iterator       Subcurve_iterator;
+  typedef typename Arrangement::Edge_iterator           Edge_iterator;
+  typedef typename Arrangement::Curve_const_iterator    Curve_const_iterator;
   typedef typename Arrangement::Subcurve_const_iterator   
                                                     Subcurve_const_iterator;
-  typedef typename Arrangement::Edge_const_iterator      Edge_const_iterator;
+  typedef typename Arrangement::Edge_const_iterator     Edge_const_iterator;
 
-  Arr_file_writer(std::ostream& o, 
-                  const Arrangement& arr, 
+  Arr_file_writer(std::ostream & o,
+                  const Arrangement & arr,
                   bool verbose = false) : 
     Pm_file_writer<Arrangement>(o, arr, verbose) {}
 
-  Arr_file_writer(std::ostream& o, 
-                  const File_header& h) : 
+  Arr_file_writer(std::ostream & o, const File_header& h) : 
     Pm_file_writer<Arrangement>(o, h) {}
   
-
   void write_curve (Curve_iterator cv){
     out () << cv->curve() << std::endl;
   }
@@ -106,15 +89,6 @@ public:
     out() << std::endl;
     } */
 };
+
 CGAL_END_NAMESPACE
 #endif // CGAL_IO_FILE_WRITER_ARR_H //
-// EOF //
-
-
-
-
-
-
-
-
-
--- Packages/Arrangement/include/CGAL/IO/Arr_iostream.h
+++ Packages/Arrangement/include/CGAL/IO/Arr_iostream.h
@@ -22,60 +22,37 @@
 #ifndef CGAL_ARR_IOSTREAM_H
 #define CGAL_ARR_IOSTREAM_H
 
-#ifndef CGAL_BASIC_H
 #include <CGAL/basic.h>
-#endif
-
-#ifndef CGAL_ARRANGEMENT_2_H
 #include <CGAL/Arrangement_2.h>
-#endif
-
-#ifndef CGAL_IO_ARR_FILE_WRITER_H
 #include <CGAL/IO/Arr_file_writer.h>
-#endif // CGAL_IO_ARR_FILE_WRITER_H
-
-#ifndef CGAL_IO_WRITE_ARR_H
 #include <CGAL/IO/write_arr.h>
-#endif // CGAL_IO_WRITE_ARR_H
-
-
 #include <iostream>
 
 CGAL_BEGIN_NAMESPACE
 
 template <class Dcel, class Traits, class Base_node> inline
-::std::ostream& operator << (::std::ostream& o, 
-                             const Arrangement_2<Dcel,Traits, 
-                             Base_node>& arr) 
+::std::ostream & operator << (::std::ostream & o, 
+                              const Arrangement_2<Dcel,Traits,Base_node> & arr)
 {
+  typedef Arrangement_2<Dcel,Traits,Base_node>        Arr_2;
+  typedef Arr_file_writer<Arr_2>                      Writer;
+
   //print_OFF(o, arr);
   
-  Arr_file_writer< Arrangement_2<Dcel,Traits, Base_node> >  writer(o, arr);
-  
-  write_arr(arr, writer, o);
-  
+  Writer writer(o, arr);
+  write_arr<Arr_2,Writer>(arr, writer, o);
   return o;
 }
- 
+
 template <class Dcel, class Traits, class Base_node> inline
-::std::istream& operator>>( std::istream& in, 
-                            Arrangement_2<Dcel,Traits, Base_node>& arr) {
+::std::istream & operator >> (std::istream & in, 
+                              Arrangement_2<Dcel,Traits, Base_node> & arr)
+{
   // reads a polyhedron from `in' and appends it to P.
-  
   arr.read(in);
-  
   return in;
 }
-
 
 CGAL_END_NAMESPACE
 
 #endif
-
-
-
-
-
-
-
-
