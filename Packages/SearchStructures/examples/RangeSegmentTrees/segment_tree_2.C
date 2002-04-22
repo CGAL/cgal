d73cfff499ad05aa92814cc8c3ad7803c05d6fb4
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2002-04-22 23:51:45 +0000

    - Fix warning \"conversion double to int\".

--- Packages/SearchStructures/examples/RangeSegmentTrees/segment_tree_2.C
+++ Packages/SearchStructures/examples/RangeSegmentTrees/segment_tree_2.C
@@ -71,10 +71,14 @@ int main()
 
 
   // insertion of the tree elements into the sequence container
-  InputList.push_back(Interval(Key(64, 139), Key(81.3, 921.3)));
-  InputList.push_back(Interval(Key(465, 504), Key(499.3, 829.0)));
-  InputList.push_back(Interval(Key(288, 875), Key(379.5, 982.7)));
-  InputList.push_back(Interval(Key(314, 465), Key(375.1, 711.5)));
+  // InputList.push_back(Interval(Key(64, 139), Key(81.3, 921.3)));
+  // InputList.push_back(Interval(Key(465, 504), Key(499.3, 829.0)));
+  // InputList.push_back(Interval(Key(288, 875), Key(379.5, 982.7)));
+  // InputList.push_back(Interval(Key(314, 465), Key(375.1, 711.5)));
+  InputList.push_back(Interval(Key(64, 139), Key(81, 921.3)));
+  InputList.push_back(Interval(Key(465, 504), Key(499, 829.0)));
+  InputList.push_back(Interval(Key(288, 875), Key(379, 982.7)));
+  InputList.push_back(Interval(Key(314, 465), Key(375, 711.5)));
  
   // creation of the segment tree
   std::list<Interval>::iterator first = InputList.begin();
@@ -83,7 +87,8 @@ int main()
   Segment_tree_2_type Segment_tree_2(first,last);
 
   // perform a window query
-  Interval a(Key(45, 500), Key(200.0, 675.1));
+  // Interval a(Key(45, 500), Key(200.0, 675.1));
+  Interval a(Key(45, 500), Key(200, 675.1));
   Segment_tree_2.window_query(a,std::back_inserter(OutputList));
 
   // output of the querey elements on stdout
@@ -95,7 +100,8 @@ int main()
 	 << (*j).first.second << "-" << (*j).second.second << std::endl; 
     j++;
   }
-  Interval b(Key(320, 900),Key(330.1,910.7));
+  // Interval b(Key(320, 900),Key(330.1,910.7));
+  Interval b(Key(320, 900),Key(330,910.7));
   Segment_tree_2.enclosing_query(b,std::back_inserter(N));
   j = N.begin();
   std::cerr << "\n enclosing_query (320, 900),(330.1,910.7)\n";
@@ -111,5 +117,3 @@ int main()
     std::cerr << "Tree is not valid\n";
   return 0; 
 }
-
-
