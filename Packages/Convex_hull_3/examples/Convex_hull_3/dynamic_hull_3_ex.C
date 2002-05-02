df58432b2eee48dc968842680488590133287044
Susan Hert <hert@mpi-sb.mpg.de>
2002-05-02 16:03:00 +0000

    - eliminate use of back_inserter for Delaunay triangulation since (a) the   push_back function is deprecated and (b) M$VC7 doesn't like this.

--- Packages/Convex_hull_3/examples/Convex_hull_3/dynamic_hull_3_ex.C
+++ Packages/Convex_hull_3/examples/Convex_hull_3/dynamic_hull_3_ex.C
@@ -36,11 +36,13 @@ typedef Delaunay::Vertex_handle                     Vertex_handle;
 int main()
 {
   CGAL::Random_points_in_sphere_3<Point_3> gen(100.0);
+  std::list<Point_3>   points;
 
   // generate 250 points randomly on a sphere of radius 100.0 
   // and insert them into the triangulation
+  CGAL::copy_n(gen, 250, std::back_inserter(points) );
   Delaunay T;
-  CGAL::copy_n( gen, 250, std::back_inserter(T) );
+  T.insert(points.begin(), points.end());
 
   std::list<Vertex_handle>  vertices;
   T.incident_vertices(T.infinite_vertex(), std::back_inserter(vertices));
