02cbca3b6880f9e50b2d1d7eba0f9024a1695247
Efi Fogel <efif@post.tau.ac.il>
2002-05-03 10:14:35 +0000

    Updated

--- Packages/Arrangement/examples/Arrangement_2/short_names.h
+++ Packages/Arrangement/examples/Arrangement_2/short_names.h
@@ -1,3 +1,5 @@
+#include <CGAL/config.h> // needed for the LONGNAME flag
+
 #if defined(CGAL_CFG_NO_LONGNAME_PROBLEM) || defined(_MSC_VER)
 #define Quotient                        Qt
 #define Homogeneous                     Hs
@@ -19,11 +21,18 @@
 #define Arr_2_halfedge_base             AHB
 #define Arr_2_vertex_base               AVB
 #define Arr_2_face_base                 AFB
-#define bidirectional_iterator_tag      BIT
 #define In_place_list_iterator          IPLI
 #define In_place_list_const_iterator    IPLCI
 #define allocator                       All
 #define Planar_map_traits_wrap          PmTW
 #define Td_X_trapezoid                  TXT
 #define PL_X_curve_plus                 PXCP
+
+// #define bidirectional_iterator_tag      BIT
+
+#if defined(_MSC_VER)
+// Has no effect, probably bug in MSVC
+#pragma warning(disable:4503)
+#endif
+
 #endif
