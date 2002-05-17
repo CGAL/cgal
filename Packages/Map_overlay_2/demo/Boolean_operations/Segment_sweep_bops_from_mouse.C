cf20c3875737047309f179dab0e1595f11b930df
Ester Ezra <estere@post.tau.ac.il>
2002-05-17 15:02:11 +0000

    The demo fpr Boolean operations.
    The user clicks on mouse in order to construct the two maps.
    After the two maps are constructed, the user can operate boolean operations on both maps.
    The provided boolean operations are: intersection, union ans symmetric difference.

---
+++ Packages/Map_overlay_2/demo/Boolean_operations/Segment_sweep_bops_from_mouse.C
@@ -0,0 +1,409 @@
+// demo/Map_overlay_2/Segment_bops_from_mouse.C
+//
+// constructs an overlay of two segment planar maps from CGAL window.
+// We use the leda traits (therefore we are using leda functions).
+
+#include <CGAL/config.h> // needed for the LONGNAME flag
+
+#ifdef CGAL_CFG_NO_LONGNAME_PROBLEM
+// Define shorter names to please linker (g++/egcs)
+#define Planar_map Pm
+#define Arr_leda_segment_exact_traits Alset
+#define Bop_default_dcel Bdd
+#define Boolean_operation Bo
+#define In_place_list_iterator IPLI
+#define Arr_2_vertex_base Avb
+#define Arr_2_halfedge_base Ahb
+#define Arr_2_face_base Afb
+#define Point_2 pT
+#define Segment_2 sT
+#define Topological_map TpM
+#define _List_iterator Lit
+#define Halfedge hE
+#define Forward_circulator_tag Fct
+#endif
+
+#include <CGAL/basic.h>
+
+#ifndef CGAL_USE_LEDA
+int main()
+{
+
+  std::cout << "Sorry, this demo needs LEDA for visualisation.";
+  std::cout << std::endl;
+
+  return 0;
+}
+
+#else
+
+#include <CGAL/Arr_leda_segment_exact_traits.h>
+#include <CGAL/Pm_walk_along_line_point_location.h>
+#include <CGAL/Planar_map_2.h>
+
+#include <CGAL/sweep_to_construct_planar_map_2.h>
+
+#ifndef CGAL_ARR_2_BOP_DCEL_H
+#include <CGAL/Bop_default_dcel.h>
+#endif
+
+#ifndef CGAL_MAP_OVERLAY_NAIVE_H
+#include <CGAL/Map_overlay.h>
+#endif
+
+#ifndef CGAL_MAP_OVERLAY_NAIVE_H
+#include <CGAL/Map_overlay_naive.h>
+#endif
+
+#ifndef BOOLEAN_OPERATIONS_H
+#include <CGAL/Boolean_operations_2.h>
+#endif
+
+#include <LEDA/rat_window.h>
+#include <CGAL/Draw_preferences.h>
+#include <vector>
+
+#if defined(LEDA_NAMESPACE)
+using namespace leda;
+#endif
+
+typedef CGAL::Arr_leda_segment_exact_traits         Traits;
+
+typedef Traits::Point_2                             Point;
+typedef Traits::Curve_2                             Curve;
+typedef Traits::X_curve                             X_curve;
+
+typedef CGAL::Bop_default_dcel<Traits>              Dcel;
+typedef CGAL::Planar_map_2<Dcel, Traits>            PM;
+
+typedef CGAL::Map_overlay_default_notifier<PM>                  Ovl_change_notification;
+typedef CGAL::Map_overlay<PM, Ovl_change_notification>          MapOverlay;
+typedef CGAL::Boolean_operations<MapOverlay>                    Bops;
+
+typedef Bops::Faces_container                      Faces_container;
+typedef Bops::Halfedges_container                  Halfedges_container;
+typedef Bops::Vertices_container                   Vertices_container;
+
+typedef CGAL::Pm_walk_along_line_point_location<PM>             PmWalkPL;
+
+//I had to add these in global namespace for the program to compile
+
+/*
+  CGAL::Window_stream& operator<<(CGAL::Window_stream& os,
+  const Point& p)
+  {
+  //return os << leda_point(p.xcoordD(),p.ycoordD());
+  return os << p.to_point();
+  }
+  
+  
+  CGAL::Window_stream& operator<<(CGAL::Window_stream& os,
+  const X_curve &c)
+  {
+  return os << c.to_segment();
+  }*/
+
+// global variables are used so that the redraw function for the LEDA window
+// can be defined to draw information found in these variables.
+static PmWalkPL pm_walk1, pm_walk2;
+static PM pm1(&pm_walk1); 
+static PM pm2(&pm_walk2);
+static CGAL::Window_stream W(700, 700, "CGAL - Segment Arrangement Demo");
+
+CGAL_BEGIN_NAMESPACE
+Window_stream& operator<<(Window_stream& os, const PM &pm)
+{
+  My_Arr_drawer< PM,
+                 PM::Ccb_halfedge_const_circulator, 
+                 PM::Holes_const_iterator> drawer(os);
+  
+  draw_pm(pm, drawer, os);
+  
+  return os;
+}
+CGAL_END_NAMESPACE
+
+// redraw function for the LEDA window. 
+// used automatically when window reappears.
+void redraw(CGAL::Window_stream * wp) 
+{ wp->start_buffering();
+  wp->clear();
+  // draw planar map.
+  W << CGAL::BLUE;
+  *wp << pm1; 
+  W << CGAL::RED;
+  *wp << pm2;
+  
+  wp->flush_buffer();
+  wp->stop_buffering();
+}
+
+void  draw_and_locate_maps (Bops& bops , 
+                            const PM& pm1, 
+                            const PM& pm2, 
+                            CGAL::Window_stream& W)
+{  
+  W<<CGAL::BLUE;
+  W<<pm1;
+  W<<CGAL::RED;
+  W<<pm2;
+
+  for (;;) {
+    double  x,y;
+
+    int b = W.read_mouse(x,y);
+    if (b==7) 
+      break;
+    
+    W.set_node_width(3);
+    W.clear();
+    W<<CGAL::BLUE;
+    W<<pm1;
+    W<<CGAL::RED;
+    W<<pm2;
+
+    leda_color fg_col = leda_violet;
+    W.set_color(fg_col);
+    W.set_fill_color(fg_col);
+
+    if (b == 4 || b == 5 || b == 6){
+      Faces_container      faces_result;
+      Halfedges_container  halfedges_result;
+      Vertices_container   vertices_result;
+
+      //Bops bops = Bops(pm1,pm2);
+      
+      if (b==4)
+        bops.intersection(faces_result,halfedges_result,vertices_result);
+
+      else if (b==5)       
+        bops.Union(faces_result,halfedges_result,vertices_result);
+      
+      else if (b==6)
+        bops.symmetric_difference(faces_result, halfedges_result, vertices_result);
+
+      std::cout<<"number of faces is "<< faces_result.size()<<std::endl;
+      std::cout<<"The number of resulting halfedges is "<<halfedges_result.size()<<std::endl;
+      std::cout<<"The number of resulting vertices is "<<vertices_result.size()<<std::endl;
+
+      for (list<PM::Face_const_handle>::iterator  f_iter = faces_result.begin(); 
+           f_iter != faces_result.end(); ++f_iter){
+        PM::Face_const_handle fh = *f_iter;
+        if (fh->is_unbounded())
+          continue;
+        
+        W.set_color(fg_col);
+        W.set_fill_color(fg_col);
+        
+        PM::Ccb_halfedge_const_circulator cc = fh->outer_ccb();
+        leda_list<leda_point> points_list;
+        do {
+          leda_point p = cc->source()->point().to_point();
+          
+          points_list.push_back(p);
+        } while (++cc != fh->outer_ccb());
+        W.draw_filled_polygon(points_list);
+        
+        for (PM::Holes_const_iterator hit = fh->holes_begin(); hit != fh->holes_end(); ++hit) {
+          leda_list<leda_point> holes_points_list;
+          PM::Ccb_halfedge_const_circulator cc(*hit);
+          W.set_fill_color(leda_black);
+          W << CGAL::WHITE;
+          do{
+            W << cc->curve();
+            
+            leda_point p = cc->source()->point().to_point();
+            
+            points_list.push_back(p);
+          } while (++cc != *hit);
+          W.draw_filled_polygon(holes_points_list);
+        }
+        
+        do {
+          W <<CGAL::WHITE;
+          W << (*cc).curve();
+          //std::cout<<cc->curve()<<std::endl;
+        } while (++cc != fh->outer_ccb());
+        }
+      
+      for (list<PM::Halfedge_const_handle>::iterator h_iter = halfedges_result.begin(); 
+           h_iter != halfedges_result.end(); ++h_iter, ++h_iter){
+        PM::Halfedge_const_handle h = *h_iter;
+
+        W.set_color(fg_col);
+        W.set_node_width(4);
+        W << h->curve();
+      }
+      
+      for (list<PM::Vertex_const_handle>::iterator  v_iter = vertices_result.begin(); 
+           v_iter != vertices_result.end(); v_iter++){
+        W.set_node_width(4);
+        //for (Vertex_const_iterator  v_iter = bop.get_map_overlay().get_arr().vertices_begin(); 
+        //v_iter !=  bop.get_map_overlay().get_arr().vertices_end(); v_iter++){
+        
+        PM::Vertex_const_handle vh = *v_iter;
+        //Vertex_const_iterator vh = v_iter;
+        
+        /*if (tmp_notf.get_first_halfedge_above(vh) != vh->incident_halfedges() && tmp_notf.get_second_halfedge_above(vh) != vh->incident_halfedges())
+          W.set_color(leda_violet);
+          else if (tmp_notf.get_first_halfedge_above(vh) != vh->incident_halfedges())
+          W<<CGAL::BLUE;
+          else if (tmp_notf.get_second_halfedge_above(vh) != vh->incident_halfedges())
+          W << CGAL::RED;
+          else
+          W << CGAL::ORANGE;*/
+        
+        W.set_color(leda_violet);
+        W << vh->point();
+      }
+    }
+  }
+}
+
+
+int  read_planar_map(PM& pm, CGAL::Window_stream& W)
+{
+  std::vector<Point> cv1;
+  std::vector<Curve> curves;
+  Point pnt;
+  bool begin=true;
+  
+  while (true)
+    {
+      double x, y;
+      int b = W.get_mouse(x,y);
+      
+      if (b==2 || b==3){
+        //creating planar map.
+        Traits traits;
+        CGAL::sweep_to_construct_planar_map_2(curves.begin(), 
+                                              curves.end(),
+                                              traits, pm);
+        W << pm;
+        return b;
+      }
+    
+      pnt = Point(x,y);
+      
+      if (b == MOUSE_BUTTON(1))
+        {
+          
+          for(std::vector<Curve>::iterator iter = curves.begin();
+              iter != curves.end(); ++iter) {
+            //we are using the leda sqr_dist func
+            if ( pnt.sqr_dist(iter->source()) < ((W.xmax() - W.xmin())/50)*((W.xmax() - W.xmin())/50) )
+              pnt=iter->source();
+            if ( pnt.sqr_dist(iter->target()) < ((W.xmax() - W.xmin())/50)*((W.xmax() - W.xmin())/50) )
+              pnt=iter->target();
+          }
+          
+          cv1.push_back(pnt);
+          W << CGAL::BLACK;
+          W << pnt;
+          W << CGAL::GREEN;
+          
+          if (!begin) {
+            if ( cv1[0] == cv1[1] ){
+              //Error. Segment has a zero length.
+              W.set_status_string("Error. Segment has a zero length.");
+              redraw( &W );
+            }
+            else{ 
+              curves.push_back(Curve(cv1[0],cv1[1]));
+              W.set_status_string("Left mouse button - segment input. "
+                                  "Finish button -query mode");
+              W << Curve(cv1[0],cv1[1]);
+            }
+            cv1.clear();
+          }
+          begin=!begin;
+        }
+    }
+}
+
+int main()
+{
+  double x0=-700,x1=700,y0=-700;
+  
+  cout<<"bogi"<<endl;
+  
+  W.init(x0,x1,y0);
+  W.set_redraw(redraw);
+  W.set_mode(leda_src_mode);
+  W.set_node_width(3);
+  W.open_status_window();
+  W.button("Insert Map I",1);
+  W.button("Insert Map II",2);
+  W.button("Start Bops",3);
+  W.button("Intersection",4);
+  W.button("Union",5);
+  W.button("Symmetric Difference",6);
+  W.button("Exit",7);
+  
+  W.display();
+  
+  //read input from window
+  std::cout << "Left button to start and end the segment.\n";
+  std::cout << "Clicking close to a vertex assumes the location" 
+	    << "is at the vertex"
+	    << std::endl;
+ 
+  W.set_status_string( "Left mouse button - segment input. "
+		       "Finish button -query mode" );
+
+  while (true){
+    double x, y;
+    int b = W.get_mouse(x,y);
+    if (b==1){
+      std::cout<<"Insert first map"<<std::endl;
+      b = read_planar_map(pm1,W);
+      if (b==2){
+        std::cout<<"Insert second map"<<std::endl;
+        b=read_planar_map(pm2,W);
+      }
+    }
+    if (b==3)
+      break;
+  }
+
+  pm1.unbounded_face()->set_ignore_bop(false); 
+  pm2.unbounded_face()->set_ignore_bop(false);
+  
+  W << CGAL::RED;
+  W << pm1;
+  W << CGAL::BLUE;
+  W << pm2;
+  
+  MapOverlay map1(pm1);
+  MapOverlay map2(pm2);
+  Bops bops(MapOverlay(map1, map2));
+  //Bops bops(pm1, pm2);
+  
+  // Point Location Queries
+  W.set_status_string("Boolean Operations. "
+		      "Finish button - exit." );
+  
+  // if first map is empty
+  if (pm1.halfedges_begin() == pm1.halfedges_end()) 
+    {
+      std::cout << std::endl;
+      std::cout << "No edges were inserted to the first planar map. First Planar map is empty. Exiting.";
+      std::cout << std::endl;
+    }
+  
+  // if second map is empty
+  if (pm2.halfedges_begin() == pm2.halfedges_end()) 
+    {
+      std::cout << std::endl;
+      std::cout << "No edges were inserted to the first planar map. First Planar map is empty. Exiting.";
+      std::cout << std::endl;
+    }
+  
+  if (pm1.halfedges_begin() != pm1.halfedges_end() && 
+      pm2.halfedges_begin() != pm2.halfedges_end() )
+    draw_and_locate_maps(bops,pm1,pm2,W);
+ 
+  return 0;  
+}
+
+#endif // CGAL_USE_LEDA
