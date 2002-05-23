9ce8441daa606bebd73b0dad58c1459abe1c979d
Ester Ezra <estere@post.tau.ac.il>
2002-05-23 20:12:14 +0000

    The DCEL used by the Holes_split class. We need to indicate each edge whether it is an original edge or a vertical spliting edge. We also indicate for each face, whether it is a face associated with a hole.

---
+++ Packages/Map_overlay_2/include/CGAL/Bops/Holes_split_dcel.h
@@ -0,0 +1,109 @@
+#ifndef CGAL_HOLES_SPLIT_DCEL_H
+#define CGAL_SOLES_SPLIT_DCEL_H 
+
+#include <CGAL/Bop_default_dcel.h>
+
+CGAL_BEGIN_NAMESPACE
+
+template <class Face_base>
+class Holes_split_face : public  Face_bop<Face_base> {
+public:
+  typedef Face_bop<Face_base>         face_base;
+  typedef Holes_split_face            face_bop;
+  typedef const face_bop              const_face_bop;
+  typedef const_face_bop*             const_pointer;
+  typedef const_face_bop&             const_ref;
+  
+  Holes_split_face() : face_base(), in_hole_(false) {}
+
+  Holes_split_face(const_pointer f) : face_base(*f), in_hole_(false) {}
+
+  Holes_split_face(const_ref f) : face_base(f), in_hole_(false) {}
+
+  virtual ~Holes_split_face() {}
+  
+  const_ref operator=(const_ref f) 
+  {
+    if (this == &f)
+      return *this;
+
+    face_base::assign(f);
+    in_hole_ = f.in_hole_;
+    
+    return *this;
+  }
+
+  void set_in_hole(bool b) { in_hole_ = b ; }
+  
+  bool in_hole() const { return in_hole_; }
+
+  virtual void assign(const_ref f)
+  {
+    operator=(f);  
+  }
+  
+private:
+  bool in_hole_;
+};
+
+template <class Halfedge_base>
+class Holes_split_halfedge : public Halfedge_bop<Halfedge_base>
+{
+public:
+  typedef Halfedge_bop<Halfedge_base>   halfedge_base;
+  typedef Holes_split_halfedge          halfedge;
+  typedef const halfedge                const_halfedge;
+  typedef const_halfedge*               const_pointer;
+  typedef const_halfedge&               const_ref;
+  
+  Holes_split_halfedge() : 
+    halfedge_base(), 
+    decomposing_(false) {}
+  
+  Holes_split_halfedge(const_pointer e) : 
+    halfedge_base(*e),
+    decomposing_(e->decomposing_) {}
+
+  Holes_split_halfedge(const_ref e) : 
+    halfedge_base(e) ,
+    decomposing_(e.decomposing_) {} 
+  
+  virtual ~Holes_split_halfedge() {}
+  
+  const_ref operator=(const_ref e) 
+  {
+    halfedge_base::assign(e);
+    
+    decomposing_ = e.decomposing_;
+    
+    return *this;
+  }
+  
+  virtual void assign(const_ref e)
+  {
+    operator=(e);  
+  }
+  
+  void set_decomposing(bool b) { decomposing_ = b; }
+  
+  bool decomposing() const { return decomposing_; } 
+  
+  
+protected:
+  bool decomposing_;
+};
+
+template <class Traits, class Vertex_base = Pm_vertex_base<typename Traits::Point>, 
+  class Halfedge_base = Pm_halfedge_base<typename Traits::X_curve> , 
+  class Face_base =  Pm_face_base> 
+class Holes_split_dcel : public Pm_dcel<Vertex_bop<Vertex_base> , 
+                                        Holes_split_halfedge<Halfedge_base>, 
+                                        Holes_split_face<Face_base> > 
+{
+public:  // CREATION
+  Holes_split_dcel() {} 
+};
+
+CGAL_END_NAMESPACE
+
+#endif
