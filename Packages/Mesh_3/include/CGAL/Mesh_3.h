e7fbc1a8c84da49d2057d832a96e8582e319015a
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2002-07-12 13:22:01 +0000

    Temp version.

--- Packages/Mesh_3/include/CGAL/Mesh_3.h
+++ Packages/Mesh_3/include/CGAL/Mesh_3.h
@@ -15,7 +15,36 @@ public:
   typedef typename Tr::Geom_traits Geom_traits;
   typedef typename Tr::Triangulation_data_structure Tds;
 
+  typedef typename Tr::Face_handle            Face_handle;
+public:
+    // constructor from a Triangulation& t
+  explicit 
+  Mesh_3(Triangulation& t, const Geom_traits& gt = Geom_traits(),
+	 bool dont_refine = false);
+
   void fill_facette_map();
-};
+
+private:
+  // PRIVATE TYPES
+  class Is_really_a_constrained_edge {
+    const Self& _m;
+  public:
+    explicit Is_really_a_constrained_edge(const Self& m) : _m(m) {};
+    bool operator()(const Constrained_edge& ce) const
+      {
+	Face_handle fh;
+	int i;
+	return _m.is_edge(ce.first, ce.second, fh,i) &&
+	  fh->is_constrained(i);
+      }
+  };
+  
+private:
+  
+  edges_to_be_conform 
+
+}; // end Mesh_3
+
+} //end namespace CGAL
 
 #endif
