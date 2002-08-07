0a2fa2e3225553ca8017b47d571ceb1a36779fbc
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2002-08-07 16:20:43 +0000

    - Test if CGAL_ALLOCATOR is already defined before defining it. - Introduce CGAL_MEMORY, similar to LEDA_MEMORY.

--- Old_Packages/Kernel_basic/include/CGAL/memory.h
+++ Old_Packages/Kernel_basic/include/CGAL/memory.h
@@ -1,6 +1,6 @@
 // ======================================================================
 //
-// Copyright (c) 1999 The CGAL Consortium
+// Copyright (c) 1999, 2002 The CGAL Consortium
 //
 // This software and related documentation is part of an INTERNAL release
 // of the Computational Geometry Algorithms Library (CGAL). It is not
@@ -15,7 +15,7 @@
 // package       : Kernel_basic
 // revision      : $Revision$
 // revision_date : $Date$
-// author(s)     : Michael Seel
+// author(s)     : Michael Seel, Sylvain Pion
 //
 // coordinator   : MPI, Saarbruecken
 // ======================================================================
@@ -23,16 +23,27 @@
 #ifndef CGAL_MEMORY_H
 #define CGAL_MEMORY_H
 
-// This file defines the macro CGAL_ALLOCATOR(t) which is the default
-// allocator used by CGAL.
+// CGAL_ALLOCATOR(t) defines the default allocator used by CGAL.
+// CGAL_MEMORY(t) overloads the new and delete operators for a given class.
 
 #ifdef CGAL_USE_LEDA
 #  include <LEDA/allocator.h>
-#  define CGAL_ALLOCATOR(t) leda_allocator< t >
-
+#  ifndef CGAL_ALLOCATOR
+#    define CGAL_ALLOCATOR(t) leda_allocator< t >
+#  endif
+#  ifndef CGAL_MEMORY
+#    define CGAL_MEMORY(t)    LEDA_MEMORY(t)
+#  endif
 #else
 #  include <memory>
-#  define CGAL_ALLOCATOR(t) std::allocator< t >
+#  ifndef CGAL_ALLOCATOR
+#    define CGAL_ALLOCATOR(t) std::allocator< t >
+// For debugging with GCC 3.1, the following allocator can be useful :
+// std::__allocator<t, std::__debug_alloc<std::__malloc_alloc_template<0> > >
+#  endif
+#  ifndef CGAL_MEMORY
+#    define CGAL_MEMORY(t)
+#  endif
 #endif
 
 #endif // CGAL_MEMORY_H
