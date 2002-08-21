10bac0e1b138cd63625e27d8352f0fec1d5fc42f
Ester Ezra <estere@post.tau.ac.il>
2002-08-21 15:43:55 +0000

    Just editing the line and fix identation.

--- Packages/Map_overlay_2/include/CGAL/Bops/Polygons_from_faces.h
+++ Packages/Map_overlay_2/include/CGAL/Bops/Polygons_from_faces.h
@@ -30,10 +30,12 @@
 CGAL_BEGIN_NAMESPACE     
 
 template <class Planar_map_, class Polygon_>
-class Polygons_from_faces{
+class Polygons_from_faces
+{
 public:
   typedef Planar_map_  Planar_map;
   typedef Polygon_     Polygon;
+  //typedef typename Polygon::Point_2  Point;
   //typedef typename Planar_map::Face_const_iterator Face_const_iterator;
   
   template <class InputIterator, class OutputIterator>
@@ -46,20 +48,22 @@ public:
     
     Polygon                        poly;
     
-    for (f_iter = faces_begin; f_iter != faces_end; ++f_iter) {
+    for (f_iter = faces_begin; f_iter != faces_end; ++f_iter) 
+      {
+        
+        if (f_iter->is_unbounded() || f_iter->in_hole())
+          continue;
+        
+        poly.erase(poly.vertices_begin(), poly.vertices_end());
+        typename Planar_map::Ccb_halfedge_circulator 
+          cc=f_iter->outer_ccb();
+        do 
+          {
+            poly.push_back(cc->source()->point());
+          } while (++cc != f_iter->outer_ccb());
       
-      if (f_iter->is_unbounded() || f_iter->in_hole())
-        continue;
-      
-      poly.erase(poly.vertices_begin(), poly.vertices_end());
-      typename Planar_map::Ccb_halfedge_circulator 
-        cc=f_iter->outer_ccb();
-      do {
-        poly.push_back(cc->source()->point());
-      } while (++cc != f_iter->outer_ccb());
-      
-      *polygons++ = poly;
-    }
+        *polygons++ = poly;
+      }
   }
   
 };
