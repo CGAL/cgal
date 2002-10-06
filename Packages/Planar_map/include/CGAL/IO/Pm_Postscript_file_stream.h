318a0a1d6c6e3aaf6c1515c2309a977cf7cd4985
Efi Fogel <efif@post.tau.ac.il>
2002-10-06 16:12:20 +0000

    added const

--- Packages/Planar_map/include/CGAL/IO/Pm_Postscript_file_stream.h
+++ Packages/Planar_map/include/CGAL/IO/Pm_Postscript_file_stream.h
@@ -28,66 +28,23 @@
 #ifndef CGAL_IO_PM_POSTSCRIPT_FILE_STREAM_H
 #define CGAL_IO_PM_POSTSCRIPT_FILE_STREAM_H
 
-#ifndef CGAL_LEDA_WINDOW_H
 #include <CGAL/IO/leda_window.h>
-#endif
-
-#ifndef CGAL_POSTSCRIPT_FILE_STREAM_H
 #include <CGAL/IO/Postscript_file_stream.h>
-#endif
-
-#ifndef CGAL_PLANAR_MAP_2_H
 #include <CGAL/Planar_map_2.h>
-#endif
-  
-/*  #ifndef CGAL_IO_PM_BOUNDING_BOX_BASE_WINDOW_STREAM_H
-  #include <CGAL/IO/Pm_bounding_box_base_Window_stream.h>
-  #endif
-*/
-
-#ifndef CGAL_IO_PM_DRAWER_H
 #include <CGAL/IO/Pm_drawer.h>
-#endif
-
-#ifndef CGAL_IO_DRAW_PM_H
 #include  <CGAL/IO/draw_pm.h>
-#endif
 
 CGAL_BEGIN_NAMESPACE
 
 template <class Dcel,class Traits>
 Postscript_file_stream & operator << (Postscript_file_stream & ps,
-                                      Planar_map_2<Dcel,Traits> & pm)
+                                      const Planar_map_2<Dcel,Traits> & pm)
 {
   Pm_drawer< Planar_map_2<Dcel,Traits> , Postscript_file_stream>  drawer(ps);
-  
   draw_pm(pm, drawer, ps);
-
   return ps;
 }  
-
-
-/*template <class Dcel,class Traits>
-  Window_stream& write(Window_stream& os, Planar_map_2<Dcel,Traits> &m)
-  {
-  //  os << *m.get_bounding_box();
-  Halfedge_iterator it = m.halfedges_begin(), end = m.halfedges_end();
-  const Traits& traits=m.get_traits();
-  while(it != end){
-	write(os,it->curve(),traits);
-        ++it;++it;
-        }
-        return os;
-        } */ 
-
 
 CGAL_END_NAMESPACE
 
 #endif
-
-
-
-
-
-
-
