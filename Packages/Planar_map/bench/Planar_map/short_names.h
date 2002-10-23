eb26b0bc1e9794c9ad2e66f39cbe2b24863b2956
Efi Fogel <efif@post.tau.ac.il>
2002-10-23 22:34:13 +0000

    updated

--- Packages/Planar_map/bench/Planar_map/short_names.h
+++ Packages/Planar_map/bench/Planar_map/short_names.h
@@ -1,32 +1,38 @@
+#include <CGAL/config.h> // needed for the LONGNAME flag
+
 #if defined(CGAL_CFG_NO_LONGNAME_PROBLEM) || defined(_MSC_VER)
 #define Quotient                        Qt
 #define Homogeneous                     Hs
 #define Cartesian                       Cn
 #define Simple_cartesian                SC
-#define Cartesian_converter             CC
 #define Filtered_kernel                 FKl
+#define Segment_2                       St
+#define Point_2                         Pt2
 #define Topological_map                 TM
-#define Pm_default_dcel                 PDD
-#define Pm_halfedge_base                PHB
-#define Pm_vertex_base                  PVB
-#define Pm_face_base                    PFB
 #define Planar_map_2                    PMp
-#define _Nonconst_traits                NT
-#define Td_traits                       TT
-#define Pm_segment_traits_2             PST
-#define Planar_map_traits_wrap          PMTW
-#define Interval_converter              IC
-#define NT_converter                    NC
+#define Arrangement_2                   Ar
+#define I_Polyhedron_iterator           IPI
+#define I_Polyhedron_const_iterator     IPCI
+#define Arr_2_default_dcel              ADD
+#define Arr_segment_exact_traits        AST
+#define Arr_circles_real_traits         ACRT
+#define Arr_polyline_traits             APT
+#define Arr_base_node                   ABN
+#define Arr_2_halfedge_base             AHB
+#define Arr_2_vertex_base               AVB
+#define Arr_2_face_base                 AFB
+#define In_place_list_iterator          IPLI
+#define In_place_list_const_iterator    IPLCI
+#define allocator                       All
+#define Planar_map_traits_wrap          PmTW
 #define Td_X_trapezoid                  TXT
-#define Interval_nt                     IN
 #define PL_X_curve_plus                 PXCP
-#define bidirectional_iterator_tag      BIT
-#define Forward_circulator_tag          FCT
 
-#define remove_in_face_interior         RIFI
-#define I_Polyhedron_const_iterator     PCI
-#define _Rb_tree_iterator               RTI
+// #define bidirectional_iterator_tag      BIT
 
-#define _Rb_tree                        RT
-//#define Trapezoidal_decomposition_2     TD
+#if defined(_MSC_VER)
+// Has no effect, probably bug in MSVC
+#pragma warning(disable:4503)
+#endif
+
 #endif
