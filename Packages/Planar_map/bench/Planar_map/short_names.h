4def410d1c8572d0eed56634667e0c8d3883a7b2
Efi Fogel <efif@post.tau.ac.il>
2002-10-23 22:02:17 +0000

    1st revision

---
+++ Packages/Planar_map/bench/Planar_map/short_names.h
@@ -0,0 +1,32 @@
+#if defined(CGAL_CFG_NO_LONGNAME_PROBLEM) || defined(_MSC_VER)
+#define Quotient                        Qt
+#define Homogeneous                     Hs
+#define Cartesian                       Cn
+#define Simple_cartesian                SC
+#define Cartesian_converter             CC
+#define Filtered_kernel                 FKl
+#define Topological_map                 TM
+#define Pm_default_dcel                 PDD
+#define Pm_halfedge_base                PHB
+#define Pm_vertex_base                  PVB
+#define Pm_face_base                    PFB
+#define Planar_map_2                    PMp
+#define _Nonconst_traits                NT
+#define Td_traits                       TT
+#define Pm_segment_traits_2             PST
+#define Planar_map_traits_wrap          PMTW
+#define Interval_converter              IC
+#define NT_converter                    NC
+#define Td_X_trapezoid                  TXT
+#define Interval_nt                     IN
+#define PL_X_curve_plus                 PXCP
+#define bidirectional_iterator_tag      BIT
+#define Forward_circulator_tag          FCT
+
+#define remove_in_face_interior         RIFI
+#define I_Polyhedron_const_iterator     PCI
+#define _Rb_tree_iterator               RTI
+
+#define _Rb_tree                        RT
+//#define Trapezoidal_decomposition_2     TD
+#endif
