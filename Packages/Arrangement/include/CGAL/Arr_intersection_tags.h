3b7ed9df038fc3ae28b0887f2d699bf6f6485bb9
Efi Fogel <efif@post.tau.ac.il>
2002-11-14 14:42:00 +0000

    intersection tages

---
+++ Packages/Arrangement/include/CGAL/Arr_intersection_tags.h
@@ -0,0 +1,32 @@
+// ======================================================================
+//
+// Copyright (c) 2000 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+//
+// release       : $$
+// release_date  : $$
+//
+// file          : include/CGAL/Arr_intersection_tags.h
+// package       : Arrangement
+// maintainer    : Eyal Flato <flato@post.tau.ac.il>
+// author(s)     : Efi Fogel <efif@post.tau.ac.il>
+// coordinator   : Tel-Aviv University (Dan Halperin <halperin@math.tau.ac.il>)
+//
+// ======================================================================
+
+#ifndef CGAL_ARR_INTERSECTION_TAGS_H
+#define CGAL_ARR_INTERSECTION_TAGS_H
+
+CGAL_BEGIN_NAMESPACE
+
+struct Lazy_intersection_tag {};
+struct Efficient_intersection_tag {};
+
+CGAL_END_NAMESPACE
+
+#endif
