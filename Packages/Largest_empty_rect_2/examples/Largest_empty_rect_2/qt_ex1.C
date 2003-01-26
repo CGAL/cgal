57579bfaba2974755c65af47d9d6606a1116d766
Eli Packer <elip@post.tau.ac.il>
2003-01-26 12:57:26 +0000

    for testing qt

---
+++ Packages/Largest_empty_rect_2/examples/Largest_empty_rect_2/qt_ex1.C
@@ -0,0 +1,25 @@
+//demo/Qt_widget/Examples/hellosegment.C
+
+#include <CGAL/Cartesian.h>
+#include <CGAL/IO/Qt_widget.h>
+
+#include <qapplication.h>
+
+typedef CGAL::Cartesian<int> Rep;
+typedef CGAL::Point_2<Rep> Point;
+typedef CGAL::Segment_2<Rep> Segment;
+
+int main( int argc, char **argv )
+{
+  QApplication app( argc, argv );
+  CGAL::Qt_widget *W = new CGAL::Qt_widget();
+    app.setMainWidget( W );
+    W->resize(600, 600);
+    W->set_window(0, 600, 0, 600);
+    W->show();
+    W->lock();
+    *W << CGAL::BackgroundColor(CGAL::ORANGE) << CGAL::RED;
+    *W << Segment(Point(100,100), Point(400,400));
+    W->unlock();
+    return app.exec();
+}
