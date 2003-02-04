a99d93837f261f48ad39eec6e82040fe5d7848a9
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2003-02-04 15:59:11 +0000

    Regular combined with the Trinagulation_hierarchy_2

---
+++ Packages/Triangulation_2/test/Triangulation_2_Delaunay/test_regular_hierarchy_2.C
@@ -0,0 +1,80 @@
+// ============================================================================
+//
+// Copyright (c) 1998 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+// 
+// release       :
+// release_date  :
+// 
+// source        : $RCSfile$
+// file          : test/Triangulation/test_triangulation_2.C
+// revision      : $revision$
+// revision_date : $Date$
+// author(s)     : Francois Rebufat (Francois.Rebufat@sophia.inria.fr)
+//                 Mariette Yvinec  <Mariette.Yvinec@sophia.inria.fr>
+//                 Andreas Fabri    <Andreas.Fabri@geometryfactory.com>
+//
+// coordinator   : INRIA Sophia-Antipolis
+// ============================================================================
+
+
+
+#include <CGAL/_test_types.h>
+#include <CGAL/Weighted_point.h>
+
+#include <CGAL/Regular_triangulation_euclidean_traits_2.h>
+#include <CGAL/Regular_triangulation_2.h>
+#include <CGAL/Triangulation_hierarchy_2.h>
+#include <CGAL/_test_cls_regular_triangulation_2.C>
+
+//__________
+typedef CGAL::Regular_triangulation_euclidean_traits_2  <Test_rep_cartesian, Ftype> RGt;
+typedef CGAL::Regular_triangulation_vertex_base_2<RGt> Vbb;
+typedef CGAL::Triangulation_hierarchy_vertex_base_2<Vbb> Vb;
+typedef CGAL::Regular_triangulation_face_base_2<RGt>  Fb;
+typedef CGAL::Triangulation_data_structure_2<Vb,Fb>  Tds;
+typedef CGAL::Regular_triangulation_2<RGt,Tds>  Rt;
+typedef CGAL::Triangulation_hierarchy_2<Rt> Regular_triangulation_cartesian;
+
+// Explicit instantiation of the whole class :
+template class CGAL::Triangulation_hierarchy_2<Rt>;
+
+//__________
+typedef CGAL::Regular_triangulation_euclidean_traits_2  <Test_rep_cartesian, Rtype> RGt2;
+typedef CGAL::Regular_triangulation_vertex_base_2<RGt2> Vbb2;
+typedef CGAL::Triangulation_hierarchy_vertex_base_2<Vbb2> Vb2;
+typedef CGAL::Regular_triangulation_face_base_2<RGt2>  Fb2;
+typedef CGAL::Triangulation_data_structure_2<Vb2,Fb2>  Tds2;
+typedef CGAL::Regular_triangulation_2<RGt2,Tds2>  Rt2;
+typedef CGAL::Triangulation_hierarchy_2<Rt2> Regular_triangulation_homogeneous;
+// Explicit instantiation of the whole class :
+//template class CGAL::Triangulation_hierarchy_2<Rt2>;
+
+
+
+
+int main()
+{
+  std::cout << "Testing Triangulation_hierarchy_2<Regular_triangulation_2>" <<std::endl;
+  std::cout << " with CGAL::Regular_triangulation_euclidean_traits_2 : "
+	    <<std::endl;
+  std::cout << "using  Cartesian  points "   <<  std::endl;
+
+  std::cout << "Testing hierarchy" << std::endl;
+  _test_cls_reg_triangulation_2( Regular_triangulation_cartesian() );
+
+  std::cout << "Testing Triangulation_hierarchy_2<Regular_triangulation_2>" <<std::endl;
+  std::cout << " with CGAL::Regular_triangulation_euclidean_traits_2 : "
+	    <<std::endl;
+  std::cout << "using  Homogeneous points "<< std::endl;
+
+  _test_cls_reg_triangulation_2( Regular_triangulation_homogeneous() );
+
+  return 0;
+}
+
