2eae825ee6d1cbd3276808553f121b32aa3d25d6
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2003-02-12 19:14:40 +0000

    - Rename hidden parameter to avoid clash on certain compilers...

--- Packages/Kernel_23/include/CGAL/Kernel/Type_equality_wrapper.h
+++ Packages/Kernel_23/include/CGAL/Kernel/Type_equality_wrapper.h
@@ -31,38 +31,35 @@ CGAL_BEGIN_NAMESPACE
 // (and similar for the other types).
 
 template < typename K_base >
-class Type_equality_wrapper
+struct Type_equality_wrapper
   : public K_base
 {
-    typedef typename K_base::Kernel                 K_;    
+    typedef K_base                                  Kernel_base;
+    typedef typename K_base::Kernel                 Kernel;
 
-public:
+    typedef CGAL::Point_2<Kernel>                   Point_2;
+    typedef CGAL::Vector_2<Kernel>                  Vector_2;
+    typedef CGAL::Direction_2<Kernel>               Direction_2;
+    typedef CGAL::Line_2<Kernel>                    Line_2;
+    typedef CGAL::Ray_2<Kernel>                     Ray_2;
+    typedef CGAL::Segment_2<Kernel>                 Segment_2;
+    typedef CGAL::Triangle_2<Kernel>                Triangle_2;
+    typedef CGAL::Circle_2<Kernel>                  Circle_2;
+    typedef CGAL::Iso_rectangle_2<Kernel>           Iso_rectangle_2;
+    typedef CGAL::Aff_transformation_2<Kernel>      Aff_transformation_2;
 
-    typedef K_base                                  Kernel_base;    
-
-    typedef CGAL::Point_2<K_>                       Point_2;
-    typedef CGAL::Vector_2<K_>                      Vector_2;
-    typedef CGAL::Direction_2<K_>                   Direction_2;
-    typedef CGAL::Line_2<K_>                        Line_2;
-    typedef CGAL::Ray_2<K_>                         Ray_2;
-    typedef CGAL::Segment_2<K_>                     Segment_2;
-    typedef CGAL::Triangle_2<K_>                    Triangle_2;
-    typedef CGAL::Circle_2<K_>                      Circle_2;
-    typedef CGAL::Iso_rectangle_2<K_>               Iso_rectangle_2;
-    typedef CGAL::Aff_transformation_2<K_>          Aff_transformation_2;
-
-    typedef CGAL::Point_3<K_>                       Point_3;
-    typedef CGAL::Vector_3<K_>                      Vector_3;
-    typedef CGAL::Direction_3<K_>                   Direction_3;
-    typedef CGAL::Line_3<K_>                        Line_3;
-    typedef CGAL::Plane_3<K_>                       Plane_3;
-    typedef CGAL::Ray_3<K_>                         Ray_3;
-    typedef CGAL::Segment_3<K_>                     Segment_3;
-    typedef CGAL::Triangle_3<K_>                    Triangle_3;
-    typedef CGAL::Tetrahedron_3<K_>                 Tetrahedron_3;
-    typedef CGAL::Sphere_3<K_>                      Sphere_3;
-    typedef CGAL::Iso_cuboid_3<K_>                  Iso_cuboid_3;
-    typedef CGAL::Aff_transformation_3<K_>          Aff_transformation_3;
+    typedef CGAL::Point_3<Kernel>                   Point_3;
+    typedef CGAL::Vector_3<Kernel>                  Vector_3;
+    typedef CGAL::Direction_3<Kernel>               Direction_3;
+    typedef CGAL::Line_3<Kernel>                    Line_3;
+    typedef CGAL::Plane_3<Kernel>                   Plane_3;
+    typedef CGAL::Ray_3<Kernel>                     Ray_3;
+    typedef CGAL::Segment_3<Kernel>                 Segment_3;
+    typedef CGAL::Triangle_3<Kernel>                Triangle_3;
+    typedef CGAL::Tetrahedron_3<Kernel>             Tetrahedron_3;
+    typedef CGAL::Sphere_3<Kernel>                  Sphere_3;
+    typedef CGAL::Iso_cuboid_3<Kernel>              Iso_cuboid_3;
+    typedef CGAL::Aff_transformation_3<Kernel>      Aff_transformation_3;
 };
 
 CGAL_END_NAMESPACE
