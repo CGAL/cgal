e822b1a18581089a17e01e81f8b1474a8ecb52f4
Kaspar Fischer <fischerk@inf.ethz.ch>
2003-02-20 09:59:04 +0000

    - Naming conventions are now respected (i.e.   constants are in upper case).

--- Packages/Min_sphere_of_spheres_d/examples/Min_sphere_of_spheres_d/min_sphere_of_spheres_d_example_3.C
+++ Packages/Min_sphere_of_spheres_d/examples/Min_sphere_of_spheres_d/min_sphere_of_spheres_d_example_3.C
@@ -8,8 +8,8 @@
 #include <CGAL/Gmpq.h>
 #include <CGAL/Min_sphere_of_spheres_d.h>
 
-const int n = 1000;                       // number of spheres
-const int Low = 0, High = 10000;          // range of coordinates and radii
+const int N = 1000;                       // number of spheres
+const int LOW = 0, HIGH = 10000;          // range of coordinates and radii
 
 typedef CGAL::Gmpq                        FT;
 //typedef double                          FT;
@@ -23,12 +23,12 @@ int main () {
   std::vector<Sphere> S;                  // n spheres
   CGAL::Random r;                         // random number generator
   
-  for (int i=0; i<n; ++i) {
-    const FT x = r.get_int(Low,High),
-             y = r.get_int(Low,High),
-             z = r.get_int(Low,High);
+  for (int i=0; i<N; ++i) {
+    const FT x = r.get_int(LOW,HIGH),
+             y = r.get_int(LOW,HIGH),
+             z = r.get_int(LOW,HIGH);
     Point p(x,y,z);                       // random center...
-    S.push_back(Sphere(p,r.get_int(Low,High))); // ...and random radius
+    S.push_back(Sphere(p,r.get_int(LOW,HIGH))); // ...and random radius
   }
   
   Min_sphere ms(S.begin(),S.end());       // check in the spheres
--- Packages/Min_sphere_of_spheres_d/examples/Min_sphere_of_spheres_d/min_sphere_of_spheres_d_example_d.C
+++ Packages/Min_sphere_of_spheres_d/examples/Min_sphere_of_spheres_d/min_sphere_of_spheres_d_example_d.C
@@ -8,28 +8,28 @@
 #include <CGAL/Gmpq.h>
 #include <CGAL/Min_sphere_of_spheres_d.h>
 
-const int n = 1000;                       // number of spheres
-const int d = 3;                          // dimension of points
-const int Low = 0, High = 10000;          // range of coordinates and radii
+const int N = 1000;                       // number of spheres
+const int D = 3;                          // dimension of points
+const int LOW = 0, HIGH = 10000;          // range of coordinates and radii
 
 typedef CGAL::Gmpq                        FT;
 //typedef double                          FT;
 typedef CGAL::Cartesian_d<FT>             K;
-typedef CGAL::Min_sphere_of_spheres_d_traits_d<K,FT,d> Traits;
+typedef CGAL::Min_sphere_of_spheres_d_traits_d<K,FT,D> Traits;
 typedef CGAL::Min_sphere_of_spheres_d<Traits> Min_sphere;
 typedef K::Point_d                        Point;
 typedef Traits::Sphere                    Sphere;
 
 int main () {
   std::vector<Sphere> S;                  // n spheres
-  FT coord[d];                            // d coordinates
+  FT coord[D];                            // d coordinates
   CGAL::Random r;                         // random number generator
   
-  for (int i=0; i<n; ++i) {
-    for (int j=0; j<d; ++j)
-      coord[j] = r.get_int(Low,High);
-    Point p(d,coord,coord+d);             // random center...
-    S.push_back(Sphere(p,r.get_int(Low,High))); // ...and random radius
+  for (int i=0; i<N; ++i) {
+    for (int j=0; j<D; ++j)
+      coord[j] = r.get_int(LOW,HIGH);
+    Point p(D,coord,coord+D);             // random center...
+    S.push_back(Sphere(p,r.get_int(LOW,HIGH))); // ...and random radius
   }
   
   Min_sphere ms(S.begin(),S.end());       // check in the spheres
