7255939b797da0867ad620bd7a5fa4334a16e992
Eli Packer <elip@post.tau.ac.il>
2003-03-04 13:07:09 +0000

    ***

--- Packages/Snap_rounding_2/demo/Snap_rounding_2/demo.C
+++ Packages/Snap_rounding_2/demo/Snap_rounding_2/demo.C
@@ -1,17 +1,3 @@
-// Making sure test doesn't fail if LEDA is not installed
-#if ! defined(CGAL_USE_LEDA)
-
-int main(int argc, char* argv[])
-{
-  std::cout << "A try to run demo with LEDA but LEDA is not installed.";
-  std::cout << std::endl;
-  std::cout << "Demo is not performed.";
-  std::cout << std::endl;
-
-  return 0;
-}
-#else
-
 #include <fstream>
 #include <CGAL/Cartesian.h>
 #include <CGAL/Segment_2.h>
@@ -22,22 +8,101 @@ int main(int argc, char* argv[])
 #include <CGAL/IO/cgal_window_redefine.h>
 #include "../../include/CGAL/Snap_rounding_2.h"
 
-#include <fstream>
+typedef leda_rational                                  Number_Type;
+typedef CGAL::Cartesian<Number_Type>                   Rep;
+typedef CGAL::Segment_2<Rep>                           Segment_2;
+typedef CGAL::Point_2<Rep>                             Point_2;
+typedef CGAL::Snap_rounding_2<Rep>                     Snap_rounding_2;
+typedef CGAL::Iso_rectangle_2<Rep>                     Iso_rectangle_2;
+typedef Snap_rounding_2::Segment_iterator              Segment_iterator;
+typedef Snap_rounding_2::Polyline_const_iterator       Polyline_const_iterator;
+typedef Snap_rounding_2::Point_const_iterator          Point_const_iterator;
+typedef CGAL::Window_stream                            Window_stream;
 
-typedef leda_rational                            Number_Type;
-typedef CGAL::Cartesian<Number_Type>             Rep;
-typedef Rep::Segment_2                           Segment_2;
-typedef Rep::Point_2                             Point_2;
-typedef Rep::Iso_rectangle_2                     Iso_rectangle_2;
-typedef CGAL::Snap_rounding_2<Rep>               Snap_rounding_2;
-typedef Snap_rounding_2::Segment_iterator        Segment_iterator;
-typedef Snap_rounding_2::Polyline_const_iterator Polyline_const_iterator;
-typedef Snap_rounding_2::Point_const_iterator    Point_const_iterator;
-typedef CGAL::Window_stream Window_stream;
+#define MIN_X 0
+#define MIN_Y 0
+#define MAX_X 10
+#define MAX_Y 10
+#define PRECISION 1
 
-void window_output(Snap_rounding_2 &s,
+Number_Type min(const Number_Type &p,const Number_Type &q,Number_Type &r)
+{
+  return(p > q ? min(q,r) : min(p,r));
+}
+
+Number_Type max(const Number_Type &p,const Number_Type &q,const Number_Type &r)
+{
+  return(p > q ? max(p,r) : max(q,r));
+}
+
+
+void get_extreme_points(std::list<Segment_2> &seg_list,
+                        Number_Type &min_x,
+                        Number_Type &min_y,
+                        Number_Type &max_x,
+                        Number_Type &max_y)
+{
+  std::list<Segment_2>::iterator iter = seg_list.begin();
+
+  min_x = min(iter->source().x(),iter->target().x());
+  max_x = max(iter->source().x(),iter->target().x());
+  min_y = min(iter->source().y(),iter->target().y());
+  max_y = max(iter->source().y(),iter->target().y());
+   
+  for(++iter;iter != seg_list.end();++iter) {
+    min_x = min(iter->source().x(),iter->target().x(),min_x);
+    max_x = max(iter->source().x(),iter->target().x(),max_x);
+    min_y = min(iter->source().y(),iter->target().y(),min_y);
+    max_y = max(iter->source().y(),iter->target().y(),max_y);
+  }
+}
+
+void show_output(Snap_rounding_2 &s,
+                 Number_Type prec,
+                 CGAL::Window_stream &w)
+{
+  // draw original segments
+  /*w << CGAL::BLACK;
+  for(Segment_iterator i1 = s.segments_begin();
+      i1 != s.segments_end();
+      ++i1)
+    w << *i1;
+  */
+  // draw isr polylines
+  for(Polyline_const_iterator i = s.polylines_begin();
+      i != s.polylines_end();
+      ++i) {
+    Point_const_iterator prev = i->begin();
+    Point_const_iterator i2 = prev;
+    bool seg_painted = false;
+    w << CGAL::GREEN << Iso_rectangle_2(Point_2(i2->x() - prec / 2.0,
+					        i2->y() - prec / 2.0),
+                                        Point_2(i2->x() + prec / 2.0,
+					        i2->y() + prec / 2.0));
+    for(++i2;
+        i2 != i->end();
+        ++i2) {
+      seg_painted = true;
+      w << CGAL::RED << Segment_2(*prev,*i2);
+      w << CGAL::GREEN << Iso_rectangle_2(Point_2(i2->x() - prec / 2.0,
+					          i2->y() - prec / 2.0),
+                                          Point_2(i2->x() + prec / 2.0,
+					          i2->y() + prec / 2.0));
+      prev = i2;
+    }
+
+    if(!seg_painted) // segment entirely inside hot pixel
+      w << *(i->begin());
+  }
+}
+
+void display_bounding_box(Snap_rounding_2 &s,CGAL::Window_stream &W)
+{
+  return;// !!!!! check
+}
+
+void window_output(Snap_rounding_2 &s,Window_stream &w,
                    Number_Type prec,
-                   Window_stream &w,
                    bool wait_for_click)
 {
   w << CGAL::BLACK;
@@ -73,7 +138,6 @@ void window_output(Snap_rounding_2 &s,
 					          i2->y() + prec / 2.0));
       prev = i2;
     }
-
     if(!seg_painted) // segment entirely inside hot pixel
       w << *(i->begin());
   }
@@ -86,49 +150,16 @@ void window_output(Snap_rounding_2 &s,
   }
 }
 
-void draw_orig(CGAL::Window_stream &w,std::list<Segment_2> &seg_list)
-{
-  w << CGAL::BLACK;
-
-  Point_2(1,1);
-
-  for(std::list<Segment_2>::iterator iter = seg_list.begin();
-      iter != seg_list.end();++iter)
-    w << *iter;
-}		   
-
 void read_data(int argc,
                char *argv[],
                Number_Type &prec,
-               std::list<Segment_2> &seg_list,
-               bool &wait_for_click,
-               int &number_of_kd_trees,
-               bool &do_isr)
+               std::list<Segment_2> &seg_list)
 {
   int number_of_segments,i;
   CGAL::Segment_data<Rep> seg;
   Number_Type x1,y1,x2,y2;
 
-  if(argc > 4 || argc < 2) {
-    std::cerr << 
-      "syntex: demo <input file name> [do_isr = t][wait for a click = f]\n";
-    std::cerr << "wait for a click: 0 - not wait, 1 - wait\n";
-    exit(1);
-  }
-
   std::ifstream is(argv[1]);
-  
-  if(argc > 2)
-    do_isr = !strcmp(argv[2],"t");
-  else
-    do_isr = true;
-
-  if(argc > 3)
-    wait_for_click = !strcmp(argv[3],"t");
-  else
-    wait_for_click = false;
-
-  number_of_kd_trees = 5;
 
   if(is.bad()) {
     std::cerr << "Bad input file : " << argv[1] << std::endl;
@@ -155,63 +186,215 @@ void read_data(int argc,
   }
 }
 
-inline Number_Type max(Number_Type a,Number_Type b,Number_Type c)
-       {Number_Type tmp = max(a,b);return(max(tmp,c));}
-inline Number_Type min(Number_Type a,Number_Type b,Number_Type c) 
-       {Number_Type tmp = min(a,b);return(min(tmp,c));}
-
-
-void get_extreme_points(std::list<Segment_2> &seg_list,
-                        Number_Type &min_x,
-                        Number_Type &min_y,
-                        Number_Type &max_x,
-                        Number_Type &max_y)
+void clear(Snap_rounding_2 &s,CGAL::Window_stream &W)
 {
-  std::list<Segment_2>::iterator iter = seg_list.begin();
+  s.clear();
 
-  min_x = min(iter->source().x(),iter->target().x());
-  max_x = max(iter->source().x(),iter->target().x());
-  min_y = min(iter->source().y(),iter->target().y());
-  max_y = max(iter->source().y(),iter->target().y());
-   
-  for(++iter;iter != seg_list.end();++iter) {
-    min_x = min(iter->source().x(),iter->target().x(),min_x);
-    max_x = max(iter->source().x(),iter->target().x(),max_x);
-    min_y = min(iter->source().y(),iter->target().y(),min_y);
-    max_y = max(iter->source().y(),iter->target().y(),max_y);
-  }
+  W.clear();
+
+  display_bounding_box(s,W);
+}
+
+void redraw(Snap_rounding_2 &s,CGAL::Window_stream &W)
+{
+  W.clear();
+
+  display_bounding_box(s,W);
+
+  W << CGAL::BLACK;
+  for(Segment_iterator i1 = s.segments_begin();
+      i1 != s.segments_end();
+      ++i1)
+    W << *i1;
 }
 
 int main(int argc,char *argv[])
 {
-  std::list<Segment_2> seg_list;
+  std::cout << "rrr 111\n";
+
+  CGAL::Window_stream W(600, 600);
+  std::ifstream *is_ptr;
+  bool automatic_show = false;
   Number_Type prec;
-  int number_of_trees;
-  bool wait_for_click,do_isr;
+  std::list<Segment_2> seg_list;
 
-  read_data(argc,argv,prec,seg_list,wait_for_click,number_of_trees,do_isr);
+  if(argc == 1 || argc == 2) {
+    // initialize window
+    W.init(MIN_X - 2,MAX_X + 3,MIN_Y - 2);
+    W.set_mode(leda_src_mode);
+    W.set_node_width(3);
+    W.button("Show Output",1);
+    W.button("Clear",2);
+    W.button("Automatic",3);
+    W.button("Manual",4);
+    W.button("Add Segments",5);
+    W.button("Remove Segments",6);
+    W.button("Exit",7);
+    W.display();
+    W.disable_button(4);
+    W.disable_button(5);
+  } else {
+    std::cerr << "Syntax : demo [input file name]\n";
+    return(1);
+  }
 
-  CGAL::Window_stream w(600,600);
+  if(argc == 2) {
+    // initialize input file
+    is_ptr = new std::ifstream(argv[1]);
+    if(is_ptr->bad()) {
+      std::cerr << "Bad input file : " << argv[1] << std::endl;
+      return(1);
+    }
+  }
+
+  // determine bounding box
   Number_Type x1,y1,x2,y2;
-  get_extreme_points(seg_list,x1,y1,x2,y2);
-  w.init((x1 - prec * 2).to_double(),x2 - x1 > y2 - y1 ? 
-         (x2 + prec * 2).to_double() : (y2 - y1 + x1 + prec * 2).to_double(),
-         (y1 - prec * 2).to_double());
-  w.set_mode(leda_src_mode);
-  w.set_node_width(3);
-  w.button("Finish",1);
-  w.display();
-  draw_orig(w,seg_list);
+  if(argc == 1) {
+    x1 = MIN_X;
+    y1 = MIN_Y;
+    x2 = MAX_X;
+    y2 = MAX_Y;
+    prec = PRECISION;
+  } else {
+    read_data(argc,argv,prec,seg_list);
+    get_extreme_points(seg_list,x1,y1,x2,y2);
+    W.init((x1 - prec * 2).to_double(),x2 - x1 > y2 - y1 ? 
+           (x2 + prec * 2).to_double() : (y2 - y1 + x1 + prec * 2).to_double(),
+           (y1 - prec * 2).to_double());
+    W.set_mode(leda_src_mode);
+    W.set_node_width(3);
+  }
 
-  CGAL::Snap_rounding_2<Rep> i(seg_list.begin(),
-                               seg_list.end(),
-                               prec,
-                               do_isr,
-                               number_of_trees);
+  W.text_box(-1.5,-1,10,"manual");
+  W.text_box(-1.5,-1,9.5,"add segments");
 
-  window_output(i,prec,w,wait_for_click);
+  Snap_rounding_2 s(prec,true,5);// !!!! do_isr instead of true which is read from argv such as others are read
+                                 // number_of_kd_trees onstead of 5
+
+  Iso_rectangle_2 b(Point_2(x1, y1), Point_2(x2, y2));// !!!! need to be displayed
+
+  display_bounding_box(s,W);
+  
+  double x3,y3,x4,y4;
+  //Number_Type x_type,y_type;
+  int mouse_input;
+  bool sr_shown;
+  bool remove_segments = false;
+
+  if(argc == 2) {
+    s.insert(seg_list.begin(),seg_list.end());
+    show_output(s,prec,W);
+    sr_shown = true;
+  } else
+    sr_shown = false;
+
+  // main loop over input points
+  for (;;) {
+    mouse_input = W.read_mouse_seg(x3,y3,x4,y4);
+    if(mouse_input == -1 && 
+       x3 >= x1 && x3 <= x2 &&
+       x4 >= x1 && x4 <= x2 &&
+       y3 >= y1 && y3 <= y2 &&
+       y4 >= y1 && y4 <= y2) {
+      if(sr_shown) {
+        // remove biggest rectangle
+        sr_shown = false;
+        redraw(s,W);
+      }
+
+      if(remove_segments) {// !!!! need a point
+	/*        double min_dist = -1,dist;
+        Snap_rounding_2::const_iterator closest_iter =
+                s.segments_end();
+        for(Segment_iterator i1 = s.segments_begin();
+            i1 != s.segments_end();
+            ++i1) {
+          dist = sqrt(CGAL::squared_distance(Point(x,y),*i1));
+
+          if(min_dist == -1 || dist < min_dist) {
+            min_dist = dist;
+            closest_iter = iter;
+	  }
+	}
+        
+        if(closest_iter != s.segments_end())
+          s.remove(*closest_iter);
+
+	  redraw(empty_rectangle,W);*/
+      } else {
+        //x_type = x;
+        //y_type = y;
+        // add a segment
+        W << CGAL::BLACK;
+        Segment_2 tmp1(Point_2(x3,y3),Point_2(x4,y4));
+        s.insert(tmp1);
+        W << tmp1;
+      }
+
+      if(automatic_show) {
+        // automatic display of biggest rectangle
+        show_output(s,prec,W);
+        sr_shown = true;
+        //W.text_box(-1.5,-1,10,"auto");
+      }
+    } else if(mouse_input == 1) {
+      // show biggest rectangle
+      show_output(s,prec,W);
+      sr_shown = true;
+    } else if(mouse_input == 2) {
+      clear(s,W);
+      sr_shown = false;
+    } else if(mouse_input == 3) {
+      // change to automatic mode
+      automatic_show = true;
+      show_output(s,prec,W);
+      W.enable_button(4);
+      W.disable_button(3);
+      W.disable_button(1);
+      sr_shown = true;
+      redraw(s,W);
+      show_output(s,prec,W);
+      sr_shown = true;
+    } else if(mouse_input == 4) {
+      // change to manual mode
+      automatic_show = false;
+      W.enable_button(1);
+      W.enable_button(3);
+      W.disable_button(4);
+      sr_shown = false;
+      redraw(s,W);
+    } else if(mouse_input == 5) {
+      W.enable_button(6);
+      W.disable_button(5);
+      remove_segments = false;
+      redraw(s,W);
+      if(automatic_show)
+        show_output(s,prec,W);
+    } else if(mouse_input == 6) {
+      W.enable_button(5);
+      W.disable_button(6);
+      remove_segments = true;
+      redraw(s,W);
+      if(automatic_show)
+        show_output(s,prec,W);
+    } else if(mouse_input == 7) {
+      // finish
+      break;
+    }
+
+    if(automatic_show)
+      W.text_box(-1.5,-1,10,"auto");
+    else
+      W.text_box(-1.5,-1,10,"manual");
+
+    if(remove_segments)
+      W.text_box(-1.5,-1,9.5,"remove points");
+    else
+      W.text_box(-1.5,-1,9.5,"add points");
+  }
+
+  if(argc == 2)
+    delete(is_ptr);
 
   return(0);
 }
-
-#endif // LEDA
