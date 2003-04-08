3a870babe353413f654c8746d5753a2979ccd91b
Efi Fogel <efif@post.tau.ac.il>
2003-04-08 16:25:35 +0000

    Fixed friend statement for gnu larger than 3.2

--- Packages/Trapezoidal_decomposition/include/CGAL/Td_X_trapezoid.h
+++ Packages/Trapezoidal_decomposition/include/CGAL/Td_X_trapezoid.h
@@ -69,12 +69,13 @@ public:
   friend class Trapezoidal_decomposition_2<Traits>;
   
 #ifdef CGAL_PM_FRIEND_CLASS
-#if defined(__GNUC__) && ((__GNUC__ < 3) || (__GNUC__ == 3) && (__GNUC_MINOR__ < 3))
-  friend typename Trapezoidal_decomposition_2<Traits>::Around_point_circulator;
-  friend typename Trapezoidal_decomposition_2<Traits>::In_face_iterator;
-#elif defined(__SUNPRO_CC)
+#if defined(__SUNPRO_CC) || \
+ (defined(__GNUC__) && ((__GNUC__ == 3) && (__GNUC_MINOR__ > 2)))
   friend class Trapezoidal_decomposition_2<Traits>::Around_point_circulator;
   friend class Trapezoidal_decomposition_2<Traits>::In_face_iterator;
+#elif defined(__GNUC__)
+  friend typename Trapezoidal_decomposition_2<Traits>::Around_point_circulator;
+  friend typename Trapezoidal_decomposition_2<Traits>::In_face_iterator;
 #else
   friend class Around_point_circulator;
   friend class In_face_iterator;
