256fa42004748e5fd6af1748f74c88dace4290ca
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2003-04-22 12:27:03 +0000

    *** empty log message ***

---
+++ Packages/Configuration/config/testfiles/CGAL_CFG_NO_LIMITS.C
@@ -0,0 +1,35 @@
+// ======================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// ----------------------------------------------------------------------
+//
+// release       : 
+// release_date  : 
+//
+// file          : config/testfiles/CGAL_CFG_NO_LIMITS.C
+// author(s)     : various
+//
+// coordinator   : Utrecht University
+//
+// ======================================================================
+
+// CGAL_CFG_NO_LIMITS.C
+// ---------------------------------------------------------------------
+// A short test program to evaluate a C++ compiler.
+// This program is used by cgal_configure.
+// The following documentation will be pasted in the generated configfile.
+// ---------------------------------------------------------------------
+
+//| If a compiler doesn't know the limits
+//| CGAL_CFG_NO_LIMITS is set. 
+
+#include <limits>
+
+
+int main()
+{
+  return 0;
+}
+
+
