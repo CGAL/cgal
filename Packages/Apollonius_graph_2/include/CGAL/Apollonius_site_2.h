0c991a351f04061c0162377f018201ab2029cb8a
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2003-05-02 17:08:34 +0000

    added file Apollonius_site_2.h

---
+++ Packages/Apollonius_graph_2/include/CGAL/Apollonius_site_2.h
@@ -0,0 +1,90 @@
+// ======================================================================
+//
+// Copyright (c) 1997 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+//
+// release       : 
+// release_date  : 
+//
+// file          : include/CGAL/Apollonius_site_2.h
+// package       : Apollonius_graph_2
+// source        : $RCSfile$
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Menelaos Karavelas <mkaravel@cse.nd.edu>
+//
+// coordinator   : Mariette Yvinec  <Mariette Yvinec@sophia.inria.fr>
+//
+// ======================================================================
+
+#ifndef CGAL_APOLLONIUS_SITE_2_H
+#define CGAL_APOLLONIUS_SITE_2_H
+
+#include <iostream>
+
+CGAL_BEGIN_NAMESPACE
+
+
+template < class K >
+class Apollonius_site_2 //: public K::Point_2
+{
+public:
+  typedef K                       Kernel;
+  typedef typename K::Point_2     Point_2;
+  typedef Apollonius_site_2<K>    Self;
+  typedef typename K::RT          RT;
+
+public:
+  //  Apollonius_site_2(const Point_2& p = Point_2(), const RT& w = RT(0))
+  //    : Point_2(p), _w(w) {}
+
+  Apollonius_site_2(const Point_2& p = Point_2(), const RT& w = RT(0))
+    : _p(p), _w(w) {}
+
+  
+
+  //  Point_2 point() const { return (*this); }
+  Point_2 point() const { return _p; }
+  RT      weight() const { return _w; }
+  RT      x() const { return _p.x(); }
+  RT      y() const { return _p.y(); }
+
+  bool operator==(const Apollonius_site_2& other) const
+  {
+    return ( _p == other._p && _w == other._w );
+  }
+
+private:
+  Point_2 _p;
+  RT  _w;
+
+};
+
+template <class K>
+std::ostream&
+operator<<(std::ostream &os, const Apollonius_site_2<K>& wp)
+{
+  return os << wp.point() << " " << wp.weight();
+}
+
+template <class K>
+std::istream&
+operator>>(std::istream& is, Apollonius_site_2<K>& wp)
+{
+  typename Apollonius_site_2<K>::RT       weight;
+  typename Apollonius_site_2<K>::Point_2  p;
+  is >> p >> weight;
+  wp = Apollonius_site_2<K>(p, weight);
+  return is;
+}
+
+
+CGAL_END_NAMESPACE
+
+#endif // CGAL_APOLLONIUS_SITE_2_H
+
