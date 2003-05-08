57be940bce74581de9c8cd6d45f1655cb761b633
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2003-05-08 09:58:45 +0000

    added converter for enumeration types

---
+++ Packages/Kernel_23/include/CGAL/Enum_converter.h
@@ -0,0 +1,52 @@
+// ======================================================================
+//
+// Copyright (c) 2003 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------
+// 
+// release       : 
+// release_date  : 
+// 
+// file          : Enum_converter.h
+// package       : Kernel_23
+// revision      : $Revision$
+// revision_date : $Date$
+// author(s)     : Menelaos Karavelas <mkaravel@cse.nd.edu>
+//
+// coordinator   :
+// ======================================================================
+
+#ifndef CGAL_ENUM_CONVERTER_H
+#define CGAL_ENUM_CONVERTER_H
+
+#include <CGAL/basic.h>
+#include <CGAL/enum.h>
+
+CGAL_BEGIN_NAMESPACE
+
+class Enum_converter
+{
+  bool              operator()(bool b) const { return b; }
+
+  Sign              operator()(Sign s) const { return s; }
+
+  Oriented_side     operator()(Oriented_side os) const { return os; }
+
+  Bounded_side      operator()(Bounded_side bs) const { return bs; }
+
+  Comparison_result operator()(Comparison_result cr) const {
+    return cr;
+  }
+
+  Angle operator()(Angle a) const { return a; }
+};
+
+
+CGAL_END_NAMESPACE
+
+
+#endif // CGAL_ENUM_CONVERTER_H
