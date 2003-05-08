d06ed61fb9c9329c3db1f75e16fed5dde220c6ed
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2003-05-08 09:04:27 +0000

    changed names of template parameters in extended cartesian converter

--- Packages/Apollonius_graph_2/include/CGAL/Apollonius_graph_kernel_wrapper_2.h
+++ Packages/Apollonius_graph_2/include/CGAL/Apollonius_graph_kernel_wrapper_2.h
@@ -76,14 +76,14 @@ template<class Cartesian_converter>
 class Extended_cartesian_converter
 {};
 
-template<class _K1, class _K2, class Converter >
-class Extended_cartesian_converter < Cartesian_converter<_K1,_K2,
+template<class K1, class K2, class Converter >
+class Extended_cartesian_converter < Cartesian_converter<K1,K2,
   Converter> >
-    : public Cartesian_converter<_K1,_K2,Converter>
+    : public Cartesian_converter<K1,K2,Converter>
 {
 private:
-  typedef Apollonius_graph_kernel_wrapper_2<_K1> K1;
-  typedef Apollonius_graph_kernel_wrapper_2<_K2> K2;
+  typedef Apollonius_graph_kernel_wrapper_2<K1> K1W;
+  typedef Apollonius_graph_kernel_wrapper_2<K2> K2W;
 
 public:
   bool
@@ -91,13 +91,13 @@ public:
     return b;
   }
 
-  typename K2::Site_2
-  operator()(const typename K1::Site_2& wp) const
+  typename K2W::Site_2
+  operator()(const typename K1W::Site_2& wp) const
   {
     Converter c;
 
-    typename K2::Point_2 p(c(wp.x()), c(wp.y()));
-    return typename K2::Site_2( p, c(wp.weight()) );
+    typename K2W::Point_2 p(c(wp.x()), c(wp.y()));
+    return typename K2W::Site_2( p, c(wp.weight()) );
   }
 };
 
