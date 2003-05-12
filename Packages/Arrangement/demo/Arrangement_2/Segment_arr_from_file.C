81b0ae3431e5b32a06f35acd55c30f7a6a14360e
Efi Fogel <efif@post.tau.ac.il>
2003-05-12 09:35:38 +0000

    fixed declaration of window_stream

--- Packages/Arrangement/demo/Arrangement_2/Segment_arr_from_file.C
+++ Packages/Arrangement/demo/Arrangement_2/Segment_arr_from_file.C
@@ -150,7 +150,7 @@ public:
 
 /*! redraw
  */
-static void redraw(leda_window * wp, double x0, double y0,
+static void redraw(CGAL::Window_stream * wp, double x0, double y0,
                    double x1, double y1)
 { wp->flush_buffer(x0, y0, x1, y1); }
 
