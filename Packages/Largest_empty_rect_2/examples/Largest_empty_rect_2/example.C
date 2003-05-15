2192201f5e89b3c9f8eeb805efdbc9e6923255ca
Eli Packer <elip@post.tau.ac.il>
2003-05-15 07:15:05 +0000

    better readability

--- Packages/Largest_empty_rect_2/examples/Largest_empty_rect_2/example.C
+++ Packages/Largest_empty_rect_2/examples/Largest_empty_rect_2/example.C
@@ -5,26 +5,36 @@
 
 #include <fstream>
 
-typedef double                                Number_Type;
-typedef CGAL::Cartesian<Number_Type>             K;
-typedef CGAL::Largest_empty_iso_rectangle_2<K> Largest_empty_rect;
-typedef K::Iso_rectangle_2              Iso_rectangle_2;
-typedef K::Point_2                      Point; 
+typedef double                                 Number_Type;
+typedef CGAL::Cartesian<Number_Type>           K;
+typedef CGAL::Largest_empty_iso_rectangle_2<K> Largest_empty_iso_rect_2;
+typedef K::Iso_rectangle_2                     Iso_rectangle_2;
+typedef K::Point_2                             Point_2; 
 
 int main()
 {
-  Iso_rectangle_2 bounding_box(Point(0, 0), Point(10, 10));
+  Iso_rectangle_2 bounding_box(Point_2(0, 0), Point_2(10, 10));
 
-  Largest_empty_rect ler(bounding_box);
-  ler.insert(Point(6,1));
-  ler.insert(Point(1,8));
-  ler.insert(Point(9,5));
-  ler.insert(Point(5,9));
+  Largest_empty_iso_rect_2 leir(bounding_box);
+  leir.insert(Point_2(6,1));
+  leir.insert(Point_2(1,8));
+  leir.insert(Point_2(9,5));
+  leir.insert(Point_2(5,9));
 
-  Iso_rectangle_2 b = ler.get_largest_empty_iso_rectangle();
+  std::cout << "The input bounding box is " << bounding_box << std::endl;
 
-  std::cout << "The largest empty iso rectangle is (" << b.min().x() << "," << b.min().y() << "),(" << b.max().x() << "," << b.max().y() << ")\n";
-  std::cout << "Its size is " << CGAL_NTS abs((b.max().x() - b.min().x()) * (b.max().y() - b.min().y())) << std::endl;
+  std::cout << "The input point set is:" << std::endl;
+  for(Largest_empty_iso_rect_2::const_iterator it = leir.begin();
+      it != leir.end();
+      ++it){
+    const Point_2& p = *it;
+    std::cout << "   " << p << std::endl;
+  }
+
+  Iso_rectangle_2 b = leir.get_largest_empty_iso_rectangle();
+
+  std::cout << std::endl << "The largest empty iso rectangle is " << b << std::endl;
+  std::cout << "Its area is " << b.area() << std::endl;
 
   return 0;
 }
