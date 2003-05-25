1f0a1f0c550c0c71f05c8bd6005de460d38cc326
Ron Wein <wein@post.tau.ac.il>
2003-05-25 14:06:02 +0000

    Switched from circle_traits to conic_traits.

--- Packages/Arrangement/examples/Arrangement_2/example3.C
+++ Packages/Arrangement/examples/Arrangement_2/example3.C
@@ -4,19 +4,27 @@
 #include "short_names.h"
 
 #include <CGAL/basic.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/leda_real.h>
 #include <CGAL/Arr_2_bases.h>
 #include <CGAL/Arr_2_default_dcel.h>
-#include <CGAL/Arr_circles_real_traits.h> 
+#include <CGAL/Arr_conic_traits_2.h> 
 #include <CGAL/Arrangement_2.h>
 
-// We use here double instead of leda_real to enable compilation without LEDA.
-// This is not recommended generally.
-// Read more in the README file or in the manual.
+#ifndef CGAL_USE_LEDA
+// To enable compilation without leda:
+int main ()
+{
+  return (0);
+}
 
-typedef double                                          NT;
-typedef CGAL::Arr_circles_real_traits<NT>               Traits;
+#else
+
+typedef leda_real                                       NT;
+typedef CGAL::Cartesian<NT>                             Kernel;
+typedef CGAL::Arr_conic_traits_2<Kernel>                Traits;
 typedef Traits::Point_2                                 Point_2;
-typedef Traits::X_monotone_curve_2                      X_monotone_curve_2;
+typedef Traits::Circle_2                                Circle_2;
 typedef Traits::Curve_2                                 Curve_2;
 typedef CGAL::Arr_base_node<Curve_2>                    Base_node;
 typedef CGAL::Arr_2_default_dcel<Traits>                Dcel;
@@ -25,10 +33,13 @@ typedef CGAL::Arrangement_2<Dcel,Traits,Base_node>      Arr_2;
 int main()
 {
   Arr_2 arr;  
-
+              
   // 2 ccw circles with radius 5 and center (0,0) and (6,0) resp.
-  Arr_2::Curve_iterator cit = arr.insert(Curve_2(0, 0, 25));
-  cit=arr.insert(Curve_2(6, 0, 25)); 
+  Circle_2  c1 (Point_2(0,0), 5*5, CGAL::COUNTERCLOCKWISE);
+  Circle_2  c2 (Point_2(6,0), 5*5, CGAL::COUNTERCLOCKWISE);
+
+  Arr_2::Curve_iterator cit = arr.insert(c1);
+  cit = arr.insert(c2); 
 
   // upward vertical ray shooting
   Arr_2::Locate_type lt;
@@ -39,3 +50,5 @@ int main()
 
   return 0;
 }
+
+#endif
