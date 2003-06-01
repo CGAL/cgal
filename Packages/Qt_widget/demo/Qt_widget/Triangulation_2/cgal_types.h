d5df5b241a393ca310e2a34487624412ab87a465
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2003-06-01 15:52:23 +0000

    ed

--- Packages/Qt_widget/demo/Qt_widget/Triangulation_2/cgal_types.h
+++ Packages/Qt_widget/demo/Qt_widget/Triangulation_2/cgal_types.h
@@ -9,19 +9,19 @@
 typedef double Coord_type;
 typedef CGAL::Cartesian<Coord_type>	    Rep;
 
-typedef CGAL::Point_2<Rep>		    Point;
-typedef CGAL::Segment_2<Rep>		    Segment;
-typedef CGAL::Line_2<Rep>		    Line;
-typedef CGAL::Triangle_2<Rep>		    Triangle;
-typedef CGAL::Circle_2<Rep>		    Circle;
+typedef CGAL::Point_2<Rep>                  Point;
+typedef CGAL::Segment_2<Rep>                Segment;
+typedef CGAL::Line_2<Rep>                   Line;
+typedef CGAL::Triangle_2<Rep>               Triangle;
+typedef CGAL::Circle_2<Rep>                 Circle;
 
-typedef CGAL::Triangulation_2<Rep>	    Triangulation;
+typedef CGAL::Triangulation_2<Rep>          Triangulation;
 typedef CGAL::Delaunay_triangulation_2<Rep> Delaunay;
 
 
-typedef Triangulation::Vertex_iterator      Vertex_iterator;
+typedef Delaunay::Vertex_iterator           Vertex_iterator;
 typedef Delaunay::Face_handle               Face_handle;
 typedef Delaunay::Vertex_handle             Vertex_handle;
 typedef Delaunay::Edge                      Edge;
-typedef Triangulation::Line_face_circulator Line_face_circulator;
+typedef Delaunay::Line_face_circulator      Line_face_circulator;
 
