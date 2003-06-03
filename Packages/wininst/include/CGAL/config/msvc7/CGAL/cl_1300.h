15b530f603f1f5790716a9702fc2ce679155eab7
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2003-06-03 13:46:06 +0000

    rursu: updated cl1300.h CGAL configuration file

--- Packages/wininst/include/CGAL/config/msvc7/CGAL/cl_1300.h
+++ Packages/wininst/include/CGAL/config/msvc7/CGAL/cl_1300.h
@@ -19,7 +19,6 @@
 // ============================================================================
 
 
-
 //+--------------------------------------------------------------------------
 // The compiler has to provide a Standard Template Library
 //+--------------------------------------------------------------------------
@@ -33,6 +32,13 @@
 //#define CGAL_CFG_CCTYPE_MACRO_BUG 1
 
 //+--------------------------------------------------------------------------
+//| The flag CGAL_CFG_EARLY_INSTANTIATION_BUG is set, if a compiler does not 
+//| not how to compile the following code. See the solution bellow.
+//| Created to workaround a cl1300 bug
+//+--------------------------------------------------------------------------
+#define CGAL_CFG_EARLY_INSTANTIATION_BUG 1
+
+//+--------------------------------------------------------------------------
 //| This flag is set, if the compiler does not promote enumeration types
 //| (which depend on a template parameter) correctly when they are used 
 //| as int template arguments. (e.g. Borland 5.5)
@@ -40,10 +46,16 @@
 //#define CGAL_CFG_ENUM_BUG 1
 
 //+--------------------------------------------------------------------------
+//| If a compiler (or assembler or linker) has problems with long names
+//| CGAL_CFG_LONGNAME_BUG is set.
+//+--------------------------------------------------------------------------
+#define CGAL_CFG_LONGNAME_BUG 1
+
+//+--------------------------------------------------------------------------
 //| This flag is set, if the compiler does not match the most
 //| specialized instance of a function template correctly,
 //| but complains about multiple matches.
-//| (e.g. SGI 7.2)
+//| (e.g. VC++ 7)
 //+--------------------------------------------------------------------------
 #define CGAL_CFG_MATCHING_BUG_2 1
 
@@ -53,16 +65,6 @@
 //| the parameter's type. (e.g. sun C++ 5.3)
 //+--------------------------------------------------------------------------
 #define CGAL_CFG_MATCHING_BUG_3 1
-
-//+--------------------------------------------------------------------------
-//| This is a test-case for a bug in VC++ 7.0 that 
-//| redefines min(a, b) and max(a, b)
-//| Files concerned: windows.h, windef.h
-//| When the bug is present, CGAL_CFG_NOMINMAX_BUG is set
-//| The file basic.h should check if this bug is present and if so, 
-//| define NOMINMAX flag
-//+--------------------------------------------------------------------------
-#define CGAL_CFG_NOMINMAX_BUG 1
 
 //+--------------------------------------------------------------------------
 //| When template implementation files are not included in the source files,
@@ -89,20 +91,18 @@
 #define CGAL_CFG_NO_BIG_ENDIAN 1
 
 //+--------------------------------------------------------------------------
-//| The class std::iterator_traits is part of the std library.
-//| It is used to access certain properties of iterators, such as
-//| their value type or iterator category (forward, bidirectional, etc.).
-//| The macro CGAL_CFG_NO_ITERATOR_TRAITS is set if std::iterator_traits
-//| is not fully supported.
-//+--------------------------------------------------------------------------
-//#define CGAL_CFG_NO_ITERATOR_TRAITS 1
-
-//+--------------------------------------------------------------------------
 //| This flag is set if the compiler doesn't support the operator Koenig
 //| lookup. That is, it does not search in the namespace of the arguments for
 //| the function.
 //+--------------------------------------------------------------------------
 #define CGAL_CFG_NO_KOENIG_LOOKUP 1
+
+//+--------------------------------------------------------------------------
+//| If a compiler doesn't know <limits> (g++-2.95)
+//| or has a bug in the implementation (Sun CC 5.4, MipsPro CC)
+//| CGAL_CFG_NO_LIMITS is set. 
+//+--------------------------------------------------------------------------
+//#define CGAL_CFG_NO_LIMITS 1
 
 //+--------------------------------------------------------------------------
 //| If a compiler doesn't know the locale classic
@@ -111,33 +111,18 @@
 //#define CGAL_CFG_NO_LOCALE 1
 
 //+--------------------------------------------------------------------------
-//| If a compiler (or assembler or linker) has problems with long names
-//| CGAL_CFG_NO_LONGNAME_PROBLEM is set.
+//| The long long built-in integral type is not part of the ISO C++ standard,
+//| but many compilers support it nevertheless since it's part of the ISO
+//| C standard.
+//| The following definition is set if it is supported.
 //+--------------------------------------------------------------------------
-//#define CGAL_CFG_NO_LONGNAME_PROBLEM 1
-
-//+--------------------------------------------------------------------------
-//| This flag is set, if a compiler does not accept the template
-//| keyword when referring to nested template classes.
-//| E.g. if the templated class C is defined within class A,
-//| one refers to it by A::template C< >.
-//+--------------------------------------------------------------------------
-//#define CGAL_CFG_NO_NESTED_TEMPLATE_KEYWORD 1
+#define CGAL_CFG_NO_LONG_LONG 1
 
 //+--------------------------------------------------------------------------
 //| If a compiler doesn't support partial specialisation of class templates,
 //| the flag CGAL_CFG_NO_PARTIAL_CLASS_TEMPLATE_SPECIALISATION is set.
 //+--------------------------------------------------------------------------
 #define CGAL_CFG_NO_PARTIAL_CLASS_TEMPLATE_SPECIALISATION 1
-
-//+--------------------------------------------------------------------------
-//| The parameter types of member functions might contain a scope
-//| operator. This works as long as the member function is implemented
-//| inline in the class. If the member function is implemented external
-//| not all compilers are able to parse the scope operators correctly.
-//| The following definition is set if the compiler fails parsing.
-//+--------------------------------------------------------------------------
-//#define CGAL_CFG_NO_SCOPE_MEMBER_FUNCTION_PARAMETERS 1
 
 //+--------------------------------------------------------------------------
 //| The flag CGAL_CFG_NO_STANDARD_HEADERS is set, if a compiler does not 
@@ -151,12 +136,6 @@
 //| C library in namespace std. (only tests for the symbols used in CGAL)
 //+--------------------------------------------------------------------------
 //#define CGAL_CFG_NO_STDC_NAMESPACE 1
-
-//+--------------------------------------------------------------------------
-//| Checks whether the compiler wants to have a <> in friend declarations
-//| of template functions.
-//+--------------------------------------------------------------------------
-//#define CGAL_CFG_NO_TEMPLATE_FRIEND_DISTINCTION 1
 
 //+--------------------------------------------------------------------------
 //| G++ 2.95.2 has problems with member functions implemented outside of
@@ -183,23 +162,11 @@
 #define CGAL_CFG_RETURN_TYPE_BUG 1
 
 //+--------------------------------------------------------------------------
-//| This is a test-case for a bug in SunPro 5.3 that occurs in the HDS.
-//| When the bug is present, CGAL_CFG_SUNPRO_PARTIAL_SPEC_BUG is set.
+//| The flag CGAL_CFG_USING_NAMESPACE_BUG is set, if a compiler does not 
+//| not how to compile the following code.
+//| Created to workaround a cl1300 bug
 //+--------------------------------------------------------------------------
-#define CGAL_CFG_SUNPRO_PARTIAL_SPEC_BUG 1
-
-//+--------------------------------------------------------------------------
-//| If a compiler complains about typename, when passing a dependent
-//| type as template parameter, the flag CGAL_CFG_TYPENAME_BUG is set.
-//+--------------------------------------------------------------------------
-//#define CGAL_CFG_TYPENAME_BUG 1
-
-//+--------------------------------------------------------------------------
-//| If a compiler does not accept a using declaration referring to a 
-//| symbol that is again declared by a using declaration, the flag
-//| CGAL_CFG_USING_USING_BUG is set.
-//+--------------------------------------------------------------------------
-//#define CGAL_CFG_USING_USING_BUG 1
+#define CGAL_CFG_USING_NAMESPACE_BUG 1
 
 //+--------------------------------------------------------------------------
 //| This is a test-case for a bug in VC++ 7.0 beta2 that occurs in the kernel.
