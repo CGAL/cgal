d3e1faf56bea81829289af52df08168d2e1a79ae
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2003-06-28 19:33:37 +0000

    VC++ workaround

--- Packages/Triangulation_3/include/CGAL/Triangulation_data_structure_3.h
+++ Packages/Triangulation_3/include/CGAL/Triangulation_data_structure_3.h
@@ -141,8 +141,13 @@ public:
 
     Cell_handle() : _c() {}
     Cell_handle(Cell_iterator c) : _c(c) {}
+#ifdef CGAL_CFG_EARLY_INSTANTIATION_BUG
+    Cell_handle(const Cell_circulator &c) : _c(c.base()._c) {}
+    Cell_handle(const Face_circulator &c) : _c(c.base()._c) {}
+#else
     Cell_handle(Cell_circulator c) : _c(c.base()._c) {}
     Cell_handle(Face_circulator c) : _c(c.base()._c) {}
+#endif
     Cell_handle(void * n) : _c()
     { CGAL_triangulation_precondition(n == NULL); }
 
