fff1563e5f6e0ce6f6ecab6e1c0676c72a02fb42
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2003-07-24 19:08:42 +0000

    - Remove the unused CGAL_FRIEND_INLINE.

--- Packages/Kernel_23/include/CGAL/kernel_basic.h
+++ Packages/Kernel_23/include/CGAL/kernel_basic.h
@@ -1,6 +1,6 @@
 // ======================================================================
 //
-// Copyright (c) 1999 The CGAL Consortium
+// Copyright (c) 1999,2003 The CGAL Consortium
 //
 // This software and related documentation is part of an INTERNAL release
 // of the Computational Geometry Algorithms Library (CGAL). It is not
@@ -17,8 +17,7 @@
 // revision_date : $Date$
 // author(s)     : Stefan Schirra
 //
-//
-// coordinator   : MPI, Saarbruecken  (<Stefan.Schirra@mpi-sb.mpg.de>)
+// coordinator   : MPI, Saarbruecken
 // ======================================================================
  
 
@@ -38,10 +37,6 @@
 
 #ifndef CGAL_KERNEL_LARGE_INLINE
 #  define CGAL_KERNEL_LARGE_INLINE
-#endif
-
-#ifndef CGAL_FRIEND_INLINE
-#  define CGAL_FRIEND_INLINE
 #endif
 
 #endif // CGAL_KERNEL_BASIC_H
