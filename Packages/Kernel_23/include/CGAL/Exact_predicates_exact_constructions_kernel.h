47102f37eb6e776c6c76b102ad02f3e0b6f590ba
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2003-07-24 18:39:44 +0000

    renamed

--- Packages/Kernel_23/include/CGAL/Exact_predicates_exact_constructions_kernel.h
+++ Packages/Kernel_23/include/CGAL/Exact_predicates_exact_constructions_kernel.h
@@ -11,35 +11,37 @@
 // release       : 
 // release_date  : 
 //
-// file          : include/CGAL/Inexact_constructions_kernel.h
+// file          : include/CGAL/Exact_predicates_exact_constructions_kernel.h
 // package       : Kernel_23
-// source        : $RCSfile$
 // revision      : $Revision$
 // revision_date : $Date$
-// author(s)     : Menelaos Karavelas <mkaravel@cse.nd.edu>
+// author(s)     : Menelaos Karavelas, Sylvain Pion
 //
 // coordinator   :
 //
 // ======================================================================
 
-#ifndef CGAL_INEXACT_CONSTRUCTIONS_KERNEL_H
-#define CGAL_INEXACT_CONSTRUCTIONS_KERNEL_H
+#ifndef CGAL_EXACT_PREDICATES_EXACT_CONSTRUCTIONS_KERNEL_H
+#define CGAL_EXACT_PREDICATES_EXACT_CONSTRUCTIONS_KERNEL_H
 
-
-#include <CGAL/basic.h>
 #include <CGAL/Simple_cartesian.h>
-#include <CGAL/Filtered_kernel.h>
+#include <CGAL/Lazy_nt_exact.h>
+
+#ifdef CGAL_USE_GMP
+#  include <CGAL/Gmpq.h>
+#else
+#  include <CGAL/Quotient.h>
+#  include <CGAL/MP_Float.h>
+#endif
 
 CGAL_BEGIN_NAMESPACE
 
-
-typedef Filtered_kernel< Simple_cartesian<double> >
-/*                                       */ Inexact_constructions_kernel;
-
+#ifdef CGAL_USE_GMP
+typedef Simple_cartesian< Lazy_exact_nt< Gmpq > >               Exact_kernel;
+#else
+typedef Simple_cartesian< Lazy_exact_nt< Quotient<MP_Float> > > Exact_kernel;
+#endif
 
 CGAL_END_NAMESPACE
 
-
-#endif // CGAL_INEXACT_CONSTRUCTIONS_KERNEL_H
-
-
+#endif // CGAL_EXACT_PREDICATES_EXACT_CONSTRUCTIONS_KERNEL_H
