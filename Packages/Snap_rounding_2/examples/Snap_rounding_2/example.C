90ea92c63dd4b39d902ad0b0cbcd4570245ad764
Eli Packer <elip@post.tau.ac.il>
2003-07-29 15:19:33 +0000

    ***

--- Packages/Snap_rounding_2/examples/Snap_rounding_2/example.C
+++ Packages/Snap_rounding_2/examples/Snap_rounding_2/example.C
@@ -1,33 +1,38 @@
 #include <CGAL/basic.h>
 #include <CGAL/Cartesian.h>
-#include "../../include/CGAL/Snap_rounding_2_traits.h"
+#include "../../include/CGAL/Snap_rounding_traits_2.h"
 #include "../../include/CGAL/Snap_rounding_2.h"
 
-typedef leda_rational                    Number_type;
-typedef CGAL::Cartesian<Number_type>     Rep;
-typedef CGAL::Snap_rounding_traits<Rep>  Sr_traits;
-typedef CGAL::Snap_rounding_2<Sr_traits> Sr;
-typedef Sr::Segment_2                    Segment_2;
-typedef Sr::Point_2                      Point_2;
-typedef Sr::Segments_container           Segments;
+typedef leda_rational                            Number_type;
+typedef CGAL::Cartesian<Number_type>             Rep;
+typedef CGAL::Snap_rounding_traits_2<Rep>        Sr_traits;
+typedef Rep::Segment_2                           Segment_2;
+typedef Rep::Point_2                             Point_2;
+typedef std::list<Segment_2>                     Segment_list_2;
+typedef std::list<Point_2>                       Polyline_2;
+typedef std::list<Polyline_2>                    Polyline_list_2;
 
 int main()
 {
-  Segments segments_container;
+  Segment_list_2 seg_list;
+  Polyline_list_2 output_list;
 
-  segments_container.push_back(Segment_2(Point_2(0,0),Point_2(10,10)));
-  segments_container.push_back(Segment_2(Point_2(0,10),Point_2(10,0)));
-  segments_container.push_back(Segment_2(Point_2(3,0),Point_2(3,10)));
-  segments_container.push_back(Segment_2(Point_2(7,0),Point_2(7,10)));
+  seg_list.push_back(Segment_2(Point_2(0,0),Point_2(10,10)));
+  seg_list.push_back(Segment_2(Point_2(0,10),Point_2(10,0)));
+  seg_list.push_back(Segment_2(Point_2(3,0),Point_2(3,10)));
+  seg_list.push_back(Segment_2(Point_2(7,0),Point_2(7,10)));
 
-  Sr snap(segments_container.begin(),segments_container.end(),1.0,1,5);
+  CGAL::snap_rounding_2<Sr_traits,std::list<Segment_2>::const_iterator,
+                        std::list<std::list<Point_2> > >(
+      seg_list.begin(),seg_list.end(),output_list,
+      1.0,true,false,5);
 
   int counter = 0;
-  for(Sr::Polyline_const_iterator iter1 = snap.polylines_begin();
-      iter1 != snap.polylines_end();
+  for(Polyline_list_2::const_iterator iter1 = output_list.begin();
+      iter1 != output_list.end();
       ++iter1) {
     std::cout << "Polyline number " << ++counter << ":\n";
-    for(Sr::Point_const_iterator iter2 = iter1->begin();
+    for(Polyline_2::const_iterator iter2 = iter1->begin();
         iter2 != iter1->end();
         ++iter2)
       std::cout << "    (" << iter2->x().to_double() << ":"
