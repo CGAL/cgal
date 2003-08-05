3c5cceecdba57e55a25b5a945b12abc06145d168
Michael Hoffmann <hoffmann@inf.ethz.ch>
2003-08-05 11:18:10 +0000

    Minor rewordings.

--- Packages/Configuration/config/testfiles/CGAL_CFG_USING_NAMESPACE_BUG.C
+++ Packages/Configuration/config/testfiles/CGAL_CFG_USING_NAMESPACE_BUG.C
@@ -1,6 +1,6 @@
 // ======================================================================
 //
-// Copyright (c) 1997 The CGAL Consortium
+// Copyright (c) 2003 The CGAL Consortium
 //
 // This software and related documentation is part of an INTERNAL release
 // of the Computational Geometry Algorithms Library (CGAL). It is not
@@ -13,7 +13,6 @@
 //
 // file          : config/testfiles/CGAL_CFG_USING_NAMESPACE_BUG.C
 // package       : Configuration (1.28)
-// source        :
 // revision      : 1.11
 // revision_date : 29 Mar 1998
 // author(s)     : various
@@ -24,15 +23,13 @@
 
 // CGAL_CFG_USING_NAMESPACE_BUG.C
 // ---------------------------------------------------------------------
-// A short test program to evaluate a C++ compiler / STL implementation
-// whether it supports the new standard headers (i.e. without the .h suffix)
 // This program is used by cgal_configure.
 // The following documentation will be pasted in the generated configfile.
 // ---------------------------------------------------------------------
 
-//| The flag CGAL_CFG_USING_NAMESPACE_BUG is set, if a compiler does not 
-//| not how to compile the following code.
-//| Created to workaround a cl1300 bug
+//| The flag CGAL_CFG_USING_NAMESPACE_BUG is set, if a compiler cannot
+//| handle using directives (using namespace ...) across different
+//| namespaces. Created to workaround a cl1300 bug
 
 namespace CGAL {
   namespace CommonFunctors {
