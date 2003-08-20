977e554da979e7354f13c96d340ba81ab8fff184
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2003-08-20 11:36:42 +0000

    this example was obsolete and did not work with recent versions of Qt_widget.

--- Packages/Qt_widget/demo/Qt_widget/Examples/layer.C
+++ Packages/Qt_widget/demo/Qt_widget/Examples/layer.C
@@ -12,33 +12,37 @@ int main(int, char*){
 #include <CGAL/IO/Qt_widget_get_point.h>
 
 #include <qapplication.h>
+#include <qmainwindow.h>
 
 typedef CGAL::Cartesian<double>             Rep;
 typedef CGAL::Point_2<Rep>                  Point;
 typedef CGAL::Delaunay_triangulation_2<Rep> Delaunay;
-typedef CGAL::Qt_widget						Qt_widget;
 
 Delaunay dt;
 
 class My_Layer : public CGAL::Qt_widget_layer{
-  void draw(CGAL::Qt_widget& win){
-    win << dt;
+  void draw(){
+    *widget << dt;
   }
 };
 
-class My_Window : public Qt_widget {
+class My_Window : public QMainWindow {
   Q_OBJECT
 public:
   My_Window(int x, int y){
+    widget = new CGAL::Qt_widget(this, "CGAL Qt_widget");
+    setCentralWidget(widget);
     resize(x,y);
-    attach(&get_point);
-    attach(&v);
-    connect(this, SIGNAL(new_cgal_object(CGAL::Object)), 
+    widget->attach(&get_point);
+    widget->attach(&v);
+    connect(widget, SIGNAL(new_cgal_object(CGAL::Object)), 
             this, SLOT(get_new_object(CGAL::Object)));
+    widget->set_window(0, 600, 0, 600);
   };
 private:	//members
   CGAL::Qt_widget_get_point<Rep> get_point;
   My_Layer v;
+  CGAL::Qt_widget *widget;
 private slots:
   void get_new_object(CGAL::Object obj)
   {
@@ -46,9 +50,8 @@ private slots:
     if (CGAL::assign(p, obj)) { 
       dt.insert(p);
     }
-    redraw();
+    widget->redraw();
   }
-
 }; //endclass
 
 //  moc_source_file : layer.C
@@ -59,7 +62,6 @@ int main( int argc, char **argv )
     QApplication app( argc, argv );
     My_Window *W = new My_Window(600,600);
     app.setMainWidget( W );
-    W->set_window(0, 600, 0, 600);
     W->show();
     return app.exec();
 }
