0a18fcde7fb6f9ce44f35d75b729260958fe6c15
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2003-08-27 17:02:10 +0000

    - Change Exact_predicates_inexact_constructions_kernel from Filtered_kernel   to Filtered_exact.

--- Packages/Kernel_23/include/CGAL/Exact_predicates_inexact_constructions_kernel.h
+++ Packages/Kernel_23/include/CGAL/Exact_predicates_inexact_constructions_kernel.h
@@ -25,12 +25,29 @@
 #define CGAL_EXACT_PREDICATES_INEXACT_CONSTRUCTIONS_KERNEL_H
 
 #include <CGAL/Simple_cartesian.h>
-#include <CGAL/Filtered_kernel.h>
+// We don't use Filtered_kernel at the moment because it's slower
+// that Filtered_exact.
+// #include <CGAL/Filtered_kernel.h>
+
+#ifdef CGAL_CFG_MATCHING_BUG_2
+#  define CGAL_IA_CT        double
+#  define CGAL_IA_PROTECTED true
+#  define CGAL_IA_CACHE     No_Filter_Cache
+#  define CGAL_IA_ET        CGAL::MP_Float
+#endif
+
+#include <CGAL/MP_Float.h>
+#include <CGAL/Filtered_exact.h>
 
 CGAL_BEGIN_NAMESPACE
 
+#if 0
 typedef Filtered_kernel< Simple_cartesian<double> >
         Exact_predicates_inexact_constructions_kernel;
+#else
+typedef Simple_cartesian<Filtered_exact<double, MP_Float> >
+        Exact_predicates_inexact_constructions_kernel;
+#endif
 
 CGAL_END_NAMESPACE
 
