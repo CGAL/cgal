8b64209cf7bb6156562b35094f05f816beae1752
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2003-08-27 08:23:36 +0000

    - Merge include/CGAL/_test_valid_finite_[double|float].h into the corresponding   .C file.

--- Packages/Number_types/test/NumberTypeSupport/_test_valid_finite_float.C
+++ Packages/Number_types/test/NumberTypeSupport/_test_valid_finite_float.C
@@ -16,14 +16,39 @@
 // revision_date : $Date$
 // author(s)     : Stefan Schirra
 //
-//
-// coordinator   : MPI, Saarbruecken  (<Stefan.Schirra@mpi-sb.mpg.de>)
+// coordinator   : MPI, Saarbruecken
 // ============================================================================
  
 
 #include <CGAL/basic.h>
 #include <cassert>
-#include <CGAL/_test_valid_finite_float.h>
+#include <cmath>
+#include <CGAL/IEEE_754_unions.h>
+
+bool
+_test_valid_finite( const float& )
+{
+  float d0 = 0.0;
+  float d1 = 1.0;
+  float d2;
+  
+  assert( CGAL::is_valid( d0) );
+  assert( CGAL::is_valid( d1) );
+  
+  assert( CGAL::is_finite( d0) );
+  assert( CGAL::is_finite( d1) );
+  
+  if ( CGAL::is_valid( d1/d0 - d1/d0 ))
+  { d2 = d1/d0 - d1/d0; show( reinterpret_cast<IEEE_754_float*>(&d2)); }
+  if ( CGAL::is_finite( d1/d0 ))
+  { d2 = d1/d0; show( reinterpret_cast<IEEE_754_float*>(&d2)); }
+  
+  assert( CGAL::is_valid( d1/d0 ));
+  assert( !CGAL::is_valid( d1/d0 - d1/d0 ));
+  assert( !CGAL::is_finite( d1/d0 ));
+
+  return true;
+}
 
 int
 main()
