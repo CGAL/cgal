1ddd30e3a3c273126d3155f09590b09a8da0fe04
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2003-09-02 20:29:25 +0000

    Added missing *s

--- Packages/Mesh_2/demo/Mesh_2/icons.h
+++ Packages/Mesh_2/demo/Mesh_2/icons.h
@@ -1,9 +1,8 @@
-extern char * 
-  polygon_xpm[],
-  point_xpm[],
-  points_xpm[],
-  contraints_xpm[],
-  marked_xpm[],
-  seeds_xpm[],
-  circle_xpm[],
-  triangulation_xpm[];
+extern char *polygon_xpm[],
+            *point_xpm[],
+            *points_xpm[],
+            *contraints_xpm[],
+            *marked_xpm[],
+            *seeds_xpm[],
+            *circle_xpm[],
+            *triangulation_xpm[];
