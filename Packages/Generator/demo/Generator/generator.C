b0200d06c5bd25a9d2fcaa42fc33b3544adac079
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2003-09-07 11:47:39 +0000

    2.69 (07 Sep 2003)    - [rursu] demo use now the new Qt_help_window class instead of HelpWindow    - [rursu] demo use new icon demoicon

--- Packages/Generator/demo/Generator/generator.C
+++ Packages/Generator/demo/Generator/generator.C
@@ -33,8 +33,10 @@ int main(int, char*){
 
 #include <CGAL/IO/Qt_widget.h>
 #include <CGAL/IO/Qt_widget_standard_toolbar.h>
-#include <CGAL/IO/Qt_widget_helpwindow.h>
+#include <CGAL/IO/Qt_help_window.h>
 #include <CGAL/IO/Qt_widget_layer.h>
+#include <CGAL/IO/pixmaps/demoicon.xpm>
+
 
 #include <qplatinumstyle.h>
 #include <qapplication.h>
@@ -185,7 +187,7 @@ private slots:
   void howto(){
     QString home;
     home = "help/index.html";
-    HelpWindow *help = new HelpWindow(home, ".", 0, "help viewer");
+    Qt_help_window *help = new Qt_help_window(home, ".", 0, "help viewer");
     help->resize(400, 400);
     help->setCaption("Demo HowTo");
     help->show();
@@ -291,6 +293,8 @@ main(int argc, char **argv)
   app.setMainWidget(&widget);
   widget.setCaption(my_title_string);
   widget.setMouseTracking(TRUE);
+  QPixmap cgal_icon = QPixmap((const char**)demoicon_xpm);
+  widget.setIcon(cgal_icon);
   widget.show();
   current_state = -1;
   return app.exec();
