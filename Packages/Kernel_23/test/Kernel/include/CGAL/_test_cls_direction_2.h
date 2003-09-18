053fcda0a04219453e5b7d270b081a9dde657197
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2003-09-18 14:14:42 +0000

    - new headers

--- Packages/Kernel_23/test/Kernel/include/CGAL/_test_cls_direction_2.h
+++ Packages/Kernel_23/test/Kernel/include/CGAL/_test_cls_direction_2.h
@@ -1,24 +1,22 @@
-// ============================================================================
+// Copyright (c) 1999  Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland), Freie Universitaet Berlin (Germany),
+// INRIA Sophia-Antipolis (France), Martin-Luther-University Halle-Wittenberg
+// (Germany), Max-Planck-Institute Saarbrucken (Germany), RISC Linz (Austria),
+// and Tel-Aviv University (Israel).  All rights reserved.
 //
-// Copyright (c) 1999 The CGAL Consortium
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
 //
-// This software and related documentation is part of an INTERNAL release
-// of the Computational Geometry Algorithms Library (CGAL). It is not
-// intended for general use.
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// ----------------------------------------------------------------------------
-// 
-// release       :
-// release_date  :
-// 
-// file          : _test_cls_direction_2.h
-// revision      : $Revision$
-// revision_date : $Date$
-// author(s)     : Stefan Schirra
+// $Source$
+// $Revision$ $Date$
+// $Name$
 //
-//
-// coordinator   : MPI, Saarbruecken
-// ============================================================================
+// Author(s)     : Stefan Schirra
  
 
 #ifndef CGAL__TEST_CLS_DIRECTION_2_H
