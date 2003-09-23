dd4cb0f7e16856ea413898c458244d0e7be49d56
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2003-09-23 16:14:37 +0000

    typo

--- Packages/Kernel_d/include/CGAL/Homogeneous_d.h
+++ Packages/Kernel_d/include/CGAL/Homogeneous_d.h
@@ -69,7 +69,7 @@ public:
   typedef CGAL::Hyperplane_d<Self>         Hyperplane_d;
   typedef CGAL::Aff_transformation_d<Self> Aff_transformation_d;
 
-  typedef typename Point_d::Cartesian_const_iterator Cartesian_const_iterator_d;
+  typedef typename Point_d_base::Cartesian_const_iterator Cartesian_const_iterator_d;
 
   template <typename K>
   class Construct_Cartesian_const_iterator
