b4d2420bb3306d21389310607ba0964c2c8f5b03
Efi Fogel <efif@post.tau.ac.il>
2003-10-01 14:33:20 +0000

    moved from Qt_widget/Planar_map

---
+++ Packages/Planar_map/demo/Planar_map/Qt_widget_toolbar.C
@@ -0,0 +1,93 @@
+// ============================================================================
+//
+// Copyright (c) 1997-2000 The CGAL Consortium
+//
+// This software and related documentation is part of an INTERNAL release
+// of the Computational Geometry Algorithms Library (CGAL). It is not
+// intended for general use.
+//
+// ----------------------------------------------------------------------------
+//
+// file          : demo/Qt_widget/Planar_map/Qt_widget_toolbar.C
+// package       : Planar_map
+// author(s)     : Efi Fogel
+// release       : 
+// release_date  : 
+//
+// coordinator   : Efi Fogel
+//
+// ============================================================================
+
+#ifdef CGAL_USE_QT
+
+#include <CGAL/IO/Qt_widget.h>
+#include <CGAL/IO/pixmaps/movepoint.xpm>
+#include <CGAL/IO/pixmaps/point.xpm>
+#include <CGAL/IO/pixmaps/line.xpm>
+#include <CGAL/IO/pixmaps/arrow.xpm>
+
+#include <qiconset.h>
+
+#include "Qt_widget_toolbar.h"
+
+Tools_toolbar::Tools_toolbar(CGAL::Qt_widget * w, 
+			     QMainWindow *mw, std::list<Curve> * l1, Planar_map * pm) :
+  QToolBar(mw, "NT")
+{
+  snaplayer.pass_the_structure(l1, pm);
+  w->attach(&pointbut);
+  w->attach(&segmentbut);
+  w->attach(&snaplayer);
+
+  //    move_deletebut.deactivate();
+  pointbut.deactivate();
+  segmentbut.deactivate();
+  snaplayer.deactivate();
+
+  //    move_deletebut.pass_the_structure(l1);
+  //set the widget
+  widget = w;
+
+  QIconSet set0(QPixmap( (const char**)arrow_small_xpm ),
+                QPixmap( (const char**)arrow_xpm ));
+  QIconSet set1(QPixmap( (const char**)point_small_xpm ),
+                QPixmap( (const char**)point_xpm ));
+  QIconSet set2(QPixmap( (const char**)line_small_xpm ),
+                QPixmap( (const char**)line_xpm ));
+  QIconSet set3(QPixmap( (const char**)movepoint_small_xpm ),
+                QPixmap( (const char**)movepoint_xpm ));
+    
+
+  but[0] = new QToolButton(this, "deactivate layer");
+  but[0]->setIconSet(set0);
+  but[0]->setTextLabel("Deactivate Layer");
+  but[1] = new QToolButton(this, "pointinput layer");
+  but[1]->setIconSet(set1);
+  but[1]->setTextLabel("Input point");
+  but[2] = new QToolButton(this, "segment layer");
+  but[2]->setIconSet(set2);
+  but[2]->setTextLabel("Segment layer");
+  but[3] = new QToolButton(this, "snapping layer");
+  but[3]->setIconSet(set3);
+  but[3]->setTextLabel("Snapping layer");
+
+  nr_of_buttons = 4;
+  button_group = new QButtonGroup(0, "My_group");
+  for (int i = 0; i<nr_of_buttons; i++) {
+    button_group->insert(but[i]);
+    but[i]->setToggleButton(true);
+  }
+  button_group->setExclusive(true);
+  
+  connect(but[1], SIGNAL(stateChanged(int)),
+          &pointbut, SLOT(stateChanged(int)));
+  connect(but[2], SIGNAL(stateChanged(int)),
+          &segmentbut, SLOT(stateChanged(int)));
+  connect(but[3], SIGNAL(stateChanged(int)),
+          &snaplayer, SLOT(stateChanged(int)));
+
+};
+
+#include "Qt_widget_toolbar.moc"
+
+#endif
---
+++ Packages/Planar_map/demo/Planar_map/planar_map.C
@@ -0,0 +1,308 @@
+// ============================================================================
+//
+// Copyright (c) 1997-2003 The CGAL Consortium
+// This software and related documentation are part of the Computational
+// Geometry Algorithms Library (CGAL).
+// This software and documentation are provided "as-is" and without warranty
+// of any kind. In no event shall the CGAL Consortium be liable for any
+// damage of any kind. 
+// ----------------------------------------------------------------------
+//
+// file          : demo.C
+// package       : Planar_map
+// author(s)     : Efi Foegl
+// coordinator   : Efi Fogel
+//
+// email         : contact@cgal.org
+// www           : http://www.cgal.org
+//
+// ======================================================================
+
+
+// if QT is not installed, a message will be issued in runtime.
+#ifndef CGAL_USE_QT
+#include <iostream>
+int main(int, char*)
+{
+  std::cout << "Sorry, this demo needs QT...";
+  std::cout << std::endl;
+
+  return 0;
+}
+
+#else
+
+#include "cgal_types.h"
+#include <CGAL/IO/Qt_widget.h>
+#include <CGAL/IO/Qt_widget_Polygon_2.h>
+#include <CGAL/IO/Qt_widget_standard_toolbar.h>
+#include <CGAL/IO/Qt_help_window.h>
+#include <CGAL/IO/Qt_widget_layer.h>
+#include "Qt_widget_toolbar.h"
+#include <CGAL/IO/pixmaps/demoicon.xpm>
+
+#include <qapplication.h>
+#include <qmainwindow.h>
+#include <qstatusbar.h>
+#include <qfiledialog.h>
+#include <qmessagebox.h>
+#include <qpopupmenu.h>
+#include <qmenubar.h>
+#include <qtoolbutton.h>
+#include <qtoolbar.h>
+#include <qfiledialog.h>
+#include <qtimer.h>
+
+
+#include <fstream>
+#include <stack>
+#include <set>
+#include <string>
+#include <list>
+
+const QString my_title_string("Planar map demo with"
+			      " CGAL Qt_widget");
+
+// global flags and variables
+int current_state;
+std::list<Curve> list_of_segments;
+Planar_map pm;
+bool pl_valid = false;
+Point pl_point;
+
+class Qt_layer_show_ch : public CGAL::Qt_widget_layer {
+public:
+	
+  Qt_layer_show_ch() {};
+
+  void draw()
+  {
+    widget->lock();
+
+    *widget << CGAL::GREEN;
+    *widget << CGAL::LineWidth(1);
+    std::list<Curve>::iterator itp = list_of_segments.begin();
+    while (itp != list_of_segments.end()) {
+      *widget << (*itp++);
+    }
+
+    if(pl_valid && !(pm.halfedges_begin() == pm.halfedges_end())) {
+      *widget << CGAL::LineWidth(3);
+      *widget << CGAL::YELLOW;
+
+      Locate_type lt;
+      Point temp_p(pl_point.x(), pl_point.y());
+      Halfedge_handle e = pm.locate(temp_p, lt);
+      // std::cout << "locate type " << lt << std::endl;
+	
+      //color the face on the screen
+      Face_handle f = e->face();
+	
+      if (f->does_outer_ccb_exist()) {
+        Ccb_halfedge_circulator cc = f->outer_ccb();
+        do {
+          *widget << cc->curve();
+        } while (++cc != f->outer_ccb());
+      }
+
+      Holes_iterator hit = f->holes_begin(), eit = f->holes_end();
+      for (;hit != eit; ++hit) {
+        Ccb_halfedge_circulator cc = *hit; 
+        do {
+          *widget << cc->curve();
+        } while (++cc != *hit);
+      }
+      *widget << CGAL::LineWidth(1);
+    }//endif
+
+    widget->unlock();
+  };	
+
+};//end class 
+
+class MyWindow : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+  MyWindow(int w, int h)
+  {
+    widget = new CGAL::Qt_widget(this);
+    setCentralWidget(widget);
+    
+    //create a timer for checking if somthing changed
+    QTimer * timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(timer_done()));
+    timer->start(200, FALSE);
+
+    // file menu
+    QPopupMenu * file = new QPopupMenu(this);
+    menuBar()->insertItem("&File", file);
+    file->insertItem("&New", this, SLOT(new_instance()), CTRL+Key_N);
+    file->insertItem("New &Window", this, SLOT(new_window()), CTRL+Key_W);
+    file->insertSeparator();
+    file->insertItem("Print", widget, SLOT(print_to_ps()), CTRL+Key_P);
+    file->insertSeparator();
+    file->insertItem("&Close", this, SLOT(close()), CTRL+Key_X);
+    file->insertItem("&Quit", qApp, SLOT(closeAllWindows()), CTRL+Key_Q);
+
+    // drawing menu
+    QPopupMenu * draw = new QPopupMenu(this);
+    menuBar()->insertItem("&Draw", draw);
+    draw->insertItem("&Generate segments", this,
+                     SLOT(gen_segments()), CTRL+Key_G);
+
+    // help menu
+    QPopupMenu * help = new QPopupMenu(this);
+    menuBar()->insertItem("&Help", help);
+    help->insertItem("How To", this, SLOT(howto()), Key_F1);
+    help->insertSeparator();
+    help->insertItem("&About", this, SLOT(about()), CTRL+Key_A);
+    help->insertItem("About &Qt", this, SLOT(aboutQt()));
+
+    //the standard toolbar
+    stoolbar = new CGAL::Qt_widget_standard_toolbar (widget, this, "ST");
+    //the new tools toolbar
+    newtoolbar = new Tools_toolbar(widget, this, &list_of_segments, &pm);	
+  
+    *widget << CGAL::LineWidth(2) << CGAL::BackgroundColor (CGAL::BLACK);
+  
+    resize(w, h);
+    widget->set_window(-1, 1, -1, 1);
+    widget->setMouseTracking(TRUE);
+	
+    //connect the widget to the main function that receives the objects
+    connect(widget, SIGNAL(new_cgal_object(CGAL::Object)), 
+            this, SLOT(get_new_object(CGAL::Object)));
+
+    //application flag stuff
+    old_state = 0;
+
+    //layers
+    widget->attach(&testlayer);
+  };
+
+private:
+  void something_changed() { current_state++; }
+  
+public slots:
+  void new_instance()
+  {
+    widget->lock();
+    list_of_segments.clear();
+    stoolbar->clear_history();
+    widget->set_window(-1.1, 1.1, -1.1, 1.1); 
+    // set the Visible Area to the Interval
+    widget->unlock();
+    something_changed();
+  }
+
+private slots:
+  void get_new_object(CGAL::Object obj)
+  {
+    pl_valid = false;
+    Curve s;
+    if (CGAL::assign(s, obj)) {
+      list_of_segments.push_back(s);
+      pm.insert(s);
+      something_changed();
+    }
+
+    Point p;
+    if (CGAL::assign(p,obj)) {
+      pl_point = p;
+      pl_valid = true;
+      
+      something_changed();
+    }
+  };
+
+  void about()
+  {
+    QMessageBox::about(this, my_title_string,
+                       "This is a demo for the Planar_map package\n"
+                       "Copyright CGAL @2003");
+  };
+
+  void aboutQt()
+  {
+    QMessageBox::aboutQt(this, my_title_string);
+  }
+
+  void howto()
+  {
+    QString home;
+    home = "help/index.html";
+    CGAL::Qt_help_window *help = new 
+      CGAL::Qt_help_window(home, ".", 0, "help viewer");
+    help->resize(400, 400);
+    help->setCaption("Demo HowTo");
+    help->show();
+  }
+
+  void new_window()
+  {
+    MyWindow *ed = new MyWindow(500, 500);
+    ed->setCaption("Layer");
+    ed->stoolbar->clear_history();
+    ed->widget->set_window(-1.1, 1.1, -1.1, 1.1);
+    ed->show();
+    something_changed();
+  }
+
+  void timer_done()
+  {
+    if(old_state!=current_state) {
+      widget->redraw();
+      old_state = current_state;
+    }
+  }	
+
+  void gen_segments()
+  {
+    stoolbar->clear_history();
+    widget->set_window(-1.1, 1.1, -1.1, 1.1); 
+    // set the Visible Area to the Interval
+
+    // send resizeEvent only on show.
+    CGAL::Random_points_in_square_2<Point> g(0.5);
+    for (int count = 0; count < 25; count++) {
+      Point p1(*g++), p2(*g++);
+      NT scale(2);
+      Curve s(Point(p1.x() * scale, p1.y() * scale)  ,
+              Point(p2.x() * scale, p2.y() * scale));
+      list_of_segments.push_back(s);
+      pm.insert(s);
+    }
+
+    pl_valid = false;
+
+    something_changed();
+  }
+	
+
+private:
+  CGAL::Qt_widget * widget;
+  CGAL::Qt_widget_standard_toolbar * stoolbar;
+  Tools_toolbar * newtoolbar;
+  int old_state;
+  Qt_layer_show_ch testlayer;
+};
+
+#include "planar_map.moc"
+
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+  MyWindow widget(500, 500); // physical window size
+  app.setMainWidget(&widget);
+  widget.setCaption(my_title_string);
+  widget.setMouseTracking(TRUE);
+  QPixmap cgal_icon = QPixmap((const char**)demoicon_xpm);
+  widget.setIcon(cgal_icon);
+  widget.show();
+  current_state = -1;
+  return app.exec();
+}
+
+#endif
