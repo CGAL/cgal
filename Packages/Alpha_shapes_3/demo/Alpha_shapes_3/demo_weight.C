50dc56cbd8eb536073c16c0cc0657b6f3f9c42b6
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2003-10-03 14:40:57 +0000

    cleanup

--- Packages/Alpha_shapes_3/demo/Alpha_shapes_3/demo_weight.C
+++ Packages/Alpha_shapes_3/demo/Alpha_shapes_3/demo_weight.C
@@ -30,21 +30,9 @@ to the Alpha Shape.
 #endif
 
 
-//#include <CGAL/IO/Triangulation_geomview_ostream_3.h>
 
-//Choose the better number type as possible
-// #ifdef CGAL_USE_LEDA
-// #include <CGAL/leda_integer.h>
-// typedef leda_integer coord_type;
-// #else//CGAL_USE_LEDA
-// #ifdef CGAL_USE_GMP
-// #include <CGAL/Gmpz.h>
-// typedef CGAL::Gmpz coord_type;
-// #else//CGAL_USE_GMP
-// #include <CGAL/double.h>
 typedef double coord_type;
-// #endif//CGAL_USE_GMP
-// #endif//CGAL_USE_LEDA
+
 
 typedef CGAL::Cartesian<coord_type>  K;
 
