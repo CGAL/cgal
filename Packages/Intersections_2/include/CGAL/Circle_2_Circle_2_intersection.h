13a52ced1de785b527c8139dabf452f46fc8dc84
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2003-10-07 10:02:39 +0000

    Moved code from .C into the header file

--- Packages/Intersections_2/include/CGAL/Circle_2_Circle_2_intersection.h
+++ Packages/Intersections_2/include/CGAL/Circle_2_Circle_2_intersection.h
@@ -25,6 +25,7 @@
 
 #include <CGAL/Circle_2.h>
 #include <CGAL/Object.h>
+#include <CGAL/squared_distance_2_1.h>
 
 CGAL_BEGIN_NAMESPACE
 
@@ -34,18 +35,31 @@ template <class K>
 bool
 do_intersect(const typename CGAL_WRAP(K)::Circle_2 & circ1, 
 	     const typename CGAL_WRAP(K)::Circle_2& circ2,
-	     const K&);
+	     const K&)
+{
+    typedef typename K::FT FT;
+    FT sr1 = circ1.squared_radius();
+    FT sr2 = circ2.squared_radius();
+    FT squared_dist = squared_distance(circ1.center(), circ2.center());
+    FT temp = sr1+sr2-squared_dist;
+    return !(FT(4)*sr1*sr2 < temp*temp);
+}
+
 } // namespace CGALi
 
-template <class R>
+
+template <class K>
+inline
 bool
-do_intersect(const Circle_2<R> &tr1, const Circle_2<R>&tr2);
+do_intersect(const Circle_2<K> & circ1, 
+	     const Circle_2<K> & circ2)
+{
+  return CGALi::do_intersect(circ1, circ2, K());
+}
+
 
 CGAL_END_NAMESPACE
 
-#ifdef CGAL_CFG_NO_AUTOMATIC_TEMPLATE_INCLUSION
-#include <CGAL/Circle_2_Circle_2_intersection.C>
-#endif
 #endif
 
 
