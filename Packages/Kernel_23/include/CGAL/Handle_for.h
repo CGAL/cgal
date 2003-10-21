3a3efc6ed8e048f726c23f0db66b4905b3a2b6c9
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2003-10-21 11:35:16 +0000

    fix LGPL headers

--- Packages/Kernel_23/include/CGAL/Handle_for.h
+++ Packages/Kernel_23/include/CGAL/Handle_for.h
@@ -9,6 +9,9 @@
 // published by the Free Software Foundation; version 2.1 of the License.
 // See the file LICENSE.LGPL distributed with CGAL.
 //
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
