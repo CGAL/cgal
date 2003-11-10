09196efa5732344666532ca42b6b788478e19459
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2003-11-10 13:54:32 +0000

    added a split_vertex method; needed for the case of intersecting segments

--- Packages/Segment_Voronoi_diagram_2/include/CGAL/Segment_Voronoi_diagram_data_structure_2.h
+++ Packages/Segment_Voronoi_diagram_2/include/CGAL/Segment_Voronoi_diagram_data_structure_2.h
@@ -27,6 +27,8 @@
 #ifndef CGAL_SEGMENT_VORONOI_DIAGRAM_DATA_STRUCTURE_2_H
 #define CGAL_SEGMENT_VORONOI_DIAGRAM_DATA_STRUCTURE_2_H
 
+#include <list>
+#include <CGAL/utility.h>
 #include <CGAL/Apollonius_graph_data_structure_2.h>
 
 
@@ -43,8 +45,133 @@ public:
   typedef Segment_Voronoi_diagram_data_structure_2<Vb, Fb>    Tds;
   typedef typename Base::Tds_base                             Tds_base;
 
+  typedef typename Tds_base::Vertex_handle    Vertex_handle;
+  typedef typename Tds_base::Face_handle      Face_handle;
+  typedef typename Tds_base::Face_circulator  Face_circulator;
+
   //  typedef typename Vb::template Rebind_TDS<Tds>::Other  Vertex_base;
   //  typedef typename Fb::template Rebind_TDS<Tds>::Other  Face_base;
+
+
+  /*
+  // The following method preforms a split operation of the vertex v
+  // using the faces f1 and g1. The split operation is shown
+  // below.
+  // The names of the variables in the method correspond to the
+  // quantities in the drawings below
+  //
+  // The configuration before the split:
+  //
+  //                  cw(i1)   v3   ccw(i2)
+  //                     *-----*-----*
+  //                    / \    |    / \
+  //                   /   \ f1|f2 /   \
+  //                  /     \  |  /     \
+  //                 /       \ | /       \
+  //                /         \|/v        \
+  //               *-----------*-----------*
+  //                \         /|\         /
+  //                 \       / | \       /
+  //                  \     /  |  \     /
+  //                   \   / g2|g1 \   /
+  //                    \ /    |    \ /
+  //                     *-----*-----*
+  //                 ccw(j2)   v4   cw(j1)
+  //
+  //
+  // The configuration after the split:
+  //
+  //
+  //               cw(i1)      v3     ccw(i2)
+  //                 *---------*---------*
+  //                / \       / \       / \
+  //               /   \  f1 /   \  f2 /   \
+  //              /     \   /  f  \   /     \
+  //             /       \ /     v2\ /       \
+  //            *---------*---------*---------*
+  //             \       / \v1     / \       /
+  //              \     /   \  g  /   \     /
+  //               \   /  g2 \   /  g1 \   /
+  //                \ /       \ /       \ /
+  //                 *---------*---------*
+  //              ccw(j2)      v4      cw(j1)
+  //
+  */
+  Quadruple<Vertex_handle, Vertex_handle, Face_handle, Face_handle>
+  split_vertex(Vertex_handle v, Face_handle f1, Face_handle g1)
+  {
+    typedef Vertex_handle VH;
+    typedef Face_handle   FH;
+
+    // 1. first we read some information that we will need
+    int i1 = f1->index(v);
+    int j1 = g1->index(v);
+    Face_handle f2 = f1->neighbor( cw(i1) );
+    Face_handle g2 = g1->neighbor( cw(j1) );
+
+    int i2 = f2->index(v);
+    int j2 = g2->index(v);
+
+    Vertex_handle v3 = f1->vertex( ccw(i1) );
+    Vertex_handle v4 = g1->vertex( ccw(j1) );
+
+    // lst is the list of faces adjecent to v stored in
+    // counterclockwise order from g2 to f1) inclusive.
+    // the list idx contains the indices of v in the
+    // faces in lst.
+    std::list<Face_handle> lst;
+    std::list<int>         idx;
+
+    Face_circulator fc(v, f2);
+    Face_handle ff(fc);
+    while ( ff != g1 ) {
+      lst.push_back( ff );
+      idx.push_back( ff->index(v) );
+      fc++;
+      ff = Face_handle(fc);
+    }
+    lst.push_back( ff );
+    idx.push_back( ff->index(v) );
+
+    // 2. we create the new vertices and the two new faces
+    Vertex_handle v1 = v;
+    Vertex_handle v2 = create_vertex();
+    Face_handle f = create_face(v1, v2, v3);
+    Face_handle g = create_face(v2, v1, v4);
+
+    // 3. we update the adjacency information for the new vertices and
+    //    the new faces
+    f->set_neighbor(0, f2);
+    f->set_neighbor(1, f1);
+    f->set_neighbor(2, g);
+    g->set_neighbor(0, g2);
+    g->set_neighbor(1, g1);
+    g->set_neighbor(2, g);
+    v1->set_face(f);
+    v2->set_face(g);
+
+    // 4. update the vertex for the faces f2 through g1 in
+    //    counterclockwise order
+    typename std::list<Face_handle>::iterator lit = lst.begin();
+    typename std::list<int>::iterator         iit = idx.begin();
+    for (; lit != lst.end(); ++lit, ++iit) {
+      lit->set_vertex(*iit, v2);
+    }
+
+    lst.clear();
+    idx.clear();
+
+    // 5. make f and g the new neighbors of f1, f2 and g1, g2
+    //    respectively.
+    f1->set_neighbor( cw(i1), f );
+    f2->set_neighbor(ccw(i2), f );
+    g1->set_neighbor( cw(j1), g );
+    g2->set_neighbor(ccw(j2), g );
+
+    // 6. return the new stuff
+    return Quadruple<VH, VH, FH, FH>(v1, v2, f, g);
+  }
+
 
 };
 
