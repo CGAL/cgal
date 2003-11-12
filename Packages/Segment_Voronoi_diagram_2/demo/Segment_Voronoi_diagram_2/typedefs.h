280cc847cc04e24a5feb4f5c31ec6940b78397db
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2003-11-12 03:30:35 +0000

    the new typedefs for the demo. it is slower but it works currently with intersecting segments; I should get it back to what it was

--- Packages/Segment_Voronoi_diagram_2/demo/Segment_Voronoi_diagram_2/typedefs.h
+++ Packages/Segment_Voronoi_diagram_2/demo/Segment_Voronoi_diagram_2/typedefs.h
@@ -10,10 +10,21 @@
 #include <CGAL/Segment_Voronoi_diagram_hierarchy_2.h>
 #include <CGAL/Segment_Voronoi_diagram_filtered_traits_2.h>
 
-struct Rep : public CGAL::Simple_cartesian<double> {};
+//struct Rep : public CGAL::Simple_cartesian<double> {};
+struct Rep : public CGAL::Simple_cartesian<CGAL::Gmpq> {};
+
+namespace CGAL {
+  CGAL::Gmpq sqrt(const CGAL::Gmpq& x)
+  {
+    return CGAL::Gmpq( sqrt(x.to_double()) );
+  }
+}
+
+//struct Gt
+//  : public CGAL::Segment_Voronoi_diagram_filtered_traits_2<Rep> {};
 
 struct Gt
-  : public CGAL::Segment_Voronoi_diagram_filtered_traits_2<Rep> {};
+  : public CGAL::Segment_Voronoi_diagram_traits_2<Rep,CGAL::Ring_tag> {};
 
 
 typedef Gt::Point_2            Point;
@@ -21,6 +32,7 @@ typedef Gt::Segment_2          Segment;
 typedef CGAL::Polygon_2<Rep>   Polygon_2;
 typedef Gt::Site_2             Site;
 
-typedef CGAL::Segment_Voronoi_diagram_hierarchy_2<Gt>          SVD_2;
+//typedef CGAL::Segment_Voronoi_diagram_hierarchy_2<Gt>          SVD_2;
+typedef CGAL::Segment_Voronoi_diagram_2<Gt>          SVD_2;
 
 #endif  // _MK___TYPEDEFS_H
