8ba844659b6ab2bd71f60a74ff910469947ffbcd
Lutz Kettner <kettner@mpi-sb.mpg.de>
2003-11-17 15:10:50 +0000

    Changed license header for basic library files.

--- Packages/Kernel_d/include/CGAL/Convex_hull_d_to_polyhedron_3.h
+++ Packages/Kernel_d/include/CGAL/Convex_hull_d_to_polyhedron_3.h
@@ -1,13 +1,9 @@
-// Copyright (c) 2000,2001  Utrecht University (The Netherlands),
-// ETH Zurich (Switzerland), Freie Universitaet Berlin (Germany),
-// INRIA Sophia-Antipolis (France), Martin-Luther-University Halle-Wittenberg
-// (Germany), Max-Planck-Institute Saarbruecken (Germany), RISC Linz (Austria),
-// and Tel-Aviv University (Israel).  All rights reserved.
+// Copyright (c) 1997-2000  Max-Planck-Institute Saarbruecken (Germany).
+// All rights reserved.
 //
-// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; version 2.1 of the License.
-// See the file LICENSE.LGPL distributed with CGAL.
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
 //
 // Licensees holding a valid commercial license may use this file in
 // accordance with the commercial license agreement provided with the software.
