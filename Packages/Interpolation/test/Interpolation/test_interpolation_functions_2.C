28de2dba43d30b8332b0a8e36375e65fad751472
Julia Flötotto <Julia.Flototto@sophia.inria.fr>
2003-12-09 10:00:01 +0000

    - added test for sibson_c1_interpolation (with sqrt)

--- Packages/Interpolation/test/Interpolation/test_interpolation_functions_2.C
+++ Packages/Interpolation/test/Interpolation/test_interpolation_functions_2.C
@@ -23,6 +23,8 @@
 #include <CGAL/basic.h>
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
 #include <CGAL/Delaunay_triangulation_2.h>
 
 #include <CGAL/_test_interpolation_functions_2.C>
@@ -31,14 +33,22 @@
 struct K : CGAL::Exact_predicates_exact_constructions_kernel {};
 typedef CGAL::Delaunay_triangulation_2<K>            Dt;
 
+struct K2 : CGAL::Exact_predicates_inexact_constructions_kernel {};
+typedef CGAL::Delaunay_triangulation_2<K2>            Dt2;
+
 //////////////////////////////// 
 int main()
 {
-
+  
   std::cout << "Testing interpolation functions with 2D NN neighbors " << std::endl; 
   std::cout << " using Exact_predicates_exact_constructions_kernel: " << std::endl ;
-  _test_interpolation_functions_2_delaunay( Dt() );
-
+  _test_interpolation_functions_2_delaunay( Dt(), K::FT(0));
   
+
+  std::cout << "Testing interpolation functions with 2D NN neighbors " << std::endl; 
+  std::cout << " using Exact_predicates_inexact_constructions_kernel: " << std::endl ;
+  _test_interpolation_functions_2_delaunay( Dt2(), K2::FT(1e-10));
+
   return 0;
 }
+//end of file
