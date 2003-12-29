cf57b0b8606e2b4202c1024583173dde5225deb0
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2003-12-29 13:28:51 +0000

    cleaned up

--- Packages/Triangulation_2/include/CGAL/Triangulation_line_face_circulator_2.h
+++ Packages/Triangulation_2/include/CGAL/Triangulation_line_face_circulator_2.h
@@ -98,11 +98,6 @@ public:
 #ifdef CGAL_T2_USE_ITERATOR_AS_HANDLE
   bool  operator==(const Face_handle& fh) const { return fh == pos; }
   bool  operator!=(const Face_handle& fh) const { return fh != pos; }
-//   bool  operator==(CGAL_NULL_TYPE CGAL_triangulation_assertion_code(n)) const;
-//   bool  operator!=(CGAL_NULL_TYPE CGAL_triangulation_assertion_code(n)) const;
-// #else
-//   bool  operator==(CGAL_NULL_TYPE n) const;
-//   bool  operator!=(CGAL_NULL_TYPE n) const;
 #endif
   bool  operator==(CGAL_NULL_TYPE  CGAL_triangulation_assertion_code(n)) const;
   bool  operator!=(CGAL_NULL_TYPE n) const;
@@ -675,20 +670,6 @@ operator==(CGAL_NULL_TYPE CGAL_triangulation_assertion_code(n)) const
   return pos ==(CGAL_CIRC_NULL);
 }
             
-// template < class Gt, class Tds >
-// inline bool
-// Triangulation_line_face_circulator_2<Gt,Tds>:: 
-// #ifdef CGAL_T2_USE_ITERATOR_AS_HANDLE
-// operator!=(CGAL_NULL_TYPE CGAL_triangulation_assertion_code(n)) const
-// {
-//   CGAL_triangulation_assertion( n == NULL); 
-// #else
-// operator!=(CGAL_NULL_TYPE n) const
-// {
-// #endif
-//   return !(*this == n);
-// }
-
 template < class Gt, class Tds >
 inline bool
 Triangulation_line_face_circulator_2<Gt,Tds>::            
