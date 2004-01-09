5ae25ce3f8febbb0619ba412034c496b6d7ae9e0
Julia Flötotto <Julia.Flototto@sophia.inria.fr>
2004-01-09 13:04:25 +0000

    - repair accidental removed files

---
+++ Packages/Interpolation/include/CGAL/natural_neighbor_coordinates_2.h
@@ -0,0 +1,205 @@
+// Copyright (c) 1997  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Frank Da, Julia Floetotto
+#ifndef CGAL_NATURAL_NEIGHBOR_COORDINATES_2_H
+#define CGAL_NATURAL_NEIGHBOR_COORDINATES_2_H
+
+#include <utility>
+#include <CGAL/Polygon_2.h>
+
+//-------------------------------------------------------------------
+CGAL_BEGIN_NAMESPACE
+//-------------------------------------------------------------------
+
+
+template <class Dt, class OutputIterator>
+std::pair< OutputIterator, typename Dt::Geom_traits::FT > 
+natural_neighbor_coordinates_2(const Dt& dt, 
+			     const typename Dt::Geom_traits::Point_2& p, 
+			     OutputIterator out, typename Dt::Face_handle start 
+			     = typename Dt::Face_handle(NULL)){
+  return natural_neighbor_coordinates_2(dt, p, out, dt.geom_traits(),start);
+};
+  
+  
+
+template <class Dt, class OutputIterator, class Traits>
+std::pair< OutputIterator, typename Traits::FT > 
+natural_neighbor_coordinates_2(const Dt& dt,
+			     const typename Traits::Point_2& p, 
+			     OutputIterator out, const Traits& traits, 
+			     typename Dt::Face_handle start 
+			     = typename Dt::Face_handle(NULL)){
+  
+  typedef typename Traits::FT            Coord_type;
+  typedef typename Traits::Point_2       Point_2;
+  typedef typename Dt::Face_handle       Face_handle;
+  typedef typename Dt::Edge              Edge;
+  typedef typename Dt::Locate_type       Locate_type;
+  
+  Locate_type lt;
+  int li;
+  Face_handle fh = dt.locate(p, lt, li, start);
+  
+  //the point must lie inside the convex hull 
+  CGAL_precondition(lt != Dt::OUTSIDE_AFFINE_HULL 
+		    && lt != Dt::OUTSIDE_CONVEX_HULL
+		    && !(lt == Dt::EDGE && 
+			 (dt.is_infinite(fh) ||
+			  dt.is_infinite(fh->neighbor(li)))));
+  
+  if(lt == Dt::VERTEX){
+    *out++= std::pair<Point_2, Coord_type>(fh->vertex(li)->point(),
+					   Coord_type(1));
+    return( std::make_pair(out, Coord_type(1)));
+  }
+  
+  std::list<Edge> hole;
+  
+  dt.get_boundary_of_conflicts(p,std::back_inserter(hole), fh); 
+  return 
+    natural_neighbor_coordinates_2
+    (dt, p, out, hole.begin(),hole.end(), traits);
+}
+
+
+
+template <class Dt, class OutputIterator, class Traits, class EdgeIterator  >
+std::pair< OutputIterator, typename Traits::FT > 
+natural_neighbor_coordinates_2(const Dt& dt, 
+			     const typename Traits::Point_2& p, 
+			     OutputIterator out, EdgeIterator
+			     hole_begin, EdgeIterator hole_end, 
+			     const Traits& traits){
+  
+  CGAL_precondition(dt.dimension()==2);
+  //precondition: p must lie inside the hole 
+  //             (=^ inside convex hull of neighbors)
+  
+  typedef typename Traits::FT            Coord_type;
+  typedef typename Traits::Point_2       Point_2;
+  
+  typedef typename Dt::Vertex_handle     Vertex_handle;
+  typedef typename Dt::Face_circulator   Face_circulator;
+
+  std::vector<Point_2> vor(3); 
+
+  Coord_type area_sum(0);
+  EdgeIterator hit = hole_end;
+  --hit;
+  //in the beginning: prev is the "last" vertex of the hole:
+  // later: prev is the last vertex processed (previously) 
+  Vertex_handle prev = hit->first->vertex(dt.cw(hit->second));
+  hit = hole_begin;
+  
+  while(hit != hole_end)
+    { 
+      Coord_type area(0);
+      Vertex_handle current = hit->first->vertex(dt.cw(hit->second));
+      
+      vor[0] = traits.construct_circumcenter_2_object()
+	  (current->point(),
+	   hit->first->vertex(dt.ccw(hit->second))->point(),
+	   p);
+      
+      Face_circulator fc = dt.incident_faces(current, hit->first);
+      ++fc;
+      vor[1] = dt.dual(fc);
+      
+     while(!fc->has_vertex(prev))
+       {
+	 ++fc;
+	 vor[2] = dt.dual(fc);
+	  
+	 area += polygon_area_2(vor.begin(), vor.end(), traits);
+	 
+	 vor[1] = vor[2];
+       };
+     vor[2] = 
+       traits.construct_circumcenter_2_object()(prev->point(),
+						current->point(),p);
+     
+     area += polygon_area_2(vor.begin(), vor.end(), traits);
+     
+     *out++= std::pair<Point_2, Coord_type>(current->point(),area);
+     area_sum += area;
+     
+     //update prev and hit:
+     prev= current;
+     ++hit;
+    }
+  return( std::make_pair(out, area_sum));
+};
+
+/**********************************************************/
+//compute the coordinates for a vertex of the triangulation 
+// with respect to the other points in the triangulation
+template <class Dt, class OutputIterator>
+std::pair< OutputIterator, typename Dt::Geom_traits::FT > 
+natural_neighbor_coordinates_2(const Dt& dt, 
+			       typename Dt::Vertex_handle vh,
+			       OutputIterator out){
+  //init the traits class in natural_neighbor_coordinates_2
+  // to dt.geom_traits()
+  return natural_neighbor_coordinates_2(dt, vh, out, 
+					dt.geom_traits());
+};
+
+template <class Dt, class OutputIterator, class Traits>
+std::pair< OutputIterator, typename Traits::FT > 
+natural_neighbor_coordinates_2(const Dt& dt, 
+			       typename Dt::Vertex_handle vh, 
+			       OutputIterator out, const Traits& traits){
+  //this functions creates a small triangulation of the 
+  // incident vertices of this vertex and computes the 
+  // natural neighbor coordinates of ch->point() wrt. it.
+  typedef typename Dt::Vertex_circulator     Vertex_circulator;
+ 
+  Dt t2;
+  Vertex_circulator vc = dt.incident_vertices(vh),
+    done(vc);
+  do{
+    assert(!dt.is_infinite(vc));
+    t2.insert(vc->point());
+  }
+  while(++vc!=done);
+    
+  return natural_neighbor_coordinates_2(t2, vh->point(), out, 
+					traits);
+};
+
+//class providing a function object:
+template <class Dt, class OutputIterator>
+class natural_neighbor_coordinates_2_object 
+{
+public:
+  std::pair< OutputIterator, typename Dt::Geom_traits::FT > 
+  operator()(const Dt& dt, 
+	     typename Dt::Vertex_handle vh,
+	     OutputIterator out){
+    return natural_neighbor_coordinates_2(dt, vh, out);
+  }
+};
+
+
+
+//-------------------------------------------------------------------
+CGAL_END_NAMESPACE
+//-------------------------------------------------------------------
+
+#endif // CGAL_NATURAL_NEIGHBOR_COORDINATES_2_H
---
+++ Packages/Interpolation/include/CGAL/surface_neighbors_3.h
@@ -0,0 +1,262 @@
+// Copyright (c) 1997  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Julia Floetotto
+//
+#ifndef CGAL_SURFACE_NEIGHBORS_3_H
+#define CGAL_SURFACE_NEIGHBORS_3_H
+
+#include <utility>
+
+#include <CGAL/Voronoi_intersection_2_traits_3.h>
+#include <CGAL/Regular_triangulation_2.h>
+
+//contains the definition of the Comparator "closer_to_point":
+#include <CGAL/surface_neighbor_coordinates_3.h>
+
+//-------------------------------------------------------------------
+CGAL_BEGIN_NAMESPACE
+//-------------------------------------------------------------------
+
+
+//without Delaunay filtering
+template <class OutputIterator, class InputIterator, class Kernel>
+OutputIterator
+surface_neighbors_3(InputIterator first, InputIterator beyond,
+		    const typename Kernel::Point_3& p, 
+		    const typename Kernel::Vector_3& normal,
+		    OutputIterator out, const Kernel& K)
+{
+  typedef Voronoi_intersection_2_traits_3<Kernel> I_gt;
+  return 
+    surface_neighbors_3(first, beyond, p, out, I_gt(p,normal));
+}
+
+
+template <class OutputIterator, class InputIterator, class ITraits>
+OutputIterator 
+surface_neighbors_3(InputIterator first, InputIterator beyond,
+		    const typename ITraits::Point_2& p,
+		    OutputIterator out, const ITraits& traits)
+{
+  //definition of the Voronoi intersection triangulation:
+  typedef Regular_triangulation_2< ITraits>           I_triangulation;
+  typedef typename I_triangulation::Vertex_handle     Vertex_handle;
+  typedef typename I_triangulation::Face_handle       Face_handle;
+  typedef typename I_triangulation::Locate_type       Locate_type;
+
+
+  //build Voronoi intersection triangulation:
+  I_triangulation it(traits);
+  it.insert(first,beyond);
+  
+  Locate_type lt;
+  int li;
+  Face_handle fh = it.locate(p, lt, li);
+  
+  if(lt == I_triangulation::VERTEX){
+    *out++ =p;
+    return out;
+  }
+  
+  Vertex_handle vh = it.insert(p, fh);
+  
+  typename I_triangulation::Vertex_circulator
+    vc(it.incident_vertices(vh)),
+    done(vc);
+  do
+    *out++= vc->point();
+  while(vc++!=done);
+  
+  return out;
+}
+//without Delaunay filtering -- certified version:
+// a boolean is returned that indicates if a sufficiently large 
+// neighborhood has been considered so that the
+// Voronoi cell of p is not affected by any point outside the smallest 
+// ball centered on p containing all points in [first,beyond)
+template <class OutputIterator, class InputIterator, class Kernel>
+std::pair< OutputIterator, bool >
+surface_neighbors_certified_3(InputIterator first, 
+			      InputIterator beyond,
+			      const typename Kernel::Point_3& p, 
+			      const typename Kernel::Vector_3& normal,
+			      OutputIterator out, const Kernel& K)
+{
+  typedef Voronoi_intersection_2_traits_3<Kernel> I_gt;
+  return 
+    surface_neighbors_certified_3(first, beyond, p, out, I_gt(p,normal));
+}
+//this function takes the radius of the sphere centered on p 
+// containing the points in [first, beyond] (i.e. the maximal 
+// distance from p to [first,beyond) as add. parameter: 
+template <class OutputIterator, class InputIterator, class Kernel>
+std::pair< OutputIterator, bool >
+surface_neighbors_certified_3(InputIterator first, 
+			      InputIterator beyond,
+			      const typename Kernel::Point_3& p, 
+			      const typename Kernel::Vector_3& normal,
+			      const typename Kernel::FT& radius, 
+			      OutputIterator out, 
+			      const Kernel& K)
+{
+  typedef Voronoi_intersection_2_traits_3<Kernel> I_gt;
+  return 
+    surface_neighbors_certified_3(first, beyond, p, radius,
+				  out, I_gt(p,normal));
+}
+// Versions with instantiated traits class:
+template <class OutputIterator, class InputIterator, class ITraits>
+std::pair< OutputIterator, bool > 
+surface_neighbors_certified_3(InputIterator first, 
+			      InputIterator beyond,
+			      const typename ITraits::Point_2& p,
+			      OutputIterator out, 
+			      const ITraits& traits)
+{
+  //find the point in [first,beyond) furthest from p:
+  InputIterator furthest = std::max_element(first, beyond, 
+		     closer_to_point<ITraits>(p, traits));
+  
+  return 
+    surface_neighbors_certified_3
+    (first, beyond, p, 
+     traits.compute_squared_distance_2_object()(p,*furthest),
+     out, traits);
+};
+
+template <class OutputIterator, class InputIterator, class ITraits>
+std::pair< OutputIterator, bool > 
+surface_neighbors_certified_3(InputIterator first, 
+			      InputIterator beyond,
+			      const typename ITraits::Point_2& p,
+			      const typename ITraits::FT& radius,
+			      OutputIterator out,  
+			      const ITraits& traits)
+{
+  //definition of the Voronoi intersection triangulation:
+  typedef Regular_triangulation_2< ITraits>      I_triangulation;
+  
+  typedef typename I_triangulation::Vertex_handle     Vertex_handle;
+  typedef typename I_triangulation::Face_handle       Face_handle; 
+  typedef typename I_triangulation::Vertex_circulator Vertex_circulator;
+  typedef typename I_triangulation::Face_circulator   Face_circulator;
+  typedef typename I_triangulation::Locate_type       Locate_type;
+
+
+  //build Voronoi intersection triangulation:
+  I_triangulation it(traits);
+  it.insert(first,beyond);
+
+  Locate_type lt;
+  int li;
+  Face_handle fh = it.locate(p, lt, li);
+
+  if(lt == I_triangulation::VERTEX){
+    *out++ =p;
+    return std::make_pair(out,true);
+  }
+  Vertex_handle vh = it.insert(p, fh);
+  CGAL_assertion(vh->is_valid());
+  
+  //determine the furthest distance from p to a vertex of its cell
+  bool valid(false);
+  Face_circulator fc(it.incident_faces(vh)),
+    fdone(fc);
+  do
+    valid = (4*radius > traits.compute_squared_distance_2_object()
+	     (p, it.dual(fc)));
+  while(!valid && ++fc!=fdone);
+  
+  //get the neighbor points:
+  Vertex_circulator
+    vc(it.incident_vertices(vh)),
+    vdone(vc);
+  do
+    *out++= vc->point();
+  while(++vc!=vdone);
+  
+  return std::make_pair(out, valid);
+};
+
+
+//using Delaunay triangulation for candidate point filtering:
+// => no certification is necessary
+template <class Dt, class OutputIterator>
+OutputIterator 
+surface_neighbors_3(const Dt& T, 
+			       const typename Dt::Geom_traits::Point_3& p,
+			       const typename Dt::Geom_traits::Vector_3& 
+			       normal, 
+			       OutputIterator out, 
+			       typename Dt::Cell_handle start 
+			       = typename Dt::Cell_handle(NULL)){
+  
+  typedef Voronoi_intersection_2_traits_3<typename Dt::Geom_traits> I_gt;
+  return surface_neighbors_3(T, p, out, I_gt(p,normal),start);
+  
+};
+template <class Dt, class OutputIterator, class ITraits>
+OutputIterator 
+surface_neighbors_3(const Dt& T,
+		    const typename ITraits::Point_2& p,
+		    OutputIterator out, const ITraits& traits,
+		    typename Dt::Cell_handle start 
+		    = typename Dt::Cell_handle(NULL)){
+  
+  typedef typename ITraits::FT            Coord_type;
+  typedef typename ITraits::Point_2       Point_3;
+  
+  typedef typename Dt::Cell_handle       Cell_handle;
+  typedef typename Dt::Facet             Facet;
+  typedef typename Dt::Locate_type       Locate_type;
+  
+  Locate_type lt;
+  int li, lj ;
+  Cell_handle c = T.locate(p, lt, li,lj,start);
+  
+  if(lt == Dt::VERTEX){
+    *out++= (c->vertex(li))->point();
+    return out;
+  }
+  
+  typename std::vector<Facet> hole;
+  T.find_conflicts(p, c, std::back_inserter(hole), 
+		   Emptyset_iterator());
+  
+  
+  typename std::set< Point_3, 
+    typename Dt::Geom_traits::Less_xyz_3 > adjacent;
+  int idx;
+  typename std::vector<Facet>::const_iterator hit 
+    = hole.begin();
+  for(; hit!=hole.end(); hit++){
+    c = hit->first;
+    idx = hit->second;
+    for(int j=0;j<4;++j)
+      if(j!=idx)
+	if(!T.is_infinite(c->vertex(j)))
+	  adjacent.insert(c->vertex(j)->point());
+  }
+  
+  return 
+    surface_neighbors_3
+    (adjacent.begin(), adjacent.end(), p, out, traits);
+};
+CGAL_END_NAMESPACE
+
+#endif // CGAL_SURFACE_NEIGHBORS_3_H
