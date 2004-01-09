3e8da86343c8fe1bc61a016ceddb1d9567e53956
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2004-01-09 18:48:20 +0000

    added a copyright header or modified the existing one; now both INRIA and ND have the copyright

--- Packages/Segment_Voronoi_diagram_2/include/CGAL/Segment_Voronoi_diagram_storage_site_2.h
+++ Packages/Segment_Voronoi_diagram_2/include/CGAL/Segment_Voronoi_diagram_storage_site_2.h
@@ -1,26 +1,21 @@
-// ======================================================================
+// Copyright (c) 2003,2004  INRIA Sophia-Antipolis (France) and
+// Notre Dame University (U.S.A.).  All rights reserved.
 //
-// Copyright (c) 2003 The CGAL Consortium
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
 //
-// This software and related documentation is part of an INTERNAL release
-// of the Computational Geometry Algorithms Library (CGAL). It is not
-// intended for general use.
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
 //
-// ----------------------------------------------------------------------
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// release       : 
-// release_date  : 
+// $Source$
+// $Revision$ $Date$
+// $Name$
 //
-// file          : include/CGAL/Segment_Voronoi_diagram_site_2.h
-// package       : Segment_Voronoi_diagram_2
-// source        : $RCSfile$
-// revision      : $Revision$
-// revision_date : $Date$
-// author(s)     : Menelaos Karavelas <mkaravel@cse.nd.edu>
-//
-// coordinator   :
-//
-// ======================================================================
+// Author(s)     : Menelaos Karavelas <mkaravel@cse.nd.edu>
 
 
 
