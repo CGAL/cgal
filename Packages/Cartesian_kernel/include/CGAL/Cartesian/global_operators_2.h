80168c92b757b919c188615543af8a45c39a8d42
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2004-01-15 17:19:41 +0000

    - SunPro CGAL_WRAP workaround.

--- Packages/Cartesian_kernel/include/CGAL/Cartesian/global_operators_2.h
+++ Packages/Cartesian_kernel/include/CGAL/Cartesian/global_operators_2.h
@@ -87,7 +87,7 @@ operator-(const Origin &, const PointC2<K> &p)
 template < class K >
 inline
 typename K::Vector_2
-operator*(const typename K::FT &c, const VectorC2<K> &w)
+operator*(const typename CGAL_WRAP(K)::FT &c, const VectorC2<K> &w)
 {
   //return K().construct_scaled_vector_2_object()(w, c);
   return typename K::Vector_2(w.x() * c, w.y() * c);
