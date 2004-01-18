bf34e62a465960d8543aa4518379d993f170b228
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2004-01-18 12:24:07 +0000

    - Remove obsolete CGAL_CFG_MATCHING_BUG_2 workaround (VC++ <= 7.0).

--- Packages/Triangulation_3/test/Triangulation_3/include/CGAL/_test_types.h
+++ Packages/Triangulation_3/test/Triangulation_3/include/CGAL/_test_types.h
@@ -23,15 +23,6 @@
 #define Simple_cartesian Sc
 
 #include <CGAL/Simple_cartesian.h>
-
-// Workaround for buggy compilers.
-#ifdef CGAL_CFG_MATCHING_BUG_2
-#  define CGAL_IA_CT          double
-#  define CGAL_IA_ET          CGAL::MP_Float
-#  define CGAL_IA_CACHE       No_Filter_Cache
-#  define CGAL_IA_PROTECTED   true
-#endif
-
 #include <CGAL/MP_Float.h>
 #include <CGAL/Filtered_exact.h>
 
