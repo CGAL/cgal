2d5c59c10cee71521251ce1dab739a4915d25534
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2004-01-18 13:54:03 +0000

    - Cleanups following VC++ 7.0 support drop.

--- Packages/Triangulation_2/include/CGAL/Trivial_iterator.h
+++ Packages/Triangulation_2/include/CGAL/Trivial_iterator.h
@@ -33,28 +33,16 @@ CGAL_BEGIN_NAMESPACE
 class Trivial_iterator_tag{};
 
 
-#if defined CGAL_LIMITED_ITERATOR_TRAITS_SUPPORT
-  template < class I, class Ref, class Ptr, class Val, class Dist >
-#else
-  template <class I>
-#endif
+template <class I>
 class Trivial_iterator
 {
 public:
   typedef I                                                 Iterator;
-#if defined CGAL_LIMITED_ITERATOR_TRAITS_SUPPORT
-  typedef Trivial_iterator<I,Ref,Ptr,Val,Dist>              Self;
-  typedef Val                                               value_type;
-  typedef Dist                                              difference_type;
-  typedef Ref                                               reference;
-  typedef Ptr                                               pointer;
-#else
   typedef Trivial_iterator<I>                               Self;
   typedef typename std::iterator_traits<I>::value_type      value_type;
   typedef typename std::iterator_traits<I>::difference_type difference_type;
   typedef typename std::iterator_traits<I>::reference       reference;
   typedef typename std::iterator_traits<I>::pointer         pointer;
-#endif
   typedef Trivial_iterator_tag                              iterator_category;
   // Special for circulators.
   typedef I_Circulator_size_traits<iterator_category,I>     C_S_Traits;
@@ -65,16 +53,9 @@ public:
   Trivial_iterator(const I &i) : base_(i) {}
 
   // To allow conversion from iterator to const_iterator.
-#if  defined CGAL_LIMITED_ITERATOR_TRAITS_SUPPORT
-  template <class I2, class Ref2, class Ptr2, class Val2, class Dist2 >
-  Trivial_iterator(const Trivial_iterator<I2, Ref2, Ptr2, Val2, Dist2> &t)
-    : base_(t.base()) {}
-#else
   template <class Iter>
   Trivial_iterator(const Trivial_iterator<Iter> &t)
     : base_(t.base()) {}
-#endif
-
 
   reference operator*() const { return *base_;  }
   pointer operator->() const  { return &*base_; }
@@ -92,29 +73,17 @@ private:
 
 class Trivial_comparable_iterator_tag{};
 
-#if  defined CGAL_LIMITED_ITERATOR_TRAITS_SUPPORT
-  template < class I, class Ref, class Ptr, class Val, class Dist >
-#else
-  template <class I>
-#endif
+template <class I>
 class Trivial_comparable_iterator
 {
 public:
   typedef I                                            Iterator;
-#if  defined CGAL_LIMITED_ITERATOR_TRAITS_SUPPORT
-  typedef Trivial_comparable_iterator<I,Ref,Ptr,Val,Dist>  Self;
-  typedef Val                                          value_type;
-  typedef Dist                                         difference_type;
-  typedef Ref                                          reference;
-  typedef Ptr                                          pointer;
-#else
   typedef Trivial_comparable_iterator<I>               Self;
   typedef typename std::iterator_traits<I>::value_type value_type;
   typedef typename std::iterator_traits<I>::difference_type
                                                        difference_type;
   typedef typename std::iterator_traits<I>::reference  reference;
   typedef typename std::iterator_traits<I>::pointer    pointer;
-#endif
   typedef Trivial_comparable_iterator_tag              iterator_category;
   // Special for circulators.
   typedef I_Circulator_size_traits<iterator_category,I> C_S_Traits;
@@ -125,16 +94,9 @@ public:
   Trivial_comparable_iterator(const I &i) : base_(i) {}
 
   // To allow conversion from iterator to const_iterator.
-#if  defined CGAL_LIMITED_ITERATOR_TRAITS_SUPPORT
-  template <class I2, class Ref2, class Ptr2, class Val2, class Dist2>
-  Trivial_comparable_iterator(const Trivial_comparable_iterator<I2, 
-			      Ref2, Ptr2, Val2, Dist2> &t)
-    : base_(t.base()) {}
-#else
   template <class Iter>
   Trivial_comparable_iterator(const Trivial_comparable_iterator<Iter> &t)
     : base_(t.base()) {}
-#endif
 
   reference operator*() const { return *base_;  }
   pointer operator->() const  { return &*base_; }
@@ -169,26 +131,6 @@ private:
 #  define CGAL_TRIVIAL_ITERATOR_CHECKER(X)    X
 #  define CGAL_TRIVIAL_COMPARABLE_ITERATOR_CHECKER(X) X
 #endif
-
-// This macro added to workaround yet another VC++ deficiency.
-#if  defined CGAL_LIMITED_ITERATOR_TRAITS_SUPPORT && \
-    !defined CGAL_NO_CONCEPT_CHECKING
-#  define CGAL_TRIVIAL_ITERATOR_CHECKER_POINTER(X) \
-          CGAL::Trivial_iterator<X*, X&, X*, X, std::ptrdiff_t>
-#  define CGAL_TRIVIAL_COMPARABLE_ITERATOR_CHECKER_POINTER(X) \
-          CGAL::Trivial_comparable_iterator<X*, X&, X*, X, std::ptrdiff_t>
-#else
-#  define CGAL_TRIVIAL_ITERATOR_CHECKER_POINTER(X) \
-          CGAL_TRIVIAL_ITERATOR_CHECKER(X*)
-#  define CGAL_TRIVIAL_COMPARABLE_ITERATOR_CHECKER_POINTER(X) \
-          CGAL_TRIVIAL_COMPARABLE_ITERATOR_CHECKER(X*)
-#endif
-
-// For backward compatibility (can be removed soon, it's not been release)
-#define CGAL_COMPARABLE_ITERATOR_CHECKER(X) \
-        CGAL_TRIVIAL_COMPARABLE_ITERATOR_CHECKER(X)
-#define CGAL_COMPARABLE_ITERATOR_CHECKER_POINTER(X) \
-        CGAL_TRIVIAL_COMPARABLE_ITERATOR_CHECKER_POINTER(X)
 
 CGAL_END_NAMESPACE
 
