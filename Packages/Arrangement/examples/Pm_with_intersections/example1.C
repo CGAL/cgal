e4c4d66905174b4c003d1bc20103db0329989967
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2004-02-09 21:18:35 +0000

    100.23 (09 February 2004) [rursu] - Updated headers in examples

--- Packages/Arrangement/examples/Pm_with_intersections/example1.C
+++ Packages/Arrangement/examples/Pm_with_intersections/example1.C
@@ -1,3 +1,9 @@
+// Copyright (c) 1999  Tel-Aviv University (Israel).
+// All rights reserved.
+//
+// This file is part of an example program for CGAL.  This example
+// program may be used, distributed and modified without limitation.
+//
 // examples/Pm_with_intersections/example1
 // ---------------------------------------
 
