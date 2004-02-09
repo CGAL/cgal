b60e6a91e34ccd97f8d05096ff931cc9059684e0
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2004-02-09 23:14:28 +0000

    Version 2.3.3 (09 February 2004) [rursu] - Updated headers in examples

--- Packages/kdtree/examples/kdtrees/example2.C
+++ Packages/kdtree/examples/kdtrees/example2.C
@@ -1,3 +1,10 @@
+// Copyright (c) 1997  Tel-Aviv University (Israel).
+// All rights reserved.
+//
+// This file is part of an example program for CGAL.  This example
+// program may be used, distributed and modified without limitation.
+//
+
 /*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
  * example2.C -
  *    Simple example the CGAL KD-tree module.
