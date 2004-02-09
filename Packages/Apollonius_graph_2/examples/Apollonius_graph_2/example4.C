41f1be0e4dd4d2c2820cc84007db46a3e6e7c279
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2004-02-09 21:05:43 +0000

    1.68 (08 February 2004) [rursu] - updated the headers of the examples files

--- Packages/Apollonius_graph_2/examples/Apollonius_graph_2/example4.C
+++ Packages/Apollonius_graph_2/examples/Apollonius_graph_2/example4.C
@@ -1,4 +1,11 @@
+// Copyright (c) 1997  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of an example program for CGAL.  This example
+// program may be used, distributed and modified without limitation.
+//
 // file: example4.C
+
 #include <CGAL/basic.h>
 
 // standard includes
