77bf5d45dd687ffe51dd91b91e6c73f7acc70c53
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2004-02-10 08:21:08 +0000

    Version 3.31 (10 February 2004) [rursu] - Updated headers in examples

--- Packages/Min_circle_2/examples/Min_circle_2/example_Min_circle_2.C
+++ Packages/Min_circle_2/examples/Min_circle_2/example_Min_circle_2.C
@@ -1,25 +1,8 @@
-// ============================================================================
+// Copyright (c) 1997-2001  Freie Universitaet Berlin (Germany).
+// All rights reserved.
 //
-// Copyright (c) 1997-2001 The CGAL Consortium
-//
-// This software and related documentation is part of an INTERNAL release
-// of the Computational Geometry Algorithms Library (CGAL). It is not
-// intended for general use.
-//
-// ----------------------------------------------------------------------------
-//
-// release       : $CGAL_Revision: CGAL-I $
-// release_date  : $CGAL_Date$
-//
-// file          : examples/Min_circle_2/example_Min_circle_2.C
-// package       : $CGAL_Package: Min_circle_2 $
-// chapter       : Geometric Optimisation
-//
-// revision      : $Revision$
-// revision_date : $Date$
-//
-// author(s)     : Sven Schönherr <sven@inf.ethz.ch>, Bernd Gärtner
-// coordinator   : ETH Zürich (Bernd Gärtner <gaertner@inf.ethz.ch>)
+// This file is part of an example program for CGAL.  This example
+// program may be used, distributed and modified without limitation.
 //
 // example progr.: 2D Smallest Enclosing Circle
 // ============================================================================
