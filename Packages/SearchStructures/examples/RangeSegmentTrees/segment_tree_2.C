05801a02307ce9c3c3a1635f698cc7c66bcb7656
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2004-02-10 11:09:58 +0000

    10 February 2004 -Updated headers in examples

--- Packages/SearchStructures/examples/RangeSegmentTrees/segment_tree_2.C
+++ Packages/SearchStructures/examples/RangeSegmentTrees/segment_tree_2.C
@@ -1,56 +1,13 @@
-// ============================================================================
+// Copyright (c) 1997  ETH Zurich (Switzerland).
+// All rights reserved.
 //
-// Copyright (c) 1997 The CGAL Consortium
-//
-// This software and related documentation is part of the
-// Computational Geometry Algorithms Library (CGAL).
-//
-// Every use of CGAL requires a license. Licenses come in three kinds:
-//
-// - For academic research and teaching purposes, permission to use and
-//   copy the software and its documentation is hereby granted free of  
-//   charge, provided that
-//   (1) it is not a component of a commercial product, and
-//   (2) this notice appears in all copies of the software and
-//       related documentation.
-// - Development licenses grant access to the source code of the library 
-//   to develop programs. These programs may be sold to other parties as 
-//   executable code. To obtain a development license, please contact
-//   the CGAL Consortium (at cgal@cs.uu.nl).
-// - Commercialization licenses grant access to the source code and the
-//   right to sell development licenses. To obtain a commercialization 
-//   license, please contact the CGAL Consortium (at cgal@cs.uu.nl).
-//
-// This software and documentation is provided "as-is" and without
-// warranty of any kind. In no event shall the CGAL Consortium be
-// liable for any damage of any kind.
-//
-// The CGAL Consortium consists of Utrecht University (The Netherlands),
-// ETH Zurich (Switzerland), Free University of Berlin (Germany),
-// INRIA Sophia-Antipolis (France), Martin-Luther-University Halle-Wittenberg
-// (Germany) Max-Planck-Institute Saarbruecken (Germany), RISC Linz (Austria),
-// and Tel-Aviv University (Israel).
-//
-// ----------------------------------------------------------------------
-//
-// release       : CGAL-1.2
-// release_date  : 1999, January 18
-//
-// file          : src/examples/RangeSegmentTrees/segment_tree_2.C
-// source        : src/examples/RangeSegmentTrees/segment_tree_2.C
-// revision      : $Revision$
-// revision_date : $Date$/
-// author(s)     : Gabriele Neyer
-//
-// coordinator   : Peter Widmayer, ETH Zurich
-//
+// This file is part of an example program for CGAL.  This example
+// program may be used, distributed and modified without limitation.
+
 // Implementation: Testprogram for 2-dimensional Segment Trees
 // A two dimensional Segment Tree is defined in this class.
 // Ti is the type of each dimension of the tree.
-//
-// email         : cgal@cs.uu.nl
-//
-// ======================================================================
+
 #include <CGAL/basic.h>
 #include <iostream>
 #include <CGAL/Segment_tree_k.h>
