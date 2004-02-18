7257afeb720a8885d1a440e1ffd6ecb8d830be4f
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2004-02-18 20:53:15 +0000

    - Remove CGAL_CFG_NO_PARTIAL_CLASS_TEMPLATE_SPECIALISATION workarounds.

--- Packages/HalfedgeDS/include/CGAL/HalfedgeDS_face_base.h
+++ Packages/HalfedgeDS/include/CGAL/HalfedgeDS_face_base.h
@@ -30,9 +30,6 @@
 
 CGAL_BEGIN_NAMESPACE
 
-#if ! defined( CGAL_CFG_NO_PARTIAL_CLASS_TEMPLATE_SPECIALISATION) \
- && ! defined( CGAL_CFG_SUNPRO_PARTIAL_SPEC_BUG)
-
 // We use Tag_false to indicate that no plane type is provided.
 
 template < class Refs, class T = Tag_true, class Pln = Tag_false>
@@ -157,85 +154,6 @@ public:
     // No longer required.
     //Normal                normal() const { return pln.orthogonal_vector();}
 };
-
-#else //    ! defined( CGAL_CFG_NO_PARTIAL_CLASS_TEMPLATE_SPECIALISATION)
-      // && ! defined( CGAL_CFG_SUNPRO_PARTIAL_SPEC_BUG)
-
-// Partial specialization doesn't work. We can factor out the
-// Plane parameter in a base class with full specialization
-// on 'Tag_false', but we cannot get rid of the halfedge reference.
-// So, we just waste the space and have it always.
-//   Furthermore, is is likely to have a non-optimal memory
-// price-tag for the base class as well if it is the empty base
-// class for the plane type 'Tag_false', since empty structs probably
-// consume at least a byte, probably a word.
-//   See HalfedgeDS_face_min_base.h for an alternative.
-
-// We use Tag_false to indicate that no plane type is provided.
-
-template < class Pln>
-struct I_HalfedgeDS_face_base_plane {
-    typedef Tag_true Supports_plane;
-    typedef Pln      Plane;
-    // No longer required.
-    //typedef Trts                                 Traits;
-    //typedef typename Traits::Plane               Plane;
-    //typedef typename Traits::Normal              Normal;
-    Plane plane;
-    I_HalfedgeDS_face_base_plane() {}
-    I_HalfedgeDS_face_base_plane( const Plane& pl) : plane(pl) {}
-};
-template <>
-struct I_HalfedgeDS_face_base_plane<Tag_false> {
-    typedef Tag_false Supports_plane;
-    struct Plane_not_supported {};
-    typedef Plane_not_supported Plane;
-    // No longer required.
-    //struct Normal_not_supported {};
-    //typedef Normal_not_supported Normal;
-};
-
-template < class Refs, class T = Tag_true, class Pln = Tag_false>
-class HalfedgeDS_face_base : public I_HalfedgeDS_face_base_plane<Pln> {
-public:
-    typedef Refs                                 HalfedgeDS;
-    typedef HalfedgeDS_face_base< Refs, T, Pln>  Base;
-    typedef Tag_true                             Supports_face_halfedge;
-    typedef typename Refs::Vertex_handle         Vertex_handle;
-    typedef typename Refs::Vertex_const_handle   Vertex_const_handle;
-    typedef typename Refs::Halfedge_handle       Halfedge_handle;
-    typedef typename Refs::Halfedge_const_handle Halfedge_const_handle;
-    typedef typename Refs::Face_handle           Face_handle;
-    typedef typename Refs::Face_const_handle     Face_const_handle;
-    typedef typename Refs::Vertex                Vertex;
-    typedef typename Refs::Halfedge              Halfedge;
-    // Additional tags and types required by Polyhedron.
-    typedef I_HalfedgeDS_face_base_plane<Pln>    Plane_base;
-    typedef typename Plane_base::Supports_plane  Supports_face_plane;
-    typedef typename Plane_base::Plane           Plane;
-    // No longer required.
-    //typedef typename Plane_base::Supports_plane  Supports_face_normal;
-    //typedef Trts                                 Traits;
-    //typedef typename Plane_base::Normal          Normal;
-private:
-    Halfedge_handle hdg;
-public:
-    HalfedgeDS_face_base() {}
-    HalfedgeDS_face_base( const Plane& g)
-        : I_HalfedgeDS_face_base_plane<Pln>(g) {}
-    Halfedge_handle       halfedge()                      { return hdg; }
-    Halfedge_const_handle halfedge() const                { return hdg; }
-    void                  set_halfedge( Halfedge_handle h){ hdg = h; }
-    Plane&                plane()        { return Plane_base::plane; }
-    const Plane&          plane()  const { return Plane_base::plane; }
-    // No longer required.
-    //Normal                normal() const {
-    //                          return Plane_base::plane.orthogonal_vector();
-    //}
-};
-
-#endif //    ! defined( CGAL_CFG_NO_PARTIAL_CLASS_TEMPLATE_SPECIALISATION)
-       // && ! defined( CGAL_CFG_SUNPRO_PARTIAL_SPEC_BUG)
 
 CGAL_END_NAMESPACE
 
--- Packages/HalfedgeDS/include/CGAL/HalfedgeDS_vertex_base.h
+++ Packages/HalfedgeDS/include/CGAL/HalfedgeDS_vertex_base.h
@@ -30,10 +30,6 @@
 
 CGAL_BEGIN_NAMESPACE
 
-
-#if ! defined( CGAL_CFG_NO_PARTIAL_CLASS_TEMPLATE_SPECIALISATION) \
- && ! defined( CGAL_CFG_SUNPRO_PARTIAL_SPEC_BUG)
-
 // We use Tag_false to indicate that no point type is provided.
 
 template < class Refs, class T = Tag_true, class P = Tag_false>
@@ -132,70 +128,6 @@ public:
     Point&                point()                           { return p; }
     const Point&          point() const                     { return p; }
 };
-
-#else //    ! defined( CGAL_CFG_NO_PARTIAL_CLASS_TEMPLATE_SPECIALISATION)
-      // && ! defined( CGAL_CFG_SUNPRO_PARTIAL_SPEC_BUG)
-
-// Partial specialization doesn't work. We can factor out the
-// Point parameter in a base class with full specialization
-// on 'Tag_false', but we cannot get rid of the halfedge reference.
-// So, we just waste the space and have it always.
-//   Furthermore, it is likely to have a non-optimal memory
-// price-tag for the base class as well if it is the empty base
-// class for point type 'Tag_false', since empty structs probably
-// consume at least a byte, probably a word.
-//   See HalfedgeDS_face_min_base.h for an alternative.
-
-// We use Tag_false to indicate that no point type is provided.
-
-template <class Pt>
-struct I_HalfedgeDS_vertex_base_point {
-    Pt point;
-    I_HalfedgeDS_vertex_base_point() {}
-    I_HalfedgeDS_vertex_base_point( const Pt& pt) : point(pt) {}
-    typedef Tag_true Supports_point;
-    typedef Pt Point;
-};
-template <>
-struct I_HalfedgeDS_vertex_base_point<Tag_false> {
-    typedef Tag_false Supports_point;
-    struct Point_not_supported {};
-    typedef Point_not_supported Point;
-};
-
-template < class Refs, class T = Tag_true, class P = Tag_false>
-class HalfedgeDS_vertex_base : public I_HalfedgeDS_vertex_base_point<P> {
-public:
-    typedef Refs                                 HalfedgeDS;
-    typedef HalfedgeDS_vertex_base< Refs, T, P>  Base;
-    typedef T                                    Supports_vertex_halfedge;
-    typedef I_HalfedgeDS_vertex_base_point<P>    Point_base;
-    typedef typename Point_base::Supports_point  Supports_vertex_point;
-    typedef typename Point_base::Point           Point;
-    typedef typename Refs::Vertex_handle         Vertex_handle;
-    typedef typename Refs::Vertex_const_handle   Vertex_const_handle;
-    typedef typename Refs::Halfedge_handle       Halfedge_handle;
-    typedef typename Refs::Halfedge_const_handle Halfedge_const_handle;
-    typedef typename Refs::Face_handle           Face_handle;
-    typedef typename Refs::Face_const_handle     Face_const_handle;
-    typedef typename Refs::Halfedge              Halfedge;
-    typedef typename Refs::Face                  Face;
-private:
-    Halfedge_handle hdg;
-
-public:
-    HalfedgeDS_vertex_base() {}
-    HalfedgeDS_vertex_base( const Point& pp)
-        : I_HalfedgeDS_vertex_base_point<P>(pp) {}
-    Halfedge_handle       halfedge()                      { return hdg; }
-    Halfedge_const_handle halfedge() const                { return hdg; }
-    void                  set_halfedge( Halfedge_handle h){ hdg = h; }
-    Point&                point()           { return Point_base::point; }
-    const Point&          point() const     { return Point_base::point; }
-};
-
-#endif //    ! defined( CGAL_CFG_NO_PARTIAL_CLASS_TEMPLATE_SPECIALISATION)
-       // && ! defined( CGAL_CFG_SUNPRO_PARTIAL_SPEC_BUG)
 
 CGAL_END_NAMESPACE
 
