89f843ffb114597db351d0208e1f775d4c1ef43d
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2004-02-20 18:35:24 +0000

    20 02 2004 Radu Ursu - added operator << for Alpha_shape_3 in VRML_2_ostream

--- Packages/Inventor/include/CGAL/IO/VRML_2_ostream.h
+++ Packages/Inventor/include/CGAL/IO/VRML_2_ostream.h
@@ -185,6 +185,7 @@ operator<<(VRML_2_ostream& os,
   os <<      "        Group {\n"
              "            children [\n"
              "                Shape {\n"
+             "                    appearance USE A1\n"
              "                    geometry\n"
              "                        PointSet {\n"
              "                            coord Coordinate {\n"
@@ -221,6 +222,7 @@ operator<<(VRML_2_ostream& os,
   os <<      "        Group {\n"
              "            children [\n"
              "                Shape {\n"
+             "                    appearance USE A1\n"
              "                    geometry\n"
              "                        IndexedLineSet {\n"
              "                            coord Coordinate {\n"
@@ -263,6 +265,7 @@ operator<<(VRML_2_ostream& os,
   os <<      "        Group {\n"
              "            children [\n"
              "                Shape {\n"
+             "                    appearance USE A1\n"
              "                    geometry\n"
              "                        IndexedLineSet {\n"
              "                            coord Coordinate {\n"
@@ -324,3 +327,82 @@ CGAL_END_NAMESPACE
 
 #endif // CGAL_IO_VRML_2_SEGMENT_3
 #endif // CGAL_SEGMENT_3_H
+
+
+#ifdef CGAL_ALPHA_SHAPE_3_H
+#ifndef CGAL_IO_VRML_2_ALPHA_SHAPE_3
+#define CGAL_IO_VRML_2_ALPHA_SHAPE_3
+
+CGAL_BEGIN_NAMESPACE
+
+template <class Dt >
+VRML_2_ostream&
+operator<<(VRML_2_ostream& os,
+           Alpha_shape_3<Dt> &as)
+{
+  // Finite vertices coordinates.
+  Alpha_shape_3<Dt>::Alpha_shape_vertices_iterator Vlist_it,
+    Vlist_begin = as.alpha_shape_vertices_begin(),
+    Vlist_end = as.alpha_shape_vertices_end();
+
+  std::map<Alpha_shape_3<Dt>::Vertex_handle, int> V;
+  int number_of_vertex = 0;
+  for( Vlist_it = Vlist_begin; Vlist_it != Vlist_end; Vlist_it++) {
+    V[*Vlist_it] = number_of_vertex++;
+  }
+
+  typename Alpha_shape_3<Dt>::Alpha_shape_facets_iterator Flist_it,
+    Flist_begin = as.alpha_shape_facets_begin(),
+    Flist_end = as.alpha_shape_facets_end();
+
+  std::map<Alpha_shape_3<Dt>::Facet, int> F;
+  int number_of_facets = 0;
+  for( Flist_it = Flist_begin; Flist_it != Flist_end; Flist_it++) {
+    F[*Flist_it] = number_of_facets++;
+  }
+
+  const char *Indent = "                                    ";
+  os <<      "        Group {\n"
+             "            children [\n"
+             "                Shape {\n"
+             "                    appearance USE A1\n"
+             "                    geometry\n"
+             "                        IndexedFaceSet {\n"
+             "                            coord Coordinate {\n"
+             "                                point [ \n"
+     <<      Indent << "  ";
+  for( Vlist_it = Vlist_begin; Vlist_it != Vlist_end; Vlist_it++) {
+    os << CGAL::to_double((*Vlist_it)->point().x()) << " ";
+    os << CGAL::to_double((*Vlist_it)->point().y()) << " ";
+    os << CGAL::to_double((*Vlist_it)->point().z()) << ",\n" << Indent << "  ";
+  }
+    os <<    "\n                                ]\n"
+             "                            } # coord\n"
+             "                            solid   FALSE\n"
+     <<      Indent << "coordIndex  [\n";
+  // Finite facets indices.
+  for( Flist_it = Flist_begin; Flist_it != Flist_end; Flist_it++){
+    os << Indent << "  ";
+      for (int i=0; i<4; i++)
+        if (i != (*Flist_it).second){
+	        os << V[(*Flist_it).first->vertex(i)];
+          os << ", ";
+        }
+    if (Flist_it != Flist_end)
+      os << "-1,\n";
+    else
+      os << "-1 \n";
+  }
+  os <<      Indent << "]\n";
+             "                        } #IndexedFaceSet\n"
+             "                } #Shape\n"
+             "            ] #children\n"
+             "        } #Group\n";
+
+  return os;
+}
+
+CGAL_END_NAMESPACE
+
+#endif // CGAL_IO_VRML_2_ALPHA_SHAPE_3
+#endif // CGAL_ALPHA_SHAPE_3_H
