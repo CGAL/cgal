7c42b7ac5d5a4922898fa79044aa6dfa6697a9d3
Efi Fogel <efif@post.tau.ac.il>
2004-03-11 17:08:16 +0000

    changed cached to non-cached

--- Packages/Arrangement/test/Arrangement_2_Traits/Arr_polyline_traits_test.C
+++ Packages/Arrangement/test/Arrangement_2_Traits/Arr_polyline_traits_test.C
@@ -1,15 +1,15 @@
 #include <CGAL/Cartesian.h>
 #include <CGAL/Quotient.h>
 #include <CGAL/MP_Float.h>
-#include <CGAL/Arr_segment_cached_traits_2.h>
+#include <CGAL/Arr_segment_traits_2.h>
 #include <CGAL/Arr_polyline_traits_2.h>
 
 #include "include/Polyline_traits_test.h"
 
-typedef CGAL::Quotient<CGAL::MP_Float>             NT;
-typedef CGAL::Cartesian<NT>                        Kernel;
-typedef CGAL::Arr_segment_cached_traits_2<Kernel>  Seg_traits;
-typedef CGAL::Arr_polyline_traits_2<Seg_traits>    Traits;
+typedef CGAL::Quotient<CGAL::MP_Float>          NT;
+typedef CGAL::Cartesian<NT>                     Kernel;
+typedef CGAL::Arr_segment_traits_2<Kernel>      Seg_traits;
+typedef CGAL::Arr_polyline_traits_2<Seg_traits> Traits;
 
 int main( int argc, char** argv )
 {
