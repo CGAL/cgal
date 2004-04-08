1c8edfb025f3badf2f0f247fd86cf8235587869c
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2004-04-08 10:40:10 +0000

    Added lisence headers.

--- Packages/Visibility_complex/include/CEP/Visibility_complex/predicates/Circle_2_Circle_2_intersection_ftC2.h
+++ Packages/Visibility_complex/include/CEP/Visibility_complex/predicates/Circle_2_Circle_2_intersection_ftC2.h
@@ -1,3 +1,22 @@
+// Copyright (c) 2001-2004  ENS of Paris (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Pierre Angelier, Michel Pocchiola
+
 #ifndef CIRCLE_2_CIRCLE_2_INTERSECTION_FTC2_H
 #define CIRCLE_2_CIRCLE_2_INTERSECTION_FTC2_H
 
