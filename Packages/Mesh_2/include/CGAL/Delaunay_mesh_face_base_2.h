3f6946d3ddfb105d3eb5328cd55e51a74ca85d23
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2004-04-08 10:27:16 +0000

    Added headers

--- Packages/Mesh_2/include/CGAL/Delaunay_mesh_face_base_2.h
+++ Packages/Mesh_2/include/CGAL/Delaunay_mesh_face_base_2.h
@@ -1,3 +1,22 @@
+// Copyright (c) 2001-2004  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Laurent RINEAU
+
 #ifndef CGAL_DELAUNAY_FACE_BASE_2_H
 #define CGAL_DELAUNAY_FACE_BASE_2_H
 
