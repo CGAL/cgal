4ac3cfb2f75d706dc622b294bab85b9b769b8480
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2004-04-17 22:51:45 +0000

    - Added CGAL_CFG_DENORMALS_COMPILE_BUG.C for PGCC.

---
+++ Packages/Configuration/config/testfiles/CGAL_CFG_DENORMALS_COMPILE_BUG.C
@@ -0,0 +1,41 @@
+// Copyright (c) 2004  Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland), Freie Universitaet Berlin (Germany),
+// INRIA Sophia-Antipolis (France), Martin-Luther-University Halle-Wittenberg
+// (Germany), Max-Planck-Institute Saarbruecken (Germany), RISC Linz (Austria),
+// and Tel-Aviv University (Israel).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Sylvain Pion
+
+// ---------------------------------------------------------------------
+// A short test program to evaluate a C++ compiler.
+// This program is used by cgal_configure.
+// The following documentation will be pasted in the generated configfile.
+// ---------------------------------------------------------------------
+
+//| This flag is set if the compiler bugs when handling denormal values at
+//| compile time.  At least PGCC 5.1-3 has the bug.
+
+template < typename T >
+void use(const T&) {}
+ 
+int main()
+{
+  double d = 5e-324;
+  use(d);
+  return 0;
+}
