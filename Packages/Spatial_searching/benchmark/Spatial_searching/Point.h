0fce4480bdab3cb92a081130ab5bfd3ec7f7e670
Hans Tangelder <hanst@cs.uu.nl>
2004-04-23 12:41:11 +0000

    *** empty log message ***

---
+++ Packages/Spatial_searching/benchmark/Spatial_searching/Point.h
@@ -0,0 +1,41 @@
+struct Point {
+  double vec[2];
+
+  Point() { vec[0]= vec[1] = 0; }
+  Point (double x, double y) { vec[0]=x; vec[1]=y; }
+ 
+  double x() const { return vec[ 0 ]; }
+  double y() const { return vec[ 1 ]; }
+
+  double& x() { return vec[ 0 ]; }
+  double& y() { return vec[ 1 ]; }
+  
+  bool operator==(const Point& p) const 
+  {
+    return (x() == p.x()) && (y() == p.y())  ;
+  }
+
+  bool  operator!=(const Point& p) const { return ! (*this == p); }
+}; //end of class
+
+
+
+namespace CGAL {
+
+  template <>
+  struct Kernel_traits<Point> {
+    struct Kernel {
+      typedef double FT;
+      typedef double RT;
+    };
+  };
+}
+
+
+struct Construct_coord_iterator {
+  const double* operator()(const Point& p) 
+  { return static_cast<const double*>(p.vec); }
+
+  const double* operator()(const Point& p, int) 
+  { return static_cast<const double*>(p.vec+2); }
+};
