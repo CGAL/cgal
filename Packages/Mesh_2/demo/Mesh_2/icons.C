ae8fd89c0dea2aa6db4650750ce3fa79a1593889
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2004-05-04 13:49:12 +0000

    Remove an old comment. Add #ifdef CGAL_USE_QT.

--- Packages/Mesh_2/demo/Mesh_2/icons.C
+++ Packages/Mesh_2/demo/Mesh_2/icons.C
@@ -1,4 +1,5 @@
-// bad hack to remove static storage specifier for xpm
+#ifdef CGAL_USE_QT
+
 #include <CGAL/IO/pixmaps/polygon.xpm>
 #include <CGAL/IO/pixmaps/point.xpm>
 #include <CGAL/IO/pixmaps/points.xpm>
@@ -7,3 +8,5 @@
 #include "seeds.xpm"
 #include <CGAL/IO/pixmaps/circle.xpm>
 #include <CGAL/IO/pixmaps/triangulation.xpm>
+
+#endif 
