3358b50071546ddc6194175c96d2226e298c679e
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2004-05-04 13:55:08 +0000

    Added const in pixmaps declaration.

--- Packages/Mesh_2/demo/Mesh_2/icons.h
+++ Packages/Mesh_2/demo/Mesh_2/icons.h
@@ -1,8 +1,8 @@
-extern char *polygon_xpm[],
-            *point_xpm[],
-            *points_xpm[],
-            *contraints_xpm[],
-            *marked_xpm[],
-            *seeds_xpm[],
-            *circle_xpm[],
-            *triangulation_xpm[];
+extern const char *polygon_xpm[],
+                  *point_xpm[],
+                  *points_xpm[],
+                  *contraints_xpm[],
+                  *marked_xpm[],
+                  *seeds_xpm[],
+                  *circle_xpm[],
+                  *triangulation_xpm[];
