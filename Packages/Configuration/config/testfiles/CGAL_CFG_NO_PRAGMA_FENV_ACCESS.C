c862f096175058ca1302440676468a52604a8937
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2004-05-09 12:40:53 +0000

    - Add CGAL_CFG_NO_PRAGMA_FENV_ACCESS.C

---
+++ Packages/Configuration/config/testfiles/CGAL_CFG_NO_PRAGMA_FENV_ACCESS.C
@@ -0,0 +1,47 @@
+// Copyright (c) 2004  Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland), Freie Universitaet Berlin (Germany),
+// INRIA Sophia-Antipolis (France), Martin-Luther-University Halle-Wittenberg
+// (Germany), Max-Planck-Institute Saarbruecken (Germany), RISC Linz (Austria),
+// and Tel-Aviv University (Israel).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Sylvain Pion
+
+// ---------------------------------------------------------------------
+// A short test program to evaluate a C++ compiler.
+// This program is used by cgal_configure.
+// The following documentation will be pasted in the generated configfile.
+// ---------------------------------------------------------------------
+
+//| This flag is set if the compiler doesn't support the FENV_ACCESS pragma.
+//| This pragma is supported by the ISO C99 standard.
+//| Note : GCC (<= 3.4 at least) only warns that it does not support it.
+
+#include <fenv.h>
+
+double fadd(double a, double b)
+{
+#pragma STDC FENV_ACCESS ON
+  return (a+b)-b;
+}
+
+int main() {
+  double a = fadd(1.2, 2.3);
+  if (a == 1.2)
+    return -1;
+  return 0;
+}
