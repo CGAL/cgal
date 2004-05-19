2a48c44f9e04c3399d1f60be6c1a0aefbef22650
Efi Fogel <efif@post.tau.ac.il>
2004-05-19 17:42:37 +0000

    fixed copyright year

--- Packages/Snap_rounding_2/include/CGAL/Snap_rounding_traits_2.h
+++ Packages/Snap_rounding_2/include/CGAL/Snap_rounding_traits_2.h
@@ -1,4 +1,4 @@
-// Copyright (c) 1997  Tel-Aviv University (Israel).
+// Copyright (c) 2001  Tel-Aviv University (Israel).
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org); you may redistribute it under
