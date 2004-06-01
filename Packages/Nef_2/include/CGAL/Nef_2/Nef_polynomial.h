51e9314423dff1ee50386908abe418c390788a28
Radu Ursu <Radu.Ursu@sophia.inria.fr>
2004-06-01 08:46:25 +0000

    01 June 2004 Radu Ursu - updated QPL header in Nef_polynomial.h

--- Packages/Nef_2/include/CGAL/Nef_2/Nef_polynomial.h
+++ Packages/Nef_2/include/CGAL/Nef_2/Nef_polynomial.h
@@ -1,29 +1,21 @@
-// ======================================================================
+// Copyright (c) 1997-2000  Max-Planck-Institute Saarbruecken (Germany).
+// All rights reserved.
 //
-// Copyright (c) 1997-2000 The CGAL Consortium
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
 //
-// This software and related documentation is part of an INTERNAL release
-// of the Computational Geometry Algorithms Library (CGAL). It is not
-// intended for general use.
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
 //
-// ----------------------------------------------------------------------
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// release       : $CGAL_Revision: CGAL-2.4-I-78 $
-// release_date  : $CGAL_Date: 2002/04/17 $
+// $Source$
+// $Revision$ $Date$
+// $Name$
 //
-// file          : include/CGAL/Nef_2/Nef_polynomial.h
-// package       : Nef_2
-// maintainer    : Peter Hachenberger <hachenberger@mpi-sb.mpg.de>
-// chapter       : Nef Polyhedra
-//
-// revision      : $Revision$
-// revision_date : $Date$
-//
-// author(s)     : Michael Seel <seel@mpi-sb.mpg.de>
-// coordinator   : Michael Seel <seel@mpi-sb.mpg.de>
-//
-// implementation: Polynomials in one variable
-// ======================================================================
+// Author(s)     : Michael Seel <seel@mpi-sb.mpg.de>
 
 #ifndef CGAL_NEF_POLYNOMIAL_H
 #define CGAL_NEF_POLYNOMIAL_H
