0c99f7abf6f590b0f846355df14772897826b3a8
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2004-07-13 21:15:28 +0000

    Changed indentation to Emacs style

--- Packages/Spatial_searching/include/CGAL/Kd_tree_rectangle.h
+++ Packages/Spatial_searching/include/CGAL/Kd_tree_rectangle.h
@@ -31,50 +31,62 @@ namespace CGAL {
     int dim;
     T *lower;
     T *upper;
-    set_bounds_from_pointer(int d, T *l, T *u) :
-	dim(d), lower(l), upper(u) {}
-    void operator() (P p) {
-		T h;
-		typename SearchTraits::Construct_cartesian_const_iterator_d construct_it;
-                typename SearchTraits::Cartesian_const_iterator_d pit = construct_it(*p);
-		for (int i = 0; i < dim; ++i, ++pit) {
-			h=(*pit);
-			if (h < lower[i]) lower[i] = h;
-			if (h > upper[i]) upper[i] = h;
-		}
+    
+    set_bounds_from_pointer(int d, T *l, T *u) 
+      : dim(d), lower(l), upper(u) 
+    {}
+
+    void 
+    operator()(P p) 
+    {
+      T h;
+      typename SearchTraits::Construct_cartesian_const_iterator_d construct_it;
+      typename SearchTraits::Cartesian_const_iterator_d pit = construct_it(*p);
+      for (int i = 0; i < dim; ++i, ++pit) {
+	h=(*pit);
+	if (h < lower[i]) lower[i] = h;
+	if (h > upper[i]) upper[i] = h;
+      }
     }
   };
 
 
-  template <class SearchTraits> class Kd_tree_rectangle {
+  template <class SearchTraits> 
+  class Kd_tree_rectangle {
   public:
     typedef typename SearchTraits::FT FT;
     typedef FT T;
-
+    
   private:
-
+    
     int dim;
     T *lower_;
     T *upper_;
     int max_span_coord_;
-
+    
   public:
-
-    inline void set_upper_bound(int i, const FT& x) {
+    
+    inline void 
+    set_upper_bound(int i, const FT& x) 
+    {
       assert(i >= 0 && i < dim);
       assert(x >= lower_[i]);
       upper_[i] = x;
       set_max_span();
     }
 
-    inline void set_lower_bound(int i, const FT& x) {
+    inline void 
+    set_lower_bound(int i, const FT& x) 
+    {
       assert(i >= 0 && i < dim);
       assert(x <= upper_[i]);
       lower_[i] = x;
       set_max_span();
     }
 
-    inline void set_max_span() {
+    inline void 
+    set_max_span() 
+    {
       FT span = upper_[0]-lower_[0];
       max_span_coord_ = 0;
       for (int i = 1; i < dim; ++i) {
@@ -86,23 +98,27 @@ namespace CGAL {
       }
     }
     
-    Kd_tree_rectangle(int d) : 
-      dim(d), lower_(new FT[d]), upper_(new FT[d]), max_span_coord_(0)
+    Kd_tree_rectangle(int d) 
+      : dim(d), lower_(new FT[d]), upper_(new FT[d]), max_span_coord_(0)
     {
       std::fill(lower_, lower_ + dim, 0);
       std::fill(upper_, upper_ + dim, 0);
-     
     }
 
-    Kd_tree_rectangle() : dim(0), lower_(0), upper_(0) {}
-
+    Kd_tree_rectangle() 
+      : dim(0), lower_(0), upper_(0) 
+    {}
     
-    explicit Kd_tree_rectangle(const Kd_tree_rectangle<SearchTraits>& r) : dim(r.dim),
-      lower_(new FT[dim]), upper_(new FT[dim]), max_span_coord_(r.max_span_coord_) {
-        std::copy(r.lower_, r.lower_+dim, lower_);
-	std::copy(r.upper_, r.upper_+dim, upper_);
+    
+    explicit 
+    Kd_tree_rectangle(const Kd_tree_rectangle<SearchTraits>& r) 
+      : dim(r.dim), lower_(new FT[dim]), upper_(new FT[dim]), 
+	max_span_coord_(r.max_span_coord_) 
+    {
+      std::copy(r.lower_, r.lower_+dim, lower_);
+      std::copy(r.upper_, r.upper_+dim, upper_);
     }
-
+    
     template <class PointPointerIter> // was PointIter
     Kd_tree_rectangle(int d,  PointPointerIter begin,  PointPointerIter end)
       : dim(d), lower_(new FT[d]), upper_(new FT[d]) 
@@ -110,12 +126,11 @@ namespace CGAL {
       // initialize with values of first point
       typename SearchTraits::Construct_cartesian_const_iterator_d construct_it;
       typename SearchTraits::Cartesian_const_iterator_d bit = construct_it(**begin);
-
-	  for (int i=0; i < dim; ++bit, ++i)
-	  {
-	    lower_[i]=(*bit); upper_[i]=lower_[i];
-	  }
-	  begin++;
+      
+      for (int i=0; i < dim; ++bit, ++i){
+	lower_[i]=(*bit); upper_[i]=lower_[i];
+      }
+      begin++;
       typedef typename std::iterator_traits<PointPointerIter>::value_type P;
       std::for_each(begin, end, set_bounds_from_pointer<SearchTraits, P,T>(dim, lower_, upper_));
       set_max_span();
@@ -123,7 +138,8 @@ namespace CGAL {
 
     template <class PointPointerIter>
     void update_from_point_pointers(PointPointerIter begin, 
-                                    PointPointerIter end) {
+                                    PointPointerIter end) 
+    {
       if (begin ==end) { // no points
 	for (int i=0; i < dim; ++i) {
 	  lower_[i]= FT(1); upper_[i]= FT(-1);
@@ -132,7 +148,7 @@ namespace CGAL {
 	// initialize with values of first point
 	typename SearchTraits::Construct_cartesian_const_iterator_d construct_it;
 	typename SearchTraits::Cartesian_const_iterator_d bit = construct_it(**begin);
-
+	
 	for (int i=0; i < dim; ++i, ++bit) {
 	  lower_[i]= *bit; upper_[i]=lower_[i];
 	}
@@ -143,65 +159,84 @@ namespace CGAL {
       }
       set_max_span();
     }
+    
+    inline int 
+    max_span_coord() const 
+    { 
+      return max_span_coord_; 
+    }
 
-    inline int max_span_coord() const { return max_span_coord_; }
-
-    inline FT max_span() const {
+    inline FT 
+    max_span() const 
+    {
       return upper_[max_span_coord_] - lower_[max_span_coord_];
     }
 
-    inline FT min_coord(int i) const {
+    inline FT 
+    min_coord(int i) const 
+    {
       return lower_[i];
     }
-
-    inline FT max_coord(int i) const {
+    
+    inline FT 
+    max_coord(int i) const 
+    {
       return upper_[i];
     }
-
-    std::ostream& print(std::ostream& s) const {
+    
+    std::ostream& 
+    print(std::ostream& s) const 
+    {
       s << "Rectangle dimension = " << dim;
       s << "\n lower: ";
       for (int i=0; i < dim; ++i)
-      s << lower_[i] << " ";
+	s << lower_[i] << " ";
       // std::copy(lower_, lower_ + dim,
       // 	      std::ostream_iterator<FT>(s," "));
       s << "\n upper: ";
       for (int j=0; j < dim; ++j)
-      s << upper_[j] << " ";
+	s << upper_[j] << " ";
       // std::copy(upper_, upper_ + dim,
       //	      std::ostream_iterator<FT>(s," "));
       s << "\n maximum span " << max_span() <<
-      " at coordinate " << max_span_coord() << std::endl;
+	" at coordinate " << max_span_coord() << std::endl;
       return s;
     }
-
+    
     // Splits rectangle by modifying itself to lower half 
     // and returns upper half
     //    Kd_tree_rectangle* 
-void
-split(Kd_tree_rectangle& r, int d, FT value) {
-		// assert(d >= 0 && d < dim);
-		// assert(lower_[d] <= value && value <= upper_[d]);
-                
-  //Kd_tree_rectangle* r = new Kd_tree_rectangle(*this);
-		upper_[d]=value;
-                r.lower_[d]=value;
-		//return r;
+    void
+    split(Kd_tree_rectangle& r, int d, FT value)
+    {
+      // assert(d >= 0 && d < dim);
+      // assert(lower_[d] <= value && value <= upper_[d]);
+      
+      //Kd_tree_rectangle* r = new Kd_tree_rectangle(*this);
+      upper_[d]=value;
+      r.lower_[d]=value;
+      //return r;
     }
-                      
-
-    ~Kd_tree_rectangle() {
+    
+    
+    ~Kd_tree_rectangle() 
+    {
       if (dim) {
 	if (lower_) delete [] lower_;
 	if (upper_) delete [] upper_;
       }
     }
 
-    int dimension() const {return dim;}
+    int 
+    dimension() const 
+    {
+      return dim;
+    }
 
  
-    Kd_tree_rectangle<SearchTraits>& operator= (const Kd_tree_rectangle<SearchTraits>& r) {
-      
+    Kd_tree_rectangle<SearchTraits>& 
+    operator=(const Kd_tree_rectangle<SearchTraits>& r) 
+    {
       if (this != &r) {
         std::copy(r.lower_, r.lower_+dim, lower_);
 	std::copy(r.upper_, r.upper_+dim, upper_);
@@ -212,10 +247,12 @@ split(Kd_tree_rectangle& r, int d, FT value) {
 
   
 
-}; // of class Kd_tree_rectangle
+  }; // of class Kd_tree_rectangle
 
   template <class SearchTraits>
-    std::ostream& operator<< (std::ostream& s, const Kd_tree_rectangle<SearchTraits>& r) {
+  std::ostream& 
+  operator<<(std::ostream& s, const Kd_tree_rectangle<SearchTraits>& r) 
+  {
     return r.print(s);
   }
 
