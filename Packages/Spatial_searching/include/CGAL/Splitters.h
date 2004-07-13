6b9aa0bec7e0e542a35640869a5b1194e7f05a84
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2004-07-13 21:24:09 +0000

    Changed indentation as Emacs does it

--- Packages/Spatial_searching/include/CGAL/Splitters.h
+++ Packages/Spatial_searching/include/CGAL/Splitters.h
@@ -25,6 +25,7 @@
 #define CGAL_SPLITTERS_H
 #include <CGAL/Point_container.h>
 #include <CGAL/Plane_separator.h>
+
 namespace CGAL {
 
   template <class FT>
@@ -32,21 +33,23 @@ namespace CGAL {
   private:
     unsigned int the_bucket_size;
     FT the_aspect_ratio;
-
-public:
+    
+  public:
     Splitter_base(unsigned int bucket_size = 3, 
 		  FT aspect_ratio = FT(3))
       : the_bucket_size(bucket_size),
 	the_aspect_ratio(aspect_ratio)
     {}
-
+    
     FT 
-    aspect_ratio() const {
+    aspect_ratio() const 
+    {
       return the_aspect_ratio;
     }
     
     unsigned int 
-    bucket_size() const {
+    bucket_size() const 
+    {
       return the_bucket_size;
     }
 
@@ -54,228 +57,237 @@ public:
 
 
   template <class SearchTraits,
-          class Separator_=Plane_separator<typename SearchTraits::FT>  >
-class Median_of_max_spread 
-  : public Splitter_base<typename SearchTraits::FT> 
-{
+            class Separator_= Plane_separator<typename SearchTraits::FT>  >
+  class Median_of_max_spread 
+    : public Splitter_base<typename SearchTraits::FT> 
+  {
 
-  typedef Splitter_base<typename SearchTraits::FT> Base;
-public:
-  typedef typename SearchTraits::FT FT;
-  typedef Point_container<SearchTraits> Container;
-  typedef Separator_ Separator;
- 
-  Median_of_max_spread()
-    : Base()
-  {}
-
-  Median_of_max_spread(unsigned int bucket_size)
-    : Base(bucket_size)
-  {}
-
-  void operator() (Separator& sep, 
-		   Container& c0,
-		   Container& c1) {        
-        sep=Separator(c0.max_tight_span_coord(),FT(0));
-        sep.set_cutting_value(c0.median(sep.cutting_dimension()));
-        c0.split(c1,sep,true);
-  }
-};
-
-template <class SearchTraits,
-	  class Separator_=Plane_separator<typename SearchTraits::FT>  > 
-class Fair
-  : public Splitter_base<typename SearchTraits::FT> {
-
-  typedef Splitter_base<typename SearchTraits::FT> Base;
-public:
-  typedef typename SearchTraits::FT FT;
-  typedef Point_container<SearchTraits> Container;
-  typedef Separator_ Separator;
-
-  Fair()
-    : Base()
-  {}
-
-  Fair(unsigned int bucket_size, 
-       FT aspect_ratio=FT(3))
-    : Base(bucket_size, aspect_ratio)
-  {}
-
-  void operator() (Separator& sep, Container& c0,
-  			     Container& c1) {
-	// find legal cut with max spread
-        sep=Separator(c0.max_tight_span_coord_balanced(aspect_ratio()),
-				FT(0));
-        sep.set_cutting_value(c0.balanced_fair(sep.cutting_dimension(),
-				aspect_ratio()));
-        c0.split(c1,sep);
-  }
-};
-
-template <class SearchTraits,
-	  class Separator_=Plane_separator<typename SearchTraits::FT>  >
-class Sliding_fair
-  : public Splitter_base<typename SearchTraits::FT> {
-
-  typedef Splitter_base<typename SearchTraits::FT> Base;
-
-public:
-  typedef typename SearchTraits::FT FT;
-  typedef Point_container<SearchTraits> Container;
-  typedef Separator_ Separator;
-
-  Sliding_fair()
-    : Base()
-  {}
-
-  Sliding_fair(unsigned int bucket_size, 
-	       FT aspect_ratio=FT(3))
-    : Base(bucket_size, aspect_ratio)
-  {}
-
-  void operator() (Separator& sep, Container& c0,
-  			     Container& c1)  {
-    // find legal cut with max spread
-   
-    sep=Separator(c0.max_tight_span_coord_balanced(aspect_ratio()),
-			    FT(0));
+    typedef Splitter_base<typename SearchTraits::FT> Base;
+  public:
+    typedef typename SearchTraits::FT FT;
+    typedef Point_container<SearchTraits> Container;
+    typedef Separator_ Separator;
     
-    sep.set_cutting_value(c0.balanced_sliding_fair(sep.cutting_dimension(),
-			 aspect_ratio()));
-    c0.split(c1,sep,true);
-  }
-};
+    Median_of_max_spread()
+      : Base()
+    {}
 
+    Median_of_max_spread(unsigned int bucket_size)
+      : Base(bucket_size)
+    {}
+    
+    void 
+    operator() (Separator& sep, 
+		Container& c0,
+		Container& c1) 
+    {        
+      sep=Separator(c0.max_tight_span_coord(),FT(0));
+      sep.set_cutting_value(c0.median(sep.cutting_dimension()));
+      c0.split(c1,sep,true);
+    }
+  };
 
-template <class SearchTraits,
-	  class Separator_=Plane_separator<typename SearchTraits::FT>  >
-class Sliding_midpoint
-  : public Splitter_base<typename SearchTraits::FT> {
-
-  typedef Splitter_base<typename SearchTraits::FT> Base;
-
-public:
-  typedef typename SearchTraits::FT FT;
-  typedef Point_container<SearchTraits> Container;
-  typedef Separator_ Separator;
-
-  Sliding_midpoint()
-    : Base()
-  {}
-
-  Sliding_midpoint(unsigned int bucket_size)
-    : Base(bucket_size)
-  {}
-
-  void operator() (Separator& sep, Container& c0,
-  			     Container& c1)
+  template <class SearchTraits,
+	    class Separator_= Plane_separator<typename SearchTraits::FT>  > 
+  class Fair
+    : public Splitter_base<typename SearchTraits::FT> 
   {
-        sep=Separator(c0.max_span_coord(),
-              (c0.max_span_upper() + c0.max_span_lower())/FT(2));
-	FT max_span_lower = 
+
+    typedef Splitter_base<typename SearchTraits::FT> Base;
+  public:
+    typedef typename SearchTraits::FT FT;
+    typedef Point_container<SearchTraits> Container;
+    typedef Separator_ Separator;
+    
+    Fair()
+      : Base()
+    {}
+
+    Fair(unsigned int bucket_size, 
+	 FT aspect_ratio=FT(3))
+      : Base(bucket_size, aspect_ratio)
+    {}
+
+    void 
+    operator()(Separator& sep, Container& c0, Container& c1) 
+    {
+      // find legal cut with max spread
+      sep=Separator(c0.max_tight_span_coord_balanced(aspect_ratio()),
+		    FT(0));
+      sep.set_cutting_value(c0.balanced_fair(sep.cutting_dimension(),
+					     aspect_ratio()));
+      c0.split(c1,sep);
+    }
+  };
+
+  template <class SearchTraits,
+           class Separator_= Plane_separator<typename SearchTraits::FT>  >
+  class Sliding_fair
+    : public Splitter_base<typename SearchTraits::FT> 
+  {
+    
+    typedef Splitter_base<typename SearchTraits::FT> Base;
+    
+  public:
+    typedef typename SearchTraits::FT FT;
+    typedef Point_container<SearchTraits> Container;
+    typedef Separator_ Separator;
+    
+    Sliding_fair()
+      : Base()
+    {}
+    
+    Sliding_fair(unsigned int bucket_size, 
+		 FT aspect_ratio=FT(3))
+      : Base(bucket_size, aspect_ratio)
+    {}
+    
+    void 
+    operator() (Separator& sep, Container& c0, Container& c1)  
+    {
+      // find legal cut with max spread
+      
+      sep = Separator(c0.max_tight_span_coord_balanced(aspect_ratio()),
+		      FT(0));
+      
+      sep.set_cutting_value(c0.balanced_sliding_fair(sep.cutting_dimension(),
+						     aspect_ratio()));
+      c0.split(c1,sep,true);
+    }
+  };
+
+
+  template <class SearchTraits,
+	    class Separator_= Plane_separator<typename SearchTraits::FT>  >
+  class Sliding_midpoint
+    : public Splitter_base<typename SearchTraits::FT> 
+  {
+    
+    typedef Splitter_base<typename SearchTraits::FT> Base;
+    
+  public:
+    typedef typename SearchTraits::FT FT;
+    typedef Point_container<SearchTraits> Container;
+    typedef Separator_ Separator;
+    
+    Sliding_midpoint()
+      : Base()
+    {}
+    
+    Sliding_midpoint(unsigned int bucket_size)
+      : Base(bucket_size)
+    {}
+
+    void 
+    operator()(Separator& sep, Container& c0, Container& c1)
+    {
+      sep = Separator(c0.max_span_coord(),
+		      (c0.max_span_upper() + c0.max_span_lower())/FT(2));
+      FT max_span_lower = 
 	c0.tight_bounding_box().min_coord(c0.max_span_coord());
-	FT max_span_upper = 
+      FT max_span_upper = 
 	c0.tight_bounding_box().max_coord(c0.max_span_coord());
-	if (max_span_upper <= sep.cutting_value()) {
-		sep.set_cutting_value(max_span_upper); 
-	};
-	if (max_span_lower >= sep.cutting_value()) {
-		sep.set_cutting_value(max_span_lower); 
-	};
-	c0.split(c1,sep,true);
-  }
-};
-
-template <class SearchTraits,
-	  class Separator_=Plane_separator<typename SearchTraits::FT>  >
-class Median_of_rectangle
-  : public Splitter_base<typename SearchTraits::FT> {
-
-  typedef Splitter_base<typename SearchTraits::FT> Base;
-
-public:
-  typedef typename SearchTraits::FT FT;
-  typedef Point_container<SearchTraits> Container;
-  typedef Separator_ Separator;
-
-
-  Median_of_rectangle()
-    : Base()
-  {}
-
-  Median_of_rectangle(unsigned int bucket_size)
-    : Base(bucket_size)
-  {}
-
-  void operator() (Separator& sep, Container& c0,
-  			     Container& c1)
+      if (max_span_upper <= sep.cutting_value()) {
+	sep.set_cutting_value(max_span_upper); 
+      };
+      if (max_span_lower >= sep.cutting_value()) {
+	sep.set_cutting_value(max_span_lower); 
+      };
+      c0.split(c1,sep,true);
+    }
+  };
+  
+  template <class SearchTraits,
+	    class Separator_= Plane_separator<typename SearchTraits::FT>  >
+  class Median_of_rectangle
+    : public Splitter_base<typename SearchTraits::FT> 
   {
-    sep=Separator(c0.max_span_coord(),FT(0));
-    sep.set_cutting_value(c0.median(sep.cutting_dimension()));
-    c0.split(c1,sep,true);
-  }
-};
+    
+    typedef Splitter_base<typename SearchTraits::FT> Base;
+    
+  public:
+    typedef typename SearchTraits::FT FT;
+    typedef Point_container<SearchTraits> Container;
+    typedef Separator_ Separator;
+    
+    
+    Median_of_rectangle()
+      : Base()
+    {}
+    
+    Median_of_rectangle(unsigned int bucket_size)
+      : Base(bucket_size)
+    {}
 
-template <class SearchTraits,
-	  class Separator_=Plane_separator<typename SearchTraits::FT>  >
-class Midpoint_of_max_spread
-  : public Splitter_base<typename SearchTraits::FT> {
+    void 
+    operator() (Separator& sep, Container& c0, Container& c1)
+    {
+      sep = Separator(c0.max_span_coord(),FT(0));
+      sep.set_cutting_value(c0.median(sep.cutting_dimension()));
+      c0.split(c1,sep,true);
+    }
+  };
 
-  typedef Splitter_base<typename SearchTraits::FT> Base;
-
-public:
-  typedef typename SearchTraits::FT FT;
-  typedef Point_container<SearchTraits> Container;
-  typedef Separator_ Separator;
-
-
-  Midpoint_of_max_spread()
-    : Base()
-  {}
-
-  Midpoint_of_max_spread(unsigned int bucket_size)
-    : Base(bucket_size)
-  {}
-
-  void operator() (Separator& sep, Container& c0,
-  			     Container& c1)
+  template <class SearchTraits,
+	   class Separator_= Plane_separator<typename SearchTraits::FT>  >
+  class Midpoint_of_max_spread
+    : public Splitter_base<typename SearchTraits::FT> 
   {
-    sep= Separator(c0.max_tight_span_coord(),
-    (c0.max_tight_span_upper() + c0.max_tight_span_lower())/FT(2));
-    c0.split(c1,sep);
-  }
-};
+    typedef Splitter_base<typename SearchTraits::FT> Base;
+    
+  public:
+    typedef typename SearchTraits::FT FT;
+    typedef Point_container<SearchTraits> Container;
+    typedef Separator_ Separator;
+    
 
-template <class SearchTraits,
-	  class Separator_=Plane_separator<typename SearchTraits::FT>  >
-class Midpoint_of_rectangle
-  : public Splitter_base<typename SearchTraits::FT> {
-
-  typedef Splitter_base<typename SearchTraits::FT> Base;
-public:
-  typedef typename SearchTraits::FT FT;
-  typedef Point_container<SearchTraits> Container;
-  typedef Separator_ Separator;
-
-
-  Midpoint_of_rectangle()
-    : Base()
-  {}
-
-  Midpoint_of_rectangle(unsigned int bucket_size)
-    : Base(bucket_size)
-  {}
-
-  void operator() (Separator& sep, Container& c0,
-  			     Container& c1)
+    Midpoint_of_max_spread()
+      : Base()
+    {}
+    
+    Midpoint_of_max_spread(unsigned int bucket_size)
+      : Base(bucket_size)
+    {}
+    
+    void 
+    operator()(Separator& sep, Container& c0, Container& c1)
+    {
+      sep = Separator(c0.max_tight_span_coord(),
+		      (c0.max_tight_span_upper() + c0.max_tight_span_lower())/FT(2));
+      c0.split(c1,sep);
+    }
+  };
+  
+  template <class SearchTraits,
+	   class Separator_= Plane_separator<typename SearchTraits::FT>  >
+  class Midpoint_of_rectangle
+  : public Splitter_base<typename SearchTraits::FT> 
   {
-    sep = Separator(c0.max_span_coord(),
-		    (c0.max_span_upper() + c0.max_span_lower())/FT(2));
-    c0.split(c1,sep);          
-  }
- 
-};
+
+    typedef Splitter_base<typename SearchTraits::FT> Base;
+  public:
+    typedef typename SearchTraits::FT FT;
+    typedef Point_container<SearchTraits> Container;
+    typedef Separator_ Separator;
+    
+    
+    Midpoint_of_rectangle()
+      : Base()
+    {}
+    
+    Midpoint_of_rectangle(unsigned int bucket_size)
+      : Base(bucket_size)
+    {}
+    
+    void 
+    operator()(Separator& sep, Container& c0, Container& c1)
+    {
+      sep = Separator(c0.max_span_coord(),
+		      (c0.max_span_upper() + c0.max_span_lower())/FT(2));
+      c0.split(c1,sep);          
+    }
+    
+  };
 
 } // namespace CGAL
 #endif // CGAL_SPLITTERS
