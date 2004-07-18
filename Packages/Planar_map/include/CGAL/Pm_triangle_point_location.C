e1f02ba21a0cc61e6d5f971893518ad6d09dd08d
Idit Haran <haranidi@post.tau.ac.il>
2004-07-18 13:41:43 +0000

    new point location strategy (no ray shootin), using delauney triangulation

---
+++ Packages/Planar_map/include/CGAL/Pm_triangle_point_location.C
@@ -0,0 +1,455 @@
+// Copyright (c) 2004  Tel-Aviv University (Israel).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Idit Haran <haranidi@post.tau.ac.il>
+
+#ifndef CGAL_PM_TRIANGLE_POINT_LOCATION_C
+#define CGAL_PM_TRIANGLE_POINT_LOCATION_C
+
+#include <CGAL/Pm_triangle_point_location.h>
+
+//#define CGAL_PM_WALK_DEBUG
+//#define CGAL_PM_DEBUG
+
+CGAL_BEGIN_NAMESPACE
+
+//----------------------------------------------------
+/*!
+ */
+//if unbounded face - returns NULL or some edge on unbounded face 
+//if its a vertex returns a halfedge pointing _at_ it
+template <class Planar_map>
+typename Pm_triangle_point_location<Planar_map>::Halfedge_handle
+Pm_triangle_point_location<Planar_map>::locate(const Point_2 & p, 
+                                                      Locate_type & lt) const
+{
+#ifdef CGAL_PM_DEBUG
+  std::cout << "------ locate point "<< p << std::endl;
+#endif
+
+  //init output
+  Face_handle f = pm->unbounded_face(), last = pm->faces_end();  
+              // invariant: p always lies in f's interior or holes 
+  Halfedge_handle e = pm->halfedges_end(); // closest halfedge so far
+  lt = Planar_map::UNBOUNDED_FACE;
+
+  //init CDT
+  if (! updated_cdt)
+  {
+    std::cout << "ERROR: CDT is not updated !" << std::endl;
+    return e;
+  }
+  //assert(cdt.is_valid());
+
+  //locate in the CDT
+  CDT_Point p1 = static_cast <CDT_Point> (p);
+  //CDT_Face_handle fh1 = cdt.locate(p1);       //locate point
+  //fh1->info() = CGAL::YELLOW;               //update the face's color
+
+  //locate point
+  int li;
+  CDT_Locate_type cdt_lt;
+  CDT_Face_handle fh = cdt.locate(p1,cdt_lt,li);
+  switch(cdt_lt) {
+  case CDT::OUTSIDE_AFFINE_HULL:
+  case CDT::OUTSIDE_CONVEX_HULL:
+    lt = Planar_map::UNBOUNDED_FACE;
+#ifdef CGAL_PM_DEBUG
+    std::cout << "unbounded face" << std::endl;
+#endif
+    return e;
+  case CDT::VERTEX:
+    {
+#ifdef CGAL_PM_DEBUG
+      std::cout << "locate type = vertex"<<li << std::endl;
+#endif
+      //get the vertex from li, which is the index of the vertex
+      Vertex_handle vertex_found = fh->vertex(li)->info();
+      e = vertex_found->incident_halfedges();
+      lt = Planar_map::VERTEX;
+#ifdef CGAL_PM_DEBUG
+      std::cout << "vertex is "<< vertex_found->point() << std::endl;
+      std::cout << "e = "<< e->source()->point()
+		<<" towards "<< e->target()->point() << std::endl;
+#endif
+      return e;
+    }
+  case CDT::EDGE:
+    {
+#ifdef CGAL_PM_DEBUG
+      std::cout << "locate type = edge"<<li << std::endl;
+#endif
+      //li is the index of the vertex OPOSITE to the edge 
+      if ( cdt.is_constrained(CDT_Edge(fh,li)) )
+      {  //the edge found is an edge in the plannar map
+#ifdef CGAL_PM_DEBUG
+	std::cout << "the edge is a constrained" << std::endl;
+#endif
+	lt = Planar_map::EDGE;
+	//get the 2 vertices incident to the edge in the plannar map 
+	int v1_index = (li+1)%3, v2_index = (li+2)%3;
+#ifdef CGAL_PM_DEBUG
+	std::cout << "v1 = "<<v1_index<<", v2 = "<<v2_index << std::endl;
+#endif
+	Vertex_handle v1_of_edge = fh->vertex(v1_index)->info();       
+	Vertex_handle v2_of_edge = fh->vertex(v2_index)->info();
+	//go over all halfedges incident to v1, and check if v2 is their source
+	Halfedge_around_vertex_circulator circ1 = v1_of_edge->incident_halfedges(); 
+	Halfedge_around_vertex_circulator circ1_done (circ1);
+
+	//std::cout << "Halfedges incident to " << v1_of_edge->point() << std::endl;
+	do {
+	  //std::cout << "edge = "<< (*circ1).curve() << std::endl;    
+	  //std::cout << "source = "<<circ1->source()->point() << std::endl; 
+	  //std::cout << "target = "<<circ1->target()->point() << std::endl;
+	  if (v2_of_edge == circ1->source())
+	  {
+	    e = circ1;
+#ifdef CGAL_PM_DEBUG
+	    std::cout << "e = "<< e->source()->point()
+		      <<" towards "<< e->target()->point() << std::endl;
+#endif
+	  }
+	} while (++circ1 != circ1_done);
+	
+	//check if e is valid
+#ifdef CGAL_PM_DEBUG
+	if (pm->halfedges_end() == e)
+	  std::cout << "ERROR: e is invalid " << std::endl;
+#endif	
+	return e; // ????
+      }
+      //if the edge is not a constrained - its not an edge of the 
+      //plannar map, which means we're inside of a pm face -
+      //lets look at the face as if it was a face case.
+      // no break - continue to the face caes
+    }
+  case CDT::FACE:
+    lt = Planar_map::FACE;
+    break;
+  }
+
+  //we're in case CDT::FACE
+#ifdef CGAL_PM_DEBUG
+  std::cout << "FACE "  << std::endl;
+#endif
+
+  //get 3 pm vertices of face
+  Vertex_handle v0 = fh->vertex(0)->info();       
+  Vertex_handle v1 = fh->vertex(1)->info();
+  Vertex_handle v2 = fh->vertex(2)->info();
+
+  //find the face in the pm correspond to the 3 vertices
+  Halfedge_around_vertex_circulator havc0 = v0->incident_halfedges(); 
+  Halfedge_around_vertex_circulator havc0_done (havc0);
+
+  Halfedge_around_vertex_circulator havc1 = v1->incident_halfedges(); 
+  Halfedge_around_vertex_circulator havc1_done (havc1);
+
+  Halfedge_around_vertex_circulator havc2 = v2->incident_halfedges(); 
+  Halfedge_around_vertex_circulator havc2_done (havc2);
+
+#ifdef CGAL_PM_DEBUG
+  //printings, debugging
+  std::cout << "Halfedges incident to " << v0->point() << std::endl;
+  do {
+    std::cout << (*havc0).curve() << std::endl;    
+  } while (++havc0 != havc0_done);
+
+  std::cout << "Halfedges incident to " << v1->point() << std::endl;
+  do {
+    std::cout << (*havc1).curve() << std::endl;    
+  } while (++havc1 != havc1_done);
+
+  std::cout << "Halfedges incident to " << v2->point() << std::endl;
+  do {
+    std::cout << (*havc2).curve() << std::endl;    
+  } while (++havc2 != havc2_done);
+#endif //CGAL_PM_DEBUG
+
+  //loop to find face
+  Face_iterator found_face = pm->unbounded_face();
+  bool found = false;
+  bool found_unbounded = false;
+  do {
+    //get face from halfedge
+    Face_iterator f0 = (*havc0).face(); 
+    //printf ("f0 = %e \n", f0);
+    //Pm_Face_iterator f0_2 = (*havc0).twin().face();
+    do {
+      Face_iterator f1 = (*havc1).face(); 
+      //printf ("f1 = %e \n", f0);
+      if ( f0 == f1 )
+      {
+#ifdef CGAL_PM_DEBUG
+	std::cout <<"f0 == f1"<< std::endl;
+#endif
+	do {
+	  Face_iterator f2 = (*havc2).face();
+	  if ( f1 == f2 )
+	  {
+#ifdef CGAL_PM_DEBUG
+	    std::cout << "f1 == f2"<< std::endl;
+#endif
+	    if (found_face != f0) {
+	      found_face = f0;
+	      e = havc0; //!!! this is the relevant update for the PL !!!
+	      found = true;
+	    }
+	    else
+	      found_unbounded = true;
+	  }
+	  //printf ("f2 = %e \n", f0);
+	} while ((++havc2 != havc2_done) && !found );
+      }
+    } while ((++havc1 != havc1_done)&& !found );
+  } while ((++havc0 != havc0_done)&& !found );
+
+  //debug new
+  //  if (!found && found_unbounded ) 
+  //  {
+  //std::cout<< "FOUND UNBOUNDED !!! " << std::endl;
+  //  }
+  if (found_face == pm->unbounded_face())
+  {
+    if (! found_unbounded)
+      std::cerr<< "NOT GOOD - face not found" << std::endl;
+    lt = Planar_map::UNBOUNDED_FACE;
+    e = pm->halfedges_end();
+    return e;
+  }
+
+#ifdef CGAL_PM_DEBUG
+  //print the curves of the found face
+  std::cout << "found face for point "<< p1 <<" is: "<<std::endl;
+  Ccb_halfedge_circulator ccb = found_face->outer_ccb();
+  Ccb_halfedge_circulator ccb_end = ccb;
+  do {
+    std::cout << ccb->source()->point()<<" towards "
+              << ccb->target()->point()<<std::endl;
+  } while ((++ccb) != ccb_end);
+  
+    std::cout << "e = "<< e->source()->point()
+	      <<" towards "<< e->target()->point() << std::endl;
+#endif //CGAL_PM_DEBUG
+
+  return e;
+}
+
+//----------------------------------------------------
+/*!
+ */
+template <class Planar_map>
+typename Pm_triangle_point_location<Planar_map>::Halfedge_handle
+Pm_triangle_point_location<Planar_map>::locate(const Point_2 & p, 
+						      Locate_type & lt) {
+
+  //  if (! updated_cdt)
+  //    triangulate_pm();
+  //  assert(cdt.is_valid());
+
+  Halfedge_handle h=((cPLp)this)->locate(p,lt);
+
+  return h;
+}
+
+
+//----------------------------------------------------
+/*!
+ */
+template <class Planar_map>
+typename Pm_triangle_point_location<Planar_map>::Halfedge_handle
+Pm_triangle_point_location<Planar_map>::
+vertical_ray_shoot(const Point_2 & p, 
+                   Locate_type & lt, 
+                   bool up) const
+{
+  std::cout << "ERROR: Vertical ray shoot NOT suppored in CDT point location"
+	    << std::endl;
+  CGAL_precondition_msg(false, 
+			"Vertical ray shoot NOT suppored in CDT point location");
+  assert(false);
+  Halfedge_handle e = pm->halfedges_end(); // closest halfedge so far  
+  lt=Planar_map::UNBOUNDED_FACE;
+  return e;
+}
+
+//----------------------------------------------------
+/*!
+ */
+template <class Planar_map>
+typename Pm_triangle_point_location<Planar_map>::Halfedge_handle
+Pm_triangle_point_location<Planar_map>::vertical_ray_shoot(
+                                                 const Point_2& p, 
+                                                 Locate_type& lt, bool up){
+  Halfedge_handle h=((cPLp)this)->vertical_ray_shoot(p,lt,up);
+  return h;
+}
+
+//----------------------------------------------------
+/*! insert an halfedge to the plannar map - 
+    insert it as a constraint to the CDT
+ */
+template <class Planar_map>
+void Pm_triangle_point_location<Planar_map>::insert_to_cdt
+(Halfedge_handle hh, const X_monotone_curve_2 &cv)
+{
+#ifdef CGAL_PM_DEBUG
+  std::cout << "in insert_to_cdt" << std::endl ;
+  //std::cout << *pm << std::endl;
+  std::cout << "cv = "<< cv << std::endl;
+  std::cout << hh->source()->point()<<" towards "
+  	    << hh->target()->point()<< std::endl;
+#endif //CGAL_PM_DEBUG
+  
+  Vertex_handle pm_vh1 = hh->source();
+  Vertex_handle pm_vh2 = hh->target();
+
+  //get points from vertices
+  Point_2 pm_p1 = (*pm_vh1).point() ;
+  Point_2 pm_p2 = (*pm_vh2).point() ;
+
+  //cast the points to be CDT points
+  CDT_Point cdt_p1 = static_cast <CDT_Point> (pm_p1);
+  CDT_Point cdt_p2 = static_cast <CDT_Point> (pm_p2);
+
+  //insert vertices to the CDT
+  CDT_Vertex_handle cdt_vh1 = cdt.insert(cdt_p1);
+  CDT_Vertex_handle cdt_vh2 = cdt.insert(cdt_p2);
+
+  //connect new CDT vertex with Pm vertex
+  cdt_vh1->info() = pm_vh1;
+  cdt_vh2->info() = pm_vh2;
+
+  //add constraint from the two points
+  cdt.insert_constraint(cdt_vh1, cdt_vh2);
+}
+
+//----------------------------------------------------
+/*! triangulate the plannar map into a cdt (Constaint
+    Delauney Triangulation) :
+    go over all halfedges, and insert each halfedge 
+    as a constraint to the cdt. 
+ */
+template <class Planar_map>
+void Pm_triangle_point_location<Planar_map>::triangulate_pm()
+{ 
+#ifdef CGAL_PM_DEBUG
+  std::cout << "in triangulate_pm" << std::endl ;
+  //std::cout << *pm << std::endl;
+#endif
+  
+  cdt.clear();
+
+  //Go over plannar map, and create a triangulation of it
+  Edge_iterator eit;
+
+#ifdef CGAL_PM_DEBUG
+  std::cout << "All edges" << std::endl;
+#endif
+
+  eit=pm->edges_begin();
+  //  std::cout << "0" << std::endl;
+
+  for (eit=pm->edges_begin(); eit != pm->edges_end(); eit++)
+  {
+    //std::cout << "1" << std::endl;
+    //get vertices from edge
+    Vertex_handle pm_vh1 = (*eit).source();
+    Vertex_handle pm_vh2 = (*eit).target();
+
+    //    std::cout << "2" << std::endl;
+    //get curve
+    X_monotone_curve_2 cv = (*eit).curve();
+    //    std::cout << "cv = "<< cv << std::endl;
+
+    //get points from vertices
+    Point_2 pm_p1 = (*pm_vh1).point() ;
+    Point_2 pm_p2 = (*pm_vh2).point() ;
+
+    //    std::cout << "3" << std::endl;
+    //cast the points to be CDT points
+    CDT_Point cdt_p1 = static_cast <CDT_Point> (pm_p1);
+    CDT_Point cdt_p2 = static_cast <CDT_Point> (pm_p2);
+
+    //    std::cout << "4" << std::endl;
+    //insert vertices to the CDT
+    CDT_Vertex_handle cdt_vh1 = cdt.insert(cdt_p1);
+    CDT_Vertex_handle cdt_vh2 = cdt.insert(cdt_p2);
+
+    //    std::cout << "5" << std::endl;
+    //connect new CDT vertex with Pm vertex
+    cdt_vh1->info() = pm_vh1;
+    cdt_vh2->info() = pm_vh2;
+
+    //    std::cout << "6" << std::endl;
+    //    std::cout << "pm_p1 = "<< pm_p1 <<" , cdt_p1 = "<< cdt_p1 << std::endl;
+    //    std::cout << "pm_p2 = "<< pm_p2 <<" , cdt_p2 = "<< cdt_p2 << std::endl;
+    //std::cout << "pm_vh1 = "<< pm_vh1 <<" , cdt_vh1 = "<< cdt_vh1 << std::endl;
+    //std::cout << "pm_vh2 = "<< pm_vh2 <<" , cdt_vh2 = "<< cdt_vh2 << std::endl;
+   
+    //add constraint from the two points
+    cdt.insert_constraint(cdt_vh1, cdt_vh2);
+
+    //print
+    //    std::cout << "7" << std::endl;
+#ifdef CGAL_PM_DEBUG
+    std::cout << "curve = " << cv ;
+    std::cout << " , source = " << pm_p1 ;
+    std::cout << " , target = " << pm_p2 << std::endl;
+#endif
+  }
+  
+
+ //the triangulation is now updated
+  updated_cdt = true;
+
+  // --- printing and debugging of CDT
+  //  std::cout << "cdt is now updated" << std::endl;
+  assert(cdt.is_valid());
+
+#ifdef CGAL_PM_DEBUG
+  int count = 0;
+  
+  for (CDT_Finite_edges_iterator eit = cdt.finite_edges_begin();
+       eit != cdt.finite_edges_end();
+       ++eit)
+    if (cdt.is_constrained(*eit)) ++count;
+  std::cout << "The number of resulting constrained edges is  ";
+  std::cout <<  count << std::endl;
+
+  CDT_Finite_faces_iterator fc = cdt.finite_faces_begin();
+  for( ; fc != cdt.finite_faces_end(); ++fc)  
+  {
+    fc->info() = CGAL::BLUE;
+  }
+
+  count=0;
+  fc = cdt.finite_faces_begin();
+  for( ; fc != cdt.finite_faces_end(); ++fc)  
+  {
+    count++;
+    std::cout << "fc number "<<count<<" ,color = " << fc->info() << std::endl;
+  }
+
+#endif  //CGAL_PM_DEBUG
+
+}
+
+CGAL_END_NAMESPACE
+
+#endif  //CGAL_PM_TRIANGLE_POINT_LOCATION_C
---
+++ Packages/Planar_map/include/CGAL/Pm_triangle_point_location.h
@@ -0,0 +1,229 @@
+// Copyright (c) 2004  Tel-Aviv University (Israel).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Idit Haran <haranidi@post.tau.ac.il>
+
+#ifndef CGAL_PM_TRIANGLE_POINT_LOCATION_H
+#define CGAL_PM_TRIANGLE_POINT_LOCATION_H
+
+#include <CGAL/Pm_point_location_base.h>
+#include <CGAL/Planar_map_2/Pm_traits_wrap_2.h>
+
+//#define CGAL_PM_WALK_DEBUG
+//#define CGAL_PM_DEBUG
+
+#ifdef CGAL_PM_DEBUG
+#ifndef CGAL_NAIVE_POINT_LOCATION_H
+#include <CGAL/Pm_naive_point_location.h>
+#endif
+#endif
+
+//----------------------------------------------------------
+// triangulation includes
+//----------------------------------------------------------
+// file          : examples/Triangulation_2/constrained.C
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <CGAL/IO/Color.h>
+#include <CGAL/Triangulation_face_base_with_info_2.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <iostream>
+#include <stdio.h>
+
+//----------------------------------------------------------
+//Pm includes
+//----------------------------------------------------------
+#include <CGAL/basic.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/MP_Float.h>
+#include <CGAL/Quotient.h>
+#include <CGAL/Arr_segment_cached_traits_2.h>
+#include <CGAL/Pm_default_dcel.h>
+#include <CGAL/Planar_map_2.h>
+#include <CGAL/Pm_with_intersections.h>
+
+
+////////////////////////////////////////////////////////
+//    TRIANGLE STRATEGY
+////////////////////////////////////////////////////////
+
+CGAL_BEGIN_NAMESPACE
+
+template <class Planar_map_> class Pm_triangle_point_location : 
+  public Pm_point_location_base<Planar_map_> {
+public:
+  //----------------------------------------------------------
+  // Pm Types
+  //----------------------------------------------------------
+
+  typedef Quotient<CGAL::MP_Float>                  Coord_type;
+  typedef Cartesian<Coord_type>                     Kernel;
+  typedef Arr_segment_cached_traits_2<Kernel>       Traits;
+
+  //----------------------------------------------------------
+  // Pm Types
+  //----------------------------------------------------------
+
+  typedef Kernel::Segment_2                               Segment;
+  typedef Traits::Point_2                                 Point_2;
+  typedef Traits::Curve_2                                 Curve_2;
+  typedef Traits::X_monotone_curve_2                      X_monotone_curve_2;
+  typedef Planar_map_                                     Planar_map;
+  //typedef Pm_default_dcel<Traits>                         Dcel;
+  //typedef typename Planar_map_2<Dcel,Traits>              Planar_map;
+  //typedef Planar_map_with_intersections_2<Planar_map>     Pmwx;
+  typedef Pm_point_location_base<Planar_map>              Base;
+  typedef Pm_triangle_point_location<Planar_map>              Self;
+  typedef typename Planar_map::Face_iterator              Face_iterator;
+  typedef typename Planar_map::Halfedge_iterator          Halfedge_iterator;
+  typedef typename Planar_map::Vertex_iterator                     Vertex_iterator;
+  typedef typename Planar_map::Edge_iterator                       Edge_iterator;
+  typedef typename Planar_map::Vertex_handle                       Vertex_handle;
+  typedef typename Planar_map::Halfedge_handle                     Halfedge_handle;
+  typedef typename Planar_map::Face_handle                         Face_handle;
+  typedef typename Planar_map::Halfedge_around_vertex_circulator 
+                                         Halfedge_around_vertex_circulator;
+  typedef typename Planar_map::Ccb_halfedge_circulator  Ccb_halfedge_circulator;
+  typedef typename Base::Halfedge_handle_iterator       Halfedge_handle_iterator;
+  typedef typename Base::Token                          Token;
+  typedef typename Planar_map::Locate_type              Locate_type;
+  typedef typename Planar_map::Traits_wrap              Traits_wrap;
+  //----------------------------------------------------------
+
+  //----------------------------------------------------------
+  // Triangulation Types
+  //----------------------------------------------------------
+  typedef Triangulation_vertex_base_with_info_2<Vertex_handle,Kernel>  Vb;
+  typedef Triangulation_face_base_with_info_2<CGAL::Color,Kernel>    Fbt;
+  typedef Constrained_triangulation_face_base_2<Kernel,Fbt>          Fb;
+  typedef Triangulation_data_structure_2<Vb,Fb>                      TDS;
+  typedef Exact_predicates_tag                                       Itag;
+  typedef Constrained_Delaunay_triangulation_2<Kernel, TDS, Itag>    CDT;
+  typedef typename CDT::Point                                  CDT_Point;
+  typedef typename CDT::Edge                                   CDT_Edge;
+  typedef typename CDT::Face_handle                      CDT_Face_handle;
+  typedef typename CDT::Vertex_handle                    CDT_Vertex_handle;
+  typedef typename CDT::Finite_faces_iterator            CDT_Finite_faces_iterator;
+  typedef typename CDT::Finite_vertices_iterator         CDT_Finite_vertices_iterator;
+  typedef typename CDT::Finite_edges_iterator            CDT_Finite_edges_iterator;
+  typedef typename CDT::Locate_type                      CDT_Locate_type;
+  //----------------------------------------------------------
+
+
+protected:
+  typedef const Self* cPLp;
+
+public:
+  // Constructor
+  Pm_triangle_point_location() : 
+    Pm_point_location_base<Planar_map>(),
+    pm(0),
+    traits(0),
+    updated_cdt(false)
+  {}
+  
+  void init(Planar_map & pmp, Traits & tr) 
+  {
+#ifdef CGAL_PM_DEBUG
+    std::cout << "init PL" << std::endl;
+#endif
+
+    CGAL_precondition_msg(pm == NULL,
+    "Point location instance should be uninitialized "
+    "(Do not use the same instance for more than one map).");
+
+    pm = &pmp;
+    traits = (Traits_wrap*)(&tr);
+
+    triangulate_pm();
+  }
+
+  inline void insert(Halfedge_handle hh, const X_monotone_curve_2 & cv) 
+  {insert_to_cdt(hh, cv); }
+
+  Halfedge_handle locate(const Point_2 & p, Locate_type & lt) const;
+
+  Halfedge_handle locate(const Point_2 & p, Locate_type & lt);
+
+  Halfedge_handle vertical_ray_shoot(const Point_2& p, Locate_type& lt, bool up)
+    const;
+  Halfedge_handle vertical_ray_shoot(const Point_2& p, Locate_type& lt, bool up);
+
+  inline void split_edge(const X_monotone_curve_2 &, Halfedge_handle, Halfedge_handle,
+                         //additions by iddo for arrangement
+                         const X_monotone_curve_2 &, const X_monotone_curve_2 &) 
+  {updated_cdt = false; triangulate_pm(); }
+
+  inline void merge_edge(const X_monotone_curve_2 &, const X_monotone_curve_2 &, Halfedge_handle, 
+                         //additions by iddo for arrangement
+                         const X_monotone_curve_2 &)   
+  {updated_cdt = false;  triangulate_pm();}
+
+  inline void remove_edge(Halfedge_handle) 
+  {updated_cdt = false;  triangulate_pm();}
+
+  inline void remove_edge(const Halfedge_handle_iterator &,
+                          const Halfedge_handle_iterator &) 
+  {updated_cdt = false; triangulate_pm(); };
+
+  inline void clear() 
+  {updated_cdt = false; triangulate_pm();}
+
+  inline void update(const Halfedge_handle_iterator &,
+                     const Halfedge_handle_iterator &,
+                     const Token& token) 
+  {updated_cdt = false; triangulate_pm(); }
+
+  //function that does the triangulation
+  void triangulate_pm() ;
+  void insert_to_cdt(Halfedge_handle hh, const X_monotone_curve_2 &cv) ;
+  
+private:
+
+#ifdef CGAL_PM_DEBUG
+
+  void debug() {}
+
+  void debug(const Halfedge_handle& e) const
+    {
+      {
+        if (e!=pm->halfedges_end()) 
+          std::cerr << "(" << e->source()->point() << "," 
+		    << e->target()->point() << ")" << std::flush;
+        else std::cerr << "(oo)";
+      }
+    }
+
+#endif
+
+public:
+  inline const Traits * get_traits() const {return traits;}
+
+protected:
+  Planar_map * pm;
+  Traits_wrap * traits;
+  CDT cdt;
+  bool updated_cdt;
+};
+  
+CGAL_END_NAMESPACE
+
+//#ifdef CGAL_CFG_NO_AUTOMATIC_TEMPLATE_INCLUSION
+#include <CGAL/Pm_triangle_point_location.C>
+//#endif
+
+#endif //PM_TRIANGLE_POINT_LOCATION_H
