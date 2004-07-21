2eb4881dd7569b5d6669ebf92fc016e025f46998
Ron Wein <wein@post.tau.ac.il>
2004-07-21 08:49:25 +0000

    Fixed the test to work with the new CORE-based conic traits.

--- Packages/Sweep_line_2/test/Sweep_line_2/test_sweep_conic.C
+++ Packages/Sweep_line_2/test/Sweep_line_2/test_sweep_conic.C
@@ -10,7 +10,8 @@ int main()
 #else
 
 #include <CGAL/Cartesian.h>
-#include <CGAL/leda_real.h>
+#include <CORE/BigInt.h>
+#include <CGAL/CORE_Expr.h>
 
 #include <CGAL/Pm_default_dcel.h>
 #include <CGAL/Planar_map_2.h>
@@ -28,16 +29,24 @@ int main()
 #include <list>
 
 
-typedef leda_real                         NT;
-typedef CGAL::Cartesian<NT>               Kernel;
-typedef CGAL::Arr_conic_traits_2<Kernel>  Traits;
+typedef CORE::BigInt                                  CfNT;
+typedef CGAL::Cartesian<CfNT>                         Int_kernel;
+typedef Int_kernel::Point_2                           Int_point_2;
+typedef Int_kernel::Segment_2                         Int_segment_2;
+typedef Int_kernel::Circle_2                          Int_circle_2;
+typedef CORE::Expr                                    CoNT;
+typedef CGAL::Cartesian<CoNT>                         Alg_kernel;
+typedef CGAL::Arr_conic_traits_2<Int_kernel, 
+				 Alg_kernel>          Traits_2;
 
-typedef Traits::X_monotone_curve_2        X_monotone_curve_2;
-typedef Traits::Point_2                   Point_2;
+typedef Traits_2::Curve_2                             Curve_2;
+typedef Traits_2::X_monotone_curve_2                  X_monotone_curve_2;
+typedef Traits_2::Point_2                             Point_2;
+typedef std::list<Curve_2>                            CurveList;
 
-typedef CGAL::Pm_default_dcel<Traits>                    Dcel;
-typedef CGAL::Planar_map_2<Dcel,Traits>                  Pm;
-typedef CGAL::Planar_map_with_intersections_2<Pm>        Arr_2;
+typedef CGAL::Pm_default_dcel<Traits_2>               Dcel;
+typedef CGAL::Planar_map_2<Dcel,Traits_2>             Pm_2;
+typedef CGAL::Planar_map_with_intersections_2<Pm_2>   Pmwx_2;
 
 typedef std::list<X_monotone_curve_2>     CurveContainer;
 typedef CurveContainer::iterator CurveContainerIter;
@@ -47,21 +56,15 @@ typedef PointList::iterator PointListIter;
 
 // global variables are used so that the redraw function for the LEDA window
 // can be defined to draw information found in these variables.
-CGAL::Pm_naive_point_location<Arr_2::Planar_map> pl; 
+CGAL::Pm_naive_point_location<Pmwx_2::Planar_map> pl; 
 
-static Arr_2               arr(&pl);
+static Pmwx_2               arr(&pl);
 
 /*! Conic reader */
 template <class Traits>
 class Conic_reader
 {
 public:
-  typedef typename Traits::Point_2      Point_2;
-  typedef typename Traits::Curve_2      Curve_2;
-  typedef typename Traits::Circle_2     Circle_2;
-  typedef typename Traits::Segment_2    Segment_2;
-  typedef typename Traits::X_monotone_curve_2    X_monotone_curve_2;
-  typedef std::list<Curve_2>            CurveList;
 
   int ReadData(const char * filename, CurveList & curves,
                CGAL::Bbox_2 & bbox)
@@ -90,191 +93,100 @@ public:
   
   void ReadCurve(std::ifstream & is, Curve_2 & cv)
   {
-      // Read a line from the input file.
-      char one_line[128];
+    // Read a line from the input file.
+    char one_line[128];
+    
+    skip_comments (is, one_line);
+    std::string stringvalues(one_line);
+    std::istringstream str_line (stringvalues, std::istringstream::in);
       
-      skip_comments (is, one_line);
-      std::string stringvalues(one_line);
-      std::istringstream str_line (stringvalues, std::istringstream::in);
+    // Get the arc type.
+    // Supported types are: 'f' - Full ellipse (or circle).
+    //                      'e' - Elliptic arc (or circular arc).
+    //                      's' - Line segment.
+    char         type;
+    bool         is_circle = false;              // Is this a circle.
+    Int_circle_2 circle;
+    CfNT         r, s, t, u, v, w;               // The conic coefficients.
+    
+    str_line >> type;
+    
+    // An ellipse (full ellipse or a partial ellipse):
+    if (type == 'f' || type == 'F' || type == 'e' || type == 'E')
+    {  
+      // Read the ellipse (using the format "a b x0 y0"):
+      //
+      //     x - x0   2      y - y0   2
+      //  ( -------- )  + ( -------- )  = 1
+      //       a               b
+      //
+      int     a, b, x0, y0;
       
-      // Get the arc type.
-      char     type;
-      bool     is_circle = false;              // Is this a circle.
-      Circle_2 circle;
-      NT       r, s, t, u, v, w;               // The conic coefficients.
+      str_line >> a >> b >> x0 >> y0;
       
-      str_line >> type;
+      CfNT     a_sq = CfNT(a*a);
+      CfNT     b_sq = CfNT(b*b);
       
-      // An ellipse (full ellipse or a partial ellipse):
-      if (type == 'f' || type == 'F' || type == 'e' || type == 'E')
-      {  
-          // Read the ellipse (using the format "a b x0 y0"):
-          //
-          //     x - x0   2      y - y0   2
-          //  ( -------- )  + ( -------- )  = 1
-          //       a               b
-          //
-          NT     a, b, x0, y0;
-          
-          str_line >> a >> b >> x0 >> y0;
-          
-          NT     a_sq = a*a;
-          NT     b_sq = b*b;
-          
-          if (a == b)
-          {
-              is_circle = true;
-              circle = Circle_2 (Point_2 (x0, y0), a*b, CGAL::CLOCKWISE);
-          }
-          else
-          {
-              r = b_sq;
-              s = a_sq;
-              t = 0;
-              u = -2*x0*b_sq;
-              v = -2*y0*a_sq;
-              w = x0*x0*b_sq + y0*y0*a_sq - a_sq*b_sq;
-          }
-          
-          if (type == 'f' || type == 'F')
-          {
-              // Create a full ellipse (or circle).
-              if (is_circle)
-                  cv = Curve_2 (circle);
-              else
-                  cv = Curve_2 (r, s, t, u, v, w);
-              
-              return;
-          }
-      }
-      else if (type == 'h' || type == 'H')
+      if (a == b)
       {
-          // Read the hyperbola (using the format "a b x0 y0"):
-          //
-          //     x - x0   2      y - y0   2
-          //  ( -------- )  - ( -------- )  = 1
-          //       a               b
-          //
-          NT     a, b, x0, y0;
-          
-          str_line >> a >> b >> x0 >> y0;
-          
-          NT     a_sq = a*a;
-          NT     b_sq = b*b;
-          
-          r = b_sq;
-          s= -a_sq;
-          t = 0;
-          u = -2*x0*b_sq;
-          v = 2*y0*a_sq;
-          w = x0*x0*b_sq - y0*y0*a_sq - a_sq*b_sq;  
-      }
-      else if (type == 'p' || type == 'P')
-      {
-          // Read the parabola (using the format "c x0 y0"):
-          //
-          //                        2
-          //  4c*(y - y0) = (x - x0)
-          //
-          NT     c, x0, y0;
-          
-          str_line >> c >> x0 >> y0;
-          
-          r = 1;
-          s = 0;
-          t = 0;
-          u = -2*x0;
-          v = -4*c;
-          w = x0*x0 + 4*c*y0;
-      }
-      else if (type == 'c' || type == 'C' || type == 'a' || type == 'A')
-      {
-          // Read a general conic, given by its coefficients <r,s,t,u,v,w>.
-          str_line >> r >> s >> t >> u >> v >> w;
-          
-          if (type == 'c' || type == 'C')
-          {
-              // Create a full conic (should work only for ellipses).
-              cv = Curve_2 (r, s, t, u, v, w);
-              return;
-          }
-      }
-      else if (type == 's' || type == 'S')
-      {
-          // Read a segment, given by its endpoints (x1,y1) and (x2,y2);
-          NT      x1, y1, x2, y2;
-          
-          str_line >> x1 >> y1 >> x2 >> y2;
-          
-          Point_2   source (x1, y1);
-          Point_2   target (x2, y2);
-          Segment_2 segment (source, target);
-          
-          // Create the segment.
-          cv = Curve_2(segment);
-          return;
-      }
-      else if (type == 'i' || type == 'I')
-      {
-          // Read a general conic, given by its coefficients <r,s,t,u,v,w>.
-          str_line >> r >> s >> t >> u >> v >> w;
-          
-          // Read the approximated source, along with a general conic 
-          // <r_1,s_1,t_1,u_1,v_1,w_1> whose intersection with <r,s,t,u,v,w>
-          // defines the source.
-          NT     r1, s1, t1, u1, v1, w1;
-          NT     x1, y1;
-          
-          str_line >> x1 >> y1;
-          str_line >> r1 >> s1 >> t1 >> u1 >> v1 >> w1;
-          
-          Point_2   app_source (x1, y1);
-          
-          // Read the approximated target, along with a general conic 
-          // <r_2,s_2,t_2,u_2,v_2,w_2> whose intersection with <r,s,t,u,v,w>
-          // defines the target.
-          NT     r2, s2, t2, u2, v2, w2;
-          NT     x2, y2;
-          
-          str_line >> x2 >> y2;
-          str_line >> r2 >> s2 >> t2 >> u2 >> v2 >> w2;
-          
-          Point_2   app_target (x2, y2);
-          
-          // Create the conic arc.
-          cv = Curve_2 (r, s, t, u, v ,w,
-                        app_source, r1, s1, t1, u1, v1, w1,
-                        app_target, r2, s2, t2, u2, v2, w2);
-          return;
+	is_circle = true;
+	circle = Int_circle_2 (Int_point_2 (CfNT(x0), CfNT(y0)),
+			       CfNT(a*b));
       }
       else
       {
-          std::cerr << "Illegal conic type specification: " << type << "."
-                    << std::endl;
-          return;
+	r = b_sq;
+	s = a_sq;
+	t = 0;
+	u = CfNT(-2*x0*b_sq);
+	v = CfNT(-2*y0*a_sq);
+	w = CfNT(x0*x0*b_sq + y0*y0*a_sq - a_sq*b_sq);
       }
       
-      // Read the end points of the arc and create it.
-      NT    x1, y1, x2, y2;
+      if (type == 'f' || type == 'F')
+      {
+	// Create a full ellipse (or circle).
+	if (is_circle)
+	  cv = Curve_2 (circle);
+	else
+	  cv = Curve_2 (r, s, t, u, v, w);
+      }
+      else
+      {
+	// Read the endpointd of the arc.
+	int       x1, y1, x2, y2;
+	
+	str_line >> x1 >> y1 >> x2 >> y2;
+
+	Point_2   source = Point_2 (CoNT(x1), CoNT(y1));
+	Point_2   target = Point_2 (CoNT(x2), CoNT(y2));
+
+	// Create the arc. Note that it is always clockwise oriented.
+	if (is_circle)
+	  cv = Curve_2 (circle,
+			CGAL::CLOCKWISE,
+			source, target);
+	else
+	  cv = Curve_2 (r, s, t, u, v, w,
+			CGAL::CLOCKWISE,
+			source, target);
+      }  
+    }
+    else if (type == 's' || type == 'S')
+    {
+      // Read a segment, given by its endpoints (x1,y1) and (x2,y2);
+      int      x1, y1, x2, y2;
       
       str_line >> x1 >> y1 >> x2 >> y2;
+          
+      // Create the segment.
+      Int_point_2   source = Int_point_2 (CfNT(x1), CfNT(y1));
+      Int_point_2   target = Int_point_2 (CfNT(x2), CfNT(y2));
       
-      Point_2 source (x1, y1);
-      Point_2 target (x2, y2);
-      
-      // Create the conic (or circular) arc.
-      if (is_circle)
-      {
-          cv = Curve_2 (circle,
-                        source, target);
-      }
-      else
-      {
-          cv = Curve_2 (r, s, t, u, v, w,
-                        source, target);
-      }
-      
-      return;
+      cv = Curve_2(Int_segment_2 (source, target));
+    }
+    
+    return;
   }
     
   void skip_comments( std::ifstream& is, char* one_line )
@@ -305,19 +217,19 @@ int main (int argc, char** argv)
   CGAL::Bbox_2 bbox;
   CurveContainer curves;
 
-  Conic_reader<Traits> reader;
+  Conic_reader<Traits_2> reader;
   reader.ReadData(argv[1], curves, bbox);
 
   // run the sweep
   std::list<X_monotone_curve_2> mylist;
 
-  CGAL::Sweep_line_2<CurveContainerIter, Traits> sl;
+  CGAL::Sweep_line_2<CurveContainerIter, Traits_2> sl;
   sl.get_subcurves(curves.begin(), curves.end(), 
 		   std::back_inserter(mylist), false);
   
   
   PointList point_list_with_ends;
-  CGAL::Sweep_line_2<CurveContainerIter, Traits> sl1;
+  CGAL::Sweep_line_2<CurveContainerIter, Traits_2> sl1;
   sl1.get_intersection_points(curves.begin(), curves.end(), 
 			      std::back_inserter(point_list_with_ends));
   int point_count_with_ends_calculated = point_list_with_ends.size();
