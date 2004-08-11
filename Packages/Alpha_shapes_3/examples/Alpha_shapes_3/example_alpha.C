a79c9fb4f26b4250dbb89c7b36c5df4590aa1bf9
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2004-08-11 15:34:43 +0000

    full redoing of example  to get examples that can be included in the doc

--- Packages/Alpha_shapes_3/examples/Alpha_shapes_3/example_alpha.C
+++ Packages/Alpha_shapes_3/examples/Alpha_shapes_3/example_alpha.C
@@ -1,152 +1,51 @@
-// file: examples/Alpha_shapes_3/example_alpha.C
+// examples/Alpha_shapes_3/example_alpha.C
 
-/***********************************************************************
-
-Takes a list of points and returns a list of segments corresponding to
-the Alpha Shape.
-
-************************************************************************/
-
-#include <CGAL/Simple_cartesian.h>
-
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <list>
-
-#include <CGAL/Alpha_shape_vertex_base_3.h>
-#include <CGAL/Triangulation_cell_base_3.h>
-#include <CGAL/Alpha_shape_cell_base_3.h>
-#include <CGAL/Triangulation_data_structure_3.h>
-
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_3.h>
 #include <CGAL/Alpha_shape_3.h>
 
+#include <fstream>
+#include <list>
 
-typedef double coord_type;
+struct K : CGAL::Exact_predicates_inexact_constructions_kernel {};
 
-
-typedef CGAL::Simple_cartesian<coord_type>  K;
-
-typedef K::Point_3    Point;
-typedef K::Segment_3  Segment;
-
-
-typedef CGAL::Alpha_shape_vertex_base_3<K> Vb;
-
-typedef CGAL::Triangulation_cell_base_3<K> Df;
-typedef CGAL::Alpha_shape_cell_base_3<K, Df>  Fb;
-
+typedef CGAL::Alpha_shape_vertex_base_3<K>          Vb;
+typedef CGAL::Alpha_shape_cell_base_3<K>            Fb;
 typedef CGAL::Triangulation_data_structure_3<Vb,Fb> Tds;
-typedef CGAL::Delaunay_triangulation_3<K,Tds> Triangulation_3;
+typedef CGAL::Delaunay_triangulation_3<K,Tds>       Triangulation_3;
+typedef CGAL::Alpha_shape_3<Triangulation_3>        Alpha_shape_3;
 
-typedef CGAL::Alpha_shape_3<Triangulation_3>  Alpha_shape_3;
-
-typedef Alpha_shape_3::Cell  Cell;
-typedef Alpha_shape_3::Vertex Vertex;
-typedef Alpha_shape_3::Edge Edge;
-typedef Alpha_shape_3::Cell_handle  Cell_handle;
-typedef Alpha_shape_3::Vertex_handle Vertex_handle;
-
-typedef Alpha_shape_3::Cell_circulator  Cell_circulator;
-
-typedef Alpha_shape_3::Locate_type Locate_type;
-
-typedef Alpha_shape_3::Cell_iterator  Cell_iterator;
-typedef Alpha_shape_3::Vertex_iterator  Vertex_iterator;
-typedef Alpha_shape_3::Edge_iterator  Edge_iterator;
-
-
-typedef Alpha_shape_3::Coord_type Coord_type;
-typedef Alpha_shape_3::Alpha_iterator Alpha_iterator;
-
-//---------------------------------------------------------------------
-
-void
-construct_alpha_shape(const std::list<Point> &V_p,
-		      Alpha_shape_3::Mode mode,
-		      Alpha_shape_3& A)
-  // Generate Alpha Shape
-{ 
-  std::vector<Segment> V_seg;
-  
-  int  n = A.make_alpha_shape(V_p.begin(), V_p.end());
-  std::cout << "Inserted " << n  << " points" << std::endl;
-  
-  A.set_mode(mode);
-}
-
-bool
-file_input(std::list<Point>& L)
-{
-
-  std::ifstream is("./data/fin", std::ios::in);
-
-  if(is.fail())
-    {
-      std::cerr << "unable to open file for input" << std::endl;
-      return false;
-    }
-
-  CGAL::set_ascii_mode(is);
-
-  int n;
-  is >> n;
-  std::cout << "Reading " << n << " points" << std::endl;
-  Point p;
-  for( ; n>0 ; n--)
-    {
-      is >> p;
-      L.push_back(p);
-    }
-  std::cout << "Points inserted" << std::endl;
-  return true;
-}
-
-void set_alpha(Alpha_shape_3& A, int alpha_index)
-{
-  // alpha corresponds to an index
-  if (A.number_of_alphas() > 0)
-  {
-    if (alpha_index < 100)
-      {
-	int n = (alpha_index * A.number_of_alphas())/ 100;
-	A.set_alpha(A.get_nth_alpha(n));
-      }
-    else
-      {
-	Alpha_shape_3::Alpha_iterator alpha_end_it = A.alpha_end();
-	A.set_alpha((*(--alpha_end_it))+1);
-      }
-  }
-  else
-    A.set_alpha(0);
-}
-    
-//------------------ main -------------------------------------------
+typedef K::Point_3                                  Point;
+typedef Alpha_shape_3::Alpha_iterator               Alpha_iterator;
 
 int main()
 {
+  std::list<Point> lp;
 
-  Alpha_shape_3 A;
+  //read input
+  std::ifstream is("./data/bunny_1000");
+  int n;   
+  is >> n;
+  std::cout << "Reading " << n << " points " << std::endl; 
+  Point p;
+  for( ; n>0 ; n--)    { 
+    is >> p;      
+    lp.push_back(p);
+  }
+  
 
+  // compute alpha shape  
+  Alpha_shape_3 as(lp.begin(),lp.end());
+  std::cout << "Alpha shape computed in REGULARIZED mode by defaut" 
+	    << std::endl;
 
-  std::list<Point> L;
-
-  file_input(L);  
-  construct_alpha_shape(L,Alpha_shape_3::GENERAL,A);
-
-  std::cout << "Alpha Shape computed" << std::endl;
-
-  int n(50);
-
-
-  if (n == 0)
-    A.set_alpha(*A.find_optimal_alpha(2));
-  else
-    set_alpha(A,n);      
-  //std::cout << A;
-
-
+  // find optimal alpha value
+  Alpha_iterator opt = as.find_optimal_alpha(1);
+  std::cout << "Optimal alpha value to get one connected component is " 
+	    <<  *opt    << std::endl;
+  as.set_alpha(*opt);
+  assert(as.number_of_solid_components() == 1);
   return 0;
 }
+
+
--- Packages/Alpha_shapes_3/examples/Alpha_shapes_3/example_weight.C
+++ Packages/Alpha_shapes_3/examples/Alpha_shapes_3/example_weight.C
@@ -1,150 +1,63 @@
-// file: examples/Alpha_shapes_3/example_weight.C
+// examples/Alpha_shapes_3/example_weight.C
 
-/***********************************************************************
-
-Takes a list of points and returns a list of segments corresponding to
-the Alpha Shape.
-
-************************************************************************/
 
 #include <CGAL/Simple_cartesian.h>
-#include <iostream>
-#include <fstream>
-#include <vector>
+#include <CGAL/MP_Float.h>
+#include <CGAL/Filtered_exact.h>
+
 #include <list>
 
-#include <CGAL/Alpha_shape_vertex_base_3.h>
-#include <CGAL/Triangulation_cell_base_3.h>
-#include <CGAL/Alpha_shape_cell_base_3.h>
-#include <CGAL/Triangulation_data_structure_3.h>
 #include <CGAL/Weighted_alpha_shape_euclidean_traits_3.h>
 #include <CGAL/Regular_triangulation_3.h>
 #include <CGAL/Alpha_shape_3.h>
 
-typedef double coord_type;
-
-typedef CGAL::Simple_cartesian<coord_type>  K;
-
-typedef K::Point_3    Point;
-typedef K::Segment_3  Segment;
+typedef CGAL::Filtered_exact<double, CGAL::MP_Float> NT;
+struct K : public CGAL::Simple_cartesian<NT> {};
 
 typedef CGAL::Weighted_alpha_shape_euclidean_traits_3<K> Gt;
-typedef Gt::Point Wpoint;
-typedef CGAL::Alpha_shape_vertex_base_3<Gt> Vb;
 
-typedef CGAL::Triangulation_cell_base_3<Gt> Df;
-typedef CGAL::Alpha_shape_cell_base_3<Gt, Df>  Fb;
-
+typedef CGAL::Alpha_shape_vertex_base_3<Gt>         Vb;
+typedef CGAL::Alpha_shape_cell_base_3<Gt>           Fb;
 typedef CGAL::Triangulation_data_structure_3<Vb,Fb> Tds;
-typedef CGAL::Regular_triangulation_3<Gt,Tds> Triangulation_3;
+typedef CGAL::Regular_triangulation_3<Gt,Tds>       Triangulation_3;
+typedef CGAL::Alpha_shape_3<Triangulation_3>        Alpha_shape_3;
 
-typedef CGAL::Alpha_shape_3<Triangulation_3>  Alpha_shape_3;
-
-typedef Alpha_shape_3::Cell  Cell;
-typedef Alpha_shape_3::Vertex Vertex;
-typedef Alpha_shape_3::Edge Edge;
-typedef Alpha_shape_3::Cell_handle  Cell_handle;
-typedef Alpha_shape_3::Vertex_handle Vertex_handle;
-
-typedef Alpha_shape_3::Cell_circulator  Cell_circulator;
-
-typedef Alpha_shape_3::Locate_type Locate_type;
-
-typedef Alpha_shape_3::Cell_iterator  Cell_iterator;
-typedef Alpha_shape_3::Vertex_iterator  Vertex_iterator;
-typedef Alpha_shape_3::Edge_iterator  Edge_iterator;
-
-
-typedef Alpha_shape_3::Coord_type Coord_type;
-typedef Alpha_shape_3::Alpha_iterator Alpha_iterator;
-
-//---------------------------------------------------------------------
-
-void
-construct_alpha_shape(const std::list<Wpoint> &V_p,
-		      Alpha_shape_3::Mode mode,
-		      Alpha_shape_3& A)
-  // Generate Alpha Shape
-{ 
-  std::vector<Segment> V_seg;
-  
-  int  n = A.make_alpha_shape(V_p.begin(), V_p.end());
-  std::cout << "Inserted " << n  << " points" << std::endl;
-  
-  A.set_mode(mode);
-}
-
-bool
-file_input(std::list<Wpoint>& L)
-{
-
-  std::ifstream is("./data/fin", std::ios::in);
-
-  if(is.fail())
-    {
-      std::cerr << "unable to open file for input" << std::endl;
-      return false;
-    }
-
-  CGAL::set_ascii_mode(is);
-
-  int n;
-  is >> n;
-  std::cout << "Reading " << n << " points" << std::endl;
-  Point p;
-  for( ; n>0 ; n--)
-    {
-      is >> p;
-      L.push_back(Wpoint(p,5));
-    }
-  std::cout << "Points inserted" << std::endl;
-  return true;
-}
-
-void set_alpha(Alpha_shape_3& A, int alpha_index)
-{
-  // alpha corresponds to an index
-  if (A.number_of_alphas() > 0)
-  {
-    if (alpha_index < 100)
-      {
-	int n = (alpha_index * A.number_of_alphas())/ 100;
-	A.set_alpha(A.get_nth_alpha(n));
-      }
-    else
-      {
-	Alpha_shape_3::Alpha_iterator alpha_end_it = A.alpha_end();
-	A.set_alpha((*(--alpha_end_it))+1);
-      }
-  }
-  else
-    A.set_alpha(0);
-}
-    
-//------------------ main -------------------------------------------
+typedef Alpha_shape_3::Cell_handle          Cell_handle;
+typedef Alpha_shape_3::Vertex_handle        Vertex_handle;
+typedef Alpha_shape_3::Facet                Facet;
+typedef Alpha_shape_3::Edge                 Edge;
+typedef Gt::Weighted_point                  Weighted_point;
+typedef Gt::Bare_point                      Bare_point;
 
 int main()
 {
+  std::list<Weighted_point> lwp;
 
-  Alpha_shape_3 A;
+  //read input : build a small protein
+  lwp.push_back(Weighted_point(Bare_point( 1, -1, -1), 4));
+  lwp.push_back(Weighted_point(Bare_point(-1,  1, -1), 4));
+  lwp.push_back(Weighted_point(Bare_point(-1, -1,  1), 4));
+  lwp.push_back(Weighted_point(Bare_point( 1,  1,  1), 4));
+  lwp.push_back(Weighted_point(Bare_point( 2,  2,  2), 1));
 
+  //buil alpha_shape  in GENERAL mode and set alpha=0
+  Alpha_shape_3  as(lwp.begin(), lwp.end(), 0, Alpha_shape_3::GENERAL);
 
-  std::list<Wpoint> L;
-
-  file_input(L);  
-  construct_alpha_shape(L,Alpha_shape_3::GENERAL,A);
-
-  std::cout << "Alpha Shape computed" << std::endl;
-
-  int n(50);
-
-
-  if (n == 0)
-    A.set_alpha(*A.find_optimal_alpha(2));
-  else
-    set_alpha(A,n);      
-  //std::cout << A;
-
-
+  //explore the 0-shape - It is dual to the boundary of the union.
+  std::list<Cell_handle> cells;
+  std::list<Facet>       facets;
+  std::list<Edge>        edges;
+  as.get_alpha_shape_cells(std::back_inserter(cells), 
+			   Alpha_shape_3::INTERIOR);
+  as.get_alpha_shape_facets(std::back_inserter(facets), 
+			    Alpha_shape_3::REGULAR);
+  as.get_alpha_shape_facets(std::back_inserter(facets), 
+			    Alpha_shape_3::SINGULAR);
+  as.get_alpha_shape_edges(std::back_inserter(edges), 
+			   Alpha_shape_3::SINGULAR);
+  std::cout << " The 0-shape has : " << std::endl;
+  std::cout << cells.size() << " interior tetrahedra" << std::endl;
+  std::cout << facets.size() << " boundary facets" << std::endl;
+  std::cout << edges.size()  << " singular edges" << std::endl;
   return 0;
 }
