8001676f76759649855ef4548cd22d4468a3088b
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2004-09-01 16:41:22 +0000

    - include all number type files from CGAL/number_type_basic.h when   CGAL_CFG_HAS_TWO_STAGE_NAME_LOOKUP is defined (g++ 3.4).

--- Packages/Number_types/include/CGAL/number_type_basic.h
+++ Packages/Number_types/include/CGAL/number_type_basic.h
@@ -39,7 +39,7 @@
 #include <cmath>
 #endif
 
-// CGAL uses std::min and std::max if feasible
+// CGAL uses std::min and std::max
 
 #include <algorithm>
 
@@ -52,9 +52,44 @@ CGAL_END_NAMESPACE
 
 #include <CGAL/Number_type_traits.h>
 #include <CGAL/number_utils.h>
-#include <CGAL/number_utils_classes.h>
 #include <CGAL/double.h>
 #include <CGAL/float.h>
 #include <CGAL/int.h>
+
+// Including all number type files is necessary for compilers implementing
+// two-stage name lookup (like g++ >= 3.4).
+// A nicer solution needs more thought.
+
+#ifdef CGAL_CFG_HAS_TWO_STAGE_NAME_LOOKUP
+
+#include <CGAL/Quotient.h>
+#include <CGAL/MP_Float.h>
+#include <CGAL/Lazy_exact_nt.h>
+#include <CGAL/Interval_arithmetic.h>
+#include <CGAL/Fixed_precision_nt.h>
+
+#ifdef CGAL_USE_GMP
+#include <CGAL/Gmpz.h>
+#include <CGAL/Gmpq.h>
+#endif
+
+#ifdef CGAL_USE_GMPXX
+#include <CGAL/gmpxx.h>
+#endif
+
+#ifdef CGAL_USE_CORE
+#include <CGAL/CORE_Expr.h>
+#endif
+
+#ifdef CGAL_USE_LEDA
+#include <CGAL/leda_integer.h>
+#include <CGAL/leda_rational.h>
+#include <CGAL/leda_bigfloat.h>
+#include <CGAL/leda_real.h>
+#endif
+
+#endif // CGAL_CFG_HAS_TWO_STAGE_NAME_LOOKUP
+
+#include <CGAL/number_utils_classes.h>
 
 #endif // CGAL_NUMBER_TYPE_BASIC_H
