1c73d399a006ee53803d4b4f5c46766425b6c3a0
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2004-09-03 17:26:51 +0000

    added 2004 as a copyright year in files that were modified in 2004

--- Packages/Apollonius_graph_2/include/CGAL/Arithmetic_filter/predicates/compare_quadratic.h
+++ Packages/Apollonius_graph_2/include/CGAL/Arithmetic_filter/predicates/compare_quadratic.h
@@ -1,4 +1,4 @@
-// Copyright (c) 1999,2000,2001  INRIA Sophia-Antipolis (France).
+// Copyright (c) 1999,2000,2001,2004  INRIA Sophia-Antipolis (France).
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org); you may redistribute it under
