db93c0d49fadd84808519fd9a3304c26de5b9e38
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2004-09-06 10:21:54 +0000

    - number_type_basic.h: added "#include<CGAL/_QP_solver/Double.h>";   this is needed for g++ 3.4

--- Packages/Number_types/include/CGAL/number_type_basic.h
+++ Packages/Number_types/include/CGAL/number_type_basic.h
@@ -361,10 +361,16 @@ Comparison_result compare(const CORE::Expr&, const CORE::Expr&);
 std::pair<double,double> to_interval (const CORE::Expr &);
 #endif // CGAL_USE_CORE
 
+// Integer and Double from _QP_solver
+
 CGAL_END_NAMESPACE
+#include <CGAL/_QP_solver/Double.h>
+CGAL_BEGIN_NAMESPACE
+
 
 // specializations for Quotient
 
+CGAL_END_NAMESPACE
 #include <CGAL/Quotient.h>
 
 #ifdef CGAL_USE_GMP
