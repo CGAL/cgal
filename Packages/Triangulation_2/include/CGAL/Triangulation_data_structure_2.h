22bafd346115860f135886221031a4784a857e0a
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2004-09-06 20:12:02 +0000

    - Code: the methods insert_degree_2 and remove_degree_2 where moved from   Apollonius_graph_data_structure_2 (obsolete) to   Triangulation_data_structure_2. - re-wrote author names in copyright notices using 7-bit characters

--- Packages/Triangulation_2/include/CGAL/Triangulation_data_structure_2.h
+++ Packages/Triangulation_2/include/CGAL/Triangulation_data_structure_2.h
@@ -355,7 +355,11 @@ public:
   inline Vertex_handle join_vertices(Edge_circulator ec) {
     return join_vertices(*ec);
   }
-  
+
+  // insert_degree_2 and remove_degree_2 operations
+  Vertex_handle insert_degree_2(Face_handle f, int i);
+  void remove_degree_2(Vertex_handle v);
+
   // CHECKING
   bool is_valid(bool verbose = false, int level = 0) const;
   
@@ -1597,6 +1601,101 @@ join_vertices(Face_handle f, int i, Vertex_handle v)
   CGAL_triangulation_expensive_postcondition( is_valid() );
 
   return v1;
+}
+
+// insert_degree_2 and remove_degree_2 operations
+template <class Vb, class Fb>
+typename Triangulation_data_structure_2<Vb,Fb>::Vertex_handle
+Triangulation_data_structure_2<Vb,Fb>::
+insert_degree_2(Face_handle f, int i)
+{
+  /*
+  // This method basically does the following transformation
+  // The remove_degree_2 method performs the same operation in the
+  // opposite direction
+  //
+  //
+  //                                                *
+  //                 i                             / \
+  //                 *                            /   \
+  //                / \                          /  f  \
+  //               /   \                        / _____	\
+  //              /  f  \                      / /  f1 \ \
+  //             /       \                     |/   v   \|
+  //  v0=ccw(i) *---------* v1=cw(i)  ===>  v0 *----*----* v1
+  //             \       /                     |\   f2  /|
+  //              \  g  /                      \ \_____/ /
+  //               \   /                        \       /
+  //                \ /                          \  g  /
+  //                 *                            \   /
+  //                 j                             \ /
+  //                                                *
+  //
+  */
+
+  Face_handle g = f->neighbor(i);
+  int j = f->mirror_index(i);
+
+  Vertex_handle  v = create_vertex();
+
+  Vertex_handle v0 = f->vertex( ccw(i) );
+  Vertex_handle v1 = f->vertex( cw(i)  );
+
+  Face_handle f_undef;
+
+  Face_handle f1 = create_face(v0, v, v1, f_undef, f, f_undef);
+  Face_handle f2 = create_face(v0, v1, v, f_undef, f_undef, g);
+
+  f1->set_neighbor(0, f2);
+  f1->set_neighbor(2, f2);
+
+  f2->set_neighbor(0, f1);
+  f2->set_neighbor(1, f1);
+
+  f->set_neighbor(i, f1);
+  g->set_neighbor(j, f2);
+
+  v->set_face(f1);
+
+  return v;
+}
+
+template <class Vb, class Fb>
+void
+Triangulation_data_structure_2<Vb,Fb>::
+remove_degree_2(Vertex_handle v)
+{
+  CGAL_precondition( v->degree() == 2 );
+
+  Face_handle f1 = v->face();
+  int i = f1->index(v);
+
+  Face_handle f2 = f1->neighbor( ccw(i) );
+  int j = f2->index(v);
+
+  Face_handle ff1 = f1->neighbor( i );
+  Face_handle ff2 = f2->neighbor( j );
+
+  int id1 = f1->mirror_index(i);
+  int id2 = f2->mirror_index(j);
+
+  ff1->set_neighbor(id1, ff2);
+  ff2->set_neighbor(id2, ff1);
+
+  Vertex_handle v1 = f1->vertex( ccw(i) );
+  //    if ( v1->face() == f1 || v1->face() == f2 ) {
+  v1->set_face(ff1);
+  //    }
+
+  Vertex_handle v2 = f1->vertex( cw(i) );
+  //    if ( v2->face() == f1 || v2->face() == f2 ) {
+  v2->set_face(ff2);
+  //    }
+
+  delete_face(f1);
+  delete_face(f2);
+
+  delete_vertex(v);
 }
 
 // CHECKING
