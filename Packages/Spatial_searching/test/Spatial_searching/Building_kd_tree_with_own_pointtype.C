8e5435c7548d64609b4f71c0f1715c4f552f7db1
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2004-09-15 13:10:49 +0000

    Added assertions

--- Packages/Spatial_searching/test/Spatial_searching/Building_kd_tree_with_own_pointtype.C
+++ Packages/Spatial_searching/test/Spatial_searching/Building_kd_tree_with_own_pointtype.C
@@ -1,40 +1,55 @@
+//file: test/Spatial_searching/Building_kd_tree_with_own_pointtype.C
+
 #include <CGAL/basic.h>
-#include <vector>
-#include <iostream>
-
-#include <CGAL/Kd_tree.h>
-#include <CGAL/Kd_tree_traits_point.h>
+#include <CGAL/Search_traits.h>
 #include <CGAL/point_generators_3.h>
-#include <CGAL/algorithm.h>
+#include <CGAL/Orthogonal_k_neighbor_search.h>
+#include "../../examples/Spatial_searching/Point.h"
+#include "../../examples/Spatial_searching/Distance.h"
 
-#include "../../examples/Spatial_searching/Point.h" 
-
-
-typedef CGAL::Kd_tree_traits_point<double, Point, const double*, Construct_coord_iterator> Traits;
-typedef CGAL::Kd_tree<Traits> Tree;
-typedef Tree::Splitter Splitter;
-typedef CGAL::Creator_uniform_3<double,Point> Creator;
+typedef CGAL::Random_points_in_cube_3<Point> Random_points_iterator;
+typedef CGAL::Counting_iterator<Random_points_iterator> N_Random_points_iterator;
+typedef CGAL::Search_traits<double, Point, const double*, Construct_coord_iterator> Traits;
+typedef CGAL::Orthogonal_k_neighbor_search<Traits, Distance> K_neighbor_search;
+typedef K_neighbor_search::Tree Tree;
 
 int main() {
-
-  int bucket_size=10;
+  const int N = 1000;
+  const int K = 5;
+  // generator for random data points in the cube ( (-1,-1,-1), (1,1,1) ) 
+  Random_points_iterator rpit( 1.0);
   
-  const int data_point_number=1000;
-  
-  typedef std::list<Point> point_list;
-  point_list data_points;
-  
-  CGAL::Random_points_in_cube_3<Point,Creator> g( 1.0);
-  CGAL::copy_n( g, data_point_number, std::back_inserter(data_points));
+  std::vector<Point> points(N_Random_points_iterator(rpit,0),
+			    N_Random_points_iterator(N));
+  // Insert number_of_data_points in the tree
+  Tree tree(points.begin(), points.end());
 
-  Splitter split(bucket_size);
-  Tree d(data_points.begin(), data_points.end(), split);
+  Point query(0.0, 0.0, 0.0);
+  Distance tr_dist;
 
-  std::cout << "created kd tree using splitting rule "  
-  << data_point_number << " points. " << std::endl;
-  d.statistics(std::cout);
+  // search K nearest neighbours
+  K_neighbor_search search(tree, query, K);
+
+  // do checking
+  double dist = 0;
+  std::vector<Point> result;
+
+  for(K_neighbor_search::iterator it = search.begin();
+      it != search.end();
+      it++){
+    result.push_back(it->first);
+    if(CGAL::to_double(it->second) > dist) dist = CGAL::to_double(it->second);
+  }
+
+  assert(result.size() == K);
+  for(std::vector<Point>::iterator it = points.begin();
+      it != points.end();
+      it++){
+    if( std::find(result.begin(), result.end(), *it) == result.end()){
+      Distance d;
+      assert(d.transformed_distance(query, *it) >= dist);
+    }
+  }
+  std::cout << "done" << std::endl;
   return 0;
-
-};
-
-
+}
