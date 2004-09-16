8cc6661d9bd600f3303e31f7cef6631188bd9336
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2004-09-16 14:53:01 +0000

    Added an end of iterator range test

--- Packages/Spatial_searching/examples/Spatial_searching/Distance_browsing.C
+++ Packages/Spatial_searching/examples/Spatial_searching/Distance_browsing.C
@@ -13,35 +13,32 @@ typedef NN_incremental_search::Tree Tree;
 
 // A functor that returns true, iff the x-coordinate of a dD point is not positive
 struct X_not_positive {
-  bool operator()(const NN_iterator& it) { return ((*it).first)[0]<=0;  }
+  bool operator()(const NN_iterator& it) { return ((*it).first)[0]<0;  }
 };
 
 // An iterator that only enumerates dD points with positive x-coordinate
 typedef CGAL::Filter_iterator<NN_iterator, X_not_positive> NN_positive_x_iterator;
 
 int main() {
-  const int D = 2;
-  const int N = 1;
 
-  std::list<Point_d> points;
-  points.push_back(Point_d(0,0));
-  points.push_back(Point_d(1,1));
-  points.push_back(Point_d(0,1));
-  points.push_back(Point_d(10,110));
-  points.push_back(Point_d(45,0));
-  points.push_back(Point_d(0,2340));
-  points.push_back(Point_d(0,30));
-
-  Tree tree(points.begin(), points.end());
+  Tree tree;
+  tree.insert(Point_d(0,0));
+  tree.insert(Point_d(1,1));
+  tree.insert(Point_d(0,1));
+  tree.insert(Point_d(10,110));
+  tree.insert(Point_d(45,0));
+  tree.insert(Point_d(0,2340));
+  tree.insert(Point_d(0,30));
   
   Point_d query(0,0);
 
   NN_incremental_search NN(tree, query);
-  NN_positive_x_iterator it(NN.end(), X_not_positive(), NN.begin());
-  
+  NN_positive_x_iterator it(NN.end(), X_not_positive(), NN.begin()), end(NN.end(), X_not_positive());
+
   std::cout <<  "The first 5 nearest neighbours with positive x-coord are: " << std::endl;
-  for (int j=0; j < 5; ++j,++it) 
-    std::cout <<   (*it).first << "; squared distance =   " << (*it).second << std::endl;
+  for (int j=0; (j < 5)&&(it!=end); ++j,++it) 
+    std::cout <<   (*it).first << "  at squared distance = " << (*it).second << std::endl;
+
   return 0;
 }
 
