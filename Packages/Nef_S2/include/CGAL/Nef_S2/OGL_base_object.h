dab8449a676bc56bb98c0e8327eef09c9335a501
Peter Hachenberger <hachenberger@mpi-inf.mpg.de>
2004-09-17 10:42:33 +0000

    correct stupid name error

--- Packages/Nef_S2/include/CGAL/Nef_S2/OGL_base_object.h
+++ Packages/Nef_S2/include/CGAL/Nef_S2/OGL_base_object.h
@@ -1,7 +1,7 @@
 #ifndef CGAL_OGL_BASE_OBJECT_H
 #define CGAL_OGL_BASE_OBJECT_H
 
-#include <CGAL/basic.h>
+#include <CGAL/Simple_cartesian.h>
 
 CGAL_BEGIN_NAMESPACE
 
@@ -10,11 +10,11 @@ namespace OGL {
   class OGL_base_object {    
   public:
 
-    typedef CGAL::Simple_cartesian<double> Double_kernel;  
-    typedef DKernel::Point_3               Double_point;
-    typedef DKernel::Vector_3              Double_vector;
-    typedef DKernel::Segment_3             Double_segment;
-    typedef DKernel::Aff_transformation_3  Affine_3;
+    typedef CGAL::Simple_cartesian<double>       Double_kernel;  
+    typedef Double_kernel::Point_3               Double_point;
+    typedef Double_kernel::Vector_3              Double_vector;
+    typedef Double_kernel::Segment_3             Double_segment;
+    typedef Double_kernel::Aff_transformation_3  Affine_3;
 
     virtual void draw() const  = 0;
     virtual void init() = 0;
