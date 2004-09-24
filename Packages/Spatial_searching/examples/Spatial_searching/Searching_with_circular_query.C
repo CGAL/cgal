954fd213f191055a533f3648aa202b845f2cbd10
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2004-09-24 08:00:53 +0000

    A better example

--- Packages/Spatial_searching/examples/Spatial_searching/Searching_with_circular_query.C
+++ Packages/Spatial_searching/examples/Spatial_searching/Searching_with_circular_query.C
@@ -1,50 +1,57 @@
 // file: examples/Spatial_searching/Searching_with_circular_query.C
 
-#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Cartesian.h>
 #include <CGAL/Kd_tree.h>
+#include <CGAL/Search_traits_2.h>
 #include <CGAL/point_generators_2.h>
 #include <CGAL/algorithm.h>
 #include <CGAL/Fuzzy_sphere.h>
-#include <CGAL/Search_traits_2.h>
-#include <iostream>
 
-typedef CGAL::Simple_cartesian<double> K;
-typedef K::Point_2 Point_d;
-typedef CGAL::Random_points_in_square_2<Point_d> Random_points_iterator;
+typedef CGAL::Cartesian<double> K;
+typedef K::Point_2 Point;
+typedef CGAL::Random_points_in_square_2<Point> Random_points_iterator;
 typedef CGAL::Counting_iterator<Random_points_iterator> N_Random_points_iterator;
 typedef CGAL::Search_traits_2<K> Traits;
-typedef CGAL::Kd_tree<Traits> Tree;  
 typedef CGAL::Fuzzy_sphere<Traits> Fuzzy_circle;
-
+typedef CGAL::Kd_tree<Traits> Tree;
+  
 int main() {
-  const int N = 160;
-  
-  std::list<Point_d> points;
-  points.push_back(Point_d(0,0));
 
-  Tree tree(points.begin(), points.end());
- 
-  std::list<Point_d> result;
-  
-  // define range query
-  Point_d center(0.2, 0.2);
+  const int N=1000;
 
-  // Searching a circle centered at c with radius 0.2
+  // generator for random data points in the square ( (-1,-1), (1,1) ) 
+  Random_points_iterator rpit( 1.0);
 
-  // exact range searching using default value 0.0 for fuzziness paramater
+  // Insert also the N points in the tree
+  Tree tree(N_Random_points_iterator(rpit,0),
+			      N_Random_points_iterator(N));
+
+  // define exact circular range query
+  Point center(0.2, 0.2);
   Fuzzy_circle exact_range(center, 0.2);
+    
+  std::list<Point> result;
   tree.search(std::back_inserter( result ), exact_range);
- 
-  std::cout << "The points in the circle centered at (0.2,0.2) with radius 0.2 are: " << std::endl;
-  std::copy (result.begin(),result.end(),std::ostream_iterator<Point_d>(std::cout,"\n") );
+  
+  std::cout << "\nPoints in cirle with center " << center << " and radius 0.2" << std::endl;
 
+  std::list<Point>::iterator it;
+  for (it=result.begin(); (it != result.end()); ++it) {
+    std::cout << *it << std::endl;
+  }
+  
   result.clear();
   // approximate range searching using value 0.1 for fuzziness parameter
   Fuzzy_circle approximate_range(center, 0.2, 0.1);
-  tree.search(std::back_inserter( result ), approximate_range);
 
-  std::cout << "\nThe points in the fuzzy circle centered at (0.2,0.2) with fuzzy radius (0.1,0.3) are: " 
-	    << std::endl;
-  std::copy (result.begin(),result.end(),std::ostream_iterator<Point_d>(std::cout,"\n") );
+  tree.search(std::back_inserter( result ), approximate_range);
+   
+  std::cout << "\n\nPoints in cirle with center " << center << " and fuzzy radius [0.1,0.3]" << std::endl;
+
+  for (it=result.begin(); (it != result.end()); ++it) {
+    std::cout << *it << std::endl;
+  }
+  std::cout << "\ndone" << std::endl;
   return 0;
 }
+
