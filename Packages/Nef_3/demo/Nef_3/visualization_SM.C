a378c4dfe95bc9e3dd3129d30beb9397968c2bf6
Peter Hachenberger <hachenberger@mpi-inf.mpg.de>
2004-09-28 10:12:15 +0000

    made QT ready

--- Packages/Nef_3/demo/Nef_3/visualization_SM.C
+++ Packages/Nef_3/demo/Nef_3/visualization_SM.C
@@ -1,10 +1,16 @@
 // file: examples/Nef_3/visualization_SM.C
 
+#ifndef CGAL_USE_QT
+#include <iostream>
+int main(int, char*){
+  std::cout << "Sorry, this demo needs QT..." << std::endl; return 0;}
+#else
 #include <CGAL/Gmpz.h>
 #include <CGAL/Homogeneous.h>
 #include <CGAL/Nef_polyhedron_3.h>
 #include <CGAL/IO/Nef_polyhedron_iostream_3.h>
-#include <CGAL/IO/Nef_polyhedron_S2_OGLUT_stream.h>
+#include <CGAL/IO/Qt_widget_Nef_S2.h>
+#include <qapplication.h>
 
 typedef CGAL::Homogeneous<CGAL::Gmpz> Kernel;
 typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron_3;
@@ -12,15 +18,17 @@ typedef Nef_polyhedron_3::Vertex_const_iterator Vertex_const_iterator;
 typedef Nef_polyhedron_3::Nef_polyhedron_S2 Nef_polyhedron_S2;
 
 
-int main() {
+int main(int argc, char* argv[]) {
   Nef_polyhedron_3 N;
   std::cin >> N;
-
   Vertex_const_iterator v = N.vertices_begin();
   Nef_polyhedron_S2 S(N.get_sphere_map(v));
-  CGAL::ogl << S;
-  CGAL::ogl << "Visualization example";
-  CGAL::ogl.display();
 
-  return 0;
+  QApplication a(argc, argv);
+  Qt_widget_Nef_S2<Nef_polyhedron_S2>* w = 
+    new Qt_widget_Nef_S2<Nef_polyhedron_S2>(S);
+  a.setMainWidget(w);
+  w->show();
+  return a.exec();
 }
+#endif
--- Packages/Nef_3/demo/Nef_3/visualization_SNC.C
+++ Packages/Nef_3/demo/Nef_3/visualization_SNC.C
@@ -1,22 +1,29 @@
 // file: examples/Nef_3/visualization_SNC.C
 
+#ifndef CGAL_USE_QT
+#include <iostream>
+int main(int, char*){
+  std::cout << "Sorry, this demo needs QT..." << std::endl; return 0;}
+#else
 #include <CGAL/Gmpz.h>
 #include <CGAL/Homogeneous.h>
 #include <CGAL/Nef_polyhedron_3.h>
-#include <CGAL/Nef_3/Visualizor.h>
 #include <CGAL/IO/Nef_polyhedron_iostream_3.h>
+#include <CGAL/IO/Qt_widget_Nef_3.h>
+#include <qapplication.h>
 
 typedef CGAL::Homogeneous<CGAL::Gmpz> Kernel;
 typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron_3;
-typedef CGAL::Visualizor_OpenGL_3<Nef_polyhedron_3> Visualizor;
 
-int main() {
+int main(int argc, char* argv[]) {
   Nef_polyhedron_3 N;
   std::cin >> N;
 
-  Visualizor V(N);
-  V.draw();
-  CGAL::OGL::start_viewer();  // returns after viewer terminates 
-
-  return 0;
+  QApplication a(argc, argv);
+  Qt_widget_Nef_3<Nef_polyhedron_3>* w = 
+    new Qt_widget_Nef_3<Nef_polyhedron_3>(N);
+  a.setMainWidget(w);
+  w->show();
+  return a.exec();
 }
+#endif
