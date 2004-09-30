789d4fc345e7665e3051c78642278850639b8a8c
Peter Hachenberger <hachenberger@mpi-inf.mpg.de>
2004-09-30 16:14:00 +0000

    added #ifdef CGAL_USE_QT

--- Packages/Nef_S2/examples/Nef_S2/visualization.C
+++ Packages/Nef_S2/examples/Nef_S2/visualization.C
@@ -1,5 +1,10 @@
 // examples/Nef_S2/visualization.C
 
+#ifndef CGAL_USE_QT
+#include <iostream>
+int main(int, char*){
+  std::cout << "Sorry, this demo needs QT..." << std::endl; return 0;}
+#else
 #include <CGAL/Gmpz.h>
 #include <CGAL/Homogeneous.h>
 #include <CGAL/Nef_polyhedron_S2.h>
@@ -23,5 +28,5 @@ int main(int argc, char* argv[]) {
   w->show();
   return a.exec();
 }
-
+#endif
 
