eaba5369094819fe84c25ba90e408ecb700cd2eb
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2004-10-11 15:48:22 +0000

    Added copyrights to demo files.

--- Packages/Mesh_2/demo/Mesh_2/Show_points.h
+++ Packages/Mesh_2/demo/Mesh_2/Show_points.h
@@ -1,3 +1,23 @@
+// Copyright (c) 2003-2004 INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Laurent Rineau
+
 #ifndef SHOW_POINTS_H
 #define SHOW_POINTS_H
 
