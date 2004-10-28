3273f358497b99b73cd53abfee82d4f52ddef0e4
Peter Hachenberger <hachenberger@mpi-inf.mpg.de>
2004-10-28 15:43:34 +0000

    *** empty log message ***

--- Packages/Nef_3/performance/Nef_3/tetrahedra_as_sat.C
+++ Packages/Nef_3/performance/Nef_3/tetrahedra_as_sat.C
@@ -11,6 +11,8 @@ typedef CGAL::Nef_polyhedron_3<Kernel> Nef_polyhedron_3;
 typedef CGAL::sat_writer<Nef_polyhedron_3> Sat_writer;
 typedef tetrahedron_generator<Kernel> tgen;
 
+bool cgal_nef3_timer_on = false;
+
 int main(int argc, char* argv[]) {
 
   assert(argc < 7);
@@ -18,14 +20,16 @@ int main(int argc, char* argv[]) {
   int nx = argc>1 ? std::atoi(argv[1]) : 2;
   int ny = argc>2 ? std::atoi(argv[2]) : 2;
   int nz = argc>3 ? std::atoi(argv[3]) : 2;
-  int s  = argc>4 ? std::atoi(argv[4]) : 5;
+  int s  = argc>4 ? std::atoi(argv[4]) : 100;
 
   std::ostringstream out;
-  tgen t(out);
-  //  if(argc>5)
-  //    t.create_tetrahedra(nx,ny,nz,s,std::atoi(argv[5]));
-  //  else 
-    t.create_tetrahedra(nx,ny,nz,s);
+  if(argc>5) {
+    tgen t(out,s,std::atoi(argv[5]));
+    t.create_tetrahedra(nx,ny,nz);
+  } else {
+    tgen t(out,s);    
+    t.create_tetrahedra(nx,ny,nz);
+  }
 
   std::istringstream in(out.str());
   Nef_polyhedron_3 N;
