e188eac7b9bf5508faaf5936001323e871dfa0e1
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2004-12-19 16:37:23 +0000

    Updated headers in demos

--- Packages/Partition_2/demo/Partition_2/partition_2_toolbar.C
+++ Packages/Partition_2/demo/Partition_2/partition_2_toolbar.C
@@ -1,22 +1,21 @@
-// ============================================================================
+// Copyright (c) 2002  Max Planck Institut fuer Informatik (Germany).
+// All rights reserved.
 //
-// Copyright (c) 1997-2000 The CGAL Consortium
-// This software and related documentation are part of the Computational
-// Geometry Algorithms Library (CGAL).
-// This software and documentation are provided "as-is" and without warranty
-// of any kind. In no event shall the CGAL Consortium be liable for any
-// damage of any kind. 
-// ----------------------------------------------------------------------
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
 //
-// file          : partition_2_toolbar.C
-// package       : Qt_widget
-// author(s)     : Radu Ursu
-// coordinator   : Laurent Rineau
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
 //
-// email         : contact@cgal.org
-// www           : http://www.cgal.org
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// ======================================================================
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Radu Ursu
 
 #ifdef CGAL_USE_QT
 
