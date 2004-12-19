425d1f063f1fbe89960d96af273c3fdf8315cc9e
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2004-12-19 16:42:36 +0000

    Updated headers in demos

--- Packages/Polygon/demo/Polygon/cgal_types.h
+++ Packages/Polygon/demo/Polygon/cgal_types.h
@@ -1,3 +1,22 @@
+// Copyright (c) 2002  ETH Zurich (Switzerland).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Radu Ursu
+
 #include <CGAL/basic.h>
 #include <CGAL/Cartesian.h>
 #include <CGAL/Polygon_2.h>
