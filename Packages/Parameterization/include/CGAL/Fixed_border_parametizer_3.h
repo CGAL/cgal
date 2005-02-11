673c6f94d342ef5a30e8d1f56a753269e16886e9
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2005-02-11 11:13:23 +0000

    Removed OpenNL code (moved to separate package)

--- Packages/Parameterization/include/CGAL/Fixed_border_parametizer_3.h
+++ Packages/Parameterization/include/CGAL/Fixed_border_parametizer_3.h
@@ -22,11 +22,11 @@
 #define FIXED_BORDER_PARAMETIZER_3_H
 
 #include <CGAL/circulator.h>
+#include <OpenNL/linear_solver.h>
 
 #include "CGAL/Parametizer_3.h"
 #include "CGAL/Circular_border_parametizer_3.h"
 #include "CGAL/parameterization_assertions.h"
-#include "OpenNL/linear_solver.h"
 
 CGAL_BEGIN_NAMESPACE
 
