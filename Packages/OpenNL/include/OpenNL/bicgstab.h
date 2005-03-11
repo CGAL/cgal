18bfc5f69c5a245ab94c10d404bece95876d7123
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2005-03-11 15:45:39 +0000

    Various changes to improve convergence with parameterization methods other than LSCM: - epsilon=1e-4 instead of 1e-6 - max_iter=10*N instead of 5*N - to find floating points numbers almost equal to 0.0, compare to 10*DBL_MIN instead of 1e-40 - added assert to check divisions by 0 - added debug traces

--- Packages/OpenNL/include/OpenNL/bicgstab.h
+++ Packages/OpenNL/include/OpenNL/bicgstab.h
@@ -41,15 +41,25 @@
  *  Laurent Saboret 01/2005: Change for CGAL:
  *		- Added OpenNL namespace
  *		- solve() returns true on success
+ *		- test divisions by zero
  */
 
 #ifndef __BICGSTAB__
 #define __BICGSTAB__
 
 #include <cmath>
+#include <cfloat>
+#include <climits>
 #include <cassert>
 
 namespace OpenNL {
+
+
+// Utility macro to display a variable's value
+// Usage: x=3.7; cerr << STREAM_TRACE(x) << endl;
+//        prints
+//        x=3.7
+#define STREAM_TRACE(var) #var << "=" << var << " "
 
 
 /**
@@ -78,7 +88,8 @@ public:
     typedef typename Vector::CoeffType CoeffType ;
 
     Solver_BICGSTAB() {
-        epsilon_ = 1e-6 ;
+		// LS 03/2005: change epsilon from 1e-6 to 1e-4 to parameterize venus-loop.off w/ authalic/square method
+        epsilon_ = 1e-4 ;										
         max_iter_ = 0 ;
     }
 
@@ -102,7 +113,8 @@ public:
         unsigned int n = A.dimension() ;						// (Square) matrix dimension
         unsigned int max_iter = max_iter_ ;						// Max number of iterations
         if(max_iter == 0) {
-            max_iter = 5 * n ;
+			// LS 03/2005: change max_iter from 5*n to 10*n to parameterize venus-loop.off w/ authalic/square method
+            max_iter = 10*n ;	
         }
         Vector rT(n) ;											// Initial residue rT=Ax-b
         Vector d(n) ;
@@ -120,47 +132,64 @@ public:
         BLAS<Vector>::axpy(-1,b,r);
 
 	    // Initially, d=h=rT=r=Ax-b
-        BLAS<Vector>::copy(r,d);
-        BLAS<Vector>::copy(d,h);
-        BLAS<Vector>::copy(h,rT);
-        assert( BLAS<Vector>::dot(rT,rT)>1e-40 );
+        BLAS<Vector>::copy(r,d);								// d = r
+        BLAS<Vector>::copy(d,h);								// h = d
+        BLAS<Vector>::copy(h,rT);								// rT = h
+        assert( ! IsZero( BLAS<Vector>::dot(rT,rT) ) );
 
-		rTh=BLAS<Vector>::dot(rT,h);							// (rT|h)
-        rTr=BLAS<Vector>::dot(r,r);								// Current error (r|r)
+		rTh=BLAS<Vector>::dot(rT,h);							// rTh = (rT|h)
+        rTr=BLAS<Vector>::dot(r,r);								// Current error rTr = (r|r)
 
-        while ( rTr>err && its < max_iter) {
-            mult(A,d,Ad);
-            rTAd=BLAS<Vector>::dot(rT,Ad);
-            assert( fabs(rTAd)>1e-40 );
-            alpha=rTh/rTAd;
-            BLAS<Vector>::axpy(-alpha,Ad,r);
+		while ( rTr>err && its < max_iter) 
+		{
+			mult(A,d,Ad);										// Ad = A*d
+            rTAd=BLAS<Vector>::dot(rT,Ad);						// rTAd = (rT|Ad)
+			assert( ! IsZero(rTAd) );
+			alpha=rTh/rTAd;										// alpha = rTh/rTAd
+            BLAS<Vector>::axpy(-alpha,Ad,r);					// r = r - alpha*Ad
             BLAS<Vector>::copy(h,s);
-            BLAS<Vector>::axpy(-alpha,Ad,s);
-            mult(A,s,t);
+            BLAS<Vector>::axpy(-alpha,Ad,s);					// h = h - alpha*Ad
+            mult(A,s,t);										// t = A*h
             BLAS<Vector>::axpy(1,t,u);
             BLAS<Vector>::scal(alpha,u);
-            st=BLAS<Vector>::dot(s,t);
-            tt=BLAS<Vector>::dot(t,t);
-            if ( fabs(st)<1e-40 || fabs(tt)<1e-40 )
+            st=BLAS<Vector>::dot(s,t);							// st = (h|t)
+            tt=BLAS<Vector>::dot(t,t);							// tt = (t|t)
+			if ( IsZero(st) || IsZero(tt) )
                 omega = 0 ;
 		    else
-		      omega = st/tt;
-            BLAS<Vector>::axpy(-omega,t,r);
-            BLAS<Vector>::axpy(-alpha,d,x);
-            BLAS<Vector>::axpy(-omega,s,x);
+	      		omega = st/tt;									// omega = st/tt
+            BLAS<Vector>::axpy(-alpha,d,x);						// x = x - alpha*d
+            BLAS<Vector>::axpy(-omega,s,x);						// x = x - omega*h
             BLAS<Vector>::copy(s,h);
-            BLAS<Vector>::axpy(-omega,t,h);
-            beta=(alpha/omega)/rTh; rTh=BLAS<Vector>::dot(rT,h); beta*=rTh;
+            BLAS<Vector>::axpy(-omega,t,r);						// r = r - omega*t
+	        rTr=BLAS<Vector>::dot(r,r);							// Current error rTr = (r|r)
+            BLAS<Vector>::axpy(-omega,t,h);						// h = h - omega*t
+			if( IsZero(omega) )									// LS 03/2005: avoid division by zero 
+				break;											// stop solver
+			if( IsZero(rTh) )									// LS 03/2005: avoid division by zero 
+				break;											// stop solver
+            beta=(alpha/omega)/rTh; 
+			rTh=BLAS<Vector>::dot(rT,h); 						// rTh = (rT|h)
+			beta*=rTh;											// beta = (rTh / previous rTh) * (alpha / omega)
             BLAS<Vector>::scal(beta,d);
             BLAS<Vector>::axpy(1,h,d);
             BLAS<Vector>::axpy(-beta*omega,Ad,d);
-            rTr=BLAS<Vector>::dot(r,r);
             its++ ;
         }
 
 		bool success = (rTr <= err);
+		std::cerr << STREAM_TRACE(success) << "(" << STREAM_TRACE(its) << STREAM_TRACE(max_iter) << STREAM_TRACE(rTr) << STREAM_TRACE(err) << STREAM_TRACE(omega) << STREAM_TRACE(rTh) << ")" << std::endl;
 		return success;
     }
+
+private:
+	// Test if a floating point number is (close to) 0.0
+	static inline bool IsZero(CoeffType a) 
+	{
+		// LS 03/2005: replace Lsolver test by Laspack test to parameterize venus-loop.off w/ authalic/square method
+		//#define IsZero(a) (fabs(a) < 1e-40)
+		return (fabs(a) < 10.0 * numeric_limits<CoeffType>::min());
+	}
 
 private:
     CoeffType epsilon_ ;
