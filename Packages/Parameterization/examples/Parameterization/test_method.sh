f93d78cb30ae73c1fe17173485fdfedd1983a027
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2005-03-21 10:38:30 +0000

    Redirect stderr to stdout

--- Packages/Parameterization/examples/Parameterization/test_method.sh
+++ Packages/Parameterization/examples/Parameterization/test_method.sh
@@ -9,27 +9,27 @@ echo "                                     *                                    
 echo "                                    ***                                    "
 echo "***************************************************************************"
 
-./test.sh "$1" "$2" blech
-./test.sh "$1" "$2" cone
-# ./test.sh "$1" "$2" crater
-./test.sh "$1" "$2" cyl
-./test.sh "$1" "$2" david
-./test.sh "$1" "$2" fandisk-top
-./test.sh "$1" "$2" foot
-./test.sh "$1" "$2" hand
-./test.sh "$1" "$2" mannequin-devil
-./test.sh "$1" "$2" maxplanck
-./test.sh "$1" "$2" nefertiti
-./test.sh "$1" "$2" one_ring
-./test.sh "$1" "$2" oni
-./test.sh "$1" "$2" rect
-# ./test.sh "$1" "$2" rotor
-./test.sh "$1" "$2" S3-pet41-fixed
-./test.sh "$1" "$2" strate2
-./test.sh "$1" "$2" three_peaks
-./test.sh "$1" "$2" vdi_punch
-./test.sh "$1" "$2" venus-loop
-./test.sh "$1" "$2" vw1-remeshed
+./test.sh "$1" "$2" blech 2>&1
+./test.sh "$1" "$2" cone 2>&1
+# ./test.sh "$1" "$2" crater 2>&1
+./test.sh "$1" "$2" cyl 2>&1
+./test.sh "$1" "$2" david 2>&1
+./test.sh "$1" "$2" fandisk-top 2>&1
+./test.sh "$1" "$2" foot 2>&1
+./test.sh "$1" "$2" hand 2>&1
+./test.sh "$1" "$2" mannequin-devil 2>&1
+./test.sh "$1" "$2" maxplanck 2>&1
+./test.sh "$1" "$2" nefertiti 2>&1
+./test.sh "$1" "$2" one_ring 2>&1
+./test.sh "$1" "$2" oni 2>&1
+./test.sh "$1" "$2" rect 2>&1
+# ./test.sh "$1" "$2" rotor 2>&1
+./test.sh "$1" "$2" S3-pet41-fixed 2>&1
+./test.sh "$1" "$2" strate2 2>&1
+./test.sh "$1" "$2" three_peaks 2>&1
+./test.sh "$1" "$2" vdi_punch 2>&1
+./test.sh "$1" "$2" venus-loop 2>&1
+./test.sh "$1" "$2" vw1-remeshed 2>&1
 
 echo "***************************************************************************"
 echo "                                    ***                                    "
