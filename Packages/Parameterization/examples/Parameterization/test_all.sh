01cd4695ec77e53c100f96a0ad2af58206aaca93
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2005-03-23 14:19:28 +0000

    Print time

--- Packages/Parameterization/examples/Parameterization/test_all.sh
+++ Packages/Parameterization/examples/Parameterization/test_all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Intensive test: test all surface parameterization methods with all models in data folder
+date
 ./test_method.sh uniform square
 ./test_method.sh floater circle
 ./test_method.sh conformal circle
 ./test_method.sh authalic square
 ./test_method.sh lscm 2pts
+date
