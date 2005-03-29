222d882080d851bc2dc9da3dadd235264b45a6bc
Baruch Zukerman <baruchzu@post.tau.ac.il>
2005-03-29 11:10:20 +0000

    added CGAL header to file

--- Packages/Sweep_line_2/include/CGAL/Sweep_line_2/Sweep_line_2_impl_debug.C
+++ Packages/Sweep_line_2/include/CGAL/Sweep_line_2/Sweep_line_2_impl_debug.C
@@ -1,3 +1,22 @@
+// Copyright (c) 1997  Tel-Aviv University (Israel).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Baruch Zukerman <baruchzu@post.tau.ac.il>
+
 #ifndef SWEEP_LINE_2_IMPL_DEBUG_H
 #define SWEEP_LINE_2_IMPL_DEBUG_H
 
