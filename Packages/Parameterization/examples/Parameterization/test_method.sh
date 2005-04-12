e685193e569d6413cb0bd4844eb20d7eab9104c6
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2005-04-12 10:15:41 +0000

    New models set for parameterization with cut

--- Packages/Parameterization/examples/Parameterization/test_method.sh
+++ Packages/Parameterization/examples/Parameterization/test_method.sh
@@ -9,27 +9,23 @@ echo "                                     *                                    
 echo "                                    ***                                    "
 echo "***************************************************************************"
 
-./test.sh "$1" "$2" blech
+./test.sh "$1" "$2" camel
 ./test.sh "$1" "$2" cone
-# ./test.sh "$1" "$2" crater
-./test.sh "$1" "$2" cyl
 ./test.sh "$1" "$2" david
-./test.sh "$1" "$2" fandisk-top
 ./test.sh "$1" "$2" foot
-./test.sh "$1" "$2" hand
+./test.sh "$1" "$2" holes
 ./test.sh "$1" "$2" mannequin-devil
-./test.sh "$1" "$2" maxplanck
 ./test.sh "$1" "$2" nefertiti
+./test.sh "$1" "$2" octopus
 ./test.sh "$1" "$2" one_ring
 ./test.sh "$1" "$2" oni
 ./test.sh "$1" "$2" rect
-# ./test.sh "$1" "$2" rotor
-./test.sh "$1" "$2" S3-pet41-fixed
-./test.sh "$1" "$2" strate2
+./test.sh "$1" "$2" rotor
+./test.sh "$1" "$2" sphere966
 ./test.sh "$1" "$2" three_peaks
-./test.sh "$1" "$2" vdi_punch
-./test.sh "$1" "$2" venus-loop
+./test.sh "$1" "$2" torus_tri
 ./test.sh "$1" "$2" vw1-remeshed
+
 
 echo "***************************************************************************"
 echo "                                    ***                                    "
