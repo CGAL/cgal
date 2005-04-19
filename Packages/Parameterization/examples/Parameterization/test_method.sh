c511d11abf706d5fb0769887c4bcd369e258a5bb
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2005-04-19 16:17:04 +0000

    Remove .off test models to save space in CGAL releases

--- Packages/Parameterization/examples/Parameterization/test_method.sh
+++ Packages/Parameterization/examples/Parameterization/test_method.sh
@@ -9,22 +9,14 @@ echo "                                     *                                    
 echo "                                    ***                                    "
 echo "***************************************************************************"
 
-./test.sh "$1" "$2" camel
-./test.sh "$1" "$2" cone
-./test.sh "$1" "$2" david
-./test.sh "$1" "$2" foot
+# ./test.sh "$1" "$2" cube
 ./test.sh "$1" "$2" holes
 ./test.sh "$1" "$2" mannequin-devil
+./test.sh "$1" "$2" mask_cone
 ./test.sh "$1" "$2" nefertiti
-./test.sh "$1" "$2" octopus
-./test.sh "$1" "$2" one_ring
-./test.sh "$1" "$2" oni
-./test.sh "$1" "$2" rect
 ./test.sh "$1" "$2" rotor
 ./test.sh "$1" "$2" sphere966
 ./test.sh "$1" "$2" three_peaks
-./test.sh "$1" "$2" torus_tri
-./test.sh "$1" "$2" vw1-remeshed
 
 
 echo "***************************************************************************"
