2bbe23f645004d07b34485f1e387e79b884aa1d1
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2005-05-09 06:18:12 +0000

    new class Combine_mesh_visitor in include/CGAL/Mesher_level_visitors.h

--- Packages/Mesh_2/include/CGAL/Mesher_level_visitors.h
+++ Packages/Mesh_2/include/CGAL/Mesher_level_visitors.h
@@ -22,8 +22,44 @@
 
 namespace CGAL {
 
-struct Null_mesh_visitor {
-  const Null_mesh_visitor& previous_level() const { return *this; }
+template <typename Previous_level>
+class Null_mesh_visitor_level
+{
+  Previous_level previous;
+public:
+  typedef Previous_level Previous_visitor;
+
+  Null_mesh_visitor_level(Previous_level p) : previous(p)
+  {
+  }
+
+  Previous_level& previous_level()
+  {
+    return previous;
+  }
+
+  template <typename E, typename P>
+  void before_conflicts(E, P) const {}
+
+  template <typename E, typename P, typename Z>
+  void before_insertion(E, P, Z) const {}
+
+  template <typename V>
+  void after_insertion(V) const {}
+
+  template <typename E, typename P, typename Z>
+  void after_no_insertion(E, P, Z) const {}
+}; // end class Null_mesh_visitor_level
+
+class Null_mesh_visitor
+{
+public:
+  typedef Null_mesh_visitor Previous_visitor;
+
+  const Null_mesh_visitor& previous_level() const 
+  {
+    return *this;
+  }
 
   template <typename E, typename P>
   void before_conflicts(E, P) const {}
@@ -47,10 +83,15 @@ struct Combine_mesh_visitor {
   V1* v1;
   V2* v2;
 
+  Combine_mesh_visitor(V1* v1_, V2* v2_)
+    : v1(v1_), v2(v2_)
+  {
+  }
+
   Previous_visitor previous_level()
   { 
-    return Combine_mesh_visitor(v1->previous_level(),
-				v2->previous_level());
+    return Previous_visitor(&(v1->previous_level()),
+			    &(v2->previous_level()));
   }
 
   template <typename E, typename P>
