f3252135d103c431b1eb18058a71a823a4245ff7
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
2005-05-26 15:04:54 +0000

    a few debug functors

--- Packages/Curved_kernel/include/CGAL/Circular_arc_traits_tracer.h
+++ Packages/Curved_kernel/include/CGAL/Circular_arc_traits_tracer.h
@@ -34,7 +34,11 @@ namespace CGAL {
 // FIXME : should it be the responsibility of the tracer
 //         to add the Debug_id stuff ?
 template < typename Traits, int debug_level = 2 >
-class Circular_arc_traits_tracer {
+class Circular_arc_traits_tracer
+ : public Traits
+{
+
+  typedef Circular_arc_traits_tracer<Traits, debug_level> Self;
 
 // debug_level :
 // 0 = no debug messages
@@ -42,52 +46,177 @@ class Circular_arc_traits_tracer {
 // 2 = maybe important messages
 // 3 = all calls
 
-  Traits traits;
+  //  Traits traits;
 
   mutable int nested_level; /// for detection of nested calls
   mutable int call_number;  /// identifies the call number
 
 // FIXME : transform these macros into member functions ?
 #define CGAL_DEBUG(level, args) if (level <= debug_level) \
-   { std::cout << "Call #" << call_number++ << " " << __FUNCTION__ \
+   { std::cout << "Call #" << t->call_number++ << " " << __PRETTY_FUNCTION__ \
                << "  ( " << args << " )"; \
-     if (nested_level != 0) \
-       std::cout << "   [ nested level " << nested_level << " ]"; \
-     ++nested_level; \
+     if (t->nested_level != 0) \
+       std::cout << "   [ nested level " << t->nested_level << " ]"; \
+     ++t->nested_level; \
      std::cout << std::endl; }
 
 #define CGAL_DEBUG_RET(level, args) if (level <= debug_level) \
    { std::cout << "         returns : " << args; \
-     --nested_level; \
-     if (nested_level != 0) \
-       std::cout << "   [ nested level " << nested_level << " ]"; \
+     --t->nested_level; \
+     if (t->nested_level != 0) \
+       std::cout << "   [ nested level " << t->nested_level << " ]"; \
      std::cout << std::endl; }
 
 public:
 
-  Circular_arc_traits_tracer(const Traits &t = Traits())
-    : traits(t), nested_level(0), call_number(0) {}
-
-  typedef typename Traits::Curve                 Curve;
+  typedef typename Traits::Kernel                Kernel;
   typedef typename Traits::Curve_2               Curve_2;
-  typedef typename Traits::X_curve               X_curve;
   typedef typename Traits::X_monotone_curve_2    X_monotone_curve_2;
 
   typedef typename Traits::Point                 Point;
   typedef typename Traits::Point_2               Point_2;
 
-  typedef typename Traits::Has_left_category     Has_left_category;
+  typedef typename Kernel::Circle_2                 Circle;
+  typedef typename Kernel::Circular_arc_2           Circular_arc;
 
-  CGAL::Comparison_result
-  CGAL_NO_INLINE
-  compare_x(const Point_2 &p, const Point_2 &q) const
-  {
-    CGAL_PROFILER(__FUNCTION__);
-    CGAL_DEBUG(2, p.id() << ", " << q.id());
-    CGAL::Comparison_result ret = traits.compare_x(p, q);
-    CGAL_DEBUG_RET(2, ret);
-    return ret;
-  }
+  typedef typename Traits::Has_left_category     Has_left_category;
+  typedef typename Traits::Has_merge_category    Has_merge_category;
+ 
+  const Traits & traits () const
+    { return static_cast<const Traits &>(*this); }
+
+  Circular_arc_traits_tracer(const Traits &t = Traits())
+    : Traits(t), nested_level(0), call_number(0) {}
+
+  struct Compare_x_2 {
+
+    Compare_x_2(const Self *tt) :t(tt) {}
+
+    const Self *t;
+
+    CGAL::Comparison_result
+    CGAL_NO_INLINE
+    operator()(const Point_2 &p, const Point_2 &q) const
+    {
+      CGAL_PROFILER(__FUNCTION__);
+      CGAL_DEBUG(2, p.id() << ", " << q.id());
+      CGAL::Comparison_result ret = t->traits().compare_x_2_object()(p, q);
+      CGAL_DEBUG_RET(2, ret);
+      return ret;
+    }
+  };
+
+  Compare_x_2
+  compare_x_2_object() const
+  { return Compare_x_2(this); }
+
+  struct Compare_y_2 {
+
+    Compare_y_2(const Self *tt) :t(tt) {}
+
+    const Self *t;
+
+    CGAL::Comparison_result
+    CGAL_NO_INLINE
+    operator()(const Point_2 &p, const Point_2 &q) const
+    {
+      CGAL_PROFILER(__FUNCTION__);
+      CGAL_DEBUG(2, p.id() << ", " << q.id());
+      CGAL::Comparison_result ret = t->traits().compare_y_2_object()(p, q);
+      CGAL_DEBUG_RET(2, ret);
+      return ret;
+    }
+  };
+
+  Compare_y_2
+  compare_y_2_object() const
+  { return Compare_y_2(this); }
+
+  struct Equal_2 {
+
+    Equal_2(const Self *tt) :t(tt) {}
+
+    const Self *t;
+
+    bool
+    CGAL_NO_INLINE
+    operator()(const Point_2 &p, const Point_2 &q) const
+    {
+      CGAL_PROFILER(__FUNCTION__);
+      CGAL_DEBUG(2, p.id() << ", " << q.id());
+      bool ret = t->traits().equal_2_object()(p, q);
+      CGAL_DEBUG_RET(2, ret);
+      return ret;
+    }
+
+    bool
+    CGAL_NO_INLINE
+    operator()(const Circular_arc &a1, const Circular_arc &a2) const
+    {
+      CGAL_PROFILER(__FUNCTION__);
+      CGAL_DEBUG(2, a1.id() << ", " << a2.id());
+      bool ret = t->traits().equal_2_object()(a1, a2);
+      CGAL_DEBUG_RET(2, ret);
+      return ret;
+    }
+  };
+
+  Equal_2
+  equal_2_object() const
+  { return Equal_2(this); }
+
+  struct Construct_intersections_2 {
+
+    Construct_intersections_2(const Self *tt) :t(tt) {}
+
+    const Self *t;
+
+    template < class OutputIterator >
+    OutputIterator
+    CGAL_NO_INLINE
+    operator()(const Circle & c1, const Circle & c2, OutputIterator res)
+    {
+      CGAL_PROFILER(__FUNCTION__);
+      CGAL_DEBUG(2, c1.id() << ", " << c2.id());
+      std::vector<CGAL::Object> ret;
+      t->traits().construct_intersections_2_object()(c1, c2,
+                                                   std::back_inserter(ret));
+      
+      if (level <= debug_level)
+	{
+	  std::cout << "         returns : ";
+          for(int i=0; i<ret.size(); ++i) {
+	    Point_2 p;
+	    Circular_arc c;
+	    std::pair<Point_2, int> pp;
+	    if (assign(ret[i], p)) {
+	      std::cout << p << "  ";
+	    }
+	    else if (assign(ret[i], c)) {
+	      std::cout << c << "  ";
+	    }
+	    else if (assign(ret[i], pp)) {
+	      std::cout << " pair ( " << pp.first << " ; " <<
+		pp.second << " )  ";
+	    }
+	    else {
+	      std::cout << " [ something else ? ] ";
+	    }
+	  }
+	  --nested_level;
+	  if (nested_level != 0)
+	    std::cout << "   [ nested level " << nested_level << " ]";
+	  std::cout << std::endl;
+	}
+      
+      return std::copy(ret.begin(), ret.end(), res);
+    }
+  };
+
+  Construct_intersections_2
+  construct_intersections_2_object() const
+  { return Construct_intersections_2(this); }
+
 
   CGAL::Comparison_result
   CGAL_NO_INLINE
