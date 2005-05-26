bc8db3fcb60261f9bb34dc1e0337676040435d30
Frans Wessendorp <fwessend@iiic.ethz.ch>
2005-05-26 17:47:32 +0000

    Filled in the code for the member functions "z_replace_original_original(..)", "z_replace_original_slack(..)", "z_replace_slack_original(..)", "z_replace_slack_slack(..)". Fixed code of member functions "copy_row_in_C(..)"  and "copy_row_in_B_O(..)". Moved "z_update_inplace(..)" from corresponding  header file to this file.

--- Packages/QP_solver/include/CGAL/QP_solver/QP_basis_inverse.C
+++ Packages/QP_solver/include/CGAL/QP_solver/QP_basis_inverse.C
@@ -174,9 +174,36 @@ z_replace_original_by_original(ForwardIterator y_l_it,
                                ForwardIterator y_x_it, const ET& k_2,
 			       unsigned int k_i)
 {
-    ET  hat_k_1 = *(y_x_it + k_i);
+
+    // assert QP case and phaseII
+    CGAL_qpe_precondition(is_QP && is_phaseII);
+
+    // prepare \hat{k}_{1} -scalar
+    ET  hat_k_1 = *y_x_it+k_i;
+    
+    // prepare \hat{v} -vector in tmp_l, tmp_x
+    std::copy(*y_l_it, *y_l_it+s, tmp_l.begin());
+    std::copy(*y_x_it, *y_x_it+b, tmp_x.begin());
+    tmp_x[k_i] -= d;
+    
+    // prepare \hat{\rho} -vector in x_l, x_x
+    copy_row_in_B_O(x_l.begin(), x_x.begin(), k_i);
+    
+    // update matrix in place
+    z_update_inplace(x_l.begin(), x_x.begin(), tmp_l.begin(), tmp_x.begin(),
+                      hat_k_1 * hat_k_1, -k_2, -hat_k_1, d*d);
+    
+    // store new denominator
+    d = hat_k_1 * hat_k_1 / d;
+
+    CGAL_qpe_postcondition( d > et0);
+
+    CGAL_qpe_debug {
+        if ( vout.verbose()) print();
+    }
     
 }
+
 
 // replacing of original by slack variable with precondition in QP-case
 // for phaseII                               (update type UZ_2)
@@ -184,7 +211,40 @@ template < class ET_, class Is_LP_ >
 void  QPE_basis_inverse<ET_,Is_LP_>::
 z_replace_original_by_slack(unsigned int k_j, unsigned int k_i)
 {
+
+    // assert QP case and phaseII
+    CGAL_qpe_precondition(is_QP && is_phaseII);
+
+    // prepare \hat{\rho} -vector in x_l, x_x
+    copy_row_in_B_O(x_l.begin(), x_x.begin(), k_i);
+    
+    // prepare \hat{\varrho} -vector in tmp_l, tmp_x
+    copy_row_in_C(tmp_l.begin(), tmp_x.begin(), k_j);
+    
+    // prepare \hat{\kappa} -scalar
+    ET  hat_kappa = (*M.begin()+k_j)[k_i];
+    
+    // prepare \hat{\xi} -scalar
+    ET hat_xi = (*M.begin()+k_j)[k_j];
+    
+    // update matrix in place
+    z_update_inplace(x_l.begin(), x_x.begin(), tmp_l.begin(), tmp_x.begin(),
+                     d * hat_kappa * hat_kappa, hat_xi, -hat_kappa, d * d);
+
+    // adapt s and b
+    --s; --b;
+		     
+    // store new denominator
+    d = -(hat_kappa * hat_kappa);
+
+    CGAL_qpe_postcondition( d > et0);
+
+    CGAL_qpe_debug {
+        if ( vout.verbose()) print();
+    }
+
 }
+
 
 // replacing of slack by original variable with precondition in QP-case
 // for phaseII                               (update type UZ_3)
@@ -196,6 +256,89 @@ z_replace_slack_by_original(ForwardIterator y_l_it,
 			    ForwardIterator u_x_it, const ET& kappa,
 		            const ET& nu)
 {
+
+    // assert QP case and phaseII
+    CGAL_qpe_precondition(is_QP && is_phaseII);
+    
+    // get copies of y_l_it and y_x_it for later use
+    ForwardIterator y_l_it_copy = y_l_it;
+    ForwardIterator y_x_it_copy = y_x_it;
+
+    // prepare \hat{\phi}
+    
+    
+    // prepare \hat{\varphi} -vector in x_l, x_x
+    multiply(u_x_it, u_x_it, x_l.begin(), x_x.begin(), Tag_false(),
+             Tag_false());
+	     
+    // prepare \hat{\kappa} -scalar
+    
+    // prepare \hat{\nu} -scalar
+    
+    // update matrix in place
+    z_update_inplace(x_l.begin(), x_x.begin(), y_l_it, y_x_it,
+                     d * kappa * kappa, -nu, kappa, d * d);
+    
+    // append new rows and columns
+    // ---------------------------
+    typename Row   ::iterator  row_it, x_l_it, x_x_it;
+    typename Matrix::iterator  matrix_it;
+    unsigned int               count;
+    
+    // insert new row and column at the end of block P
+    if (M[s].size()==0) {
+	// row has to be filled first
+        M[s].insert(M[s].end(), s+1, et0);
+    }
+    
+    // P-block: left of diagonal (including element on diagonal)
+    y_l_it = y_l_it_copy;
+    for (  row_it = M[s].begin(), x_l_it = x_l.begin();
+           row_it != M[s].end();
+	 ++row_it,  ++x_l_it,  ++y_l_it                ) {
+        *row_it = ((nu * *x_l_it) - (kappa * *y_l_it)) / d;  
+    }
+    *row_it = -nu;
+    
+    // Q-block
+    y_x_it = y_x_it_copy;
+    for (  matrix_it = M.begin()+l, count = 0, x_x_it = x_x.begin();
+           count < b;
+	 ++matrix_it,  ++count, ++x_x_it, ++y_x_it                  ) {
+        (*matrix_it)[s] = ((nu * *x_x_it) - (kappa * *y_x_it)) / d;
+    }
+           
+    // insert new row and column at the end of blocks Q and R
+    ensure_physical_row(l+b);
+    
+    // Q-block
+    for (  row_it = M[l+b].begin(), count = 0, x_l_it = x_l.begin();
+           count < s;
+	 ++row_it,  ++count,  ++x_l_it                              ) {
+        *row_it = -kappa * *x_l_it / d;
+    }
+    *row_it = kappa;
+    
+    // R-block
+    for (  row_it = M[l+b].begin()+l, count = 0, x_x_it = x_x.begin();
+           count < b;
+	 ++row_it,  ++count,  ++x_x_it                                ) {
+        *row_it = -kappa * *x_x_it / d;
+    }
+    *row_it = et0;
+    
+    //adapt s and b
+    ++s; ++b; 
+
+    // store new denominator
+    d = -(kappa * kappa);
+
+    CGAL_qpe_postcondition( d > et0);
+
+    CGAL_qpe_debug {
+        if ( vout.verbose()) print();
+    }
+		     
 }
 
 
@@ -206,10 +349,41 @@ template < class ForwardIterator >
 void  QPE_basis_inverse<ET_,Is_LP_>::
 z_replace_slack_by_slack(ForwardIterator u_x_it, unsigned int k_j)
 {
+
+    // assert QP case and phaseII
+    CGAL_qpe_precondition(is_QP && is_phaseII);
+
+    // prepare \hat{v} -vector in x_l, x_x
+    multiply(u_x_it, u_x_it, x_l.begin(), x_x.begin(),Tag_false(),
+             Tag_false());
+    x_l[k_j] -= d;
+    
+    // prepare \varrho -vector in tmp_l, tmp_x
+    copy_row_in_C(tmp_l.begin(), tmp_x.begin(), k_j);
+    
+    // prepare \hat{k}_{1} -scalar
+    ET  hat_k_1 = inner_product_x(tmp_x.begin(), u_x_it);
+    
+    // prepare \hat{k}_{3} -scalar
+    ET  hat_k_3 = (*M.begin()+k_j)[k_j];
+    
+    // update matrix in place
+    z_update_inplace(x_l.begin(), x_x.begin(), tmp_l.begin(), tmp_x.begin(),
+                     hat_k_1 * hat_k_1, -hat_k_3, -hat_k_1, d * d);
+		     
+    // store new denominator
+    d = hat_k_1 * hat_k_1 / d;
+
+    CGAL_qpe_postcondition( d > et0);
+
+    CGAL_qpe_debug {
+        if ( vout.verbose()) print();
+    }
+      
 }
 
 
-// copying of reduced basis inverse row in (upper) C-block
+// copying of reduced basis inverse row in (upper) C-half
 template < class ET_, class Is_LP_ >
 template < class OutIt >
 void  QPE_basis_inverse<ET_,Is_LP_>::
@@ -219,7 +393,7 @@ copy_row_in_C(OutIt y_l_it, OutIt y_x_it, unsigned int r)
     typename Row   ::const_iterator     row_it;
     unsigned int  count;
     
-    // left of diagonal (including element on diagonal)
+    // P-block: left of diagonal (including element on diagonal)
     matrix_it = M.begin()+r;
     for (  row_it = matrix_it->begin();
            row_it != matrix_it->end(); 
@@ -227,22 +401,23 @@ copy_row_in_C(OutIt y_l_it, OutIt y_x_it, unsigned int r)
         *y_l_it = *row_it;    
     }
     
-    // right of diagonal (excluding element on diagonal)
+    // P-block: right of diagonal (excluding element on diagonal)
     for (  matrix_it = M.begin()+r+1, count = r+1;
            count < s; 
-	 ++matrix_it,  ++count                    ) {
+	 ++matrix_it,  ++count,  ++y_l_it         ) {
         *y_l_it = (*matrix_it)[r];
     }
     
-    // B_O x C -block
+    // Q-block
     for (  matrix_it = M.begin()+l, count = 0;
            count < b;
-	 ++matrix_it,  ++count                ) {
+	 ++matrix_it,  ++count,  ++y_x_it     ) {
 	*y_x_it = (*matrix_it)[r]; 
     } 
 }
 
-// copying of reduced basis inverse row in upper B_O-block
+
+// copying of reduced basis inverse row in (lower) B_O-half
 template < class ET_, class Is_LP_ >
 template < class OutIt >
 void  QPE_basis_inverse<ET_,Is_LP_>::
@@ -252,29 +427,97 @@ copy_row_in_B_O(OutIt y_l_it, OutIt y_x_it, unsigned int r)
     typename Row   ::const_iterator     row_it;
     unsigned int  count;
     
-    // B_O x C -block
+    // Q-block
     matrix_it = M.begin()+l+r;
     for (  row_it = matrix_it->begin(), count = 0;
            count < s;
-	 ++row_it,  ++count                       ) {
+	 ++row_it,  ++count,  ++y_l_it           ) {
         *y_l_it = *row_it;
     }
     
-    // left of diagonal (including element on diagonal)
+    // R-block: left of diagonal (including element on diagonal)
     for (  row_it = matrix_it->begin()+l; 
            row_it != matrix_it->end();
-	 ++row_it                     ) {
+	 ++row_it,  ++y_x_it            ) {
         *y_x_it = *row_it;
     }
     
-    //right of diagonal (excluding element on diagonal)
+    // R-block: right of diagonal (excluding element on diagonal)
     for (  matrix_it = M.begin()+l+r+1, count = r+1;
            count < b;
-	 ++matrix_it,  ++count                    ) {
+	 ++matrix_it,  ++count,  ++y_x_it           ) {
         *y_x_it = (*matrix_it)[l+r];
     }
 
 }
+
+template < class ET_, class Is_LP_ >
+template < class ForIt >
+void  QPE_basis_inverse<ET_,Is_LP_>::
+z_update_inplace( ForIt psi1_l_it, ForIt psi1_x_it,
+                  ForIt psi2_l_it, ForIt psi2_x_it,
+	          const ET& omega0, const ET& omega1,
+		  const ET& omega2, const ET& omega3)
+{
+    typename Matrix::      iterator  matrix_it;
+    typename Row   ::      iterator     row_it;
+    typename Row   ::const_iterator      y_it1_r, y_it1_c, y_it2_r, y_it2_c;
+	
+    unsigned int  row, col, k = l+b;
+    ET           u_elem;
+
+    // rows: 0..s-1  ( P )
+    for (  row = 0, matrix_it = M.begin(),
+           y_it1_r = psi1_l_it,  y_it2_r = psi2_l_it;
+	   row < s;
+         ++row, ++matrix_it, ++y_it1_r, ++y_it2_r  ) {
+	      
+        // columns: 0..row  ( P )
+        for (   row_it =  matrix_it->begin(),
+	        y_it1_c = psi1_l_it,  y_it2_c = psi2_l_it;
+                row_it != matrix_it->end();
+              ++row_it,  ++y_it1_c,  ++y_it2_c            ) {
+                
+            u_elem = *y_it1_r * *y_it2_c + *y_it2_r * *y_it1_c;
+	    u_elem *= omega2;
+	    u_elem += omega1 * *y_it1_r * *y_it1_c;
+            update_entry( *row_it, omega0, u_elem, omega3);
+        } 
+    }
+	
+    // rows: l..k-1  ( Q R )
+    for (  row = l, matrix_it = M.begin()+l,
+	   y_it1_r = psi1_x_it,  y_it2_r = psi2_x_it;
+	   row != k;
+	 ++row,  ++matrix_it,  ++y_it1_r,  ++y_it2_r ) {
+	    
+        // columns: 0..s-1  ( Q )
+        for (   col = 0,   row_it =  matrix_it->begin(),
+	        y_it1_c = psi1_l_it,  y_it2_c = psi2_l_it;
+                col < s;
+              ++col, ++row_it,  ++y_it1_c,  ++y_it2_c     ){
+    
+            u_elem = *y_it1_r * *y_it2_c + *y_it2_r * *y_it1_c;
+	    u_elem *= omega2;
+	    u_elem += omega1 * *y_it1_r * *y_it1_c; 
+	    update_entry( *row_it, omega0, u_elem, omega3);
+        }
+    
+        // columns: l..k-1  ( R )
+        for (  row_it = matrix_it->begin()+l,
+	       y_it1_c = psi1_x_it,  y_it2_c = psi2_x_it;
+               row_it != matrix_it->end();
+             ++row_it,  ++y_it1_c,  ++y_it2_c            ){
+		 
+            u_elem = *y_it1_r * *y_it2_c + *y_it2_r * *y_it1_c;
+            u_elem *= omega2;
+	    u_elem += omega1 * *y_it1_r * *y_it1_c;     
+            update_entry( *row_it, omega0, u_elem, omega3);
+        }
+	    
+    } 
+	
+} 
 
 
 // swap functions
