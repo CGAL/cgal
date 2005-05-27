d93463dc3d24d71312566455e941a2ef7f1e869d
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2005-05-27 18:41:22 +0000

    added example that computes the number of connected components of the Voronoi skeleton

---
+++ Packages/Voronoi_diagram_2/examples/Voronoi_diagram_2/connected_components.C
@@ -0,0 +1,156 @@
+// file: connected_components.C
+#include <CGAL/basic.h>
+
+#include <iostream>
+#include <fstream>
+#include <cassert>
+
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Apollonius_graph_2.h>
+#include <CGAL/Apollonius_graph_filtered_traits_2.h>
+
+#include <CGAL/Voronoi_diagram_adaptor_2.h>
+#include <CGAL/Apollonius_graph_Voronoi_traits_2.h>
+
+typedef CGAL::Simple_cartesian<double>  K;
+
+typedef CGAL::Apollonius_graph_filtered_traits_2<K>   Gt;
+typedef CGAL::Apollonius_graph_2<Gt>                  AG;
+typedef CGAL::Apollonius_graph_Voronoi_traits_2<AG>   VT;
+typedef CGAL::Voronoi_diagram_adaptor_2<AG,VT>        VD;
+
+template<class VDA_t>
+class Connected_components
+{
+ private:
+  typedef VDA_t                                            VDA;
+  typedef typename VDA::Halfedge_iterator                  Halfedge_iterator;
+  typedef typename VDA::Halfedge_handle                    Halfedge_handle;
+  typedef typename VDA::Vertex_handle                      Vertex_handle;
+  typedef typename VDA::Halfedge                           Halfedge;
+  typedef typename VDA::Vertex                             Vertex;
+  typedef typename VDA::Halfedge_around_vertex_circulator  HAVC;
+
+  typedef HAVC Halfedge_around_vertex_circulator;
+
+ public:
+  typedef VDA  Voronoi_diagram_adaptor_2;
+
+  typedef typename Voronoi_diagram_adaptor_2::size_type  size_type;
+
+  typedef size_type                                      result_type;
+  typedef Voronoi_diagram_adaptor_2                      argument_type;
+
+  struct Arity { enum { arity = 1 }; };
+
+ private:
+  struct Halfedge_less {
+    bool operator()(const Halfedge& h1,	const Halfedge& h2) const {
+      typename Voronoi_diagram_adaptor_2::Dual_edge e1 = h1.dual_edge();
+      typename Voronoi_diagram_adaptor_2::Dual_edge e2 = h2.dual_edge();
+
+      if ( e1.first != e2.first ) { return e1.first < e2.first; }
+      return e1.second < e2.second;
+    }
+  };
+
+  typedef std::map<Halfedge,bool,Halfedge_less>  Halfedge_map;
+
+  void mark_edge(const Halfedge& e, Halfedge_map& e_map) const
+  {
+    e_map[e] = true;
+    e_map[*e.opposite()] = true;
+  }
+
+  bool is_unmarked(const Halfedge& e, const Halfedge_map& e_map) const
+  {
+    return e_map.find(e) == e_map.end();
+  }
+
+  void dfs(const Voronoi_diagram_adaptor_2& vda, const Halfedge& e,
+	      Halfedge_map& e_map) const
+  {
+    CGAL_precondition( !vda.dual().is_infinite(e.dual_edge()) );
+
+    Halfedge e_opp = *e.opposite();
+    mark_edge(e, e_map);
+
+    if ( e.has_source() ) {
+      HAVC ec =	vda.incident_halfedges(e.source());
+      HAVC ec_start = ec;
+
+      do {
+	if ( *ec != e && *ec != e_opp && is_unmarked(*ec, e_map) ) {
+	  dfs(vda, *ec, e_map);
+	}
+	ec++;
+      } while (ec != ec_start);
+    }
+
+    if ( e.has_target() ) {
+      HAVC ec = vda.incident_halfedges(e.target());
+      HAVC ec_start = ec;
+
+      do {
+	if ( *ec != e && *ec != e_opp && is_unmarked(*ec, e_map) ) {
+	  dfs(vda, *ec, e_map);
+	}
+	ec++;
+      } while (ec != ec_start);
+    }
+  }
+
+ public:
+  size_type operator()(const Voronoi_diagram_adaptor_2& vda) const
+  {
+    Halfedge_map e_map;
+
+    size_type n_components = 0;
+    for(Halfedge_iterator eit = vda.halfedges_begin();
+	eit != vda.halfedges_end(); ++eit) {
+      if ( is_unmarked(*eit, e_map) ) {
+	n_components++;
+	dfs(vda, *eit, e_map);
+      }
+    }
+    return n_components;
+  }
+};
+
+
+void eval_num_of_connected_components(char* fname)
+{
+  std::ifstream ifs(fname);
+  assert( ifs );
+
+  AG ag;
+
+  std::cout << std::endl << "Input sites:" << std::endl;
+  AG::Site_2 s;
+  while ( ifs >> s ) {
+    std::cout << s << std::endl;
+    ag.insert(s);
+  }
+  ifs.close();
+
+  std::cout << std::endl;
+
+  VD vd(ag);
+
+  std::cout << "# of connected components: "
+	    << Connected_components<VD>()(vd) << std::endl;
+  std::cout << std::endl;
+  std::cout << "==========================================="
+	    << std::endl;
+}
+
+
+int main()
+{
+  eval_num_of_connected_components("data/data1.ag.cin");
+  eval_num_of_connected_components("data/data2.ag.cin");
+  eval_num_of_connected_components("data/data3.ag.cin");
+  eval_num_of_connected_components("data/degenerate.ag.cin");
+
+  return 0;
+}
