ff7121a9d43b31c61737b05a253cbf14b80ac96f
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2005-05-31 13:30:43 +0000

    Use TAUCS more often for tests

--- Packages/Parameterization/examples/Parameterization/test_all.sh
+++ Packages/Parameterization/examples/Parameterization/test_all.sh
@@ -4,11 +4,11 @@
 date
 ./test_method.sh uniform square opennl
 date
-./test_method.sh floater circle opennl
+./test_method.sh floater circle taucs
 date
-./test_method.sh conformal circle opennl
+./test_method.sh conformal circle taucs
 date
-./test_method.sh authalic square opennl
+./test_method.sh authalic square taucs
 date
 ./test_method.sh lscm 2pts opennl
 date
