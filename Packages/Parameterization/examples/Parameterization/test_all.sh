3516d6ba8dfc5151659cd205bf26dacf6d58f115
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2005-06-07 12:06:24 +0000

    Port to Windows/Visual C++ 7.1

--- Packages/Parameterization/examples/Parameterization/test_all.sh
+++ Packages/Parameterization/examples/Parameterization/test_all.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 # Intensive test: test all surface parameterization methods with all models in data folder
-date
-./test_method.sh uniform square opennl | tee test_all.log
-date
-./test_method.sh floater circle taucs | tee -a test_all.log
-date
+./test_method.sh uniform square opennl  | tee test_all.log
+./test_method.sh floater circle taucs   | tee -a test_all.log
 ./test_method.sh conformal circle taucs | tee -a test_all.log
-date
-./test_method.sh authalic square taucs | tee -a test_all.log
-date
-./test_method.sh lscm 2pts opennl | tee -a test_all.log
-date
+./test_method.sh authalic square taucs  | tee -a test_all.log
+./test_method.sh lscm 2pts opennl       | tee -a test_all.log
