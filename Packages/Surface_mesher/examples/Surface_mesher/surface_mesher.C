9f94f589b534777f6b59f46ecbca488d8f0235f8
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2005-06-16 16:22:22 +0000

    version polyedrique ajoutee

--- Packages/Surface_mesher/examples/Surface_mesher/surface_mesher.C
+++ Packages/Surface_mesher/examples/Surface_mesher/surface_mesher.C
@@ -24,8 +24,9 @@
 #include <CGAL/Surface_mesher/Criteria/Standard_criteria.h>
 #include <CGAL/IO/Complex_2_in_triangulation_3_file_writer.h>
 
-#include <CGAL/Surface_mesher/Oracles/Implicit_oracle.h>
-#include "implicit_function.h"
+// #include <CGAL/Surface_mesher/Oracles/Implicit_oracle.h>
+// #include "implicit_function.h"
+#include <CGAL/Surface_mesher/Oracles/Polyhedral.h>
 
 #include <fstream>
 
@@ -43,8 +44,9 @@ typedef CGAL::Triangulation_data_structure_3<Vb2, Cb> Tds;
 
 typedef CGAL::Delaunay_triangulation_3<K, Tds> Del;
 typedef CGAL::Complex_2_in_triangulation_3_surface_mesh<Del> C2t3;
-typedef Function <K::FT> Func;
-typedef CGAL::Surface_mesher::Implicit_oracle<K, Func> Oracle;
+// typedef Function <K::FT> Func;
+// typedef CGAL::Surface_mesher::Implicit_oracle<K, Func> Oracle;
+typedef CGAL::Surface_mesher::Polyhedral <Del> Oracle;
 typedef CGAL::Surface_mesher::Refine_criterion<Del> Criterion;
 typedef CGAL::Surface_mesher::Standard_criteria <Criterion > Criteria;
 
@@ -53,14 +55,16 @@ typedef CGAL::Surface_mesher::Surface_mesher_regular_edges<Del, Oracle, Criteria
 typedef CGAL::Surface_mesher::Surface_mesher_regular_edges_without_boundary<Del, Oracle, Criteria> SMREWB;
 typedef CGAL::Surface_mesher::Surface_mesher_manifold<Del, Oracle, Criteria> SMM;
 typedef CGAL::Surface_mesher::Surface_mesher_regular_edges_without_boundary_base<Del, Oracle, Criteria> SMREWBB;
-typedef CGAL::Surface_mesher::Surface_mesher_manifold<Del, Oracle, Criteria, SMREWBB> SMMWB;
+typedef CGAL::Surface_mesher::Surface_mesher_manifold<Del, Oracle, Criteria, 
+CGAL::Surface_mesher::Surface_mesher_manifold_base <Del, Oracle, Criteria, SMREWBB> 
+> SMMWB;
 
 
-//typedef SM Surface;
+typedef SM Surface;  // basic mesher
 //typedef SMRE Surface;
 //typedef SMREWB Surface;
-typedef SMM Surface;
-//typedef SMMWB Surface;
+// typedef SMM Surface;  // manifold with boundary
+//typedef SMMWB Surface;  // manifold without boundary
 
 
 
@@ -68,12 +72,20 @@ typedef SMM Surface;
 
 int main(int argc, char **argv) {
 
+  if (argc < 2) {
+    std::cout << "Usage : " << argv[0] << " <off_file>" << std::endl;
+    exit(0);
+  }
+
+
   // Function
-  Func F;
+//   Func F;
 
   // Oracle (NB: parity oracle is toggled)
-  //Oracle O (F, K::Point_3 (0,0,0), 4, 1e-3, true);  
-  Oracle O (F, K::Point_3 (0,0,0), 4, 1e-6, true);  
+//   Oracle O (F, K::Point_3 (0,0,0), 4, 1e-6, true);  
+  std::ifstream is (argv[1]);
+  Oracle O (is);
+  is.close ();
 
   // 3D-Delaunay triangulation
   Del T;
@@ -82,19 +94,18 @@ int main(int argc, char **argv) {
   // Initial point sample
   //Oracle::Points initial_point_sample = O.random_points (10);
   //  Oracle::Points initial_point_sample = O.random_points (20);
-  Oracle::Points initial_point_sample = O.random_points (20);
+  Oracle::Points initial_point_sample = O.random_points (50);
   typedef Del::Point Point;
   T.insert (initial_point_sample.begin(), initial_point_sample.end());
   
   // Meshing criteria
-  CGAL::Surface_mesher::Curvature_size_criterion<Del> c_s_crit (1000);
-  CGAL::Surface_mesher::Uniform_size_criterion<Del> u_s_crit (5e-2);
-  //CGAL::Surface_mesher::Uniform_size_criterion<Del> u_s_crit (10000);
+  CGAL::Surface_mesher::Curvature_size_criterion<Del> c_s_crit (1e-3);
+  CGAL::Surface_mesher::Uniform_size_criterion<Del> u_s_crit (10000);
   CGAL::Surface_mesher::Aspect_ratio_criterion<Del> a_r_crit (30);
-  std::vector<Criterion*> crit_vect(3);
-  crit_vect[0] = &c_s_crit;
-  crit_vect[1] = &u_s_crit;
-  crit_vect[2] = &a_r_crit;
+  std::vector<Criterion*> crit_vect;
+  crit_vect.push_back (&a_r_crit);
+  crit_vect.push_back (&c_s_crit);
+  crit_vect.push_back (&u_s_crit);
   //std::vector<Criterion*> crit_vect(1);
   //crit_vect[0] = &u_s_crit;
   //crit_vect[1] = &a_r_crit;
@@ -114,9 +125,9 @@ int main(int argc, char **argv) {
             << std::endl;
 
   // Output
-  if (argc >= 2) {
-    std::cout << "Writing output to file " << argv[1] << "...";
-    std::ofstream os(argv[1]);
+  if (argc >= 3) {
+    std::cout << "Writing output to file " << argv[2] << "...";
+    std::ofstream os(argv[2]);
     output_surface_facets_to_off (os, T);
     os.close();
     std::cout << " done\n";
