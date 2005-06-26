b662a3f910c1f3fc8773b6465a104e74a62cc30b
Baruch Zukerman <baruchzu@post.tau.ac.il>
2005-06-26 12:32:44 +0000

    clean

--- Packages/Arrangement_2/include/CGAL/Arrangement_2/Arrangement_2_insert.h
+++ Packages/Arrangement_2/include/CGAL/Arrangement_2/Arrangement_2_insert.h
@@ -316,8 +316,7 @@ void insert_non_intersecting (Arrangement_2<Traits,Dcel>& arr,
   arr_access.notify_before_global_change();
 
   // Perform the aggregated insertion.
-  Arr_non_x_aggregate_insert<Arrangement_2>  agg_insert_obj (arr.get_traits(), 
-							     &arr);
+  Arr_non_x_aggregate_insert<Arrangement_2>  agg_insert_obj (&arr);
   agg_insert_obj.insert_curves(begin, end);
 
   // Notify the arrangement observers that the global operation has been
