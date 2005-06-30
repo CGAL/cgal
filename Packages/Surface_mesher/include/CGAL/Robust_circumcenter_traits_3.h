9fe3753795f520b0f806c1bee149cd01d668ff29
Mariette Yvinec <Mariette.Yvinec@sophia.inria.fr>
2005-06-30 09:24:06 +0000

    remove the To_double in Robust_construct_circumcenter_3, CGAL::To_double can be used instead

--- Packages/Surface_mesher/include/CGAL/Robust_circumcenter_traits_3.h
+++ Packages/Surface_mesher/include/CGAL/Robust_circumcenter_traits_3.h
@@ -5,6 +5,7 @@
 #ifndef CGAL_ROBUST_CIRCUMCENTER_TRAITS_3_H
 #define CGAL_ROBUST_CIRCUMCENTER_TRAITS_3_H
 
+#include <CGAL/number_utils_classes.h>
 #include <CGAL/Cartesian_converter.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 
@@ -15,23 +16,11 @@ template < class K >
 class Robust_construct_circumcenter_3
 {
   typedef Exact_predicates_exact_constructions_kernel EK;
-  //  typedef Cartesian_converter<K,EK>   To_exact_converter;
-  //  typedef Cartesian_converter<EK,K>   Back_from_exact_converter;
   typedef typename K::Point_3         Point_3;
 
   public:
     typedef Point_3          result_type;
     typedef Arity_tag< 4 >   Arity;
-
-  template < class NT1>
-  struct To_double : public CGAL_STD::unary_function< NT1,double> 
-  {
-    double
-    operator()(const NT1 &a) const
-    {
-      return to_double(a);
-    }
-  };
 
   Point_3 operator() (const Point_3 & p,
 		      const Point_3 & q,
@@ -40,7 +29,8 @@ class Robust_construct_circumcenter_3
     {
    
       Cartesian_converter<K,EK> to_exact;
-      Cartesian_converter<EK,K, To_double<EK::FT> > back_from_exact; 
+      Cartesian_converter<EK,K, To_double<EK::FT> > back_from_exact;
+      //Cartesian_converter<EK,K > back_from_exact;
       EK::Construct_circumcenter_3 
 	exact_circumcenter = EK().construct_circumcenter_3_object();
 
@@ -63,7 +53,6 @@ class Robust_construct_circumcenter_3
       return back_from_exact(exact_circumcenter( to_exact(p),
 						 to_exact(q),
 						 to_exact(r)));
-/*       return K(). construct_circumcenter_3_object()(p,q,r); */
     }
 };
 
