f68dab030ecb692797914fce833c10ee7b36781c
Efi Fogel <efif@post.tau.ac.il>
2005-07-04 12:19:38 +0000

    1st revision

---
+++ Packages/Arrangement_2/include/CGAL/IO/Arr_iostream.h
@@ -0,0 +1,49 @@
+// Copyright (c) 2001  Tel-Aviv University (Israel).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source$
+// $Revision$
+// $Name$
+//
+// Author(s)     : Eti Ezra <estere@post.tau.ac.il>
+
+#ifndef CGAL_ARR_IOSTREAM_H
+#define CGAL_ARR_IOSTREAM_H
+
+#include <CGAL/basic.h>
+#include <CGAL/Arrangement_2.h>
+#include <iostream>
+
+CGAL_BEGIN_NAMESPACE
+
+/*! Exporter operator */
+template <class Traits, class Dcel> inline
+std::ostream & operator << (std::ostream & os, 
+                            const Arrangement_2<Traits,Dcel> & arr)
+{
+  typedef Arrangement_2<Traits,Dcel>            Arrangement_2;
+  return os;
+}
+
+/*! Improter operator */
+template <class Traits, class Dcel> inline
+std::istream & operator >> (std::istream & is, 
+                            Arrangement_2<Traits,Dcel> & arr)
+{
+  typedef Arrangement_2<Traits,Dcel>            Arrangement_2;
+  return is;
+}
+
+CGAL_END_NAMESPACE
+
+#endif
