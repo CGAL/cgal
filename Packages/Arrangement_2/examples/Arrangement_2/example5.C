11b01aa1f3c385ce9bfc219779f54e2e9f13c154
Ron Wein <wein@post.tau.ac.il>
2005-07-10 10:43:38 +0000

    Added example5 and example6 for point location and vertical ray-shooting.

--- Packages/Arrangement_2/examples/Arrangement_2/example5.C
+++ Packages/Arrangement_2/examples/Arrangement_2/example5.C
@@ -1,107 +1,49 @@
 // file: examples/Arrangement_2/example5.C
 
-
-//#include "short_names.h"
-
-#include <CGAL/Cartesian.h>
-#include <CGAL/CORE_algebraic_number_traits.h>
-#include <CGAL/Arr_conic_traits_2.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Arr_segment_traits_2.h>
 #include <CGAL/Arrangement_2.h>
 #include <CGAL/Arr_naive_point_location.h>
+#include <CGAL/Arr_landmarks_point_location.h>
 
-typedef CGAL::CORE_algebraic_number_traits            Nt_traits;
-typedef Nt_traits::Rational                           Rational;
-typedef Nt_traits::Algebraic                          Algebraic;
-typedef CGAL::Cartesian<Rational>                     Rat_kernel;
-typedef Rat_kernel::Point_2                           Rat_point_2;
-typedef Rat_kernel::Segment_2                         Rat_segment_2;
-typedef Rat_kernel::Circle_2                          Rat_circle_2;
-typedef CGAL::Cartesian<Algebraic>                    Alg_kernel;
-typedef CGAL::Arr_conic_traits_2<Rat_kernel, 
-				 Alg_kernel,
-				 Nt_traits>           Traits_2;
+#include "point_location_utils.h"
+
+typedef int                                           Number_type;
+typedef CGAL::Simple_cartesian<Number_type>           Kernel;
+typedef CGAL::Arr_segment_traits_2<Kernel>            Traits_2;
 typedef Traits_2::Point_2                             Point_2;
-typedef Traits_2::Curve_2                             Conic_arc_2;
 typedef CGAL::Arrangement_2<Traits_2>                 Arrangement_2;
-typedef CGAL::Arr_naive_point_location<Arrangement_2> Point_location;
+typedef CGAL::Arr_naive_point_location<Arrangement_2>      Naive_pl;
+typedef CGAL::Arr_landmarks_point_location<Arrangement_2>  Landmarks_pl;
 
 int main ()
 {
+  // Construct the arrangement.
   Arrangement_2    arr;
-  Point_location   pl (arr);
+  Naive_pl         naive_pl (arr);
+  Landmarks_pl     landmarks_pl;
 
-  // Insert a hyperbolic arc, supported by the hyperbola y = 1/x
-  // (or: xy - 1 = 0) with the end-points (1/5, 4) and (2, 1/2).
-  // Note that the arc is counterclockwise oriented.
-  Point_2       ps1 (Rational(1,4), 4);
-  Point_2       pt1 (2, Rational(1,2));
-  Conic_arc_2   cv1 (0, 0, 1, 0, 0, -1, CGAL::COUNTERCLOCKWISE, ps1, pt1);
+  construct_segments_arr (arr);
 
-  insert (arr, pl, cv1);
+  // Perform some point-location queries using the naive strategy.
+  Point_2          q1 (1, 4);
+  Point_2          q2 (4, 3);
+  Point_2          q3 (6, 3);
 
-  // Insert a full ellipse, which is (x/4)^2 + (y/2)^2 = 0 rotated by
-  // phi=36.87 degree (such that sin(phi) = 0.6, cos(phi) = 0.8),
-  // yielding: 58x^2 + 72y^2 - 48xy - 360 = 0.
-  Conic_arc_2   cv2 (58, 72, -48, 0, 0, -360);
+  point_location_query (naive_pl, q1);
+  point_location_query (naive_pl, q2);
+  point_location_query (naive_pl, q3);
+
+  // Attach the landmarks object to the arrangement and perform queries.
+  Point_2          q4 (3, 2);
+  Point_2          q5 (5, 2);
+  Point_2          q6 (1, 0);
+
+  landmarks_pl.init (arr);
+  point_location_query (landmarks_pl, q4);
+  point_location_query (landmarks_pl, q5);
+  point_location_query (landmarks_pl, q6);
   
-  insert (arr, pl, cv2);
-
-  // Insert the segment (1, 1) -- (0, -3).
-  Rat_point_2   ps3 (1, 1);
-  Rat_point_2   pt3 (0, -3);
-  Conic_arc_2   cv3 (Rat_segment_2 (ps3, pt3));
-
-  insert (arr, pl, cv3);
-
-  // Insert a circular arc supported by the circle x^2 + y^2 = 5^2,
-  // with (-3, 4) and (4, 3) as its endpoints. We want the arc to be
-  // clockwise oriented, so it passes through (0, 5) as well.
-  Rat_point_2   ps4 (-3, 4);
-  Rat_point_2   pm4 (0, 5);
-  Rat_point_2   pt4 (4, 3);
-  Conic_arc_2   cv4 (ps4, pm4, pt4);
-
-  insert (arr, pl, cv4);
-
-  // Insert a full unit circle that is centered at (0, 4).
-  Rat_circle_2  circ5 (Rat_point_2(0,4), 1);
-  Conic_arc_2   cv5 (circ5);
-  
-  insert (arr, pl, cv5);
-
-  // Insert a parabolic arc that is supported by a parabola y = -x^2
-  // (or: x^2 + y = 0) and whose end-points are (-sqrt(3), -3) ~ (-1.73, -3)
-  // and (sqrt(2), -2) ~ (1.41, -2). Notice that since the x-coordinates 
-  // of the end-points cannot be acccurately represented, we specify them
-  // as the intersections of the parabola with the lines y = -3 and y = -2.
-  // Note that the arc is clockwise oriented.
-  Conic_arc_2   cv6 =
-    Conic_arc_2 (1, 0, 0, 0, 1, 0,       // The parabola.
-		 CGAL::CLOCKWISE,
-		 Point_2 (-1.73, -3),    // Approximation of the source.
-		 0, 0, 0, 0, 1, 3,       // The line: y = -3.
-		 Point_2 (1.41, -2),     // Approximation of the target.
-		 0, 0, 0, 0, 1, 2);      // The line: y = -2.
-
-  insert (arr, pl, cv6);
-
-  // Insert the right half of the circle centered at (4, 2.5) whose radius
-  // is 1/2 (therefore its squared radius is 1/4).
-  Rat_circle_2  circ7 (Rat_point_2(4, Rational(5,2)), Rational(1,4));
-  Point_2       ps7 (4, 3);
-  Point_2       pt7 (4, 2);
-  Conic_arc_2   cv7 (circ7, CGAL::CLOCKWISE, ps7, pt7);
-  
-  insert (arr, pl, cv7);
-
-  // Print out the number of vertices, edges and faces in the arrangement.
-  std::cout << "Number of vertices: " 
-	    << arr.number_of_vertices() << std::endl;
-  std::cout << "Number of edges: " 
-	    << arr.number_of_edges() << std::endl;
-  std::cout << "Number of faces: " 
-	    << arr.number_of_faces() << std::endl;
-
   return (0);
 }
 
---
+++ Packages/Arrangement_2/examples/Arrangement_2/point_location_utils.h
@@ -0,0 +1,114 @@
+//-----------------------------------------------------------------------------
+// Perform a point-location query and print the result.
+//
+template <class PointLocation>
+void point_location_query
+    (const PointLocation& pl,
+     const typename PointLocation::Arrangement_2::Point_2& q)
+{
+  // Perform the point-location query.
+  CGAL::Object    obj = pl.locate (q);
+
+  // Print the result.
+  typedef typename PointLocation::Arrangement_2  Arrangement_2;
+  
+  typename Arrangement_2::Vertex_const_handle    v;
+  typename Arrangement_2::Halfedge_const_handle  e;
+  typename Arrangement_2::Face_const_handle      f;
+
+  if (CGAL::assign (f, obj))
+  {
+    // q is located inside a face:
+    if (f->is_unbounded())
+      std::cout << "Inside the unbounded face." << std::endl;
+    else
+      std::cout << "Inside a bounded face." << std::endl;
+  }
+  else if (CGAL::assign (e, obj))
+  {
+    // q is located on an edge:
+    std::cout << "On an edge: " << e->curve() << std::endl;
+  }
+  else if (CGAL::assign (v, obj))
+  {
+    // q is located on a vertex:
+    if (v->is_isolated())
+      std::cout << "On an isolated vertex: " << v->point() << std::endl;
+    else
+      std::cout << "On a vertex: " << v->point() << std::endl;
+  }
+  else
+  {
+    CGAL_assertion_msg (false, "Invalid object.");
+  }
+
+  return;
+}
+
+//-----------------------------------------------------------------------------
+// Perform a vertical ray-shooting query and print the result.
+//
+template <class VerticalRayShoot>
+void vertical_ray_shooting_query
+    (const VerticalRayShoot& vrs,
+     const typename VerticalRayShoot::Arrangement_2::Point_2& q)
+{
+  // Perform the point-location query.
+  CGAL::Object    obj = vrs.ray_shoot_up (q);
+
+  // Print the result.
+  typedef typename VerticalRayShoot::Arrangement_2  Arrangement_2;
+  
+  typename Arrangement_2::Vertex_const_handle    v;
+  typename Arrangement_2::Halfedge_const_handle  e;
+
+  if (CGAL::assign (e, obj))
+  {
+    // We hit an edge:
+    std::cout << "Hit an edge: " << e->curve() << std::endl;
+  }
+  else if (CGAL::assign (v, obj))
+  {
+    // We hit a vertex:
+    if (v->is_isolated())
+      std::cout << "Hit an isolated vertex: " << v->point() << std::endl;
+    else
+      std::cout << "Hit a vertex: " << v->point() << std::endl;
+  }
+  else
+  {
+    // We did not hit anything:
+    CGAL_assertion_msg (obj.is_empty(), "Invalid object.");
+    
+    std::cout << "Hit nothing." << std::endl; 
+  }
+
+  return;
+}
+
+//-----------------------------------------------------------------------------
+// Construct the arrangement of segments needed for the point-location and
+// the vertical ray-shooting examples.
+// The function assumes that the arrangement is of line segments with integer
+// coordinates.
+//			
+template <class Arrangement>
+void construct_segments_arr (Arrangement& arr)
+{
+  typedef typename Arrangement::Point_2              Point_2;
+  typedef typename Arrangement::X_monotone_curve_2   Segment_2;
+  typedef typename Arrangement::Halfedge_handle      Halfedge_handle;
+
+  Point_2    p0 (3,2), p1 (0,3), p2 (2,5), p3 (4,5), p4 (6,3), p5 (3,0);
+  Segment_2  s1 (p1, p2), s2 (p2, p3), s3 (p3, p4), s4 (p4, p5), s5 (p5, p1);
+
+  arr.insert_isolated_vertex (p0, arr.unbounded_face());
+
+  Halfedge_handle e1 = arr.insert_in_face_interior (s1, arr.unbounded_face());
+  Halfedge_handle e2 = arr.insert_from_left_vertex (s2, e1->target());
+  Halfedge_handle e3 = arr.insert_from_left_vertex (s3, e2->target());
+  Halfedge_handle e4 = arr.insert_from_right_vertex (s4, e3->target());
+  Halfedge_handle e5 = arr.insert_at_vertices (s5, e4->target(), e1->source());
+
+  return;
+}
