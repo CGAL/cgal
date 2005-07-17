03327ce683fad2c9f115b44ddc69fea4af1b54c6
Ron Wein <wein@post.tau.ac.il>
2005-07-17 14:03:32 +0000

    Changed example12 to match the manual.

--- Packages/Arrangement_2/examples/Arrangement_2/example12.C
+++ Packages/Arrangement_2/examples/Arrangement_2/example12.C
@@ -1,58 +1,59 @@
 // file: examples/Arrangement_2/example12.C
 
 #include <CGAL/Cartesian.h>
+#include <CGAL/Quotient.h>
 #include <CGAL/MP_Float.h>
 #include <CGAL/Arr_segment_traits_2.h>
 #include <CGAL/Arr_polyline_traits_2.h>
 #include <CGAL/Arrangement_2.h>
+#include <vector>
+#include <list>
 
 #include "arr_print.h"
 
-typedef CGAL::MP_Float                                  Number_type;
+typedef CGAL::Quotient<CGAL::MP_Float>                  Number_type;
 typedef CGAL::Cartesian<Number_type>                    Kernel;
 typedef CGAL::Arr_segment_traits_2<Kernel>              Segment_traits_2;
 typedef CGAL::Arr_polyline_traits_2<Segment_traits_2>   Traits_2;
 typedef Traits_2::Point_2                               Point_2;
-typedef Traits_2::X_monotone_curve_2                    Polyline_2;
+typedef Traits_2::Curve_2                               Polyline_2;
 typedef CGAL::Arrangement_2<Traits_2>                   Arrangement_2;
 
 int main ()
 {
-  Arrangement_2 arr;
+  Arrangement_2         arr;
 
-  Point_2 points1[5];
-  points1[0] = Point_2(0, 0);
-  points1[1] = Point_2(4, 8);
-  points1[2] = Point_2(6, 0);
-  points1[3] = Point_2(8, 8);
-  points1[4] = Point_2(12, 0);
-  Polyline_2 polyline1(&points1[0], &points1[5]);
+  Point_2               points1[5];
+  points1[0] = Point_2 (0, 0);
+  points1[1] = Point_2 (2, 4);
+  points1[2] = Point_2 (3, 0);
+  points1[3] = Point_2 (4, 4);
+  points1[4] = Point_2 (6, 0);
+  Polyline_2            pi1 (&points1[0], &points1[5]);
 
-  Point_2 points2[5];
-  points2[0] = Point_2(0, 4);
-  points2[1] = Point_2(2, 4);
-  points2[2] = Point_2(6, 12);
-  points2[3] = Point_2(10, 4);
-  points2[4] = Point_2(12, 4);
-  Polyline_2 polyline2(&points2[0], &points2[5]);
+  std::list<Point_2>    points2;
+  points2.push_back (Point_2 (1, 3));
+  points2.push_back (Point_2 (0, 2));
+  points2.push_back (Point_2 (1, 0));
+  points2.push_back (Point_2 (2, 1));
+  points2.push_back (Point_2 (3, 0));
+  points2.push_back (Point_2 (4, 1));
+  points2.push_back (Point_2 (5, 0));
+  points2.push_back (Point_2 (6, 2));
+  points2.push_back (Point_2 (5, 3));
+  points2.push_back (Point_2 (4, 2));
+  Polyline_2            pi2 (points2.begin(), points2.end());
 
-  Point_2 points3[11];
-  points3[0] = Point_2(4, 4);
-  points3[1] = Point_2(2, 6);
-  points3[2] = Point_2(0, 4);
-  points3[3] = Point_2(2, 0);
-  points3[4] = Point_2(4, 2);
-  points3[5] = Point_2(6, 0);
-  points3[6] = Point_2(8, 2);
-  points3[7] = Point_2(10, 0);
-  points3[8] = Point_2(12, 4);
-  points3[9] = Point_2(10, 6);
-  points3[10] = Point_2(8, 4);
-  Polyline_2 polyline3(&points3[0], &points3[11]);
+  std::vector<Point_2>  points3 (4);
+  points3[0] = Point_2 (0, 2);
+  points3[1] = Point_2 (1, 2);
+  points3[2] = Point_2 (3, 6);
+  points3[3] = Point_2 (5, 2);
+  Polyline_2            pi3 (points3.begin(), points3.end());
   
-  insert(arr, polyline1);
-  insert(arr, polyline2);
-  insert(arr, polyline3);
+  insert(arr, pi1);
+  insert(arr, pi2);
+  insert(arr, pi3);
   
   print_arrangement (arr);
   return 0;
