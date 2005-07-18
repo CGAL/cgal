7ea01c31e88024e48e83ad69b1adce0e9bd4adf4
Costas Tsirogiannis <analekta@gmail.com>
2005-07-18 13:23:47 +0000

    Added bbox() member function

--- Packages/Curved_kernel/include/CGAL/Curved_kernel/Circular_arc_endpoint_2.h
+++ Packages/Curved_kernel/include/CGAL/Curved_kernel/Circular_arc_endpoint_2.h
@@ -20,6 +20,9 @@
 #include <cassert>
 #include <CGAL/Curved_kernel/Debug_id.h>
 
+#include <CGAL/Bbox_2.h>
+#include <CGAL/Interval_arithmetic.h>
+
 #include <CGAL/global_functions_on_circle_2.h>
 //#include <CGAL/global_functions_on_roots_and_polynomials_2_2.h> 
 // fixme, devrait
@@ -146,6 +149,15 @@ namespace CGALi {
     const Circle_2 & circle(int i) const { return i==0 ? _c0 : _c1; }
 
     bool is_left() const { return _b; }
+
+    Bbox_2 bbox() const
+    {
+       std::pair<double,double> ix=to_interval(x()),
+                                iy=to_interval(y());
+
+       return Bbox_2 (ix.first,iy.first,
+                      ix.second,iy.second);
+    }
 
   private:
     Circle_2  _c0, _c1;
