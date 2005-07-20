38758c3daf9c981e33ed4152191ed4559658ed33
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2005-07-20 21:15:46 +0000

    added copyright headers in demo files

--- Packages/Apollonius_graph_2/demo/Apollonius_graph_2/qt_file_toolbar.h
+++ Packages/Apollonius_graph_2/demo/Apollonius_graph_2/qt_file_toolbar.h
@@ -1,3 +1,23 @@
+// Copyright (c) 2003,2005  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $Source: /CVSROOT/CGAL/Packages/Apollonius_graph_2/include/CGAL/Apollonius_gr
+aph_2.h,v $
+// $Revision$ $Date$
+// $Name$
+//
+// Author(s)     : Menelaos Karavelas <mkaravel@cse.nd.edu>
+
 #ifndef QT_FILE_TOOLBAR_H
 #define QT_FILE_TOOLBAR_H
 
