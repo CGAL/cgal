442216892c42c24f142845bfe5c7b0f3d36d3572
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2005-07-26 15:44:36 +0000

    - Fix Qualified_result_of<CartesianKernelFunctors::Compute_squared_radius_2<K>,   typename K::Circle_2, int >

--- Packages/Cartesian_kernel/include/CGAL/Cartesian/function_objects.h
+++ Packages/Cartesian_kernel/include/CGAL/Cartesian/function_objects.h
@@ -889,12 +889,14 @@ namespace CartesianKernelFunctors {
 } //namespace CartesianKernelFunctors
 
 template < typename K>
-struct Qualified_result_of<CartesianKernelFunctors::Compute_squared_radius_2<K>, typename K::Circle_2, int >
+struct Qualified_result_of<CartesianKernelFunctors::Compute_squared_radius_2<K>,
+                           typename K::Circle_2>
 {
   typedef typename K::FT const &   type;
 };
 
-// For the non specialized template will do the right thing, namely return a copy of an FT
+// For the non specialized template will do the right thing,
+// namely return a copy of an FT
 
 namespace CartesianKernelFunctors {
 
