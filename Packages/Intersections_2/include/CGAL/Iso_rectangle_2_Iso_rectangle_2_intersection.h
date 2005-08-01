ca52a6aad076eed9b6af3edd42258515ab064ffb
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2005-08-01 16:40:09 +0000

    - Use Rational_traits instead of K::FT_{numerator|denominator}.

--- Packages/Intersections_2/include/CGAL/Iso_rectangle_2_Iso_rectangle_2_intersection.h
+++ Packages/Intersections_2/include/CGAL/Iso_rectangle_2_Iso_rectangle_2_intersection.h
@@ -39,6 +39,8 @@ intersection(
     const typename CGAL_WRAP(K)::Iso_rectangle_2 &irect2,
     const K&)
 {
+    typedef typename K::FT  FT;
+    Rational_traits<FT>  rt;
     typename K::Construct_point_2 construct_point_2;
     typename K::Construct_object_2 construct_object;
     typename K::Construct_iso_rectangle_2 construct_iso_rectangle_2;
@@ -56,22 +58,22 @@ intersection(
     miny = (min1.y() >= min2.y()) ? min1.y() : min2.y();
     maxy = (max1.y() <= max2.y()) ? max1.y() : max2.y();
     if (maxy < miny)
-        return Object(); 
-    if (K::FT_denominator(minx) == K::FT_denominator(miny)) {
-        newmin = construct_point_2(K::FT_numerator(minx), K::FT_numerator(miny),
-				   K::FT_denominator(minx));
+        return Object();
+    if (rt.denominator(minx) == rt.denominator(miny)) {
+        newmin = construct_point_2(rt.numerator(minx), rt.numerator(miny),
+				   rt.denominator(minx));
     } else {
-        newmin = construct_point_2(K::FT_numerator(minx)*K::FT_denominator(miny),
-				   K::FT_numerator(miny)*K::FT_denominator(minx),
-				   K::FT_denominator(minx) * K::FT_denominator(miny));
+        newmin = construct_point_2(rt.numerator(minx)   * rt.denominator(miny),
+				   rt.numerator(miny)   * rt.denominator(minx),
+				   rt.denominator(minx) * rt.denominator(miny));
     }
-    if (K::FT_denominator(maxx) == K::FT_denominator(maxy)) {
-        newmax = construct_point_2(K::FT_numerator(maxx), K::FT_numerator(maxy),
-				   K::FT_denominator(maxx));
+    if (rt.denominator(maxx) == rt.denominator(maxy)) {
+        newmax = construct_point_2(rt.numerator(maxx), rt.numerator(maxy),
+				   rt.denominator(maxx));
     } else {
-        newmax = construct_point_2(K::FT_numerator(maxx)*K::FT_denominator(maxy),
-				   K::FT_numerator(maxy)*K::FT_denominator(maxx),
-				   K::FT_denominator(maxx) * K::FT_denominator(maxy));
+        newmax = construct_point_2(rt.numerator(maxx)   * rt.denominator(maxy),
+				   rt.numerator(maxy)   * rt.denominator(maxx),
+				   rt.denominator(maxx) * rt.denominator(maxy));
     }
     return construct_object(construct_iso_rectangle_2(newmin, newmax));
 }
