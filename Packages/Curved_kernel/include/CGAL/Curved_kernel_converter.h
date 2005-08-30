de00b87875936b022ebbf039c66d6ccf743c02a4
Costas Tsirogiannis <analekta@gmail.com>
2005-08-30 12:32:07 +0000

    Added lots of stuff having to do with Lazy curved kernel utilisation e.g. converter for types encapsulated in objects or object vectors, pairs etc. Maybe there must be a derivation of this converter from the linear kernel converter

--- Packages/Curved_kernel/include/CGAL/Curved_kernel_converter.h
+++ Packages/Curved_kernel/include/CGAL/Curved_kernel_converter.h
@@ -6,6 +6,7 @@
 
 #include <CGAL/Cartesian_converter.h>
 #include <CGAL/Algebraic_kernel_converter.h>
+#include <CGAL/Object.h>
 
 // TODO :
 // - we should have a better default than Cartesian_converter.
@@ -29,12 +30,19 @@ public:
 	typedef AK_converter                		      Algebraic_kernel_converter;
 	typedef typename Linear_kernel_converter::Number_type_converter       RT_type_converter;
 	typedef typename Algebraic_kernel_converter::Root_of_type_converter   Root_of_type_converter;
+	
 
 	typename C2::Circle_2
 	operator()(const typename C1::Circle_2 &a) const
 	{
-		return Linear_kernel_converter()(a);
-	}
+	  return LK_converter()(a);
+        }
+
+	typename C2::Line_2
+	operator()(const typename C1::Line_2 &a) const
+	{
+	  return LK_converter()(a);
+        }
 
 	typename C2::Circular_arc_point_2
 	operator()(const typename C1::Circular_arc_point_2 &a) const
@@ -49,10 +57,71 @@ public:
 	typename C2::Circular_arc_2
 	operator()(const typename C1::Circular_arc_2 &a) const
 	{
-		return typename C2::Circular_arc_2 ( operator()( a.supporting_circle() ),
+		return typename C2::Circular_arc_2(operator()(a.supporting_circle()),
+		                     	           operator()(a.source()),
+		                     	           operator()(a.target()));	     
+	}
+
+
+	typename C2::Line_arc_2
+	operator()(const typename C1::Line_arc_2 &a) const
+	{
+		return typename C2::Line_arc_2 (     operator()( a.supporting_line() ),
 		                     		     operator()( a.source() ),
 		                     		     operator()( a.target() ) );
 	}
+	
+
+
+	  typename C2::Object_2
+    operator()(const typename C1::Object_2 &obj) const
+    {
+
+      if (const typename C1::Circular_arc_2 * ptr = object_cast<typename C1::Circular_arc_2>(&obj)) {
+        return make_object(operator()(*ptr));
+      } else if (const typename C1::Circular_arc_point_2 * ptr = 
+                 object_cast<typename C1::Circular_arc_point_2>(&obj)) {
+        return make_object(operator()(*ptr));
+      } else if (const std::pair<typename C1::Circular_arc_point_2,unsigned int> * ptr = 
+                 object_cast<std::pair<typename C1::Circular_arc_point_2,unsigned int> >(&obj)) {
+        return make_object(std::make_pair(operator()(ptr->first),ptr->second));
+      } else if (const typename C1::Line_arc_2 * ptr = 
+                 object_cast<typename C1::Line_arc_2>(&obj)) {
+        return make_object(operator()(*ptr));
+      }
+      CGAL_assertion_msg(false,"CurvedK_converter is unable to determine what is wrapped in the Object");
+      return Object();
+	
+    }
+	
+	
+  std::vector<typename C2::Object_2>
+  operator()(const std::vector<typename C1::Object_2>& v) const
+  {
+    std::vector<typename C2::Object_2> res;
+    res.reserve(v.size());
+    for(unsigned int i = 0; i < v.size(); i++){
+      res.push_back(operator()(v[i]));
+    }
+    return res;
+  }
+	
+        
+        
+  std::pair<typename C2::Line_arc_2,typename C2::Line_arc_2>
+  operator()(const std::pair<typename C1::Line_arc_2,typename C1::Line_arc_2> &a) const
+  {
+    return std::make_pair (operator()( a.first ),
+		           operator()( a.second ));
+  }
+
+  std::pair<typename C2::Circular_arc_2,typename C2::Circular_arc_2>
+  operator()(const std::pair<typename C1::Circular_arc_2,typename C1::Circular_arc_2> &a) const
+  {
+    return std::make_pair (operator()( a.first ),
+		           operator()( a.second ));
+  }
+	
 }; 
 
 CGAL_END_NAMESPACE
