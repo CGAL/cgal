d804628016d5b174892042ff388f21d7272c1fee
Julien Hazebrouck <Julien.Hazebrouck@sophia.inria.fr>
2005-09-05 11:51:32 +0000

    add examples

---
+++ Packages/Curved_kernel/examples/Curved_kernel/example_Circular_kernel_arrangement.C
@@ -0,0 +1,87 @@
+//#include <fstream>
+
+#include <CGAL/basic.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/point_generators_2.h>
+
+#include <CGAL/MP_Float.h>
+
+#include <CGAL/Algebraic_kernel_2_2.h>
+
+#include <CGAL/NT_extensions_Root_of/CGAL_Quotient.h>
+#include <CGAL/NT_extensions_Root_of/CGAL_Gmpq.h>
+//#include <CGAL/NT_extensions_Root_of/CGAL_Lazy_exact_nt.h>
+
+
+#include <CGAL/intersections.h>
+
+#include <CGAL/Circular_kernel.h>
+#include <CGAL/Circular_arc_traits.h>
+#include <CGAL/Circular_arc_traits_tracer.h>
+
+#include <CGAL/Arrangement_2.h>
+#include <CGAL/Arr_naive_point_location.h>
+
+
+
+#include <CGAL/Random.h>
+
+
+// typedef CGAL::MP_Float                                          NT;
+typedef CGAL::Gmpq                                          NT;
+// typedef CGAL::Lazy_exact_nt<CGAL::Gmpq>                     NT;
+// typedef CGAL::Lazy_exact_nt<CGAL::Quotient<CGAL::MP_Float> >    NT;
+// typedef CGAL::Lazy_exact_nt<CGAL::Quotient<CGAL::Gmpz> >    NT;
+typedef CGAL::Cartesian<NT>                                 Linear_k;
+
+typedef CGAL::Algebraic_kernel_2_2<NT>                      Algebraic_k;
+typedef CGAL::Curved_kernel<Linear_k,Algebraic_k>           Curved_k;
+// typedef CGAL::Curved_kernel<Linear_k>                        Curved_k;
+
+typedef Curved_k::Circular_arc_2                            Arc;
+typedef std::vector<Arc>                                    ArcContainer;
+
+#ifndef CGAL_CURVED_KERNEL_DEBUG
+typedef CGAL::Circular_arc_traits<Curved_k>                  Traits;
+#else
+typedef CGAL::Circular_arc_traits<Curved_k>                  Traits0;
+typedef CGAL::Circular_arc_traits_tracer<Traits0>            Traits;
+#endif
+
+//typedef Traits::Point_2                             Point_2;
+typedef Traits::Curve_2                             Conic_arc_2;
+typedef CGAL::Arrangement_2<Traits>                 Pmwx;
+typedef CGAL::Arr_naive_point_location<Pmwx>        Point_location;
+
+typedef Traits::X_monotone_curve_2                          X_monotone_curve_2;
+typedef Curved_k::Point_2                    Point_2;
+typedef Curved_k::Circle_2                    Circle_2;
+typedef Curved_k::Circular_arc_2              Circular_arc_2;
+int main(){
+  
+  CGAL::Random generatorOfgenerator;
+  int random_seed = generatorOfgenerator.get_int(0, 123456);
+  std::cout << "random_seed = " << random_seed << std::endl;
+  CGAL::Random theRandom(random_seed);
+  int random_max = 128;
+  int random_min = -128;
+  ArcContainer ac;
+  int x;
+  int y;
+  for(int i = 0; i < 10; i++){
+    x = theRandom.get_int(random_min,random_max);
+    y = theRandom.get_int(random_min,random_max);
+    ac.push_back( Circle_2( Point_2(x,y), x*x + y*y));
+  }
+
+  Pmwx _pm;
+  Point_location _pl(_pm);
+  for (ArcContainer::const_iterator it=ac.begin();
+       it != ac.end(); ++it) {
+    //insert(_pm,_pl,*it);
+    insert(_pm,*it,_pl);
+      };
+  
+  return 0;
+};
+
---
+++ Packages/Curved_kernel/examples/Curved_kernel/example_Circular_kernel_arrangement_Line_arc.C
@@ -0,0 +1,106 @@
+//#include <fstream>
+
+#include <CGAL/basic.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/point_generators_2.h>
+
+#include <CGAL/MP_Float.h>
+
+#include <CGAL/Algebraic_kernel_2_2.h>
+
+#include <CGAL/NT_extensions_Root_of/CGAL_Quotient.h>
+#include <CGAL/NT_extensions_Root_of/CGAL_Gmpq.h>
+//#include <CGAL/NT_extensions_Root_of/CGAL_Lazy_exact_nt.h>
+
+#include <CGAL/intersections.h>
+
+#include <CGAL/Circular_kernel.h>
+//#include <CGAL/Circular_arc_traits.h>
+#include <CGAL/Line_arc_traits.h>
+
+
+#include <CGAL/Arrangement_2.h>
+#include <CGAL/Arr_naive_point_location.h>
+
+
+
+#include <CGAL/Random.h>
+
+
+typedef CGAL::Gmpq                                          NT;
+typedef CGAL::Cartesian<NT>                                 Linear_k;
+
+typedef CGAL::Algebraic_kernel_2_2<NT>                      Algebraic_k;
+typedef CGAL::Curved_kernel<Linear_k,Algebraic_k>           Curved_k;
+
+//typedef Curved_k::Circular_arc_2                            Arc;
+typedef Curved_k::Line_arc_2                                Arc;
+typedef std::vector<Arc>                                    ArcContainer;
+
+#ifndef CGAL_CURVED_KERNEL_DEBUG
+typedef CGAL::Line_arc_traits<Curved_k>                  Traits;
+#else
+typedef CGAL::Line_arc_traits<Curved_k>                  Traits0;
+typedef CGAL::Circular_arc_traits_tracer<Traits0>            Traits;
+#endif
+
+
+typedef Traits::Curve_2                             Conic_arc_2;
+typedef CGAL::Arrangement_2<Traits>                 Pmwx;
+typedef CGAL::Arr_naive_point_location<Pmwx>        Point_location;
+
+typedef Traits::X_monotone_curve_2                  X_monotone_curve_2;
+typedef Curved_k::Point_2                           Point_2;
+typedef Curved_k::Circle_2                          Circle_2;
+typedef Curved_k::Circular_arc_2                    Circular_arc_2;
+typedef Curved_k::Line_arc_2                        Line_arc_2;
+int main(){
+  
+  CGAL::Random generatorOfgenerator;
+  int random_seed = generatorOfgenerator.get_int(0, 123456);
+  //std::cout << "random_seed = " << random_seed << std::endl;
+  CGAL::Random theRandom(random_seed);
+  int random_max = 5;
+  int random_min = -5;
+  for(int j = 0; j < 10 ; j++){
+  
+  ArcContainer ac;
+  int x1;
+  int y1;
+  int x2;
+  int y2;
+
+    for(int i = 0; i < 50; i++){
+      x1 = theRandom.get_int(random_min,random_max);
+      y1 = theRandom.get_int(random_min,random_max);
+      do{
+   	x2 = theRandom.get_int(random_min,random_max);
+   	y2 = theRandom.get_int(random_min,random_max);
+      }while((x1 == x2) && (y1 == y2));
+      
+    
+      std::cout << x1 << " "
+   		<< y1 << " "
+   		<< x2 << " "
+   		<< y2 << std::endl;
+      ac.push_back( Line_arc_2(Point_2(x1,y1), Point_2(x2,y2)));
+    }
+  
+  //while(std::cin >> x1 >> y1 >> x2 >> y2){
+   // ac.push_back( Line_arc_2(Point_2(x1,y1), Point_2(x2,y2)));
+  //}
+
+
+    int i = 0;
+    Pmwx _pm;
+    Point_location _pl(_pm);
+    for (ArcContainer::const_iterator it=ac.begin();
+	 it != ac.end(); ++it) {
+      std::cout << i++ << std::endl;
+      //insert(_pm,_pl,*it);
+      insert(_pm,*it,_pl);
+    };
+  }
+  return 0;
+};
+
