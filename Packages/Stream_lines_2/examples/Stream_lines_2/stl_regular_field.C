ef71123da143f95cd6f1fbca028f201d7460fe86
Abdelkrim Mebarki <Abdelkrim.Mebarki@sophia.inria.fr>
2005-09-14 16:14:33 +0000

    commit source files

--- Packages/Stream_lines_2/examples/Stream_lines_2/stl_regular_field.C
+++ Packages/Stream_lines_2/examples/Stream_lines_2/stl_regular_field.C
@@ -1,20 +1,17 @@
-#include <iostream.h>
-#include <fstream.h>
-
-#include <assert.h>
+#include <iostream>
+#include <fstream>
 
 #include <CGAL/basic.h>
 #include <CGAL/Cartesian.h>
 
-#include "CGAL/Stream_lines_2.h"
-#include "CGAL/Euler_integrator_2.h"
-#include "CGAL/Runge_kutta_integrator_2.h"
-#include "CGAL/Regular_grid_2.h"
-#include "CGAL/Triangular_field_2.h"
+#include <CGAL/Stream_lines_2.h>
+#include <CGAL/Euler_integrator_2.h>
+#include <CGAL/Runge_kutta_integrator_2.h>
+#include <CGAL/Regular_grid_2.h>
+#include <CGAL/Triangular_field_2.h>
 
 typedef double coord_type;
-typedef CGAL::Cartesian<coord_type> K1;
-typedef  struct K : public K1 {};
+typedef CGAL::Cartesian<coord_type> K;
 typedef CGAL::Regular_grid_2<K> Field;
 typedef CGAL::Euler_integrator_2<Field> Euler_integrator;
 typedef CGAL::Runge_kutta_integrator_2<Euler_integrator, Field> Runge_kutta_integrator;
@@ -26,27 +23,25 @@ int main(int argc, char **argv)
   Euler_integrator euler_integrator(1);  
   Runge_kutta_integrator runge_kutta_integrator(euler_integrator);
   
-  ifstream infile(argv[1], ios::in);
+  std::ifstream infile("data/vnoise.vec.cin", std::ios::in);
   double iXSize, iYSize;
   iXSize = iYSize = 512;
   Field regular_grid_2(infile, iXSize, iYSize);
   infile.close();
   
   /* the placement of streamlines */  
-  std::cout << "processing... " << endl;
-  double dSep = (long double) atof(argv[3]);
-  double dRat = (long double) atof(argv[4]);
+  std::cout << "processing...\n";
+  double dSep = 3.5;
+  double dRat = 1.6;
   Stl Stream_lines(regular_grid_2, runge_kutta_integrator,dSep,dRat);
-  std::cout << "success" << endl;
+  std::cout << "placement generated\n";
   
   Stl_iterator begin_iterator = Stream_lines.begin();
   Stl_iterator end_iterator = Stream_lines.end();
   
-  char * cName;
-  cName = argv[2];
-  strcat(cName, ".stl");
-  std::cout << cName << endl;
-  ofstream fw(cName,ios::out);
+	/*writing streamlines to streamlines_on_regular_grid.stl */
+  std::cout << "streamlines_on_regular_grid.stl\n";
+  std::ofstream fw("streamlines_on_regular_grid.stl",std::ios::out);
   Stream_lines.print_stream_lines(fw);
   
 }
--- Packages/Stream_lines_2/examples/Stream_lines_2/stl_triangular_field.C
+++ Packages/Stream_lines_2/examples/Stream_lines_2/stl_triangular_field.C
@@ -1,19 +1,16 @@
-#include <iostream.h>
-#include <fstream.h>
-
-#include <assert.h>
+#include <iostream>
+#include <fstream>
 
 #include <CGAL/basic.h>
 #include <CGAL/Cartesian.h>
 
-#include "CGAL/Stream_lines_2.h"
-#include "CGAL/Euler_integrator_2.h"
-#include "CGAL/Runge_kutta_integrator_2.h"
-#include "CGAL/Triangular_field_2.h"
+#include <CGAL/Stream_lines_2.h>
+#include <CGAL/Euler_integrator_2.h>
+#include <CGAL/Runge_kutta_integrator_2.h>
+#include <CGAL/Triangular_field_2.h>
 
 typedef double coord_type;
-typedef CGAL::Cartesian<coord_type> K1;
-typedef  struct K : public K1 {};
+typedef CGAL::Cartesian<coord_type> K;
 typedef CGAL::Triangular_field_2<K> Field;
 typedef CGAL::Euler_integrator_2<Field> Euler_integrator;
 typedef CGAL::Runge_kutta_integrator_2<Euler_integrator, Field> Runge_kutta_integrator;
@@ -25,24 +22,22 @@ int main(int argc, char **argv)
   Euler_integrator euler_integrator(1);
   Runge_kutta_integrator runge_kutta_integrator(euler_integrator);
   
-  ifstream infile(argv[1], ios::in);
+  std::ifstream infile("data/irregular_data.tri.cin", std::ios::in);
   Field triangular_field(infile);
   infile.close();
   
   /* the placement of streamlines */  
-  std::cout << "processing... " << endl;
-  double dSep = (long double) atof(argv[3]);
-  double dRat = (long double) atof(argv[4]);
+  std::cout << "processing...\n";
+  double dSep = 30.0;
+  double dRat = 1.6;
   Stl Stream_lines(triangular_field, runge_kutta_integrator,dSep,dRat);
-  std::cout << "success" << endl;
+  std::cout << "placement generated\n";
   
   stl_iterator begin_iterator = Stream_lines.begin();
   stl_iterator end_iterator = Stream_lines.end();
   
-  char * cName;
-  cName = argv[2];
-  strcat(cName, ".stl");
-  std::cout << cName << endl;
-  ofstream fw(cName,ios::out);
+	/*writing streamlines to streamlines.stl */
+  std::cout << "streamlines.stl\n";
+  std::ofstream fw("streamlines.stl",std::ios::out);
   Stream_lines.print_stream_lines(fw);
 }
