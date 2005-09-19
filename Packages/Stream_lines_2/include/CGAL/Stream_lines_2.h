5acf30a7a9ae4508b9378b07d87fe640e525c9ac
Abdelkrim Mebarki <Abdelkrim.Mebarki@sophia.inria.fr>
2005-09-19 09:58:41 +0000

    restoring print_eps

--- Packages/Stream_lines_2/include/CGAL/Stream_lines_2.h
+++ Packages/Stream_lines_2/include/CGAL/Stream_lines_2.h
@@ -119,8 +119,8 @@ public:
 	Stream_line_iterator_2 end();
 	// for visualizing streamlines
 	void print_stream_lines(std::ofstream & fw);
+	void print_stream_lines_eps(std::ofstream & fw);
 	std::list<Point_2> get_pq();
-// 	void print_stream_lines_eps(std::ofstream & fw);
 	unsigned int number_of_lines(){return _number_of_lines;};
 	std::list< std::pair<Point_2, Point_2> > get_tr()
 	{
@@ -705,9 +705,46 @@ void Stream_lines_2<VectorField_2, Integrator_2>::make_iterator(){
 	begin_iterator = iterator_container.begin();
 	end_iterator = iterator_container.end();}
 
+
+// output an eps file
+template <class VectorField_2, class Integrator_2>
+void
+Stream_lines_2<VectorField_2, Integrator_2>::print_stream_lines_eps(std::ofstream & fw)
+{
+	typename Stream_line_container_2::iterator begin_iterator;
+	Stream_line_container_2 stl_container_temp = stl_container;
+	Point_container_2 stl;
+	fw << "%!PS-Adobe-2.0 EPSF-2.0\n";
+	fw << "%%BoundingBox: 0 0" << " " << max_x - min_x << " " << max_y - min_y << "\n";
+	fw << "gsave\n";
+	fw << "/L {moveto lineto stroke} bind def\n";
+	fw << 0.5 << " setlinewidth\n";
+	fw << 0.0 << " " << 0.0 << " " << 0.0 << " setrgbcolor\n";
+	
+	for(begin_iterator=stl_container_temp.begin();begin_iterator!=stl_container_temp.end();begin_iterator++){
+		typename Point_container_2::iterator begin_point_iterator = (*begin_iterator).begin();
+		typename Point_container_2::iterator end_point_iterator = (*begin_iterator).end();
+		
+		FT i_prec = (*begin_point_iterator).x() - min_x;
+		FT j_prec = (*begin_point_iterator).y() - min_y;
+		begin_point_iterator++;
+
+		FT i , j;
+		for(;begin_point_iterator!=end_point_iterator;begin_point_iterator++){
+			Point_2 p = *begin_point_iterator;
+			i = p.x() - min_x;
+			j = p.y() - min_y;
+				fw << i_prec << " " << j_prec << " " << i << " " << j << " L\n";
+			i_prec = i;
+			j_prec = j;}};
+	fw << "grestore\n";
+	fw << "showpage\n";
+	fw.close();}
+
 // output an stl file
 template <class VectorField_2, class Integrator_2>
-void Stream_lines_2<VectorField_2, Integrator_2>::print_stream_lines(std::ofstream & fw)
+void 
+Stream_lines_2<VectorField_2, Integrator_2>::print_stream_lines(std::ofstream & fw)
 {
 	typename Stream_line_container_2::iterator begin_iterator;
 	Stream_line_container_2 stl_container_temp = stl_container;
