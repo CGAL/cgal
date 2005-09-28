b73b8a3b3f933388f97f6ad03124d04e8e50da88
Abdelkrim Mebarki <Abdelkrim.Mebarki@sophia.inria.fr>
2005-09-28 14:55:32 +0000

    update the example with the new definition of Triangular_fild class

--- Packages/Stream_lines_2/examples/Stream_lines_2/stl_triangular_field.C
+++ Packages/Stream_lines_2/examples/Stream_lines_2/stl_triangular_field.C
@@ -8,20 +8,26 @@
 #include <CGAL/Runge_kutta_integrator_2.h>
 #include <CGAL/Triangular_field_2.h>
 
-typedef double coord_type;
-typedef CGAL::Cartesian<coord_type> K;
-typedef CGAL::Triangular_field_2<K> Field;
-typedef CGAL::Runge_kutta_integrator_2<Field> Runge_kutta_integrator;
-typedef CGAL::Stream_lines_2<Field, Runge_kutta_integrator> Stl;
-typedef Stl::Stream_line_iterator_2 stl_iterator;
+typedef double                                                      coord_type;
+typedef CGAL::Cartesian<coord_type>                                 K;
+typedef K::Point_2                                                  Point;
+typedef K::Vector_2                                                 Vector;
+typedef CGAL::Triangular_field_2<K>                                 Field;
+typedef CGAL::Runge_kutta_integrator_2<Field>                       Runge_kutta_integrator;
+typedef CGAL::Stream_lines_2<Field, Runge_kutta_integrator>         Stl;
+typedef Stl::Stream_line_iterator_2                                 stl_iterator;
 
 int main()
 {
 	Runge_kutta_integrator runge_kutta_integrator(1);
 
-	std::ifstream infile("data/irregular_data.tri.cin", std::ios::in);
-	Field triangular_field(infile);
-	infile.close();
+	std::ifstream inp("data/datap.tri.cin");
+	std::ifstream inv("data/datav.tri.cin");
+  std::istream_iterator<Point> beginp(inp);
+  std::istream_iterator<Vector> beginv(inv);
+  std::istream_iterator<Point> endp;
+	
+	Field triangular_field(beginp, endp, beginv);
 
 	/* the placement of streamlines */
 	std::cout << "processing...\n";
