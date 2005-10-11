8e11566d869a5606b55788dccabb3ec53f911492
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
2005-10-11 14:02:09 +0000

    FT replaced by RT (looked like a bug to me - fix to be reversed if I am wrong...)

--- Packages/Algebraic_kernel/include/CGAL/Polynomials_1_2.h
+++ Packages/Algebraic_kernel/include/CGAL/Polynomials_1_2.h
@@ -1,44 +1,42 @@
 #ifndef CGAL_ALGEBRAIC_KERNEL_POLYNOMIALS_1_2_H
 #define CGAL_ALGEBRAIC_KERNEL_POLYNOMIALS_1_2_H
 
-//////////// FIXME - pb RT (cas general Polynomial_1_2) ou FT (ici)
-
 #include <CGAL/enum.h>
 
 CGAL_BEGIN_NAMESPACE
 
-template < typename FT_ >
+template < typename RT_ >
 class Polynomial_1_2
 {
-  FT_ rep[3]; // stores a, b, c for line ax+by+c=0
+  RT_ rep[3]; // stores a, b, c for line ax+by+c=0
   
 public:
   
-  typedef FT_ FT;
+  typedef RT_ RT;
   
   Polynomial_1_2(){}
   
-  Polynomial_1_2(const FT & a, const FT & b, const FT & c)
+  Polynomial_1_2(const RT & a, const RT & b, const RT & c)
   { 
     rep[0]=a;
     rep[1]=b;
     rep[2]=c;
   }
 
-  const FT & a() const
+  const RT & a() const
   { return rep[0]; }
 
-  const FT & b() const
+  const RT & b() const
   { return rep[1]; }
 
-  const FT & c() const
+  const RT & c() const
   { return rep[2]; }
 };
 
-template < typename FT >
+template < typename RT >
 bool 
-operator == ( const Polynomial_1_2<FT> & p1,
-	      const Polynomial_1_2<FT> & p2 )
+operator == ( const Polynomial_1_2<RT> & p1,
+	      const Polynomial_1_2<RT> & p2 )
 {
   return( (p1.a() == p2.a()) && 
               (p1.b() == p2.b()) &&
