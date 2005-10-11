11d170e6c9fa2901428633093aa2ec9aadabe1aa
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
2005-10-11 13:51:02 +0000

    cosmetic

--- Packages/Algebraic_kernel/include/CGAL/Polynomials_1_2.h
+++ Packages/Algebraic_kernel/include/CGAL/Polynomials_1_2.h
@@ -5,48 +5,46 @@
 
 #include <CGAL/enum.h>
 
-namespace CGAL {
+CGAL_BEGIN_NAMESPACE
 
-  template < typename FT_ >
-    class Polynomial_1_2
-    {
-      FT_ rep[3]; // stores a, b, c for line ax+by+c=0
-
-    public:
-
-      typedef FT_ FT;
-
-      Polynomial_1_2(){}
-
-      Polynomial_1_2(const FT & a,
-		     const FT & b,
-		     const FT & c)
-	{ 
-	  rep[0]=a;
-	  rep[1]=b;
-	  rep[2]=c;
-	}
-
-      const FT & a() const
-	{ return rep[0]; }
-      const FT & b() const
-	{ return rep[1]; }
-      const FT & c() const
-	{ return rep[2]; }
-
-    };
-
-  template < typename FT >
-  bool 
-  operator == ( const Polynomial_1_2<FT> & p1,
-		const Polynomial_1_2<FT> & p2 )
-  {
-    return( (p1.a() == p2.a()) && 
-	    (p1.b() == p2.b()) &&
-	    (p1.c() == p2.c()) );
+template < typename FT_ >
+class Polynomial_1_2
+{
+  FT_ rep[3]; // stores a, b, c for line ax+by+c=0
+  
+public:
+  
+  typedef FT_ FT;
+  
+  Polynomial_1_2(){}
+  
+  Polynomial_1_2(const FT & a, const FT & b, const FT & c)
+  { 
+    rep[0]=a;
+    rep[1]=b;
+    rep[2]=c;
   }
-    
 
-} // namespace CGAL
+  const FT & a() const
+  { return rep[0]; }
+
+  const FT & b() const
+  { return rep[1]; }
+
+  const FT & c() const
+  { return rep[2]; }
+};
+
+template < typename FT >
+bool 
+operator == ( const Polynomial_1_2<FT> & p1,
+	      const Polynomial_1_2<FT> & p2 )
+{
+  return( (p1.a() == p2.a()) && 
+              (p1.b() == p2.b()) &&
+              (p1.c() == p2.c()) );
+}
+    
+CGAL_END_NAMESPACE
 
 #endif //CGAL_ALGEBRAIC_KERNEL_POLYNOMIALS_1_2_H
