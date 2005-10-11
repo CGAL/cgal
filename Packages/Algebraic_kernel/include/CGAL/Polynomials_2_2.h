71bbbe4a02cb03309a0d96d49d05911eb590cbc0
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
2005-10-11 14:12:35 +0000

    cosmetic

--- Packages/Algebraic_kernel/include/CGAL/Polynomials_2_2.h
+++ Packages/Algebraic_kernel/include/CGAL/Polynomials_2_2.h
@@ -5,49 +5,47 @@
 
 #include <CGAL/enum.h>
 
-namespace CGAL {
+CGAL_BEGIN_NAMESPACE
 
-  // polynomials of the form (X-a)^2 + (Y-b)^2 - R^2
-  template < typename FT_ >
-    class Polynomial_for_circles_2_2
-    {
-      FT_ rep[3]; // stores a, b, R^2
-
-    public:
-
-      typedef FT_ FT;
-
-      Polynomial_for_circles_2_2(){}
-
-      Polynomial_for_circles_2_2(const FT & a,
-				 const FT & b,
-				 const FT & rsq)
-	{ 
-	  rep[0]=a;
-	  rep[1]=b;
-	  rep[2]=rsq;
-	}
-
-      const FT & a() const
-	{ return rep[0]; }
-      const FT & b() const
-	{ return rep[1]; }
-      const FT & r_sq() const
-	{ return rep[2]; }
-
-    };
-
-  template < typename FT >
-  bool 
-  operator == ( const Polynomial_for_circles_2_2<FT> & p1,
-		const Polynomial_for_circles_2_2<FT> & p2 )
-  {
-    return( (p1.a() == p2.a()) && 
-	    (p1.b() == p2.b()) &&
-	    (p1.r_sq() == p2.r_sq()) );
+// polynomials of the form (X-a)^2 + (Y-b)^2 - R^2
+template < typename FT_ >
+class Polynomial_for_circles_2_2
+{
+  FT_ rep[3]; // stores a, b, R^2
+  
+public:
+  
+  typedef FT_ FT;
+  
+  Polynomial_for_circles_2_2(){}
+  
+  Polynomial_for_circles_2_2(const FT & a, const FT & b, const FT & rsq)
+  { 
+    rep[0]=a;
+    rep[1]=b;
+    rep[2]=rsq;
   }
-    
 
-} // namespace CGAL
+  const FT & a() const
+  { return rep[0]; }
+
+  const FT & b() const
+  { return rep[1]; }
+  
+  const FT & r_sq() const
+  { return rep[2]; }
+};
+
+template < typename FT >
+bool 
+operator == ( const Polynomial_for_circles_2_2<FT> & p1,
+	      const Polynomial_for_circles_2_2<FT> & p2 )
+{
+  return( (p1.a() == p2.a()) && 
+              (p1.b() == p2.b()) &&
+              (p1.r_sq() == p2.r_sq()) );
+}
+
+CGAL_END_NAMESPACE
 
 #endif //CGAL_ALGEBRAIC_KERNEL_POLYNOMIALS_2_2_H
