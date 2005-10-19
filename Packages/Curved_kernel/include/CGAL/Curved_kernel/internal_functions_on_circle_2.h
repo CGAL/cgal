59d106166f352f6b6c38f4ce0ffadfb087589dff
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
2005-10-19 15:22:02 +0000

    typo bug fix + indentation

--- Packages/Curved_kernel/include/CGAL/Curved_kernel/internal_functions_on_circle_2.h
+++ Packages/Curved_kernel/include/CGAL/Curved_kernel/internal_functions_on_circle_2.h
@@ -2,10 +2,9 @@
 #define CGAL_CURVED_KERNEL_INTERNAL_FUNCTIONS_ON_CIRCLE_2_H
 #include <CGAL/global_functions_on_roots_and_polynomials_2_2.h>
 
-namespace CGAL {
+CGAL_BEGIN_NAMESPACE
 
 // temporary function : where to put it, if we want to keep it ?
-
 template< class CK>
 typename CK::Circular_arc_point_2
 circle_intersect( const typename CK::Circle_2 & c1,
@@ -13,24 +12,28 @@ circle_intersect( const typename CK::Circle_2 & c1,
 		  bool b )
 {
   typedef std::vector<CGAL::Object > solutions_container;
-
+  
   solutions_container solutions;
-  CGAL::intersect_2<CK>
-	( c1, c2, std::back_inserter(solutions) );
+  CGAL::intersect_2<CK>( c1, c2, std::back_inserter(solutions) );
+  
   typename solutions_container::iterator it = solutions.begin();
-
+  
   CGAL_kernel_precondition( it != solutions.end() ); 
   // the circles intersect
-
+  
   const std::pair<typename CK::Circular_arc_point_2, uint> *result;
   result = CGAL::object_cast< 
     std::pair<typename CK::Circular_arc_point_2, uint> >(&(*it));
+  
   if ( result->second == 2 ) // double solution
     return result->first;
-  if (b) return result->first;
+  
+  if (b) 
+    return result->first;
+  
   ++it;
   result = CGAL::object_cast< 
-        std::pair<typename CK::Circular_arc_point_2, uint> >(&(*it));
+    std::pair<typename CK::Circular_arc_point_2, uint> >(&(*it));
   return result->first;
 }
 
@@ -44,7 +47,7 @@ namespace CircularFunctors {
  
     typedef typename CK::Algebraic_kernel   AK;
 
-    return AK().construct_polynomial_circle_2_2_object()
+    return AK().construct_polynomial_for_circles_2_2_object()
       ( c.center().x(), c.center().y(), c.squared_radius() );
   }
   
@@ -58,36 +61,33 @@ namespace CircularFunctors {
   template< class CK, class OutputIterator>
   OutputIterator
   intersect_2( const typename CK::Circle_2 & c1,
-			     const typename CK::Circle_2 & c2,
-			     OutputIterator res )
+	       const typename CK::Circle_2 & c2,
+	       OutputIterator res )
   {
     typedef typename CK::Polynomial_for_circles_2_2 Equation; 
     typedef typename CK::Root_for_circles_2_2 Root_for_circles_2_2;
     Equation e1 = get_equation<CK>(c1);
     Equation e2 = get_equation<CK>(c2);
     
-     if(e1 == e2){
+    if (e1 == e2) {
       *res++ = make_object(e1);
       return res;
     }
 
-    typedef std::vector
-      < std::pair < Root_for_circles_2_2, unsigned > > 
+    typedef std::vector< std::pair < Root_for_circles_2_2, unsigned > > 
       solutions_container;
     solutions_container solutions;
     
     CGAL::solve<typename CK::Algebraic_kernel>
       ( e1,e2, std::back_inserter(solutions) ); // to be optimized
 
-
     typedef typename CK::Circular_arc_point_2 Circular_arc_point_2;
-
+    
     for ( typename solutions_container::iterator it = solutions.begin(); 
-	    it != solutions.end(); ++it )
+	  it != solutions.end(); ++it )
       {
-	*res++ = make_object
-	  (std::make_pair(Circular_arc_point_2(it->first)
-			  , it->second ));
+        *res++ = make_object(std::make_pair(Circular_arc_point_2(it->first),
+			                      it->second ));
       }
 
     return res;
@@ -128,6 +128,7 @@ namespace CircularFunctors {
 
 
 } // namespace CircularFunctors
-} // namespace CGAL
+
+CGAL_END_NAMESPACE
 
 #endif // CGAL_CURVED_KERNEL_INTERNAL_FUNCTIONS_ON_CIRCLE_2_H
