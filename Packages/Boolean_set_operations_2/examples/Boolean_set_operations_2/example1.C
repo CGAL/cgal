97241315ce7c2799a7f1ca3eae5c8421b08e0a39
Efi Fogel <efif@post.tau.ac.il>
2005-11-20 09:39:59 +0000

    1st revision

---
+++ Packages/Boolean_set_operations_2/examples/Boolean_set_operations_2/example1.C
@@ -0,0 +1,22 @@
+#include <CGAL/Cartesian.h>
+#include <CGAL/Polygon_2.h>
+#include <CGAL/Gmpq.h>
+#include <CGAL/Boolean_set_operations_2.h>
+
+typedef CGAL::Gmpq                                      Number_type;
+typedef CGAL::Cartesian<Number_type>                    Kernel;
+typedef Kernel::Point_2                                 Point;
+typedef CGAL::Polygon_2<Kernel>                         Polygon;
+
+int main ()
+{
+  Polygon p1, p2;
+  p1.push_back(Point(-1,1));
+  p1.push_back(Point(0,-1));
+  p1.push_back(Point(1,1));
+  p2.push_back(Point(-1,-1));
+  p2.push_back(Point(1,-1));
+  p2.push_back(Point(0,1));
+  std::cout << CGAL::do_intersect(p1, p2) ? "TRUE" : "FALSE";
+  return 0;
+}
