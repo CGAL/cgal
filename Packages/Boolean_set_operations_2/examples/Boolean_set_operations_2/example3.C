ad32052186ab7763010487283831104a8bc728c2
Baruch Zukerman <baruchzu@post.tau.ac.il>
2005-11-29 09:29:20 +0000

    fixed the example

--- Packages/Boolean_set_operations_2/examples/Boolean_set_operations_2/example3.C
+++ Packages/Boolean_set_operations_2/examples/Boolean_set_operations_2/example3.C
@@ -15,18 +15,26 @@ typedef CGAL::Cartesian<Number_type>                    Kernel;
 typedef Kernel::Point_2                                 Point;
 typedef CGAL::Polygon_2<Kernel>                         Polygon;
 typedef CGAL::General_polygon_with_holes_2<Polygon>     Polygon_with_holes;
-typedef CGAL::Gps_segment_traits_2                      Traits;
+typedef CGAL::Gps_segment_traits_2<Kernel>              Traits;
 typedef CGAL::General_polygon_set_2<Traits>             Polygon_set;
 
 int main(int argc, char * argv[])
 {
   Polygon p1, p2, rect;
-  p1.push_back(Point(0,1)); p1.push_back(Point(2,0));
-  p1.push_back(Point(1,1)); p1.push_back(Point(2,2));
-  p2.push_back(Point(3,1)); p2.push_back(Point(1,2));
-  p2.push_back(Point(2,1)); p2.push_back(Point(1,0));
-  p2.push_back(Point(0,0)); p2.push_back(Point(3,0));
-  p2.push_back(Point(3,2)); p2.push_back(Point(0,2));
+  p1.push_back(Point(0,1));
+  p1.push_back(Point(2,0));
+  p1.push_back(Point(1,1));
+  p1.push_back(Point(2,2));
+
+  p2.push_back(Point(3,1));
+  p2.push_back(Point(1,2));
+  p2.push_back(Point(2,1));
+  p2.push_back(Point(1,0));
+
+  rect.push_back(Point(0,0));
+  rect.push_back(Point(3,0));
+  rect.push_back(Point(3,2));
+  rect.push_back(Point(0,2));
   
   Polygon_set ps;
   ps.insert(p1);
@@ -35,11 +43,11 @@ int main(int argc, char * argv[])
   ps.intersection(rect);        // Compute the intersection
   
   std::list<Polygon_with_holes> result;
-  ps.polygons(result);
+  ps.polygons_with_holes(std::back_inserter(result));
 
   // Export to standard output:
-  std::copy(results.begin(), results.end(),
-            std::ostream_iterator<Polygon_with_holes>(std::cout, ","));
+  std::copy(result.begin(), result.end(),
+            std::ostream_iterator<Polygon_with_holes>(std::cout, "\n"));
   std::cout << std::endl;
   
   return 0;
