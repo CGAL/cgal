8f707092ae023ad7479f9c91d68911af7b9421e8
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2005-12-17 19:25:26 +0000

    - New file which for declaring functions on number types   (needed for the current NT interface with g++ >= 3.4)

---
+++ Packages/Algebraic_kernel/include/CGAL/Algebraic_kernel/basic.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2005  INRIA Sophia-Antipolis (France) 
+// All rights reserved.
+//
+// Authors : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
+//           Sylvain Pion     <Sylvain.Pion@sophia.inria.fr>
+// 
+// Partially supported by the IST Programme of the EU as a Shared-cost
+// RTD (FET Open) Project under Contract No  IST-2000-26473 
+// (ECG - Effective Computational Geometry for Curves and Surfaces) 
+// and a STREP (FET Open) Project under Contract No  IST-006413 
+// (ACS -- Algorithms for Complex Shapes)
+
+#ifndef CGAL_ALGEBRAIC_KERNEL_BASIC_H
+#define CGAL_ALGEBRAIC_KERNEL_BASIC_H
+
+// This file currently gathers function declarations.
+// This is needed by the way the current NT interface works.
+
+#include <CGAL/number_type_basic.h>
+#include <utility>
+
+CGAL_BEGIN_NAMESPACE
+
+class MP_Float;
+template < typename T > class Root_of_2;
+template < typename T > class Lazy_exact_nt;
+
+template < typename T >
+std::pair<double,double> to_interval(const Root_of_2<T>&);
+
+CGAL_END_NAMESPACE
+
+#endif // CGAL_ALGEBRAIC_KERNEL_BASIC_H
