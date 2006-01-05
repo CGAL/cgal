bb70d5a0f25db4511afdb7d5133c73f0426f611d
Ron Wein <wein@post.tau.ac.il>
2006-01-05 16:12:13 +0000

    Revised the User Manual and most of the examples.

--- Packages/Boolean_set_operations_2/examples/Boolean_set_operations_2/ex_circle_segment.C
+++ Packages/Boolean_set_operations_2/examples/Boolean_set_operations_2/ex_circle_segment.C
@@ -9,69 +9,103 @@
 
 #include <list>
 
-typedef CGAL::Gmpq                                      NT;
-typedef CGAL::Cartesian<NT>                             Kernel;
-typedef CGAL::Gps_circle_segment_traits_2<Kernel>       Traits;
-typedef CGAL::General_polygon_set_2<Traits>             Polygon_set_2;
-typedef Traits::Polygon_2                               Polygon_2;
-typedef Traits::Polygon_with_holes_2                    Polygon_with_holes_2;
-typedef Traits::Curve_2                                 Curve_2;
-typedef Traits::X_monotone_curve_2                      X_monotone_curve_2;
-typedef Kernel::Point_2                                 Point_2;
-typedef Kernel::Circle_2                                Circle_2;
+typedef CGAL::Gmpq                                  NT;
+typedef CGAL::Cartesian<NT>                         Kernel;
+typedef Kernel::Point_2                             Point_2;
+typedef Kernel::Circle_2                            Circle_2;
+typedef CGAL::Gps_circle_segment_traits_2<Kernel>   Traits_2;
 
-void circle_2_polygon(Circle_2 circle, Polygon_2 & polygon)
+typedef CGAL::General_polygon_set_2<Traits_2>    Polygon_set_2;
+typedef Traits_2::Polygon_2                      Polygon_2;
+typedef Traits_2::Polygon_with_holes_2           Polygon_with_holes_2;
+typedef Traits_2::Curve_2                        Curve_2;
+typedef Traits_2::X_monotone_curve_2             X_monotone_curve_2;
+
+// Construct a polygon from a circle.
+Polygon_2 construct_polygon (const Circle_2& circle)
 {
-  Traits traits;
-  Curve_2 curve(circle);
-  std::list<CGAL::Object> objects;
-  traits.make_x_monotone_2_object()(curve, std::back_inserter(objects));
-  std::list<CGAL::Object>::iterator i = objects.begin();
-  X_monotone_curve_2 xcurve;
-  CGAL::assign(xcurve, *i++);   polygon.push_back(xcurve);
-  CGAL::assign(xcurve, *i);     polygon.push_back(xcurve);
+  // Subdivide the circle into two x-monotone arcs.
+  Traits_2                 traits;
+  Curve_2                  curve (circle);
+  std::list<CGAL::Object>  objects;
+  traits.make_x_monotone_2_object() (curve,
+                                     std::back_inserter(objects));
+  CGAL_assertion (objects.size() == 2);
+
+  // Construct the polygon.
+  Polygon_2                         pgn;
+  X_monotone_curve_2                arc;
+  std::list<CGAL::Object>::iterator iter;
+
+  for (iter = objects.begin(); iter != objects.end(); ++iter)
+  {
+    CGAL::assign (arc, *iter);
+    pgn.push_back (arc);
+  }
+
+  return (pgn);
 }
 
-void rect_2_polygon(Point_2 p1, Point_2 p2, Point_2 p3, Point_2 p4,
-                    Polygon_2 & polygon)
+// Construct a point from a rectangle.
+Polygon_2 construct_polygon (const Point_2& p1, const Point_2& p2,
+                             const Point_2& p3, const Point_2& p4)
 {
-  X_monotone_curve_2 s1(p1, p2);  polygon.push_back(s1);
-  X_monotone_curve_2 s2(p2, p3);  polygon.push_back(s2);
-  X_monotone_curve_2 s3(p3, p4);  polygon.push_back(s3);
-  X_monotone_curve_2 s4(p4, p1);  polygon.push_back(s4);
+  Polygon_2          pgn;
+  X_monotone_curve_2 s1(p1, p2);
+  pgn.push_back(s1);
+  X_monotone_curve_2 s2(p2, p3);
+  pgn.push_back(s2);
+  X_monotone_curve_2 s3(p3, p4);
+  pgn.push_back(s3);
+  X_monotone_curve_2 s4(p4, p1);
+  pgn.push_back(s4);
+
+  return (pgn);
 }
 
-int main(int argc, char * argv[])
+// The main program:
+int main ()
 {
-  Polygon_2 pc1, pc2, pc3, pc4, pr1, pr2, pr3, pr4;
-  Polygon_set_2 ps;
+  // Insert four non-intersecting circles.
+  Polygon_set_2  S; 
+  Polygon_2      circ1, circ2, circ3, circ4;
 
-  // Input non-intersecting circles:
-  circle_2_polygon(Circle_2(Point_2(1, 1), 1), pc1);  ps.insert(pc1);
-  circle_2_polygon(Circle_2(Point_2(5, 1), 1), pc2);  ps.insert(pc2);
-  circle_2_polygon(Circle_2(Point_2(5, 5), 1), pc3);  ps.insert(pc3);
-  circle_2_polygon(Circle_2(Point_2(1, 5), 1), pc4);  ps.insert(pc4);
+  circ1 = construct_polygon (Circle_2 (Point_2 (1, 1), 1));
+  S.insert (circ1);
+  circ2 = construct_polygon (Circle_2 (Point_2 (5, 1), 1));
+  S.insert (circ2);
+  circ3 = construct_polygon (Circle_2 (Point_2 (5, 5), 1));
+  S.insert (circ3);
+  circ4 = construct_polygon (Circle_2 (Point_2 (1, 5), 1));
+  S.insert (circ4);
   
-  // Perform union with rectangles incrementally:
-  rect_2_polygon(Point_2(1,0), Point_2(5,0), Point_2(5,2), Point_2(1,2), pr1);
-  ps.join(pr1);
+  // Compute the union with four rectangles incrementally.
+  Polygon_2      rect1, rect2, rect3, rect4;
 
-  rect_2_polygon(Point_2(1,4), Point_2(5,4), Point_2(5,6), Point_2(1,6), pr2);
-  ps.join(pr2);
+  rect1 = construct_polygon (Point_2 (1, 0), Point_2 (5, 0),
+                             Point_2 (5, 2), Point_2 (1, 2));
+  S.join (rect1);
 
-  rect_2_polygon(Point_2(0,1), Point_2(0,5), Point_2(2,5), Point_2(2,1), pr3);
-  ps.join(pr3);
+  rect2 = construct_polygon (Point_2 (1, 4), Point_2 (5, 4),
+                             Point_2 (5, 6), Point_2 (1, 6));
+  S.join (rect2);
 
-  rect_2_polygon(Point_2(4,1), Point_2(4,5), Point_2(6,5), Point_2(6,1), pr4);
-  ps.join(pr4);
+  rect3 = construct_polygon (Point_2 (0, 1), Point_2 (0, 5),
+                             Point_2 (2, 5), Point_2 (2, 1));
+  S.join (rect3);
 
-  // Output:
-  std::list<Polygon_with_holes_2> result;
-  ps.polygons_with_holes(std::back_inserter(result));
+  rect4 = construct_polygon (Point_2 (4, 1), Point_2 (4, 5),
+                             Point_2 (6, 5), Point_2 (6, 1));
+  S.join (rect4);
 
-  std::copy(result.begin(), result.end(),       // export to standard output
-            std::ostream_iterator<Polygon_with_holes_2>(std::cout, "\n"));
+  // Print the output.
+  std::list<Polygon_with_holes_2>   res;
+  S.polygons_with_holes (std::back_inserter (res));
+
+  std::copy (res.begin(), res.end(),
+             std::ostream_iterator<Polygon_with_holes_2>(std::cout,
+                                                         "\n"));
   std::cout << std::endl;
   
-  return 0;
+  return (0);
 }
--- Packages/Boolean_set_operations_2/examples/Boolean_set_operations_2/ex_set_union.C
+++ Packages/Boolean_set_operations_2/examples/Boolean_set_operations_2/ex_set_union.C
@@ -8,57 +8,83 @@
 
 #include <list>
 #include <stdlib.h>
-#include <cmath>
+#include <math.h>
 
-typedef CGAL::Gmpq                                      NT;
-typedef CGAL::Cartesian<NT>                             Kernel;
-typedef CGAL::Gps_circle_segment_traits_2<Kernel>       Traits;
-typedef Traits::Polygon_2                               Polygon_2;
-typedef Traits::Polygon_with_holes_2                    Polygon_with_holes_2;
-typedef Traits::Curve_2                                 Curve_2;
-typedef Traits::X_monotone_curve_2                      X_monotone_curve_2;
-typedef Kernel::Point_2                                 Point_2;
-typedef Kernel::Circle_2                                Circle_2;
+typedef CGAL::Gmpq                                  NT;
+typedef CGAL::Cartesian<NT>                         Kernel;
+typedef Kernel::Point_2                             Point_2;
+typedef Kernel::Circle_2                            Circle_2;
+typedef CGAL::Gps_circle_segment_traits_2<Kernel>   Traits_2;
 
-void circle_2_polygon(Circle_2 circle, Polygon_2 & polygon)
+typedef CGAL::General_polygon_set_2<Traits_2>    Polygon_set_2;
+typedef Traits_2::Polygon_2                      Polygon_2;
+typedef Traits_2::Polygon_with_holes_2           Polygon_with_holes_2;
+typedef Traits_2::Curve_2                        Curve_2;
+typedef Traits_2::X_monotone_curve_2             X_monotone_curve_2;
+
+// Construct a polygon from a circle.
+Polygon_2 construct_polygon (const Circle_2& circle)
 {
-  Traits traits;
-  Curve_2 curve(circle);
-  std::list<CGAL::Object> objects;
-  traits.make_x_monotone_2_object()(curve, std::back_inserter(objects));
-  std::list<CGAL::Object>::iterator i = objects.begin();
-  X_monotone_curve_2 xcurve;
-  CGAL::assign(xcurve, *i++);   polygon.push_back(xcurve);
-  CGAL::assign(xcurve, *i);     polygon.push_back(xcurve);
+  // Subdivide the circle into two x-monotone arcs.
+  Traits_2                 traits;
+  Curve_2                  curve (circle);
+  std::list<CGAL::Object>  objects;
+  traits.make_x_monotone_2_object() (curve,
+                                     std::back_inserter(objects));
+  CGAL_assertion (objects.size() == 2);
+
+  // Construct the polygon.
+  Polygon_2                         pgn;
+  X_monotone_curve_2                arc;
+  std::list<CGAL::Object>::iterator iter;
+
+  for (iter = objects.begin(); iter != objects.end(); ++iter)
+  {
+    CGAL::assign (arc, *iter);
+    pgn.push_back (arc);
+  }
+
+  return (pgn);
 }
 
-int main(int argc, char * argv[])
+// The main program:
+int main (int argc, char * argv[])
 {
-  const double pi = std::atan(1.0) * 4;
+  // Read the number of circles from the command line.
+  unsigned int   n_circles = 8;
 
-  unsigned int circles_num = 8;
-  if (argc > 1) circles_num = atoi(argv[1]);
+  if (argc > 1) 
+    n_circles = atoi(argv[1]);
 
-  double circles_num_reciep = 1.0 / circles_num;
-  double radius = 1;
-  double frac = 2 * pi * circles_num_reciep;
-  std::list<Polygon_2> polygons;
-  for (unsigned int i = 0; i < circles_num; ++i) {
-    double angle = frac * i;
-    double x = radius * std::sin(angle);
-    double y = radius * std::cos(angle);
-    Point_2 center = Point_2(x, y);
-    Circle_2 circle(center, radius);
-    Polygon_2 polygon;
-    circle_2_polygon(circle, polygon);
-    polygons.push_back(polygon);
+  // Create the circles, equally spaced of the circle x^2 + y^2 = 1.
+  const double          pi = std::atan(1.0) * 4;
+  const double          n_circles_reciep = 1.0 / n_circles;
+  const double          radius = 1;
+  const double          frac = 2 * pi * n_circles_reciep;
+  std::list<Polygon_2>  circles;
+  unsigned int          k;
+
+  for (k = 0; k < n_circles; k++)
+  {
+    const double   angle = frac * k;
+    const double   x = radius * std::sin(angle);
+    const double   y = radius * std::cos(angle);
+    Point_2        center = Point_2(x, y);
+    Circle_2       circle(center, radius);
+
+    circles.push_back (construct_polygon (circle));
   }
   
-  std::list<Polygon_with_holes_2> result;
-  join(polygons.begin(), polygons.end(), std::back_inserter(result));
+  // Compute the union aggragately.
+  std::list<Polygon_with_holes_2>   res;
 
-  std::copy(result.begin(), result.end(),       // export to standard output
-            std::ostream_iterator<Polygon_with_holes_2>(std::cout, "\n"));
+  CGAL::join (circles.begin(), circles.end(),
+              std::back_inserter (res));
+
+  // Print the result.
+  std::copy (res.begin(), res.end(),
+             std::ostream_iterator<Polygon_with_holes_2>(std::cout, 
+                                                         "\n"));
   std::cout << std::endl;
   
   return 0;
