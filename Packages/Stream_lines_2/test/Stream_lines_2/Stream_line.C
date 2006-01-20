0eae376370c7907e13c49c218184a00849eea795
Abdelkrim Mebarki <Abdelkrim.Mebarki@sophia.inria.fr>
2006-01-20 09:50:57 +0000

    filtered_kernel

--- Packages/Stream_lines_2/test/Stream_lines_2/Stream_line.C
+++ Packages/Stream_lines_2/test/Stream_lines_2/Stream_line.C
@@ -2,6 +2,7 @@
 #include <fstream>
 
 #include <CGAL/Cartesian.h>
+#include <CGAL/Filtered_kernel.h>
 
 #include <CGAL/Stream_lines_2.h>
 #include <CGAL/Euler_integrator_2.h>
@@ -10,7 +11,8 @@
 #include <CGAL/Triangular_field_2.h>
 
 typedef double coord_type;
-typedef CGAL::Cartesian<coord_type> K;
+typedef CGAL::Cartesian<coord_type> CK;
+typedef CGAL::Filtered_kernel<CK> K;
 typedef CGAL::Regular_grid_2<K> Field;
 typedef CGAL::Runge_kutta_integrator_2<Field> Runge_kutta_integrator;
 typedef CGAL::Stream_lines_2<Field, Runge_kutta_integrator> Stl;
