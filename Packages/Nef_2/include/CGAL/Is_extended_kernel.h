e6750738aee4347d1e8bd76a70546ec32e8fd796
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2006-01-20 15:20:26 +0000

    Needed by various extended and not extended kernels

---
+++ Packages/Nef_2/include/CGAL/Is_extended_kernel.h
@@ -0,0 +1,14 @@
+#ifndef CGAL_IS_EXTENDED_KERNEL_H
+#define CGAL_IS_EXTENDED_KERNEL_H
+
+
+CGAL_BEGIN_NAMESPACE
+
+template<class Kernel>
+struct Is_extended_kernel {
+       typedef Tag_false value_type;
+};
+
+CGAL_END_NAMESPACE
+
+#endif
