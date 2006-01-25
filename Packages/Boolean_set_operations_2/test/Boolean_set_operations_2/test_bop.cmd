b1861bd3ca68a348fa60e799c9ed56cd5de33c92
Baruch Zukerman <baruchzu@post.tau.ac.il>
2006-01-25 14:37:50 +0000

    1st revision of the tests

---
+++ Packages/Boolean_set_operations_2/test/Boolean_set_operations_2/test_bop.cmd
@@ -0,0 +1,8 @@
+./data/bop/test01.txt
+./data/bop/test02.txt
+./data/bop/test03.txt
+./data/bop/test04.txt
+./data/bop/test05.txt
+./data/bop/test06.txt
+./data/bop/test07.txt
+./data/bop/test08.txt
