395631fbfd49177ad37c482951c42a0c5c179462
Baruch Zukerman <baruchzu@post.tau.ac.il>
2006-01-25 18:04:22 +0000

    new test suites

--- Packages/Boolean_set_operations_2/test/Boolean_set_operations_2/test_bop.cmd
+++ Packages/Boolean_set_operations_2/test/Boolean_set_operations_2/test_bop.cmd
@@ -6,3 +6,8 @@
 ./data/bop/test06.txt
 ./data/bop/test07.txt
 ./data/bop/test08.txt
+./data/bop/test09.txt
+./data/bop/test10.txt
+./data/bop/test11.txt
+./data/bop/test12.txt
+./data/bop/test13.txt
