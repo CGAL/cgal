7f58a5098cfd1ebf557d5ffc0d5e21158c226e80
Kaspar Fischer <fischerk@inf.ethz.ch>
2006-01-25 13:33:45 +0000

    - fixed name 'mel' to 'ame' - added axes and center output

--- Packages/Approximate_min_ellipsoid_d/examples/Approximate_min_ellipsoid_d/example.C
+++ Packages/Approximate_min_ellipsoid_d/examples/Approximate_min_ellipsoid_d/example.C
@@ -30,9 +30,31 @@ int main()
 
   // compute approximation:
   Traits traits;
-  AME mel(eps, P.begin(), P.end(), traits);
+  AME ame(eps, P.begin(), P.end(), traits);
   
   // write EPS file:
-  if (mel.is_full_dimensional() && d == 2)
-    mel.write_eps("example.eps");
+  if (ame.is_full_dimensional() && d == 2)
+    ame.write_eps("example.eps");
+
+  // output center coordinates:
+  std::cout << "Cartesian center coordinates: ";
+  for (AME::Center_coordinate_iterator c_it = ame.center_cartesian_begin();
+       c_it != ame.center_cartesian_end();
+       ++c_it)
+    std::cout << *c_it << ' ';
+  std::cout << ".\n";
+
+  if (d == 2 || d == 3) {
+    // output  axes:
+    AME::Axes_lengths_iterator axes = ame.axes_lengths_begin();
+    for (int i = 0; i < d; ++i) {
+      std::cout << "Semiaxis " << i << " has length " << *axes++  << "\n"
+		<< "and Cartesian coordinates ";
+      for (AME::Axes_direction_coordinate_iterator
+	     d_it = ame.axis_direction_cartesian_begin(i);
+	   d_it != ame.axis_direction_cartesian_end(i); ++d_it)
+	std::cout << *d_it << ' ';
+      std::cout << ".\n";
+    }
+  }
 }
