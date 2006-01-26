c0361c95fce40dc396228229f742fba2678b8d51
Daniel Russel <drussel@gmail.com>
2006-01-26 01:36:38 +0000

    switched to using audit time

--- Packages/Kinetic_data_structures/test/Kinetic_data_structures/include/sort_test.h
+++ Packages/Kinetic_data_structures/test/Kinetic_data_structures/include/sort_test.h
@@ -11,54 +11,54 @@ bool sort_test(Traits &tr)
   //CGAL_exactness_assertion_code(bool test_compiled_with_exact_checks;);
   CGAL_exactness_assertion_code(eret=true);
   
-    typedef CGAL::KDS::Sort<Traits> Sort;
-    Sort sort(tr);
+  typedef CGAL::KDS::Sort<Traits> Sort;
+  Sort sort(tr);
 
-    //#ifdef _MSC_VER
-    ///#pragma warning(disable:1572)
-    //#endif
+  //#ifdef _MSC_VER
+  ///#pragma warning(disable:1572)
+  //#endif
 
-    while (tr.simulator_pointer()->next_event_time() != tr.simulator_pointer()->end_time()) {
-//std::cout << *tr.simulator_pointer() << std::endl;
-        tr.simulator_pointer()->set_current_event_number(tr.simulator_pointer()->current_event_number()+1);
-    }
+  while (tr.simulator_pointer()->next_event_time() != tr.simulator_pointer()->end_time()) {
+    //std::cout << *tr.simulator_pointer() << std::endl;
+    tr.simulator_pointer()->set_current_event_number(tr.simulator_pointer()->current_event_number()+1);
+  }
 
-    //#ifdef _MSC_VER
-    ///#pragma warning(enable:1572)
-    //#endif
+  //#ifdef _MSC_VER
+  ///#pragma warning(enable:1572)
+  //#endif
 
-    std::cout << tr.simulator_pointer()->current_event_number() << " events processed.\n";
+  std::cout << tr.simulator_pointer()->current_event_number() << " events processed.\n";
 
-    typedef typename Sort::Key_iterator Kit;
-    Kit c= sort.begin();
-    Kit b=c;
-    ++c;
-    bool error =false;
+  typedef typename Sort::Key_iterator Kit;
+  Kit c= sort.begin();
+  Kit b=c;
+  ++c;
+  bool error =false;
 
-    typename Traits::NT ratt;
-    if (tr.simulator_pointer()->has_rational_current_time()) {
-        ratt=tr.simulator_pointer()->rational_current_time();
-    }
-    else {
-      std::cerr << etag << "Out of events, but the time is not rational." << std::endl;
-        std::cerr << "Current time is " << tr.simulator_pointer()->current_time()
-            << " the end time is " << tr.simulator_pointer()->end_time() << std::endl;
-        ratt= CGAL::to_interval(tr.simulator_pointer()->end_time()).second;
-	error=eret;
-    }
+  typename Traits::NT ratt;
+  if (tr.simulator_pointer()->has_audit_time()) {
+    ratt=tr.simulator_pointer()->audit_time();
+  }
+  else {
+    std::cerr << etag << "Out of events, but the time is not rational." << std::endl;
+    std::cerr << "Current time is " << tr.simulator_pointer()->current_time()
+	      << " the end time is " << tr.simulator_pointer()->end_time() << std::endl;
+    ratt= CGAL::to_interval(tr.simulator_pointer()->end_time()).second;
+    error=eret;
+  }
 
-    while (c != sort.end()) {
-      typename Traits::Simulator::Function_kernel::Function f= tr.active_objects_table_pointer()->at(*c).x() - tr.active_objects_table_pointer()->at(*b).x();
-      if ( f(ratt) < 0 ) {
-	std::cerr << etag << "Objects " << *c << " = " << tr.active_objects_table_pointer()->at(*c).x() << " and "
+  while (c != sort.end()) {
+    typename Traits::Simulator::Function_kernel::Function f= tr.active_objects_table_pointer()->at(*c).x() - tr.active_objects_table_pointer()->at(*b).x();
+    if ( f(ratt) < 0 ) {
+      std::cerr << etag << "Objects " << *c << " = " << tr.active_objects_table_pointer()->at(*c).x() << " and "
                 << *b << " = " << tr.active_objects_table_pointer()->at(*b).x() << " out of order at end of time "
                 <<ratt << std::endl;
-            error=eret;
-        }
-
-        ++b;
-        ++c;
+      error=eret;
     }
-    return error;
+
+    ++b;
+    ++c;
+  }
+  return error;
 }
 #endif
