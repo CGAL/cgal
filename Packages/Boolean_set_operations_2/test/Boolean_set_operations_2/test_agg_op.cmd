bf46788c19c52dc1e893d0db19eb25f112eaed1e
Baruch Zukerman <baruchzu@post.tau.ac.il>
2006-01-30 12:56:36 +0000

    added a test program for aggregate operations

---
+++ Packages/Boolean_set_operations_2/test/Boolean_set_operations_2/test_agg_op.cmd
@@ -0,0 +1,12 @@
+./data/agg_op/test01.txt
+./data/agg_op/test02.txt
+./data/agg_op/test03.txt
+./data/agg_op/test04.txt
+./data/agg_op/test05.txt
+./data/agg_op/test06.txt
+./data/agg_op/test07.txt
+./data/agg_op/test08.txt
+./data/agg_op/test09.txt
+./data/agg_op/test10.txt
+./data/agg_op/test11.txt
+./data/agg_op/test12.txt
