c2297de31b98968e38d3cd242cc331c5d72dafac
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2006-02-03 07:44:36 +0000

    fixed typo

--- Packages/Core/include/CORE/BigRat.h
+++ Packages/Core/include/CORE/BigRat.h
@@ -308,7 +308,7 @@ public:
     return mpq_set_str(get_mp(), s, base);
   }
   /// convert to <tt>std::string</tt>
-  std::string get_str(int base Bi= 10) const {
+  std::string get_str(int base = 10) const {
     int n = mpz_sizeinbase(mpq_numref(get_mp()), base) + mpz_sizeinbase(mpq_denref(get_mp()), base)+ 3;
     char *buffer = new char[n];
     mpq_get_str(buffer, base, get_mp());
