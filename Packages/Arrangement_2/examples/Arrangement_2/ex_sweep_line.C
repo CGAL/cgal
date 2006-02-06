0d9ea3abfacf9b7be707bceaf5a691f10e44ee24
Baruch Zukerman <baruchzu@post.tau.ac.il>
2006-02-06 16:18:11 +0000

    changed the example as the sweep line global function interface has changed (using default traits)

--- Packages/Arrangement_2/examples/Arrangement_2/ex_sweep_line.C
+++ Packages/Arrangement_2/examples/Arrangement_2/ex_sweep_line.C
@@ -4,35 +4,31 @@
 #include <CGAL/Cartesian.h>
 #include <CGAL/MP_Float.h>
 #include <CGAL/Quotient.h>
-#include <CGAL/Arr_segment_traits_2.h>
 #include <CGAL/Sweep_line_2_algorithms.h>
 #include <list>
 
 typedef CGAL::Quotient<CGAL::MP_Float>                  NT;
 typedef CGAL::Cartesian<NT>                             Kernel;
-typedef CGAL::Arr_segment_traits_2<Kernel>              Traits_2;
-typedef Traits_2::Point_2                               Point_2;
-typedef Traits_2::Curve_2                               Segment_2;
+typedef Kernel::Point_2                                 Point_2;
+typedef Kernel::Segment_2                               Segment_2;
 
 int main()
 {
   // Construct the input segments.
-  std::list<Segment_2>   segments;
+  Segment_2 segments[] = {Segment_2 (Point_2 (1, 5), Point_2 (8, 5)),
+                          Segment_2 (Point_2 (1, 1), Point_2 (8, 8)),
+                          Segment_2 (Point_2 (3, 1), Point_2 (3, 8)),
+                          Segment_2 (Point_2 (8, 5), Point_2 (8, 8))};
   
-  segments.push_back (Segment_2 (Point_2 (1, 5), Point_2 (8, 5)));
-  segments.push_back (Segment_2 (Point_2 (1, 1), Point_2 (8, 8)));
-  segments.push_back (Segment_2 (Point_2 (3, 1), Point_2 (3, 8)));
-  segments.push_back (Segment_2 (Point_2 (8, 5), Point_2 (8, 8)));
-
   // Compute all intersection points.
-  Traits_2               traits;
   std::list<Point_2>     pts;
 
-  CGAL::get_intersection_points (segments.begin(), segments.end(),
-                                 std::back_inserter (pts), traits);
+  CGAL::get_intersection_points (segments, segments + 4,
+                                 std::back_inserter (pts));
   
   // Print the result.
-  std::copy (pts.begin(), pts.end(), std::ostream_iterator<Point_2>(std::cout, "\n"));
+  std::copy (pts.begin(), pts.end(),
+             std::ostream_iterator<Point_2>(std::cout, "\n"));
 
   return (0);
 }
