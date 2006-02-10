a4a56be114181d60b01cdf5223256d2a011435d1
Ilya Suslov <Ilya.Suslov@sophia.inria.fr>
2006-02-10 14:33:49 +0000

    *** empty log message ***

--- Packages/Curved_kernel/Benchmarks/arrangement_traits/IntStr.h
+++ Packages/Curved_kernel/Benchmarks/arrangement_traits/IntStr.h
@@ -11,34 +11,6 @@ bool from_string(T &t,
    return !(iss>>f>>t).fail();
 }
 
-// int main()
-// {
-//    int i;
-//    float f;
-//    // the third parameter of from_string() should be 
-//    // one of std::hex, std::dec or std::oct
-//    if(from_string<int>(i, std::string("ff"), std::hex)){
-//       std::cout<<i<<std::endl;
-//    }
-//    else{
-//       std::cout<<"from_string failed"<<std::endl;
-//    }
-//    if(from_string<float>(f, 
-//                                std::string("123.456"),
-//                                std::dec))
-//    {
-//       std::cout<<f<<std::endl;
-//    }
-//    else{
-//       std::cout<<"from_string failed"<<std::endl;
-//    }
-//    return 0;
-// } 
-
-/* output:
-255
-123.456
-*/
 
 template <class T>
 std::string to_string(T t, std::ios_base & (*f)(std::ios_base&)=std::dec)
@@ -47,14 +19,5 @@ std::string to_string(T t, std::ios_base & (*f)(std::ios_base&)=std::dec)
    oss << f << t;
    return oss.str();
 }
-
-// int main()
-// {
-//    // the second parameter of to_string() should be one of 
-//    // std::hex, std::dec or std::oct
-//    std::cout<<to_string<long>(123456, std::hex)<<std::endl;
-//    std::cout<<to_string<long>(123456, std::oct)<<std::endl;
-//    return 0;
-// } 
 
  
