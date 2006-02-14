99e0edbb5b246396a6fff1e7d14db4bda8f96cd1
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2006-02-14 15:38:24 +0000

    - Fix header.

--- Interpolation/include/CGAL/natural_neighbor_coordinates_3.h
+++ Interpolation/include/CGAL/natural_neighbor_coordinates_3.h
@@ -1,28 +1,21 @@
-
-// ======================================================================
+// Copyright (c) 2005  INRIA Sophia-Antipolis (France).
+// All rights reserved.
 //
-// Copyright (c) 1999 The CGAL Consortium
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
 //
-// This software and related documentation is part of an INTERNAL release
-// of the Computational Geometry Algorithms Library (CGAL). It is not
-// intended for general use.
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
 //
-// ----------------------------------------------------------------------
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// release       : 
-// release_date  : 
+// $URL$
+// $Id$ $Date$
 //
-// file          : Natural_coordinates_3.h
-// package       : Interpolation
-// maintainer    : Raphaelle Chaine
-// revision      : 
-// author(s)     : Raphaelle Chaine <Raphaelle.Chaine@sophia.inria.fr, raphaelle.chaine@liris.cnrs.fr>
 //
-// coordinator   : INRIA Sophia Antipolis 
-//                 
-//
-// ======================================================================
-
+// Author(s)     : Raphaelle Chaine
 
 #ifndef CGAL_NATURAL_NEIGHBORS_3_H
 #define CGAL_NATURAL_NEIGHBORS_3_H
