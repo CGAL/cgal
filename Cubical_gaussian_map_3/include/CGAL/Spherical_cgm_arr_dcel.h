9836cd6edbb58eac698043fd4bcb7ecf94d521fb
Efi Fogel <efif@post.tau.ac.il>
2006-02-15 17:43:24 +0000

    fixed headers one more time

--- Cubical_gaussian_map_3/include/CGAL/Spherical_cgm_arr_dcel.h
+++ Cubical_gaussian_map_3/include/CGAL/Spherical_cgm_arr_dcel.h
@@ -1,3 +1,11 @@
+// Copyright (c) 2005  Tel-Aviv University (Israel).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
 // accordance with the commercial license agreement provided with the software.
 //
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
@@ -6,7 +14,6 @@
 // $URL$
 // $Id$
 // 
-//
 //
 // Author(s)     : Kapelushnik Lior <liorkape@post.tau.ac.il>
 
