b235f7d5e76376bd4f378c02c5c648f19079f3b5
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2006-02-23 14:52:22 +0000

    - Add QPL headers.

--- Algebraic_kernel/include/CGAL/NT_extensions_Root_of/leda_real.h
+++ Algebraic_kernel/include/CGAL/NT_extensions_Root_of/leda_real.h
@@ -1,17 +1,26 @@
-// Copyright (c) 2005  INRIA Sophia-Antipolis (France) 
+// Copyright (c) 2003-2006  INRIA Sophia-Antipolis (France).
 // All rights reserved.
 //
-// Authors : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
-//           Sylvain Pion     <Sylvain.Pion@sophia.inria.fr>
-//           Athanasios Kakargias
-// 
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+// Author(s)     : Monique Teillaud, Sylvain Pion, Athanasios Kakargias
+
 // Partially supported by the IST Programme of the EU as a Shared-cost
 // RTD (FET Open) Project under Contract No  IST-2000-26473 
 // (ECG - Effective Computational Geometry for Curves and Surfaces) 
 // and a STREP (FET Open) Project under Contract No  IST-006413 
 // (ACS -- Algorithms for Complex Shapes)
-
-// file : include/CGAL/Root_of/leda_real.h
 
 #ifndef CGAL_ROOT_OF_LEDA_REAL_H
 #define CGAL_ROOT_OF_LEDA_REAL_H
