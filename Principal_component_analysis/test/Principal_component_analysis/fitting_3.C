24c8aa272b2f52a0a265f2f117e9f6400073d041
Pierre Alliez <Pierre.Alliez@sophia.inria.fr>
2006-03-02 15:32:26 +0000

    update test suite 3D

--- Principal_component_analysis/test/Principal_component_analysis/fitting_3.C
+++ Principal_component_analysis/test/Principal_component_analysis/fitting_3.C
@@ -18,6 +18,28 @@ typedef Kernel::Plane_3 Plane;
 typedef Kernel::Vector_3 Vector;
 typedef Kernel::Triangle_3 Triangle;
 
+void fit_point_set(std::list<Point>& points,
+                   Plane& plane,
+                   Line& line)
+{
+  // fit a plane
+  // call all versions of the function
+  std::cout << "fit 3D plane...";
+  Kernel k;
+  FT quality;
+  Point centroid;
+
+  quality = linear_least_squares_fitting_3(points.begin(),points.end(),line);
+  quality = linear_least_squares_fitting_3(points.begin(),points.end(),line,centroid);
+  quality = linear_least_squares_fitting_3(points.begin(),points.end(),line,centroid,k);
+
+  quality = linear_least_squares_fitting_3(points.begin(),points.end(),plane);
+  quality = linear_least_squares_fitting_3(points.begin(),points.end(),plane,centroid);
+  quality = linear_least_squares_fitting_3(points.begin(),points.end(),plane,centroid,k);
+
+  std::cout << "done (quality: " << quality << ")" << std::endl;
+}
+
 // case with only one point in container
 // the fitting plane must be horizontal by default
 void test_3D()
@@ -25,17 +47,10 @@ void test_3D()
   std::list<Point> points;
   points.push_back(Point(0,0,0));
 
-  // fit a plane
-  // call all versions of the function
-  std::cout << "fit 3D plane...";
-  Kernel k;
-  FT quality;
+  // fit plane
   Plane plane;
-  Point centroid;
-  quality = linear_least_squares_fitting_3(points.begin(),points.end(),plane);
-  quality = linear_least_squares_fitting_3(points.begin(),points.end(),plane,centroid);
-  quality = linear_least_squares_fitting_3(points.begin(),points.end(),plane,centroid,k);
-  std::cout << "done (quality: " << quality << ")" << std::endl;
+  Line line;
+  fit_point_set(points,plane,line);
 
   Plane horizontal_plane(Point(0,0,0),Vector(0,0,1));
   if(!parallel(horizontal_plane,plane))
@@ -62,17 +77,10 @@ void test_3D_point_set(const unsigned int nb_points)
   for(i=0;i<nb_points;i++)
     points.push_back(random_point_xy());
 
-  // fit a plane
-  // call all versions of the function
-  std::cout << "fit 3D plane...";
-  Kernel k;
-  FT quality;
+  // fit plane
   Plane plane;
-  Point centroid;
-  quality = linear_least_squares_fitting_3(points.begin(),points.end(),plane);
-  quality = linear_least_squares_fitting_3(points.begin(),points.end(),plane,centroid);
-  quality = linear_least_squares_fitting_3(points.begin(),points.end(),plane,centroid,k);
-  std::cout << "done (quality: " << quality << ")" << std::endl;
+  Line line;
+  fit_point_set(points,plane,line);
 
   Plane horizontal_plane(Point(0,0,0),Vector(0,0,1));
   if(!parallel(horizontal_plane,plane))
@@ -95,12 +103,19 @@ void test_3D_triangle_set(const unsigned int nb_triangles)
   // call all versions of the function
   std::cout << "fit 3D plane...";
   Kernel k;
+  Line line;
   FT quality;
   Plane plane;
   Point centroid;
+
+  quality = linear_least_squares_fitting_3(triangles.begin(),triangles.end(),line);
+  quality = linear_least_squares_fitting_3(triangles.begin(),triangles.end(),line,centroid);
+  quality = linear_least_squares_fitting_3(triangles.begin(),triangles.end(),line,centroid,k);
+
   quality = linear_least_squares_fitting_3(triangles.begin(),triangles.end(),plane);
   quality = linear_least_squares_fitting_3(triangles.begin(),triangles.end(),plane,centroid);
   quality = linear_least_squares_fitting_3(triangles.begin(),triangles.end(),plane,centroid,k);
+
   std::cout << "done (quality: " << quality << ")" << std::endl;
 
   Plane horizontal_plane(Point(0,0,0),Vector(0,0,1));
@@ -110,7 +125,6 @@ void test_3D_triangle_set(const unsigned int nb_triangles)
     exit(1); // failure
   }
 }
-
 
 int main()
 {
