d984d97c32581f1c9b5a68ed9f3046e6d273b21a
Nico Kruithof <Nico@cs.rug.nl>
2006-03-03 14:50:25 +0000

    added documentation

--- Skin_surface_3/include/CGAL/Marching_tetrahedra_traits_skin_surface_3.h
+++ Skin_surface_3/include/CGAL/Marching_tetrahedra_traits_skin_surface_3.h
@@ -32,16 +32,13 @@ public:
   typedef HalfedgeDS                                   Halfedge_DS;
   typedef Converter_                                   Converter;
 
-  typedef typename Triangulation::Vertex_handle        Vertex_handle;
-  typedef typename Triangulation::Edge                 Edge;
   typedef typename Triangulation_3::Cell_handle        Cell_handle;
-  typedef typename Triangulation::Geom_traits::Point_3 Triang_point;
+  
 
-  typedef typename HalfedgeDS::Traits                  HDS_K;
-  typedef typename HDS_K::Point_3                      HDS_point;
-  typedef typename HDS_point::R::RT                    HDS_rt;
+  typedef typename HalfedgeDS::Traits::Point_3         HDS_point;
+  typedef typename HDS_point::R::RT                    HDS_RT;
 
-  Marching_tetrahedra_traits_skin_surface_3(HDS_rt iso_value=0)
+  Marching_tetrahedra_traits_skin_surface_3(HDS_RT iso_value=0)
     : iso_value(iso_value) {
   }
 
@@ -57,16 +54,18 @@ public:
   }
 
 private:
+  typedef typename Triangulation::Geom_traits::Point_3 Triang_point;
+  
   // Additional functions, not belonging to the traits concept:
-  HDS_rt value(const Cell_handle &ch, const HDS_point &p) const {
+  HDS_RT value(const Cell_handle &ch, const HDS_point &p) const {
     return ch->surf->value(p);
   }
-  HDS_rt value(const Cell_handle &ch, const Triang_point &p) const {
+  HDS_RT value(const Cell_handle &ch, const Triang_point &p) const {
     return ch->surf->value(converter(p));
   }
 
   Converter converter;
-  HDS_rt iso_value;
+  HDS_RT iso_value;
 };
 
 CGAL_END_NAMESPACE 
