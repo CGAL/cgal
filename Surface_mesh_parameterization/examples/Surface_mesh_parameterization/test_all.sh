5108d76708b6ed90b1990e1eedac0c7e935632bb
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2006-03-03 10:09:51 +0000

    Added same test as cgal_test

--- Surface_mesh_parameterization/examples/Surface_mesh_parameterization/test_all.sh
+++ Surface_mesh_parameterization/examples/Surface_mesh_parameterization/test_all.sh
@@ -4,20 +4,25 @@
 
 echo "***************************************************************"
 
-./test_model.sh cube
-./test_model.sh holes
-./test_model.sh mannequin-devil
-./test_model.sh mask_cone
-./test_model.sh nefertiti
-./test_model.sh rotor
-./test_model.sh sphere966
-./test_model.sh three_peaks
+# ./test_model.sh cube
+# ./test_model.sh holes
+# ./test_model.sh mannequin-devil
+# ./test_model.sh mask_cone
+# ./test_model.sh nefertiti
+# ./test_model.sh rotor
+# ./test_model.sh sphere966
+# ./test_model.sh three_peaks
 
 echo "***************************************************************"
 
-for tst in *_parameterization
+for TST in Authalic_parameterization Simple_parameterization Taucs_parameterization Mesh_cutting_parameterization Square_border_parameterization polyhedron_ex_parameterization
 do
     echo " "
-    echo "*** $tst ***"
-    $tst `cat $tst.cmd`
+    echo "*** $TST ***"
+
+    # Find executable name (different on Windows and Unix)
+    [ -f ./release/$TST.exe ] && PARAM_APPLICATION="./release/$TST.exe"
+    [ -x ./$TST ] && PARAM_APPLICATION="./$TST"
+
+    $PARAM_APPLICATION `cat $TST.cmd`
 done
