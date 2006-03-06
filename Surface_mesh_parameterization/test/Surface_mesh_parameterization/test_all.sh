d6748af3a336e80226c15b9b5a36c3da6e0803c7
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2006-03-06 15:22:04 +0000

    Fixed executable property

--- Surface_mesh_parameterization/test/Surface_mesh_parameterization/test_all.sh
+++ Surface_mesh_parameterization/test/Surface_mesh_parameterization/test_all.sh
@@ -2,10 +2,6 @@
 
 # Intensive test: test all surface parameterization methods with all models in data folder
 
-echo "***************************************************************"
-date
-echo "***************************************************************"
-
 # Find executable name (different on Windows and Unix)
 [ -f ./release/extensive_parameterization_test.exe ] && PARAM_APPLICATION="./release/extensive_parameterization_test.exe"
 [ -x ./extensive_parameterization_test ] && PARAM_APPLICATION="./extensive_parameterization_test"
@@ -15,9 +11,3 @@ set -x
 
 $PARAM_APPLICATION data/*.off 2>&1
 
-# echo off
-set +x
-
-echo "***************************************************************"
-date
-echo "***************************************************************"
