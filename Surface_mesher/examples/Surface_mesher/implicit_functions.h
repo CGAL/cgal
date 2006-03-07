f51c84caf503295ef245f8d620c24721dd63da96
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2006-03-07 10:01:08 +0000

    updated example

---
+++ Surface_mesher/examples/Surface_mesher/implicit_functions.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2006  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://lrineau@scm.gforge.inria.fr/svn/cgal/trunk/Surface_mesher/examples/Surface_mesher/implicit_function.C $
+// $Id: implicit_function.C 29089 2006-03-06 15:17:07Z lrineau $
+//
+//
+// Author(s)     : Steve OUDOT
+
+
+///////////////// Definitions of several famous surfaces /////////////////
+double sphere_function (double, double, double);  // (c=(0,0,0), r=1)
+double ellipsoid_function (double, double, double);  // (c=(0,0,0), r=1)
+double torus_function (double, double, double);  // (c=(0,0,0), r=2)
+double chair_function (double, double, double);  // (c=(0,0,0), r=6)
+double tanglecube_function (double, double, double);  // (c=(0,0,0), r=4)
+double octic_function (double, double, double);  // (c=(0,0,0), r=2)
+double heart_function (double, double, double);  // (c=(0,0,0), r=2)
+double klein_function (double, double, double);  // (c=(0,0,0), r=4)
+double ring_function (double, double, double);  // (c=(0,0,0), r=?)
+double false_knot_function (double, double, double);  // (c=(0,0,0), r=1)
+double knot1_function (double, double, double);  // (c=(0,0,0), r=4)
+double knot2_function (double, double, double);  // (c=(0,0,0), r=4)
+double knot3_function (double, double, double);  // (c=(0,0,0), r=4)
---
+++ Surface_mesher/examples/Surface_mesher/implicit_surface_mesher.C
@@ -0,0 +1,43 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh_cell_base_3.h>
+#include <CGAL/Surface_mesh_vertex_base_3.h>
+#include <CGAL/Delaunay_triangulation_3.h>
+#include <CGAL/Surface_mesh_default_criteria_3.h>
+
+#include <CGAL/make_surface_mesh.h>
+#include <CGAL/Implicit_surface_3.h>
+
+struct Kernel : public CGAL::Exact_predicates_inexact_constructions_kernel {};
+typedef CGAL::Surface_mesh_vertex_base_3<Kernel> Vb;
+typedef CGAL::Surface_mesh_cell_base_3<Kernel> Cb;
+typedef CGAL::Triangulation_data_structure_3<Vb, Cb> Tds;
+typedef CGAL::Delaunay_triangulation_3<Kernel, Tds> Tr;
+typedef CGAL::Complex_2_in_triangulation_3<Tr> C2t3;
+typedef Kernel::Sphere_3 Sphere_3;
+
+typedef double (*implicit_function)(double, double, double);
+typedef CGAL::Implicit_surface_3<Kernel, implicit_function> Surface_3;
+
+double sphere_function (double x, double y, double z) {
+  const double x2=x*x, y2=y*y, z2=z*z;
+  return x2+y2+z2-1;
+}
+
+int main(int, char **) {
+  Tr tr;            // 3D-Delaunay triangulation
+  C2t3 c2t3 (tr);   // 2D-complex in 3D-Delaunay triangulation
+
+  // defining the surface
+  Surface_3 surface(sphere_function, 
+                    Sphere_3(CGAL::ORIGIN, 2.),
+                    1e-06);
+
+  // defining meshing criteria
+  CGAL::Surface_mesh_default_criteria_3<Tr> criteria(30.,
+                                                     0.05,
+                                                     0.01);
+  // meshing surface
+  make_surface_mesh(c2t3, surface, criteria, CGAL::Non_manifold_tag());
+
+  std::cout << "Final number of points: " << tr.number_of_vertices() << "\n";
+}
