5e9460e8e49f6daeff03ad89d98a86341dc95446
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2006-03-07 17:35:06 +0000

    stupidly forgot to call refine_mesh()

--- Surface_mesher/include/CGAL/make_surface_mesh.h
+++ Surface_mesher/include/CGAL/make_surface_mesh.h
@@ -11,9 +11,8 @@
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// $Source: 
+// $URL: $
 // $Revision: 1.1 $ $Date: 2005/12/12 16:20:58 $
-// $Name:  $
 //
 // Author(s)     : Laurent Rineau
 
@@ -73,6 +72,7 @@ void make_surface_mesh(C2T3& c2t3,
 
   get_initial_points(surface, CGAL::inserter(c2t3.triangulation()));
   Mesher mesher(c2t3, surface, traits, criteria);
+  mesher.refine_mesh();
   // TODO initial, then refine()
 }
 
