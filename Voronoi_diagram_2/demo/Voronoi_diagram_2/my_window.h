ac832d3a859ba3bbb5c526d8c7eca1cae16022d5
Menelaos Karavelas <mkaravel@iacm.forth.gr>
2006-03-07 23:41:02 +0000

    - demos now include CGAL/basic.h before testing for Qt; this is essential   due to the recent changes in install_cgal - added copyright notices in demo files - modified year in existing copyright notices

--- Voronoi_diagram_2/demo/Voronoi_diagram_2/my_window.h
+++ Voronoi_diagram_2/demo/Voronoi_diagram_2/my_window.h
@@ -1,3 +1,22 @@
+// Copyright (c) 2006 Foundation for Research and Technology-Hellas (Greece).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : Menelaos Karavelas <mkaravel@iacm.forth.gr>
+
 #ifndef MY_WINDOW_H
 #define MY_WINDOW_H
 
@@ -576,7 +595,7 @@ private slots:
 			"This is a demo for the Voronoi diagram adaptor\n\n"
 			"Author: Menelaos Karavelas "
 			"<mkaravel@iacm.forth.gr>\n\n"
-			"Copyright (c) CGAL 2005");
+			"Copyright (c) CGAL 2006");
   }
 
   void aboutQt()
