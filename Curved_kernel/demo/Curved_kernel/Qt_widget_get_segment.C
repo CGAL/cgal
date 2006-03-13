6dc7208959605207121f20f446ff579083f99c2b
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
2006-03-13 11:01:36 +0000

    order of lines  #ifdef CGAL_USE_QT  #include <CGAL/basic.h> reversed

--- Curved_kernel/demo/Curved_kernel/Qt_widget_get_segment.C
+++ Curved_kernel/demo/Curved_kernel/Qt_widget_get_segment.C
@@ -8,9 +8,9 @@
 // RTD (FET Open) Project under Contract No  IST-2000-26473 
 // (CGAL - Effective Computational Geometry for Curves and Surfaces) 
 
-#ifdef CGAL_USE_QT
-
 #include <CGAL/basic.h>
+
+#ifdef CGAL_USE_QT
 
 #include "Qt_widget_get_segment.h"
 
