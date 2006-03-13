62c75f9e0108fa691b13a57b7493ed61e1d1e1bc
Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
2006-03-13 09:56:15 +0000

    GMP replaced by MP_Float

--- Algebraic_kernel/test/Algebraic_kernel/test_Algebraic_kernel.C
+++ Algebraic_kernel/test/Algebraic_kernel/test_Algebraic_kernel.C
@@ -2,15 +2,16 @@
 #include <CGAL/Algebraic_kernel_2_2.h>
 #include <CGAL/MP_Float.h>
 #include <CGAL/Quotient.h>
-#include <CGAL/Gmpq.h>
+//#include <CGAL/Gmpq.h>
 #include <CGAL/NT_extensions_Root_of/CGAL_Quotient.h>
-#include <CGAL/NT_extensions_Root_of/CGAL_Gmpq.h>
+//#include <CGAL/NT_extensions_Root_of/CGAL_Gmpq.h>
 #include <CGAL/_test_predicates.h>
 #include <CGAL/_test_constructor.h>
 
 int main()
 {
-  typedef CGAL::Gmpq                                           NT1;
+  typedef CGAL::Quotient<CGAL::MP_Float>                       NT1;
+  //typedef CGAL::Gmpq                                           NT1;
   typedef CGAL::Algebraic_kernel_for_circles_2_2<NT1>          Algebraic_k1;
   Algebraic_k1 ak1;
   _test_solve(ak1);
