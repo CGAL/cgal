a0cb873181dee799c05152d0e40c7576a08dd530
Daniel Russel <drussel@gmail.com>
2006-03-16 06:32:50 +0000

    added test for program options, lets hope this doesn't break anything :-)

---
+++ Installation/config/support/test_BOOST_PROGRAM_OPTIONS.C
@@ -0,0 +1,54 @@
+// Copyright (c) 2005  Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland), Freie Universitaet Berlin (Germany),
+// INRIA Sophia-Antipolis (France), Martin-Luther-University Halle-Wittenberg
+// (Germany), Max-Planck-Institute Saarbruecken (Germany), RISC Linz (Austria),
+// and Tel-Aviv University (Israel).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/trunk/Configuration/config/testfiles/CGAL_CFG_NO_BOOST.C $
+// $Id: CGAL_CFG_NO_BOOST.C 28567 2006-02-16 14:30:13Z lsaboret $
+// 
+//
+// Author(s)     : Sylvain Pion
+
+// CGAL_CFG_NO_BOOST_PROGRAM_OPTIONS.C
+// ---------------------------------------------------------------------
+// A short test program to evaluate a machine architecture.
+// This program is used by cgal_configure.
+// The following documentation will be pasted in the generated configfile.
+// ---------------------------------------------------------------------
+
+//| Tests if BOOST_PROGRAM_OPTIONS is available.
+
+#include <iostream>
+#include <boost/program_options.hpp>
+#include <iostream>
+
+
+int main(int ac, char *av[])
+{
+  boost::program_options::options_description desc("Allowed options");
+  desc.add_options()
+    ("help", "produce help message")
+    ;
+
+  boost::program_options::variables_map vm;
+  boost::program_options::store(boost::program_options::parse_command_line(ac, av, desc), vm);
+  boost::program_options::notify(vm);    
+
+  if (vm.count("help")) {
+    std::cout << "Help" << "\n";
+    return 1;
+  }
+  return 0;
+}
