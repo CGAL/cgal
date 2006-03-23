62aa010da2ec5abead439bb44c94672fe1a6333c
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2006-03-23 15:48:00 +0000

    - new version, more didactic, of   examples/Surface_mesher/3d_image_surface_mesher.C

--- Surface_mesher/examples/Surface_mesher/3d_image_surface_mesher.C
+++ Surface_mesher/examples/Surface_mesher/3d_image_surface_mesher.C
@@ -18,27 +18,27 @@ int main(int, char **) {
   Tr tr;            // 3D-Delaunay triangulation
   C2t3 c2t3 (tr);   // 2D-complex in 3D-Delaunay triangulation
 
+  // the 'function' is a 3D gray level image
   Gray_level_image image("ImageIO/data/skull_2.9.inr.gz", 2.9);
 
-  // bounding_sphere_center is a point inside the surface defined by image.
+  // Carefully choosen bounding sphere: the center must be inside the
+  // surface defined by 'image' and the radius must be high enough so that
+  // the sphere actually bounds the whole image.
   Kernel::Point_3 bounding_sphere_center(122., 102., 117.);
-
-  // bounding_sphere_squared_radius is chosen so that the sphere bounds the
-  // whole image
   Kernel::FT bounding_sphere_squared_radius = 200.*200.*2.;
-
   Kernel::Sphere_3 bounding_sphere(bounding_sphere_center,
                                    bounding_sphere_squared_radius);
 
-  // definition of the surface
+  // definition of the surface, with the 10^-2 as relative precision
   Surface_3 surface(image, bounding_sphere);
 
   // defining meshing criteria
   CGAL::Surface_mesh_default_criteria_3<Tr> criteria(30.,
                                                      10.,
                                                      10.);
-  // meshing surface
-  make_surface_mesh(c2t3, surface, criteria, CGAL::Non_manifold_tag());
+
+  // meshing surface, with the "manifold without boundary" algorithm
+  make_surface_mesh(c2t3, surface, criteria, CGAL::Manifold_tag());
 
   std::cout << "Final number of points: " << tr.number_of_vertices() << "\n";
 }
