d5f6332ecff3ec43f56b56e49dbc1197634471ff
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2006-03-28 10:05:35 +0000

    Added error message

--- Surface_mesh_parameterization/examples/Surface_mesh_parameterization/test.sh
+++ Surface_mesh_parameterization/examples/Surface_mesh_parameterization/test.sh
@@ -2,12 +2,16 @@
 
 # Utility script to run the surface parameterization example without knowing the executable name
 # Usage: test.sh parameterization-method border-parameterization solver output-format source-file-root
-# Example: test.sh floater circle opennl obj sphere966 
-# Example: test.sh lscm 2pts taucs eps sphere966 
+# Example: test.sh floater circle opennl obj sphere966
+# Example: test.sh lscm 2pts taucs eps sphere966
 
 # Find executable name (different on Windows and Unix)
 [ -f ./release/polyhedron_ex_parameterization.exe ] && PARAM_APPLICATION="./release/polyhedron_ex_parameterization.exe"
 [ -x ./polyhedron_ex_parameterization ] && PARAM_APPLICATION="./polyhedron_ex_parameterization"
+if [[ ! -f $PARAM_APPLICATION ]] ; then
+    echo "Cannot find polyhedron_ex_parameterization application"
+    exit 1;
+fi
 
 # Find source file in data or data/extras folders
 [ -f data/extras/"$5".off ] && SOURCE_FILE="data/extras/"$5".off"
