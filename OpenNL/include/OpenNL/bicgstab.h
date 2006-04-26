c5eeb30f737265f3eb429c0fc8f4198f62d24371
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2006-04-26 15:07:24 +0000

    New OpenNL license (LGPL) provided by Bruno Lévy on 04/21/2006

--- OpenNL/include/OpenNL/bicgstab.h
+++ OpenNL/include/OpenNL/bicgstab.h
@@ -1,42 +1,30 @@
 /*
- *  OpenNL<T>: Generic Numerical Library
- *  Copyright (C) 2004 Bruno Levy
+ * author:  Bruno Levy, INRIA, project ALICE
+ * website: http://www.loria.fr/~levy/software
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * Scientific work that use this software can reference the website and
+ * the following publication:
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *  In addition, as a special exception, the INRIA gives permission to link the
- *  code of this program with the CGAL library, and distribute linked combinations
- *  including the two. You must obey the GNU General Public License in all respects
- *  for all of the code used other than CGAL.
- *
- *  If you modify this software, you should include a notice giving the
- *  name of the person performing the modification, the date of modification,
- *  and the reason for such modification.
- *
- *  Contact: Bruno Levy
- *
- *     levy@loria.fr
- *
- *     ISA-ALICE Project
- *     LORIA, INRIA Lorraine,
- *     Campus Scientifique, BP 239
- *     54506 VANDOEUVRE LES NANCY CEDEX
- *     FRANCE
- *
- *  Note that the GNU General Public License does not permit incorporating
- *  the Software into proprietary programs.
+ * @INPROCEEDINGS {levy:NMDGP:05,
+ *    AUTHOR = Bruno Levy,
+ *    TITLE  = Numerical Methods for Digital Geometry Processing,
+ *    BOOKTITLE =Israel Korea Bi-National Conference,
+ *    YEAR=November 2005,
+ *    URL=http://www.loria.fr/~levy/php/article.php?pub=../publications/papers/2005/Numerics
+ * }
  *
  *  Laurent Saboret 01/2005-04/2005: Changes for CGAL:
  *      - Added OpenNL namespace
