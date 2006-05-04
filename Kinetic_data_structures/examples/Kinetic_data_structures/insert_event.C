9d4d76bc27027525c6c219112f163441058bb468
Daniel Russel <drussel@gmail.com>
2006-05-04 08:47:04 +0000

    shortened insert_event runtime

--- Kinetic_data_structures/examples/Kinetic_data_structures/insert_event.C
+++ Kinetic_data_structures/examples/Kinetic_data_structures/insert_event.C
@@ -1,33 +1,32 @@
-#include <CGAL/Kinetic/Delaunay_triangulation_2.h>
+#include <CGAL/Kinetic/Sort.h>
 #include <CGAL/Kinetic/Insert_event.h>
-#include <CGAL/Kinetic/Exact_simulation_traits_2.h>
+#include <CGAL/Kinetic/Exact_simulation_traits_1.h>
 
 int main(int, char *[])
 {
 
-    typedef CGAL::Kinetic::Exact_simulation_traits_2 Simulation_traits;
-    typedef Simulation_traits::Kinetic_kernel::Point_2 Moving_point_2;
-    typedef CGAL::Kinetic::Insert_event<Simulation_traits::Active_points_2_table> Insert_event;
-    typedef CGAL::Kinetic::Delaunay_triangulation_2<Simulation_traits> KDel;
+    typedef CGAL::Kinetic::Exact_simulation_traits_1 Simulation_traits;
+    typedef Simulation_traits::Kinetic_kernel::Point_1 Moving_point_1;
+    typedef CGAL::Kinetic::Insert_event<Simulation_traits::Active_points_1_table> Insert_event;
+    typedef CGAL::Kinetic::Sort<Simulation_traits> KDS;
 
     Simulation_traits tr;
     Simulation_traits::Simulator::Handle sp= tr.simulator_handle();
 
-    KDel kdel(tr);
+    KDS kds(tr);
   
-    std::ifstream in("data/points_2");
-    in >> *tr.active_points_2_table_handle();
+    std::ifstream in("data/points_1");
+    in >> *tr.active_points_1_table_handle();
 
     sp->new_event(Simulation_traits::Simulator::Time(3), 
-		  Insert_event(Moving_point_2(Moving_point_2::NT(0),
-					      Moving_point_2::NT(0)),
-			       tr.active_points_2_table_handle()));
+		  Insert_event(Moving_point_1(Moving_point_1::Coordinate(0)),
+			       tr.active_points_1_table_handle()));
 
     while (sp->next_event_time()
     < sp->end_time()) {
         sp->set_current_event_number(sp->current_event_number()+10);
         std::cout << "At time " << sp->current_time() << ":\n";
-        std::cout << kdel.triangulation_data_structure();
+        std::cout << kds;
     }
 
     return EXIT_SUCCESS;
