87c117db5c8a4490fd186509a0cf89e8a0607b92
Bernd Gärtner <gaertner@inf.ethz.ch>
2006-05-12 13:20:12 +0000

    somewhat elaborated these examples and marked them as experimental

--- QP_solver/examples/QP_solver/qp_solver1.C
+++ QP_solver/examples/QP_solver/qp_solver1.C
@@ -1,56 +1,91 @@
-#include <CGAL/QP_solver/gmp_double.h>
+// This program shows how the QP-solver can be called from a C++
+// program. PLEASE NOTE: the API of the QP-solver has been submitted 
+// to the CGAL editorial board and is currently under revision. IT IS
+// NOT GUARANTEED (ACTUALLY UNLIKELY) THAT THE CODE AS BELOW WILL STILL
+// WORK IN FUTURE RELEASES. It will have to be adpated according to the
+// final API of the package. Please contact Bernd Gaertner 
+// (gaertner at inf.ethz.ch) in case of problems or feedback. 
+
+
+// This example program solves the quadratic program 
+//    minimize    x^T D x + c^T x
+//    subject to  A x <rel> b
+//                L <= x <= u
+// where <rel> stands for a vector of relations from {<=, ==, >=}
+//
+// In the concrete example below, we have the following data:
+//     D      c     A      b    L     U
+// ---------------------------------------------------------------------
+//     1 0    0 3   1 2    1    0 0   infinity 1
+//     0 0
+//
+// In other words, we minimize x^2 + 3y subject to x + 2y = 1
+// and x >= 0, 0 <= y <= 1. Let's see what this gives: if we
+// substitute x with 1 - 2y in the objective function, we get
+// 4y^2 - y + 1. The derivative is 8y-1, so this is minimzed
+// for y = 1/8. This value (as well as x = 1 - 2(1/8) = 3/4)
+// are within the bounds, so the solution should be x = 3/4
+// and y = 1/8. 
+
 #include <CGAL/Gmpz.h>
-#include <CGAL/QP_solver/Double.h>
 #include <CGAL/QP_solver.h>
 
+// here we declare the types of the various QP entries
 struct Traits {
- enum Row_type {LESS_EQUAL, EQUAL, GREATER_EQUAL};
- typedef CGAL::Double ET;
+  enum Row_type {LESS_EQUAL, EQUAL, GREATER_EQUAL}; 
+  typedef Row_type *Row_type_iterator; // iterator for the constraint type
 
- typedef double **A_iterator;
- typedef double *B_iterator;
- typedef double *C_iterator;
- typedef double **D_iterator;
- typedef bool *FL_iterator;
- typedef bool *FU_iterator;
- typedef double *L_iterator;
- typedef double *U_iterator;
- typedef Row_type *Row_type_iterator;
+  // the exact internal type (input type must be convertible to this)
+  typedef CGAL::Gmpz ET;
 
- typedef CGAL::Tag_false Is_linear;
- typedef CGAL::Tag_true Is_symmetric;
- typedef CGAL::Tag_true Has_equalities_only_and_full_rank;
- typedef CGAL::Tag_false Is_in_standard_form;
+  // the input type is int, and the problem comes via arrays 
+  typedef int **A_iterator;  // for the columns of A
+  typedef int *B_iterator;   // for the right-hand side b
+  typedef int *C_iterator;   // for the objective function c
+  typedef int **D_iterator;  // for the rows of D
+  typedef bool *FL_iterator;    // for finiteness of lower bound L
+  typedef bool *FU_iterator;    // for finiteness of upper bound U
+  typedef int *L_iterator;   // for lower bounds L
+  typedef int *U_iterator;   // for upper bounds U
+
+  typedef CGAL::Tag_false Is_linear;     // we have a proper QP
+  typedef CGAL::Tag_true Is_symmetric;   // the matrix D is symmetric
+  typedef CGAL::Tag_true Has_equalities_only_and_full_rank;
+     // A has indeed full rank 1, and there are only equalities
+  typedef CGAL::Tag_false Is_in_standard_form; // not only x >=0 bounds
 };
 
-typedef CGAL::QP_solver<Traits> Solver;
+typedef CGAL::QP_solver<Traits> Solver;  // the solver type
 
 
 int main() {
- double c[] = {0, 3};
- double D_row_0[] = {1, 0};
- double D_row_1[] = {0, 0};
- double A_col_0[] = {1};
- double A_col_1[] = {2};
- double b[] = {1};
+  int c[]       = {0, 3};
+  int D_row_0[] = {1, 0};
+  int D_row_1[] = {0, 0};
+  int A_col_0[] = {1};
+  int A_col_1[] = {2};
+  int b[]       = {1};
 
- Traits::Row_type rt[] = {Traits::EQUAL};
- bool fl[] = {true, true};
- bool fu[] = {false, true};
- double l[] = {0, 0};
- double u[] = {0, 1};
+  Traits::Row_type rt[] = {Traits::EQUAL};
+  bool fl[]        = {true, true};
+  bool fu[]        = {false, true};  // x has upper bound infinity...
+  int l[]       = {0, 0};
+  int u[]       = {0, 1};         // ... and it's u-entry is ignored
 
- double *rows_of_D[] = {D_row_0, D_row_1};
- double *cols_of_A[] = {A_col_0, A_col_1};
+  int *rows_of_D[] = {D_row_0, D_row_1};
+  int *cols_of_A[] = {A_col_0, A_col_1};
 
- Solver solver(2, 1, cols_of_A, b, c, rows_of_D, rt, fl, l, fu, u);
+  // now call the solver; the first two arguments are
+  // the number of variables and the number of constraints (rows of A) 
+  Solver solver(2, 1, cols_of_A, b, c, rows_of_D, rt, fl, l, fu, u);
 
- if (solver.status() != Solver::INFEASIBLE) {
-   std::cout << "Optimal feasible solution x: ";
-   for (Solver::Variable_value_iterator it = solver.variables_value_begin(); it != solver.variables_value_end(); ++it)
-     std::cout << *it << " ";
-   std::cout << "f(x): " << solver.solution() << std::endl;
- }
+  if (solver.status() == Solver::OPTIMAL) { // we know that, don't we?
+    std::cout << "Optimal feasible solution x: ";
+    for (Solver::Variable_value_iterator it = solver.variables_value_begin(); 
+	 it != solver.variables_value_end(); ++it)
+      std::cout << *it << " "; // variable values
+    std::cout << "f(x): " << solver.solution() << std::endl;
+  }
 
- return 0;
+  return 0;
 } 
--- QP_solver/examples/QP_solver/qp_solver2.C
+++ QP_solver/examples/QP_solver/qp_solver2.C
@@ -1,75 +1,94 @@
+// This program shows how the QP-solver can be called from a C++
+// program. PLEASE NOTE: the API of the QP-solver has been submitted 
+// to the CGAL editorial board and is currently under revision. IT IS
+// NOT GUARANTEED (ACTUALLY UNLIKELY) THAT THE CODE AS BELOW WILL STILL
+// WORK IN FUTURE RELEASES. It will have to be adpated according to the
+// final API of the package. Please contact Bernd Gaertner 
+// (gaertner at inf.ethz.ch) in case of problems or feedback. 
+
+
+// This example program solves the quadratic program 
+//    minimize    x^T D x + c^T x
+//    subject to  A x <rel> b
+//                L <= x <= u
+// where <rel> stands for a vector of relations from {<=, ==, >=}
+//
+// In the concrete example below, we have the following data:
+//     D          c         A          b      L         U
+// -----------------------------------------------------------------------
+//     1.0 0.0    0.0 3.0   0.5 1.0    0.5    0.0 0.0   infinity, infinity
+//     0.0 0.0    
+//
+// In other words, we minimize x^2 + 3y subject to x/2 + y >= 1/2
+// and x,y >= 0. Let's see what this gives: it is clear that in an
+// optimal solution, the constraint is satisfied with equality,
+// otherwise, we could decrease the objective function value. Let's 
+// substitute x with 1 - 2y in the objective function; we get
+// 4y^2 - y + 1. The derivative is 8y-1, so this is minimzed
+// for y = 1/8. This value (as well as x = 1 - 2(1/8) = 3/4)
+// are within the bounds, so the solution should be x = 3/4
+// and y = 1/8. 
+
+// for some reason, almost no other include order works...
 #include <CGAL/QP_solver/gmp_double.h>
 #include <CGAL/Gmpz.h>
 #include <CGAL/QP_solver/Double.h>
 #include <CGAL/QP_solver.h>
-#include <iostream>
 
-
+// here we declare the types of the various QP entries
 struct Traits {
-  enum Row_type {LESS_EQUAL, EQUAL, GREATER_EQUAL};
-  typedef CGAL::Double ET;
-  
-  typedef double **A_iterator;
-  typedef double *B_iterator;
-  typedef double *C_iterator;
-  typedef double **D_iterator;
-  typedef bool *FL_iterator;
-  typedef bool *FU_iterator;
-  typedef double *L_iterator;
-  typedef double *U_iterator;
-  typedef Row_type *Row_type_iterator;
+  enum Row_type {LESS_EQUAL, EQUAL, GREATER_EQUAL}; 
+  typedef Row_type *Row_type_iterator; // iterator for the constraint type
 
-  typedef CGAL::Tag_false Is_linear;
-  typedef CGAL::Tag_true Is_symmetric;
-  typedef CGAL::Tag_false Has_equalities_only_and_full_rank;
-  typedef CGAL::Tag_false Is_in_standard_form;
+  // the exact internal type (input type must be convertible to this)
+  typedef CGAL::Double ET;  // experimental type, not officially in CGAL yet!!
+
+  // the input type is double, and the problem comes via arrays 
+  typedef double **A_iterator;  // for the columns of A
+  typedef double *B_iterator;   // for the right-hand side b
+  typedef double *C_iterator;   // for the objective function c
+  typedef double **D_iterator;  // for the rows of D
+  typedef bool *FL_iterator;    // for finiteness of lower bound L
+  typedef bool *FU_iterator;    // for finiteness of upper bound U
+  typedef double *L_iterator;   // for lower bounds L
+  typedef double *U_iterator;   // for upper bounds U
+
+  typedef CGAL::Tag_false Is_linear;     // we have a proper QP
+  typedef CGAL::Tag_true Is_symmetric;   // the matrix D is symmetric
+  typedef CGAL::Tag_true Has_equalities_only_and_full_rank;
+     // A has indeed full rank 1, and there are only equalities
+  typedef CGAL::Tag_true Is_in_standard_form; // only x >=0 bounds
 };
 
-typedef CGAL::QP_solver<Traits> Solver;
+typedef CGAL::QP_solver<Traits> Solver;  // the solver type
 
 
 int main() {
+  double c[]       = {0.0, 3.0};
+  double D_row_0[] = {1.0, 0.0};
+  double D_row_1[] = {0.0, 0.0};
+  double A_col_0[] = {0.5};
+  double A_col_1[] = {1.0};
+  double b[]       = {0.5};
 
-  double c[5] = {1, 1, 1, 1, 1};
-  double D_row_0[5] = {0, 0, 0, 0, 0};
-  double D_row_1[5] = {0, 0, 0, 0, 0};
-  double D_row_2[5] = {0, 0, 0, 0, 0};
-  double D_row_3[5] = {0, 0, 0, 0, 0};
-  double D_row_4[5] = {0, 0, 0, 0, 0};
+  double *rows_of_D[] = {D_row_0, D_row_1};
+  double *cols_of_A[] = {A_col_0, A_col_1};
 
-  double A_col_0[10] = {1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
-  double A_col_1[10] = {1, 0, 0, 0, 1, 1, 1, 0, 0, 0};
-  double A_col_2[10] = {0, 1, 0, 0, 1, 0, 0, 1, 1, 0};
-  double A_col_3[10] = {0, 0, 1, 0, 0, 1, 0, 1, 0, 1};
-  double A_col_4[10] = {0, 0, 0, 1, 0, 0, 1, 0, 1, 1};
-  double b[10] = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+  Traits::Row_type rt[] = {Traits::GREATER_EQUAL};
 
-  Traits::Row_type rt[10] = {Traits::GREATER_EQUAL, Traits::GREATER_EQUAL, Traits::GREATER_EQUAL, Traits::GREATER_EQUAL, Traits::GREATER_EQUAL,
-			     Traits::GREATER_EQUAL, Traits::GREATER_EQUAL, Traits::GREATER_EQUAL, Traits::GREATER_EQUAL, Traits::GREATER_EQUAL};
-  bool fl[5] = {false, true, false, false, false};
-  bool fu[5] = {false, true, false, false, false};
-  double l[5] = {0, 2, 0, 0, 0};
-  double u[5] = {10000, 4, 10000, 10000, 10000};
-  
-  double* rows_of_D[5] = {D_row_0, D_row_1, D_row_2, D_row_3, D_row_4};
-  double* cols_of_A[5] = {A_col_0, A_col_1, A_col_2, A_col_3, A_col_4};
-  
-  Solver solver(5, 10, cols_of_A, b, c, rows_of_D, rt, fl, l, fu, u);
-  
-  if (solver.status() != Solver::INFEASIBLE) {
-    std::cout << "Basic variables x: ";
-    for (Solver::Basic_variable_value_iterator it = solver.basic_original_variables_value_begin(); it != solver.basic_original_variables_value_end(); ++it)
-      std::cout << to_double(*it) << " ";
+  // now call the solver; the first two arguments are
+  // the number of variables and the number of constraints (rows of A) 
+  // the final 0's are for the bounds; since we are in standard form,
+  // the solver will never access them
+  Solver solver(2, 1, cols_of_A, b, c, rows_of_D, rt, 0, 0, 0, 0);
 
-    std::cout << std::endl << "Full variables x: ";
-    for (Solver::Variable_value_iterator it = solver.variables_value_begin(); it != solver.variables_value_end(); ++it)
-      std::cout << to_double(*it) << " ";
+  if (solver.status() == Solver::OPTIMAL) { // we know that, don't we?
+    std::cout << "Optimal feasible solution x: ";
+    for (Solver::Variable_value_iterator it = solver.variables_value_begin(); 
+	 it != solver.variables_value_end(); ++it)
+      std::cout << CGAL::to_double(*it) << " "; // variable values
+    std::cout << "f(x): " << CGAL::to_double(solver.solution()) << std::endl;
+  }
 
-    std::cout << std::endl << "f(x): " << to_double(solver.solution()) << std::endl;
-  }  
-  else
-    std::cout << "No feasible solution found" << std::endl;
-  
-return 0;
-
-}
+  return 0;
+} 
