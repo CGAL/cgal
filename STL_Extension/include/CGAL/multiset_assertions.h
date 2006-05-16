623b747d424811e80433512da81bb5f15257a5a1
Ron Wein <wein@post.tau.ac.il>
2006-05-16 10:11:35 +0000

    Changed the header such that the copyright is only for TAU.

--- STL_Extension/include/CGAL/multiset_assertions.h
+++ STL_Extension/include/CGAL/multiset_assertions.h
@@ -1,8 +1,4 @@
-// Copyright (c) 2005  Utrecht University (The Netherlands),
-// ETH Zurich (Switzerland), Freie Universitaet Berlin (Germany),
-// INRIA Sophia-Antipolis (France), Martin-Luther-University Halle-Wittenberg
-// (Germany), Max-Planck-Institute Saarbruecken (Germany), RISC Linz (Austria),
-// and Tel-Aviv University (Israel).  All rights reserved.
+// Copyright (c) 2005  Tel-Aviv University (Israel).  All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org); you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License as
