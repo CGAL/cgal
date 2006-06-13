9f7e8fe5c97d6e885882048ab7fc263a8139bdda
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2006-06-13 09:47:59 +0000

    merge r31512 and r3152 into the branch CGAL-3.2-branch:   - fix examples/Triangulation_3/example_regular.C   - add a paragraph about Regular_triangulation_filtered_traits_3<FK> in the     user manual (this patch has been written by Monique Teillaud).

--- Triangulation_3/examples/Triangulation_3/example_regular.C
+++ Triangulation_3/examples/Triangulation_3/example_regular.C
@@ -3,13 +3,13 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Regular_triangulation_3.h>
 #include <CGAL/Regular_triangulation_euclidean_traits_3.h>
-
+#include <CGAL/Regular_triangulation_filtered_traits_3.h>
 #include <cassert>
 #include <vector>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 
-typedef CGAL::Regular_triangulation_euclidean_traits_3<K>   Traits;
+typedef CGAL::Regular_triangulation_filtered_traits_3<K>    Traits;
 
 typedef Traits::RT                                          Weight;
 typedef Traits::Bare_point                                  Point;
