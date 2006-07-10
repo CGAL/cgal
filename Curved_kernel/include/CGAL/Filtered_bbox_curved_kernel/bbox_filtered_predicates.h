c1b7d5caeaf24f423f8b964056b4e0ba4ac511ad
Pedro Machado Manhaes de Castro <Pedro.Machado@sophia.inria.fr>
2006-07-10 09:39:33 +0000

    Compile errors

--- Curved_kernel/include/CGAL/Filtered_bbox_curved_kernel/bbox_filtered_predicates.h
+++ Curved_kernel/include/CGAL/Filtered_bbox_curved_kernel/bbox_filtered_predicates.h
@@ -743,7 +743,12 @@ class Do_overlap_2
 	       const Circular_arc_point_2 &p,
 	       Line_arc_2 &ha1, Line_arc_2 &ha2) const
     {  
-      CK().split_2_object()(A.arc(), p.point(), ha1, ha2);
+      Rline_arc_2 ca1 , ca2;
+
+      CK().split_2_object()(A.arc(), p.point(), ca1, ca2);
+
+      ha1=Line_arc_2(ca1); 
+      ha2=Line_arc_2(ca2);
     }
     
   };
