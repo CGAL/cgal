65fdc4de764fbaf64e2b9af8d89ddfeed34e18c5
Fernando Cacciola <fernando.cacciola@geometryfactory.com>
2006-07-17 16:51:54 +0000

    Fixed typo and merge conflicts

--- Straight_skeleton_2/include/CGAL/Straight_skeleton_2/Straight_skeleton_builder_2_impl.h
+++ Straight_skeleton_2/include/CGAL/Straight_skeleton_2/Straight_skeleton_builder_2_impl.h
@@ -47,7 +47,7 @@ Straight_skeleton_builder_2<Gt,SS,V>::Straight_skeleton_builder_2 ( Traits const
  ,mVertexID(0)
  ,mEdgeID(0)
  ,mEventID(0)
- ,mStepID(-1)
+ ,mStepID(0)
  ,mPQ(mEventCompare)
  ,mSSkel( new SSkel() )
 {
