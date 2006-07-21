419ddf211294085a95eb0933a4e81b83fb461484
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2006-07-21 16:25:28 +0000

    The Filtered_predicate example in the doc was not tested, and hence was buggy. Make it a real example which works.

---
+++ Filtered_kernel/examples/Filtered_kernel/filtered_predicate.cpp
@@ -0,0 +1,45 @@
+// file : examples/Filtered_kernel/filtered_predicate.cpp
+
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Filtered_predicate.h>
+#include <CGAL/MP_Float.h>
+#include <CGAL/Cartesian_converter.h>
+
+typedef CGAL::Simple_cartesian<double> K;
+typedef CGAL::Simple_cartesian<CGAL::Interval_nt_advanced> FK;
+typedef CGAL::Simple_cartesian<CGAL::MP_Float> EK;
+typedef CGAL::Cartesian_converter<K, EK> C2E;
+typedef CGAL::Cartesian_converter<K, FK> C2F;
+
+// Define my predicate, parameterized by a kernel.
+template < typename K >
+struct My_orientation_2
+{
+  typedef typename K::RT            RT;
+  typedef typename K::Point_2       Point_2;
+
+  typedef typename K::Orientation   result_type;
+  typedef CGAL::Arity_tag< 3 >      Arity;
+
+  result_type
+  operator()(const Point_2 &p, const Point_2 &q, const Point_2 &r) const
+  {
+    RT prx = p.x() - r.x();
+    RT pry = p.y() - r.y();
+    RT qrx = q.x() - r.x();
+    RT qry = q.y() - r.y();
+    return CGAL::sign( prx*qry - qrx*pry );
+  }
+};
+
+typedef CGAL::Filtered_predicate<My_orientation_2<EK>,
+                                 My_orientation_2<FK>, C2E, C2F> Orientation_2;
+
+int main()
+{
+  K::Point_2 p(1,2), q(2,3), r(3,4);
+  Orientation_2 orientation;
+  orientation(p, q, r);
+  return 0;
+}
+
