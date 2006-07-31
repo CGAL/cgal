e737ee4e88c3a8556a907e54744d5ba12ef8cd5d
Pedro Machado Manhaes de Castro <Pedro.Machado@sophia.inria.fr>
2006-07-31 09:35:31 +0000

    Algebraic Kernel (for the Curved Kernel 3D)

---
+++ Algebraic_kernel_for_spheres/include/CGAL/Algebraic_kernel_for_spheres/internal_functions_comparison_root_for_spheres_2_3.h
@@ -0,0 +1,62 @@
+// Copyright (c) 2005  INRIA Sophia-Antipolis (France) 
+// All rights reserved.
+//
+// Authors : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
+//           Sylvain Pion     <Sylvain.Pion@sophia.inria.fr>
+//           Julien Hazebrouck
+//           Damien Leroy
+// 
+// Partially supported by the IST Programme of the EU as a Shared-cost
+// RTD (FET Open) Project under Contract No  IST-2000-26473 
+// (ECG - Effective Computational Geometry for Curves and Surfaces) 
+// and a STREP (FET Open) Project under Contract No  IST-006413 
+// (ACS -- Algorithms for Complex Shapes)
+
+#ifndef CGAL_ALGEBRAIC_KERNEL_COMPARISON_ROOT_FOR_SPHERES_2_3_H
+#define CGAL_ALGEBRAIC_KERNEL_COMPARISON_ROOT_FOR_SPHERES_2_3_H
+
+
+namespace CGAL {
+  namespace AlgebraicSphereFunctors{
+  
+    template <typename RT>
+      Comparison_result 
+      compare_x(const CGAL::Root_for_spheres_2_3<RT>& r1, const CGAL::Root_for_spheres_2_3<RT>& r2){
+      return compare(r1.x(), r2.x());
+    }
+  
+    template <typename RT>
+      Comparison_result 
+      compare_y(const CGAL::Root_for_spheres_2_3<RT>& r1, const CGAL::Root_for_spheres_2_3<RT>& r2){
+      return compare(r1.y(), r2.y());
+    }
+
+    template <typename RT>
+      Comparison_result 
+      compare_z(const CGAL::Root_for_spheres_2_3<RT>& r1, const CGAL::Root_for_spheres_2_3<RT>& r2){
+      return compare(r1.z(), r2.z());
+    }
+  
+    template <typename RT>
+      Comparison_result
+      compare_xy(const CGAL::Root_for_spheres_2_3<RT>& r1, const CGAL::Root_for_spheres_2_3<RT>& r2){
+      Comparison_result compx = compare_x(r1, r2);
+      if(compx != 0)
+	return compx;
+      return compare_y(r1, r2);
+    }
+
+     template <typename RT>
+      Comparison_result
+      compare_xyz(const CGAL::Root_for_spheres_2_3<RT>& r1, const CGAL::Root_for_spheres_2_3<RT>& r2){
+      Comparison_result compxy = compare_xy(r1, r2);
+      if(compxy != 0)
+	return compxy;
+      return compare_z(r1, r2);
+    }
+
+
+  } // namespace AlgebraicSphereFunctors
+} // namespace CGAL
+
+#endif // CGAL_ALGEBRAIC_KERNEL_COMPARISON_ROOT_FOR_SPHERES_2_3_H
---
+++ Algebraic_kernel_for_spheres/include/CGAL/Algebraic_kernel_for_spheres/internal_functions_on_roots_and_polynomials_2_3.h
@@ -0,0 +1,156 @@
+// Copyright (c) 2005  INRIA Sophia-Antipolis (France) 
+// All rights reserved.
+//
+// Authors : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
+//           Sylvain Pion     <Sylvain.Pion@sophia.inria.fr>
+//           Julien Hazebrouck
+//           Damien Leroy
+// 
+// Partially supported by the IST Programme of the EU as a Shared-cost
+// RTD (FET Open) Project under Contract No  IST-2000-26473 
+// (ECG - Effective Computational Geometry for Curves and Surfaces) 
+// and a STREP (FET Open) Project under Contract No  IST-006413 
+// (ACS -- Algorithms for Complex Shapes)
+
+#ifndef CGAL_ALGEBRAIC_KERNEL_FUNCTIONS_ON_ROOTS_AND_POLYNOMIALS_2_3_H
+#define CGAL_ALGEBRAIC_KERNEL_FUNCTIONS_ON_ROOTS_AND_POLYNOMIALS_2_3_H
+
+#include <CGAL/Algebraic_kernel_for_spheres/internal_functions_on_roots_and_polynomial_1_3_and_2_3.h>
+
+namespace CGAL {
+  namespace AlgebraicSphereFunctors {
+
+  template < class AK, class OutputIterator >
+  inline 
+  OutputIterator
+    solve( const typename AK::Polynomial_for_spheres_2_3 &e1,
+           const typename AK::Polynomial_for_spheres_2_3 &e2,
+	   const typename AK::Polynomial_for_spheres_2_3 &e3,
+	   OutputIterator res )
+  {
+    CGAL_kernel_precondition(!((e1 == e2) && (e2 == e3)));
+    // we put as a precondition that the polynomial for spheres represents
+    // a sphere and not an isolated point or an empty_space
+    CGAL_kernel_precondition(!(e1.empty_space() || e1.isolated_point())); 
+    CGAL_kernel_precondition(!(e2.empty_space() || e2.isolated_point())); 
+    CGAL_kernel_precondition(!(e3.empty_space() || e3.isolated_point())); 
+    typedef typename AK::Polynomial_1_3 Polynomial_1_3;
+    // The degenerated cases are 2 tangent spheres
+    // os 2 non-intersecting spheres
+    // beacause we cannot have infinitely many solutions
+    if(e1 == e2) {
+      if(tangent<AK>(e1,e3)) {
+        Polynomial_1_3 p = plane_from_2_spheres<AK>(e1,e3);
+        return CGALi::solve_tangent<AK>(p,e1,res);
+      }
+      CGAL_kernel_precondition(!(intersect<AK>(e1,e3)));
+      return res;
+    }
+    if((e1 == e3) || (e2 == e3)) {
+      if(tangent<AK>(e1,e2)) {
+        Polynomial_1_3 p = plane_from_2_spheres<AK>(e1,e2);
+        return CGALi::solve_tangent<AK>(p,e1,res);
+      }
+      CGAL_kernel_precondition(!(intersect<AK>(e1,e2)));
+      return res;
+    }
+    
+    // non degenerated case
+    if(intersect<AK>(e1,e2)) {
+      Polynomial_1_3 p1 = plane_from_2_spheres<AK>(e1,e2);
+      if(intersect<AK>(e2,e3)) {
+        Polynomial_1_3 p2 = plane_from_2_spheres<AK>(e2,e3); 
+        return solve<AK>(p1,p2,e2,res);
+      } return res;
+    } return res;
+  }
+
+  template <class AK>
+  typename AK::Root_for_spheres_2_3
+  x_critical_point(const typename AK::Polynomial_for_spheres_2_3 & s, bool i)
+  {
+    typedef typename AK::Root_of_2 Root_of_2;
+    typedef typename AK::Root_for_spheres_2_3 Root_for_spheres_2_3; 
+
+    return Root_for_spheres_2_3(make_root_of_2(s.a(),i?-1:1,s.r_sq()),
+                                Root_of_2(s.b()),
+                                Root_of_2(s.c()));
+  }
+
+  template <class AK, class OutputIterator>
+  OutputIterator
+  x_critical_points(const typename AK::Polynomial_for_spheres_2_3 & s, OutputIterator res)
+  {
+    typedef typename AK::Root_of_2 Root_of_2;
+    typedef typename AK::Root_for_spheres_2_3 Root_for_spheres_2_3; 
+
+    *res++ =  Root_for_spheres_2_3(make_root_of_2(s.a(),-1,s.r_sq()),
+                                Root_of_2(s.b()),
+                                Root_of_2(s.c()));
+    *res++ =  Root_for_spheres_2_3(make_root_of_2(s.a(),1,s.r_sq()),
+                                Root_of_2(s.b()),
+                                Root_of_2(s.c()));
+    return res;
+  }
+
+  template <class AK>
+  typename AK::Root_for_spheres_2_3
+  y_critical_point(const typename AK::Polynomial_for_spheres_2_3 &s, bool i)
+  {
+    typedef typename AK::Root_of_2 Root_of_2;
+    typedef typename AK::Root_for_spheres_2_3 Root_for_spheres_2_3; 
+
+    return Root_for_spheres_2_3(Root_of_2(s.a()),
+                                make_root_of_2(s.b(),i?-1:1,s.r_sq()),
+                                Root_of_2(s.c()));
+  }
+  
+  template <class AK, class OutputIterator>
+  OutputIterator
+  y_critical_points(const typename AK::Polynomial_for_spheres_2_3 & s, OutputIterator res)
+  {
+    typedef typename AK::Root_of_2 Root_of_2;
+    typedef typename AK::Root_for_spheres_2_3 Root_for_spheres_2_3; 
+
+    *res++ =  Root_for_spheres_2_3(Root_of_2(s.a()),
+                                make_root_of_2(s.b(),-1,s.r_sq()),
+                                Root_of_2(s.c()));
+    *res++ =  Root_for_spheres_2_3(Root_of_2(s.a()),
+                                make_root_of_2(s.b(),1,s.r_sq()),
+                                Root_of_2(s.c()));
+    return res;
+  }
+
+  template <class AK>
+  typename AK::Root_for_spheres_2_3
+  z_critical_point(const typename AK::Polynomial_for_spheres_2_3 &s, bool i)
+  {
+    typedef typename AK::Root_of_2 Root_of_2;
+    typedef typename AK::Root_for_spheres_2_3 Root_for_spheres_2_3; 
+
+    return Root_for_spheres_2_3(Root_of_2(s.a()),
+                                Root_of_2(s.b()),
+                                make_root_of_2(s.c(),i?-1:1,s.r_sq()));
+  }
+  
+  template <class AK, class OutputIterator>
+  OutputIterator
+  z_critical_points(const typename AK::Polynomial_for_spheres_2_3 & s, OutputIterator res)
+  {
+    typedef typename AK::Root_of_2 Root_of_2;
+    typedef typename AK::Root_for_spheres_2_3 Root_for_spheres_2_3; 
+
+    *res++ =  Root_for_spheres_2_3(Root_of_2(s.a()),
+                                Root_of_2(s.b()),
+                                make_root_of_2(s.c(),-1,s.r_sq()));
+    *res++ =  Root_for_spheres_2_3(Root_of_2(s.a()),
+                                Root_of_2(s.b()),
+                                make_root_of_2(s.c(),1,s.r_sq()));
+    return res;
+  }
+
+
+  } // namespace AlgebraicSphereFunctors
+} // namespace CGAL
+
+#endif //  CGAL_ALGEBRAIC_KERNEL_FUNCTIONS_ON_ROOTS_AND_POLYNOMIALS_2_3_H
