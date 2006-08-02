b69dd83c5f58b378845f49fed3f06dfe517cbd3d
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2006-08-02 11:43:40 +0000

    Use CGAL_Kernel_obj.

--- Kernel_23/include/CGAL/Kernel/Type_equality_wrapper.h
+++ Kernel_23/include/CGAL/Kernel/Type_equality_wrapper.h
@@ -38,31 +38,13 @@ struct Type_equality_wrapper
 {
     typedef K_base                                  Kernel_base;
 
-    typedef CGAL::Point_2<Kernel>                   Point_2;
-    typedef CGAL::Vector_2<Kernel>                  Vector_2;
-    typedef CGAL::Direction_2<Kernel>               Direction_2;
-    typedef CGAL::Line_2<Kernel>                    Line_2;
-    typedef CGAL::Ray_2<Kernel>                     Ray_2;
-    typedef CGAL::Segment_2<Kernel>                 Segment_2;
-    typedef CGAL::Triangle_2<Kernel>                Triangle_2;
-    typedef CGAL::Circle_2<Kernel>                  Circle_2;
-    typedef CGAL::Iso_rectangle_2<Kernel>           Iso_rectangle_2;
-    typedef CGAL::Aff_transformation_2<Kernel>      Aff_transformation_2;
+#define CGAL_Kernel_obj(X)   typedef CGAL::X<Kernel> X;
+
+#include <CGAL/Kernel/interface_macros.h>
 
     // Undocumented stuff.
     typedef CGAL::Conic_2<Kernel>                   Conic_2;
-
-    typedef CGAL::Point_3<Kernel>                   Point_3;
-    typedef CGAL::Vector_3<Kernel>                  Vector_3;
-    typedef CGAL::Direction_3<Kernel>               Direction_3;
-    typedef CGAL::Line_3<Kernel>                    Line_3;
-    typedef CGAL::Plane_3<Kernel>                   Plane_3;
-    typedef CGAL::Ray_3<Kernel>                     Ray_3;
-    typedef CGAL::Segment_3<Kernel>                 Segment_3;
-    typedef CGAL::Triangle_3<Kernel>                Triangle_3;
-    typedef CGAL::Tetrahedron_3<Kernel>             Tetrahedron_3;
-    typedef CGAL::Sphere_3<Kernel>                  Sphere_3;
-    typedef CGAL::Iso_cuboid_3<Kernel>              Iso_cuboid_3;
+    typedef CGAL::Aff_transformation_2<Kernel>      Aff_transformation_2;
     typedef CGAL::Aff_transformation_3<Kernel>      Aff_transformation_3;
 };
 
