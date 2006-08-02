7b120847c956ccf0b1e4006ed44b830786da49e1
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2006-08-02 12:17:19 +0000

    Do_intersect_3 is a predicate, not a construction.

--- Kernel_23/include/CGAL/Kernel/interface_macros.h
+++ Kernel_23/include/CGAL/Kernel/interface_macros.h
@@ -419,7 +419,7 @@ CGAL_Kernel_pred(Counterclockwise_in_between_2,
 		 counterclockwise_in_between_2_object)
 CGAL_Kernel_pred(Do_intersect_2,
 		 do_intersect_2_object)
-CGAL_Kernel_cons(Do_intersect_3,
+CGAL_Kernel_pred(Do_intersect_3,
 		 do_intersect_3_object)
 CGAL_Kernel_pred(Equal_xy_3,
 		 equal_xy_3_object)
