a9ef419c3146978e73d172551d5b13a64f868778
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2006-08-03 11:29:56 +0000

    More progress towards a fully working Lazy_kernel...

--- Filtered_kernel/include/CGAL/Lazy_kernel.h
+++ Filtered_kernel/include/CGAL/Lazy_kernel.h
@@ -34,15 +34,18 @@ CGAL_BEGIN_NAMESPACE
 
 // EK = exact kernel that will be made lazy
 // Kernel = lazy kernel
+
+// the Generic base simplies applies the generic magic functor stupidly.
+// then the real base fixes up a few special cases.
 template < typename EK_, typename AK_, typename E2A_, typename Kernel >
-class Lazy_kernel_base {
+class Lazy_kernel_generic_base {
 public:
   typedef AK_   AK;
   typedef EK_   EK;
   typedef E2A_  E2A;
 
   template < typename Kernel2 >
-  struct Base { typedef Lazy_kernel_base<EK, AK, E2A, Kernel2>  Type; };
+  struct Base { typedef Lazy_kernel_generic_base<EK, AK, E2A, Kernel2>  Type; };
 
   // What to do with the tag ?
   // Probably this should not exist, should it ?
@@ -102,6 +105,51 @@ public:
 		   intersect_with_iterators_2_object)
 #else 
 #define CGAL_Kernel_cons(C, Cf) \
+    typedef typename boost::mpl::if_< boost::is_same<typename AK::C::result_type, typename AK::FT>,\
+                                      Lazy_construction_nt<Kernel,typename AK::C, typename EK::C>,\
+                                      typename boost::mpl::if_<boost::is_same<typename AK::C::result_type, Object >,\
+                                                               Lazy_construction_object<Kernel,typename AK::C, typename EK::C>,\
+                                                               Lazy_construction<Kernel,typename AK::C, typename EK::C> >::type >::type  C; \
+    C Cf() const { return C(); }
+
+#endif //CGAL_INTERSECT_WITH_ITERATORS_2
+
+
+#include <CGAL/Kernel/interface_macros.h>
+
+};
+
+template < typename EK_, typename AK_, typename E2A_, typename Kernel >
+class Lazy_kernel_base
+  : public Lazy_kernel_generic_base<EK_, AK_, E2A_, Kernel>
+{
+public:
+  typedef AK_   AK;
+  typedef EK_   EK;
+  typedef E2A_  E2A;
+
+  template < typename Kernel2 >
+  struct Base { typedef Lazy_kernel_base<EK, AK, E2A, Kernel2>  Type; };
+
+#if 0
+    // We change the constructions.
+#ifdef CGAL_INTERSECT_WITH_ITERATORS_2
+#define CGAL_Kernel_cons(C, Cf) \
+    typedef typename boost::mpl::if_<boost::is_same<typename AK::C, typename AK::Intersect_with_iterators_2>, \
+                                     Lazy_intersect_with_iterators<Kernel,typename AK::C, typename EK::C>, \
+                                     typename boost::mpl::if_<boost::is_same<typename AK::C::result_type, Bbox_2>, \
+                                                              Lazy_construction_bbox<Kernel,typename AK::C, typename EK::C>, \
+                                                              typename boost::mpl::if_<boost::is_same<typename AK::C::result_type, typename AK::FT>,\
+                                                                                       Lazy_construction_nt<Kernel,typename AK::C, typename EK::C>,\
+                                                                                       typename boost::mpl::if_<boost::is_same<typename AK::C::result_type, Object >,\
+                                                                                                                Lazy_construction_object<Kernel,typename AK::C, typename EK::C>,\
+                                                                                                                Lazy_construction<Kernel,typename AK::C, typename EK::C> >::type >::type > ::type > ::type C; \
+    C Cf() const { return C(); }
+
+  CGAL_Kernel_cons(Intersect_with_iterators_2,
+		   intersect_with_iterators_2_object)
+#else 
+#define CGAL_Kernel_cons(C, Cf) \
     typedef typename boost::mpl::if_<boost::is_same<typename AK::C, typename AK::Construct_cartesian_const_iterator_2>, \
                                      Lazy_cartesian_const_iterator_2<Kernel,typename AK::C, typename EK::C>, \
                                      typename boost::mpl::if_<boost::is_same<typename AK::C, typename AK::Construct_cartesian_const_iterator_3>, \
@@ -119,10 +167,41 @@ public:
 
 #endif //CGAL_INTERSECT_WITH_ITERATORS_2
 
+#endif // 0
 
-#include <CGAL/Kernel/interface_macros.h>
+  typedef CommonKernelFunctors::Assign_2<Kernel>        Assign_2;
+  typedef CommonKernelFunctors::Assign_3<Kernel>        Assign_3;
+  typedef Lazy_construction_bbox<Kernel, typename AK::Construct_bbox_2, typename EK::Construct_bbox_2>             Construct_bbox_2;
+  typedef Lazy_construction_bbox<Kernel, typename AK::Construct_bbox_3, typename EK::Construct_bbox_3>             Construct_bbox_3;
+  typedef Lazy_cartesian_const_iterator_2<Kernel, typename AK::Construct_cartesian_const_iterator_2, typename EK::Construct_cartesian_const_iterator_2>   Construct_cartesian_const_iterator_2;
+  typedef Lazy_cartesian_const_iterator_3<Kernel, typename AK::Construct_cartesian_const_iterator_3, typename EK::Construct_cartesian_const_iterator_3>   Construct_cartesian_const_iterator_3;
+
+  Assign_2
+  assign_2_object() const
+  { return Assign_2(); }
+
+  Assign_3
+  assign_3_object() const
+  { return Assign_3(); }
+
+  Construct_bbox_2
+  construct_bbox_2_object() const
+  { return Construct_bbox_2(); }
+
+  Construct_bbox_3
+  construct_bbox_3_object() const
+  { return Construct_bbox_3(); }
+
+  Construct_cartesian_const_iterator_2
+  construct_cartesian_const_iterator_2_object() const
+  { return Construct_cartesian_const_iterator_2(); }
+
+  Construct_cartesian_const_iterator_3
+  construct_cartesian_const_iterator_3_object() const
+  { return Construct_cartesian_const_iterator_3(); }
 
 };
+
 
 template <class EK, class AK, class E2A>
 struct Lazy_kernel_adaptor
