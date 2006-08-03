0dfa9845b8f27683b7252c310bd88ffe1b2b2c07
Nico Kruithof <Nico@cs.rug.nl>
2006-08-03 15:35:23 +0000

    Forgot two files

---
+++ Skin_surface_3/include/CGAL/Mixed_complex_filtered_traits_3.h
@@ -0,0 +1,89 @@
+// Copyright (c) 2005 Rijksuniversiteit Groningen (Netherlands)
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : Nico Kruithof <Nico@cs.rug.nl>
+
+#ifndef CGAL_MIXED_COMPLEX_FILTERED_TRAITS_3_H
+#define CGAL_MIXED_COMPLEX_FILTERED_TRAITS_3_H
+
+#include <CGAL/basic.h>
+#include <CGAL/Mixed_complex_traits_3.h>
+#include <CGAL/Regular_triangulation_filtered_traits_3.h>
+#include <CGAL/Filtered_predicate.h>
+
+CGAL_BEGIN_NAMESPACE 
+
+template <class K>
+class Mixed_complex_filtered_traits_3
+  : public Mixed_complex_traits_base_3<K>
+{
+  // Exact traits is based on the exact kernel.
+  typedef Mixed_complex_traits_3<typename K::EK>
+                                                   Exact_traits;
+  // Filtering traits is based on the filtering kernel.
+  typedef Mixed_complex_traits_3<typename K::FK>
+                                                   Filtering_traits;
+
+  typedef typename K::C2E C2E;
+  typedef typename K::C2F C2F;
+
+  typedef Mixed_complex_traits_base_3<K>           Base;
+public:
+  typedef Filtered_predicate<
+            typename Exact_traits::Side_of_mixed_cell_3,
+            typename Filtering_traits::Side_of_mixed_cell_3,
+            Weighted_converter_3<C2E>,
+            Weighted_converter_3<C2F> >  Side_of_mixed_cell_3;
+
+  typedef Filtered_predicate<
+            typename Exact_traits::Construct_weighted_circumcenter_3,
+            typename Filtering_traits::Construct_weighted_circumcenter_3,
+            Weighted_converter_3<C2E>,
+            Weighted_converter_3<C2F> >  Construct_weighted_circumcenter_3;
+
+  typedef Filtered_predicate<
+            typename Exact_traits::Construct_anchor_point_3,
+            typename Filtering_traits::Construct_anchor_point_3,
+            Weighted_converter_3<C2E>,
+            Weighted_converter_3<C2F> >  Construct_anchor_point_3;
+
+ 
+  Mixed_complex_filtered_traits_3() {}
+  Mixed_complex_filtered_traits_3(typename Base::FT s) : Base(s) {}
+
+  Side_of_mixed_cell_3 
+  side_of_mixed_cell_3_object() const 
+  { 
+//     std::cout << "L" << __LINE__ << "(" << __FILE__ <<") "
+// 	      << "FILTERED_SIDE_OF_MIXED_CELL_3_OBJECT "
+// 	      << Base::get_shrink() << std::endl;
+    return Side_of_mixed_cell_3(Base::get_shrink()); 
+  }
+
+//   Construct_weighted_circumcenter_3
+//   construct_weighted_circumcenter_3_object() const
+//   { return Construct_weighted_circumcenter_3(); }
+
+  Construct_anchor_point_3
+  construct_anchor_point_3_object() const
+  {
+    return Construct_anchor_point_3(Base::get_shrink()); 
+  }
+};
+
+CGAL_END_NAMESPACE
+#endif // CGAL_MIXED_COMPLEX_FILTERED_TRAITS_3_H
