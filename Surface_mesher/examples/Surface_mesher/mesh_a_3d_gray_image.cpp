23b7d4e311f64c8d1cbc6df42e86381630714216
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2006-08-09 13:57:34 +0000

    - documentation of Surface_mesh_default_triangulation_3 - big cleaning of examples/Surface_mesher/

--- Surface_mesher/examples/Surface_mesher/mesh_a_3d_gray_image.cpp
+++ Surface_mesher/examples/Surface_mesher/mesh_a_3d_gray_image.cpp
@@ -1,19 +1,19 @@
-// file examples/Surface_mesher/3d_image_surface_mesher.cpp
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Delaunay_triangulation_3.h>
+// file examples/Surface_mesher/mesh_a_3d_gray_image.cpp
+#include <CGAL/Surface_mesh_default_triangulation_3.h>
+#include <CGAL/Complex_2_in_triangulation_3.h>
 #include <CGAL/make_surface_mesh.h>
 #include <CGAL/Gray_level_image_3.h>
 #include <CGAL/Implicit_surface_3.h>
 
-struct Kernel : public CGAL::Exact_predicates_inexact_constructions_kernel {};
-typedef CGAL::Surface_mesh_vertex_base_3<Kernel> Vb;
-typedef CGAL::Surface_mesh_cell_base_3<Kernel> Cb;
-typedef CGAL::Triangulation_data_structure_3<Vb, Cb> Tds;
-typedef CGAL::Delaunay_triangulation_3<Kernel, Tds> Tr;
-typedef CGAL::Surface_mesh_complex_2_in_triangulation_3<Tr> C2t3;
+// default triangulation for Surface_mesher
+typedef CGAL::Surface_mesh_default_triangulation_3 Tr;
 
-typedef CGAL::Gray_level_image_3<Kernel::FT, Kernel::Point_3> Gray_level_image;
-typedef CGAL::Implicit_surface_3<Kernel, Gray_level_image> Surface_3;
+// c2t3
+typedef CGAL::Complex_2_in_triangulation_3<Tr> C2t3;
+
+typedef Tr::Geom_traits GT;
+typedef CGAL::Gray_level_image_3<GT::FT, GT::Point_3> Gray_level_image;
+typedef CGAL::Implicit_surface_3<GT, Gray_level_image> Surface_3;
 
 int main(int, char **) {
   Tr tr;            // 3D-Delaunay triangulation
@@ -25,9 +25,9 @@ int main(int, char **) {
   // Carefully choosen bounding sphere: the center must be inside the
   // surface defined by 'image' and the radius must be high enough so that
   // the sphere actually bounds the whole image.
-  Kernel::Point_3 bounding_sphere_center(122., 102., 117.);
-  Kernel::FT bounding_sphere_squared_radius = 200.*200.*2.;
-  Kernel::Sphere_3 bounding_sphere(bounding_sphere_center,
+  GT::Point_3 bounding_sphere_center(122., 102., 117.);
+  GT::FT bounding_sphere_squared_radius = 200.*200.*2.;
+  GT::Sphere_3 bounding_sphere(bounding_sphere_center,
                                    bounding_sphere_squared_radius);
 
   // definition of the surface, with 10^-2 as relative precision
--- Surface_mesher/examples/Surface_mesher/mesh_an_implicit_function.cpp
+++ Surface_mesher/examples/Surface_mesher/mesh_an_implicit_function.cpp
@@ -1,22 +1,23 @@
-// file examples/Surface_mesher/implicit_surface_mesher.cpp
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Delaunay_triangulation_3.h>
+// file examples/Surface_mesher/mesh_an_implicit_function.cpp
+#include <CGAL/Surface_mesh_default_triangulation_3.h>
+#include <CGAL/Complex_2_in_triangulation_3.h>
 #include <CGAL/make_surface_mesh.h>
 #include <CGAL/Implicit_surface_3.h>
 
-struct Kernel : public CGAL::Exact_predicates_inexact_constructions_kernel {};
-typedef CGAL::Surface_mesh_vertex_base_3<Kernel> Vb;
-typedef CGAL::Surface_mesh_cell_base_3<Kernel> Cb;
-typedef CGAL::Triangulation_data_structure_3<Vb, Cb> Tds;
-typedef CGAL::Delaunay_triangulation_3<Kernel, Tds> Tr;
-typedef CGAL::Surface_mesh_complex_2_in_triangulation_3<Tr> C2t3;
-typedef Kernel::Sphere_3 Sphere_3;
-typedef Kernel::Point_3 Point_3;
-typedef Kernel::FT FT;
+// default triangulation for Surface_mesher
+typedef CGAL::Surface_mesh_default_triangulation_3 Tr;
+
+// c2t3
+typedef CGAL::Complex_2_in_triangulation_3<Tr> C2t3;
+
+typedef Tr::Geom_traits GT;
+typedef GT::Sphere_3 Sphere_3;
+typedef GT::Point_3 Point_3;
+typedef GT::FT FT;
 
 typedef FT (*Function)(Point_3);
 
-typedef CGAL::Implicit_surface_3<Kernel, Function> Surface_3;
+typedef CGAL::Implicit_surface_3<GT, Function> Surface_3;
 
 FT sphere_function (Point_3 p) {
   const FT x2=p.x()*p.x(), y2=p.y()*p.y(), z2=p.z()*p.z();
