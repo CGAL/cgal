4cefe79f4b812c88e537aee658d81a39b96f0a28
Pedro Machado Manhaes de Castro <Pedro.Machado@sophia.inria.fr>
2006-08-23 12:32:02 +0000

    Header

--- Circular_kernel_3/include/CGAL/Circular_kernel_3/internal_functions_on_line_3.h
+++ Circular_kernel_3/include/CGAL/Circular_kernel_3/internal_functions_on_line_3.h
@@ -1,16 +1,28 @@
-// Copyright (c) 2005  INRIA Sophia-Antipolis (France) 
+// Copyright (c) 2005-2006  INRIA Sophia-Antipolis (France).
 // All rights reserved.
 //
-// Authors : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
-//           Sylvain Pion     <Sylvain.Pion@sophia.inria.fr>
-//           Julien Hazebrouck
-//           Damien Leroy
-// 
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
 // Partially supported by the IST Programme of the EU as a Shared-cost
 // RTD (FET Open) Project under Contract No  IST-2000-26473 
 // (ECG - Effective Computational Geometry for Curves and Surfaces) 
 // and a STREP (FET Open) Project under Contract No  IST-006413 
 // (ACS -- Algorithms for Complex Shapes)
+//
+// $URL: $
+// $Id: $
+//
+// Author(s) : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
+//             Sylvain Pion     <Sylvain.Pion@sophia.inria.fr>
+//             Pedro Machado    <tashimir@gmail.com>
 
 #ifndef CGAL_SPHERICAL_KERNEL_PREDICATES_ON_LINE_3_H
 #define CGAL_SPHERICAL_KERNEL_PREDICATES_ON_LINE_3_H
