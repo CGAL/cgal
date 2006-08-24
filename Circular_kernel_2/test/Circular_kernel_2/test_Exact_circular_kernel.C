36eddb51e648c1ecd9bc72c8f3b9240bc414836b
Pedro Machado Manhaes de Castro <Pedro.Machado@sophia.inria.fr>
2006-08-24 15:19:27 +0000

    Curved_kernel -> Circular_arc_2 [adapt]

---
+++ Circular_kernel_2/test/Circular_kernel_2/test_Exact_circular_kernel.C
@@ -0,0 +1,23 @@
+#include <CGAL/basic.h>
+#include <CGAL/Exact_circular_kernel_2.h>
+#include <CGAL/intersections.h>
+#include <iostream>
+
+typedef CGAL::Exact_circular_kernel_2 CK;
+
+  CK ck;
+
+#include <CGAL/_test_circles_predicates.h>
+#include <CGAL/_test_circles_constructions.h>
+#include <CGAL/_test_circles_extention.h>
+  
+int main() {
+
+  _test_circle_predicat(ck);
+  _test_circle_construct(ck);
+  _test_circle_bbox(ck);
+  _test_circular_arc_bbox(ck);
+  _test_has_on(ck);
+
+  return 0;
+}
