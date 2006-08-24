a307cf6cf4df772af168fa3704bac65c56b25ebf
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2006-08-24 15:09:34 +0000

    for our friends in Seattle

---
+++ Installation/config/testfiles/CGAL_CFG_NO_NEXTAFTER.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) 1997  Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland), Freie Universitaet Berlin (Germany),
+// INRIA Sophia-Antipolis (France), Martin-Luther-University Halle-Wittenberg
+// (Germany), Max-Planck-Institute Saarbruecken (Germany), RISC Linz (Austria),
+// and Tel-Aviv University (Israel).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : various
+
+// CGAL_CFG_NO_NEXTAFTER.C
+// ---------------------------------------------------------------------
+// A short test program to evaluate a C++ compiler.
+// This program is used by cgal_configure.
+// The following documentation will be pasted in the generated configfile.
+// ---------------------------------------------------------------------
+
+//| If a compiler doesn't know nextafter (or only knows _nextafter as VC++7.1)
+//| CGAL_CFG_NO_NEXTAFTER is set. 
+
+#include <cmath>
+
+int main()
+{
+  double d = nextafter(0,0);
+  return 0;
+}
