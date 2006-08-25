f74ac52c18d7dabc8e0c13dfee9a281e4c0d5fd3
Pedro Machado Manhaes de Castro <Pedro.Machado@sophia.inria.fr>
2006-08-25 13:14:39 +0000

    Algebraic_kernel_for_circles (instead of Algebraic_kernel) Curved_kernel -> Circular_kernel_2

--- Circular_kernel_2/Benchmarks/benchmark_CK2.C
+++ Circular_kernel_2/Benchmarks/benchmark_CK2.C
@@ -7,7 +7,7 @@
 #include <CGAL/MP_Float.h>
 #include <CGAL/Gmpz.h>
 #include <CGAL/Gmpq.h>
-#include <CGAL/Algebraic_kernel_2_2.h>
+#include <CGAL/Algebraic_kernel_for_circles_2_2.h>
 #include <CGAL/intersections.h>
 #include <CGAL/Circular_kernel_2.h>
 #include <CGAL/Arr_circular_arc_traits.h>
