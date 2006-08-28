6105b22d504814501b6a58a7c7a980d938daa28c
Pedro Machado Manhaes de Castro <Pedro.Machado@sophia.inria.fr>
2006-08-28 10:44:07 +0000

    The test for the Exact_circular_kernel_3

---
+++ Circular_kernel_3/test/Circular_kernel_3/test_Exact_circular_kernel.C
@@ -0,0 +1,48 @@
+// Copyright (c) 2005-2006  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// Partially supported by the IST Programme of the EU as a Shared-cost
+// RTD (FET Open) Project under Contract No  IST-2000-26473 
+// (ECG - Effective Computational Geometry for Curves and Surfaces) 
+// and a STREP (FET Open) Project under Contract No  IST-006413 
+// (ACS -- Algorithms for Complex Shapes)
+//
+// $URL: $
+// $Id: $
+//
+// Author(s) : Monique Teillaud <Monique.Teillaud@sophia.inria.fr>
+//             Sylvain Pion     <Sylvain.Pion@sophia.inria.fr>
+//             Pedro Machado    <tashimir@gmail.com>
+
+#include <CGAL/Cartesian.h>
+#include <CGAL/Spherical_kernel_3.h>
+#include <CGAL/Algebraic_kernel_for_spheres_2_3.h>
+#include <CGAL/MP_Float.h>
+#include <CGAL/Quotient.h>
+#include <CGAL/Exact_circular_kernel_3.h>
+#include <CGAL/_test_sphere_predicates.h>
+#include <CGAL/_test_sphere_constructions.h>
+#include <CGAL/_test_sphere_compute.h>
+#include <CGAL/Polynomials_1_3.h>
+#include <CGAL/Polynomials_2_3.h>
+#include <CGAL/Polynomials_for_line_3.h>
+
+int main()
+{ 
+  CGAL::Exact_circular_kernel_3  sk1;
+  _test_spherical_kernel_predicates(sk1);
+  _test_spherical_kernel_construct(sk1); 
+  _test_spherical_kernel_compute(sk1);
+  return 0;
+}
+
