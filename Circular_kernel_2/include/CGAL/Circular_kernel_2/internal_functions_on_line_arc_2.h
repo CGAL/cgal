d2e9194f94c5d5f402d442c0a057d1c90aa73475
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2006-09-07 12:25:09 +0000

    All checks based on identity

--- Circular_kernel_2/include/CGAL/Circular_kernel_2/internal_functions_on_line_arc_2.h
+++ Circular_kernel_2/include/CGAL/Circular_kernel_2/internal_functions_on_line_arc_2.h
@@ -40,7 +40,7 @@ namespace CircularFunctors {
     return ( (CircularFunctors::compare_x<CK>(p, A.source()) != CircularFunctors::compare_x<CK>(p, A.target())) 
 	     || (CircularFunctors::compare_x<CK>(p, A.source()) == CGAL::EQUAL) );
   }
-  
+ 
   template < class CK >
   bool
   equal(const typename CK::Line_arc_2 &A1,
@@ -76,6 +76,10 @@ namespace CircularFunctors {
 	 const typename CK::Circular_arc_point_2 &p,
          const bool has_on_supporting_line = false)
   {
+    if(p.equal_ref(a.source()) || p.equal_ref(a.target())){
+      return true;
+    }
+
     if(!has_on_supporting_line) {
       if(!CGAL::LinearFunctors::has_on<CK>(a.supporting_line(),p))
         return false;
@@ -111,6 +115,9 @@ namespace CircularFunctors {
   compare_y_at_x(const typename CK::Circular_arc_point_2 &p,
                  const typename CK::Line_arc_2 &A1)
   {
+    if(p.equal_ref(A1.source()) || p.equal_ref(A1.target())){
+      return EQUAL;
+    }
     //CGAL_kernel_precondition (CircularFunctors::point_in_x_range<CK>(A1, p));
     //vertical case
     if (CircularFunctors::is_vertical<CK>(A1)) {
@@ -234,23 +241,106 @@ namespace CircularFunctors {
 	((tangent_1_x > 0) && (tangent_2_x < 0))){
       Root_of_2 prod_left = tangent_1_y * tangent_2_x;
       Root_of_2 prod_right = tangent_2_y * tangent_1_x;
-      if (prod_left < prod_right)
+      if (prod_left < prod_right){
 	return CGAL::LARGER;
-      if (prod_left == prod_right)
+      }
+      if (prod_left == prod_right){
 	return  A2.on_upper_part() ? CGAL::LARGER : CGAL::SMALLER;
+      }
       return CGAL::SMALLER;
     }
     else{
       Root_of_2 prod_left = tangent_1_y * tangent_2_x;
       Root_of_2 prod_right = tangent_2_y * tangent_1_x;
-      if (prod_left < prod_right)
+      if (prod_left < prod_right){
 	return CGAL::SMALLER;
-      if (prod_left == prod_right)
+      }
+      if (prod_left == prod_right){
 	return A2.on_upper_part() ? CGAL::LARGER : CGAL::SMALLER;
+      }
       return CGAL::LARGER;
     }
   }
-   
+
+  
+  template < class CK >
+  Comparison_result 
+  compare_y_to_left(const typename CK::Line_arc_2 &A1,
+		    const typename CK::Circular_arc_2 &A2, 
+		    const typename CK::Circular_arc_point_2 &p)
+  {
+    //CGAL_kernel_precondition (A2.is_x_monotone());
+    if(A1.supporting_line().is_vertical())
+      return CGAL::LARGER;
+
+    typedef typename CK::Polynomial_1_2          Polynomial_1_2;
+    typedef typename CK::Root_of_2               Root_of_2;
+
+    const typename CK::Circle_2 & C2 = A2.supporting_circle();
+
+    Root_of_2 b2_y = C2.center().y() - p.y();
+
+    int s_b2_y = CGAL::sign(b2_y);
+
+    if (s_b2_y == 0) {
+      return A2.on_upper_part() ? CGAL::SMALLER : CGAL::LARGER;
+    }
+
+    typename CK::Root_of_2 b2_x = C2.center().x() - p.x();
+
+    Root_of_2 tangent_2_x;
+    Root_of_2 tangent_2_y;
+    if (b2_y < 0){ 
+       tangent_2_x = -b2_y;
+       tangent_2_y = b2_x;
+    }
+    else{
+      tangent_2_x = b2_y;
+      tangent_2_y = -b2_x;
+    }
+
+    Polynomial_1_2 equation = 
+      CGAL::LinearFunctors::get_equation<CK>(A1.supporting_line());
+    typedef typename CK::FT FT;
+    FT tangent_1_x;
+    FT tangent_1_y;
+    if (equation.b() < 0){
+      tangent_1_x = -equation.b();
+      tangent_1_y = equation.a();
+    }
+    else{
+      tangent_1_x = equation.b();
+      tangent_1_y = -equation.a();
+    }
+      
+
+
+    if (((tangent_1_x < 0) && (tangent_2_x > 0)) || 
+	((tangent_1_x > 0) && (tangent_2_x < 0))){
+      Root_of_2 prod_left = tangent_1_y * tangent_2_x;
+      Root_of_2 prod_right = tangent_2_y * tangent_1_x;
+      if (prod_left < prod_right){
+	return CGAL::SMALLER;
+      }
+      if (prod_left == prod_right){
+	return  A2.on_upper_part() ? CGAL::LARGER : CGAL::SMALLER;
+      }
+      return CGAL::LARGER;
+    }
+    else{
+      Root_of_2 prod_left = tangent_1_y * tangent_2_x;
+      Root_of_2 prod_right = tangent_2_y * tangent_1_x;
+      if (prod_left < prod_right){
+	return CGAL::LARGER;
+      }
+      if (prod_left == prod_right){
+	      return A2.on_upper_part() ? CGAL::LARGER : CGAL::SMALLER;
+      }
+      return CGAL::SMALLER;
+    }
+  }
+
+ 
   template < class CK >
   Comparison_result 
   compare_y_to_right(const typename CK::Circular_arc_2 &A1,
@@ -296,6 +386,26 @@ namespace CircularFunctors {
     typedef typename CK::Point_2                  Point_2;
     typedef typename CK::Root_of_2                Root_of_2;
     typedef typename CK::Root_for_circles_2_2     Root_for_circles_2_2;
+
+#ifdef CGAL_CK_EXPLOIT_IDENTITY
+    bool a1s_a2s = a1.source().equal_ref(a2.source());
+    bool a1s_a2t = a1.source().equal_ref(a2.target());
+    bool a1t_a2s = a1.target().equal_ref(a2.source());
+    bool a1t_a2t = a1.target().equal_ref(a2.target());
+
+    if((a1s_a2s && a1t_a2t) || (a1s_a2t && a1t_a2s)){
+
+      *res++ = make_object(a1); 
+      return res;
+    }
+    if(a1s_a2s || a1s_a2t || a1t_a2s || a1t_a2t) {
+      if(! LinearFunctors::non_oriented_equal<CK>(a1.supporting_line(),a2.supporting_line())){
+	if(a1s_a2s || a1s_a2t) *res++ = make_object(std::make_pair(a1.source(), 1u));
+	if(a1t_a2s || a1t_a2t) *res++ = make_object(std::make_pair(a1.target(), 1u));
+      return res;
+      }
+    }
+#endif
     
     if(LinearFunctors::non_oriented_equal<CK>(
       a1.supporting_line(),a2.supporting_line())) {
@@ -305,8 +415,7 @@ namespace CircularFunctors {
 	  if(compare_xy(a1.right(),a2.right()) <= 0){
 	    *res++ = make_object
 	      (Line_arc_2(a1.supporting_line(), a2.left(), a1.right() ));
-	  }
-	  else{
+	  } else{
 	    *res++ = make_object
 	      (Line_arc_2(a1.supporting_line(), a2.left(), a2.right() ));
 	  }
@@ -414,8 +523,106 @@ namespace CircularFunctors {
 	       OutputIterator res )
   {
     typedef typename CK::Circular_arc_2 Circular_arc_2;
+    typedef typename CK::Circular_arc_point_2 Circular_arc_point_2;
     typedef typename CK::Line_arc_2 Line_arc_2;
     typedef std::vector<CGAL::Object > solutions_container;
+
+#ifdef CGAL_CK_EXPLOIT_IDENTITY
+    bool ls_cs = l.source().equal_ref(c.source());
+    bool ls_ct = l.source().equal_ref(c.target());
+    bool lt_cs = l.target().equal_ref(c.source());
+    bool lt_ct = l.target().equal_ref(c.target());
+    
+    if((ls_cs && lt_ct) || (ls_ct && lt_cs)){ // Case 0
+      if (compare_xy<CK>(l.source(), l.target()) == SMALLER){
+	*res++ = make_object(std::make_pair(l.source(), 1u));
+	*res++ = make_object(std::make_pair(l.target(), 1u));
+      } else {
+	*res++ = make_object(std::make_pair(l.target(), 1u));
+	*res++ = make_object(std::make_pair(l.source(), 1u));
+      }
+      return res;
+    } else if (ls_cs || lt_ct || ls_ct || lt_cs) {
+      Circular_arc_point_2 p,q,r;
+      
+      if(ls_cs){
+	p = l.target();
+	q = l.source();
+	r = c.target();
+      } else if(ls_ct){
+	p = l.target();
+	q = l.source();
+	r = c.source();
+      } else if(lt_cs){
+	p = l.source();
+	q = l.target();
+	r = c.target();
+      } else { // lt_ct
+	p = l.source();
+	q = l.target();
+	r = c.source();
+      }
+      
+      if( (CircularFunctors::compare_x<CK>(p,q) == EQUAL) || CircularFunctors::point_in_x_range<CK>(p,r,q)){ // Case 1
+	*res++ = make_object(std::make_pair(q,1u));
+	return res;
+      }
+       else if(c.on_upper_part()){ // Case 2
+	if(CircularFunctors::compare_x<CK>(r,q) == LARGER){
+	  if(CircularFunctors::orientation<CK>(q,r,p) == RIGHT_TURN   // Case 2
+	     || CircularFunctors::compare_y_to_right<CK>(l,c,q) == LARGER){ // Case 3a
+	    *res++ = make_object(std::make_pair(q,1u));
+	    return res;
+	  }
+	} else {
+	  if(CircularFunctors::orientation<CK>(r,q,p) == RIGHT_TURN  // Case 2
+	  || CircularFunctors::compare_y_to_left<CK>(l,c,q) == LARGER){ // Case 3c 
+	    *res++ = make_object(std::make_pair(q,1u));
+	    return res;
+	  }
+	}
+       } else { // c on lower part
+	if(CircularFunctors::compare_x<CK>(r,q) == LARGER){
+	  if (CircularFunctors::orientation<CK>(q,r,p) == LEFT_TURN // Case 2
+	    || CircularFunctors::compare_y_to_right<CK>(l,c,q) == SMALLER){  // Case 3b
+	    *res++ = make_object(std::make_pair(q,1u));
+	    return res;
+	  } 
+	} else {
+	  if(CircularFunctors::orientation<CK>(r,q,p) == LEFT_TURN
+	     || CircularFunctors::compare_y_to_left<CK>(l,c,q) == SMALLER){ // Case 3d 
+	    *res++ = make_object(std::make_pair(q,1u));
+	    return res;
+	  } 
+	} 
+       }
+       
+      typename CK::Linear_kernel::Bounded_side bs = CircularFunctors::bounded_side<CK>(c.supporting_circle(),p);
+      if(bs == ON_BOUNDED_SIDE){ 
+	*res++ = make_object(std::make_pair(q,1u));
+	return res;
+      } else { //Case 4b
+	solutions_container solutions;
+	CGAL::LinearFunctors::intersect_2<CK>( l.supporting_line(), c.supporting_circle(),
+					       std::back_inserter(solutions) );
+	
+	if(CircularFunctors::compare_x<CK>(r,q) == LARGER){
+	  *res++ = make_object(std::make_pair(q,1u));
+	  *res++ = solutions.back();
+	  return res;
+	} else {
+	  *res++ = solutions.front();
+	  *res++ = make_object(std::make_pair(q,1u));
+	  return res;
+	}
+	
+      } // Case 4b
+      
+      
+      std::cout << "oops we missed a case" << std::endl;
+      
+    }
+#endif // CGAL_CK_EXPLOIT_IDENTITY
     
     solutions_container solutions;
 
@@ -428,15 +635,14 @@ namespace CircularFunctors {
       CGAL::LinearFunctors::intersect_2<CK>
       ( l.supporting_line(), c.supporting_circle(),
 	std::back_inserter(solutions) );
-
+    
 #ifdef CGAL_INTERSECTION_MAP_FOR_SUPPORTING_CIRCLES
         Line_arc_2::template 
           put_intersection_circle_line< std::vector < CGAL::Object > >
           (c,l,solutions);
       }
 #endif
-    
-    
+
     for (typename solutions_container::iterator it = solutions.begin();
 	 it != solutions.end(); ++it) {
       const std::pair<typename CK::Circular_arc_point_2, unsigned>
@@ -449,6 +655,9 @@ namespace CircularFunctors {
     }
     return res;
   }
+
+
+
 
   /*template< class CK, class OutputIterator>
   OutputIterator
