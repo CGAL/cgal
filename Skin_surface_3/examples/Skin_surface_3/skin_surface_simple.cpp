b8b79e95005372c574a2d99be3d60b7c22a726ed
Nico Kruithof <Nico@cs.rug.nl>
2006-09-10 09:15:52 +0000

    programming the new interface

--- Skin_surface_3/examples/Skin_surface_3/skin_surface_simple.cpp
+++ Skin_surface_3/examples/Skin_surface_3/skin_surface_simple.cpp
@@ -4,13 +4,13 @@
 #include <list>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Weighted_point<K>                             Weighted_point;
 typedef K::Point_3                                          Bare_point;
+typedef CGAL::Weighted_point<Bare_point,K::RT>              Weighted_point;
 typedef CGAL::Polyhedron_3<K>                               Polyhedron;
 
 int main(int argc, char *argv[]) {
   std::list<Weighted_point> l;
-  RT                        shrinkfactor = 0.5;
+  double                    shrinkfactor = 0.5;
 
   l.push_front(Weighted_point(Bare_point( 1,-1,-1), 1.25));
   l.push_front(Weighted_point(Bare_point( 1, 1, 1), 1.25));
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
 
   Polyhedron p;
 
-  CGAL::make_skin_surface_mesh_3(p, l.begin(), l.end(), .5);
+  CGAL::make_skin_surface_mesh_3(p, l.begin(), l.end(), shrinkfactor);
 
   return 0;
 }
