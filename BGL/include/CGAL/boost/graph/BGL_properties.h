a46cbe0dc59d91a2e119ccdbd3958bd79083b98d
Fernando Cacciola <fernando.cacciola@geometryfactory.com>
2006-09-11 17:05:59 +0000

    Added some missing files

---
+++ BGL/include/CGAL/boost/graph/BGL_properties.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2006 Geometry Factory (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://fcacciola@scm.gforge.inria.fr/svn/cgal/trunk/Surface_mesh_simplification/include/CGAL/Polyhedron_BGL_properties.h $
+// $Id: Polyhedron_BGL_properties.h 33400 2006-08-17 17:29:20Z fcacciola $
+// 
+//
+// Author(s): Andreas Fabri <andreas.fabri@geometryfactory.com>, Fernando Cacciola <fernando.cacciola@gmail.com>
+
+#ifndef CGAL_BOOST_GRAPH_BGL_PROPERTIES_H
+#define CGAL_BOOST_GRAPH_BGL_PROPERTIES_H
+
+
+CGAL_BEGIN_NAMESPACE
+
+struct edge_is_border_t   {} ;
+struct vertex_is_border_t {} ;
+struct vertex_point_t     {} ;
+
+CGAL_END_NAMESPACE
+
+#endif // CGAL_BOOST_GRAPH_BGL_PROPERTIES_H
---
+++ BGL/include/CGAL/boost/graph/Extended_BGL.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2006 Geometry Factory (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://fcacciola@scm.gforge.inria.fr/svn/cgal/trunk/Surface_mesh_simplification/include/CGAL/Polyhedron_extended_BGL.h $
+// $Id: Polyhedron_extended_BGL.h 32155 2006-06-29 17:08:41Z fcacciola $
+// 
+//
+// Author(s) : Fernando Caccciola <fernando.cacciola@gmail.com>
+
+#ifndef CGAL_BOOST_GRAPH_EXTENDED_BGL_H
+#define CGAL_BOOST_GRAPH_EXTENDED_BGL_H
+
+CGAL_BEGIN_NAMESPACE
+
+template<class T> struct geometric_graph_traits ;
+template<class T> struct halfedge_graph_traits ;
+
+CGAL_END_NAMESPACE
+
+#endif // CGAL_BOOST_GRAPH_EXTENDED_BGL_H
