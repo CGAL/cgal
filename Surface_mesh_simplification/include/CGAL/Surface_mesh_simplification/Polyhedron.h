f4bc13742426e92dbc87af4e0f302cd1e8f3f7a9
Fernando Cacciola <fernando.cacciola@geometryfactory.com>
2006-09-13 00:13:03 +0000

    Some changed based on the current manual

---
+++ Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Polyhedron.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2005, 2006 Fernando Luis Cacciola Carballal. All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://fcacciola@scm.gforge.inria.fr/svn/cgal/trunk/Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification_edge_collapse.h $
+// $Id: Surface_mesh_simplification_edge_collapse.h 33697 2006-08-24 17:52:41Z fcacciola $
+//
+// Author(s)     : Fernando Cacciola <fernando_cacciola@ciudad.com.ar>
+//
+#ifndef CGAL_SURFACE_MESH_SIMPLIFICATION_POLYHEDRON_H
+#define CGAL_SURFACE_MESH_SIMPLIFICATION_POLYHEDRON_H
+
+#include <CGAL/Polyhedron_3.h>
+
+#include <CGAL/boost/graph/Polyhedron_BGL.h>
+#include <CGAL/boost/graph/Polyhedron_extended_BGL.h>
+#include <CGAL/boost/graph/Polyhedron_BGL_properties.h>
+
+
+#endif // CGAL_SURFACE_MESH_SIMPLIFICATION_POLYHEDRON_H
+// EOF //
+ 
+ 
