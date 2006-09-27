87510ffb9db0045b1515b896dc25a253e0d8f792
Naceur Meskini <Naceur.Meskini@sophia.inria.fr>
2006-09-27 14:10:59 +0000

    adding tests for natural_neighbors_2

---
+++ Interpolation/test/Interpolation/test_natural_neighbors_2.C
@@ -0,0 +1,38 @@
+// Copyright (c) 2003   INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://nmeskini@scm.gforge.inria.fr/svn/cgal/trunk/Interpolation/test/Interpolation/test_regular_neighbors_2.C $
+// $Id: test_regular_neighbors_2.C 28567 2006-02-16 14:30:13Z lsaboret $
+// 
+//
+// Author(s)     : Naceur MESKINI.
+
+#include <CGAL/basic.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+#include <CGAL/_test_natural_neighbors_2.C>
+
+
+struct K : CGAL::Exact_predicates_inexact_constructions_kernel {};
+typedef CGAL::Delaunay_triangulation_2<K>            Dt;
+
+
+int main()
+{
+  std::cout << "Testing NN_neighbors_2 " << std::endl;
+  std::cout << " with Exact_predicates_inexact_constructions_kernel: " << std::endl ;
+  _test_natural_neighbors_2( Dt() );
+
+  return 0;
+}
