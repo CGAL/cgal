19290c121c2b2b517f06d6b77ec35ed0ee4440bc
Luis Peñaranda <penarand@loria.fr>
2006-10-03 15:18:30 +0000

    Added Gbrs_polynomial_1 testfiles.

---
+++ Algebraic_kernel_GBRS/test/Gbrs_polynomial/Gbrs_polynomial_1.C
@@ -0,0 +1,42 @@
+#include <CGAL/Gbrs_algebraic_kernel.h>
+#include <gmp.h>
+#include <vector>
+
+typedef CGAL::GBRS_algebraic_kernel<CGAL::Gmpq>	AlgKernel;
+typedef AlgKernel::Coefficient			Coefficient;
+typedef AlgKernel::Algebraic_real_1		Algebraic;
+typedef AlgKernel::Polynomial_1			Polynomial;
+
+int main () {
+	AlgKernel ker;
+	// construct the polynomial x^3-2x
+	std::vector<Coefficient> coefs;
+	coefs.push_back (Coefficient (1));
+	coefs.push_back (Coefficient (0));
+	coefs.push_back (Coefficient (-2));
+	coefs.push_back (Coefficient (0));
+	coefs.push_back (Coefficient (0));
+	coefs.push_back (Coefficient (0));
+	Polynomial p = ker.construct_polynomial_1_object()
+		(coefs.begin (), coefs.end ());
+	std::cout << "p(x) = " << p << std::endl;
+	std::cout << "p(x) * 3 = " << p*3 << std::endl;
+	mpq_t x;
+	mpq_init (x);
+	mpq_set_ui (x, 3, 2);
+	std::cout << "p(x) * 3/2 = " << p*x << std::endl;
+	mpq_clear (x);
+
+	// find the roots of p
+	std::vector<Algebraic> roots (3);
+	std::vector<Algebraic>::iterator r_end =
+		ker.construct_solve_1_object ()
+			(p, roots.begin (), false);
+	std::vector<Algebraic>::iterator it;
+	std::cout << "roots:" << std::endl;
+	for (it = roots.begin (); it != r_end; ++it)
+		std::cout << *it << std::endl;
+
+	return 0;
+}
+
