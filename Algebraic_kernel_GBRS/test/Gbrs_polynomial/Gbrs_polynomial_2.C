ddd1d351f0487f85480be04a047d929475b6c5e2
Luis Peñaranda <penarand@loria.fr>
2006-10-10 09:23:19 +0000

    Added examples of bivariate polynomials.

---
+++ Algebraic_kernel_GBRS/test/Gbrs_polynomial/Gbrs_polynomial_2.C
@@ -0,0 +1,28 @@
+#include <CGAL/Gbrs_polynomial_2.h>
+
+typedef CGAL::Rational_polynomial_2	Polynomial;
+
+int main () {
+	Polynomial p (2, 1);	// d_x=2, d_y=1
+	Polynomial q (7, 8);
+	Polynomial zero (1, 4);	// zero(x) = 0
+
+	p.set_coef (2, 1, 2);	// 2*x^2*y
+	p.set_coef (1, 0, 3);	// 3*x
+	p.set_coef (0, 0, 7);	// 7
+	// p = 2*x^2*y + 3*x + 7
+	q = -p;
+	p.set_coef (0, 0, 5);
+	std::cout << p << "\n" << q << "\n" << zero << "\n";
+	std::cout << "p==q : " << (p==q) << "\np!=q : " << (p!=q) << "\n";
+
+	Polynomial r;
+	Polynomial s (1, 0);
+	s.set_coef (1, 0, 1);	// s = 1 * x^1
+	r = p*s;
+	std::cout << "r = " << r << "\n" << "s = " << s <<
+		"\np*=(s*2) = " << (p*=(s*2)) << std::endl;
+	
+	return 0;
+}
+
