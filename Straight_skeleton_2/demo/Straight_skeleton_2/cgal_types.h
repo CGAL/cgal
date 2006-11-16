16731b4d2e8a95836d2dd45a909cb3a4920c5d1c
Michael Hemmer <hemmer@mpi-inf.mpg.de>
2006-11-16 13:40:50 +0000

    rm use of Number_type_traits

--- Straight_skeleton_2/demo/Straight_skeleton_2/cgal_types.h
+++ Straight_skeleton_2/demo/Straight_skeleton_2/cgal_types.h
@@ -30,25 +30,7 @@
 
 #if USE_BIG_FLOAT
 
-#include <CORE/BigFloat.h>
-
-namespace CGAL
-{
-
-template<> struct Number_type_traits<CORE::BigFloat>
-{
-  typedef Tag_false Has_exact_ring_operations;
-  typedef Tag_false Has_exact_division;
-  typedef Tag_false Has_exact_sqrt;
-
-  typedef Tag_true Has_gcd;
-  typedef Tag_true Has_division;
-  typedef Tag_true Has_sqrt;
-};
-
-CORE::BigFloat sqrt( CORE::BigFloat const& n ) { return CORE::sqrt(n); }
-
-}
+#include <CGAL/CORE_BigFloat.h>
 
 #endif
 
