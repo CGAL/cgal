2bb6c10021505d15246bad383a6f65eb39360a7b
Michael Hemmer <hemmer@mpi-inf.mpg.de>
2006-11-16 15:49:29 +0000

    test of defaults

---
+++ Algebraic_foundations/test/Algebraic_foundations/Coercion_traits.C
@@ -0,0 +1,22 @@
+#include <CGAL/basic.h>
+#include <CGAL/Coercion_traits.h>
+#include <CGAL/Testsuite/assert.h>
+int main(){
+    {
+    typedef CGAL::Coercion_traits<int,int> CT;
+    BOOST_STATIC_ASSERT(
+            ( boost::is_same<CT::Are_implicit_interoperable,CGAL::Tag_true>::value));
+    BOOST_STATIC_ASSERT(
+            ( boost::is_same<CT::Are_explicit_interoperable,CGAL::Tag_true>::value));
+    CGAL_test_assert( 5 == CT::Cast()(5));
+    }   
+    {
+    typedef CGAL::Coercion_traits<CGAL::Tag_true,CGAL::Tag_false> CT;
+    BOOST_STATIC_ASSERT(
+            ( boost::is_same<CT::Are_implicit_interoperable,CGAL::Tag_false>::value));
+    BOOST_STATIC_ASSERT(
+            ( boost::is_same<CT::Are_explicit_interoperable,CGAL::Tag_false>::value));
+    BOOST_STATIC_ASSERT(
+            ( boost::is_same<CT::Cast,CGAL::Null_functor>::value));
+    }      
+}
---
+++ Algebraic_foundations/test/Algebraic_foundations/Real_embeddable_traits.C
@@ -0,0 +1,31 @@
+#include <CGAL/basic.h>
+#include <CGAL/Real_embeddable_traits.h>
+#include <CGAL/Testsuite/assert.h>
+
+
+#define CGAL_IS_RET_NULL_FUNCTOR(NAME)                                  \
+    {                                                                   \
+        typedef RET::NAME NAME;                                         \
+        BOOST_STATIC_ASSERT(                                            \
+                (::boost::is_same<CGAL::Null_functor,NAME>::value));    \
+    }      
+
+int main(){
+    typedef CGAL::Real_embeddable_traits<void> RET;
+    
+    typedef RET::Real_embeddable Real_embeddable;
+    BOOST_STATIC_ASSERT((::boost::is_same<void,Real_embeddable>::value)); 
+
+    typedef RET::Is_real_embeddable Is_real_embeddable;
+    BOOST_STATIC_ASSERT((::boost::is_same<CGAL::Tag_false,Is_real_embeddable>::value)); 
+    
+    CGAL_IS_RET_NULL_FUNCTOR(Abs);
+    CGAL_IS_RET_NULL_FUNCTOR(Sign);
+    CGAL_IS_RET_NULL_FUNCTOR(Is_finite);
+    CGAL_IS_RET_NULL_FUNCTOR(Is_positive);
+    CGAL_IS_RET_NULL_FUNCTOR(Is_negative);
+    CGAL_IS_RET_NULL_FUNCTOR(Is_zero);
+    CGAL_IS_RET_NULL_FUNCTOR(Compare);
+    CGAL_IS_RET_NULL_FUNCTOR(To_double);
+    CGAL_IS_RET_NULL_FUNCTOR(To_interval);
+}
