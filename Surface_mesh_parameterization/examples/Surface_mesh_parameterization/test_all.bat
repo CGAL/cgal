3ecd838242ba74a8136043ddc007faaba18d86da
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2006-12-04 08:36:32 +0000

    Log file moved to test/ subfolder

--- Surface_mesh_parameterization/examples/Surface_mesh_parameterization/test_all.bat
+++ Surface_mesh_parameterization/examples/Surface_mesh_parameterization/test_all.bat
@@ -3,5 +3,5 @@
 rem Double-clickable version of test_all.sh on Windows/Cygwin
 rem sh.exe must be in %PATH%
 
-sh .\test_all.sh | tee test_all.log
+sh ./test_all.sh 2>&1 | tee test/test_all.log
 pause
