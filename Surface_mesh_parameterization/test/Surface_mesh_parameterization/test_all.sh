b63c7336a1311cb29d3accf2d2c5114d9def584e
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2006-12-04 11:02:33 +0000

    Better error handling

--- Surface_mesh_parameterization/test/Surface_mesh_parameterization/test_all.sh
+++ Surface_mesh_parameterization/test/Surface_mesh_parameterization/test_all.sh
@@ -5,9 +5,12 @@
 # Find executable name (different on Windows and Unix)
 [ -f ./release/extensive_parameterization_test.exe ] && PARAM_APPLICATION="./release/extensive_parameterization_test.exe"
 [ -x ./extensive_parameterization_test ] && PARAM_APPLICATION="./extensive_parameterization_test"
+if [ -z "$PARAM_APPLICATION" ]; then
+    echo "Cannot find extensive_parameterization_test executable"
+    exit 1
+fi
 
-# echo on
+# run test (echo on)
 set -x
-
 $PARAM_APPLICATION data/*.off 2>&1
 
