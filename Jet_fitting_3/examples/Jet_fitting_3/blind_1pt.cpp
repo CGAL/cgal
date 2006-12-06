3f42713b40d016fce942d59fc8dfbb68e6cb4dfa
Marc Pouget <marc.pouget@loria.fr>
2006-12-06 10:43:37 +0000

    last before official?

--- Jet_fitting_3/examples/Jet_fitting_3/blind_1pt.cpp
+++ Jet_fitting_3/examples/Jet_fitting_3/blind_1pt.cpp
@@ -20,7 +20,8 @@ int main(int argc, char *argv[])
   //check command line  
   if (argc<4)
     {
-      std::cout << " Usage : " << argv[0] << " <inputPoints.txt> <d_fitting> <d_monge>" << std::endl;
+      std::cout << " Usage : " << argv[0] 
+                << " <inputPoints.txt> <d_fitting> <d_monge>" << std::endl;
       exit(-1);
     }
   //open the input file
