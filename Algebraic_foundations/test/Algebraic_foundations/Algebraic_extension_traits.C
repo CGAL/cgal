0bfee076bf3af2c21e8ab2275bbaaf38308462c8
Michael Hemmer <hemmer@mpi-inf.mpg.de>
2006-12-11 15:37:15 +0000

    added test Algebraic_extension_traits

---
+++ Algebraic_foundations/test/Algebraic_foundations/Algebraic_extension_traits.C
@@ -0,0 +1,32 @@
+#include <CGAL/basic.h>
+#include <CGAL/Algebraic_extension_traits.h>
+#include <CGAL/Testsuite/assert.h>
+
+int main(){
+    typedef CGAL::Algebraic_extension_traits<int> AET;
+    
+    typedef AET::Type Type;
+    BOOST_STATIC_ASSERT((::boost::is_same<int,Type>::value)); 
+
+    typedef AET::Is_extended Is_extended;
+    BOOST_STATIC_ASSERT((::boost::is_same<CGAL::Tag_false,Is_extended>::value));
+    
+    typedef AET::Normalization_factor Normalization_factor; 
+    {
+        typedef Normalization_factor::argument_type argument_type;
+        BOOST_STATIC_ASSERT((::boost::is_same<argument_type,int>::value));
+        typedef Normalization_factor::result_type result_type;
+        BOOST_STATIC_ASSERT((::boost::is_same<result_type,int>::value));
+        Normalization_factor nfac;
+        CGAL_test_assert(nfac(3)==1);
+    }
+    typedef AET::Denominator_for_algebraic_integers DFAI; 
+    {
+        typedef DFAI::argument_type argument_type;
+        BOOST_STATIC_ASSERT((::boost::is_same<argument_type,int>::value));
+        typedef DFAI::result_type result_type;
+        BOOST_STATIC_ASSERT((::boost::is_same<result_type,int>::value));
+        DFAI dfai;
+        CGAL_test_assert(dfai(3)==1);
+    }
+}
