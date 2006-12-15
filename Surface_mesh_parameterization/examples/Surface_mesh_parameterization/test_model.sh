5acf3212bf076056bf250c8cdf4158433b63f65e
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2006-12-15 10:16:52 +0000

    Undo revision 35475

--- Surface_mesh_parameterization/examples/Surface_mesh_parameterization/test_model.sh
+++ Surface_mesh_parameterization/examples/Surface_mesh_parameterization/test_model.sh
@@ -8,41 +8,25 @@ echo ""
 echo "                    ************************"
 echo ""
 
-./test.sh barycentric square taucs eps "$1"
+./test.sh barycentric square opennl eps "$1"
 echo "                                -"
-<<<<<<< .mine
-./test.sh floater circle taucs obj "$1"
-=======
 ./test.sh barycentric circle taucs obj "$1"
 echo "                                -"
 ./test.sh floater circle opennl obj "$1"
->>>>>>> .r35474
 echo "                                -"
-<<<<<<< .mine
-./test.sh conformal square taucs obj "$1"
-=======
 ./test.sh floater square taucs eps "$1"
 echo "                                -"
 ./test.sh conformal circle taucs obj "$1"
->>>>>>> .r35474
 echo "                                -"
-<<<<<<< .mine
-./test.sh authalic circle taucs obj "$1"
-=======
 ./test.sh conformal square opennl eps "$1"
 echo "                                -"
 ./test.sh authalic square taucs obj "$1"
->>>>>>> .r35474
 echo "                                -"
-<<<<<<< .mine
-./test.sh lscm 2pts taucs obj "$1"
-=======
 ./test.sh authalic circle opennl eps "$1"
 echo "                                -"
 ./test.sh lscm 2pts opennl obj "$1"
 echo "                                -"
 ./test.sh lscm 2pts taucs eps "$1"
->>>>>>> .r35474
 
 echo ""
 echo "                    ************************"
