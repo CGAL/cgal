26e2e082d3cc2216911d2069a9f7003ced8efe85
Daniel Russel <drussel@gmail.com>
2007-02-07 06:42:22 +0000

    collapsed files

--- Kinetic_data_structures/src/CGAL/KDS_Log.cpp
+++ Kinetic_data_structures/src/CGAL/KDS_Log.cpp
@@ -20,9 +20,25 @@
 
 
 #include <CGAL/Kinetic/internal/Log.h>
+#include <CGAL/Kinetic/internal/debug_counters.h>
+#include <iostream>
 
 CGAL_KINETIC_BEGIN_INTERNAL_NAMESPACE
 
 Logs kds_logs;
+
+namespace internal {
+  unsigned int function_degeneracies__=0;
+  unsigned int zero_certificates__=0;
+  unsigned int io_errors__=0;
+  unsigned int audit_failures__=0;
+
+  void write_debug_counters(std::ostream &out) {
+    out << "Degeneracies " << function_degeneracies__ << std::endl;
+    out << "Zero functions " << zero_certificates__ << std::endl;
+    if (io_errors__ != 0) out << "I/O errors " << io_errors__ << std::endl;
+    if (audit_failures__ != 0) out << "Audit failures " << audit_failures__ << std::endl;
+  }
+}
 
 CGAL_KINETIC_END_INTERNAL_NAMESPACE
