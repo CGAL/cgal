1c5cbb9cefe3aada2a8b647565cb44012ebd5e57
Peter Hachenberger <hachenberger@mpi-inf.mpg.de>
2007-02-16 11:03:06 +0000

    added header

--- Nef_3/include/CGAL/Nef_3/SNC_indexed_items.h
+++ Nef_3/include/CGAL/Nef_3/SNC_indexed_items.h
@@ -1,5 +1,24 @@
-#ifndef CGAL_NEF_SNC_ITEMS_H
-#define CGAL_NEF_SNC_ITEMS_H
+// Copyright (c) 1997-2002  Max-Planck-Institute Saarbruecken (Germany).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: 
+// $Id: 
+// 
+//
+// Author(s)     :     Peter Hachenberger  <hachenberger@mpi-sb.mpg.de>
+
+#ifndef CGAL_NEF_SNC_INDEXED_ITEMS_H
+#define CGAL_NEF_SNC_INDEXED_ITEMS_H
 #include <CGAL/Nef_3/Vertex.h>
 #include <CGAL/Nef_3/Halfedge.h>
 #include <CGAL/Nef_3/Halffacet.h>
@@ -92,4 +111,4 @@ class SNC_indexed_items {
 };
 
 CGAL_END_NAMESPACE
-#endif // CGAL_NEF_SNC_ITEMS_H
+#endif // CGAL_NEF_SNC_INDEXED_ITEMS_H
