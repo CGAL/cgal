c014cfa6d17b0f1ad1cfd17ab0c448954e0b1e10
Daniel Russel <drussel@gmail.com>
2007-02-22 07:31:26 +0000

    returning const iterator is a bit silly

--- PDB/include/CGAL/PDB/internal/tnt/tnt_vec.h
+++ PDB/include/CGAL/PDB/internal/tnt/tnt_vec.h
@@ -150,8 +150,8 @@ class Vector
 
     iterator begin() { return v_;}
     iterator end()   { return v_ + n_; }
-    const iterator begin() const { return v_;}
-    const iterator end() const  { return v_ + n_; }
+    iterator begin() const { return v_;}
+    iterator end() const  { return v_ + n_; }
 
     // destructor
 
