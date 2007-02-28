74da68e595cfd6d417d4bbca04c6944c3d9c7345
Sebastian Limbach <slimbach@mpi-inf.mpg.de>
2007-02-28 16:24:56 +0000

    Moved the functions to the INTERN_POLYNOMIAL namespace.

--- Polynomial/include/CGAL/Polynomial/univariate_polynomial_utils.h
+++ Polynomial/include/CGAL/Polynomial/univariate_polynomial_utils.h
@@ -21,53 +21,56 @@
 
 CGAL_BEGIN_NAMESPACE
 
-//! return an upper bound on the absolute value of all real roots of \c P.
-/*! The upper bound is a power of two. Only works for univariate polynomials.
- *  \pre \c NT must be \c RealComparable.
- *  \relates NiX::Polynomial
- */
-template <class NT>
-NT weak_upper_root_bound(const Polynomial<NT>& P) { 
-    // code comes from Kurt Mehlhorn
-    // see [Mignotte, 1992], p.144 for a proof
-    CGAL_precondition(Polynomial_traits_d<NT>::d == 0);
-    typename Real_embeddable_traits<NT>::Abs abs;
-    const int n = P.degree();
-    NT x(1); 
-    NT val;
-    for (;;) {
-        val = -abs(P[n]);
+namespace INTERN_POLYNOMIAL {
+    //! return an upper bound on the absolute value of all real roots of \c P.
+    /*! The upper bound is a power of two. Only works for univariate polynomials.
+     *  \pre \c NT must be \c RealComparable.
+     *  \relates NiX::Polynomial
+     */
+    template <class NT>
+    NT weak_upper_root_bound(const Polynomial<NT>& P) { 
+        // code comes from Kurt Mehlhorn
+        // see [Mignotte, 1992], p.144 for a proof
+        CGAL_precondition(Polynomial_traits_d<NT>::d == 0);
+        typename Real_embeddable_traits<NT>::Abs abs;
+        const int n = P.degree();
+        NT x(1); 
+        NT val;
+        for (;;) {
+            val = -abs(P[n]);
+            for (int i = n-1; i >= 0; i--) {
+                val = val*x + abs(P[i]);
+            }
+            if (val < NT(0)) return x;
+            x *= NT(2);
+        }
+    }
+    
+    //! return the number of sign variations in the coefficient sequence of \c P.
+    /*! This is the number of sign changes (+ to - or - to +) in the
+     *  coefficient sequence of the polynomial, ignoring zeroes.
+     *  Only meaningful for univariate polynomials.
+     *  \pre \c NT must be \c RealComparable.
+     *  \relates NiX::Polynomial
+     */
+    template <class NT>
+    int sign_variations(const Polynomial<NT>& P) { 
+        typename Real_embeddable_traits<NT>::Sign sign;
+        const int n = P.degree();
+        int variations = 0;
+        int old_sign = sign(P[n]); // never zero unless P is zero
         for (int i = n-1; i >= 0; i--) {
-            val = val*x + abs(P[i]);
+            int s = sign(P[i]);
+            if (s == 0) continue;
+            if (old_sign != s) {
+                old_sign = s;
+                variations++;
+            }
         }
-        if (val < NT(0)) return x;
-        x *= NT(2);
+        return variations;
     }
-}
 
-//! return the number of sign variations in the coefficient sequence of \c P.
-/*! This is the number of sign changes (+ to - or - to +) in the
- *  coefficient sequence of the polynomial, ignoring zeroes.
- *  Only meaningful for univariate polynomials.
- *  \pre \c NT must be \c RealComparable.
- *  \relates NiX::Polynomial
- */
-template <class NT>
-int sign_variations(const Polynomial<NT>& P) { 
-    typename Real_embeddable_traits<NT>::Sign sign;
-    const int n = P.degree();
-    int variations = 0;
-    int old_sign = sign(P[n]); // never zero unless P is zero
-    for (int i = n-1; i >= 0; i--) {
-        int s = sign(P[i]);
-        if (s == 0) continue;
-        if (old_sign != s) {
-            old_sign = s;
-            variations++;
-        }
-    }
-    return variations;
-}
+} // namespace INTERN_POLYNOMIAL
 
 CGAL_END_NAMESPACE
 
