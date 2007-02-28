44e4adcbb7762a890270376abe3fe1b32d2a2d61
Sebastian Limbach <slimbach@mpi-inf.mpg.de>
2007-02-28 16:09:53 +0000

    hgdelta_update function from Polynomial.h is now located in this separate file.

---
+++ Polynomial/include/CGAL/Polynomial/hgdelta_update.h
@@ -0,0 +1,45 @@
+// TODO: Add licence
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL:$
+// $Id: $
+// 
+//
+// Author(s)     :  
+//
+// ============================================================================
+
+// TODO: The comments are all original EXACUS comments and aren't adapted. So
+//         they may be wrong now.
+
+#ifndef CGAL_POLYNOMIAL_HGDELTA_UPDATE_H
+#define CGAL_POLYNOMIAL_HGDELTA_UPDATE_H
+
+CGAL_BEGIN_NAMESPACE
+
+// This subroutine has been retained here for use in both new files.
+namespace INTERN_POLYNOMIAL {
+    template <class NT> inline
+    void hgdelta_update(NT& h, const NT& g, int delta) {
+        typename Algebraic_structure_traits<NT>::Integral_division idiv;
+    
+        // compute h = h^(1-delta) * g^delta
+        switch (delta) {
+        case 0:
+            // h = h;
+            break;
+        case 1:
+            h = g;
+            break;
+        default:
+            h = idiv(ipower(g, delta), ipower(h, delta-1));
+            break;
+        }
+    }
+} // namespace INTERN_POLYNOMIAL
+
+CGAL_END_NAMESPACE
+
+#endif // CGAL_POLYNOMIAL_HGDELTA_UPDATE_H
