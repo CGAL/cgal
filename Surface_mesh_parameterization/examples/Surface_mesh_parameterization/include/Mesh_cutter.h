13ff38aeba6f731485fff9f3bbe79086ef51a8a2
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2007-03-08 15:12:26 +0000

    removed the accidental GPL header

--- Surface_mesh_parameterization/examples/Surface_mesh_parameterization/include/Mesh_cutter.h
+++ Surface_mesh_parameterization/examples/Surface_mesh_parameterization/include/Mesh_cutter.h
@@ -1,19 +1,3 @@
-/***************************************************************************
-
-    begin                : jan 02
-    copyright            : (C) 2002 by Pierre Alliez
-    email                : pierre.alliez@sophia.inria.fr
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
 #ifndef MESH_CUTTER_H
 #define MESH_CUTTER_H
 
--- Surface_mesh_parameterization/examples/Surface_mesh_parameterization/include/Polyhedron_ex.h
+++ Surface_mesh_parameterization/examples/Surface_mesh_parameterization/include/Polyhedron_ex.h
@@ -1,20 +1,3 @@
-/***************************************************************************
-Polyhedron_ex.h  -  description
-                             -------------------
-begin                : jan 2002
-copyright            : (C) 2002 by Pierre Alliez - INRIA
-email                : pierre.alliez@sophia.inria.fr
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
 #ifndef POLYHEDRON_EX_H_INCLUDED
 #define POLYHEDRON_EX_H_INCLUDED
 
