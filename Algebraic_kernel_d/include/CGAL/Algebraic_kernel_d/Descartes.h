24e3531d3efecde7ef1737e9c3deddd6e012cd42
Sebastian Limbach <slimbach@mpi-inf.mpg.de>
2007-03-15 13:05:31 +0000

    Using the new construct_binary.h file.

--- Algebraic_kernel_d/include/CGAL/Algebraic_kernel_d/Descartes.h
+++ Algebraic_kernel_d/include/CGAL/Algebraic_kernel_d/Descartes.h
@@ -34,76 +34,12 @@
 #include <CGAL/Polynomial.h> 
 
 #include <CGAL/Polynomial/univariate_polynomial_utils.h>
+#include <CGAL/Algebraic_kernel_d/construct_binary.h>
 
-#ifdef CGAL_USE_LEDA
-#include <CGAL/leda_integer.h>
-#include <CGAL/leda_rational.h>
-#endif
-#ifdef CGAL_USE_CORE
-#include <CGAL/CORE_BigInt.h>
-#include <CGAL/CORE_BigRat.h>
-#endif
 
 CGAL_BEGIN_NAMESPACE
 
 namespace CGALi {
-
-// TODO: Copied from EXACUS, not part of any concept!?
-
-#ifdef CGAL_USE_LEDA
-// Constructs 2^e from an integer e. Needed in Descartes
-inline void construct_binary(const ::leda::integer& e, ::leda::integer& x) {
-   typedef ::leda::integer  Integer;
-   x = Integer(1) << e.to_long();
-}
-
-// Constructs m*2^e from two integers m,e. Needed in Descartes
-inline void construct_binary(const ::leda::integer& m, const ::leda::integer& e,
-              ::leda::rational& x) {
-                
-                // TODO: Default implementation should use the construct_binary function above
-   typedef ::leda::integer  Integer;
-   typedef ::leda::rational Rational;
-
-   Integer den(1);
-   Integer num(m);
-   if(e>0) {
-      num <<= e.to_long();
-   }
-   else {
-      den <<= (-e).to_long();
-   }
-   x = Rational(num, den);
-}
-
-#endif // CGAL_USE_LEDA
-
-#ifdef CGAL_USE_CORE
-// Constructs 2^e from an integer e. Needed in Descartes
-inline void construct_binary(const ::CORE::BigInt& e, ::CORE::BigInt& x) {
-   typedef ::CORE::BigInt Integer;
-   x = Integer(1) << ::CORE::ulongValue(e);
-}
-
-// Constructs m*2^e from two integers m,e. Needed in Descardes
-inline void construct_binary(const ::CORE::BigInt& m, const ::CORE::BigInt& e, 
-              ::CORE::BigRat& x) {
-   typedef ::CORE::BigInt Integer;
-   typedef ::CORE::BigRat Rational;
-  
-   Integer den(1);
-   Integer num(m);
-   if(e>0) {
-      num <<= ::CORE::ulongValue(e);
-   }
-   else {
-      den <<= ::CORE::ulongValue(-e);
-   }
-   x = Rational(num, den);
-}
-
-#endif // CGAL_USE_CORE
-
 
 /*! \ingroup NiX_Algebraic_real
  *  \brief A model of concept RealRootIsolator. 
