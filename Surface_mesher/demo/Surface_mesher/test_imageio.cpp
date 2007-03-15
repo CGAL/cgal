2cd5da66e7d3e82d5bb87634a08d39d7f9839861
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2007-03-15 11:01:53 +0000

    New tool: test_imageio.cpp.   Without parameter: displays supported image file formats   With a parameter: displays informations about the file, if supported.
    (reminder: demo/Surface_mesher/ is not submitted).

---
+++ Surface_mesher/demo/Surface_mesher/test_imageio.cpp
@@ -0,0 +1,62 @@
+#include "../../../CGALimageIO/src/CGALimageIO/imageio/ImageIO.h"
+#include <iostream>
+
+#define SHOW(attribut) "\n  "#attribut": " << image->attribut
+#define SHOWENUM(enumitem) #enumitem"=" << enumitem
+
+int main(int argc, char** argv)
+{
+  ::printSupportedFileFormat();
+
+  if(argc > 1)
+  {
+    _image* image = ::_readImage(argv[1]);
+    if(image)
+    {
+      std::cerr 
+	<< "Image infos:"
+	<< "\ndimensions"
+	<< SHOW(xdim)
+	<< SHOW(ydim)
+	<< SHOW(zdim)
+	<< SHOW(vdim)
+	<< "\nvoxel size"
+	<< SHOW(vx)
+	<< SHOW(vy)
+	<< SHOW(vz)
+	<<"\nrotation vector"
+	<< SHOW(rx)
+	<< SHOW(ry)
+	<< SHOW(rz)
+	<<"\nimage center"
+	<< SHOW(cx)
+	<< SHOW(cy)
+	<< SHOW(cz)
+	<< "\nword size (in bytes)"
+	<< SHOW(wdim)
+	<< "\nimage format"
+	<< "\n  " << image->imageFormat->realName 
+	<< " (extension list: " << image->imageFormat->fileExtension << ")" 
+	<< "\nvectors interlaced or not (internal code)"
+	<< SHOW(vectMode)
+	<< "\nword kind ("
+	<< SHOWENUM(WK_FIXED) << ", "
+	<< SHOWENUM(WK_FLOAT) << ", "
+	<< SHOWENUM(WK_UNKNOWN)
+	<< ")"
+	<< SHOW(wordKind)
+	<< "\nword sign ("
+	<< SHOWENUM(SGN_SIGNED) << ", "
+	<< SHOWENUM(SGN_UNSIGNED) << ", "
+	<< SHOWENUM(SGN_UNKNOWN)
+	<< ")"
+	<< SHOW(sign)
+	<< "\n";
+      
+      ::_freeImage(image);
+    }
+
+    
+  } 
+    
+}
