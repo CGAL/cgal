7d0dd3e7a1ebaf772c9ff2c3b0c7cc3a97edba67
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2007-03-19 07:23:20 +0000

    remoded unsused parameters

--- Boolean_set_operations_2/include/CGAL/Boolean_set_operations_2/Gps_simplifier_traits.h
+++ Boolean_set_operations_2/include/CGAL/Boolean_set_operations_2/Gps_simplifier_traits.h
@@ -185,7 +185,7 @@ public:
     Intersect_2 (const Base_Intersect_2& base,
                  const Base_Compare_endpoints_xy_2& base_cmp_endpoints,
                  const Base_Compare_xy_2& base_cmp_xy,
-                 const Base_Construct_min_vertex_2& ctr_min_v,
+                 const Base_Construct_min_vertex_2& ,
                  const Self*  tr) : 
       m_base(base),
       m_base_cmp_endpoints(base_cmp_endpoints),
