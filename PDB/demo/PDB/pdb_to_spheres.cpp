42f6e903682d528d02235b120181140dd9ba7e12
Joachim Reichel <joachim.reichel@gmx.de>
2007-03-20 19:41:46 +0000

    replace reference to COPYING.LIB by LICENSE.LGPL

--- PDB/demo/PDB/pdb_to_spheres.cpp
+++ PDB/demo/PDB/pdb_to_spheres.cpp
@@ -14,7 +14,7 @@ or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with the DSR PDB Library; see the file COPYING.LIB.  If not, write to
+along with the DSR PDB Library; see the file LICENSE.LGPL.  If not, write to
 the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA 02110-1301, USA. */
 
