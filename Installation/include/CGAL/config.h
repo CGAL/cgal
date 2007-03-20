b1d04562014f5959da651158aed94901c9a21aff
Marc Glisse <Marc.Glisse@inria.fr>
2007-03-20 16:15:56 +0000

    I added the check on __i386 for sunCC but it is supposed to define __i386__ as well (though it is buggy right now) so remove.

--- Installation/include/CGAL/config.h
+++ Installation/include/CGAL/config.h
@@ -135,8 +135,7 @@ namespace CGAL {
    || defined(__x86_64) || defined(__x86_64__) \
    || defined(__ia64) || defined(__ia64__) \
    || defined(_M_IX86) || defined(_M_IA64) \
-   || defined(_M_ALPHA) || defined(_WIN64) \
-   || defined(__i386)
+   || defined(_M_ALPHA) || defined(_WIN64)
 #  define CGAL_LITTLE_ENDIAN
 #else
 #  error Unknown endianness
