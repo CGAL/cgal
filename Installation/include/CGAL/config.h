f038ffdd56cdbc9a4e77ee3bf304734626905653
Joachim Reichel <joachim.reichel@gmx.de>
2007-03-20 19:09:33 +0000

    drop support for external CORE

--- Installation/include/CGAL/config.h
+++ Installation/include/CGAL/config.h
@@ -196,6 +196,13 @@ using std::max;
 #endif
 
 //-------------------------------------------------------------------//
+// Is CORE usable ?
+#ifdef CGAL_USE_GMP
+#  define CGAL_USE_CORE CGAL_USE_GMP
+#endif
+
+
+//-------------------------------------------------------------------//
 // Is Geomview usable ?
 #if !defined(__BORLANDC__) && !defined(_MSC_VER) && !defined(__MINGW32__)
 #  define CGAL_USE_GEOMVIEW
