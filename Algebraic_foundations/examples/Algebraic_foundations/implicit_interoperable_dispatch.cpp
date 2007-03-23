ac960f4d4210ce67a6384943189db02e23aa4e54
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2007-03-23 14:33:09 +0000

    Rename binary_function to binary_func as it clashes with something else with pgcc.

--- Algebraic_foundations/examples/Algebraic_foundations/implicit_interoperable_dispatch.cpp
+++ Algebraic_foundations/examples/Algebraic_foundations/implicit_interoperable_dispatch.cpp
@@ -25,7 +25,7 @@ binary_function_(const A& a , const B& b, CGAL::Tag_true){
 // this function selects the correct implementation
 template <typename A, typename B>
 typename CGAL::Coercion_traits<A,B>::Type
-binary_function(const A& a , const B& b){
+binary_func(const A& a , const B& b){
     typedef CGAL::Coercion_traits<A,B> CT;
     typedef typename CT::Are_implicit_interoperable Are_implicit_interoperable;
     return binary_function_(a,b,Are_implicit_interoperable());
@@ -35,12 +35,12 @@ int main(){
     CGAL::set_pretty_mode(std::cout);
 
     // Function call for ImplicitInteroperable types
-    std::cout<< binary_function(double(3), int(5)) << std::endl;
+    std::cout<< binary_func(double(3), int(5)) << std::endl;
 
     // Function call for ExplicitInteroperable types
     CGAL::Quotient<int>           rational(1,3);    // == 1/3
     CGAL::Sqrt_extension<int,int> extension(1,2,3); // == 1+2*sqrt(3)
-    CGAL::Sqrt_extension<CGAL::Quotient<int>,int> result = binary_function(rational, extension);
+    CGAL::Sqrt_extension<CGAL::Quotient<int>,int> result = binary_func(rational, extension);
     std::cout<< result << std::endl;
 
     return 0;
