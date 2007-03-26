3fc1f76a5673a66c5e6e2f5ea262b630c9112d85
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2007-03-26 08:03:27 +0000

    removed unused parameters

--- Surface_mesh_simplification/include/CGAL/Modifiable_priority_queue.h
+++ Surface_mesh_simplification/include/CGAL/Modifiable_priority_queue.h
@@ -51,7 +51,7 @@ public:
   
   handle update ( value_type const& v, handle h ) { mHeap.update(v); return h ; }
   
-  handle erase ( value_type const& v, handle h ) { mHeap.remove(v); return null_handle() ; }
+  handle erase ( value_type const& v, handle  ) { mHeap.remove(v); return null_handle() ; }
 
   value_type top() const { return mHeap.top() ; }
   
