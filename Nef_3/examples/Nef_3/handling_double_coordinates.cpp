02530f52c8c614d3b73815d6896e05e3e153d107
Peter Hachenberger <hachenberger@mpi-inf.mpg.de>
2007-03-26 17:37:36 +0000

    exchanged kernel

--- Nef_3/examples/Nef_3/handling_double_coordinates.cpp
+++ Nef_3/examples/Nef_3/handling_double_coordinates.cpp
@@ -1,16 +1,11 @@
-// file: examples/Nef_3/handling_double_coordinates.C
-
-#include <CGAL/Gmpq.h>
-#include <CGAL/Lazy_kernel.h>
-#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/IO/Polyhedron_iostream.h>
 #include <CGAL/Nef_polyhedron_3.h>
 #include <CGAL/Nef_3/SNC_indexed_items.h>
 #include <CGAL/IO/Nef_polyhedron_iostream_3.h>
 
-typedef CGAL::Gmpq  NT;
-typedef CGAL::Lazy_kernel<CGAL::Simple_cartesian<NT> > Kernel;
+typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
 typedef CGAL::Polyhedron_3<Kernel>  Polyhedron;
 typedef CGAL::SNC_indexed_items Items;
 typedef CGAL::Nef_polyhedron_3<Kernel, Items>  Nef_polyhedron;
