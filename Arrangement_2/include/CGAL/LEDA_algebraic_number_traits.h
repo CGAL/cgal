1042b9ba63e110eadef48c9cb54f70fe74763b06
Efi Fogel <efif@post.tau.ac.il>
2007-04-03 18:28:48 +0000

    Added the proper SVN keywords

--- Arrangement_2/include/CGAL/LEDA_algebraic_number_traits.h
+++ Arrangement_2/include/CGAL/LEDA_algebraic_number_traits.h
@@ -11,6 +11,8 @@
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
+// $URL$
+// $Id$
 //
 // Author(s)     : Marcel Janer <marcel_janer@terra.es> 
 //                 Ron Wein <wein@post.tau.ac.il>
