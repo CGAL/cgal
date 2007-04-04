9287f07dc3f9ac740c3fa739c36aa9ae5d7a4840
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2007-04-04 14:40:31 +0000

    Undo previous change as it is buggy

--- Intersections_2/include/CGAL/Segment_2_Segment_2_intersection.h
+++ Intersections_2/include/CGAL/Segment_2_Segment_2_intersection.h
@@ -464,7 +464,7 @@ Segment_2_Segment_2_pair<K>::intersection(typename K::Segment_2 &result) const
 
 template <class K>
 Object
-intersectionSS(const typename CGAL_WRAP(K)::Segment_2 &seg1, 
+intersection(const typename CGAL_WRAP(K)::Segment_2 &seg1, 
 	     const typename CGAL_WRAP(K)::Segment_2 &seg2,
 	     const K&)
 {
