3bbdc0071d1627091d4fa9ac91e97a4ccd905316
Sebastian Limbach <slimbach@mpi-inf.mpg.de>
2007-04-04 14:32:03 +0000

    * Functors implemented. * Default template parameters added to replace #define directives.

--- Algebraic_kernel_d/include/CGAL/Algebraic_kernel_d_1.h
+++ Algebraic_kernel_d/include/CGAL/Algebraic_kernel_d_1.h
@@ -7,7 +7,8 @@
 // $Id: $
 // 
 //
-// Author(s)     :  
+// Author(s)     : Michael Hemmer    <hemmer@mpi-inf.mpg.de>
+//                 Sebastian Limbach <slimbach@mpi-inf.mpg.de>
 //
 // ============================================================================
 
@@ -19,6 +20,8 @@
 
 #include <CGAL/Algebraic_kernel_d/Algebraic_real_pure.h>
 #include <CGAL/Algebraic_kernel_d/Descartes.h>
+#include <CGAL/Algebraic_kernel_d/Real_roots.h>
+#include <CGAL/Algebraic_kernel_d/refine_zero_against.h>
 #include <CGAL/Polynomial/ipower.h>
 
 CGAL_BEGIN_NAMESPACE
@@ -32,9 +35,10 @@ namespace CGALi {
             typedef Isolator_                   Isolator;
             
             typedef typename Algebraic_real_1::Coefficient      Coefficient;
-            typedef Polynomial< Coefficient > Polynomial_1;
+            typedef Polynomial< Coefficient >                   Polynomial_1;
             
             class Algebraic_real_traits {
+              public:
                 typedef Algebraic_real_1                      Type;
                 typedef typename Algebraic_real_1::Rational   Boundary;
                 
@@ -98,28 +102,129 @@ namespace CGALi {
                     }
                 };                
             }; // class Algebraic_real_traits
+            
+            // Functors of Algebraic_kernel_d_1
+            struct Solve_1 {
+                template< class OutputIterator >
+                OutputIterator operator()( const Polynomial_1& p,
+                                           OutputIterator res,
+                                           bool known_to_be_square_free = false ) const {
+                    Polynomial_1 poly = (known_to_be_square_free) ? p :
+                             typename Polynomial_traits_d< Polynomial_1 >::Make_square_free()(p);
+                             
+                    CGALi::Real_roots< Algebraic_real_1, Isolator > real_roots;
+                    
+                    real_roots( poly, res );
+                    return res;
+                }
+                
+                template< class OutputIteratorRoots, class OutputIteratorMults >
+                std::pair< OutputIteratorRoots, OutputIteratorMults >
+                operator()( const Polynomial_1& p, OutputIteratorRoots roots,
+                            OutputIteratorMults mults ) const {
+                    CGALi::Real_roots< Algebraic_real_1, Isolator > real_roots;
+                    
+                    real_roots( p, roots, mults );
+                    
+                    return std::pair< OutputIteratorRoots, OutputIteratorMults >(
+                        roots, mults );                
+                }                
+            };
+            
+            struct Sign_at_1 
+                : public Binary_function< Polynomial_1, Algebraic_real_1, CGAL::Sign > {
+                    CGAL::Sign operator()( const Polynomial_1& p, const Algebraic_real_1& ar ) const {
+                        typedef typename Algebraic_real_traits::Boundary Boundary;
+                        Boundary low = ar.low();
+                        Boundary high = ar.high();
+                        if( low == high ) {
+                            return p.sign_at( low );
+                        }
+                        
+                        if( strong_refine_zero_against( low, high, ar.polynomial(), p ) ) {
+                            return CGAL::ZERO;
+                        } else {                                
+                            CGAL_postcondition( p.sign_at(low) == p.sign_at(high) );                                                
+
+                            return p.sign_at( low );
+                        }                        
+                    }
+            };                
+            
+            struct Is_square_free_1 
+                : public Unary_function< Polynomial_1, bool > {
+                    bool operator()( const Polynomial_1& p ) const {
+                        return INTERN_POLYNOMIAL::is_square_free(p);
+                    }
+            };
+            
+            struct Is_coprime_1
+                : public Binary_function< Polynomial_1, Polynomial_1, bool > {
+                    bool operator()( const Polynomial_1& p1, const Polynomial_1& p2 ) const {
+                        typename CGAL::Polynomial_traits_d< Polynomial_1 >::Total_degree total_degree;
+                        
+                        // TODO: Is GCD already filtered? 
+                        return( total_degree( gcd_utcf( p1, p2 ) ) == 0 );                        
+                    } 
+            }; 
+            
+            struct Make_square_free_1
+                : public Unary_function< Polynomial_1, Polynomial_1 > {
+                    Polynomial_1 operator()( const Polynomial_1& p ) const {
+                        return typename CGAL::Polynomial_traits_d< Polynomial_1 >::Make_square_free()( p );
+                    }
+            };
+            
+            struct Make_coprime_1 {
+                typedef bool         result_type;
+                typedef Polynomial_1 first_argument_type;
+                typedef Polynomial_1 second_argument_type;
+                typedef Polynomial_1 third_argument_type;
+                typedef Polynomial_1 fourth_argument_type;
+                typedef Polynomial_1 fifth_argument_type;
+                
+                bool operator()( const Polynomial_1& p1,
+                                 const Polynomial_1& p2,
+                                 Polynomial_1& g, // ggT utcf 
+                                 Polynomial_1& q1, // Rest utcf
+                                 Polynomial_1& q2 ) const {
+                    g = typename CGAL::Polynomial_traits_d< Polynomial_1 >::Gcd_up_to_constant_factor()( p1, p2 );
+                    q1 = p1 / g;
+                    q2 = p2 / g;
+                    return Is_coprime_1()( q1, q2 );
+                }                 
+            };
+            
+            struct Square_free_factorization_1 {
+                typedef int result_type;
+                
+                template< class OutputIterator1, class OutputIterator2 >
+                int operator()( const Polynomial_1& p, OutputIterator1 it1,
+                                OutputIterator2 it2 ) const {
+                    return typename CGAL::Polynomial_traits_d< Polynomial_1 >::Square_free_factorization_up_to_constant_factor()( p, it1, it2 );
+                } 
+            };
+            
     };
 } // namespace CGALi
 
-#define CGAL_AK_BOUNDARY    Rational
-#define CGAL_AK_REPCLASS    CGALi::Algebraic_real_rep
-#define CGAL_AK_ISOLATOR    CGALi::Descartes
 
-
-template< class Coefficient >
+template< class Coefficient,
+          class Boundary = typename CGALi::Get_arithmetic_kernel< Coefficient >::Arithmetic_kernel::Rational,
+          class RepClass = CGALi::Algebraic_real_rep< Coefficient, Boundary >,
+          class Isolator = CGALi::Descartes< typename CGAL::Polynomial< Coefficient >, Boundary > >
 class Algebraic_kernel_d_1    
     : public CGALi::Algebraic_kernel_d_1_base< 
 
     // Template argument #1 (AlgebraicReal1)        
         CGALi::Algebraic_real_pure< 
             Coefficient, 
-            typename CGALi::Get_arithmetic_kernel< Coefficient >::CGAL_AK_BOUNDARY,
+            Boundary,
             ::CGAL::Handle_policy_no_union,     
-            CGAL_AK_REPCLASS< Coefficient, typename CGALi::Get_arithmetic_kernel< Coefficient >::CGAL_AK_BOUNDARY > >,
+            RepClass >,
         
     // Template argument #2 (Isolator_)
-        CGAL_AK_ISOLATOR< typename CGAL::Polynomial< Coefficient >,
-                          typename CGALi::Get_arithmetic_kernel< Coefficient >::CGAL_AK_BOUNDARY > >
+        Isolator >
             
      {};
 
