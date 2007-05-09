c77455015c2d70df0637338abacf3ba7e14b104a
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2007-05-09 08:24:04 +0000

    Fix of taucs_system_memory_size() and taucs_available_memory_size() in TAUCS for CGAL 3.3 alpha

--- Surface_mesh_parameterization/include/CGAL/Taucs_fix.h
+++ Surface_mesh_parameterization/include/CGAL/Taucs_fix.h
@@ -64,39 +64,156 @@
 extern "C"
 {
 
-/********************************************************************/
-/*                                                                  */
-/* FIX OF TAUCS BUGGY FUNCTION taucs_available_memory_size()        */
-/*                                                                  */
-/********************************************************************/
 
-/* taucs_available_memory_size/cgal_taucs_available_memory_size     */
-/*   returns size of memory available for allocation                */
-inline double cgal_taucs_available_memory_size()
-{
-    double m;       /* size of memory available for allocation */
+/********************************************************************/
+/* FIX OF taucs_system_memory_size()                                */
+/*   returns size of memory reported by the operating system        */
+/*   should not normally be called by the user (call _avail_)       */
+/* (see taucs_memory.c)                                             */
+/********************************************************************/
 
 #ifdef OSTYPE_linux
-    /* taucs_available_memory_size() is buggy on Linux 2.6 */
-    /* It returns only 1% of the actual memory             */
+
+/* Redirect call to avoid link error */
+#define taucs_system_memory_size cgal_taucs_system_memory_size
+
+double taucs_system_memory_size()
+{
+  /* LS 2006: The original code below is buggy on Linux 2.6 */
+  /*          (because /proc/meminfo format changed)        */
+#if 0
+  FILE* f;
+  double m;
+  double m_sc;
+
+  m_sc  = (double) sysconf(_SC_PAGESIZE);
+  m_sc *= (double) sysconf(_SC_PHYS_PAGES);
+
+  /* total memory is the first number in /proc/meminfo */
+
+  f = fopen("/proc/meminfo","r");
+  if (f==NULL) return m_sc;
+  if (fscanf(f,"%*[a-zA-Z :\n\r]%lf",&m) != 1) return m_sc;
+
+  if (m != m_sc) {
+    taucs_printf("Warning: /proc/meminfo reports %lfMB of memory while\n",
+	       m/1048576.0);
+    taucs_printf("         sysconf       reports %lfMB of memory\n",
+	       m_sc/1048576.0);
+  }
+
+  return m;
+#endif
+  /* LS 2006: fix */
+  double m_sc;
+  m_sc  = (double) sysconf(_SC_PAGESIZE);
+  m_sc *= (double) sysconf(_SC_PHYS_PAGES);
+taucs_printf((char*)"taucs_system_memory_size returns %lfMB\n", m_sc/1048576.0);
+  return m_sc;
+}
+#endif
+
+/* LS 2007: Cygwin implementation */
+#ifdef OSTYPE_cygwin
+
+/* Redirect call to avoid link error */
+#define taucs_system_memory_size cgal_taucs_system_memory_size
+
+double taucs_system_memory_size()
+{
+  double m_sc;
+  m_sc  = (double) 4096; // page size
+  m_sc *= (double) sysconf(_SC_PHYS_PAGES);
+taucs_printf((char*)"taucs_system_memory_size returns %lfMB\n", m_sc/1048576.0);
+  return m_sc;
+}
+#endif
+
+
+
+/********************************************************************/
+/* FIX OF taucs_available_memory_size()                             */
+/*   returns size of memory available for allocation                */
+/* (see taucs_memory.c)                                             */
+/********************************************************************/
+
+/* Redirect calls to avoid link error */
+#define taucs_available_memory_size cgal_taucs_available_memory_size
+#define taucs_malloc                malloc
+#define taucs_free                  free
+
+double taucs_available_memory_size() 
+{
+/* LS 2007: The original code below creates an infinite loop on Linux     */
+/*          (optimistic memory allocation => malloc() never returns NULL) */
+#ifndef OSTYPE_linux
+
+  double m_sys;
+  double m,m_low,m_high,m_tol;
+  char*  p;
+
+  m_sys = taucs_system_memory_size();
+  
+  /* malloc test */
+
+  m = 1048576.0;
+
+  while ( (p=(char*) taucs_malloc( (size_t) (m*2.0) )) != NULL ) {
+    taucs_free(p);
+    m = m*2.0;
+  }
+
+  m_low  = m;
+  m_high = 2.0*m;
+  m_tol  = m / 128.0;
+
+  while ( m_high - m_low > m_tol ) {
+    m = m_low + ( (m_high-m_low)/2.0 );
+    taucs_printf("taucs_avail_memory_size: [%.0lf %.0lf %.0lf]\n",
+	       m_low  / 1048576.0,
+	       m      / 1048576.0,
+	       m_high / 1048576.0);
+    if ( (p=(char*) taucs_malloc( (size_t) m )) != NULL ) 
+      m_low = m;
+    else 
+      m_high = m;
+    taucs_free(p);
+  }
+
+  m = m_low;
+
+  taucs_printf("taucs_avail_memory_size: malloc test=%.0lf MB sys test=%.0lf MB\n",
+	     m / 1048576.0,
+	     m_sys / 1048576.0
+	     );
+
+  /* if m_sys is meaningful, then we limit m by 0.75*m_sys */
+
+  if (m_sys > 0) {
+    m_sys = floor(0.75 * m_sys); 
+    if (m_sys < m) m = m_sys;
+  }
+
+  return m;
+
+#else // LS 2007: fix on Linux
+
     double m_sys;   /* size of physical memory */
+    double m;       /* size of memory available for allocation */
+
     m_sys  = (double) sysconf(_SC_PAGESIZE);
     m_sys *= (double) sysconf(_SC_PHYS_PAGES);
 
     /* we limit m by 0.75*m_sys */
     m = floor(0.75 * m_sys);
-#else
-    m = taucs_available_memory_size();
-#endif
 
-    taucs_printf((char*)"cgal_taucs_available_memory_size returns %lfMB\n", m/1048576.0);
+    taucs_printf((char*)"taucs_available_memory_size returns %lfMB\n", m/1048576.0);
 
     return m;
+    
+#endif
 }
 
-/* Redirect calls to cgal_taucs_available_memory_size()             */
-/* to taucs_available_memory_size()                                 */
-#define taucs_available_memory_size cgal_taucs_available_memory_size
 
 } // extern "C"
 
