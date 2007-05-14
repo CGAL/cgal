3f4e168e0845fa2c7066577aac26b9647116f113
Ankit Gupta <ankitgupta00@gmail.com>
2007-05-14 08:27:58 +0000

    A    Principal_component_analysis/linear_least_squares_fitting_segments.cpp A    Principal_component_analysis/linear_least_squares_fitting_triangles.cpp

---
+++ Principal_component_analysis/examples/Principal_component_analysis/linear_least_squares_fitting_segments.cpp
@@ -0,0 +1,24 @@
+// Example program for the linear_least_square_fitting function
+
+#include <CGAL/Cartesian.h>
+#include <CGAL/linear_least_squares_fitting_2.h>
+
+typedef double               FT;
+typedef CGAL::Cartesian<FT>  K;
+typedef K::Line_2            Line_2;
+typedef K::Point_2           Point_2;
+typedef K::Segment_2         Segment_2;
+
+int main()
+{
+  std::list<Segment_2> segments;
+  segments.push_back(Segment_2(Point_2(0.0,0.0),Point_2(0.0,1.0)));
+  segments.push_back(Segment_2(Point_2(0.0,2.0),Point_2(0.0,10.0)));
+
+  Line_2 line;
+  FT i = linear_least_squares_fitting_2(segments.begin(),segments.end(),line);
+
+  std::cout<<"accuracy: "<<i<<std::endl;
+  std::cout<<line<<std::endl;
+  return 0;
+}
---
+++ Principal_component_analysis/examples/Principal_component_analysis/linear_least_squares_fitting_triangles.cpp
@@ -0,0 +1,24 @@
+// Example program for the linear_least_square_fitting function
+
+#include <CGAL/Cartesian.h>
+#include <CGAL/linear_least_squares_fitting_2.h>
+
+typedef double               FT;
+typedef CGAL::Cartesian<FT>  K;
+typedef K::Line_2            Line_2;
+typedef K::Point_2           Point_2;
+typedef K::Triangle_2        Triangle_2;
+
+int main()
+{
+  std::list<Triangle_2> triangles;
+  triangles.push_back(Triangle_2(Point_2(1.0,0.0),Point_2(0.0,1.0),Point_2(1.0,1.0)));
+  triangles.push_back(Triangle_2(Point_2(-1.0,0.0),Point_2(0.0,1.0),Point_2(-1.0,1.0)));
+
+  Line_2 line;
+  FT i = linear_least_squares_fitting_2(triangles.begin(),triangles.end(),line);
+
+  std::cout<<"accuracy: "<<i<<std::endl;
+  std::cout<<line<<std::endl;
+  return 0;
+}
