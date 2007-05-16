1aa65cf8dedded3de575fc104107aea3dcc0d4b7
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2007-05-16 15:13:52 +0000

    Path to Cygwin is hard-coded to avoid bugs in batch files (MS-DOS prompt was able to call Cygwin shell commands on my machine)

--- Surface_mesh_parameterization/test/Surface_mesh_parameterization/test_all.bat
+++ Surface_mesh_parameterization/test/Surface_mesh_parameterization/test_all.bat
@@ -1,7 +1,9 @@
 @echo off
 
 rem Double-clickable version of test_all.sh on Windows/Cygwin
-rem sh.exe must be in %PATH%
+
+rem Path to Cygwin is hard-coded
+PATH=%PATH%;c:\Applis\cygwin\bin
 
 sh .\test_all.sh | tee test_all.log
 pause
