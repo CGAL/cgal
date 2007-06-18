350de24ea4559dc669f5dc92e8ab12a3766e476b
Sebastian Limbach <slimbach@mpi-inf.mpg.de>
2007-06-18 11:06:11 +0000

    Some member functions copied from Algebraic_real_rep to avoid calling of wrong refine functions. virtual sign_of_polynomial_at function added.

--- Algebraic_kernel_d/include/CGAL/Algebraic_kernel_d/Algebraic_real_rep_bfi.h
+++ Algebraic_kernel_d/include/CGAL/Algebraic_kernel_d/Algebraic_real_rep_bfi.h
@@ -35,7 +35,7 @@
 
 
 CGAL_BEGIN_NAMESPACE
-
+;
 namespace CGALi {
 
 template <class NT> class Get_arithmetic_kernel;
@@ -74,7 +74,7 @@ class Algebraic_real_rep_bfi
 
     mutable std::vector<BFI>          polynomial_approx; 
     mutable long                      current_prec;
-
+        
 private:
     template <class NT, class OI> 
     inline 
@@ -112,8 +112,148 @@ private:
         } 
     }
 
+    typedef CGAL::Sign                              TRI_BOOL;
 
 public:
+    const Field&           low()           const {return this->low_;}
+    const Field&           high()          const {return this->high_;}
+    const Poly&            polynomial()    const {return this->polynomial_;}
+    const TRI_BOOL&        sign_at_low()   const {return this->sign_at_low_;}
+    
+    template <class NTX>
+    void strong_refine(const NTX& m) const{
+        if(is_rational()) return;
+
+        if( NTX(low()) <= m && m <= NTX(high()) ){
+            CGAL_precondition(polynomial().sign_at(m)!=CGAL::ZERO);
+            refine();
+            while( NTX(low()) <= m && m <= NTX(high())) refine();
+        }
+    }
+
+    bool                   is_rational()   const {return this->is_rational_;}
+    const Field&           rational()      const {
+        CGAL_precondition(is_rational());
+        CGAL_precondition(low()==high());
+        CGAL_precondition(polynomial().sign_at(low())==CGAL::ZERO);
+        return this->low_;
+    }
+
+    CGAL::Comparison_result
+    compare(const Field& y, bool are_distinct = false) const {  
+        if(is_rational()) return CGAL::compare(rational(),y);
+        if(y <= low()) {
+            strong_refine(y);
+            return CGAL::LARGER;
+        }
+        if(high() <= y) { 
+            strong_refine(y);
+            return CGAL::SMALLER;
+        }
+        
+        // now: low < y < high 
+        if(!are_distinct){
+            if(sign_of_polynomial_at(y)==CGAL::ZERO){
+                learn_from(y);
+                return CGAL::EQUAL ;
+            }
+        }else{
+            CGAL_precondition(polynomial().sign_at(y)!=CGAL::ZERO);
+        }
+        strong_refine(y);
+        CGAL_postcondition(y < low() || high() < y );
+        if(y < low()) return CGAL::LARGER;
+        else          return CGAL::SMALLER;
+    }
+    
+    CGAL::Comparison_result
+    compare (const Algebraic_real_rep_bfi& y, bool are_distinct = false) const{
+        if(  is_rational()) return -y.compare(  rational());
+        if(y.is_rational()) return    compare(y.rational());
+        
+        // type of both x and y IS_GENERAL
+        if ( high() <= y.low() ) return CGAL::SMALLER;
+        if ( low()  >= y.high()) return CGAL::LARGER;
+
+        // intersection isolating intervals is ]L,R[
+        Field L = (low()  > y.low() ) ? low()  : y.low()  ;
+        Field R = (high() < y.high()) ? high() : y.high() ;
+
+        // refine to smaller intervals at intersection interval boundaries
+        // this can change type() only to IS_RATIONAL
+        refine_at(L);
+        refine_at(R);
+        y.refine_at(L);
+        y.refine_at(R);
+
+        if (  is_rational()) return -y.compare(  rational());
+        if (y.is_rational()) return    compare(y.rational());
+
+        // type of both x and y still IS_GENERAL
+        if ( high()   <= y.low() ) return CGAL::SMALLER;
+        if ( y.high() <=   low() ) return CGAL::LARGER;
+        
+        // filter 1 (optional): determine distinctness by refining intervals
+#if NiX_REFINEMENTS_BEFORE_GCD > 0
+        if (!are_distinct) {
+            // we may want to refine a bit and hope for the best
+            // because computing the gcd is expensive
+            for (int ntries=0; ntries < NiX_REFINEMENTS_BEFORE_GCD; ntries++) {
+                y.refine();
+                refine();
+                if (y.is_rational()) return    compare(y.rational());
+                if (  is_rational()) return -y.compare(  rational());
+                if (  high() <= y.low()) return CGAL::SMALLER;
+                if (y.high() <=   low()) return CGAL::LARGER;
+            }
+        }
+#endif
+        // filter 2: probabilistically check coprimality
+        if (!are_distinct) {
+            are_distinct = !(may_have_common_factor(polynomial(),
+                                                    y.polynomial()));
+        }
+        if (!are_distinct) {
+            // OK, filters failed. So we have to do the actual work
+            // and compute the gcd of the defining polynomials.
+
+            // we have ]low(), high()[ == ]y.low(),y.high()[ == ]L,R[
+            // and let both numbers decide for the gcd or its complement
+            Poly F1,F2,G;
+            G = gcd_utcf(polynomial(),y.polynomial()); 
+            F1 = div_utcf(polynomial(),G);
+            CGAL_postcondition(F1.degree()==polynomial().degree()-G.degree());
+            F2 = div_utcf(y.polynomial(),G);
+            CGAL_postcondition(F2.degree()==y.polynomial().degree()-G.degree());
+           
+            learn_from(G,F1);
+            y.learn_from(G,F2);
+
+            // this may simplify them due to degree loss
+            if (y.is_rational()) return    compare(y.rational());
+            if (  is_rational()) return -y.compare(  rational());
+        
+            // type of x and y is still IS_GENERAL
+            // check for equality
+            if (G.sign_at(L)!=G.sign_at(R)){
+                introduce(y);
+                return CGAL::EQUAL;
+            }
+        }
+        
+        // if we are here, we know the numbers to be distinct
+        // refine to disjointness
+        for (;;) {
+            y.refine();
+            refine();
+            if (y.is_rational()) return    compare(y.rational());
+            if (  is_rational()) return -y.compare(  rational());
+            if (  high() <= y.low()) return CGAL::SMALLER;
+            if (y.high() <=   low()) return CGAL::LARGER;
+        }
+    }
+
+
     //! creates the algebraic real from int \a i.
     explicit Algebraic_real_rep_bfi(int i = 0)
         :Base(i){}
@@ -165,7 +305,7 @@ public:
         return result; 
     }
   
-    void refine_approximation() const {
+    void refine_poly_approximation() const {
         CGAL_precondition(current_prec > 1);
         current_prec *= 2;
         // std::cout <<"ALGREAL: refine approx: "<<  current_prec<<std::endl;
@@ -184,49 +324,82 @@ public:
         }
     };
     
+    void update_poly_approximation() const {
+        long old_prec = set_precision( BF(), current_prec );
+        
+        polynomial_approx.clear();
+        convert_coeffs( this->polynomial(), std::back_inserter( polynomial_approx ) );
+                    
+        set_precision( BF(), old_prec );
+        
+        // TODO: Problems if the next block gets executed
+//        Self const *next = static_cast<const Self *>(this->next);
+//        while(this != next){
+//            // std::cout << this << " " << next << std::endl;
+//            next->polynomial_approx = polynomial_approx; 
+//            next->current_prec      = current_prec; 
+//            next = static_cast<const Self *>(next->next);
+//        }
+    }
+    
     void refine() const{              
         if(this->is_rational()) return;
         
        // std::cout << "refine begin ------- "<< std::endl;
 
+        Field m = (this->low()+this->high())/Field(2);
+
+        // Currently, sign_of_polynomial_at performs exactly the needed
+        // refinement. 
+        // TODO: But what if this changes? 
+        sign_of_polynomial_at( m );
+        //std::cout << "refine end ----------------- "<<current_prec<<  std::endl;
+    }
+
+protected:
+    virtual CGAL::Sign sign_of_polynomial_at( const Field& f ) const {
+        //return polynomial().sign_at( f );
         long old_prec = set_precision(BF(),current_prec);
-        
+
+        Field m = f;
+        CGAL::simplify(m);
+
+        // if polynomial has changed, the approximated polynomial gets
+        // updated  
         if ( (int) polynomial_approx.size() != this->polynomial_.degree()+1) {
-            refine_approximation();
+            update_poly_approximation();
         }        
         
         CGAL_postcondition(polynomial_approx.size() > 0);
-
-        Field m = (this->low()+this->high())/Field(2);
-        CGAL::simplify(m);
-
+        
         BFI eval = evaluate_polynomial_approx(convert_to_bfi(m));
-        //std::cout <<"approx eval: "<< eval << std::endl;
-    
-        //std::cout <<"true eval: "<< convert_to_bfi(this->polynomial().evaluate(m)) << std::endl;
+                
         CGAL::Sign s = CGAL::sign(CGALi::lower(eval));
-       
+              
         // correct sign if needed
         if( s*CGAL::sign(CGALi::upper(eval) ) != CGAL::POSITIVE ){
+            
             //std::cout << "APPROX FAILED-------------------------------"<<std::endl;
             s = this->polynomial().sign_at(m);
             if ( s != CGAL::ZERO ) {
-                refine_approximation(); 
+                refine_poly_approximation(); 
             }
         }
         
         CGAL_postcondition(s == this->polynomial_.sign_at(m));
-       
+
         if ( s == CGAL::ZERO ) {
             this->learn_from(m);
         }else{
             if ( s == this->sign_at_low() ) this->low_  = m;
             else                            this->high_ = m; 
         }
-        
+
         set_precision(BF(),old_prec);
-        //std::cout << "refine end ----------------- "<<current_prec<<  std::endl;
-    }
+        
+        return s;
+    }    
+    
 };
 
 }//namespace CGALi
