bee3cc0ae273c114d875045b3c4ca799f53e1446
Shlomo Golubev <golubevs@post.tau.ac.il>
2007-08-21 13:33:15 +0000

    fix vertical decomposition

--- Arrangement_on_surface_2/examples/Arrangement_on_surface_2/vertical_decomposition.cpp
+++ Arrangement_on_surface_2/examples/Arrangement_on_surface_2/vertical_decomposition.cpp
@@ -3,16 +3,16 @@
 
 #include <CGAL/Cartesian.h>
 #include <CGAL/MP_Float.h>
-#include <CGAL/Arr_segment_traits_2.h>
+#include <CGAL/Arr_linear_traits_2.h>
 #include <CGAL/Arrangement_2.h>
 #include <CGAL/Arr_vertical_decomposition_2.h>
 #include <list>
 
 typedef CGAL::MP_Float                                  Number_type;
 typedef CGAL::Cartesian<Number_type>                    Kernel;
-typedef CGAL::Arr_segment_traits_2<Kernel>              Traits_2;
+typedef CGAL::Arr_linear_traits_2<Kernel>               Traits_2;
 typedef Traits_2::Point_2                               Point_2;
-typedef Traits_2::X_monotone_curve_2                    Segment_2;
+typedef Traits_2::Segment_2                             Segment_2;
 typedef CGAL::Arrangement_2<Traits_2>                   Arrangement_2;
 typedef Arrangement_2::Vertex_const_handle              Vertex_const_handle;
 typedef Arrangement_2::Halfedge_const_handle            Halfedge_const_handle;
