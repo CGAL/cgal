a25098ab2c1dcd7349c1608987802edde9138af6
Efi Fogel <efif@post.tau.ac.il>
2007-08-28 12:42:07 +0000

    updated

--- Arrangement_on_surface_2/examples/Arrangement_on_surface_2/tracing_counting.cpp
+++ Arrangement_on_surface_2/examples/Arrangement_on_surface_2/tracing_counting.cpp
@@ -9,32 +9,38 @@
 #include <CGAL/Arr_counting_traits_2.h>
 #include <list>
 
-typedef CGAL::Quotient<int>                           Number_type;
-typedef CGAL::Cartesian<Number_type>                  Kernel;
-typedef CGAL::Arr_segment_traits_2<Kernel>            Segment_traits_2;
-typedef CGAL::Arr_tracing_traits_2<Segment_traits_2>  Tracing_traits_2;
-typedef CGAL::Arr_counting_traits_2<Tracing_traits_2> Geom_traits_2;
-typedef Geom_traits_2::Point_2                        Point_2;
-typedef Geom_traits_2::X_monotone_curve_2             Segment_2;
-typedef CGAL::Arrangement_2<Geom_traits_2>            Arrangement_2;
+typedef CGAL::Quotient<int>                             Number_type;
+typedef CGAL::Cartesian<Number_type>                    Kernel;
+typedef CGAL::Arr_segment_traits_2<Kernel>              Segment_traits_2;
+typedef CGAL::Arr_tracing_traits_2<Segment_traits_2>    Tracing_traits_2;
+typedef CGAL::Arr_counting_traits_2<Tracing_traits_2>   Geom_traits_2;
+typedef Geom_traits_2::Point_2                          Point_2;
+typedef Geom_traits_2::X_monotone_curve_2               Segment_2;
+typedef CGAL::Arrangement_2<Geom_traits_2>              Arrangement_2;
 
 int main ()
 {
-  // Construct the arrangement of five intersecting segments.
+  const Segment_2 s1(Point_2(0, 0), Point_2(2, 2));
+  const Segment_2 s2(Point_2(2, 0), Point_2(0, 2));
+  std::list<Segment_2> segments;
+  segments.push_back(s1);
+  segments.push_back(s2);
+
   Geom_traits_2 traits;
   traits.disable_all_traces();
   traits.enable_trace(Tracing_traits_2::INTERSECT_OP);
-  Arrangement_2           arr(&traits);;
-  std::list<Segment_2>    segments;
 
-  segments.push_back (Segment_2 (Point_2(1, 0), Point_2(2, 4)));
-  segments.push_back (Segment_2 (Point_2(5, 0), Point_2(5, 5)));
-  segments.push_back (Segment_2 (Point_2(1, 0), Point_2(5, 3)));  
-  segments.push_back (Segment_2 (Point_2(0, 2), Point_2(6, 0)));
-  segments.push_back (Segment_2 (Point_2(3, 0), Point_2(5, 5)));
-
-  insert (arr, segments.begin(), segments.end());
+  // Construct an arrangement using aggregated insertion:
+  Arrangement_2 arr1(&traits);
+  insert(arr1, segments.begin(), segments.end());
   std::cout << traits << std::endl;
-
+  traits.clear_counters();
+  
+  // Construct an arrangement using incremental insertion:
+  Arrangement_2 arr2(&traits);
+  insert(arr2, s1);
+  insert(arr2, s2);
+  std::cout << traits << std::endl;
+  
   return 0;
 }
