78dd7f3d6399a3f0bf8382b945e9cd723bfeaf38
Luc Habert <Luc.Habert@normalesup.org>
2007-08-31 14:19:48 +0000

    Changed conversion to CORE types.

--- Visibility_complex_2/include/CGAL/Visibility_complex_2/CORE_Root_of_traits.h
+++ Visibility_complex_2/include/CGAL/Visibility_complex_2/CORE_Root_of_traits.h
@@ -22,13 +22,17 @@
 
 #include <CGAL/basic.h>
 #include<CGAL/CORE_Expr.h>
+#include<CGAL/CORE_BigInt.h>
+#include<sstream>
 
 CGAL_BEGIN_NAMESPACE
 namespace Visibility_complex_2_details {
 
 template<class RT> class CORE_Root_of_traits {
-  static CORE::Expr convert(const RT&x) {
-    return CORE::Expr(CGAL_NTS to_double(x));
+  static CORE::BigInt convert(const RT&x) {
+    std::ostringstream os;
+    os<<x;
+    return CORE::BigInt(os.str());
   }
 public:
   class Root_of_4 {
@@ -37,13 +41,13 @@ public:
     Root_of_4 () {};
     Root_of_4 (const RT& c4, const RT& c3, const RT& c2, const RT& c1, 
                const RT& c0, int idx) {
-      CORE::Expr ploum[5];
+      CORE::BigInt ploum[5];
       ploum[0]=convert(c0);
       ploum[1]=convert(c1);
       ploum[2]=convert(c2);
       ploum[3]=convert(c3);
       ploum[4]=convert(c4);
-      root=CORE::Expr(CORE::Polynomial<CORE::Expr>(4,ploum),idx+1);
+      root=CORE::Expr(CORE::Polynomial<CORE::BigInt>(4,ploum),idx+1);
     };
     double to_double() const {
       return root.doubleValue();
@@ -55,12 +59,12 @@ public:
     Root_of_3 () {};
     Root_of_3 (const RT& c3, const RT& c2, const RT& c1, const RT& c0,
                int idx) {
-      CORE::Expr ploum[4];
+      CORE::BigInt ploum[4];
       ploum[0]=convert(c0);
       ploum[1]=convert(c1);
       ploum[2]=convert(c2);
       ploum[3]=convert(c3);
-      root=CORE::Expr(CORE::Polynomial<CORE::Expr>(3,ploum),idx+1);
+      root=CORE::Expr(CORE::Polynomial<CORE::BigInt>(3,ploum),idx+1);
     };
   };
   class Root_of_2 {
@@ -68,11 +72,11 @@ public:
     CORE::Expr root;
     Root_of_2 () {};
     Root_of_2 (const RT& c2, const RT& c1, const RT& c0, int idx) {
-      CORE::Expr ploum[3];
+      CORE::BigInt ploum[3];
       ploum[0]=convert(c0);
       ploum[1]=convert(c1);
       ploum[2]=convert(c2);
-      root=CORE::Expr(CORE::Polynomial<CORE::Expr>(2,ploum),idx+1);
+      root=CORE::Expr(CORE::Polynomial<CORE::BigInt>(2,ploum),idx+1);
     };
   };
   class Root_of_1 {
@@ -80,10 +84,10 @@ public:
     CORE::Expr root;
     Root_of_1 () {};
     Root_of_1 (const RT& c1, const RT& c0) {
-      CORE::Expr ploum[2];
+      CORE::BigInt ploum[2];
       ploum[0]=convert(c0);
       ploum[1]=convert(c1);
-      root=CORE::Expr(CORE::Polynomial<CORE::Expr>(1,ploum),1);
+      root=CORE::Expr(CORE::Polynomial<CORE::BigInt>(1,ploum),1);
     }
   };
   struct compare_object {
@@ -95,7 +99,7 @@ public:
     };
   };
   class Polynom_1 {
-    CORE::Expr coeffs[2];
+    CORE::BigInt coeffs[2];
   public:
     Polynom_1() {}
     Polynom_1(const RT& c1,const RT& c0)  {
@@ -114,7 +118,7 @@ public:
     }
   };
   class Polynom_2 {
-    CORE::Expr coeffs[3];
+    CORE::BigInt coeffs[3];
   public:
     Polynom_2() {};
     Polynom_2(const RT& c2,const RT& c1,const RT& c0)  {
