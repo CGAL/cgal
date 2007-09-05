448ce230e06dfc578f30e34a3ca06e94a583bc2d
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2007-09-05 07:39:27 +0000

    Fixed typo

--- Triangulation_2/examples/Triangulation_2/adding_handles.cpp
+++ Triangulation_2/examples/Triangulation_2/adding_handles.cpp
@@ -2,7 +2,7 @@
 #include <CGAL/Triangulation_2.h>
 #include <cassert>
 
-/* A facet with an additionnal handle */
+/* A vertex class with an additionnal handle */
 template < class Gt, class Vb = CGAL::Triangulation_vertex_base_2<Gt> >
 class My_vertex_base
   : public  Vb
