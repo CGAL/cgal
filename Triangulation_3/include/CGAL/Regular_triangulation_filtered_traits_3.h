d6efb7a292d568456b56ce7048040a5a0cefc90a
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2007-09-07 09:49:48 +0000

    Update following renaming EK -> Exact_kernel and FK -> Approximate_kernel.

--- Triangulation_3/include/CGAL/Regular_triangulation_filtered_traits_3.h
+++ Triangulation_3/include/CGAL/Regular_triangulation_filtered_traits_3.h
@@ -60,10 +60,10 @@ class Regular_triangulation_filtered_traits_3
   : public Regular_triangulation_euclidean_traits_base_3<K>
 {
   // Exact traits is based on the exact kernel.
-  typedef Regular_triangulation_euclidean_traits_3<typename K::EK>
+  typedef Regular_triangulation_euclidean_traits_3<typename K::Exact_kernel>
                                                    Exact_traits;
   // Filtering traits is based on the filtering kernel.
-  typedef Regular_triangulation_euclidean_traits_3<typename K::FK>
+  typedef Regular_triangulation_euclidean_traits_3<typename K::Approximate_kernel>
                                                    Filtering_traits;
 
   typedef typename K::C2E C2E;
