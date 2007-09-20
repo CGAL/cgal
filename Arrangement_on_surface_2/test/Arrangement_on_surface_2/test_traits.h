17a68cba91cec2beb57b98b62f35be15202a29bc
Shlomo Golubev <golubevs@post.tau.ac.il>
2007-09-20 10:17:24 +0000

    change Arr_spherical_arc_traits_2 to Arr_great_circular_arc_on_sphere_traits_2

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_traits.h
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_traits.h
@@ -51,7 +51,7 @@
 #include <CGAL/Arr_Bezier_curve_traits_2.h>
 
 #elif TEST_TRAITS == SPHERICAL_ARC_TRAITS
-#include <CGAL/Arr_spherical_arc_traits_2.h>
+#include <CGAL/Arr_great_circular_arc_on_sphere_traits_2.h>
 
 #elif TEST_TRAITS == RATIONAL_ARC_TRAITS
 #include <CGAL/CORE_algebraic_number_traits.h>
@@ -155,7 +155,7 @@ typedef Traits::Bezier_cache                            Bezier_cache;
 #define TRAITS_TYPE "Bezier"
 
 #elif TEST_TRAITS == SPHERICAL_ARC_TRAITS
-typedef CGAL::Arr_spherical_arc_traits_2<Kernel>        Traits;
+typedef CGAL::Arr_great_circular_arc_on_sphere_traits_2<Kernel>        Traits;
 #define TRAITS_TYPE "Spherical Arc"
 
 #elif TEST_TRAITS == RATIONAL_ARC_TRAITS
