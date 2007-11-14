3dd3f660438646e8ffb35c143eac50c0bb506801
Efi Fogel <efif@post.tau.ac.il>
2007-11-14 22:25:45 +0000

    fixed license

--- Benchmark/src/Benchmark/Option_parser.cpp
+++ Benchmark/src/Benchmark/Option_parser.cpp
@@ -1,9 +1,10 @@
 // Copyright (c) 1997  Tel-Aviv University (Israel).
 // All rights reserved.
 //
-// This file is part of CGAL (www.cgal.org); you may redistribute it under
-// the terms of the Q Public License version 1.0.
-// See the file LICENSE.QPL distributed with CGAL.
+// This file is part of CGAL (www.cgal.org); you may redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
 //
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
