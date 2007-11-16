09e5e60229f88e8501beeeddbe2337b8a39b87da
Pavel Emeliyanenko <asm@mpi-sb.mpg.de>
2007-11-16 15:51:37 +0000

    benchmark revised

--- Algebraic_kernel_d/benchmark/Algebraic_kernel_d/Algebraic_curve_kernel_2.cpp
+++ Algebraic_kernel_d/benchmark/Algebraic_kernel_d/Algebraic_curve_kernel_2.cpp
@@ -1,4 +1,4 @@
-// TODO: Add licence
+ // TODO: Add licence
 //
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
@@ -26,6 +26,13 @@
 #include <CGAL/Algebraic_kernel_1.h>
 #include <CGAL/Algebraic_curve_kernel_2.h>
 
+#define BENCH_DEBUG_OUT
+#ifdef BENCH_DEBUG_OUT
+#define BENCH_OUT(x) std::cerr << x;
+#else
+#define BENCH_OUT(x)
+#endif
+
 template <class Integer>
 struct Max_bit_size
 {
@@ -52,6 +59,7 @@ struct Benchmark_result {
     
     float solve_time;
     float sort_time;
+    float to_double_time;
     float total_time;
 };
 
@@ -60,7 +68,8 @@ std::ostream& operator <<(std::ostream& os, const Benchmark_result& res) {
     os << "n polys: " << res.number_of_polys << "\tn_bits: " << res.bits 
        << "\tn_real_roots: " << res.number_of_real_roots_found 
        << "\tdegre of polys: " << res.degree_of_polys << "\tsolve time: " <<
-            res.solve_time << "\tsort time: " << res.sort_time << 
+            res.solve_time << "\tsort time: " << res.sort_time <<
+            "\t to_double time: " << res.to_double_time << 
             "\t total time: " << res.total_time << std::endl;
     return os;
 }
@@ -117,10 +126,11 @@ public:
         _m_mults.clear();    
         typename AK_2::Solve_2 solve_2;
         
-        std::cout << "number of curves: " << n << std::endl;
+        BENCH_OUT("number of curves: " << n << std::endl);
         for(int i = 0; i < n-1; i++)
         for(int j = i+1; j < n; j++) {
-            std::cout << "i = " << i << "; j = " << j << "\n";
+
+            BENCH_OUT("i = " << i << "; j = " << j << "\n");
               
             // CGAL::remove_scalar_factor( poly ); ??
             solve_2(_m_curves[i], _m_curves[j], std::back_inserter(_m_roots),
@@ -181,6 +191,91 @@ private:
 
     Root_vector *_m_proots;
 };
+
+template< class AlgebraicCurveKernel_2 >
+class Bench_to_double {
+
+public:
+    //! this instance's first template argument    
+    typedef AlgebraicCurveKernel_2 AK_2;
+    //! in this setting Curve_2 == Polynomial_2
+    typedef typename AK_2::Curve_2 Curve_2;
+    //! type of result (bivariate polynomials solution)
+    typedef typename AK_2::Xy_coordinate_2 Xy_coordinate_2;
+        
+    // shall we pass as a parameter internal polynomials instead of curves  ?
+    //! container of input curves
+    typedef std::vector<Curve_2> Curve_vector;
+    //! container of resulting solutions
+    typedef std::vector<Xy_coordinate_2> Root_vector;
+    //! container of multiplicities
+    typedef std::vector<int> Mult_vector;
+    
+public:
+        
+    void setup(Root_vector *p_roots, Mult_vector *p_mults) { 
+        
+        CGAL_precondition(p_roots != NULL && p_mults != NULL);
+        _m_proots = p_roots;
+        _m_pmults = p_mults;
+    }
+    
+    int init() { return 0; }
+    void clean() {}
+    void sync() {}
+    
+    Root_vector *get_proots(){
+        return _m_proots;
+    }
+ 
+    void op() 
+    {
+        typedef typename Xy_coordinate_2::Boundary Boundary;
+        typedef typename Xy_coordinate_2::Boundary_interval
+                Boundary_interval;
+
+        ::CGAL::set_mode(std::cerr, ::CGAL::IO::PRETTY);
+        BENCH_OUT("Roots in ascending order: \n");
+        
+        int i = 0;
+        typename Root_vector::const_iterator rit;
+        typename Mult_vector::const_iterator mit;
+
+        typedef typename AK_2::X_real_traits_1 X_traits;
+        typedef typename AK_2::Y_real_traits_1 Y_traits;
+        
+        typename Y_traits::Lower_boundary lower_2;
+        typename Y_traits::Upper_boundary upper_2;
+        typename Y_traits::Refine refine_2;
+            
+        Boundary eps = Boundary(1)/CGAL::POLYNOMIAL::ipower(Boundary(2), 53);
+        
+        for(rit = _m_proots->begin(), mit = _m_pmults->begin(); rit !=
+            _m_proots->end(); rit++, mit++, i++) {
+
+            Boundary prec_x = (rit->x().high() - rit->x().low())*eps,
+                     prec_y = (upper_2(*rit) - lower_2(*rit))*eps;
+            while(1) {
+                Boundary x_len = rit->x().high() - rit->x().low(),
+                         y_len = upper_2(*rit) - lower_2(*rit);
+                
+                if(x_len <= prec_x && y_len <= prec_y)
+                    break;
+                // keep x/y-intervals comparable in size
+                (x_len > y_len) ? rit->x().refine() : refine_2(*rit);
+            }
+            BENCH_OUT(i << ":\n (" <<
+                NiX::to_double((rit->x().high() + rit->x().low())/2) << ", " <<
+                NiX::to_double((upper_2(*rit) + lower_2(*rit))/2) <<
+                "); multiplicity: " << *mit << "\n\n");
+        }
+    }
+    
+private:
+
+    Root_vector *_m_proots;
+    Mult_vector *_m_pmults;
+};
     
 
 template<class AlgebraicCurveKernel_2>
@@ -207,9 +302,13 @@ Benchmark_result do_benchmark(std::string filename, int n_samples = 5)
         
     CGAL::benchmark::Benchmark<Bench_sort<AK_2> > 
         bench_sort(filename, 0, true);
+
+    CGAL::benchmark::Benchmark<Bench_to_double<AK_2> >
+        bench_to_double(filename, 0, true);
         
     bench_solve_2.set_samples(n_samples);
     bench_sort.set_samples(n_samples);
+    bench_to_double.set_samples(n_samples);
         
     // read in the polynomials for testing
     std::ifstream file(filename.c_str());
@@ -225,8 +324,8 @@ Benchmark_result do_benchmark(std::string filename, int n_samples = 5)
     for(int i = 0; i < n_polys; i++) {
        file >> tmp;
        // CGAL::remove_scalar_factor(poly); ??
-       Curve_2 c = kernel_2.construct_curve_2_object()(
-            NiX::canonicalize_polynomial(tmp));
+       Curve_2 c = kernel_2.construct_curve_2_object()
+            (NiX::canonicalize_polynomial(tmp));
        curves.push_back(c);
        CGAL_assertion(!file.eof());
     }
@@ -254,38 +353,13 @@ Benchmark_result do_benchmark(std::string filename, int n_samples = 5)
     bench_sort.get_benchable().setup(&roots);
     bench_sort();
     result.sort_time = bench_sort.get_period() / n_samples;
-    
-    ::CGAL::set_mode(std::cout, ::CGAL::IO::PRETTY);
-    std::cout << "Roots in ascending order: \n";
-    int i = 0;
-    typename Root_vector::const_iterator rit;
-    typename Mult_vector::const_iterator mit;
-    for(rit = roots.begin(), mit = mults.begin(); rit != roots.end(); 
-            rit++, mit++) {
-        typedef typename Xy_coordinate_2::Boundary Boundary;
-        typedef typename Xy_coordinate_2::Boundary_interval Boundary_interval;
-        Boundary_interval x_iv = rit->get_approximation_x(),
-            y_iv = rit->get_approximation_y();
-        while(x_iv.upper() - x_iv.lower() > Boundary(1,10000)) {
-            rit->refine_x();
-            x_iv = rit->get_approximation_x();
-        }
-        while(y_iv.upper() - y_iv.lower() > Boundary(1,10000)) {
-            rit->refine_y();
-            y_iv = rit->get_approximation_y();
-        }
-        std::cout << i++ << ": " << std::flush;
-        std::cout << " ( " << std::flush;
-        std::cout << NiX::to_double((x_iv.upper() + x_iv.lower())/2) 
-                  << std::flush;
-        std::cout << ", " << std::flush;
-        std::cout << NiX::to_double((y_iv.upper() + y_iv.lower())/2) 
-                  << std::flush;
-        std::cout << " ) " << std::flush;
-        std::cout << "; multiplicity: " << *mit << std::endl << std::endl;
-    }
-    
-    result.total_time = result.solve_time + result.sort_time;
+
+    bench_to_double.get_benchable().setup(&roots, &mults);
+    bench_to_double();
+    result.to_double_time = bench_to_double.get_period() / n_samples;
+
+    result.total_time = result.solve_time + result.sort_time +
+        result.to_double_time;
     return result;
 }
 
