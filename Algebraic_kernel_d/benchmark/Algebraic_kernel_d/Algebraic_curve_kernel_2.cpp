f756e7131e193abbbba01123e123ed16fe7ed5a0
Pavel Emeliyanenko <asm@mpi-sb.mpg.de>
2007-11-19 11:01:54 +0000

    to_double()

--- Algebraic_kernel_d/benchmark/Algebraic_kernel_d/Algebraic_curve_kernel_2.cpp
+++ Algebraic_kernel_d/benchmark/Algebraic_kernel_d/Algebraic_curve_kernel_2.cpp
@@ -244,16 +244,16 @@ public:
         typedef typename AK_2::X_real_traits_1 X_traits;
         typedef typename AK_2::Y_real_traits_1 Y_traits;
         
-        typename Y_traits::Lower_boundary lower_2;
-        typename Y_traits::Upper_boundary upper_2;
-        typename Y_traits::Refine refine_2;
-            
-        Boundary eps = Boundary(1)/CGAL::POLYNOMIAL::ipower(Boundary(2), 53);
+        //typename Y_traits::Lower_boundary lower_2;
+        //typename Y_traits::Upper_boundary upper_2;
+        typename Y_traits::To_interval to_interval;
+        //Boundary eps = Boundary(1)/CGAL::POLYNOMIAL::ipower(Boundary(2), 53);
         
         for(rit = _m_proots->begin(), mit = _m_pmults->begin(); rit !=
             _m_proots->end(); rit++, mit++, i++) {
 
-            Boundary prec_x = (rit->x().high() - rit->x().low())*eps,
+            std::pair<double, double> res = to_interval(*rit);
+            /*Boundary prec_x = (rit->x().high() - rit->x().low())*eps,
                      prec_y = (upper_2(*rit) - lower_2(*rit))*eps;
             while(1) {
                 Boundary x_len = rit->x().high() - rit->x().low(),
@@ -263,10 +263,9 @@ public:
                     break;
                 // keep x/y-intervals comparable in size
                 (x_len > y_len) ? rit->x().refine() : refine_2(*rit);
-            }
-            BENCH_OUT(i << ":\n (" <<
-                NiX::to_double((rit->x().high() + rit->x().low())/2) << ", " <<
-                NiX::to_double((upper_2(*rit) + lower_2(*rit))/2) <<
+            }*/
+            BENCH_OUT(i << ":\n (" << NiX::to_double(rit->x()) << ", " <<
+                (res.first + res.second)/2 <<
                 "); multiplicity: " << *mit << "\n\n");
         }
     }
