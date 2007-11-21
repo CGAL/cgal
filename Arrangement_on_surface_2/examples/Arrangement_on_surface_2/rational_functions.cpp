84f0bda94b72e8c4ff4c391138618077074fb6a5
Shlomo Golubev <golubevs@post.tau.ac.il>
2007-11-21 12:00:46 +0000

    hecked if the curve ends (left

--- Arrangement_on_surface_2/examples/Arrangement_on_surface_2/rational_functions.cpp
+++ Arrangement_on_surface_2/examples/Arrangement_on_surface_2/rational_functions.cpp
@@ -23,60 +23,63 @@ typedef CGAL::Cartesian<Algebraic>                    Alg_kernel;
 typedef CGAL::Arr_rational_arc_traits_2<Alg_kernel,
                                         Nt_traits>    Traits_2;
 typedef Traits_2::Point_2                             Point_2;
-typedef Traits_2::Curve_2                             Rational_arc_2;
+typedef Traits_2::X_monotone_curve_2                  Rational_arc_2;
 typedef Traits_2::Rat_vector                          Rat_vector;
 typedef std::list<Rational_arc_2>                     Rat_arcs_list;
 typedef CGAL::Arrangement_2<Traits_2>                 Arrangement_2;
 
 int main ()
 {
-  // Create an arc supported by the polynomial y = x^4 - 6x^2 + 8,
-  // defined over the interval [-2.1, 2.1]:
-  Rat_vector        P1(5);  
-  P1[4] = 1; P1[3] = 0; P1[2] = -6; P1[1] = 0; P1[0] = 8;
 
-  Rational_arc_2    a1 (P1, Algebraic(-2.1), Algebraic(2.1));
+  Traits_2 m_traits;
 
-  // Create an arc supported by the function y = x / (1 + x^2),
-  // defined over the interval [-3, 3]:
-  Rat_vector        P2(2);
-  P2[1] = 1; P2[0] = 0;
+  // Create an arc supported by the polynomial y = 1
+  Rat_vector        P1(1);
+  P1[0] = 1;
 
-  Rat_vector        Q2(3);
-  Q2[2] = 1; Q2[1] = 0; Q2[0] = 1;
+  Rat_vector        P2(3);  
+  P2[2] = -1; P2[1] = 0; P2[0] = 1;
 
-  Rational_arc_2    a2 (P2, Q2, Algebraic(-3), Algebraic(3));
+  Rational_arc_2    a1 (P1, P2, Algebraic(-1), Algebraic(1));
 
-  // Create an arc supported by the parbola y = 8 - x^2,
-  // defined over the interval [-2, 3]:
-  Rat_vector        P3(5);  
-  P3[2] = -1; P3[1] = 0; P3[0] = 8;
+  Rational_arc_2    a1left (P1, P2, Algebraic(-1), Algebraic(0));
 
-  Rational_arc_2    a3 (P3, Algebraic(-2), Algebraic(3));
+  Rational_arc_2    a1right (P1, P2, Algebraic(0), Algebraic(1));
 
-  // Create an arc supported by the line y = -2x,
-  // defined over the interval [-3, 0]:
-  Rat_vector        P4(2);  
-  P4[1] = -2; P4[0] = 0;
+  Rational_arc_2 cv1,cv2;
 
-  Rational_arc_2    a4 (P4, Algebraic(-3), Algebraic(0));
+  Point_2 split_point1(0,1);
 
-  // Construct the arrangement of the four arcs.
-  Arrangement_2              arr;
-  std::list<Rational_arc_2>  arcs;
+  m_traits.split_2_object()(a1, split_point1, cv1, cv2);
 
-  arcs.push_back (a1);
-  arcs.push_back (a2);
-  arcs.push_back (a3);
-  arcs.push_back (a4);
-  insert (arr, arcs.begin(), arcs.end());
 
-  // Print the arrangement size.
-  std::cout << "The arrangement size:" << std::endl
-            << "   V = " << arr.number_of_vertices()
-            << ",  E = " << arr.number_of_edges() 
-            << ",  F = " << arr.number_of_faces() << std::endl;
+  std::cout <<"a1left " << a1left << std::endl;
+  std::cout <<"a1right " << a1right << std::endl;
+  std::cout <<"cv1 " << cv1 << std::endl;
+  std::cout <<"cv2 " << cv2 << std::endl;
+  std::cout <<"m_traits.equal_2_object() " << m_traits.equal_2_object()(cv1,a1left) << std::endl;
+  std::cout <<"m_traits.equal_2_object() " << m_traits.equal_2_object()(cv2,a1right) << std::endl;
 
+  //***********************************************//
+/*
+  Rational_arc_2    a2 (P1, P2, Algebraic(-1), false);
+
+  Point_2 split_point2(-3,-0.125);
+
+  m_traits.split_2_object()(a2, split_point2, cv1, cv2);
+
+  std::cout <<"cv1 " << cv1 << std::endl;
+  std::cout <<"cv2 " << cv2 << std::endl;
+
+  Rational_arc_2    a3 (P1, P2, Algebraic(1), true);
+
+  Point_2 split_point3(3,-0.125);
+
+  m_traits.split_2_object()(a3, split_point3, cv1, cv2);
+
+  std::cout <<"cv1 " << cv1 << std::endl;
+  std::cout <<"cv2 " << cv2 << std::endl;
+*/
   return 0;
 }
 
