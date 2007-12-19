73364cf8144c0fa6a5339d449f257abca0f714e1
Eric Berberich <eric.berberich@cgal.org>
2007-12-19 11:04:08 +0000

    added Rep to templates

--- Curved_kernel_via_analysis_2/include/CGAL/Curved_kernel_via_analysis_2l.h
+++ Curved_kernel_via_analysis_2/include/CGAL/Curved_kernel_via_analysis_2l.h
@@ -3,7 +3,7 @@
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// $URL:$
+// $URL$
 // $Id$
 // 
 //
@@ -32,7 +32,7 @@ CGAL_BEGIN_NAMESPACE
 namespace CGALi {
 
 // pre-declaration
-template < class CurvedKernelViaAnalysis_2l >
+template < class CurvedKernelViaAnalysis_2l, class Rep_ >
 class Surface_point_2l;
 
 
@@ -60,38 +60,42 @@ protected:
     mutable int _m_sheet;
     
     // befriending the handle
-    friend class Surface_point_2l< Curved_kernel_via_analysis_2l >;
+    friend class Surface_point_2l< Curved_kernel_via_analysis_2l, Self >;
 };
 
 
 // TODO documentation
-template < class CurvedKernelViaAnalysis_2l >
+template < 
+  class CurvedKernelViaAnalysis_2l, 
+  class Rep_ = CGALi::Surface_point_2l_rep< CurvedKernelViaAnalysis_2l >
+ >
 class Surface_point_2l : 
     public CGALi::Point_2< 
         CurvedKernelViaAnalysis_2l, 
-        CGALi::Surface_point_2l_rep< CurvedKernelViaAnalysis_2l > >
+        Rep_  >
 {
 public:
 
-    //! this instance template parameter
+    //! this instance's first template parameter
     typedef CurvedKernelViaAnalysis_2l Curved_kernel_via_analysis_2l;
     
+    //! this instance's second template parameter
+    typedef Rep_ Rep;
+
     //! the instance itself
-    typedef Surface_point_2l< Curved_kernel_via_analysis_2l > Self;
-    
-    //! the type of the representation
-    typedef CGALi::Surface_point_2l_rep< Curved_kernel_via_analysis_2l > Rep;
+    typedef Surface_point_2l< Curved_kernel_via_analysis_2l, Rep > Self;
     
     //! the base type
     typedef CGALi::Point_2< Curved_kernel_via_analysis_2l, Rep > Base;
     
-private:
-    typedef typename Base::Point_2 Point_2;
+    // TODO document
+public:
+#if 1
     typedef typename Base::Curve_kernel_2 Curve_kernel_2;
     typedef typename Base::Curve_2 Curve_2;
     typedef typename Base::X_coordinate_1 X_coordinate_1;
     typedef typename Base::Xy_coordinate_2 Xy_coordinate_2;
-
+#endif
 
 public:
     // TODO add constructors
@@ -118,7 +122,7 @@ public:
 
 
 // pre-declaration
-template < class CurvedKernelViaAnalysis_2l >
+template < class CurvedKernelViaAnalysis_2l, class Rep_ >
 class Surface_arc_2l;
 
 // TODO documentation
@@ -150,32 +154,35 @@ protected:
 
 
     // befriending the handle
-    friend class Surface_arc_2l< Curved_kernel_via_analysis_2l >;
+    friend class Surface_arc_2l< Curved_kernel_via_analysis_2l, Self >;
 
 };
 
 
 // TODO documentation
-template < class CurvedKernelViaAnalysis_2l >
+template < 
+  class CurvedKernelViaAnalysis_2l, 
+  class Rep_ = CGALi::Surface_arc_2l_rep< CurvedKernelViaAnalysis_2l >
+>
 class Surface_arc_2l :
     public CGALi::Arc_2_base< 
         CurvedKernelViaAnalysis_2l, 
         Surface_arc_2l< CurvedKernelViaAnalysis_2l >, 
-        CGALi::Surface_arc_2l_rep< CurvedKernelViaAnalysis_2l > 
+        Rep_ 
     > 
 {
 
 public:
 
-    //! this type's template parameter
+    //! this type's first template parameter
     typedef CurvedKernelViaAnalysis_2l Curved_kernel_via_analysis_2l;
     
-    //! the class itself
-    typedef Surface_arc_2l< Curved_kernel_via_analysis_2l > Self;
-    
-    //! the representation
-    typedef CGALi::Surface_arc_2l_rep< Curved_kernel_via_analysis_2l > Rep;
+    //! this type's first template parameter
+    typedef Rep_ Rep;
 
+    //! the class itself
+    typedef Surface_arc_2l< Curved_kernel_via_analysis_2l, Rep > Self;
+    
     //! the base class
     typedef CGALi::Arc_2_base< Curved_kernel_via_analysis_2l, Self, Rep > Base;
     
@@ -203,11 +210,28 @@ public:
 
 
 // TODO documentation
-template < class CurveKernel_2 >
+template < 
+  class CurveKernel_2, 
+  template < class CK_2 > class PointTemplate_2 = CGALi::Surface_point_2l,
+  template < class CK_2 > class ArcTemplate_2 = CGALi::Surface_arc_2l
+ >
 class Curved_kernel_via_analysis_2l : 
         public Curved_kernel_via_analysis_2< 
-            CurveKernel_2, CGALi::Surface_point_2l, CGALi::Surface_arc_2l 
+            CurveKernel_2, PointTemplate_2, ArcTemplate_2
 > {
+public:
+    //! this instance's first template parameter
+    typedef CurveKernel_2 Curve_kernel_2;
+
+    //! this instance's second template parameter
+    //typedef PointTemplate_2 Point_template_2;
+
+    //! this instance's third template parameter
+    //typedef ArcTemplate_2 Arc_template_2;
+    
+    //! the class itself
+    typedef Curved_kernel_via_analysis_2l< 
+         Curve_kernel_2, PointTemplate_2, ArcTemplate_2 > Self;
     
     // TODO add constructors
     
