affe68b4f7e55b5174c3dd1e1351e5b8529e3130
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2007-12-19 16:12:20 +0000

    Add detection of Boost.Thread.

---
+++ Installation/config/support/test_BOOST_THREAD.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2007  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/trunk/Installation/config/support/test_BOOST_PROGRAM_OPTIONS.cpp $
+// $Id: test_BOOST_PROGRAM_OPTIONS.cpp 37323 2007-03-20 19:14:02Z reichel $
+// 
+//
+// Author(s)     : Sylvain Pion
+
+// Tests if BOOST.THREAD is available.
+
+#include <iostream>
+#include <cassert>
+#include <boost/version.hpp>
+#include <boost/thread/tss.hpp>
+
+int main()
+{
+  std::cout << "version=" << BOOST_VERSION/100000 << "."
+            << ((BOOST_VERSION / 100) % 100) << "."
+            << BOOST_VERSION % 100 << std::endl;
+
+  boost::thread_specific_ptr<int> z;
+  if (z.get() == NULL) {
+    z.reset(new int(1));
+  }
+  assert(*z.get() == 1);
+
+  return 0;
+}
