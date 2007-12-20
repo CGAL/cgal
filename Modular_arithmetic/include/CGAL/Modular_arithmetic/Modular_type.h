204f5340083ac8d06f713f6d255a8aedb660cb31
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2007-12-20 10:28:54 +0000

    Add protect macro and header.

--- Modular_arithmetic/include/CGAL/Modular_arithmetic/Modular_type.h
+++ Modular_arithmetic/include/CGAL/Modular_arithmetic/Modular_type.h
@@ -1,3 +1,26 @@
+// Copyright (c) 2007  INRIA Sophia-Antipolis (France), Max-Planck-Institute
+// Saarbruecken (Germany).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/trunk/Number_types/include/CGAL/MP_Float.h $
+// $Id: MP_Float.h 40709 2007-10-25 14:18:35Z slimbach $
+//
+// Author(s)     : Sylvain Pion, Michael Hemmer
+
+#ifndef CGAL_MODULAR_TYPE_H
+#define CGAL_MODULAR_TYPE_H
+
 #include <CGAL/basic.h>
 
 #include <cfloat>
@@ -296,3 +319,5 @@ inline std::istream& operator >> (std::istream& is, Modular& p) {
 }
 
 CGAL_END_NAMESPACE
+
+#endif // CGAL_MODULAR_TYPE_H
