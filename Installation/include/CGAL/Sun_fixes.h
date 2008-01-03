f5a9c2d26e4f432162f03e2f708ff37f79a092dc
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2008-01-03 15:22:29 +0000

    Finish removal of CGAL_CFG_SUNPRO_RWSTD obsolete workarounds.

--- Installation/include/CGAL/Sun_fixes.h
+++ Installation/include/CGAL/Sun_fixes.h
@@ -27,116 +27,12 @@
 #ifdef __SUNPRO_CC
 #  include <iterator>
 #  ifdef _RWSTD_NO_CLASS_PARTIAL_SPEC
-#    define CGAL_CFG_SUNPRO_RWSTD
+#    error "CGAL does not support SunPRO with the old Rogue Wave STL: use STLPort."
 #  endif
 #endif
 
 // Sun CC has an issue with templates that means overloading
 // Qualified_result_of does not work so well.
 #define CGAL_CFG_DONT_OVERLOAD_TOO_MUCH 1
-
-#ifdef CGAL_CFG_SUNPRO_RWSTD
-
-//----------------------------------------------------------------------//
-//             if member templates for stdlib are not enabled           //
-//----------------------------------------------------------------------//
-
-/*
-
-For reasons of binary backward compatibility, Sun CC does not enable 
-member templates of the STL classes in the Rogue Wave STL.
-
-An #undef creates runtime errors for some packages, so it is not a 
-viable solution. Instead, we have to offer workarounds in CGAL
-code, whereever we use this feature.
-
-*/
- 
-#include <stdcomp.h>
-
-//----------------------------------------------------------------------//
-//             fake iterator_traits                                     //
-//----------------------------------------------------------------------//
-
-#include <iterator>
-
-namespace std {
-
-  template <class Iterator> struct iterator_traits
-  {
-    typedef typename Iterator::value_type value_type;
-    typedef typename Iterator::difference_type difference_type;
-    typedef typename Iterator::pointer pointer;
-    typedef typename Iterator::reference reference;
-    typedef typename Iterator::iterator_category iterator_category;
-  };
-
-  template <class T> struct iterator_traits<T*>
-  {
-    typedef T value_type;
-    typedef ptrdiff_t difference_type;
-    typedef T* pointer;
-    typedef T& reference;
-    typedef random_access_iterator_tag iterator_category;
-  };
-
-  template <class T> struct iterator_traits<const T*>
-  {
-    typedef T value_type;
-    typedef ptrdiff_t difference_type;
-    typedef const T* pointer;
-    typedef const T& reference;
-    typedef random_access_iterator_tag iterator_category;
-  };
-
-  template <class ForwardIterator>
-  inline ptrdiff_t
-  distance (ForwardIterator first, ForwardIterator last)
-  {
-    ptrdiff_t n = 0;
-    __distance(first, last, n, 
-               iterator_traits<ForwardIterator>::iterator_category());
-    return n;
-  }
-
-  template <class InputIterator, class T>
-  inline typename iterator_traits<InputIterator>::difference_type
-  count (InputIterator first, InputIterator last, const T& value)
-  {
-    typename iterator_traits<InputIterator>::difference_type result;
-    count(first,last,value,result);
-    return result;
-  }
-
-  template <class InputIterator, class Predicate>
-  inline typename iterator_traits<InputIterator>::difference_type
-  count_if (InputIterator first, InputIterator last, Predicate pred)
-  {
-    typename iterator_traits<InputIterator>::difference_type result;
-    count_if(first,last,pred,result);
-    return result;
-  }
-
-  template < class T >
-  inline typename T::value_type*
-  __value_type (const T&)
-  { return (typename T::value_type*)(0); }
-
-  template < class T >
-  inline typename T::difference_type*
-  __distance_type(const T&)
-  { return (typename T::difference_type*)(0); }
-
-  template < class T >
-  inline typename T::iterator_category
-  __iterator_category (const T&)
-  {
-    typename T::iterator_category tmp;
-    return tmp;
-  }
-
-} // namespace std
-
-#endif // CGAL_CFG_SUNPRO_RWSTD
 
 #endif // CGAL_SUN_FIXES_H
