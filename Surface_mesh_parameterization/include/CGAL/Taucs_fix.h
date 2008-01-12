4e76054a50d57c5044d0e583c912010a7567ddca
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2008-01-12 20:31:14 +0000

    Protect by CGAL_USE_TAUCS.

--- Surface_mesh_parameterization/include/CGAL/Taucs_fix.h
+++ Surface_mesh_parameterization/include/CGAL/Taucs_fix.h
@@ -20,6 +20,9 @@
 #ifndef CGAL_TAUCS_FIX
 #define CGAL_TAUCS_FIX
 
+#include <CGAL/basic.h>
+
+#ifdef CGAL_USE_TAUCS
 
 //
 // Include TAUCS main header taucs.h
@@ -54,5 +57,6 @@ extern "C" {
     #undef max
 #endif
 
+#endif // CGAL_USE_TAUCS
 
 #endif // CGAL_TAUCS_FIX
