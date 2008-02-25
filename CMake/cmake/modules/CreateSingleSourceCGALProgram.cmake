af7d2be6db8851e40b9b09edcba622c27ab024d2
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2008-02-25 14:08:24 +0000

    Make CreateSingleSourceCGALProgram work even if some demos/examples are in dont_submit.

--- CMake/cmake/modules/CreateSingleSourceCGALProgram.cmake
+++ CMake/cmake/modules/CreateSingleSourceCGALProgram.cmake
@@ -1,30 +1,32 @@
 macro(create_single_source_cgal_program first )
 
-  set( all ${first} )
-  
-  foreach( i ${ARGN} )
-    set( all ${all} ${i} ) 
-  endforeach()
-  
-  get_filename_component(exe_name ${first} NAME_WE)
-  
-  add_executable  (${exe_name} ${all})
-  
-  #add_dependencies(${exe_name} CGAL CGAL_CORE)
+  if(EXISTS ${first})
+    set( all ${first} )
     
-  # Link the executable to CGAL and third-party libraries
-  if ( NOT AUTO_LINK_ENABLED )    
-    target_link_libraries(${exe_name} ${CGAL_LIBRARIES} ${CGAL_3RD_PARTY_LIBRARIES})
-  endif()
-
-  # TODO: get test parameters from ${exe_name}.cmd
-  #set ( test_args )
-  # 
-  #if ( EXISTS ${exe_name}.cmd )
-  #  file( READ ${exe_name}.cmd ${test_args} )
-  #  message( STATUS "Command line arguments for ${exe_name}: ${test_args} )  
-  #endif()
-  # 
-  #add_test( ${exe_name} ${exe_name} ${ARGS} )
+    foreach( i ${ARGN} )
+      set( all ${all} ${i} ) 
+    endforeach()
+    
+    get_filename_component(exe_name ${first} NAME_WE)
+    
+    add_executable  (${exe_name} ${all})
+    
+    #add_dependencies(${exe_name} CGAL CGAL_CORE)
+      
+    # Link the executable to CGAL and third-party libraries
+    if ( NOT AUTO_LINK_ENABLED )    
+      target_link_libraries(${exe_name} ${CGAL_LIBRARIES} ${CGAL_3RD_PARTY_LIBRARIES})
+    endif()
   
+    # TODO: get test parameters from ${exe_name}.cmd
+    #set ( test_args )
+    # 
+    #if ( EXISTS ${exe_name}.cmd )
+    #  file( READ ${exe_name}.cmd ${test_args} )
+    #  message( STATUS "Command line arguments for ${exe_name}: ${test_args} )  
+    #endif()
+    # 
+    #add_test( ${exe_name} ${exe_name} ${ARGS} )
+  endif(EXISTS ${first})
+    
 endmacro()
--- CMake/cmake/modules/CreateSingleSourceCGALQt4Program.cmake
+++ CMake/cmake/modules/CreateSingleSourceCGALQt4Program.cmake
@@ -1,29 +1,31 @@
 macro(create_single_source_cgal_qt4_program first )
 
-  set( all ${first} )
+  if(EXISTS ${first})
+    set( all ${first} )
   
-  foreach( i ${ARGN} )
-    set( all ${all} ${i} ) 
-  endforeach()
-  
-  get_filename_component(exe_name ${first} NAME_WE)
-  
-  QT4_AUTOMOC( ${all} )
-  
-  # Make sure the compiler can find generated .moc files
-  include_directories(BEFORE ${CMAKE_CURRENT_BINARY_DIR})
-  include_directories(BEFORE ${CMAKE_CURRENT_SOURCE_DIR})
-  
-  add_executable  (${exe_name} ${all})
-  add_dependencies(${exe_name} CGAL CGAL_CORE)
+    foreach( i ${ARGN} )
+      set( all ${all} ${i} ) 
+    endforeach()
     
-  # Link the executable to CGAL and third-party libraries
-  if ( NOT AUTO_LINK_ENABLED )    
-    target_link_libraries(${exe_name} ${CGAL_LIBRARIES} ${QT_LIBRARIES} ${CGAL_3RD_PARTY_LIBRARIES})
-  endif()
+    get_filename_component(exe_name ${first} NAME_WE)
+    
+    QT4_AUTOMOC( ${all} )
+    
+    # Make sure the compiler can find generated .moc files
+    include_directories(BEFORE ${CMAKE_CURRENT_BINARY_DIR})
+    include_directories(BEFORE ${CMAKE_CURRENT_SOURCE_DIR})
+    
+    add_executable  (${exe_name} ${all})
+    add_dependencies(${exe_name} CGAL CGAL_CORE)
+    
+    # Link the executable to CGAL and third-party libraries
+    if ( NOT AUTO_LINK_ENABLED )    
+      target_link_libraries(${exe_name} ${CGAL_LIBRARIES} ${QT_LIBRARIES} ${CGAL_3RD_PARTY_LIBRARIES})
+    endif()
 
-  # Add " make test" rule for executable
-  # TODO: get test parameters from ${exe_name}.cmd
-  # add_test(${exe_name} ${exe_name})
+    # Add " make test" rule for executable
+    # TODO: get test parameters from ${exe_name}.cmd
+    # add_test(${exe_name} ${exe_name})
+  endif(EXISTS ${first})
   
 endmacro()
