8e40af475e7726530378915f4b68a9608b395ec2
Pedro Machado Manhaes de Castro <Pedro.Machado@sophia.inria.fr>
2008-02-26 15:38:19 +0000

    Flexibility

--- Triangulation_2/include/CGAL/Delaunay_triangulation_2.h
+++ Triangulation_2/include/CGAL/Delaunay_triangulation_2.h
@@ -180,8 +180,7 @@ public:
   {
     bool blocked = false;
     std::map<Vertex_handle, int> hash;
-    std::list< std::pair<Vertex_handle, Point> > to_move;
-    while(first != last) to_move.push_back(first++);
+    std::list< std::pair<Vertex_handle, Point> > to_move(first, last);
     while(!to_move.empty()) {
       std::pair<Vertex_handle, Point> pp = to_move.front();
       to_move.pop_front();
