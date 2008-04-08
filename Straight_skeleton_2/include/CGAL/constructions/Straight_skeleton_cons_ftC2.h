e9bc3893a005cd55ff3f60f68be404727409c4e6
Fernando Cacciola <fernando.cacciola@geometryfactory.com>
2008-04-08 19:40:54 +0000

    Merged removal of unnecessary cast for SunPro

--- Straight_skeleton_2/include/CGAL/constructions/Straight_skeleton_cons_ftC2.h
+++ Straight_skeleton_2/include/CGAL/constructions/Straight_skeleton_cons_ftC2.h
@@ -49,8 +49,7 @@ inline CORE::BigFloat MP_Float_to_BigFloat( MP_Float const& b )
     d += d_exp * CORE::BigFloat(b.of_exp(i));
   }
 
-  // The cast is necessary for SunPro.
-  return d * CORE::BigFloat::exp2(static_cast<int>(exp * log_limb));
+  return d * CORE::BigFloat::exp2(exp * log_limb);
 }
 #endif
 
