bd70e1f5e87c7998b87b41b0e54c7f2d1019e64a
Nader Salman <Nader.Salman@sophia.inria.fr>
2008-05-16 09:30:41 +0000

    Utility class aims at merging points: - inserts a range of elements from first to beyond, - but only if value doesn't already exist : allows only unique points with an epsilon tolerance.
    Observations :
    - Quick but approximate.

---
+++ Surface_reconstruction_3/include/CGAL/merge_epsilon_nearest_points_3.h
@@ -0,0 +1,177 @@
+// Copyright (c) 2007-08  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute point_it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+//
+// Author(s) : Laurent Saboret and Nader Salman
+
+#ifndef CGAL_MERGE_EPSILON_NEAREST_POINTS_3_H
+#define CGAL_MERGE_EPSILON_NEAREST_POINTS_3_H
+
+#include <CGAL/basic.h>
+
+#include <iterator>
+#include <set>
+#include <algorithm>
+#include <cmath>
+
+CGAL_BEGIN_NAMESPACE
+
+
+// Round number to multiples of epsilon
+inline double round_epsilon(double value, double epsilon)
+{
+    return double(int(value/epsilon + 0.5)) * epsilon;
+}
+
+/// Utility class for merge_epsilon_nearest_points_3():
+/// Compare 2 3D points with an epsilon tolerance.
+template <class Point_3>
+struct Less_epsilon_points_3 
+{
+    double m_epsilon;
+    Less_epsilon_points_3 (double epsilon = 0)
+        : m_epsilon (epsilon)
+    {}
+    bool operator() (const Point_3& a, const Point_3& b) const 
+    {
+        // Quick but approximate test:
+        // Round points to multiples of 2.0*m_epsilon, then compare.
+        Point_3 rounded_a(round_epsilon(a.x(), 2.0*m_epsilon),
+                          round_epsilon(a.y(), 2.0*m_epsilon),
+                          round_epsilon(a.z(), 2.0*m_epsilon));
+        Point_3 rounded_b(round_epsilon(b.x(), 2.0*m_epsilon),
+                          round_epsilon(b.y(), 2.0*m_epsilon),
+                          round_epsilon(b.z(), 2.0*m_epsilon));
+        return (rounded_a < rounded_b);
+    }
+};
+
+/// Utility class for merge_epsilon_nearest_points_3():
+/// 3D points set which allows only unique points with an epsilon tolerance.
+template <class Point_3>
+class Epsilon_point_set_3 : public std::set<Point_3,Less_epsilon_points_3<Point_3> >
+{
+private:
+
+    typedef std::set<Point_3,Less_epsilon_points_3<Point_3> > Base;
+    
+public:
+
+    Epsilon_point_set_3 (double epsilon = 0)
+        : Base( Less_epsilon_points_3<Point_3>(epsilon) )
+    {}
+    
+    // default copy constructor, operator =() and destructor are fine.
+};
+
+/// Merge points:
+/// - inserts a range of elements from first to beyond,
+/// - but only if value doesn't already exist.
+/// This variant requires the kernel.
+///
+/// Precondition: epsilon > 0.
+template < typename InputIterator,  ///< InputIterator value_type is Point_3.
+typename OutputIterator,            ///< OutputIterator value_type is Point_3.
+typename Kernel                     ///< Geometric traits class.
+>
+OutputIterator                      ///< return past-the-end iterator of output
+merge_epsilon_nearest_points_3(InputIterator first,       ///< input points
+                               InputIterator beyond,
+                               OutputIterator output,     ///< output points
+                               const Kernel& /*kernel*/,
+                               double epsilon)            ///< tolerance value when comparing 3D points
+{
+    typedef typename Kernel::Point_3 Point;
+
+    // precondition: at least one element in the container
+    CGAL_precondition(first != beyond);
+
+    CGAL_precondition(epsilon > 0);
+
+    // Merge points identical with an epsilon tolerance
+    //std::set<Point_3,Less_epsilon_points_3<Point_3> > merged_points(Less_epsilon_points_3(epsilon));
+    Epsilon_point_set_3<Point_3> merged_points(epsilon);
+    merged_points.insert(first, beyond);
+
+    // Copy merged points to output
+    std::copy(merged_points.begin(), merged_points.end(), output);
+    return output;
+}
+
+/// Merge points:
+/// - inserts a range of elements from first to beyond,
+/// - but only if value doesn't already exist.
+/// This function is mutating the input point set.
+/// This variant requires the kernel.
+///
+/// Precondition: epsilon > 0.
+template < typename InputIterator, ///< InputIterator value_type is Point_3.
+           typename Kernel         ///< Geometric traits class.
+>
+void
+merge_epsilon_nearest_points_3(
+                     InputIterator first,       ///< input points
+                     InputIterator beyond,
+                     const Kernel& /*kernel*/,
+                     double epsilon)            ///< tolerance value when comparing 3D points
+{
+  CGAL_precondition(false); // nyi
+}
+
+/// Merge points:
+/// - inserts a range of elements from first to beyond,
+/// - but only if value doesn't already exist.
+/// This variant deduces the kernel from iterator types.
+///
+/// Precondition: epsilon > 0.
+template < typename InputIterator, ///< InputIterator value_type is Point_3
+           typename OutputIterator ///< OutputIterator value_type is Point_3
+>
+OutputIterator                     ///< return past-the-end iterator of output
+merge_epsilon_nearest_points_3(
+                     InputIterator first,       ///< input points
+                     InputIterator beyond,
+                     OutputIterator output,     ///< output points
+                     double epsilon)            ///< tolerance value when comparing 3D points
+{
+  typedef typename std::iterator_traits<InputIterator>::value_type Value_type;
+  typedef typename Kernel_traits<Value_type>::Kernel Kernel;
+  return merge_epsilon_nearest_points_3(first,beyond,output,Kernel(),epsilon);
+}
+
+/// Merge points:
+/// - inserts a range of elements from first to beyond,
+/// - but only if value doesn't already exist.
+/// This function is mutating the input point set.
+/// This variant deduces the kernel from iterator types.
+///
+/// Precondition: epsilon > 0.
+template < typename InputIterator               ///< InputIterator value_type is Point_3
+>
+void
+merge_epsilon_nearest_points_3(
+                     InputIterator first,       ///< input points
+                     InputIterator beyond,
+                     double epsilon)            ///< tolerance value when comparing 3D points
+{
+  typedef typename std::iterator_traits<InputIterator>::value_type Value_type;
+  typedef typename Kernel_traits<Value_type>::Kernel Kernel;
+  merge_epsilon_nearest_points_3(first,beyond,Kernel(),epsilon);
+}
+
+
+CGAL_END_NAMESPACE
+
+#endif // CGAL_MERGE_EPSILON_NEAREST_POINTS_3_H
+
