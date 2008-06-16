67c887e71f9a6edd5f882fb966efba32470ba4bc
Peter Hachenberger <hachenberger@mpi-inf.mpg.de>
2008-06-16 07:37:46 +0000

    additional test

--- Convex_decomposition_3/test/Convex_decomposition_3/edge_sorter.cpp
+++ Convex_decomposition_3/test/Convex_decomposition_3/edge_sorter.cpp
@@ -42,6 +42,17 @@ public:
   }
 };
 
+template<typename Iterator, typename Compare>
+bool check_sorting(Iterator begin, Iterator end, Compare& compare)
+{
+  if(begin == end) return true;
+  Iterator next = begin;
+  for(++next; next != end; ++begin, ++next) {
+    if((*next)->source()->point() < (*begin)->source()->point())
+      return false;
+  }
+  return true;
+}
 
 typedef CGAL::Need_to_split<Kernel, std::less<Point_3> > SplitTest;
 typedef CGAL::Generic_edge_sorter<Point_3, std::less<Point_3>,  std::less<FT>,
@@ -59,11 +70,19 @@ int main(int argc, char* argv[]) {
   t0->set_twin(e0);
   e1->set_twin(t1);
   t1->set_twin(e1);
-  edges.push_back(e0);
-  edges.push_back(e1);
-  
+
   SplitTest st;
   Report_new_vertex<LEdge, LVertex> rnv;
   GesSM gesSM;
+
+  edges.push_back(e0);
+  edges.push_back(e1);
   gesSM(edges, st, rnv);
+  std::less<Point_3> Less;
+  CGAL_assertion(check_sorting(edges.begin(), edges.end(), Less));
+  CGAL_assertion(edges.size() == 3);
+  CGAL_assertion(edges[1]->twin()->source()->point() == Point_3(-1,0,0));
+  CGAL_assertion(edges[2]->source()->point() == Point_3(-1,0,0));
+
+  
 }
