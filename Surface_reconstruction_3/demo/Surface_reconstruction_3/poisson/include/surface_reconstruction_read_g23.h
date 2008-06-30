a6ae5efe8b87bc7c6540d99fb18613a067f89e3b
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2008-06-30 16:35:40 +0000

    To be compatible with MatchMover, 3D points have now a unique label in .g23 format. I updated surface_reconstruction_read_g23() accordingly.

--- Surface_reconstruction_3/demo/Surface_reconstruction_3/poisson/include/surface_reconstruction_read_g23.h
+++ Surface_reconstruction_3/demo/Surface_reconstruction_3/poisson/include/surface_reconstruction_read_g23.h
@@ -10,22 +10,24 @@
 #include <string>
 
 
-/// .g23 format:
-///
-/// G23
-/// movie_file_name
-/// number_of_cameras number_of_3D_points number_of_2D_points
-/// camera_index Cx Cy Cz                 for each camera
-/// X Y Z                                 for each 3D point
-/// camera_index point_3D_index x y       for each 2D point
-/// with:
-/// - camera_index is the camera's index (aka frame's index).
-///   Camera indices may not be consecutive.
-/// - (Cx, Cy, Cz) is the camera's position.
-/// - point_3D_index is the 3D point's index.
-///   3D point indices must start at 1 and be consecutive.
-/// - (X,Y,Z) is 3D the point's position.
-/// - (x,y) is 2D the point's position.
+// .g23 format:
+//
+// G23
+// movie_file_name
+// number_of_cameras number_of_3D_points number_of_2D_points
+// camera_index Cx Cy Cz                 for each camera
+// point_3D_label X Y Z                  for each 3D point
+// camera_index point_3D_label x y       for each 2D point
+// with:
+// - movie_file_name is the tracked movie's name, with path
+//   (using % notation for image sequences, e.g. foo/bar%3d.tga).
+// - camera_index is the camera's index (aka frame's index).
+//   Camera indices may not be consecutive.
+// - (Cx, Cy, Cz) is the camera's position.
+// - point_3D_label is the 3D point's label (between double quotes)
+//   3D point indices must start at 1 and be consecutive.
+// - (X,Y,Z) is 3D the point's position.
+// - (x,y) is 2D the point's position.
 
 
 /// Read 3D points + cameras from a Gyroviz .g23 file.
@@ -64,7 +66,7 @@ bool surface_reconstruction_read_g23(const char* pFilename,
   int lineNumber = 0; // current line number
   char pLine[4096]; // current line buffer
   std::map<int, Point_3> cameras; // container of (indexed) cameras
-  std::vector<Gyroviz_point> gyroviz_points; // container of 3D points + (camera, 2D point) pairs
+  std::map<std::string, Gyroviz_point> gyroviz_points; // container of (labelled) 3D points + camera/2D point pairs
   
   *movie_file_name = "";
   
@@ -86,8 +88,8 @@ bool surface_reconstruction_read_g23(const char* pFilename,
       char signature[512];
       if ( (sscanf(pLine,"%s",signature) != 1) || (strcmp(signature, "G23") != 0) )
       {
-        // if unsupported file format
-        std::cerr << "Incorrect file format line " << lineNumber << " of " << pFilename << std::endl;
+        // if incorrect file format
+        std::cerr << "Fatal error line " << lineNumber << " of " << pFilename << std::endl;
         return false;
       }
     }    
@@ -98,7 +100,7 @@ bool surface_reconstruction_read_g23(const char* pFilename,
       char file_name[512];
       if (sscanf(pLine,"%s",file_name) != 1)
       {
-        std::cerr << "Error line " << lineNumber << " of " << pFilename << std::endl;
+        std::cerr << "Fatal error line " << lineNumber << " of " << pFilename << std::endl;
         return false;
       }
       *movie_file_name = file_name;
@@ -109,7 +111,7 @@ bool surface_reconstruction_read_g23(const char* pFilename,
     {
       if (sscanf(pLine,"%ld %ld %ld",&cameras_count,&positions_3D_count,&positions_2D_count) != 3)
       {
-        std::cerr << "Error line " << lineNumber << " of " << pFilename << std::endl;
+        std::cerr << "Fatal error line " << lineNumber << " of " << pFilename << std::endl;
         return false;
       }
     }       
@@ -120,56 +122,64 @@ bool surface_reconstruction_read_g23(const char* pFilename,
       // Read index + position...
       int camera_index;
       double Cx,Cy,Cz;   
-      if(sscanf(pLine, "%d %lg %lg %lg", &camera_index, &Cx,&Cy,&Cz) == 4)
+      if(sscanf(pLine, "%d %lg %lg %lg", &camera_index, &Cx,&Cy,&Cz) != 4)
       {
-        Point_3 camera(Cx,Cy,Cz);
-        cameras[camera_index] = camera;
+        std::cerr << "Warning: skip incorrect line " << lineNumber << " of " << pFilename << std::endl;
+        continue;
       }
+      Point_3 camera(Cx,Cy,Cz);
+      cameras[camera_index] = camera;
     }
 
     // Read 3D points on next lines
     else if (gyroviz_points.size() < positions_3D_count)
     {
-      // Read position...
+      // Read label + position...
+      // WARNING: this code does not support spaces in labels.
+      char point_3D_label[512];
       double X,Y,Z;   
-      if(sscanf(pLine, "%lg %lg %lg", &X,&Y,&Z) == 3)
+      if (sscanf(pLine, "%s %lg %lg %lg", point_3D_label, &X,&Y,&Z) != 4)
       {
-        Point_3 position_3D(X,Y,Z);
-        gyroviz_points.push_back(position_3D);
+        std::cerr << "Warning: skip incorrect line " << lineNumber << " of " << pFilename << std::endl;
+        continue;
       }
+      Point_3 position_3D(X,Y,Z);
+      gyroviz_points[point_3D_label] = position_3D;
     }
 
     // Read 2D points + camera indices + 3D points indices on remaining lines
     else 
     {
-      // Read camera index, 3D point index and 2D position...
-      int camera_index, point_3D_index;
+      // Read camera index, 3D point label and 2D position...
+      // WARNING: this code does not support spaces in labels.
+      int camera_index;
+      char point_3D_label[512];
       double x,y;   
-      if(sscanf(pLine, "%d %d %lg %lg", &camera_index, &point_3D_index, &x,&y) == 4)
+      if (sscanf(pLine, "%d %s %lg %lg", &camera_index, point_3D_label, &x,&y) != 4 ||
+          cameras.find(camera_index) == cameras.end() || 
+          gyroviz_points.find(point_3D_label) == gyroviz_points.end())
       {
-        if (cameras.find(camera_index) == cameras.end() || point_3D_index-1 >= gyroviz_points.size())
-        {
-          std::cerr << "Error line " << lineNumber << " of " << pFilename << std::endl;
-          return false;
-        }
-        Point_3 camera = cameras[camera_index];
-        Point_2 position_2D(x, y);
-        gyroviz_points[point_3D_index-1].add_camera_point2_pair( std::make_pair(camera, position_2D) );
+        //std::cerr << "Warning: skip incorrect line " << lineNumber << " of " << pFilename << std::endl;
+        continue;
       }
+      Point_3 camera = cameras[camera_index];
+      Point_2 position_2D(x, y);
+      gyroviz_points[point_3D_label].add_camera_point2_pair( std::make_pair(camera, position_2D) );
     }
   }
 
   fclose(pFile);
 
   // Copy gyroviz_points[] to gyroviz_point_output
-  //std::copy(gyroviz_points.begin(), gyroviz_points.end(), gyroviz_point_output);
-  for (std::vector<Gyroviz_point>::iterator it=gyroviz_points.begin(); it != gyroviz_points.end(); it++)
+  for (std::map<std::string,Gyroviz_point>::iterator it=gyroviz_points.begin(); it != gyroviz_points.end(); it++)
   {
+    Gyroviz_point gpt = it->second;
+    
     // TEMPORARY? Skip 3D points with no cameras.
-    if (it->cameras_begin() != it->cameras_end())
-      *gyroviz_point_output++ = *it;
+    if (gpt.cameras_begin() != gpt.cameras_end())
+      *gyroviz_point_output++ = gpt;
     //else
-    //  std::cerr << "Skip (" << (Point_3)*it << ") line " << lineNumber << " of " << pFilename << std::endl;
+    //  std::cerr << "Skip (" << (Point_3)gpt << ") line " << lineNumber << " of " << pFilename << std::endl;
   }
   
   // Copy cameras[] to camera_output
