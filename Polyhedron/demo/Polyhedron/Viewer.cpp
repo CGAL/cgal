387084df22dced501c71773a858de172bcb3d5ab
Pierre Alliez <Pierre.Alliez@sophia.inria.fr>
2008-07-11 15:46:51 +0000

    cleanup interface and viewer

--- Polyhedron/demo/Polyhedron/Viewer.cpp
+++ Polyhedron/demo/Polyhedron/Viewer.cpp
@@ -9,12 +9,10 @@ Viewer::Viewer(QWidget* parent, bool antialiasing)
   setBackgroundColor(::Qt::white);
 }
 
-void 
-Viewer::setScene(Scene* scene)
+void Viewer::setScene(Scene* scene)
 {
   this->scene = scene;
 }
-
 
 void Viewer::setAntiAliasing(bool b)
 {
@@ -22,30 +20,29 @@ void Viewer::setAntiAliasing(bool b)
   updateGL();
 }
 
-void
-Viewer::draw()
+void Viewer::draw()
 {
   QGLViewer::draw();
   if(scene == 0)
     return;
 
-  ::glClearColor(1., 1., 1., 0.);
+  ::glLineWidth(1.0f);
   ::glEnable(GL_POLYGON_OFFSET_FILL);
   ::glPolygonOffset(1.0f,1.0f);
-  ::glLineWidth(1.0f);
+  ::glClearColor(1.0f,1.0f,1.0f,0.0f);
   ::glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
 
   if(antiAliasing())
   {
-    ::glEnable(GL_LINE_SMOOTH);
     ::glEnable(GL_BLEND);
-    ::glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    ::glEnable(GL_LINE_SMOOTH);
     ::glHint(GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
+    ::glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   }
   else
   {
+    ::glDisable(GL_BLEND);
     ::glDisable(GL_LINE_SMOOTH);
-    ::glDisable (GL_BLEND);
     ::glBlendFunc(GL_ONE, GL_ZERO);
     ::glHint(GL_LINE_SMOOTH_HINT, GL_FASTEST);
   }
