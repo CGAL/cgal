8f569aa38227f1fd467781d3700476647ec4b365
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2008-07-16 13:48:26 +0000

    Use QInputDialog::getInteger to get the parameter for the simplification algorithm.

--- Polyhedron/demo/Polyhedron/MainWindow_simplify.cpp
+++ Polyhedron/demo/Polyhedron/MainWindow_simplify.cpp
@@ -1,6 +1,8 @@
 #include "MainWindow.h"
 #include "Scene.h"
 
+#include <QInputDialog>
+
 #include <CGAL/Surface_mesh_simplification/HalfedgeGraph_Polyhedron_3.h>
 #include <CGAL/Surface_mesh_simplification/edge_collapse.h>
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Count_stop_predicate.h>
@@ -9,16 +11,28 @@ void MainWindow::on_actionSimplify_triggered()
 {
 	if(onePolygonIsSelected())
 	{
-		// wait cursor
-		QApplication::setOverrideCursor(Qt::WaitCursor);
-
 		// get selected polyhedron
 		int index = getSelectedPolygonIndex();
 		Polyhedron* pMesh = scene->polyhedron(index);
 
 		// simplify
-		const unsigned int nb_edges = 1000; // TODO: should be an option 
+                bool ok;
+		const unsigned int nb_edges = 
+                  QInputDialog::getInteger(this, tr("Stop condition"),
+                                           tr("Number of edges:"),
+                                           pMesh->size_of_halfedges () / 4, 
+                                           // current value: nb of edges /2 
+                                           10, // min
+                                           pMesh->size_of_halfedges(), // max
+                                           1, // step for the spinbox
+                                           &ok);
+                if(!ok) return;
+
+1000; // TODO: should be an option 
 		namespace SMS = CGAL::Surface_mesh_simplification;
+
+		// wait cursor
+		QApplication::setOverrideCursor(Qt::WaitCursor);
 
 		SMS::Count_stop_predicate< Polyhedron > stop(nb_edges); // target # edges
 		SMS::edge_collapse( *pMesh, stop,
@@ -29,6 +43,6 @@ void MainWindow::on_actionSimplify_triggered()
 		scene->polyhedronChanged(index);
 
 		// default cursor
-		QApplication::setOverrideCursor(Qt::ArrowCursor);
+		QApplication::restoreOverrideCursor();
 	}
 }
