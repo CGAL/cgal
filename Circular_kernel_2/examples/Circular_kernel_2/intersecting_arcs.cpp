0cee71677e3c6f345d55aebb0a4a56b655573932
Pedro Machado Manhaes de Castro <Pedro.Machado@sophia.inria.fr>
2008-07-24 15:52:12 +0000

    making life easier for the user, replacing CK().etcetc_2_obejct()(x,y) by CGAL::etcetc(x,y)

--- Circular_kernel_2/examples/Circular_kernel_2/intersecting_arcs.cpp
+++ Circular_kernel_2/examples/Circular_kernel_2/intersecting_arcs.cpp
@@ -21,11 +21,9 @@ double prob_2() {
     Point_2 p1, p2, p3, p4, p5, p6;
     p1 = *g++; p2 = *g++; p3 = *g++;
     p4 = *g++; p5 = *g++; p6 = *g++;
-    if(Circular_k().orientation_2_object()(p1, p2, p3) !=
-      CGAL::COUNTERCLOCKWISE) std::swap(p1, p3);
+    if(CGAL::orientation(p1, p2, p3) != CGAL::COUNTERCLOCKWISE) std::swap(p1, p3);
     T o1 = T(p1, p2, p3);
-    if(Circular_k().orientation_2_object()(p4, p5, p6) !=
-      CGAL::COUNTERCLOCKWISE) std::swap(p4, p6);
+    if(CGAL::orientation(p4, p5, p6) != CGAL::COUNTERCLOCKWISE) std::swap(p4, p6);
     T o2 = T(p4, p5, p6);
     std::vector< CGAL::Object > res;
     Circular_k().intersect_2_object()(o1, o2, std::back_inserter(res));
