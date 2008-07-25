c36fe883972d5991683fdee419635ce730c2e554
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2008-07-25 15:21:06 +0000

    Mark Triple and Quadruple "not-recommended anymore".

--- STL_Extension/include/CGAL/utility.h
+++ STL_Extension/include/CGAL/utility.h
@@ -28,6 +28,10 @@
 
 #include <CGAL/basic.h>
 
+// The Triple and Quadruple classes are NOT RECOMMENDED anymore.
+// We recommend that you use boost::tuple or boost::array instead
+// for new uses.
+
 CGAL_BEGIN_NAMESPACE
 
 //+---------------------------------------------------------------------+
