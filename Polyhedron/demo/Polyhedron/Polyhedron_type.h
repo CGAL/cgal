2bf2b4ff3e3fba47ae25965e0bd7726489cf85f1
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2008-07-25 21:21:02 +0000

    No need to include <CGAL/basic.h>

--- Polyhedron/demo/Polyhedron/Polyhedron_type.h
+++ Polyhedron/demo/Polyhedron/Polyhedron_type.h
@@ -2,8 +2,6 @@
 #define POLYHEDRON_TYPE_H
 
 // CGAL
-#include <CGAL/basic.h>
-
 // kernel
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
