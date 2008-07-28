f7d133e2189f54d0106b6a1a5c6910f276ba719a
Michael Hemmer <hemmer@mpi-inf.mpg.de>
2008-07-28 14:34:12 +0000

    example for gcd vs gcd_utcf

---
+++ Polynomial/examples/Polynomial/gcd_up_to_constant_factor.cpp
@@ -0,0 +1,38 @@
+#include <CGAL/basic.h>
+#include <CGAL/Polynomial.h>
+#include <CGAL/Polynomial_traits_d.h>
+#include <CGAL/Polynomial_type_generator.h>
+#include <CGAL/Gmpz.h>
+
+int main(){
+  CGAL::set_pretty_mode(std::cout);
+  typedef CGAL::Polynomial_type_generator<CGAL::Gmpz,1>::Type Poly_1;
+  typedef CGAL::Polynomial_traits_d<Poly_1>                   PT_1;
+  
+  PT_1::Shift                     shift;
+  PT_1::Gcd                       gcd;
+  PT_1::Gcd_up_to_constant_factor gcd_utcf;
+  PT_1::Multivariate_content      mcontent;
+  PT_1::Canonicalize              canonicalize;
+  
+  //construction using shift 
+  Poly_1 x = shift(Poly_1(1),1,0); // x_0^1
+  
+  // common factor 7 * (x^2-2) 
+  Poly_1 F = 21*(x-5)*(x*x-2);
+  Poly_1 G = 14*(x-3)*(x*x-2);
+  
+  std::cout << "The univariate polynomial F: " << F << std::endl;
+  std::cout << "The univariate polynomial G: " << G << std::endl;
+  std::cout << "Common multivariate content: " 
+            << CGAL::gcd(mcontent(F),mcontent(G)) << std::endl;
+  std::cout << std::endl;
+  
+  std::cout << "The gcd of F and G:                       " 
+            << gcd(F,G) << std::endl;
+  std::cout << "The gcd up to constant factor of F and G: " 
+            << gcd_utcf(F,G) << std::endl;
+  std::cout << "Same as canonicalized gcd of F and G:     " 
+            << canonicalize(gcd_utcf(F,G)) << std::endl;
+  
+}
