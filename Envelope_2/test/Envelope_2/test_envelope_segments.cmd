fed62f020df805dbaea3e52243d0ed5a44f3fa52
Ophir Setter <ophirset@post.tau.ac.il>
2008-07-30 09:43:17 +0000

    adding ./ at beginning of file

--- Envelope_2/test/Envelope_2/test_envelope_segments.cmd
+++ Envelope_2/test/Envelope_2/test_envelope_segments.cmd
@@ -1,4 +1,4 @@
-data/segs_4.dat -i
-data/random_500.dat -i
-data/onebig_100.dat -i
-data/Europe.dat -d
+./data/segs_4.dat -i
+./data/random_500.dat -i
+./data/onebig_100.dat -i
+./data/Europe.dat -d
