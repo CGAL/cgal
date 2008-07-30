40eeb98d619731a6be95bc7336980afbb6292ae1
Ophir Setter <ophirset@post.tau.ac.il>
2008-07-30 09:51:00 +0000

    removing cgal_test file

---
+++ Minkowski_sum_2/test/Minkowski_sum_2/test_approx_offset.cmd
@@ -0,0 +1,2 @@
+./data/rooms_part1.dat 3/1 -sohg
+./data/comb_part1.dat 1/1 -so
---
+++ Minkowski_sum_2/test/Minkowski_sum_2/test_exact_offset.cmd
@@ -0,0 +1,3 @@
+./data/random_part1.dat 5/1 -sohg
+./data/comb_part1.dat 1/1 -sohg
+./data/wheels_part1.dat 100000/1 -sohg
