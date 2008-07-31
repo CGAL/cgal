ff620d343022d4f56ec5d62dc27d37d42b7e88b3
Michael Hemmer <hemmer@mpi-inf.mpg.de>
2008-07-31 13:01:34 +0000

    reduced interface

--- Polynomial/include/CGAL/Polynomial/Cached_extended_euclidean_algorithm.h
+++ Polynomial/include/CGAL/Polynomial/Cached_extended_euclidean_algorithm.h
@@ -1,61 +1,59 @@
+// ----------------------------------------------------------------------------
+//
+// Library       : CGAL
+// File          : include/CGAL/Arithmetic_kernel.h
+// CGAL_release   : $Name:  $
+// Revision      : $Revision$
+// Revision_date : $Date$
+//
+// Author(s)     : Michael Hemmer <mhemmer@uni-mainz.de>
+//                 
+// ============================================================================
 
-#ifndef CGAL_POLYNOMIAL_CACHED_EEA_H
-#define CGAL_POLYNOMIAL_CACHED_EEA_H
-
+#ifndef CGAL_CACHED_EXTENDED_EUCLIDEAN_ALGORITHM_H
+#define CGAL_CACHED_EXTENDED_EUCLIDEAN_ALGORITHM_H
 
 #include <CGAL/basic.h>
-#include <CGAL/extended_euclidean_algorithm.h>
 #include <CGAL/Cache.h>
+#include <CGAL/extended_euclidean_algorithm.h>
 
-namespace CGAL{ 
+CGAL_BEGIN_NAMESPACE
 namespace CGALi{
 
-template <class UFD> 
-struct Extended_euclidean_algorithm{
+template <class UFD, int i = 0 > 
+struct Cached_extended_euclidean_algorithm{
+
+  struct Extended_euclidean_algorithm{
     typedef std::pair<UFD,UFD> result_type;
     typedef std::pair<UFD,UFD> first_argument_type; 
     result_type operator()(const first_argument_type& pq){
-        result_type result; 
-        CGAL::extended_euclidean_algorithm(
-                pq.first, pq.second,result.first, result.second);
-        return result;
+      result_type result; 
+      CGAL::extended_euclidean_algorithm(
+          pq.first, pq.second, result.first, result.second);
+      return result;
     }
+  };
+  
+  typedef std::pair<UFD,UFD> PAIR; 
+  typedef Extended_euclidean_algorithm FUNC;
+  typedef CGAL::Cache<PAIR,PAIR,FUNC> CACHE;
+  
+  static CACHE cache;
+  
+  void operator()(const UFD& p, const UFD& q, UFD& s, UFD& t){
+    PAIR pq(p,q);
+    PAIR result = cache(pq);
+    s = result.first;
+    t = result.second;
+  }    
 };
 
-template <class UFD> 
-struct Cached_extended_euclidean_algorithm{
-    typedef std::pair<UFD,UFD> PAIR; 
-    typedef Extended_euclidean_algorithm<UFD> FUNC;
-    typedef CGAL::Cache<PAIR,PAIR,FUNC,CGAL::Identity<PAIR>, CGAL::Less<PAIR> > CACHE;
-    
-    static CACHE cache;
-    
-    void operator()(const UFD& p, const UFD& q, UFD& s, UFD& t){
-        PAIR pq(p,q);
-        PAIR result = cache(pq);
-        s = result.first;
-        t = result.second;
-    }    
-};
+template <class UFD, int i> 
+typename Cached_extended_euclidean_algorithm<UFD,i>::CACHE 
+Cached_extended_euclidean_algorithm<UFD,i>::cache;
 
-template <class UFD> 
-typename Cached_extended_euclidean_algorithm<UFD>::CACHE 
-Cached_extended_euclidean_algorithm<UFD>::cache;
-
-template <class UFD>
-void
-cached_extended_euclidean_algorithm(
-        const UFD& p, 
-        const UFD& q, 
-        UFD& s, 
-        UFD& t){
-    // CGAL::extended_euclidean_algorithm(p,q,s,t); return ;
-    Cached_extended_euclidean_algorithm<UFD> eea_cache;
-    eea_cache(p,q,s,t);
-    
-}
 
 } // namespace CGALi
-} // namespace CGAL
+CGAL_END_NAMESPACE
 
-#endif // CGAL_POLYNOMIAL_CACHED_EEA_H
+#endif//CGAL_CACHED_EXTENDED_EUCLIDEAN_ALGORITHM_H
