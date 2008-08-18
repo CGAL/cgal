dd033c1748fabaa087d7edf2a28b97124dd0f4a2
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2008-08-18 15:08:55 +0000

    revert revision 44882: the matrix is modified in place, by its member function scale().

--- GraphicsView/demo/Triangulation_2/Constrained_Delaunay_triangulation_2.cpp
+++ GraphicsView/demo/Triangulation_2/Constrained_Delaunay_triangulation_2.cpp
@@ -246,7 +246,7 @@ MainWindow::MainWindow()
   this->graphicsView->setMouseTracking(true);
 
   // Turn the vertical axis upside down
-  this->graphicsView->setMatrix(this->graphicsView->matrix().scale(1, -1));
+  this->graphicsView->matrix().scale(1, -1);
                                                       
   // The navigation adds zooming and translation functionality to the
   // QGraphicsView
