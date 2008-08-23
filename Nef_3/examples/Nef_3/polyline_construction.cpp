e4c8f198815c296496e53d57495334f33c434548
Peter Hachenberger <hachenberger@mpi-inf.mpg.de>
2008-08-23 16:51:00 +0000

    adapted for new interface for polyline construction

--- Nef_3/examples/Nef_3/polyline_construction.cpp
+++ Nef_3/examples/Nef_3/polyline_construction.cpp
@@ -21,8 +21,6 @@ int main() {
 		   Point_3(1,1,0), Point_3(0,1,0)};
   polyline poly;
   poly.push_back(point_range(pl,pl+4));
-  Nef_polyhedron N;
-  Polyline_constructor pc(poly.begin(), poly.end());
-  N.delegate(pc,true);
+  Nef_polyhedron N(poly.begin(), poly.end(), Nef_polyhedron::Polylines_tag());
   std::cout << N;
 }
