e7dd28125fe3eb23322470f31ba19c4d49911cfc
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2008-08-26 14:24:07 +0000

    Add explicit static_cast to fix error with gcc-4.4 in -std=c++0x mode.

--- Number_types/include/CGAL/MP_Float.h
+++ Number_types/include/CGAL/MP_Float.h
@@ -156,7 +156,7 @@ public:
   static
   void split(limb2 l, limb & high, limb & low)
   {
-    to_signed l2 = {l};
+    to_signed l2 = {static_cast<limb>(l)};
     low = l2.s;
     high = (l - low) >> (8*sizeof(limb));
   }
