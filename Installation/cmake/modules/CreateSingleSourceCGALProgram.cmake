ab07405874fddd06ef8bf2b1954edf4fb886a8fc
Fernando Cacciola <fernando.cacciola@geometryfactory.com>
2008-09-04 18:58:09 +0000

    Use unique names for example, demo and test targets Renamed CGAL-Qt as CGAL-Qt3 Re-spelled cgal components as CORE, ImageIO, PDB, Qt3 and Qt4

--- Installation/cmake/modules/CreateSingleSourceCGALProgram.cmake
+++ Installation/cmake/modules/CreateSingleSourceCGALProgram.cmake
@@ -1,4 +1,4 @@
-macro(create_single_source_cgal_program first )
+macro(create_single_source_cgal_program prefix first )
 
   if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${first})
   
@@ -10,15 +10,18 @@ macro(create_single_source_cgal_program first )
     
     get_filename_component(exe_name ${first} NAME_WE)
     
-    add_executable(${exe_name} ${all})
+    set( target_name "${prefix}_${exe_name}" )
     
-    add_to_cached_list( CGAL_EXECUTABLES ${exe_name} )
+    add_executable(${target_name} ${all})
+    
+    set( CGAL_EXECUTABLE_TARGETS )
+    set( CGAL_EXECUTABLE_TARGETS "${CGAL_EXECUTABLE_TARGETS}" "${target_name}" CACHE INTERNAL "" FORCE )
     
     # Link the executable to CGAL and third-party libraries
     if ( AUTO_LINK_ENABLED )    
-      target_link_libraries(${exe_name} ${CGAL_3RD_PARTY_LIBRARIES} )
+      target_link_libraries(${target_name} ${CGAL_3RD_PARTY_LIBRARIES} )
     else()
-      target_link_libraries(${exe_name} ${CGAL_3RD_PARTY_LIBRARIES} ${CGAL_LIBRARIES} )
+      target_link_libraries(${target_name} ${CGAL_3RD_PARTY_LIBRARIES} ${CGAL_LIBRARIES} )
     endif()
   
   endif()
