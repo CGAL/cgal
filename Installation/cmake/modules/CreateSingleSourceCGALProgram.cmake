da490ca6984b9dd6db97dfad2f725863256a476a
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2008-09-05 12:16:37 +0000

    Partially revert revision 45319. The follwing has been revert:    "Use unique names for example, demo and test targets"

--- Installation/cmake/modules/CreateSingleSourceCGALProgram.cmake
+++ Installation/cmake/modules/CreateSingleSourceCGALProgram.cmake
@@ -1,4 +1,4 @@
-macro(create_single_source_cgal_program prefix first )
+macro(create_single_source_cgal_program first )
 
   if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${first})
   
@@ -10,18 +10,16 @@ macro(create_single_source_cgal_program prefix first )
     
     get_filename_component(exe_name ${first} NAME_WE)
     
-    set( target_name "${prefix}_${exe_name}" )
-    
-    add_executable(${target_name} ${all})
+    add_executable(${exe_name} ${all})
     
     set( CGAL_EXECUTABLE_TARGETS )
-    set( CGAL_EXECUTABLE_TARGETS "${CGAL_EXECUTABLE_TARGETS}" "${target_name}" CACHE INTERNAL "" FORCE )
+    set( CGAL_EXECUTABLE_TARGETS "${CGAL_EXECUTABLE_TARGETS}" "${exe_name}" CACHE INTERNAL "" FORCE )
     
     # Link the executable to CGAL and third-party libraries
     if ( AUTO_LINK_ENABLED )    
-      target_link_libraries(${target_name} ${CGAL_3RD_PARTY_LIBRARIES} )
+      target_link_libraries(${exe_name} ${CGAL_3RD_PARTY_LIBRARIES} )
     else()
-      target_link_libraries(${target_name} ${CGAL_3RD_PARTY_LIBRARIES} ${CGAL_LIBRARIES} )
+      target_link_libraries(${exe_name} ${CGAL_3RD_PARTY_LIBRARIES} ${CGAL_LIBRARIES} )
     endif()
   
   endif()
