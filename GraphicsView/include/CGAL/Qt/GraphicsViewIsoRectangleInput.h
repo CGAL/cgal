1c56306968fdc22b34d3ca2af6e2439d9ff42117
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2008-09-09 11:48:15 +0000

    Add input tool for iso-rectangles

---
+++ GraphicsView/include/CGAL/Qt/GraphicsViewIsoRectangleInput.h
@@ -0,0 +1,135 @@
+
+#ifndef CGAL_QT_GRAPHICS_VIEW_ISO_RECTANGLE_INPUT_H
+#define CGAL_QT_GRAPHICS_VIEW_ISO_RECTANGLE_INPUT_H
+
+#include <QGraphicsView>
+#include <QRectF>
+#include <QPointF>
+#include <QGraphicsItem>
+#include <QGraphicsRectItem> 
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QPainter>
+#include <QStyleOption>
+
+#include <CGAL/Qt/Converter.h>
+#include <CGAL/Qt/GraphicsViewInput.h>
+
+#include <CGAL/array.h>
+
+namespace CGAL {
+namespac Qt {
+
+template <typename K>
+class GraphicsViewIsoRectangleInput : public GraphicsViewInput
+{
+public:
+  GraphicsViewIsoRectangleInput(QGraphicsScene* s); 
+  ~GraphicsViewIsoRectangleInput();
+
+protected:
+    
+  virtual void mousePressEvent(QGraphicsSceneMouseEvent *event);
+  virtual void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+  virtual void keyPressEvent(QKeyEvent *event);
+  
+  bool eventFilter(QObject *obj, QEvent *event);
+  
+
+  
+
+private:
+
+  typedef typename K::Point_2 Point_2;
+  QPointF qp, qq, qr;
+  Point_2 p, q, r;
+  QGraphicsRectItem rectItem;
+  QPointF rect_first_point;
+  QGraphicsScene *scene_;  
+  Converter<K> convert;
+};
+
+
+template <typename K>
+GraphicsViewIsoRectangleInput<K>::GraphicsViewIsoRectangleInput(QGraphicsScene* s)
+  : rectItem(new QGraphicsRectItem),scene_(s)
+{
+  rectItem->setBrush(QBrush());
+  scene_->addItem(rectItem);
+  rectItem->hide();
+  rectItem->setZValue(10000);
+}
+
+
+template <typename K>
+GraphicsViewIsoRectangleInput<K>::~GraphicsViewIsoRectangleInput()
+{
+  delete rectItem;
+}
+
+
+template <typename K>
+void 
+GraphicsViewIsoRectangleInput<K>::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{ 
+  // todo: only do this if no modifiers are pressed at the same time
+  if(mouseEvent->button() == ::Qt::LeftButton) {
+    if(rectItem->isVisible()) {
+      // we have clicked twice
+      emit generate(CGAL::make_object(convert(rectItem->rect())));
+      rectItem->hide();
+    } else { 
+      // we enter a first point
+      rect_first_point = event->scenePos();
+      rectItem->setRect(QRectF(rect_first_point, rect_first_point));
+      rectItem->show();
+    }
+  }
+}
+
+
+template <typename K>
+void 
+GraphicsViewIsoRectangleInput<K>::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+  // todo: only do this if no modifiers are pressed at the same time
+  if(rectItem->isVisible()) {
+    rectItem->setRect(QRectF(rect_first_point,
+			     event->scenePos()));
+  }
+}
+
+
+template <typename K>
+void 
+GraphicsViewIsoRectangleInput<K>::keyPressEvent ( QKeyEvent * event ) 
+{
+}
+
+
+
+template <typename K>
+bool 
+GraphicsViewIsoRectangleInput<K>::eventFilter(QObject *obj, QEvent *event)
+{
+  if (event->type() == QEvent::GraphicsSceneMousePress) {
+    QGraphicsSceneMouseEvent *mouseEvent = static_cast<QGraphicsSceneMouseEvent *>(event);
+    mousePressEvent(mouseEvent);
+    return true;
+  } else if (event->type() == QEvent::GraphicsSceneMouseMove) {
+    QGraphicsSceneMouseEvent *mouseEvent = static_cast<QGraphicsSceneMouseEvent *>(event);
+    mouseMoveEvent(mouseEvent);
+    return true;
+  } else if (event->type() == QEvent::KeyPress) {
+    QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+    keyPressEvent(keyEvent);
+    return true;
+  } else{
+    // standard event processing
+    return QObject::eventFilter(obj, event);
+  }
+} 
+
+} // namespace CGAL
+
+#endif // CGAL_QT_GRAPHICS_VIEW_ISO_RECTANGLE_INPUT_H
