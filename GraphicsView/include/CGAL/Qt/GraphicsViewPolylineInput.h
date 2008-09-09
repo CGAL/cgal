a3d016def5c74e55a261d29dd0d51c6b402d3070
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2008-09-09 11:56:57 +0000

    Added license header

--- GraphicsView/include/CGAL/Qt/GraphicsViewPolylineInput.h
+++ GraphicsView/include/CGAL/Qt/GraphicsViewPolylineInput.h
@@ -1,3 +1,22 @@
+// Copyright (c) 2008  GeometryFactory Sarl (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : Andreas Fabri <Andreas.Fabri@geometryfactory.com>
+//                 Laurent Rineau <Laurent.Rineau@geometryfactory.com>
 
 #ifndef CGAL_QT_GRAPHICS_VIEW_POLYLINE_INPUT_H
 #define CGAL_QT_GRAPHICS_VIEW_POLYLINE_INPUT_H
