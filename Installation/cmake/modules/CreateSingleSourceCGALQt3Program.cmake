b52c7a09e93300db875e33c2a4be0d4c01d206e7
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2008-09-24 10:32:44 +0000

    - Fix soname and soversion of libraries (they were exchanged). - Make get_dependency_version found the print_${LIB}_version.cpp, even   if the lib is compiled separately. - Rename FindQt3.cmake to FindQt3-patched.cmake and Qt3Macros to   Qt3Macros-patched. - Make CGAL-Qt3 compilable separately. - Fix all commited CMakeLists.txt:     - use Qt3-patched and Qt3Macros-patched, and s/QT_/QT3_/     - add a comment about FindQt3-patched,     - wrap target creation with if(CGAL_FOUND AND QT3_FOUND)/else/endif - Modified cgal_create_cmake_script

--- Installation/cmake/modules/CreateSingleSourceCGALQt3Program.cmake
+++ Installation/cmake/modules/CreateSingleSourceCGALQt3Program.cmake
@@ -15,7 +15,7 @@ macro(create_single_source_cgal_qt3_program first )
     include_directories(BEFORE ${CMAKE_CURRENT_BINARY_DIR})
     include_directories(BEFORE ${CMAKE_CURRENT_SOURCE_DIR})
     
-    include_directories( ${QT_INCLUDE_DIR} )
+    include_directories( ${QT3_INCLUDE_DIR} )
     
     add_executable  (${exe_name} ${all})
   
@@ -23,9 +23,9 @@ macro(create_single_source_cgal_qt3_program first )
     
     # Link the executable to CGAL and third-party libraries
     if ( AUTO_LINK_ENABLED )    
-      target_link_libraries(${exe_name} ${CGAL_3RD_PARTY_LIBRARIES} ${QT_LIBRARIES} )
+      target_link_libraries(${exe_name} ${CGAL_3RD_PARTY_LIBRARIES} ${QT3_LIBRARIES} )
     else()
-      target_link_libraries(${exe_name} ${CGAL_LIBRARIES} ${CGAL_QT_LIBRARIES} ${CGAL_3RD_PARTY_LIBRARIES} ${QT_LIBRARIES})
+      target_link_libraries(${exe_name} ${CGAL_LIBRARIES} ${CGAL_QT_LIBRARIES} ${CGAL_3RD_PARTY_LIBRARIES} ${QT3_LIBRARIES})
     endif()
     
   endif(EXISTS ${first})
