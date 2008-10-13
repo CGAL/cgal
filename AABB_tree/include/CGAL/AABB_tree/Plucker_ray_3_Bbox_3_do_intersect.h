2465573f24f120ed07b5e1c622e1e501b34e8571
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2008-10-13 18:41:47 +0000

    Add license header

--- AABB_tree/include/CGAL/AABB_tree/Plucker_ray_3_Bbox_3_do_intersect.h
+++ AABB_tree/include/CGAL/AABB_tree/Plucker_ray_3_Bbox_3_do_intersect.h
@@ -1,3 +1,21 @@
+// Copyright (c) 2008  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : 
 #ifndef CGAL_PLUCKER_RAY_3_BBOX_3_DO_INTERSECT_H
 #define CGAL_PLUCKER_RAY_3_BBOX_3_DO_INTERSECT_H
 
