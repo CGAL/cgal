620de7792e05109b2df8dd3d21c51574ad0c2756
Fernando Cacciola <fernando.cacciola@geometryfactory.com>
2008-10-17 19:04:04 +0000

    LEDA support integrated into CGAL itself

--- Installation/cmake/modules/CGAL_SetupLEDA.cmake
+++ Installation/cmake/modules/CGAL_SetupLEDA.cmake
@@ -8,15 +8,15 @@ if ( NOT LEDA_FOUND )
     message( STATUS "LEDA libraries:   ${LEDA_LIBRARIES}" )
     message( STATUS "LEDA definitions: ${LEDA_DEFINITIONS}" )
     
+    set ( CGAL_USE_LEDA 1 )
+    
+    include(CGAL_Macros)
+    
+    cache_set(CGAL_3RD_PARTY_INCLUDE_DIRS ${CGAL_3RD_PARTY_INCLUDE_DIRS} ${LEDA_INCLUDE_DIR} "" )
+    cache_set(CGAL_3RD_PARTY_LIBRARIES    ${CGAL_3RD_PARTY_LIBRARIES}    ${LEDA_LIBRARIES}   "" )
+    cache_set(CGAL_3RD_PARTY_DEFINITIONS  ${CGAL_3RD_PARTY_DEFINITIONS}  ${LEDA_DEFINITIONS} "" )
+    
     #get_dependency_version(LEDA)
-    
-    include_directories ( ${LEDA_INCLUDE_DIR} )     
-          
-    add_definitions( ${LEDA_DEFINITIONS} "-DCGAL_USE_LEDA=1" )
-  
-    link_directories( ${LEDA_LIBRARIES_DIR} )
-    
-    set( CGAL_3RD_PARTY_LIBRARIES  ${CGAL_3RD_PARTY_LIBRARIES} ${LEDA_LIBRARIES} )
     
   endif()
   
