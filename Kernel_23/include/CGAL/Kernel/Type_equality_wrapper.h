bde0aee592205612896ce03d49eb2157ffa1537c
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2008-10-21 12:29:38 +0000

    In deep derivation hierarchies VC8 has problems when we use the same name for a template parameter  at several hierarchy levels, so we rename it here

--- Kernel_23/include/CGAL/Kernel/Type_equality_wrapper.h
+++ Kernel_23/include/CGAL/Kernel/Type_equality_wrapper.h
@@ -32,20 +32,20 @@ CGAL_BEGIN_NAMESPACE
 // Kernel::Point_2 and CGAL::Point_2<Kernel>, by deriving from
 // K_base::Point_2 (and similar for the other types).
 
-template < typename K_base, typename Kernel >
+template < typename K_base, typename Kernel_ >
 struct Type_equality_wrapper
   : public K_base
 {
     typedef K_base                                  Kernel_base;
 
-#define CGAL_Kernel_obj(X)   typedef CGAL::X<Kernel> X;
+#define CGAL_Kernel_obj(X)   typedef CGAL::X<Kernel_> X;
 
 #include <CGAL/Kernel/interface_macros.h>
 
     // Undocumented stuff.
-    typedef CGAL::Conic_2<Kernel>                   Conic_2;
-    typedef CGAL::Aff_transformation_2<Kernel>      Aff_transformation_2;
-    typedef CGAL::Aff_transformation_3<Kernel>      Aff_transformation_3;
+    typedef CGAL::Conic_2<Kernel_>                   Conic_2;
+    typedef CGAL::Aff_transformation_2<Kernel_>      Aff_transformation_2;
+    typedef CGAL::Aff_transformation_3<Kernel_>      Aff_transformation_3;
 };
 
 CGAL_END_NAMESPACE
