db6c20f1a705e0d107fcf89a28e10599222b4441
Pierre Alliez <Pierre.Alliez@sophia.inria.fr>
2008-10-21 08:32:16 +0000

    polyhedron demo: code cleanup in mainwindow.h

--- Polyhedron/demo/Polyhedron/MainWindow.h
+++ Polyhedron/demo/Polyhedron/MainWindow.h
@@ -39,26 +39,26 @@ protected slots:
   void readSettings();
   void writeSettings();
 
-
+	// load, erase, duplicate
+  void on_actionEraseAll_triggered();
   void on_actionLoadPolyhedron_triggered();
   bool on_actionErasePolyhedron_triggered();
-  void on_actionEraseAll_triggered();
   void on_actionDuplicatePolyhedron_triggered();
 
+	// activate
   void on_actionActivatePolyhedron_triggered();
   void on_actionSetPolyhedronA_triggered();
   void on_actionSetPolyhedronB_triggered();
+
+	// inside out
   void on_actionInsideOut_triggered();
 
   // defined in MainWindow_parameterization.cpp
   void on_actionMVC_triggered();
   void on_actionDCP_triggered();
 
-  // save
-  // TODO: save all, save current (do we store the current file name?)
-  void on_actionSaveAs_triggered(); // save selected polyhedron as...
-
-  // merge (TODO)
+	// save selected polyhedron as...
+	void on_actionSaveAs_triggered(); 
 
   // defined in MainWindow_simplify.cpp
   void on_actionSimplify_triggered();
@@ -69,29 +69,28 @@ protected slots:
   // defined in MainWindow_kernel.cpp
   void on_actionKernel_triggered();
 
-  // subdivision methods are defined in MainWindow_subdivision_methods.cpp
+  // subdivision methods, in MainWindow_subdivision_methods.cpp
   void on_actionLoop_triggered();
   void on_actionCatmullClark_triggered();
   void on_actionSqrt3_triggered();
 
-  // Boolean operations defined in MainWindow_boolean_operations.cpp
+  // Boolean operations, in MainWindow_boolean_operations.cpp
   void on_actionUnion_triggered();
-  void on_actionIntersection_triggered();
   void on_actionDifference_triggered();
+  void on_actionIntersection_triggered();
 
   // curvature estimation, in MainWindow_curvature_estimation.cpp
   void on_actionEstimateCurvature_triggered();
 
   // PCA, in MainWindow_pca.cpp
-  void on_actionFitPlane_triggered();
   void on_actionFitLine_triggered();
+  void on_actionFitPlane_triggered();
 
   // self intersection, in MainWindow_self_intersection.cpp
   void on_actionSelfIntersection_triggered();
 
   // remeshing, in MainWindow_remeshing.cpp
   void on_actionRemeshing_triggered();
-
 
 
 protected:
