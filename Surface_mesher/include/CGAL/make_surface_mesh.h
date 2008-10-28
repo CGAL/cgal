afa5e5203bbfa2700181d5a8037d524cf33baf42
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2008-10-28 17:42:27 +0000

    Split <CGAL/make_surface_mesh.h> in two files. <CGAL/Surface_mesher_generator.h> will not be documented. It can be used to apply rules step be steps.

--- Surface_mesher/include/CGAL/make_surface_mesh.h
+++ Surface_mesher/include/CGAL/make_surface_mesh.h
@@ -19,12 +19,8 @@
 #ifndef CGAL_MAKE_SURFACE_MESH_H
 #define CGAL_MAKE_SURFACE_MESH_H
 
-#include <CGAL/Surface_mesher/Surface_mesher.h>
-#include <CGAL/Surface_mesher/Surface_mesher_manifold.h>
-#include <CGAL/Surface_mesher/Surface_mesher_edges_level.h>
-#include <CGAL/Surface_mesher/Surface_mesher_visitor.h>
-#include <CGAL/Surface_mesher/Surface_mesher_edges_level_visitor.h>
-#include <CGAL/Surface_mesh_traits_generator_3.h>
+#include <CGAL/Surface_mesher_generator.h>
+
 #include <CGAL/Surface_mesh_complex_2_in_triangulation_3.h>
 
 #include <CGAL/Surface_mesh_cell_base_3.h>
@@ -32,100 +28,17 @@
 
 #include <CGAL/Surface_mesh_default_criteria_3.h>
 
-#include <CGAL/Surface_mesher/Has_edges.h>
-
 #include <CGAL/iterator.h> // CGAL::inserter()
 
+#include <CGAL/Surface_mesher/Verbose_flag.h>
+
+#ifdef CGAL_SURFACE_MESHER_VERBOSE
+#  define CGAL_SURFACE_MESHER_VERBOSITY ::CGAL::Surface_mesher::VERBOSE
+#else
+#  define CGAL_SURFACE_MESHER_VERBOSITY ::CGAL::Surface_mesher::NOT_VERBOSE
+#endif
+
 namespace CGAL {
-
-  struct Non_manifold_tag {};
-  struct Manifold_tag {};
-  struct Manifold_with_boundary_tag {};
-
-  template <
-    typename C2T3,
-    typename SurfaceMeshTraits_3,
-    typename Criteria,
-    typename Tag // generic version: generates a compile time error
-  >
-  struct Make_surface_mesh_helper {
-    template <typename T>
-    struct Tag_does_not_exist_error {};
-    typedef Tag_does_not_exist_error<Tag> Mesher_base;
-  };
-
-  template <
-    typename C2T3,
-    typename SurfaceMeshTraits_3,
-    typename Criteria
-  >
-  struct Make_surface_mesh_helper<
-    C2T3,
-    SurfaceMeshTraits_3,
-    Criteria,
-    Non_manifold_tag> // Non_manifold_tag partial specialization
-  {
-    typedef Surface_mesher::Surface_mesher_base<
-      C2T3,
-      typename SurfaceMeshTraits_3::Surface_3,
-      SurfaceMeshTraits_3,
-      Criteria> Mesher_base;
-  };
-
-  template <
-    typename C2T3,
-    typename SurfaceMeshTraits_3,
-    typename Criteria
-  >
-  struct Make_surface_mesh_helper<
-    C2T3,
-    SurfaceMeshTraits_3,
-    Criteria,
-    Manifold_with_boundary_tag> // Manifold_with_boundary_tag partial
-                                // specialization
-  {
-    typedef Surface_mesher::Surface_mesher_regular_edges_base<
-      C2T3,
-      typename SurfaceMeshTraits_3::Surface_3,
-      SurfaceMeshTraits_3,
-      Criteria,
-      true> /* true means "with boundary"*/ Regular_edge_base;
-
-    typedef Surface_mesher::Surface_mesher_manifold_base<
-      C2T3,
-      typename SurfaceMeshTraits_3::Surface_3,
-      SurfaceMeshTraits_3,
-      Criteria,
-      Regular_edge_base
-      > Mesher_base;
-  };
-
-  template <
-    typename C2T3,
-    typename SurfaceMeshTraits_3,
-    typename Criteria
-  >
-  struct Make_surface_mesh_helper<
-    C2T3,
-    SurfaceMeshTraits_3,
-    Criteria,
-    Manifold_tag> // Manifold_tag partial specialization
-  {
-    typedef Surface_mesher::Surface_mesher_regular_edges_base<
-      C2T3,
-      typename SurfaceMeshTraits_3::Surface_3,
-      SurfaceMeshTraits_3,
-      Criteria> Regular_edge_without_boundary_base;
-
-    typedef Surface_mesher::Surface_mesher_manifold_base<
-      C2T3,
-      typename SurfaceMeshTraits_3::Surface_3,
-      SurfaceMeshTraits_3,
-      Criteria,
-      Regular_edge_without_boundary_base
-      > Mesher_base;
-  };
-
 template <typename C2T3,
 	  typename Surface,
 	  typename Criteria,
@@ -154,21 +67,28 @@ void make_surface_mesh(C2T3& c2t3,
                        Tag,
                        int initial_number_of_points = 20)
 {
-  typedef typename Make_surface_mesh_helper<
+//   typedef typename Make_surface_mesh_helper<
+//     C2T3,
+//     SurfaceMeshTraits_3,
+//     Criteria,
+//     Tag>::Mesher_base Mesher_base;
+
+// #ifdef CGAL_SURFACE_MESHER_VERBOSE
+//   typedef Surface_mesher::Surface_mesher<
+//     Mesher_base,
+//     typename Surface_mesher::details::Facet_generator<Mesher_base>::type,
+//     Null_mesher_level,
+//     Surface_mesher::VERBOSE> Mesher;
+// #else
+//   typedef Surface_mesher::Surface_mesher<Mesher_base> Mesher;
+// #endif
+
+  typedef typename Surface_mesher_generator<
     C2T3,
     SurfaceMeshTraits_3,
     Criteria,
-    Tag>::Mesher_base Mesher_base;
-
-#ifdef CGAL_SURFACE_MESHER_VERBOSE
-  typedef Surface_mesher::Surface_mesher<
-    Mesher_base,
-    typename Surface_mesher::details::Facet_generator<Mesher_base>::type,
-    Null_mesher_level,
-    Surface_mesher::VERBOSE> Mesher;
-#else
-  typedef Surface_mesher::Surface_mesher<Mesher_base> Mesher;
-#endif
+    Tag,
+    CGAL_SURFACE_MESHER_VERBOSITY >::type Mesher;
 
   typename SurfaceMeshTraits_3::Construct_initial_points get_initial_points =
     surface_mesh_traits.construct_initial_points_object();
