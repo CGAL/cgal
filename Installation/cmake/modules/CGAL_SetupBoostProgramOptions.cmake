f7f4320e16098a037cd638d6f6ca65c5175c122e
Fernando Cacciola <fernando.cacciola@geometryfactory.com>
2008-10-30 15:26:31 +0000

    Added CGAL_SetupBoostProgramOptions

---
+++ Installation/cmake/modules/CGAL_SetupBoostProgramOptions.cmake
@@ -0,0 +1,31 @@
+if ( NOT Boost_PROGRAM_OPTIONS_FOUND )
+  
+  if ( NOT BUILD_SHARED_LIBS )
+    set(Boost_USE_STATIC_LIBS ON)
+  endif()
+  
+  set(Boost_FIND_VERSION 1.33.1 )
+  set(Boost_FIND_VERSION_MAJOR 1 )
+  set(Boost_FIND_VERSION_MINOR 33 )
+  set(Boost_FIND_VERSION_PATCH 1 )
+  
+  find_package( Boost QUIET COMPONENTS program_options )
+  
+  if ( Boost_PROGRAM_OPTIONS_FOUND )
+  
+    message( STATUS "Boost.ProgramOptions library: ${Boost_PROGRAM_OPTIONS_LIBRARY}" )
+        
+    add_definitions( "-DCGAL_USE_BOOST_PROGRAM_OPTIONS" )
+    
+    if ( NOT MSVC )
+      link_libraries( ${Boost_PROGRAM_OPTIONS_LIBRARY} )
+    endif()
+  
+  else()
+  
+    message( STATUS "Could not find Boost.ProgramOptions library" )
+    
+  endif()
+  
+endif()
+
