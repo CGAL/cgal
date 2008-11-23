dbf39bf4b541ecb1d8f491798f4c7a6b66709523
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2008-11-23 14:42:54 +0000

    Split Polynomial testsuit as VC++ is running out of heap space

---
+++ Polynomial/test/Polynomial/Polynomial_using_leda.cpp
@@ -0,0 +1,21 @@
+
+#include "test_polynomial.h"
+
+int main() {
+  
+    // Set wrong rounding mode to test modular arithmetic 
+    CGAL::Protect_FPU_rounding<true> pfr(CGAL_FE_UPWARD);
+
+    CGAL::set_pretty_mode(std::cout);     
+
+#ifdef CGAL_USE_LEDA
+    {        
+        typedef CGAL::LEDA_arithmetic_kernel AT;
+        test_AT<AT>();
+    }
+#endif
+
+    return 0;
+}
+
+
