7d5e6b119c9f79c0094e917ef1e4d8e236893f59
Pierre Alliez <Pierre.Alliez@sophia.inria.fr>
2008-12-02 15:19:30 +0000

    back to only points for bounding box

--- Principal_component_analysis/examples/Principal_component_analysis/bounding_box.cpp
+++ Principal_component_analysis/examples/Principal_component_analysis/bounding_box.cpp
@@ -1,5 +1,4 @@
-// Example program for the bounding_box() function for 2D points,
-// 3D points and 3D segments.
+// Example program for the bounding_box() function for 2D points and 3D points.
 
 #include <CGAL/Cartesian.h>
 #include <CGAL/bounding_box.h>
@@ -12,7 +11,6 @@ typedef CGAL::Cartesian<FT> K;
 typedef K::Point_2          Point_2;
 typedef K::Point_3          Point_3;
 typedef K::Point_3          Point_3;
-typedef K::Segment_3        Segment_3;
 
 int main()
 {
@@ -32,17 +30,6 @@ int main()
   points_3.push_back(Point_3(3.0, 5.0, 4.5));
 
   K::Iso_cuboid_3 c3 = CGAL::bounding_box(points_3.begin(), points_3.end());
-  std::cout << c3 << std::endl;
-
-  // axis-aligned bounding box of 3D segments
-  std::list<Segment_3> segments_3;
-  Point_3 p(1.0, 2.0, 3.0);
-  Point_3 q(4.0, 5.0, 6.0);
-  Point_3 r(3.0, 3.0, 0.5);
-  segments_3.push_back(Segment_3(p,q));
-  segments_3.push_back(Segment_3(p,r));
-
-  c3 = CGAL::bounding_box(segments_3.begin(), segments_3.end());
   std::cout << c3 << std::endl;
 
   return 0;
