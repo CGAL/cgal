c84186a37208b62543f643fa523ec28ccd37bdcd
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2008-12-06 21:45:17 +0000

    Add license header

--- Algebraic_foundations/include/CGAL/ipower.h
+++ Algebraic_foundations/include/CGAL/ipower.h
@@ -1,15 +1,27 @@
-// TODO: Add licence
+// Copyright (c) 2008 Max-Planck-Institute Saarbruecken (Germany).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://afabri@scm.gforge.inria.fr/svn/cgal/trunk/Polynomial/include/CGAL/Polynomial.h $
+// $Id: Polynomial.h 47254 2008-12-06 21:18:27Z afabri $
+// 
+//
+// Author(s)     : Michael Hemmer// TODO: Add licence
 //
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
 // $URL:$
-// $Id:$
-// 
-//
-// Author(s)     : 
-// 
-// ============================================================================
 
 #ifndef CGAL_POLYNOMIAL_IPOWER_H
 #define CGAL_POLYNOMIAL_IPOWER_H
