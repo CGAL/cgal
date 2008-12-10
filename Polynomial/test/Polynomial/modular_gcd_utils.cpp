85ee2f262fa6b4bfc9bd0173799ccaee8d29cadd
Michael Hemmer <hemmer@mpi-inf.mpg.de>
2008-12-10 10:42:10 +0000

    Enforce IEEE double precision and to nearest before using modular arithmetic

--- Polynomial/test/Polynomial/modular_gcd_utils.cpp
+++ Polynomial/test/Polynomial/modular_gcd_utils.cpp
@@ -64,9 +64,6 @@ void test_modular_gcd_utils() {
   }
     
   {  
-    // testing univariate polynomials with modular coefficients
-    // Enforce IEEE double precision before using modular arithmetic
-    CGAL::Set_ieee_double_precision pfr;
     typedef CGAL::Polynomial<Integer> Poly;
     typedef typename CGAL::Modular_traits<Poly>::Residue_type   MPoly;
     CGAL::Residue::set_current_prime(43);
@@ -101,10 +98,7 @@ void test_modular_gcd_utils() {
     }
   }
 
-  {  
-    // testing univariate polynomials with modular sqrt coefficients
-    // Enforce IEEE double precision before using modular arithmetic
-    CGAL::Set_ieee_double_precision pfr;  
+  { 
     typedef CGAL::Sqrt_extension<Integer,Integer> EXT;
     typedef CGAL::Polynomial<EXT>  Poly_Ext;
     typedef typename CGAL::Modular_traits<Poly_Ext>::Residue_type   MPoly_Ext;
@@ -140,10 +134,9 @@ void test_modular_gcd_utils() {
   }
 }
 int main(){
-    
-  // Set wrong rounding mode to test modular arithmetic 
-  CGAL::Set_ieee_double_precision pfr;
-
+  // Enforce IEEE double precision and to nearest before using modular arithmetic
+  CGAL::Protect_FPU_rounding<true> pfr(CGAL_FE_TONEAREST);  
+ 
 #ifdef CGAL_USE_LEDA
   test_modular_gcd_utils<CGAL::LEDA_arithmetic_kernel>();       
 #endif // CGAL_USE_LEDA    
