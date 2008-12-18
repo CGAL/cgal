e8cb05259760dde1b96b63fc4ed2dd5338426991
Pierre Alliez <Pierre.Alliez@sophia.inria.fr>
2008-12-18 13:47:12 +0000

    polyhedron demo: removed two unused menus and one comment

--- Polyhedron/demo/Polyhedron/Polyhedron_3.cpp
+++ Polyhedron/demo/Polyhedron/Polyhedron_3.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
   // Import resources from libCGALQt4.
   // See http://doc.trolltech.com/4.4/qdir.html#Q_INIT_RESOURCE
   Q_INIT_RESOURCE(File);
-  Q_INIT_RESOURCE(Triangulation_2); // PA: sounds weird here
+  Q_INIT_RESOURCE(Triangulation_2); 
   Q_INIT_RESOURCE(Input);
   Q_INIT_RESOURCE(CGAL);
 
