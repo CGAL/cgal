cc92427102ba02119a48b9f3db7e3602739532cc
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2009-02-27 09:52:31 +0000

    - Automatic update of the ref manual for PA. - Fixed compilation error in jet_smoothing_example.cpp  for PA.

--- Surface_reconstruction_3/examples/Surface_reconstruction_3/jet_smoothing_example.cpp
+++ Surface_reconstruction_3/examples/Surface_reconstruction_3/jet_smoothing_example.cpp
@@ -11,20 +11,20 @@ typedef Kernel::Point_3 Point;
 
 int main(void)
 {
-		// generate point set
+    // generate point set
     std::deque<Point> points;
-		points.push_back( 0.0, 0.0, 0.01);
-		points.push_back(-0.1,-0.1, 0.02);
-		points.push_back(-0.1,-0.2, 0.01);
-		points.push_back(-0.1, 0.1, 0.02);
-		points.push_back( 0.1,-0.1, 0.00);
-		points.push_back( 0.1, 0.2, 0.01);
-		points.push_back( 0.2, 0.0, 0.02);
+    points.push_back(Point( 0.0, 0.0, 0.01));
+    points.push_back(Point(-0.1,-0.1, 0.02));
+    points.push_back(Point(-0.1,-0.2, 0.01));
+    points.push_back(Point(-0.1, 0.1, 0.02));
+    points.push_back(Point( 0.1,-0.1, 0.00));
+    points.push_back(Point( 0.1, 0.2, 0.01));
+    points.push_back(Point( 0.2, 0.0, 0.02));
 
-		// smoothing
+    // smoothing
     std::deque<Point> output;
-		const unsigned int nb_neighbors = 5;
-		CGAL::jet_smoothing_3(points.begin(),points.end(),std::back_inserter(output),nb_neighbors);
+    const unsigned int nb_neighbors = 5;
+    CGAL::jet_smoothing_3(points.begin(),points.end(),std::back_inserter(output),nb_neighbors);
 
     return EXIT_SUCCESS;
 }
