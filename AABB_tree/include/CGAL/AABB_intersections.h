3a3c75c66efce0ad8340e46d0e59adf742d9054d
Pierre Alliez <Pierre.Alliez@sophia.inria.fr>
2009-04-26 17:32:34 +0000

    AABB tree: rename intersection files

--- AABB_tree/include/CGAL/AABB_intersections.h
+++ AABB_tree/include/CGAL/AABB_intersections.h
@@ -1,12 +1,16 @@
 // TODO: move later to the kernel folder
 
 #include <CGAL/intersections.h>
-#include <CGAL/Ray_3_Bbox_3_do_intersect.h>
-#include <CGAL/Bbox_3_Bbox_3_do_intersect.h>
-#include <CGAL/Segment_3_Bbox_3_do_intersect.h>
-#include <CGAL/Plane_3_Bbox_3_do_intersect.h>
-#include <CGAL/Triangle_3_Bbox_3_do_intersect.h>
-#include <CGAL/Line_3_Bbox_3_do_intersect.h>
-#include <CGAL/Sphere_3_Bbox_do_intersect.h>
+
+#include <CGAL/Bbox_3_ray_3_do_intersect.h>
+#include <CGAL/Bbox_3_bbox_3_do_intersect.h>
+#include <CGAL/Bbox_3_segment_3_do_intersect.h>
+#include <CGAL/Bbox_3_plane_3_do_intersect.h>
+#include <CGAL/Bbox_3_triangle_3_do_intersect.h>
+#include <CGAL/Bbox_3_line_3_do_intersect.h>
+#include <CGAL/Bbox_3_sphere_3_do_intersect.h>
+
 #include <CGAL/Triangle_3_segment_3_intersection.h>
 #include <CGAL/Triangle_3_ray_3_intersection.h>
+#include <CGAL/Triangle_3_line_3_intersection.h>
+#include <CGAL/Triangle_3_plane_3_intersection.h>
