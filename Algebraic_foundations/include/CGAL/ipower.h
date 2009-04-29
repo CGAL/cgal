3753a9e3376ec107db5858447557a6af629fba7d
Michael Hemmer <hemmer@mpi-inf.mpg.de>
2009-04-29 13:53:31 +0000

    added version for long

--- Algebraic_foundations/include/CGAL/ipower.h
+++ Algebraic_foundations/include/CGAL/ipower.h
@@ -52,6 +52,29 @@ NT ipower(const NT& base, int expn) {
     return res;
 }
 
+template <typename NT>
+inline
+NT ipower(const NT& base, long expn) {
+    // compute base^expn using square-and-multiply
+    CGAL_precondition(expn >= 0);
+    
+    // handle trivial cases efficiently
+    if (expn == 0) return NT(1);
+    if (expn == 1) return base;
+    
+    // find the most significant non-zero bit of expn
+    int e = expn, msb = 0;
+    while (e >>= 1) msb++;
+    
+    // computing base^expn by square-and-multiply
+    NT res = base;
+    int b = 1<<msb;
+    while (b >>= 1) { // is there another bit right of what we saw so far?
+        res *= res;
+        if (expn & b) res *= base;
+    }
+    return res;
+}
 
 CGAL_END_NAMESPACE
 
