a6cf3261543bf92f20165274d6ff562ac7daed73
Pierre Alliez <Pierre.Alliez@sophia.inria.fr>
2009-05-04 20:08:00 +0000

    AABB tree: - examples primitive files in a separate folder - more on doc from second review.

--- AABB_tree/examples/AABB_tree/AABB_segment_3_example.cpp
+++ AABB_tree/examples/AABB_tree/AABB_segment_3_example.cpp
@@ -27,7 +27,7 @@
 
 #include <CGAL/AABB_tree.h>
 #include <CGAL/AABB_traits.h>
-#include <CGAL/AABB_segment_primitive.h>
+#include <CGAL/primitives/AABB_segment_primitive.h>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
--- AABB_tree/examples/AABB_tree/AABB_triangle_3_example.cpp
+++ AABB_tree/examples/AABB_tree/AABB_triangle_3_example.cpp
@@ -27,7 +27,7 @@
 
 #include <CGAL/AABB_tree.h>
 #include <CGAL/AABB_traits.h>
-#include <CGAL/AABB_triangle_primitive.h>
+#include <CGAL/primitives/AABB_triangle_primitive.h>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
