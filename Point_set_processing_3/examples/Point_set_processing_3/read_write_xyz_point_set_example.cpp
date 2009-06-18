584f0e513140dad3625bbf999c3ac90f3d2a44e0
Pierre Alliez <Pierre.Alliez@sophia.inria.fr>
2009-06-18 10:09:44 +0000

    point set processing: user manual: intro + io

--- Point_set_processing_3/examples/Point_set_processing_3/read_write_xyz_point_set_example.cpp
+++ Point_set_processing_3/examples/Point_set_processing_3/read_write_xyz_point_set_example.cpp
@@ -11,38 +11,39 @@ typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef Kernel::Point_3 Point;
 typedef Kernel::Vector_3 Vector;
 
-// Point with normal vector stored in a std::pair.
-typedef std::pair<Point, Vector> PointVectorPair; 
+// Point with normal vector stored as a std::pair.
+typedef std::pair<Point, Vector> Pwn; 
 
 int main(void)
 {
     // Reads a .xyz point set file in points[].
-    // Note: read_xyz_points_and_normals() requires an output iterator over points
-    // + property maps to access each point's position and normal.
-    std::vector<PointVectorPair> points;
+    // Note: read_xyz_points_and_normals() requires an output iterator
+    // over points and as well as property maps to access each
+    // point position and normal.
+    std::vector<Pwn> points;
     std::ifstream in("data/oni.xyz");
     if (!in ||
         !CGAL::read_xyz_points_and_normals(
             in,std::back_inserter(points),
-            CGAL::First_of_pair_property_map<PointVectorPair>(),
-            CGAL::Second_of_pair_property_map<PointVectorPair>()))
+            CGAL::First_of_pair_property_map<Pwn>(),
+            CGAL::Second_of_pair_property_map<Pwn>()))
     {
       return EXIT_FAILURE;
     }
 
     // Saves point set.
-    // Note: write_xyz_points_and_normals() requires an output iterator over points
-    // + property maps to access each point's position and normal.
+    // Note: write_xyz_points_and_normals() requires an output iterator
+    // over points as well as property maps to access each
+    // point position and normal.
     std::ofstream out("oni_copy.xyz");
     if (!out ||
         !CGAL::write_xyz_points_and_normals(
             out, points.begin(), points.end(),
-            CGAL::First_of_pair_property_map<PointVectorPair>(),
-            CGAL::Second_of_pair_property_map<PointVectorPair>()))
+            CGAL::First_of_pair_property_map<Pwn>(),
+            CGAL::Second_of_pair_property_map<Pwn>()))
     {
       return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
 }
-
