cae412f83499f6ae6cda3ca56bb953c5a4035c10
Nader Salman <Nader.Salman@sophia.inria.fr>
2009-06-18 09:12:49 +0000

    added a small doc on the dereference_property_map in io.tex added a small example on the use of CGAL::Dereference_property_map<T> in remove_outliers_example.cpp

--- Point_set_processing_3/examples/Point_set_processing_3/remove_outliers_example.cpp
+++ Point_set_processing_3/examples/Point_set_processing_3/remove_outliers_example.cpp
@@ -1,4 +1,5 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/property_map.h>
 #include <CGAL/remove_outliers.h>
 #include <CGAL/IO/read_xyz_points.h>
 
@@ -12,19 +13,24 @@ typedef Kernel::Point_3 Point;
 int main(void)
 {
   // Reads a .xyz point set file in points[].
+  // The dereference property map can be omitted here as it is the default value.
   std::vector<Point> points;
   std::ifstream stream("data/oni.xyz");
   if (!stream ||
-      !CGAL::read_xyz_points(stream, std::back_inserter(points)))
+      !CGAL::read_xyz_points(stream, std::back_inserter(points),
+                             CGAL::Dereference_property_map<Point>()))
   {
     return EXIT_FAILURE;
   }
 
-  // Removes outliers using erase-remove idiom
+  // Removes outliers using erase-remove idiom.
+  // The dereference property map can be omitted here as it is the default value.
   const double removed_percentage = 5.0; // percentage of points to remove
   const int nb_neighbors = 7; // considers 7 nearest neighbor points
-  points.erase(CGAL::remove_outliers(points.begin(), points.end(), nb_neighbors, removed_percentage),
-               points.end());
+  points.erase( 
+         CGAL::remove_outliers(points.begin(), points.end(),
+                               CGAL::Dereference_property_map<Point>(),
+                               nb_neighbors,removed_percentage), points.end());
 
   // Optional: after erase(), use Scott Meyer's "swap trick" to trim excess capacity
   std::vector<Point>(points).swap(points);
