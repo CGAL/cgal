546e8eeaf69eb4c4c7ae49621a08d0ff2224cc2b
Nader Salman <Nader.Salman@sophia.inria.fr>
2009-06-26 16:21:25 +0000

    Added funtion in arcball : get_camera_position

--- Surface_reconstruction_points_3/demo/Surface_reconstruction_points_3/poisson/include/arcball.h
+++ Surface_reconstruction_points_3/demo/Surface_reconstruction_points_3/poisson/include/arcball.h
@@ -3,7 +3,7 @@
 
 class Arcball 
 {
- private:
+private:
   // xside = half of the width of the window
   // yside = half of the height of the window
   // side = min(xside, yside)
@@ -13,25 +13,30 @@ class Arcball
   float size;
   Director* b;
 
- public:
+public:
+
+  const float* get_camera_position()
+  {
+    return b->center();
+  }
 
   Director* setDirector(float max_size,
-                        float cx, 
-                        float cy, 
-                        float cz)
+    float cx, 
+    float cy, 
+    float cz)
   {
     Director* b = new Director();
     b->radius() = 1.0f/max_size;
     b->setOffset(cx, cy, cz);
     return b;
   }
-  
+
   Arcball(int width,
-          int height,
-          float max_size, 
-          float cx,
-          float cy,
-          float cz )
+    int height,
+    float max_size, 
+    float cx,
+    float cy,
+    float cz )
   {
     size = max_size;
     b = setDirector(max_size, cx, cy, cz);
@@ -44,7 +49,7 @@ class Arcball
 
     resize(width, height);
   }
-  
+
   ~Arcball()
   {
     delete b;
@@ -54,7 +59,7 @@ class Arcball
     float r = (float)y/(float)x;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    
+
     glViewport(0, 0, x, y);
     if (x<y){
       side = x/2;
@@ -66,93 +71,93 @@ class Arcball
     }
     xside = x/2;
     yside = y/2;
-    
+
     glMatrixMode(GL_MODELVIEW);
   }
 
- void draw_sphere()
- {
-   const float* offset = b->getOffset();
-   glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+  void draw_sphere()
+  {
+    const float* offset = b->getOffset();
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
 
-   glPushMatrix();
+    glPushMatrix();
 
-   glDisable(GL_LIGHTING);
+    glDisable(GL_LIGHTING);
 
-   glTranslated(-offset[0], -offset[1], -offset[2]);
+    glTranslated(-offset[0], -offset[1], -offset[2]);
 
-   const float scale = 1.2f;
-   glScaled(size * scale, size * scale, size * scale);
+    const float scale = 1.2f;
+    glScaled(size * scale, size * scale, size * scale);
 
-   glColor3d(0., 0., 1.);
-   unitCircle(1.);
+    glColor3d(0., 0., 1.);
+    unitCircle(1.);
 
-   glRotated(90., 1., 0., 0.);
-   glColor3d(0., 1., 0.);
-   unitCircle(1.);
+    glRotated(90., 1., 0., 0.);
+    glColor3d(0., 1., 0.);
+    unitCircle(1.);
 
-   glRotated(90., 0., 1., 0.);
-   glColor3d(1., 0., 0.);
-   unitCircle(1.);
+    glRotated(90., 0., 1., 0.);
+    glColor3d(1., 0., 0.);
+    unitCircle(1.);
 
-   glPopMatrix();
- }
+    glPopMatrix();
+  }
 
- void draw_sphere_scene()
- {
-   glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+  void draw_sphere_scene()
+  {
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
 
-   glPushMatrix();
+    glPushMatrix();
 
-   glDisable(GL_LIGHTING);
+    glDisable(GL_LIGHTING);
 
-   const float scale = 1.f;
-   glScaled(scale, scale, scale);
+    const float scale = 1.f;
+    glScaled(scale, scale, scale);
 
-   glColor3d(0., 0., 1.);
-   unitCircle(1.);
+    glColor3d(0., 0., 1.);
+    unitCircle(1.);
 
-   glRotated(90., 1., 0., 0.);
-   glColor3d(0., 1., 0.);
-   unitCircle(1.);
+    glRotated(90., 1., 0., 0.);
+    glColor3d(0., 1., 0.);
+    unitCircle(1.);
 
-   glRotated(90., 0., 1., 0.);
-   glColor3d(1., 0., 0.);
-   unitCircle(1.);
+    glRotated(90., 0., 1., 0.);
+    glColor3d(1., 0., 0.);
+    unitCircle(1.);
 
-   glPopMatrix();
- }
+    glPopMatrix();
+  }
 
 
- void unitCircle(const double r)
- {
-   glBegin(GL_LINE_LOOP);
-   for(double i = 0.; i < 320.; ++i)
-     glVertex3d(r*cos(i/160.*3.1416), r*sin(i/160.*3.1416), 0.);
-   glEnd();
- }
-  
+  void unitCircle(const double r)
+  {
+    glBegin(GL_LINE_LOOP);
+    for(double i = 0.; i < 320.; ++i)
+      glVertex3d(r*cos(i/160.*3.1416), r*sin(i/160.*3.1416), 0.);
+    glEnd();
+  }
+
   void setCamera() {
     const float* offset;
     offset = b->getOffset();
 
     glMatrixMode(GL_MODELVIEW);
-    
+
     glLoadMatrixf(b->mat());
     glTranslated(offset[0], offset[1], offset[2]);
   }
-  
+
   void setCamera_no_offset_no_scale() {
     glMatrixMode(GL_MODELVIEW);
-    
+
     glLoadMatrixf(b->matSansTransZoom());
   }
 
-  
+
   void zoom(float factor) {
     b->radius() *= factor;
   }
-  
+
   void recenter() {
     b->recenter(0., 0., 0.);
   }
@@ -162,18 +167,18 @@ class Arcball
     b->radius() = 1.0f/size;
     recenter();
   }
-  
-  void translate(int xold, int yold, int x, int y) {
-      float Xold = ((float) xold - xside)/side;
-      float Yold = ((float) yold - yside)/side;
-      float X = ((float) x - xside)/side;
-      float Y = ((float) y - yside)/side;
 
-      float vx = X - Xold;
-      float vy = Y - Yold;
-      b->translation(vx, -vy, 0.);
+  void translate(int xold, int yold, int x, int y) {
+    float Xold = ((float) xold - xside)/side;
+    float Yold = ((float) yold - yside)/side;
+    float X = ((float) x - xside)/side;
+    float Y = ((float) y - yside)/side;
+
+    float vx = X - Xold;
+    float vy = Y - Yold;
+    b->translation(vx, -vy, 0.);
   }
-  
+
   void rotate_obj(int xold, int yold, int x, int y) 
   {
     float Xold = ((float) xold - xside)/side;
@@ -191,18 +196,18 @@ class Arcball
     const float scale = 1.2f;
 
     b->rotationObj((Xold - imCenter[0])/(b->radius()*size*scale), 
-        (-Yold - imCenter[1])/(b->radius()*size*scale), 
-        (X - imCenter[0])/(b->radius()*size*scale), 
-        (-Y - imCenter[1])/(b->radius()*size*scale));
+      (-Yold - imCenter[1])/(b->radius()*size*scale), 
+      (X - imCenter[0])/(b->radius()*size*scale), 
+      (-Y - imCenter[1])/(b->radius()*size*scale));
   }
-  
-  void rotate_scene(int xold, int yold, int x, int y) {
-      float Xold = ((float) xold - xside)/side;
-      float Yold = ((float) yold - yside)/side;
-      float X = ((float) x - xside)/side;
-      float Y = ((float) y - yside)/side;
 
-      b->rotation(Xold, -Yold, X, -Y);
+  void rotate_scene(int xold, int yold, int x, int y) {
+    float Xold = ((float) xold - xside)/side;
+    float Yold = ((float) yold - yside)/side;
+    float X = ((float) x - xside)/side;
+    float Y = ((float) y - yside)/side;
+
+    b->rotation(Xold, -Yold, X, -Y);
 
   }
 };
