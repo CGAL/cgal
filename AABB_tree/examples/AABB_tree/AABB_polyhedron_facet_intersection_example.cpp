3edf2601c3897aabca4539f29b668b5fc90adad1
Pierre Alliez <Pierre.Alliez@sophia.inria.fr>
2009-06-28 20:57:55 +0000

    aabb tree: enrich one example with intersection of facets with plane query (intersections are generically segments)

--- AABB_tree/examples/AABB_tree/AABB_polyhedron_facet_intersection_example.cpp
+++ AABB_tree/examples/AABB_tree/AABB_polyhedron_facet_intersection_example.cpp
@@ -33,6 +33,8 @@
 
 typedef CGAL::Simple_cartesian<double> K;
 typedef K::Point_3 Point;
+typedef K::Plane_3 Plane;
+typedef K::Vector_3 Vector;
 typedef K::Segment_3 Segment;
 typedef CGAL::Polyhedron_3<K> Polyhedron;
 typedef CGAL::AABB_polyhedron_triangle_primitive<K,Polyhedron> Primitive;
@@ -53,24 +55,25 @@ int main()
 	// constructs AABB tree
 	Tree tree(polyhedron.facets_begin(),polyhedron.facets_end());
 
-	// construct segment query
+	// constructs segment query
 	Point a(-0.2, 0.2, -0.2);
 	Point b(1.3, 0.2, 1.3);
-	Segment query(a,b);
+	Segment segment_query(a,b);
 
-	// test intersections
-	if(tree.do_intersect(query))
+	// test intersections with segment query
+	if(tree.do_intersect(segment_query))
 		std::cout << "intersection(s)" << std::endl;
 	else
 		std::cout << "no intersection" << std::endl;
 
-	// computes #intersections
-	std::cout << tree.number_of_intersected_primitives(query)
+	// computes #intersections with segment query
+	std::cout << tree.number_of_intersected_primitives(segment_query)
 		<< " intersection(s)" << std::endl;
 
-	// computes first encountered intersection
+	// computes first encountered intersection with segment query
+	// (generically a point)
 	boost::optional<Object_and_primitive_id> intersection =
-		tree.any_intersection(query);
+		tree.any_intersection(segment_query);
 	if(intersection)
 	{
 		// get intersection object
@@ -83,13 +86,32 @@ int main()
 		}
 	}
 
-	// computes all intersections (as pairs object - primitive_id)
+	// computes all intersections with segment query (as pairs object - primitive_id)
 	std::list<Object_and_primitive_id> intersections;
-	tree.all_intersections(query, std::back_inserter(intersections));
+	tree.all_intersections(segment_query, std::back_inserter(intersections));
 
-	// computes all intersected primitives as primitive ids
+	// computes all intersected primitives with segment query as primitive ids
 	std::list<Primitive_id> primitives;
-	tree.all_intersected_primitives(query, std::back_inserter(primitives));
+	tree.all_intersected_primitives(segment_query, std::back_inserter(primitives));
+
+	// constructs plane query
+	Vector vec(0.0,0.0,1.0);
+	Plane plane_query(a,vec);
+
+	// computes first encountered intersection with plane query
+	// (generically a segment)
+	intersection = tree.any_intersection(plane_query);
+	if(intersection)
+	{
+		// get intersection object
+		Object_and_primitive_id op = *intersection;
+		CGAL::Object object = op.first;
+		Segment segment;
+		if(CGAL::assign(segment,object))
+		{
+			std::cout << "intersection object is a segment" << std::endl;
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
