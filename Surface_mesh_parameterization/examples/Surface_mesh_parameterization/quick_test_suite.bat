d879d0191abb834dc382fac2ef323244bf5bf6f9
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2009-07-16 18:16:24 +0000

    Fixed potential conflicts with third party libraries (Visual C++ only): Removed _HAS_ITERATOR_DEBUGGING=0 setting to go back to default settings. This setting could create conflicts with third party libraries like _SECURE_SCL=0 crashes boost program options.
    Note that no conflict with _HAS_ITERATOR_DEBUGGING was reported. This is just a safety change.

--- Surface_mesh_parameterization/examples/Surface_mesh_parameterization/quick_test_suite.bat
+++ Surface_mesh_parameterization/examples/Surface_mesh_parameterization/quick_test_suite.bat
@@ -8,5 +8,3 @@ PATH=%PATH%;c:\Applis\cygwin\bin
 sh ./quick_test_suite.sh
 pause
 
-rem Compare logs with Beyond Compare
-"C:\Program Files\Beyond Compare 2\BC2.exe" ./quick_test_suite.log.bak ./quick_test_suite.log
