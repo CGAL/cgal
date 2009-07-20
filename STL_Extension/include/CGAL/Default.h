6335879bb6e227b97ff2bd8abb8433d3037ad470
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2009-07-20 08:52:20 +0000

    Properties.

--- STL_Extension/include/CGAL/Default.h
+++ STL_Extension/include/CGAL/Default.h
@@ -11,8 +11,8 @@
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/trunk/Kernel_23/include/CGAL/basic.h $
-// $Id: basic.h 28567 2006-02-16 14:30:13Z lsaboret $
+// $URL$
+// $Id$
 //
 // Author(s)     : Sylvain Pion
  
