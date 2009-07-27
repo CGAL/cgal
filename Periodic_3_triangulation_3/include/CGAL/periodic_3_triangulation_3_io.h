283a880e119a7a6a4903058a84f887530312256e
Manuel Caroli <Manuel.Caroli@sophia.inria.fr>
2009-07-27 11:19:44 +0000

    - forgotten propset - forgotten licence header

--- Periodic_3_triangulation_3/include/CGAL/periodic_3_triangulation_3_io.h
+++ Periodic_3_triangulation_3/include/CGAL/periodic_3_triangulation_3_io.h
@@ -1,3 +1,25 @@
+// Copyright (c) 2009   INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : Manuel Caroli <Manuel.Caroli@sophia.inria.fr>
+
+#ifndef CGAL_PERIODIC_3_TRIANGULATION_3_IO_H
+#define CGAL_PERIODIC_3_TRIANGULATION_3_IO_H
+
 template <class Stream, class Triangulation>
 Stream &write_triangulation_to_off(Stream &out, Triangulation &t) {
   typedef typename Triangulation::Point Point;
@@ -111,4 +133,6 @@ Stream& draw_dual_to_off(Stream &os) {
   return os;
 }
 #endif
+
+#endif //CGAL_PERIODIC_3_TRIANGULATION_3_IO_H
 
