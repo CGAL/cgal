f81561f3d411610ff1e075c54e5600e3941bb914
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2009-07-31 21:06:50 +0000

    Add copyright and license header

--- AABB_tree/include/CGAL/AABB_intersections.h
+++ AABB_tree/include/CGAL/AABB_intersections.h
@@ -1,3 +1,22 @@
+// Copyright (c) 2008-2009  INRIA Sophia-Antipolis (France), ETHZ (Suisse).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://lrineau@scm.gforge.inria.fr/svn/cgal/trunk/AABB_tree/include/CGAL/AABB_intersections/Bbox_3_ray_3_do_intersect.h $
+// $Id: Bbox_3_ray_3_do_intersect.h 50124 2009-06-26 17:55:17Z palliez $
+// 
+//
+// Author(s)     : Camille Wormser, Jane Tournois, Pierre Alliez
+
 #include <CGAL/intersections.h>
 
 #include <CGAL/AABB_intersections/Bbox_3_ray_3_do_intersect.h>
