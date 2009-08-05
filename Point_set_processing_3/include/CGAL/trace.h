92a542c94cef5837848422bc96c244c9f0bd94e4
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2009-08-05 11:05:54 +0000

    Fixed bug in trace macro

--- Point_set_processing_3/include/CGAL/trace.h
+++ Point_set_processing_3/include/CGAL/trace.h
@@ -20,19 +20,33 @@
 #define CGAL_TRACE_H
 
 #include <stdio.h>
+#include <stdarg.h>
 #include <iostream>
 #include <fstream>
 
 
-// Trace macros
-// ------------
+// Trace utilities
+// ---------------
 
+// print_stderr() = printf-like function to print to stderr
+inline void print_stderr(const char *fmt, ...)
+{
+  va_list argp;
+  va_start(argp, fmt);
+  vfprintf(stderr, fmt, argp);
+  va_end(argp);
+}
+
+// CGAL_TRACE() = printf-like function to print to stderr
+// if DEBUG_TRACE is defined (ignored otherwise)
 #ifdef DEBUG_TRACE
-  #define CGAL_TRACE  printf
+  #define CGAL_TRACE  print_stderr
 #else
-  #define CGAL_TRACE  if (false) printf
+  #define CGAL_TRACE  if (false) print_stderr
 #endif
 
+// CGAL_TRACE_STREAM = C++ stream that prints to std::cerr 
+// if DEBUG_TRACE is defined (ignored otherwise)
 #ifdef DEBUG_TRACE
   #define CGAL_TRACE_STREAM  std::cerr
 #else
