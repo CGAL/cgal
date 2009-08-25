146aa66bca800b1ee01f44233135c4201e92e3c1
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2009-08-25 14:58:19 +0000

    Remove commented code.

--- Kernel_23/include/CGAL/Kernel/function_objects.h
+++ Kernel_23/include/CGAL/Kernel/function_objects.h
@@ -238,13 +238,6 @@ namespace CommonKernelFunctors {
           return LARGER;
         }
     }
-
-    // result_type
-    // operator()(const Tetrahedron_3& t1, const Tetrahedron_3& t2) const
-    // {
-    //   return this->operator(t1[0], t1[1], t1[2], t1[3],
-    //                         t2[0], t2[1], t2[2], t2[3]);
-    // }
   };
 
   template <typename K>
