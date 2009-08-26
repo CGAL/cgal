be09d80d95751b4aba5bafb294d1c10114d4c9f7
Laurent Saboret <Laurent.Saboret@sophia.inria.fr>
2009-08-26 16:23:28 +0000

    * Bug fix: fixed compatibility of TAUCS/BLAS/LAPACK precompiled libraries with Visual C++ 9.0 2008: CGAL 3.5 will include TAUCS/BLAS/LAPACK precompiled libraries for Visual C++ 8.0 and 9.0. Auto-link is modified accordingly.
    * Also fixed compatibility of cgal_test_with_cmake.bat scripts with VC++ 9.0.

--- Point_set_processing_3/test/Point_set_processing_3/cgal_test_with_cmake.bat
+++ Point_set_processing_3/test/Point_set_processing_3/cgal_test_with_cmake.bat
@@ -2,11 +2,17 @@
 
 rem Double-clickable version of cgal_test_with_cmake on Windows/Cygwin/VisualC++
 
-rem Path to Cygwin and VisualC++ is hard-coded
-PATH=c:\Applis\cygwin\bin;C:\Program Files\Microsoft Visual Studio 8\Common7\IDE;%PATH%
+rem Path to Cygwin is hard-coded
+PATH=c:\Applis\cygwin\bin;%PATH%
 
+rem Add path to VisualC++
+if "%CMAKE_GENERATOR%"=="-GVisual Studio 8 2005" PATH=%VS80COMNTOOLS%\..\IDE;%PATH%
+if "%CMAKE_GENERATOR%"=="-GVisual Studio 9 2008" PATH=%VS90COMNTOOLS%\..\IDE;%PATH%
+
+rem Call shell script
 sh ./cgal_test_with_cmake
+
 pause
 
 rem Compare logs with Beyond Compare
-"C:\Program Files\Beyond Compare 2\BC2.exe" ./cgal_test_with_cmake.log.bak ./cgal_test_with_cmake.log
+BC2.exe ./cgal_test_with_cmake.log.bak ./cgal_test_with_cmake.log
