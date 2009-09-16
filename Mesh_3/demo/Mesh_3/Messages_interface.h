320f070af87c4fb814bf3bf8788c36e32a31fdcb
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2009-09-16 07:50:39 +0000

    Early stage of a Mesh_3 Qt4 demo. The difficulty will be to share as much as possible with the Polyhedron_3 demo.

---
+++ Mesh_3/demo/Mesh_3/Messages_interface.h
@@ -0,0 +1,17 @@
+#ifndef MESSAGES_INTERFACE_H
+#define MESSAGES_INTERFACE_H
+
+#include <QString>
+#include <QObject>
+
+class Messages_interface {
+public:
+  virtual void warning(QString) = 0;
+  virtual void error(QString) = 0;
+  virtual void information(QString) = 0;
+};
+
+Q_DECLARE_INTERFACE(Messages_interface, 
+                    "com.geometryfactory.PolyhedronDemo.MessagesInterface/1.0");
+
+#endif // MESSAGES_INTERFACE_H
