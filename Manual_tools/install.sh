d399652b2f4b815c8834cc8e3ec1c2643c6e97a4
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2009-09-18 13:19:55 +0000

    Remove EPS files.

--- Manual_tools/install.sh
+++ Manual_tools/install.sh
@@ -2,7 +2,7 @@
 
   . ./install.config
 
-install -d $LATEX_CONV_BIN $LATEX_CONV_CONFIG $STYLE_FILES $STYLE_FILES/eps_tabs $STYLE_FILES/eps_tabs_grey
+install -d $LATEX_CONV_BIN $LATEX_CONV_CONFIG $STYLE_FILES $STYLE_FILES/eps_tabs
 
 cd src
 #make clean
@@ -12,8 +12,8 @@ cd ..
 
 cp scripts/index_fix scripts/cc_make_ref_pages scripts/cc_ref_wizard scripts/tex2doxy $LATEX_CONV_BIN
 cp sty/*.sty $STYLE_FILES
-cp sty/eps_tabs/*.pdf sty/eps_tabs/*.eps $STYLE_FILES/eps_tabs
-cp  sty/eps_tabs_grey/*.eps $STYLE_FILES/eps_tabs_grey # sty/eps_tabs_grey/*.pdf
+cp sty/eps_tabs/*.pdf $STYLE_FILES/eps_tabs
+# cp  sty/eps_tabs_grey/*.eps $STYLE_FILES/eps_tabs_grey # sty/eps_tabs_grey/*.pdf
 
 echo ""
 echo "================================================"
