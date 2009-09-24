fcafd2e20e3beda187f4ac77de66c06a08180c04
Fernando Cacciola <fernando.cacciola@geometryfactory.com>
2009-09-24 20:26:27 +0000

    Updates to Boolean_operations_2 Qt4 demo (entering beziers)

--- Boolean_set_operations_2/demo/Boolean_set_operations_2_GraphicsView/include/CGAL/Qt/GraphicsViewBezierBoundaryInput.h
+++ Boolean_set_operations_2/demo/Boolean_set_operations_2_GraphicsView/include/CGAL/Qt/GraphicsViewBezierBoundaryInput.h
@@ -46,30 +46,57 @@ namespace Qt {
     
     typedef typename Traits::Curve_2                      Curve;
     typedef typename Traits::X_monotone_curve_2           X_monotone_curve;
-    typedef typename Bezier_gps_traits::General_polygon_2 Boundary;
+    typedef typename Bezier_gps_traits::General_polygon_2 Bezier_Boundary;
     typedef typename Traits::Rat_kernel::Vector_2         Vector ;
     typedef typename Traits::Rat_kernel::Point_2          Point ;
     
     typedef std::vector<Curve> Curve_vector ;
     
+    struct Boundary
+    {
+      Boundary() : mIsClosed(false) {}
+      
+      Curve_vector mPieces ;
+      bool         mIsClosed ; 
+    } ;
+
+    typedef boost::shared_ptr<Boundary> Boundary_ptr ;
+    
+    typedef std::vector<Boundary_ptr> Boundary_vector ;
+    
     typedef typename Curve_vector::const_iterator const_curve_terator ;
     
     typedef Bezier_boundary_pieces_graphics_item<Curve_vector> GI ;
 
-    GraphicsViewBezierBoundaryInput(QObject* parent, QGraphicsScene* s)
-      : GraphicsViewInput(parent)
-      , mScene(s)
-      , mIsClosed(false)
-      , mState(Start)
-      , mOngoingPieceAdded(false)
-      , mHandle0Item(NULL)
-      , mHandle1Item(NULL)
-      , mBezierPen( QColor(255,0,0) )
-      , mHandlePen( QColor(0,0,255) )
+    GraphicsViewBezierBoundaryInput(QObject* aParent, QGraphicsScene* aScene)
+      :
+        GraphicsViewInput( aParent         )
+      , mScene           ( aScene          )
+      , mState           ( Start           )
+      , mBoundaryPen     ( QColor(0,255,0) )
+      , mOngoingCurvePen ( QColor(255,0,0) )
+      , mHandlePen       ( QColor(0,0,255) )
+      , mCurrBoundary    ( new Boundary()  ) 
+      , mCurrBoundaryGI  ( 0               )
     {
-      mGI = new GI(&mPieces) ;
-      mGI->setPen(mBezierPen);
-      mScene->addItem(mGI);
+      mOngoingPieceGI = new GI(&mOngoingPieceCtr) ;
+      mHandle0GI      = new QGraphicsLineItem();
+      mHandle1GI      = new QGraphicsLineItem();
+      
+      mOngoingPieceGI->setPen(mOngoingCurvePen);
+      mHandle0GI     ->setPen(mHandlePen);
+      mHandle1GI     ->setPen(mHandlePen);
+      
+      mHandle0GI->setLine(0,0,1,1);
+      mHandle1GI->setLine(0,0,1,1);
+      mHandle0GI->hide();
+      mHandle1GI->hide();
+      
+      mScene->addItem(mOngoingPieceGI);
+      mScene->addItem(mHandle0GI);
+      mScene->addItem(mHandle1GI);
+      
+      SetupCurrBoundary();
     }
     
     ~GraphicsViewBezierBoundaryInput()
@@ -79,65 +106,58 @@ namespace Qt {
       //RemoveHandleItems();      
     }
     
-    bool eventFilter(QObject *obj, QEvent *event)
+    bool eventFilter(QObject *obj, QEvent *aEvent)
     {
       bool rHandled = false ;
       
-      if (event->type() == QEvent::GraphicsSceneMousePress) 
+      if (aEvent->type() == QEvent::GraphicsSceneMousePress) 
       {
-        rHandled = mousePressEvent( static_cast<QGraphicsSceneMouseEvent *>(event) ) ;
+        rHandled = mousePressEvent( static_cast<QGraphicsSceneMouseEvent *>(aEvent) ) ;
       } 
-      else if (event->type() == QEvent::GraphicsSceneMouseRelease) 
+      else if (aEvent->type() == QEvent::GraphicsSceneMouseRelease) 
       {
-        rHandled = mouseReleaseEvent( static_cast<QGraphicsSceneMouseEvent *>(event) ) ;
+        rHandled = mouseReleaseEvent( static_cast<QGraphicsSceneMouseEvent *>(aEvent) ) ;
       } 
-      else if (event->type() == QEvent::GraphicsSceneMouseMove) 
+      else if (aEvent->type() == QEvent::GraphicsSceneMouseMove) 
       {
-        rHandled = mouseMoveEvent( static_cast<QGraphicsSceneMouseEvent *>(event) ) ;
+        rHandled = mouseMoveEvent( static_cast<QGraphicsSceneMouseEvent *>(aEvent) ) ;
       } 
-      else if (event->type() == QEvent::KeyPress) 
+      else if (aEvent->type() == QEvent::KeyPress) 
       {
-        rHandled = keyPressEvent( static_cast<QKeyEvent *>(event) ) ;
+        rHandled = keyPressEvent( static_cast<QKeyEvent *>(aEvent) ) ;
       }
       
       if ( !rHandled )
-        rHandled = QObject::eventFilter(obj, event);
+        rHandled = QObject::eventFilter(obj, aEvent);
               
       return rHandled ;  
     }
     
   protected:
 
-    enum State
-    {
-      Start,
-      CurveStarted,
-      PieceStarted, PieceProceeding, PieceEnded,
-      HandleStarted, HandleProceeding, HandleEnded
-    } ;
+    enum State { Start, PieceOrHandleStarted, PieceOngoing, HandleOngoing, PieceEnded, CurveEnded } ;
     
     Point cvt ( QPointF const& aP ) const { return Point(aP.x(),aP.y()) ; }
 
-    bool mousePressEvent(QGraphicsSceneMouseEvent *event)
+    bool mousePressEvent(QGraphicsSceneMouseEvent *aEvent)
     {
       bool rHandled = false ;
       
-      Point lP = cvt(event->scenePos());
+      Point lP = cvt(aEvent->scenePos());
       
-      if ( event->button() == ::Qt::LeftButton )
+      if ( aEvent->button() == ::Qt::LeftButton )
       {
         switch (mState)
         {
           case Start: 
-            mState  = HandleStarted;
-            mP1     = lP;
+            mP0      = lP;
+            mState   = PieceOrHandleStarted;
             rHandled = true;
             break;
 
-          case PieceProceeding: 
-
-            mState = PieceEnded;
-            mP1 = lP;
+          case PieceOngoing: 
+            mP1      = lP;
+            mState   = HandleOngoing;
             rHandled = true;
             break;
         }
@@ -146,141 +166,99 @@ namespace Qt {
       return rHandled ;
     }
     
-    bool mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+    bool mouseReleaseEvent(QGraphicsSceneMouseEvent *aEvent)
     {
       bool rHandled = false ;
       
-      Point lP = cvt(event->scenePos());
+      Point lP = cvt(aEvent->scenePos());
       
-      if ( event->button() == ::Qt::LeftButton )
+      if ( aEvent->button() == ::Qt::LeftButton )
       {
         switch (mState)
         {
-          case HandleStarted:
-            mState = PieceStarted;
+          case PieceOrHandleStarted:
+            mState   = PieceOngoing;
             rHandled = true;
             break;
             
-          case CurveStarted: 
-            mState = PieceStarted;
-            rHandled = true;
-            break;
-
-          case PieceEnded:
-            CommitOngoingPiece();
-            mState = PieceStarted;
-            rHandled = true;
-            break;
-
-          case HandleProceeding: 
-            CommitOngoingPiece();
-            mState = HandleEnded;
+          case HandleOngoing: 
+            UpdateHandles(lP);
+            CommitOngoingPiece(lP);
+            mState   = PieceEnded;
             rHandled = true;
             break;
         }
       }
-      else if ( event->button() == ::Qt::RightButton )
+      else if ( aEvent->button() == ::Qt::RightButton )
       {
-        
+        switch (mState)
+        {
+          case PieceOngoing: 
+            CommitCurrBoundary();
+            mState = Start ;     
+            rHandled = true;
+            break;
+        }    
       }
       
       return rHandled ;
     }
 
-    bool mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+    bool mouseMoveEvent(QGraphicsSceneMouseEvent *aEvent)
     {
       bool rHandled = false ;
       
-      Point lP = cvt(event->scenePos());
+      Point lP = cvt(aEvent->scenePos());
       
       switch (mState)
       {
-        case HandleStarted:
-          mState = HandleProceeding ;
-          mP0 = mP1;
-          rHandled = true ;
-          break;
-          
-        case PieceStarted: 
-          mState = PieceProceeding;
-          mP0 = mP1;
-          rHandled = true ;
-          break;
-
-        case PieceProceeding: 
+        case PieceOngoing: 
           mP1 = lP;
           UpdateOngoingPiece();
           rHandled = true ;
           break;
 
-        case HandleProceeding:
+        case HandleOngoing:
         
-          mState = HandleProceeding;
-          
-          if ( mPieces.size() > 0 )
-          {
-            mH0 = lP ;
-            mH1 = mP1 - (lP - mP1);
-            
-            if ( !mHandle0Item )
-            {
-              mHandle0Item = new QGraphicsLineItem();
-              mHandle0Item->setPen(mHandlePen);
-              mScene->addItem(mHandle0Item);
-            }
-            
-            if ( !mHandle1Item )
-            {
-              mHandle1Item = new QGraphicsLineItem();
-              mHandle1Item->setPen(mHandlePen);
-              mScene->addItem(mHandle1Item);
-            }
-            
-            mHandle0Item->setLine( to_double(mP1.x()), to_double(mP1.y()), to_double(mH0->x()), to_double(mH0->y()));
-            mHandle1Item->setLine( to_double(mP1.x()), to_double(mP1.y()), to_double(mH1->x()), to_double(mH1->y()));
-          }
-          else
-          {
-            mH1 = lP ;
-            
-            if ( !mHandle0Item )
-            {
-              mHandle0Item = new QGraphicsLineItem();
-              mHandle0Item->setPen(mHandlePen);
-              mScene->addItem(mHandle0Item);
-            }
-            
-            mHandle0Item->setLine( to_double(mP0.x()), to_double(mP0.y()), to_double(mH1->x()), to_double(mH1->y()));
-          }
+          UpdateHandles(lP);
           UpdateOngoingPiece();
           rHandled = true ;
           break;
-
-        case HandleEnded: 
-          mPrevH0 = mH0 ;
-          RemoveHandleItems();
-          mState = PieceStarted;
-          rHandled = true ;
+          
+        case PieceEnded:
+          mState   = PieceOngoing;
+          rHandled = true;
           break;
       }
       
       return rHandled ;
     }
     
-    bool keyPressEvent(QKeyEvent *event)
+    bool keyPressEvent(QKeyEvent *aEvent)
     {
-      return false ;
+      bool rHandled = false ;
+      
+      if (aEvent->text() == "c" )
+      {
+        CloseCurrBundary();
+        CommitCurrBoundary();
+        mState = Start ;     
+        rHandled = true;
+      }
+      
+      return rHandled ;
     }
 
     
     virtual void generate_boundary() 
     {
+/*
       Traits traits ;
       Traits::Make_x_monotone_2 make_x_monotone = traits.make_x_monotone_2_object();
       
       std::vector<X_monotone_curve> xcvs;
 
-      for ( const_curve_terator it = mPieces.begin() ; it != mPieces.end() ; ++ it )
+      for ( const_curve_terator it = mCurrBdryPieces.begin() ; it != mCurrBdryPieces.end() ; ++ it )
       {       
         std::vector<CGAL::Object>                 x_objs;
         std::vector<CGAL::Object>::const_iterator xoit;
@@ -294,41 +272,32 @@ namespace Qt {
             xcvs.push_back (xcv);
         }    
       }
-      
       emit(generate(CGAL::make_object( Boundary(xcvs.begin(), xcvs.begin()) )));
+*/
     }
     
   private:
 
-    void RemoveHandleItems()
+    Curve const* ongoing_piece() const { return mOngoingPieceCtr.size() == 1 ? &mOngoingPieceCtr[0] : NULL ; }
+    
+    void HideHandles()
     {
-      if ( mHandle0Item )
-      {
-        mScene->removeItem(mHandle0Item);
-        delete mHandle0Item ;
-        mHandle0Item = NULL ;
-      }
-      
-      if ( mHandle1Item )
-      {
-        mScene->removeItem(mHandle1Item);
-        delete mHandle1Item ;
-        mHandle1Item = NULL ;
-      }
+      mHandle0GI->hide();
+      mHandle1GI->hide();
     }  
 
     Curve CreatePiece()
     {
-      if ( mPrevH0 && mH1 )
+      if ( mPrevH0 && mH1 && *mPrevH0 != *mH1 && *mPrevH0 != mP0 && *mH1 != mP1 )
       {
         Point lControlPoints[4] = { mP0
-                                  , midpoint(mP0,*mPrevH0)
-                                  , midpoint(*mH1,mP1)
+                                  , *mPrevH0
+                                  , *mH1
                                   , mP1 
                                   } ;
         return Curve( lControlPoints, lControlPoints + 4 ) ;
       }
-      else if ( mPrevH0 && !mH1 )
+      else if ( mPrevH0 && !mH1 && *mPrevH0 != mP0 && *mPrevH0 != mP1 )
       {
         Point lControlPoints[3] = { mP0
                                   , *mPrevH0
@@ -336,7 +305,7 @@ namespace Qt {
                                   } ;
         return Curve( lControlPoints, lControlPoints + 3 ) ;
       }
-      else if ( !mPrevH0 && mH1 )
+      else if ( !mPrevH0 && mH1 && *mH1 != mP0 && *mH1 != mP1 )
       {
         Point lControlPoints[3] = { mP0
                                   , *mH1
@@ -353,49 +322,116 @@ namespace Qt {
       }
     }
     
-    void AddPiece()
-    {
-      if ( mOngoingPieceAdded && mPieces.size() > 0 )
-        mPieces.pop_back();
-        
-      mPieces.push_back( CreatePiece() ) ;
-      
-      mGI->modelChanged();
-      
-    }
-    
     void UpdateOngoingPiece()
     {
-      AddPiece();
-      mOngoingPieceAdded = true ;
+      if ( mOngoingPieceCtr.size() > 0 )
+        mOngoingPieceCtr.clear();
+      mOngoingPieceCtr.push_back(CreatePiece());  
+      mOngoingPieceGI->modelChanged();
     }      
     
-    void CommitOngoingPiece()
+    void CommitOngoingPiece( Point const& aP )
     {
-      AddPiece();
-      mOngoingPieceAdded = false ;
+      mCurrBoundary->mPieces.push_back( *ongoing_piece() ) ;
+      mCurrBoundaryGI->modelChanged();
+      mOngoingPieceCtr.clear();
+      mOngoingPieceGI->modelChanged();
+      mP0 = mP1 ;
+      mP1 = aP ;
+      mPrevH0 = mH0 ;
+      mH0 = mH1 = boost::optional<Point>();
     }      
+
+    void UpdateHandles(Point const& aP)
+    {
+      if ( squared_distance(mP1,aP) >= 9 )
+      {
+        if ( ongoing_piece() )
+        {
+          mH0 = aP ;
+          mH1 = mP1 - (aP - mP1);
+          
+          mHandle0GI->setLine( to_double(mP1.x()), to_double(mP1.y()), to_double(mH0->x()), to_double(mH0->y()));
+          mHandle1GI->setLine( to_double(mP1.x()), to_double(mP1.y()), to_double(mH1->x()), to_double(mH1->y()));
+          mHandle0GI->show();
+          mHandle1GI->show();
+        }
+      }
+      else
+      {
+        HideHandles();
+        mH0 = mH1 = boost::optional<Point>();
+      }
+      
+    }    
+
+    void CloseCurrBundary()
+    {
+      if ( mCurrBoundary->mPieces.size() > 0 && ongoing_piece()!= NULL )
+      {
+        std::vector<Point> lControlPoints(ongoing_piece()->control_points_begin(),ongoing_piece()->control_points_end());
+        
+        lControlPoints.back() = mCurrBoundary->mPieces.front().control_point(0);
+              
+        mCurrBoundary->mPieces.push_back( Curve( lControlPoints.begin(), lControlPoints.end() ) ) ;
+        
+        mCurrBoundary->mIsClosed = true ;
+      }
+    }
+        
+    void CommitCurrBoundary()
+    {
+      mOngoingPieceCtr.clear();
+      mOngoingPieceGI->modelChanged();
+      
+      if ( mCurrBoundary->mPieces.size() > 0 )
+      {
+        mComittedBoundaries.push_back(mCurrBoundary) ;
+        
+        mCommittedBoundariesGI.push_back(mCurrBoundaryGI);
+      }
+      
+      SetupCurrBoundary();
+      
+      HideHandles();
+    }
+    
+    void SetupCurrBoundary()
+    {
+      mCurrBoundary = Boundary_ptr( new Boundary() ) ;
+      
+      mCurrBoundaryGI = new GI(&mCurrBoundary->mPieces) ;
+      
+      mCurrBoundaryGI->setPen(mBoundaryPen);
+      
+      mScene->addItem(mCurrBoundaryGI);
+    }
     
   private:
   
     QGraphicsScene*    mScene ;
-    GI*                mGI ; 
-    QGraphicsLineItem* mHandle0Item ;          
-    QGraphicsLineItem* mHandle1Item ;          
+    std::vector<GI*>   mCommittedBoundariesGI ;
+    GI*                mCurrBoundaryGI ; 
+    GI*                mOngoingPieceGI ; 
+    QGraphicsLineItem* mHandle0GI ;          
+    QGraphicsLineItem* mHandle1GI ;          
 
-    QPen mBezierPen ;
+    QPen mBoundaryPen ;
+    QPen mOngoingCurvePen ;
     QPen mHandlePen ;    
     
-    Curve_vector mPieces ;  
-    bool         mIsClosed;
-    int          mState;
-    bool         mOngoingPieceAdded;
-    Point        mP0;
-    Point        mP1;
+    Boundary_vector mComittedBoundaries ;  
+    Boundary_ptr    mCurrBoundary ;  
+    Curve_vector    mOngoingPieceCtr ;  
+
+    int mState;
     
-    boost::optional<Point>  mPrevH0;
-    boost::optional<Point>  mH0;
-    boost::optional<Point>  mH1;
+    Point mP0;
+    Point mP1;
+    
+    boost::optional<Point> mPrevH0;
+    boost::optional<Point> mH0;
+    boost::optional<Point> mH1;
   
   }; // end class GraphicsViewBezierBoundaryInput
 
