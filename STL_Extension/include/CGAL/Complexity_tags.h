2f3c5bf500afc1462d6302e9d5da9569f300ea6d
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2009-11-03 15:17:02 +0000

    Merged revisions 52226 via svnmerge from svn+ssh://scm.gforge.inria.fr/svn/cgal/branches/STL_Extension_Complexity_tags
    ........
      r52226 | spion | 2009-10-09 13:43:18 +0200 (Fri, 09 Oct 2009) | 2 lines
      
      Code+doc+tests for complexity tags Fast, Compact, Location_policy, Fast_location and Compact_location.
    ........

---
+++ STL_Extension/include/CGAL/Complexity_tags.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2009  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+// Author(s)     : Sylvain Pion
+
+#ifndef CGAL_COMPLEXITY_TAGS_H
+#define CGAL_COMPLEXITY_TAGS_H
+
+namespace CGAL {
+
+// Tags to be used for complexity policies.
+
+struct Compact {};
+struct Fast {};
+
+} // namespace CGAL
+
+#endif // CGAL_COMPLEXITY_TAGS_H
