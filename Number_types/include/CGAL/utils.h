a32e3e9188afc03dd2ce5d949932d11fefed1b7e
Sylvain Pion <Sylvain.Pion@sophia.inria.fr>
2009-11-13 09:50:04 +0000

    Remove spurious ";" after function block (breaks pedantic mode).

--- Number_types/include/CGAL/utils.h
+++ Number_types/include/CGAL/utils.h
@@ -30,32 +30,32 @@ template< class T >
 inline T min BOOST_PREVENT_MACRO_SUBSTITUTION
 ( const T& x , const T& y) {
     return Min<T>()( x , y );
-};
+}
 
 template< class T , class Compare >
 inline T min BOOST_PREVENT_MACRO_SUBSTITUTION
 ( const T& x , const T& y, const Compare& c) {
     return Min<T, Compare>(c)( x , y );
-};
+}
 
 // use Max
 template< class T >
 inline T max BOOST_PREVENT_MACRO_SUBSTITUTION
 ( const T& x , const T& y) {
     return Max<T>()( x , y );
-};
+}
 
 template< class T , class Compare >
 inline T max BOOST_PREVENT_MACRO_SUBSTITUTION
 ( const T& x , const T& y, const Compare& c) {
     return Max<T, Compare>(c)( x , y );
-};
+}
 
 // use Is_valid
 template< class T >
 inline bool is_valid( const T& x ) {
   return Is_valid< T >()( x );
-};
+}
 
 CGAL_END_NAMESPACE
 
