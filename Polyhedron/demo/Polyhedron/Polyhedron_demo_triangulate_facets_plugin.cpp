a15d4c1c618486aa5fa775f11cd8a3d140caacf3
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2009-12-09 15:34:36 +0000

    The Euler operations is not the good way to retriangulate a facet of a polyhedron. Use a Modifier class instead, that remove the current facets, and fill the hole with new facets (and new halfedges).
    I can now retriangulate the dodecahedron... I need to find off files with
    non-convex facets.

--- Polyhedron/demo/Polyhedron/Polyhedron_demo_triangulate_facets_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Polyhedron_demo_triangulate_facets_plugin.cpp
@@ -6,11 +6,13 @@
 #include "Polyhedron_demo_plugin_helper.h"
 #include "Scene_polyhedron_item.h"
 #include "Polyhedron_type.h"
+#include <CGAL/HalfedgeDS_decorator.h>
 
 #include <CGAL/assertions_behaviour.h>
 #include <CGAL/exceptions.h>
 
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <CGAL/Triangulation_face_base_with_info_2.h>
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
 #include <CGAL/Constrained_triangulation_plus_2.h>
 #include <CGAL/Triangulation_2_filtered_projection_traits_3.h>
@@ -19,17 +21,109 @@
       // Vector n = compute_facet_normal<Facet,Kernel>(*f);
       // ::glNormal3d(n.x(),n.y(),n.z());
 
-typedef CGAL::Triangulation_2_filtered_projection_traits_3<Kernel>   Traits;
+typedef Polyhedron::Halfedge_handle Halfedge_handle;
 
-typedef CGAL::Triangulation_vertex_base_with_info_2<Polyhedron::Halfedge_handle,
-                                                    Traits>      Vb;
+typedef CGAL::Triangulation_2_filtered_projection_traits_3<Kernel>     Traits;
 
-typedef CGAL::Constrained_triangulation_face_base_2<Traits>      Fb;
-typedef CGAL::Triangulation_data_structure_2<Vb,Fb>              TDS;
-typedef CGAL::No_intersection_tag                                Itag;
-typedef CGAL::Constrained_Delaunay_triangulation_2<Traits, TDS,
-                                                   Itag>         CDTbase;
-typedef CGAL::Constrained_triangulation_plus_2<CDTbase>          CDT;
+typedef CGAL::Triangulation_vertex_base_with_info_2<Halfedge_handle,
+                                                    Traits>            Vb;
+
+struct Face_info {
+  Halfedge_handle e[3];
+};  
+
+typedef CGAL::Triangulation_face_base_with_info_2<Face_info,
+                                                  Traits>              Fb1;
+
+typedef CGAL::Constrained_triangulation_face_base_2<Traits, Fb1>       Fb;
+typedef CGAL::Triangulation_data_structure_2<Vb,Fb>                    TDS;
+typedef CGAL::No_intersection_tag                                      Itag;
+typedef CGAL::Constrained_Delaunay_triangulation_2<Traits, TDS,Itag>   CDTbase;
+typedef CGAL::Constrained_triangulation_plus_2<CDTbase>                CDT;
+
+typedef Polyhedron::HalfedgeDS HDS;
+
+class Modifier : public CGAL::Modifier_base<HDS> {
+  CDT* cdt;
+  Polyhedron::Facet_handle fh;
+
+public:
+  Modifier(CDT* cdt,
+           Polyhedron::Facet_handle fh) 
+    : cdt(cdt), fh(fh)
+  {
+  }
+
+  bool is_external(CDT::Face_handle fh) const {
+    return cdt->is_infinite(fh);
+  }
+
+  void operator()(HDS& hds) {
+    CGAL::HalfedgeDS_decorator<HDS> decorator(hds);
+    typedef Polyhedron::Halfedge Halfedge;
+
+    decorator.make_hole(fh->halfedge());
+    for(CDT::Finite_edges_iterator
+          eit = cdt->finite_edges_begin(),
+          end = cdt->finite_edges_end();
+        eit != end; ++eit)
+    {
+      CDT::Face_handle fh = eit->first;
+      const int index = eit->second;
+      CDT::Face_handle opposite_fh = fh->neighbor(eit->second);
+      const int opposite_index = opposite_fh->index(fh);
+      const CDT::Vertex_handle va = fh->vertex(cdt-> cw(index));
+      const CDT::Vertex_handle vb = fh->vertex(cdt->ccw(index));
+
+      if( ! (is_external(fh) && is_external(opposite_fh)) && 
+          ! cdt->is_constrained(*eit) ) 
+      {
+        // strictly internal edge
+        Halfedge_handle h = hds.edges_push_back(Halfedge(),
+                                                Halfedge());
+        fh->info().e[index] = h;
+        opposite_fh->info().e[opposite_index] = h->opposite();
+
+        decorator.set_vertex(h, va->info()->vertex());
+        decorator.set_vertex(h->opposite(), vb->info()->vertex());
+      }
+      if( cdt->is_constrained(*eit) )
+      {
+        if(!is_external(fh)) {
+          fh->info().e[index] = va->info();
+        }
+        if(!is_external(opposite_fh)) {
+          opposite_fh->info().e[opposite_index] = vb->info();
+        }
+      }
+    }
+    for(CDT::Finite_faces_iterator
+          fit = cdt->finite_faces_begin(),
+          end = cdt->finite_faces_end();
+        fit != end; ++fit)
+    {
+      if(!is_external(fit)) 
+      {
+        Halfedge_handle h0 = fit->info().e[0];
+        Halfedge_handle h1 = fit->info().e[1];
+        Halfedge_handle h2 = fit->info().e[2];
+        CGAL_assertion( h0 != Halfedge_handle() );
+        CGAL_assertion( h1 != Halfedge_handle() );
+        CGAL_assertion( h2 != Halfedge_handle() );
+
+        typedef Halfedge::Base HBase;
+        h0->HBase::set_next(h1);
+        decorator.set_prev(h1, h0);
+        h1->HBase::set_next(h2);
+        decorator.set_prev(h2, h1);
+        h2->HBase::set_next(h0);
+        decorator.set_prev(h0, h2);
+
+        decorator.fill_hole(h0);        
+      }
+    }
+  }
+};
 
 class Polyhedron_demo_triangulate_facets_plugin : 
   public QObject,
@@ -76,11 +170,27 @@ public slots:
 
       typedef Polyhedron::Facet Facet;
       typedef Polyhedron::Facet_iterator Facet_iterator;
+      typedef Polyhedron::Facet_handle Facet_handle;
+
+      // One need to store facet handles into a vector, because the list of
+      // facets of the polyhedron will be modified during the loop, and
+      // that invalidates the range [facets_begin(), facets_end()[.
+      std::vector<Facet_handle> facets;
+      facets.reserve(pMesh->size_of_facets());
       for(Facet_iterator 
             fit = pMesh->facets_begin(),
             end = pMesh->facets_end();
-          fit != end; ++fit)
+          fit != end; ++fit) {
+        facets.push_back(fit);
+      }
+
+      // Iterates on the vector of facet handles
+      for(std::vector<Facet_handle>::iterator 
+            fit_it = facets.begin(),
+            end = facets.end();
+          fit_it != end; ++fit_it)
       {
+        Facet_handle fit = *fit_it;
         Kernel::Vector_3 normal = compute_facet_normal<Facet,Kernel>(*fit);
 
         Traits cdt_traits(normal);
@@ -104,23 +214,10 @@ public slots:
         cdt.insert_constraint(previous, first);
 
         std::cerr << cdt.number_of_vertices() << std::endl;
-        for(CDT::Finite_edges_iterator
-              eit = cdt.finite_edges_begin(),
-              end = cdt.finite_edges_end();
-            eit != end; ++eit)
-        {
-          if(!cdt.is_constrained(*eit)) {
-            const CDT::Vertex_handle va = eit->first->vertex(cdt. cw(eit->second));
-            const CDT::Vertex_handle vb = eit->first->vertex(cdt.ccw(eit->second));
-            std::cerr << va->point() << " " 
-                      << (void*)&*va->info() << " "
-                      << (void*)&*va->info()->next() << "\n";
-            std::cerr << vb->point() << " " 
-                      << (void*)&*vb->info() << " "
-                      << (void*)&*vb->info()->next() << "\n";
-            pMesh->split_facet(va->info(),vb->info());
-          }
-        }
+        Modifier modifier(&cdt, fit);
+        pMesh->delegate(modifier);
+        CGAL_assertion_code(pMesh->normalize_border());
+        CGAL_assertion(pMesh->is_valid(true, 3));
       }
       scene->itemChanged(item);
     }
