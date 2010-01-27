968a4abc45fa67277576bad73802c0850785f33e
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2010-01-27 14:49:38 +0000

    std::min  -> (std::min) for VC++

--- Triangulation_3/include/CGAL/Triangulation_hierarchy_3.h
+++ Triangulation_3/include/CGAL/Triangulation_hierarchy_3.h
@@ -506,7 +506,7 @@ random_level()
   boost::geometric_distribution<> proba(1.0/ratio);
   boost::variate_generator<boost::rand48&, boost::geometric_distribution<> > die(random, proba);
 
-  return std::min(die(), (int)maxlevel)-1;
+  return (std::min)(die(), (int)maxlevel)-1;
 }
 
 CGAL_END_NAMESPACE
