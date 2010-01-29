93b2b7cbac3e194e5a9e7941cce14d507ac5d666
Sébastien Loriot <sloriot.ml@gmail.com>
2010-01-29 15:19:39 +0000

    Add bounded priority queue as an internal data-structure. Use the bounded_priority_queue inside Orthogonal_k_neighbor search and K_neighbor_search, a maximum of code is now shared by these two classes. This reduces computation time and memory needed. Extend the interface: to save time the sequence  k-nearest neighbors can be sorted (default) or not. Add a new test that ensures all methods find the same kth closest neighbor. Update manual.

--- Spatial_searching/include/CGAL/K_neighbor_search.h
+++ Spatial_searching/include/CGAL/K_neighbor_search.h
@@ -19,228 +19,108 @@
 
 #ifndef  CGAL_K_NEIGHBOR_SEARCH_H
 #define  CGAL_K_NEIGHBOR_SEARCH_H
-#include <cstring>
-#include <list>
-#include <queue>
-#include <memory>
-#include <CGAL/Kd_tree_node.h>
-#include <CGAL/Kd_tree.h>
-#include <CGAL/Euclidean_distance.h>
-#include <CGAL/Splitters.h>
+
+#include <CGAL/internal/K_neighbor_search.h>
 
 namespace CGAL {
 
-  template <class SearchTraits,
-            class Distance_=Euclidean_distance<SearchTraits>,
-            class Splitter_=Sliding_midpoint<SearchTraits> , 
-            class Tree_=Kd_tree<SearchTraits, Splitter_, Tag_false> >
-
-  class K_neighbor_search {
-
-  public:
-
-    typedef Splitter_ Splitter;
-    typedef Distance_ Distance;
-    typedef Tree_ Tree;
-    typedef typename SearchTraits::Point_d Point_d;
-    typedef typename SearchTraits::FT FT;
-    typedef std::pair<Point_d,FT> Point_with_transformed_distance;
+template <class SearchTraits, 
+          class Distance= Euclidean_distance<SearchTraits>,
+          class Splitter= Sliding_midpoint<SearchTraits> ,
+          class Tree= Kd_tree<SearchTraits, Splitter, Tag_true> >
+class K_neighbor_search: public internal::K_neighbor_search<SearchTraits,Distance,Splitter,Tree> {
+  typedef  internal::K_neighbor_search<SearchTraits,Distance,Splitter,Tree> Base;
   
-    typedef typename Tree::Node_handle Node_handle;
-  
-    typedef typename Tree::Point_d_iterator Point_d_iterator;
-    typedef Kd_tree_rectangle<SearchTraits> Rectangle;
-    typedef typename Distance::Query_item Query_item;
+public:
+  typedef typename Base::FT FT;  
 
-  private:
+  K_neighbor_search(Tree& tree, const typename Base::Query_item& q,  
+    unsigned int k=1, FT Eps=FT(0.0), bool Search_nearest=true, const Distance& d=Distance(),bool sorted=true)
+    : Base(tree,q,k,Eps,Search_nearest,d,sorted) 
+  {
+    compute_neighbors_general(tree.root(),tree.bounding_box());
+    if (sorted) this->queue.sort();    
+  };
 
-    int number_of_internal_nodes_visited;
-    int number_of_leaf_nodes_visited;
-    int number_of_items_visited;
-  
-    bool search_nearest;
-    Distance distance_instance;
-  
-    FT multiplication_factor;
-    Query_item query_object;
-    int total_item_number;
-    FT distance_to_root;   
-  
-    typedef std::list<Point_with_transformed_distance> NN_list;
+private:  
+  using Base::Node_handle;
 
-  public:
+  void 
+  compute_neighbors_general(typename Base::Node_handle N, const Kd_tree_rectangle<SearchTraits>& r) 
+  {
+    if (!(N->is_leaf())) {
+      this->number_of_internal_nodes_visited++;
+      int new_cut_dim=N->cutting_dimension();
+      FT  new_cut_val=N->cutting_value();
 
-    typedef typename NN_list::const_iterator iterator;
+      Kd_tree_rectangle<SearchTraits> r_lower(r);
 
-  private:
+      // modifies also r_lower to lower half
+      Kd_tree_rectangle<SearchTraits> r_upper(r_lower);
+      r_lower.split(r_upper, new_cut_dim, new_cut_val);
 
-    NN_list l;
-    unsigned int max_k;
-    unsigned int actual_k;
-  
-  
-    bool 
-    branch(FT distance) 
-    {
-      if (actual_k<max_k) return true;
-      else 
-	if (search_nearest) return 
-			      ( distance * multiplication_factor < l.rbegin()->second);
-	else return ( distance >
-		      l.begin()->second * multiplication_factor);
-    
-    }
-  
-    void 
-    insert(Point_d* I, FT dist) 
-    {
-      bool insert;
-      if (actual_k<max_k) insert=true;
-      else 
-	if (search_nearest) insert=
-			      ( dist < l.rbegin()->second ); 
-	else insert=(dist > l.begin()->second);
-      if (insert) {
-	actual_k++;	 	
-	typename NN_list::iterator it=l.begin();
-	for (; (it != l.end()); ++it) 
-	  { if (dist < it->second) break;}
-	Point_with_transformed_distance NN_Candidate(*I,dist);
-	l.insert(it,NN_Candidate);
-	if (actual_k > max_k) {
-	  actual_k--;
-	  if (search_nearest) l.pop_back();
-	  else l.pop_front();
-	}
-      }
-    
-    }
-  
-	
-  public:
+      FT distance_to_lower_half;
+      FT distance_to_upper_half;
 
+      if (this->search_nearest) { 
 
-    iterator 
-    begin() const
-    {
-      return l.begin();
-    }
+        distance_to_lower_half = 
+          this->distance_instance. min_distance_to_rectangle(this->query_object, 
+                                                       r_lower);
+                              
+        distance_to_upper_half = 
+          this->distance_instance.min_distance_to_rectangle(this->query_object, 
+                                                      r_upper);
+                      
 
-    iterator 
-    end() const
-    {
-      return l.end();
-    }
-
-    // constructor
-    K_neighbor_search(Tree& tree, const Query_item& q, 
-		      unsigned int k=1, FT Eps=FT(0.0), 
-		      bool Search_nearest=true,
-		      const Distance& d=Distance())
-      : number_of_internal_nodes_visited(0), number_of_leaf_nodes_visited(0), 
-      number_of_items_visited(0), search_nearest(Search_nearest), 
-      distance_instance(d), multiplication_factor(distance_instance.transformed_distance(FT(1.0)+Eps)), query_object(q),
-      total_item_number(tree.size()), max_k(k), actual_k(0)
-    {
-      compute_neighbors_general(tree.root(), tree.bounding_box());
-    }
-
-    // Print statistics of the general standard search process.
-    std::ostream& statistics (std::ostream& s) {
-      s << "General search statistics:" << std::endl;
-      s << "Number of internal nodes visited:" 
-	<< number_of_internal_nodes_visited << std::endl;
-      s << "Number of leaf nodes visited:" 
-	<< number_of_leaf_nodes_visited << std::endl;
-      s << "Number of items visited:" 
-	<< number_of_items_visited << std::endl;
-      return s;
-    }
-
-
-  private:
-   
-
-    void 
-    compute_neighbors_general(Node_handle N, const Kd_tree_rectangle<SearchTraits>& r) 
-    {
-      if (!(N->is_leaf())) {
-	number_of_internal_nodes_visited++;
-	int new_cut_dim=N->cutting_dimension();
-	FT  new_cut_val=N->cutting_value();
-
-	Kd_tree_rectangle<SearchTraits> r_lower(r);
-
-	// modifies also r_lower to lower half
-	Kd_tree_rectangle<SearchTraits> r_upper(r_lower);
-	r_lower.split(r_upper, new_cut_dim, new_cut_val);
-
-	FT distance_to_lower_half;
-	FT distance_to_upper_half;
-
-	if (search_nearest) { 
-
-	  distance_to_lower_half = 
-	    distance_instance. min_distance_to_rectangle(query_object, 
-							 r_lower);
-				
-	  distance_to_upper_half = 
-	    distance_instance.min_distance_to_rectangle(query_object, 
-							r_upper);
-			
-
-	} 
-	else
-	  { 
-
-	    distance_to_lower_half = 
-	      distance_instance.max_distance_to_rectangle(query_object, 
-							  r_lower);
-
-	    distance_to_upper_half = 
-	      distance_instance.max_distance_to_rectangle(query_object, 
-							  r_upper);
-
-	  }
- 
-	if ( (( search_nearest) && 
-	      (distance_to_lower_half < distance_to_upper_half)) 
-	     ||
-	     ((!search_nearest) && 
-	      (distance_to_lower_half >= 
-	       distance_to_upper_half))  )
-	  {
-	    compute_neighbors_general(N->lower(), r_lower);
-	    if (branch(distance_to_upper_half)) 
-	      compute_neighbors_general (N->upper(), r_upper);
-	  }  
-	else
-	  {	compute_neighbors_general(N->upper(), r_upper);
-	  if (branch(distance_to_lower_half)) 
-	    compute_neighbors_general (N->lower(), 
-				       r_lower);
-	  }
-
-      }
+      } 
       else
-	{
-	  // n is a leaf
-	  number_of_leaf_nodes_visited++;
-	  if (N->size() > 0)
-	    for (Point_d_iterator it = N->begin(); it != N->end(); it++) {
-	      number_of_items_visited++;
-	      FT distance_to_query_object =
-		distance_instance.transformed_distance(query_object,**it);
-	      insert(*it,distance_to_query_object);
-	    }
-	}
+        { 
+
+          distance_to_lower_half = 
+            this->distance_instance.max_distance_to_rectangle(this->query_object, 
+                                                        r_lower);
+
+          distance_to_upper_half = 
+            this->distance_instance.max_distance_to_rectangle(this->query_object, 
+                                                        r_upper);
+
+        }
+
+      if ( (( this->search_nearest) && 
+            (distance_to_lower_half < distance_to_upper_half)) 
+           ||
+           ((!this->search_nearest) && 
+            (distance_to_lower_half >= 
+             distance_to_upper_half))  )
+        {
+          compute_neighbors_general(N->lower(), r_lower);
+          if (branch(distance_to_upper_half)) 
+            compute_neighbors_general (N->upper(), r_upper);
+        }  
+      else
+        {	compute_neighbors_general(N->upper(), r_upper);
+        if (branch(distance_to_lower_half)) 
+          compute_neighbors_general (N->lower(), 
+                                     r_lower);
+        }
+
     }
-        
-
-    
-  }; // class 
-
-
+    else
+      {
+        // n is a leaf
+        this->number_of_leaf_nodes_visited++;
+        if (N->size() > 0)
+          for (typename Base::Point_d_iterator it = N->begin(); it != N->end(); it++) {
+            this->number_of_items_visited++;
+            FT distance_to_query_object =
+              this->distance_instance.transformed_distance(this->query_object,**it);
+            this->queue.insert(std::make_pair(*it,distance_to_query_object));
+          }
+      }
+  }
+  
+}; // class 
 
 } // namespace CGAL
 
--- Spatial_searching/include/CGAL/Orthogonal_k_neighbor_search.h
+++ Spatial_searching/include/CGAL/Orthogonal_k_neighbor_search.h
@@ -15,192 +15,56 @@
 // $Id$
 // 
 //
-// Author(s)     : Hans Tangelder (<hanst@cs.uu.nl>)
+// Author(s)     : Gael Guennebaud (gael.guennebaud@inria.fr), Hans Tangelder (<hanst@cs.uu.nl>)
 
 #ifndef CGAL_ORTHOGONAL_K_NEIGHBOR_SEARCH_H
 #define CGAL_ORTHOGONAL_K_NEIGHBOR_SEARCH_H
 
-#include <cstring>
-#include <list>
-#include <queue>
-#include <set>
-#include <memory>
-#include <CGAL/Kd_tree_node.h>
-#include <CGAL/Kd_tree.h>
-#include <CGAL/Euclidean_distance.h>
-#include <CGAL/Splitters.h>
+#include <CGAL/internal/K_neighbor_search.h>
 
 namespace CGAL {
 
 template <class SearchTraits, 
-          class Distance_= Euclidean_distance<SearchTraits>,
-          class Splitter_= Sliding_midpoint<SearchTraits> ,
-          class Tree_= Kd_tree<SearchTraits, Splitter_, Tag_true> >
-class Orthogonal_k_neighbor_search {
-
-public:
-
-  typedef Splitter_ Splitter;
-  typedef Tree_  Tree;
-  typedef Distance_ Distance;
-  typedef typename SearchTraits::Point_d Point_d;
-  typedef typename Distance::Query_item Query_item;
-
-  typedef typename SearchTraits::FT FT;
-  typedef std::pair<Point_d,FT> Point_with_transformed_distance;
-
-  typedef typename Tree::Node_handle Node_handle;
-
-  typedef typename Tree::Point_d_iterator Point_d_iterator;
-
-private:
-
-  // Comparison functor to sort a set of points
-  // in increasing or decreasing order (key is distance).
-  class Distance_larger 
-  {
-    bool search_nearest;
-
-  public:
-
-    Distance_larger(bool search_the_nearest_neighbour) 
-      : search_nearest(search_the_nearest_neighbour)
-    {}
-
-    bool operator()(const Point_with_transformed_distance& p1, 
-                    const Point_with_transformed_distance& p2) const  
-    {
-      if (search_nearest)
-        return (p1.second < p2.second);
-      else
-        return (p2.second < p1.second);
-    }
-  };
+          class Distance= Euclidean_distance<SearchTraits>,
+          class Splitter= Sliding_midpoint<SearchTraits> ,
+          class Tree= Kd_tree<SearchTraits, Splitter, Tag_true> >
+class Orthogonal_k_neighbor_search: public internal::K_neighbor_search<SearchTraits,Distance,Splitter,Tree> {
+  typedef  internal::K_neighbor_search<SearchTraits,Distance,Splitter,Tree> Base;
   
-  // Set of points, sorted by distance, in increasing or decreasing order.
-  typedef std::set<Point_with_transformed_distance, Distance_larger> NN_list;
-
 public:
+  typedef typename Base::FT FT;
 
-  typedef typename NN_list::const_iterator iterator;
-
-private:
-
-  int number_of_internal_nodes_visited;
-  int number_of_leaf_nodes_visited;
-  int number_of_items_visited;
-
-  bool search_nearest;
-
-  FT multiplication_factor;
-  Query_item query_object;
-  int total_item_number;
-  FT distance_to_root;   
-
-  NN_list l; // Set of points, sorted by distance
-  unsigned int max_k;
-  
-  Distance distance_instance;
-
-private:
-
-  // Test if we should continue searching 
-  inline bool branch(FT distance) 
+  Orthogonal_k_neighbor_search(Tree& tree, const typename Base::Query_item& q,  
+                               unsigned int k=1, FT Eps=FT(0.0), bool Search_nearest=true, const Distance& d=Distance(),bool sorted=true)
+    : Base(tree,q,k,Eps,Search_nearest,d,sorted) 
   {
-    if (l.size()<max_k) 
-      return true;
+    if (this->search_nearest) 
+      this->distance_to_root = this->distance_instance.min_distance_to_rectangle(q, tree.bounding_box());
     else 
-      if (search_nearest) 
-        return (distance*multiplication_factor < (--l.end())->second);
-      else 
-        return (distance > l.begin()->second*multiplication_factor);
-  }
+      this->distance_to_root = this->distance_instance.max_distance_to_rectangle(q, tree.bounding_box());
 
-  // Try to insert point *I
-  void insert(Point_d* I, FT dist) 
-  {
-    // Shall we insert I?
-    bool insert;
-    if (l.size()<max_k) 
-      insert=true;
-    else if (search_nearest) 
-      insert = ( dist < (--l.end())->second ); 
-    else 
-      insert=(dist > (--l.end())->second);
       
-    if (insert) 
-    {
-      Point_with_transformed_distance NN_Candidate(*I,dist);
-      l.insert(NN_Candidate);
-      if (l.size() > max_k)
-        l.erase(--l.end()); 
-    }
+    compute_neighbors_orthogonally(tree.root(), this->distance_to_root);      
+      
+    if (sorted) this->queue.sort();      
   }
-
-
-public:
-
-  iterator begin() const
-  {
-    return l.begin();
-  }
-
-  iterator end() const
-  {
-    return l.end();
-  }
-
-
-  // constructor
-  Orthogonal_k_neighbor_search(Tree& tree, const Query_item& q,  
-    unsigned int k=1, FT Eps=FT(0.0), bool Search_nearest=true, const Distance& d=Distance())
-    : number_of_internal_nodes_visited(0), number_of_leaf_nodes_visited(0), number_of_items_visited(0), 
-    search_nearest(Search_nearest), multiplication_factor(d.transformed_distance(1.0+Eps)), query_object(q), 
-    total_item_number(tree.size()), l(Distance_larger(Search_nearest)), max_k(k), distance_instance(d) 
-
-  {
-    if (search_nearest) 
-      distance_to_root = d.min_distance_to_rectangle(q, tree.bounding_box());
-    else 
-      distance_to_root = d.max_distance_to_rectangle(q, tree.bounding_box());
-
-    compute_neighbors_orthogonally(tree.root(), distance_to_root);
-
-  }
-
-
-  // Print statistics of the k_neighbor search process.
-  std::ostream& statistics (std::ostream& s) 
-  {
-    s << "K_Neighbor search statistics:" << std::endl;
-    s << "Number of internal nodes visited:" 
-      << number_of_internal_nodes_visited << std::endl;
-    s << "Number of leaf nodes visited:" 
-      << number_of_leaf_nodes_visited << std::endl;
-    s << "Number of items visited:" 
-      << number_of_items_visited << std::endl;
-    return s;
-  }
-
-
-
 private:
 
-  void compute_neighbors_orthogonally(Node_handle N, FT rd)
+  void compute_neighbors_orthogonally(typename Base::Node_handle N, FT rd)
   {
     typename SearchTraits::Construct_cartesian_const_iterator_d construct_it;
-    typename SearchTraits::Cartesian_const_iterator_d query_object_it = construct_it(query_object);
+    typename SearchTraits::Cartesian_const_iterator_d query_object_it = construct_it(this->query_object);
     if (!(N->is_leaf())) 
     {
-      number_of_internal_nodes_visited++;
+      this->number_of_internal_nodes_visited++;
       int new_cut_dim=N->cutting_dimension();
       FT old_off, new_rd;
       FT new_off = *(query_object_it + new_cut_dim) - N->cutting_value();
-      if ( ((new_off <  FT(0.0)) && (search_nearest)) ||
-           ((new_off >= FT(0.0)) && (!search_nearest)) ) 
+      if ( ((new_off <  FT(0.0)) && (this->search_nearest)) ||
+           ((new_off >= FT(0.0)) && (!this->search_nearest)) ) 
       {
         compute_neighbors_orthogonally(N->lower(),rd);
-        if (search_nearest) {
+        if (this->search_nearest) {
           old_off= *(query_object_it + new_cut_dim) - N->low_value();
           if (old_off>FT(0.0)) 
             old_off=FT(0.0);
@@ -210,14 +74,14 @@ private:
           if (old_off<FT(0.0)) 
             old_off=FT(0.0);
         }
-        new_rd = distance_instance.new_distance(rd,old_off,new_off,new_cut_dim);
+        new_rd = this->distance_instance.new_distance(rd,old_off,new_off,new_cut_dim);
         if (branch(new_rd)) 
           compute_neighbors_orthogonally(N->upper(), new_rd);                               
       }
       else // compute new distance
       {
         compute_neighbors_orthogonally(N->upper(),rd); 
-        if (search_nearest) {
+        if (this->search_nearest) {
           old_off= N->high_value() - *(query_object_it + new_cut_dim);
           if (old_off>FT(0.0)) 
             old_off=FT(0.0);
@@ -227,7 +91,7 @@ private:
           if (old_off<FT(0.0)) 
             old_off=FT(0.0);
         }  
-        new_rd = distance_instance. new_distance(rd,old_off,new_off,new_cut_dim);
+        new_rd = this->distance_instance. new_distance(rd,old_off,new_off,new_cut_dim);
         if (branch(new_rd)) 
           compute_neighbors_orthogonally(N->lower(), new_rd);       
       }
@@ -235,20 +99,20 @@ private:
     else
     {
       // n is a leaf
-      number_of_leaf_nodes_visited++;
+      this->number_of_leaf_nodes_visited++;
       if (N->size() > 0)
       {
-        for (Point_d_iterator it=N->begin(); it != N->end(); it++) 
+        for (typename Base::Point_d_iterator it=N->begin(); it != N->end(); it++) 
         {
-          number_of_items_visited++;
+          this->number_of_items_visited++;
           FT distance_to_query_object=
-            distance_instance.transformed_distance(query_object,**it);
-          insert(*it,distance_to_query_object);
+            this->distance_instance.transformed_distance(this->query_object,**it);
+          this->queue.insert(std::make_pair(*it,distance_to_query_object));
         }
       }
     }
-  }
-
+  }    
+  
 }; // class 
 
 } // namespace CGAL
