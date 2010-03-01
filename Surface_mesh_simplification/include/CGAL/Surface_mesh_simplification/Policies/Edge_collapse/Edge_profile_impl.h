c352a0126833008e996de918484d1e16f58ea38a
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2010-03-01 08:26:45 +0000

    Wrap in CGAL_assertion_code in order to avoid a warning

--- Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_profile_impl.h
+++ Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_profile_impl.h
@@ -196,7 +196,7 @@ void Edge_profile<ECM>::Extract_triangles_and_link( VertexIdxMap const&    verte
     if ( v2 != mV1 )
     {
       mLink.push_back(v2) ;
-      bool lInserted = lCollected.insert(vertex_idx[v2]).second ;
+      CGAL_assertion_code( bool lInserted = ) lCollected.insert(vertex_idx[v2]).second ;
       CGAL_assertion(lInserted);
     }
       
