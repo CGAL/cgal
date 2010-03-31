3b4006f577e0be89453ea5784909a6c6c252b010
Luis Peñaranda <penarand@loria.fr>
2010-03-31 12:52:26 +0000

    removed old test files and protected the rest from compiling when RS is not present by means of #ifdef's

--- Algebraic_kernel_d/test/Algebraic_kernel_d/io_test.cpp
+++ Algebraic_kernel_d/test/Algebraic_kernel_d/io_test.cpp
@@ -1,20 +1,24 @@
-// Copyright (c) 2009 Inria Lorraine (France). All rights reserved.
-// 
+// Copyright (c) 2009,2010 Inria Lorraine (France). All rights reserved.
+//
 // This file is part of CGAL (www.cgal.org); you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation; version 2.1 of the License.
 // See the file LICENSE.LGPL distributed with CGAL.
-// 
+//
 // Licensees holding a valid commercial license may use this file in
 // accordance with the commercial license agreement provided with the software.
-// 
+//
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-// 
+//
 // $URL$
 // $Id$
-// 
+//
 // Author: Luis Peñaranda <luis.penaranda@loria.fr>
+
+#include <CGAL/basic.h>
+
+#if defined(CGAL_USE_GMP) && defined(CGAL_USE_MPFI) && defined(CGAL_USE_RS)
 
 #include <CGAL/Algebraic_kernel_d_1_RS_Gmpz.h>
 
@@ -58,3 +62,8 @@ int main(){
 
   return 0;
 }
+#else
+int main(){
+        return 0;
+}
+#endif
