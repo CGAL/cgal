e66e3c23d8be6ec7a5d36b286fad295204bbdd37
Luis Peñaranda <penarand@loria.fr>
2010-04-01 11:29:00 +0000

    updated examples, using the new files and classes names

--- Algebraic_kernel_d/examples/Algebraic_kernel_d/Compare_1.cpp
+++ Algebraic_kernel_d/examples/Algebraic_kernel_d/Compare_1.cpp
@@ -5,10 +5,10 @@
 
 #if defined(CGAL_USE_GMP) && defined(CGAL_USE_MPFI) && defined(CGAL_USE_RS)
 
-#include <CGAL/Algebraic_kernel_d_1_RS_Gmpz.h>
+#include <CGAL/Algebraic_kernel_rs_gmpz_1.h>
 #include <vector>
 
-typedef CGAL::Algebraic_kernel_d_1_RS_Gmpz              AK;
+typedef CGAL::Algebraic_kernel_rs_gmpz_1                AK;
 typedef AK::Coefficient                                 Coefficient;
 typedef AK::Polynomial_1                                Polynomial_1;
 typedef AK::Algebraic_real_1                            Algebraic_real_1;
