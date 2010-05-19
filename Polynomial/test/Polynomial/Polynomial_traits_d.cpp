0d1c400bb472187ea137ca8469105ea0ff6e04d8
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2010-05-19 07:57:16 +0000

    Split up the function in orer to avoid ICE of VC++ (It seems that it still additionally needs /bigobj)

--- Polynomial/test/Polynomial/Polynomial_traits_d.cpp
+++ Polynomial/test/Polynomial/Polynomial_traits_d.cpp
@@ -16,30 +16,52 @@
 
 
 template < typename AT> 
-void test_AT(){
+void test_AT_1(){
+  CGAL::set_pretty_mode(std::cout);
+  CGAL::set_pretty_mode(std::cerr);
+
+  typedef typename AT::Integer Integer;
+  typedef typename AT::Rational Rational; 
+
+
+  typedef CGAL::Polynomial<Integer> Poly;
+  typedef CGAL::Polynomial_traits_d<Poly> PT; 
+  std::cerr << std::endl;
+  std::cerr << "Test for coefficient type Integer" << std::endl;
+  std::cerr << "--------------------------------------" << std::endl;
+  CGAL::Test_Pol::test_multiple_dimensions(PT());  
+
+}
+
+
+template < typename AT> 
+void test_AT_2(){
   CGAL::set_pretty_mode(std::cout);
   CGAL::set_pretty_mode(std::cerr);
 
   typedef typename AT::Integer Integer;
   typedef typename AT::Rational Rational; 
   
-  {
-  typedef CGAL::Polynomial<Integer> Poly;
-  typedef CGAL::Polynomial_traits_d<Poly> PT; 
-  std::cerr << std::endl;
-  std::cerr << "Test for coefficient type Integer" << std::endl;
-  std::cerr << "--------------------------------------" << std::endl;
-  CGAL::Test_Pol::test_multiple_dimensions(PT());
-  }
-  {
+
   typedef CGAL::Polynomial<Rational> Poly;
   typedef CGAL::Polynomial_traits_d<Poly> PT; 
   std::cerr << std::endl;
   std::cerr << "Test for coefficient type Rational" << std::endl;
   std::cerr << "---------------------------------------" << std::endl;
   CGAL::Test_Pol::test_multiple_dimensions(PT());
-  }
-  {
+
+}
+
+
+template < typename AT> 
+void test_AT_3(){
+  CGAL::set_pretty_mode(std::cout);
+  CGAL::set_pretty_mode(std::cerr);
+
+  typedef typename AT::Integer Integer;
+  typedef typename AT::Rational Rational; 
+  
+
   typedef CGAL::Polynomial< CGAL::Sqrt_extension< Integer, Integer > > Poly;
   typedef CGAL::Polynomial_traits_d<Poly> PT;  
   std::cerr << std::endl;
@@ -49,8 +71,20 @@ void test_AT(){
     "----------------------------------------------------------------------"
             << std::endl;    
   CGAL::Test_Pol::test_multiple_dimensions(PT());    
-  }
-  {
+
+
+}
+
+
+template < typename AT> 
+void test_AT_4(){
+  CGAL::set_pretty_mode(std::cout);
+  CGAL::set_pretty_mode(std::cerr);
+
+  typedef typename AT::Integer Integer;
+  typedef typename AT::Rational Rational; 
+  
+
   typedef CGAL::Polynomial< CGAL::Sqrt_extension< Rational, Integer > > Poly;
   typedef CGAL::Polynomial_traits_d<Poly> PT;
   std::cerr << std::endl;
@@ -60,8 +94,19 @@ void test_AT(){
     "----------------------------------------------------------------------"
             << std::endl;    
   CGAL::Test_Pol::test_multiple_dimensions(PT());    
-  }
-  {
+}
+
+
+template < typename AT> 
+void test_AT_5(){
+  CGAL::set_pretty_mode(std::cout);
+  CGAL::set_pretty_mode(std::cerr);
+
+  typedef typename AT::Integer Integer;
+  typedef typename AT::Rational Rational; 
+  
+
+
   typedef CGAL::Polynomial< CGAL::Sqrt_extension< Rational, Rational > > Poly;
   typedef CGAL::Polynomial_traits_d<Poly> PT;
   std::cerr << std::endl;
@@ -72,8 +117,18 @@ void test_AT(){
     "----------------------------------------------------------------------"
             << std::endl;    
   CGAL::Test_Pol::test_multiple_dimensions(PT());   
-  }
-  {
+}
+
+
+template < typename AT> 
+void test_AT_6(){
+  CGAL::set_pretty_mode(std::cout);
+  CGAL::set_pretty_mode(std::cerr);
+
+  typedef typename AT::Integer Integer;
+  typedef typename AT::Rational Rational; 
+ 
+
   //  Enforce IEEE double precision and to nearest before 
   //  using modular arithmetic
   CGAL::Protect_FPU_rounding<true> pfr(CGAL_FE_TONEAREST);
@@ -88,8 +143,8 @@ void test_AT(){
     "----------------------------------------------------------------------"
             << std::endl;    
   CGAL::Test_Pol::test_multiple_dimensions(PT());   
-  }
 }    
+
 
 int main(){
 
@@ -99,13 +154,24 @@ int main(){
 #ifdef CGAL_USE_LEDA
   {        
     typedef CGAL::LEDA_arithmetic_kernel AT;
-    test_AT<AT>();
+    test_AT_1<AT>();
+    test_AT_2<AT>();
+    test_AT_3<AT>();
+    test_AT_4<AT>();
+    test_AT_5<AT>();
+    test_AT_6<AT>();
   }
 #endif
+
 #ifdef CGAL_USE_CORE
   {    
     typedef CGAL::CORE_arithmetic_kernel AT;
-    test_AT<AT>();
+    test_AT_1<AT>();
+    test_AT_2<AT>();
+    test_AT_3<AT>();
+    test_AT_4<AT>();
+    test_AT_5<AT>();
+    test_AT_6<AT>();
   }
 #endif
 
