0bf96f10dc3aac8823a909cd5fd4b780c2c6f838
Efi Fogel <efif@post.tau.ac.il>
2010-06-15 12:57:39 +0000

    enhanced

--- Minkowski_sum_2/test/Minkowski_sum_2/test_approx_offset.cpp
+++ Minkowski_sum_2/test/Minkowski_sum_2/test_approx_offset.cpp
@@ -17,85 +17,71 @@
 #include <CGAL/Small_side_angle_bisector_decomposition_2.h>
 #include <CGAL/Polygon_convex_decomposition_2.h>
 #include <CGAL/Boolean_set_operations_2.h>
-#include "read_polygon.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <list>
 #include <cstring> // for std::strchr
-#include <cstdio> // for std::sscanf
 
+#include "read_polygon.h"
 
-// instead of
-//typedef CGAL::Cartesian<Rational>                   Kernel;
-// workaround for VC++ 
-struct Kernel : public CGAL::Cartesian<Rational> {};
+typedef CGAL::Cartesian<Rational>                 Kernel;
+typedef Kernel::Point_2                           Point_2;
+typedef CGAL::Polygon_2<Kernel>                   Polygon_2;
 
-typedef Kernel::Point_2                                 Point_2;
-typedef CGAL::Polygon_2<Kernel>                         Polygon_2;
-
-typedef CGAL::Gps_circle_segment_traits_2<Kernel>       Gps_traits_2;
-typedef Gps_traits_2::Polygon_2                         Offset_polygon_2;
-typedef Gps_traits_2::Polygon_with_holes_2   Offset_polygon_with_holes_2;
+typedef CGAL::Gps_circle_segment_traits_2<Kernel> Gps_traits_2;
+typedef Gps_traits_2::Polygon_2                   Offset_polygon_2;
+typedef Gps_traits_2::Polygon_with_holes_2        Offset_polygon_with_holes_2;
 
 /*! Check if two polygons with holes are the same. */
 bool are_equal (const Offset_polygon_with_holes_2& ph1,
                 const Offset_polygon_with_holes_2& ph2)
 {
   std::list<Offset_polygon_with_holes_2>   sym_diff;
-
-  CGAL::symmetric_difference (ph1, ph2,
-                              std::back_inserter(sym_diff));
-
+  CGAL::symmetric_difference (ph1, ph2, std::back_inserter(sym_diff));
   return (sym_diff.empty());
 }
 
 /*! The main program. */
-int main (int argc, char **argv)
+int main (int argc, char* argv[])
 {
+  const double eps = 0.0001;
+
   // Read the input file. Because of the structure of the *.cmd file 
   // (which is concatenated to the command line) we need to get all the
   // inputs in one command line. This is the reason we read triplets/pairs of
   // arguments. Each triplet/double is one input for the program.
-  if (argc < 3)
-  {
+  if (argc < 3) {
     std::cerr << "Usage (input is triplets of): <polygon> <radius> "
-      "[decomposition flags]" << 
-      std::endl;
-    return (1);
+      "[decomposition flags]" << std::endl;
+    return -1;
   }
 
-  // Read the polygon from the input file.
-  Polygon_2   pgn;
-  
-  int i = 1;
-  while (i < argc)
-  {
-    if (! read_polygon (argv[i], pgn))
-    {
-      std::cerr << "Failed to read: <" << argv[i] << ">." << std::endl;
-      return (1);
+  unsigned int i = 1;
+  while (i < argc) {
+    // Read the polygon from the input file.
+    Polygon_2   pgn;
+    const char* filename = argv[i];
+    if (! read_polygon (filename, pgn)) {
+      std::cerr << "Failed to read: <" << filename << ">." << std::endl;
+      return -1;
     }
     
     // Read the offset radius.
-    int         numer, denom;
-    
-    if (std::sscanf (argv[i+1], "%d/%d", &numer, &denom) != 2)
-    {
-      std::cerr << "Invalid radius: " << argv[2] << std::endl;
-      return (1);
-    }
-    
-    std::cout << "Testing " << argv[i] << " and " << argv[i+1] << std::endl;
+    Rational r;
+    std::istringstream iss (argv[i+1], std::istringstream::in);
+    iss >> r;
 
-    Rational     r = Rational (numer, denom);
-    const double eps = 0.0001;
-    
+    std::cout << "Testing " << filename << " and " << r << std::endl;
+
     // Read the decomposition flags.
-    bool         use_ssab = true;
-    bool         use_opt = true;
-    bool         use_hm = true;
-    bool         use_greene = true;
+    bool use_ssab = true;
+    bool use_opt = true;
+    bool use_hm = true;
+    bool use_greene = true;
     
-    if (i+2 < argc && argv[i+2][0] == '-')
-    {
+    if (((i+2) < argc) && (argv[i+2][0] == '-')) {
       use_ssab = (std::strchr (argv[i+2], 's') != NULL);
       use_opt = (std::strchr (argv[i+2], 'o') != NULL);
       use_hm = (std::strchr (argv[i+2], 'h') != NULL);
@@ -103,7 +89,7 @@ int main (int argc, char **argv)
     }
     
     // Compute the Minkowski sum using the convolution method.
-    Offset_polygon_with_holes_2                             offset_conv;
+    Offset_polygon_with_holes_2 offset_conv;
     
     std::cout << "Using the convolution method ... ";
     offset_conv = approximated_offset_2 (pgn, r, eps);
@@ -116,67 +102,44 @@ int main (int argc, char **argv)
     CGAL::Greene_convex_decomposition_2<Kernel>             greene_decomp;
     Offset_polygon_with_holes_2                             offset_decomp;
     
-    if (use_ssab)
-    {
+    if (use_ssab) {
       std::cout << "Using the small-side angle-bisector decomposition ... ";
       offset_decomp = approximated_offset_2 (pgn, r, eps, ssab_decomp);
       if (are_equal (offset_conv, offset_decomp))
-      {
         std::cout << "OK." << std::endl;
-      }
       else
-      {
         std::cout << "ERROR (different result)." << std::endl;
-      }
     }
     
-    if (use_opt)
-    {
+    if (use_opt) {
       std::cout << "Using the optimal convex decomposition ... ";
       offset_decomp = approximated_offset_2 (pgn, r, eps, opt_decomp);
       if (are_equal (offset_conv, offset_decomp))
-      {
         std::cout << "OK." << std::endl;
-      }
       else
-      {
         std::cout << "ERROR (different result)." << std::endl;
-      }
     }
     
-    if (use_hm)
-    {
+    if (use_hm) {
       std::cout << "Using the Hertel--Mehlhorn decomposition ... ";
       offset_decomp = approximated_offset_2 (pgn, r, eps, hm_approx_decomp);
       if (are_equal (offset_conv, offset_decomp))
-      {
         std::cout << "OK." << std::endl;
-      }
       else
-      {
         std::cout << "ERROR (different result)." << std::endl;
-      }
     }
     
-    if (use_greene)
-    {
+    if (use_greene) {
       std::cout << "Using the Greene decomposition ... ";
       offset_decomp = approximated_offset_2 (pgn, r, eps, greene_decomp);
       if (are_equal (offset_conv, offset_decomp))
-      {
         std::cout << "OK." << std::endl;
-      }
       else
-      {
         std::cout << "ERROR (different result)." << std::endl;
-      }
     }
     
-    if (i+2 < argc && argv[i+2][0] == '-')
-      i += 3;
-    else
-      i += 2;
+    i += (((i+2) < argc) && (argv[i+2][0] == '-')) ? 3 : 2;
   }
-  return (0);
+  
+  return 0;
 }
-
