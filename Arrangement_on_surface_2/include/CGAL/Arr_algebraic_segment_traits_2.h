406fcce63bc95c4ce78b53e96645dcc09c2263a1
Michael Kerber <mkerber@mpi-inf.mpg.de>
2010-07-01 21:09:42 +0000

    Added copyright header

--- Arrangement_on_surface_2/include/CGAL/Arr_algebraic_segment_traits_2.h
+++ Arrangement_on_surface_2/include/CGAL/Arr_algebraic_segment_traits_2.h
@@ -1,3 +1,25 @@
+// Copyright (c) 2006-2009 Max-Planck-Institute Saarbruecken (Germany).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL: svn+ssh://hemmer@scm.gforge.inria.fr/svn/cgal/trunk/Arrangement_on_surface_2/include/CGAL/Arr_algebraic_segment_traits_2.h $
+// $Id: Polynomial.h 47254 2008-12-06 21:18:27Z afabri $
+// 
+//
+// Author(s)     : Michael Kerber    <mkerber@mpi-inf.mpg.de>
+//
+// ============================================================================
+
 #include <CGAL/basic.h>
 
 #include <CGAL/Algebraic_kernel_d_1.h>
