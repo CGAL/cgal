2280e4641db614907143a1a8b0ec8c003cb1ec19
Michael Hemmer <hemmer@mpi-inf.mpg.de>
2010-07-07 07:59:47 +0000

    use default arithmetic kernel if present

--- Polynomial/test/Polynomial/Get_arithmetic_kernel.cpp
+++ Polynomial/test/Polynomial/Get_arithmetic_kernel.cpp
@@ -2,29 +2,24 @@
 #include <CGAL/Arithmetic_kernel.h>
 #include <CGAL/Polynomial.h>
 
-template<class AT>
+template<class AK>
 void test_get_arithmetic_kernel(){
-  typedef CGAL::Arithmetic_kernel AK;
-  typedef AK::Integer Integer;
+  typedef typename AK::Integer Integer;
   {
     typedef CGAL::Polynomial<Integer> POLY;
-    typedef CGAL::Get_arithmetic_kernel<POLY>::Arithmetic_kernel AK_;
+    typedef typename CGAL::Get_arithmetic_kernel<POLY>::Arithmetic_kernel AK_;
     BOOST_STATIC_ASSERT((boost::is_same<AK,AK_>::value));
   }{
     typedef CGAL::Polynomial<CGAL::Polynomial<Integer> > POLY;
-    typedef CGAL::Get_arithmetic_kernel<POLY>::Arithmetic_kernel AK_;
+    typedef typename CGAL::Get_arithmetic_kernel<POLY>::Arithmetic_kernel AK_;
     BOOST_STATIC_ASSERT((boost::is_same<AK,AK_>::value));
   }
 }
 
 int main(){ 
-#ifdef CGAL_USE_LEDA
-   test_get_arithmetic_kernel<CGAL::LEDA_arithmetic_kernel>();
-#endif // CGAL_USE_LEDA
-
-#ifdef CGAL_USE_CORE
-    test_get_arithmetic_kernel<CGAL::CORE_arithmetic_kernel>();
-#endif // CGAL_USE_CORE
+#ifdef CGAL_HAS_DEFAULT_ARITHMETIC_KERNEL
+   test_get_arithmetic_kernel<CGAL::Arithmetic_kernel>();
+#endif // CGAL_HAS_DEFAULT_ARITHMETIC_KERNEL
     return 0;
 }
 
