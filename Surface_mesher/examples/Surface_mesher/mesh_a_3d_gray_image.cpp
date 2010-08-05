e1793f56f4919a6e99b3bdf9f56c1f7cc8f70ca2
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2010-08-05 12:02:59 +0000

    Add f to a floating point constant in order to suppress VC++ warning

--- Surface_mesher/examples/Surface_mesher/mesh_a_3d_gray_image.cpp
+++ Surface_mesher/examples/Surface_mesher/mesh_a_3d_gray_image.cpp
@@ -22,7 +22,7 @@ int main() {
   C2t3 c2t3 (tr);   // 2D-complex in 3D-Delaunay triangulation
 
   // the 'function' is a 3D gray level image
-  Gray_level_image image("data/skull_2.9.inr", 2.9);
+  Gray_level_image image("data/skull_2.9.inr", 2.9f);
 
   // Carefully choosen bounding sphere: the center must be inside the
   // surface defined by 'image' and the radius must be high enough so that
