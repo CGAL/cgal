2f702cdbf418f5080cbad370758983848758bd87
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2010-08-20 14:31:12 +0000

    Add a script to create all zip files of demos, from test results.

---
+++ Scripts/developer_scripts/create_demos_zip_files.sh
@@ -0,0 +1,16 @@
+pushd AABB_tree_Demo;                   zip ../AABB_demo.zip          *    ;      popd
+pushd Alpha_shapes_3_Demo;              zip ../alpha_shape_3.zip      *    ;      popd
+pushd Bounding_volumes_Demo;            zip ../bounding_volumes_2.zip *    ;      popd
+pushd Circular_kernel_2_Demo;           zip ../circular_kernel.zip    *    ;      popd
+pushd Mesh_3_Demo;                      zip ../mesh_3.zip             *    ;      popd
+pushd Periodic_3_triangulation_3_Demo;  zip ../periodic_3_triangulation_3.zip *;  popd
+pushd Periodic_Lloyd_3_Demo;            zip ../periodic_3_triangulation_3.zip *;  popd
+pushd Polygon_Demo;                     zip ../polygon.zip *               ;      popd
+pushd Polyhedron_Demo;                  zip ../polyhedron_3.zip *          ;      popd
+pushd Segment_Delaunay_graph_2_Demo;    zip ../segment_voronoi_diagram_2.zip *;   popd
+pushd Surface_mesher_Demo;              zip ../surface_mesher.zip *;              popd
+pushd Triangulation_2_Demo;
+  zip ../regular_triangulation_2.zip Regular_triangulation_2.exe
+  zip ../constrained_delaunay_triangulation_2.zip Constrained_Delaunay_triangulation_2.exe
+  zip ../delaunay_triangulation_2.zip Delaunay_triangulation_2.exe
+popd
