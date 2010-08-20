e8003c231978207f3bb742bf5a8f0cbfecbe5e7b
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2010-08-20 16:35:23 +0000

    Backport from trunk:   | ------------------------------------------------------------------------   | r58207 | lrineau | 2010-08-20 18:28:49 +0200 (Fri, 20 Aug 2010) | 3 lines   | Changed paths:   |    M /trunk/Installation/include/CGAL/auto_link/TAUCS.h   |   | Fix the auto-link of libtaucs and libmetis on 32 bits: no name   | mangling. Same for 32 and 64 bits.   |   | ------------------------------------------------------------------------

--- Installation/include/CGAL/auto_link/TAUCS.h
+++ Installation/include/CGAL/auto_link/TAUCS.h
@@ -26,52 +26,20 @@
 // Skip the whole file if auto-link is off
 #if !defined(CGAL_NO_AUTOLINK_TAUCS) && !defined(CGAL_NO_AUTOLINK)
 
+#  if defined(_WIN32) || defined(_WIN64) 
 
-#if defined(_WIN32) && !defined(_WIN64) // if Windows 32 bits
+#    define CGAL_LIB_NAME libtaucs
+#    define CGAL_AUTO_LINK_NOMANGLE
+#    include <CGAL/auto_link/auto_link.h>
 
-// CGAL ships with TAUCS for Windows 32 bits, i.e this set of libraries (e.g. for VC++ 8 /MD):
-// libtaucs-vc80-mt.lib libmetis-vc80-mt.lib liblapack.lib libf77blas.lib libcblas.lib libatlas.lib vcf2c-vc80-mt.lib
-//
-// Notes: - Order matters.
-//        - Libraries with no "vc" toolset are compiled by gcc/g77. They are
-//          compatible with VC++ 7.1, 8.0 and 9.0, and with all VC++ runtimes.
-//        - Tested with 7.1, 8.0 and 9.0.
-
-#define CGAL_LIB_NAME libtaucs
-#include <CGAL/auto_link/auto_link.h>
-
-#define CGAL_LIB_NAME libmetis
-#include <CGAL/auto_link/auto_link.h>
+#    define CGAL_LIB_NAME libmetis
+#    define CGAL_AUTO_LINK_NOMANGLE
+#    include <CGAL/auto_link/auto_link.h>
 
 // Link with LAPACK, BLAS and F2C
-#include <CGAL/auto_link/LAPACK.h>
+#    include <CGAL/auto_link/LAPACK.h>
 
-#endif // Win32
-
-
-#ifdef _WIN64 // if Windows 64 bits
-
-// ATLAS is not compatible with Win64, thus CGAL ships with CLAPACK and CBLAS.
-// VC++ >= 8.0 is compatible with Windows 64 bits.
-// The set set of libraries is (e.g. for VC++ 8 /MD):
-// libtaucs-vc80-mt.lib libmetis-vc80-mt.lib clapack-vc80-mt.lib blas-vc80-mt.lib vcf2c-vc80-mt.lib
-//
-// Notes: - Order matters.
-//        - Tested with VC++ 8.0 and 9.0.
-
-#define CGAL_LIB_NAME libtaucs
-#define CGAL_AUTO_LINK_NOMANGLE
-#include <CGAL/auto_link/auto_link.h>
-
-#define CGAL_LIB_NAME libmetis
-#define CGAL_AUTO_LINK_NOMANGLE
-#include <CGAL/auto_link/auto_link.h>
-
-// Link with LAPACK, BLAS and F2C
-#include <CGAL/auto_link/LAPACK.h>
-
-#endif // _WIN64
-
+#  endif // Win32|Win64
 
 #endif // CGAL_NO_AUTOLINK_TAUCS && CGAL_NO_AUTOLINK
 
