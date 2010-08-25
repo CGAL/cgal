6208b71aaf8533447f59486ac31aaafa0fa8313e
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2010-08-25 15:04:34 +0000

    Towards a Qt4 based Apollonius_graph_2 demo

---
+++ GraphicsView/include/CGAL/Qt/ApolloniusGraphGraphicsItem.h
@@ -0,0 +1,116 @@
+// Copyright (c) 2008  GeometryFactory Sarl (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you may redistribute it under
+// the terms of the Q Public License version 1.0.
+// See the file LICENSE.QPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : Andreas Fabri <Andreas.Fabri@geometryfactory.com>
+//                 Laurent Rineau <Laurent.Rineau@geometryfactory.com>
+
+#ifndef CGAL_QT_APOLLONIUS_GRAPH_GRAPHICS_ITEM_H
+#define CGAL_QT_APOLLONIUS_GRAPH_GRAPHICS_ITEM_H
+
+
+
+#include <CGAL/Qt/GraphicsItem.h>
+#include <CGAL/Qt/PainterOstream.h>
+#include <CGAL/Qt/utility.h>
+
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QPainter>
+#include <QStyleOption>
+
+class QGraphicsSceneMouseEvent;
+
+
+namespace CGAL {
+namespace Qt {
+
+template <typename AG, typename K>
+class ApolloniusGraphGraphicsItem : public GraphicsItem
+{
+public:
+  ApolloniusGraphGraphicsItem(AG* ag);
+
+
+  QRectF 
+  boundingRect() const;
+  
+  void 
+  paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+  
+  void 
+  modelChanged();
+
+  const QPen& edgesPen() const
+  {
+    return edges_pen;
+  }
+
+  void setEdgesPen(const QPen& pen)
+  {
+    edges_pen = pen;
+  }
+
+private:
+  AG * ag;
+  QPen edges_pen;
+};
+
+
+
+template <typename AG, typename K>
+ApolloniusGraphGraphicsItem<AG,K>::ApolloniusGraphGraphicsItem(AG * ag)
+  :  ag(ag)
+{
+  setZValue(3);
+}
+
+template <typename AG, typename K>
+QRectF 
+ApolloniusGraphGraphicsItem<AG,K>::boundingRect() const
+{
+  QRectF rect = CGAL::Qt::viewportsBbox(scene());
+  return rect;
+}
+
+
+template <typename AG, typename K>
+void 
+ApolloniusGraphGraphicsItem<AG,K>::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *w)
+{
+  QRectF rect = option->exposedRect;
+  PainterOstream<K> pos(painter, rect);
+  
+  for(typename AG::Sites_iterator it = ag->sites_begin();
+	 it != ag->sites_end(); it++ ) {
+    pos << typename K::Circle_2(it->point(), it->weight());
+  }
+  std::cerr << "paint" << std::endl;
+  ag->draw_dual(pos);
+}
+
+
+  template <typename AG, typename K>
+void 
+  ApolloniusGraphGraphicsItem<AG,K>::modelChanged()
+{
+  update();
+}
+
+} // namespace Qt
+} // namespace CGAL
+
+#endif // CGAL_QT_APOLLONIUS_GRAPH_GRAPHICS_ITEM_H
