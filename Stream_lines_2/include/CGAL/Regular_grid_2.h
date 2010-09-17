a73c890e6d14fcf4eb99914d9066b498d3775f26
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2010-09-17 20:59:34 +0000

    Add a GraphicsItem for the vector field

--- Stream_lines_2/include/CGAL/Regular_grid_2.h
+++ Stream_lines_2/include/CGAL/Regular_grid_2.h
@@ -21,8 +21,7 @@
 #define CGAL_REGULAR_GRID_2_H_
 
 #include <CGAL/basic.h>
-#include <CGAL/Cartesian.h>
-#include "streamlines_assertions.h"
+#include <CGAL/streamlines_assertions.h>
 
 namespace CGAL {
 
@@ -41,22 +40,22 @@ public:
   typedef typename StreamLinesTraits_2::Vector_2 Vector_2;
 protected:
   FT *vector_field;
-  inline int get_index(const int & i,const int & j) const;
+  inline int get_index(int i,int j) const;
   int number_of_samples_x;
   int number_of_samples_y;
   FT domain_size_x;
   FT domain_size_y;
   Vector_2 get_vector_field(const Point_2 & p) const;
   FT get_density_field(const Point_2 & p) const;
-  bool is_in_samples(const int & i,const int & j) const;
+  bool is_in_samples(int i,int j) const;
 public:
-  Regular_grid_2(const int & m, const int & n,const FT & x, const FT & y);
+  Regular_grid_2(int m, int n,const FT & x, const FT & y);
   //   Regular_grid_2();
   ~Regular_grid_2()
     {
       delete [] vector_field;
     }
-  void fill(std::ifstream & f, const FT & x, const FT & y);
+
   inline typename Geom_traits::Iso_rectangle_2 bbox() const;
 
   std::pair<Vector_2,FT> 
@@ -71,7 +70,9 @@ public:
   inline bool is_in_domain(const Point_2 & p) const;
   inline FT get_integration_step(const Point_2 &) const;
   inline FT get_integration_step() const;
-  inline void set_field(const int & i, const int & j, const Vector_2 & v);
+  inline void set_field(int i, int j, const Vector_2 & v);
+  inline Vector_2 get_field(int i, int j) const;
+
   inline std::pair<int, int> get_dimension()
     {
       return std::pair<int, int>(number_of_samples_x, number_of_samples_y);
@@ -83,7 +84,7 @@ public:
 	    domain_size_y);
     }
     
-  inline FT container_velue(int i) const
+  inline FT container_value(int i) const
     {
       if (i < 2*number_of_samples_x*number_of_samples_y)
         return vector_field[i];
@@ -104,15 +105,14 @@ Regular_grid_2<StreamLinesTraits_2>::bbox() const
 
 template <class StreamLinesTraits_2>
 inline int 
-Regular_grid_2<StreamLinesTraits_2>::get_index(const int & i, const
-					       int & j) const
+Regular_grid_2<StreamLinesTraits_2>::get_index(int i, int j) const
 {
   return 2*(number_of_samples_x*j + i);
 }
 
 template <class StreamLinesTraits_2>
-Regular_grid_2<StreamLinesTraits_2>::Regular_grid_2(const int & m,
-						    const int &
+Regular_grid_2<StreamLinesTraits_2>::Regular_grid_2(int m,
+						    int
 						    n,const FT & x,
 						    const FT & y)
 {
@@ -123,10 +123,20 @@ Regular_grid_2<StreamLinesTraits_2>::Regular_grid_2(const int & m,
   vector_field = new FT[number_of_samples_x*number_of_samples_y* 2];
 }
 
+
+template <class StreamLinesTraits_2>
+inline typename Regular_grid_2<StreamLinesTraits_2>::Vector_2
+Regular_grid_2<StreamLinesTraits_2>::get_field(int i, int j) const
+{
+  CGAL_streamlines_precondition(is_in_samples(i,j));
+  int index = get_index(i,j);
+  return Vector_2(vector_field[index], vector_field[index+1]);
+}
+
 template <class StreamLinesTraits_2>
 inline void
-Regular_grid_2<StreamLinesTraits_2>::set_field(const int & i, const
-					       int & j, const Vector_2
+Regular_grid_2<StreamLinesTraits_2>::set_field(int i, 
+					       int j, const Vector_2
 					       & v)
 {
   CGAL_streamlines_precondition(is_in_samples(i,j));
@@ -146,8 +156,8 @@ Regular_grid_2<StreamLinesTraits_2>::is_in_domain(const Point_2 & p)
 
 template <class StreamLinesTraits_2>
 bool 
-Regular_grid_2<StreamLinesTraits_2>::is_in_samples(const int & i,
-						   const int & j)
+Regular_grid_2<StreamLinesTraits_2>::is_in_samples(int i,
+						   int j)
   const
 {
   return ((i>=0) && (i<=number_of_samples_x-1) && (j>=0) &&
@@ -170,10 +180,10 @@ Regular_grid_2<StreamLinesTraits_2>::get_vector_field(const Point_2 &
   int iIndex_2 = get_index(i+1,j);
   int iIndex_3 = get_index(i,j+1);
   int iIndex_4 = get_index(i+1,j+1);
-  FT x00 = container_velue(iIndex_1);
-  FT x01 = container_velue(iIndex_2);
-  FT x10 = container_velue(iIndex_3);
-  FT x11 = container_velue(iIndex_4);
+  FT x00 = container_value(iIndex_1);
+  FT x01 = container_value(iIndex_2);
+  FT x10 = container_value(iIndex_3);
+  FT x11 = container_value(iIndex_4);
   FT x0 = x00 + xfract * (x01 - x00);
   FT x1 = x10 + xfract * (x11 - x10);
   fXv = x0 + yfract * (x1 - x0);
@@ -181,10 +191,10 @@ Regular_grid_2<StreamLinesTraits_2>::get_vector_field(const Point_2 &
   iIndex_2++;
   iIndex_3++;
   iIndex_4++;
-  FT y00 = container_velue(iIndex_1);
-  FT y01 = container_velue(iIndex_2);
-  FT y10 = container_velue(iIndex_3);
-  FT y11 = container_velue(iIndex_4);
+  FT y00 = container_value(iIndex_1);
+  FT y01 = container_value(iIndex_2);
+  FT y10 = container_value(iIndex_3);
+  FT y11 = container_value(iIndex_4);
   FT y0 = y00 + xfract * (y01 - y00);
   FT y1 = y10 + xfract * (y11 - y10);
   fYv = y0 + yfract * (y1 - y0);
