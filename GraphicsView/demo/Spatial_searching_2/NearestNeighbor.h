086ce1423f89855970db809fd3af495537754812
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2010-09-22 14:31:13 +0000

    First version of a Qt4 based 2D spatial searching demo

---
+++ GraphicsView/demo/Spatial_searching_2/NearestNeighbor.h
@@ -0,0 +1,146 @@
+#ifndef CGAL_QT_NEAREST_NEIGHBOR_H
+#define CGAL_QT_NEAREST_NEIGHBOR_H
+
+#include <vector>
+#include <QGraphicsSceneMouseEvent> 
+#include <QGraphicsScene>
+#include <QGraphicsEllipseItem>
+#include <QEvent>
+#include <QPen>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Qt/GraphicsViewInput.h>
+
+#include <CGAL/Qt/PointsGraphicsItem.h>
+
+namespace CGAL {
+namespace Qt {
+
+template <typename T>
+class NearestNeighbor : public GraphicsViewInput
+{
+  typedef typename T::Point_d Point_2; 
+public:
+  NearestNeighbor(QGraphicsScene* s, typename T::Tree  * t_, QObject* parent, int N);
+  ~NearestNeighbor();
+ 
+  void setPen(const QPen& pen);
+
+  void show();
+  void hide();
+  
+  void setN(int n)
+  {
+    N = n;
+  }
+
+protected:
+
+  virtual void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+  bool eventFilter(QObject *obj, QEvent *event);
+
+private:
+
+  int N;
+  typename T::Tree * t;
+  QGraphicsScene *scene_;
+  QGraphicsEllipseItem* circle;
+  std::vector<Point_2> points;
+  CGAL::Qt::PointsGraphicsItem<std::vector<Point_2> > * pgi;
+};
+
+
+template <typename T>
+NearestNeighbor<T>::NearestNeighbor(QGraphicsScene* s,
+                                    typename T::Tree * t,
+                                    QObject* parent,
+                                    int N)
+  :  GraphicsViewInput(parent), t(t), scene_(s), N(N)
+{
+  pgi = new PointsGraphicsItem<std::vector<Point_2> >(&points);
+  pgi->setVerticesPen(QPen(::Qt::red, 3, ::Qt::SolidLine, ::Qt::RoundCap, ::Qt::RoundJoin));
+  pgi->setZValue(10);
+  scene_->addItem(pgi);
+
+  circle = new QGraphicsEllipseItem();
+  circle->hide();
+  scene_->addItem(circle);
+}
+
+
+template <typename T>
+NearestNeighbor<T>::~NearestNeighbor()
+{
+}
+
+
+template <typename T>
+void
+NearestNeighbor<T>::setPen(const QPen& pen)
+{
+  circle->setPen(pen);
+}
+
+
+template <typename T>
+void
+NearestNeighbor<T>::show()
+{
+  circle->show();
+}
+
+
+template <typename T>
+void
+NearestNeighbor<T>::hide()
+{
+  circle->hide();
+}
+
+
+template <typename T>
+void 
+NearestNeighbor<T>::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+  if(t->empty()){
+    points.clear();
+    circle->hide();
+    return;
+  }
+  Point_2 query(event->scenePos().x(), event->scenePos().y());
+  T search(*t, query, N);
+
+  points.clear();
+  double sr = 0;
+  for(T::iterator it = search.begin(); it != search.end(); ++it){
+    if(sr < it->second){
+      points.push_back(it->first);
+      sr = it->second;
+    }
+  }
+  pgi->modelChanged();
+  typename CGAL::Kernel_traits<Point_2>::Kernel::Circle_2 c(query, sr);
+  CGAL::Bbox_2 bb = c.bbox();
+  circle->setRect(bb.xmin(), bb.ymin(), bb.xmax()-bb.xmin(), bb.ymax()-bb.ymin());
+  circle->show();
+}
+
+
+template <typename T>
+bool 
+NearestNeighbor<T>::eventFilter(QObject *obj, QEvent *event)
+{
+  if (event->type() == QEvent::GraphicsSceneMouseMove) {
+    QGraphicsSceneMouseEvent *mouseEvent = static_cast<QGraphicsSceneMouseEvent *>(event);
+    mouseMoveEvent(mouseEvent);
+    return false; // don't consume the event
+  } else{
+    // standard event processing
+    return QObject::eventFilter(obj, event);
+  }
+} 
+
+} // namespace Qt
+} // namespace CGAL
+
+#endif // CGAL_QT_NEAREST_NEIGHBOR_H
---
+++ GraphicsView/demo/Spatial_searching_2/Spatial_searching_2.cpp
@@ -0,0 +1,303 @@
+#include <fstream>
+// CGAL headers
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/point_generators_2.h>
+#include <CGAL/Orthogonal_k_neighbor_search.h>
+#include <CGAL/Search_traits_2.h>
+
+// Qt headers
+#include <QtGui>
+#include <QString>
+#include <QFileDialog>
+#include <QGraphicsLineItem>
+
+// GraphicsView items and event filters (input classes)
+#include <CGAL/Qt/PointsGraphicsItem.h>
+#include <CGAL/Qt/utility.h>
+  
+// the two base classes
+#include "ui_Spatial_searching_2.h"
+#include <CGAL/Qt/DemosMainWindow.h>
+
+#include "NearestNeighbor.h"
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::Point_2 Point_2;
+typedef K::Vector_2 Vector_2;
+typedef K::Segment_2 Segment_2;
+typedef K::Iso_rectangle_2 Iso_rectangle_2;
+typedef CGAL::Search_traits_2<K> TreeTraits;
+typedef CGAL::Orthogonal_k_neighbor_search<TreeTraits> Neighbor_search;
+typedef Neighbor_search::Tree Tree;
+
+typedef CGAL::Qt::NearestNeighbor<Neighbor_search> NearestNeighbor;
+
+class MainWindow :
+  public CGAL::Qt::DemosMainWindow,
+  public Ui::Spatial_searching_2
+{
+  Q_OBJECT
+  
+private:
+  Tree tree;
+
+  CGAL::Qt::Converter<K> convert;
+  QGraphicsScene scene;  
+
+  CGAL::Qt::PointsGraphicsItem<Tree> * pgi;
+  NearestNeighbor * nearest_neighbor;
+
+public:
+  MainWindow();
+
+  template <typename G>
+  void
+  on_actionGenerate_triggered()
+  {
+
+    QRectF rect = CGAL::Qt::viewportsBbox(&scene);
+    CGAL::Qt::Converter<K> convert;  
+    Iso_rectangle_2 isor = convert(rect);
+    Point_2 center = CGAL::midpoint(isor[0], isor[2]);
+    Vector_2 offset = center - CGAL::ORIGIN;
+    double w = isor.xmax() - isor.xmin();
+    double h = isor.ymax() - isor.ymin();
+    double radius = (w<h) ? w/2 : h/2;
+
+    G pg(radius);
+    bool ok = false;
+    const int number_of_points = 
+      QInputDialog::getInteger(this, 
+                               tr("Number of random points"),
+                               tr("Enter number of random points"),
+                               100,
+                               0,
+                               std::numeric_limits<int>::max(),
+                               1,
+                               &ok);
+
+    if(!ok) {
+      return;
+    }
+
+    // wait cursor
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+
+    std::vector<Point_2> points; 
+
+    points.reserve(number_of_points);
+    for(int i = 0; i < number_of_points; ++i){
+      points.push_back(*pg + offset);
+      ++pg;
+    }
+    tree.insert(points.begin(), points.end());
+    
+    // default cursor
+    QApplication::restoreOverrideCursor();
+    emit(changed());
+  }
+
+public slots:
+
+  virtual void open(QString fileName);
+  void N_changed(int i);
+  void on_actionClear_triggered();
+  void on_actionLoadPoints_triggered();
+  void on_actionRecenter_triggered();
+  void on_actionGeneratePointsOnCircle_triggered();
+  void on_actionGeneratePointsInSquare_triggered();
+  void on_actionGeneratePointsInDisc_triggered();
+
+  void clear();
+
+signals:
+  void changed();
+};
+
+
+MainWindow::MainWindow()
+  : DemosMainWindow()
+{
+  setupUi(this);
+
+  this->graphicsView->setAcceptDrops(false);
+
+  std::vector<Point_2> points; 
+  points.push_back(Point_2(0,0));
+  points.push_back(Point_2(2,2));
+  points.push_back(Point_2(102,2));
+  tree.insert(points.begin(), points.end());
+  // Add a GraphicItem for the point set
+  pgi = new CGAL::Qt::PointsGraphicsItem<Tree>(&tree);
+
+  QObject::connect(this, SIGNAL(changed()),
+		   pgi, SLOT(modelChanged()));
+
+  pgi->setVerticesPen(QPen(Qt::black, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+  scene.addItem(pgi);
+
+  nearest_neighbor = new NearestNeighbor(&scene, &tree, this, this->nn->value());
+  nearest_neighbor->setPen(QPen(Qt::red, 0, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin));
+  scene.installEventFilter(nearest_neighbor);
+
+  // 
+  // Manual handling of actions
+  //
+
+  QObject::connect(this->nn, SIGNAL(valueChanged(int)),
+		   this, SLOT(N_changed(int)));
+
+  QObject::connect(this->actionQuit, SIGNAL(triggered()), 
+		   this, SLOT(close()));
+
+ 
+  //
+  // Setup the scene and the view
+  //
+  scene.setItemIndexMethod(QGraphicsScene::NoIndex);
+  scene.setSceneRect(0, 0, 10, 10);
+  this->graphicsView->setScene(&scene);
+  this->graphicsView->setMouseTracking(true);
+
+  // Uncomment the following line to get antialiasing by default.
+//   actionUse_Antialiasing->setChecked(true);
+
+  // Turn the vertical axis upside down
+  this->graphicsView->scale(1, -1);
+                                                      
+  // The navigation adds zooming and translation functionality to the
+  // QGraphicsView
+  this->addNavigation(this->graphicsView);
+
+  this->setupStatusBar();
+  this->setupOptionsMenu();
+  this->addAboutDemo(":/cgal/help/about_Spatial_searching_2.html");
+  this->addAboutCGAL();
+
+  this->addRecentFiles(this->menuFile, this->actionQuit);
+  connect(this, SIGNAL(openRecentFile(QString)),
+	  this, SLOT(open(QString)));
+}
+
+
+
+void MainWindow::N_changed(int i)
+{
+  nearest_neighbor->setN(i);
+  emit(changed());
+}
+
+
+/* 
+ *  Qt Automatic Connections
+ *  http://doc.trolltech.com/4.4/designer-using-a-component.html#automatic-connections
+ * 
+ *  setupUi(this) generates connections to the slots named
+ *  "on_<action_name>_<signal_name>"
+ */
+
+
+
+void
+MainWindow::on_actionClear_triggered()
+{
+  clear();
+  emit(changed());
+}
+
+void
+MainWindow::on_actionRecenter_triggered()
+{
+  // this->graphicsView->setSceneRect(convert(square));
+  // this->graphicsView->fitInView(convert(square), Qt::KeepAspectRatio);  
+}
+
+void
+MainWindow::on_actionGeneratePointsOnCircle_triggered()
+{
+  typedef CGAL::Random_points_on_circle_2<Point_2> Generator;
+  on_actionGenerate_triggered<Generator>();
+}
+
+
+void
+MainWindow::on_actionGeneratePointsInSquare_triggered()
+{
+  typedef CGAL::Random_points_in_square_2<Point_2> Generator;
+  on_actionGenerate_triggered<Generator>();
+}
+
+
+void
+MainWindow::on_actionGeneratePointsInDisc_triggered()
+{
+  typedef CGAL::Random_points_in_disc_2<Point_2> Generator;
+  on_actionGenerate_triggered<Generator>();
+}
+
+
+void
+MainWindow::on_actionLoadPoints_triggered()
+{
+  QString fileName = QFileDialog::getOpenFileName(this,
+						  tr("Open Points file"),
+						  ".");
+  if(! fileName.isEmpty()){
+    open(fileName);
+  }
+}
+
+
+
+void
+MainWindow::open(QString fileName)
+{
+  // wait cursor
+  QApplication::setOverrideCursor(Qt::WaitCursor);
+  std::ifstream ifs(qPrintable(fileName));
+  
+  K::Point_2 p;
+  std::vector<K::Point_2> points;
+  while(ifs >> p) {
+    points.push_back(p);
+  }
+  tree.insert(points.begin(), points.end());
+
+  // default cursor
+  QApplication::restoreOverrideCursor();
+  this->addToRecentFiles(fileName);
+  actionRecenter->trigger();
+  emit(changed());
+    
+}
+
+void
+MainWindow::clear()
+{
+  tree.clear();  
+}
+
+
+#include "Spatial_searching_2.moc"
+
+int main(int argc, char **argv)
+{
+  QApplication app(argc, argv);
+
+  app.setOrganizationDomain("geometryfactory.com");
+  app.setOrganizationName("GeometryFactory");
+  app.setApplicationName("Spatial_searching_2 demo");
+
+  // Import resources from libCGALQt4.
+  // See http://doc.trolltech.com/4.4/qdir.html#Q_INIT_RESOURCE
+  Q_INIT_RESOURCE(File);
+  Q_INIT_RESOURCE(Spatial_searching_2);
+  Q_INIT_RESOURCE(Input);
+  Q_INIT_RESOURCE(CGAL);
+
+  MainWindow mainWindow;
+  mainWindow.show();
+  QApplication::setOverrideCursor(Qt::CrossCursor);
+  mainWindow.on_actionRecenter_triggered();
+  return app.exec();
+}
