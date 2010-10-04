360ae76da73d46499c021c10f4a0aea08c57c50d
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2010-10-04 08:36:27 +0000

    Fix the compute_normal: sum normals and *then* divide the normal by its length.

--- Polyhedron/demo/Polyhedron/include/CGAL/compute_normal.h
+++ Polyhedron/demo/Polyhedron/include/CGAL/compute_normal.h
@@ -16,9 +16,7 @@ typename Kernel::Vector_3 compute_facet_normal(const Facet& f)
     const Point& curr = he->vertex()->point();
     const Point& next = he->next()->vertex()->point();
     Vector n = CGAL::cross_product(next-curr,prev-curr);
-    if((n*n) > 0.000000001){
-      normal = normal + (n / std::sqrt(n*n));
-    }
+    normal = normal + n;
   }
   return normal / std::sqrt(normal * normal);
 }
