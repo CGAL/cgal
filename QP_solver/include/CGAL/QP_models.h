03f90cf9b888e6da90b04403c8703de7e348cb50
Yves Brise <ybrise@inf.ethz.ch>
2010-10-15 09:28:49 +0000

    Counting iterator with non-default template arguments in QP_models.h

--- QP_solver/include/CGAL/QP_models.h
+++ QP_solver/include/CGAL/QP_models.h
@@ -426,7 +426,8 @@ private:
 				    boost::counting_iterator<int> >
   Sparse_vector_iterator;  
   typedef boost::transform_iterator<CGAL::Map_with_default<Sparse_r_vector>,
-  boost::counting_iterator<int> >
+  boost::counting_iterator<int,boost::use_default,int> >
+  //boost::counting_iterator<int,boost::use_default,boost::use_default> >
   Sparse_r_vector_iterator;
   typedef boost::transform_iterator<CGAL::Map_with_default<Sparse_f_vector>,
 				    boost::counting_iterator<int> >
@@ -529,7 +530,8 @@ public:
   R_iterator get_r() const    
   { 
     CGAL_qpe_assertion(is_valid());
-    return R_iterator (boost::counting_iterator<int>(0),
+    return R_iterator (boost::counting_iterator<int,boost::use_default,int>(0),
+    //return R_iterator (boost::counting_iterator<int,boost::use_default,boost::use_default>(0),
 		        CGAL::Map_with_default<Sparse_r_vector>
 		       (&r_vector, default_r));
   }
