52aadfb0844bdf0f5d56d79c2ba7e33931771edf
Alexander Kobel <akobel@mpi-inf.mpg.de>
2010-10-21 10:13:43 +0000

    fix push_back -> operator[] access for known vector size

--- Algebraic_kernel_d/include/CGAL/Algebraic_kernel_d/Bitstream_coefficient_kernel_at_alpha.h
+++ Algebraic_kernel_d/include/CGAL/Algebraic_kernel_d/Bitstream_coefficient_kernel_at_alpha.h
@@ -201,7 +201,7 @@ public:
                 CGAL::Polynomial_traits_d<Coefficient>::Get_coefficient coeff;
             std::vector<Bigfloat_interval> coeffs(CGAL::degree(f)+1);
             for(int i = 0; i <= CGAL::degree(f); i++) {
-                coeffs.push_back(CGAL::convert_to_bfi(coeff(f,i)));
+                coeffs[i] = CGAL::convert_to_bfi(coeff(f,i));
             }
             return typename CGAL::Polynomial_traits_d<Coefficient>
                 ::template Rebind<Bigfloat_interval,1>::Other
