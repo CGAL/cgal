55540024a0bbe86350a66218b8ad6a8c4bdf2373
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2010-12-02 10:51:50 +0000

    Remove that old not submitted directory. No need to keep old stuff in our SVN working copies.

--- Polyhedron/demo/Polyhedron.old/Scene_tex_rendering.cpp
+++
@@ -1,8 +0,0 @@
-#include "Textured_polyhedron_type.h"
-#include <QtOpenGL/qgl.h>
-#include <CGAL/gl_render.h>
-
-void gl_render_tex_polyhedron_facets(Textured_polyhedron *p)
-{
-  p->gl_draw_textured_triangles(true,true,1.0);
-}
