2cf6ef07f15ce46f9eb3a1631b45f135a8e8a565
Sébastien Loriot <sloriot.ml@gmail.com>
2010-12-08 07:21:28 +0000

    avoid unused var warning

--- Alpha_shapes_3/test/Alpha_shapes_3/include/CGAL/_count_alpha.h
+++ Alpha_shapes_3/test/Alpha_shapes_3/include/CGAL/_count_alpha.h
@@ -193,13 +193,11 @@ count_faces(const AS &A, bool verbose)
 	   + count_interior_vertices == A.number_of_vertices());
   }
 
-  std::size_t ncc = A.number_of_solid_components();
-
   if (count_cells >= 1 && A.get_mode()== Alpha_shape_3::REGULARIZED) {
      //this relation might not be valid for any alpha_shape
      // if connected components are touching
      // through an edge or a vertex
-     assert(count_regular_facets == 2*count_regular_vertices - 4*ncc);
+     assert(count_regular_facets == 2*count_regular_vertices - 4*A.number_of_solid_components());
    }
 
 }
