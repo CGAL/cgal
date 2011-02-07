263ff08e7d32c739416c21a9472b0a4b27fa6f8f
Sébastien Loriot <sloriot.ml@gmail.com>
2011-02-07 15:43:37 +0000

    correct buggy comment

--- Arrangement_on_surface_2/include/CGAL/Arrangement_2/Arrangement_on_surface_2_impl.h
+++ Arrangement_on_surface_2/include/CGAL/Arrangement_2/Arrangement_on_surface_2_impl.h
@@ -3453,10 +3453,9 @@ _compare_vertices_xy_impl (const DVertex * v1, const DVertex * v2,
 //
 //Note that the lowest halfedge incident to the leftmost vertex (he_left_low)
 //is the lowest in the open loop. In the case one needs to have the lowest incident 
-//halfedge in the interior of the closed loop, the first curve in compare_y_at_x_right_2
-//he_left_low->curve() must be replaced by (he_left_low!=he_before?he_left_low->curve():cv).
-//Note that in both cases, the result of the (only) calling function _is_inside_new_face will be
-//the same (the turn is the same).
+//halfedge in the interior of the closed loop, further handling must be done in the
+//case he_before is he_left_low (cv cannot be used instead of he_left_low->curve()
+//in that case as cv may not be defined to the right of the left lowest point).
 template<class GeomTraits, class TopTraits>
 std::pair<const typename Arrangement_on_surface_2<GeomTraits,
                                                   TopTraits>::DVertex*,
