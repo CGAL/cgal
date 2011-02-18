4f92a1ae73ff8b107b28c526f77a80689ff5e1b2
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2011-02-18 16:59:07 +0000

    Untabify, to ease the reading.

--- Mesh_3/applications/mesher_tester_scripts/create_histogram.pl
+++ Mesh_3/applications/mesher_tester_scripts/create_histogram.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 
 BEGIN {
-	push(@INC,$ENV{'HOME'}."/local/perl"); #where the SVG.pm file lives
-	push(@INC,$ENV{'HOME'}."/local/perl/SVG"); # where the Utils.pm file lives
+  push(@INC,$ENV{'HOME'}."/local/perl"); #where the SVG.pm file lives
+  push(@INC,$ENV{'HOME'}."/local/perl/SVG"); # where the Utils.pm file lives
 }
 
 
@@ -17,50 +17,50 @@ my $svg=new SVG(width=>$width,height=>$height+30, style=>{'background'=>'lightgr
 
 my $file = @ARGV[0];
 if ( -f $file ) {
-	open(my $in, $file) || die("Impossible d'ouvrir $file: $!");
+  open(my $in, $file) || die("Impossible d'ouvrir $file: $!");
 
-	my @values;
-	my $max_value = 0;
-	while (<$in>) {
-		my $line = $_;
-		chomp($line);
-		if ( $line > $max_value ) {
-			$max_value = $line;
-		}
+  my @values;
+  my $max_value = 0;
+  while (<$in>) {
+    my $line = $_;
+    chomp($line);
+    if ( $line > $max_value ) {
+      $max_value = $line;
+    }
 
-		push(@values,$line);
-	}
-
-
-	# normalize values
-	my $min = shift(@values);
-	my $max = shift(@values);
-	my @norm = map( { $_/$max_value*$height } @values);
+    push(@values,$line);
+  }
 
 
-	# draw
-	my $i = 0;
-	my $color = "black";
-	foreach my $v (@norm) {
-		# use red for the isotrop tet
-		if ( $i == 71 ) {
-			$color = "red";
-		}
-		
-		my $tag = $svg->rectangle(x=>$i+$shift, y=>$height-$v+$padding_v,
-															width=>1, height=>$v,
-															rx=>0, ry=>0,
-															id=>"rect_$i",
-															style=>{'fill'=>$color});
-		$color = "black";
-		$i++;
-	}
-	
-	$svg->text(id=>'min', x=>5, y=>$height+20)->cdata($min);
-	$svg->text(id=>'max', x=>160, y=>$height+20)->cdata($max);
+  # normalize values
+  my $min = shift(@values);
+  my $max = shift(@values);
+  my @norm = map( { $_/$max_value*$height } @values);
+
+
+  # draw
+  my $i = 0;
+  my $color = "black";
+  foreach my $v (@norm) {
+    # use red for the isotrop tet
+    if ( $i == 71 ) {
+      $color = "red";
+    }
+    
+    my $tag = $svg->rectangle(x=>$i+$shift, y=>$height-$v+$padding_v,
+                              width=>1, height=>$v,
+                              rx=>0, ry=>0,
+                              id=>"rect_$i",
+                              style=>{'fill'=>$color});
+    $color = "black";
+    $i++;
+  }
+  
+  $svg->text(id=>'min', x=>5, y=>$height+20)->cdata($min);
+  $svg->text(id=>'max', x=>160, y=>$height+20)->cdata($max);
 }
 else {
-	$svg->text(id=>'error', x=>$width/2-50, y=>$height/2+20, style=>{'fill'=>'black'})->cdata("No data found...");
+  $svg->text(id=>'error', x=>$width/2-50, y=>$height/2+20, style=>{'fill'=>'black'})->cdata("No data found...");
 }
 
 # now render the SVG object, implicitly use svg namespace
