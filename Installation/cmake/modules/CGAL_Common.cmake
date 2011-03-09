684fd15bd3a391ed190c6e2ddac2ee965b26a3fb
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2011-03-09 13:33:08 +0000

    Add comments near the CMake policy we set.
    Set the new policy CMP0017 of CMake-2.8.4 to OLD.

--- Installation/cmake/modules/CGAL_Common.cmake
+++ Installation/cmake/modules/CGAL_Common.cmake
@@ -26,9 +26,27 @@ if( NOT CGAL_COMMON_FILE_INCLUDED )
   endif()
     
   if ( COMMAND cmake_policy )
+    # CMP0007 OLD means: list command ignores empty elements
     cmake_policy( SET CMP0007 OLD )  
     if ( CMAKE_2_6_3_OR_ABOVE )
+      # CMP0011 OLD means: Included scripts do NOT automatic cmake_policy
+      # PUSH and POP.
+      # We set that policy to OLD to avoid a warning. We probably could
+      # have set to the NEW policy.
       cmake_policy( SET CMP0011 OLD )  
+    endif()
+    if(POLICY CMP0017)
+      # New policy since CMake 2.8.4
+      cmake_policy( SET CMP0017 OLD )
+      # CMP0017 OLD means: we want to be able to override CMake modules
+      # that are shipped with CMake.
+      # For the moment, we override:
+      #   FindBLAS.cmake
+      #   FindBoost.cmake
+      #   FindLAPACK.cmake
+      #   FindOpenGL.cmake
+      #   FindPackageMessage.cmake
+      # Laurent Rineau, 2011/03/09
     endif()
   endif()
   
