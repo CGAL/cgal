0223ee8abb68235ed26993d911fe249443fbe6ef
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2011-03-16 13:38:37 +0000

    Can also bench comparisons

---
+++ Filtered_kernel/benchmark/Filtered_kernel/README.txt
@@ -0,0 +1,1 @@
+This is a test to check why VC++ is much slower than g++
--- Filtered_kernel/benchmark/Filtered_kernel/orientation_3.cpp
+++ Filtered_kernel/benchmark/Filtered_kernel/orientation_3.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Timer.h>
+#include <CGAL/Real_timer.h>
 #include <CGAL/algorithm.h>
 #include <CGAL/point_generators_3.h>
 
@@ -11,7 +11,7 @@
 
 typedef CGAL::Simple_cartesian<double> K;
 typedef K::Point_3 Point_3;
-#if 1
+#ifndef ONLY_TEST_COMPARISONS
 typedef CGAL::internal::Static_filters_predicates::Orientation_3_benchmark<Point_3> Predicate;
 #else
 
@@ -25,18 +25,30 @@ typedef CGAL::internal::Static_filters_predicates::Orientation_3_benchmark<Point
 
 int main()
 {
-  const int N = 100000000;
+  const int N = 10000000; // 10M
+  CGAL::Real_timer timer;
+  timer.start();
   std::vector<Point_3> points;
   points.reserve(N);
   CGAL::Random_points_in_sphere_3<Point_3> g( 100.0);
   CGAL::copy_n( g, N, std::back_inserter(points));
-  CGAL::Timer timer;
+  timer.stop();
+  std::cout << "Fill vector: " << timer.time() << " sec" << std::endl;
+  timer.reset();
   timer.start();
   int res=0;
   Predicate predicate;
-  for(int i = 0; i < N-4; i++){
-    res += predicate(points[i], points[i+1], points[i+2], points[i+3]);
-  }  
+  for(int j = 0; j < 10; ++j) {
+    for(int i = 0; i < N-4; i++){
+      res += predicate(points[i], points[i+1], points[i+2], points[i+3]);
+    }  
+  }
   timer.stop();
-  std::cout << res << std::endl << timer.time() << " sec" << std::endl;
+  std::cout << "result = " << res << std::endl;
+#ifndef ONLY_TEST_COMPARISONS
+  std::cout << "Orientation: ";
+#else
+  std::cout << "Comparisons: ";
+#endif
+  std::cout << timer.time() << " sec" << std::endl;
 }
