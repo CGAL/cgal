aa3cdf9afe5ef14985dde436900730760e9e7d28
Olivier Devillers <Olivier.Devillers@inria.fr>
2011-05-11 15:41:19 +0000

    one pass on user manual

--- Triangulation/examples/Triangulation/triangulation.cpp
+++ Triangulation/examples/Triangulation/triangulation.cpp
@@ -1,6 +1,6 @@
 #include <CGAL/point_generators_d.h>
 #include <CGAL/Filtered_kernel_d.h>
-#include <CGAL/Pure_complex.h>
+#include <CGAL/Triangulation.h>
 #include <CGAL/Cartesian_d.h>
 #include <CGAL/algorithm.h>
 #include <CGAL/Random.h>
@@ -13,49 +13,49 @@ int main()
     const int D = 5;   // we work in euclidean 5-space
     const int N = 100; // we will insert 100 points
 
-    // |Cartesian_d| is a model of the concept PureComplexTraits
+    // |Cartesian_d| is a model of the concept TriangulationTraits
     typedef CGAL::Cartesian_d<double> K;
 
     // |Filtered_kernel_d|  provides exact geometric predicates
     typedef CGAL::Filtered_kernel_d<K> FK;
 
-    // Here is our Pure_complex type:
-    typedef CGAL::Pure_complex<FK> PC;
+    // Here is our Triangulation type:
+    typedef CGAL::Triangulation<FK> T;
 
-    typedef PC::Point_d Point;
+    typedef T::Point_d Point;
 
     // - - - - - - - - - - - - - - - - - - - - - - - - STEP 1
 
     // Instanciate a random point generator
     CGAL::Random rng;
-    typedef CGAL::Random_points_in_iso_box_d<Point> Random_points_iterator;
+    typedef CGAL::Random_points_in_cube_d<Point> Random_points_iterator;
     Random_points_iterator rand_it(D, 1.0, rng);
 
     // Generate N random points
     std::vector<Point> points;
     CGAL::copy_n(rand_it, N, std::back_inserter(points));
 
-    PC pc(D);
-    assert(pc.empty());
+    T t(D);
+    assert(t.empty());
 
     // insert the points in the pure complex
-    pc.insert(points.begin(), points.end());
-    assert( pc.is_valid() );
+    t.insert(points.begin(), points.end());
+    assert( t.is_valid() );
 
     // - - - - - - - - - - - - - - - - - - - - - - - - STEP 2
 
-    typedef PC::Face Face;
+    typedef T::Face Face;
     typedef std::vector<Face> Faces;
     Faces edges;
     std::back_insert_iterator<Faces> out(edges);
-    pc.gather_incident_upper_faces(pc.infinite_vertex(), 1, out);
+    t.incident_upper_faces(t.infinite_vertex(), 1, out);
     // Count the number of points on the convex hull
-    std::cout << "\nThere are " << edges.size() << " vertices on the convex hull.";
+    std::cout << "There are " << edges.size() << " vertices on the convex hull.";
     edges.clear();
 
     // cleanup
-    pc.clear();
-    assert(pc.empty());
+    t.clear();
+    assert(t.empty());
 
     std::cout << std::endl;
     return 0;
--- Triangulation/examples/Triangulation/triangulation_data_structure.cpp
+++ Triangulation/examples/Triangulation/triangulation_data_structure.cpp
@@ -1,74 +1,74 @@
-#include <CGAL/Pure_complex_data_structure.h>
+#include <CGAL/Triangulation_data_structure.h>
 #include <iostream>
 #include <vector>
 
 int main()
 {
-    const int sdim = 27; // the dimension of the PCDS with compile-time dimension
-    const int ddim = 15; // the dimension of the PCDS with dynamic dimension
+  const int sdim = 27; // dimension of TDS with compile-time dimension
+  const int ddim = 15; // dimension of TDS with dynamic dimension
 
-    typedef CGAL::Pure_complex_data_structure<CGAL::Dynamic_dimension_tag>
-            Dynamic_pcds;
-    typedef CGAL::Pure_complex_data_structure<CGAL::Dimension_tag<sdim> >
-            Static_pcds;
-    typedef Static_pcds::Face           Face;
-    typedef Static_pcds::Facet          Facet;
-    typedef Static_pcds::Vertex_handle  Vertex_handle;
-    typedef Static_pcds::Simplex_handle Simplex_handle;
+  typedef CGAL::Triangulation_data_structure<CGAL::Dynamic_dimension_tag>
+          Dynamic_tds;
+  typedef CGAL::Triangulation_data_structure<CGAL::Dimension_tag<sdim> >
+          Static_tds;
+  typedef Static_tds::Face             Face;
+  typedef Static_tds::Facet            Facet;
+  typedef Static_tds::Vertex_handle    Vertex_handle;
+  typedef Static_tds::Full_cell_handle Full_cell_handle;
 
-    Static_pcds  S(158);  // the argument is not taken into account.
-    Dynamic_pcds D(ddim); // the argument is taken into account.
+  Static_tds  S(158);  // the argument is not taken into account.
+  Dynamic_tds D(ddim); // the argument is taken into account.
 
-    assert( sdim == S.ambient_dimension() );
-    assert( ddim == D.ambient_dimension() );
+  assert( sdim == S.ambient_dimension() );
+  assert( ddim == D.ambient_dimension() );
 
-    assert( -2 == S.current_dimension() );
-    assert( S.is_valid() );
+  assert( -2 == S.current_dimension() );
+  assert( S.is_valid() );
 
-    std::vector<Vertex_handle> V(10);
-    V[0] = S.insert_increase_dimension();
-    assert( -1 == S.current_dimension() );
+  std::vector<Vertex_handle> V(10);
+  V[0] = S.insert_increase_dimension();
+  assert( -1 == S.current_dimension() );
 
-    for( int i = 1; i <= 5; ++i )
-        V[i] = S.insert_increase_dimension(V[rand() % i]);
+  for( int i = 1; i <= 5; ++i )
+      V[i] = S.insert_increase_dimension(V[rand() % i]);
 
-    assert( 4 == S.current_dimension() );
-    assert( 6 == S.number_of_vertices() );
-    assert( 6 == S.number_of_simplices() );
+  assert( 4 == S.current_dimension() );
+  assert( 6 == S.number_of_vertices() );
+  assert( 6 == S.number_of_full_cells() );
 
-    Simplex_handle s = V[5]->simplex();
+  Full_cell_handle c = V[5]->full_cell();
 
-    V[6] = S.insert_in_simplex(s);
-    
-    assert( 7 == S.number_of_vertices() );
-    assert( 10 == S.number_of_simplices() );
+  V[6] = S.insert_in_full_cell(c);
+  
+  assert( 7 == S.number_of_vertices() );
+  assert( 10 == S.number_of_full_cells() );
 
-    s = V[3]->simplex();
-    // ft will designate the Facet opposite to vertex 2 in s:
-    Facet ft(s, 2);
+  c = V[3]->full_cell();
+  // ft will designate the Facet opposite to vertex 2 in c:
+  Facet ft(c, 2);
 
-    V[7] = S.insert_in_facet(ft);
-    assert( 8 == S.number_of_vertices() );
-    assert( 16 == S.number_of_simplices() );
+  V[7] = S.insert_in_facet(ft);
+  assert( 8 == S.number_of_vertices() );
+  assert( 16 == S.number_of_full_cells() );
 
-    s = V[3]->simplex();
-    // face will designate the edge joining vertices 2 and 4 in simplex s:
-    Face face(s);
-    face.set_index(0, 2);
-    face.set_index(1, 4);
+  c = V[3]->full_cell();
+  // face will be the edge joining vertices 2 and 4 in full_cell c:
+  Face face(c);
+  face.set_index(0, 2);
+  face.set_index(1, 4);
 
-    V[8] = S.insert_in_face(face);
-    assert( S.is_valid() );
+  V[8] = S.insert_in_face(face);
+  assert( S.is_valid() );
 
-    Simplex_handle hole[2];
-    hole[0] = V[8]->simplex();
-    hole[1] = hole[0]->neighbor(0);
-    // ft will be a face on the boundary of the union of hole[0] and hole[1]:
-    ft = Facet(hole[0], 1);
+  Full_cell_handle hole[2];
+  hole[0] = V[8]->full_cell();
+  hole[1] = hole[0]->neighbor(0);
+  // ft will be a face on the boundary of hole[0] union hole[1]:
+  ft = Facet(hole[0], 1);
 
-    V[9] = S.insert_in_hole(hole, hole+2, ft);
-    assert( S.is_valid() );
+  V[9] = S.insert_in_hole(hole, hole+2, ft);
+  assert( S.is_valid() );
 
-    std::cout << "Ok.\n";
-    return 0;
+  std::cout << "Ok."<<std::endl;
+  return 0;
 }
