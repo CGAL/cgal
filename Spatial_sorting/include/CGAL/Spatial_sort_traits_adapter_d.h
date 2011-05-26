b35ab8afb65379a2a901b747359e43f9b682d084
Sébastien Loriot <sloriot.ml@gmail.com>
2011-05-26 06:41:06 +0000

    conditionnal include path for property_map.hpp

--- Spatial_sorting/include/CGAL/Spatial_sort_traits_adapter_d.h
+++ Spatial_sorting/include/CGAL/Spatial_sort_traits_adapter_d.h
@@ -22,7 +22,13 @@
 #define CGAL_SPATIAL_SORT_TRAITS_ADAPTER_D_H
 
 #include <boost/call_traits.hpp>
-#include <boost/property_map/property_map.hpp>
+#include <boost/version.hpp>
+#if BOOST_VERSION >= 104000
+  #include <boost/property_map/property_map.hpp>
+#else
+  #include <boost/property_map.hpp>
+#endif
+
 
 namespace CGAL{
 
