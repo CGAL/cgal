8bf2ebe1b0721a021eed229d503d67001803b2ca
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2011-05-27 12:26:34 +0000

    remove copyright from examples

--- Combinatorial_map/examples/Combinatorial_map/map_3_marks.cpp
+++ Combinatorial_map/examples/Combinatorial_map/map_3_marks.cpp
@@ -1,21 +1,3 @@
-// Copyright (c) 2010 CNRS, LIRIS, http://liris.cnrs.fr/, All rights reserved.
-//
-// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; version 2.1 of the License.
-// See the file LICENSE.LGPL distributed with CGAL.
-//
-// Licensees holding a valid commercial license may use this file in
-// accordance with the commercial license agreement provided with the software.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-//
-// $URL$
-// $Id$
-//
-// Author(s)     : Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
-//
 #include <CGAL/Combinatorial_map.h>
 #include <CGAL/Combinatorial_map_constructors.h>
 #include <CGAL/Combinatorial_map_operations.h>
