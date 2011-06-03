9670c424030f9e0a617042a983cc7b3364848c15
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2011-06-03 14:40:06 +0000

    Begin to update Polyhedron demo from work done in the Mesh_3 dev'branch
    Step 3:
    
      - Update the API of the Scene and Viewer class (some methods are not yet used).
    
      - Update the CMakeLists.txt, to compile all major classes of the
        framework (Scene, Viewer, Scene_item) into a demo_framework library.

--- Polyhedron/demo/Polyhedron/Viewer.h
+++ Polyhedron/demo/Polyhedron/Viewer.h
@@ -1,18 +1,26 @@
 #ifndef VIEWER_H
 #define VIEWER_H
 
+#include "Viewer_config.h"
+
 #include <QGLViewer/qglviewer.h>
+#include <QPoint>
 
 // forward declarations
 class QWidget;
 class Scene_draw_interface;
+class QMouseEvent;
+class QKeyEvent;
 
-class Viewer : public QGLViewer {
+class Viewer_impl;
+
+class VIEWER_EXPORT Viewer : public QGLViewer {
 
   Q_OBJECT
 
 public:
   Viewer(QWidget * parent, bool antialiasing = false);
+  ~Viewer();
 
   // overload several QGLViewer virtual functions
   void draw();
@@ -21,21 +29,33 @@ public:
   void postSelection(const QPoint&);
 
   void setScene(Scene_draw_interface* scene);
-  bool antiAliasing() const { return antialiasing; }
+  bool antiAliasing() const;
+
+  static bool readFrame(QString, qglviewer::Frame&);
+  static QString dumpFrame(const qglviewer::Frame&);
 
 signals:
   void selected(int);
+  void requestContextMenu(QPoint global_pos);
+  void selectedPoint(double, double, double);
+  void selectionRay(double, double, double, double, double, double);
 
 public slots:
   void setAntiAliasing(bool b);
   void setTwoSides(bool b);
 
-private:
-  void draw_aux(bool with_names);
+  void turnCameraBy180Degres();
 
-  Scene_draw_interface* scene;
-  bool antialiasing;
-  bool twosides;
+  QString dumpCameraCoordinates();
+  bool moveCameraToCoordinates(QString, 
+                               float animation_duration = 0.5f);
+
+protected:
+  void mousePressEvent(QMouseEvent*);
+  void keyPressEvent(QKeyEvent*);
+
+protected:
+  Viewer_impl* d;
 }; // end class Viewer
 
 #endif // VIEWER_H
