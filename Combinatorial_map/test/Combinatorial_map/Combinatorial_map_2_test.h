7edb0c49d81209271b909862bd3af2f5c8b3b21a
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2011-06-09 13:25:19 +0000

    copyright 2010-2011

--- Combinatorial_map/test/Combinatorial_map/Combinatorial_map_2_test.h
+++ Combinatorial_map/test/Combinatorial_map/Combinatorial_map_2_test.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 CNRS (France), All rights reserved.
+// Copyright (c) 2010-2011 CNRS (France), All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org); you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License as
