4cb71dd2be4335220c357b514cac758c4b0f5237
Philipp Möller <Philipp.Moeller@geometryfactory.com>
2011-06-14 14:55:11 +0000

    Missing includes and switched to static assertions

--- Number_types/test/Number_types/known_bit_size_integers.cpp
+++ Number_types/test/Number_types/known_bit_size_integers.cpp
@@ -1,23 +1,24 @@
-#include <cassert>
 #include <iostream>
+#include <boost/cstdint.hpp>
+#include <boost/static_assert.hpp>
 
 int main()
 {
   std::cout << "Verifying the sizes of boost::[u]int{8,16,32,64}_t"
             << std::endl;
-
-  assert(sizeof(boost::int8_t)  == 1);
-  assert(sizeof(boost::int16_t) == 2);
-  assert(sizeof(boost::int32_t) == 4);
+  
+  BOOST_STATIC_ASSERT(sizeof(boost::int8_t)  == 1);
+  BOOST_STATIC_ASSERT(sizeof(boost::int16_t) == 2);
+  BOOST_STATIC_ASSERT(sizeof(boost::int32_t) == 4);
 #ifndef BOOST_NO_INT64_T
-  assert(sizeof(boost::int64_t) == 8);
+  BOOST_STATIC_ASSERT(sizeof(boost::int64_t) == 8);
 #endif
 
-  assert(sizeof(boost::uint8_t)  == 1);
-  assert(sizeof(boost::uint16_t) == 2);
-  assert(sizeof(boost::uint32_t) == 4);
+  BOOST_STATIC_ASSERT(sizeof(boost::uint8_t)  == 1);
+  BOOST_STATIC_ASSERT(sizeof(boost::uint16_t) == 2);
+  BOOST_STATIC_ASSERT(sizeof(boost::uint32_t) == 4);
 #ifndef BOOST_NO_INT64_T
-  assert(sizeof(boost::uint64_t) == 8);
+  BOOST_STATIC_ASSERT(sizeof(boost::uint64_t) == 8);
 #endif
 
   return 0;
