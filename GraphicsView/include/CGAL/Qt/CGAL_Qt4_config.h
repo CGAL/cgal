acc2ae58a6155334fc11130ad2aa38761cb1d0b1
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2011-06-14 10:13:51 +0000

    If CGAL_Qt4 is not a DLL, declspec must not be used.

--- GraphicsView/include/CGAL/Qt/CGAL_Qt4_config.h
+++ GraphicsView/include/CGAL/Qt/CGAL_Qt4_config.h
@@ -3,10 +3,15 @@
 
 #include <QtCore/qglobal.h>
 
-#ifdef CGAL_Qt4_EXPORTS
-#  define CGAL_QT4_EXPORT Q_DECL_EXPORT
+#ifdef CGAL_Qt4_DLL
+#  ifdef CGAL_Qt4_EXPORTS
+#    define CGAL_QT4_EXPORT Q_DECL_EXPORT
+#  else
+#    define CGAL_QT4_EXPORT Q_DECL_IMPORT
+#  endif
 #else
-#  define CGAL_QT4_EXPORT Q_DECL_IMPORT
+// empty definition
+#  define CGAL_QT4_EXPORT
 #endif
 
 #endif // CGAL_QT4_CONFIG_H
