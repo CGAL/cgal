a67a03d29705404ab9276edbabdf075f807f414a
Philipp Möller <Philipp.Moeller@geometryfactory.com>
2011-06-23 14:02:53 +0000

    Fixed naming and include guard

--- STL_Extension/include/CGAL/Overload.h
+++ STL_Extension/include/CGAL/Overload.h
@@ -1,5 +1,5 @@
-#ifndef _COMPOSITE_VISITOR_H_
-#define _COMPOSITE_VISITOR_H_
+#ifndef CGAL_OVERLOAD_H
+#define CGAL_OVERLOAD_H
 
 #include <CGAL/compiler_config.h>
 
@@ -10,17 +10,17 @@
 namespace CGAL {
 
 template<typename T, std::size_t i, class U >
-struct overload_helper : public overload_helper<T, i - 1, U> {
+struct Overload_helper : public Overload_helper<T, i - 1, U> {
   typedef typename std::tuple_element<i - 1, T>::type inner_type;
  
   typename inner_type::result_type operator()(typename inner_type::argument_type x) {
     return std::get<i - 1>(static_cast<U*>(this)->t)(x); }
  
-  using overload_helper<T, i - 1, U>::operator();
+  using Overload_helper<T, i - 1, U>::operator();
 };
  
 template<typename T, class U>
-struct overload_helper<T, 1, U> {
+struct Overload_helper<T, 1, U> {
   typedef typename std::tuple_element<0 ,T>::type inner_type;
  
   typename inner_type::result_type operator()(typename inner_type::argument_type x) {
@@ -28,21 +28,21 @@ struct overload_helper<T, 1, U> {
 };
  
 template<typename T>
-struct overload : public overload_helper<T, std::tuple_size<T>::value, overload<T> >
+struct Overload : public Overload_helper<T, std::tuple_size<T>::value, Overload<T> >
 {
   // 
   typedef typename std::tuple_element<0, T>::type::result_type result_type;
 
   T t;
-  overload(T&& t) : t(t) {}
-  overload(const T& t) : t(t) {}
+  Overload(T&& t) : t(t) {}
+  Overload(const T& t) : t(t) {}
 };
 
 template<typename T>
-overload<T> make_overload(T&& t) { return overload<T>(t); }
+Overload<T> make_overload(T&& t) { return Overload<T>(t); }
 
 } // namespace CGAL
 #endif // !defined(CGAL_CFG_NO_CPP0X_VARIADIC_TEMPLATES) && !defined(CGAL_CFG_NO_CPP0X_TUPLE)
 
-#endif /* _COMPOSITE_VISITOR_H_ */
+#endif /* CGAL_OVERLOAD_H */
 
