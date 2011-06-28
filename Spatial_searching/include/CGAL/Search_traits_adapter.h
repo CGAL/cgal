76fb93e7e88fa44952025f6ef43a870b83adbe3f
Sébastien Loriot <sloriot.ml@gmail.com>
2011-06-28 13:19:40 +0000

    update copyright date

--- Spatial_searching/include/CGAL/Search_traits_adapter.h
+++ Spatial_searching/include/CGAL/Search_traits_adapter.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 GeometryFactory (France).
+// Copyright (c) 2011 GeometryFactory (France).
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org); you may redistribute it under
