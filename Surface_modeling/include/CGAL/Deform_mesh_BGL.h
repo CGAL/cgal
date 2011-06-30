ab0a5350463dae83619aef95722d6e92a5988dab
Yin Xu <yinxu.zju@gmail.com>
2011-06-30 14:43:10 +0000

    first full version of arap modeling

--- Surface_modeling/include/CGAL/Deform_mesh_BGL.h
+++ Surface_modeling/include/CGAL/Deform_mesh_BGL.h
@@ -4,13 +4,12 @@
 
 #include <CGAL/trace.h>
 #include <CGAL/Timer.h>
-#include <CGAL/Cartesian.h>
-#include <CGAL/Polyhedron_3.h>
 #include <CGAL/IO/Polyhedron_iostream.h>
 #include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
 #include <CGAL/boost/graph/properties_Polyhedron_3.h>
 #include <CGAL/boost/graph/halfedge_graph_traits_Polyhedron_3.h>
-#include <CGAL/Taucs_solver_traits.h>
+#include <svd.h>
+
 
 
 #include <iostream>
@@ -29,9 +28,10 @@ class Deform_mesh_BGL
 // Public types
 public:
 
-  // Geometric types
-  typedef CGAL::Cartesian<double>::Point_3                                              Point;
-  typedef CGAL::Cartesian<double>::Vector_3                                             Vector;
+  // Geometric types              
+  typedef typename Polyhedron::Traits         Kernel;
+  typedef typename Kernel::Vector_3           Vector;
+  typedef typename Kernel::Point_3            Point;
 
   // Repeat Polyhedron types
   typedef typename boost::graph_traits<Polyhedron>::vertex_descriptor		vertex_descriptor;
@@ -39,41 +39,43 @@ public:
   typedef typename boost::graph_traits<Polyhedron>::edge_descriptor		  edge_descriptor;
   typedef typename boost::graph_traits<Polyhedron>::edge_iterator		    edge_iterator;
   typedef typename boost::graph_traits<Polyhedron>::in_edge_iterator		in_edge_iterator;
-  typedef typename boost::graph_traits<Polyhedron>::out_edge_iterator		out_edge_iterator;
 
 
   // Data members.
 public:
 
-  Polyhedron polyhedron;                // target mesh
-  std::map<vertex_descriptor, vertex_descriptor> s2t;          // access from source mesh to target mesh
+  Polyhedron* polyhedron;                     // target mesh, must be modified
   std::vector<vertex_descriptor> roi;
-  std::vector<vertex_descriptor> hdl;           // user specified handles, storing the target positions
+  std::vector<vertex_descriptor> hdl;         // user specified handles, storing the target positions
 
-  SparseLinearAlgebraTraits_d m_solver;
+  int iterations;                                     // number of iterations
+  std::map<vertex_descriptor, std::vector<double> > rot_mtr;               // rotations matrices of all vertices
+  std::map<edge_descriptor, double> edge_weight;           // weight of edges
+  SparseLinearAlgebraTraits_d m_solver;               // linear sparse solver
+  std::map<vertex_descriptor, Point> solution;        // storing vertex solution during iterations
 
 
   // Public methods
 public:
 
-  // The constructor gets the Polyhedron that we will model
-  Deform_mesh_BGL(Polyhedron &P)
+  // The constructor gets the polyhedron that we will model
+  Deform_mesh_BGL(Polyhedron* P)
     :polyhedron(P)
   {
-    int index = 0;
-    vertex_iterator vb_s, ve_s, vb_t, ve_t;
-    // boost::tie assigns the first and second element of the std::pair
-    // returned by boost::vertices to the variables vb and ve
-    boost::tie(vb_s,ve_s) = boost::vertices(P);
-    for(boost::tie(vb_t,ve_t) = boost::vertices(polyhedron); vb_t != ve_t; ++vb_t ){
-      vertex_descriptor  vd_t = *vb_t;
-      vd_t->id() = index;
-      vertex_descriptor  vd_s = *vb_s;
-      vd_s->id() = index;
-      s2t[vd_s] = vd_t;
-      vb_s++;
-      index++;
+    polyhedron = P;
+
+    vertex_iterator vb, ve;
+    for(boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
+    {
+      rot_mtr[*vb].resize(6);
     }
+
+    vertex_iterator vb, ve;
+    for(boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
+    {
+      solution[*vb] = (*vb)->point();
+    }
+
 
   }
 
@@ -82,15 +84,15 @@ public:
   {
   }
 
-  // The region of interest and the handles are a set of vertices on target mesh, iterators come from source mesh
+  // The region of interest and the handles are a set of vertices on target mesh
   void region_of_interest(vertex_iterator begin, vertex_iterator end, size_t k)
   {
     roi.clear();
     for (vertex_iterator vit = begin; vit != end; vit ++)
     {
-      roi.push_back(s2t[*vit]);
+      roi.push_back(*vit);
     }
-    roi.push_back(s2t[*end]);
+    roi.push_back(*end);
   
     int idx_lv = 0;    // pointing the neighboring vertices on current level
     int idx_lv_end;
@@ -101,10 +103,10 @@ public:
       for ( ;idx_lv < idx_lv_end; idx_lv++ )
       {
         vertex_descriptor vd = roi[idx_lv];
-        out_edge_iterator e, e_end;
-        for (boost::tie(e,e_end) = boost::out_edges(vd, polyhedron); e != e_end; e++)
+        in_edge_iterator e, e_end;
+        for (boost::tie(e,e_end) = boost::in_edges(vd, *polyhedron); e != e_end; e++)
         {
-          vertex_descriptor vt = boost::target(*e, polyhedron);
+          vertex_descriptor vt = boost::source(*e, *polyhedron);
           std::vector<vertex_descriptor> ::iterator result = find(roi.begin(), roi.end(), vt);
           if (result == roi.end())
           {
@@ -118,14 +120,35 @@ public:
   }
 
 
+  void roi_clear()
+  {
+    roi.clear();
+  }
+
+  void roi_push(vertex_descriptor vd)   
+  {
+    roi.push_back(vd);
+  }
+
+
   void handles(vertex_iterator begin, vertex_iterator end)
   {
     hdl.clear();
     for (vertex_iterator vit = begin; vit != end; vit ++)
     {
-      hdl.push_back(s2t[*vit]);
+      hdl.push_back(*vit);
     }
-    hdl.push_back(s2t[*end]);
+    hdl.push_back(*end);
+  }
+
+  void handle_clear()
+  {
+    hdl.clear();
+  }
+
+  void handle_push(vertex_descriptor vd)  
+  {
+    hdl.push_back(vd);
   }
 
 
@@ -140,13 +163,13 @@ public:
     Timer task_timer; task_timer.start();
 
     // get #variables
-    unsigned int nb_variables = boost::num_vertices(polyhedron);
+    unsigned int nb_variables = boost::num_vertices(*polyhedron);
 
     CGAL_TRACE_STREAM << "  Creates matrix...\n";
     // Assemble linear system A*X=B
     typename SparseLinearAlgebraTraits_d::Matrix A(nb_variables); // matrix is symmetric definite positive
-    typename SparseLinearAlgebraTraits_d::Vector X(nb_variables), B(nb_variables);
 
+    compute_edge_weight();
     assemble_laplacian(A, "cot");
 
     CGAL_TRACE_STREAM << "  Creates matrix: done (" << task_timer.time() << " s)\n";
@@ -163,90 +186,121 @@ public:
 
   }
 
-
+  // pre-compute cotangent weight of edges
+  void compute_edge_weight()
+  {
+    edge_iterator eb, ee;
+    for(boost::tie(eb,ee) = boost::edges(*polyhedron); eb != ee; ++eb )
+    {
+      edge_weight[*eb] = cot_value(*eb) / 2.0;
+    }
+  }
 
 	// Assemble Laplacian matrix A of linear system A*X=B 
   ///
   /// @commentheading Template parameters:
-  /// @param SparseLinearAlgebraTraits_d Symmetric definite positive sparse linear solver.
+  /// @param SparseLinearAlgebraTraits_d definite positive sparse linear solver.
 	void assemble_laplacian(typename SparseLinearAlgebraTraits_d::Matrix& A, std::string type)
 	{
+    std::map<vertex_descriptor, int> idx;         // index of vertices
     vertex_iterator vb, ve;
-		for(boost::tie(vb,ve) = boost::vertices(polyhedron); vb != ve; ++vb )
+    int index = 0;
+    for(boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
+    {
+      idx[*vb] = index;
+      index++;
+    }
+
+    // build trivial diagonal matrix
+    for (int i = 0; i < boost::num_vertices(*polyhedron); i++)
+    {
+      A.set_coef(i, i, 1.0, true);
+    }
+
+    // determine vertices inside roi while outside handles
+    // the rows of these vertices are non-trivial
+    std::map<vertex_descriptor, int> non_trivial;
+    for(boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
+    {
+      non_trivial[*vb] = 0;
+    }
+
+    for (int i = 0; i < roi.size(); i++)
+    {
+      non_trivial[roi[i]] = 1;
+    }
+    for (int i = 0; i < hdl.size(); i++)
+    {
+      non_trivial[hdl[i]] = 0;
+    }
+
+    /// assign cotangent Laplacian to non-trivial vertices
+		for(boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
 		{
 			vertex_descriptor vi = *vb;
-      double diagonal = 0;
-			int idx_i = vi->id();
-      out_edge_iterator e, e_end;
-      for (boost::tie(e,e_end) = boost::out_edges(vi, polyhedron); e != e_end; e++)
+      if (non_trivial[vi])
       {
-        vertex_descriptor vj = boost::target(*e, polyhedron);
-        //std::cout << vj->id();
-        double wij = 1;
-        if (type == "cot")   // cotangent Laplacian weights
+        double diagonal = 0;
+        int idx_i = idx[vi];
+        in_edge_iterator e, e_end;
+        for (boost::tie(e,e_end) = boost::in_edges(vi, *polyhedron); e != e_end; e++)
         {
-          wij = cot_value(*e);
+          vertex_descriptor vj = boost::source(*e, *polyhedron);
+          double wij = 1;
+          if (type == "cot")   // cotangent Laplacian weights
+          {
+            wij = edge_weight[*e];
+          }
+          int idx_j = idx[vj];
+          A.set_coef(idx_i, idx_j, -wij, true);	// off-diagonal coefficient
+          diagonal += wij;
         }
-        int idx_j = vj->id();
-        A.set_coef(idx_i, idx_j, -wij, true);	// off-diagonal coefficient
-        diagonal += wij;
-      }
-			// diagonal coefficient
-			A.set_coef(idx_i, idx_i, diagonal, true);
+        // diagonal coefficient
+        A.set_coef(idx_i, idx_i, diagonal);
+      } 
 		}
 
-		// handle constraints
-		for (int i = 0; i < hdl.size(); i++)
-		{
-			int idx_i = hdl[i]->id();
-			A.set_coef(idx_i, idx_i, 1.0);
-		}
 	}
 
   
   // Returns the cotanget value of specified edge_descriptor
   double cot_value(edge_descriptor e)
   {
-     vertex_descriptor v0 = boost::source(e, polyhedron);
-     vertex_descriptor v1 = boost::target(e, polyhedron);
+     vertex_descriptor v0 = boost::target(e, *polyhedron);
+     vertex_descriptor v1 = boost::source(e, *polyhedron);
      // Only one triangle for border edges
-     if (boost::get(CGAL::edge_is_border, polyhedron, e)||boost::get(CGAL::edge_is_border, polyhedron, CGAL::opposite_edge(e, polyhedron)))
+     if (boost::get(CGAL::edge_is_border, *polyhedron, e)||boost::get(CGAL::edge_is_border, *polyhedron, CGAL::opposite_edge(e, *polyhedron)))
      {
        
-       edge_descriptor e_cw = CGAL::next_edge_cw(e, polyhedron);
-       vertex_descriptor v2 = boost::target(e_cw, polyhedron);
-       if (boost::get(CGAL::edge_is_border, polyhedron, e_cw) || boost::get(CGAL::edge_is_border, polyhedron, CGAL::opposite_edge(e_cw, polyhedron)) )
+       edge_descriptor e_cw = CGAL::next_edge_cw(e, *polyhedron);
+       vertex_descriptor v2 = boost::source(e_cw, *polyhedron);
+       if (boost::get(CGAL::edge_is_border, *polyhedron, e_cw) || boost::get(CGAL::edge_is_border, *polyhedron, CGAL::opposite_edge(e_cw, *polyhedron)) )
        {
-          edge_descriptor e_ccw = CGAL::next_edge_ccw(e, polyhedron);
-          v2 = boost::target(e_ccw, polyhedron);
+          edge_descriptor e_ccw = CGAL::next_edge_ccw(e, *polyhedron);
+          v2 = boost::source(e_ccw, *polyhedron);
        }
       
-       //std::cout << v0->id() << " " << v1->id() << " " << v2->id();
-       return cot_value(v0, v2, v1);
+       return ( cot_value(v0, v2, v1) );
      }
      else
      {
-        edge_descriptor e_cw = CGAL::next_edge_cw(e, polyhedron);
-        vertex_descriptor v2 = boost::target(e_cw, polyhedron);
-        edge_descriptor e_ccw = CGAL::next_edge_ccw(e, polyhedron);
-        vertex_descriptor v3 = boost::target(e_ccw, polyhedron);
+        edge_descriptor e_cw = CGAL::next_edge_cw(e, *polyhedron);
+        vertex_descriptor v2 = boost::source(e_cw, *polyhedron);
+        edge_descriptor e_ccw = CGAL::next_edge_ccw(e, *polyhedron);
+        vertex_descriptor v3 = boost::source(e_ccw, *polyhedron);
 
-        //std::cout << v0->id() << " " << v1->id() << " " << v2->id() << " " << v3->id();
-
-        return ( cot_value(v0, v2, v1)/2.0 + cot_value(v0, v3, v1)/2.0 );
+        return ( cot_value(v0, v2, v1) + cot_value(v0, v3, v1) );
      }
   }
 
   // Returns the cotanget value of angle v0_v1_v2
   double cot_value(vertex_descriptor v0, vertex_descriptor v1, vertex_descriptor v2)
   {
-    Point p0 = v0->point();
-    Point p1 = v1->point();
-    Point p2 = v2->point();
+    
     Vector vec0 = v1->point() - v2->point();
     Vector vec1 = v2->point() - v0->point();
     Vector vec2 = v0->point() - v1->point();
-    double e0 = std::sqrt(vec0.squared_length());
+    double e0 = std::sqrt(vec0.squared_length()); 
     double e1 = std::sqrt(vec1.squared_length());
     double e2 = std::sqrt(vec2.squared_length());
     double cos_angle = (e0*e0+e2*e2-e1*e1)/2.0/e0/e2;
@@ -255,15 +309,226 @@ public:
     return (cos_angle/sin_angle);
 
   }
+
+
+  // Set the number of iterations made in operator()
+  void set_iterations(unsigned int ite)
+  {
+    iterations = ite;
+  }
   
 	// The operator will be called in a real time loop from the GUI.
-	void operator()(vertex_iterator vit, Vector v)
+  // assign translation vector to handles
+	void operator()(Vector translation)
 	{
-    Point p = s2t[*vit]->point();
-		p = p-v;
-    s2t[*vit]->point() = p;
-    std::cout << hdl[0]->point();
+    for (int idx = 0; idx < hdl.size(); idx ++)
+    {
+      vertex_descriptor vd = hdl[idx];
+      solution[vd] = vd->point() + translation;
+    }
 	}
+
+  // Local step of iterations, computing optimal rotation matrices
+  void optimal_rotations()
+  {
+    // malloc memory for svd decomposition
+    float** u = new (float*)[4];    // covariance matrix
+    for (int i = 0; i < 4; i++)
+    {
+      u[i] = new float[4];
+    }
+    float** v = new (float*)[4];
+    for (int i = 0; i < 4; i++)
+    {
+      v[i] = new float[4];
+    }
+    float* w = new float[4];
+
+    vertex_iterator vb, ve;
+    int idx = 0;
+    for ( boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
+    {
+      // only accumulate roi vertices
+      std::vector<vertex_descriptor> ::iterator result = find(roi.begin(), roi.end(), *vb);
+      if ( result == roi.end() )
+      {
+        rot_mtr[*vb][0] = 1; rot_mtr[*vb][1] = 0; rot_mtr[*vb][2] = 0;
+        rot_mtr[*vb][3] = 0; rot_mtr[*vb][4] = 1; rot_mtr[*vb][5] = 0;
+        rot_mtr[*vb][6] = 0; rot_mtr[*vb][7] = 0; rot_mtr[*vb][8] = 1;
+      }
+      else
+      {
+        // compute covariance matrix
+        for (int i = 0; i < 4; i++)
+        {
+          w[i] = 0;
+          for (int j = 0; j < 4; j++)
+          {
+            u[i][j] = 0;
+            v[i][j] = 0;
+          }
+        }
+        vertex_descriptor vi = *vb;
+        in_edge_iterator e, e_end;
+        for (boost::tie(e,e_end) = boost::in_edges(vi, *polyhedron); e != e_end; e++)
+        {
+          vertex_descriptor vj = boost::source(*e, *polyhedron);
+          Vector pij = vi->point() - vj->point();
+          Vector qij = solution[vi] - solution[vj];
+          double wij = edge_weight[*e];
+          for (int i = 0; i < 3; i++)
+          {
+            for (int j = 0; j < 3; j++)
+            {
+              u[i+1][j+1] += wij*pij[i]*qij[j]; 
+            }
+          }
+        }
+
+        // svd decomposition
+        svdcmp(u, 3, 3, w, v);
+
+        // extract rotation matrix
+        for (int i = 0; i < 6; i++)
+        {
+          rot_mtr[*vb][i] = 0;
+        }
+        for (int i = 0; i < 3; i++)      // row index
+        {
+          for (int j = 0; j < 3; j++)   // column index
+          {
+            for (int k = 0; k < 3; k++)
+            {
+              rot_mtr[*vb][i*3+j] += v[i+1][k+1] * u[j+1][k+1];
+            }  
+          }
+        }
+
+      }
+    }
+  
+    for (int i = 0; i < 4; i++)
+    {
+      delete [] u[i];
+      delete [] v[i];
+    }
+    delete [] u;
+    delete [] v;
+    delete [] w;
+
+  }
+
+  // Global step of iterations, updating solution
+  void update_solution()
+  {
+    unsigned int nb_variables = boost::num_vertices(*polyhedron);
+    typename SparseLinearAlgebraTraits_d::Vector X(nb_variables), Bx(nb_variables);
+    typename SparseLinearAlgebraTraits_d::Vector Y(nb_variables), By(nb_variables);
+    typename SparseLinearAlgebraTraits_d::Vector Z(nb_variables), Bz(nb_variables);
+
+    // assemble right columns of linear system
+    vertex_iterator vb, ve;
+    int idx = 0;
+    for ( boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
+    {
+      // only accumulate roi vertices
+      std::vector<vertex_descriptor> ::iterator result = find(roi.begin(), roi.end(), *vb);
+      if ( result == roi.end() )
+      {
+        Bx[idx] = (*vb)->point().x(); By[idx] = (*vb)->point().y(); Bz[idx] = (*vb)->point().z();
+      }
+      else
+      {
+        Bx[idx] = 0; By[idx] = 0;Bz[idx] = 0;
+        vertex_descriptor vi = *vb;
+        in_edge_iterator e, e_end;
+        for (boost::tie(e,e_end) = boost::in_edges(vi, *polyhedron); e != e_end; e++)
+        {
+          vertex_descriptor vj = boost::source(*e, *polyhedron);
+          Vector pij = vi->point() - vj->point();
+          double wij = edge_weight[*e];
+          Vector rot_p(0, 0, 0);                 // vector ( r_i + r_j )*p_ij
+          for (int i = 0; i < 3; i++)
+          {
+            for (int j = 0; j < 3; j++)
+            {
+              rot_p[i] += (rot_mtr[vi][i*3+j]+rot_mtr[vj][i*3+j])*pij[j];
+            }
+          }
+          Vector vec = wij*rot_p/2.0;
+          Bx[idx] += vec.x(); By[idx] += vec.y(); Bz[idx] += vec.z();
+        }
+      }
+      idx++;
+    }
+
+    // solve "A*X = B".
+    m_solver.solve(Bx, X); m_solver.solve(By, Y); m_solver.solve(Bz, Z);
+
+    // copy to solution
+    idx = 0;
+    for ( boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
+    {
+      solution[*vb].x() = X[idx]; solution[*vb].y() = Y[idx]; solution[*vb].z() = Z[idx]; 
+    }
+
+  }
+
+  // Compute modeling energy
+  double energy()
+  {
+    double sum_of_energy = 0;
+    vertex_iterator vb, ve;
+    for(boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
+    {
+      // only accumulate roi vertices
+      std::vector<vertex_descriptor> ::iterator result = find(roi.begin(), roi.end(), *vb);
+      if ( result != roi.end() )
+      {
+        vertex_descriptor vi = *vb;
+        in_edge_iterator e, e_end;
+        for (boost::tie(e,e_end) = boost::in_edges(vi, *polyhedron); e != e_end; e++)
+        {
+          vertex_descriptor vj = boost::source(*e, *polyhedron);
+          Vector pij = vi->point() - vj->point();
+          double wij = edge_weight[*e];
+          Vector rot_p(0, 0, 0);                 // vector rot_i*p_ij
+          for (int i = 0; i < 3; i++)
+          {
+            for (int j = 0; j < 3; j++)
+            {
+              rot_p[i] += rot_mtr[vi][i*3+j] * pij[j];
+            }
+          }
+          Vector vec = (solution[vi] - solution[vj]) - rot_p;
+          sum_of_energy += wij*vec.squared_length();
+        }
+      }
+    }
+    return sum_of_energy;
+  }
+
+  // Deformation on roi vertices
+  void deform()
+  {
+    // iterations
+    CGAL_TRACE_STREAM << "iteration started...\n";
+    optimal_rotations();
+    for ( int ite = 0; ite < iterations; ite ++)
+    {
+      update_solution();
+      optimal_rotations();
+      CGAL_TRACE_STREAM << ite << " iterations: energy = " << energy() << "\n";
+    }
+    CGAL_TRACE_STREAM << "iteration end!\n";
+
+    // copy solution to polyhedron
+    vertex_iterator vb, ve;
+    for(boost::tie(vb,ve) = boost::vertices(*polyhedron); vb != ve; ++vb )
+    {
+      (*vb)->point() = solution[*vb];
+    }
+  }
 };
 
 
---
+++ Surface_modeling/include/CGAL/svd.h
@@ -0,0 +1,209 @@
+#pragma once
+
+#include <math.h>
+#include "nrutil.h"
+
+float pythag(float a, float b);
+
+inline void svdcmp(float **a, int m, int n, float w[], float **v)
+{
+	float pythag(float a, float b);
+	int flag,i,its,j,jj,k,l,nm;
+	float anorm,c,f,g,h,s,scale,x,y,z,*rv1;
+	rv1=vec(1,n);
+	g=scale=anorm=0.0;									//Householder reduction to bidiagonal form.
+		for (i=1;i<=n;i++) {
+			l=i+1;
+			rv1[i]=scale*g;
+			g=s=scale=0.0;
+			if (i <= m) {
+				for (k=i;k<=m;k++) scale += fabs(a[k][i]);
+				if (scale) {
+					for (k=i;k<=m;k++) {
+						a[k][i] /= scale;
+						s += a[k][i]*a[k][i];
+					}
+					f=a[i][i];
+					g = -SIGN(sqrt(s),f);
+					h=f*g-s;
+					a[i][i]=f-g;
+					for (j=l;j<=n;j++) {
+						for (s=0.0,k=i;k<=m;k++) s += a[k][i]*a[k][j];
+						f=s/h;
+						for (k=i;k<=m;k++) a[k][j] += f*a[k][i];
+					}
+					for (k=i;k<=m;k++) a[k][i] *= scale;
+				}
+			}
+			w[i]=scale *g;
+			g=s=scale=0.0;
+			if (i <= m && i != n) {
+				for (k=l;k<=n;k++) scale += fabs(a[i][k]);
+				if (scale) {
+					for (k=l;k<=n;k++) {
+						a[i][k] /= scale;
+						s += a[i][k]*a[i][k];
+					}
+					f=a[i][l];
+					g = -SIGN(sqrt(s),f);
+					h=f*g-s;
+					a[i][l]=f-g;
+					for (k=l;k<=n;k++) rv1[k]=a[i][k]/h;
+					for (j=l;j<=m;j++) {
+						for (s=0.0,k=l;k<=n;k++) s += a[j][k]*a[i][k];
+						for (k=l;k<=n;k++) a[j][k] += s*rv1[k];
+					}
+					for (k=l;k<=n;k++) a[i][k] *= scale;
+				}
+			}
+			anorm=FMAX(anorm,(fabs(w[i])+fabs(rv1[i])));
+		}
+		for (i=n;i>=1;i--) {                                             //Accumulation of right-hand transformations.
+			if (i < n) {
+				if (g) {
+					for (j=l;j<=n;j++)									//Double division to avoid possible underflow.
+						v[j][i]=(a[i][j]/a[i][l])/g;
+					for (j=l;j<=n;j++) {
+						for (s=0.0,k=l;k<=n;k++) s += a[i][k]*v[k][j];
+						for (k=l;k<=n;k++) v[k][j] += s*v[k][i];
+					}
+				}
+				for (j=l;j<=n;j++) v[i][j]=v[j][i]=0.0;
+			}
+			v[i][i]=1.0;
+			g=rv1[i];
+			l=i;
+		}
+		for (i=IMIN(m,n);i>=1;i--) {									//Accumulation of left-hand transformations.
+			l=i+1;
+		g=w[i];
+		for (j=l;j<=n;j++) a[i][j]=0.0;
+		if (g) {
+			g=1.0/g;
+			for (j=l;j<=n;j++) {
+				for (s=0.0,k=l;k<=m;k++) s += a[k][i]*a[k][j];
+				f=(s/a[i][i])*g;
+				for (k=i;k<=m;k++) a[k][j] += f*a[k][i];
+			}
+			for (j=i;j<=m;j++) a[j][i] *= g;
+		} else for (j=i;j<=m;j++) a[j][i]=0.0;
+		++a[i][i];
+		}
+		for (k=n;k>=1;k--) 
+		{										//Diagonalization of the bidiagonal form: Loop over
+			for (its=1;its<=30;its++)
+			{ 
+				flag=1;
+				for (l=k;l>=1;l--)
+				{										//Test for splitting.
+					nm=l-1;													//Note that rv1[1] is always zero.
+					if ((float)(fabs(rv1[l])+anorm) == anorm)
+					{
+						flag=0;
+						break;
+					}
+					if ((float)(fabs(w[nm])+anorm) == anorm) break;
+				}
+				if (flag)
+				{
+					c=0.0;													//Cancellation of rv1[l], if l > 1.
+					s=1.0;
+					for (i=l;i<=k;i++)
+					{
+						f=s*rv1[i];
+						rv1[i]=c*rv1[i];
+						if ((float)(fabs(f)+anorm) == anorm) 
+							break;
+						g=w[i];
+						h=pythag(f,g);
+						w[i]=h;
+						h=1.0/h;
+						c=g*h;
+						s = -f*h;
+						for (j=1;j<=m;j++) 
+						{
+							y=a[j][nm];
+							z=a[j][i];
+							a[j][nm]=y*c+z*s;
+							a[j][i]=z*c-y*s;
+						}
+					}
+				}
+				z=w[k];
+				if (l == k)
+				{												//Convergence.
+					if (z < 0.0)
+					{											//Singular value is made nonnegative.
+						w[k] = -z;
+						for (j=1;j<=n;j++) 
+							v[j][k] = -v[j][k];
+					}
+					break;
+				}
+			/*	if (its == 30) 
+					nrerror("no convergence in 30 svdcmp iterations");*/
+				x=w[l];														//Shift from bottom 2-by-2 minor.
+				nm=k-1;
+				y=w[nm];
+				g=rv1[nm];
+				h=rv1[k];
+				f=((y-z)*(y+z)+(g-h)*(g+h))/(2.0*h*y);
+				g=pythag(f,1.0);
+				f=((x-z)*(x+z)+h*((y/(f+SIGN(g,f)))-h))/x;
+				c=s=1.0;													//Next QR transformation:
+				for (j=l;j<=nm;j++) 
+				{
+					i=j+1;
+					g=rv1[i];
+					y=w[i];
+					h=s*g;
+					g=c*g;
+					z=pythag(f,h);
+					rv1[j]=z;
+					c=f/z;
+					s=h/z;
+					f=x*c+g*s;
+					g = g*c-x*s;
+					h=y*s;
+					y *= c;
+					for (jj=1;jj<=n;jj++)
+					{
+						x=v[jj][j];
+						z=v[jj][i];
+						v[jj][j]=x*c+z*s;
+						v[jj][i]=z*c-x*s;
+					}
+					z=pythag(f,h);
+					w[j]=z;													//Rotation can be arbitrary if z = 0.
+					if (z) 
+					{
+						z=1.0/z;
+						c=f*z;
+						s=h*z;
+					}
+					f=c*g+s*y;
+					x=c*y-s*g;
+					for (jj=1;jj<=m;jj++) 
+					{
+						y=a[jj][j];
+						z=a[jj][i];
+						a[jj][j]=y*c+z*s;
+						a[jj][i]=z*c-y*s;
+					}
+				}
+				rv1[l]=0.0;
+				rv1[k]=f;
+				w[k]=x;
+			}
+		}
+		free_vector(rv1,1,n);
+}
+
+inline float pythag(float a, float b)
+{
+	float absa,absb;
+	absa=fabs(a);
+	absb=fabs(b);
+	if (absa > absb) return absa*sqrt(1.0+SQR(absb/absa));
+	else return (absb == 0.0 ? 0.0 : absb*sqrt(1.0+SQR(absa/absb)));
+}
