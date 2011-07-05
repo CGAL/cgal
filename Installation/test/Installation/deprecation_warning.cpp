50ffcdefbe3314f652d77f0b082842dcc6fa468a
Philipp Möller <Philipp.Moeller@geometryfactory.com>
2011-07-05 13:17:52 +0000

    Added deprecation_warning.h and a pseudo useful test.

---
+++ Installation/test/Installation/deprecation_warning.cpp
@@ -0,0 +1,20 @@
+// uncomment to test
+// would trigger errors/warning in the testsuite
+
+// fail to compile
+// #define CGAL_NO_DEPRECATED_CODE 1
+
+// message 1
+// #define CGAL_DEPRECATED_HEADER "test_header.h"
+
+
+// // message 2
+// #define CGAL_DEPRECATED_HEADER "test_header.h"
+// #define CGAL_REPLACEMENT_HEADER "test_replacement_header.h"
+
+// #include <CGAL/internal/deprecation_warning.h>
+
+int main()
+{
+  return 0;
+}
