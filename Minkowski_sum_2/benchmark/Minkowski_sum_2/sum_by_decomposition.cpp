2adb126cb388b6b8f5760cce2fdf2af6cddadfb4
Ophir Setter <ophirset@post.tau.ac.il>
2011-08-13 06:25:44 +0000

    moved files again

--- Minkowski_sum_2/benchmark/Minkowski_sum_2/sum_by_decomposition.cpp
+++ Minkowski_sum_2/benchmark/Minkowski_sum_2/sum_by_decomposition.cpp
@@ -7,13 +7,7 @@
 #include <CGAL/Small_side_angle_bisector_decomposition_2.h>
 #include <iostream>
 #include <fstream>
-
-#include <time.h>
-
-#if !defined(CGAL_MINKOWSKI_SUM_2_SERIAL) && defined(_OPENMP)   
-#include<omp.h>
-#endif
-
+#include <CGAL/Timer.h>
 #include "print_utils.h"
 
 struct Kernel : public CGAL::Exact_predicates_exact_constructions_kernel {};
@@ -24,8 +18,9 @@ typedef CGAL::Polygon_with_holes_2<Kernel>            Polygon_with_holes_2;
 
 int main ()
 {
+  CGAL::Timer                    t_mink_sum;
   // Open the input file.
-  std::ifstream    in_file ("random2.dat");
+  std::ifstream    in_file ("data/random2.dat");
 
   if (! in_file.is_open())
   {
@@ -39,32 +34,17 @@ int main ()
   in_file >> P >> Q;
   in_file.close();
 
+
   // Compute the Minkowski sum using the decomposition approach.
   CGAL::Small_side_angle_bisector_decomposition_2<Kernel>  ssab_decomp;
 
-  time_t                         s,e;
-  s = time(NULL);
-
-  #if !defined(CGAL_MINKOWSKI_SUM_2_SERIAL) && defined(_OPENMP)   
-  double start,end;
-  start = omp_get_wtime(); 
-  #endif  
+  t_mink_sum.start();
 
   Polygon_with_holes_2  sum = minkowski_sum_2 (P, Q, ssab_decomp);
 
-  #if !defined(CGAL_MINKOWSKI_SUM_2_SERIAL) && defined(_OPENMP)   
-  end = omp_get_wtime();
-  #endif
+  t_mink_sum.stop();
 
-  e = time(NULL);
-
-  #if !defined(CGAL_MINKOWSKI_SUM_2_SERIAL) && defined(_OPENMP)   
-  std::cout << "Done! time using omp_get_wtime() (" << end-start << " seconds)." << std::endl;
-  #endif 
-  
-  std::cout << "Done! time using time() (" << difftime(e,s) << " seconds)." << std::endl;
-
-  std::cout << "P (+) Q = "; print_polygon_with_holes (sum);
+  std::cout << "Done! Time:" << t_mink_sum.time() << " seconds\n P (+) Q = "; print_polygon_with_holes (sum);
 
   return (0);
 }
