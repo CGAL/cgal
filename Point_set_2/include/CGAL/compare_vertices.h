dff6dda8b3614f1e25102d80ad1fcdee6f4d6ceb
Sébastien Loriot <sloriot.ml@gmail.com>
2011-09-21 19:46:31 +0000

    Remove from copyright holder Freie Universitaet Berlin (Germany), Martin-Luther-University Halle-Wittenberg (Germany) and RISC Linz (Austria) as they transfer the copyright to other sites.

--- Point_set_2/include/CGAL/compare_vertices.h
+++ Point_set_2/include/CGAL/compare_vertices.h
@@ -1,5 +1,9 @@
-// Copyright (c) 1999  Martin-Luther-University Halle-Wittenberg (Germany).
-// All rights reserved.
+// Copyright (c) 1999  
+// Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland),
+// INRIA Sophia-Antipolis (France),
+// Max-Planck-Institute Saarbruecken (Germany),
+// and Tel-Aviv University (Israel).  All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org); you may redistribute it under
 // the terms of the Q Public License version 1.0.
