eccf82009dbfab816e5a845b83cee8fe876c6cc6
Sébastien Loriot <sloriot.ml@gmail.com>
2011-09-22 08:09:11 +0000

    update copyright holders

--- Point_set_2/include/CGAL/Point_set_2.h
+++ Point_set_2/include/CGAL/Point_set_2.h
@@ -1,9 +1,5 @@
 // Copyright (c) 1999  
-// Utrecht University (The Netherlands),
-// ETH Zurich (Switzerland),
-// INRIA Sophia-Antipolis (France),
-// Max-Planck-Institute Saarbruecken (Germany),
-// and Tel-Aviv University (Israel).  All rights reserved.
+// Max-Planck-Institute Saarbruecken (Germany). All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org); you may redistribute it under
 // the terms of the Q Public License version 1.0.
