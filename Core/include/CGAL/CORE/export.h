1960fb828ca4588b5a2339031ae8dc1fdaa9f8b5
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2011-10-06 09:58:26 +0000

    Add NYU copyright header

--- Core/include/CGAL/CORE/export.h
+++ Core/include/CGAL/CORE/export.h
@@ -1,3 +1,42 @@
+/****************************************************************************
+ * Core Library Version 1.7, August 2004
+ * Copyright (c) 1995-2004 Exact Computation Project
+ * All rights reserved.
+ *
+ * This file is part of CORE (http://cs.nyu.edu/exact/core/); you may
+ * redistribute it under the terms of the Q Public License version 1.0.
+ * See the file LICENSE.QPL distributed with CORE.
+ *
+ * Licensees holding a valid commercial license may use this file in
+ * accordance with the commercial license agreement provided with the
+ * software.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ * File: Expr.h
+ * Synopsis: a class of Expression in Level 3
+ * 
+ * Written by 
+ *       Koji Ouchi <ouchi@simulation.nyu.edu>
+ *       Chee Yap <yap@cs.nyu.edu>
+ *       Igor Pechtchanski <pechtcha@cs.nyu.edu>
+ *       Vijay Karamcheti <vijayk@cs.nyu.edu>
+ *       Chen Li <chenli@cs.nyu.edu>
+ *       Zilin Du <zilin@cs.nyu.edu>
+ *       Sylvain Pion <pion@cs.nyu.edu> 
+ *       Vikram Sharma<sharma@cs.nyu.edu>
+ *
+ * WWW URL: http://cs.nyu.edu/exact/
+ * Email: exact@cs.nyu.edu
+ *
+ * $URL$
+ * $Id$
+ ***************************************************************************/
+
+// Author(s)     : Andreas Fabri
+
 #ifndef CGAL_CORE_EXPORT_H
 #define CGAL_CORE_EXPORT_H
 
