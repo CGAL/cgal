ebc4f53646e7aa9080b89d1b35748cd948ce5d3f
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2011-10-06 13:14:17 +0000

    Add declspecs for Visual C++ so that we can build dlls

---
+++ GraphicsView/include/CGAL/Qt/export.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2011 GeometryFactory (France). All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the License.
+// See the file LICENSE.LGPL distributed with CGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : Andreas Fabri
+#ifndef CGAL_QT4_EXPORT_H
+#define CGAL_QT4_EXPORT_H
+
+#include <boost/config.hpp>
+
+#if defined(BOOST_MSVC)
+
+#if defined(CGAL_Qt4_EXPORTS) // add by CMake or in cpp files of the dll
+#define	CGAL_QT4_EXPORT __declspec (dllexport)
+#define CGAL_QT4_EXPIMP_TEMPLATE
+#else
+#define CGAL_QT4_EXPORT __declspec (dllimport)
+#define CGAL_QT4_EXPIMP_TEMPLATE extern
+#endif
+ 
+#else 
+
+#define  CGAL_QT4_EXPORT
+#define CGAL_QT4_EXPIMP_TEMPLATE 
+#endif
+
+#endif //  CGAL_QT4_EXPORT_H
+
+
