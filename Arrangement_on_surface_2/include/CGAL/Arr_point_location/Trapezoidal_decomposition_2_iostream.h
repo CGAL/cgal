51b20e7ff48cb68d33e7d3df9966bdbdd316874e
Michal Kleinbort <michal.balas@gmail.com>
2011-10-10 17:06:59 +0000

    unbounded RIC feature

--- Arrangement_on_surface_2/include/CGAL/Arr_point_location/Trapezoidal_decomposition_2_iostream.h
+++ Arrangement_on_surface_2/include/CGAL/Arr_point_location/Trapezoidal_decomposition_2_iostream.h
@@ -46,7 +46,7 @@ template < class Traits>
 std::ostream& operator<<(
 			 std::ostream &out,const Trapezoidal_decomposition_2<Traits>& td)
 {
-  return write(out,td.data_structure(),td.get_traits());
+  return write(out,td.dag_root(),td.get_traits());
 }
 
 template < class Traits>
@@ -58,14 +58,16 @@ std::ostream& write(std::ostream &out,const Td_X_trapezoid<Traits>& t,
   typedef X_trapezoid* pointer;
   bool pad=false;
 
+  //MICHAL: this may fail for inactive trapezoids
+
   out << "(";
-  if (!t.is_left_unbounded()) out << t.left(); else out << "-oo";
+  if (!t.is_on_left_boundary()) out << t.left(); else out << "-oo"; //MICHAL: this may fail for removed point
   out << ",";
-  if (!t.is_right_unbounded()) out << t.right(); else out << "+oo";
+  if (!t.is_on_right_boundary()) out << t.right(); else out << "+oo";
   out << "," << std::flush;
-  if (!t.is_bottom_unbounded()) out << t.bottom(); else out << "-oo";
+  if (!t.is_on_bottom_boundary()) out << t.bottom(); else out << "-oo";
   out << ",";
-  if (!t.is_top_unbounded()) out << t.top(); else out << "+oo";
+  if (!t.is_on_top_boundary()) out << t.top(); else out << "+oo";
   out << ",neighbours(" << std::flush;
 	
   // debug neighbours equivalence relation
@@ -75,11 +77,11 @@ std::ostream& write(std::ostream &out,const Td_X_trapezoid<Traits>& t,
   X_trapezoid* value[] =
     {
       0,
-      (X_trapezoid*)CGAL_TRAPEZOIDAL_DECOMPOSITION_2_DELETE_SIGNATURE,
-      t.left_bottom_neighbour(),
-      t.left_top_neighbour(),
-      t.right_bottom_neighbour(),
-      t.right_top_neighbour()
+      (X_trapezoid*)CGAL_TD_DELETE_SIGNATURE,
+      t.lb(),
+      t.lt(),
+      t.rb(),
+      t.rt()
     };
   typedef char debug_string[256];
   debug_string name[]=
@@ -125,15 +127,15 @@ std::ostream& write(std::ostream &out,const Td_X_trapezoid<Traits>& t,
 	  if (t.is_unbounded())
 	    {
 	      out << ",U=";
-	      if (t.is_left_unbounded())   out << ")";
-	      if (t.is_bottom_unbounded()) {
-		if (t.is_top_unbounded())    
+	      if (t.is_on_left_boundary())   out << ")";
+	      if (t.is_on_bottom_boundary()) {
+		if (t.is_on_top_boundary())    
 		  out << "/\\/";
 		else 
 		  out << "/\\";
 	      }
-	      else if (t.is_top_unbounded())    out << "\\/";
-	      if (t.is_right_unbounded())  out << "(";
+	      else if (t.is_on_top_boundary())    out << "\\/";
+	      if (t.is_on_right_boundary())  out << "(";
 	    }
 	  else
 	    out << ",T";
@@ -169,14 +171,16 @@ std::ostream& operator<<(std::ostream &out,const Td_X_trapezoid<Traits>& t)
   typedef X_trapezoid* pointer;
   Traits traits;
 
+  //MICHAL: this may fail for inactive trapezoids
+
   out << "(";
-  if (!t.is_left_unbounded()) out << t.left(); else out << "-oo";
+  if (!t.is_on_left_boundary()) out << t.left(); else out << "-oo";
   out << ",";
-  if (!t.is_right_unbounded()) out << t.right(); else out << "+oo";
+  if (!t.is_on_right_boundary()) out << t.right(); else out << "+oo";
   out << "," << std::flush;
-  if (!t.is_bottom_unbounded()) out << t.bottom(); else out << "-oo";
+  if (!t.is_on_bottom_boundary()) out << t.bottom(); else out << "-oo";
   out << ",";
-  if (!t.is_top_unbounded()) out << t.top(); else out << "+oo";
+  if (!t.is_on_top_boundary()) out << t.top(); else out << "+oo";
   out << ",neighbours(" << std::flush;
 	
   // debug neighbours equivalence relation
@@ -186,11 +190,11 @@ std::ostream& operator<<(std::ostream &out,const Td_X_trapezoid<Traits>& t)
   X_trapezoid* value[] =
     {
       0,
-      (X_trapezoid*)CGAL_TRAPEZOIDAL_DECOMPOSITION_2_DELETE_SIGNATURE,
-      t.left_bottom_neighbour(),
-      t.left_top_neighbour(),
-      t.right_bottom_neighbour(),
-      t.right_top_neighbour()
+      (X_trapezoid*)CGAL_TD_DELETE_SIGNATURE,
+      t.lb(),
+      t.lt(),
+      t.rb(),
+      t.rt()
     };
   typedef char debug_string[256];
   debug_string name[]=
@@ -235,13 +239,13 @@ std::ostream& operator<<(std::ostream &out,const Td_X_trapezoid<Traits>& t)
 	  if (t.is_unbounded())
 	    {
 	      out << ",U";
-	      if (t.is_left_unbounded())
+	      if (t.is_on_left_boundary())
 		out << (char) 174;
-	      if (t.is_bottom_unbounded())
+	      if (t.is_on_bottom_boundary())
 		out << (char) 25;
-	      if (t.is_top_unbounded())
+	      if (t.is_on_top_boundary())
 		out << (char) 24;
-	      if (t.is_right_unbounded())
+	      if (t.is_on_right_boundary())
 		out << (char) 175;
 	    }
 	  else
