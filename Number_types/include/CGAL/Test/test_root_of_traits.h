5dee43f347faa16543ad09f93da10fb495b20316
Michael Hemmer <hemmer@mpi-inf.mpg.de>
2011-10-10 16:42:42 +0000

    fix test for new name

--- Number_types/include/CGAL/Test/test_root_of_traits.h
+++ Number_types/include/CGAL/Test/test_root_of_traits.h
@@ -83,33 +83,33 @@ void test_root_of_traits(){
     bool is_not_exact = !CGAL::Algebraic_structure_traits<T>::Is_exact::value; 
     {
       std::vector<Root_of_2> roots;
-      CGAL::solve_1(T(1),T(0),T(-2),std::back_inserter(roots));
+      CGAL::compute_roots_of_2(T(1),T(0),T(-2),std::back_inserter(roots));
       assert(roots.size()==2);  
       assert(roots[0]==-CGAL::make_sqrt(T(2)) || is_not_exact );
       assert(roots[1]== CGAL::make_sqrt(T(2)) || is_not_exact );
     }  
     {
       Root_of_2 roots[2]= {Root_of_2(1),Root_of_2(1)};
-      CGAL::solve_1(T(13),T(4),T(-23),roots); 
+      CGAL::compute_roots_of_2(T(13),T(4),T(-23),roots); 
       assert(roots[0]==CGAL::make_root_of_2(T(13),T(4),T(-23),true)  || is_not_exact );
       assert(roots[1]==CGAL::make_root_of_2(T(13),T(4),T(-23),false) || is_not_exact );
     }   
     {
       std::vector<Root_of_2> roots;
-      CGAL::solve_1(T(1),T(-6),T(9),std::back_inserter(roots));
+      CGAL::compute_roots_of_2(T(1),T(-6),T(9),std::back_inserter(roots));
       assert(roots.size()==1);
       assert(roots[0]==Root_of_2(3) || is_not_exact );
     }  
     {
       std::vector<Root_of_2> roots;
-      CGAL::solve_1(T(1),T(0),T(2),std::back_inserter(roots));
+      CGAL::compute_roots_of_2(T(1),T(0),T(2),std::back_inserter(roots));
       assert(roots.size()==0);
     } 
     {
       std::vector<Root_of_2> roots;
-      CGAL::solve_1(T(0),T(3),T(2),std::back_inserter(roots));
+      CGAL::compute_roots_of_2(T(0),T(2),T(3),std::back_inserter(roots));
       assert(roots.size()==1);
-      assert(roots[0]==-Root_of_2(2)/Root_of_2(3));
+      assert(roots[0]==-Root_of_2(3)/Root_of_2(2) || is_not_exact );
     } 
     
 }
