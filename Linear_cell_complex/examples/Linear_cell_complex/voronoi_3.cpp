69c88def771778e08c13b8c6b19496d86e563791
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2011-11-08 12:04:20 +0000

    Update the voronoi3 example to draw only volumes that are not adjacent to the infinite volume.

--- Linear_cell_complex/examples/Linear_cell_complex/voronoi_3.cpp
+++ Linear_cell_complex/examples/Linear_cell_complex/voronoi_3.cpp
@@ -7,14 +7,54 @@
 
 /* If you want to use a viewer, you can use one of the following file
  * depending if you use vtk or qglviewer. */
-//#include "linear_cell_complex_3_viewer_qt.h"
-//#include "linear_cell_complex_3_viewer_vtk.h"
+#ifdef CGAL_LCC_USE_QT
+#include "linear_cell_complex_3_viewer_qt.h"
+#else 
+#ifdef CGAL_LCC_USE_VTK
+#include "linear_cell_complex_3_viewer_vtk.h"
+#endif
+#endif
 
 typedef CGAL::Linear_cell_complex<3> LCC_3;
 typedef LCC_3::Dart_handle           Dart_handle;
 typedef LCC_3::Point                 Point;
 
 typedef CGAL::Delaunay_triangulation_3<LCC_3::Traits> Triangulation;
+
+// Function used to display the voronoi diagram.
+void display_voronoi(LCC_3& alcc, Dart_handle adart)
+{
+  // We remove all the volumes containing one dart of the infinite volume
+  std::stack<Dart_handle> toremove;
+  int mark_toremove=alcc.get_new_mark();
+
+  // We cannot view the infinite volume since it does not have
+  // a correct geometry. For that we have to remove the infinite volume.
+  toremove.push(adart);
+  CGAL::mark_cell<LCC_3,3>(alcc, adart, mark_toremove);
+ 
+  // Plus all the volumes sharing a face with it.
+  for (LCC_3::Dart_of_cell_range<3>::iterator
+         it=alcc.darts_of_cell<3>(adart).begin(),
+         itend=alcc.darts_of_cell<3>(adart).end(); it!=itend; ++it)
+  {
+    if ( !alcc.is_marked(it->beta(3), mark_toremove) )
+    {
+      CGAL::mark_cell<LCC_3,3>(alcc, it->beta(3), mark_toremove);
+      toremove.push(it->beta(3));
+    }    
+  }
+  
+  while( !toremove.empty() )
+  {
+    CGAL::remove_cell<LCC_3, 3>(alcc, toremove.top());
+    toremove.pop();
+  }
+
+#ifdef CGAL_LCC_USE_VIEWER
+  display_lcc(alcc);
+#endif // CGAL_LCC_USE_VIEWER
+}
 
 int main(int argc, char** argv)
 {
@@ -42,19 +82,16 @@ int main(int argc, char** argv)
  
   // 2) Convert the triangulation into a 3D lcc.
   LCC_3 lcc;
-  CGAL::import_from_triangulation_3<LCC_3, Triangulation>(lcc, T);
+  Dart_handle dh=
+    CGAL::import_from_triangulation_3<LCC_3, Triangulation>(lcc, T);
 
   std::cout<<"Delaunay triangulation :"<<std::endl<<"  ";
   lcc.display_characteristics(std::cout) << ", valid=" 
                                          << lcc.is_valid() << std::endl;
 
-  /* The following line is possible only if you use
-   *  cgal_lcc_viewer_qt or cgal_lcc_viewer_vtk */
-  //  display_lcc(lcc); 
-
   // 3) Compute the dual lcc.
   LCC_3 dual_lcc;
-  CGAL::dual<LCC_3>(lcc,dual_lcc);
+  Dart_handle ddh=CGAL::dual<LCC_3>(lcc, dual_lcc, dh);
   // Here, dual_lcc is the 3D Voronoi diagram.
   
   // 4) Display the dual_lcc characteristics.
@@ -62,6 +99,10 @@ int main(int argc, char** argv)
   dual_lcc.display_characteristics(std::cout) << ", valid=" 
                                               << dual_lcc.is_valid()
                                               << std::endl;
+
+#ifdef CGAL_LCC_USE_VIEWER
+  display_voronoi(dual_lcc, ddh);
+#endif // CGAL_LCC_USE_VIEWER
 
   return EXIT_SUCCESS;
 }
