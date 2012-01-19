c84cf08626a4a7b178b5b5adc5827ac58c00b2bc
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2012-01-19 16:49:52 +0000

    That one was forgotten in the switch from LGPLv2 to LGPLv3+.

--- Core/src/CGALCore/GmpIO.cpp
+++ Core/src/CGALCore/GmpIO.cpp
@@ -20,7 +20,7 @@ This file is part of the GNU MP Library.
 
 The GNU MP Library is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 2.1 of the License, or (at your
+the Free Software Foundation; either version 3 of the License, or (at your
 option) any later version.
 
 The GNU MP Library is distributed in the hope that it will be useful, but
