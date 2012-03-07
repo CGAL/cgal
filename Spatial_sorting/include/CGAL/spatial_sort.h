e0dd26123d94056c2262625bd23c115718789be1
Philipp Möller <Philipp.Moeller@geometryfactory.com>
2012-03-07 14:08:55 +0000

    * exchanged std::random_shuffle with CGAL::random_shuffle

--- Spatial_sorting/include/CGAL/spatial_sort.h
+++ Spatial_sorting/include/CGAL/spatial_sort.h
@@ -25,6 +25,7 @@
 
 #include <CGAL/hilbert_sort.h>
 #include <CGAL/Multiscale_sort.h>
+#include <CGAL/random_shuffle.h>
 
 #include <boost/random.hpp>
 #include <boost/random/linear_congruential.hpp>
@@ -49,7 +50,7 @@ namespace internal {
       typedef Hilbert_sort_2<Kernel, Policy> Sort;
         boost::rand48 random;
         boost::random_number_generator<boost::rand48> rng(random);
-        std::random_shuffle(begin,end,rng);
+        CGAL::random_shuffle(begin,end,rng);
 
 	if (threshold_hilbert==0) threshold_hilbert=4;
 	if (threshold_multiscale==0) threshold_multiscale=16;
@@ -72,7 +73,7 @@ namespace internal {
       typedef Hilbert_sort_3<Kernel, Policy> Sort;
         boost::rand48 random;
         boost::random_number_generator<boost::rand48> rng(random);
-        std::random_shuffle(begin,end, rng);
+        CGAL::random_shuffle(begin,end, rng);
 
 	if (threshold_hilbert==0) threshold_hilbert=8;
 	if (threshold_multiscale==0) threshold_multiscale=64;
@@ -95,7 +96,7 @@ namespace internal {
       typedef Hilbert_sort_d<Kernel, Policy> Sort;
         boost::rand48 random;
         boost::random_number_generator<boost::rand48> rng(random);
-        std::random_shuffle(begin,end, rng);
+        CGAL::random_shuffle(begin,end, rng);
 	
 	if (threshold_hilbert==0) threshold_hilbert=10;
 	if (threshold_multiscale==0) threshold_multiscale=500;
