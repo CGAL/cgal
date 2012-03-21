2869bf0c5908374a567fa14c272bfe9318a09535
Marc Glisse <Marc.Glisse@inria.fr>
2012-03-21 18:13:38 +0000

    make typeset functional.

--- NewKernel_d/include/CGAL/typeset.h
+++ NewKernel_d/include/CGAL/typeset.h
@@ -3,7 +3,7 @@
 #ifdef CGAL_CXX0X
 #include <type_traits>
 #else
-#include <boost/type_traits/conditional.hpp>
+#include <boost/type_traits.hpp>
 #endif
 
 // Sometimes using tuple just to list types is overkill (takes forever to
@@ -12,24 +12,50 @@
 namespace CGAL {
 #ifdef CGAL_CXX0X
   template<class...> struct typeset;
-  template<class T,class...U> struct typeset<T,U...> {
-    typedef T head;
+  template<class H,class...U> struct typeset<H,U...> {
+    typedef H head;
     typedef typeset<U...> tail;
-    template<class X> struct contains {
-      enum { value = std::conditional<std::is_same<T,X>::value,std::true_type,tail::contains<X>>::type::value };
-    };
-    template<class X> struct add {
-      typedef std::conditional<contains<X>::value,typeset<T,U...>,typeset<T,U...,X>>::type type;
-    };
+    typedef typeset type;
+    template<class X> using contains = typename
+      std::conditional<
+        std::is_same<H,X>::value,
+        std::true_type,
+        typename tail::template contains<X>
+      >::type;
+    template<class X> using add = typename
+      std::conditional<
+        contains<X>::value,
+        typeset<H,U...>,
+	typeset<H,U...,X>
+      >::type;
   };
   template<> struct typeset<> {
-    template<class X> struct contains : boost::false_type {};
-    template<class X> struct add {
-      typedef typeset<X> type;
-    };
+    typedef typeset type;
+    template<class X> using contains = std::false_type;
+    template<class X> using add = typeset<X>;
   };
 #else
-    // ???
+  template<class,class> struct typeset;
+  template<class H=void, class T=typename
+    boost::mpl::if_<boost::is_same<H,void>, void, typeset<void, void> >::type >
+  struct typeset {
+    typedef typeset type;
+    typedef H head;
+    typedef T tail;
+    template<class X> struct contains :
+      boost::mpl::if_<boost::is_same<H,X>,boost::true_type,typename tail::template contains<X> >::type
+    {};
+    template<class X,class=void> struct add :
+      //boost::mpl::if_<boost::is_same<H,X>,typeset,typeset<X,typeset> >::type
+      typeset<H,typename tail::template add<X>::type>
+    {};
+    template<class V> struct add<H,V> : typeset {};
+  };
+  template<> struct typeset<> {
+    typedef typeset type;
+    template<class X> struct contains : boost::false_type {};
+    template<class X> struct add : typeset<X> {};
+  };
 #endif
 }
 #endif
