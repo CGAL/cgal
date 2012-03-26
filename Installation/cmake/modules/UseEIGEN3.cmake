a2a3192695e342837d135b2ed07ce7e5d97bac5b
Sébastien Loriot <sloriot.ml@gmail.com>
2012-03-26 13:34:36 +0000

    remove duplicated file

--- Installation/cmake/modules/UseEIGEN3.cmake
+++
@@ -1,8 +0,0 @@
-# This module setups the compiler for using EIGEN3 library.
-# It assumes that find_package(EIGEN3) was already called.
-
-
-include_directories( ${EIGEN3_INCLUDE_DIR} )
-include_directories( ${EIGEN3_INCLUDE_DIR}/unsupported )
-
-add_definitions(-DCGAL_EIGEN3_ENABLED)
