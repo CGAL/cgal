14636d413194c363036fcada5669335ac7b20ee1
Clément Jamin <clement.jamin@inria.fr>
2012-04-04 07:07:39 +0000

    Tested a new locking grid with thread priorities. Actually, it decreases performance with the current algorithm, but we'll try it again later...

--- Mesh_3/include/CGAL/Mesh_3/Locking_data_structures.h
+++ Mesh_3/include/CGAL/Mesh_3/Locking_data_structures.h
@@ -25,6 +25,7 @@
 #include <CGAL/Bbox_3.h>
 
 #include <tbb/tbb.h>
+#include <tbb/compat/thread>
 #include <tbb/enumerable_thread_specific.h>
 
 #include <algorithm>
@@ -110,11 +111,11 @@ public:
   std::pair<bool, int> try_lock(const P3 &point, int lock_radius = 0)
   {
     // Compute indices on grid
-    int index_x = static_cast<int>( (point.x() - m_xmin) * m_resolution_x);
+    int index_x = static_cast<int>( (to_double(point.x()) - m_xmin) * m_resolution_x);
     index_x = std::max( 0, std::min(index_x, m_num_grid_cells_per_axis - 1) );
-    int index_y = static_cast<int>( (point.y() - m_ymin) * m_resolution_y);
+    int index_y = static_cast<int>( (to_double(point.y()) - m_ymin) * m_resolution_y);
     index_y = std::max( 0, std::min(index_y, m_num_grid_cells_per_axis - 1) );
-    int index_z = static_cast<int>( (point.z() - m_zmin) * m_resolution_z);
+    int index_z = static_cast<int>( (to_double(point.z()) - m_zmin) * m_resolution_z);
     index_z = std::max( 0, std::min(index_z, m_num_grid_cells_per_axis - 1) );
     
     int index = 
@@ -244,14 +245,13 @@ protected:
   TLS_locked_cells                                m_tls_locked_cells;
 };
 
-
-class Simple_grid_locking_ds_with_mutex
+class Simple_grid_locking_ds_with_thread_ids
 {
 public:
   // Constructors
   
-  Simple_grid_locking_ds_with_mutex(
-    const Bbox_3 &bbox, int num_grid_cells_per_axis)
+  Simple_grid_locking_ds_with_thread_ids(const Bbox_3 &bbox, 
+                         int num_grid_cells_per_axis)
     : m_num_grid_cells_per_axis(num_grid_cells_per_axis),
       m_tls_grids(
         [=]() -> bool* // CJTODO: lambdas OK?
@@ -263,11 +263,20 @@ public:
             local_grid[i] = false;
           return local_grid; 
         }
+      ),      m_tls_thread_ids(
+        [=]() -> unsigned int // CJTODO: lambdas OK?
+        {
+          static unsigned int last_id = 0;
+          return ++last_id;
+        }
       )
   {
     int num_cells = 
       num_grid_cells_per_axis*num_grid_cells_per_axis*num_grid_cells_per_axis;
-    m_grid = new tbb::recursive_mutex[num_cells];
+    m_grid = new tbb::atomic<unsigned int>[num_cells];
+    // Initialize grid
+    for (int i = 0 ; i < num_cells ; ++i)
+      m_grid[i] = 0;
 
     // Keep mins and resolutions
     m_xmin = bbox.xmin();
@@ -280,7 +289,7 @@ public:
   }
 
   /// Destructor
-  ~Simple_grid_locking_ds_with_mutex()
+  ~Simple_grid_locking_ds_with_thread_ids()
   {
     delete [] m_grid;
 
@@ -303,13 +312,23 @@ public:
     // Otherwise, try to lock it
     else
     {
-      bool locked = m_grid[cell_index].try_lock();
-      if (locked == true)
+      unsigned int this_thread_id = m_tls_thread_ids.local();
+      unsigned int old_value;
+      do
       {
-        ret = true;
-        m_tls_grids.local()[cell_index] = true;
-        m_tls_locked_cells.local().push_back(cell_index);
-      }
+        old_value = m_grid[cell_index].compare_and_swap(
+          this_thread_id, 0);
+        if (old_value == 0)
+        {
+          ret = true;
+          m_tls_grids.local()[cell_index] = true;
+          m_tls_locked_cells.local().push_back(cell_index);
+        }
+        else
+        {
+          std::this_thread::yield();
+        }
+      } while (ret == false && old_value < this_thread_id);
     }
     return ret;
   }
@@ -320,11 +339,11 @@ public:
   std::pair<bool, int> try_lock(const P3 &point, int lock_radius = 0)
   {
     // Compute indices on grid
-    int index_x = static_cast<int>( (point.x() - m_xmin) * m_resolution_x);
+    int index_x = static_cast<int>( (to_double(point.x()) - m_xmin) * m_resolution_x);
     index_x = std::max( 0, std::min(index_x, m_num_grid_cells_per_axis - 1) );
-    int index_y = static_cast<int>( (point.y() - m_ymin) * m_resolution_y);
+    int index_y = static_cast<int>( (to_double(point.y()) - m_ymin) * m_resolution_y);
     index_y = std::max( 0, std::min(index_y, m_num_grid_cells_per_axis - 1) );
-    int index_z = static_cast<int>( (point.z() - m_zmin) * m_resolution_z);
+    int index_z = static_cast<int>( (to_double(point.z()) - m_zmin) * m_resolution_z);
     index_z = std::max( 0, std::min(index_z, m_num_grid_cells_per_axis - 1) );
     
     int index = 
@@ -407,6 +426,218 @@ public:
   void unlock(int cell_index)
   {
     // Unlock lock and shared grid
+    m_grid[cell_index] = 0;
+    m_tls_grids.local()[cell_index] = false;
+  }
+  
+  void unlock_all_tls_locked_cells()
+  {
+    std::vector<int> &tls_locked_cells = m_tls_locked_cells.local();
+    std::vector<int>::const_iterator it = tls_locked_cells.begin();
+    std::vector<int>::const_iterator it_end = tls_locked_cells.end();
+    for( ; it != it_end ; ++it)
+    {
+      // If we still own the lock
+      int cell_index = *it;
+      if (m_tls_grids.local()[cell_index] == true)
+        unlock(cell_index);
+    }
+    tls_locked_cells.clear();
+  }
+  
+  bool check_if_all_tls_cells_are_unlocked()
+  {
+    int num_cells = m_num_grid_cells_per_axis*
+      m_num_grid_cells_per_axis*m_num_grid_cells_per_axis;
+    bool unlocked = true;
+    for (int i = 0 ; unlocked && i < num_cells ; ++i)
+      unlocked = (m_tls_grids.local()[i] == false);
+    return unlocked; 
+  }
+
+protected:
+  int                                             m_num_grid_cells_per_axis;
+  double                                          m_xmin;
+  double                                          m_ymin;
+  double                                          m_zmin;
+  double                                          m_resolution_x;
+  double                                          m_resolution_y;
+  double                                          m_resolution_z;
+  tbb::atomic<unsigned int> *                     m_grid;
+
+  // TLS
+  typedef tbb::enumerable_thread_specific<bool*>              TLS_grid;
+  typedef tbb::enumerable_thread_specific<std::vector<int> >  TLS_locked_cells;
+  typedef tbb::enumerable_thread_specific<unsigned int>       TLS_thread_uint_ids;
+
+  TLS_grid                                        m_tls_grids;
+  TLS_locked_cells                                m_tls_locked_cells;
+  TLS_thread_uint_ids                             m_tls_thread_ids;
+};
+
+
+class Simple_grid_locking_ds_with_mutex
+{
+public:
+  // Constructors
+  
+  Simple_grid_locking_ds_with_mutex(
+    const Bbox_3 &bbox, int num_grid_cells_per_axis)
+    : m_num_grid_cells_per_axis(num_grid_cells_per_axis),
+      m_tls_grids(
+        [=]() -> bool* // CJTODO: lambdas OK?
+        {
+          int num_cells = num_grid_cells_per_axis*
+            num_grid_cells_per_axis*num_grid_cells_per_axis;
+          bool *local_grid = new bool[num_cells];
+          for (int i = 0 ; i < num_cells ; ++i)
+            local_grid[i] = false;
+          return local_grid; 
+        }
+      )
+  {
+    int num_cells = 
+      num_grid_cells_per_axis*num_grid_cells_per_axis*num_grid_cells_per_axis;
+    m_grid = new tbb::recursive_mutex[num_cells];
+
+    // Keep mins and resolutions
+    m_xmin = bbox.xmin();
+    m_ymin = bbox.ymin();
+    m_zmin = bbox.zmin();
+    double n = static_cast<double>(num_grid_cells_per_axis);
+    m_resolution_x = n / (bbox.xmax() - m_xmin);
+    m_resolution_y = n / (bbox.ymax() - m_ymin);
+    m_resolution_z = n / (bbox.zmax() - m_zmin);
+  }
+
+  /// Destructor
+  ~Simple_grid_locking_ds_with_mutex()
+  {
+    delete [] m_grid;
+
+    for( TLS_grid::iterator it_grid = m_tls_grids.begin() ; 
+             it_grid != m_tls_grids.end() ; 
+             ++it_grid )
+    {
+      delete [] *it_grid;
+    }
+  }
+
+  bool try_lock(int cell_index)
+  {
+    bool ret = false;
+    // Already locked by this thread?
+    if (m_tls_grids.local()[cell_index])
+    {
+      ret = true;
+    }
+    // Otherwise, try to lock it
+    else
+    {
+      bool locked = m_grid[cell_index].try_lock();
+      if (locked == true)
+      {
+        ret = true;
+        m_tls_grids.local()[cell_index] = true;
+        m_tls_locked_cells.local().push_back(cell_index);
+      }
+    }
+    return ret;
+  }
+
+  /// P3 must provide .x(), .y(), .z()
+  /// Returns a pair "success or not + index of the grid cell"
+  template <typename P3>
+  std::pair<bool, int> try_lock(const P3 &point, int lock_radius = 0)
+  {
+    // Compute indices on grid
+    int index_x = static_cast<int>( (to_double(point.x()) - m_xmin) * m_resolution_x);
+    index_x = std::max( 0, std::min(index_x, m_num_grid_cells_per_axis - 1) );
+    int index_y = static_cast<int>( (to_double(point.y()) - m_ymin) * m_resolution_y);
+    index_y = std::max( 0, std::min(index_y, m_num_grid_cells_per_axis - 1) );
+    int index_z = static_cast<int>( (to_double(point.z()) - m_zmin) * m_resolution_z);
+    index_z = std::max( 0, std::min(index_z, m_num_grid_cells_per_axis - 1) );
+    
+    int index = 
+      index_z*m_num_grid_cells_per_axis*m_num_grid_cells_per_axis
+      + index_y*m_num_grid_cells_per_axis 
+      + index_x;
+
+    //if(index == 0) // CJTOTO TEMP
+    //    return std::make_pair(true, index);
+
+    if (lock_radius == 0)
+    {
+      return std::make_pair(try_lock(index), index);
+    }
+    else
+    {
+      // We have to lock the square
+      std::vector<int> locked_cells_tmp;
+
+      // For each cell inside the square
+      for (int i = std::max(0, index_x-lock_radius) ; 
+           i <= std::min(m_num_grid_cells_per_axis - 1, index_x+lock_radius) ; 
+           ++i)
+      {
+        for (int j = std::max(0, index_y-lock_radius) ; 
+             j <= std::min(m_num_grid_cells_per_axis - 1, index_y+lock_radius) ; 
+             ++j)
+        {
+          for (int k = std::max(0, index_z-lock_radius) ; 
+               k <= std::min(m_num_grid_cells_per_axis - 1, index_z+lock_radius) ;
+               ++k)
+          {
+            int index_to_lock = 
+              k*m_num_grid_cells_per_axis*m_num_grid_cells_per_axis
+              + j*m_num_grid_cells_per_axis 
+              + i;
+            // Try to lock it
+            if (try_lock(index_to_lock))
+            {
+              locked_cells_tmp.push_back(index_to_lock);
+            }
+            else
+            {
+              // failed => we unlock already locked cells and return false
+              std::vector<int>::const_iterator it = locked_cells_tmp.begin();
+              std::vector<int>::const_iterator it_end = locked_cells_tmp.end();
+              for( ; it != it_end ; ++it)
+              {
+                unlock(*it);
+              }
+              return std::make_pair(false, index);
+            }
+          }
+        }
+      }
+
+      return std::make_pair(true, index);
+    }
+  }
+
+  template <typename P3>
+  void unlock(const P3 &point)
+  {
+    // Compute indices on grid
+    int index_x = static_cast<int>( (to_double(point.x()) - m_xmin) * m_resolution_x);
+    index_x = std::max( 0, std::min(index_x, m_num_grid_cells_per_axis - 1) );
+    int index_y = static_cast<int>( (to_double(point.y()) - m_ymin) * m_resolution_y);
+    index_y = std::max( 0, std::min(index_y, m_num_grid_cells_per_axis - 1) );
+    int index_z = static_cast<int>( (to_double(point.z()) - m_zmin) * m_resolution_z);
+    index_z = std::max( 0, std::min(index_z, m_num_grid_cells_per_axis - 1) );
+    
+    int index = 
+      index_z*m_num_grid_cells_per_axis*m_num_grid_cells_per_axis
+      + index_y*m_num_grid_cells_per_axis 
+      + index_x;
+
+    unlock(index);
+  }
+
+  void unlock(int cell_index)
+  {
+    // Unlock lock and shared grid
     m_grid[cell_index].unlock();
     m_tls_grids.local()[cell_index] = false;
   }
@@ -454,8 +685,9 @@ protected:
   TLS_locked_cells                                m_tls_locked_cells;
 };
 
-typedef Simple_grid_locking_ds_with_mutex Refinement_grid_type;
-//typedef Simple_grid_locking_ds Refinement_grid_type;
+typedef Simple_grid_locking_ds Refinement_grid_type;
+//typedef Simple_grid_locking_ds_with_mutex Refinement_grid_type;
+//typedef Simple_grid_locking_ds_with_thread_ids Refinement_grid_type;
 
 
 } //namespace Mesh_3
