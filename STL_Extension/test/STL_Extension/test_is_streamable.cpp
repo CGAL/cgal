9d041ed59ae9e34479168d6543a0686e39fe4494
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2012-04-16 12:48:50 +0000

    Better testing of CGAL::is_streamable<T>

--- STL_Extension/test/STL_Extension/test_is_streamable.cpp
+++ STL_Extension/test/STL_Extension/test_is_streamable.cpp
@@ -5,12 +5,17 @@
 
 struct A {};
 struct B {};
+struct C {};
+struct D {};
 
 using std::ostream;
 using std::istream;
 
 ostream& operator<<(ostream& os, const B&) { return os; }
 istream& operator>>(istream& is, const B&) { return is; }
+
+ostream& operator<<(ostream& os, const C&) { return os; }
+istream& operator>>(istream& is, const D&) { return is; }
 
 int main() {
   typedef std::vector<int>::const_iterator vector_it;
@@ -19,6 +24,8 @@ int main() {
 
   CGAL_static_assertion(!is_streamable<A>::value);
   CGAL_static_assertion(is_streamable<B>::value);
+  CGAL_static_assertion(!is_streamable<C>::value);
+  CGAL_static_assertion(!is_streamable<D>::value);
   CGAL_static_assertion(is_streamable<int>::value);
   CGAL_static_assertion(is_streamable<double>::value);
   CGAL_static_assertion(! (is_streamable<std::pair<int, int> >::value) );
