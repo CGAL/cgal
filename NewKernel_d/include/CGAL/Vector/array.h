8d8b159090a625601f6701b66c0524c9c70c2fc0
Marc Glisse <Marc.Glisse@inria.fr>
2012-04-21 10:05:42 +0000

    move

--- NewKernel_d/include/CGAL/Vector/array.h
+++ NewKernel_d/include/CGAL/Vector/array.h
@@ -8,7 +8,7 @@
 #include <boost/preprocessor/repetition.hpp>
 #include <boost/preprocessor/repetition/enum.hpp>
 
-#include <CGAL/Vector/determinant_of_points.h>
+#include <CGAL/Vector/determinant_of_points_from_vectors.h>
 #include <CGAL/Vector/determinant_of_vectors_small_dim.h>
 #include <CGAL/Vector/determinant_of_iterator_to_points_from_iterator_to_vectors.h>
 #include <CGAL/Vector/determinant_of_iterator_to_points_from_points.h>
