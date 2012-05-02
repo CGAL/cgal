15ad33b51e633377d3ce47b5395152be033854ca
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2012-05-02 09:45:40 +0000

    Remove '*1'

--- Solver_interface/include/CGAL/Eigen_matrix.h
+++ Solver_interface/include/CGAL/Eigen_matrix.h
@@ -59,7 +59,7 @@ public:
 
     m_is_symmetric = is_symmetric;
     // reserve memory for a regular 3D grid
-    m_triplets.reserve(dim*1);
+    m_triplets.reserve(dim);
   }
 
   /// Create a rectangular matrix initialized with zeros.
@@ -78,7 +78,7 @@ public:
 
     m_is_symmetric = is_symmetric;
     // reserve memory for a regular 3D grid
-    m_triplets.reserve(rows*1);
+    m_triplets.reserve(rows);
   }
 
   /// Delete this object and the wrapped TAUCS matrix.
