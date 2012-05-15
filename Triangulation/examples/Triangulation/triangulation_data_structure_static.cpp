3c8c4cf576dddc41ccfdd337b20b5a2f2a4d18c1
Olivier Devillers <Olivier.Devillers@inria.fr>
2012-05-15 14:37:55 +0000

    splitting basic example

--- Triangulation/examples/Triangulation/triangulation_data_structure_static.cpp
+++ Triangulation/examples/Triangulation/triangulation_data_structure_static.cpp
@@ -4,23 +4,15 @@
 
 int main()
 {
-  const int sdim = 27; // dimension of TDS with compile-time dimension
-  const int ddim = 15; // dimension of TDS with dynamic dimension
+  typedef CGAL::Triangulation_data_structure<CGAL::Dimension_tag<7> > TDS;
+  typedef TDS::Face             Face;
+  typedef TDS::Facet            Facet;
+  typedef TDS::Vertex_handle    Vertex_handle;
+  typedef TDS::Full_cell_handle Full_cell_handle;
 
-  typedef CGAL::Triangulation_data_structure<CGAL::Dynamic_dimension_tag>
-          Dynamic_tds;
-  typedef CGAL::Triangulation_data_structure<CGAL::Dimension_tag<sdim> >
-          Static_tds;
-  typedef Static_tds::Face             Face;
-  typedef Static_tds::Facet            Facet;
-  typedef Static_tds::Vertex_handle    Vertex_handle;
-  typedef Static_tds::Full_cell_handle Full_cell_handle;
+  TDS  S(158);  // the argument is not taken into account. dimension is 7.
 
-  Static_tds  S(158);  // the argument is not taken into account.
-  Dynamic_tds D(ddim); // the argument is taken into account.
-
-  assert( sdim == S.ambient_dimension() );
-  assert( ddim == D.ambient_dimension() );
+  assert( 7 == S.ambient_dimension() );
   assert( -2 == S.current_dimension() );
   assert( S.is_valid() );
 
