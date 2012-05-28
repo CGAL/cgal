06994b3b353ba87eaa307ba2c56a18eddb8ccee3
Ílker Yaz <ilkeryaz@gmail.com>
2012-05-28 09:59:18 +0000

    Polyhedron changes.

---
+++ Polyhedron/demo/Polyhedron/Polyhedron_demo_mesh_segmentation_plugin.cpp
@@ -0,0 +1,70 @@
+#include "Polyhedron_demo_plugin_helper.h"
+#include "Polyhedron_demo_plugin_interface.h"
+
+#include "Scene_polyhedron_item.h"
+#include "Scene_polyhedron_with_color_item.h"
+#include "Polyhedron_type.h"
+#include <CGAL/Surface_mesh_segmentation.h>
+#include <QApplication>
+#include <QMainWindow>
+#include <QInputDialog>
+#include <QTime>
+#include <QDebug>
+
+class Polyhedron_demo_mesh_segmentation_plugin : 
+  public QObject,
+  public Polyhedron_demo_plugin_helper
+{
+  Q_OBJECT
+  Q_INTERFACES(Polyhedron_demo_plugin_interface)
+
+public:
+  // used by Polyhedron_demo_plugin_helper
+  QStringList actionsNames() const {
+    return QStringList() << "actionSegmentation";
+  }
+
+public slots:
+  void on_actionSegmentation_triggered();
+
+};
+
+void Polyhedron_demo_mesh_segmentation_plugin::on_actionSegmentation_triggered()
+{
+  const Scene_interface::Item_id index = scene->mainSelectionIndex();
+  qDebug("afadsf");
+  Scene_polyhedron_item* item = 
+    qobject_cast<Scene_polyhedron_item*>(scene->item(index));
+
+  if(item)
+  {
+    Polyhedron* pMesh = item->polyhedron();
+	
+	Scene_polyhedron_with_color_item* new_item = new Scene_polyhedron_with_color_item(pMesh);
+	std::vector<QColor> color_vector;
+
+	//int counter = 0;
+ //   for(Polyhedron::Facet_iterator it = pMesh->facets_begin(); it != pMesh->facets_end(); ++it)
+	//{
+	//	int color = (counter++) % 255;
+	//	color_vector.push_back(QColor(color,color,color));
+	//	std::cout << color << std::endl;
+	//}
+	
+	CGAL::Surface_mesh_segmentation<Polyhedron> segmentation(pMesh);	
+	for(CGAL::Surface_mesh_segmentation<Polyhedron>::Facet_iterator facet_it = segmentation.mesh->facets_begin(); 
+        facet_it != segmentation.mesh->facets_end(); ++facet_it)   
+    {
+		int color = (int) (255 * segmentation.sdf_values[facet_it]);
+		color_vector.push_back(QColor(color,color,color));
+	}
+	new_item->set_color_vector(color_vector);
+	scene->itemChanged(index);
+	scene->addItem(new_item);
+	
+  }
+}
+
+Q_EXPORT_PLUGIN2(Polyhedron_demo_mesh_segmentation_plugin, Polyhedron_demo_mesh_segmentation_plugin)
+
+#include "Polyhedron_demo_mesh_segmentation_plugin.moc"
---
+++ Polyhedron/demo/Polyhedron/Scene_polyhedron_with_color_item.h
@@ -0,0 +1,91 @@
+#ifndef SCENE_POLYHEDRON_WITH_COLOR_ITEM_H
+#define SCENE_POLYHEDRON_WITH_COLOR_ITEM_H
+
+#include "Scene_polyhedron_with_color_item_config.h"
+#include "Scene_item_with_display_list.h"
+#include "Polyhedron_type_fwd.h"
+#include <iostream>
+
+#include <set>
+#include <vector>
+
+#include <QColor>
+
+class QMenu;
+
+// This class represents a polyhedron in the OpenGL scene
+class SCENE_POLYHEDRON_WITH_COLOR_ITEM_EXPORT Scene_polyhedron_with_color_item 
+  : public Scene_item_with_display_list {
+  Q_OBJECT
+public:  
+  Scene_polyhedron_with_color_item();
+//   Scene_polyhedron_item(const Scene_polyhedron_item&);
+  Scene_polyhedron_with_color_item(const Polyhedron& p);
+  Scene_polyhedron_with_color_item(Polyhedron* const p);
+  ~Scene_polyhedron_with_color_item();
+
+  Scene_polyhedron_with_color_item* clone() const;
+  
+  // IO
+  bool load(std::istream& in);
+  bool save(std::ostream& out) const;
+
+  // Function for displaying meta-data of the item
+  virtual QString toolTip() const;
+
+  // Function to override the context menu
+  QMenu* contextMenu();
+  
+  // Indicate if rendering mode is supported
+  virtual bool supportsRenderingMode(RenderingMode) const { return true; }
+  // Points/Wireframe/Flat/Gouraud OpenGL drawing in a display list
+  virtual void direct_draw() const;
+  virtual void direct_draw_edges() const;
+  virtual void draw() const;
+  // Get wrapped polyhedron
+  Polyhedron*       polyhedron();
+  const Polyhedron* polyhedron() const;
+
+  // Get dimensions
+  bool isFinite() const { return true; }
+  bool isEmpty() const;
+  Bbox bbox() const;
+
+  void set_color_vector(std::vector<QColor>& colors);
+  
+public slots:
+  virtual void changed();
+  void show_only_feature_edges(bool);
+  void enable_facets_picking(bool);
+  void set_erase_next_picked_facet(bool);
+
+  void select(double orig_x,
+              double orig_y,
+              double orig_z,
+              double dir_x,
+              double dir_y,
+              double dir_z);
+
+signals:
+  void selected_vertex(void*);
+  void selected_facet(void*);
+
+private:
+  // Initialization
+  void init();
+  
+private:
+  Polyhedron* poly;
+
+private:
+  typedef Scene_item_with_display_list Base;
+  typedef std::vector<QColor> Color_vector;
+    
+  Color_vector colors_;
+
+  bool show_only_feature_edges_m;
+  bool facet_picking_m;
+  bool erase_next_picked_facet_m;
+}; // end class Scene_polyhedron_item
+
+#endif // SCENE_POLYHEDRON_WITH_COLOR_ITEM_H
