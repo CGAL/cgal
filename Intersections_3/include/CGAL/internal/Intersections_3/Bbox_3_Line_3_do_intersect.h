02754f17f9455e32916d362cf188996c49c4ed1d
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2012-06-15 15:27:17 +0000

    Revert previous commit for CGAL/internal/Intersections_3/Bbox_3_Line_3_do_intersect.h
    The code cannot be easily reused because a line does not store
    two points but the a, b, c, d coefficients.

--- Intersections_3/include/CGAL/internal/Intersections_3/Bbox_3_Line_3_do_intersect.h
+++ Intersections_3/include/CGAL/internal/Intersections_3/Bbox_3_Line_3_do_intersect.h
@@ -26,14 +26,106 @@
 #include <CGAL/Line_3.h>
 #include <CGAL/Bbox_3.h>
 
-#include <CGAL/internal/Intersections_3/Bbox_3_Segment_3_do_intersect.h>
-// for CGAL::internal::do_intersect_bbox_segment_aux
-
 // inspired from http://cag.csail.mit.edu/~amy/papers/box-jgt.pdf
 
 namespace CGAL {
 
 namespace internal {
+
+  template <typename FT>
+  inline
+  bool
+  bbox_line_do_intersect_aux(const FT& px, const FT& py, const FT& pz,
+                             const FT& vx, const FT& vy, const FT& vz,
+                             const FT& bxmin, const FT& bymin, const FT& bzmin,
+                             const FT& bxmax, const FT& bymax, const FT& bzmax)
+  {
+    // -----------------------------------
+    // treat x coord
+    // -----------------------------------
+    FT dmin, tmin, tmax;
+    if ( vx >= 0 )
+    {
+      tmin = bxmin - px;
+      tmax = bxmax - px;
+      dmin = vx;
+    }
+    else
+    {
+      tmin = px - bxmax;
+      tmax = px - bxmin;
+      dmin = -vx;
+    }
+
+    //if px is not in the x-slab
+    if ( dmin == FT(0) && (tmin > FT(0) || tmax < FT(0)) ) return false;
+
+    FT dmax = dmin;
+
+    // -----------------------------------
+    // treat y coord
+    // -----------------------------------
+    FT d_, tmin_, tmax_;
+    if ( vy >= 0 )
+    {
+      tmin_ = bymin - py;
+      tmax_ = bymax - py;
+      d_ = vy;
+    }
+    else
+    {
+      tmin_ = py - bymax;
+      tmax_ = py - bymin;
+      d_ = -vy;
+    }
+    
+    
+    
+    
+    if ( d_ == FT(0) ){
+      //if py is not in the y-slab
+      if( (tmin_ > FT(0) || tmax_ < FT(0)) ) return false;
+    }
+    else
+      if ( (dmin*tmax_) < (d_*tmin) || (dmax*tmin_) > (d_*tmax) )
+        return false;
+
+    if( (dmin*tmin_) > (d_*tmin) )
+    {
+      tmin = tmin_;
+      dmin = d_;
+    }
+
+    if( (dmax*tmax_) < (d_*tmax) )
+    {
+      tmax = tmax_;
+      dmax = d_;
+    }
+
+    // -----------------------------------
+    // treat z coord
+    // -----------------------------------
+    if ( vz >= 0 )
+    {
+      tmin_ = bzmin - pz;
+      tmax_ = bzmax - pz;
+      d_ = vz;
+    }
+    else
+    {
+      tmin_ = pz - bzmax;
+      tmax_ = pz - bzmin;
+      d_ = -vz;
+    }
+    
+    //if pz is not in the z-slab
+    //if ( d_ == FT(0) && (tmin_ > FT(0) || tmax_ < FT(0)) ) return false;
+    //The previous line is not needed as either dmin or d_ are not 0 
+    //(otherwise the direction of the line would be null). 
+    // The following is equivalent to the in z-slab test if d_=0.
+
+    return ( (dmin*tmax_) >= (d_*tmin) && (dmax*tmin_) <= (d_*tmax) );
+  }
 
   template <class K>
   bool do_intersect(const typename K::Line_3& line,
@@ -47,16 +139,11 @@ namespace internal {
     const Point_3& point = line.point();
     const Vector_3& v = line.to_vector();
 
-    return do_intersect_bbox_segment_aux
-      <FT,
-       false, // not bounded at t=0 
-       false, // not bounded at t=1
-       false> // do not use static filters
-      (
-       point.x(), point.y(), point.z(),
-       v.x(), v.y(), v.z(),
-       bbox
-       );
+    return bbox_line_do_intersect_aux(
+                         point.x(), point.y(), point.z(),
+                         v.x(), v.y(), v.z(),
+                         FT(bbox.xmin()), FT(bbox.ymin()), FT(bbox.zmin()),
+                         FT(bbox.xmax()), FT(bbox.ymax()), FT(bbox.zmax()) );
   }
 
 } // namespace internal
