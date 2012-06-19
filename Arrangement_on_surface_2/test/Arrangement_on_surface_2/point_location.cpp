eaa10a38ca97bd639e75cd66f88ef1e5b42c9b7d
Efi Fogel <efif@post.tau.ac.il>
2012-06-19 16:26:24 +0000

    updated

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/point_location.cpp
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/point_location.cpp
@@ -25,33 +25,38 @@
 #include <CGAL/Arr_segment_traits_2.h>
 #elif TEST_TRAITS == CIRCLE_SEGMENT_TRAITS
 #include <CGAL/Arr_circle_segment_traits_2.h>
+#elif TEST_TRAITS == LINEAR_TRAITS
+#include <CGAL/Arr_linear_traits_2.h>
 #endif
 
 #if TEST_TRAITS == SEGMENT_TRAITS
-typedef CGAL::Arr_segment_traits_2<Kernel>              Traits;
+typedef CGAL::Arr_segment_traits_2<Kernel>          Traits;
 #define TRAITS_TYPE "point_location_Segments"
 
 #elif TEST_TRAITS == CIRCLE_SEGMENT_TRAITS
-typedef CGAL::Arr_circle_segment_traits_2<Kernel>       Traits;
-typedef CGAL::Cartesian<Number_type>                    Rat_kernel;
-typedef Rat_kernel::FT                                  Rat_nt;
-typedef Rat_kernel::Circle_2                            Circle_2;
-typedef Rat_kernel::Line_2                              Line_2;
-typedef Rat_kernel::Segment_2                           Segment_2;
-typedef Rat_kernel::Point_2                             Rat_point_2;
+typedef CGAL::Arr_circle_segment_traits_2<Kernel>   Traits;
+typedef CGAL::Cartesian<Number_type>                Rat_kernel;
+typedef Rat_kernel::FT                              Rat_nt;
+typedef Rat_kernel::Circle_2                        Circle_2;
+typedef Rat_kernel::Line_2                          Line_2;
+typedef Rat_kernel::Segment_2                       Segment_2;
+typedef Rat_kernel::Point_2                         Rat_point_2;
 #define TRAITS_TYPE "point_location_circle Segments"
+
+#elif TEST_TRAITS == LINEAR_TRAITS
+typedef CGAL::Arr_linear_traits_2<Kernel>           Traits;
 #endif
 
-typedef Traits::Point_2                                 Point_2;
-typedef Traits::Curve_2                                 Curve_2;
-typedef Traits::X_monotone_curve_2                      X_monotone_curve_2;
+typedef Traits::Point_2                             Point_2;
+typedef Traits::Curve_2                             Curve_2;
+typedef Traits::X_monotone_curve_2                  X_monotone_curve_2;
 
 #include "IO_test.h"
 
-typedef CGAL::Arrangement_2<Traits>                     Arrangement_2;
-typedef Arrangement_2::Halfedge_handle                  Halfedge_handle;
-typedef Arrangement_2::Edge_const_iterator              Edge_const_iterator;
-typedef Arrangement_2::Vertex_const_iterator            Vertex_const_iterator;
+typedef CGAL::Arrangement_2<Traits>                 Arrangement_2;
+typedef Arrangement_2::Halfedge_handle              Halfedge_handle;
+typedef Arrangement_2::Edge_const_iterator          Edge_const_iterator;
+typedef Arrangement_2::Vertex_const_iterator        Vertex_const_iterator;
 
 typedef CGAL::Arr_naive_point_location<Arrangement_2>     
                                                     Naive_point_location;
@@ -100,66 +105,70 @@ typedef Objects_vector::iterator                          Object_iterator;
 
 // ===> Change the number of point-location startegies
 //      when a new point location is added. <===
-#define NUM_OF_POINT_LOCATION_STRATEGIES 10
+#define MAX_NUM_POINT_LOCATION_STRATEGIES 10
 
 /*! */
 int check_point_location(Arrangement_2 &arr, Points_vector &query_points)
 {
   //init - all point locations
-  CGAL::Timer            timer;
+  CGAL::Timer timer;
 
-  Naive_point_location            naive_pl (arr);                 // 0
+  Naive_point_location naive_pl(arr);                           // 0
+  Simple_point_location simple_pl(arr);                         // 1
+  Walk_point_location walk_pl(arr);                             // 2
 
-  Simple_point_location           simple_pl (arr);                // 1
-  Walk_point_location             walk_pl (arr);                  // 2
-
-  timer.reset(); timer.start();
-  Lm_point_location               lm_pl (arr);                    // 3
-  timer.stop(); 
-  std::cout << "Lm (vert) construction took " << timer.time() <<std::endl;
+#if (TEST_TRAITS == SEGMENT_TRAITS) || (TEST_TRAITS == LINEAR_TRAITS)
 
   timer.reset(); timer.start();
-  Random_lm_generator             random_g(arr);    
-  Lm_random_point_location        random_lm_pl (arr, &random_g);  // 4
+  Lm_point_location lm_pl(arr);                                 // 3
   timer.stop(); 
-  std::cout << "Random lm construction took " << timer.time() <<std::endl;
+  std::cout << "Lm (vert) construction took " << timer.time() << std::endl;
 
   timer.reset(); timer.start();
-  Grid_lm_generator               grid_g(arr);      
-  Lm_grid_point_location          grid_lm_pl (arr, &grid_g);      // 5
+  Random_lm_generator random_g(arr);    
+  Lm_random_point_location random_lm_pl(arr, &random_g);        // 4
   timer.stop(); 
-  std::cout << "Grid lm construction took " << timer.time() <<std::endl;
+  std::cout << "Random lm construction took " << timer.time() << std::endl;
 
   timer.reset(); timer.start();
-  Halton_lm_generator             halton_g(arr);
-  Lm_halton_point_location        halton_lm_pl (arr, &halton_g);  // 6
+  Grid_lm_generator grid_g(arr);      
+  Lm_grid_point_location grid_lm_pl(arr, &grid_g);              // 5
   timer.stop(); 
-  std::cout << "Halton lm construction took " << timer.time() <<std::endl;
+  std::cout << "Grid lm construction took " << timer.time() << std::endl;
 
   timer.reset(); timer.start();
-  Middle_edges_generator         middle_edges_g(arr);
-  Lm_middle_edges_point_location middle_edges_lm_pl(arr, &middle_edges_g);  // 7
+  Halton_lm_generator halton_g(arr);
+  Lm_halton_point_location halton_lm_pl(arr, &halton_g);        // 6
   timer.stop(); 
-  std::cout << "Middle edges lm construction took " << timer.time() <<std::endl;
+  std::cout << "Halton lm construction took " << timer.time() << std::endl;
+
+  timer.reset(); timer.start();
+  Middle_edges_generator middle_edges_g(arr);
+  Lm_middle_edges_point_location middle_edges_lm_pl(arr, &middle_edges_g); // 7
+  timer.stop(); 
+  std::cout << "Middle edges lm construction took " << timer.time()
+            << std::endl;
 
   Specified_points_generator::Points_set points;
-  //points.push_back(Point_2(1,1));
-  //points.push_back(Point_2(2,2));
+  //points.push_back(Point_2(1, 1));
+  //points.push_back(Point_2(2, 2));
   timer.reset(); timer.start();
   //Specified_points_generator specified_points_g(arr,points);
   Specified_points_generator specified_points_g(arr);
   Lm_specified_points_point_location
-    specified_points_lm_pl(arr, &specified_points_g);  // 8
+    specified_points_lm_pl(arr, &specified_points_g);                   // 8
   timer.stop(); 
   std::cout << "Specified_points lm construction took "
             << timer.time() << std::endl;
 
   // timer.reset(); timer.start();
-  // Lm_triangulation_point_location        triangulation_lm_pl (arr);  // 9
+  // Lm_triangulation_point_location triangulation_lm_pl(arr);          // 9
   // timer.stop(); 
   // std::cout << "Triangulation lm construction took "
   //           << timer.time() << std::endl;
 
+#endif
+  
   timer.reset(); timer.start();
   Trapezoid_ric_point_location trapezoid_ric_pl(arr);                   // 9
   timer.stop(); 
@@ -169,147 +178,163 @@ int check_point_location(Arrangement_2 &arr, Points_vector &query_points)
 
   // ===> Add new point location instance here. <===
 
-  CGAL::Object                    obj;
-  Objects_vector                  objs[NUM_OF_POINT_LOCATION_STRATEGIES];
-  Object_iterator                 ob_iter[NUM_OF_POINT_LOCATION_STRATEGIES];
+  Objects_vector objs[MAX_NUM_POINT_LOCATION_STRATEGIES];
   Arrangement_2::Vertex_const_handle    vh_ref, vh_curr;
   Arrangement_2::Halfedge_const_handle  hh_ref, hh_curr;
   Arrangement_2::Face_const_handle      fh_ref, fh_curr;
   
-  Point_2               q;
-  Point_iterator        piter;
+  // LOCATE the points in the list using all PL strategies
 
-  //LOCATE the points in the list using all PL strategies
-
-  //std::cout << "Time in seconds" <<std::endl; ;
+  // std::cout << "Time in seconds" << std::endl;
   std::cout << std::endl;
-  timer.reset(); 
-  timer.start(); //START
+
+  Point_iterator piter;
+  unsigned int pl_index = 0;
+
+  // Naive
+  timer.reset(); timer.start();
   for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = naive_pl.locate (q);
-    objs[0].push_back(obj);
+    Point_2 q = (*piter);
+    CGAL::Object obj = naive_pl.locate(q);
+    objs[pl_index].push_back(obj);
   }
-  timer.stop(); ///END
+  timer.stop();
+  ++pl_index;
   std::cout << "Naive location took " << timer.time() << std::endl;
 
-  timer.reset(); 
-  timer.start(); //START
+  // Simple
+  timer.reset(); timer.start();
   for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = simple_pl.locate (q);
-    objs[1].push_back(obj);
+    Point_2 q = (*piter);
+    CGAL::Object obj = simple_pl.locate(q);
+    objs[pl_index].push_back(obj);
   }
-  timer.stop(); ///END
+  timer.stop();
+  ++pl_index;
   std::cout << "Simple location took " << timer.time() << std::endl;
 
-  timer.reset(); 
-  timer.start(); //START
+  // Walk
+  timer.reset(); timer.start();
   for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = walk_pl.locate (q);
-    objs[2].push_back(obj);
+    Point_2 q = (*piter);
+    CGAL::Object obj = walk_pl.locate(q);
+    objs[pl_index].push_back(obj);
   }
-  timer.stop(); ///END
+  timer.stop();
+  ++pl_index;
   std::cout << "Walk location took " << timer.time() << std::endl;
 
-  timer.reset(); 
-  timer.start(); //START
-  for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = lm_pl.locate (q);
-    objs[3].push_back(obj);
-  }
-  timer.stop(); ///END
-  std::cout << "Landmarks (vertices) location took " << timer.time() <<std::endl;
+#if (TEST_TRAITS == SEGMENT_TRAITS) || (TEST_TRAITS == LINEAR_TRAITS)
 
-  timer.reset(); 
-  timer.start(); //START
+  // Landmarks (vertices)
+  timer.reset(); timer.start();
   for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = random_lm_pl.locate (q);
-    objs[4].push_back(obj);
+    Point_2 q = (*piter);
+    CGAL::Object obj = lm_pl.locate(q);
+    objs[pl_index].push_back(obj);
   }
-  timer.stop(); ///END
+  timer.stop();
+  ++pl_index;
+  std::cout << "Landmarks (vertices) location took " << timer.time()
+            << std::endl;
+
+  // Landmarks random
+  timer.reset(); timer.start();
+  for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
+    Point_2 q = (*piter);
+    CGAL::Object obj = random_lm_pl.locate(q);
+    objs[pl_index].push_back(obj);
+  }
+  timer.stop();
+  ++pl_index;
   std::cout << "Random LM location took " << timer.time() << std::endl;
 
-  timer.reset(); 
-  timer.start(); //START
+  // Landmarks grid
+  timer.reset(); timer.start();
   for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = grid_lm_pl.locate (q);
-    objs[5].push_back(obj);
+    Point_2 q = (*piter);
+    CGAL::Object obj = grid_lm_pl.locate(q);
+    objs[pl_index].push_back(obj);
   }
-  timer.stop(); ///END
+  timer.stop();
+  ++pl_index;
   std::cout << "Grid LM location took " << timer.time() << std::endl;
 
-
-  timer.reset(); 
-  timer.start(); //START
+  // Landmarks Halton
+  timer.reset(); timer.start();
   for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = halton_lm_pl.locate (q);
-    objs[6].push_back(obj);
+    Point_2 q = (*piter);
+    CGAL::Object obj = halton_lm_pl.locate(q);
+    objs[pl_index].push_back(obj);
   }
-  timer.stop(); ///END
+  timer.stop();
+  ++pl_index;
   std::cout << "Halton LM location took " << timer.time() << std::endl;
 
-  timer.reset(); 
-  timer.start(); //START
+  // Landmarks Middle edges
+  timer.reset(); timer.start();
   for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = middle_edges_lm_pl.locate (q);
-    objs[7].push_back(obj);
+    Point_2 q = (*piter);
+    CGAL::Object obj = middle_edges_lm_pl.locate(q);
+    objs[pl_index].push_back(obj);
   }
-  timer.stop(); ///END
+  timer.stop();
+  ++pl_index;
   std::cout << "Middle edges LM location took " << timer.time() <<std::endl;
 
-  timer.reset(); 
-  timer.start(); //START
+  // Landmarks specified points
+  timer.reset(); timer.start();
   for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = specified_points_lm_pl.locate (q);
-    objs[8].push_back(obj);
+    Point_2 q = (*piter);
+    CGAL::Object obj = specified_points_lm_pl.locate(q);
+    objs[pl_index].push_back(obj);
   }
-  timer.stop(); ///END
-  std::cout << "Specified points LM location took " << timer.time() << std::endl;
+  timer.stop();
+  ++pl_index;
+  std::cout << "Specified points LM location took " << timer.time()
+            << std::endl;
 
-  // timer.reset(); 
-  // timer.start(); //START
+  // Triangulation
+  // timer.reset(); timer.start();
   // for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-  //   q = (*piter);
-  //   obj = triangulation_lm_pl.locate(q);
-  //   objs[8].push_back(obj);
+  //   Point_2 q = (*piter);
+  //   CGAL::Object obj = triangulation_lm_pl.locate(q);
+  //   objs[pl_index].push_back(obj);
   // }
-  // timer.stop(); ///END
+  // timer.stop();
+  // ++pl_index;
   // std::cout << "Triangulation LM location took " << timer.time() << std::endl;
 
-  timer.reset(); 
-  timer.start(); //START
+#endif
+
+  // Trapezoidal RIC
+  timer.reset(); timer.start(); //START
   for (piter = query_points.begin(); piter != query_points.end(); ++piter) {
-    q = (*piter);
-    obj = trapezoid_ric_pl.locate(q);
-    objs[9].push_back(obj);
+    Point_2 q = (*piter);
+    CGAL::Object obj = trapezoid_ric_pl.locate(q);
+    objs[pl_index].push_back(obj);
   }
   timer.stop(); ///END
-  std::cout << "Trapezoid RIC location took " << timer.time() << std::endl;
+  ++pl_index;
+  std::cout << "Trapezoidal RIC location took " << timer.time() << std::endl;
   
   std::cout << std::endl;
 
   // ===> Add a call to operate the the new point location. <===
 
   //END LOCATION 
-  int pls_num = NUM_OF_POINT_LOCATION_STRATEGIES;
-  int pl_index;
+  unsigned int pls_num = pl_index;
+  std::cout << "Number of strategies is " << pl_index << std::endl;
   int result = 0;
 
   //init all obejct iterators
+  Object_iterator ob_iter[MAX_NUM_POINT_LOCATION_STRATEGIES];
   for (pl_index = 0; pl_index < pls_num; ++pl_index)
     ob_iter[pl_index] = objs[pl_index].begin();
 
   //get size of objects
   unsigned int size = objs[0].size();
-  //std::cout <<"size is "<< size << std::endl;
+  std::cout << "size is " << size << std::endl;
 
   for (pl_index = 0; pl_index < pls_num; ++pl_index) {
     if (size != objs[pl_index].size()) {
@@ -355,12 +380,13 @@ int check_point_location(Arrangement_2 &arr, Points_vector &query_points)
 
     //assign object to a halfedge
     else if (CGAL::assign (hh_ref, ob_iter[0][qi])) {
-      std::cout << "Halfedge: "<< hh_ref->curve() << std::endl;
+      std::cout << "Halfedge: " << hh_ref->curve() << std::endl;
       for (int pl_index = 1; pl_index < pls_num; ++pl_index) {
         if (! CGAL::assign(hh_curr, ob_iter[pl_index][qi])) {
           std::cout << "Error in point location number " << pl_index;
           if (CGAL::assign(fh_curr, ob_iter[pl_index][qi])) {
-            std::cout << ", a face returned instead of an halfedge" << std::endl;
+            std::cout << ", a face returned instead of an halfedge"
+                      << std::endl;
           }
           else if (CGAL::assign(hh_curr, ob_iter[pl_index][qi])) {
             std::cout << ", a vertex returned instead of an halfedge"
@@ -383,7 +409,7 @@ int check_point_location(Arrangement_2 &arr, Points_vector &query_points)
 
     //assign object to a vertex
     else if (CGAL::assign(vh_ref, ob_iter[0][qi])) {
-      for (int pl_index=1; pl_index<pls_num; ++pl_index) {
+      for (int pl_index = 1; pl_index < pls_num; ++pl_index) {
         if (! CGAL::assign(vh_curr, ob_iter[pl_index][qi])) {
           std::cout << "Error in point location number " << pl_index;
           if (CGAL::assign(fh_curr, ob_iter[pl_index][qi])) {
@@ -462,7 +488,7 @@ bool test(const char* curves_filename, const char* points_filename)
   //read curves and insert them into the arrangement 
   Curves_vector curves;
   std::cout << "curves_filename: " << curves_filename << std::endl;
-  read_curves(curves_filename, curves);  
+  read_curves(curves_filename, curves);
   std::cout << "curves.size(): " << curves.size() << std::endl;
   
   //insert all curves into the arrangement
