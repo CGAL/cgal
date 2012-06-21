b5ac8afe11f7e3cab62ad6650f4a23aa458db82a
Sébastien Loriot <sloriot.ml@gmail.com>
2012-06-21 12:04:22 +0000

    add comment

--- Installation/cmake/modules/CGAL_Macros.cmake
+++ Installation/cmake/modules/CGAL_Macros.cmake
@@ -390,7 +390,7 @@ if( NOT CGAL_MACROS_FILE_INCLUDED )
        if ( WITH_${lib} ) 
 
          set (vlib ${CGAL_EXT_LIB_${lib}_PREFIX} )
-         
+         #the next 'if' is needed to avoid ${vlib} config variables to be overidden in case of a local configuration change
          file( APPEND ${CMAKE_BINARY_DIR}/CGALConfig.cmake "if (NOT CGAL_IGNORE_PRECONFIGURED_${vlib})\n")
          file( APPEND ${CMAKE_BINARY_DIR}/CGALConfig.cmake "  set( CGAL_EXT_LIB_${lib}_PREFIX           \"${CGAL_EXT_LIB_${lib}_PREFIX}\" )\n")
          file( APPEND ${CMAKE_BINARY_DIR}/CGALConfig.cmake "  set( ${vlib}_FOUND           \"${${vlib}_FOUND}\" )\n")
@@ -400,6 +400,7 @@ if( NOT CGAL_MACROS_FILE_INCLUDED )
          file( APPEND ${CMAKE_BINARY_DIR}/CGALConfig.cmake "  set( ${vlib}_DEFINITIONS     \"${${vlib}_DEFINITIONS}\" )\n")
          file( APPEND ${CMAKE_BINARY_DIR}/CGALConfig.cmake "endif()\n\n")
 
+         #the next 'if' is needed to avoid ${vlib} config variables to be overidden in case of a local configuration change
          file( APPEND ${CMAKE_BINARY_DIR}/config/CGALConfig.cmake "if (NOT CGAL_IGNORE_PRECONFIGURED_${vlib})\n")
          file( APPEND ${CMAKE_BINARY_DIR}/config/CGALConfig.cmake "  set( CGAL_EXT_LIB_${lib}_PREFIX           \"${CGAL_EXT_LIB_${lib}_PREFIX}\" )\n")
          file( APPEND ${CMAKE_BINARY_DIR}/config/CGALConfig.cmake "  set( ${vlib}_FOUND           \"${${vlib}_FOUND}\")\n")
