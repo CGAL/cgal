bfe3c6032d87951e9d0a61c35d508070dcba42b5
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2012-07-05 13:49:48 +0000

    Replace assert with CGAL_assertion

--- Combinatorial_map/test/Combinatorial_map/Combinatorial_map_3_test.h
+++ Combinatorial_map/test/Combinatorial_map/Combinatorial_map_3_test.h
@@ -1003,7 +1003,7 @@ template<class Map>
   map.template sew<3>(d1,d2);   
   d3 = d1->beta(2);
   d4 = map.beta(d1, 1,3,1,2);
-  assert(d4==d1->beta(1)->beta(3)->beta(1)->beta(2));
+  CGAL_assertion(d4==d1->beta(1)->beta(3)->beta(1)->beta(2));
   map.display_characteristics(cout) << ", valid=" << map.is_valid() << endl;
   cout << "remove facet4: " << flush; CGAL::remove_cell<Map,2>(map,d1);
   map.display_characteristics(cout) << ", valid=" << map.is_valid() << endl;
