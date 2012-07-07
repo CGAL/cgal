d9eb0e8ece792af47d99b52e4846f29fbab37416
Eric Berberich <eric.berberich@cgal.org>
2012-07-07 08:17:07 +0000

    Intel compiler does not like typename friends
    error: friend class name may not be introduced with "typename"
    So reverted change

--- Arrangement_on_surface_2/include/CGAL/Curved_kernel_via_analysis_2/Arc_2.h
+++ Arrangement_on_surface_2/include/CGAL/Curved_kernel_via_analysis_2/Arc_2.h
@@ -3234,7 +3234,7 @@ public:
         return  is_equal(arc2);
     }
 
-#if defined(_MSC_VER) || defined(__clang__) || defined(__INTEL_COMPILER)
+#if defined(_MSC_VER) || defined(__clang__)
     // befriending the kernel point
     friend typename Curved_kernel_via_analysis_2::Point_2;
 
