0d151cefeef8612e401b0fb1be5889f16eebccab
Efi Fogel <efif@post.tau.ac.il>
2012-07-18 10:36:19 +0000

    updated

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_spherical_removal.cmd
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_spherical_removal.cmd
@@ -1,2 +1,15 @@
 data/test_spherical_remove/test01.txt
 data/test_spherical_remove/test02.txt
+data/test_spherical_remove/test03.txt
+data/test_spherical_remove/test04.txt
+data/test_spherical_remove/test05.txt
+data/test_spherical_remove/test06.txt
+data/test_spherical_remove/test07.txt
+data/test_spherical_remove/test08.txt
+data/test_spherical_remove/test09.txt
+data/test_spherical_remove/test10.txt
+data/test_spherical_remove/test11.txt
+data/test_spherical_remove/test12.txt
+data/test_spherical_remove/test13.txt
+data/test_spherical_remove/test14.txt
+data/test_spherical_remove/test15.txt
