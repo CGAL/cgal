b1dd56a5a3e38234fb3708544d0e023785851c4b
Jane Tournois <Jane.Tournois@sophia.inria.fr>
2012-07-23 14:15:23 +0000

    Restructure to avoid calling the area of faces three times for a tetrahedron
    r66607 from Mesh_3-experimental-GF

--- Mesh_3/include/CGAL/Mesh_3/min_dihedral_angle.h
+++ Mesh_3/include/CGAL/Mesh_3/min_dihedral_angle.h
@@ -27,30 +27,8 @@
 namespace CGAL {
 
 namespace Mesh_3 {
-  
-  namespace details {
-    
-    template <typename K>
-    typename K::FT
-    min_dihedral_angle_aux_compute_quotient(const typename K::Point_3& p0,
-                                            const typename K::Point_3& p1,
-                                            const typename K::Point_3& p2,
-                                            const typename K::Point_3& p3,
-                                            K k = K())
-    {
-      typename K::Compute_area_3 area = 
-      k.compute_area_3_object();
-      typename K::Compute_squared_distance_3 sq_distance = 
-      k.compute_squared_distance_3_object();
-      
-      return CGAL::sqrt(sq_distance(p0, p1))
-        / area(p0, p1, p3)
-        / area(p0, p1, p2);
-    }
-    
-  } // end namespace details;
-  
-  
+ 
+
 template <typename K>
 typename K::FT
 minimum_dihedral_angle(
@@ -61,29 +39,37 @@ minimum_dihedral_angle(
      K k = K())
 {
   typedef typename K::FT FT;
+
+  typename K::Compute_squared_distance_3 sq_distance = 
+    k.compute_squared_distance_3_object();
   typename K::Compute_volume_3 volume = 
     k.compute_volume_3_object();
+  typename K::Compute_area_3 area = 
+    k.compute_area_3_object();
 
-  using details::min_dihedral_angle_aux_compute_quotient;
+  FT a_012 = area(p0,p1,p2);
+  FT a_013 = area(p0,p1,p3);
+  FT a_123 = area(p1,p2,p3);
+  FT a_023 = area(p0,p2,p3);
 
   FT min_quotient = 
-    min_dihedral_angle_aux_compute_quotient(p0, p1, p2, p3, k);
+    CGAL::sqrt(sq_distance(p0, p1)) / a_012 / a_013;
 
   min_quotient = 
-    (std::min)(min_quotient,
-       min_dihedral_angle_aux_compute_quotient(p0, p2, p1, p3, k));
+    (CGAL::min)(min_quotient,
+                CGAL::sqrt(sq_distance(p0, p2)) / a_012 / a_023);
   min_quotient = 
-    (std::min)(min_quotient,
-       min_dihedral_angle_aux_compute_quotient(p0, p3, p1, p2, k));
+    (CGAL::min)(min_quotient,
+               CGAL::sqrt(sq_distance(p0, p3)) / a_013 / a_023);
   min_quotient = 
-    (std::min)(min_quotient,
-       min_dihedral_angle_aux_compute_quotient(p1, p2, p0, p3, k));
+    (CGAL::min)(min_quotient,
+               CGAL::sqrt(sq_distance(p1, p2)) / a_012 / a_123);
   min_quotient = 
-    (std::min)(min_quotient,
-       min_dihedral_angle_aux_compute_quotient(p1, p3, p0, p2, k));
+    (CGAL::min)(min_quotient,
+               CGAL::sqrt(sq_distance(p1, p3)) / a_013 / a_123);
   min_quotient = 
-    (std::min)(min_quotient,
-       min_dihedral_angle_aux_compute_quotient(p2, p3, p0, p1, k));
+    (CGAL::min)(min_quotient,
+               CGAL::sqrt(sq_distance(p2, p3)) / a_023 / a_123);
 
   const FT result (std::asin( FT(1.5) * volume(p0, p1, p2, p3) * min_quotient )
     * FT(180) / FT(CGAL_PI));
