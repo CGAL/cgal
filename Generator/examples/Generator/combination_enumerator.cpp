ece428cde4106697bd0a4f60401b398bcac5388d
Samuel Hornus <Samuel.Hornus@sophia.inria.fr>
2012-07-26 11:56:34 +0000

    2nd iteration. Remarks from Sebastien and Laurent

--- Generator/examples/Generator/combination_enumerator.cpp
+++ Generator/examples/Generator/combination_enumerator.cpp
@@ -4,17 +4,19 @@ using namespace std;
 int main()
 {
     unsigned int n(0);
-    const int K(3), First(10), Last(20);
-    cout << "Taking " << K << " distinct integers in the range [" <<
-            First << ", " << Last << "]:";
+    const int k(3), first(10), last(15);
+    cout << "Taking " << k << " distinct integers in the range [" <<
+            first << ", " << last << "]:";
 
-    CGAL::Combination_enumerator<int> combi(K, First, Last+1);
-    while( ! combi.finished() )
-    {
-        cout << " (";
-        for(int i = 0; i < K - 1; ++i)
-            cout << combi[i] << ' ';
-        cout << combi[K-1] << ')';
+    CGAL::Combination_enumerator<int> combi(k, first, last + 1);
+    while( ! combi.finished() ) {
+        cout << " {";
+        for(int i = 0; i < k; ++i) {
+            cout << combi[i];
+            if( i < k - 1 )
+                cout << ' ';
+        }
+        cout << '}';
         ++n;
         ++combi;
     }
