285d3264a3a0d0d8f98851310838437693cb1f54
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2012-07-28 18:02:03 +0300

    fix vertex conflict predicate PPPS
    The following is a problematic predicate:
    debug: Vertex_conflict (pqrt)=
    (p [50;50] [100;100])
    (p [0;0] [0;0])
    (p [200;200] [-100;-100])
    (s [0;0] [100;100] [0;0] [0;0])
    returns -1
    
    It should in fact return 0. A similar change has to be made
    in the ring variant of Voronoi vertex.

--- include/CGAL/Segment_Delaunay_graph_Linf_2/Voronoi_vertex_sqrt_field_new_C2.h
+++ include/CGAL/Segment_Delaunay_graph_Linf_2/Voronoi_vertex_sqrt_field_new_C2.h
@@ -1383,9 +1383,10 @@ private:
     std::cout << "debug incircle_xxxs numendpts_of_t= " 
       << numendpts_of_t << std::endl;
 
-    if ((numendpts_of_t > 0) and (numpts_in_pqr < 3)) {
+    if (numendpts_of_t > 0) {
       bool is_t_horizontal = t.segment().is_horizontal();
       bool is_t_vertical   = t.segment().is_vertical();
+
       if (is_t_horizontal or is_t_vertical) {
         CGAL_assertion(numendpts_of_t == 1);
 
@@ -1397,23 +1398,30 @@ private:
           endp = t.target_site();
         }
 
-        Site_2 other;
 
+        // numothers will be the number of segments
+        // in {p,q,r} that have endp as an endpoint
         unsigned int numothers = 0;
 
-        if ((not is_p_point) and is_endpoint_of(endp, p)) {
-          numothers++;
-          other = p;
-        }
+        // a possible segment in {p,q,r} which has endpoint endp
+        Site_2 other;
 
-        if ((not is_q_point) and is_endpoint_of(endp, q)) {
-          numothers++;
-          other = q;
-        }
-               
-        if ((not is_r_point) and is_endpoint_of(endp, r)) {
-          numothers++;
-          other = r;
+        // if there is a segment in {p,q,r}, try its endpoints
+        if (numpts_in_pqr < 3) {
+          if ((not is_p_point) and is_endpoint_of(endp, p)) {
+            numothers++;
+            other = p;
+          }
+
+          if ((not is_q_point) and is_endpoint_of(endp, q)) {
+            numothers++;
+            other = q;
+          }
+
+          if ((not is_r_point) and is_endpoint_of(endp, r)) {
+            numothers++;
+            other = r;
+          }
         }
 
         CGAL_assertion(numothers < 2);
@@ -1426,9 +1434,25 @@ private:
               (is_t_vertical and is_other_vertical)       ) {
             return POSITIVE;
           }
-        }
+        } else {
+          CGAL_assertion(numothers == 0);
+          compute_vv(p, q, r, type);
+
+          Comparison_result ptcmpxve = 
+            CGAL::compare(vv.x(), endp.point().x());
+          Comparison_result ptcmpyve = 
+            CGAL::compare(vv.y(), endp.point().y());
+
+          std::cout << "debug vv = " << vv << std::endl; 
+
+          if ( ( (ptcmpxve == EQUAL) and is_t_horizontal ) or
+               ( (ptcmpyve == EQUAL) and is_t_vertical   )    ) {
+            return ZERO;
+          }
+
+        } // end of case numothers == 0
       }  // endif (is_t_horizontal or is_t_vertical)
-    } // endif ((numendpts_of_t > 0) and (numpts_in_pqr < 3))
+    } // endif ((numendpts_of_t > 0) 
 
 
     Line_2 l = compute_supporting_line(t.supporting_site());
