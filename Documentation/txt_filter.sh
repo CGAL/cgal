4a24b439f45af8b4b958e54aa5e949cd10a0b3f4
Philipp Möller <Philipp.Moeller@geometryfactory.com>
2012-08-08 09:21:40 +0000

    Layouting and package building changes
    Fiddle with the master doxygen file, improve package overview
    building, add a set of packages to the doxyassist XML, fix up
    straggling packages that didn't have those changes

--- Documentation/txt_filter.sh
+++ Documentation/txt_filter.sh
@@ -4,7 +4,7 @@ while read -r line
 do     
     pkg=$(echo ${line} | sed -n '/\\package_listing{[^}]*}/p' | sed 's/\\package_listing{\([^}]*\)}/\1/')
     if [ -n "${pkg}" ]; then
-        cat "../${pkg}/doc/${pkg}/PackageDescription.txt" | sed -n '/PkgDescriptionBegin/,/PkgDescriptionEnd/p'
+        cat "../${pkg}/doc/${pkg}/PackageDescription.txt" | sed -n '/PkgDescriptionBegin/,/PkgDescriptionEnd/p' | sed "s/\(\\PkgDescriptionBegin{[^}]*\)}/\1,${pkg}}/"
     else
         echo -E "${line}"
     fi
