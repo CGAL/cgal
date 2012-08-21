09f0c3abfeb005fea96d2215c3a7f3ddabb2e3dc
Philipp Möller <Philipp.Moeller@geometryfactory.com>
2012-08-21 12:34:18 +0000

    Add possiblity for packages in the package overview where top level and subdirectory diverge.

--- Documentation/pkglist_filter.sh
+++ Documentation/pkglist_filter.sh
@@ -4,7 +4,13 @@ while read -r line
 do     
     pkg=$(echo ${line} | sed -n '/\\package_listing{[^}]*}/p' | sed 's/\\package_listing{\([^}]*\)}/\1/')
     if [ -n "${pkg}" ]; then
-        cat "../${pkg}/doc/${pkg}/PackageDescription.txt" | sed -n '/PkgDescriptionBegin/,/PkgDescriptionEnd/p' | sed "s/\(\\PkgDescriptionBegin{[^}]*\)}/\1,${pkg}}/"
+        top_level=${pkg%/*}
+        lower_level=${pkg##*/}
+        if [ -n "${top_level}" ]; then
+            cat "../${top_level}/doc/${lower_level}/PackageDescription.txt" | sed -n '/PkgDescriptionBegin/,/PkgDescriptionEnd/p' | sed "s/\(\\PkgDescriptionBegin{[^}]*\)}/\1,${lower_level}}/"
+        else
+            cat "../${pkg}/doc/${pkg}/PackageDescription.txt" | sed -n '/PkgDescriptionBegin/,/PkgDescriptionEnd/p' | sed "s/\(\\PkgDescriptionBegin{[^}]*\)}/\1,${pkg}}/"
+        fi
     else
         echo -E "${line}"
     fi
