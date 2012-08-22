5c4ae5d47717ea9da658ba4fff7034ca3f305983
Sébastien Loriot <sloriot.ml@gmail.com>
2012-08-22 06:48:49 +0000

    windows mixes up Fb template parameter with nested Fb type from base class

--- Triangulation_2/include/CGAL/Triangulation_face_base_with_info_2.h
+++ Triangulation_2/include/CGAL/Triangulation_face_base_with_info_2.h
@@ -28,29 +28,29 @@
 namespace CGAL {
 
 template < typename Info_, typename GT,
-           typename Fb = Triangulation_face_base_2<GT> >
+           typename Fb_ = Triangulation_face_base_2<GT> >
 class Triangulation_face_base_with_info_2
-  : public Fb
+  : public Fb_
 {
   Info_ _info;
 public:
-  typedef typename Fb::Vertex_handle                   Vertex_handle;
-  typedef typename Fb::Face_handle                     Face_handle;
+  typedef typename Fb_::Vertex_handle                   Vertex_handle;
+  typedef typename Fb_::Face_handle                     Face_handle;
   typedef Info_                                        Info;
 
   template < typename TDS2 >
   struct Rebind_TDS {
-    typedef typename Fb::template Rebind_TDS<TDS2>::Other       Fb2;
+    typedef typename Fb_::template Rebind_TDS<TDS2>::Other       Fb2;
     typedef Triangulation_face_base_with_info_2<Info, GT, Fb2>  Other;
   };
 
   Triangulation_face_base_with_info_2()
-    : Fb() {}
+    : Fb_() {}
 
   Triangulation_face_base_with_info_2(Vertex_handle v0, 
 				      Vertex_handle v1,
                                       Vertex_handle v2)
-    : Fb(v0, v1, v2) {}
+    : Fb_(v0, v1, v2) {}
 
   Triangulation_face_base_with_info_2(Vertex_handle v0, 
 				      Vertex_handle v1,
@@ -58,7 +58,7 @@ public:
                                       Face_handle   n0, 
 				      Face_handle   n1,
                                       Face_handle   n2 )
-    : Fb(v0, v1, v2, n0, n1, n2) {}
+    : Fb_(v0, v1, v2, n0, n1, n2) {}
 
   const Info& info() const { return _info; }
   Info&       info()       { return _info; }
