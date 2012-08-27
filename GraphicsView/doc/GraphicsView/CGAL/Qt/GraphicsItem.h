a1724dfcc7b08f63def5867bd10092fa8fdd4052
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2012-08-27 19:50:44 +0000

    typo

--- GraphicsView/doc/GraphicsView/CGAL/Qt/GraphicsItem.h
+++ GraphicsView/doc/GraphicsView/CGAL/Qt/GraphicsItem.h
@@ -3,7 +3,7 @@ namespace Qt {
 /*!
 \ingroup PkgGraphicsView
 
-The \cgal graphics items hold pointers to \cgal datastructures. When a datastructure changes, 
+The \cgal graphics items hold pointers to \cgal data structures. When a data structure changes, 
 a signal is emitted. An object derived from type `GraphicsItem` must provide an implementation of the 
 virtual slot `modelChanged()`. This typically triggers redrawing, as well as recomputation 
 of the bounding box which in turn may trigger redrawing of overlapping graphics items. 
