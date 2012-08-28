c6e2a05c2cb551c9578823fc067016b4f84e0438
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2012-08-28 15:38:38 +0000

    typos

--- Bounding_volumes/doc/Bounding_volumes/Concepts/Ellipse.h
+++ Bounding_volumes/doc/Bounding_volumes/Concepts/Ellipse.h
@@ -2,9 +2,9 @@
 \ingroup PkgBoundingVolumesConcepts
 \cgalconcept
 
-An object of the class `Ellipse` is an ellipse in two-dimensional
+An object `ellipse` of the class `Ellipse` is an ellipse in two-dimensional
 Euclidean plane \f$ \E^2\f$. Its boundary splits the plane into a bounded
-and an unbounded side. By definition, an empty `#1` has no
+and an unbounded side. By definition, an empty `ellipse` has no
 boundary and no bounded side, i.e. its unbounded side equals the
 whole plane \f$ \E^2\f$.
 */
@@ -30,7 +30,7 @@ void  set( );
 
 
 /*!
-sets `ellipse` to the ellipse containing exactly \f$ \{\mbox{\ccc{p}}\}\f$.
+sets `ellipse` to the ellipse containing exactly `p`.
 */
 void  set( const Point& p);
 
@@ -102,7 +102,7 @@ returns `CGAL::ON_BOUNDED_SIDE`,
 `CGAL::ON_UNBOUNDED_SIDE` iff `p` lies properly
 inside, on the boundary, or properly outside of `ellipse`, resp.
 
-\note Only needed, if the corresponding predicate of `Min_ellipse_2` is used.
+\note Only needed, if the corresponding predicate of `CGAL::Min_ellipse_2` is used.
 */
 CGAL::Bounded_side
                    bounded_side( const Point& p) const;
@@ -112,7 +112,7 @@ CGAL::Bounded_side
 
 returns `true`, iff `p` lies properly inside `ellipse`.
 
-\note Only needed, if the corresponding predicate of `Min_ellipse_2` is used.
+\note Only needed, if the corresponding predicate of `CGAL::Min_ellipse_2` is used.
 */
 bool  has_on_bounded_side( const Point& p) const;
 
@@ -122,7 +122,7 @@ bool  has_on_bounded_side( const Point& p) const;
 returns `true`, iff `p` lies on the boundary
 of `ellipse`.
 
-\note Only needed, if the corresponding predicate of `Min_ellipse_2` is used.
+\note Only needed, if the corresponding predicate of `CGAL::Min_ellipse_2` is used.
 */
 bool  has_on_boundary( const Point& p) const;
 
@@ -132,30 +132,30 @@ bool  has_on_boundary( const Point& p) const;
 returns `true`, iff `ellipse` is empty (this implies
 degeneracy).
 
-\note Only needed, if the corresponding predicate of `Min_ellipse_2` is used.
+\note Only needed, if the corresponding predicate of `CGAL::Min_ellipse_2` is used.
 */
 bool  is_empty( ) const;
 
 
 /*!
-returns `true`, iff `ellipse` is degenerate, i.e. if
+returns `true`, iff `ellipse` is degenerate, i.e.\ if
 `ellipse` is empty or equal to a single point.
 
-\note Only needed, if the corresponding predicate of `Min_ellipse_2` is used.
+\note Only needed, if the corresponding predicate of `CGAL::Min_ellipse_2` is used.
 */
 bool  is_degenerate( ) const;
 
 
 /// \name I/O 
 /// The following I/O operator is only needed, if the corresponding I/O
-/// operator of `Min_ellipse_2` is used.
+/// operator of `CGAL::Min_ellipse_2` is used.
 /// @{
 
 /*!
 writes `ellipse` to output stream `os`.
 
-\note Only need , if the corresponding I/O
-operator of `Min_ellipse_2` is used.
+\note Only needed, if the corresponding I/O
+operator of `CGAL::Min_ellipse_2` is used.
 
 */
 ostream& operator<<(ostream& os, const Ellipse& ellipse);
