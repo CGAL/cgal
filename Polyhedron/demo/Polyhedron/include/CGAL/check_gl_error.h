df2fb628cf3a99dcd6ef7f581f9f73f31061f41b
Sébastien Loriot <sloriot.ml@gmail.com>
2012-08-28 11:48:12 +0000

    clear incorrect tag

--- Polyhedron/demo/Polyhedron/include/CGAL/check_gl_error.h
+++ Polyhedron/demo/Polyhedron/include/CGAL/check_gl_error.h
@@ -12,8 +12,8 @@
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/branches/next/Installation/include/CGAL/gl.h $
-// $Id: gl.h 45375 2008-09-08 11:40:39Z spion $
+// $URL$
+// $Id$
 //
 // Author: Laurent Rineau
 
