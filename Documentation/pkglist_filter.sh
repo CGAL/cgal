a36aba2d15a7cadcfddfc64fb7f7ea73cd20fa70
Laurent Rineau <Laurent.Rineau__CGAL@normalesup.org>
2012-09-03 12:59:39 +0000

    Commit a simplification of the script

--- Documentation/pkglist_filter.sh
+++ Documentation/pkglist_filter.sh
@@ -2,18 +2,18 @@
 
 while read -r line         
 do     
-    pkg=$(echo ${line} | sed -n '/\\package_listing{[^}]*}/p' | sed 's/\\package_listing{\([^}]*\)}/\1/')
+    pkg=$(echo ${line} | sed -n '/\\package_listing{[^}]*}/ { s/\\package_listing{\([^}]*\)}/\1/; p }')
     if [ -n "${pkg}" ]; then
         top_level=${pkg%/*}
         lower_level=${pkg##*/}
         if [ -n "${top_level}" ]; then
-            cat "../${top_level}/doc/${lower_level}/PackageDescription.txt" | sed -n '/PkgDescriptionBegin/,/PkgDescriptionEnd/p' | \
-                awk '/\\PkgDescriptionBegin{[^}]*}/ { match($0, "(\\\\PkgDescriptionBegin{)([^}]*)}", a); esc=a[2]; gsub(" ", "-", esc); printf("%s%s,%s}", a[1], a[2], esc); next} {print}'
+            filename="../${top_level}/doc/${lower_level}/PackageDescription.txt"
         else
-            cat "../${pkg}/doc/${pkg}/PackageDescription.txt" | sed -n '/PkgDescriptionBegin/,/PkgDescriptionEnd/p' | \
-                awk '/\\PkgDescriptionBegin{[^}]*}/ { match($0, "(\\\\PkgDescriptionBegin{)([^}]*)}", a); esc=a[2]; gsub(" ", "-", esc); printf("%s%s,%s}", a[1], a[2], esc); next} {print}'
+            filename="../${pkg}/doc/${pkg}/PackageDescription.txt"
         fi
+        sed -n '/PkgDescriptionBegin/,/PkgDescriptionEnd/p' < "$filename" | \
+            awk '/\\PkgDescriptionBegin{[^}]*}/ { match($0, "(\\\\PkgDescriptionBegin{)([^}]*)}", a); esc=a[2]; gsub(" ", "-", esc); printf("%s%s,%s}", a[1], a[2], esc); next} {print}'
     else
         echo -E "${line}"
     fi
-done <$1
+done <"$1"
