fc1a5126c73b76f700c1774b4f987374f3133d4a
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2012-09-04 08:31:17 +0000

    Add a comment to a \file

--- Convex_hull_2/examples/Convex_hull_2/array_convex_hull_2.cpp
+++ Convex_hull_2/examples/Convex_hull_2/array_convex_hull_2.cpp
@@ -1,3 +1,7 @@
+/*!
+\file array_convex_hull_2.cpp
+Example for a the convex hull algorithm reading and writing points from and to a built-in array.
+*/ 
 #include <iostream>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/convex_hull_2.h>
