99a4a3dbf40741310a7d996300f6ccb06ea63c31
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2012-09-05 15:36:37 +0000

    prepare for angles+area

--- Hole_filling/fill.cpp
+++ Hole_filling/fill.cpp
@@ -7,10 +7,42 @@ typedef CGAL::Simple_cartesian<double> K;
 typedef K::Point_3 Point_3;
 typedef std::vector<Point_3> Polyline_3;
 
-double
+
+struct Weight {
+
+  std::pair<double,double> w;
+
+  Weight()
+    : w(std::make_pair<double>(0, 0))
+  {}
+
+  Weight(double angle, double area)
+    : w(angle, area)
+  {}
+};
+
+Weight operator+(const Weight& w1, const Weight& w2)
+{
+  return Weight((std::max)(w1.w.first, w2.w.first), w1.w.second + w2.w.second);
+}
+
+bool operator<(const Weight& w1, const Weight& w2)
+{
+  if(w1.w.first < w2.w.first){
+    return true;
+  }else if(w1.w.first == w2.w.first){
+    return w1.w.second < w2.w.second;
+  }
+  return false;
+}
+
+
+
+
+Weight
 sigma(const Polyline_3& P, int i, int j, int k)
 {
-  return sqrt(CGAL::squared_area(P[i], P[j], P[k]));
+  return Weight(0, sqrt(CGAL::squared_area(P[i], P[j], P[k])));
 }
 
 
@@ -58,7 +90,7 @@ void fill(const Polyline_3& P)
 {
   //  std::cerr << "n = " << P.size() << std::endl;
   int n = P.size() -1; // because the first and last point are equal
-  std::vector<double> W(n*n,0);
+  std::vector<Weight> W(n*n,Weight(0,0));
   std::vector<int> lambda(n*n,-1);
 
   for(int i=0; i < n-2; ++i){
@@ -70,9 +102,9 @@ void fill(const Polyline_3& P)
       int k = i+j;
       //      std::cerr << "i = " << i <<  "  j = " << j << "  k = " << k << std::endl;
       int m_min = 0;
-      double w_min = (std::numeric_limits<double>::max)();
+      Weight w_min((std::numeric_limits<double>::max)(), (std::numeric_limits<double>::max)());
       for(int m = i+1; m<k; ++m){
-        double w = W[i*n + m] + W[m*n + k] + sigma(P,i,m,k);
+        Weight w = W[i*n + m] + W[m*n + k] + sigma(P,i,m,k);
         if(w < w_min){
           w_min = w;
           m_min = m;
