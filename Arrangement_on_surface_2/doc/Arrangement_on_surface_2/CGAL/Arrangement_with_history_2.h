7d74f7b78452ab100f4677bf02fd82ce5956b067
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2012-09-13 07:56:26 +0000

    fix \calC; use \code for examples; no \sc in \subsection

--- Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/Arrangement_with_history_2.h
+++ Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/Arrangement_with_history_2.h
@@ -7,18 +7,18 @@ namespace CGAL {
 \anchor arr_refarr_with_hist 
 
 An object `arr` of the class `Arrangement_with_history_2` represents the 
-planar subdivision induced by a set of input curves \f$ \calC\f$. 
+planar subdivision induced by a set of input curves \f$ \cal C\f$. 
 The arrangement is represented as a doubly-connected edge-list (<span class="textsc">Dcel</span>). 
 As is the case for the `Arrangement_2<Traits,Dcel>`, each <span class="textsc">Dcel</span> 
 vertex is associated with a point and each edge is associated with an 
 \f$ x\f$-monotone curve whose interior is disjoint from all other edges and 
 vertices. Each such \f$ x\f$-monotone curve is a subcurve of some 
-\f$ C \in \calC\f$ - or may represent an overlap among several curves 
-in \f$ \calC\f$. 
+\f$ C \in \cal C\f$ - or may represent an overlap among several curves 
+in \f$ \cal C\f$. 
 
 The `Arrangement_with_history_2` class-template extends the `Arrangement_2` 
 class-template by keeping an additional container of input curves 
-representing \f$ \calC\f$, and by maintaining a cross-mapping between these 
+representing \f$ \cal C\f$, and by maintaining a cross-mapping between these 
 curves and the arrangement edges they induce. This way it is possible 
 to determine the inducing curve(s) of each arrangement edge. This mapping 
 also allows the traversal of input curves, and the traversal of edges 
