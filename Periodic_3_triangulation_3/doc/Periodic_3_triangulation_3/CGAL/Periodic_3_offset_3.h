6420a2b878870d70427b4fa16e5cc655000b639d
Philipp Möller <Philipp.Moeller@geometryfactory.com>
2012-09-17 15:24:37 +0000

    Add Periodic_3 doc

---
+++ Periodic_3_triangulation_3/doc/Periodic_3_triangulation_3/CGAL/Periodic_3_offset_3.h
@@ -0,0 +1,20 @@
+
+namespace CGAL {
+
+/*!
+\ingroup PkgPeriodic3Triangulation3
+
+The class `Periodic_3_offset_3` is a model of the concept 
+`Periodic_3Offset_3`. 
+
+\models ::Periodic_3Offset_3 
+
+*/
+
+class Periodic_3_offset_3 {
+public:
+
+/// @}
+
+}; /* end Periodic_3_offset_3 */
+} /* end namespace CGAL */
---
+++ Periodic_3_triangulation_3/doc/Periodic_3_triangulation_3/Concepts/Periodic_3TriangulationDSVertexBase_3.h
@@ -0,0 +1,66 @@
+
+/*!
+\ingroup PkgPeriodic3Triangulation3Concepts
+\cgalconcept
+
+At the base level of 3D-triangulations 
+(see Sections \ref P3Triangulation3secdesign and \ref TDS3secdesign), 
+a vertex provides access to one of its incident cells through a handle. 
+
+For optimization purposes we need to extend the concept 
+`TriangulationDSVertexBase_3` as follows. 
+
+\refines ::TriangulationDSVertexBase_3 
+
+\refines Periodic_3TriangulationDSVertexBase_3 needs to store an offset 
+   and some index inside the vertex. 
+
+\hasModel CGAL::Periodic_3_triangulation_ds_vertex_base_3 
+
+\sa `TriangulationDataStructure_3` 
+\sa `TriangulationDSVertexBase_3` 
+\sa `Periodic_3TriangulationDSCellBase_3` 
+
+*/
+
+class Periodic_3TriangulationDSVertexBase_3 {
+public:
+
+/// \name Types 
+/// @{
+
+/*! 
+A model of the concept 
+`Periodic_3Offset_3` 
+*/ 
+typedef Hidden_type Periodic_3_offset_3; 
+
+/// @} 
+
+/// \name Access Functions 
+/// @{
+
+/*! 
+Returns the offset stored in the vertex. 
+*/ 
+Periodic_3_offset_3 offset() const; 
+
+/*! 
+Returns `true` if the offset has been set, `false` otherwise. 
+*/ 
+bool get_offset_flag() const; 
+
+/*! 
+Sets the offset and sets the offset flag to `true`. 
+*/ 
+void set_offset(Periodic_3_offset_3 o); 
+
+/*! 
+Sets the offset flag to `false` and clears the offset. 
+*/ 
+void clear_offset(); 
+
+/// @}
+
+}; /* end Periodic_3TriangulationDSVertexBase_3 */
+
