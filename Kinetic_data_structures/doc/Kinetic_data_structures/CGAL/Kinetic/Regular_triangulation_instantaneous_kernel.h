0665a8503c230be8c3d93d298e67a5d296081bd0
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2012-09-21 19:37:42 +0000

    nicer enumeration

--- Kinetic_data_structures/doc/Kinetic_data_structures/CGAL/Kinetic/Regular_triangulation_instantaneous_kernel.h
+++ Kinetic_data_structures/doc/Kinetic_data_structures/CGAL/Kinetic/Regular_triangulation_instantaneous_kernel.h
@@ -3,7 +3,7 @@ namespace CGAL {
 namespace Kinetic {
 
 /*!
-\ingroup PkgKds
+\ingroup PkgKdsTri
 
 The class `Kinetic::Regular_triangulation_instantaneous_kernel` is an instantaneous kernel for use with a 
 regular triangulation data structure. There is not currently a reason 
@@ -11,7 +11,7 @@ for the user to call this directly unless the user wants created their
 own simulation traits as it is included as part of the 
 `Kinetic::Regular_triangulation_exact_simulation_traits`. 
 
-\models ::CGAL::RegularTriangulationTraits_3
+\models ::Kinetic::RegularTriangulationTraits_3
 \models ::Kinetic::InstantaneousKernel 
 
 \sa `Kinetic::Regular_triangulation_3<Traits, Visitor, Triangulation>`
