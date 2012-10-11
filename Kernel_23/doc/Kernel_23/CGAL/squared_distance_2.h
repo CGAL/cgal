4905c23e75562f9d8c5e7b9a34d39f268b7376ef
Sébastien Loriot <sloriot.ml@gmail.com>
2012-10-11 22:35:05 +0000

    avoid brief

--- Kernel_23/doc/Kernel_23/CGAL/squared_distance_2.h
+++ Kernel_23/doc/Kernel_23/CGAL/squared_distance_2.h
@@ -3,13 +3,9 @@ namespace CGAL {
 /*!
 \addtogroup squared_distance squared_distance
 \ingroup kernel_global_function
-2D functions:
 \code
-#include <squared_distance_2.h>
-\endcode
-3D functions:
-\code
-#include <squared_distance_3.h>
+#include <squared_distance_2.h> //for 2D functions
+#include <squared_distance_3.h> //for 3D functions
 \endcode  
 
 */
