80ea1a7d331caa7a73a6baca13feabb0033e3ed0
Clément Jamin <clement.jamin@inria.fr>
2012-10-19 14:27:16 +0000

    This "CGAL_UNDOABLE_INSERT_USE_MAP" thing was not used

--- Mesh_3/include/CGAL/Compact_mesh_cell_base_3.h
+++ Mesh_3/include/CGAL/Compact_mesh_cell_base_3.h
@@ -485,11 +485,6 @@ private:
   // The index of the cell of the input complex that contains me
   Subdomain_index subdomain_index_;
 
-#ifndef CGAL_UNDOABLE_INSERT_USE_MAP
-public:
-  int undoable_insert_marker;
-#endif
-
   TDS_data      _tds_data;
   char bits_;
   mutable bool sliver_cache_validity_;
