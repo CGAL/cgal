358b8bb92b91d5a9e94d0d56ee56b972af50a5c3
Eric Berberich <eric.berberich@cgal.org>
2012-10-22 23:09:58 +0000

    search for git parent dir + commented some verbosity

--- Installation/cmake/modules/CGAL_SCM.cmake
+++ Installation/cmake/modules/CGAL_SCM.cmake
@@ -2,25 +2,35 @@
 if( NOT CGAL_SCM_FILE_INCLUDED )
   set(CGAL_SCM_FILE_INCLUDED 1 )
 
-if ( EXISTS ${CMAKE_SOURCE_DIR}/.git )
-  set ( CGAL_SCM_NAME "git" )
-else()
-  if ( EXISTS ${CMAKE_SOURCE_DIR}/.svn )
-    set ( CGAL_SCM_NAME "svn" )
-  else ()
-    message( ERROR "Neither 'svn' nor 'git' as SCM found" ) 
+set(GIT_PARENT_DIR "${CMAKE_SOURCE_DIR}")
+set(GIT_DIR "${GIT_PARENT_DIR}/.git")
+while(NOT EXISTS "${GIT_DIR}")	# .git dir not found, search parent directories
+  set(GIT_PREVIOUS_PARENT "${GIT_PARENT_DIR}")
+  get_filename_component(GIT_PARENT_DIR ${GIT_PARENT_DIR} PATH)
+  if(GIT_PARENT_DIR STREQUAL GIT_PREVIOUS_PARENT)
+    # We have reached the root directory, we are not in git
+    set(${_refspecvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
+    set(${_hashvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
+  else()
+    set(GIT_DIR "${GIT_PARENT_DIR}/.git")
   endif()
+ endwhile()
+
+if ( NOT "${_refspecvar}" STREQUAL "GITDIR-NOTFOUND" )
+  set ( CGAL_SCM_NAME "git" )
 endif()
+
+#message ("GPD: ${GIT_PARENT_DIR}")
 
 if ( ${CGAL_SCM_NAME} STREQUAL "git" )
 
   find_program(GIT_EXECUTABLE git DOC "git command line client")
-EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} --git-dir=${CMAKE_SOURCE_DIR}/.git symbolic-ref HEAD 
+  EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} --git-dir=${GIT_PARENT_DIR}/.git symbolic-ref HEAD 
      OUTPUT_VARIABLE CGAL_GIT_BRANCH_OUT
      OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   string ( REGEX REPLACE "refs/heads/" "" CGAL_GIT_BRANCH ${CGAL_GIT_BRANCH_OUT})
-  message( STATUS "Git branch ${CGAL_GIT_BRANCH}") 
+  # message( STATUS "Git branch ${CGAL_GIT_BRANCH}") 
 
  set ( CGAL_SCM_BRANCH_NAME "${CGAL_GIT_BRANCH}" )
 
