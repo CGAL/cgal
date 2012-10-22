fe43dd017108af59a0c1b16be0c201d924d5e776
Eric Berberich <eric.berberich@cgal.org>
2012-10-22 23:38:25 +0000

    better handling if source of executable is not in a git repo

--- Installation/cmake/modules/CGAL_SCM.cmake
+++ Installation/cmake/modules/CGAL_SCM.cmake
@@ -2,27 +2,30 @@
 if( NOT CGAL_SCM_FILE_INCLUDED )
   set(CGAL_SCM_FILE_INCLUDED 1 )
 
+#message ("-----CSD: ${CMAKE_SOURCE_DIR}")
+
 set(GIT_PARENT_DIR "${CMAKE_SOURCE_DIR}")
 set(GIT_DIR "${GIT_PARENT_DIR}/.git")
 while(NOT EXISTS "${GIT_DIR}")	# .git dir not found, search parent directories
   set(GIT_PREVIOUS_PARENT "${GIT_PARENT_DIR}")
   get_filename_component(GIT_PARENT_DIR ${GIT_PARENT_DIR} PATH)
-  if(GIT_PARENT_DIR STREQUAL GIT_PREVIOUS_PARENT)
+  #message ("-----GPD1: ${GIT_PARENT_DIR}")
+  #message ("-----PPD1: ${GIT_PREVIOUS_PARENT}")
+  if( "${GIT_PARENT_DIR}" STREQUAL "${GIT_PREVIOUS_PARENT}" )
     # We have reached the root directory, we are not in git
-    set(${_refspecvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
-    set(${_hashvar} "GITDIR-NOTFOUND" PARENT_SCOPE)
+    set(_refspecvar "GITDIR-NOTFOUND")
+    set(_hashvar "GITDIR-NOTFOUND")
+    set ( CGAL_SCM_NAME "" )
+    break()
   else()
-    set(GIT_DIR "${GIT_PARENT_DIR}/.git")
+    set( GIT_DIR "${GIT_PARENT_DIR}/.git" )
+    set( CGAL_SCM_NAME "git" )
   endif()
- endwhile()
+endwhile()
 
-if ( NOT "${_refspecvar}" STREQUAL "GITDIR-NOTFOUND" )
-  set ( CGAL_SCM_NAME "git" )
-endif()
 
-#message ("GPD: ${GIT_PARENT_DIR}")
-
-if ( ${CGAL_SCM_NAME} STREQUAL "git" )
+if ( "${CGAL_SCM_NAME}" STREQUAL "git" )
+  #message ("====GPD: ${GIT_PARENT_DIR}")
 
   find_program(GIT_EXECUTABLE git DOC "git command line client")
   EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} --git-dir=${GIT_PARENT_DIR}/.git symbolic-ref HEAD 
