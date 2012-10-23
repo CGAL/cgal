f5473b62d394520693f4039b8e134f975271fa95
Andreas Fabri <Andreas.Fabri@geometryfactory.com>
2012-10-23 19:50:22 +0000

    finsh

--- Skin_surface_3/doc/Skin_surface_3/CGAL/Skin_surface_3.h
+++ Skin_surface_3/doc/Skin_surface_3/CGAL/Skin_surface_3.h
@@ -4,9 +4,8 @@ namespace CGAL {
 /*!
 \ingroup PkgSkinSurface3
 
-The `Skin_surface_3` is the main class in the 
-`Skin_surface_3` package. It is a model of 
-the concept `SkinSurface_3`. 
+The `Skin_surface_3` is the main class in this package. 
+It is a model of the concept `SkinSurface_3`. 
 
 The template argument must be a model of the concept 
 `SkinSurfaceTraits_3`, which means that it provides predicates 
--- Skin_surface_3/doc/Skin_surface_3/CGAL/Skin_surface_polyhedral_items_3.h
+++ Skin_surface_3/doc/Skin_surface_3/CGAL/Skin_surface_polyhedral_items_3.h
@@ -4,7 +4,7 @@ namespace CGAL {
 /*!
 \ingroup PkgSkinSurface3
 
-An items class for the `Polyhedron_3` that stores information in faces instead of an auxiliary data structure. Using it accelerates the subdivision function `subdivide_skin_surface_mesh_3`.
+An items class for the `Polyhedron_3` that stores information in faces instead of an auxiliary data structure. Using it accelerates the subdivision function `subdivide_skin_surface_mesh_3()`.
 */
 
 template<class SkinSurface3>
