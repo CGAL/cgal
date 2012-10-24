376ac8824b0888f228e417ebc6a9a7d0d6c72482
Mikhail Bogdanov <bogdanov.mike@gmail.com>
2012-10-24 20:46:59 +0000

    tests are changed for the paper

--- Hyperbolic_triangulation_2/examples/Hyperbolic_triangulation_2/test_euclidean_triangulation.cpp
+++ Hyperbolic_triangulation_2/examples/Hyperbolic_triangulation_2/test_euclidean_triangulation.cpp
@@ -2,14 +2,12 @@
 
 // CGAL headers
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-
 #include <CGAL/Delaunay_triangulation_2.h>
-
 #include <CGAL/Timer.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-
 typedef K::Point_2 Point_2;
+typedef K::FT FT;
 
 typedef CGAL::Delaunay_triangulation_2<K> Dt;
 
@@ -21,21 +19,42 @@ int main()
   
   std::ifstream f("points.cin");
   
+  // read radius of the enclosing sphere
+  FT r;
+  f >> r;
+  
   Point_2 p;
   while(f >> p) {
     pts.push_back(p);
   }
   f.close();
   
+  Dt dt = Dt();
+  std::vector<Point_2>::iterator ip = pts.begin();
+  
+  /*//
+  std::vector<Point_2> pts2;
+  std::ifstream f2("points2.cin");
+  f2 >> r;
+  
+  while(f2 >> p) {
+    pts2.push_back(p);
+  }
+  f2.close();
+  
+  dt.insert(pts2.begin(), pts2.end());
+  //*/
+  
   timer.start();
   
-  Dt dt = Dt();
-  
-  dt.insert(pts.begin(), pts.end());
+  while(ip != pts.end()) {
+    dt.insert(*ip++);
+  }
   
   timer.stop();
   
   std::cout << "R^2" << std::endl;
+  std::cout << "Radius: " << r << std::endl;
   std::cout << "Number of points: " << dt.number_of_vertices() << std::endl;
   std::cout << "Time: " << timer.time() << std::endl;
   
--- Hyperbolic_triangulation_2/examples/Hyperbolic_triangulation_2/test_hyperbolic_triangulation2.cpp
+++ Hyperbolic_triangulation_2/examples/Hyperbolic_triangulation_2/test_hyperbolic_triangulation2.cpp
@@ -2,13 +2,9 @@
 
 // CGAL headers
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-
 #include <CGAL/Delaunay_hyperbolic_triangulation_2.h>
-
 #include <CGAL/Triangulation_hyperbolic_traits_2.h>
-
 #include <CGAL/Timer.h>
-
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef CGAL::Triangulation_hyperbolic_traits_2<K> Gt;
@@ -17,14 +13,21 @@ typedef K::Point_2 Point_2;
 typedef K::FT FT;
 
 typedef CGAL::Delaunay_hyperbolic_triangulation_2<Gt> HDt;
+typedef HDt::Vertex_handle Vertex_handle;
+typedef HDt::Face_handle Face_handle;
 
 int main()
 {
   CGAL::Timer timer;
   
   std::vector<Point_2> pts;
+  std::vector<Point_2>::iterator ip;
   
   std::ifstream f("points.cin");
+  
+  // read radius of the enclosing sphere
+  FT r;
+  f >> r;
   
   Point_2 p;
   while(f >> p) {
@@ -32,18 +35,34 @@ int main()
   }
   f.close();
   
-  // Radius of the Poincare Disk
-  FT r = 100;
+  /*
+  //
+  std::vector<Point_2> pts2;
+  std::ifstream f2("points2.cin");
+  f2 >> r;
+  
+  while(f2 >> p) {
+    pts2.push_back(p);
+  }
+  f2.close();
+  
+  HDt hdt = HDt(Gt(r));
+  hdt.insert(pts2.begin(), pts2.end());
+  //*/
+  
+  HDt hdt = HDt(Gt(r));
+  ip = pts.begin();
   
   timer.start();
   
-  HDt hdt = HDt(Gt(r));
-  
-  hdt.insert2(pts.begin(), pts.end());
+  while(ip != pts.end()) {
+    hdt.insert(*ip++);
+  }
   
   timer.stop();
   
   std::cout << "H^2" << std::endl;
+  std::cout << "Radius: " << r << std::endl;
   std::cout << "Number of points: " << hdt.number_of_vertices() << std::endl;
   std::cout << "Time: " << timer.time() << std::endl;
   
