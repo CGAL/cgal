60091a3e876b8ecd3a9471415fa6d3cd4c54c329
Philipp Möller <Philipp.Moeller@geometryfactory.com>
2012-10-30 13:14:34 +0000

    conceptify now requires its working directory as argument

--- Documentation/conceptify.py
+++ Documentation/conceptify.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 # Copyright (c) 2012 GeometryFactory (France). All rights reserved.
 # All rights reserved.
-# 
+#
 # This file is part of CGAL (www.cgal.org).
 # You can redistribute it and/or modify it under the terms of the GNU
 # General Public License as published by the Free Software Foundation,
 # either version 3 of the License, or (at your option) any later version.
-# 
+#
 # Licensees holding a valid commercial license may use this file in
 # accordance with the commercial license agreement provided with the software.
-# 
+#
 # This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 # WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-# 
+#
 # $URL:
 # $Id:
-# 
-# 
+#
+#
 # Author(s)     : Philipp Moeller
 
+import argparse
 import os
 import re
 import glob
@@ -36,7 +37,6 @@ def conceptify_nested_classes(d):
     nested_classes=nested_classes.filter(lambda i: pq(this).attr('class') == 'memitem:')
     nested_classes.children(".memItemLeft").text("concept")
 
-
 def conceptify(d):
     # fix the title
     title = d(".title")
@@ -47,7 +47,7 @@ def conceptify(d):
     include_statement.empty()
 
     conceptify_nested_classes(d)
-    
+
 # just an alias
 def conceptify_ns(d):
     conceptify_nested_classes(d)
@@ -61,19 +61,19 @@ def write_out_html(d, fn):
 
 # remove duplicate files
 def clean_doc():
-    duplicate_files=glob.glob('./output/CGAL.CGAL.*/html/jquery.js')
-    duplicate_files.extend(glob.glob('./output/CGAL.CGAL.*/html/dynsections.js'))
-    duplicate_files.extend(glob.glob('./output/CGAL.CGAL.*/html/resize.js'))
-    duplicate_files.extend(glob.glob('./output/CGAL.CGAL.*/html/stylesheet.css'))
+    duplicate_files=glob.glob('./CGAL.CGAL.*/html/jquery.js')
+    duplicate_files.extend(glob.glob('./CGAL.CGAL.*/html/dynsections.js'))
+    duplicate_files.extend(glob.glob('./CGAL.CGAL.*/html/resize.js'))
+    duplicate_files.extend(glob.glob('./CGAL.CGAL.*/html/stylesheet.css'))
     # kill _all_, including the one in CGAL.CGAL tabs.css files
-    duplicate_files.extend(glob.glob('./output/CGAL.CGAL*/html/tabs.css'))
+    duplicate_files.extend(glob.glob('./CGAL.CGAL*/html/tabs.css'))
     # left-over by doxygen?
-    duplicate_files.extend(glob.glob('./output/CGAL.CGAL.*/bib2xhtml.pl'))
-    duplicate_files.extend(glob.glob('./output/CGAL.CGAL.*/cgal_manual.bib'))
-    duplicate_files.extend(glob.glob('./output/CGAL.CGAL.*/citelist.doc'))
-    duplicate_files.extend(glob.glob('./output/CGAL.CGAL.*/doxygen.bst'))
-    duplicate_files.extend(glob.glob('./output/CGAL.CGAL.*/geom.bib'))
-    
+    duplicate_files.extend(glob.glob('./CGAL.CGAL.*/bib2xhtml.pl'))
+    duplicate_files.extend(glob.glob('./CGAL.CGAL.*/cgal_manual.bib'))
+    duplicate_files.extend(glob.glob('./CGAL.CGAL.*/citelist.doc'))
+    duplicate_files.extend(glob.glob('./CGAL.CGAL.*/doxygen.bst'))
+    duplicate_files.extend(glob.glob('./CGAL.CGAL.*/geom.bib'))
+
     for fn in duplicate_files:
         os.remove(fn)
 
@@ -93,72 +93,89 @@ def re_replace_in_file(pat, s_after, fname):
         out.close()
         os.rename(out_fname, fname)
 
-class_files=glob.glob('./output/CGAL.CGAL.*/html/class*.html')
-for fn in class_files:
-    d = pq(filename=fn, parser='html')
-    ident = d('#CGALConcept')
-    if ident.size() == 1:
-        conceptify(d);
-        # in case of a second pass don't process this again
-        d.remove("#CGALConcept")
-    # there is a doxygen bug that prevents the correct linkage of the CGAL breadcrumb 
-    ident = d('#nav-path .navelem').eq(0).children().eq(0)
-    if ident and ident.attr('href') == 'namespaceCGAL.html':
-        ident.attr('href', '../../CGAL.CGAL/html/namespaceCGAL.html')
-    write_out_html(d, fn)
+def main():
+    parser = argparse.ArgumentParser(
+        description='''This script makes adjustments to the doxygen output. 
+It replaces some text in specifically marked classes with the appropriate text for a concept, 
+removes some unneeded files, and performs minor repair on some glitches.''')
+    parser.add_argument('--output', metavar='/path/to/doxygen/output', required=True)
+    
+    args = parser.parse_args()
+    os.chdir(args.output)
 
-namespace_files=glob.glob('./output/CGAL.CGAL.*/html/namespace*.html')
-for fn in namespace_files:
-    d = pq(filename=fn, parser='html')
-    ident = d('#CGALConceptNS')
-    if ident.size() == 1:
-        conceptify_ns(d);
-        d.remove("#CGALConceptNS")
-    write_out_html(d, fn)
-
-# in a group we only need to change the nested-classes
-group_files=glob.glob('./output/CGAL.CGAL.*/html/group*Concepts*.html')
-for fn in group_files:
-    d = pq(filename=fn, parser='html')
-    conceptify_nested_classes(d)
-    write_out_html(d, fn)    
-
-# fix up Files
-files_files=glob.glob('./output/CGAL.CGAL.*/html/files.html')
-for fn in files_files:
-    d = pq(filename=fn, parser='html')
-    table = d("table.directory")
-    row_id=table("td.entry").filter(lambda i: pq(this).text() == 'Concepts').parent().attr('id')
-    if row_id != None:
-        # figure out the rowid and then drop everything from the table that matches
-        table("tr").filter(lambda i: re.match(row_id + '*', pq(this).attr('id'))).remove()
+    class_files=glob.glob('./CGAL.CGAL.*/html/class*.html')
+    for fn in class_files:
+        d = pq(filename=fn, parser='html')
+        ident = d('#CGALConcept')
+        if ident.size() == 1:
+            conceptify(d);
+            # in case of a second pass don't process this again
+            d.remove("#CGALConcept")
+        # there is a doxygen bug that prevents the correct linkage of the CGAL breadcrumb
+        ident = d('#nav-path .navelem').eq(0).children().eq(0)
+        if ident and ident.attr('href') == 'namespaceCGAL.html':
+            ident.attr('href', '../../CGAL.CGAL/html/namespaceCGAL.html')
         write_out_html(d, fn)
 
-filesjs_files=glob.glob('./output/CGAL.CGAL.*/html/files.js')
-for fn in filesjs_files:
-    re_replace_in_file('^.*\[ "Concepts",.*$', '', fn)
+    namespace_files=glob.glob('./CGAL.CGAL.*/html/namespace*.html')
+    for fn in namespace_files:
+        d = pq(filename=fn, parser='html')
+        ident = d('#CGALConceptNS')
+        if ident.size() == 1:
+            conceptify_ns(d);
+            d.remove("#CGALConceptNS")
+        write_out_html(d, fn)
+
+    # in a group we only need to change the nested-classes
+    group_files=glob.glob('./CGAL.CGAL.*/html/group*Concepts*.html')
+    for fn in group_files:
+        d = pq(filename=fn, parser='html')
+        conceptify_nested_classes(d)
+        write_out_html(d, fn)
+
+    # fix up Files
+    files_files=glob.glob('./CGAL.CGAL.*/html/files.html')
+    for fn in files_files:
+        d = pq(filename=fn, parser='html')
+        table = d("table.directory")
+        row_id=table("td.entry").filter(lambda i: pq(this).text() == 'Concepts').parent().attr('id')
+        if row_id != None:
+            # figure out the rowid and then drop everything from the table that matches
+            table("tr").filter(lambda i: re.match(row_id + '*', pq(this).attr('id'))).remove()
+            write_out_html(d, fn)
+
+    filesjs_files=glob.glob('./CGAL.CGAL.*/html/files.js')
+    for fn in filesjs_files:
+        re_replace_in_file('^.*\[ "Concepts",.*$', '', fn)
+
+    # external is placed by doxygen to mark a class from a tagfile, this
+    # is more confusing then helpful in our case
+
+    # TODO change the color of icons for concepts here
+    re_replace_in_file('\[external\]', '', './CGAL.CGAL/html/annotated.html')
+
+    # fix class/concept mismatch in generated pages
+    relationship_pages=glob.glob('./CGAL.CGAL.*/html/hasModels.html')
+    relationship_pages.extend(glob.glob('./CGAL.CGAL.*/html/generalizes.html'))
+    relationship_pages.extend(glob.glob('./CGAL.CGAL.*/html/refines.html'))
+    for fn in relationship_pages:
+        d = pq(filename=fn, parser='html')
+        dts=d(".textblock .reflist dt")
+        # no contents() on pyquery, do it the hard way
+        dts.each(lambda i: pq(this).html(re.sub("Class ", "Concept ", pq(this).html())))
+        write_out_html(d, fn)
+
+    # throw out nav-sync and the detailed description title
+    all_pages=glob.glob('./CGAL.CGAL*/html/*.html')
+    for fn in all_pages:
+        d = pq(filename=fn, parser='html')
+        d('#nav-sync').hide()
+        d('h2.groupheader').filter(lambda i: pq(this).text() == 'Detailed Description').remove()
+        # TODO count figures
+        write_out_html(d, fn)
+
+    clean_doc()
     
-# external is placed by doxygen to mark a class from a tagfile, this
-# is more confusing then helpful in our case
-re_replace_in_file('\[external\]', '', './output/CGAL.CGAL/html/annotated.html')
-
-# fix class/concept mismatch in generated pages
-relationship_pages=glob.glob('./output/CGAL.CGAL.*/html/hasModels.html')
-relationship_pages.extend(glob.glob('./output/CGAL.CGAL.*/html/generalizes.html'))
-relationship_pages.extend(glob.glob('./output/CGAL.CGAL.*/html/refines.html'))
-for fn in relationship_pages:
-    d = pq(filename=fn, parser='html')
-    dts=d(".textblock .reflist dt")
-    # no contents() on pyquery, do it the hard way
-    dts.each(lambda i: pq(this).html(re.sub("Class ", "Concept ", pq(this).html())))
-    write_out_html(d, fn)
-
-# throw out nav-sync and the detailed description title
-all_pages=glob.glob('./output/CGAL.CGAL*/html/*.html')
-for fn in all_pages:
-    d = pq(filename=fn, parser='html')
-    d('#nav-sync').hide()
-    d('h2.groupheader').filter(lambda i: pq(this).text() == 'Detailed Description').remove()
-    write_out_html(d, fn)
-
-clean_doc()
+    
+if __name__ == "__main__":
+    main()
