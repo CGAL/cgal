2864a76bb0b3db00de96ecb47311e434db889bbe
Sébastien Loriot <sebastien.loriot@cgal.org>
2012-11-13 23:11:08 +0100

    add script to rename doxygen macros

---
+++ Documentation/conversion_tools/rename_macros.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+nb_lines=`wc -l rename_macros.input | awk '{print $1}'`
+sort rename_macros.input -r > /tmp/sorted_rename_macros.input
+
+for i in `seq 1 $nb_lines`; do
+  l=`head -n $i /tmp/sorted_rename_macros.input | tail -n 1`
+  echo $l | awk '{print "s/\\\\" $1 "/\\\\" $2 "/g"}'
+done > /tmp/rename_macros.sed
+
+find ../.. -name '*.txt' -o -name '*.h' | xargs sed -i -f /tmp/rename_macros.sed
+
+
+for i in `seq 1 $nb_lines`; do
+  l=`head -n $i /tmp/sorted_rename_macros.input | tail -n 1`
+  echo $l | awk '{print "s/\\\"" $1 "=/\\\"" $2 "=/g"}'
+  echo $l | awk '{print "s/\\\"" $1 "{/\\\"" $2 "{/g"}'
+done > /tmp/rename_macros.sed
+
+sed -i -f /tmp/rename_macros.sed ../Doxyfile
+
+#~ find ../.. -name '*.txt' -o -name '*.h'
+
