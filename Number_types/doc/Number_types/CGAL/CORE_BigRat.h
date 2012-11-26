d6c705e661f5988480f6bea3653592a19ce2448d
Andreas Fabri <andreas.fabri@geometryfactory.com>
2012-11-26 13:05:54 +0100

    R -> Q

--- Number_types/doc/Number_types/CGAL/CORE_BigRat.h
+++ Number_types/doc/Number_types/CGAL/CORE_BigRat.h
@@ -4,7 +4,7 @@ namespace CORE {
 /*!
 \ingroup nt_core
 
-The class `CORE::BigRat` provides exact computation in \f$ \R\f$. 
+The class `CORE::BigRat` provides exact computation in \f$ \Q\f$. 
 Operations and comparisons between objects of this type are guaranteed to be exact. 
 This number type is provided by the <span class="textsc">Core</span> library \cite klpy-clp-99. 
 
