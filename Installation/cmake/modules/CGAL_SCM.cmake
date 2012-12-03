6497d48f2f4243128a162d6fa6a4f03cfe049304
Philipp Möller <bootsarehax@googlemail.com>
2012-12-03 18:37:33 +0100

    Convert all CRLF files to LF
    Conflicts:
    
    	Documentation/stylesheet.css
    	Installation/cmake/modules/CGAL_SCM.cmake

--- Installation/cmake/modules/CGAL_SCM.cmake
+++ Installation/cmake/modules/CGAL_SCM.cmake
@@ -2,24 +2,23 @@
 if( NOT CGAL_SCM_FILE_INCLUDED )
   set(CGAL_SCM_FILE_INCLUDED 1 )
 
-#message ("-----CSD: ${CMAKE_SOURCE_DIR}")
+# CGAL_SCM_NAME can be either git or n/a as an indicator if we have a
+# build system or not
 
+# TODO: make that scm agnostic and turn it into a boolean
 set(GIT_PARENT_DIR "${CMAKE_SOURCE_DIR}")
 set(GIT_DIR "${GIT_PARENT_DIR}/.git")
-#message ("-----GD: ${GIT_DIR}")
 if (EXISTS "${GIT_DIR}")
   set( CGAL_SCM_NAME "git" )
 else()
   while(NOT EXISTS "${GIT_DIR}")	# .git dir not found, search parent directories
     set(GIT_PREVIOUS_PARENT "${GIT_PARENT_DIR}")
     get_filename_component(GIT_PARENT_DIR ${GIT_PARENT_DIR} PATH)
-    #message ("-----GPD1: ${GIT_PARENT_DIR}")
-    #message ("-----PPD1: ${GIT_PREVIOUS_PARENT}")
     if( "${GIT_PARENT_DIR}" STREQUAL "${GIT_PREVIOUS_PARENT}" )
       # We have reached the root directory, we are not in git
-      set(_refspecvar "GITDIR-NOTFOUND")
-      set(_hashvar "GITDIR-NOTFOUND")
-      set ( CGAL_SCM_NAME "" )
+      set( _refspecvar "GITDIR-NOTFOUND" )
+      set( _hashvar "GITDIR-NOTFOUND" )
+      set( CGAL_SCM_NAME "n/a" )
       break()
     else()
       set( GIT_DIR "${GIT_PARENT_DIR}/.git" )
@@ -28,21 +27,25 @@ else()
   endwhile()
 endif()
 
-#message ("====GPD1: ${GIT_PARENT_DIR}")
-
 if ( "${CGAL_SCM_NAME}" STREQUAL "git" )
-  #message ("====GPD2: ${GIT_PARENT_DIR}")
-
   find_program(GIT_EXECUTABLE git DOC "git command line client")
   EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} --git-dir=${GIT_PARENT_DIR}/.git symbolic-ref HEAD
-     OUTPUT_VARIABLE CGAL_GIT_BRANCH_OUT
-     OUTPUT_STRIP_TRAILING_WHITESPACE)
+    OUTPUT_VARIABLE CGAL_GIT_BRANCH_OUT
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   string ( REGEX REPLACE "refs/heads/" "" CGAL_GIT_BRANCH ${CGAL_GIT_BRANCH_OUT})
-  # message( STATUS "Git branch ${CGAL_GIT_BRANCH}")
+  set( CGAL_SCM_BRANCH_NAME "${CGAL_GIT_BRANCH}" )
 
- set ( CGAL_SCM_BRANCH_NAME "${CGAL_GIT_BRANCH}" )
+  EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} --git-dir=${GIT_PARENT_DIR}/.git rev-parse HEAD
+    OUTPUT_VARIABLE CGAL_GIT_HASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
+  # backward compatible
+  set( CGAL_CREATED_SVN_REVISION "99999" )
+else()
+  set( CGAL_SCM_BRANCH_NAME "n/a")
+  set( CGAL_CREATED_GIT_HASH "n/a" )
+  set( CGAL_CREATED_SVN_REVISION "n/a" )
 endif()
 
 endif()
