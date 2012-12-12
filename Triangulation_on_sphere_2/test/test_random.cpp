fa470bb51266ebc182437ee46c6c95f3b0a97580
Claudia Werner <claudia.werner@inria.fr>
2012-12-12 13:13:01 +0100

    added tests for comparing runningtimes .

--- Triangulation_on_sphere_2/test/test_random.cpp
+++ Triangulation_on_sphere_2/test/test_random.cpp
@@ -11,44 +11,48 @@
 
 
 //convex Hull
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/point_generators_3.h>
 #include <CGAL/algorithm.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/convex_hull_3.h>
+#include <CGAL/convex_hull_3_to_polyhedron_3.h>
+#include <CGAL/Delaunay_triangulation_3.h>
+#include <CGAL/convex_hull_incremental_3.h>
+#include <CGAL/Timer.h>
 #include <vector>
 
 
+typedef CGAL::Exact_predicates_inexact_constructions_kernel  K;
+typedef CGAL::Polyhedron_3<K>                     Polyhedron_3;
+
+typedef K::Segment_3                              Segment_3;
+typedef CGAL::Delaunay_triangulation_3<K>         Delaunay;
 
 
 
-
-typedef CGAL::Exact_predicates_inexact_constructions_kernel         K;
 typedef CGAL::Delaunay_triangulation_sphere_traits_2<K>             Gt;
 typedef CGAL::Projection_sphere_traits_3<K>							Gt2;
 typedef CGAL::Delaunay_triangulation_sphere_2<Gt>              RTOS;
 typedef CGAL::Delaunay_triangulation_sphere_2<Gt2>              RTOS2;
 typedef RTOS::Vertex_handle                             Vertex_handle;
 typedef RTOS::Face_handle                                 Face_handle;
-typedef RTOS::Point                                             Point;
+typedef K::Point_3                                            Point;
 typedef RTOS::All_faces_iterator                            Face_iterator;
 typedef RTOS::All_vertices_iterator                           Vertex_iterator;
 typedef RTOS::Solid_faces_iterator						Solid_faces_iterator;
 typedef RTOS::All_edges_iterator						All_edges_iterator;
 typedef RTOS::Locate_type                                 Locate_type;
 typedef RTOS::Edge                                               Edge;
-typedef RTOS::Point											Point;
+
 
 
 typedef CGAL::Polyhedron_3<K>                     Polyhedron_3;
 typedef K::Segment_3							Segment_3;
+typedef CGAL::Delaunay_triangulation_3<K, CGAL::Fast_location> Delaunay_fast;
+typedef CGAL::Creator_uniform_3<double, Point>  PointCreator;
 
-struct Plane_from_facet {
-	Polyhedron_3::Plane_3 operator()(Polyhedron_3::Facet& f) {
-		Polyhedron_3::Halfedge_handle h = f.halfedge();
-		return Polyhedron_3::Plane_3( h->vertex()->point(),
-									 h->next()->vertex()->point(),
-									 h->opposite()->vertex()->point());
-	}
-};
 
 
 
@@ -58,12 +62,12 @@ int main(){
 	
 	int nu_of_pts;
 	double radius;
-	nu_of_pts =10;
+	nu_of_pts =pow(2,10);
 	radius=6000000;
 	double minDist = radius * pow (2, -25);
 	double minDist2 = pow(minDist, 2);
 	int invalid = 0;
-	CGAL::Timer t1, t2, t3;
+	CGAL::Timer time;
 
 	CGAL::Random random(nu_of_pts);
 	typedef CGAL::Creator_uniform_3<double, Point> Creator;
@@ -72,7 +76,7 @@ int main(){
 	
 	std::vector<Point> points;
 	std::vector<Vertex_handle> vertices;
-	vertices.reserve(nu_of_pts*2);
+	vertices.reserve(nu_of_pts);
 	
 	
 	for (int count=0; count<nu_of_pts; count++) {
@@ -80,47 +84,78 @@ int main(){
 		points.push_back(p);
 		on_sphere++;
 	}
+	
+	
+	
 	//Triangulation with points on the sphere
 	RTOS rtos;
 	rtos.set_radius(radius);
 
 	std::cout<<" ***STARTING***"<<std::endl;
-	t1.start();
+	time.start();
 	rtos.insert(points.begin(),points.end());
-	t1.stop();
-	std::cout<<"running time triangulation sphere    "<< t1.time()<<std::endl;
+	time.stop();
+	std::cout<<"running time triangulation sphere    "<< time.time()<<std::endl;
 
-	std::cout<<"number of vertices    "<<rtos.number_of_vertices()<<std::endl;
+	
 
 	
 	//Triangulation with points on the sphere
 	RTOS2 rtos2;
 	rtos2.set_radius(radius);
 	
-	
-	t2.start();
+	time.reset();
+	time.start();
 	rtos2.insert(points.begin(),points.end());
-	t2.stop();
-	std::cout<<"running time triangulation sphere projection traits:   "<< t2.time()<<std::endl;
+	time.stop();
+	std::cout<<"running time triangulation sphere projection traits:   "<< time.time()<<std::endl;
 	
-	std::cout<<"number of vertices    "<<rtos2.number_of_vertices()<<std::endl;
 	
-	rtos.is_valid();
-	rtos2.is_valid();
-	/*
-	//*****Convex Hull***********
+	
 		
-	// define polyhedron to hold convex hull
 	Polyhedron_3 poly;
 	
-	// compute convex hull of non-collinear points
-	t3.start();
+	time.reset();
+	time.start();
 	CGAL::convex_hull_3(points.begin(), points.end(), poly);
-	t3.stop();
-	std::cout<<"running time convex hull t2  "<< t3.time()<<std::endl;
+	time.stop();
+	std::cout << "Static :" << time.time() <<" "<<  std::endl;
 	
-	std::cout << "The convex hull contains    " << poly.size_of_vertices() << " vertices" << std::endl;
+	poly.clear();
+		
 	
-	//*/
-
+	
+	time.reset();
+	time.start();
+	CGAL::convex_hull_incremental_3( points.begin(), points.end(), poly, false);
+	time.stop();
+	std::cout << "incremental EPIC :" << time.time() << std::endl;
+	
+		
+	time.reset();
+	time.start();
+	Delaunay T_on(points.begin(), points.end());
+	time.stop();
+	std::cout << "Delaunay on sphere:" << time.time() << std::endl;
+	
+	time.reset();
+	time.start();
+	Delaunay_fast T_fast_on(points.begin(), points.end());
+	time.stop();
+	std::cout << "Delaunay fast location on sphere  :" << time.time() << std::endl;
+	
+	
+	
+	
+	
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
