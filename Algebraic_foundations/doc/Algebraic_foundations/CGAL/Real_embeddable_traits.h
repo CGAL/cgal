0b5021a4619c6899a003e645a3449c05f2c7cbc7
Philipp Möller <bootsarehax@googlemail.com>
2012-12-18 22:09:21 +0100

    Flatten the modules and integrate the Classified Reference

--- Algebraic_foundations/doc/Algebraic_foundations/CGAL/Real_embeddable_traits.h
+++ Algebraic_foundations/doc/Algebraic_foundations/CGAL/Real_embeddable_traits.h
@@ -2,7 +2,7 @@
 namespace CGAL {
 
 /*!
-\ingroup PkgAlgebraicFoundationsRealEmbeddable
+\ingroup PkgAlgebraicFoundations
 
 An instance of `Real_embeddable_traits` is a model of `RealEmbeddableTraits`, where <span class="textsc">T</span> is the associated type. 
 
