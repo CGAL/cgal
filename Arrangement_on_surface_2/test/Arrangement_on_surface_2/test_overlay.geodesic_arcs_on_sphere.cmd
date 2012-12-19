e1cd4d22d5c383ccd7b9cf73384f745282e9bce8
Efi Fogel <efifogel@gmail.com>
2012-12-19 20:25:58 +0200

    added overlay geodesics test

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_overlay.geodesic_arcs_on_sphere.cmd
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_overlay.geodesic_arcs_on_sphere.cmd
@@ -3,3 +3,4 @@ data/test_overlay/geodesic_arcs_on_sphere/test02.txt
 data/test_overlay/geodesic_arcs_on_sphere/test03.txt
 data/test_overlay/geodesic_arcs_on_sphere/test04.txt
 data/test_overlay/geodesic_arcs_on_sphere/test05.txt
+data/test_overlay/geodesic_arcs_on_sphere/test06.txt
