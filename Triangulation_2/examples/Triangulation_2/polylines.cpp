8700a37c431a7e1a80903510fd2381d693de224e
Andreas Fabri <andreas.fabri@geometryfactory.com>
2013-01-23 15:46:44 +0100

    improve example

--- Triangulation_2/examples/Triangulation_2/polylines.cpp
+++ Triangulation_2/examples/Triangulation_2/polylines.cpp
@@ -2,8 +2,8 @@
 #include <CGAL/Polygon_2.h>
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
 #include <CGAL/Polyline_constrained_triangulation_2.h>
-#include <CGAL/Polyline_constrained_triangulation_vertex_base_2.h>
-#include <list>
+
+#include <vector>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel      K;
 typedef CGAL::Polygon_2<K>                                       Polygon_2;
@@ -13,14 +13,12 @@ typedef CGAL::Constrained_triangulation_face_base_2<K>           Fb;
 typedef CGAL::Triangulation_data_structure_2<CVb,Fb>             TDS;
 typedef CGAL::Exact_predicates_tag                               Itag;
 typedef CGAL::Constrained_Delaunay_triangulation_2<K,TDS, Itag>  CDT;
-typedef CGAL::Polyline_constrained_triangulation_2<>             PCT;
-// equal to CGAL::Polyline_constrained_triangulation_2<CDT>
+typedef CGAL::Polyline_constrained_triangulation_2<CDT>          PCT;
+
 typedef PCT::Point                                               Point;
 typedef PCT::Constraint_id                                       Cid;
 typedef PCT::Vertices_in_constraint                              Vertices_in_constraint;
 typedef PCT::Vertex_handle                                       Vertex_handle;
-typedef PCT::Face_handle                                         Face_handle;
-typedef PCT::Edge                                                Edge;
 
 void 
 print(const PCT& cdt, Cid cid)
@@ -44,22 +42,22 @@ main( )
   std::vector<Point> points;
   points.push_back(Point(1,1));
   points.push_back(Point(5,2));
-  points.push_back(Point(15,3));
+  points.push_back(Point(6,0));
+  points.push_back(Point(3,0));
   Cid id1 = pct.insert_constraint(points.begin(), points.end());
 
   print(pct, id1);
 
   Polygon_2 poly;
-  poly.push_back(Point(15,3));
-  poly.push_back(Point(15,7));
-  poly.push_back(Point(7,7));
+  poly.push_back(Point(2,3));
+  poly.push_back(Point(4,0));
+  poly.push_back(Point(5,0));
+  poly.push_back(Point(6,2));
 
   Cid id2 = pct.insert_constraint(poly);
-  print(pct, id2);
 
-  std::vector<std::list<Point> > polylines;
-  
-  // pct.insert_constraints(polylines.begin(), polylines.end());
+  print(pct, id1);
+  print(pct, id2);
 
   return 0;
 }
