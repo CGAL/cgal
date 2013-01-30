e3f12f64a782c366f9dacc8111470bd32af80df8
Sandeep Kumar Dey <sandeep.kr.dey@gmail.com>
2013-01-30 16:09:48 +0100

    print-sdg for both sdg and sdg_hv
    examples to run:
    (now we can give option -h for sdg_hv)
    ./print-sdg
    Default file from the code and general sdg will be printed
    ./print-sdg -h
    Default file and sdg_hv
    ./print-sdg *.cin
    works for *.cin and prints general sdg
    ./print-sdg *.cin -h
    ./print-sdg -h *.cin
    works for *.cin and prints sdg_hv

--- examples/Segment_Delaunay_graph_Linf_2/print-sdg.cpp
+++ examples/Segment_Delaunay_graph_Linf_2/print-sdg.cpp
@@ -22,38 +22,66 @@ typedef CGAL::Filtered_kernel<CK>         Kernel;
 
 // typedefs for the traits and the algorithm
 #include <CGAL/Segment_Delaunay_graph_Linf_traits_2.h>
+#include <CGAL/Segment_Delaunay_graph_Linf_hv_traits_2.h>
 #include <CGAL/Segment_Delaunay_graph_Linf_2.h>
 
 typedef CGAL::Segment_Delaunay_graph_Linf_traits_2<Kernel>  Gt;
 typedef CGAL::Segment_Delaunay_graph_Linf_2<Gt>             SDG2;
 
+typedef CGAL::Segment_Delaunay_graph_Linf_hv_traits_2<Kernel>  Gt_hv;
+typedef CGAL::Segment_Delaunay_graph_Linf_2<Gt_hv>             SDG2_HV;
+
 using namespace std;
 
 int main( int argc, char *argv[] ) {
-  if ( not (( argc == 1 ) or (argc == 2)) ) {
-    std::cout <<"usage: "<< argv[0] <<" [filename]\n";
+  bool use_hv = false;
+  int fileat = 0;
+  if ( argc >= 4 ) {
+    std::cout <<"usage: "<< argv[0] <<" [filename]\n" <<
+    "[-h] option for sdg_hv" << std::endl;
   }
-
-  ifstream ifs( (argc == 1) ? "data/sites2.cin" : argv[1] );
+  use_hv = (argc == 1) ? false :
+  (argc == 2 and (argv[1][0] == '-' and argv[1][1] == 'h') ) ? true :
+  (argc == 3 and ( (argv[1][0] == '-' and argv[1][1] == 'h')
+                 or(argv[2][0] == '-' and argv[2][1] == 'h') )) ? true : false;
+  fileat = (argc == 1) ? 0 :
+  (argc == 2) ? (use_hv == true ? 0 : 1) :
+  (argc == 3 and (argv[1][0] == '-' and argv[1][1] == 'h')) ? 1 : 2;
+  
+  ifstream ifs( (fileat == 0) ? "data/sites2.cin" :
+                (fileat == 1) ? argv[1] : argv[2] );
   assert( ifs );
 
   SDG2          sdg;
+  SDG2_HV       sdg_hv;
   SDG2::Site_2  site;
 
   // read the sites from the stream and insert them in the diagram
-  while ( ifs >> site ) { sdg.insert( site ); }
-
+  if( use_hv ) {
+    while ( ifs >> site ) {
+      sdg_hv.insert( site );
+    }
+  } else {
+    while ( ifs >> site ) {
+      sdg.insert( site );
+    }
+  }
+  
   ifs.close();
 
 
-  std::cout << "About to validate diagram ..." << std::endl;
+  //std::cout << "About to validate diagram ..." << std::endl;
 
   // validate the diagram
   //assert( sdg.is_valid(true, 1) );
-  cout << endl << endl;
+  //cout << endl << endl;
 
-  std::cout << "Diagram validated." << std::endl;
-
+  //std::cout << "Diagram validated." << std::endl;
+  (use_hv) ?
+  std::cout << "About to print sdg_hv for input file: " << ((fileat == 0) ?
+               "data/sites2.cin" : (fileat == 1) ? argv[1] : argv[2]) << std::endl :
+  std::cout << "About to print sdg for input file: " << ((fileat == 0) ?
+               "data/sites2.cin" : (fileat == 1) ? argv[1] : argv[2]) << std::endl ;
   sdg.file_output_verbose(std::cout);
   
   return 0;
