600fed1a549ebb3ec5966f17d900f847e624b7cf
Sébastien Loriot <sebastien.loriot@cgal.org>
2013-02-11 10:42:27 +0100

    initialize a std::size_t to max() instead of -1
    follow up of b641753c0ca42853f5a55f3692bc08f39bee2fdb

--- Surface_modeling/include/CGAL/Deform_mesh.h
+++ Surface_modeling/include/CGAL/Deform_mesh.h
@@ -30,6 +30,7 @@
 #include <Eigen/Eigen>
 #include <Eigen/SVD>
 
+#include <limits>
 
 namespace CGAL {
 
@@ -219,7 +220,8 @@ public:
 
     // initialize the indices of ros vertices 
     ros_id.clear();
-    ros_id.resize(boost::num_vertices(polyhedron), -1);
+    // we use the max number representable using std::size_t to refer to a new element
+    ros_id.resize(boost::num_vertices(polyhedron), std::numeric_limits<std::size_t>::max());
     std::size_t ros_idx;
     for ( ros_idx = 0; ros_idx < ros.size(); ros_idx++)
     {
@@ -234,7 +236,7 @@ public:
       {
         vertex_descriptor vt = boost::source(*e, polyhedron);
         std::size_t idx = get(vertex_index_map, vt);
-        if ( !is_roi[idx] && ros_id[idx] == -1 )    // neighboring vertices outside roi && not visited 
+        if ( !is_roi[idx] && ros_id[idx] == std::numeric_limits<std::size_t>::max() )    // neighboring vertices outside roi && not visited
         {
           ros.push_back(vt);
           ros_id[idx] = ros_idx++;
