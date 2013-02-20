667b9e036ddd7793e25f77ec937db8a45e39ec1e
iyaz <ilkeryaz@gmail.com>
2013-02-20 02:14:23 +0200

    Adding header and config file for Custom_manipulated_frame, in Cmake custom_manipulated_frame is linked with scene_edit_polyhedron_item rather than edit_polyhedron_plugin

--- Polyhedron/demo/Polyhedron/Custom_manipulated_frame.cpp
+++ Polyhedron/demo/Polyhedron/Custom_manipulated_frame.cpp
@@ -1,38 +1,28 @@
 #include "Custom_manipulated_frame.h"
 
-CustomManipulatedFrame::CustomManipulatedFrame() : isHoldingMouse(false)
+CustomManipulatedFrame::CustomManipulatedFrame(int timer_msec) : isHoldingMouse(false)
 {
-  std::cerr << "-----------------------------------------" << std::endl;
-  std::cerr << "Construct" << std::endl;
-  std::cerr << "-----------------------------------------" << std::endl;
   timer = new QTimer(this);
   connect(timer, SIGNAL(timeout()), this, SLOT(timerUpdate()));
-  timer->start(1000);
+  timer->start(timer_msec);
 }
 
 void CustomManipulatedFrame::mousePressEvent(QMouseEvent* const event, qglviewer::Camera* const camera)
 {
-  std::cerr << "-----------------------------------------" << std::endl;
-  std::cerr << "Holding TRUE" << std::endl;
-  std::cerr << "-----------------------------------------" << std::endl;
   isHoldingMouse = true;
   ManipulatedFrame::mousePressEvent(event, camera);
 }
 
 void CustomManipulatedFrame::mouseReleaseEvent(QMouseEvent* const event, qglviewer::Camera* const camera)
 {
-  std::cerr << "-----------------------------------------" << std::endl;
-  std::cerr << "Holding FALSE" << std::endl;
-  std::cerr << "-----------------------------------------" << std::endl;
   isHoldingMouse = false;
   ManipulatedFrame::mouseReleaseEvent(event, camera);
 }
 
 void CustomManipulatedFrame::timerUpdate()
 {
-  std::cerr << "Timer UPDATE "<<isHoldingMouse << std::endl;
   if(isHoldingMouse)
-    Q_EMIT manipulated();
+  { emit modified(); }
 }
 
 #include "Custom_manipulated_frame.moc"
