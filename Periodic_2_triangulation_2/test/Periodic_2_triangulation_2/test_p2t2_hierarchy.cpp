c79093892aca9756939230a1123eed2150469b5e
Nico Kruithof <Nico@nghk.nl>
2013-03-20 07:54:32 +0100

    Check Hierarchy with other kernels

--- Periodic_2_triangulation_2/test/Periodic_2_triangulation_2/test_p2t2_hierarchy.cpp
+++ Periodic_2_triangulation_2/test/Periodic_2_triangulation_2/test_p2t2_hierarchy.cpp
@@ -39,50 +39,46 @@ typedef CGAL::Periodic_2_Delaunay_triangulation_2<PTT1,Tds1>         PDT1;
 // Explicit instantiation of the whole class :
 template class CGAL::Periodic_2_triangulation_hierarchy_2<PDT1>;
 
-// #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-// typedef CGAL::Exact_predicates_exact_constructions_kernel            K2;
-// typedef CGAL::Periodic_2_triangulation_traits_2<K2>                  PTT2;
-// typedef CGAL::Periodic_2_triangulation_vertex_base_2<PTT2>           DSVB2;
-// typedef CGAL::Periodic_2_triangulation_face_base_2<PTT2>             DSFB2;
-// typedef CGAL::Triangulation_vertex_base_2<PTT2,DSVB2>                VBB2;
-// typedef CGAL::Triangulation_hierarchy_vertex_base_2<VBB2>            VB2;
-// typedef CGAL::Triangulation_face_base_2<PTT2,DSFB2>                  FB2;
-// typedef CGAL::Triangulation_data_structure_2<VB2,FB2>                Tds2;
-// typedef CGAL::Periodic_2_Delaunay_triangulation_2<PTT2,Tds2>         PDT2;
-// // Explicit instantiation of the whole class :
-// template class CGAL::Periodic_2_triangulation_hierarchy_2<PDT2>;
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+typedef CGAL::Exact_predicates_exact_constructions_kernel            K2;
+typedef CGAL::Periodic_2_triangulation_traits_2<K2>                  PTT2;
+typedef CGAL::Periodic_2_triangulation_vertex_base_2<PTT2>           DSVB2;
+typedef CGAL::Periodic_2_triangulation_face_base_2<PTT2>             DSFB2;
+typedef CGAL::Triangulation_vertex_base_2<PTT2,DSVB2>                VBB2;
+typedef CGAL::Triangulation_hierarchy_vertex_base_2<VBB2>            VB2;
+typedef CGAL::Triangulation_face_base_2<PTT2,DSFB2>                  FB2;
+typedef CGAL::Triangulation_data_structure_2<VB2,FB2>                Tds2;
+typedef CGAL::Periodic_2_Delaunay_triangulation_2<PTT2,Tds2>         PDT2;
+// Explicit instantiation of the whole class :
+template class CGAL::Periodic_2_triangulation_hierarchy_2<PDT2>;
 
-// #include <CGAL/MP_Float.h>
-// #include <CGAL/Simple_homogeneous.h>
-// typedef CGAL::Simple_homogeneous<CGAL::MP_Float>                     K3;
-// typedef CGAL::Periodic_2_triangulation_traits_2<K3>                  PTT3;
-// typedef CGAL::Periodic_2_triangulation_vertex_base_2<PTT3>           DSVB3;
-// typedef CGAL::Periodic_2_triangulation_face_base_2<PTT3>             DSFB3;
-// typedef CGAL::Triangulation_vertex_base_2<PTT3,DSVB3>                VBB3;
-// typedef CGAL::Triangulation_hierarchy_vertex_base_2<VBB3>            VB3;
-// typedef CGAL::Triangulation_face_base_2<PTT3,DSFB3>                  FB3;
-// typedef CGAL::Triangulation_data_structure_2<VB3,FB3>                Tds3;
-// typedef CGAL::Periodic_2_Delaunay_triangulation_2<PTT3,Tds3>         PDT3;
-// // Explicit instantiation of the whole class :
-// template class CGAL::Periodic_2_triangulation_hierarchy_2<PDT3>;
+#include <CGAL/MP_Float.h>
+#include <CGAL/Simple_homogeneous.h>
+typedef CGAL::Simple_homogeneous<CGAL::MP_Float>                     K3;
+typedef CGAL::Periodic_2_triangulation_traits_2<K3>                  PTT3;
+typedef CGAL::Periodic_2_triangulation_vertex_base_2<PTT3>           DSVB3;
+typedef CGAL::Periodic_2_triangulation_face_base_2<PTT3>             DSFB3;
+typedef CGAL::Triangulation_vertex_base_2<PTT3,DSVB3>                VBB3;
+typedef CGAL::Triangulation_hierarchy_vertex_base_2<VBB3>            VB3;
+typedef CGAL::Triangulation_face_base_2<PTT3,DSFB3>                  FB3;
+typedef CGAL::Triangulation_data_structure_2<VB3,FB3>                Tds3;
+typedef CGAL::Periodic_2_Delaunay_triangulation_2<PTT3,Tds3>         PDT3;
+// Explicit instantiation of the whole class :
+template class CGAL::Periodic_2_triangulation_hierarchy_2<PDT3>;
 
-// #include <CGAL/_test_cls_periodic_3_delaunay_3.h>
 int main()
 {
-  // TODO(NGHK): Re-enable
-
   typedef CGAL::Periodic_2_triangulation_hierarchy_2< PDT1 > P2T2_1;
   test<P2T2_1>();
   test_nearest<P2T2_1>();
-  //_test_cls_periodic_2_delaunay_2( P2T2_1() );
 
-  // typedef CGAL::Periodic_2_triangulation_hierarchy_2< PDT2 > P2T2_2;
-  // // this takes too much time for the test suite.
-  // //_test_cls_periodic_2_delaunay_2( P2T2_2() );
+  typedef CGAL::Periodic_2_triangulation_hierarchy_2< PDT2 > P2T2_2;
+  test<P2T2_2>();
+  test_nearest<P2T2_2>();
 
-  // typedef CGAL::Periodic_2_triangulation_hierarchy_2< PDT3 > P2T2_3;
-  // // this takes too much time for the test suite.
-  // //_test_cls_periodic_2_delaunay_2( P2T2_3(), true );
+  typedef CGAL::Periodic_2_triangulation_hierarchy_2< PDT3 > P2T2_3;
+  test<P2T2_3>();
+  test_nearest<P2T2_3>();
 
   return 0;
 }
