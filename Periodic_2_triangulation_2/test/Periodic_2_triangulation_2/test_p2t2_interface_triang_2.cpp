aa38449c5589de87b0e1a644537f64ad526463e5
Nico Kruithof <Nico@nghk.nl>
2013-03-20 07:54:14 +0100

    Check compilation of the periodic triangulation with other kernels.

--- Periodic_2_triangulation_2/test/Periodic_2_triangulation_2/test_p2t2_interface_triang_2.cpp
+++ Periodic_2_triangulation_2/test/Periodic_2_triangulation_2/test_p2t2_interface_triang_2.cpp
@@ -2,6 +2,20 @@
 #include "interface_test.h"
 #include <CGAL/Periodic_2_triangulation_2.h>
 
+
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+typedef CGAL::Exact_predicates_exact_constructions_kernel            K2;
+typedef CGAL::Periodic_2_triangulation_traits_2<K2>                  PTT2;
+// Explicit instantiation of the whole class :
+template class CGAL::Periodic_2_Delaunay_triangulation_2<PTT2>;
+
+#include <CGAL/MP_Float.h>
+#include <CGAL/Simple_homogeneous.h>
+typedef CGAL::Simple_homogeneous<CGAL::MP_Float>                     K3;
+typedef CGAL::Periodic_2_triangulation_traits_2<K3>                  PTT3;
+// Explicit instantiation of the whole class :
+template class CGAL::Periodic_2_Delaunay_triangulation_2<PTT3>;
+
 int main() {
   typedef Periodic_2_triangulation_2<Gt>              P2T2;
   typedef Periodic_2_Delaunay_triangulation_2<Gt>     DP2T2;
