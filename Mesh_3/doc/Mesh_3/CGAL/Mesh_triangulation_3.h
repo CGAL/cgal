508084ac7ca5a8489d882be746793a596023a0d3
Clement Jamin <clement.jamin@inria.fr>
2013-03-28 11:21:46 +0100

    Updated Mesh_triangulation_3 doc.

--- Mesh_3/doc/Mesh_3/CGAL/Mesh_triangulation_3.h
+++ Mesh_3/doc/Mesh_3/CGAL/Mesh_triangulation_3.h
@@ -3,7 +3,7 @@ namespace CGAL {
 /*!
 \ingroup PkgMesh_3MeshClasses
 
-The class `Mesh_triangulation_3` provides a default triangulation to be used as the 3D 
+The class `Mesh_triangulation_3` is a metafunctor which provides the triangulation to be used as the 3D 
 triangulation of a mesh generation process. 
 
 \tparam MD stands for a model of `MeshDomain_3`. 
@@ -15,11 +15,20 @@ and defaults to `Kernel_traits<MD>::Kernel`.
 algorithms. Possible values are `CGAL::Sequential_tag` (the default) and
 `CGAL::Parallel_tag`.
 
+\tparam Vertex_base stands for a model of `MeshVertexBase_3` 
+and defaults to `Mesh_vertex_base_3<Gt, MD>`. 
+
+\tparam Cell_base stands for a model of `MeshCellBase_3`
+and defaults to `Mesh_cell_base_3<Gt, MD>`. 
+
 \sa `CGAL::make_mesh_3()` 
 \sa `CGAL::Mesh_complex_3_in_triangulation_3<Tr,CornerIndex,CurveSegmentIndex>` 
 
 */
-template< typename MD, typename Gt, class Concurrency_tag >
+template< typename MD, typename Gt,
+          typename Concurrency_tag,
+          typename Vertex_base,
+          typename Cell_base >
 class Mesh_triangulation_3 {
 public:
 
@@ -29,8 +38,7 @@ public:
 /*! 
 `CGAL::Regular_triangulation_3` type 
 whose vertex and cell classes are respectively 
-`Mesh_vertex_base_3<MD,Gt>` and 
-`Mesh_cell_base_3<MD,Gt>`. 
+`Vertex_base` and `Cell_base`. 
 */ 
 typedef Hidden_type type; 
 
