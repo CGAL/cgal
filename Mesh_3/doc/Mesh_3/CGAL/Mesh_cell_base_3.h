ef60b9361b03a91b1134e1e65874381682ae10db
Jane Tournois <jane.tournois@geometryfactory.com>
2013-03-29 17:15:48 +0100

    fix order of template parameters. Make it consistent with the code

--- Mesh_3/doc/Mesh_3/CGAL/Mesh_cell_base_3.h
+++ Mesh_3/doc/Mesh_3/CGAL/Mesh_cell_base_3.h
@@ -7,12 +7,12 @@ The class `Mesh_cell_base_3` is a model of the concept `MeshCellBase_3`.
 It is designed to serve as cell base class for the 3D triangulation 
 used in the 3D mesh generation process. 
 
+\tparam Gt is the geometric traits class. 
+It has to be a model of the concept `RegularTriangulationTraits_3`. 
+
 \tparam MD provides the types of indices used to identify 
 the faces of the input complex. It has to be a model 
 of the concept `MeshDomain_3`. 
-
-\tparam Gt is the geometric traits class. 
-It has to be a model of the concept `RegularTriangulationTraits_3`. 
 
 \tparam Cb is the cell base class. It has to be a model 
 of the concept `RegularTriangulationCellBase_3` and defaults to 
@@ -23,7 +23,7 @@ of the concept `RegularTriangulationCellBase_3` and defaults to
 \sa `CGAL::Mesh_complex_3_in_triangulation_3<Tr,CornerIndex,CurveSegmentIndex>` 
 
 */
-template< typename MD, typename Gt, typename Cb >
+template< typename Gt,  typename MD, typename Cb >
 class Mesh_cell_base_3 : public Cb {
 public:
 
