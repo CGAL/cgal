c7547ce8ee4d313d8d37b3684eb7dd25c21fc8a2
iyaz <ilkeryaz@gmail.com>
2013-04-04 00:44:32 +0300

    fix member initialization order

--- Surface_modeling/include/CGAL/Deform_mesh.h
+++ Surface_modeling/include/CGAL/Deform_mesh.h
@@ -201,13 +201,14 @@ public:
               double tolerance = 1e-4,
               Weight_calculator weight_calculator = Weight_calculator())
     : polyhedron(polyhedron), vertex_index_map(vertex_index_map), edge_index_map(edge_index_map),
-      iterations(iterations), tolerance(tolerance), weight_calculator(weight_calculator),
+      ros_id_map(std::vector<std::size_t>(boost::num_vertices(polyhedron), (std::numeric_limits<std::size_t>::max)() )),
+      is_roi_map(std::vector<bool>(boost::num_vertices(polyhedron), false)),
+      is_hdl_map(std::vector<bool>(boost::num_vertices(polyhedron), false)),
+      iterations(iterations), tolerance(tolerance),
       need_preprocess_factorization(true), 
       need_preprocess_region_of_solution(true),
       last_preprocess_successful(false),
-      is_roi_map(std::vector<bool>(boost::num_vertices(polyhedron), false)),
-      is_hdl_map(std::vector<bool>(boost::num_vertices(polyhedron), false)),
-      ros_id_map(std::vector<std::size_t>(boost::num_vertices(polyhedron), (std::numeric_limits<std::size_t>::max)() ))
+      weight_calculator(weight_calculator)
   {
     // assign id to each vertex and edge
     vertex_iterator vb, ve;
