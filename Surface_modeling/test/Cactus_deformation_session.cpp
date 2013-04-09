cb3ef39f36fee378f1c143d73451a893b0306f30
iyaz <ilkeryaz@gmail.com>
2013-04-10 00:50:33 +0300

    update test (Cactus_deformation_session.cpp): make it suitable for test performance (not active by default) make it suitable for test suite (precomputed mesh difs active by default)

--- Surface_modeling/test/Cactus_deformation_session.cpp
+++ Surface_modeling/test/Cactus_deformation_session.cpp
@@ -12,6 +12,7 @@
 #include "Property_maps_for_edit_plugin.h"
 
 #include <CGAL/Eigen_solver_traits.h>
+#include <CGAL/Timer.h>
 
 #define CGAL_SUPERLU_ENABLED
 
@@ -20,16 +21,6 @@
 	typedef CGAL::Eigen_solver_traits<Eigen::SuperLU<CGAL::Eigen_sparse_matrix<double>::EigenType> > DefaultSolver;
 #else
 	#include <Eigen/SparseLU>
-	///////////////////////////////////////////////// 
-	namespace CGAL {
-	namespace internal {
-		template <class FT, class EigenMatrix, class EigenOrdering>
-		struct Get_eigen_matrix< ::Eigen::SparseLU<EigenMatrix, EigenOrdering >, FT> {
-			typedef Eigen_sparse_matrix<FT, ::Eigen::ColMajor> type;
-		};
-	} // internal
-	} // CGAL
-	///////////////////////////////////////////////// 
 	typedef CGAL::Eigen_solver_traits<
 			Eigen::SparseLU<
 			  CGAL::Eigen_sparse_matrix<double, Eigen::ColMajor>::EigenType,
@@ -42,7 +33,17 @@ typedef CGAL::Polyhedron_3<Kernel, CGAL::Polyhedron_items_with_id_3>  Polyhedron
 typedef Polyhedron_vertex_deformation_index_map<Polyhedron> Vertex_index_map;
 typedef Polyhedron_edge_deformation_index_map<Polyhedron>   Edge_index_map;
 
-typedef CGAL::Deform_mesh<Polyhedron, DefaultSolver, Vertex_index_map, Edge_index_map> Deform_mesh;
+typedef CGAL::Deform_mesh<Polyhedron, DefaultSolver, Vertex_index_map, Edge_index_map, CGAL::ORIGINAL_ARAP>  Deform_mesh_original;
+typedef CGAL::Deform_mesh<Polyhedron, DefaultSolver, Vertex_index_map, Edge_index_map, CGAL::SPOKES_AND_RIMS> Deform_mesh_spokes;
+
+typedef boost::graph_traits<Polyhedron>::vertex_iterator     vertex_iterator;
+typedef boost::graph_traits<Polyhedron>::edge_iterator       edge_iterator;
+
+const double squared_threshold = 0.0001; // alert if average difs between precomputed and deformed mesh models is above threshold
+
+// #define PERFORMANCE            // define for checking performance
+#define MESH_DIFFERENCE        // define for checking difs with precomputed-saved deformations
+
 
 template <class T>
 std::string toString(const T& t)
@@ -52,7 +53,8 @@ std::string toString(const T& t)
   return ss.str();
 }
 
-Deform_mesh::Handle_group read_rois(Deform_mesh& deform_mesh)
+template<class DeformMesh>
+typename DeformMesh::Handle_group read_rois(DeformMesh& deform_mesh)
 {
    // load handles and roi from txt
   std::ifstream handle_stream("data/cactus_handle.txt"); // there is only one handle in cactus_handle.txt
@@ -63,48 +65,46 @@ Deform_mesh::Handle_group read_rois(Deform_mesh& deform_mesh)
   while(handle_stream >> id) { handles.push_back(id); }
   while(roi_stream >> id) { rois.push_back(id); }
 
-  Deform_mesh::Handle_group active_handle_group = deform_mesh.create_handle_group();
+  typename DeformMesh::Handle_group active_handle_group = deform_mesh.create_handle_group();
 
+  typename DeformMesh::Polyhedron const& polyhedron = deform_mesh.halfedge_graph();
   id = 0;
-  for(Polyhedron::Vertex_iterator it = deform_mesh.polyhedron.vertices_begin(); it != deform_mesh.polyhedron.vertices_end();
-	  ++it, ++id)
+  vertex_iterator vb, ve;  
+  for(boost::tie(vb, ve) = boost::vertices(polyhedron); vb != ve; ++vb, ++id)
 	{
     // not efficient but small poly
     if(std::find(handles.begin(), handles.end(), id) != handles.end()) { 
-      deform_mesh.insert_handle(active_handle_group, it);
+      deform_mesh.insert_handle(active_handle_group, *vb);
     }
 
     if(std::find(rois.begin(), rois.end(), id) != rois.end()) {
-      deform_mesh.insert_roi(it);
+      deform_mesh.insert_roi(*vb);
     }
   }
 
   return active_handle_group;
 }
 
-void compare_mesh(Polyhedron& mesh_1, Polyhedron& mesh_2)
+bool compare_mesh(const Polyhedron& mesh_1, const Polyhedron& mesh_2)
 {
-  Polyhedron::Vertex_iterator it_1 = mesh_1.vertices_begin();
-  Polyhedron::Vertex_iterator it_2 = mesh_2.vertices_begin();
-  double total_dif_x, total_dif_y, total_dif_z;
-  total_dif_x = total_dif_y = total_dif_z = 0;
+  Polyhedron::Vertex_const_iterator it_1 = mesh_1.vertices_begin();
+  Polyhedron::Vertex_const_iterator it_2 = mesh_2.vertices_begin();
+  Kernel::Vector_3 total_dif(0,0,0);
   for( ; it_1 != mesh_1.vertices_end(); ++it_1 , ++it_2)
   {
-    Kernel::Vector_3 dif = (it_1->point() - it_2->point());
-    total_dif_x += std::abs(dif.x());
-    total_dif_y += std::abs(dif.y());
-    total_dif_z += std::abs(dif.z());
+    total_dif = total_dif + (it_1->point() - it_2->point());    
   }
+  total_dif = total_dif / mesh_1.size_of_vertices();
 
-  total_dif_x /= mesh_1.size_of_vertices();
-  total_dif_y /= mesh_1.size_of_vertices();
-  total_dif_z /= mesh_1.size_of_vertices();
+  std::cout << total_dif << std::endl;
 
-  std::cout << total_dif_x << " " << total_dif_y << " " << total_dif_z << std::endl;
+  bool fail = total_dif.squared_length() > squared_threshold;
+  return fail;
 }
 
 // read deformation session saved as a handle differences
-void read_handle_difs_and_deform(Deform_mesh& deform_mesh, Deform_mesh::Handle_group& active_handle_group)
+template<class DeformMesh>
+bool read_handle_difs_and_deform(DeformMesh& deform_mesh, typename DeformMesh::Handle_group& active_handle_group)
 {
   std::ifstream dif_stream("data/cactus_handle_differences.txt");
   std::vector<Kernel::Vector_3> dif_vector;
@@ -113,22 +113,37 @@ void read_handle_difs_and_deform(Deform_mesh& deform_mesh, Deform_mesh::Handle_g
   {
     dif_vector.push_back(Kernel::Vector_3(x, y, z));
   }
-
-  for(int i = 0; i < dif_vector.size(); ++i)
+  CGAL::Timer timer;
+  
+  for(std::size_t i = 0; i < dif_vector.size(); ++i)
   {
+    timer.start();
     deform_mesh.translate(active_handle_group, dif_vector[i]);
     deform_mesh.deform();
-
+    timer.stop();
+#ifdef MESH_DIFFERENCE
     // read pre-deformed cactus
     std::string predeformed_cactus_file = "data/cactus_deformed/cactus_deformed_" + toString(i) + ".off";
     Polyhedron predeformed_cactus;
 	
     std::ifstream(predeformed_cactus_file) >> predeformed_cactus;
-	  compare_mesh(predeformed_cactus, deform_mesh.polyhedron);
+	  bool fail = compare_mesh(predeformed_cactus, deform_mesh.halfedge_graph());
+    if( fail ) { return true; }
+#endif
 	  // for saving deformation
-    //std::ofstream(predeformed_cactus_file) << deform_mesh.polyhedron;
+    //std::ofstream(predeformed_cactus_file) << deform_mesh.halfedge_graph();
     //std::cout << predeformed_cactus_file << std::endl;
   }
+
+#ifdef PERFORMANCE
+  std::cout << "---------------------------------------" << std::endl;
+  std::cout << "Deformation performance: " << timer.time() << std::endl;
+  std::cout << "---------------------------------------" << std::endl; 
+  std::cout << "Press ENTER to continue...";
+  std::cin.get();
+#endif
+
+  return false;
 }
 
 int main()
@@ -136,15 +151,22 @@ int main()
   Polyhedron mesh;
   std::ifstream("data/cactus.off") >> mesh;
 
-  Deform_mesh deform_mesh(mesh, Vertex_index_map(), Edge_index_map()); 
+  Deform_mesh_original deform_mesh_1(mesh, Vertex_index_map(), Edge_index_map()); 
   // load handles and roi from txt
-  Deform_mesh::Handle_group active_handle_group = read_rois(deform_mesh);
+  Deform_mesh_original::Handle_group active_handle_group_1 = read_rois(deform_mesh_1);
+  deform_mesh_1.preprocess();
 
-  deform_mesh.preprocess();
+  bool fail_1 = read_handle_difs_and_deform(deform_mesh_1, active_handle_group_1);
+  if(fail_1) { return EXIT_FAILURE; }
+  std::cout << "ORIGINAL ARAP Success!" << std::endl;
 
-  read_handle_difs_and_deform(deform_mesh, active_handle_group);
+  Deform_mesh_spokes deform_mesh_2(mesh, Vertex_index_map(), Edge_index_map()); 
+  // load handles and roi from txt
+  Deform_mesh_spokes::Handle_group active_handle_group_2 = read_rois(deform_mesh_2);
+  deform_mesh_2.preprocess();
+
+  bool fail_2 = read_handle_difs_and_deform(deform_mesh_2, active_handle_group_2);
+  if(fail_2) { return EXIT_FAILURE; }
+  std::cout << "SPOKES AND RIMS ARAP Success!" << std::endl;
 }
-
-
-
 
