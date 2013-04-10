e7d6352f0ba3853ff9b13288ec188a0a779f3859
Clement Jamin <clement.jamin@inria.fr>
2013-04-10 17:30:12 +0200

    Bugfix: "remove(first, beyond)" was missing from the documentation (cherry picked from commit f69d3e5e88a7a761d2519d17b1b7524b8713f9c4)

--- Triangulation_3/doc/Triangulation_3/CGAL/Regular_triangulation_3.h
+++ Triangulation_3/doc/Triangulation_3/CGAL/Regular_triangulation_3.h
@@ -229,6 +229,17 @@ Removes the vertex `v` from the triangulation.
 */ 
 void remove(Vertex_handle v); 
 
+/*! 
+Removes the vertices specified by the iterator range `[first, beyond)`. 
+The function `remove(Vertex_handle)` is called over each element of the range. 
+The number of vertices removed is returned. 
+\pre (i) all vertices of the range are finite vertices of the triangulation; and (ii) no vertices are repeated in the range. 
+
+\tparam InputIterator must be an input iterator with value type `Vertex_handle`.
+*/ 
+template < typename InputIterator > 
+int remove(InputIterator first, InputIterator beyond); 
+
 /// @} 
 
 /*! \name Queries 
