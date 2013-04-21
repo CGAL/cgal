51b20f8facb919badc6189f955208d15073f8bc3
iyaz <ilkeryaz@gmail.com>
2013-04-22 01:28:27 +0300

    Another model for DeformationClosestRotationTraits_3 from experimental polar decomp code inside Deform_mesh.h

---
+++ Surface_modeling/include/CGAL/internal/Surface_modeling/Deformation_Eigen_polar_closest_rotation_traits_3.h
@@ -0,0 +1,62 @@
+#ifndef CGAL_DEFORMATION_EIGEN_POLAR_CLOSEST_ROTATION_TRAITS_3_H
+#define CGAL_DEFORMATION_EIGEN_POLAR_CLOSEST_ROTATION_TRAITS_3_H
+
+#include <Eigen/Eigen>
+#include <Eigen/SVD>
+
+#include <CGAL/Deformation_Eigen_closest_rotation_traits_3.h>
+
+namespace CGAL {
+/// \ingroup PkgSurfaceModeling
+/// A wrapper class to compute closest rotation to a 3x3 Matrix using `Eigen` library.
+/// The internal computation relies on hybrid system of `Eigen::SelfAdjointEigenSolver<>` and `Eigen::JacobiSVD<>` solvers.
+///
+/// \cgalModels `DeformationClosestRotationTraits_3`
+class Deformation_Eigen_polar_closest_rotation_traits_3 : 
+ public Deformation_Eigen_closest_rotation_traits_3{
+public:
+    
+  /// Computes closest rotation to `m` and places it into `R`
+  /// Warning: it is adapted from previous experimental code, and not checked deeply
+  void compute_closest_rotation(const Matrix& m, Matrix& R)
+  {
+    bool solved = polar_eigen(m, R);
+	
+    if(!solved) { 
+      Deformation_Eigen_closest_rotation_traits_3::compute_closest_rotation(m, R); 
+    }       
+  }
+
+private:
+  // polar decomposition using Eigen, 5 times faster than SVD
+  bool polar_eigen(const Matrix& A, Matrix& R)
+  {
+    if(A.determinant() < 0)
+    { return false; }
+
+    typedef Matrix::Scalar Scalar;
+
+    const Scalar th = std::sqrt(Eigen::NumTraits<Scalar>::dummy_precision());
+
+    Eigen::SelfAdjointEigenSolver<Matrix> eig;
+    feclearexcept(FE_UNDERFLOW);
+    eig.computeDirect(A.transpose()*A);
+    if(fetestexcept(FE_UNDERFLOW) || eig.eigenvalues()(0)/eig.eigenvalues()(2)<th)
+    { return false; }
+
+    Vector S = eig.eigenvalues().cwiseSqrt();
+    R = A  * eig.eigenvectors() * S.asDiagonal().inverse()
+      * eig.eigenvectors().transpose();
+
+    if(std::abs(R.squaredNorm()-3.) > th || R.determinant() < 0)
+    { return false; }
+
+    R.transposeInPlace(); // the optimal rotation matrix should be transpose of decomposition result
+	  return true;
+  }
+  /// \endcond
+
+};
+
+}//namespace CGAL
+#endif // CGAL_DEFORMATION_EIGEN_POLAR_CLOSEST_ROTATION_TRAITS_3_H
