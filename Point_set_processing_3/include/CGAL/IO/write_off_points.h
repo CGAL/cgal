cd0163b95c7ba86ead969b16d7fa25ad0016f52d
iyaz <ilkeryaz@gmail.com>
2013-05-08 15:43:53 +0300

    should be iterator_traits - fixed

--- Point_set_processing_3/include/CGAL/IO/write_off_points.h
+++ Point_set_processing_3/include/CGAL/IO/write_off_points.h
@@ -145,7 +145,7 @@ write_off_points_and_normals(
     make_dereference_property_map(first),
 #else
     make_typed_identity_property_map_by_reference(
-    typename std::input_iterator<ForwardIterator>::value_type()),
+    typename std::iterator_traits<ForwardIterator>::value_type()),
 #endif
     normal_pmap);
 }
