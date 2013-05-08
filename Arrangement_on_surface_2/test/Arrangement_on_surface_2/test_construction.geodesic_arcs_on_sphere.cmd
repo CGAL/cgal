3b351bf48ec762a5d9cf11e9ba6358ef482b1c1d
Efi Fogel <efifogel@gmail.com>
2013-05-08 23:31:50 +0300

    added tests

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_construction.geodesic_arcs_on_sphere.cmd
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_construction.geodesic_arcs_on_sphere.cmd
@@ -2,3 +2,6 @@
 ./data/test_construction/geodesic_arcs_on_sphere/test02.txt
 ./data/test_construction/geodesic_arcs_on_sphere/test03.txt
 ./data/test_construction/geodesic_arcs_on_sphere/test04.txt
+./data/test_construction/geodesic_arcs_on_sphere/test05.txt
+./data/test_construction/geodesic_arcs_on_sphere/test06.txt
+./data/test_construction/geodesic_arcs_on_sphere/test07.txt
