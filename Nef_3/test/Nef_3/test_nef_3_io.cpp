273a1805b2c7644be146e299e1f8c9c3bb543bd1
Sébastien Loriot <sebastien.loriot@cgal.org>
2013-05-20 16:09:03 +0200

    add a test to ensure io are valid

---
+++ Nef_3/test/Nef_3/test_nef_3_io.cpp
@@ -0,0 +1,66 @@
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Homogeneous.h>
+#include <CGAL/Simple_homogeneous.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Lazy_exact_nt.h>
+
+#include <CGAL/Nef_polyhedron_3.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Nef_polyhedron_iostream_3.h>
+
+#include <fstream>
+
+typedef CGAL::Exact_predicates_exact_constructions_kernel EPEC;
+typedef CGAL::internal::Exact_type_selector<int>::Type    FT;
+typedef CGAL::Fraction_traits<FT>::Numerator_type         RT;
+typedef CGAL::Lazy_exact_nt < FT >                        Lazy_FT;
+typedef CGAL::Lazy_exact_nt < RT >                        Lazy_RT;
+typedef CGAL::Cartesian< Lazy_FT >                        C_Lazy_FT;
+typedef CGAL::Simple_cartesian< Lazy_FT >                 SC_Lazy_FT;
+typedef CGAL::Homogeneous< Lazy_RT >                      H_Lazy_RT;
+typedef CGAL::Simple_homogeneous< Lazy_RT >               SH_Lazy_RT;
+
+
+template <class Kernel>
+void test_write_read()
+{
+  typedef CGAL::Nef_polyhedron_3< Kernel > Nef_polyhedron;
+  typedef CGAL::Polyhedron_3< Kernel >     Polyhedron;
+  typedef typename Kernel::Point_3         Point;
+
+  Point p(sqrt(2),      0,      0);
+  Point q(      0,sqrt(2),      0);
+  Point r(      0,      0,sqrt(2));
+  Point s(      0,      0,      0);
+
+  Polyhedron P;
+  P.make_tetrahedron( p, q, r, s);
+  Nef_polyhedron nef_1( P );
+
+  std::ofstream out ("temp.nef");
+  out << nef_1;
+  out.close();
+
+  std::ifstream in ("temp.nef");
+  Nef_polyhedron nef_2;
+  in >> nef_2;
+
+  assert( nef_1 == nef_2);
+}
+
+int main()
+{
+  std::cout << "Testing Exact_predicates_exact_constructions_kernel\n";
+  test_write_read<EPEC>();
+  std::cout << "Testing Cartesian< Lazy_exact_nt<FT> >\n";
+  test_write_read<C_Lazy_FT>();
+  std::cout << "Testing Simple_cartesian< Lazy_exact_nt<FT> >\n";
+  test_write_read<SC_Lazy_FT>();
+  std::cout << "Testing Homogeneous< Lazy_exact_nt<RT> >\n";
+  test_write_read<H_Lazy_RT>();
+  std::cout << "Testing Simple_homogeneous< Lazy_exact_nt<RT> >\n";
+  test_write_read<SH_Lazy_RT>();
+
+  return 0;
+}
