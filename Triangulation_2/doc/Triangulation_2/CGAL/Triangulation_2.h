efec975bd8bbc1d331f4527b9b9c8db16306ae71
Jane Tournois <jane.tournois@geometryfactory.com>
2013-05-28 16:08:05 +0200

    add comment about what inexact_locate returns when triangulation is not in full dimension

--- Triangulation_2/doc/Triangulation_2/CGAL/Triangulation_2.h
+++ Triangulation_2/doc/Triangulation_2/CGAL/Triangulation_2.h
@@ -556,6 +556,7 @@ This function returns a handle on a face that is a good approximation of the exa
 location of `query`, while being faster. 
 Note that it may return a handle on a facet whose interior does not contain 
 `query`.
+When the triangulation has dimension smaller than 2, `hint` is returned.
 */
 Face_handle
 inexact_locate(const Point & query, 
