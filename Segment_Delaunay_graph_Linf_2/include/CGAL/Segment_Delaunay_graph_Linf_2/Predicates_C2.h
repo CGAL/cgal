e464c3d08b8ca6b3d9373d6c2ca7ddf6eaa4d82b
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2013-05-28 15:10:55 +0200

    whitespace, comment removal in SDG Linf predicates
    Signed-off-by: Panagiotis Cheilaris <philaris@cs.ntua.gr>

--- Segment_Delaunay_graph_Linf_2/include/CGAL/Segment_Delaunay_graph_Linf_2/Predicates_C2.h
+++ Segment_Delaunay_graph_Linf_2/include/CGAL/Segment_Delaunay_graph_Linf_2/Predicates_C2.h
@@ -1,25 +1,3 @@
-// Copyright (c) 2003,2004,2005,2006  INRIA Sophia-Antipolis (France) and
-// Notre Dame University (U.S.A.).  All rights reserved.
-//
-// This file is part of CGAL (www.cgal.org); you may redistribute it under
-// the terms of the Q Public License version 1.0.
-// See the file LICENSE.QPL distributed with CGAL.
-//
-// Licensees holding a valid commercial license may use this file in
-// accordance with the commercial license agreement provided with the software.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-//
-// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/trunk/Segment_Delaunay_graph_Linf_2/include/CGAL/Segment_Delaunay_graph_Linf_2/Predicates_C2.h $
-// $Id: Predicates_C2.h 54854 2010-03-22 10:19:22Z mkaravel $
-// 
-//
-// Author(s)     : Menelaos Karavelas <mkaravel@cse.nd.edu>
-
-
-
-
 #ifndef CGAL_SEGMENT_DELAUNAY_GRAPH_LINF_2_PREDICATES_C2_H
 #define CGAL_SEGMENT_DELAUNAY_GRAPH_LINF_2_PREDICATES_C2_H
 
