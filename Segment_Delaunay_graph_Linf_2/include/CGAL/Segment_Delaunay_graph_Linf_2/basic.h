112ec6ce7056abb236416778d519f18859a05e35
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2013-05-28 15:09:49 +0200

    whitespace fix in SDG Linf basic.h
    Signed-off-by: Panagiotis Cheilaris <philaris@cs.ntua.gr>

--- Segment_Delaunay_graph_Linf_2/include/CGAL/Segment_Delaunay_graph_Linf_2/basic.h
+++ Segment_Delaunay_graph_Linf_2/include/CGAL/Segment_Delaunay_graph_Linf_2/basic.h
@@ -1,22 +1,3 @@
-// Copyright (c) 2005,2006  INRIA Sophia-Antipolis (France) and
-// Notre Dame University (U.S.A.).  All rights reserved.
-//
-// This file is part of CGAL (www.cgal.org); you may redistribute it under
-// the terms of the Q Public License version 1.0.
-// See the file LICENSE.QPL distributed with CGAL.
-//
-// Licensees holding a valid commercial license may use this file in
-// accordance with the commercial license agreement provided with the software.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-//
-// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/trunk/Segment_Delaunay_graph_Linf_2/include/CGAL/Segment_Delaunay_graph_Linf_2/basic.h $
-// $Id: basic.h 56668 2010-06-09 08:45:58Z sloriot $
-// 
-//
-// Author(s)     : Menelaos Karavelas <mkaravel@cse.nd.edu>
-
 #ifndef CGAL_SEGMENT_DELAUNAY_GRAPH_LINF_2_BASIC_H
 #define CGAL_SEGMENT_DELAUNAY_GRAPH_LINF_2_BASIC_H
 
@@ -24,6 +5,5 @@
 
 #define CGAL_SEGMENT_DELAUNAY_GRAPH_LINF_2_NS \
   CGAL::SegmentDelaunayGraphLinf_2
-
 
 #endif // CGAL_SEGMENT_DELAUNAY_GRAPH_LINF_2_BASIC_H
