ceab41bda62f5b48a893637197cadeb52ac9534f
Sébastien Loriot <sebastien.loriot@cgal.org>
2013-06-11 11:06:14 +0200

    do not reset item list when replacing an item

--- Polyhedron/demo/Polyhedron/Scene.cpp
+++ Polyhedron/demo/Polyhedron/Scene.cpp
@@ -67,7 +67,7 @@ Scene::replaceItem(Scene::Item_id index, Scene_item* item)
   }
   emit updated();
   itemChanged(index);
-  QAbstractListModel::reset();
+  // QAbstractListModel::reset();
   return item;
 }
 
