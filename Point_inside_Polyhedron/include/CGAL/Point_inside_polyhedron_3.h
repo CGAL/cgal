28839c45438b812d58b0626c367fd42bfc48b1da
iyaz <ilkeryaz@gmail.com>
2013-06-13 00:48:58 +0300

    Vertical ray casting approach is removed from old implementation, cleaned documented with small changes

---
+++ Point_inside_Polyhedron/include/CGAL/Point_inside_polyhedron_3.h
@@ -0,0 +1,108 @@
+#ifndef CGAL_POINT_INSIDE_POLYHEDRON_H
+#define CGAL_POINT_INSIDE_POLYHEDRON_H
+
+#include <CGAL/internal/Point_inside_Polyhedron/Ray_3_Triangle_3_traversal_traits.h>
+
+#include <CGAL/AABB_tree.h>
+#include <CGAL/AABB_traits.h>
+#include <CGAL/point_generators_3.h>
+
+#include <boost/optional.hpp>
+
+namespace CGAL {
+
+/** 
+ * Class providing the functionality of locating query points with respect to closed triangulated polyhedron.
+ * @tparam Polyhedron_3 a %CGAL polyhedron
+ * @tparam Kernel a %CGAL kernel
+ */
+template <class Polyhedron_3, class Kernel>
+class Point_inside_polyhedron_3 {
+// typedefs
+  typedef CGAL::AABB_const_polyhedron_triangle_primitive<Kernel,Polyhedron_3> Primitive;
+  typedef CGAL::AABB_traits<Kernel, Primitive> Traits;
+  typedef typename Traits::Bounding_box Bounding_box;
+  typedef CGAL::AABB_tree<Traits> Tree;
+  typedef typename Kernel::Point_3 Point;
+  typedef typename Kernel::Ray_3 Ray;
+//members
+  Kernel m_kernel;
+  Tree tree;
+
+public:
+  /** 
+   * The constructor of query object. It also processes given polyhedron for multiple queries.
+   * @pre @a polyhedron.is_pure_triangle()
+   * @pre @a polyhedron.is_closed()
+   * @param polyhedron polyhedron on with queries are executed
+   * @param kernel kernel object
+   */
+  Point_inside_polyhedron_3(const Polyhedron_3& polyhedron, const Kernel& kernel=Kernel())
+    : m_kernel(kernel)
+  {
+    CGAL_assertion(polyhedron.is_pure_triangle());
+    CGAL_assertion(polyhedron.is_closed());
+
+    tree.insert(polyhedron.facets_begin(),polyhedron.facets_end());
+    tree.build();
+  }
+
+  /**
+   * Query function to determine point location.
+   * @return 
+   *   - CGAL::ON_BOUNDED_SIDE if the point is inside the polyhedron
+   *   - CGAL::ON_BOUNDARY if the point is on polyhedron
+   *   - CGAL::ON_UNBOUNDED_SIDE if the point is outside polyhedron
+   */
+  Bounded_side operator()(const Point& p) const
+  {
+    const Bounding_box& bbox = tree.bbox();
+
+    if(   p.x() < bbox.xmin() || p.x() > bbox.xmax()
+       || p.y() < bbox.ymin() || p.y() > bbox.ymax()
+       || p.z() < bbox.zmin() || p.z() > bbox.zmax() )
+    {
+      return ON_UNBOUNDED_SIDE;
+    }
+
+    typename Kernel::Construct_ray_3 make_ray = m_kernel.construct_ray_3_object();
+    typename Kernel::Construct_vector_3 make_vector = m_kernel.construct_vector_3_object();
+     
+    Random_points_on_sphere_3<Point> random_point(1.);
+    //the direction of the vertical ray depends on the position of the point in the bbox
+    //in order to limit the expected number of nodes visited.
+    Ray query = make_ray(p, make_vector(0,0,(2*p.z() <  tree.bbox().zmax()+tree.bbox().zmin()?-1:1)));
+
+    boost::optional<Bounded_side> res = is_inside_ray_tree_traversal<Ray,true>(query);
+    while (!res){
+      //retry with a random ray
+      query = make_ray(p, make_vector(CGAL::ORIGIN,*random_point++));
+      res = is_inside_ray_tree_traversal<Ray,false>(query);
+    }
+    return *res;
+  }
+
+private:
+  template <class Query,bool ray_is_vertical>
+  boost::optional<Bounded_side>
+    is_inside_ray_tree_traversal(const Query& query) const 
+  {
+    std::pair<boost::logic::tribool,std::size_t> status( boost::logic::tribool(boost::logic::indeterminate), 0);
+    internal::Ray_3_Triangle_3_traversal_traits<Traits,Polyhedron_3,Kernel,Boolean_tag<ray_is_vertical> > traversal_traits(status);
+    tree.traversal(query, traversal_traits);
+
+    if ( !boost::logic::indeterminate(status.first) )
+    {
+      if (status.first) {
+        return (status.second&1) == 1 ? ON_BOUNDED_SIDE : ON_UNBOUNDED_SIDE;
+      }
+      //otherwise the point is on the facet
+      return ON_BOUNDARY;
+    }
+    return boost::optional<Bounded_side>(); // indeterminate
+  }
+};
+
+} // namespace CGAL
+
+#endif //CGAL_POINT_INSIDE_POLYHEDRON_H
