49a98d1ed96f3b575cc9d3333718d0459ab70b7a
iyaz <ilkeryaz@gmail.com>
2013-06-14 16:15:35 +0300

    (Self intersection) return pair of Facet_const_handle get Kernel object as a parameter

--- Point_inside_Polyhedron/examples/Point_inside_Polyhedron/self_intersection_polyhedron_test.cpp
+++ Point_inside_Polyhedron/examples/Point_inside_Polyhedron/self_intersection_polyhedron_test.cpp
@@ -13,6 +13,7 @@ typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef K::Point_3 Point;
 typedef K::Triangle_3 Triangle;
 typedef CGAL::Polyhedron_3<K> Polyhedron;
+typedef Polyhedron::Facet_const_handle Facet_const_handle;
 
 int main() {
   std::ifstream input("camel.off");
@@ -25,15 +26,15 @@ int main() {
   CGAL::Timer timer;
   timer.start();
 
-  std::vector<Triangle> intersected_tris;
+  std::vector<std::pair<Facet_const_handle, Facet_const_handle> > intersected_tris;
   CGAL::self_intersect<K>(poly, back_inserter(intersected_tris));
 
   std::cerr << "Self-intersection test took " << timer.time() << " sec." << std::endl;
-  std::cerr << intersected_tris.size() << " triangles are intersecting." << std::endl;
+  std::cerr << intersected_tris.size() << " pair of triangles are intersecting." << std::endl;
 
   timer.reset();
   bool intersecting = CGAL::self_intersect<K>(poly);
 
   std::cerr << "Is self-intersection test took " << timer.time() << " sec." << std::endl;
-  std::cerr << (intersecting ? "There is an self-intersection." : "There is no self-intersection.") << std::endl;
+  std::cerr << (intersecting ? "There is a self-intersection." : "There is no self-intersection.") << std::endl;
 }
--- Point_inside_Polyhedron/include/CGAL/Self_intersection_polyhedron_3.h
+++ Point_inside_Polyhedron/include/CGAL/Self_intersection_polyhedron_3.h
@@ -18,22 +18,24 @@ namespace internal {
 template <class Polyhedron, class Kernel, class OutputIterator>
 struct Intersect_facets
 {
-  typedef typename Kernel::Point_3      Point;
-  typedef typename Kernel::Vector_3     Vector;
+// typedefs
   typedef typename Kernel::Segment_3    Segment;
   typedef typename Kernel::Triangle_3   Triangle;
   typedef typename Polyhedron::Facet_const_handle       Facet_const_handle;
-  typedef typename Polyhedron::Facet_const_iterator     Facet_const_iterator;
   typedef typename Polyhedron::Halfedge_const_handle    Halfedge_const_handle;
   typedef typename CGAL::Box_intersection_d::Box_with_handle_d<double, 3, Facet_const_handle> Box;
+// members
   mutable OutputIterator m_iterator;
+  typename Kernel::Construct_segment_3  segment_functor;
+  typename Kernel::Construct_triangle_3 triangle_functor;
+  typename Kernel::Do_intersect_3       do_intersect_3_functor;
 
-public:
-
-  Intersect_facets(OutputIterator it)
-    : m_iterator(it)
-  {
-  }
+  Intersect_facets(OutputIterator it, const Kernel& kernel)
+    : m_iterator(it),
+    segment_functor(kernel.construct_segment_3_object()),
+    triangle_functor(kernel.construct_triangle_3_object()),
+    do_intersect_3_functor(kernel.do_intersect_3_object())
+  { }
 
   void operator()(const Box* b,
     const Box* c) const
@@ -83,42 +85,29 @@ public:
       // found shared vertex: 
       CGAL_assertion(h->vertex() == v->vertex());
       // geometric check if the opposite segments intersect the triangles
-      Triangle t1( h->vertex()->point(),
-  h->next()->vertex()->point(),
-  h->next()->next()->vertex()->point());
-      Triangle t2( v->vertex()->point(),
-  v->next()->vertex()->point(),
-  v->next()->next()->vertex()->point());
-      Segment s1( h->next()->vertex()->point(),
-  h->next()->next()->vertex()->point());
-      Segment s2( v->next()->vertex()->point(),
-  v->next()->next()->vertex()->point());
+      Triangle t1 = triangle_functor( h->vertex()->point(), h->next()->vertex()->point(), h->next()->next()->vertex()->point());
+      Triangle t2 = triangle_functor( v->vertex()->point(), v->next()->vertex()->point(), v->next()->next()->vertex()->point());
 
-      if(CGAL::do_intersect(t1,s2))
+      Segment s1 = segment_functor( h->next()->vertex()->point(), h->next()->next()->vertex()->point());
+      Segment s2 = segment_functor( v->next()->vertex()->point(), v->next()->next()->vertex()->point());
+
+      if(do_intersect_3_functor(t1,s2))
       {
-  *m_iterator++ = t1;
-  *m_iterator++ = t2;
+        *m_iterator++ = std::make_pair(b->handle(), c->handle());
       }
-      else
-  if(CGAL::do_intersect(t2,s1))
-  {
-    *m_iterator++ = t1;
-    *m_iterator++ = t2;
-  }
-  return;
+      else if(do_intersect_3_functor(t2,s1))
+      {
+        *m_iterator++ = std::make_pair(b->handle(), c->handle());
+      }
+      return;
     }
 
     // check for geometric intersection
-    Triangle t1( h->vertex()->point(),
-      h->next()->vertex()->point(),
-      h->next()->next()->vertex()->point());
-    Triangle t2( g->vertex()->point(),
-      g->next()->vertex()->point(),
-      g->next()->next()->vertex()->point());
-    if(CGAL::do_intersect(t1, t2))
+    Triangle t1 = triangle_functor( h->vertex()->point(), h->next()->vertex()->point(), h->next()->next()->vertex()->point());
+    Triangle t2 = triangle_functor( g->vertex()->point(), g->next()->vertex()->point(), g->next()->next()->vertex()->point());
+    if(do_intersect_3_functor(t1, t2))
     {
-      *m_iterator++ = t1;
-      *m_iterator++ = t2;
+      *m_iterator++ = std::make_pair(b->handle(), c->handle());
     }
   } // end operator ()
 }; // end struct Intersect_facets
@@ -136,18 +125,18 @@ struct Throw_at_output {
 }// namespace internal
 
 /** 
- * Function computing self intersecting triangles inside a triangulated polyhedron
- * @pre @a polyhedron.is_pure_triangle()
+ * Detects and reports self-intersections of a triangulated polyhedral surface
+ * @pre @a p.is_pure_triangle()
  *
- * @tparam Kernel a %CGAL kernel
- * @tparam Polyhedron a %CGAL polyhedron
- * @tparam OutputIterator iterator holding `Kernel::Triangle_3`
+ * @tparam Kernel a \cgal kernel
+ * @tparam Polyhedron a \cgal polyhedron
+ * @tparam OutputIterator Output iterator accepting objects of type `std::pair<Polyhedron::Facet_const_handle, Polyhedron::Facet_const_handle>`
  *
- * @param polyhedron polyhedron on which self intersection test is applied
- * @param out iterator over intersecting facets
+ * @param p polyhedron to be checked
+ * @param out all pairs of non-adjacent facets intersecting are put in it
  */
 template <class Kernel, class Polyhedron, class OutputIterator>
-void self_intersect(const Polyhedron& polyhedron, OutputIterator out)
+void self_intersect(const Polyhedron& polyhedron, OutputIterator out, const Kernel& kernel = Kernel())
 {
   CGAL_assertion(polyhedron.is_pure_triangle());
 
@@ -178,28 +167,27 @@ void self_intersect(const Polyhedron& polyhedron, OutputIterator out)
     box_ptr.push_back(&*b);
 
   // compute self-intersections filtered out by boxes
-  internal::Intersect_facets<Polyhedron,Kernel,OutputIterator> intersect_facets(out);
+  internal::Intersect_facets<Polyhedron,Kernel,OutputIterator> intersect_facets(out, kernel);
   std::ptrdiff_t cutoff = 2000;
   CGAL::box_self_intersection_d(box_ptr.begin(), box_ptr.end(),intersect_facets,cutoff);
 }
 
 /** 
- * Function computing whether a polyhedron is self-intersecting or not
- * @pre @a polyhedron.is_pure_triangle()
+ * Checks if a polyhedron is self-intersecting
+ * @pre @a p.is_pure_triangle()
  *
  * @tparam Kernel a %CGAL kernel
  * @tparam Polyhedron a %CGAL polyhedron
  *
- * @param polyhedron polyhedron on which is-self intersecting test is applied
- * @return true if @a polyhedron is self-intersecting
+ * @param p polyhedron to be tested
  */
 template <class Kernel, class Polyhedron>
-bool self_intersect(const Polyhedron& polyhedron)
+bool self_intersect(const Polyhedron& polyhedron, const Kernel& kernel = Kernel())
 {
   try 
   {
     typedef boost::function_output_iterator<internal::Throw_at_output> OutputIterator;
-    self_intersect<Kernel>(polyhedron, OutputIterator()); 
+    self_intersect<Kernel>(polyhedron, OutputIterator(), kernel); 
   }
   catch( internal::Throw_at_output::Throw_at_output_exception& ) 
   { return true; }
