a81fa86acdefe205034480e096e4ccbb30773470
Dror Atariah <dror.atariah@fu-berlin.de>
2013-06-19 10:01:08 +0200

    Updated the example polylines.cpp
    - Changed the construction of the polylines to use the construction
      functors.
    - Provided two possible definitions of the Arrangement traits class. One
      using the defaults and the other using a user-defined kernel and
      SegmentTraits.

--- Arrangement_on_surface_2/examples/Arrangement_on_surface_2/polylines.cpp
+++ Arrangement_on_surface_2/examples/Arrangement_on_surface_2/polylines.cpp
@@ -11,17 +11,34 @@
 #include <list>
 
 #include "arr_print.h"
-
-typedef CGAL::Quotient<CGAL::MP_Float>                  Number_type;
-typedef CGAL::Cartesian<Number_type>                    Kernel;
-typedef CGAL::Arr_segment_traits_2<Kernel>              Segment_traits_2;
-typedef CGAL::Arr_polyline_traits_2<Segment_traits_2>   Traits_2;
+/*
+  Define the Arrangement traits class to be used. You can either use some user
+  defined kernel and SegmentTraits or the defaults.
+ */
+/*
+  Instantiate the traits class using the default SegmentTraits.
+  Note that in this case the Exact_predicates_exact_constructions_kernel
+  is used.
+*/
+typedef CGAL::Arr_polyline_traits_2<>                   Traits_2;
+/*
+  Instantiate the traits class using a user-defined kernel and SegmentTraits
+ */
+// typedef CGAL::Quotient<CGAL::MP_Float>                  Number_type;
+// typedef CGAL::Cartesian<Number_type>                    Kernel;
+// typedef CGAL::Arr_segment_traits_2<Kernel>              Segment_traits_2;
+// typedef CGAL::Arr_polyline_traits_2<Segment_traits_2>   Traits_2;
 typedef Traits_2::Point_2                               Point_2;
 typedef Traits_2::Curve_2                               Polyline_2;
 typedef CGAL::Arrangement_2<Traits_2>                   Arrangement_2;
 
 int main ()
 {
+  Traits_2 traits;
+
+  Traits_2::Construct_curve_2 polyline_construct =
+    traits.construct_curve_2_object();
+
   Arrangement_2         arr;
 
   Point_2               points1[5];
@@ -30,7 +47,7 @@ int main ()
   points1[2] = Point_2 (3, 0);
   points1[3] = Point_2 (4, 4);
   points1[4] = Point_2 (6, 0);
-  Polyline_2            pi1 (&points1[0], &points1[5]);
+  Polyline_2 pi1 = polyline_construct(&points1[0], &points1[5]);
 
   std::list<Point_2>    points2;
   points2.push_back (Point_2 (1, 3));
@@ -43,19 +60,19 @@ int main ()
   points2.push_back (Point_2 (6, 2));
   points2.push_back (Point_2 (5, 3));
   points2.push_back (Point_2 (4, 2));
-  Polyline_2            pi2 (points2.begin(), points2.end());
+  Polyline_2 pi2 = polyline_construct(points2.begin(), points2.end());
 
   std::vector<Point_2>  points3 (4);
   points3[0] = Point_2 (0, 2);
   points3[1] = Point_2 (1, 2);
   points3[2] = Point_2 (3, 6);
   points3[3] = Point_2 (5, 2);
-  Polyline_2            pi3 (points3.begin(), points3.end());
-  
+  Polyline_2 pi3 = polyline_construct(points3.begin(), points3.end());
+
   insert (arr, pi1);
   insert (arr, pi2);
   insert (arr, pi3);
-  
+
   print_arrangement (arr);
   return 0;
 }
