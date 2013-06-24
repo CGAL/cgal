b27c0a4efbde93aa48313ac9cede2e4b88109020
Laurent Rineau <laurent.rineau@cgal.org>
2013-06-24 12:50:27 +0200

    Revert the merge of Periodic_2 into master. It was pushed by error.
    Revert "Merge branch 'master' of ssh://scm.cgal.org/var/git/cgal"
    
    This reverts commit 5ac2c05249e413213d117230c308b00095dadda6, reversing
    changes made to 93b77ce529f4f26be002ff6b0cc284216e4d186d.

--- Periodic_2_triangulation_2/test/Periodic_2_triangulation_2/test_p2t2_delaunay_insert_grid.cpp
+++
@@ -1,8 +0,0 @@
-// Author(s)     : Nico Kruithof  <Nico@nghk.nl>
-
-#define CGAL_USE_DELAUNAY
-
-#include "./test_p2t2_triangulation_insert_grid.cpp"
-
-/// This is to trick the cgal_create_cmake_script
-/// main(
--- Periodic_2_triangulation_2/test/Periodic_2_triangulation_2/test_p2t2_delaunay_insert_vertex.cpp
+++
@@ -1,8 +0,0 @@
-// Author(s)     : Nico Kruithof  <Nico@nghk.nl>
-
-#define CGAL_USE_DELAUNAY 1
-
-#include "./test_p2t2_triangulation_insert_vertex.cpp"
-
-/// This is to trick the cgal_create_cmake_script
-/// main()
