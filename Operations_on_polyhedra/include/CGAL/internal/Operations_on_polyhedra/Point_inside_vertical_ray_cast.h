4620b38ffd3429a1d0536694d48b89f39ce52ec0
Sébastien Loriot <sebastien.loriot@cgal.org>
2013-06-24 17:27:41 +0200

    update include path

--- Operations_on_polyhedra/include/CGAL/internal/Operations_on_polyhedra/Point_inside_vertical_ray_cast.h
+++ Operations_on_polyhedra/include/CGAL/internal/Operations_on_polyhedra/Point_inside_vertical_ray_cast.h
@@ -1,7 +1,7 @@
 #ifndef CGAL_POINT_INSIDE_POLYHEDRON_POINT_INSIDE_VERTICAL_RAY_CAST_H
 #define CGAL_POINT_INSIDE_POLYHEDRON_POINT_INSIDE_VERTICAL_RAY_CAST_H
 
-#include <CGAL/internal/Point_inside_Polyhedron/Ray_3_Triangle_3_traversal_traits.h>
+#include <CGAL/internal/Operations_on_polyhedra/Ray_3_Triangle_3_traversal_traits.h>
 #include <CGAL/point_generators_3.h>
 
 #include <boost/optional.hpp>
