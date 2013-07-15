852d42fb8d4632d15131737284d727a3c041fb35
kanhuang <huangkandiy@gmail.com>
2013-07-14 21:40:34 -0400

    fix bugs in test_naive_visibility.cpp

--- Visibility_2/test/test_naive_visibility.cpp
+++ Visibility_2/test/test_naive_visibility.cpp
@@ -34,10 +34,14 @@ int main() {
         for (int i=1; i <= case_number; i++) {
             std::cout<<"Test "<<i<<" begins"<<std::endl;
             std::string input_arr_file, ans_file;
-            input_arr_file = "data/Arrangement_Test/in" + std::string(i);
-            ans_file = "data/Arrangement_Test/non_regular_out" + std::string(i);
-            std::ifstream input(input_arr_file);
-            std::ifstream ans_input(ans_file);
+            std::stringstream ss("data/Arrangement_Test/in");
+            ss << i;
+            ss >> input_arr_file;
+            std::stringstream ss2("data/Arrangement_Test/non_regular_out");
+            ss2 << i;
+            ss2 >> ans_file;
+            std::ifstream input(input_arr_file.c_str());
+            std::ifstream ans_input(ans_file.c_str());
             Arrangement_2 arr_in, arr_out, arr_vb;
             CGAL::create_arrangement_from_file<Arrangement_2>(arr_in, input);
             CGAL::create_arrangement_from_file<Arrangement_2>(arr_out, ans_input);
@@ -56,21 +60,7 @@ int main() {
         typedef CGAL::Arrangement_2<Traits_2>                           Arrangement_2;
 
         std::cout<<"Kernel: Exact_predicates_exact_constructions"<<std::endl;
-        for (int i=1; i<=case_number; i++) {
-            std::cout<<"Test "<<i<<" begins"<<std::endl;
-            std::string input_arr_file, ans_file;
-            input_arr_file = "data/Arrangement_Test/in" + std::string(i);
-            ans_file = "data/Arrangement_Test/non_regular_out" + std::string(i);
-            std::ifstream input(input_arr_file);
-            std::ifstream ans_input(ans_file);
-            Arrangement_2 arr_in, arr_out, arr_vb;
-            CGAL::create_arrangement_from_file<Arrangement_2>(arr_in, input);
-            CGAL::create_arrangement_from_file<Arrangement_2>(arr_out, ans_input);
 
-            CGAL::Visibility_2::Naive_visibility_2<Arrangement_2, CGAL::Tag_false> vb(arr_in);
-            vb.visibility_region(Point_2(0, 0), face, arr_vb);
-            assert(true == (CGAL::test_are_equal<Arrangement_2>(arr_out, arr_vb)));
-        }
     }
     return 0;
 }
