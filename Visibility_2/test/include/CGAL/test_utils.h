a3187158679450c10a943ab8e17b3b0fa9d0fc94
Francisc <francisc@Ultrabook.(none)>
2013-07-17 20:00:57 +0300

    corrected arrangement comparison function

--- Visibility_2/test/include/CGAL/test_utils.h
+++ Visibility_2/test/include/CGAL/test_utils.h
@@ -29,8 +29,10 @@ bool test_are_equal(const _Arrangement_2 &arr1, const _Arrangement_2 &arr2) {
 	typedef typename Arrangement_2::Geometry_traits_2	  Geometry_traits_2;
 	typedef typename Arrangement_2::Vertex_const_iterator Vertex_const_iterator;
     typedef typename Arrangement_2::Edge_const_iterator   Edge_const_iterator;
+    typedef typename Arrangement_2::Halfedge              Halfedge;
     typedef typename Geometry_traits_2::Segment_2         Segment_2;
 	typedef typename Geometry_traits_2::Point_2	          Point_2;
+
 
     // First make sure they have the same size
     if (arr1.number_of_vertices() != arr2.number_of_vertices()) {
@@ -39,76 +41,36 @@ bool test_are_equal(const _Arrangement_2 &arr1, const _Arrangement_2 &arr2) {
     if (arr1.number_of_edges() != arr2.number_of_edges()) {
         return false;
     }
-
-	Vertex_const_iterator vit_fst, vit_snd;
-	vit_fst = arr1.vertices_begin();
-    vit_snd = arr2.vertices_begin();
-
-    Point_2 min_fst = vit_fst->point();
-    Point_2 min_snd = vit_snd->point();
-    vit_fst++;
-    vit_snd++;
-
-    for (vit_fst ; vit_fst != arr1.vertices_end(); ++vit_fst) {
-        if (vit_fst->point().y() < min_fst.y()) {
-            min_fst = vit_fst->point();
-        }
-        else if (vit_fst->point().y() == min_fst.y() &&
-                vit_fst->point().x() < min_fst.x()) {
-            min_fst = vit_fst->point();
-        }
+    if (arr1.number_of_isolated_vertices() != arr2.number_of_isolated_vertices()) {
+        return false;
+    }
+    if (arr1.number_of_faces() != arr2.number_of_faces()) {
+        return false;
     }
 
-    for (vit_snd ; vit_snd != arr2.vertices_end() ; ++vit_snd) {
-        if (vit_snd->point().y() < min_snd.y()) {
-            min_snd = vit_snd->point();
-        }
-        else if (vit_snd->point().y() == min_snd.y() &&
-                vit_snd->point().x() < min_snd.y()) {
-            min_snd = vit_snd->point();
-        }
+    Edge_const_iterator eit_fst, eit_snd;
+    std::vector<Halfedge> halfedges_fst, halfedges_snd;
+
+    for (eit_fst = arr1.edges_begin(), eit_snd = arr2.edges_begin() ; 
+        eit_fst != arr1.edges_end(), eit_snd != arr2.edges_end() ; 
+        ++eit_fst, ++eit_snd) {
+
+        halfedges_fst.push_back(*eit_fst);
+        halfedges_snd.push_back(*eit_snd);
     }
 
-    // Determine from which edge to start from 
-    // (the one that contains the minimum vertex)
-    Edge_const_iterator eit_fst, eit_snd, eit_fst_mid, eit_snd_mid;
-
-    for (eit_fst = arr1.edges_begin(); eit_fst != arr1.edges_end(); ++eit_fst) {
-        Segment_2 curr = eit_fst->curve();
-        if (curr.source() == min_fst || curr.target() == min_fst) {
-            break;
+    // Compare the two vectors
+    for (unsigned int i = 0 ; i < halfedges_fst.size() ; i++) {
+        Halfedge he_curr = halfedges_fst[i];
+        bool found = false;
+        for (unsigned int j = 0 ; j < halfedges_snd.size() ; j++) {
+            if (he_curr.source()->point() == halfedges_snd[j].source()->point() &&
+                he_curr.target()->point() == halfedges_snd[j].target()->point()) {
+                found = true;
+                break;
+            }
         }
-    }
-
-    for (eit_snd = arr2.edges_begin() ; eit_snd != arr2.edges_end(); ++eit_snd){
-        Segment_2 curr = eit_snd->curve();
-        if (curr.source() == min_snd || curr.target() == min_snd) {
-            break;
-        }
-    }
-    eit_fst_mid = eit_fst;
-    eit_snd_mid = eit_snd;
-
-    // Now we know where to start the edge comparisons from
-    for (eit_fst, eit_snd ; eit_fst != arr1.edges_end(), 
-         eit_snd != arr2.edges_end() ; ++eit_fst, ++eit_snd) {
-
-        Segment_2 seg_fst = eit_fst->curve();
-        Segment_2 seg_snd = eit_snd->curve();
-        std::cout << seg_fst << std::endl;
-        std::cout << seg_snd << std::endl;
-        if ((seg_fst.source() != seg_snd.source() || seg_fst.target() != seg_snd.target()) &&
-            (seg_fst.source() != seg_snd.target() || seg_fst.target() != seg_snd.source())) {
-            return false;
-        }
-    }
-    // Check first edges as well
-    for (eit_fst = arr1.edges_begin(), eit_snd = arr2.edges_begin() ;
-         eit_fst != eit_fst_mid, eit_snd != eit_snd_mid ; ++eit_fst, ++eit_snd){
-
-        Segment_2 seg_fst = eit_fst->curve();
-        Segment_2 seg_snd = eit_snd->curve();
-        if (seg_fst != seg_snd) {
+        if (found == false) {
             return false;
         }
     }
