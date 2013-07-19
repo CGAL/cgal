71c9e2b42707ceedce76a94ac52dfc25f9efa4ce
Marc Glisse <marc.glisse@inria.fr>
2013-07-19 19:50:55 +0200

    Follow some reviewer comments. Untested (the more I rebuild the doc, the longer it takes...).

--- Kernel_d/doc/Kernel_d/CGAL/Epick_d.h
+++ Kernel_d/doc/Kernel_d/CGAL/Epick_d.h
@@ -9,8 +9,9 @@ geometric objects. The parameter `Dimension` is the dimension of the
 ambient Euclidean space. It may be either `Dimension_tag<d>` or
 `Dynamic_dimension_tag`. It supports construction of points from `double`
 %Cartesian coordinates. It provides exact geometric predicates, but
-inexact geometric constructions. The geometric predicates are made exact
-without sacrificing speed thanks to the use of filters.
+the geometric constructions are not guaranteed to be exact. The geometric
+predicates are made exact without sacrificing speed thanks to the use of
+filters.
 
 This kernel is default constructible and copyable. It does not carry any
 state so it is possible to use objects created by one instance with
@@ -40,7 +41,9 @@ represents a point in the Euclidean space
 */
 class Point_d {
 public:
-/*! introduces a point with coordinates (x0, x1, ...). */
+/*! introduces a point with coordinates (x0, x1, ...) where the number of
+    coordinates matches the dimension.
+    \pre `Dimension` is a fixed dimension, not `Dynamic_dimension_tag` */
 Point_d(double x0, double x1, ...);
 
 /*! returns the i'th coordinate of a point.
