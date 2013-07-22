741b937aded3ada20b51ef1679411bb0f5728fdb
Francisc <francisc@Ultrabook.(none)>
2013-07-22 14:38:54 +0300

    added details about algorithm to documentation and removed extended traits

--- Visibility_2/doc/Visibility_2/Concepts/Visibility_2.h
+++ Visibility_2/doc/Visibility_2/Concepts/Visibility_2.h
@@ -6,13 +6,13 @@
 A model of the concept `Visibility_2` offers visibility queries within 
 an Arrangement. 
 
-\cgalHasModel `CGAL::Simple_visibility_2<ArrExtensionTraits_2, Regularization_tag>`
-\cgalHasModel `CGAL::Naive_visibility_2<ArrExtensionTraits_2, Regularization_tag>`
-\cgalHasModel `CGAL::Preprocessed_visibility_2<ArrExtensionTraits_2, Regularization_tag>`
+\cgalHasModel `CGAL::Simple_visibility_2<ArrTraits_2, Regularization_tag>`
+\cgalHasModel `CGAL::Naive_visibility_2<ArrTraits_2, Regularization_tag>`
+\cgalHasModel `CGAL::Preprocessed_visibility_2<ArrTraits_2, Regularization_tag>`
 
-\sa `CGAL::Simple_visibility_2<ArrExtensionTraits_2, Regularization_tag>`
-\sa `CGAL::Naive_visibility_2<ArrExtensionTraits_2, Regularization_tag>`
-\sa `CGAL::Preprocessed_visibility_2<ArrExtensionTraits_2, Regularization_tag>`
+\sa `CGAL::Simple_visibility_2<ArrTraits_2, Regularization_tag>`
+\sa `CGAL::Naive_visibility_2<ArrTraits_2, Regularization_tag>`
+\sa `CGAL::Preprocessed_visibility_2<ArrTraits_2, Regularization_tag>`
 
 */
 class Visibility_2 {
@@ -73,7 +73,7 @@ Visibility_2();
 /*! 
 Constructs a `Visibility_2` object from a given `Arrangement_2` and a given Regularization tag.
 */ 
-Visibility_2(const Input_Arrangement_2& arr);
+Visibility_2(const Input_Arrangement_2 &arr);
 
 /// @}
 
@@ -81,47 +81,46 @@ Visibility_2(const Input_Arrangement_2& arr);
 /// \name functions 
 /// @{
 
-
-
 /*!
-Return whether the object is attachted to an arrangement.
+Returns whether an arrangement is attached to the visibility object
 */
   bool is_attached ();
 
 /*!
-Attaches visibility object to the given arrangement arr.
+Attaches the given arrangement to the visibility object.
 */
-  void attach ( Input_Arrangement_2 arr);
+  void attach (const Input_Arrangement_2 &arr);
 
   
 /*!
-Detaches the object from the arrangement it is currently attached to.
+Detaches the arrangement from the visibility object it is currently attached to
 */
-  void detach ();
+  void detach();
 
 /*!
-Access to the attached Arrangement_2.
+Access to the attached arrangement
 */
   Input_Arrangement_2 arr();
 
 /*! 
 Computes the visibility region for the given query point q. Visibility polygon of q and its face will be saved to out_arr and out_face.
 \pre face is a face of  this->arr()
-\pre p is in the interior of face 
-
+\pre p is in the interior of face
+\pre out_arr is the output arrangement 
+\return a face handle to the bounded face of the output
 */ 
-  void visibility_region(const Point_2& q, const Face_handle& face, Output_Arrangement_2& out_arr, const Out_face_handle& out_face);
+  Face_handle visibility_region(const Point_2& q, const Face_handle& face, Output_Arrangement_2& out_arr);
 
 /*! 
 Computes for the given query point q the visibility region that is on the side of the given halfedge. Visibility polygon of q and its face will be saved to out_arr and out_face.
 \pre half_edge is a half edge of  this->arr()
-\pre p is on halfedge  
-
+\pre p is on halfedge
+\pre out_arr is the output arrangement  
+\return a face handle to the bounded face of the output
 */ 
-  void visibility_region(const Point_2& q, const Halfedge_handle& halfedge, Output_Arrangement_2& out_arr, const Out_face_handle& out_face);
+  Face_handle visibility_region(const Point_2& q, const Halfedge_handle& halfedge, Output_Arrangement_2& out_arr);
 
 /// @}
-
 
 }; /* end Visibility_2 */
 
