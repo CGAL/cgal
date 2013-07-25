aeea58e2511781d1573a790d3d4b4ea6500031da
kanhuang <huangkandiy@gmail.com>
2013-07-25 09:57:36 -0400

    new definition

--- Visibility_2/examples/Visibility_2/simple_visibility_2.cpp
+++ Visibility_2/examples/Visibility_2/simple_visibility_2.cpp
@@ -1,33 +1,43 @@
 #include <CGAL/tags.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <CGAL/Cartesian.h>
-#include <CGAL/Point_2.h>
 #include <CGAL/Arrangement_2.h>
 #include <CGAL/Arr_segment_traits_2.h>
 #include <CGAL/Simple_visibility_2.h>
-#include <vector>
+#include <CGAL/Point_2.h>
+#include <CGAL/Segment_2.h>
+#include <istream>
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel               Kernel;
+typedef CGAL::Point_2<Kernel>                                           Point_2;
+typedef CGAL::Segment_2<Kernel>                                         Segment_2;
 typedef CGAL::Arr_segment_traits_2<Kernel>                              Traits_2;
 typedef CGAL::Arrangement_2<Traits_2>                                   Arrangement_2;
-typedef Arrangement_2::Point_2                                          Point_2;
-typedef Arrangement_2::X_monotone_curve_2                               Segment_2;
-
 
 int main() {
-    Point_2 p(0.5, 2);
+    //create environment
+    Point_2 p1(0, 4), p2(0, 0), p3(3, 2), p4(4, 0), p5(4, 4), p6(1, 2);
+    Segment_2 s[6];
+    s[0] = Segment_2(p1, p2);
+    s[1] = Segment_2(p2, p3);
+    s[2] = Segment_2(p3, p4);
+    s[3] = Segment_2(p4, p5);
+    s[4] = Segment_2(p5, p6);
+    s[5] = Segment_2(p6, p1);
     Arrangement_2 env;
-    int vertice_x[6] = {0, 0, 3, 4, 4, 1};
-    int vertice_y[6] = {4, 0, 2, 0, 4, 2};
-    std::vector<Point_2> points;
-    for (int i = 0; i != 6; i++) {
-        points.push_back(Point_2(vertice_x[i], vertice_y[i]));
-    }
-    for (int i = 0; i != 5; i++) {
-        CGAL::insert(env, Segment_2(points[i], points[i+1]));
-    }
-    Arrangement_2 output;
-    CGAL::Visibility_2::Simple_visibility_2<Arrangement_2, CGAL::Tag_false> simple_visibility(env);
-    simple_visibility.visibility_region(p, env.faces_begin(), output);
+    CGAL::insert_curves(env, &s[0], &s[6]);
+    //visibility query
+    Point_2 query_point(0.5, 2);
+    Arrangement_2::Face_const_handle face = ++env.faces_begin();
+
+    Arrangement_2 non_regular_output;
+    CGAL::Visibility_2::Simple_visibility_2<Arrangement_2, CGAL::Tag_false> non_regular_visibility(env);
+    non_regular_visibility.visibility_region(query_point, face, non_regular_output);
+    std::cout<<"Non-regularized visibility region of p has "<<non_regular_output.number_of_vertices()<<" vertices."<<std::endl;
+
+    Arrangement_2 regular_output;
+    CGAL::Visibility_2::Simple_visibility_2<Arrangement_2, CGAL::Tag_true> regular_visibility(env);
+    regular_visibility.visibility_region(query_point, face, regular_output);
+    std::cout<<"Regularized visibility region of p has "<<regular_output.number_of_vertices()<<" vertices."<<std::endl;
+
 }
 
