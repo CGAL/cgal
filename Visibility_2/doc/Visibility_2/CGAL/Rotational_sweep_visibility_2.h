9d9918895e94456628872f7be071b024ffc8b0b2
kanhuang <huangkandiy@gmail.com>
2013-08-01 10:32:14 -0400

    review update

--- Visibility_2/doc/Visibility_2/CGAL/Rotational_sweep_visibility_2.h
+++ Visibility_2/doc/Visibility_2/CGAL/Rotational_sweep_visibility_2.h
@@ -127,17 +127,18 @@ The visibility region of `q` will be stored in `out_arr`.
 */ 
   Face_handle visibility_region(const Point_2& q, const Face_handle& f, Output_arrangement_2& out_arr);
 
-/*! 
-Computes  the visibility region for the given query point `q` that is on the side of `halfedge`.
-The visibility region of `q` will be stored in `out_arr`.
-\param q is the query point from which the visibility region is computed
-\param halfedge the halfedge on which `q` is located
-\param out_arr is the output arrangement  
-\pre `halfedge` is a half edge of  `this->arr()`
-\pre `q` is on halfedge
-\return the face handle to the face in `out_arr` that represents the visibility region
-*/ 
-  Face_handle visibility_region(const Point_2& q, const Halfedge_handle& halfedge, Output_arrangement_2& out_arr);
+  /*!
+  Computes the visibility region for the given query point `q` that is on `halfedge`.If `q` is an interior point of `halfedge`, the computed visibility region is restricted to the halfplane indicated by `halfedge`. If `q` is an endpoint of `halfedge`, the visibility region is restricted by `halfedge` and its next.
+  The visibility region of `q` will be stored in `out_arr`.
+  \param q is the query point from which the visibility region is computed
+  \param halfedge the halfedge on which `q` is located
+  \param out_arr is the output arrangement
+  \pre `halfedge` is a halfedge of  `this->arr()`
+  \pre `q` is on `halfedge`
+  \pre `q` equals to `halfedge->target()->point()` if `q` is an endpoint of `halfedge`
+  \return the face handle to the face in `out_arr` that represents the visibility region
+  */
+    Face_handle visibility_region(const Point_2& q, const Halfedge_handle& halfedge, Output_arrangement_2& out_arr);
 
 /// @}
 
