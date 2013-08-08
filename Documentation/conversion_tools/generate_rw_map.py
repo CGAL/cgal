b57543fe75d7e8a31a9feab9292338c6af457c6c
Sébastien Loriot <sebastien.loriot@cgal.org>
2013-08-08 11:49:35 +0200

    update the script to include the package name in the rw map

--- Documentation/conversion_tools/generate_rw_map.py
+++ Documentation/conversion_tools/generate_rw_map.py
@@ -22,13 +22,18 @@ f = codecs.open("res.txt", 'w', encoding='utf-8')
 
 def only_html(text):
   return text.split("/")[-1].split("#")[0]
-  
+def only_html_and_prefix(text):
+  address_split=text.split("/")
+  pkg=address_split[-2]
+  html=address_split[-1].split("#")[0]
+  return pkg+"/"+html
 
 for el in pq(u).find('li'):
   html_text = pq(el).html()
   res = re_new_old_urls.search(html_text)
   if res:
-    f.write(only_html(res.group(2))+' '+only_html(res.group(1))+"\n")
+    f.write(only_html_and_prefix(res.group(2))+' '+only_html_and_prefix(res.group(1))+"\n")
+    #f.write(only_html(res.group(2))+' '+only_html(res.group(1))+"\n")
     #print "https://cgal.geometryfactory.com/CGAL/test-old-doc-rewrite-rules/"+res.group(2)
     #print res.group(2).split("/")[1], res.group(1).split("/")[1]
   else:
