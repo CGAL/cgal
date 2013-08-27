96df7dd38c83845a12a7e69cc902eeae66c2150f
kanhuang <huangkandiy@gmail.com>
2013-08-27 10:34:23 -0400

    add funnel sort for needles

---
+++ Visibility_2/test/Visibility_2/test_rotational_visibility.cpp
@@ -0,0 +1,80 @@
+// Copyright (c) 2013 Technical University Braunschweig (Germany).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+//
+// Author(s):  Francisc Bungiu <fbungiu@gmail.com>
+//             Kan Huang <huangkandiy@gmail.com>
+
+#include <CGAL/basic.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/Gmpq.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Arr_segment_traits_2.h>
+#include <CGAL/Arrangement_2.h>
+#include <CGAL/test_model_methods.h>
+#include <CGAL/test_utils.h>
+#include <CGAL/Rotational_sweep_visibility_2_.h>
+
+#include <iostream>
+#include <fstream>
+
+int main() {
+//{
+//    typedef CGAL::Gmpq                                              Number_type;
+//    typedef CGAL::Cartesian<Number_type> 							Kernel;
+//    typedef CGAL::Arr_segment_traits_2<Kernel> 						Traits_2;
+//    typedef Traits_2::Point_2										Point_2;
+//    typedef Traits_2::X_monotone_curve_2							Segment_2;
+//    typedef CGAL::Arrangement_2<Traits_2>							Arrangement_2;
+    
+//    {
+//      typedef CGAL::Naive_visibility_2<Arrangement_2, CGAL::Tag_false>                                                                    Naive_visibility_2;
+
+//      CGAL::test_model_methods<Naive_visibility_2>();
+//      std::cout << "Running test suite with " << GREEN << "Cartesian" << RESET << " Kernel..." << std::endl;
+//      CGAL::run_tests<Naive_visibility_2>(10, 2);
+//    }{
+//      typedef CGAL::Naive_visibility_2<Arrangement_2, CGAL::Tag_true>                                                                    Naive_visibility_2;
+
+//      CGAL::test_model_methods<Naive_visibility_2>();
+//      std::cout << "Running test suite with " << GREEN << "Cartesian" << RESET << " Kernel..." << std::endl;
+//      CGAL::run_tests<Naive_visibility_2>(10, 2);
+//    }
+//}
+{
+    typedef CGAL::Exact_predicates_exact_constructions_kernel       Kernel;
+    typedef CGAL::Arr_segment_traits_2<Kernel>                      Traits_2;
+    typedef Traits_2::Point_2                                       Point_2;
+    typedef Traits_2::X_monotone_curve_2                            Segment_2;
+    typedef CGAL::Arrangement_2<Traits_2>                           Arrangement_2;
+    {
+      typedef CGAL::Rotational_sweep_visibility_2<Arrangement_2, CGAL::Tag_false>
+        RSV;
+      CGAL::test_model_methods<RSV>();
+      std::cout << "Running test suite with " << GREEN << "EPECK" << RESET << " Kernel..." << std::endl;
+      CGAL::run_tests<RSV>(5, 0);
+    }
+    {
+      typedef CGAL::Rotational_sweep_visibility_2<Arrangement_2, CGAL::Tag_true>
+        RSV;
+      CGAL::test_model_methods<RSV>();
+      std::cout << "Running test suite with " << GREEN << "EPECK" << RESET << " Kernel..." << std::endl;
+      CGAL::run_tests<RSV>(5, 0);
+    }
+}
+    return 0;
+}
