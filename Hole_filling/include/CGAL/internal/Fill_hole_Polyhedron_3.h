e7b5b73b598e6f16da158699c3a5934eb8bc92e0
iyaz <ilkeryaz@gmail.com>
2013-08-27 16:15:21 +0300

    Documentation refinements, add Fairing_continuity as a parameter

--- Hole_filling/include/CGAL/internal/Fill_hole_Polyhedron_3.h
+++ Hole_filling/include/CGAL/internal/Fill_hole_Polyhedron_3.h
@@ -13,22 +13,22 @@ namespace CGAL {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/**
- * \ingroup PkgHoleFilling
- * @brief Function triangulating and refining a hole in surface mesh.
- * 
- * @tparam Polyhedron a %CGAL polyhedron
- * @tparam FacetOutputIterator iterator holding 'Polyhedron::Facet_handle' for patch facets.
- * @tparam VertexOutputIterator iterator holding 'Polyhedron::Vertex_handle' for patch vertices.
- *
- * @param[in, out] polyhedron surface mesh which has the hole
- * @param border_halfedge a border halfedge incident to the hole
- * @param[out] facet_out iterator over patch facets
- * @param[out] vertex_out iterator over patch vertices without including boundary
- * @param density_control_factor factor for density where larger values cause denser refinements
- * 
- * @return pair of `facet_out` and `vertex_out`
- */
+/*!
+\ingroup PkgHoleFilling
+@brief Function triangulating and refining a hole in surface mesh.
+
+@tparam Polyhedron a %CGAL polyhedron
+@tparam FacetOutputIterator iterator holding `Polyhedron::Facet_handle` for patch facets.
+@tparam VertexOutputIterator iterator holding `Polyhedron::Vertex_handle` for patch vertices.
+
+@param polyhedron surface mesh which has the hole
+@param border_halfedge a border halfedge incident to the hole
+@param facet_out iterator over patch facets
+@param vertex_out iterator over patch vertices without including boundary
+@param density_control_factor factor for density where larger values cause denser refinements
+
+@return pair of @a facet_out and @a vertex_out
+*/
 template<class Polyhedron, class FacetOutputIterator, class VertexOutputIterator>
 std::pair<FacetOutputIterator, VertexOutputIterator> 
 triangulate_and_refine_hole(
@@ -47,27 +47,27 @@ triangulate_and_refine_hole(
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/**
- * \ingroup PkgHoleFilling
- * @brief Function triangulating, refining and fairing a hole in surface mesh.
- *
- * @tparam SparseLinearSolver a model of SparseLinearAlgebraTraitsWithPreFactor_d and can be omitted if Eigen defined...(give exact models etc)
- * @tparam WeightCalculator a model of "weight model" and default to Cotangent weights
- * @tparam Polyhedron a %CGAL polyhedron
- * @tparam FacetOutputIterator iterator holding 'Polyhedron::Facet_handle' for patch facets.
- * @tparam VertexOutputIterator iterator holding 'Polyhedron::Vertex_handle' for patch vertices.
- *
- * @param polyhedron surface mesh which has the hole
- * @param border_halfedge a border halfedge incident to the hole
- * @param[out] facet_out iterator over patch facets
- * @param[out] vertex_out iterator over patch vertices without including boundary
- * @param weight_calculator function object to calculate weights, default to Cotangent weights and can be omitted
- * @param density_control_factor factor for density where larger values cause denser refinements
- * 
- * @return tuple of 
- *  - bool: true if fairing is successful
- *  - `facet_out`
- *  - `vertex_out`
+/*!
+\ingroup PkgHoleFilling
+@brief Function triangulating, refining and fairing a hole in surface mesh.
+
+@tparam SparseLinearSolver a model of SparseLinearAlgebraTraitsWithPreFactor_d and can be omitted if Eigen defined...(give exact models etc)
+@tparam WeightCalculator a model of FairWeightCalculator and can be omitted to use default Cotangent weights
+@tparam Polyhedron a %CGAL polyhedron
+@tparam FacetOutputIterator iterator holding `Polyhedron::Facet_handle` for patch facets.
+@tparam VertexOutputIterator iterator holding `Polyhedron::Vertex_handle` for patch vertices.
+
+@param polyhedron surface mesh which has the hole
+@param border_halfedge a border halfedge incident to the hole
+@param facet_out iterator over patch facets
+@param vertex_out iterator over patch vertices without including boundary
+@param weight_calculator function object to calculate weights, default to Cotangent weights and can be omitted
+@param density_control_factor factor for density where larger values cause denser refinements
+@param continuity tangential continuity, default to FAIRING_C_1 and can be omitted
+@return tuple of 
+ - bool: true if fairing is successful
+ - @a facet_out
+ - @a vertex_out
  */
 template<class SparseLinearSolver, class WeightCalculator, class Polyhedron, class FacetOutputIterator, class VertexOutputIterator>
 boost::tuple<bool, FacetOutputIterator, VertexOutputIterator>
@@ -78,7 +78,8 @@ triangulate_refine_and_fair_hole(
   VertexOutputIterator vertex_out,
   WeightCalculator weight_calculator,
   double density_control_factor = std::sqrt(2.0),
-  bool use_delaunay_triangulation = false
+  bool use_delaunay_triangulation = false,
+  Fairing_continuity continuity = FAIRING_C_1
   )
 {
   std::vector<typename Polyhedron::Vertex_handle> patch;
@@ -87,7 +88,7 @@ triangulate_refine_and_fair_hole(
     (polyhedron, border_halfedge, facet_out, std::back_inserter(patch), density_control_factor, use_delaunay_triangulation)
               .first;
 
-  bool fair_success = fair<SparseLinearSolver>(polyhedron, patch.begin(), patch.end(), weight_calculator);
+  bool fair_success = fair<SparseLinearSolver>(polyhedron, patch.begin(), patch.end(), weight_calculator, continuity);
 
   vertex_out = std::copy(patch.begin(), patch.end(), vertex_out);
   return boost::make_tuple(fair_success, facet_out, vertex_out);
@@ -103,12 +104,13 @@ triangulate_refine_and_fair_hole(
   VertexOutputIterator vertex_out,
   WeightCalculator weight_calculator,
   double density_control_factor = std::sqrt(2.0),
-  bool use_delaunay_triangulation = false
+  bool use_delaunay_triangulation = false,
+  Fairing_continuity continuity = FAIRING_C_1
   )
 {
   typedef CGAL::internal::Fair_default_sparse_linear_solver::Solver Sparse_linear_solver;
   return triangulate_refine_and_fair_hole<Sparse_linear_solver, WeightCalculator, Polyhedron, FacetOutputIterator, VertexOutputIterator>
-    (polyhedron, border_halfedge, facet_out, vertex_out, weight_calculator, density_control_factor, use_delaunay_triangulation);
+    (polyhedron, border_halfedge, facet_out, vertex_out, weight_calculator, density_control_factor, use_delaunay_triangulation, continuity);
 }
 
 //use default WeightCalculator
@@ -120,12 +122,13 @@ triangulate_refine_and_fair_hole(
   FacetOutputIterator facet_out,
   VertexOutputIterator vertex_out,
   double density_control_factor = std::sqrt(2.0),
-  bool use_delaunay_triangulation = false
+  bool use_delaunay_triangulation = false,
+  Fairing_continuity continuity = FAIRING_C_1
   )
 {
   typedef CGAL::internal::Cotangent_weight_with_voronoi_area_fairing<Polyhedron> Weight_calculator;
   return triangulate_refine_and_fair_hole<SparseLinearSolver, Weight_calculator, Polyhedron, FacetOutputIterator, VertexOutputIterator>
-    (polyhedron, border_halfedge, facet_out, vertex_out,  Weight_calculator(), density_control_factor, use_delaunay_triangulation);
+    (polyhedron, border_halfedge, facet_out, vertex_out,  Weight_calculator(), density_control_factor, use_delaunay_triangulation, continuity);
 }
 
 //use default SparseLinearSolver and WeightCalculator
@@ -137,12 +140,13 @@ triangulate_refine_and_fair_hole(
   FacetOutputIterator facet_out,
   VertexOutputIterator vertex_out,
   double density_control_factor = std::sqrt(2.0),
-  bool use_delaunay_triangulation = false
+  bool use_delaunay_triangulation = false,
+  Fairing_continuity continuity = FAIRING_C_1
   )
 {
   typedef CGAL::internal::Fair_default_sparse_linear_solver::Solver Sparse_linear_solver;
   return triangulate_refine_and_fair_hole<Sparse_linear_solver, Polyhedron, FacetOutputIterator, VertexOutputIterator>
-    (polyhedron, border_halfedge, facet_out, vertex_out, density_control_factor, use_delaunay_triangulation);
+    (polyhedron, border_halfedge, facet_out, vertex_out, density_control_factor, use_delaunay_triangulation, continuity);
 }
 
 } // namespace CGAL
