3bd849e64eb4bc1506dc35b01d4507e0ae5a6acc
iyaz <ilkeryaz@gmail.com>
2013-09-05 01:14:53 +0300

    update example with comments and close files

--- Hole_filling/examples/Hole_filling/refine_polyhedron_example.cpp
+++ Hole_filling/examples/Hole_filling/refine_polyhedron_example.cpp
@@ -38,6 +38,8 @@ int main() {
 
   std::vector<Polyhedron::Facet_handle>  new_facets;
   std::vector<Polyhedron::Vertex_handle> new_vertices;
+  // `facets_begin()` returns `Facet_iterator` which is an iterator over `Facet`, 
+  // what `refine()` requires is an iterator over `Facet_handle`, hence a transformer is used
   CGAL::refine(poly_1, 
     boost::make_transform_iterator(poly_1.facets_begin(), Facet_to_facet_handle()),
     boost::make_transform_iterator(poly_1.facets_end()  , Facet_to_facet_handle()),
@@ -45,11 +47,14 @@ int main() {
   
   std::ofstream poly_1_off("data/poly_1.off");
   poly_1_off << poly_1;
-
+  poly_1_off.close();
+  
   CGAL::refine(poly_2, 
     boost::make_transform_iterator(poly_2.facets_begin(), Facet_to_facet_handle()),
     boost::make_transform_iterator(poly_2.facets_end()  , Facet_to_facet_handle()),
     Nop_out(), Nop_out(), 3.0);
+	
   std::ofstream poly_2_off("data/poly_2.off");
   poly_2_off << poly_2;
+  poly_2_off.close();
 }
