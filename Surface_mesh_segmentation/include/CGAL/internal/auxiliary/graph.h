9d608a86885efde784d9b60b5a5d219aae8a5872
iyaz <ilkeryaz@gmail.com>
2013-09-06 01:11:38 +0300

    temproray work-around compilation prob where sizeof(int) == sizeof(long) -> the same specialization will be defined twice

--- Surface_mesh_segmentation/include/CGAL/internal/auxiliary/graph.h
+++ Surface_mesh_segmentation/include/CGAL/internal/auxiliary/graph.h
@@ -458,9 +458,11 @@ struct Int_to_ptr;
 template<> struct Int_to_ptr<sizeof(int)> {
   typedef int type;
 };
+#if INT_MAX != LONG_MAX
 template<> struct Int_to_ptr<sizeof(long)> {
   typedef long type;
 };
+#endif
 
 
 class Graph
