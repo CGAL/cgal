9384588dda22f22c29012d72d7813f82d4ec1c1c
Clement Jamin <clement.jamin@inria.fr>
2013-09-05 14:10:29 +0200

    Create pkglist_filter.bat (for MSVC) + fix its content to make it silent
    Some messages were polluting the output of the filter.
    Approved by the Release Manager

--- Documentation/pkglist_filter.bat
+++ Documentation/pkglist_filter.bat
@@ -1,18 +1,18 @@
 @echo off
 
-@where python
+@where /q python
 if not errorlevel 1 ( set python=python )
 
-@where python2
+@where /q python2
 if not errorlevel 1 ( set python=python2 )
 
-@where python2.6
+@where /q python2.6
 if not errorlevel 1 ( set python=python2.6 )
 
-@where python2.7
+@where /q python2.7
 if not errorlevel 1 ( set python=python2.7 )
-
-@echo on
 
 :go
 %python% ${CMAKE_BINARY_DIR}/pkglist_filter.py %1
+
+@echo on
