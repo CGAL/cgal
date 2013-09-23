32640b6f9c918cb6748206da9b24188291a9469e
Shihao Wu <shihao.wu312@gmail.com>
2013-09-24 00:06:27 +0800

    change parameters of WLop_AABB_tree

--- Point_set_processing_3/examples/Point_set_processing_3/wlop_simplify_and_regularize_point_set_example.cpp
+++ Point_set_processing_3/examples/Point_set_processing_3/wlop_simplify_and_regularize_point_set_example.cpp
@@ -1,6 +1,6 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/regularize_and_simplify_point_set.h>
-#include <CGAL/wlop_simplify_and_regularize_point_set.h>
+#include <CGAL/wlop_simplify_and_regularize_point_set_test_AABB_tree.h>
+//#include <CGAL/wlop_simplify_and_regularize_point_set.h>
 #include <CGAL/IO/read_xyz_points.h>
 #include <CGAL/IO/write_xyz_points.h>
 #include <CGAL/Timer.h>
@@ -17,8 +17,8 @@ typedef Kernel::Point_3 Point;
 
 int main(void)
 {
-  //const std::string INPUT_FILENAME_WITHOUT_EXT = "data/sphere_20k";
-  const std::string INPUT_FILENAME_WITHOUT_EXT = "data/saint_jean_370K";
+  const std::string INPUT_FILENAME_WITHOUT_EXT = "data/sphere_20k";
+  //const std::string INPUT_FILENAME_WITHOUT_EXT = "data/saint_jean_370K";
   //const std::string INPUT_FILENAME_WITHOUT_EXT = "data/qtr_piston_noise";
 
   // Reads a .xyz point set file in points[], *with normals*.
@@ -33,9 +33,9 @@ int main(void)
   }
 
   //Algorithm parameters
-  const double retain_percentage = 2;   // percentage of points to retain.
-  const double neighbor_radius = 0.03;   // neighbors size.
-  const unsigned int iter_number = 30;     // number of iterations.
+  const double retain_percentage = 5;   // percentage of points to retain.
+  const double neighbor_radius = 0.25;   // neighbors size.
+  const unsigned int iter_number = 35;     // number of iterations.
   const bool need_compute_density = true;  // if needed to compute density.
   
   // Make room for sample points
@@ -47,50 +47,52 @@ int main(void)
   std::cout << "Run algorithm example: " << std::endl;
 
   std::vector<Point> output;
+
   //Run algorithm 
-
   //after add output parameter
-  //CGAL::wlop_simplify_and_regularize_point_set<CGAL::Parallel_tag>(
-  //          points.begin(), 
-  //          points.end(), 
-  //          std::back_inserter(output),
-  //          retain_percentage, 
-  //          neighbor_radius,
-  //          iter_number,
-  //          need_compute_density);
-
-  // long memory = CGAL::Memory_sizer().virtual_size();
-  // std::cout << "total done: " << task_timer.time() << " seconds, " 
-  //          << (memory>>20) << " Mb allocated" << std::endl;
-
-  // //Save point set.
-  // std::ofstream out(INPUT_FILENAME_WITHOUT_EXT + "_WLOPED.xyz");  
-  // if (!out ||
-  //   !CGAL::write_xyz_points(
-  //   out, output.begin(), output.end()))
-  // {
-  //   return EXIT_FAILURE;
-  // }
-
-  //std::vector<Point>::const_iterator sample_points_begin =
-  //CGAL::wlop_simplify_and_regularize_point_set<CGAL::Parallel_tag>(
+  //CGAL::wlop_simplify_and_regularize_point_set<CGAL::Sequential_tag>(
   //points.begin(), 
-  //points.end(),
-  //output);
+  //points.end(), 
+  //std::back_inserter(output),
+  //retain_percentage, 
+  //neighbor_radius,
+  //iter_number,
+  //need_compute_density);
 
+  //long memory = CGAL::Memory_sizer().virtual_size();
+  //std::cout << "total done: " << task_timer.time() << " seconds, " 
+  //        << (memory>>20) << " Mb allocated" << std::endl;
+
+  ////Save point set.
+  //std::ofstream out(INPUT_FILENAME_WITHOUT_EXT + "_WLOPED.xyz");  
+  //if (!out ||
+  //  !CGAL::write_xyz_points(
+  //  out, output.begin(), output.end()))
+  //{
+  //  return EXIT_FAILURE;
+  //}
+
+  /*std::vector<Point>::const_iterator sample_points_begin =
+  CGAL::wlop_simplify_and_regularize_point_set<CGAL::Parallel_tag>(
+  points.begin(), 
+  points.end(),
+  output);*/
+
+
+  //begin
   std::vector<Point>::const_iterator sample_points_begin = 
-    CGAL::regularize_and_simplify_point_set(
-    points.begin(),
-    points.end(),
-    retain_percentage, 
-    neighbor_radius,
-    iter_number,
-    need_compute_density);
+  CGAL::wlop_simplify_and_regularize_point_set<CGAL::Parallel_tag>(
+  points.begin(),
+  points.end(),
+  back_inserter(output),
+  retain_percentage, 
+  neighbor_radius,
+  iter_number,
+  need_compute_density);
 
   long memory = CGAL::Memory_sizer().virtual_size();
   std::cout << "total done: " << task_timer.time() << " seconds, " 
-            << (memory>>20) << " Mb allocated" << std::endl;
-  
+            << (memory>>20) << " Mb allocated" << std::endl;  
   // Copy results to sample points
   std::copy(sample_points_begin,
             static_cast<std::vector<Point>::const_iterator>(points.end()),
@@ -103,6 +105,7 @@ int main(void)
   {
     return EXIT_FAILURE;
   }
+  //end
 
   system("Pause");
 
