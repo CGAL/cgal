e1b3a2d5826c5a63907b1de4515e93a43a052610
Sébastien Loriot <sebastien.loriot@cgal.org>
2013-09-25 18:09:42 +0200

    fix incorrect else

--- Intersections_3/include/CGAL/Intersections_3/intersection_3_1_impl.h
+++ Intersections_3/include/CGAL/Intersections_3/intersection_3_1_impl.h
@@ -538,8 +538,8 @@ intersection(const typename K::Segment_3 &s1,
     #else
     return apply_visitor(L_p_visitor<K>(s1, s2) , *v);
     #endif
-  } else
-    return intersection_return<typename K::Intersect_3, typename K::Segment_3, typename K::Segment_3>();
+  }
+  return intersection_return<typename K::Intersect_3, typename K::Segment_3, typename K::Segment_3>();
 }
 
 template <class K>
