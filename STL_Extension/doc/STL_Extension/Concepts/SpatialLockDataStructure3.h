c734677e9fcc8a154754589a7c412eb39a208003
Clement Jamin <clement.jamin@inria.fr>
2013-10-15 09:43:28 +0200

    Doc fixes: "p" => "point" + removed "embedded"

--- STL_Extension/doc/STL_Extension/Concepts/SpatialLockDataStructure3.h
+++ STL_Extension/doc/STL_Extension/Concepts/SpatialLockDataStructure3.h
@@ -10,7 +10,7 @@ must provide x(), y() and z() functions,
 returning the respective point coordinates as numbers whose type is a 
 model of the concept of `CGAL::RealEmbeddable`.
 
-Note that it is allowed to \"lock too much\". E.g., the embedded data structure 
+Note that it is allowed to \"lock too much\". E.g., the data structure 
 may be a voxel grid and locking a point may be locking the voxel containing this point.
 The only requirement is that when a thread owns the lock of a point, no other
 thread can lock a point with the exact same coordinates.
@@ -31,20 +31,20 @@ public:
   template <typename P3>
   bool is_locked_by_this_thread(const P3 &point);
 
-  /// Try to lock a point in the domain. Returns true if the point is already locked by this thread or if the point could be locked.
+  /// Try to lock `point`. Returns `true` if the point is already locked by this thread or if the point could be locked.
   template <typename P3>
   bool try_lock(const P3 &point);
 
-  /// Try to lock the point `p`. Returns true if the point is already locked by this thread or if the point could be locked.
+  /// Try to lock `point`. Returns `true` if the point is already locked by this thread or if the point could be locked.
   /// \tparam no_spin If true, force non-blocking operation (never blocks). If false, use the default behavior (same as previous function).
   template <bool no_spin, typename P3>
-  bool try_lock(const P3 &p);
+  bool try_lock(const P3 &point);
 
-  /// Unlock all the locations locked by this thread
+  /// Unlock all the locations locked by this thread.
   void unlock_all_points_locked_by_this_thread();
   
-  /// Unlock all the locations locked by this thread except point `p`
+  /// Unlock all the locations locked by this thread except `point`.
   template <typename P3>
-  void unlock_all_tls_locked_locations_but_one_point(const P3 &p);
+  void unlock_all_tls_locked_locations_but_one_point(const P3 &point);
 /// @} 
 };
