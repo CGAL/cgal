9165a9f965d61d6ab52d11bf624d5a28667aaf2a
Marc Glisse <marc.glisse@inria.fr>
2013-11-18 18:09:33 +0100

    Use consistent notations.

--- Kernel_d/doc/Kernel_d/Concepts/Kernel--Lift_to_paraboloid_d.h
+++ Kernel_d/doc/Kernel_d/Concepts/Kernel--Lift_to_paraboloid_d.h
@@ -14,8 +14,8 @@ public:
 
 /*! 
 returns \f$ p = (x_0,\ldots,x_{d-1})\f$ lifted to the paraboloid of 
-revolution which is the point \f$ (p_0, \ldots,p_{d-1},\sum_{0 \le i < 
-d}p_i^2)\f$ in \f$ (d+1)\f$-space. 
+revolution which is the point \f$ (x_0, \ldots,x_{d-1},\sum_{0 \le i <
+d}x_i^2)\f$ in \f$ (d+1)\f$-space.
 */ 
 Kernel_d::Point_d operator()(const Kernel_d::Point_d& 
 p); 
