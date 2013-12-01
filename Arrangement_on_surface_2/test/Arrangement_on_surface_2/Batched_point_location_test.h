5482fd59479ea2cd2f0c1c0237602e46d9e11f00
Eric Berberich <eric.berberich@cgal.org>
2013-12-01 18:01:47 +0100

    move print results into the execution loop

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/Batched_point_location_test.h
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/Batched_point_location_test.h
@@ -209,12 +209,12 @@ verify(InputIterator begin, InputIterator end)
 {
   typedef TopolTraits_T TopolTraits;
 
-  if (m_verbose_level > 1) {
-    for (InputIterator it = begin; it != end; ++it) print_results(*it);
-  }
-
   typename TopolTraits::Default_point_location_strategy pl(m_arr);
   for (InputIterator it = begin; it != end; ++it) {
+
+    if (m_verbose_level > 1)
+      print_results(*it);
+
     // Perform (single) point location.
     Result_type obj = pl.locate(it->first);
 
