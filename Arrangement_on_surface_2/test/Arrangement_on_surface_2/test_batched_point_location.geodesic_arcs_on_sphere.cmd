e79706bb79a0d24ba5fca5a084a4e0a6cd989f78
Efi Fogel <efifogel@gmail.com>
2013-12-01 14:43:54 +0200

    batched point location for geodesic arcs

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_batched_point_location.geodesic_arcs_on_sphere.cmd
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_batched_point_location.geodesic_arcs_on_sphere.cmd
@@ -1,1 +1,2 @@
 ./data/empty.zero ./data/empty.zero ./data/batched_point_location/geodesic_arcs_on_sphere/curves/test01.txt ./data/batched_point_location/geodesic_arcs_on_sphere/queries/test01.txt
+./data/empty.zero ./data/empty.zero ./data/batched_point_location/geodesic_arcs_on_sphere/curves/test02.txt ./data/batched_point_location/geodesic_arcs_on_sphere/queries/test02.txt
