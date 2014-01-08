077fa0c93376bd0e76182cc1e07675276bbd5d70
Sébastien Loriot <sebastien.loriot@cgal.org>
2014-01-08 10:10:21 +0100

    remove trailing whitespaces

--- Surface_mesh_segmentation/doc/Surface_mesh_segmentation/Concepts/SegmentationGeomTraits.h
+++ Surface_mesh_segmentation/doc/Surface_mesh_segmentation/Concepts/SegmentationGeomTraits.h
@@ -5,7 +5,7 @@
 The concept `SegmentationGeomTraits` describes the set of requirements
 of the geometric traits needed by the segmentation functions.
 
-\cgalHasModel All the \cgal Kernels 
+\cgalHasModel All the \cgal Kernels
 
 \cgalRefines AABBGeomTraits
 
@@ -22,40 +22,40 @@ A number type model of `FieldNumberType` for which an overload of the function `
 typedef Hidden_type FT;
 /// @}
 
-/// \name Geometric Types 
+/// \name Geometric Types
 /// @{
 
-/*! 
-The vector type. 
-*/ 
-typedef Hidden_type Point_3; 
+/*!
+The vector type.
+*/
+typedef Hidden_type Point_3;
 
-/*! 
-The vector type. 
-*/ 
-typedef Hidden_type Vector_3; 
+/*!
+The vector type.
+*/
+typedef Hidden_type Vector_3;
 
-/*! 
-The segment type. 
-*/ 
-typedef Hidden_type Segment_3; 
+/*!
+The segment type.
+*/
+typedef Hidden_type Segment_3;
 
-/*! 
-The plane type. 
-*/ 
-typedef Hidden_type Plane_3; 
+/*!
+The plane type.
+*/
+typedef Hidden_type Plane_3;
 
-/*! 
-The ray type. 
-*/ 
-typedef Hidden_type Ray_3; 
+/*!
+The ray type.
+*/
+typedef Hidden_type Ray_3;
 
 /// @}
 
 /// \name Function Object Types
-/// @{ 
+/// @{
 
-/*! 
+/*!
 Function object model of Kernel::Angle_3 for the aforementioned geometric types.
 */
 typedef Hidden_type Angle_3;
@@ -75,27 +75,27 @@ Function object model of Kernel::ConstructCrossProductVector_3 for the aforement
 */
 typedef Hidden_type Construct_cross_product_vector_3;
 
-/*! 
+/*!
 Function object model of Kernel::ConstructNormal_3 for the aforementioned geometric types.
 */
 typedef Hidden_type Construct_normal_3;
 
-/*! 
+/*!
 Function object model of Kernel::ConstructCentroid_3 for the aforementioned geometric types.
 */
 typedef Hidden_type Construct_centroid_3;
 
-/*! 
+/*!
 Function object model of Kernel::ConstructTranslatedPoint_3 for the aforementioned geometric types.
 */
 typedef Hidden_type Construct_translated_point_3;
 
-/*! 
+/*!
 Function object model of Kernel::ConstructScaledVector_3 for the aforementioned geometric types.
 */
 typedef Hidden_type Construct_scaled_vector_3;
 
-/*! 
+/*!
 Function object model of Kernel::ConstructSumOfVectors_3 for the aforementioned geometric types.
 */
 typedef Hidden_type Construct_sum_of_vectors_3;
