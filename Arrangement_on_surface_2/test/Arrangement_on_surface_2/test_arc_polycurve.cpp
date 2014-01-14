91bd9299d1084f722a4173de53e3a89f2ec68471
Waqar Khan <waqark1@gmail.com>
2014-01-14 21:35:48 +0100

    14.01.14, Added a few more test cases

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_arc_polycurve.cpp
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_arc_polycurve.cpp
@@ -37,55 +37,32 @@ typedef Arc_traits_2::Curve_2                                             Arc_se
 typedef Arc_traits_2::X_monotone_curve_2                                  Arc_section_x_monotone_2;
 typedef CGAL::Arrangement_2<Polycurve_arc_traits_2>                       Arc_arrangment_2;
 
-void check_equal(Polycurve_arc_traits_2::Equal_2 equal_2, Polycurve_arc_traits_2::Make_x_monotone_2  make_x_monotone_2)
+void check_equal()
 {
+  Polycurve_arc_traits_2 Polycurve_traits_2;
+  Polycurve_arc_traits_2::Equal_2 equal_2 = Polycurve_traits_2.equal_2_object();
+  Polycurve_arc_traits_2::Construct_x_monotone_curve_2  construct_x_monotone_curve_2 = Polycurve_traits_2.construct_x_monotone_curve_2_object();
 
   Arc_section_2 curve1, curve2, curve3;
 
-  // Create a circular arc that correspond to the upper half of the
-  // circle centered at (1,1) with squared radius 3. We create the
-  // circle with clockwise orientation, so the arc is directed from
-  // (1 - sqrt(3), 1) to (1 + sqrt(3), 1).
-  Kernel::Point_2   c1 = Kernel::Point_2 (1, 1);
-  Kernel::Circle_2  circ1 = Kernel::Circle_2 (c1, 3, CGAL::CLOCKWISE);
-  CoordNT           one_minus_sqrt_3 = CoordNT (1, -1, 3);
-  CoordNT           one_plus_sqrt_3 = CoordNT (1, 1, 3);
-  Arc_point_2       s1 = Arc_point_2 (one_minus_sqrt_3, CoordNT (1));
-  Arc_point_2       t1 = Arc_point_2 (one_plus_sqrt_3, CoordNT (1));
-  curve1= Arc_section_2 (circ1, s1, t1);
-  curve2= Arc_section_2 (circ1, s1, t1);
-  //push the same semi circle again
-  //curves.push_back (Arc_section_2 (circ1, s1, t1));
+  Kernel::Point_2  p1 = Kernel::Point_2 (-5, 0);
+  Kernel::Point_2  mid = Kernel::Point_2 (0, 5);
+  Kernel::Point_2  p2 = Kernel::Point_2 (5, 0);
+  curve1= Arc_section_2 (p1, mid, p2);
+  curve2= Arc_section_2 (p1, mid, p2);
 
-  //make x_monotone
-  std::vector<CGAL::Object> X_monotone_curves;
-  make_x_monotone_2(curve1, std::back_inserter(X_monotone_curves));
-  make_x_monotone_2(curve2, std::back_inserter(X_monotone_curves));
 
-  Arc_section_x_monotone_2 X_monotone_curve1, X_monotone_curve2, X_monotone_curve3 ;
-  CGAL::assign( X_monotone_curve1, X_monotone_curves[ 0 ] );
-  CGAL::assign( X_monotone_curve2, X_monotone_curves[ 1 ] );
+  // //make x_monotone
+   //Polycurve_arc_traits_2::X_monotone_curve_2 xmc1 = construct_x_monotone_curve_2(curve1);
+  // Polycurve_arc_traits_2::X_monotone_curve_2 xmc2 = construct_x_monotone_curve_2(curve2);
+  // Polycurve_arc_traits_2::X_monotone_curve_2 xmc3 = construct_x_monotone_curve_2(curve3);
 
-  //simple equal
-  bool Are_equal = equal_2(X_monotone_curve1, X_monotone_curve2);
+  // //simple equal
+  // bool Are_equal = equal_2(xmc1, xmc1);
+  // std::cout << "Equal_2::Two equal semi circles are computed as: " << ( (Are_equal) ? "Equal" : "Not equal") << std::endl;
 
-  std::cout << "The two semi circles are " << ( (Are_equal) ? "equal" : "Not equal") << std::endl;
-
- // Create a circular arc of the unit circle, directed clockwise from
-  // (-1/2, sqrt(3)/2) to (1/2, sqrt(3)/2). Note that we orient the
-  // supporting circle accordingly.
-  Kernel::Point_2  c6 = Kernel::Point_2 (0, 0);
-  CoordNT          sqrt_3_div_2 = CoordNT (Number_type(0), Number_type(1,2), Number_type(3));
-  Arc_point_2          s6 = Arc_point_2 (Number_type (-1, 2), sqrt_3_div_2);
-  Arc_point_2          t6 = Arc_point_2 (Number_type (1, 2), sqrt_3_div_2);
-  
-  curve3 = Arc_section_2 (c6, 1, CGAL::CLOCKWISE, s6, t6);
-  make_x_monotone_2(curve3, std::back_inserter(X_monotone_curves));
-  CGAL::assign( X_monotone_curve3, X_monotone_curves[ 2 ] );
-
-  Are_equal = equal_2(X_monotone_curve1, X_monotone_curve3);
-
-  std::cout << "The two semi circles are " << ( (Are_equal) ? "equal" : "Not equal") << std::endl;
+  // Are_equal = equal_2(xmc1, xmc3);
+  // std::cout << "Equal_2::Two un-equal semi circles are computed as: " << ( (Are_equal) ? "Equal" : "Not equal") << std::endl;
 }
 
 void check_intersect(Polycurve_arc_traits_2::Make_x_monotone_2  make_x_monotone_2,  Polycurve_arc_traits_2::Intersect_2  intersect_2)
@@ -386,7 +363,7 @@ int main ()
 
 
 
-  check_equal(equal_2, make_x_monotone_2);
+  //check_equal();
   check_intersect(make_x_monotone_2, intersect_2);
   check_compare_end_points_xy_2(compare_endpoints_xy_2, make_x_monotone_2);
   check_split(split_2, make_x_monotone_2);
