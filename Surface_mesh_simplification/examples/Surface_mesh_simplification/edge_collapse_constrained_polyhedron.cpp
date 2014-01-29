235c1cc3a6f7e78bb968f2b32feb872c6ac82c37
Sébastien Loriot <sebastien.loriot@cgal.org>
2014-01-29 19:00:46 +0100

    update example and add new one using the constrained edge map

--- Surface_mesh_simplification/examples/Surface_mesh_simplification/edge_collapse_constrained_polyhedron.cpp
+++ Surface_mesh_simplification/examples/Surface_mesh_simplification/edge_collapse_constrained_polyhedron.cpp
@@ -18,54 +18,50 @@
 #include <CGAL/Unique_hash_map.h>
 
 typedef CGAL::Simple_cartesian<double> Kernel;
-typedef CGAL::Polyhedron_3<Kernel> Surface; 
-
+typedef CGAL::Polyhedron_3<Kernel> Surface;
+typedef boost::graph_traits<Surface const>::edge_descriptor edge_descriptor;
 namespace SMS = CGAL::Surface_mesh_simplification ;
 
 //
-// BGL property map which indicates whether an edge is border OR is marked as non-removable
+// BGL property map which indicates whether an edge is marked as non-removable
 //
-class Constrains_map : public boost::put_get_helper<bool,Constrains_map>
+struct Constrained_edge_map : public boost::put_get_helper<bool,Constrained_edge_map>
 {
-public:
+  typedef boost::readable_property_map_tag      category;
+  typedef bool                                  value_type;
+  typedef bool                                  reference;
+  typedef edge_descriptor                       key_type;
 
-  typedef boost::readable_property_map_tag                    category;
-  typedef bool                                                value_type;
-  typedef bool                                                reference;
-  typedef boost::graph_traits<Surface const>::edge_descriptor key_type;
+  Constrained_edge_map(const CGAL::Unique_hash_map<key_type,bool>& aConstraints)
+    : mConstraints(aConstraints) {}
 
-  Constrains_map() : mConstrains(false) {}
+  reference operator[](key_type const& e) const { return  is_constrained(e); }
 
-  reference operator[](key_type const& e) const { return e->is_border() || is_constrained(e) ; }
-  
-  void set_is_constrained ( key_type const& e, bool is )  { mConstrains[e]=is; }
-  
-  bool is_constrained( key_type const& e ) const { return mConstrains.is_defined(e) ? mConstrains[e] : false ; }
-  
+  bool is_constrained( key_type const& e ) const {
+    return mConstraints.is_defined(e) ? mConstraints[e] : false ; }
+
 private:
-  
-  CGAL::Unique_hash_map<key_type,bool> mConstrains ;
-  
+  const CGAL::Unique_hash_map<key_type,bool>& mConstraints;
 };
 
-int main( int argc, char** argv ) 
+int main( int argc, char** argv )
 {
-  Surface surface; 
-  
+  Surface surface;
+
   std::ifstream is(argv[1]) ; is >> surface ;
 
   // This is a stop predicate (defines when the algorithm terminates).
   // In this example, the simplification stops when the number of undirected edges
   // left in the surface drops below the specified number (1000)
   SMS::Count_stop_predicate<Surface> stop(10);
-     
-  Constrains_map constrains_map ;
-     
-     
-  // This example marks ALL edges as non-removable, but a real world application would mark only selected ones.   
-  for( Surface::Halfedge_iterator eb = surface.halfedges_begin(), ee = surface.halfedges_end() ; eb != ee ; ++ eb ) 
-    constrains_map.set_is_constrained(eb,true);
-     
+
+  // This example marks ALL edges as non-removable, but a real world application would mark only selected ones.
+  CGAL::Unique_hash_map<edge_descriptor,bool> constrained_edge_hmap;
+  for( Surface::Halfedge_iterator eb = surface.halfedges_begin(), ee = surface.halfedges_end() ; eb != ee ; ++ eb )
+    constrained_edge_hmap[eb]=true;
+
+  Constrained_edge_map constrained_edge_map(constrained_edge_hmap);
+
   // This the actual call to the simplification algorithm.
   // The surface and stop conditions are mandatory arguments.
   // The index maps are needed because the vertices and edges
@@ -73,15 +69,15 @@ int main( int argc, char** argv )
   int r = SMS::edge_collapse
             (surface
             ,stop
-            ,CGAL::vertex_index_map(boost::get(CGAL::vertex_external_index,surface)) 
-                  .edge_index_map  (boost::get(CGAL::edge_external_index  ,surface)) 
-                  .edge_is_border_map(constrains_map)
+            ,CGAL::vertex_index_map(boost::get(CGAL::vertex_external_index,surface))
+                  .edge_index_map  (boost::get(CGAL::edge_external_index  ,surface))
+                  .edge_is_constrained_map(constrained_edge_map)
             );
-  
-  std::cout << "\nFinished...\n" << r << " edges removed.\n" 
+
+  std::cout << "\nFinished...\n" << r << " edges removed.\n"
             << (surface.size_of_halfedges()/2) << " final edges.\n" ;
-        
+
   std::ofstream os( argc > 2 ? argv[2] : "out.off" ) ; os << surface ;
-  
-  return 0 ;      
+
+  return 0 ;
 }
