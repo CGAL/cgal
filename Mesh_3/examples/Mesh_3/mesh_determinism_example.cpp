f8df426d1a57388d938a75e06777963764cb4150
Jane Tournois <jane.tournois@geometryfactory.com>
2014-02-14 17:19:23 +0100

    add a macro to mesh_determinism_example to choose mesh domain
    define or undef the macro CGAL_MESH_3_EXAMPLE_POLYHEDRAL_DOMAIN
    to mesh a polyhedral or image domain

--- Mesh_3/examples/Mesh_3/mesh_determinism_example.cpp
+++ Mesh_3/examples/Mesh_3/mesh_determinism_example.cpp
@@ -1,41 +1,60 @@
+#define CGAL_MESH_3_EXAMPLE_POLYHEDRAL_DOMAIN
+
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
-std::size_t TS;
+//std::size_t TS;
 
-template <typename C3T3>
-void incident(const C3T3& c3t3)
-{
-  typedef typename C3T3::Triangulation Tr;
-
- const Tr& tr_ = c3t3.triangulation();
- for(typename Tr::Finite_vertices_iterator it = tr_.finite_vertices_begin();
-          it!= tr_.finite_vertices_end();
-        ++it){
-      typedef std::vector<typename Tr::Facet> Facet_vector;
-      Facet_vector facets;
-      typename Tr::Vertex_handle vh = it; 
-      tr_.finite_incident_facets(vh, std::back_inserter(facets));
-      std::cout << "vertex " << vh->ts << std::endl;
-      for(typename Facet_vector::iterator fit2 = facets.begin() ;
-           fit2 != facets.end() ;
-           ++fit2 ){
-        typename Tr::Cell_handle c = fit2->first;
-        int ii = fit2->second;
-        std::cout << "  f  " << c->ts << " " << ii; 
-        typename Tr::Facet mf = tr_.mirror_facet(*fit2);
-        c = mf.first;
-        ii = mf.second;
-        std::cout << "  n  " << c->ts << " " << ii << std::endl;
-      }      
- }
-
-}
+//template <typename C3T3>
+//void incident(const C3T3& c3t3)
+//{
+//  typedef typename C3T3::Triangulation Tr;
+//
+//  const Tr& tr_ = c3t3.triangulation();
+//  for(typename Tr::Finite_vertices_iterator it = tr_.finite_vertices_begin();
+//    it!= tr_.finite_vertices_end();
+//    ++it)
+//  {
+//    typename C3T3::Vertex_handle v = it;
+//    incident(c3t3,v);
+//  }
+//}
+//
+//template <typename C3T3>
+//void incident(const C3T3& c3t3,
+//  typename C3T3::Vertex_handle vh)
+//{
+//  typedef typename C3T3::Triangulation Tr;
+//
+//  const Tr& tr_ = c3t3.triangulation();
+//  typedef std::vector<typename Tr::Facet> Facet_vector;
+//  Facet_vector facets;
+//
+//  tr_.finite_incident_facets(vh, std::back_inserter(facets));
+//  std::cout << "vertex " << vh->ts << std::endl;
+//  for(typename Facet_vector::iterator fit2 = facets.begin() ;
+//    fit2 != facets.end() ;
+//    ++fit2 )
+//  {
+//    typename Tr::Cell_handle c = fit2->first;
+//    int ii = fit2->second;
+//    std::cout << "  f  " << c->ts << " " << ii;
+//
+//    typename Tr::Facet mf = tr_.mirror_facet(*fit2);
+//    c = mf.first;
+//    ii = mf.second;
+//    std::cout << "  n  " << c->ts << " " << ii << std::endl;
+//  }
+//}
 
 #include <CGAL/Mesh_triangulation_3.h>
 #include <CGAL/Mesh_complex_3_in_triangulation_3.h>
 #include <CGAL/Mesh_criteria_3.h>
 
-#include <CGAL/Labeled_image_mesh_domain_3.h>
+#ifdef CGAL_MESH_3_EXAMPLE_POLYHEDRAL_DOMAIN
+  #include <CGAL/Polyhedral_mesh_domain_with_features_3.h>
+#else
+  #include <CGAL/Labeled_image_mesh_domain_3.h>
+#endif
 #include <CGAL/make_mesh_3.h>
 #include <CGAL/lloyd_optimize_mesh_3.h>
 #include <CGAL/odt_optimize_mesh_3.h>
@@ -47,13 +66,23 @@ void incident(const C3T3& c3t3)
 #include <sstream>
 #include <cstring>
 
+
 // Domain
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+#ifdef CGAL_MESH_3_EXAMPLE_POLYHEDRAL_DOMAIN
+typedef CGAL::Polyhedral_mesh_domain_with_features_3<K> Mesh_domain;
+#else
 typedef CGAL::Labeled_image_mesh_domain_3<CGAL::Image_3,K> Mesh_domain;
+#endif
 
 // Triangulation
 typedef CGAL::Mesh_triangulation_3<Mesh_domain>::type Tr;
+#ifdef CGAL_MESH_3_EXAMPLE_POLYHEDRAL_DOMAIN
+typedef CGAL::Mesh_complex_3_in_triangulation_3<
+  Tr,Mesh_domain::Corner_index,Mesh_domain::Curve_segment_index> C3t3;
+#else
 typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
+#endif
 
 // Mesh Criteria
 typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
@@ -110,9 +139,24 @@ int main(int argc, char* argv[])
     }
   }
 
+#ifdef CGAL_MESH_3_EXAMPLE_POLYHEDRAL_DOMAIN
+  // Domain
+  Mesh_domain domain("data/fandisk.off");
+  
+  // Get sharp features
+  domain.detect_features();
+
+  // Mesh criteria
+  Mesh_criteria criteria(edge_size = 0.02,
+                         facet_angle = 30, facet_size = 0.02, facet_distance = 0.002,
+                         cell_radius_edge_ratio = 3, cell_size = 0.02);
+  //Mesh_criteria criteria(edge_size = 0.025,
+  //                       facet_angle = 25, facet_size = 0.05, facet_distance = 0.005,
+  //                       cell_radius_edge_ratio = 3, cell_size = 0.05);
+#else
   // Domain
   CGAL::Image_3 image;
-  image.read("data/liver.inr");
+  image.read("data/liver.inr.gz");
   Mesh_domain domain(image);
 
   // Mesh criteria
@@ -121,13 +165,14 @@ int main(int argc, char* argv[])
                          facet_distance=1.5,
                          cell_radius_edge_ratio=2, 
                          cell_size=7);//3);
-
+#endif
+  
   CGAL::Timer time;
   double total_time = 0.;
 
   for(std::size_t i = 0; i < nb_runs; ++i)
   {
-    TS = 0;
+//    TS = 0;
 
     CGAL::default_random = CGAL::Random(0);
 
@@ -146,8 +191,6 @@ int main(int argc, char* argv[])
     std::ofstream medit_file(num_str + std::string("out0-refinement.mesh"));
     c3t3.output_to_medit(medit_file);
   
-    incident(c3t3);
-
     //LLOYD
     if(do_lloyd)
     {
@@ -185,7 +228,7 @@ int main(int argc, char* argv[])
       c3t3.output_to_medit(medit_file4);
     }
     std::cout << "[Timer at " << time.time() << " sec]" << std::endl;
-    std::cerr << "TS = " << TS << std::endl;
+//    std::cerr << "TS = " << TS << std::endl;
   }
 
   std::cout << "Total time :         " << time.time() << std::endl;
