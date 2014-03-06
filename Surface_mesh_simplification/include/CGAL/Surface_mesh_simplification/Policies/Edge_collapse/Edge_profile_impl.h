6812889b8fc8b7940c1bc0711a6b8ec76af29469
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2014-03-06 19:20:09 +0100

    use a value from insert in Edge_profile_impl
    This fix removes a warning in compilation of mesh_simplification_plugin
    in demos (in some environments).

--- Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_profile_impl.h
+++ Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_profile_impl.h
@@ -196,8 +196,9 @@ void Edge_profile<ECM>::Extract_triangles_and_link( VertexIdxMap const&    verte
     if ( v2 != mV1 )
     {
       mLink.push_back(v2) ;
-      CGAL_assertion_code( bool lInserted = ) lCollected.insert(vertex_idx[v2]).second ;
+      bool lInserted = lCollected.insert(vertex_idx[v2]).second ;
       CGAL_assertion(lInserted);
+      CGAL_USE(lInserted);
     }
       
     Extract_triangle(v0,v1,v2,e02,is_border);
