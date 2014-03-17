575c3521f9df16bad303b9b81ab19dfbcaa83451
Shihao Wu <shihao.wu312@gmail.com>
2014-03-17 15:13:07 +0800

    * restore grid_simplification_example.cpp to its original state.

--- Point_set_processing_3/examples/Point_set_processing_3/grid_simplification_example.cpp
+++ Point_set_processing_3/examples/Point_set_processing_3/grid_simplification_example.cpp
@@ -1,9 +1,6 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/grid_simplify_point_set.h>
-#include <CGAL/random_simplify_point_set.h>
 #include <CGAL/IO/read_xyz_points.h>
-#include <CGAL/IO/write_xyz_points.h>
-#include <CGAL/Timer.h>
 
 #include <vector>
 #include <fstream>
@@ -16,7 +13,7 @@ int main(void)
 {
   // Reads a .xyz point set file in points[].
   std::vector<Point> points;
-  std::ifstream stream("data/sphere_20k.xyz");
+  std::ifstream stream("data/oni.xyz");
   if (!stream ||
       !CGAL::read_xyz_points(stream, std::back_inserter(points)))
   {
@@ -24,35 +21,13 @@ int main(void)
     return EXIT_FAILURE;
   }
 
-  CGAL::Timer task_timer;
-  task_timer.start();
-  std::cout << "Run algorithm example: " << std::endl;
-
   // simplification by clustering using erase-remove idiom
-  double cell_size = 0.12;
+  double cell_size = 0.001;
   points.erase(CGAL::grid_simplify_point_set(points.begin(), points.end(), cell_size),
                points.end());
-  //points.erase(CGAL::random_simplify_point_set(points.begin(), points.end(), 90),
-  //  points.end());
-
-  std::cout << "total done: " << task_timer.time() << " seconds, "  << std::endl;
 
   // Optional: after erase(), use Scott Meyer's "swap trick" to trim excess capacity
   std::vector<Point>(points).swap(points);
-
-  // Saves point set.
-  // Note: write_xyz_points_and_normals() requires an output iterator
-  // over points as well as property maps to access each
-  // point position and normal.
-  std::ofstream out("data/sphere_20k_grid_downsampled.xyz");
-  if (!out ||
-	  !CGAL::write_xyz_points(
-	  out, points.begin(), points.end()))
-  {
-	  return EXIT_FAILURE;
-  }
-
-  system("Pause");
 
   return EXIT_SUCCESS;
 }
