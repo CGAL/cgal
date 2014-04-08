90a4c1c98909b0260db3d1292259d2f2d58cf6bb
Sébastien Loriot <sebastien.loriot@cgal.org>
2014-04-08 14:49:03 +0200

    update path to include file

--- Polyhedron/demo/Polyhedron/Scene_edit_polyhedron_item.h
+++ Polyhedron/demo/Polyhedron/Scene_edit_polyhedron_item.h
@@ -9,7 +9,7 @@
 #include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
 #include <CGAL/boost/graph/properties_Polyhedron_3.h>
 #include <CGAL/boost/graph/halfedge_graph_traits_Polyhedron_3.h>
-#include <CGAL/compute_normal.h>
+#include <CGAL/internal/Operations_on_polyhedra/compute_normal.h>
 
 #include <iostream>
 #include <fstream>
