933661a60d0062ce4d7e5ce5dfcda7ac8365d2c4
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2014-04-22 22:45:18 +0200

    SDG Linf doc changes from Andreas Fabri review 1

--- Segment_Delaunay_graph_Linf_2/doc/Segment_Delaunay_graph_Linf_2/CGAL/Segment_Delaunay_graph_Linf_filtered_traits_2.h
+++ Segment_Delaunay_graph_Linf_2/doc/Segment_Delaunay_graph_Linf_2/CGAL/Segment_Delaunay_graph_Linf_filtered_traits_2.h
@@ -4,22 +4,22 @@ namespace CGAL {
 /*!
 \ingroup PkgSDGLinf
 
-The class `Segment_Delaunay_graph_Linf_filtered_traits_2` provides a model for the 
+The class `Segment_Delaunay_graph_Linf_filtered_traits_2` provides a model for the
 `SegmentDelaunayGraphLinfTraits_2` concept.
 The class is derived from class
 `CGAL::Segment_Delaunay_graph_filtered_traits_2<CK,CM,EK,EM,FK,FM>`.
 
 \cgalModels `SegmentDelaunayGraphLinfTraits_2`
 
-\sa `SegmentDelaunayGraphLinfTraits_2` 
-\sa `CGAL::Segment_Delaunay_graph_filtered_traits_2<CK,CM,EK,EM,FK,FM>` 
-\sa `CGAL::Field_tag` 
-\sa `CGAL::Field_with_sqrt_tag` 
-\sa `CGAL::Segment_Delaunay_graph_2<Gt,DS>` 
-\sa `CGAL::Segment_Delaunay_graph_hierarchy_2<Gt,STag,DS>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_traits_2<K,MTag>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_traits_without_intersections_2<K,MTag>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>` 
+\sa `SegmentDelaunayGraphLinfTraits_2`
+\sa `CGAL::Segment_Delaunay_graph_filtered_traits_2<CK,CM,EK,EM,FK,FM>`
+\sa `CGAL::Field_tag`
+\sa `CGAL::Field_with_sqrt_tag`
+\sa `CGAL::Segment_Delaunay_graph_Linf_2<Gt,STag,DS>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_hierarchy_2<Gt,STag,DS>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_traits_2<K,MTag>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_traits_without_intersections_2<K,MTag>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>`
 
 */
 template< typename CK, typename CM, typename EK, typename EM, typename FK, typename FM >
@@ -33,23 +33,23 @@ namespace CGAL {
 \ingroup PkgSDGLinf
 
 The class `Segment_Delaunay_graph_Linf_filtered_traits_without_intersections_2`
-provides a model for the 
-`SegmentDelaunayGraphLinfTraits_2` concept. 
+provides a model for the
+`SegmentDelaunayGraphLinfTraits_2` concept.
 The class is derived from class
 `CGAL::Segment_Delaunay_graph_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>`.
 
 
 \cgalModels `SegmentDelaunayGraphLinfTraits_2`
 
-\sa `SegmentDelaunayGraphLinfTraits_2` 
-\sa `CGAL::Segment_Delaunay_graph_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>` 
-\sa `CGAL::Euclidean_ring_tag` 
-\sa `CGAL::Field_with_sqrt_tag` 
-\sa `CGAL::Segment_Delaunay_graph_2<Gt,DS>` 
-\sa `CGAL::Segment_Delaunay_graph_hierarchy_2<Gt,STag,DS>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_traits_2<K,MTag>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_traits_without_intersections_2<K,MTag>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_2<CK,CM,EK,EM,FK,FM>` 
+\sa `SegmentDelaunayGraphLinfTraits_2`
+\sa `CGAL::Segment_Delaunay_graph_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>`
+\sa `CGAL::Euclidean_ring_tag`
+\sa `CGAL::Field_with_sqrt_tag`
+\sa `CGAL::Segment_Delaunay_graph_2<Gt,DS>`
+\sa `CGAL::Segment_Delaunay_graph_hierarchy_2<Gt,STag,DS>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_traits_2<K,MTag>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_traits_without_intersections_2<K,MTag>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_2<CK,CM,EK,EM,FK,FM>`
 
 */
 template< typename CK, typename CM, typename EK, typename EM, typename FK, typename FM >
--- Segment_Delaunay_graph_Linf_2/doc/Segment_Delaunay_graph_Linf_2/CGAL/Segment_Delaunay_graph_Linf_traits_2.h
+++ Segment_Delaunay_graph_Linf_2/doc/Segment_Delaunay_graph_Linf_2/CGAL/Segment_Delaunay_graph_Linf_traits_2.h
@@ -4,22 +4,22 @@ namespace CGAL {
 /*!
 \ingroup PkgSDGLinf
 
-The class `Segment_Delaunay_graph_Linf_traits_2` provides a model for the 
-`SegmentDelaunayGraphLinfTraits_2` concept. 
+The class `Segment_Delaunay_graph_Linf_traits_2` provides a model for the
+`SegmentDelaunayGraphLinfTraits_2` concept.
 The class is derived from class
 `CGAL::Segment_Delaunay_graph_traits_2<K,MTag>`.
 
 \cgalModels `SegmentDelaunayGraphLinfTraits_2`
 
-\sa `SegmentDelaunayGraphLinfTraits_2` 
-\sa `CGAL::Segment_Delaunay_graph_traits_2<K,MTag>` 
-\sa `CGAL::Field_tag` 
-\sa `CGAL::Field_with_sqrt_tag` 
-\sa `CGAL::Segment_Delaunay_graph_2<Gt,DS>` 
-\sa `CGAL::Segment_Delaunay_graph_hierarchy_2<Gt,STag,DS>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_traits_without_intersections_2<K,MTag>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_2<CK,CM,EK,EM,FK,FM>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>` 
+\sa `SegmentDelaunayGraphLinfTraits_2`
+\sa `CGAL::Segment_Delaunay_graph_traits_2<K,MTag>`
+\sa `CGAL::Field_tag`
+\sa `CGAL::Field_with_sqrt_tag`
+\sa `CGAL::Segment_Delaunay_graph_Linf_2<Gt,DS>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_hierarchy_2<Gt,STag,DS>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_traits_without_intersections_2<K,MTag>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_2<CK,CM,EK,EM,FK,FM>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>`
 
 */
 template< typename K, typename MTag >
@@ -33,22 +33,22 @@ namespace CGAL {
 \ingroup PkgSDGLinf
 
 The class `Segment_Delaunay_graph_Linf_traits_without_intersections_2`
-provides a model for the 
-`SegmentDelaunayGraphLinfTraits_2` concept. 
+provides a model for the
+`SegmentDelaunayGraphLinfTraits_2` concept.
 The class is derived from class
 `CGAL::Segment_Delaunay_graph_traits_without_intersections_2<K,MTag>`.
 
 \cgalModels `SegmentDelaunayGraphLinfTraits_2`
 
-\sa `SegmentDelaunayGraphLinfTraits_2` 
-\sa `CGAL::Segment_Delaunay_graph_traits_without_intersections_2<K,MTag>` 
-\sa `CGAL::Euclidean_ring_tag` 
-\sa `CGAL::Field_with_sqrt_tag` 
-\sa `CGAL::Segment_Delaunay_graph_2<Gt,DS>` 
-\sa `CGAL::Segment_Delaunay_graph_hierarchy_2<Gt,STag,DS>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_traits_2<K,MTag>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_2<CK,CM,EK,EM,FK,FM>` 
-\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>` 
+\sa `SegmentDelaunayGraphLinfTraits_2`
+\sa `CGAL::Segment_Delaunay_graph_traits_without_intersections_2<K,MTag>`
+\sa `CGAL::Euclidean_ring_tag`
+\sa `CGAL::Field_with_sqrt_tag`
+\sa `CGAL::Segment_Delaunay_graph_2<Gt,DS>`
+\sa `CGAL::Segment_Delaunay_graph_hierarchy_2<Gt,STag,DS>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_traits_2<K,MTag>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_2<CK,CM,EK,EM,FK,FM>`
+\sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>`
 
 */
 template< typename K, typename MTag >
