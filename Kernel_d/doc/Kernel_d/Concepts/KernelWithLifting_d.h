5c85856e143630a3947ef19b08a17827ced9c316
Marc Glisse <marc.glisse@inria.fr>
2014-05-07 19:01:26 +0200

    Remove trailing whitespace.

--- Kernel_d/doc/Kernel_d/Concepts/KernelWithLifting_d.h
+++ Kernel_d/doc/Kernel_d/Concepts/KernelWithLifting_d.h
@@ -9,39 +9,39 @@ unclear in kernels of fixed dimension.
 
 \cgalRefines `Kernel_d`
 \cgalHasModel `CGAL::Cartesian_d<FieldNumberType>`
-\cgalHasModel `CGAL::Homogeneous_d<RingNumberType>` 
+\cgalHasModel `CGAL::Homogeneous_d<RingNumberType>`
 */
 class KernelWithLifting_d {
 public:
 
-/// \name Constructions 
+/// \name Constructions
 /// @{
 
-/*! 
-a model of `KernelWithLifting_d::Lift_to_paraboloid_d` 
-*/ 
-typedef unspecified_type Lift_to_paraboloid_d; 
+/*!
+a model of `KernelWithLifting_d::Lift_to_paraboloid_d`
+*/
+typedef unspecified_type Lift_to_paraboloid_d;
 
-/*! 
-a model of `KernelWithLifting_d::Project_along_d_axis_d` 
-*/ 
-typedef unspecified_type Project_along_d_axis_d; 
+/*!
+a model of `KernelWithLifting_d::Project_along_d_axis_d`
+*/
+typedef unspecified_type Project_along_d_axis_d;
 
-/// @} 
+/// @}
 
-/// \name Operations 
+/// \name Operations
 /// The following member functions return function objects of the
 /// types listed above.
 /// @{
 
-/*! 
+/*!
 
-*/ 
+*/
 KernelWithLifting_d::Lift_to_paraboloid_d lift_to_paraboloid_d_object() const;
 
-/*! 
+/*!
 
-*/ 
+*/
 KernelWithLifting_d::Project_along_d_axis_d project_along_d_axis_d_object() const;
 
 /// @}
