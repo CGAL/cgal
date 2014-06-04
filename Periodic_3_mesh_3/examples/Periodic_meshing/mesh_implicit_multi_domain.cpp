487bc68b272313c259b27dfed39394e0d79e648d
Aymeric PELLE <aymeric.pelle@inria.fr>
2014-06-04 17:59:33 +0200

    The multi domain example uses the multi domain wrapper now.

--- Periodic_3_mesh_3/examples/Periodic_meshing/mesh_implicit_multi_domain.cpp
+++ Periodic_3_mesh_3/examples/Periodic_meshing/mesh_implicit_multi_domain.cpp
@@ -5,13 +5,14 @@
 #include <CGAL/Mesh_triangulation_3.h>
 #include <CGAL/Mesh_complex_3_in_triangulation_3.h>
 
-#include <CGAL/Mesh_3/Implicit_to_labeled_function_wrapper.h>
+#include <CGAL/Implicit_to_labeling_function_wrapper.h>
 #include <CGAL/Periodic_mesh_facet_criteria_3.h>
 #include <CGAL/Periodic_mesh_cell_criteria_3.h>
 #include <CGAL/Mesh_criteria_3.h>
 #include <CGAL/Periodic_mesh_3/Implicit_to_labeled_subdomains_function_wrapper.h>
 
 #include <CGAL/Periodic_implicit_mesh_domain_3.h>
+#include <CGAL/Periodic_labeled_mesh_domain_3.h>
 #include <CGAL/make_periodic_mesh_3.h>
 #include <CGAL/Mesh_3_periodic_triangulation_3.h>
 
@@ -24,13 +25,11 @@ typedef K::FT                                               FT;
 typedef K::Point_3                                          Point;
 
 // Function
-//typedef FT (*Function)(const Point&);
-//typedef CGAL::Mesh_3::Implicit_multi_domain_to_labeled_function_wrapper<Function, K> Wrapper;
-typedef FT (Function)(const Point&);
+typedef FT (*Function)(const Point&);
+typedef CGAL::Implicit_multi_domain_to_labeling_function_wrapper<Function> Wrapper;
 
 // Domain
-typedef CGAL::Mesh_3::Implicit_to_labeled_function_wrapper<Function, K> Wrapper;
-typedef CGAL::Periodic_implicit_mesh_domain_3<Function, K, Wrapper>                 Periodic_mesh_domain;
+typedef CGAL::Periodic_labeled_mesh_domain_3<Wrapper, K>                 Periodic_mesh_domain;
 
 // Triangulation
 typedef CGAL::Mesh_3_periodic_triangulation_3_generator<Periodic_mesh_domain>::type Mesh_3_periodic_triangulation_3;
@@ -57,6 +56,12 @@ FT sphere_function_1 (const Point& p)
 { return CGAL::squared_distance(p, Point(0.5, 0.5, 0.5))-0.2; }
 FT sphere_function_2 (const Point& p)
 { return CGAL::squared_distance(p, Point(0.66, 0.5, 0.5))-0.2; }
+FT schwarz_p(const Point& p) {
+  const FT x2=std::cos( p.x() * 2*PI ),
+  y2=std::cos( p.y() * 2*PI ),
+  z2=std::cos( p.z() * 2*PI );
+  return x2 + y2 + z2;
+}
 
 
 typedef CGAL::Mesh_constant_domain_field_3<Periodic_mesh_domain::R, Periodic_mesh_domain::Index> Field;
@@ -64,27 +69,42 @@ typedef CGAL::Mesh_constant_domain_field_3<Periodic_mesh_domain::R, Periodic_mes
 int main(int argc, char** argv)
 {
   int domain_size = 1;
-  std::string index = "1";
+  std::string index = ".";
 
   if (argc > 1)
   {
-	  domain_size = atof(argv[1]);
-	  index = argv[1];
+    index = argv[1];
+    if (argc > 2)
+      domain_size = atof(argv[2]);
   }
 
-//  std::vector<Function> funcs;
-//  funcs.push_back(&sphere_function_1);
-//  funcs.push_back(&sphere_function_2);
-//  Periodic_mesh_domain domain(Wrapper(funcs, "--"), CGAL::Bbox_3(0, 0, 0, domain_size, domain_size, domain_size));
-  Periodic_mesh_domain domain(sphere_function_1, CGAL::Iso_cuboid_3<K>(0, 0, 0, domain_size, domain_size, domain_size));
+  std::vector<Function> funcs;
+  funcs.push_back(&sphere_function_1);
+  std::vector<std::string> vps;
+  vps.push_back("-");
+  vps.push_back("+");
+  Wrapper wrapper(funcs, vps);
+  Periodic_mesh_domain domain(wrapper, CGAL::Iso_cuboid_3<K>(0, 0, 0, domain_size, domain_size, domain_size));
   
-  Mesh_criteria criteria(domain
-//  , facet_angle=30, facet_size=0.05 * domain_size, facet_distance=0.025 * domain_size,
-//                              cell_radius_edge_ratio=2, cell_size = 0.05
-//                              );
-    , facet_angle=30, facet_size=0.10 * domain_size, facet_distance=0.25 * domain_size,
-                                cell_radius_edge_ratio=2, cell_size = 0.10
-                                 );
+  double kidney_size = 0.3;
+  int volume_dimension = 3;
+  Field size(8);
+  size.set_size(kidney_size, volume_dimension,
+                domain.index_from_subdomain_index(2));
+
+  size.set_size(0.06, volume_dimension,
+                domain.index_from_subdomain_index(1));
+
+  Mesh_criteria criteria(domain, facet_angle=30, facet_size=0.05, facet_distance=0.025,
+                                cell_radius_edge_ratio=2, cell_size = size);
+//  Mesh_criteria criteria(domain
+////      , facet_angle=30, facet_size=0.05 * domain_size, facet_distance=0.025 * domain_size,
+////      cell_radius_edge_ratio=2, cell_size = 0.05
+//      , facet_angle=30, facet_size=0.05, facet_distance=0.025,
+//      cell_radius_edge_ratio=2, cell_size = 0.05
+////      , facet_angle=30, facet_size=0.10 * domain_size, facet_distance=0.25 * domain_size,
+////      cell_radius_edge_ratio=2, cell_size = 0.10
+//                                 );
 
   
   // Mesh generation
@@ -93,7 +113,7 @@ int main(int argc, char** argv)
   // Output
   std::ofstream medit_file( (std::string("output_") + index + ".mesh").data() );
   
-  write_complex_to_medit(medit_file, c3t3, 1);
+  write_complex_to_medit(medit_file, c3t3);
   
   medit_file.close();
   
