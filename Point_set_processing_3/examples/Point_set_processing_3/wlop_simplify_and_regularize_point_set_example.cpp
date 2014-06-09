0aa78d62024863ac11f18223610a129aa1d18395
Shihao Wu <shihao.wu312@gmail.com>
2014-06-09 15:26:33 +0800

    added INPUT_FILENAME

--- Point_set_processing_3/examples/Point_set_processing_3/wlop_simplify_and_regularize_point_set_example.cpp
+++ Point_set_processing_3/examples/Point_set_processing_3/wlop_simplify_and_regularize_point_set_example.cpp
@@ -13,26 +13,28 @@ typedef Kernel::Point_3 Point;
 int main(void)
 {
   const std::string INPUT_FILENAME_WITHOUT_EXT = "data/saint_jean_370K";
+  const std::string INPUT_FILENAME = INPUT_FILENAME_WITHOUT_EXT + ".xyz";
 
   // Reads a .xyz point set file in points[], *with normals*.
 	// TOFIX: do we have an extension for points with normals?
   std::vector<Point> points;
-  std::ifstream stream(INPUT_FILENAME_WITHOUT_EXT + ".xyz");
+  std::ifstream stream(INPUT_FILENAME.c_str());
 
   if (!stream || !CGAL::read_xyz_points(stream, std::back_inserter(points)))
   {
     std::cerr << "Error: cannot read file "
-              << INPUT_FILENAME_WITHOUT_EXT << ".xyz" << std::endl;
+              << INPUT_FILENAME.c_str()  << std::endl;
     return EXIT_FAILURE;
   }
 
   std::vector<Point> output;
 
   //with default parameters begins
-  CGAL::wlop_simplify_and_regularize_point_set<CGAL::Parallel_tag>(
-                                               points.begin(), 
-                                               points.end(),
-                                               back_inserter(output));
+  CGAL::wlop_simplify_and_regularize_point_set
+                          <CGAL::Parallel_tag> // parallel version
+                          (points.begin(), 
+                           points.end(),
+                           back_inserter(output));
   //with default parameters ends
 
   //with all parameters begins
@@ -42,14 +44,15 @@ int main(void)
   const double neighbor_radius = 0.03;      // neighbors size.
   const unsigned int iter_number = 30;      // number of iterations.
   const bool compute_density = true;      //compute density of input point set.
-  CGAL::wlop_simplify_and_regularize_point_set<CGAL::Parallel_tag>(
-                                               points.begin(),
-                                               points.end(),
-                                               back_inserter(output),
-                                               retain_percentage, 
-                                               neighbor_radius,
-                                               iter_number,
-                                               compute_density);
+  CGAL::wlop_simplify_and_regularize_point_set
+                          <CGAL::Parallel_tag> // parallel version
+                          (points.begin(),
+                           points.end(),
+                           back_inserter(output),
+                           retain_percentage, 
+                           neighbor_radius,
+                           iter_number,
+                           compute_density);
   */
   //with all parameters ends
   
