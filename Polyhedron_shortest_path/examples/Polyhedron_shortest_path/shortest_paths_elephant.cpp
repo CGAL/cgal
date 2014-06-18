3923a148b40281a19da15b97cae5361619b69320
Stephen Kiazyk <s_kiazyk@hotmail.com>
2014-06-18 11:09:26 -0400

    Fix a bug where source nodes propagate on the wrong edges.
    Also removed a number of compiler warnings due to out of order
    member variable initialization in constructors.

--- Polyhedron_shortest_path/examples/Polyhedron_shortest_path/shortest_paths_elephant.cpp
+++ Polyhedron_shortest_path/examples/Polyhedron_shortest_path/shortest_paths_elephant.cpp
@@ -19,6 +19,8 @@
 
 #include <fstream>
 
+#define UNUSED(X) (void)sizeof(X)
+
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Polyhedron_3<Kernel> Polyhedron_3;
 typedef CGAL::Polyhedron_shortest_path_default_traits<Kernel, Polyhedron_3> Traits;
@@ -44,8 +46,94 @@ struct Point_sequence_collector
   }
 };
 
+enum Sequence_item_type
+{
+  SEQUENCE_ITEM_VERTEX,
+  SEQUENCE_ITEM_EDGE,
+  SEQUENCE_ITEM_FACE,
+};
+
+template <class Traits>
+struct Sequence_item
+{
+  typedef typename Traits::Barycentric_coordinate Barycentric_coordinate;
+  typedef typename Traits::FT FT;
+  
+  Sequence_item_type type;
+  size_t index;
+  Barycentric_coordinate faceAlpha;
+  FT edgeAlpha;
+};
+
+template <class Traits, 
+  class VIM = typename boost::property_map<typename Traits::Polyhedron, CGAL::vertex_external_index_t>::type,
+  class HIM = typename boost::property_map<typename Traits::Polyhedron, CGAL::halfedge_external_index_t>::type,
+  class FIM = typename boost::property_map<typename Traits::Polyhedron, CGAL::face_external_index_t>::type>
+struct Edge_sequence_collector
+{
+  typedef typename Traits::Polyhedron Polyhedron;
+  typedef typename Traits::FT FT;
+  typedef typename Traits::Barycentric_coordinate Barycentric_coordinate;
+  typedef VIM VertexIndexMap;
+  typedef HIM HalfedgeIndexMap;
+  typedef FIM FaceIndexMap;
+  typedef typename boost::graph_traits<Polyhedron> GraphTraits;
+  typedef typename GraphTraits::vertex_descriptor vertex_descriptor;
+  typedef typename GraphTraits::halfedge_descriptor halfedge_descriptor;
+  typedef typename GraphTraits::face_descriptor face_descriptor;
+
+  VertexIndexMap m_vertexIndexMap;
+  HalfedgeIndexMap m_halfedgeIndexMap;
+  FaceIndexMap m_faceIndexMap;
+  
+  std::vector<Sequence_item<Traits> > m_sequence;
+  
+  Edge_sequence_collector(Polyhedron& p)
+    : m_vertexIndexMap(CGAL::get(boost::vertex_external_index, p))
+    , m_halfedgeIndexMap(CGAL::get(CGAL::halfedge_external_index, p))
+    , m_faceIndexMap(CGAL::get(CGAL::face_external_index, p))
+  {
+  }
+
+  Edge_sequence_collector(VertexIndexMap& vertexIndexMap, HalfedgeIndexMap& halfedgeIndexMap, FaceIndexMap& faceIndexMap)
+    : m_vertexIndexMap(vertexIndexMap)
+    , m_halfedgeIndexMap(halfedgeIndexMap)
+    , m_faceIndexMap(faceIndexMap)
+  {
+  }
+  
+  void edge(halfedge_descriptor he, FT alpha)
+  {
+    Sequence_item<Traits> item;
+    item.type = SEQUENCE_ITEM_EDGE;
+    item.index = m_halfedgeIndexMap[he];
+    item.edgeAlpha = alpha;
+    m_sequence.push_back(item);
+  }
+  
+  void vertex(vertex_descriptor v)
+  {
+    Sequence_item<Traits> item;
+    item.type = SEQUENCE_ITEM_VERTEX;
+    item.index = m_vertexIndexMap[v];
+    m_sequence.push_back(item);
+  }
+  
+  void face(face_descriptor f, Barycentric_coordinate alpha)
+  {
+    Sequence_item<Traits> item;
+    item.type = SEQUENCE_ITEM_FACE;
+    item.index = m_faceIndexMap[f];
+    item.faceAlpha = alpha;
+    m_sequence.push_back(item);
+  }
+};
+
 int main(int argc, char** argv)
 {
+  UNUSED(argc);
+  UNUSED(argv);
+
   Polyhedron_3 polyhedron;
   
   std::ifstream inStream("data/elephant.off");
