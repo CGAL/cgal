ad798d8390ffc5e957c019fbd121fb96d1709ff1
Laurent Rineau <laurent.rineau@cgal.org>
2014-06-20 16:33:01 +0200

    Rename variables

--- Intersections_3/include/CGAL/internal/Intersections_3/Bbox_3_Triangle_3_do_intersect.h
+++ Intersections_3/include/CGAL/internal/Intersections_3/Bbox_3_Triangle_3_do_intersect.h
@@ -287,30 +287,30 @@ namespace internal {
     const Bbox_3& bbox = a_bbox;
 #endif    
     
-    Uncertain<bool> ind = make_uncertain(true);
+    // Create a "certainly true"
+    Uncertain<bool> ind_or_true = make_uncertain(true);
     
-    Uncertain<bool> b = make_uncertain(true);
     if (forbidden_axis!=0){
       if (forbidden_size!=0){
-        b = do_axis_intersect<K,0,0>(triangle, sides, bbox);
+        Uncertain<bool> b = do_axis_intersect<K,0,0>(triangle, sides, bbox);
         if(is_indeterminate(b)){
-          ind = b;
+          ind_or_true = b;
         } else if(! b){
           return false;
         }
       }
       if (forbidden_size!=1){
-        b = do_axis_intersect<K,0,1>(triangle, sides, bbox);
+        Uncertain<bool> b = do_axis_intersect<K,0,1>(triangle, sides, bbox);
         if(is_indeterminate(b)){
-          ind = b;
+          ind_or_true = b;
         } else if(! b){
           return false;
         }
       }
       if (forbidden_size!=2){
-        b = do_axis_intersect<K,0,2>(triangle, sides, bbox);
+        Uncertain<bool> b = do_axis_intersect<K,0,2>(triangle, sides, bbox);
         if(is_indeterminate(b)){
-          ind = b;
+          ind_or_true = b;
         } else if(! b){
           return false;
         }
@@ -319,25 +319,25 @@ namespace internal {
     
     if (forbidden_axis!=1){
       if (forbidden_size!=0){
-        b = do_axis_intersect<K,1,0>(triangle, sides, bbox);
+        Uncertain<bool> b = do_axis_intersect<K,1,0>(triangle, sides, bbox);
         if(is_indeterminate(b)){
-          ind = b;
+          ind_or_true = b;
         } else if(! b){
           return false;
         }
       }
       if (forbidden_size!=1){
-        b = do_axis_intersect<K,1,1>(triangle, sides, bbox);
+        Uncertain<bool> b = do_axis_intersect<K,1,1>(triangle, sides, bbox);
         if(is_indeterminate(b)){
-          ind = b;
+          ind_or_true = b;
         } else if(! b){
           return false;
         }
       }
       if (forbidden_size!=2){
-        b = do_axis_intersect<K,1,2>(triangle, sides, bbox);
+        Uncertain<bool> b = do_axis_intersect<K,1,2>(triangle, sides, bbox);
         if(is_indeterminate(b)){
-          ind = b;
+          ind_or_true = b;
         } else if(! b){
           return false;
         }
@@ -346,31 +346,31 @@ namespace internal {
     
     if (forbidden_axis!=2){
       if (forbidden_size!=0){
-        b = do_axis_intersect<K,2,0>(triangle, sides, bbox);
+        Uncertain<bool> b = do_axis_intersect<K,2,0>(triangle, sides, bbox);
         if(is_indeterminate(b)){
-          ind = b;
+          ind_or_true = b;
         } else if(! b){
           return false;
         }
       }
       if (forbidden_size!=1){
-        b = do_axis_intersect<K,2,1>(triangle, sides, bbox);
+        Uncertain<bool> b = do_axis_intersect<K,2,1>(triangle, sides, bbox);
         if(is_indeterminate(b)){
-          ind = b;
+          ind_or_true = b;
         } else if(! b){
           return false;
         }
       }
       if (forbidden_size!=2){
-        b = do_axis_intersect<K,2,2>(triangle, sides, bbox);
+        Uncertain<bool> b = do_axis_intersect<K,2,2>(triangle, sides, bbox);
         if(is_indeterminate(b)){
-          ind = b;
+          ind_or_true = b;
         } else if(! b){
           return false;
         }
       }
     }
-    return ind; // throws exception in case it is indeterminate
+    return ind_or_true; // throws exception in case it is indeterminate
   }
 
   template <class K>
