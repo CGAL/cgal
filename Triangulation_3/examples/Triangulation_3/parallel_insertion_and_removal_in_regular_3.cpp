9ca5bf92bee0182224616d1c1cf407bef4228270
Clement Jamin <clement.jamin@inria.fr>
2014-07-01 16:18:37 +0200

    Improve code and CMakeLists.txt for tests
    There was no need for a CMake option here since the parallel code is tested by
    specific cpp files.

--- Triangulation_3/examples/Triangulation_3/parallel_insertion_and_removal_in_regular_3.cpp
+++ Triangulation_3/examples/Triangulation_3/parallel_insertion_and_removal_in_regular_3.cpp
@@ -7,48 +7,58 @@
 #include <fstream>
 #include <vector>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-
-typedef CGAL::Regular_triangulation_euclidean_traits_3<K>   Traits;
-
-typedef Traits::RT                                          Weight;
-typedef Traits::Bare_point                                  Point;
-typedef Traits::Weighted_point                              Weighted_point;
-
-// Regular T3
-typedef CGAL::Triangulation_data_structure_3< 
-  CGAL::Triangulation_vertex_base_3<Traits>, 
-  CGAL::Regular_triangulation_cell_base_3<Traits>, 
-  CGAL::Parallel_tag>                                       Tds;
-
-typedef CGAL::Regular_triangulation_3<Traits, Tds>          Rt;
-typedef Rt::Vertex_handle                                   Vertex_handle;
-
 int main()
 {
+#ifdef CGAL_LINKED_WITH_TBB
+  typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+
+  typedef CGAL::Regular_triangulation_euclidean_traits_3<K>   Traits;
+
+  typedef Traits::RT                                          Weight;
+  typedef Traits::Bare_point                                  Point;
+  typedef Traits::Weighted_point                              Weighted_point;
+
+  // Regular T3
+  typedef CGAL::Triangulation_data_structure_3< 
+    CGAL::Triangulation_vertex_base_3<Traits>, 
+    CGAL::Regular_triangulation_cell_base_3<Traits>, 
+    CGAL::Parallel_tag>                                       Tds;
+
+  typedef CGAL::Regular_triangulation_3<Traits, Tds>          Rt;
+  typedef Rt::Vertex_handle                                   Vertex_handle;
+
+  const int NUM_INSERTED_POINTS = 100000;
+
   CGAL::Random_points_in_cube_3<Point> rnd(1.);
 
   // Construction from a vector of 1,000,000 points
   std::vector<Point> V;
-  V.reserve(1000000);
-  for (int i = 0; i != 1000000; ++i)
+  V.reserve(NUM_INSERTED_POINTS);
+  for (int i = 0; i != NUM_INSERTED_POINTS; ++i)
     V.push_back(*rnd++);
   
   // Construct the locking data-structure, using the bounding-box of the points
   Rt::Lock_data_structure locking_ds(
     CGAL::Bbox_3(-1., -1., -1., 1., 1., 1.), 50);
   // Contruct the triangulation in parallel
+  std::cerr << "Construction and insertion" << std::endl;
   Rt rtr(V.begin(), V.end(), &locking_ds);
 
-  std::cerr << "Remove";
-  // Remove the first 100,000 vertices
+  assert(rtr.is_valid());
+
+  std::cerr << "Remove" << std::endl;
+  // Remove the first 1/10 vertices
   std::vector<Vertex_handle> vertices_to_remove;
   int i = 0;
   Rt::Finite_vertices_iterator vit = rtr.finite_vertices_begin();
-  for (int i = 0 ; i < 100000 ; ++i)
+  for (int i = 0 ; i < NUM_INSERTED_POINTS/10 ; ++i)
     vertices_to_remove.push_back(vit++);
   // Parallel remove
   rtr.remove(vertices_to_remove.begin(), vertices_to_remove.end());
+  
+  assert(rtr.is_valid());
+
+#endif //CGAL_LINKED_WITH_TBB
 
   return 0;
 }
--- Triangulation_3/examples/Triangulation_3/parallel_insertion_in_delaunay_3.cpp
+++ Triangulation_3/examples/Triangulation_3/parallel_insertion_in_delaunay_3.cpp
@@ -6,25 +6,28 @@
 #include <fstream>
 #include <vector>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-
-// Delaunay T3
-typedef CGAL::Triangulation_data_structure_3< 
-  CGAL::Triangulation_vertex_base_3<K>, 
-  CGAL::Triangulation_cell_base_3<K>, 
-  CGAL::Parallel_tag>                          Tds;
-typedef CGAL::Delaunay_triangulation_3<K, Tds> Triangulation;
-
-typedef Triangulation::Point          Point;
-
 int main()
 {
+#ifdef CGAL_LINKED_WITH_TBB
+  typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+
+  // Delaunay T3
+  typedef CGAL::Triangulation_data_structure_3< 
+    CGAL::Triangulation_vertex_base_3<K>, 
+    CGAL::Triangulation_cell_base_3<K>, 
+    CGAL::Parallel_tag>                          Tds;
+  typedef CGAL::Delaunay_triangulation_3<K, Tds> Triangulation;
+
+  typedef Triangulation::Point          Point;
+  
+  const int NUM_INSERTED_POINTS = 100000;
+
   CGAL::Random_points_in_cube_3<Point> rnd(1.);
 
   // Construction from a vector of 1,000,000 points
   std::vector<Point> V;
-  V.reserve(1000000);
-  for (int i = 0; i != 1000000; ++i)
+  V.reserve(NUM_INSERTED_POINTS);
+  for (int i = 0; i != NUM_INSERTED_POINTS; ++i)
     V.push_back(*rnd++);
   
   // Construct the locking data-structure, using the bounding-box of the points
@@ -32,6 +35,10 @@ int main()
     CGAL::Bbox_3(-1., -1., -1., 1., 1., 1.), 50);
   // Construct the triangulation in parallel
   Triangulation T(V.begin(), V.end(), &locking_ds);
+
+  assert(T.is_valid());
+
+#endif //CGAL_LINKED_WITH_TBB
 
   return 0;
 }
