65a176d0e78301b7c63523eca4cdd1f2578224b7
Sébastien Loriot <sebastien.loriot@cgal.org>
2014-07-04 15:29:06 +0200

    update license

--- BGL/include/CGAL/boost/graph/graph_traits_PolyMesh_ArrayKernelT.h
+++ BGL/include/CGAL/boost/graph/graph_traits_PolyMesh_ArrayKernelT.h
@@ -11,8 +11,8 @@
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/branches/features/Polyhedron_TMPL_IN_TMPL-GF/BGL/include/CGAL/boost/graph/graph_traits_HalfedgeDS.h $
-// $Id: graph_traits_HalfedgeDS.h 67093 2012-01-13 11:22:39Z lrineau $
+// $URL$
+// $Id$
 // 
 //
 // Author(s)     : Andreas Fabri, Philipp Moeller
