6713a7083f6284dd7dcc4edabe77a4ec5d3e6684
Sébastien Loriot <sebastien.loriot@cgal.org>
2014-07-04 15:47:49 +0200

    fix license headers

--- Number_types/include/CGAL/FPU_msvc.h
+++ Number_types/include/CGAL/FPU_msvc.h
@@ -2,17 +2,14 @@
 //
 // This file is part of CGAL (www.cgal.org); you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; version 2.1 of the License.
-// See the file LICENSE.LGPL distributed with CGAL.
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
 //
 // Licensees holding a valid commercial license may use this file in
 // accordance with the commercial license agreement provided with the software.
 //
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-//
-// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/branches/features/gsoc2011-surface-modeling-Yzju/Number_types/include/CGAL/FPU_msvc.h $
-// $Id: FPU_msvc.h 65157 2011-08-17 14:02:29Z afabri $
+// $URL$
+// $Id$
 //
 //
 // Author(s)     : Laurent Rineau
