CGAL/cgal

commit 31042b8d813f7aff3db0d2eb09c7400fe018343a

fix warning in the Polyhedron demo edit plugin

Polyhedron/demo/Polyhedron/Scene_edit_polyhedron_item.cpp
@@ -158,7 +158,7 @@ void Scene_edit_polyhedron_item::draw_edges() const {
 
   glEnableClientState(GL_VERTEX_ARRAY);
   glVertexPointer(3, GL_DOUBLE, 0, positions.data());
-  glDrawElements(GL_LINES, edges.size(), GL_UNSIGNED_INT, edges.data());
+  glDrawElements(GL_LINES, (GLsizei) edges.size(), GL_UNSIGNED_INT, edges.data());
   glDisableClientState(GL_VERTEX_ARRAY); 
 
   if(rendering_mode == Wireframe) {
@@ -171,7 +171,7 @@ void Scene_edit_polyhedron_item::draw() const {
 
   glVertexPointer(3, GL_DOUBLE, 0, positions.data());
   glNormalPointer(GL_DOUBLE, 0, normals.data());
-  glDrawElements(GL_TRIANGLES, tris.size(), GL_UNSIGNED_INT, tris.data());
+  glDrawElements(GL_TRIANGLES, (GLsizei) tris.size(), GL_UNSIGNED_INT, tris.data());
 
   glDisableClientState(GL_VERTEX_ARRAY);
   glDisableClientState(GL_NORMAL_ARRAY);

Polyhedron/demo/Polyhedron/Scene_edit_polyhedron_item.h
@@ -263,8 +263,8 @@ private:
   Scene_polyhedron_item* poly_item;
   // For drawing
   std::vector<double> positions;
-  std::vector<unsigned int> tris;
-  std::vector<unsigned int> edges;
+  std::vector<std::size_t> tris;
+  std::vector<std::size_t> edges;
   std::vector<double> normals;
 
   Deform_mesh deform_mesh;

Polyhedron/demo/Polyhedron/Polyhedron_demo_edit_polyhedron_plugin.cpp
@@ -249,7 +249,7 @@ void Polyhedron_demo_edit_polyhedron_plugin::on_Select_isolated_components_butto
   boost::optional<std::size_t> minimum = 
     edit_item->select_isolated_components(ui_widget.Threshold_size_spin_box->value());
   if(minimum) {
-    ui_widget.Threshold_size_spin_box->setValue(*minimum);
+    ui_widget.Threshold_size_spin_box->setValue((int) *minimum);
   }
 }
 
@@ -260,7 +260,7 @@ void Polyhedron_demo_edit_polyhedron_plugin::on_Get_minimum_button_clicked() {
 
   boost::optional<std::size_t> minimum = edit_item->get_minimum_isolated_component();
   if(minimum) {
-    ui_widget.Threshold_size_spin_box->setValue(*minimum);
+    ui_widget.Threshold_size_spin_box->setValue((int) *minimum);
   }
 }
 

commit d5f7ba8ff3282196b054f55b1934576b4cd1d05b

fix a bug introduced in 31042b8d
  glDrawElements call expect unsigned int!

trivial bug-fix for master

Polyhedron/demo/Polyhedron/Scene_edit_polyhedron_item.cpp
@@ -60,16 +60,16 @@ Scene_edit_polyhedron_item::Scene_edit_polyhedron_item
   tris.resize(polyhedron()->size_of_facets()*3);
   counter = 0;
   for(Polyhedron::Facet_handle fb = polyhedron()->facets_begin(); fb != polyhedron()->facets_end(); ++fb, ++counter) {
-    tris[counter*3] = fb->halfedge()->vertex()->id();
-    tris[counter*3+1] = fb->halfedge()->next()->vertex()->id();
-    tris[counter*3+2] = fb->halfedge()->prev()->vertex()->id();
+    tris[counter*3] =  static_cast<unsigned int>(fb->halfedge()->vertex()->id());
+    tris[counter*3+1] = static_cast<unsigned int>(fb->halfedge()->next()->vertex()->id());
+    tris[counter*3+2] = static_cast<unsigned int>(fb->halfedge()->prev()->vertex()->id());
   }
 
   edges.resize(polyhedron()->size_of_halfedges());
   counter = 0;
   for(Polyhedron::Edge_iterator eb = polyhedron()->edges_begin(); eb != polyhedron()->edges_end(); ++eb, ++counter) {
-    edges[counter*2] = eb->vertex()->id();
-    edges[counter*2+1] = eb->opposite()->vertex()->id();
+    edges[counter*2] = static_cast<unsigned int>(eb->vertex()->id());
+    edges[counter*2+1] = static_cast<unsigned int>(eb->opposite()->vertex()->id());
   }
 }
 

Polyhedron/demo/Polyhedron/Scene_edit_polyhedron_item.h
@@ -263,8 +263,8 @@ private:
   Scene_polyhedron_item* poly_item;
   // For drawing
   std::vector<double> positions;
-  std::vector<std::size_t> tris;
-  std::vector<std::size_t> edges;
+  std::vector<unsigned int> tris;
+  std::vector<unsigned int> edges;
   std::vector<double> normals;
 
   Deform_mesh deform_mesh;