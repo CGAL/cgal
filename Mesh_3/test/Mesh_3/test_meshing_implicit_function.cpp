c8c7ed03a6d33188ae8065eb7791278ee17df48e
Clement Jamin <clement.jamin@inria.fr>
2014-07-17 17:46:37 +0200

    Relax the "number of facets/vertices" criteria

--- Mesh_3/test/Mesh_3/test_meshing_implicit_function.cpp
+++ Mesh_3/test/Mesh_3/test_meshing_implicit_function.cpp
@@ -92,7 +92,7 @@ struct Implicit_tester : public Tester<K>
     // Parallel
     if (boost::is_convertible<Concurrency_tag, CGAL::Parallel_tag>::value)
     {
-      this->verify(c3t3, domain, criteria, Bissection_tag(), 40, 62, 70, 100);
+      this->verify(c3t3, domain, criteria, Bissection_tag(), 40, 65, 60, 110);
     }
     else
 #endif //CGAL_LINKED_WITH_TBB
