4b1c043ae7d485468ee9a37fd9e5bdd062640581
Sebastian Morr <sebastian@morr.cc>
2014-07-22 12:11:23 +0200

    Refactored build_reduced_convolution

--- Minkowski_sum_2/include/CGAL/Minkowski_sum_2/new/Minkowski_sum_conv_lien_2.h
+++ Minkowski_sum_2/include/CGAL/Minkowski_sum_2/new/Minkowski_sum_conv_lien_2.h
@@ -23,17 +23,6 @@ private:
     typedef typename Kernel::Vector_2 Vector_2;
     typedef typename Kernel::Direction_2 Direction_2;
 
-    // Kernel functors:
-    typedef typename Kernel::Construct_translated_point_2 Translate_point_2;
-    typedef typename Kernel::Construct_vector_2 Construct_vector_2;
-    typedef typename Kernel::Construct_direction_2 Construct_direction_2;
-    typedef typename Kernel::Orientation_2 Compute_orientation_2;
-    typedef typename Kernel::Compare_xy_2 Compare_xy_2;
-    typedef typename Kernel::Counterclockwise_in_between_2 Ccw_in_between_2;
-    typedef typename Kernel::Compare_x_2 Compare_x_2;
-    typedef typename Kernel::Compute_x_2 Compute_x_2;
-    typedef typename Kernel::Compute_y_2 Compute_y_2;
-
     // Traits-related types:
     typedef Arr_segment_traits_2<Kernel> Traits_2_A;
     typedef Arr_SegmentData_traits<Traits_2_A> Traits_2;
@@ -57,20 +46,22 @@ private:
     typedef typename Arrangement_history_2::Ccb_halfedge_circulator Ccb_halfedge_circulator;
 
     typedef typename Arrangement_history_2::Originating_curve_iterator Originating_curve_iterator;
-    typedef std::pair<int, int> StatePair;
+    typedef std::pair<int, int> State;
 
     // Data members:
-    Translate_point_2 f_add;
-    Construct_vector_2 f_vector;
-    Construct_direction_2 f_direction;
-    Compute_orientation_2 f_orientation;
-    Compare_xy_2 f_compare_xy;
-    Ccw_in_between_2 f_ccw_in_between;
-    Compute_x_2 f_compute_x;
-    Compute_y_2 f_compute_y;
+    typename Kernel::Construct_translated_point_2 f_add;
+    typename Kernel::Construct_vector_2 f_vector;
+    typename Kernel::Construct_direction_2 f_direction;
+    typename Kernel::Orientation_2 f_orientation;
+    typename Kernel::Compare_xy_2 f_compare_xy;
+    typename Kernel::Counterclockwise_in_between_2 f_ccw_in_between;
+    typename Kernel::Compute_x_2 f_compute_x;
+    typename Kernel::Compute_y_2 f_compute_y;
 
     typename Traits_2::Compare_y_at_x_2 f_compare_y_at_x;
     typename Traits_2::Compare_x_2 f_compare_x;
+
+    AABBCollisionDetector<Kernel, Container> *aabb_collision_detector;
 
 public:
 
@@ -100,11 +91,11 @@ public:
         CGAL_precondition(pgn2.orientation() == CGAL::COUNTERCLOCKWISE);
 
         const Polygon_2 inversed_p1 = transform(Aff_transformation_2<Kernel>(SCALING, -1), pgn1);
-        _aabb_collision_detector = new AABBCollisionDetector<Kernel, Container>(pgn2, inversed_p1);
+        aabb_collision_detector = new AABBCollisionDetector<Kernel, Container>(pgn2, inversed_p1);
 
         // compute the reduced convolution
         Segments_list reduced_conv;
-        buildReducedConvolutionFiberGrid(pgn1, pgn2, reduced_conv);
+        build_reduced_convolution(pgn1, pgn2, reduced_conv);
 
         // split the segments at their intersection points
         Arrangement_history_2 arr;
@@ -121,27 +112,134 @@ public:
             handleFace(arr, itr, rotated_pgn1, pgn2, sum_holes);
         }
 
-        delete _aabb_collision_detector;
+        delete aabb_collision_detector;
 
         return sum_holes;
     }
 
 private:
 
+    // Builds the reduced convolution using the fiber grid approach. For each
+    // starting vertex, try to add two outgoing next states. If a visited
+    // vertex is reached, then do not explore further. This is a BFS like
+    // iteration beginning from each vertex in the first column of the fiber
+    // grid.
+    void build_reduced_convolution(const Polygon_2 &pgn1, const Polygon_2 &pgn2, Segments_list &reduced_conv) const {
+        unsigned int n1 = pgn1.size();
+        unsigned int n2 = pgn2.size();
+
+        // Init the direcions of both polygons
+        std::vector<Direction_2> p1_dirs = directions_of_polygon(pgn1);
+        std::vector<Direction_2> p2_dirs = directions_of_polygon(pgn2);
+
+        boost::unordered_set<State> visited_vertices;
+        boost::unordered_map<std::pair<int, int>, Point_2> points_map;
+
+        // Init the queue with vertices from the first column
+        std::queue<State> state_queue;
+        for (int i = n1-1; i >= 0; --i) {
+            state_queue.push(State(i, 0));
+        }
+
+        while (state_queue.size() > 0) {
+            State curr_state = state_queue.front();
+            state_queue.pop();
+
+            int i1 = curr_state.first;
+            int i2 = curr_state.second;
+
+            if (visited_vertices.count(curr_state) > 0) {
+                continue;
+            }
+            visited_vertices.insert(curr_state);
+
+            // add two outgoing edges:
+            int next_i1 = (i1+1) % n1;
+            int next_i2 = (i2+1) % n2;
+            int prev_i1 = (n1+i1-1) % n1;
+            int prev_i2 = (n2+i2-1) % n2;
+
+            // add geometric entites of the transition from state (i,j) to (i+1,j) and (i,j+1), if they are in the reduced convolution.
+
+            bool is_end_coincide;
+            bool is_start_coincide;
+
+            // Add an edge from pgn2
+            if (p1_dirs[prev_i1] == p2_dirs[i2] || f_ccw_in_between(p2_dirs[i2], p1_dirs[prev_i1], p1_dirs[i1])) {
+                state_queue.push(State(i1, next_i2));
+
+                if (check_convex(pgn1[prev_i1], pgn1[i1], pgn1[next_i1])) {
+                    Point_2 start_point = get_point(i1, i2, points_map, pgn1, pgn2);
+                    Point_2 end_point = get_point(i1, next_i2, points_map, pgn1, pgn2);
+
+                    CGAL::Comparison_result cres = f_compare_xy(start_point, end_point);
+                    Segment_2 conv_seg = Segment_2(typename Traits_2_A::Segment_2(start_point, end_point), Segment_Data_Label(state(i1, i2), state(i1, next_i2), cres, 1));
+
+                    reduced_conv.push_back(conv_seg);
+                }
+            }
+
+            // Add an edge from pgn1
+            if (p2_dirs[i2] == p1_dirs[i1] || f_ccw_in_between(p1_dirs[i1], p2_dirs[prev_i2], p2_dirs[i2])) {
+                state_queue.push(State(next_i1, i2));
+
+                if (check_convex(pgn2[prev_i2], pgn2[i2], pgn2[next_i2])) {
+                    Point_2 start_point = get_point(i1, i2, points_map, pgn1, pgn2);
+                    Point_2 end_point = get_point(next_i1, i2, points_map, pgn1, pgn2);
+
+                    CGAL::Comparison_result cres = f_compare_xy(start_point, end_point);
+                    Segment_2 conv_seg = Segment_2(typename Traits_2_A::Segment_2(start_point, end_point), Segment_Data_Label(state(i1, i2), state(next_i1, i2), cres, 0));
+                    reduced_conv.push_back(conv_seg);
+                }
+            }
+        }
+    }
+
+    std::vector<Direction_2> directions_of_polygon(const Polygon_2 &pgn1) const {
+        std::vector<Direction_2> directions;
+        unsigned int n = pgn1.size();
+
+        for (int i = 0; i < n-1; ++i) {
+            directions.push_back(f_direction(f_vector(pgn1[i], pgn1[i+1])));
+        }
+        directions.push_back(f_direction(f_vector(pgn1[n-1], pgn1[0])));
+
+        return directions;
+    }
+
+    bool check_convex(const Point_2 &prev, const Point_2 &curr, const Point_2 &next) const {
+        return f_orientation(prev, curr, next) == LEFT_TURN;
+    }
+
+    // Gets point corresponding to a state (i,j) if exists, creates this point if asked for first time.
+    Point_2 get_point(int i1, int i2, boost::unordered_map<std::pair<int, int>, Point_2> &points_map, const Polygon_2 &pgn1, const Polygon_2 &pgn2) const {
+        Point_2 result;
+
+        if (points_map.count(State(i1, i2)) == 0) {
+            result = f_add(pgn1[i1], Vector_2(Point_2(ORIGIN), pgn2[i2]));
+            points_map[State(i1, i2)] = result;
+        } else {
+            result = points_map[State(i1, i2)];
+        }
+
+        return result;
+    }
+
+    // Put the outside loop of the arrangement in 'out_bound'
     void markOutsideLoop(Arrangement_history_2 &arr, Polygon_2 &out_bound) {
-        Face_iterator ub_face = arr.unbounded_face();
-        Hole_iterator holes_itr = ub_face->holes_begin();
-        Ccb_halfedge_circulator circ_start = *holes_itr;
+        Ccb_halfedge_circulator circ_start = *(arr.unbounded_face()->holes_begin());
         Ccb_halfedge_circulator circ = circ_start;
 
         do {
             out_bound.push_back(circ->source()->point());
-            --circ;
-        } while (circ != circ_start);
+        } while (--circ != circ_start);
     }
 
+    // Check whether the face is on the M-sum's border. Add it to 'holes' if it is.
     template <class OutputIterator>
     void handleFace(Arrangement_history_2 &arr, Face_handle itr, const Polygon_2 &reverse_pgn1, const Polygon_2 &pgn2, OutputIterator holes) {
+
+        // If the face contains holes, it can't be on the Minkowski sum's border
         if (itr->holes_begin() != itr->holes_end()) {
             return;
         }
@@ -149,19 +247,15 @@ private:
         Ccb_halfedge_circulator start = itr->outer_ccb();
         Ccb_halfedge_circulator circ = start;
 
-        // orientation check
+        // The face needs to be orientable
         do {
-            if (!checkTripNotSameDirWithSegment(arr, circ)) {
+            if (!check_originating_edge_has_same_direction(arr, circ)) {
                 return;
             }
+        } while (++circ != start);
 
-            ++circ;
-        } while (circ != start);
-
-        // collision detection check.
-        bool coll_detect = checkCollisionDetection(arr, start, reverse_pgn1, pgn2);
-
-        if (coll_detect) {
+        // Check whether this is a false hole (TODO: explain)
+        if (checkCollisionDetection(arr, start, reverse_pgn1, pgn2)) {
             return;
         }
 
@@ -171,119 +265,25 @@ private:
 
         do {
             pgn_hole.push_back(circ->source()->point());
-            --circ;
-        } while (circ != start);
+        } while (--circ != start);
 
         *holes = pgn_hole;
         ++holes;
     }
 
-    std::vector<Direction_2> fillPolyDirs(const Polygon_2 &pgn1) const {
-        std::vector<Direction_2> outVec;
-        unsigned int n = pgn1.size();
+    // Check whether the originating edge(s) had the same direction as the current half edge
+    bool check_originating_edge_has_same_direction(Arrangement_history_2 &arr, Halfedge_handle he) const {
+        Originating_curve_iterator segment_itr;
 
-        for (int i = 0; i < n-1; ++i) {
-            outVec.push_back(f_direction(f_vector(pgn1[i], pgn1[i+1])));
-        }
-        outVec.push_back(f_direction(f_vector(pgn1[n-1], pgn1[0])));
-
-        return outVec;
-    }
-
-    // Gets point corresponding to a state (i,j) if exists, creates this point if asked for first time.
-    Point_2 addGetPoint(int i1, int i2, boost::unordered_map<std::pair<int, int>, Point_2> &points_map, const Polygon_2 &pgn1, const Polygon_2 &pgn2) const {
-        Point_2 result;
-
-        if (points_map.count(StatePair(i1, i2)) == 0) {
-            result = f_add(pgn1[i1], Vector_2(Point_2(ORIGIN), pgn2[i2]));
-            points_map[StatePair(i1, i2)] = result;
-        } else {
-            result = points_map[StatePair(i1, i2)];
-        }
-
-        return result;
-    }
-
-    // Builds the reduced convolution using the fiber grid approach. For each
-    // starting vertex, try to add out-going next states (two states). If a
-    // visited vertex is reached then do not explore. This is a BFS like
-    // iteration beginning from each vertex in the first column of the fiber
-    // grid.
-    void buildReducedConvolutionFiberGrid(const Polygon_2 &pgn1, const Polygon_2 &pgn2, Segments_list &reduced_conv) const {
-        unsigned int n1 = pgn1.size();
-        unsigned int n2 = pgn2.size();
-
-        // Init the direcions of both polygons
-        std::vector<Direction_2> p1_dirs = fillPolyDirs(pgn1);
-        std::vector<Direction_2> p2_dirs = fillPolyDirs(pgn2);
-
-        boost::unordered_set<StatePair> visited_vertices_set;
-        std::queue<StatePair> state_queue;
-        boost::unordered_map<std::pair<int, int>, Point_2> points_map;
-
-        // Init the queue with vertices from the first column
-        for (int i = n1-1; i >= 0; --i) {
-            state_queue.push(StatePair(i, 0));
-        }
-
-        while (state_queue.size() > 0) {
-            StatePair curr_state = state_queue.front();
-            state_queue.pop();
-
-            int i1 = curr_state.first;
-            int i2 = curr_state.second;
-
-            if (visited_vertices_set.count(curr_state) > 0) {
-                continue;
-            }
-            visited_vertices_set.insert(curr_state);
-
-            // add two outgoing edges:
-            int next_p1 = (i1+1) % n1;
-            int next_p2 = (i2+1) % n2;
-            int prev_p1 = (n1+i1-1) % n1;
-            int prev_p2 = (n2+i2-1) % n2;
-
-            StatePair next_state_p1 = StatePair(next_p1, i2);
-            StatePair next_state_p2 = StatePair(i1, next_p2);
-
-            // add geometric entites of the transition from state (i,j) to (i+1,j) and (i,j+1), if they are in the reduced convolution.
-
-            bool is_end_coincide;
-            bool is_start_coincide;
-
-            // Add an edge from Q
-            if (checkSwept(p1_dirs[prev_p1], p1_dirs[i1], p2_dirs[i2], is_start_coincide, is_end_coincide) && !is_end_coincide) {
-                state_queue.push(next_state_p2);
-
-                if (!checkReflex(pgn1[prev_p1], pgn1[i1], pgn1[next_p1])) {
-                    Point_2 start_point = addGetPoint(i1, i2, points_map, pgn1, pgn2);
-                    Point_2 end_point = addGetPoint(i1, next_p2, points_map, pgn1, pgn2);
-
-                    CGAL::Comparison_result cres = f_compare_xy(start_point, end_point);
-                    Segment_2 conv_seg = Segment_2(typename Traits_2_A::Segment_2(start_point, end_point), Segment_Data_Label(state(i1, i2), state(i1, next_p2), cres, 1));
-
-                    reduced_conv.push_back(conv_seg);
-                }
-            }
-
-            // Add an edge from P
-            if (checkSwept(p2_dirs[prev_p2], p2_dirs[i2], p1_dirs[i1], is_start_coincide, is_end_coincide) && !is_start_coincide) {
-                state_queue.push(next_state_p1);
-
-                if (!checkReflex(pgn2[prev_p2], pgn2[i2], pgn2[next_p2])) {
-                    Point_2 start_point = addGetPoint(i1, i2, points_map, pgn1, pgn2);
-                    Point_2 end_point = addGetPoint(next_p1, i2, points_map, pgn1, pgn2);
-
-                    CGAL::Comparison_result cres = f_compare_xy(start_point, end_point);
-                    Segment_2 conv_seg = Segment_2(typename Traits_2_A::Segment_2(start_point, end_point), Segment_Data_Label(state(i1, i2), state(next_p1, i2), cres, 0));
-                    reduced_conv.push_back(conv_seg);
-                }
+        for (segment_itr = arr.originating_curves_begin(he); segment_itr != arr.originating_curves_end(he); ++segment_itr) {
+            if (segment_itr->label()._orientation == (CGAL::Comparison_result)he->direction()) {
+                return false;
             }
         }
+
+        return true;
     }
 
-    AABBCollisionDetector<Kernel, Container> *_aabb_collision_detector;
 
     /*
     This version assumes poly1 is reflected through origin. (as called from nested loops filter)
@@ -291,8 +291,8 @@ private:
     bool checkCollisionDetection(Arrangement_history_2 &arr, Halfedge_handle &handle, const Polygon_2 &pgn1, const Polygon_2 &pgn2) const {
         Point_2 mid_point = findInsidePoint(arr, handle);
         Polygon_2 t_pgn1 = transform(typename Kernel::Aff_transformation_2(CGAL::Translation(), Vector_2(CGAL::ORIGIN, mid_point)), pgn1);
-        _aabb_collision_detector->setTranslationPoint(mid_point);
-        return _aabb_collision_detector->checkCollision(t_pgn1, pgn2);
+        aabb_collision_detector->setTranslationPoint(mid_point);
+        return aabb_collision_detector->checkCollision(t_pgn1, pgn2);
     }
 
     Point_2 findInsidePoint(Arrangement_history_2 &arr, Halfedge_handle &handle) const {
@@ -304,7 +304,7 @@ private:
             ++currHandle;
             ++nextHandle;
 
-            if (checkReflex(currHandle->source()->point(), currHandle->target()->point(), nextHandle->target()->point())) {
+            if (!check_convex(currHandle->source()->point(), currHandle->target()->point(), nextHandle->target()->point())) {
                 break;
             }
         }
@@ -393,36 +393,6 @@ private:
         typename Kernel::FT y = (y_best - y_point) / 2 + y_point;
 
         return Point_2(x0, y);
-    }
-
-    bool checkTripNotSameDirWithSegment(Arrangement_history_2 &arr, Halfedge_handle he) const {
-        Originating_curve_iterator segment_itr;
-
-        for (segment_itr = arr.originating_curves_begin(he); segment_itr != arr.originating_curves_end(he); ++segment_itr) {
-            Segment_2 segment = *segment_itr;
-
-            CGAL::Comparison_result c1 = segment.label()._orientation;
-            CGAL::Comparison_result c2 = (CGAL::Comparison_result)he->direction();
-            bool same_dir = (c1 != c2);
-
-            if (same_dir) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    bool checkReflex(const Point_2 &prev, const Point_2 &curr, const Point_2 &next) const {
-        CGAL::Orientation res_ori = f_orientation(prev, curr, next);
-        return ((res_ori == RIGHT_TURN) || (res_ori == COLLINEAR));
-    }
-
-    bool checkSwept(Direction_2 &dir_start, Direction_2 &dir_end, Direction_2 &dir_new, bool &isStartConcide, bool &isEndConcide) const {
-        isStartConcide = dir_new == dir_start;
-        isEndConcide = dir_end == dir_new;
-
-        return isStartConcide || f_ccw_in_between(dir_new, dir_start, dir_end) || isEndConcide;
     }
 };
 
