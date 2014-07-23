2d2653447f4fcf2b833e1d4641a188c6466b481f
Sébastien Loriot <sebastien.loriot@cgal.org>
2014-07-23 13:21:20 +0200

    remove unused type

--- Mean_curvature_skeleton/include/CGAL/internal/Mean_curvature_skeleton/get_normal.h
+++ Mean_curvature_skeleton/include/CGAL/internal/Mean_curvature_skeleton/get_normal.h
@@ -31,7 +31,6 @@ typename Kernel::Vector_3 get_vertex_normal(const Vertex& v)
   typedef typename Kernel::Point_3 Point;
   typedef typename Kernel::Vector_3 Vector;
   typedef typename Vertex::Halfedge_around_vertex_const_circulator HV_circulator;
-  typedef typename Vertex::Facet Facet;
   Vector normal = CGAL::NULL_VECTOR;
   HV_circulator he = v.vertex_begin();
   HV_circulator end = he;
