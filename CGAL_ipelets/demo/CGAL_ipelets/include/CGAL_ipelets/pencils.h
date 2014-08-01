d9cdb1f580948efa92f20986f304ff9a37bb2998
Olivier Devillers <Olivier.Devillers@inria.fr>
2014-08-01 15:20:23 +0200

    There was a bug in the computation of pencil of circles: degenerate case when circle degenerate in the radical axis were not taken in account.
    This has an implact on the ipelets "pencil of circels"and "hyperbolic".
    
    A very small ipelet computing the distance between two points is also added.

--- CGAL_ipelets/demo/CGAL_ipelets/include/CGAL_ipelets/pencils.h
+++ CGAL_ipelets/demo/CGAL_ipelets/include/CGAL_ipelets/pencils.h
@@ -30,6 +30,9 @@ compute_circle_in_pencil(typename Kernel::Circle_2 c,typename Kernel::Circle_2 c
   typename Kernel::FT l2 = CGAL::squared_distance(c2.center(),origin) - c2.squared_radius() ;
   l1 += -2*((c1.center()-origin)*(c.center()-origin));
   l2 += -2*((c2.center()-origin)*(c.center()-origin));
+  if (l1==l2){ // degenerate case, radical axis
+    return  typename Kernel::Circle_2();
+  }
   lambda= - (lambda+l2)/(l1-l2);
   typename Kernel::Point_2 center=origin+lambda*(c1.center()-origin)+(1-lambda)*(c2.center()-origin);
   typename Kernel::FT sqradius=-    lambda*(CGAL::squared_distance(c1.center(),origin)-c1.squared_radius())
@@ -51,6 +54,9 @@ compute_circle_orthogonal(typename Kernel::Circle_2 c,typename Kernel::Circle_2 
   typename Kernel::FT det=-(c1.center().x() * c2.center().y() - c1.center().y() * c2.center().x())
                           +(c.center().x() * c2.center().y() - c.center().y() * c2.center().x())
                           -(c.center().x() * c1.center().y() - c.center().y() * c1.center().x());
+  if (det==0.0){ // degenerate casse, radical axis
+    return  typename Kernel::Circle_2();
+  }
   typename Kernel::FT x=(  -(z1 * c2.center().y() - c1.center().y() * z2)
 	                +(z * c2.center().y() - c.center().y() * z2)
 	                -(z * c1.center().y() - c.center().y() * z1))/2/det;
