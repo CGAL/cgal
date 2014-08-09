1bc8f2896952bbe1fbc804b8a94e7272d2e80dc4
Sebastian Morr <sebastian@morr.cc>
2014-08-09 08:58:52 +0200

    Adapt to package style: Indent two spaces, brackets on own lines
    Astyle options:
    
        --style=break
        --indent=spaces=2
        --max-code-length=80
        --attach-namespaces

--- Minkowski_sum_2/include/CGAL/Minkowski_sum_2/AABB_collision_detector_2.h
+++ Minkowski_sum_2/include/CGAL/Minkowski_sum_2/AABB_collision_detector_2.h
@@ -9,40 +9,48 @@ namespace CGAL {
 
 // Tests whether two polygons P and Q overlap for different translations of Q.
 template <class Kernel_, class Container_>
-class AABB_collision_detector_2 {
+class AABB_collision_detector_2
+{
 
 public:
 
-    typedef typename Kernel_::Point_2 Point_2;
-    typedef typename Kernel_::Vector_2 Vector_2;
-    typedef typename CGAL::Polygon_2<Kernel_> Polygon_2;
-    typedef typename Polygon_2::Edge_const_iterator Edge_iterator;
-    typedef AABB_segment_2_primitive<Kernel_, Edge_iterator, Polygon_2> Tree_segment_2;
-    typedef AABB_traits_2<Kernel_, Tree_segment_2> Tree_traits;
-    typedef AABB_tree<Tree_traits> Tree_2;
+  typedef typename Kernel_::Point_2 Point_2;
+  typedef typename Kernel_::Vector_2 Vector_2;
+  typedef typename CGAL::Polygon_2<Kernel_> Polygon_2;
+  typedef typename Polygon_2::Edge_const_iterator Edge_iterator;
+  typedef AABB_segment_2_primitive<Kernel_, Edge_iterator, Polygon_2>
+  Tree_segment_2;
+  typedef AABB_traits_2<Kernel_, Tree_segment_2> Tree_traits;
+  typedef AABB_tree<Tree_traits> Tree_2;
 
 public:
 
-    AABB_collision_detector_2(const Polygon_2 &p, const Polygon_2 &q)
-        : m_stationary_tree((p.edges_begin()), (p.edges_end())), m_translating_tree((q.edges_begin()), (q.edges_end())), m_p(q), m_q(p) {
+  AABB_collision_detector_2(const Polygon_2 &p, const Polygon_2 &q)
+    : m_stationary_tree((p.edges_begin()), (p.edges_end())),
+      m_translating_tree((q.edges_begin()), (q.edges_end())), m_p(q), m_q(p)
+  {
+  }
+
+  // Returns true iff the polygons' boundaries intersect or one polygon is completely inside of the other one. Q is translated by t.
+  bool check_collision(const Point_2 &t)
+  {
+    if (m_stationary_tree.do_intersect(m_translating_tree, t))
+    {
+      return true;
     }
 
-    // Returns true iff the polygons' boundaries intersect or one polygon is completely inside of the other one. Q is translated by t.
-    bool check_collision(const Point_2 &t) {
-        if (m_stationary_tree.do_intersect(m_translating_tree, t)) {
-            return true;
-        }
-
-        Polygon_2 translated_p = transform(typename Kernel_::Aff_transformation_2(Translation(), Vector_2(ORIGIN, t)), m_p);
-        return (translated_p.has_on_bounded_side(*(m_q.vertices_begin())) || m_q.has_on_bounded_side(*(translated_p.vertices_begin())));
-    }
+    Polygon_2 translated_p = transform(typename Kernel_::Aff_transformation_2(
+                                         Translation(), Vector_2(ORIGIN, t)), m_p);
+    return (translated_p.has_on_bounded_side(*(m_q.vertices_begin()))
+            || m_q.has_on_bounded_side(*(translated_p.vertices_begin())));
+  }
 
 private:
 
-    Tree_2 m_stationary_tree;
-    Tree_2 m_translating_tree;
-    const Polygon_2 &m_p;
-    const Polygon_2 &m_q;
+  Tree_2 m_stationary_tree;
+  Tree_2 m_translating_tree;
+  const Polygon_2 &m_p;
+  const Polygon_2 &m_q;
 };
 
 } // namespace CGAL
--- Minkowski_sum_2/include/CGAL/Minkowski_sum_2/AABB_segment_2_primitive.h
+++ Minkowski_sum_2/include/CGAL/Minkowski_sum_2/AABB_segment_2_primitive.h
@@ -5,40 +5,46 @@ namespace CGAL {
 
 // Wraps around a Segment_2 and provides its iterator as Id
 template <class GeomTraits, class Iterator_, class ContainerType>
-class AABB_segment_2_primitive {
+class AABB_segment_2_primitive
+{
 
 public:
 
-    typedef Iterator_ Id;
-    typedef typename GeomTraits::Segment_2 Datum;
-    typedef typename GeomTraits::Point_2 Point;
-    typedef ContainerType Container;
+  typedef Iterator_ Id;
+  typedef typename GeomTraits::Segment_2 Datum;
+  typedef typename GeomTraits::Point_2 Point;
+  typedef ContainerType Container;
 
-    AABB_segment_2_primitive() {}
+  AABB_segment_2_primitive() {}
 
-    AABB_segment_2_primitive(Id it) : m_it(it) {
-    }
+  AABB_segment_2_primitive(Id it) : m_it(it)
+  {
+  }
 
-    AABB_segment_2_primitive(const AABB_segment_2_primitive &primitive) {
-        m_it = primitive.id();
-    }
+  AABB_segment_2_primitive(const AABB_segment_2_primitive &primitive)
+  {
+    m_it = primitive.id();
+  }
 
-    const Id &id() const {
-        return m_it;
-    }
+  const Id &id() const
+  {
+    return m_it;
+  }
 
-    const Datum datum() const {
-        return *m_it;
-    }
+  const Datum datum() const
+  {
+    return *m_it;
+  }
 
-    // Return a point on the primitive
-    Point reference_point() const {
-        return datum().source();
-    }
+  // Return a point on the primitive
+  Point reference_point() const
+  {
+    return datum().source();
+  }
 
 private:
 
-    Id m_it;
+  Id m_it;
 
 };
 
