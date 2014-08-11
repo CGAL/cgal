722ea5c9da1fb423b90f2622fb5d6a2368608a0f
Sebastian Morr <sebastian@morr.cc>
2014-08-11 17:19:32 +0200

    Rewrote test_minkowski_sum

--- Minkowski_sum_2/test/Minkowski_sum_2/read_polygon.h
+++ Minkowski_sum_2/test/Minkowski_sum_2/read_polygon.h
@@ -1,52 +1,22 @@
-#ifndef CGAL_READ_POLYGON_TEST_H
-#define CGAL_READ_POLYGON_TEST_H
+#ifndef CGAL_READ_POLYGON_H
+#define CGAL_READ_POLYGON_H
 
 #include <CGAL/Polygon_2.h>
 #include <iostream>
 #include <fstream>
 
-/*!
- * Read a polygon from an input file.
- * \param filename The name of the input file.
- * \param pgn Output: The polygon.
- * \return Whether the polygon was successfuly read.
- */
 template <class Kernel>
-bool read_polygon (const char *filename, CGAL::Polygon_2<Kernel>& pgn)
+void read_polygon (const char *filename, CGAL::Polygon_2<Kernel>& pgn)
 {
-  std::ifstream ifile(filename);
-  ifile >> pgn;
+  std::ifstream file(filename);
 
-  // Make sure the polygon is simple.
-  if (! pgn.is_simple())
+  if (!file)
   {
-    std::cerr << "Error - the polygon is not simple." << std::endl;
-    return false;
+    std::cerr << "Failed to open " << filename << std::endl;
+    exit(1);
   }
 
-  return true;
-}
-
-template <class Kernel>
-bool read_polygon_with_holes (const char *filename, CGAL::Polygon_with_holes_2<Kernel>& pgn)
-{
-  std::ifstream ifile(filename);
-  ifile >> pgn;
-  // TODO: what can go wrong?
-  return true;
-}
-
-template <class Kernel>
-bool write_polygon_with_holes (const char *filename, CGAL::Polygon_with_holes_2<Kernel>& pgn)
-{
-  std::cout << filename << std::endl;
-  std::ofstream ofile(filename);
-  if (ofile.is_open()) {
-      ofile << pgn;
-  } else {
-      exit(1);
-  }
-  return true;
+  file >> pgn;
 }
 
 #endif
