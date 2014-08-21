c8365ed554a933cb804f6131b24e041cb4eff2ca
Waqar Khan <waqark1@gmail.com>
2014-08-21 13:21:49 +0200

    new concept added.

---
+++ Arrangement_on_surface_2/doc/Arrangement_on_surface_2/Concepts/ArrApproximateTraits_2.h
@@ -0,0 +1,38 @@
+/*
+
+
+
+
+
+
+*/
+class ArrApproximateTraits_2{
+public:
+	/// \name Types 
+	/// @{
+
+	/*!
+	the number type used to approximate point coordinates. 
+	*/ 
+	typedef unspecified_type Approximate_number_type; 
+
+	/// @} 
+
+	/// \name Functor Types 
+	/// @{
+
+	/*!
+	models the concept `ArrTraits::Approximate_2`. 
+	*/ 
+	typedef unspecified_type Approximate_2; 
+
+	/// \name Accessing Functor Objects 
+	/// @{
+
+	/*!
+
+	*/ 
+	Approximate_2 approximate_2_object() const; 
+
+	/*!
+}
