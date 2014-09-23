93060c6649ff6a9f768101043a59dbb9de91d986
Stephen Kiazyk <s_kiazyk@hotmail.com>
2014-09-23 11:50:43 -0400

    Incorrect script name in run_benchmarks.sh

--- Surface_mesh_shortest_path/benchmark/Surface_mesh_shortest_path/run_benchmarks.sh
+++ Surface_mesh_shortest_path/benchmark/Surface_mesh_shortest_path/run_benchmarks.sh
@@ -3,7 +3,7 @@
 if [ ! -a CMakeLists.txt ] 
   then
     echo "CMakeLists.txt does not exist, generating..."
-    cgal_create_CMakeLists -b program_options:timer
+    cgal_create_CMakeLists -b program_options:timer -c Core
 fi
 
-python compilePlots.py testModels.txt _modeldata benchmark_table benchmark_plot 6062699
+python compileBenchmarks.py testModels.txt _modeldata benchmark_table benchmark_plot 6062699
