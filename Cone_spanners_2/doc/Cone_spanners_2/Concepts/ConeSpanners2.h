35ab88bd17d73fac962f2da01f84194eb113f23c
Andreas Fabri <andreas.fabri@geometryfactory.com>
2014-09-23 12:10:21 +0200

    Add concept ConeSpanners_2

---
+++ Cone_spanners_2/doc/Cone_spanners_2/Concepts/ConeSpanners2.h
@@ -0,0 +1,29 @@
+
+/*!
+\ingroup PkgConeBasedSpannersConcepts
+\cgalConcept
+
+The concept `ConeSpanners2` defines . 
+
+\cgalHasModel CGAL::Yao_graph_2`
+
+
+*/
+
+class ConeSpanners_2 {
+public:
+
+/// \name Types 
+/// @{
+
+
+/*!
+Point type. 
+*/ 
+typedef unspecified_type Point_3; 
+
+
+/// @}
+
+}; /* end ConeSpanners_2 */
+
