99462e7faaf41d58300844766ae1faa5a3f0e789
Laurent Rineau <laurent.rineau@cgal.org>
2014-10-03 16:26:33 +0200

    Fix missing header

--- Mesh_3/include/CGAL/Mesh_3/Protect_edges_sizing_field.h
+++ Mesh_3/include/CGAL/Mesh_3/Protect_edges_sizing_field.h
@@ -33,6 +33,7 @@
 #ifndef CGAL_NO_ASSERTIONS
 #  include <boost/math/special_functions/next.hpp> // for float_prior
 #endif
+#include <boost/function_output_iterator.hpp>
 
 namespace CGAL {
 namespace Mesh_3 {
