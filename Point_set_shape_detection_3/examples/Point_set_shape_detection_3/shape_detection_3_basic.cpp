239f41d73ccdc903b416c1d89d3322d04988b67c
Clement Jamin <clement.jamin@inria.fr>
2014-10-31 17:08:52 +0100

    Remove unused includes and typedef

--- Point_set_shape_detection_3/examples/Point_set_shape_detection_3/shape_detection_3_basic.cpp
+++ Point_set_shape_detection_3/examples/Point_set_shape_detection_3/shape_detection_3_basic.cpp
@@ -2,36 +2,29 @@
 #include <CGAL/IO/read_xyz_points.h>
 #include <CGAL/Point_with_normal_3.h>
 #include <CGAL/property_map.h>
-#include <CGAL/Timer.h> 
 
 #include <CGAL/Shape_detection_3.h>
 #include <CGAL/Plane_shape.h>
-#include <CGAL/Cylinder_shape.h>
-#include <CGAL/Cone_shape.h>
-#include <CGAL/Sphere_shape.h>
-#include <CGAL/Torus_shape.h>
 
 #include <iostream>
 #include <fstream>
 
 // Type declarations
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
-typedef Kernel::FT FT;
-typedef Kernel::Point_3 Point;
-typedef CGAL::Point_with_normal_3<Kernel> Point_with_normal;
-typedef std::vector<Point_with_normal> Pwn_list;
-typedef CGAL::Identity_property_map<Point_with_normal> Point_pmap;
-typedef CGAL::Normal_of_point_with_normal_pmap<Kernel> Normal_pmap;
+typedef CGAL::Point_with_normal_3<Kernel>                   Point_with_normal;
+typedef std::vector<Point_with_normal>                      Pwn_list;
+typedef CGAL::Identity_property_map<Point_with_normal>      Point_pmap;
+typedef CGAL::Normal_of_point_with_normal_pmap<Kernel>      Normal_pmap;
 
 // In Shape_detection_traits_3 the basic types, i.e., Point and Vector types
 // as well as iterator type and property maps, are defined.
 typedef CGAL::Shape_detection_traits_3<Kernel,
-  Point_list::iterator, Point_pmap, Normal_pmap> ShapeDetectionTraits;
-typedef CGAL::Shape_detection_3<ShapeDetectionTraits> Shape_detection;
+  Point_list::iterator, Point_pmap, Normal_pmap>            Traits;
+typedef CGAL::Shape_detection_3<Traits>                     Shape_detection;
 
 
-int main(int argc, char **argv) {
-
+int main(int argc, char **argv) 
+{
   // List of points with normals.
   Pwn_list points;
 	
@@ -40,10 +33,10 @@ int main(int argc, char **argv) {
   // and a property map for storing the normal vector associated to each point.
   std::ifstream stream("cube.pwn");
 
-  if (!stream ||
-    !CGAL::read_xyz_points_and_normals(stream,
+  if (!stream || !CGAL::read_xyz_points_and_normals(stream,
     std::back_inserter(points),
-    Normal_pmap())) {
+    Normal_pmap()))
+  {
       std::cerr << "Error: cannot read file cube.pwn" << std::endl;
       return EXIT_FAILURE;
   }
@@ -54,7 +47,7 @@ int main(int argc, char **argv) {
 
   // Registers planar shapes via the template Shape_factory
   sd.add_shape_factory(new 
-    CGAL::Shape_factory<CGAL::Plane_shape<ShapeDetectionTraits> >);
+    CGAL::Shape_factory<CGAL::Plane_shape<Traits> >);
 
   // Detects registered shapes with default parameters.
   sd.detect();
