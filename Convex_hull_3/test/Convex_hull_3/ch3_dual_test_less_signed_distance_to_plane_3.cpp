b5f728bfb543edd003d80f9fd36ffd8662e5cabe
Sébastien Loriot <sebastien.loriot@cgal.org>
2014-11-13 11:57:08 +0100

    fix warning

--- Convex_hull_3/test/Convex_hull_3/ch3_dual_test_less_signed_distance_to_plane_3.cpp
+++ Convex_hull_3/test/Convex_hull_3/ch3_dual_test_less_signed_distance_to_plane_3.cpp
@@ -14,21 +14,21 @@ typedef CGAL::Convex_hull_3::Plane_dual<K> Plane_dual;
 typedef CGAL::Convex_hull_3::Convex_hull_traits_dual_3<K> Hull_traits_dual;
 typedef Hull_traits_dual::Less_signed_distance_to_plane_3 Less_signed_distance_to_plane_3_dual;
 
-typedef CGAL::Convex_hull_traits_3<K> Hull_traits;
-typedef Hull_traits::Less_signed_distance_to_plane_3 Less_signed_distance_to_plane_3;
+// typedef CGAL::Convex_hull_traits_3<K> Hull_traits;
+// typedef Hull_traits::Less_signed_distance_to_plane_3 Less_signed_distance_to_plane_3;
 
 int main () {
     Hull_traits_dual traits_dual;
-    Hull_traits traits;
+    // Hull_traits traits;
 
-    Less_signed_distance_to_plane_3 less_signed_distance = traits.less_signed_distance_to_plane_3_object();
+    // Less_signed_distance_to_plane_3 less_signed_distance = traits.less_signed_distance_to_plane_3_object();
     Less_signed_distance_to_plane_3_dual less_signed_distance_dual = traits_dual.less_signed_distance_to_plane_3_object();
 
     // dual plane : x = 1
     Plane a(1, 1, 0, -1), b(1, 2, 1, -1), c(1, 3, 0, -1);
     Plane_dual p(a,b,c);
 
-    Plane q(2, 0, 0, -1), r(3, 0, 0, -1), rr(0.5, 0, 0, -1);
+    Plane q(2, 0, 0, -1), r(3, 0, 0, -1)/* , rr(0.5, 0, 0, -1) */;
 
     // False
     /* std::cout << less_signed_distance_dual(p, q, r) << std::endl; */
