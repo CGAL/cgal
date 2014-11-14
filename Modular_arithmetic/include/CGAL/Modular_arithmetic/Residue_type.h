b92bcae1a9186e5ff13305613d3b675df4cd85fa
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2014-11-14 14:53:54 +0100

    Header only for Residue_type.cpp

--- Modular_arithmetic/include/CGAL/Modular_arithmetic/Residue_type.h
+++ Modular_arithmetic/include/CGAL/Modular_arithmetic/Residue_type.h
@@ -32,8 +32,75 @@
 
 namespace CGAL {
 
+#ifdef CGAL_HAS_THREADS
+
+#ifdef CGAL_HEADER_ONLY
+inline boost::thread_specific_ptr<int>& get_static_prime_int_()
+{
+  static boost::thread_specific_ptr<int> prime_int_;
+  return prime_int_;
+}
+inline boost::thread_specific_ptr<double>& get_static_prime_()
+{
+  static boost::thread_specific_ptr<double> prime_;
+  return prime_;
+}
+inline boost::thread_specific_ptr<double>& get_static_prime_inv_()
+{
+  static boost::thread_specific_ptr<double> prime_inv_;
+  return prime_inv_;
+}
+#else // CGAL_HEADER_ONLY
+inline boost::thread_specific_ptr<int>& get_static_prime_int_()
+{ return Residue::prime_int_; }
+inline boost::thread_specific_ptr<double>& get_static_prime_()
+{ return Residue::prime_; }
+inline boost::thread_specific_ptr<double>& get_static_prime_inv_()
+{ return Residue::prime_inv_; }
+#endif // CGAL_HEADER_ONLY
+
+#else // CGAL_HAS_THREADS
+
+#ifdef CGAL_HEADER_ONLY
+inline int& get_static_prime_int()
+{
+  static int prime_int = 67111067;
+  return prime_int;
+}
+inline double& get_static_prime()
+{
+  static double prime = 67111067.0;
+  return prime;
+}
+inline double& get_static_prime_inv()
+{
+  static double prime_inv = 1/67111067.0;
+  return prime_inv;
+}
+#else // CGAL_HEADER_ONLY
+inline int& get_static_prime_int()
+{ return Residue::prime_int; }
+inline double& get_static_prime()
+{ return Residue::prime; }
+inline double& get_static_prime_inv()
+{ return Residue::prime_inv; }
+#endif // CGAL_HEADER_ONLY
+
+#endif // CGAL_HAS_THREADS
+
+#ifdef CGAL_HEADER_ONLY
+inline double& get_static_CST_CUT()
+{
+  static double CST_CUT = std::ldexp( 3., 51 );
+  return CST_CUT;
+}
+#else // CGAL_HEADER_ONLY
+inline double& get_static_CST_CUT()
+{ return Residue::CST_CUT; }
+#endif // CGAL_HEADER_ONLY
+
 class Residue;
-    
+
 Residue operator + (const Residue&);
 Residue operator - (const Residue&);
 
@@ -41,78 +108,86 @@ std::ostream& operator << (std::ostream& os, const Residue& p);
 std::istream& operator >> (std::istream& is, Residue& p);
 
 /*! \ingroup CGAL_Modular_traits
- * \brief This class represents the Field Z mod p. 
- *  
- * This class uses the type double for representation. 
+ * \brief This class represents the Field Z mod p.
+ *
+ * This class uses the type double for representation.
  * Therefore the value of p is restricted to primes less than 2^26.
  * By default p is set to 67111067.
  *
  * It provides the standard operators +,-,*,/ as well as in&output.
- * 
+ *
  * \see Modular_traits
  */
 class Residue:
     boost::ordered_field_operators1< Residue,
     boost::ordered_field_operators2< Residue, int > >{
-    
+
 public:
   typedef Residue Self;
   typedef Residue NT;
-  
+
 private:
-  CGAL_EXPORT static const double  CST_CUT; 
-  
+#ifndef CGAL_HEADER_ONLY
+  CGAL_EXPORT static const double  CST_CUT;
+#endif // CGAL_HEADER_ONLY
+
 #ifdef CGAL_HAS_THREADS
+
+#ifndef CGAL_HEADER_ONLY
   CGAL_EXPORT static boost::thread_specific_ptr<int>    prime_int_;
   CGAL_EXPORT static boost::thread_specific_ptr<double> prime_;
   CGAL_EXPORT static boost::thread_specific_ptr<double> prime_inv_;
-  
+#endif // CGAL_HEADER_ONLY
+
   static void init_class_for_thread(){
-    CGAL_precondition(prime_int_.get() == NULL); 
-    CGAL_precondition(prime_.get()     == NULL); 
-    CGAL_precondition(prime_inv_.get() == NULL); 
-    prime_int_.reset(new int(67111067));
-    prime_.reset(new double(67111067.0));
-    prime_inv_.reset(new double(1.0/67111067.0));
+    CGAL_precondition(get_static_prime_int_().get() == NULL);
+    CGAL_precondition(get_static_prime_().get()     == NULL);
+    CGAL_precondition(get_static_prime_inv_().get() == NULL);
+    get_static_prime_int_().reset(new int(67111067));
+    get_static_prime_().reset(new double(67111067.0));
+    get_static_prime_inv_().reset(new double(1.0/67111067.0));
   }
-  
+
   static inline int get_prime_int(){
-    if (prime_int_.get() == NULL)
+    if (get_static_prime_int_().get() == NULL)
       init_class_for_thread();
-    return *prime_int_.get();
+    return *get_static_prime_int_().get();
   }
-  
+
   static inline double get_prime(){
-    if (prime_.get() == NULL)
+    if (get_static_prime_().get() == NULL)
       init_class_for_thread();
-    return *prime_.get();
+    return *get_static_prime_().get();
   }
-  
+
   static inline double get_prime_inv(){
-    if (prime_inv_.get() == NULL)
+    if (get_static_prime_inv_().get() == NULL)
       init_class_for_thread();
-    return *prime_inv_.get();
+    return *get_static_prime_inv_().get();
   }
-#else
+#else // CGAL_HAS_THREADS
+
+#ifndef CGAL_HEADER_ONLY
   CGAL_EXPORT  static int prime_int;
   CGAL_EXPORT  static double prime;
   CGAL_EXPORT  static double prime_inv;
-  static int get_prime_int(){ return prime_int;}
-  static double get_prime()    { return prime;}
-  static double get_prime_inv(){ return prime_inv;}  
+#endif // CGAL_HEADER_ONLY
+  static int get_prime_int(){ return get_static_prime_int();}
+  static double get_prime()    { return get_static_prime();}
+  static double get_prime_inv(){ return get_static_prime_inv();}
 #endif
 
-    /* Quick integer rounding, valid if a<2^51. for double */ 
-    static inline 
+    /* Quick integer rounding, valid if a<2^51. for double */
+    static inline
     double RES_round (double a){
       // call CGAL::Protect_FPU_rounding<true> pfr(CGAL_FE_TONEAREST)
-      // before using modular arithmetic 
+      // before using modular arithmetic
       CGAL_assertion(FPU_get_cw() == CGAL_FE_TONEAREST);
-      return ( (a + CST_CUT)  - CST_CUT);      
+      return ( (a + get_static_CST_CUT())  - get_static_CST_CUT());
     }
 
     /* Big modular reduction (e.g. after multiplication) */
-    static inline 
+    static inline
     double RES_reduce (double a){
       double result = a - get_prime() * RES_round(a * get_prime_inv());
       CGAL_postcondition(2*result <  get_prime());
@@ -121,7 +196,7 @@ private:
     }
 
     /* Little modular reduction (e.g. after a simple addition). */
-    static inline 
+    static inline
     double RES_soft_reduce (double a){
       double p = get_prime();
         double b = 2*a;
@@ -129,16 +204,16 @@ private:
             ((b<-p) ? a+p : a);
     }
 
-    
+
     /* -a */
-    static inline 
+    static inline
     double RES_negate(double a){
         return RES_soft_reduce(-a);
     }
 
 
     /* a*b */
-    static inline 
+    static inline
     double RES_mul (double a, double b){
         double c = a*b;
         return RES_reduce(c);
@@ -146,21 +221,21 @@ private:
 
 
     /* a+b */
-    static inline 
+    static inline
     double RES_add (double a, double b){
         double c = a+b;
         return RES_soft_reduce(c);
     }
 
-    
+
     /* a^-1, using Bezout (extended Euclidian algorithm). */
-    static inline 
+    static inline
     double RES_inv (double ri1){
         double bi = 0.0;
         double bi1 = 1.0;
         double ri = get_prime();
         double p, tmp, tmp2;
-    
+
         Real_embeddable_traits<double>::Abs double_abs;
         while (double_abs(ri1) != 1.0)
         {
@@ -175,131 +250,131 @@ private:
 
         return ri1 * RES_soft_reduce(bi1);	/* Quicker !!!! */
     }
-    
+
     /* a/b */
-    static inline 
+    static inline
     double RES_div (double a, double b){
         return RES_mul(a, RES_inv(b));
-    }    
+    }
 
 public:
-    /*! \brief sets the current prime. 
-     *  
+    /*! \brief sets the current prime.
+     *
      *  Note that you are going to change a static member!
      *  \pre p is prime, but we abstained from such a test.
      *  \pre 0 < p < 2^26
-     *  
+     *
      */
-    static int 
-    set_current_prime(int p){   
-      int old_prime = get_prime_int();  
+    static int
+    set_current_prime(int p){
+      int old_prime = get_prime_int();
 #ifdef CGAL_HAS_THREADS
-      *prime_int_.get() = p;
-      *prime_.get() = double(p);
-      *prime_inv_.get() = 1.0/double(p);
+      *get_static_prime_int_().get() = p;
+      *get_static_prime_().get() = double(p);
+      *get_static_prime_inv_().get() = 1.0/double(p);
 #else
-      prime_int = p;
-      prime = double(p);
-      prime_inv = 1.0 / prime;
+      get_static_prime_int() = p;
+      get_static_prime() = double(p);
+      get_static_prime_inv() = 1.0 / prime;
 #endif
-      return old_prime; 
+      return old_prime;
     }
- 
+
   /*! \brief return the current prime.  */
     static int get_current_prime(){
       return get_prime_int();
     }
-  
+
   int  get_value() const{
     CGAL_precondition(2*x_ <  get_prime());
     CGAL_precondition(2*x_ > -get_prime());
     return int(x_);
   }
-    
+
 private:
     double x_;
 
-public: 
+public:
 
-    //! constructor of Residue, from int 
+    //! constructor of Residue, from int
     Residue(int n = 0){
         x_= RES_reduce(n);
     }
 
-    //! constructor of Residue, from long 
+    //! constructor of Residue, from long
     Residue(long n){
         x_= RES_reduce((double)n);
     }
-   
-    //! Access operator for x, \c const 
+
+    //! Access operator for x, \c const
     const double& x() const { return x_; }
     //! Access operator for x
-    double&       x()       { return x_; }                     
+    double&       x()       { return x_; }
 
-    Self& operator += (const Self& p2) { 
-        x() = RES_add(x(),p2.x()); 
-        return (*this); 
+    Self& operator += (const Self& p2) {
+        x() = RES_add(x(),p2.x());
+        return (*this);
     }
-    Self& operator -= (const Self& p2){ 
-        x() = RES_add(x(),RES_negate(p2.x())); 
-        return (*this); 
+    Self& operator -= (const Self& p2){
+        x() = RES_add(x(),RES_negate(p2.x()));
+        return (*this);
     }
-    Self& operator *= (const Self& p2){ 
-        x() = RES_mul(x(),p2.x()); 
-        return (*this); 
+    Self& operator *= (const Self& p2){
+        x() = RES_mul(x(),p2.x());
+        return (*this);
     }
-    Self& operator /= (const Self& p2) { 
-        x() = RES_div(x(),p2.x()); 
-        return (*this); 
+    Self& operator /= (const Self& p2) {
+        x() = RES_div(x(),p2.x());
+        return (*this);
     }
-    // 
-    Self& operator += (int p2) { 
-        x() = RES_add(x(),Residue(p2).x()); 
-        return (*this); 
+    //
+    Self& operator += (int p2) {
+        x() = RES_add(x(),Residue(p2).x());
+        return (*this);
     }
-    Self& operator -= (int p2){ 
-        x() = RES_add(x(),Residue(-p2).x()); 
-        return (*this); 
-    }
-
-    Self& operator *= (int p2){ 
-        x() = RES_mul(x(),Residue(p2).x()); 
-        return (*this); 
+    Self& operator -= (int p2){
+        x() = RES_add(x(),Residue(-p2).x());
+        return (*this);
     }
 
-    Self& operator /= (int p2) { 
-        x() = RES_div(x(),Residue(p2).x()); 
-        return (*this); 
+    Self& operator *= (int p2){
+        x() = RES_mul(x(),Residue(p2).x());
+        return (*this);
     }
-  
+
+    Self& operator /= (int p2) {
+        x() = RES_div(x(),Residue(p2).x());
+        return (*this);
+    }
+
     friend Self operator + (const Self&);
-    friend Self operator - (const Self&);                
+    friend Self operator - (const Self&);
 };
 
 inline Residue operator + (const Residue& p1)
 { return p1; }
 
-inline Residue operator - (const Residue& p1){ 
+inline Residue operator - (const Residue& p1){
     typedef Residue RES;
-    Residue r; 
+    Residue r;
     r.x() = RES::RES_negate(p1.x());
-    return r; 
+    return r;
 }
 
 inline bool operator == (const Residue& p1, const Residue& p2)
-{ return ( p1.x()==p2.x() ); }   
+{ return ( p1.x()==p2.x() ); }
 inline bool operator == (const Residue& p1, int p2)
-{ return ( p1 == Residue(p2) ); }   
+{ return ( p1 == Residue(p2) ); }
 
 
 inline bool operator < (const Residue& p1, const Residue& p2)
-{ return ( p1.x() < p2.x() ); }   
+{ return ( p1.x() < p2.x() ); }
 inline bool operator < (const Residue& p1, int p2)
-{ return ( p1.x() < Residue(p2).x() ); }   
+{ return ( p1.x() < Residue(p2).x() ); }
 
 
-// I/O 
-inline std::ostream& operator << (std::ostream& os, const Residue& p) {   
+// I/O
+inline std::ostream& operator << (std::ostream& os, const Residue& p) {
     typedef Residue RES;
     os <<"("<< int(p.x())<<"%"<<RES::get_current_prime()<<")";
     return os;
