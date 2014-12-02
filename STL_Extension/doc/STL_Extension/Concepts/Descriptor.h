2f3b5fcc8676b82e9fb189019b89759bb45a2d61
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2014-12-02 11:02:52 +0100

    Add the two concepts Descriptor and Index (concern only the doc).

---
+++ STL_Extension/doc/STL_Extension/Concepts/Descriptor.h
@@ -0,0 +1,16 @@
+/*!
+\ingroup PkgStlExtensionConcepts
+\cgalConcept
+
+The concept `Descriptor` allows to describe a unique object in an abstract model.
+
+\cgalRefines DefaultConstructible
+\cgalRefines CopyConstructible
+\cgalRefines Assignable
+\cgalRefines EqualityComparable
+
+\cgalHasModel `Index`
+\cgalHasModel `Handle`
+*/
+
+class Descriptor {};
