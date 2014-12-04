07860bb1ca5f9aaab4fd4bc08d7cc68208c01594
Aymeric PELLE <aymeric.pelle@inria.fr>
2014-12-04 15:34:51 +0100

    Update Periodic_3_triangulation_3 doc.
    Concepts:
    Periodic_3TriangulationTraits_3
    Periodic_3DelaunayTriangulationTraits_3
    
    Traits:
    Periodic_3_Delaunay_triangulation_traits_3
    Periodic_3_triangulation_traits_3

--- Periodic_3_triangulation_3/doc/Periodic_3_triangulation_3/CGAL/Periodic_3_Delaunay_triangulation_traits_3.h
+++ Periodic_3_triangulation_3/doc/Periodic_3_triangulation_3/CGAL/Periodic_3_Delaunay_triangulation_traits_3.h
@@ -4,10 +4,10 @@ namespace CGAL {
 /*!
 \ingroup PkgPeriodic3Triangulation3TraitsClasses
 
-The class `Periodic_3_triangulation_traits_3` is designed as a default traits class for the
-class `Periodic_3_triangulation_3<Periodic_3TriangulationTraits_3,TriangulationDataStructure_3>`. 
+The class `Periodic_3_Delaunay_triangulation_traits_3` is designed as a default traits class for the
+class `Periodic_3_Delaunay_triangulation_3<Periodic_3DelaunayTriangulationTraits_3,TriangulationDataStructure_3>`.
 
-\tparam Traits must be a model of the `Periodic_3TriangulationTraits_3` concept.
+\tparam Traits must be a model of the `Periodic_3DelaunayTriangulationTraits_3` concept.
 \tparam Periodic_3Offset_3 must be a model of the concept `Periodic_3Offset_3` and defaults to `Periodic_3_offset_3`. 
 
 Note that this template class is specialized for 
@@ -16,10 +16,10 @@ filtered predicates. This holds implicitly for
 `CGAL::Exact_predicates_inexact_constructions_kernel`, as it is an 
 instantiation of `CGAL::Filtered_kernel`. 
 
-\cgalModels `Periodic_3TriangulationTraits_3`
+\cgalModels `Periodic_3DelaunayTriangulationTraits_3`
 */
 template< typename Traits, typename Periodic_3Offset_3 >
-class Periodic_3_triangulation_traits_3 : public Traits {
+class Periodic_3_Delaunay_triangulation_traits_3 : public Traits {
 public:
-}; /* end Periodic_3_triangulation_traits_3 */
+}; /* end Periodic_3_Delaunay_triangulation_traits_3 */
 } /* end namespace CGAL */
--- Periodic_3_triangulation_3/doc/Periodic_3_triangulation_3/Concepts/Periodic_3TriangulationTraits_3.h
+++ Periodic_3_triangulation_3/doc/Periodic_3_triangulation_3/Concepts/Periodic_3TriangulationTraits_3.h
@@ -3,31 +3,31 @@
 \ingroup PkgPeriodic3Triangulation3Concepts
 \cgalConcept
 
-The concept `Periodic_3DelaunayTriangulationTraits_3` is the first template parameter of the classes 
-`Periodic_3_Delaunay_triangulation_3`. It refines the concept
-`DelaunayTriangulationTraits_3` from the \cgal \ref PkgTriangulation3 package. 
+The concept `Periodic_3TriangulationTraits_3` is the first template parameter of the classes
+`Periodic_3_triangulation_3`. It refines the concept 
+`TriangulationTraits_3` from the \cgal \ref PkgTriangulation3 package.
 It redefines the geometric objects, predicates and constructions to 
 work with point-offset pairs. In most cases the offsets will be 
-(0,0,0) and the predicates from `DelaunayTriangulationTraits_3` 
+(0,0,0) and the predicates from `TriangulationTraits_3`
 can be used directly. For efficiency reasons we maintain for each 
 functor the version without offsets. 
 
-\cgalRefines `DelaunayTriangulationTraits_3` 
+\cgalRefines `TriangulationTraits_3`
 
-\cgalHasModel CGAL::Periodic_3_Delaunay_triangulation_traits_3 
+\cgalHasModel CGAL::Periodic_3_triangulation_traits_3
 
-\sa `DelaunayTriangulationTraits_3` 
+\sa `TriangulationTraits_3`
 
 In addition to the requirements described for the traits class 
-DelaunayTriangulationTraits_3, the geometric traits class of a 
-Periodic Delaunay triangulation must fulfill the following 
+TriangulationTraits_3, the geometric traits class of a
+Periodic triangulation must fulfill the following
 requirements.
 
 \note The optional types must be provided in any case, however they
 can be replaced by dummy types if the respective functions are not
 used.
 */
-class Periodic_3DelaunayTriangulationTraits_3 {
+class Periodic_3TriangulationTraits_3 {
 public:
 
 /// \name Types 
@@ -119,39 +119,6 @@ returns `NEGATIVE` if `(s,o_s)` lies on the negative side of
 */ 
 typedef unspecified_type Orientation_3; 
 
-/*!
-A predicate object that must provide the function operators 
-
-`Oriented_side operator()(Point_3 p, Point_3 q, Point_3 r, Point_3 s, Point_3 t)`, 
-
-which determines on which side of the oriented sphere circumscribing 
-`p, q, r, s` the point `t` lies and 
-
-`Oriented_side operator()(Point_3 p, Point_3 q, Point_3 r, Point_3 s, Point_3 t, Periodic_3_offset_3 o_p, Periodic_3_offset_3 o_q, Periodic_3_offset_3 o_r, Periodic_3_offset_3 o_s, Periodic_3_offset_3 o_t)`, 
-
-which determines on which side of the oriented sphere circumscribing 
-`(p,o_p), (q,o_q), (r,o_r), (s,o_s)` the point-offset pair 
-`(t,o_t)` lies. 
-\pre `p`, `q`, `r`, `s`, `t` lie inside the domain. 
-*/ 
-typedef unspecified_type Side_of_oriented_sphere_3; 
-
-/*!
-A predicate object that must provide the function operators 
-
-`Comparison_result operator()(Point_3 p, Point_3 q, Point_3 r)`, 
-
-which compares the distance between `p` and `q` to the distance 
-between `p` and `r` and 
-
-`Comparison_result operator()(Point_3 p, Point_3 q, Point_3 r, Periodic_3_offset_3 o_p, Periodic_3_offset_3 o_q, Periodic_3_offset_3 o_r)`, 
-
-which compares the distance between `(p,o_p)` and `(q,o_q)` to 
-the distance between `(p,o_p)` and `(r,o_r)`. 
-\pre `p`, `q`, `r` lie inside the domain. 
-*/ 
-typedef unspecified_type Compare_distance_3; 
-
 /// @}
 
 /// \name
@@ -192,53 +159,6 @@ which determines the bounded side of the circle defined by
 \pre `p,q,r`, and `s` are coplanar and `p,q`, and `r` are not collinear, `(p,o_p),(q,o_q),(r,o_r)`, and `(s,o_s)` are coplanar and `(p,o_p),(q,o_q)`, and `(r,o_r)` are not collinear, respectively, and `p`, `q`, `r`, `s`, `t` lie inside the domain. 
 */ 
 typedef unspecified_type Coplanar_side_of_bounded_circle_3; 
-
-/// @}
-
-/// \name
-/// In addition, only when `is_Gabriel` is used, the traits class must
-/// provide the following predicate object:
-/// @{
-
-/*!
-A predicate object that must provide the function operators 
-
-`Bounded_side operator()(Point_3 p, Point_3 q, Point_3 t)`, 
-
-which returns the position of the point `t` relative to the sphere 
-that has `pq` as its diameter, 
-
-`Bounded_side operator()(Point_3 p, Point_3 q, Point_3 t, Periodic_3_offset_3 o_p, Periodic_3_offset_3 o_q, Periodic_3_offset_3 o_t)`, 
-
-which returns the position of the point-offset pair `(t,o_t)` 
-relative to the sphere that has `(p,o_p)(q,o_q)` as its diameter, 
-
-`Bounded_side operator()(Point_3 p, Point_3 q, Point_3 r, Point_3 t)`, 
-
-which returns the position of the point `t` relative to the sphere 
-passing through `p, q`, and `r` and whose center is in the 
-plane defined by these three points, 
-
-`Bounded_side operator()(Point_3 p, Point_3 q, Point_3 r, Point_3 t, Periodic_3_offset_3 o_p, Periodic_3_offset_3 o_q, Periodic_3_offset_3 o_r, Periodic_3_offset_3 o_q)`, 
-
-which returns the position of the point-offset pair `(t,o_t)` 
-relative to the sphere passing through `(p,o_p), (q,o_q)`, and 
-`(r,o_r)` and whose center is in the plane defined by these three 
-point-offset pairs, 
-
-`Bounded_side operator()(Point_3 p, Point_3 q, Point_3 r, Point_3 s, Point_3 t)`, 
-
-which returns the relative position of point `t` to the sphere 
-defined by `p, q, r`, and `s`; the order of the points `p, q, r`, and `s` does not matter, and 
-
-`Bounded_side operator()(Point_3 p, Point_3 q, Point_3 r, Point_3 s, Point_3 t, Periodic_3_offset_3 o_p, Periodic_3_offset_3 o_q, Periodic_3_offset_3 o_r, Periodic_3_offset_3 o_s, Periodic_3_offset_3 o_q)`, 
-
-which returns the relative position of the point-offset pair 
-`(t,o_t)` to the sphere defined by `(p,o_p), (q,o_q), (r,o_r)`, and `(s,o_s)`; the order of the point-offset pairs 
-`(p,o_p), (q,o_q), (r,o_r)`, and `(s,o_s)` does not matter. 
-\pre `p, q, r`, and `s` are not coplanar, `(p,o_p), (q,o_q), (r,o_r)`, and `(s,o_s)` are not coplanar, `p`, `q`, `r`, `s`, `t` lie inside the domain. 
-*/ 
-typedef unspecified_type Side_of_bounded_sphere_3; 
 
 /// @}
 
@@ -301,39 +221,18 @@ typedef unspecified_type Construct_tetrahedron_3;
 
 /// @}
 
-/// \name
-/// In addition, only when the dual operations are used, the traits
-/// class must provide the following constructor object:
-/// @{
-
-/*!
-A constructor object that must provide the function operators 
-
-`Point_3 operator()(Point_3 p, Point_3 q, Point_3 r, Point_3 s)`, 
-
-which constructs the circumcenter of four points and 
-
-`Point_3 operator()(Point_3 p, Point_3 q, Point_3 r, Point_3 s, Periodic_3_offset_3 o_p, Periodic_3_offset_3 o_q, Periodic_3_offset_3 o_r, Periodic_3_offset_3 o_s)`, 
-
-which constructs the circumcenter of four point-offset pairs. 
-\pre `p`, `q`, `r` and `s` as well as `(p,o_p)`, `(q,o_q)`, `(r,o_r)` and `(s,o_s)` must be non coplanar. `p`, `q`, `r`, `s` lie inside the domain. 
-*/ 
-typedef unspecified_type Construct_circumcenter_3; 
-
-/// @} 
-
 /// \name Creation 
 /// @{
 
 /*!
 Default constructor. 
 */ 
-Periodic_3_Delaunay_triangulation_traits_3(); 
+Periodic_3_triangulation_traits_3();
 
 /*!
 Copy constructor. 
 */ 
-Periodic_3_Delaunay_triangulation_traits_3(const Periodic_triangulation_traits_3 & tr); 
+Periodic_3_triangulation_traits_3(const Periodic_triangulation_traits_3 & tr);
 
 /// @} 
 
@@ -364,16 +263,6 @@ Compare_xyz_3 compare_xyz_3_object();
 */ 
 Orientation_3 orientation_3_object(); 
 
-/*!
-
-*/ 
-Side_of_oriented_sphere_3 side_of_oriented_sphere_3_object(); 
-
-/*!
-
-*/ 
-Compare_distance_3 compare_distance_3_object(); 
-
 /// @}
 
 /// \name
@@ -394,16 +283,7 @@ coplanar_side_of_bounded_circle_3_object();
 
 /// @}
 
-/// \name
-/// The following function must be provided only if the `is_Gabriel`
-/// methods of `Periodic_3_Delaunay_triangulation_3` are used;
-/// otherwise a dummy function can be provided.
 /// @{
-
-/*!
-
-*/ 
-Side_of_bounded_sphere_3 side_of_bounded_sphere_3_object(); 
 
 /*!
 
@@ -419,20 +299,6 @@ Construct_triangle_3 construct_triangle_3_object();
 
 */ 
 Construct_tetrahedron_3 construct_tetrahedron_3_object(); 
-
-/// @}
-
-/// \name 
-/// The following function must be provided only if the methods of
-/// `Periodic_3_Delaunay_triangulation_3` returning elements of the
-/// Voronoi diagram are used; otherwise a dummy function can be
-/// provided:
-/// @{
-
-/*!
-
-*/ 
-Construct_circumcenter_3 construct_circumcenter_3_object(); 
 
 /// @}
 
