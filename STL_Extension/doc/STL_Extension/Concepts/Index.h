726f10a0a77313935ffed8e574d56ccfa1cb48bf
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2014-12-04 12:25:46 +0100

    Add missing spaces, replace size_t by std::size_t.

--- STL_Extension/doc/STL_Extension/Concepts/Index.h
+++ STL_Extension/doc/STL_Extension/Concepts/Index.h
@@ -2,7 +2,7 @@
 \ingroup PkgStlExtensionConcepts
 \cgalConcept
 
-The concept `Index` is a refinement of `Descriptor` which must be convertible from and to `size_t`, and must be incrementable and decrementable.
+The concept `Index` is a refinement of `Descriptor` which must be convertible from and to `std::size_t`, and must be incrementable and decrementable.
 
 \cgalRefines `Descriptor`
 
@@ -13,18 +13,18 @@ The concept `Index` is a refinement of `Descriptor` which must be convertible fr
 
 <dl>
 <dt>`I`</dt> 	  <dd>Object of type Index.</dd>
-<dt>`n`</dt> 	  <dd>Object of type size_t.</dd>
+<dt>`n`</dt> 	  <dd>Object of type `std::size_t`.</dd>
 </dl>
 
 \cgalHeading{Valid Expressions}
 
 Expression                              | Returns                                                                      | Description
 --------------------------------------- | ---------------------------------------------------------------------------- | -----------
-`Index I(n)`                            | -                                                                            | Creates an index `I` from a `size_t`.
-`(size_t)I`                             | `size_t`                                                                     | Converts `I` into a `size_t`.
-`++I`                                   | `Index&`                                                                     | Pre-increment s`I`.
+`Index I(n)`                            | -                                                                            | Creates an index `I` from a `std::size_t`.
+`(std::size_t)I`                        | `std::size_t`                                                                | Converts `I` into a `std::size_t`.
+`++I`                                   | `Index&`                                                                     | Pre-increments `I`.
 `I++`                                   | `Index`                                                                      | Post-increments `I`.
-`--I`                                   | `Index&`                                                                     | Pre-decrements`I`.
+`--I`                                   | `Index&`                                                                     | Pre-decrements `I`.
 `I--`                                   | `Index`                                                                      | Post-decrements `I`.
 */
 
