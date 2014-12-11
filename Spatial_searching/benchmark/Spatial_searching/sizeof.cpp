5e7deafb5fe6c8781f32ba41bc0f5f0c4deda9de
Andreas Fabri <andreas.fabri@geometryfactory.com>
2014-12-11 14:38:07 +0100

    Add sizeof.cpp

---
+++ Spatial_searching/benchmark/Spatial_searching/sizeof.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <boost/cstdint.hpp>
+
+
+struct Node {
+  bool b;
+};
+
+
+struct Plane_separator{ 
+  double cutting_val;
+  boost::uint16_t cutting_dim;
+};
+
+
+  struct InternalA : public Node {
+    Plane_separator ps;
+    double d;
+  };
+
+  struct InternalB : public Node {
+  boost::uint16_t cutting_dim;
+  double cutting_val;
+    double d;
+  };
+
+  struct InternalC : public Node {
+    //Plane_separator ps;
+  double cutting_val;
+  int cutting_dim;
+    double d;
+  };
+
+  struct Leaf : public Node {
+    boost::uint16_t i;
+    double d;
+  };
+
+
+
+
+int main()
+{
+  Node n;
+  Plane_separator ps;
+  InternalA ia;
+  InternalB ib;
+  InternalC ic;
+  Leaf l;
+  std::cerr << sizeof(int) << std::endl;
+  std::cerr << sizeof(double) << std::endl;
+  std::cerr << sizeof(n) << std::endl;
+  std::cerr << sizeof(ps) << std::endl;
+  std::cerr << sizeof(ia) << std::endl;
+  std::cerr << sizeof(ib) << std::endl;
+  std::cerr << sizeof(ic) << std::endl;
+  std::cerr << sizeof(l) << std::endl;
+
+  Leaf  leafs[10];
+  std::cerr << sizeof(leafs) << std::endl;
+  
+  Node nodes[10];
+  std::cerr << sizeof(nodes) << std::endl;
+  return 0;
+}
