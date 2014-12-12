8b0993375203c227db2004a86a179ae7bd84757e
Clement Jamin <clement.jamin@inria.fr>
2014-12-12 16:39:15 +0100

    Forgot to add the StdAfx files

---
+++ Tangential_complex/test/Tangential_complex/StdAfx.h
@@ -0,0 +1,13 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+// CGAL
+#include <CGAL/Epick_d.h>
+#include <CGAL/Regular_triangulation_euclidean_traits.h>
+#include <CGAL/Regular_triangulation.h>
+#include <CGAL/Delaunay_triangulation.h>
+#include <CGAL/Tangential_complex/utilities.h>
+#include <CGAL/Tangential_complex/Point_cloud.h>
+#include <CGAL/Combination_enumerator.h>
+
+#endif //STDAFX_H
