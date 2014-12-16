bff2e043dc65bf8c7c1707a67f94ec4bd48bc009
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2014-12-16 10:30:14 +0100

    Initialize consts.

--- Kernel_23/src/CGAL/kernel.cpp
+++ Kernel_23/src/CGAL/kernel.cpp
@@ -27,11 +27,11 @@
 
 namespace CGAL {
 
-const Translation             TRANSLATION;
-const Rotation                ROTATION;
-const Scaling                 SCALING;
-const Reflection              REFLECTION;
-const Identity_transformation IDENTITY;
+const Translation             TRANSLATION = Translation();
+const Rotation                ROTATION = Rotation();
+const Scaling                 SCALING = Scaling();
+const Reflection              REFLECTION = Reflection();
+const Identity_transformation IDENTITY = Identity_transformation();
 
 const Origin      ORIGIN = Origin();
 const Null_vector NULL_VECTOR = Null_vector();
