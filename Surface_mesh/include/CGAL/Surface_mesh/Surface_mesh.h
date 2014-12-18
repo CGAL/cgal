5c135e0d304d2ef3515241d0b70a0251c3e770c5
Sébastien Loriot <sebastien.loriot@cgal.org>
2014-12-18 08:11:08 +0100

    fix bool for add_property

--- Surface_mesh/include/CGAL/Surface_mesh/Surface_mesh.h
+++ Surface_mesh/include/CGAL/Surface_mesh/Surface_mesh.h
@@ -285,12 +285,11 @@ public:
     std::pair<Property_map<Key, T>, bool>
     add(const std::string& name, const T t=T())
     {
-        // if a property with this name already exists, return an invalid property
         for (unsigned int i=0; i<parrays_.size(); ++i)
         {
             if (parrays_[i]->name() == name)
             {
-              return std::make_pair(Property_map<Key, T>(dynamic_cast<Property_array<T>*>(parrays_[i])), true);
+              return std::make_pair(Property_map<Key, T>(dynamic_cast<Property_array<T>*>(parrays_[i])), false);
             }
         }
 
@@ -298,7 +297,7 @@ public:
         Property_array<T>* p = new Property_array<T>(name, t);
         p->resize(size_);
         parrays_.push_back(p);
-        return std::make_pair(Property_map<Key, T>(p), false);
+        return std::make_pair(Property_map<Key, T>(p), true);
     }
 
 
