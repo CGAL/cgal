a4ea4f342f8c71cf4163be97847465a95ec52d4e
Sébastien Loriot <sebastien.loriot@cgal.org>
2014-12-19 03:07:42 +0100

    update comment

--- Mean_curvature_skeleton/include/CGAL/internal/Mean_curvature_skeleton/Curve_skeleton.h
+++ Mean_curvature_skeleton/include/CGAL/internal/Mean_curvature_skeleton/Curve_skeleton.h
@@ -160,7 +160,7 @@ public:
     for (int i = 0; i < id; ++i)
     {
       id_to_vd[i] = boost::add_vertex(curve);
-      /// \todo remove the setting of the id
+      /// \todo remove the setting of the id if possible
       curve[id_to_vd[i]].id = i;
     }
 
