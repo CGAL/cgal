7fdb146a11f5ffcf2b8f881c5de8274ba0c45fae
Andreas Fabri <andreas.fabri@geometryfactory.com>
2014-12-23 15:06:51 +0100

    use the overloads that call get(vertex_point_t)

--- Surface_mesh_segmentation/examples/Surface_mesh_segmentation/segmentation_from_sdf_values_Surface_mesh_example.cpp
+++ Surface_mesh_segmentation/examples/Surface_mesh_segmentation/segmentation_from_sdf_values_Surface_mesh_example.cpp
@@ -34,25 +34,18 @@ int main(int argc, char** argv )
 
   sdf_property_map = mesh.add_property_map<face_descriptor,double>("f:sdf").first;
 
-
-  // why does the following not work??
-  typedef boost::property_map<Mesh, boost::vertex_point_t>::type Ppmap;
-  Ppmap ppmap = mesh.points();
-
   // compute SDF values
   // We can't use default parameters for number of rays, and cone angle
   // and the postprocessing
-  CGAL::sdf_values(mesh, sdf_property_map, 2.0 / 3.0 * CGAL_PI, 25, true, ppmap);
+  CGAL::sdf_values(mesh, sdf_property_map, 2.0 / 3.0 * CGAL_PI, 25, true);
 
   // create a property-map for segment-ids
   typedef Mesh::Property_map<face_descriptor, std::size_t> Facet_int_map;
   Facet_int_map segment_property_map = mesh.add_property_map<face_descriptor,std::size_t>("f:sid").first;;
 
-  std::size_t number_of_clusters = 5;
-  double smoothing_lambda = 0.26;  // importance of surface features, suggested to be in-between [0,1]
   // segment the mesh using default parameters for number of levels, and smoothing lambda
   // Any other scalar values can be used instead of using SDF values computed using the CGAL function
-  std::size_t number_of_segments = CGAL::segmentation_from_sdf_values(mesh, sdf_property_map, segment_property_map, number_of_clusters, smoothing_lambda, false, ppmap );
+  std::size_t number_of_segments = CGAL::segmentation_from_sdf_values(mesh, sdf_property_map, segment_property_map);
 
   std::cout << "Number of segments: " << number_of_segments << std::endl;
   // print segment-ids
@@ -63,10 +56,10 @@ int main(int argc, char** argv )
   }
   std::cout << std::endl;
 
-  number_of_clusters = 4;       // use 4 clusters in soft clustering
-  smoothing_lambda = 0.3;  // importance of surface features, suggested to be in-between [0,1]
+  const std::size_t number_of_clusters = 4;       // use 4 clusters in soft clustering
+  const double smoothing_lambda = 0.3;  // importance of surface features, suggested to be in-between [0,1]
 
   // Note that we can use the same SDF values (sdf_property_map) over and over again for segmentation.
   // This feature is relevant for segmenting the mesh several times with different parameters.
-  CGAL::segmentation_from_sdf_values(mesh, sdf_property_map, segment_property_map, number_of_clusters, smoothing_lambda, false, ppmap);
+  CGAL::segmentation_from_sdf_values(mesh, sdf_property_map, segment_property_map, number_of_clusters, smoothing_lambda);
 }
