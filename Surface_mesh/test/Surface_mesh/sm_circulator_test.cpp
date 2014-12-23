95af2ea3578015ff76d7ee66cccae09f5c945419
Andreas Fabri <andreas.fabri@geometryfactory.com>
2014-12-23 16:08:25 +0100

    don't use the boost testing framework

--- Surface_mesh/test/Surface_mesh/sm_circulator_test.cpp
+++ Surface_mesh/test/Surface_mesh/sm_circulator_test.cpp
@@ -1,82 +1,95 @@
-#define BOOST_TEST_MODULE surface_mesh circulator test
-#include <boost/test/included/unit_test.hpp>
-#include <boost/test/unit_test.hpp>
 
 #include <CGAL/circulator.h>
 
 #include "SM_common.h"
 
 // trivial circulator testing
-BOOST_FIXTURE_TEST_CASE(circ_size, Surface_fixture)
+struct test_circ_size : public Surface_fixture
 {
-  Sm::Vertex_around_target_circulator vvc(m.halfedge(v),m);
-  Sm::Halfedge_around_target_circulator hvc(m.halfedge(v),m);
-  Sm::Face_around_target_circulator fvc(m.halfedge(v),m);
-  Sm::Vertex_around_face_circulator vaf(m.halfedge(f1),m);
-  Sm::Halfedge_around_face_circulator hfc(m.halfedge(f1),m);
-  
-  BOOST_CHECK_EQUAL(CGAL::circulator_size(vvc), 4);
-  BOOST_CHECK_EQUAL(CGAL::circulator_size(hvc), 4);
-  BOOST_CHECK_EQUAL(CGAL::circulator_size(fvc), 4);
-  BOOST_CHECK_EQUAL(CGAL::circulator_size(vaf), 3);
-  BOOST_CHECK_EQUAL(CGAL::circulator_size(hfc), 3);
-  
-}
+  void operator()()
+  {
+    Sm::Vertex_around_target_circulator vvc(m.halfedge(v),m);
+    Sm::Halfedge_around_target_circulator hvc(m.halfedge(v),m);
+    Sm::Face_around_target_circulator fvc(m.halfedge(v),m);
+    Sm::Vertex_around_face_circulator vaf(m.halfedge(f1),m);
+    Sm::Halfedge_around_face_circulator hfc(m.halfedge(f1),m);
+    
+    assert(CGAL::circulator_size(vvc) == 4);
+    assert(CGAL::circulator_size(hvc) == 4);
+    assert(CGAL::circulator_size(fvc) == 4);
+    assert(CGAL::circulator_size(vaf) == 3);
+    assert(CGAL::circulator_size(hfc) == 3);
+ }
+};
 
-BOOST_FIXTURE_TEST_CASE(circ_distance, Surface_fixture)
+
+
+struct test_circ_distance : public Surface_fixture
 {
-  // the distance against end
-  Sm::Vertex_around_target_circulator vvc(m.halfedge(v),m), vvce(vvc); 
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(vvc, vvce), 4);
+  void operator()()
+  {
+    // the distance against end
+    Sm::Vertex_around_target_circulator vvc(m.halfedge(v),m), vvce(vvc); 
+    assert(CGAL::circulator_distance(vvc, vvce) == 4);
 
-  vvc =  vvce = Sm::Vertex_around_target_circulator(m.halfedge(u),m);
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(vvc, vvce), 2);
+    vvc =  vvce = Sm::Vertex_around_target_circulator(m.halfedge(u),m);
+    assert(CGAL::circulator_distance(vvc, vvce) == 2);
 
-  vvc = vvce = Sm::Vertex_around_target_circulator(m.halfedge(w),m);
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(vvc, vvce), 3);
+    vvc = vvce = Sm::Vertex_around_target_circulator(m.halfedge(w),m);
+    assert(CGAL::circulator_distance(vvc, vvce) == 3);
 
-  vvc = vvce = Sm::Vertex_around_target_circulator(m.halfedge(x),m);
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(vvc, vvce), 3);
+    vvc = vvce = Sm::Vertex_around_target_circulator(m.halfedge(x),m);
+    assert(CGAL::circulator_distance(vvc, vvce) == 3);
 
-  Sm::Halfedge_around_target_circulator hvc(m.halfedge(v),m), hvce(hvc); 
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(hvc, hvce), 4);
+    Sm::Halfedge_around_target_circulator hvc(m.halfedge(v),m), hvce(hvc); 
+    assert(CGAL::circulator_distance(hvc, hvce) == 4);
   
-  Sm::Face_around_target_circulator fvc(m.halfedge(v),m), fvce(fvc);
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(fvc, fvce), 4);
+    Sm::Face_around_target_circulator fvc(m.halfedge(v),m), fvce(fvc);
+    assert(CGAL::circulator_distance(fvc, fvce) == 4);
 
-  Sm::Vertex_around_face_circulator vfc(m.halfedge(f1),m), vfce(vfc);
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(vfc, vfce), 3);
+    Sm::Vertex_around_face_circulator vfc(m.halfedge(f1),m), vfce(vfc);
+    assert(CGAL::circulator_distance(vfc, vfce) == 3);
 
-  Sm::Halfedge_around_face_circulator hfc(m.halfedge(f1),m), hfce(hfc); 
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(hfc, hfce), 3);
-}
-
-BOOST_FIXTURE_TEST_CASE(emptiness, Surface_fixture)
-{
-  // add an isolated vertex
-  Sm::Vertex_index iv = m.add_vertex(Point_3(2,2,0));
-  BOOST_CHECK(m.is_isolated(iv));
-
-  Sm::Vertex_around_target_range vr = m.vertices_around_target(m.halfedge(iv));
-  BOOST_CHECK(is_empty_range(boost::begin(vr), boost::end(vr)));
-
-  Sm::Face_around_target_range fr = m.faces_around_target(m.halfedge(iv));
-  BOOST_CHECK(is_empty_range(boost::begin(fr), boost::end(fr)));
-
-  Sm::Halfedge_around_target_range hr = m.halfedges_around_target(m.halfedge(iv));
-  BOOST_CHECK(is_empty_range(boost::begin(hr), boost::end(hr)));
-  // not true for everything else
-  m.remove_vertex(iv);
-  BOOST_CHECK(m.is_removed(iv));
-  Sm::Vertex_iterator vb, ve;
-  for(boost::tie(vb, ve) = m.vertices(); vb != ve; ++vb) {
-    Sm::Vertex_around_target_range vr = m.vertices_around_target(m.halfedge(*vb));
-    BOOST_CHECK(!is_empty_range(boost::begin(vr), boost::end(vr)));
+    Sm::Halfedge_around_face_circulator hfc(m.halfedge(f1),m), hfce(hfc); 
+    assert(CGAL::circulator_distance(hfc, hfce) == 3);
   }
-}
+};
 
-// trick cgal_create_CMakeLists
-// int main()
-// {
-//   return 0;
-// }
+struct test_emptiness : public Surface_fixture
+{
+  void operator()()
+  {
+    // add an isolated vertex
+    Sm::Vertex_index iv = m.add_vertex(Point_3(2,2,0));
+    assert(m.is_isolated(iv));
+
+    Sm::Vertex_around_target_range vr = m.vertices_around_target(m.halfedge(iv));
+    assert(is_empty_range(boost::begin(vr), boost::end(vr)));
+
+    Sm::Face_around_target_range fr = m.faces_around_target(m.halfedge(iv));
+    assert(is_empty_range(boost::begin(fr), boost::end(fr)));
+
+    Sm::Halfedge_around_target_range hr = m.halfedges_around_target(m.halfedge(iv));
+    assert(is_empty_range(boost::begin(hr), boost::end(hr)));
+    // not true for everything else
+    m.remove_vertex(iv);
+    assert(m.is_removed(iv));
+    Sm::Vertex_iterator vb, ve;
+    for(boost::tie(vb, ve) = m.vertices(); vb != ve; ++vb) {
+      Sm::Vertex_around_target_range vr = m.vertices_around_target(m.halfedge(*vb));
+      assert(!is_empty_range(boost::begin(vr), boost::end(vr)));
+    }
+  }
+};
+
+int main()
+{
+  test_circ_size tcs;
+  tcs();
+  test_circ_distance tcd;
+  tcd();
+  test_emptiness te;
+  te();
+  std::cerr << "done"<< std::endl;
+  return 0;
+}
--- Surface_mesh/test/Surface_mesh/surface_mesh_test.cpp
+++ Surface_mesh/test/Surface_mesh/surface_mesh_test.cpp
@@ -1,6 +1,3 @@
-#define BOOST_TEST_MODULE surface_mesh test
-#include <boost/test/included/unit_test.hpp>
-#include <boost/test/unit_test.hpp>
 
 #include "SM_common.h"
 
@@ -12,7 +9,7 @@
 
 #include <CGAL/use.h>
 
-BOOST_AUTO_TEST_CASE( constructors_test )
+void constructors_test()
 {
   Sm mesh;
   Sm mesh2(mesh);
@@ -24,12 +21,12 @@ template<typename Iterator>
 void test_iterator(Iterator begin, Iterator end, 
                    typename std::iterator_traits<Iterator>::difference_type distance) 
 {
-  BOOST_CHECK(begin != end);
-  BOOST_CHECK(begin == begin);
-  BOOST_CHECK(std::distance(begin, end) == distance);
+  assert(begin != end);
+  assert(begin == begin);
+  assert(std::distance(begin, end) == distance);
 }
 
-BOOST_AUTO_TEST_CASE( standard_iterators ) 
+void standard_iterators() 
 {
   Surface_fixture f;
 
@@ -50,61 +47,51 @@ BOOST_AUTO_TEST_CASE( standard_iterators )
   test_iterator(fb, fe, 3);
 }
 
-
-BOOST_AUTO_TEST_CASE( const_iterators ) {
-  Surface_fixture f;
-}
-
-BOOST_AUTO_TEST_CASE( iter_inteop ) { 
-  /* unimplemented */
-}
-
-BOOST_AUTO_TEST_CASE( test_descriptors ) {
+void test_descriptors() 
+{
   Sm::Vertex_index v;
   Sm::Halfedge_index h;
   v < v;
-  h == h;
-  // does not compile and so it should
-  // v < h;
+  h == h; 
 }
 
-BOOST_AUTO_TEST_CASE( test_remove_edge ) 
+void test_remove_edge () 
 {
   Surface_fixture f;
   Sm::Halfedge_index wv, uw, wx, xv, vu;
   wv = f.m.halfedge(f.w, f.v);
-  BOOST_CHECK_EQUAL(f.m.target(wv), f.v);
-  BOOST_CHECK(wv.is_valid());
+  assert(f.m.target(wv) == f.v);
+  assert(wv.is_valid());
   uw = f.m.halfedge(f.u, f.w);
-  BOOST_CHECK(uw.is_valid());
+  assert(uw.is_valid());
   wx = f.m.halfedge(f.w, f.x);
-  BOOST_CHECK(wx.is_valid());
+  assert(wx.is_valid());
   xv = f.m.halfedge(f.x, f.v);
-  BOOST_CHECK(xv.is_valid());
+  assert(xv.is_valid());
   vu = f.m.halfedge(f.v, f.u);
-  BOOST_CHECK(vu.is_valid());
-  BOOST_CHECK_EQUAL(f.m.degree(f.v), 4);
-  BOOST_CHECK_EQUAL(f.m.degree(f.w), 3);
+  assert(vu.is_valid());
+  assert(f.m.degree(f.v)== 4);
+  assert(f.m.degree(f.w)== 3);
 
   f.m.set_next(uw, wx);
   f.m.set_next(xv, vu);
   f.m.remove_edge(Sm::Edge_index(wv));
   Sm::Halfedge_around_target_circulator a(f.m.halfedge(f.w),f.m), b(a);
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(a, b), 2);
-  BOOST_CHECK_EQUAL(f.m.degree(f.w), 2);
+  assert(CGAL::circulator_distance(a, b)== 2);
+  assert(f.m.degree(f.w)== 2);
   a = b = Sm::Halfedge_around_target_circulator(f.m.halfedge(f.v),f.m);
-  BOOST_CHECK_EQUAL(CGAL::circulator_distance(a, b), 3);
-  BOOST_CHECK_EQUAL(f.m.degree(f.v), 3);
+  assert(CGAL::circulator_distance(a, b)== 3);
+  assert(f.m.degree(f.v)== 3);
 
   // now remove a border edge to check if this works
   // this should not lower the number of faces
   Sm::size_type old_removed_faces_size = f.m.number_of_removed_faces();
   f.m.remove_edge(Sm::Edge_index(wx));
-  BOOST_CHECK_EQUAL(f.m.number_of_removed_faces(), old_removed_faces_size);
+  assert(f.m.number_of_removed_faces()== old_removed_faces_size);
 }
 
 
-BOOST_AUTO_TEST_CASE( memory_reuse_test )
+void memory_reuse_test()
 {
   Cube_fixture f;
   // buffer all faces
@@ -120,7 +107,7 @@ BOOST_AUTO_TEST_CASE( memory_reuse_test )
       do {
         faces.back().push_back(*vafb);
       } while(++vafb != vafe);
-    BOOST_CHECK_EQUAL(f.m.degree(*fb), faces.back().size());
+    assert(f.m.degree(*fb)== faces.back().size());
   }
 
   Sm::Vertex_iterator vb, ve;
@@ -132,21 +119,21 @@ BOOST_AUTO_TEST_CASE( memory_reuse_test )
   std::size_t old_face_size = f.m.number_of_faces(); 
   std::size_t old_removed_face_size = f.m.number_of_removed_faces();
   boost::range::for_each(f.m.faces(), boost::bind(&Sm::remove_face, boost::ref(f.m), _1));
-  BOOST_CHECK_EQUAL(f.m.number_of_faces(), 0);
-  BOOST_CHECK_EQUAL(f.m.number_of_removed_faces(), old_face_size + old_removed_face_size);
+  assert(f.m.number_of_faces()== 0);
+  assert(f.m.number_of_removed_faces()== old_face_size + old_removed_face_size);
   // remove all edges
   std::size_t old_edge_size = f.m.number_of_edges();
   std::size_t old_removed_edge_size = f.m.number_of_removed_edges();
   boost::range::for_each(f.m.edges(), 
                          boost::bind(static_cast<void (Sm::*)(Sm::Edge_index)>(&Sm::remove_edge), 
                                      boost::ref(f.m), _1));
-  BOOST_CHECK_EQUAL(f.m.number_of_faces() , 0);
-  BOOST_CHECK_EQUAL(f.m.number_of_removed_edges(), old_edge_size + old_removed_edge_size);
+  assert(f.m.number_of_faces() == 0);
+  assert(f.m.number_of_removed_edges()== old_edge_size + old_removed_edge_size);
 
   // add all again
   for(Faces::iterator it = faces.begin(); it != faces.end(); ++it) {
     Sm::Face_index fd = f.m.add_face(*it);
-    BOOST_CHECK(fd.is_valid());
+    assert(fd.is_valid());
     f.m.set_vertex_halfedge_to_border_halfedge(f.m.halfedge(fd));
     for(VecFace::iterator it2 = it->begin(); it2 != it->end(); ++it2) { 
 
@@ -156,8 +143,8 @@ BOOST_AUTO_TEST_CASE( memory_reuse_test )
     }
   }
   
-  BOOST_CHECK_EQUAL(f.m.number_of_edges() , old_edge_size);
-  BOOST_CHECK_EQUAL(f.m.number_of_faces() , old_face_size);
+  assert(f.m.number_of_edges() == old_edge_size);
+  assert(f.m.number_of_faces() == old_face_size);
 
 
   // remove all vertices
@@ -165,8 +152,8 @@ BOOST_AUTO_TEST_CASE( memory_reuse_test )
   std::size_t old_removed_size = f.m.number_of_removed_vertices();
 
   boost::range::for_each(f.m.vertices(), boost::bind(&Sm::remove_vertex, boost::ref(f.m), _1));
-  BOOST_CHECK_EQUAL(f.m.number_of_vertices() , 0);
-  BOOST_CHECK_EQUAL(f.m.number_of_removed_vertices(), old_size + old_removed_size);
+  assert(f.m.number_of_vertices() == 0);
+  assert(f.m.number_of_removed_vertices()== old_size + old_removed_size);
 
   for(unsigned int i = 0; i < old_size; ++i)
   {
@@ -174,51 +161,51 @@ BOOST_AUTO_TEST_CASE( memory_reuse_test )
   }
 
   // the size must remain the same
-  BOOST_CHECK_EQUAL(f.m.number_of_vertices(), old_size);
+  assert(f.m.number_of_vertices()== old_size);
 }
 
-BOOST_AUTO_TEST_CASE( test_validate )
+void test_validate()
 {
   Cube_fixture cf;
   Surface_fixture f1;
   Surface_fixture_2 f2;
   Surface_fixture_3 f3;
-  BOOST_CHECK(cf.m.is_valid());
-  BOOST_CHECK(f1.m.is_valid());
-  BOOST_CHECK(f2.m.is_valid());  
-  BOOST_CHECK(f3.m.is_valid());
+  assert(cf.m.is_valid());
+  assert(f1.m.is_valid());
+  assert(f2.m.is_valid());  
+  assert(f3.m.is_valid());
 }
 
-BOOST_AUTO_TEST_CASE(isolated_vertex_check)
+void isolated_vertex_check()
 {
   Surface_fixture f;
   Sm::Vertex_index isolated = f.m.add_vertex(Point_3(10, 10, 10));
-  BOOST_CHECK(f.m.is_isolated(isolated));
-  BOOST_CHECK(!f.m.halfedge(isolated).is_valid());
-  BOOST_CHECK(! f.m.is_border(isolated));
-  BOOST_CHECK(f.m.degree(isolated) == 0);
+  assert(f.m.is_isolated(isolated));
+  assert(!f.m.halfedge(isolated).is_valid());
+  assert(! f.m.is_border(isolated));
+  assert(f.m.degree(isolated) == 0);
 }
 
-BOOST_AUTO_TEST_CASE(embedded_vertex_check)
+void embedded_vertex_check()
 {
   Surface_fixture_2 f;
-  BOOST_CHECK(!f.m.is_isolated(f.y));
-  BOOST_CHECK(!f.m.is_border(f.y));
-  BOOST_CHECK(f.m.halfedge(f.y).is_valid());
-  BOOST_CHECK(f.m.degree(f.y) == 4);
+  assert(!f.m.is_isolated(f.y));
+  assert(!f.m.is_border(f.y));
+  assert(f.m.halfedge(f.y).is_valid());
+  assert(f.m.degree(f.y) == 4);
 }
 
-BOOST_AUTO_TEST_CASE(border_vertex_check)
+void border_vertex_check()
 {
   Surface_fixture f;
-  BOOST_CHECK(!f.m.is_isolated(f.y));
-  BOOST_CHECK(f.m.is_border(f.y));
-  BOOST_CHECK(f.m.degree(f.y) == 2);
-  BOOST_CHECK(f.m.halfedge(f.y).is_valid());
+  assert(!f.m.is_isolated(f.y));
+  assert(f.m.is_border(f.y));
+  assert(f.m.degree(f.y) == 2);
+  assert(f.m.halfedge(f.y).is_valid());
 }
 
 
-BOOST_AUTO_TEST_CASE( point_position_accessor )
+void point_position_accessor ()
 {
   Surface_fixture f;
   // by property
@@ -227,26 +214,37 @@ BOOST_AUTO_TEST_CASE( point_position_accessor )
   f.m.point(f.x);
   // const as an lvalue
   f.m.point(f.x) = CGAL::ORIGIN;
-  BOOST_CHECK(f.m.point(f.x) == CGAL::ORIGIN);
+  assert(f.m.point(f.x) == CGAL::ORIGIN);
 }
 
-BOOST_AUTO_TEST_CASE( properties ) {
+void properties () {
   Surface_fixture f;
   
 
   Sm::Property_map<Sm::Vertex_index, int> prop;
-  bool exists = true;
+  bool created = false;
 
-  boost::tie(prop,exists) = f.m.add_property_map<Sm::Vertex_index, int>("illuminatiproperty", 23);
-  BOOST_CHECK(exists == false);
+  boost::tie(prop,created) = f.m.add_property_map<Sm::Vertex_index, int>("illuminatiproperty", 23);
+  assert(created == true);
 
-  boost::tie(prop,exists)= f.m.add_property_map<Sm::Vertex_index, int>("illuminatiproperty");
-  BOOST_CHECK(exists == true);
+  boost::tie(prop, created)= f.m.add_property_map<Sm::Vertex_index, int>("illuminatiproperty");
+  assert(created == false);
 }
 
 
-// trick cgal_create_CMakeLists
-// int main()
-// {
-//   return 0;
-// }
+int main()
+{
+  constructors_test();
+  standard_iterators();
+  test_descriptors();
+  test_remove_edge();
+  memory_reuse_test();
+  test_validate();
+  isolated_vertex_check();
+  embedded_vertex_check();
+  border_vertex_check();
+  point_position_accessor();
+  properties();
+  std::cout << "done" << std::endl;
+  return 0;
+}
