28dc50fdfccea5e6a8255d40af04f7b887aee835
Andreas Fabri <andreas.fabri@geometryfactory.com>
2014-12-23 14:24:51 +0100

    point property map must be passed as argument

--- Surface_mesh_segmentation/examples/Surface_mesh_segmentation/segmentation_from_sdf_values_Surface_mesh_example.cpp
+++ Surface_mesh_segmentation/examples/Surface_mesh_segmentation/segmentation_from_sdf_values_Surface_mesh_example.cpp
@@ -48,9 +48,11 @@ int main(int argc, char** argv )
   typedef Mesh::Property_map<face_descriptor, std::size_t> Facet_int_map;
   Facet_int_map segment_property_map = mesh.add_property_map<face_descriptor,std::size_t>("f:sid").first;;
 
+  std::size_t number_of_clusters = 5;
+  double smoothing_lambda = 0.26;  // importance of surface features, suggested to be in-between [0,1]
   // segment the mesh using default parameters for number of levels, and smoothing lambda
   // Any other scalar values can be used instead of using SDF values computed using the CGAL function
-  std::size_t number_of_segments = CGAL::segmentation_from_sdf_values(mesh, sdf_property_map, segment_property_map);
+  std::size_t number_of_segments = CGAL::segmentation_from_sdf_values(mesh, sdf_property_map, segment_property_map, number_of_clusters, smoothing_lambda, false, ppmap );
 
   std::cout << "Number of segments: " << number_of_segments << std::endl;
   // print segment-ids
@@ -61,10 +63,10 @@ int main(int argc, char** argv )
   }
   std::cout << std::endl;
 
-  const std::size_t number_of_clusters = 4;       // use 4 clusters in soft clustering
-  const double smoothing_lambda = 0.3;  // importance of surface features, suggested to be in-between [0,1]
+  number_of_clusters = 4;       // use 4 clusters in soft clustering
+  smoothing_lambda = 0.3;  // importance of surface features, suggested to be in-between [0,1]
 
   // Note that we can use the same SDF values (sdf_property_map) over and over again for segmentation.
   // This feature is relevant for segmenting the mesh several times with different parameters.
-  CGAL::segmentation_from_sdf_values(mesh, sdf_property_map, segment_property_map, number_of_clusters, smoothing_lambda);
+  CGAL::segmentation_from_sdf_values(mesh, sdf_property_map, segment_property_map, number_of_clusters, smoothing_lambda, false, ppmap);
 }
