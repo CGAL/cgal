30d7453630bf0828b06f78853e6ea69e345de1d1
Jane Tournois <jane.tournois@geometryfactory.com>
2015-01-16 16:36:15 +0100

    rename Polyhedron to polygon_mesh and TriangleMesh in Point_inside_polyhedron_3

--- Polygon_mesh_processing/include/CGAL/Point_inside_polygon_mesh_3.h
+++ Polygon_mesh_processing/include/CGAL/Point_inside_polygon_mesh_3.h
@@ -19,11 +19,11 @@
 // Author(s)     : Sebastien Loriot and Ilker O. Yaz
 
 
-#ifndef CGAL_POINT_INSIDE_POLYHEDRON_H
-#define CGAL_POINT_INSIDE_POLYHEDRON_H
+#ifndef CGAL_POINT_INSIDE_POLYGON_MESH_H
+#define CGAL_POINT_INSIDE_POLYGON_MESH_H
 
-#include <CGAL/internal/Point_inside_polyhedron_3/Point_inside_vertical_ray_cast.h>
-#include <CGAL/internal/Point_inside_polyhedron_3/AABB_triangle_accessor_3_primitive.h>
+#include <CGAL/internal/Point_inside_polygon_mesh/Point_inside_vertical_ray_cast.h>
+#include <CGAL/internal/Point_inside_polygon_mesh/AABB_triangle_accessor_3_primitive.h>
 
 #include <CGAL/AABB_tree.h>
 #include <CGAL/AABB_traits.h>
@@ -38,23 +38,23 @@ namespace CGAL {
  * In case several polyhedral surface are provided as input, a point is said to be inside the domain
  * if an odd number of surfaces is crossed when walking from infinity to the point.
  * The implementation depends on the package \ref PkgAABB_treeSummary.
- * @tparam Polyhedron a triangulated polyhedral surface
+ * @tparam TriangleMesh a triangulated polyhedral surface
  * @tparam Kernel a \cgal kernel
  * @tparam TriangleAccessor a model of the concept `TriangleAccessor_3`, with `TriangleAccessor_3::Triangle_3` being `Kernel::Triangle_3`. 
- *         If `Polyhedron` is a \cgal Polyhedron, a default is provided.
+ *         If `TriangleMesh` is a \cgal Polyhedron, a default is provided.
  * \todo Code: Use this class as an implementation detail of Mesh_3's Polyhedral_mesh_domain_3
- * \todo Code: current version puts all polyhedra under one AABB, more proper approach might be using separate AABB for each polyhedron 
+ * \todo Code: current version puts all polyhedra under one AABB, more proper approach might be using separate AABB for each triangle mesh 
  *       and filtering query point with bboxes of polyhedra...
- * \todo `Polyhedron` should be a model of `FaceListGraph`
- * \todo Remove `TriangleAccessor_3` as well as the concept in Mesh_3 since making `Polyhedron` a model of `FaceListGraph` will make it useless
- * \todo Add a constructor from AABB-tree (once Polyhedron is a FaceListGraph, the type is hardcoded)
+ * \todo `TriangleMesh` should be a model of `FaceListGraph`
+ * \todo Remove `TriangleAccessor_3` as well as the concept in Mesh_3 since making `TriangleMesh` a model of `FaceListGraph` will make it useless
+ * \todo Add a constructor from AABB-tree (once TriangleMesh is a FaceListGraph, the type is hardcoded)
  * \todo check the implementation
  */
-template <class Polyhedron, 
+template <class TriangleMesh, 
           class Kernel,
-          class TriangleAccessor_3 = Triangle_accessor_3<Polyhedron, typename Polyhedron::Traits> 
+          class TriangleAccessor_3 = Triangle_accessor_3<TriangleMesh, typename TriangleMesh::Traits> 
 >
-class Point_inside_polyhedron_3{
+class Point_inside_polygon_mesh{
   // typedefs
   typedef CGAL::internal::AABB_triangle_accessor_3_primitive<Kernel, TriangleAccessor_3> Primitive;
   typedef CGAL::AABB_traits<Kernel, Primitive> Traits;
@@ -70,27 +70,27 @@ public:
   /**
    * Default constructor. The domain is considered to be empty.
    */
-  Point_inside_polyhedron_3(const Kernel& kernel=Kernel())
+  Point_inside_polygon_mesh(const Kernel& kernel=Kernel())
   : ray_functor(kernel.construct_ray_3_object()),
   vector_functor(kernel.construct_vector_3_object())
   { }
  
   /** 
-   * Constructor with one polyhedral surface. `polyhedron` must be closed and triangulated.
+   * Constructor with one polyhedral surface. `mesh` must be closed and triangulated.
    */
-  Point_inside_polyhedron_3(const Polyhedron& polyhedron, const Kernel& kernel=Kernel()) 
+  Point_inside_polygon_mesh(const TriangleMesh& mesh, const Kernel& kernel=Kernel()) 
   : ray_functor(kernel.construct_ray_3_object()),
   vector_functor(kernel.construct_vector_3_object())
   {
-    add_polyhedron(polyhedron);
+    add_triangle_mesh(mesh);
   }
 
   /** 
    * Constructor with several polyhedral surfaces. All the polyhedral surfaces must be closed, triangulated and disjoint.
-   * \tparam InputIterator is an input iterator with `Polyhedron` or `cpp11::reference_wrapper<Polyhedron>` as value type.
+   * \tparam InputIterator is an input iterator with `TriangleMesh` or `cpp11::reference_wrapper<TriangleMesh>` as value type.
    */
   template <class InputIterator>
-  Point_inside_polyhedron_3(InputIterator begin, InputIterator beyond, const Kernel& kernel=Kernel()) 
+  Point_inside_polygon_mesh(InputIterator begin, InputIterator beyond, const Kernel& kernel=Kernel()) 
   : ray_functor(kernel.construct_ray_3_object()),
   vector_functor(kernel.construct_vector_3_object())
   {
@@ -103,35 +103,35 @@ public:
   void build() { tree.build(); }
 
   /**
-   * `polyhedron` is added as input
+   * `mesh` is added as input
    */
-  void add_polyhedron(const Polyhedron& polyhedron) 
+  void add_triangle_mesh(const TriangleMesh& mesh) 
   {
-    CGAL_assertion(polyhedron.is_pure_triangle());
-    CGAL_assertion(polyhedron.is_closed());
+    CGAL_assertion(mesh.is_pure_triangle());
+    CGAL_assertion(mesh.is_closed());
 
-    tree.insert(TriangleAccessor_3().triangles_begin(polyhedron),
-                TriangleAccessor_3().triangles_end(polyhedron));
+    tree.insert(TriangleAccessor_3().triangles_begin(mesh),
+                TriangleAccessor_3().triangles_end(mesh));
   }
  
   /**
    * The polyhedral surfaces in the range `[begin,beyond[` are added as input
-   * \tparam InputIterator is an input iterator with `Polyhedron` or `cpp11::reference_wrapper<Polyhedron>` as value type.
+   * \tparam InputIterator is an input iterator with `TriangleMesh` or `cpp11::reference_wrapper<TriangleMesh>` as value type.
    */
   template<class InputIterator>
   void add_polyhedra(InputIterator begin, InputIterator beyond) 
   {
     for(; begin != beyond; ++begin) {
-      add_polyhedron(*begin);
+      add_triangle_mesh(*begin);
     }
   }
  
   /**
    * Query function to determine point location.
    * @return 
-   *   - CGAL::ON_BOUNDED_SIDE if the point is inside the polyhedron
-   *   - CGAL::ON_BOUNDARY if the point is on polyhedron
-   *   - CGAL::ON_UNBOUNDED_SIDE if the point is outside polyhedron
+   *   - CGAL::ON_BOUNDED_SIDE if the point is inside the mesh
+   *   - CGAL::ON_BOUNDARY if the point is on mesh
+   *   - CGAL::ON_UNBOUNDED_SIDE if the point is outside mesh
    */
   Bounded_side operator()(const Point& point) const
   {
@@ -141,4 +141,4 @@ public:
 
 } // namespace CGAL
 
-#endif //CGAL_POINT_INSIDE_POLYHEDRON_H
+#endif //CGAL_POINT_INSIDE_POLYGON_MESH_H
