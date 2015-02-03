2d753bdbd7a24a0322443af416cc4dde06adb800
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2015-02-03 21:36:09 +0100

    SDG Linf doc remove \sa from hierarchy

--- Segment_Delaunay_graph_Linf_2/doc/Segment_Delaunay_graph_Linf_2/CGAL/Segment_Delaunay_graph_Linf_hierarchy_2.h
+++ Segment_Delaunay_graph_Linf_2/doc/Segment_Delaunay_graph_Linf_2/CGAL/Segment_Delaunay_graph_Linf_hierarchy_2.h
@@ -16,20 +16,12 @@ The `Segment_Delaunay_graph_Linf_hierarchy_2` class derives publicly from
 the `Segment_Delaunay_graph_Linf_2<Gt,DS>` class.
 The interface is the same as its base class.
 
-\cgalModels `DefaultConstructible`
-\cgalModels `CopyConstructible`
-\cgalModels `Assignable`
-
-\sa `SegmentDelaunayGraphDataStructure_2`
 \sa `SegmentDelaunayGraphLinfTraits_2`
-\sa `SegmentDelaunayGraphHierarchyVertexBase_2`
 \sa `CGAL::Segment_Delaunay_graph_Linf_2<Gt,DS>`
-\sa `CGAL::Triangulation_data_structure_2<Vb,Fb>`
 \sa `CGAL::Segment_Delaunay_graph_Linf_traits_2<K,MTag>`
 \sa `CGAL::Segment_Delaunay_graph_Linf_traits_without_intersections_2<K,MTag>`
 \sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_2<CK,CM,EK,EM,FK,FM>`
 \sa `CGAL::Segment_Delaunay_graph_Linf_filtered_traits_without_intersections_2<CK,CM,EK,EM,FK,FM>`
-\sa `CGAL::Segment_Delaunay_graph_hierarchy_vertex_base_2<Vbb>`
 
 */
 template< typename Gt, typename STag, typename DS >
