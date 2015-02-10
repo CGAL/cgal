6f27c5a75bb0615f0d3ce090a5ec2bd1d2b78327
Jane Tournois <jane.tournois@geometryfactory.com>
2015-02-10 17:49:22 +0100

    BGL-ization triangulate_faces(PolygonMesh) function

--- Polygon_mesh_processing/include/CGAL/triangulate_faces.h
+++ Polygon_mesh_processing/include/CGAL/triangulate_faces.h
@@ -22,6 +22,8 @@
 
 #include <CGAL/Modifier_base.h>
 #include <CGAL/HalfedgeDS_decorator.h>
+#include <CGAL/boost/graph/helpers.h>
+#include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
 
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
 #include <CGAL/Triangulation_face_base_with_info_2.h>
@@ -30,6 +32,8 @@
 #include <CGAL/Triangulation_2_filtered_projection_traits_3.h>
 
 #include <CGAL/Polygon_mesh_processing/compute_normal.h>
+
+#include <boost/foreach.hpp>
 
 #include <queue>
 #include <vector>
@@ -41,25 +45,25 @@ namespace Polygon_mesh_processing {
 
 namespace internal {
 
-template <class Polyhedron>
+template <class PM, typename Kernel>
 class Triangulate_modifier
-  : public CGAL::Modifier_base<typename Polyhedron::HalfedgeDS> 
+  : public CGAL::Modifier_base<PM>
 {
-  typedef typename Polyhedron::HalfedgeDS HDS;
-  typedef typename Polyhedron::Traits Traits;
+  typedef typename Kernel Traits;
 
-  typedef typename Polyhedron::Halfedge_handle Halfedge_handle;
-  typedef typename Polyhedron::Facet Facet;
-  typedef typename Polyhedron::Facet_iterator Facet_iterator;
-  typedef typename Polyhedron::Facet_handle Facet_handle;
+  typedef typename boost::graph_traits<PM>::vertex_descriptor vertex_descriptor;
+  typedef typename boost::graph_traits<PM>::halfedge_descriptor halfedge_descriptor;
+  typedef typename boost::graph_traits<PM>::face_descriptor face_descriptor;
+  typedef typename boost::graph_traits<PM>::edge_descriptor edge_descriptor;
+  typedef typename PM::Point Point;
 
   typedef CGAL::Triangulation_2_filtered_projection_traits_3<Traits>   P_traits;
 
-  typedef CGAL::Triangulation_vertex_base_with_info_2<Halfedge_handle,
+  typedef CGAL::Triangulation_vertex_base_with_info_2<halfedge_descriptor,
                                                       P_traits>        Vb;
 
   struct Face_info {
-    typename Polyhedron::Halfedge_handle e[3];
+    typename boost::graph_traits<PM>::halfedge_descriptor e[3];
     bool is_external;
   };
 
@@ -83,63 +87,61 @@ public:
     return fh->info().is_external;
   }
 
-  void operator()(HDS& hds) {
-    CGAL::HalfedgeDS_decorator<HDS> decorator(hds);
-    typedef typename HDS::Halfedge Halfedge;
+  void operator()(PM& pmesh)
+  {
+    typename boost::property_map<PM, boost::vertex_point_t>::type
+      ppmap = get(boost::vertex_point, pmesh);
 
     // One need to store facet handles into a vector, because the list of
     // facets of the polyhedron will be modified during the loop, and
     // that invalidates the range [facets_begin(), facets_end()[.
-    std::vector<Facet_handle> facets;
-    facets.reserve(hds.size_of_faces());
-    for(Facet_iterator 
-          fit = hds.faces_begin(),
-          end = hds.faces_end();
-        fit != end; ++fit) {
+    std::vector<face_descriptor> facets;
+    facets.reserve(num_faces(pmesh));
+    BOOST_FOREACH(face_descriptor fit, faces(pmesh))
       facets.push_back(fit);
-    }
 
     // Iterates on the vector of facet handles
-    for(typename std::vector<Facet_handle>::iterator 
-          fit_it = facets.begin(),
-          end = facets.end();
-        fit_it != end; ++fit_it)
+    for (unsigned int i = 0; i < facets.size(); ++i)
     {
-      Facet_handle fit = *fit_it;
-      typename Traits::Vector_3 normal = 
-        Polygon_mesh_processing::compute_facet_normal<Traits>(fit, Polyhedron());
+      face_descriptor f = facets[i];
+      typename Traits::Vector_3 normal =
+        Polygon_mesh_processing::compute_facet_normal(f, PM());
 
       P_traits cdt_traits(normal);
       CDT cdt(cdt_traits);
 
-      typename Facet::Halfedge_around_facet_circulator 
-        he_circ = fit->facet_begin(),
-        he_circ_end(he_circ);
-      typename CDT::Vertex_handle previous, first;
-      do {
-        typename CDT::Vertex_handle vh = cdt.insert(he_circ->vertex()->point());
+      // Halfedge_around_facet_circulator
+      typedef typename CDT::Vertex_handle Tr_Vertex_handle;
+      halfedge_descriptor start = halfedge(f, pmesh);
+      halfedge_descriptor h = start;
+      Tr_Vertex_handle previous, first;
+      do
+      {
+        Tr_Vertex_handle vh = cdt.insert(ppmap[target(h, pmesh)]);
         if(first == 0) {
           first = vh;
         }
-        vh->info() = he_circ;
-        if(previous != 0 && previous != vh) {
+        vh->info() = h;
+        if(previous != Tr_Vertex_handle() && previous != vh) {
           cdt.insert_constraint(previous, vh);
         }
         previous = vh;
-      } while( ++he_circ != he_circ_end );
+        h = next(h, pmesh);
+
+      } while( h != start );
       cdt.insert_constraint(previous, first);
 
       // sets mark is_external
-      for(typename CDT::All_faces_iterator
-            fit = cdt.all_faces_begin(),
-            end = cdt.all_faces_end();
-          fit != end; ++fit)
+      for(typename CDT::All_faces_iterator fit = cdt.all_faces_begin(),
+                                           end = cdt.all_faces_end();
+                                           fit != end; ++fit)
       {
         fit->info().is_external = false;
       }
       std::queue<typename CDT::Face_handle> face_queue;
       face_queue.push(cdt.infinite_vertex()->face());
-      while(! face_queue.empty() ) {
+      while(! face_queue.empty() )
+      {
         typename CDT::Face_handle fh = face_queue.front();
         face_queue.pop();
         if(fh->info().is_external) continue;
@@ -153,30 +155,32 @@ public:
       }
 
       // then modify the polyhedron
-      decorator.make_hole(fit->halfedge());
-      for(typename CDT::Finite_edges_iterator
-            eit = cdt.finite_edges_begin(),
-            end = cdt.finite_edges_end();
-          eit != end; ++eit)
+      // make_hole
+      remove_face(face(halfedge(f, pmesh), pmesh), pmesh);
+      set_face_in_face_loop(halfedge(f, pmesh), face_descriptor(), pmesh);
+
+      for(typename CDT::Finite_edges_iterator eit = cdt.finite_edges_begin(),
+                                              end = cdt.finite_edges_end();
+                                              eit != end; ++eit)
       {
         typename CDT::Face_handle fh = eit->first;
         const int index = eit->second;
         typename CDT::Face_handle opposite_fh = fh->neighbor(eit->second);
         const int opposite_index = opposite_fh->index(fh);
-        const typename CDT::Vertex_handle va = fh->vertex(cdt. cw(index));
-        const typename CDT::Vertex_handle vb = fh->vertex(cdt.ccw(index));
+        const Tr_Vertex_handle va = fh->vertex(cdt. cw(index));
+        const Tr_Vertex_handle vb = fh->vertex(cdt.ccw(index));
 
-        if( ! (is_external(fh) && is_external(opposite_fh)) && 
-            ! cdt.is_constrained(*eit) ) 
+        if( ! (is_external(fh) && is_external(opposite_fh))
+          && ! cdt.is_constrained(*eit) ) 
         {
           // strictly internal edge
-          Halfedge_handle h = hds.edges_push_back(Halfedge(),
-                                                  Halfedge());
+          edge_descriptor e = add_edge(pmesh);
+          //ici
           fh->info().e[index] = h;
           opposite_fh->info().e[opposite_index] = h->opposite();
 
-          decorator.set_vertex(h, va->info()->vertex());
-          decorator.set_vertex(h->opposite(), vb->info()->vertex());
+          set_target(h, target(va->info(), pmesh), pmesh);
+          set_target(opposite(h, pmesh), target(vb->info(), pmesh), pmesh);
         }
         if( cdt.is_constrained(*eit) )
         {
@@ -188,47 +192,60 @@ public:
           }
         }
       }
-      for(typename CDT::Finite_faces_iterator
-            fit = cdt.finite_faces_begin(),
-            end = cdt.finite_faces_end();
-          fit != end; ++fit)
+      for(typename CDT::Finite_faces_iterator fit = cdt.finite_faces_begin(),
+                                              end = cdt.finite_faces_end();
+                                              fit != end; ++fit)
       {
         if(!is_external(fit)) 
         {
-          Halfedge_handle h0 = fit->info().e[0];
-          Halfedge_handle h1 = fit->info().e[1];
-          Halfedge_handle h2 = fit->info().e[2];
-          CGAL_assertion( h0 != Halfedge_handle() );
-          CGAL_assertion( h1 != Halfedge_handle() );
-          CGAL_assertion( h2 != Halfedge_handle() );
+          halfedge_descriptor h0 = fit->info().e[0];
+          halfedge_descriptor h1 = fit->info().e[1];
+          halfedge_descriptor h2 = fit->info().e[2];
+          CGAL_assertion(h0 != halfedge_descriptor());
+          CGAL_assertion(h1 != halfedge_descriptor());
+          CGAL_assertion(h2 != halfedge_descriptor());
 
-          typedef typename Halfedge::Base HBase;
-          h0->HBase::set_next(h1);
-          decorator.set_prev(h1, h0);
-          h1->HBase::set_next(h2);
-          decorator.set_prev(h2, h1);
-          h2->HBase::set_next(h0);
-          decorator.set_prev(h0, h2);
+          set_next(h0, h1, pmesh);
+          set_next(h1, h2, pmesh);
+          set_next(h2, h0, pmesh);
 
-          decorator.fill_hole(h0);        
+          // fill_hole(h0)
+          face_descriptor fd = add_face(pmesh);
+          set_face_in_face_loop(h0, fd, pmesh);
+          set_halfedge(fd, h0, pmesh);
         }
       }
     } // end loop on facets of the input polyhedron
   }
+
+  void set_face_in_face_loop(halfedge_descriptor end,
+                             face_descriptor f,
+                             PM& pmesh)
+  {
+    halfedge_descriptor h = end;
+    do {
+      set_face(h, f, pmesh);
+      h = next(h, pmesh);
+    } while (h != end);
+  }
+
 }; // end class Triangulate_modifier
 
 }//end namespace internal
 
 
 /// \ingroup PkgPolygonMeshProcessing
-/// Triangulates faces of the polygon mesh `p`. Depends on \ref PkgTriangulation2Summary
+/// Triangulates faces of the polygon mesh `pmesh`. Depends on \ref PkgTriangulation2Summary
 /// @tparam PolygonMesh a model of `FaceListGraph` and `MutableFaceGraph`
-/// \todo `PolygonMesh` should be a model of `FaceListGraph` and `MutableFaceGraph`
-template <typename PolygonMesh>
-void triangulate_faces(PolygonMesh& pmesh)
+/// @todo : add point property map as a parameter with a default
+/// @todo : add a default value for Kernel
+template <typename Kernel,
+          typename PolygonMesh>
+void triangulate_faces(PolygonMesh& pmesh,
+  const Kernel& k = Kernel())
 {
-  internal::Triangulate_modifier<PolygonMesh> modifier;
-  pmesh.delegate(modifier);
+  internal::Triangulate_modifier<PolygonMesh, Kernel> modifier;
+  modifier(pmesh);
 }
 
 } // end namespace Polygon_mesh_processing
--- Polyhedron/demo/Polyhedron/Polyhedron_demo_triangulate_facets_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Polyhedron_demo_triangulate_facets_plugin.cpp
@@ -82,9 +82,9 @@ public slots:
               !eit_copy->opposite()->vertex()->is_bivalent())
           {
             Kernel::Vector_3 v1 =
-              CGAL::Polygon_mesh_processing::compute_facet_normal<Kernel>(fh1, *pMesh);
+              CGAL::Polygon_mesh_processing::compute_facet_normal(fh1, *pMesh);
             Kernel::Vector_3 v2 =
-              CGAL::Polygon_mesh_processing::compute_facet_normal<Kernel>(fh2, *pMesh);
+              CGAL::Polygon_mesh_processing::compute_facet_normal(fh2, *pMesh);
             if(v1 * v2 > 0.99) {
               std::cerr << "join\n";
               // pMesh->is_valid(true);
@@ -119,7 +119,7 @@ public slots:
 
       QApplication::setOverrideCursor(Qt::WaitCursor);
 
-      CGAL::Polygon_mesh_processing::triangulate_faces(*pMesh);
+      CGAL::Polygon_mesh_processing::triangulate_faces<Kernel>(*pMesh);
 
       CGAL_assertion_code(pMesh->normalize_border());
       CGAL_assertion(pMesh->is_valid(false, 3));
