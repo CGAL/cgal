735ade3bc72d643eb18951b0ade0040cbd8f38f6
Weisheng Si <w.si@uws.edu.au>
2015-02-18 09:33:08 +1100

    Describing the concept ConeSpanners_2

--- Cone_spanners_2/doc/Cone_spanners_2/Concepts/ConeSpanners2.h
+++ Cone_spanners_2/doc/Cone_spanners_2/Concepts/ConeSpanners2.h
@@ -3,10 +3,10 @@
 \ingroup PkgConeBasedSpannersConcepts
 \cgalConcept
 
-The concept `ConeSpanners2` defines . 
+The concept `ConeSpanners_2` describes the set of requirements to be fulfilled by any class that constructs a cone based spanner, such as a Theta graph and a Yao graph, etc. 
 
-\cgalHasModel CGAL::Yao_graph_2`
-
+\cgalHasModel `CGAL::Theta_graph_2`
+\cgalHasModel `CGAL::Yao_graph_2`
 
 */
 
@@ -16,14 +16,49 @@ public:
 /// \name Types 
 /// @{
 
-
 /*!
-Point type. 
-*/ 
-typedef unspecified_type Point_3; 
-
+Any model of this concept must use `boost::adjacency_list` to store the constructed cone based spanner. Of the seven
+template parameters of `boost::adjacency_list`, the fourth parameter 'VertexProperties' must be Point_2 from \cgal, 
+and other parameters can be decided freely. This particular type is named 'Graph'.
+*/
+typedef adjacency_list<OutEdgeList, VertexList, Directed,
+               Point_2, EdgeProperties,
+               GraphProperties, EdgeList> Graph;
 
 /// @}
+
+
+/// \name Creation 
+/// @{
+
+/** @brief constructor for a ConeSpanners_2 object.
+*
+* @tparam PointInputIterator   Must be Point_2 of a certain type of \cgal Kernel.
+* @param k     Number of cones to divide space into
+* @param start An iterator pointing to the first point (vertex) in the graph.
+*              (default: nullptr)
+* @param end   An iterator pointing to the place that passes the last point.  (default: nullptr)
+* @param ray0  The direction of the first ray. This allows the first ray to be at an arbitary 
+*              direction.  (default: positive x-axis) 
+*/
+template <typename PointInputIterator>  
+ConeSpanners_2(const unsigned int k,
+			const PointInputIterator& start=nullptr, 
+			const PointInputIterator& end=nullptr,
+			const Direction_2& ray0 = Direction_2(1,0)	);
+
+/// @} 
+
+
+/// \name Member Access Functions 
+/// @{
+
+/** @brief returns the `boost::adjacency_list` object for the constructed cone based spanner.
+ */
+Graph graph();
+
+
+/// @} 
 
 }; /* end ConeSpanners_2 */
 
