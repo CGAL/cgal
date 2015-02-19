a3a3dee4cd5ce38552735ac0364b9dd807d8576f
Sébastien Loriot <sebastien.loriot@cgal.org>
2015-02-19 14:44:30 +0100

    add missing header files

--- Point_set_shape_detection_3/include/CGAL/Shape_detection_3.h
+++ Point_set_shape_detection_3/include/CGAL/Shape_detection_3.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2013-2015 INRIA Sophia-Antipolis (France).
+// Copyright (c) 2015 INRIA Sophia-Antipolis (France).
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org).
 // You can redistribute it and/or modify it under the terms of the GNU
-// General Public License as published by the Free Software Foundation, 
+// General Public License as published by the Free Software Foundation,
 // either version 3 of the License, or (at your option) any later version.
 //
 // Licensees holding a valid commercial license may use this file in
@@ -16,13 +16,8 @@
 // $Id$
 //
 //
-// Author(s)     : Sven Oesau, Yannick Verdié, Clément Jamin, Pierre Alliez
+// Author(s)     : Sven Oesau, Yannick Verdie, ClÃ©ment Jamin, Pierre Alliez
 //
-//******************************************************************************
-// File Description :
-//
-//
-//******************************************************************************
 
 #ifndef CGAL_SHAPE_DETECTION_3_H
 #define CGAL_SHAPE_DETECTION_3_H
