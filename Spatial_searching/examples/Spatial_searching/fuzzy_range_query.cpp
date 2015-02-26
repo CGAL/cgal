af84e62b6c2a1fa2dbd748fa938c461cc368d8e5
Andreas Fabri <andreas.fabri@geometryfactory.com>
2015-02-26 18:07:37 +0100

    Use the Epick_d kernel in two examples, and for 4d and not 2D

--- Spatial_searching/examples/Spatial_searching/fuzzy_range_query.cpp
+++ Spatial_searching/examples/Spatial_searching/fuzzy_range_query.cpp
@@ -1,13 +1,15 @@
-#include <CGAL/Cartesian_d.h>
+#include <CGAL/Epick_d.h>
 #include <CGAL/point_generators_d.h>
 #include <CGAL/Kd_tree.h>
 #include <CGAL/Fuzzy_sphere.h>
 #include <CGAL/Fuzzy_iso_box.h>
 #include <CGAL/Search_traits_d.h>
 
-typedef CGAL::Cartesian_d<double> K;
+const int D = 4;
+
+typedef CGAL::Epick_d<CGAL::Dimension_tag<D> > K;
 typedef K::Point_d Point_d;
-typedef CGAL::Search_traits_d<K> Traits;
+typedef CGAL::Search_traits_d<K,CGAL::Dimension_tag<D> >  Traits;
 typedef CGAL::Random_points_in_cube_d<Point_d>       Random_points_iterator;
 typedef CGAL::Counting_iterator<Random_points_iterator> N_Random_points_iterator;
 typedef CGAL::Kd_tree<Traits> Tree;
@@ -15,7 +17,7 @@ typedef CGAL::Fuzzy_sphere<Traits> Fuzzy_sphere;
 typedef CGAL::Fuzzy_iso_box<Traits> Fuzzy_iso_box;
 
 int main() {
-  const int D = 4;
+
   const int N = 1000;
   // generator for random data points in the square ( (-1000,-1000), (1000,1000) )
   Random_points_iterator rpit(4, 1000.0);
@@ -27,8 +29,8 @@ int main() {
   // define range query objects
   double  pcoord[D] = { 300, 300, 300, 300 };
   double  qcoord[D] = { 900.0, 900.0, 900.0, 900.0 };
-  Point_d p(D, pcoord, pcoord+D);
-  Point_d q(D, qcoord, qcoord+D);
+  Point_d p(D, pcoord+0, pcoord+D);
+  Point_d q(D, qcoord+0, qcoord+D);
   Fuzzy_sphere fs(p, 700.0, 100.0);
   Fuzzy_iso_box fib(p, q, 100.0);
 
