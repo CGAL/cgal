430962c5243e01caa823e767dc7deec5e1103637
Jane Tournois <jane.tournois@geometryfactory.com>
2015-03-05 17:11:49 +0100

    add header including all the functions of the namespace Polygon_mesh_processing

---
+++ Polygon_mesh_processing/include/CGAL/polygon_mesh_processing.h
@@ -0,0 +1,32 @@
+/**
+* \ingroup PkgPolygonMeshProcessing
+* \file CGAL/polygon_mesh_processing.h
+* Convenience header file only including the headers for all
+* functions of this package that are part of
+* the namespace `Polygon_mesh_processing` :
+* `CGAL/orient_polygon_soup.h`,
+* `CGAL/polygon_mesh_self_intersections.h`,
+* `CGAL/polygon_soup_to_polygon_mesh.h`,
+* `CGAL/stitch_polygon_mesh.h`,
+* `CGAL/triangulate_faces.h`,
+* `CGAL/triangulate_hole.h`,
+* `CGAL/Polygon_mesh_processing/compute_normal.h`,
+* `CGAL/Polygon_mesh_processing/Connected_components.h`,
+* `CGAL/Polygon_mesh_processing/fair.h`,
+* `CGAL/Polygon_mesh_processing/is_oriented.h`, and
+* `CGAL/Polygon_mesh_processing/refine.h`.
+
+*/
+
+#include <CGAL/orient_polygon_soup.h>
+#include <CGAL/polygon_mesh_self_intersections.h>
+#include <CGAL/polygon_soup_to_polygon_mesh.h>
+#include <CGAL/stitch_polygon_mesh.h>
+#include <CGAL/triangulate_faces.h>
+#include <CGAL/triangulate_hole.h>
+#include <CGAL/Polygon_mesh_processing/compute_normal.h>
+#include <CGAL/Polygon_mesh_processing/Connected_components.h>
+#include <CGAL/Polygon_mesh_processing/fair.h>
+#include <CGAL/Polygon_mesh_processing/is_oriented.h>
+#include <CGAL/Polygon_mesh_processing/refine.h>
+
