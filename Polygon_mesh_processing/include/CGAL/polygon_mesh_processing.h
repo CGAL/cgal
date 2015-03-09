6288df3e708d45d7bada29ed3fc1c71ff0e4d64f
Jane Tournois <jane.tournois@geometryfactory.com>
2015-03-09 12:31:54 +0100

    completes latest commit

--- Polygon_mesh_processing/include/CGAL/polygon_mesh_processing.h
+++ Polygon_mesh_processing/include/CGAL/polygon_mesh_processing.h
@@ -7,21 +7,20 @@
 * `CGAL/orient_polygon_soup.h`,
 * `CGAL/polygon_mesh_self_intersections.h`,
 * `CGAL/polygon_soup_to_polygon_mesh.h`,
-* `CGAL/stitch_polygon_mesh.h`,
 * `CGAL/triangulate_faces.h`,
 * `CGAL/triangulate_hole.h`,
 * `CGAL/Polygon_mesh_processing/compute_normal.h`,
 * `CGAL/Polygon_mesh_processing/Connected_components.h`,
 * `CGAL/Polygon_mesh_processing/fair.h`,
-* `CGAL/Polygon_mesh_processing/is_oriented.h`, and
-* `CGAL/Polygon_mesh_processing/refine.h`.
+* `CGAL/Polygon_mesh_processing/is_oriented.h`,
+* `CGAL/Polygon_mesh_processing/refine.h`, and
+* `CGAL/Polygon_mesh_processing/stitch_borders.h`.
 
 */
 
 #include <CGAL/orient_polygon_soup.h>
 #include <CGAL/polygon_mesh_self_intersections.h>
 #include <CGAL/polygon_soup_to_polygon_mesh.h>
-#include <CGAL/stitch_polygon_mesh.h>
 #include <CGAL/triangulate_faces.h>
 #include <CGAL/triangulate_hole.h>
 #include <CGAL/Polygon_mesh_processing/compute_normal.h>
@@ -29,4 +28,5 @@
 #include <CGAL/Polygon_mesh_processing/fair.h>
 #include <CGAL/Polygon_mesh_processing/is_oriented.h>
 #include <CGAL/Polygon_mesh_processing/refine.h>
+#include <CGAL/Polygon_mesh_processing/stitch_borders.h>
 
