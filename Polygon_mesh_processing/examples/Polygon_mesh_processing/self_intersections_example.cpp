d7b8390000cbd6d0d844a71f422b5dd3bb21e762
Jane Tournois <jane.tournois@geometryfactory.com>
2015-03-13 17:31:57 +0100

    add Point_inside_polygon_mesh example

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/self_intersections_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/self_intersections_example.cpp
@@ -1,10 +1,38 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/boost/graph/graph_traits_Surface_mesh.h>
+
 #include <CGAL/polygon_mesh_self_intersections.h>
 
-//CGAL::Polygon_mesh_processing::is_self_intersecting()
-//CGAL::Polygon_mesh_processing::self_intersections()
+#include <fstream>
 
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Surface_mesh<K::Point_3> Mesh;
+typedef boost::graph_traits<Mesh>::face_descriptor face_descriptor;
 
-int main()
+int main(int, char** argv)
 {
+  std::ifstream input(argv[1]);
+  Mesh mesh;
+
+  if (!input || !(input >> mesh))
+  {
+    std::cerr << "Not a valid off file." << std::endl;
+    return 1;
+  }
+
+  bool intersecting =
+    CGAL::Polygon_mesh_processing::is_self_intersecting<K>(mesh);
+
+  std::cout
+    << (intersecting ? "There is a self-intersection." : "There is no self-intersection.")
+    << std::endl;
+
+  std::vector<std::pair<face_descriptor, face_descriptor> > intersected_tris;
+  CGAL::Polygon_mesh_processing::self_intersections<K>
+            (mesh, back_inserter(intersected_tris));
+
+  std::cout << intersected_tris.size() << " pairs of triangles are intersecting." << std::endl;
+  
   return 0;
 }
