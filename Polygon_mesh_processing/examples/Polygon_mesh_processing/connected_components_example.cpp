d95f3060adc6d988938aa0a66bd3b3c68594da5b
Jane Tournois <jane.tournois@geometryfactory.com>
2015-03-16 17:01:48 +0100

    connected components example

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/connected_components_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/connected_components_example.cpp
@@ -1,19 +1,23 @@
-#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Surface_mesh.h>
+
 #include <CGAL/Polygon_mesh_processing/connected_components.h>
+
+#include <boost/property_map/property_map.hpp>
+#include <boost/foreach.hpp>
 #include <iostream>
 #include <fstream>
 
-namespace PMP = CGAL::Polygon_mesh_processing;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::Point_3                                     Point;
+typedef Kernel::Compare_dihedral_angle_3                    Compare_dihedral_angle_3;
 
-typedef CGAL::Simple_cartesian<double>     Kernel;
-typedef Kernel::Point_3                    Point;
-typedef Kernel::Compare_dihedral_angle_3   Compare_dihedral_angle_3;
-typedef CGAL::Surface_mesh<Point>          Mesh;
+typedef CGAL::Surface_mesh<Point>                           Mesh;
 
 
 template <typename G>
-struct Constraint : public boost::put_get_helper<bool,Constraint<G> >{
+struct Constraint : public boost::put_get_helper<bool,Constraint<G> >
+{
   typedef typename boost::graph_traits<G>::edge_descriptor edge_descriptor;
   typedef boost::readable_property_map_tag      category;
   typedef bool                                  value_type;
@@ -21,24 +25,24 @@ struct Constraint : public boost::put_get_helper<bool,Constraint<G> >{
   typedef edge_descriptor                       key_type;
 
   Constraint()
-    :g(NULL)
+    :g_(NULL)
   {}
 
   Constraint(G & g, double bound) 
-    : g(&g), bound(bound)
+    : g_(&g), bound_(bound)
   {}
 
   bool operator[](edge_descriptor e) const {
-    return compare((*g).point(source(e,*g)),
-                   (*g).point(target(e,*g)),
-                   (*g).point(target(next(halfedge(e,*g),*g),*g)),
-                   (*g).point(target(next(opposite(halfedge(e,*g),*g),*g),*g)),
-                   bound) == CGAL::SMALLER;
+    return compare_((*g_).point(source(e,*g_)),
+                   (*g_).point(target(e,*g_)),
+                   (*g_).point(target(next(halfedge(e,*g_),*g_),*g_)),
+                   (*g_).point(target(next(opposite(halfedge(e,*g_),*g_),*g_),*g_)),
+                   bound_) == CGAL::SMALLER;
   }
 
-  G* g;
-  Compare_dihedral_angle_3 compare;
-  double bound;
+  G* g_;
+  Compare_dihedral_angle_3 compare_;
+  double bound_;
 };
 
 
@@ -46,36 +50,40 @@ int main(int, char* argv[])
 {
   typedef boost::graph_traits<Mesh>::face_descriptor face_descriptor;
   const double bound = std::cos(0.7* CGAL_PI);
-  Mesh sm;
-  std::ifstream in(argv[1]);
-  in >> sm;
-  
+
+  Mesh mesh;
+  std::ifstream input("data/eight.off");
+  if (!input || !(input >> mesh) || mesh.is_empty()) {
+    std::cerr << "Not a valid off file." << std::endl;
+    return 1;
+  }
+
   std::vector<face_descriptor> cc;
-  face_descriptor fd = *faces(sm).first;
+  face_descriptor fd = *faces(mesh).first;
   CGAL::Polygon_mesh_processing::connected_component(fd,
-                                                     sm,
-                                                     std::back_inserter(cc));
+                                        mesh,
+                                        std::back_inserter(cc));
 
   std::cerr << "connected components without edge constraints" << std::endl;
   std::cerr << cc.size() << " faces in the CC of " << fd << std::endl;
 
 
   std::cerr << "\nconnected components with edge constraints (dihedral angle < 3/4 pi)" << std::endl;
-  Mesh::Property_map<face_descriptor,std::size_t> fccmap;
-  fccmap = sm.add_property_map<face_descriptor,std::size_t>("f:CC").first; 
-  std::size_t num = PMP::connected_components(sm,
-                                              fccmap,
-                                              Constraint<Mesh>(sm,bound)
-                                              );
+  Mesh::Property_map<face_descriptor, std::size_t> fccmap;
+  fccmap = mesh.add_property_map<face_descriptor, std::size_t>("f:CC").first;
+  std::size_t num = CGAL::Polygon_mesh_processing::connected_components(mesh,
+                                        fccmap,
+                                        Constraint<Mesh>(mesh, bound));
   
- std::cerr << "The graph has " << num << " connected components (face connectivity)" << std::endl;
- BOOST_FOREACH(face_descriptor f , faces(sm)){
-   std::cout  << f << " in connected component " << fccmap[f] << std::endl;
+  std::cerr << "The graph has " << num << " connected components (face connectivity)" << std::endl;
+  BOOST_FOREACH(face_descriptor f , faces(mesh)){
+    std::cout  << f << " in connected component " << fccmap[f] << std::endl;
   }
  
- std::cerr << "We keep the two largest components" << std::endl; 
- PMP::keep_largest_connected_components(sm,2,Constraint<Mesh>(sm,bound));
+  std::cerr << "We keep the two largest components" << std::endl; 
+  CGAL::Polygon_mesh_processing::keep_largest_connected_components(mesh,
+                                        2,
+                                        Constraint<Mesh>(mesh, bound));
 
- std::cout << "mesh:\n" << sm << std::endl;
   return 0;
 }
