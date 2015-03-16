ad91bcb215384eb612bf6ca4892b3aa7c2eb3500
Jane Tournois <jane.tournois@geometryfactory.com>
2015-03-16 13:06:19 +0100

    stitch_borders_example "converted" to Polyhedron
    because it only works for a Polyhedron until now

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/stitch_borders_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/stitch_borders_example.cpp
@@ -1,7 +1,7 @@
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Surface_mesh.h>
-#include <CGAL/boost/graph/graph_traits_Surface_mesh.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
 
 #include <CGAL/Polygon_mesh_processing/stitch_borders.h>
 
@@ -9,12 +9,11 @@
 #include <fstream>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_3 Point_3;
-typedef CGAL::Surface_mesh<Point_3> Surface_mesh;
+typedef CGAL::Polyhedron_3<K> Polyhedron;
 
 int main()
 {
-  Surface_mesh mesh;
+  Polyhedron mesh;
   std::ifstream input("data/full_border_quads.off");
   if (!input || !(input >> mesh) || mesh.is_empty()) {
     std::cerr << "Not a valid off file." << std::endl;
@@ -22,16 +21,16 @@ int main()
   }
 
   std::cout << "Before stitching : " << std::endl;
-  std::cout << "\t Number of vertices :\t" << mesh.number_of_vertices() << std::endl;
-  std::cout << "\t Number of edges :\t" << mesh.number_of_edges() << std::endl;
-  std::cout << "\t Number of facets :\t" << mesh.number_of_faces() << std::endl;
+  std::cout << "\t Number of vertices  :\t" << mesh.size_of_vertices() << std::endl;
+  std::cout << "\t Number of halfedges :\t" << mesh.size_of_halfedges() << std::endl;
+  std::cout << "\t Number of facets    :\t" << mesh.size_of_facets() << std::endl;
 
   CGAL::Polygon_mesh_processing::stitch_borders(mesh);
 
   std::cout << "Stitching done : " << std::endl;
-  std::cout << "\t Number of vertices :\t" << mesh.number_of_vertices() << std::endl;
-  std::cout << "\t Number of edges :\t" << mesh.number_of_edges() << std::endl;
-  std::cout << "\t Number of facets :\t" << mesh.number_of_faces() << std::endl;
+  std::cout << "\t Number of vertices  :\t" << mesh.size_of_vertices() << std::endl;
+  std::cout << "\t Number of halfedges :\t" << mesh.size_of_halfedges() << std::endl;
+  std::cout << "\t Number of facets    :\t" << mesh.size_of_facets() << std::endl;
 
   return 0;
 }
