f2576275de7d969db4853bd8e83e5fb846990806
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2015-03-16 16:26:28 +0100

    typo in #include

--- CGAL_ImageIO/src/CGAL_ImageIO/fgetns.cpp
+++ CGAL_ImageIO/src/CGAL_ImageIO/fgetns.cpp
@@ -21,7 +21,7 @@
 
 #ifndef CGAL_HEADER_ONLY
 
-#include <CGAL/fgets.h>
-#include <CGAL/fgets_impl.h>
+#include <CGAL/fgetns.h>
+#include <CGAL/fgetns_impl.h>
 
 #endif // CGAL_HEADER_ONLY
--- CGAL_ImageIO/src/CGAL_ImageIO/inr.cpp
+++ CGAL_ImageIO/src/CGAL_ImageIO/inr.cpp
@@ -21,7 +21,7 @@
 
 #ifndef CGAL_HEADER_ONLY
 
-#include "inr.h"
-#include "inr_impl.h"
+#include <CGAL/inr.h>
+#include <CGAL/inr_impl.h>
 
 #endif // CGAL_HEADER_ONLY
