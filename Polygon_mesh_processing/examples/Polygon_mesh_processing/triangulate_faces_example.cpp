86426f9982d196fde66067d8b541d383eb315100
Jane Tournois <jane.tournois@geometryfactory.com>
2015-03-16 11:00:37 +0100

    triangulate_faces_example.cpp and add a quad cube

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/triangulate_faces_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/triangulate_faces_example.cpp
@@ -1,6 +1,29 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/boost/graph/graph_traits_Surface_mesh.h>
+
 #include <CGAL/triangulate_faces.h>
+
+#include <fstream>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::Point_3                    Point;
+typedef CGAL::Surface_mesh<Point>          Surface_mesh;
 
 int main()
 {
+  std::ifstream input("data/cube_quad.off");
+
+  Surface_mesh mesh;
+  if (!input || !(input >> mesh) || mesh.is_empty())
+  {
+    std::cerr << "Not a valid off file." << std::endl;
+    return 1;
+  }
+
+  CGAL::Polygon_mesh_processing::triangulate_faces(mesh);
+
+  CGAL_assertion(is_pure_triangle(mesh));
+
   return 0;
 }
