33b6ac4a30b6bcec43e395f2afa4771fb6c0695a
Jane Tournois <jane.tournois@geometryfactory.com>
2015-03-17 11:19:15 +0100

    improve example to use a bbox of size adaptable to input

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/point_inside_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/point_inside_example.cpp
@@ -1,16 +1,32 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
 #include <CGAL/point_generators_3.h>
 
 #include <CGAL/Point_inside_polygon_mesh.h>
 
 #include <vector>
 #include <fstream>
+#include <limits>
+#include <boost/foreach.hpp>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef K::Point_3 Point;
 typedef CGAL::Polyhedron_3<K> Polyhedron;
+
+double max_coordinate(const Polyhedron& poly)
+{
+  double max_coord = std::numeric_limits<double>::min();
+  BOOST_FOREACH(Polyhedron::Vertex_handle v, vertices(poly))
+  {
+    Point p = v->point();
+    max_coord = (std::max)(max_coord, p.x());
+    max_coord = (std::max)(max_coord, p.y());
+    max_coord = (std::max)(max_coord, p.z());
+  }
+  return max_coord;
+}
 
 int main(int argc, char* argv[])
 {
@@ -24,19 +40,19 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  std::cerr << "#vertices = " << num_vertices(poly) << std::endl;
-
   CGAL::Point_inside_polygon_mesh<Polyhedron, K> inside(poly);
 
-  unsigned int nb_points = 100;
-  
-  double size = 1000.;//todo : max absolute coordinate of poly vertices
+  double size = max_coordinate(poly);
 
+  unsigned int nb_points = 100;
   std::vector<Point> points;
   points.reserve(nb_points);
   CGAL::Random_points_in_cube_3<Point> gen(size);
   for (unsigned int i = 0; i < nb_points; ++i)
     points.push_back(*gen++);
+
+  std::cout << "Test " << nb_points << " random points in cube "
+    << "[-" << size << "; " << size <<"]" << std::endl;
 
   int nb_inside = 0;
   int nb_boundary = 0;
@@ -50,7 +66,7 @@ int main(int argc, char* argv[])
 
   std::cerr << "Total query size: " << points.size() << std::endl;
   std::cerr << "  " << nb_inside << " points inside " << std::endl;
-  std::cerr << "  " << nb_boundary << " points boundary " << std::endl;
+  std::cerr << "  " << nb_boundary << " points on boundary " << std::endl;
   std::cerr << "  " << points.size() - nb_inside - nb_boundary << " points outside " << std::endl;
 
   return 0;
