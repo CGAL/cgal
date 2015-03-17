9cf0a3fd3783dfe3b78df1da204d030fa8a29f68
Sébastien Loriot <sebastien.loriot@cgal.org>
2015-03-17 13:26:19 +0100

    add missing license header

--- BGL/include/CGAL/boost/graph/Dual.h
+++ BGL/include/CGAL/boost/graph/Dual.h
@@ -1,3 +1,22 @@
+// Copyright (c) 2015  GeometryFactory (France).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+//
+// Author(s)     : Andreas Fabri
+
 #ifndef CGAL_BGL_DUAL_H
 #define CGAL_BGL_DUAL_H
 
