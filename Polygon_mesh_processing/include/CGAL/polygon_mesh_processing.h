4c92719c990e5f8cfad85f3ba67257a49d0a34b5
Sébastien Loriot <sebastien.loriot@cgal.org>
2015-03-18 17:17:18 +0100

    do not document which files are included

--- Polygon_mesh_processing/include/CGAL/polygon_mesh_processing.h
+++ Polygon_mesh_processing/include/CGAL/polygon_mesh_processing.h
@@ -1,21 +1,8 @@
 /**
 * \ingroup PkgPolygonMeshProcessing
 * \file CGAL/polygon_mesh_processing.h
-* Convenience header file only including the headers for all
-* functions of this package that are part of
-* the namespace `Polygon_mesh_processing` :
-* `CGAL/Polygon_mesh_processing/orient_polygon_soup.h`,
-* `CGAL/Polygon_mesh_processing/self_intersections.h`,
-* `CGAL/Polygon_mesh_processing/polygon_soup_to_polygon_mesh.h`,
-* `CGAL/Polygon_mesh_processing/triangulate_faces.h`,
-* `CGAL/Polygon_mesh_processing/triangulate_hole.h`,
-* `CGAL/Polygon_mesh_processing/compute_normal.h`,
-* `CGAL/Polygon_mesh_processing/connected_components.h`,
-* `CGAL/Polygon_mesh_processing/fair.h`,
-* `CGAL/Polygon_mesh_processing/orientation.h`,
-* `CGAL/Polygon_mesh_processing/refine.h`, and
-* `CGAL/Polygon_mesh_processing/stitch_borders.h`.
-
+* Convenience header file including the headers for all
+* the free functions of this package.
 */
 
 #include <CGAL/Polygon_mesh_processing/orient_polygon_soup.h>
