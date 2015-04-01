234ff516627b7c09d9e56d31617bc6844808e353
Jane Tournois <jane.tournois@geometryfactory.com>
2015-04-01 16:44:37 +0200

    remove useless typedefs

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/remove_degeneracies_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/remove_degeneracies_example.cpp
@@ -7,12 +7,8 @@
 #include <fstream>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_3 Point;
-typedef K::Vector_3 Vector;
 
-typedef CGAL::Surface_mesh<Point> Surface_mesh;
-typedef boost::graph_traits<Surface_mesh>::vertex_descriptor vertex_descriptor;
-typedef boost::graph_traits<Surface_mesh>::face_descriptor face_descriptor;
+typedef CGAL::Surface_mesh<K::Point_3> Surface_mesh;
 
 int main(int argc, char* argv[])
 {
