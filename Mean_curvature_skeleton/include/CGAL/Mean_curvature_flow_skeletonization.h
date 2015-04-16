87dce106d4a94518fd9d3e3f62fd64007aa6c362
Sébastien Loriot <sebastien.loriot@cgal.org>
2015-04-16 10:15:54 +0200

    simplify doc

--- Mean_curvature_skeleton/include/CGAL/Mean_curvature_flow_skeletonization.h
+++ Mean_curvature_skeleton/include/CGAL/Mean_curvature_flow_skeletonization.h
@@ -794,7 +794,7 @@ public:
 
 
   /**
-   * Iteratively calls the sequence `contract_geometry()`,  `collapse_edges()`, `split_faces()`, and `detect_degeneracies()`
+   * Iteratively calls `contract()`
    * until the change of surface area of the meso-skeleton after one iteration is smaller than
    * `area_variation_factor()*original_area` where `original_area` is the area of the input triangle mesh,
    * or if the maximum number of iterations has been reached.
