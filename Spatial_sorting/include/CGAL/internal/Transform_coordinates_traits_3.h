e185efc2dca56da4902136b3645b12664efc8757
Sébastien Loriot <sebastien.loriot@cgal.org>
2015-04-16 10:55:53 +0200

    update license to GPL

--- Spatial_sorting/include/CGAL/internal/Transform_coordinates_traits_3.h
+++ Spatial_sorting/include/CGAL/internal/Transform_coordinates_traits_3.h
@@ -1,9 +1,10 @@
 // Copyright (c) 2013  INRIA Sophia-Antipolis (France).
 // All rights reserved.
 //
-// This file is part of CGAL (www.cgal.org); you may redistribute it under
-// the terms of the Q Public License version 1.0.
-// See the file LICENSE.QPL distributed with CGAL.
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
 //
 // Licensees holding a valid commercial license may use this file in
 // accordance with the commercial license agreement provided with the software.
@@ -11,9 +12,8 @@
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
-// $URL: svn+ssh://scm.gforge.inria.fr/svn/cgal/branches/next/Kernel_23/include/CGAL/internal/Projection_traits_3.h $
-// $Id: Projection_traits_3.h 64319 2011-06-23 11:59:29Z sloriot $
-// 
+// $URL$
+// $Id$
 //
 // Author(s)     : Olivier Devillers
 //                 Pedro Machado Manhaes de Castro
