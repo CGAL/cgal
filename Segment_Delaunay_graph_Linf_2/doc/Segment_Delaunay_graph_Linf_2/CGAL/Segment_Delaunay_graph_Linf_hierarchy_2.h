3c822a8cfd6000d37450521da0db240eb99a70d3
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2015-04-19 21:07:43 +0200

    sdglinf doc: explain use of hierarchy in reference

--- Segment_Delaunay_graph_Linf_2/doc/Segment_Delaunay_graph_Linf_2/CGAL/Segment_Delaunay_graph_Linf_hierarchy_2.h
+++ Segment_Delaunay_graph_Linf_2/doc/Segment_Delaunay_graph_Linf_2/CGAL/Segment_Delaunay_graph_Linf_hierarchy_2.h
@@ -9,12 +9,24 @@ but it uses `Segment_Delaunay_graph_Linf_2<Gt,DS>`
 at every level of the hierarchy
 (instead of `Segment_Delaunay_graph_2<Gt,DS>` at each level of
 `Segment_Delaunay_graph_hierarchy_2`).
+
+The `Segment_Delaunay_graph_Linf_hierarchy_2` class derives publicly from
+the `Segment_Delaunay_graph_Linf_2<Gt,DS>` class and has the same
+interface.
 For more details related to the template parameters, see the documentation
 of `Segment_Delaunay_graph_hierarchy_2`.
 
-The `Segment_Delaunay_graph_Linf_hierarchy_2` class derives publicly from
-the `Segment_Delaunay_graph_Linf_2<Gt,DS>` class.
-The interface is the same as its base class.
+The class `Segment_Delaunay_graph_Linf_hierarchy_2`
+should be preferred when the size of the input is large
+and the input sites are inserted one by one (e.g., when the
+whole input is not known in advance):
+Experiments suggest that the hierarchy is faster than the plain
+segment Delaunay graph when the size of the input exceeds 1000 sites.
+If the whole input is known in advance, another option is to use
+the plain segment Delaunay graph but only after
+spatial sorting of the sites.
+See also \ref secsdglinfexamples for different uses of the hierarchy
+and spatial sorting.
 
 \sa `SegmentDelaunayGraphLinfTraits_2`
 \sa `CGAL::Segment_Delaunay_graph_Linf_2<Gt,DS>`
