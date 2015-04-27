a7edcfbd9aa97e962edb0d0623b928405e50208c
Sébastien Loriot <sebastien.loriot@cgal.org>
2015-04-27 10:20:32 +0200

    improve debug macro

--- Surface_mesh_shortest_path/test/Surface_mesh_shortest_path/check.h
+++ Surface_mesh_shortest_path/test/Surface_mesh_shortest_path/check.h
@@ -8,5 +8,7 @@ void CHECK_EQUAL(const FT& a, const FT2& b)
 template <typename FT, typename FT2>
 void CHECK_CLOSE(const FT& a, const FT2& b, const FT& eps)
 {
+  if ( !(CGAL::abs(a-b) < eps) )
+    std::cerr << "ERROR: difference (" << CGAL::abs(a-b) << ") is larger than eps (" << eps << ").\n";
   assert(CGAL::abs(a-b) < eps);
 }
