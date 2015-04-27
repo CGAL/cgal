7d562833e0c827b941d4bfeb71e16ca769fa7b62
Michael Hemmer <mhsaar@gmail.com>
2015-04-27 13:29:50 +0200

    rm warnings

--- Kernel_23/test/Kernel_23/Exact_predicates_exact_constructions_kernel.cpp
+++ Kernel_23/test/Kernel_23/Exact_predicates_exact_constructions_kernel.cpp
@@ -2,6 +2,7 @@
 #include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel_with_kth_root.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel_with_root_of.h>
+#include <CGAL/use.h> 
 
 int main(){
 
@@ -10,5 +11,10 @@ int main(){
   typedef CGAL::Exact_predicates_exact_constructions_kernel_with_kth_root EPEKK;
   typedef CGAL::Exact_predicates_exact_constructions_kernel_with_root_of  EPEKR;
   
+  CGAL_USE_TYPE(EPEK); 
+  CGAL_USE_TYPE(EPEKS);
+  CGAL_USE_TYPE(EPEKK);
+  CGAL_USE_TYPE(EPEKR);
+
   return 0;
 }
