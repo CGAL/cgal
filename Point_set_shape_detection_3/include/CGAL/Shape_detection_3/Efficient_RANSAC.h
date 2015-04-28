e3761ae9d23bb67480c70acf385ffaeaf95f2b4c
Sven Oesau <sven.oesau@inria.fr>
2015-04-28 17:49:38 +0200

    added example call to Efficient_RANSAC::add_shape_factory() documentation

--- Point_set_shape_detection_3/include/CGAL/Shape_detection_3/Efficient_RANSAC.h
+++ Point_set_shape_detection_3/include/CGAL/Shape_detection_3/Efficient_RANSAC.h
@@ -218,6 +218,7 @@ shape. The implementation follows \cgalCite{schnabel2007efficient}.
     }
     /*!
       Registers in the detection engine the shape type `ShapeType` that must inherit from `Shape_base`.
+      Example: add_shape_factory<Shape_detection_3::Plane<Traits>>();
     */ 
     template <class ShapeType>
     void add_shape_factory() {
