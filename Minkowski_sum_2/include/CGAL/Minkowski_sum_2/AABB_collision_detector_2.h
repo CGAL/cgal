423ce309f43484567db26ba178107291fdee1aee
Efi Fogel <efifogel@gmail.com>
2015-05-12 21:48:40 +0300

    Added missing Container template parameter

--- Minkowski_sum_2/include/CGAL/Minkowski_sum_2/AABB_collision_detector_2.h
+++ Minkowski_sum_2/include/CGAL/Minkowski_sum_2/AABB_collision_detector_2.h
@@ -27,51 +27,52 @@ namespace CGAL {
 template <class Kernel_, class Container_>
 class AABB_collision_detector_2
 {
+public:
+  typedef Kernel_                                       Kernel;
+  typedef Container_                                    Container;
+
+  typedef typename Kernel::Point_2                      Point_2;
+  typedef typename Kernel::Vector_2                     Vector_2;
+  typedef typename CGAL::Polygon_2<Kernel, Container>   Polygon_2;
+  typedef typename CGAL::Polygon_with_holes_2<Kernel, Container>
+                                                        Polygon_with_holes_2;
+  typedef typename Polygon_2::Edge_const_iterator       Edge_iterator;
+  typedef AABB_segment_2_primitive<Kernel, Edge_iterator, Polygon_with_holes_2>
+                                                        Tree_segment_2;
+  typedef AABB_traits_2<Kernel, Tree_segment_2>         Tree_traits;
+  typedef AABB_tree_with_join<Tree_traits>              Tree_2;
 
 public:
-
-  typedef typename Kernel_::Point_2 Point_2;
-  typedef typename Kernel_::Vector_2 Vector_2;
-  typedef typename CGAL::Polygon_2<Kernel_> Polygon_2;
-  typedef typename CGAL::Polygon_with_holes_2<Kernel_> Polygon_with_holes_2;
-  typedef typename Polygon_2::Edge_const_iterator Edge_iterator;
-  typedef AABB_segment_2_primitive<Kernel_, Edge_iterator, Polygon_with_holes_2>
-  Tree_segment_2;
-  typedef AABB_traits_2<Kernel_, Tree_segment_2> Tree_traits;
-  typedef AABB_tree_with_join<Tree_traits> Tree_2;
-
-public:
-
-  AABB_collision_detector_2(const Polygon_with_holes_2 &p, const Polygon_with_holes_2 &q)
-    : m_p(q), m_q(p)
+  AABB_collision_detector_2(const Polygon_with_holes_2& p,
+                            const Polygon_with_holes_2& q) :
+    m_p(q), m_q(p)
   {
-      m_stationary_tree.insert(p.outer_boundary().edges_begin(), p.outer_boundary().edges_end());
+    m_stationary_tree.insert(p.outer_boundary().edges_begin(),
+                             p.outer_boundary().edges_end());
 
-      typename Polygon_with_holes_2::Hole_const_iterator it = p.holes_begin();
-      while(it != p.holes_end())
-      {
-        m_stationary_tree.insert(it->edges_begin(), it->edges_end());
-        ++it;
-      }
+    typename Polygon_with_holes_2::Hole_const_iterator it = p.holes_begin();
+    while (it != p.holes_end())
+    {
+      m_stationary_tree.insert(it->edges_begin(), it->edges_end());
+      ++it;
+    }
 
-      m_translating_tree.insert(q.outer_boundary().edges_begin(), q.outer_boundary().edges_end());
+    m_translating_tree.insert(q.outer_boundary().edges_begin(),
+                              q.outer_boundary().edges_end());
 
-      it = q.holes_begin();
-      while(it != q.holes_end())
-      {
-        m_translating_tree.insert(it->edges_begin(), it->edges_end());
-        ++it;
-      }
+    it = q.holes_begin();
+    while (it != q.holes_end())
+    {
+      m_translating_tree.insert(it->edges_begin(), it->edges_end());
+      ++it;
+    }
   }
 
   // Returns true iff the polygons' boundaries intersect or one polygon is
   // completely inside of the other one. Q is translated by t.
   bool check_collision(const Point_2 &t)
   {
-    if (m_stationary_tree.do_intersect(m_translating_tree, t))
-    {
-      return true;
-    }
+    if (m_stationary_tree.do_intersect(m_translating_tree, t)) return true;
 
     // If t_q is inside of P, or t_p is inside of Q, one polygon is completely
     // inside of the other.
@@ -80,18 +81,20 @@ public:
 
     // Use bounded_side_2() instead of on_bounded_side() because the latter
     // checks vor simplicity every time.
-    return bounded_side_2(m_p.outer_boundary().vertices_begin(), m_p.outer_boundary().vertices_end(), t_q, m_p.outer_boundary().traits_member()) == ON_BOUNDED_SIDE
-        || bounded_side_2(m_q.outer_boundary().vertices_begin(), m_q.outer_boundary().vertices_end(), t_p, m_q.outer_boundary().traits_member()) == ON_BOUNDED_SIDE;
+    return
+      bounded_side_2(m_p.outer_boundary().vertices_begin(),
+                     m_p.outer_boundary().vertices_end(), t_q,
+                     m_p.outer_boundary().traits_member()) == ON_BOUNDED_SIDE ||
+      bounded_side_2(m_q.outer_boundary().vertices_begin(),
+                     m_q.outer_boundary().vertices_end(), t_p,
+                     m_q.outer_boundary().traits_member()) == ON_BOUNDED_SIDE;
   }
 
 private:
-
-
-
   Tree_2 m_stationary_tree;
   Tree_2 m_translating_tree;
-  const Polygon_with_holes_2 &m_p;
-  const Polygon_with_holes_2 &m_q;
+  const Polygon_with_holes_2& m_p;
+  const Polygon_with_holes_2& m_q;
 };
 
 } // namespace CGAL
