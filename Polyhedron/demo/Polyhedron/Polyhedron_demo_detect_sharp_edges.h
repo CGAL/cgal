139949d739a71c2f9f3ffd3b88c5f393e6faed39
Jane Tournois <jane.tournois@geometryfactory.com>
2015-05-21 14:39:15 +0200

    split detect_sharp_edges in two functions

--- Polyhedron/demo/Polyhedron/Polyhedron_demo_detect_sharp_edges.h
+++ Polyhedron/demo/Polyhedron/Polyhedron_demo_detect_sharp_edges.h
@@ -7,20 +7,26 @@
 namespace CGAL
 {
   template<typename Polyhedron>
-  void detect_sharp_edges(Polyhedron* pMesh, const double angle)
+  void reset_sharp_edges(Polyhedron* pMesh)
   {
-    CGAL::Mesh_3::Detect_features_in_polyhedra<Polyhedron> detect_features;
-
     for (typename Polyhedron::Edge_iterator
-          eit = pMesh->edges_begin(),
-          end = pMesh->edges_end(); eit != end; ++eit)
+      eit = pMesh->edges_begin(),
+      end = pMesh->edges_end(); eit != end; ++eit)
     {
       eit->set_feature_edge(false);
     }
+  }
+
+  template<typename Polyhedron>
+  void detect_sharp_edges(Polyhedron* pMesh, const double angle)
+  {
+    reset_sharp_edges(pMesh);
 
     // Detect edges in current polyhedron
+    CGAL::Mesh_3::Detect_features_in_polyhedra<Polyhedron> detect_features;
     detect_features.detect_sharp_edges(*pMesh, angle);
   }
+
 }//end namespace CGAL
 
 #endif //POLYHEDRON_DEMO_DETECT_SHARP_EDGES_H
