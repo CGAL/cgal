fb0e11a2aa8e944c4d33894da9819523f9a79391
Clement Jamin <clement.jamin@inria.fr>
2015-05-27 17:47:34 +0200

    Fix collapse

--- Tangential_complex/include/CGAL/Tangential_complex/Simplicial_complex.h
+++ Tangential_complex/include/CGAL/Tangential_complex/Simplicial_complex.h
@@ -124,69 +124,76 @@ public:
     typedef std::vector<Simplex_iterator>             Simplex_iterator_list;
     typedef std::map<Simplex, Simplex_iterator_list>  Cofaces_map;
 
-    // Create a map associating each non-maximal k-faces to the list of its
-    // maximal cofaces
-    Cofaces_map cofaces_map;
-    for (Complex::const_iterator it_simplex = m_complex.begin(),
-                                 it_simplex_end = m_complex.end() ;
-         it_simplex != it_simplex_end ;
-         ++it_simplex)
+    std::size_t num_collapsed_maximal_simplices = 0;
+    do 
     {
-      if (it_simplex->size() > k + 1)
+      num_collapsed_maximal_simplices = 0;
+      // Create a map associating each non-maximal k-faces to the list of its
+      // maximal cofaces
+      Cofaces_map cofaces_map;
+      for (Complex::const_iterator it_simplex = m_complex.begin(),
+                                   it_simplex_end = m_complex.end() ;
+           it_simplex != it_simplex_end ;
+           ++it_simplex)
       {
-        std::vector<Simplex> k_faces;
-        // Get the k-faces composing the simplex
-        combinations(*it_simplex, k + 1, std::back_inserter(k_faces));
-        for (const auto &comb : k_faces) // CJTODO C++1
-          cofaces_map[comb].push_back(it_simplex);
-      }
-    }
-
-    // For each non-maximal k-face F, if F has only one maximal coface Cf:
-    // - Look for the other k-faces F2, F3... of Cf in the map and:
-    //    * if the list contains only Cf, clear the list (we don't remove the
-    //      list since it creates troubles with the iterators) and add the F2,
-    //      F3... to the complex
-    //    * otherwise, remove Cf from the associated list
-    // - Remove Cf from the complex
-    for (Cofaces_map::const_iterator it_map_elt = cofaces_map.begin(),
-                                     it_map_end = cofaces_map.end() ;
-         it_map_elt != it_map_end ;
-         ++it_map_elt)
-    {
-      if (it_map_elt->second.size() == 1)
-      {
-        std::vector<Simplex> k_faces;
-        const Simplex_iterator_list::value_type &it_Cf =
-          *it_map_elt->second.begin();
-        CGAL_assertion(it_Cf->size() == max_simplex_dim + 1);
-        // Get the k-faces composing the simplex
-        combinations(*it_Cf, k + 1, std::back_inserter(k_faces));
-        for (const auto &f2 : k_faces) // CJTODO C++1
+        if (it_simplex->size() > k + 1)
         {
-          // Skip F
-          if (f2 != it_map_elt->first)
+          std::vector<Simplex> k_faces;
+          // Get the k-faces composing the simplex
+          combinations(*it_simplex, k + 1, std::back_inserter(k_faces));
+          for (const auto &comb : k_faces) // CJTODO C++1
+            cofaces_map[comb].push_back(it_simplex);
+        }
+      }
+
+      // For each non-maximal k-face F, if F has only one maximal coface Cf:
+      // - Look for the other k-faces F2, F3... of Cf in the map and:
+      //    * if the list contains only Cf, clear the list (we don't remove the
+      //      list since it creates troubles with the iterators) and add the F2,
+      //      F3... to the complex
+      //    * otherwise, remove Cf from the associated list
+      // - Remove Cf from the complex
+      for (Cofaces_map::const_iterator it_map_elt = cofaces_map.begin(),
+                                       it_map_end = cofaces_map.end() ;
+           it_map_elt != it_map_end ;
+           ++it_map_elt)
+      {
+        if (it_map_elt->second.size() == 1)
+        {
+          std::vector<Simplex> k_faces;
+          const Simplex_iterator_list::value_type &it_Cf =
+            *it_map_elt->second.begin();
+          CGAL_assertion(it_Cf->size() == max_simplex_dim + 1);
+          // Get the k-faces composing the simplex
+          combinations(*it_Cf, k + 1, std::back_inserter(k_faces));
+          for (const auto &f2 : k_faces) // CJTODO C++1
           {
-            Cofaces_map::iterator it_comb_in_map = cofaces_map.find(f2);
-            if (it_comb_in_map->second.size() == 1)
+            // Skip F
+            if (f2 != it_map_elt->first)
             {
-              it_comb_in_map->second.clear();
-              m_complex.insert(f2);
-            }
-            else // it_comb_in_map->second.size() > 1
-            {
-              Simplex_iterator_list::iterator it = std::find(
-                it_comb_in_map->second.begin(),
-                it_comb_in_map->second.end(),
-                it_Cf);
-              CGAL_assertion(it != it_comb_in_map->second.end());
-              it_comb_in_map->second.erase(it);
+              Cofaces_map::iterator it_comb_in_map = cofaces_map.find(f2);
+              if (it_comb_in_map->second.size() == 1)
+              {
+                it_comb_in_map->second.clear();
+                m_complex.insert(f2);
+              }
+              else // it_comb_in_map->second.size() > 1
+              {
+                Simplex_iterator_list::iterator it = std::find(
+                  it_comb_in_map->second.begin(),
+                  it_comb_in_map->second.end(),
+                  it_Cf);
+                CGAL_assertion(it != it_comb_in_map->second.end());
+                it_comb_in_map->second.erase(it);
+              }
             }
           }
+          m_complex.erase(it_Cf);
+          ++num_collapsed_maximal_simplices;
         }
-        m_complex.erase(it_Cf);
       }
-    }
+      // Repeat until no maximal simplex got removed
+    } while (num_collapsed_maximal_simplices > 0);
 
     // Collapse the lower dimension simplices
     if (k > 0)
