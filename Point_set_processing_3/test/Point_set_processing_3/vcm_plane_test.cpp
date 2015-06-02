d74bd22b8aad47799d8335508d22d09e497c5fff
Sébastien Loriot <sebastien.loriot@cgal.org>
2015-06-02 08:36:53 +0200

    be more tolerant with the output

--- Point_set_processing_3/test/Point_set_processing_3/vcm_plane_test.cpp
+++ Point_set_processing_3/test/Point_set_processing_3/vcm_plane_test.cpp
@@ -28,8 +28,13 @@ int main (void) {
                          CGAL::Second_of_pair_property_map<PointWithNormal>(),
                          R, 0.0);
 
+    std::cout << "Normal is " << points[0].second << std::endl;
+
     // The normal at the origin should be (0, 0, -1)
-    assert(points[0].second == Vector_3(0, 0, -1));
+    double epsilon=1e-5;
+    assert(points[0].second.x() < epsilon && points[0].second.x() > -epsilon);
+    assert(points[0].second.y() < epsilon && points[0].second.y() > -epsilon);
+    assert(points[0].second.z() < -1+epsilon && points[0].second.z() > -1-epsilon);
 
     return 0;
 }
