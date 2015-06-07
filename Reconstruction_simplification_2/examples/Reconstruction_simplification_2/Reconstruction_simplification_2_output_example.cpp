5f1194c0f906222114fad8e2efc848100915cd9f
Pierre Alliez <pierre.alliez@inria.fr>
2015-06-07 17:12:59 +0200

    use terms vertices/edges for indexed output

--- Reconstruction_simplification_2/examples/Reconstruction_simplification_2/Reconstruction_simplification_2_output_example.cpp
+++ Reconstruction_simplification_2/examples/Reconstruction_simplification_2/Reconstruction_simplification_2_output_example.cpp
@@ -31,19 +31,19 @@ void list_output(Rs_2& rs2)
 {
   std::cout << "(-------------List output---------- )" << std::endl;
   
-  std::vector<Point> isolated_vertices;
-  std::vector<Segment> edges;
+  std::vector<Point> isolated_points;
+  std::vector<Segment> segments;
   
   rs2.extract_list_output(
-    std::back_inserter(isolated_vertices), std::back_inserter(edges));
+    std::back_inserter(isolated_points), std::back_inserter(segments));
   
-  std::vector<Point>::iterator vit;
-  for (vit = isolated_vertices.begin(); vit != isolated_vertices.end(); vit++) 
-    std::cout  <<  *vit << std::endl;
+  std::vector<Point>::iterator pit;
+  for (pit = isolated_points.begin(); pit != isolated_points.end(); pit++) 
+    std::cout  << *pit << std::endl;
   
-  std::vector<Segment>::iterator eit;
-  for (eit = edges.begin(); eit != edges.end(); eit++) 
-    std::cout << *eit << std::endl;
+  std::vector<Segment>::iterator sit;
+  for (sit = segments.begin(); sit != segments.end(); sit++) 
+    std::cout << *sit << std::endl;
 }
 
 
@@ -52,20 +52,27 @@ void index_output(Rs_2& rs2)
   std::cout << "(-------------Off output---------- )" << std::endl;
   
   std::vector<Point> points;
-  std::vector<std::size_t> isolated_points;
+  std::vector<std::size_t> isolated_vertices;
   std::vector<std::pair<std::size_t,std::size_t> > edges;
 
   rs2.indexed_output(
     std::back_inserter(points), 
-    std::back_inserter(isolated_points),
+    std::back_inserter(isolated_vertices),
     std::back_inserter(edges));
   
   std::cout << "OFF " << points.size() << " 0 " << edges.size()  << std::endl;
 
+  // points
   std::vector<Point>::iterator pit;
   for (pit = points.begin(); pit != points.end(); pit++) 
 	  std::cout << *pit << std::endl;
 
+  // isolated vertices
+  std::vector<std::size_t>::iterator vit;
+  for (vit = isolated_vertices.begin(); vit != isolated_vertices.end(); vit++)
+    std::cout << "1 "  << *vit << std::endl;
+
+  // edges
   std::vector<std::pair<std::size_t, std::size_t> >::iterator eit;
   for (eit = edges.begin(); eit != edges.end(); eit++)
     std::cout << "2 "  << eit->first << " " << eit->second << std::endl;
