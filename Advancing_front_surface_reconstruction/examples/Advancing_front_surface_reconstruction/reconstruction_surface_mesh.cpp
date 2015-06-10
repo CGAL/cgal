15e33a861fb49791b09251c00c145a5dc62c4925
Andreas Fabri <andreas.fabri@geometryfactory.com>
2015-06-10 18:26:41 +0200

    Write into a wrapper of a Surface_mesh as ouput iterator

---
+++ Advancing_front_surface_reconstruction/examples/Advancing_front_surface_reconstruction/reconstruction_surface_mesh.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Advancing_front_surface_reconstruction.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/tuple.h>
+
+typedef CGAL::cpp11::tuple<std::size_t,std::size_t,std::size_t> Facet;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::Point_3  Point_3;
+
+typedef CGAL::Surface_mesh<Point_3> Mesh;
+
+struct Construct{
+  Mesh& mesh;
+  
+  template < typename PointIterator>
+  Construct(Mesh& mesh,PointIterator b, PointIterator e)
+    : mesh(mesh)
+  {
+    for(; b!=e; ++b){
+      boost::graph_traits<Mesh>::vertex_descriptor v;
+      v = add_vertex(mesh);
+      mesh.point(v) = *b;
+    }
+  }    
+   
+  Construct& operator=(const Facet f)
+  {
+    typedef boost::graph_traits<Mesh>::vertex_descriptor vertex_descriptor;
+    mesh.add_face(vertex_descriptor(get<0>(f)), 
+                  vertex_descriptor(get<1>(f)),
+                  vertex_descriptor(get<2>(f)));
+    return *this;
+  }
+
+  Construct&
+  operator*() { return *this; }
+
+  Construct&
+  operator++() { return *this; }
+
+  Construct
+  operator++(int) { return *this; }
+  
+};
+
+int main(int argc, char* argv[])
+{
+  std::ifstream in((argc>1)?argv[1]:"data/half.xyz"); 
+  std::vector<Point_3> points;
+  std::vector<Facet> facets;
+  Mesh m;
+
+  std::copy(std::istream_iterator<Point_3>(in), 
+            std::istream_iterator<Point_3>(), 
+            std::back_inserter(points));
+  
+  Construct construct(m,points.begin(),points.end());
+
+  CGAL::advancing_front_surface_reconstruction(points.begin(),
+                                               points.end(),
+                                               construct);
+
+  std::cout << m  << std::endl;
+
+  return 0;
+}
