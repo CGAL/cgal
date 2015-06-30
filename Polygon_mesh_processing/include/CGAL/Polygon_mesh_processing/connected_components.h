9356e59af050f6c174f8cb7d62287f7269b13924
Andreas Fabri <andreas.fabri@geometryfactory.com>
2015-06-30 19:54:25 +0200

    fix copyrght headers

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/connected_components.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/connected_components.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 GeometryFactory (France).
+// Copyright (c) 2011, 2015 GeometryFactory (France).
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org).
--- Polygon_mesh_processing/include/CGAL/Side_of_triangle_mesh.h
+++ Polygon_mesh_processing/include/CGAL/Side_of_triangle_mesh.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2013 GeometryFactory (France).
+// Copyright (c) 2013, 2014, 2015 GeometryFactory (France).
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org).
