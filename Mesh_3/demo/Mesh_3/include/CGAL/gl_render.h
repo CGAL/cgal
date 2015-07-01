cd3493d139b386122f23fddb01e4b84936805261
Jane Tournois <jane.tournois@geometryfactory.com>
2015-07-01 14:40:22 +0200

    fix compilation

--- Mesh_3/demo/Mesh_3/include/CGAL/gl_render.h
+++ Mesh_3/demo/Mesh_3/include/CGAL/gl_render.h
@@ -11,6 +11,7 @@ void gl_render_facets(Polyhedron& polyhedron)
   typedef typename Kernel::Point_3	    Point;
   typedef typename Kernel::Vector_3	    Vector;
   typedef typename Polyhedron::Facet	    Facet;
+  typedef typename Polyhedron::Facet_handle   Facet_handle;
   typedef typename Polyhedron::Facet_iterator Facet_iterator;
   typedef typename Polyhedron::Halfedge_around_facet_circulator HF_circulator;
 
@@ -28,7 +29,8 @@ void gl_render_facets(Polyhedron& polyhedron)
     // If Flat shading: 1 normal per polygon
     if (shading == GL_FLAT)
     {
-      Vector n = CGAL::Polygon_mesh_processing::compute_facet_normal<Kernel>(*f);
+      Vector n =
+        CGAL::Polygon_mesh_processing::compute_face_normal(f, polyhedron);
       ::glNormal3d(n.x(),n.y(),n.z());
     }
 
@@ -40,7 +42,8 @@ void gl_render_facets(Polyhedron& polyhedron)
       // If Gouraud shading: 1 normal per vertex
       if (shading == GL_SMOOTH)
       {
-        Vector n = CGAL::Polygon_mesh_processing::compute_vertex_normal<Kernel>(*he->vertex());
+        Vector n =
+          CGAL::Polygon_mesh_processing::compute_vertex_normal(he->vertex(), polyhedron);
         ::glNormal3d(n.x(),n.y(),n.z());
       }
 
