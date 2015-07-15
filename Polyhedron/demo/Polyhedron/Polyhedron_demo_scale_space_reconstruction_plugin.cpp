5bcc14593c8dc396fbebde1155ee0e76655d3de2
Maxime Gimeno <maxime.gimeno@gmail.com>
2015-07-15 14:14:39 +0200

    Segfault fix -Q_PLUGIN_METADATA was misplaced in the space_scale_plugin

--- Polyhedron/demo/Polyhedron/Polyhedron_demo_scale_space_reconstruction_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Polyhedron_demo_scale_space_reconstruction_plugin.cpp
@@ -18,7 +18,6 @@
 class Polyhedron_demo_scale_space_reconstruction_plugin_dialog : public QDialog, private Ui::ScaleSpaceOptionsDialog
 {
   Q_OBJECT
-     Q_PLUGIN_METADATA(IID "com.geometryfactory.PolyhedronDemo.PluginInterface/1.0")
   public:
     Polyhedron_demo_scale_space_reconstruction_plugin_dialog(QWidget* /*parent*/ = 0)
     {
@@ -38,6 +37,8 @@ class Polyhedron_demo_scale_space_reconstruction_plugin :
   public Polyhedron_demo_plugin_helper
 {
   Q_OBJECT
+  Q_PLUGIN_METADATA(IID "com.geometryfactory.PolyhedronDemo.PluginInterface/1.0")
+
   Q_INTERFACES(Polyhedron_demo_plugin_interface)
   QAction* actionScaleSpaceReconstruction;
 
@@ -158,7 +159,5 @@ void Polyhedron_demo_scale_space_reconstruction_plugin::on_actionScaleSpaceRecon
     QApplication::restoreOverrideCursor();
   }
 }
-
-//Q_EXPORT_PLUGIN2(Polyhedron_demo_scale_space_reconstruction_plugin, Polyhedron_demo_scale_space_reconstruction_plugin)
 
 #include "Polyhedron_demo_scale_space_reconstruction_plugin.moc"
