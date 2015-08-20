f20024ff41211a2007114ba403574b247065497b
Simon Giraudot <simon.giraudot@geometryfactory.com>
2015-08-20 11:57:21 +0200

    Update moved out-of-date file

--- Solver_interface/doc/Solver_interface/CGAL/Eigen_solver_traits.h
+++ Solver_interface/doc/Solver_interface/CGAL/Eigen_solver_traits.h
@@ -7,16 +7,36 @@ namespace CGAL {
 The class `Eigen_solver_traits` provides an interface to the sparse solvers of \ref thirdpartyEigen "Eigen".
 The version 3.1 (or greater) of \ref thirdpartyEigen "Eigen" must be available on the system. 
 
-\cgalModels `SparseLinearAlgebraTraitsWithFactor_d`
+\cgalModels `SparseLinearAlgebraTraitsWithFactor_d` and `#NormalEquationSparseLinearAlgebraTraits_d`
 
 
-\tparam T a sparse solver of \ref thirdpartyEigen "Eigen". The default solver is the iterative bi-congugate gradient stabilized solver 
-<a href="http://eigen.tuxfamily.org/dox/classEigen_1_1BiCGSTAB.html">`Eigen::BiCGSTAB`</a> for `double`. 
+`T`: a sparse solver of \ref thirdpartyEigen "Eigen". The default solver is the iterative bi-congugate gradient stabilized solver 
+`Eigen::BiCGSTAB` for `double`. 
 
 \sa `CGAL::Eigen_sparse_matrix<T>`
 \sa `CGAL::Eigen_sparse_symmetric_matrix<T>`
 \sa `CGAL::Eigen_vector<T>`
+\sa http://eigen.tuxfamily.org
 
+Example 
+-------------- 
+
+The instantiation of this class assumes an \ref thirdpartyEigen "Eigen" sparse solver is provided. Here are few examples: 
+
+\code{.cpp} 
+
+typedef CGAL::Eigen_sparse_matrix<double>::EigenType EigenMatrix; 
+
+//iterative general solver 
+typedef CGAL::Eigen_solver_traits< Eigen::BiCGSTAB<EigenMatrix> > Iterative_general_solver; 
+
+//iterative symmetric solver 
+typedef CGAL::Eigen_solver_traits< Eigen::ConjugateGradient<EigenMatrix> > Iterative_symmetric_solver; 
+
+//direct symmetric solver 
+typedef CGAL::Eigen_solver_traits< Eigen::SimplicialCholesky<EigenMatrix> > Direct_symmetric_solver; 
+
+\endcode 
 
 */
 template< typename T >
@@ -37,7 +57,7 @@ typedef typename T::Scalar NT;
 typedef CGAL::Eigen_vector<NT> Vector; 
 
 /*!
-If `T` is <a href="http://eigen.tuxfamily.org/dox/classEigen_1_1ConjugateGradient.html">`Eigen::ConjugateGradient<M>`</a> or <a href="http://eigen.tuxfamily.org/dox/classEigen_1_1SimplicialCholesky.html">`Eigen::SimplicialCholesky<M>`</a>, `Matrix` is `CGAL::Eigen_sparse_symmetric_matrix<T>` 
+If `T` is `Eigen::ConjugateGradient<M>` or `Eigen::SimplicialCholesky<M>`, `Matrix` is `CGAL::Eigen_sparse_symmetric_matrix<T>` 
 and `CGAL::Eigen_sparse_matrix<T>` otherwise. 
 
 */ 
