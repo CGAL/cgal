c6284432c471f6dff7c5ff93584940bc62dc85c8
Aymeric PELLE <aymeric.pelle@inria.fr>
2015-08-24 10:08:54 +0200

    Add a comment and change typedefs in a p3rt3_insert_only.

--- Periodic_3_triangulation_3/examples/Periodic_3_triangulation_3/p3rt3_insert_only.cpp
+++ Periodic_3_triangulation_3/examples/Periodic_3_triangulation_3/p3rt3_insert_only.cpp
@@ -13,6 +13,9 @@ typedef K::FT FT;
 typedef CGAL::Regular_triangulation_euclidean_traits_3<K>   RT;
 typedef CGAL::Periodic_3_regular_triangulation_traits_3<RT> Traits;
 
+/* If remove() isn't called in our program, we can use a triangulation data structure more appropriate
+ * which saves some memory resources.
+ */
 typedef CGAL::Triangulation_vertex_base_3<Traits, CGAL::Periodic_3_triangulation_ds_vertex_base_3<> > Vb;
 typedef CGAL::Triangulation_cell_base_3<Traits, CGAL::Periodic_3_triangulation_ds_cell_base_3<> > Cb;
 typedef CGAL::Triangulation_data_structure_3<Vb, Cb> Tds;
@@ -20,10 +23,10 @@ typedef CGAL::Triangulation_data_structure_3<Vb, Cb> Tds;
 typedef CGAL::Periodic_3_regular_triangulation_3<Traits, Tds>    P3RT3;
 
 typedef Traits::Iso_cuboid_3 Iso_cuboid;
+typedef Traits::Weighted_point Weighted_point;
+typedef Traits::Bare_point Bare_point;
 
 typedef P3RT3::Vertex_handle Vertex_handle;
-typedef P3RT3::Weighted_point Weighted_point;
-typedef P3RT3::Bare_point Bare_point;
 
 int main ()
 {
