8a46d3cd5e5ddd22fba94f15d2ea20cc707eef61
Simon Giraudot <simon.giraudot@geometryfactory.com>
2015-08-26 14:48:11 +0200

    Include Lapack svd in Solver interface (another model for SvdTraits)

--- Solver_interface/examples/Solver_interface/singular_value_decomposition.cpp
+++ Solver_interface/examples/Solver_interface/singular_value_decomposition.cpp
@@ -1,11 +1,20 @@
 #include <CGAL/Simple_cartesian.h>
+
+#ifdef CGAL_EIGEN3_ENABLED
 #include <CGAL/Eigen_matrix.h>
 #include <CGAL/Eigen_vector.h>
 #include <CGAL/Eigen_svd.h>
+typedef CGAL::Eigen_svd Svd;
+#else
+#ifdef CGAL_LAPACK_ENABLED
+#include <CGAL/Lapack_svd.h>
+typedef CGAL::Lapack_svd Svd;
+#endif
+#endif
 
-typedef CGAL::Eigen_svd::FT     FT;
-typedef CGAL::Eigen_svd::Vector Eigen_vector;
-typedef CGAL::Eigen_svd::Matrix Eigen_matrix;
+typedef Svd::FT     FT;
+typedef Svd::Vector Eigen_vector;
+typedef Svd::Matrix Eigen_matrix;
 
 int main(void)
 {
@@ -13,7 +22,6 @@ int main(void)
   
   Eigen_vector B (degree);
   Eigen_matrix M (degree, degree);
-
 
   // Fill B and M with random numbers
   for (std::size_t i = 0; i < degree; ++ i)
@@ -24,7 +32,7 @@ int main(void)
     }
 
   // Solve MX=B
-  std::cout << CGAL::Eigen_svd::solve(M, B) << std::endl;
+  std::cout << Svd::solve(M, B) << std::endl;
 
   // Print result
   std::cout << "Solution of SVD = [ ";
