c965754d00051b05a218ff91329ce0d709839bc3
Simon Giraudot <simon.giraudot@geometryfactory.com>
2015-09-14 12:52:05 +0200

    Update example of hierarchical clustering

--- Point_set_processing_3/examples/Point_set_processing_3/hierarchical_clustering_example.cpp
+++ Point_set_processing_3/examples/Point_set_processing_3/hierarchical_clustering_example.cpp
@@ -1,6 +1,10 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/hierarchical_clustering.h>
 #include <CGAL/IO/read_xyz_points.h>
+#include <CGAL/IO/write_xyz_points.h>
+#include <CGAL/Timer.h>
+#include <CGAL/Memory_sizer.h>
+
 
 #include <vector>
 #include <fstream>
@@ -21,11 +25,23 @@ int main(int argc, char*argv[])
     std::cerr << "Error: cannot read file " << fname << std::endl;
     return EXIT_FAILURE;
   }
+  std::cout << "Read " << points.size () << " point(s)" << std::endl;
 
-  std::vector<Point> output; // Algorithm generate a new set of points
+  CGAL::Timer task_timer; task_timer.start();
+  
+  std::vector<Point> output; // Algorithm generates a new set of points
   CGAL::hierarchical_clustering (points.begin (), points.end (),
-				 std::back_inserter (output));
+				 std::back_inserter (output), 100);
 
+  std::size_t memory = CGAL::Memory_sizer().virtual_size();
+  
+  std::cout << output.size () << " point(s) generated in "
+	    << task_timer.time() << " seconds, "
+	    << (memory>>20) << " Mib allocated." << std::endl;
+
+  std::ofstream f ("out.xyz");
+  CGAL::write_xyz_points (f, output.begin (), output.end ());
+  
   return EXIT_SUCCESS;
 }
 
