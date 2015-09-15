00afaca516d72c70505910de380d858abab29f6b
Aymeric Pellé <aymeric.pelle@inria.fr>
2015-09-15 21:00:42 +0200

    Fix warning : typedefs locally defined but not used.

--- Periodic_3_triangulation_3/include/CGAL/Periodic_3_regular_triangulation_3.h
+++ Periodic_3_triangulation_3/include/CGAL/Periodic_3_regular_triangulation_3.h
@@ -6787,9 +6787,6 @@ operator>> (std::istream& is, Periodic_3_regular_triangulation_3<GT,TDS> &tr)
 {
   typedef Periodic_3_regular_triangulation_3<GT,TDS>   P3RT3;
   typedef typename P3RT3::Base                          Base;
-  typedef typename P3RT3::Vertex_iterator               Vertex_iterator;
-  typedef typename GT::FT FT;
-  typedef typename P3RT3::Vertex_handle                 Vertex_handle;
 
   is >> static_cast<Base&>(tr);
 
