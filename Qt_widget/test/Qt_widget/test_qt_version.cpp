6e214c5f4a572b43a1cc06772f85d8695632ec63
Andreas Fabri <andreas.fabri@geometryfactory.com>
2015-09-30 12:51:40 +0200

    remove Qt3 demos and empty dontsubmit files

--- Qt_widget/test/Qt_widget/test_qt_version.cpp
+++
@@ -1,9 +0,0 @@
-#include <CGAL/basic.h>
-  #include <iostream>
-  #include <qglobal.h>
-  int main(int, char**)
-  {
-    std::cout << "QT_VERSION= " << QT_VERSION << std::endl
-	      << "QT_VERSION_STR= " << QT_VERSION_STR << std::endl;
-    return 0;
-  }
