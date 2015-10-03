67c29a637493d18e85ff1a3fd6ea395956fcd0c9
Efi Fogel <efifogel@gmail.com>
2015-10-03 15:31:41 +0300

    Used new definitions of Gps_halfedge_base and Gps_face_base

--- Boolean_set_operations_2/doc/Boolean_set_operations_2/CGAL/Boolean_set_operations_2/Gps_default_dcel.h
+++ Boolean_set_operations_2/doc/Boolean_set_operations_2/CGAL/Boolean_set_operations_2/Gps_default_dcel.h
@@ -1,32 +1,47 @@
-
 namespace CGAL {
 
 /*!
 \ingroup PkgBooleanSetOperations2
 
-The class `Gps_default_dcel` is the default <span class="textsc">Dcel</span> class used by the 
-`General_polygon_set_2` and `Polygon_set_2` class-templates 
-to represent the underlying internal `Arrangement_2` data structure. 
+The default <span class="textsc">Dcel</span> class template used by the
+`General_polygon_set_2` and `General_polygon_with_holes_2` class templates.
+This template is parameterized by a traits class, which is a model of the
+`GeneralPolygonSetTraits_2` concept. It uses the types `Traits::Point_2` and
+`Traits::X_monotone_curve_2` nested in the traits class to instantiate the
+base vertex, halfedge, and face types, respectively. Recall, that the
+<span class="textsc">Dcel</span> stores the incidence relations between
+the arrangement calls and the geometric data attached to vertices and edges.
+This <span class="textsc">Dcel</span> also stores data used to determine
+whether a face is interior and exterior of the general polygon set, and
+additional data used for optimizations.
+
+You need to override this default and use a different
+<span class="textsc">Dcel</span> only if you intend to obtain the underlying
+arrangement of the general polygon set and process it further.
 
 \cgalModels `GeneralPolygonSetDcel`
 
+\sa `Arr_dcel_base<V,H,F>`
+\sa `Gps_halfedge_base<X_monotone_curve_2>`
+\sa `Gps_face_base<Point_2, X_monotone_curve_2>`
 */
-template< typename Traits >
-class Gps_default_dcel 
-  : CGAL::Arr_dcel_base< Arr_vertex_base<typename Traits_::Point_2>,
-                         Arr_halfedge_base<typename Traits_::X_monotone_curve_2>,
-                         Gps_face_base> {
+template <typename Traits>
+class Gps_default_dcel :
+  public Arr_dcel_base<Arr_vertex_base<typename Traits::Point_2>,
+                       Gps_halfedge_base<typename Traits::X_monotone_curve_2>,
+                       Gps_face_base<Traits> >
+{
 public:
 
-/// \name Types 
+/// \name Types
 /// @{
 
 /*!
-allows the rebinding of the <span class="textsc">Dcel</span> with a different traits class `T`. 
-*/ 
-typedef unspecified_type template <class T> rebind; 
+allows the rebinding of the <span class="textsc">Dcel</span> with a different traits class `T`.
+*/
+typedef unspecified_type template <class T> rebind;
 
 /// @}
 
-}; /* end Gps_default_dcel */
+};
 } /* end namespace CGAL */
