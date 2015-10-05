d6866de068f04b0cca39b36777dff66040a2015c
Laurent Rineau <laurent.rineau@cgal.org>
2015-10-05 13:00:07 +0200

    Fix license headers
    Follow-up to 3293c2a68d63637ed25feab71e015b0a5aa3f6f1
    
    Fix #48

--- CGAL_Core/src/CGAL_Core/GmpIO.cpp
+++ CGAL_Core/src/CGAL_Core/GmpIO.cpp
@@ -3,6 +3,15 @@
  * Copyright (c) 1995-2004 Exact Computation Project
  * All rights reserved.
  *
+ * This file is part of CGAL (www.cgal.org).
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Licensees holding a valid commercial license may use this file in
+ * accordance with the commercial license agreement provided with the
+ * software.
+ *
  * file: GmpIO.cpp
  * 		Adapted from multi-files under /cxx in GMP's source distribution
  *
