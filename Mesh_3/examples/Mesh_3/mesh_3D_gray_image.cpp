8ba792f40c7aeb336e4e242c10c56e17e5273728
Jane Tournois <jane.tournois@geometryfactory.com>
2015-10-15 10:48:21 +0200

    add copyright

--- Mesh_3/examples/Mesh_3/mesh_3D_gray_image.cpp
+++ Mesh_3/examples/Mesh_3/mesh_3D_gray_image.cpp
@@ -1,4 +1,27 @@
-#define CGAL_MESH_3_VERBOSE 1
+// Copyright (c) 2012 GeometryFactory (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+//
+// Author(s)     : Laurent Rineau
+//
+//******************************************************************************
+// File Description :
+//***************************************************************************
+
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
 #include <CGAL/Mesh_triangulation_3.h>
