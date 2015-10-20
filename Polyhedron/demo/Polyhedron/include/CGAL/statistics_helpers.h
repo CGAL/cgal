a00245a44310f8da524c5394affaf799b6f4fc1c
Jane Tournois <jane.tournois@geometryfactory.com>
2015-10-20 09:56:20 +0200

    add missing typenames

--- Polyhedron/demo/Polyhedron/include/CGAL/statistics_helpers.h
+++ Polyhedron/demo/Polyhedron/include/CGAL/statistics_helpers.h
@@ -18,24 +18,24 @@ using namespace boost::accumulators;
 template<typename Polyhedron>
 void angles(Polyhedron* poly, double& mini, double& maxi, double& ave)
 {
-  typedef boost::graph_traits<Polyhedron>::face_descriptor face_descriptor;
+  typedef typename boost::graph_traits<Polyhedron>::face_descriptor     face_descriptor;
+  typedef typename boost::graph_traits<Polyhedron>::halfedge_descriptor halfedge_descriptor;
 
   double rad_to_deg = 180. / CGAL_PI;
 
   accumulator_set< double,
     features< tag::min, tag::max, tag::mean > > acc;
 
-  boost::property_map<Polyhedron, CGAL::vertex_point_t>::type
+  typename boost::property_map<Polyhedron, CGAL::vertex_point_t>::type
     vpmap = get(CGAL::vertex_point, *poly);
-  typedef boost::graph_traits<Polyhedron>::halfedge_descriptor halfedge_descriptor;
   BOOST_FOREACH(halfedge_descriptor h, halfedges(*poly))
   {
-    Kernel::Point_3 a = get(vpmap, source(h, *poly));
-    Kernel::Point_3 b = get(vpmap, target(h, *poly));
-    Kernel::Point_3 c = get(vpmap, target(next(h, *poly), *poly));
+    typename Kernel::Point_3 a = get(vpmap, source(h, *poly));
+    typename Kernel::Point_3 b = get(vpmap, target(h, *poly));
+    typename Kernel::Point_3 c = get(vpmap, target(next(h, *poly), *poly));
 
-    Kernel::Vector_3 ba(b, a);
-    Kernel::Vector_3 bc(b, c);
+    typename Kernel::Vector_3 ba(b, a);
+    typename Kernel::Vector_3 bc(b, c);
     double cos_angle = (ba * bc)
       / std::sqrt(ba.squared_length() * bc.squared_length());
 
