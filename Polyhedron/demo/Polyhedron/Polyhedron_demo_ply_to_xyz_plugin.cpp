aff4c2839aa29e9ce1bfdd9f05b573cbdf938e22
Sébastien Loriot <sebastien.loriot@cgal.org>
2015-10-22 11:15:36 +0200

    Adapt the ply plugin to the API of Three

--- Polyhedron/demo/Polyhedron/Polyhedron_demo_ply_to_xyz_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Polyhedron_demo_ply_to_xyz_plugin.cpp
@@ -1,14 +1,14 @@
 #include "Scene_points_with_normal_item.h"
-#include "Polyhedron_demo_io_plugin_interface.h"
+#include <CGAL/Three/Polyhedron_demo_io_plugin_interface.h>
 
 #include <fstream>
 
 class Polyhedron_demo_ply_to_xyz_plugin :
   public QObject,
-  public Polyhedron_demo_io_plugin_interface
+  public CGAL::Three::Polyhedron_demo_io_plugin_interface
 {
   Q_OBJECT
-  Q_INTERFACES(Polyhedron_demo_io_plugin_interface)
+  Q_INTERFACES(CGAL::Three::Polyhedron_demo_io_plugin_interface)
   Q_PLUGIN_METADATA(IID "com.geometryfactory.PolyhedronDemo.IOPluginInterface/1.0")
 
 public:
