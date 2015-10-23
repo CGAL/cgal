bdd51636ded7bea7d1f3e930a1e2b8693e8b97db
Sébastien Loriot <sebastien.loriot@cgal.org>
2015-10-23 11:48:30 +0200

    fix constness issue

--- Operations_on_polyhedra/include/CGAL/internal/Polyhedron_plane_clipping_3.h
+++ Operations_on_polyhedra/include/CGAL/internal/Polyhedron_plane_clipping_3.h
@@ -168,7 +168,7 @@ Polyhedron* clip_polyhedron(Polyhedron& P, Polyhedron& clipping_polyhedron)
 }
 
 template <class Polyhedron, class Plane_3>
-Polyhedron* clip_polyhedron(Polyhedron& P, const Plane_3& p)
+Polyhedron* clip_polyhedron(const Polyhedron& P, const Plane_3& p)
 {
   CGAL::Bbox_3 bbox = CGAL::bbox_3(P.points_begin(), P.points_end());
   //extend the bbox a bit to avoid border cases
@@ -186,7 +186,8 @@ Polyhedron* clip_polyhedron(Polyhedron& P, const Plane_3& p)
     else
       return new Polyhedron(P);
   }
-  return clip_polyhedron(P,clipping_polyhedron);
+  Polyhedron copy(P);
+  return clip_polyhedron(copy, clipping_polyhedron);
 }
 
 } } // CGAL::corefinement
