2f263bb697f5d136d50d64ebc41feea3464fd4c9
Philipp Möller <bootsarehax@gmail.com>
2015-10-30 14:20:23 +0100

    Add the AABBRayIntersectionGeomTraits concept
    This concept is necessary to know when we are able to define
    Intersection_distance in AABB_traits

---
+++ AABB_tree/doc/AABB_tree/Concepts/AABBRayIntersectionGeomTraits.h
@@ -0,0 +1,57 @@
+/*!
+\ingroup PkgAABB_treeConcepts
+\cgalConcept
+
+The concept `AABBRayIntersectionGeomTraits` is a refinement of the
+Model `AABBGeomTraits`. In addition to the types required by
+`AABBGeomTraits` it also requires
+
+\cgalRefines `AABBGeomTraits`
+\cgalHasModel Any 3D Kernel is a model of this concept.
+
+\sa `CGAL::AABB_traits<AABBGeomTraits,AABBPrimitive>`
+\sa `CGAL::AABB_tree<AABBTraits>`
+\sa `AABBPrimitive`
+
+*/
+class AABBRayIntersectionGeomTraits {
+public:
+  /*!
+    Type of a 3D point.
+  */
+  typedef unspecified_type Point_3;
+
+  /*!
+    Type of a 3D ray.
+  */
+  typedef unspecified_type Ray_3;
+
+  /*!
+    Type of a 3D vector.
+  */
+  typedef unspecified_type Vector_3;
+
+  /*!
+    A functor object to construct the source point of a ray. Provides the operator:
+    `Point_3 operator()(const Ray_3&);`
+   */
+  typedef unspecified_type Construct_source_3;
+
+  /*!
+   */
+  Construct_source_3 construct_source_3_object();
+
+  /*!
+    A model of `CartesianConstIterator3`.
+   */
+  typedef unspecified_type Cartesian_const_iterator_3;
+
+  /*!
+    A model of `ConstructCartesianConstIterator3`.
+   */
+  typedef unspecified_type  Construct_cartesian_const_iterator_3;
+
+  /*!
+   */
+  Construct_source_3 construct_cartesian_const_iterator_3_object();
+};
