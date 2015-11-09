8f43f196d62ec0948bf49d69c9a6b0bbe1cc78a1
Pierre Alliez <pierre.alliez@inria.fr>
2015-11-09 11:34:54 +0100

    simpler type

--- Reconstruction_simplification_2/examples/Reconstruction_simplification_2/rs2_simplest_example.cpp
+++ Reconstruction_simplification_2/examples/Reconstruction_simplification_2/rs2_simplest_example.cpp
@@ -1,4 +1,4 @@
-// Example for Reconstruction_simplification_2, with no mass
+// Simplest example for Reconstruction_simplification_2, with no mass
 // attributes for the input points
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
@@ -12,17 +12,17 @@
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef K::Point_2                                          Point;
 
-typedef CGAL::Reconstruction_simplification_2<K>            Rs_2;
+typedef CGAL::Reconstruction_simplification_2<K>            Rs;
 
 int main ()
 {
-  // Generate a set of random points.
+  // Generate a set of random points on the boundary of a square.
   std::vector<Point> points;
   CGAL::Random_points_on_square_2<Point> point_generator(1.);
   CGAL::cpp11::copy_n(point_generator, 100, std::back_inserter(points));
 
-  Rs_2 rs2(points);
-  rs2.run(100); // 100 steps
+  Rs rs(points);
+  rs.run(100); // 100 steps
 
   return 0;
 }
