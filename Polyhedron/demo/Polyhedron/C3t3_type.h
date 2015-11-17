f68eee378c7b8bf5d881c8e20292ea0fd024cb04
Maxime Gimeno <maxime.gimeno@gmail.com>
2015-11-17 09:47:14 +0100

    Fix for #46
    - Addition of an IO plugin for the c3t3 meshes. The function try_load_other_binary_format() has been
      commented because it generates a compilation error that I couldn't fix.

--- Polyhedron/demo/Polyhedron/C3t3_type.h
+++ Polyhedron/demo/Polyhedron/C3t3_type.h
@@ -65,6 +65,8 @@ typedef CGAL::Polyhedron_demo_labeled_mesh_domain_3<Function_domain> Function_me
 #endif
 typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
 
+typedef CGAL::Robust_weighted_circumcenter_filtered_traits_3<Kernel> Geom_traits;
+
 // 3D complex
 
 #endif // CGAL_DEMO_MESH_3_C3T3_TYPE_H
