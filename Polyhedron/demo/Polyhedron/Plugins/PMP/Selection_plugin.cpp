9e2f611feeed3bd8a0520cce345e19a5592074df
Maxime Gimeno <maxime.gimeno@gmail.com>
2015-11-23 16:49:34 +0100

    Segfault and warning fix
    - The segfault was caused by the absence of a call to unselect_all() after the creation of the point_set from
      a selection.

--- Polyhedron/demo/Polyhedron/Plugins/PMP/Selection_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/PMP/Selection_plugin.cpp
@@ -117,7 +117,7 @@ public Q_SLOTS:
       int item_id = scene->addItem(new_item);
       QObject* scene_ptr = dynamic_cast<QObject*>(scene);
       if (scene_ptr)
-        connect(new_item,SIGNAL(simplicesSelected(Scene_item*)), scene_ptr, SLOT(setSelectedItem(Scene_item*)));
+        connect(new_item,SIGNAL(simplicesSelected(CGAL::Three::Scene_item*)), scene_ptr, SLOT(setSelectedItem(CGAL::Three::Scene_item*)));
       scene->setSelectedItem(item_id);
     }
   }
@@ -215,7 +215,7 @@ public Q_SLOTS:
        begin != selection_item->selected_vertices.end(); ++begin) {
        point_item->point_set()->push_back((*begin)->point());
     }
-    
+    point_item->point_set()->unselect_all();
     scene->setSelectedItem( scene->addItem(point_item) );
     scene->itemChanged(point_item);
   }
