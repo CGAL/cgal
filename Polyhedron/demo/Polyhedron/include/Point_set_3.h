613f61f8f4277fd783bdf8e9e29dc67323f0f8dc
Simon Giraudot <simon.giraudot@geometryfactory.com>
2016-01-15 15:01:50 +0100

    WIP: rewrite Point_set_3 to use property maps of Surface_mesh

--- Polyhedron/demo/Polyhedron/include/Point_set_3.h
+++ Polyhedron/demo/Polyhedron/include/Point_set_3.h
@@ -7,7 +7,8 @@
 #include <CGAL/Min_sphere_of_spheres_d.h>
 #include <CGAL/Min_sphere_of_points_d_traits_3.h>
 #include <CGAL/Min_sphere_of_spheres_d_traits_3.h>
-#include <UI_point_3.h>
+#include <CGAL/Surface_mesh.h>
+
 #include <algorithm>
 #include <vector>
 # include <CGAL/gl.h>
@@ -31,292 +32,82 @@
 /// @param Gt       Geometric traits class.
 
 template <class Gt>
-class Point_set_3 : public std::vector<UI_point_3<Gt> >
+class Point_set_3
 {
-// Private types
+public:
+  
+  typedef typename Gt::FT FT;
+  typedef typename Gt::Point_3 Point;
+  typedef typename Gt::Vector_3 Vector;
+  typedef CGAL::cpp11::array<FT, 3> Color;
+  
+  typedef CGAL::Surface_mesh<Point> Base;
+  typedef typename Base::Vertex_index Index;
+  typedef typename Base::template Property_map<Index, Vector> Vector_pmap;
+  typedef typename Base::template Property_map<Index, Color> Color_pmap;
+  
 private:
-
-  // Base class
-  typedef std::vector<UI_point_3<Gt> > Base;
-
-// Public types
+  Base m_base;
+  Vector_pmap m_normals;
+  Color_pmap m_colors;
+  
 public:
 
-  // Repeat base class' types
-  /// @cond SKIP_IN_MANUAL
-  typedef typename Base::iterator iterator;
-  typedef typename Base::const_iterator const_iterator;
+  Point_set_3 () : m_base() { }
 
-  using Base::erase;
-
-  /// @endcond
-
-  // Classic CGAL geometric types
-  typedef Gt  Geom_traits; ///< Geometric traits class.
-  typedef typename Geom_traits::FT FT;
-  typedef typename Geom_traits::Point_3 Point;  ///< typedef to Geom_traits::Point_3
-  typedef typename Geom_traits::Vector_3 Vector; ///< typedef to Geom_traits::Vector_3
-  typedef typename Geom_traits::Iso_cuboid_3 Iso_cuboid;
-  typedef typename Geom_traits::Sphere_3 Sphere;
-
-  /// Type of points in Point_set_3
-  typedef UI_point_3<Gt> UI_point; ///< Position + normal + selection flag
-  // Its superclass:
-  typedef typename UI_point::Point_with_normal Point_with_normal; ///< Position + normal
-
-// Data members
-private:
-
-  // Indicate if m_barycenter, m_bounding_box, m_bounding_sphere and
-  // m_diameter_standard_deviation below are valid.
-  mutable bool m_bounding_box_is_valid;
-
-  mutable Iso_cuboid m_bounding_box; // point set's bounding box
-  mutable Sphere m_bounding_sphere; // point set's bounding sphere
-  mutable Point m_barycenter; // point set's barycenter
-  mutable FT m_diameter_standard_deviation; // point set's standard deviation
-
-  std::size_t m_nb_selected; // handle selection
-
-  bool m_radii_are_uptodate;
-
-  // Assignment operator not implemented and declared private to make
-  // sure nobody uses the default one without knowing it
-  Point_set_3& operator= (const Point_set_3&)
+  void push_back (const Point& p)
   {
-    return *this;
+    m_base.add_vertex (p);
   }
+
+  bool empty() const { return m_base.is_empty(); }
+  std::size_t size () const { return m_base.number_of_vertices(); }
+  Point& operator[] (std::size_t index) { return m_base.point (Index (index)); }
+  const Point& operator[] (std::size_t index) const { return (*this)[index]; }
 
   
-// Public methods
-public:
-
-  /// Default constructor.
-  Point_set_3()
+  bool has_normals() const
   {
-    m_nb_selected = 0;
-    m_bounding_box_is_valid = false;
-    m_radii_are_uptodate = false;
+    std::pair<Vector_pmap, bool> pm = m_base.template property_map<Index, Vector> ("normal");
+    return pm.second;
   }
-
-  // copy constructor 
-  Point_set_3 (const Point_set_3& p) : Base (p)
+  bool add_normal_property()
   {
-    m_bounding_box_is_valid = p.m_bounding_box_is_valid;
-    m_bounding_box = p.m_bounding_box;
-    m_barycenter = p.m_barycenter;
-    m_diameter_standard_deviation = p.m_diameter_standard_deviation;
-
-    m_nb_selected = p.nb_selected_points ();
-    
-    m_radii_are_uptodate = p.m_radii_are_uptodate;
+    bool out = false;
+    boost::tie (m_normals, out) = m_base.template add_property_map<Index, Vector> ("normal");
+    return out;
   }
-
-  // Repeat base class' public methods used below
-  /// @cond SKIP_IN_MANUAL
-  using Base::begin;
-  using Base::end;
-  using Base::size;
-  /// @endcond
-
-  iterator first_selected() { return end() - m_nb_selected; }
-  const_iterator first_selected() const { return end () - m_nb_selected; }
-  void set_first_selected(iterator it)
+  void remove_normal_property()
   {
-    m_nb_selected = static_cast<std::size_t>(std::distance (it, end()));
+    m_base.remove_property_map (m_normals);
   }
+  Vector& normal (std::size_t index) { return m_normals[Index (index)]; }
+  const Vector& normal (std::size_t index) const { return this->normal(index); }
 
-  // Test if point is selected
-  bool is_selected(const_iterator it) const
+  bool has_colors() const
   {
-    return static_cast<std::size_t>(std::distance (it, end())) <= m_nb_selected;
+    std::pair<Color_pmap, bool> pm = m_base.template property_map<Index, Color> ("color");
+    return pm.second;
   }
-
-  /// Gets the number of selected points.
-  std::size_t nb_selected_points() const
+  bool add_color_property()
   {
-    return m_nb_selected;
+    bool out = false;
+    boost::tie (m_colors, out) = m_base.template add_property_map<Index, Color> ("color");
+    return out;
   }
-
-  /// Mark a point as selected/not selected.
-  void select(iterator it, bool selected = true)
+  void remove_color_property()
   {
-    bool currently = is_selected (it);
-    iterator first = first_selected();
-    if (currently && !selected)
-      {
-        std::swap (*it, *first);
-        -- m_nb_selected;
-      }
-    else if (!currently && selected)
-      {
-        std::swap (*it, *first);
-        ++ m_nb_selected;
-      }
+    m_base.remove_property_map (m_colors);
   }
+  Color& color (std::size_t index) { return m_colors[Index (index)]; }
+  const Color& color (std::size_t index) const { return this->color(index); }
 
-  void select_all()
-  {
-    m_nb_selected = size ();
-  }
-  void unselect_all()
-  {
-    m_nb_selected = 0;
-  }
+  
 
-
-  // Invert selection
-  void invert_selection()
-  {
-    iterator sel = end() - 1;
-    iterator unsel = begin();
-
-    iterator first = first_selected();
-
-    while (sel != first - 1 && unsel != first)
-      std::swap (*(sel --), *(unsel ++));
-    
-    m_nb_selected = size() - m_nb_selected;
-  }
-
-  /// Deletes selected points.
-  void delete_selection()
-  {
-    // Deletes selected points using erase-remove idiom
-    erase (first_selected(), end ());
-
-    // after erase(), use Scott Meyer's "swap trick" to trim excess capacity
-    Point_set_3(*this).swap(*this);
-    m_nb_selected = 0;
-    invalidate_bounds();
-  }
-
-  /// Gets the bounding box.
-  Iso_cuboid bounding_box() const
-  {
-    if (!m_bounding_box_is_valid)
-      update_bounds();
-
-    return m_bounding_box;
-  }
-
-  /// Gets bounding sphere.
-  Sphere bounding_sphere() const
-  {
-    if (!m_bounding_box_is_valid)
-      update_bounds();
-
-    return m_bounding_sphere;
-  }
-
-  /// Gets points barycenter.
-  Point barycenter() const
-  {
-    if (!m_bounding_box_is_valid)
-      update_bounds();
-
-    return m_barycenter;
-  }
-
-  /// Gets the standard deviation of the distance to barycenter.
-  FT diameter_standard_deviation() const
-  {
-    if (!m_bounding_box_is_valid)
-      update_bounds();
-
-    return m_diameter_standard_deviation;
-  }
-
-  // Gets the region of interest, ignoring the outliers.
-  // This method is used to define the OpenGL arcball sphere.
-  Sphere region_of_interest() const
-  {
-    if (!m_bounding_box_is_valid)
-      update_bounds();
-
-    // A good candidate is a sphere containing the dense region of the point cloud:
-    // - center point is barycenter
-    // - Radius is 2 * standard deviation
-    float radius = 2.f * (float)m_diameter_standard_deviation;
-    return Sphere(m_barycenter, radius*radius);
-  }
-
-  /// Update barycenter, bounding box, bounding sphere and standard deviation.
-  /// User is responsible to call invalidate_bounds() after adding, moving or removing points.
-  void invalidate_bounds()
-  {
-    m_bounding_box_is_valid = false;
-  }
-
-
+private:
 
 
   
-  bool are_radii_uptodate() const { return m_radii_are_uptodate; }
-  void set_radii_uptodate(bool /*on*/) { m_radii_are_uptodate = false; }
-
-// Private methods:
-private:
-
-  /// Recompute barycenter, bounding box, bounding sphere and standard deviation.
-  void update_bounds() const
-  {
-    if (begin() == end())
-      return;
-
-    // Update bounding box and barycenter.
-    // TODO: we should use the functions in PCA component instead.
-    FT xmin,xmax,ymin,ymax,zmin,zmax;
-    xmin = ymin = zmin =  1e38;
-    xmax = ymax = zmax = -1e38;
-    Vector v = CGAL::NULL_VECTOR;
-    FT norm = 0;
-    for (const_iterator it = begin(); it != end(); it++)
-    {
-      const Point& p = *it;
-
-      // update bbox
-      xmin = (std::min)(p.x(),xmin);
-      ymin = (std::min)(p.y(),ymin);
-      zmin = (std::min)(p.z(),zmin);
-      xmax = (std::max)(p.x(),xmax);
-      ymax = (std::max)(p.y(),ymax);
-      zmax = (std::max)(p.z(),zmax);
-
-      // update barycenter
-      v = v + (p - CGAL::ORIGIN);
-      norm += 1;
-    }
-    //
-    Point p(xmin,ymin,zmin);
-    Point q(xmax,ymax,zmax);
-    m_bounding_box = Iso_cuboid(p,q);
-    //
-    m_barycenter = CGAL::ORIGIN + v / norm;
-
-    // Computes bounding sphere
-    typedef CGAL::Min_sphere_of_points_d_traits_3<Gt,FT> Traits;
-    typedef CGAL::Min_sphere_of_spheres_d<Traits> Min_sphere;
-
-    Min_sphere ms(begin(),end());
-
-    typename Min_sphere::Cartesian_const_iterator coord = ms.center_cartesian_begin();
-    FT cx = *coord++;
-    FT cy = *coord++;
-    FT cz = *coord++;
-    m_bounding_sphere = Sphere(Point(cx,cy,cz), ms.radius()*ms.radius());
-
-    // Computes standard deviation of the distance to barycenter
-    typename Geom_traits::Compute_squared_distance_3 sqd;
-    FT sq_radius = 0;
-    for (const_iterator it = begin(); it != end(); it++)
-        sq_radius += sqd(*it, m_barycenter);
-    sq_radius /= FT(size());
-    m_diameter_standard_deviation = CGAL::sqrt(sq_radius);
-
-    m_bounding_box_is_valid = true;
-  }
-
 }; // end of class Point_set_3
 
 
