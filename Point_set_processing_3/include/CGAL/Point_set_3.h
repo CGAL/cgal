b5bbafb2f4a7665919de94ab42e8580ba8eae2a1
Simon Giraudot <simon.giraudot@geometryfactory.com>
2016-01-22 15:43:52 +0100

    Add index property map + functors for back insertion and point/normal push pmap

--- Point_set_processing_3/include/CGAL/Point_set_3.h
+++ Point_set_processing_3/include/CGAL/Point_set_3.h
@@ -55,27 +55,115 @@ template <class Gt>
 class Point_set_3
 {
 public:
-  
+
+  typedef Point_set_3<Gt> Point_set;
   typedef typename Gt::FT FT;
   typedef typename Gt::Point_3 Point;
   typedef typename Gt::Vector_3 Vector;
   typedef CGAL::cpp11::array<FT, 3> Color;
   typedef typename Gt::Iso_cuboid_3 Iso_cuboid;
   typedef typename Gt::Sphere_3 Sphere;
-
   
   typedef CGAL::Surface_mesh<Point> Base;
-  typedef typename Base::Vertex_index Index;
-  typedef typename Base::template Property_map<Index, Point> Point_pmap;
-  typedef typename Base::template Property_map<Index, Vector> Vector_pmap;
-  typedef typename Base::template Property_map<Index, Color> Color_pmap;
+  typedef typename Base::Vertex_index Item;
+
+  typedef typename Base::template Property_map<Item, std::size_t> Index_pmap;
+  typedef typename Base::template Property_map<Item, Point> Point_pmap;
+  typedef typename Base::template Property_map<Item, Vector> Vector_pmap;
+  typedef typename Base::template Property_map<Item, Color> Color_pmap;
 
   typedef typename Base::Vertex_range::iterator iterator;
   typedef typename Base::Vertex_range::const_iterator const_iterator;
   
 private:
+
+  struct Index_back_inserter {
+
+    typedef std::output_iterator_tag iterator_category;
+    typedef std::size_t              value_type;
+    typedef std::ptrdiff_t           difference_type;
+    typedef void                     pointer;
+    typedef void                     reference;
+
+  private:
+
+    Point_set& ps;
+    std::size_t ind;
   
+  public:
+  
+    Index_back_inserter(Point_set& ps, std::size_t ind=0)
+      : ps(ps), ind(ind)
+    {}
+
+    Index_back_inserter& operator++()
+    {
+      return *this;
+    }
+    Index_back_inserter& operator++(int)
+    {
+      return *this;
+    }
+  
+    Index_back_inserter& operator= (std::size_t& ind)
+    {
+      if(! ps.surface_mesh().has_valid_index(ind)){
+        ps.surface_mesh().add_vertex();
+      }
+      put(ps.indices(), Point_set::Item(ind),ind);
+      ++ ind;
+      return *this;
+    }
+                                  
+    Index_back_inserter& operator*()
+    {
+      return *this;
+    }
+
+  };
+
+  struct  Point_push_pmap {
+    Point_set& ps;
+    std::size_t ind;
+
+    Point_push_pmap(Point_set& ps, std::size_t ind=0)
+      : ps(ps), ind(ind)
+    {}
+
+    inline friend void put(Point_push_pmap& pm, std::size_t& i, Point& p)
+    {
+      if(! pm.ps.surface_mesh().has_valid_index(pm.ind)){
+        pm.ps.surface_mesh().add_vertex();
+      }
+      put(pm.ps.points(), Point_set::Item(pm.ind),p);
+      i = pm.ind;
+      ++pm.ind;
+    }
+  };
+
+  struct  Normal_push_pmap {
+    Point_set& ps;
+    std::size_t ind;
+
+    Normal_push_pmap(Point_set& ps, std::size_t ind=0)
+      : ps(ps), ind(ind)
+    {}
+
+    inline friend void put(Normal_push_pmap& pm, std::size_t& i  , Vector& v)
+    {
+      if(! pm.ps.surface_mesh().has_valid_index(pm.ind)){
+        pm.ps.surface_mesh().add_vertex();
+      }
+      put(pm.ps.normals(), Point_set::Item(pm.ind),v);
+      i = pm.ind;
+      ++pm.ind;
+    }
+  };
+
+
+
   Base m_base;
+  Index_pmap m_indices;
   Vector_pmap m_normals;
   Color_pmap m_colors;
 
@@ -94,20 +182,30 @@ public:
   Point_set_3 () : m_base()
   {
     m_nb_selected = 0;
+    assert (add_index_property());
   }
 
   // copy constructor 
   Point_set_3 (const Point_set_3& p) : m_base(p.m_base)
   {
     m_nb_selected = p.nb_selected_points ();
+    assert (add_index_property());
   }
 
+  void push_back (const Point& p)
+  {
+    m_base.add_vertex (p);
+  }
 
   Base& surface_mesh()
   {
     return m_base;
   }
 
+  Index_pmap& indices()
+  {
+    return m_indices;
+  }
 
   Point_pmap points()
   {
@@ -115,27 +213,11 @@ public:
   }
 
 
-  Vector_pmap normals()
+  Vector_pmap& normals()
   {
     return m_normals;
   }
 
-
-  void push_back (const Point& p)
-  {
-    m_base.add_vertex (p);
-  }
-
-  void push_back (const Point& p, const Vector& n)
-  {
-    Index i = m_base.add_vertex (p);
-    if (!has_normals())
-      {
-        std::cerr << "Warning: trying to affect normal to point set without normal attributes" << std::endl;
-        return;
-      }
-    normal(i) = n;
-  }
 
   iterator begin() { return m_base.vertices().begin(); }
   iterator end() { return m_base.vertices().end(); }
@@ -144,9 +226,23 @@ public:
   bool empty() const { return m_base.is_empty(); }
   std::size_t size () const { return m_base.number_of_vertices(); }
   void clear() { m_base.clear(); }
-  Point& operator[] (std::size_t index) { return m_base.point (Index (index)); }
+  Point& operator[] (std::size_t index) { return m_base.point (Item (m_indices[Item(index)])); }
   const Point& operator[] (std::size_t index) const { return (*this)[index]; }
+  std::size_t index (std::size_t i) { return m_indices[Item(i)]; }
 
+  Index_back_inserter index_back_inserter ()
+  {
+    return Index_back_inserter (*this);
+  }
+  Point_push_pmap point_push_pmap ()
+  {
+    return Point_push_pmap (*this);
+  }
+  Normal_push_pmap normal_push_pmap ()
+  {
+    return Normal_push_pmap (*this);
+  }
+    
   iterator first_selected() { return end() - m_nb_selected; }
   const_iterator first_selected() const { return end() - m_nb_selected; }
   void set_first_selected(iterator it)
@@ -211,45 +307,52 @@ public:
   {
     // Deletes selected points using erase-remove idiom
     for (std::size_t i = size() - 1; i != 0; -- i)
-      m_base.remove_vertex (Index (i));
+      m_base.remove_vertex (Item (i));
 
     m_nb_selected = 0;
   }
 
+  bool add_index_property()
+  {
+    bool out = false;
+    boost::tie (m_indices, out) = m_base.template add_property_map<Item, std::size_t> ("index");
+    return out;
+  }
+
   bool has_normals() const
   {
-    std::pair<Vector_pmap, bool> pm = m_base.template property_map<Index, Vector> ("normal");
+    std::pair<Vector_pmap, bool> pm = m_base.template property_map<Item, Vector> ("normal");
     return pm.second;
   }
   bool add_normal_property()
   {
     bool out = false;
-    boost::tie (m_normals, out) = m_base.template add_property_map<Index, Vector> ("normal");
+    boost::tie (m_normals, out) = m_base.template add_property_map<Item, Vector> ("normal");
     return out;
   }
   void remove_normal_property()
   {
     m_base.remove_property_map (m_normals);
   }
-  Vector& normal (std::size_t index) { return m_normals[Index (index)]; }
+  Vector& normal (std::size_t index) { return m_normals[Item (m_indices[Item(index)])]; }
   const Vector& normal (std::size_t index) const { return this->normal(index); }
 
   bool has_colors() const
   {
-    std::pair<Color_pmap, bool> pm = m_base.template property_map<Index, Color> ("color");
+    std::pair<Color_pmap, bool> pm = m_base.template property_map<Item, Color> ("color");
     return pm.second;
   }
   bool add_color_property()
   {
     bool out = false;
-    boost::tie (m_colors, out) = m_base.template add_property_map<Index, Color> ("color");
+    boost::tie (m_colors, out) = m_base.template add_property_map<Item, Color> ("color");
     return out;
   }
   void remove_color_property()
   {
     m_base.remove_property_map (m_colors);
   }
-  Color& color (std::size_t index) { return m_colors[Index (index)]; }
+  Color& color (std::size_t index) { return m_colors[Item (index)]; }
   const Color& color (std::size_t index) const { return this->color(index); }
 
   static CGAL::Second_of_pair_property_map<std::pair<Point, Vector> >
@@ -262,6 +365,11 @@ public:
   {
     return CGAL::make_first_of_pair_property_map (std::pair<Point, Vector>());
   }
+
+private:
+
+
+  
   
   
 }; // end of class Point_set_3
