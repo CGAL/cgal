4208d3a1aa3c65872c66351d847247f3bdeae3f8
Philipp Möller <bootsarehax@gmail.com>
2016-01-22 16:01:27 +0100

    Remove license headers from test cases

--- BGL/test/BGL/graph_concept_Triangulation_2.cpp
+++ BGL/test/BGL/graph_concept_Triangulation_2.cpp
@@ -1,23 +1,3 @@
-// Copyright (c) 2012 GeometryFactory (France). All rights reserved.
-// All rights reserved.
-//
-// Licensees holding a valid commercial license may use this file in
-// accordance with the commercial license agreement provided with the software.
-//
-// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; version 2.1 of the License.
-// See the file LICENSE.LGPL distributed with CGAL.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-//
-// $URL$
-// $Id$
-// 
-//
-// Author(s)     : Philipp Möller
-
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/boost/graph/graph_traits_Triangulation_2.h>
 
