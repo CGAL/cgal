201e23a503a3193c711b2b54ab88a9764bb11280
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-02-01 17:16:45 +0100

    include graph_traits file

--- Polyhedron/demo/Polyhedron/Scene_textured_polyhedron_item.cpp
+++ Polyhedron/demo/Polyhedron/Scene_textured_polyhedron_item.cpp
@@ -1,6 +1,7 @@
 #include "Scene_textured_polyhedron_item.h"
 #include "Textured_polyhedron_type.h"
 #include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
 #include <CGAL/Polygon_mesh_processing/compute_normal.h>
 
 #include <QObject>
