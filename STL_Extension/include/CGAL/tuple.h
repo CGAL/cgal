c20bbba168a4c5a4a8f44f1936124e4f0018b0ea
Simon Giraudot <simon.giraudot@geometryfactory.com>
2016-02-04 18:24:18 +0100

    Work-around C++0x tuple bug with GCC4.4

--- STL_Extension/include/CGAL/tuple.h
+++ STL_Extension/include/CGAL/tuple.h
@@ -32,6 +32,10 @@
   #if defined( _LIBCPP_VERSION ) // check if libc++ is used
     #define CGAL_CFG_NO_CPP0X_TUPLE
   #endif
+  #if defined(__GNUC__) && defined(__GNUC_MINOR__) && (__GNUC__ <= 4) && (__GNUC_MINOR__ <= 4)
+    #define CGAL_CFG_NO_CPP0X_TUPLE
+    #define CGAL_CFG_NO_CPP0X_VARIADIC_TEMPLATES
+  #endif
 #endif
 
 #ifndef CGAL_CFG_NO_CPP0X_TUPLE
