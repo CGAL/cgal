478edc9f3e0a00471695d0f64a9c4a36373acc51
Sébastien Loriot <sebastien.loriot@cgal.org>
2016-02-17 10:27:43 +0100

    move function in correct header

--- Straight_skeleton_2/doc/Straight_skeleton_2/CGAL/create_straight_skeleton_2.h
+++ Straight_skeleton_2/doc/Straight_skeleton_2/CGAL/create_straight_skeleton_2.h
@@ -65,41 +65,6 @@ create_exterior_straight_skeleton_2 ( FT max_offset
 , K const& k = Exact_predicates_inexact_constructions_kernel
 ) ;
 
-} /* namespace CGAL */
-
-namespace CGAL {
-
-/*!
-\ingroup PkgStraightSkeleton2Functions
-
-The function `create_offset_polygons_2()` creates a straight skeleton in the interior of a 2D polygon with holes. 
-The function returns a container with all the offset polygons at distance `offset` obtained from 
-the straight skeleton `s`. 
-
-
-<OL> 
-<LI>`K` is any \cgal kernel. 
-<LI>`FT` is any number type implicitly convertible to `K::FT`. 
-<LI>`StraightSkeleton` is `Straight_skeleton_2<K2>`. 
-If `K != K2` the straight skeleton is converted to `Straight_skeleton_2<K>`. 
-<LI>`Polygon` is a model of `VertexContainer_2`. 
-If this first template parameter is omitted, `Polygon_2` is used. 
-</OL> 
-
-\sa `create_interior_straight_skeleton_2()`
-\sa `create_exterior_straight_skeleton_2()`
-\sa `create_interior_skeleton_and_offset_polygons_2()`
-\sa `create_exterior_skeleton_and_offset_polygons_2()`
-
-
-*/
-template<class Polygon, class FT, class StraightSkeleton, class K>
-std::vector< boost::shared_ptr<Polygon> >
-create_offset_polygons_2 ( FT offset
-, StraightSkeleton const& s,
-, K const& k = Exact_predicates_inexact_constructions_kernel
-) ;
-
 /*!
 \ingroup PkgStraightSkeleton2Functions
 
