0bcad0e66fd10b7cafadbbccf64e93b86c4df344
Efi Fogel <efifogel@gmail.com>
2016-02-24 18:45:47 +0200

    Fixed typo

--- Minkowski_sum_2/doc/Minkowski_sum_2/CGAL/Polygon_nop_decomposition_2.h
+++ Minkowski_sum_2/doc/Minkowski_sum_2/CGAL/Polygon_nop_decomposition_2.h
@@ -6,7 +6,7 @@ namespace CGAL {
 \anchor mink_refnop_decomp
 
 The `Polygon_nop_decomposition_2` class implements a convex
-decompistion of a polygon, which merely passes the input polygon into
+decompistion of a polygon, which merely passes the input polygon to
 the list of output convex polygons. It should be used when it is known
 that the input polygon is convex to start with.
 
