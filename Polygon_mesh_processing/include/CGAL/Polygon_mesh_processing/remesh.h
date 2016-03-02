c0f30e18a882962dc62dcbabfe49edca79d6c897
Jane Tournois <jane.tournois@geometryfactory.com>
2016-03-02 17:00:39 +0100

    add constrained vertices to isotropic remeshing
    those vertices cannot be modified

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/remesh.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/remesh.h
@@ -234,6 +234,8 @@ void split_long_edges(const EdgeRange& edges
   typedef PolygonMesh PM;
   typedef typename boost::graph_traits<PM>::edge_descriptor edge_descriptor;
   typedef typename boost::graph_traits<PM>::vertex_descriptor vertex_descriptor;
+  typedef typename boost::graph_traits<PM>::edge_descriptor edge_descriptor;
+  typedef typename boost::graph_traits<PM>::vertex_descriptor vertex_descriptor;
   using boost::choose_pmap;
   using boost::choose_param;
   using boost::get_param;
