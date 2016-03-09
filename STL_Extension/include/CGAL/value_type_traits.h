d12e511097201788489b13b0a04b0295b5f5850b
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-03-09 17:04:07 +0100

    Change license to LGPL for files that were moved from point set processing to STL_extension and Property_map

--- STL_Extension/include/CGAL/value_type_traits.h
+++ STL_Extension/include/CGAL/value_type_traits.h
@@ -1,10 +1,11 @@
 // Copyright (c) 2007-09  INRIA Sophia-Antipolis (France).
 // All rights reserved.
 //
-// This file is part of CGAL (www.cgal.org).
-// You can redistribute it and/or modify it under the terms of the GNU
-// General Public License as published by the Free Software Foundation,
-// either version 3 of the License, or (at your option) any later version.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
 //
 // Licensees holding a valid commercial license may use this file in
 // accordance with the commercial license agreement provided with the software.
@@ -24,7 +25,7 @@
 
 namespace CGAL {
 
-/// \ingroup PkgPointSetProcessing
+/// \ingroup  PkgStlExtension
 /// Class providing the value type of an iterator, and
 /// in the case of an output iterator, a type of objects that can be put in it.
 ///
