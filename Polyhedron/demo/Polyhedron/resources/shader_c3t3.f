c52758d5906aca25210c691e5b92f6d797f88873
Laurent Rineau <laurent.rineau@cgal.org>
2016-03-29 18:38:20 +0200

    Improve the draw of the intersection
    I have tweaked the vertex and fragment shaders, to avoid the curious
    color interpolation for triangles of the intersection with the cutting
    plane.
    
    The component `color.w` is set to the signed distance to the cutting
    plane (no longer any conditional in the vertex shader).
    
    Then in the fragment shader, the drawing color is composed as such:
    
        if(color.w<0)
        {
          vec4 my_color = vec4(color.xzy, 1.);
          // [...]
        }
        else
          discard;

--- Polyhedron/demo/Polyhedron/resources/shader_c3t3.f
+++ Polyhedron/demo/Polyhedron/resources/shader_c3t3.f
@@ -10,8 +10,9 @@ uniform highp float spec_power ;
 uniform int is_two_side; 
 uniform bool is_selected;
 void main(void) {
- if(color.w>0)
+ if(color.w<0)
  {
+    vec4 my_color = vec4(color.xzy, 1.);
     highp vec3 L = light_pos.xyz - fP.xyz; 
     highp vec3 V = -fP.xyz; 
     highp vec3 N; 
@@ -24,11 +25,11 @@ void main(void) {
     highp vec3 R = reflect(-L, N); 
     vec4 diffuse; 
     if(is_two_side == 1) 
-        diffuse = abs(dot(N,L)) * light_diff * color; 
+        diffuse = abs(dot(N,L)) * light_diff * my_color; 
     else 
-        diffuse = max(dot(N,L), 0.0) * light_diff * color; 
+        diffuse = max(dot(N,L), 0.0) * light_diff * my_color; 
     highp vec4 specular = pow(max(dot(R,V), 0.0), spec_power) * light_spec; 
-    vec4 ret_color = vec4((color*light_amb).xyz + diffuse.xyz + specular.xyz,1); 
+    vec4 ret_color = vec4((my_color*light_amb).xyz + diffuse.xyz + specular.xyz,1); 
     if(is_selected)
         gl_FragColor = vec4(ret_color.r+70.0/255.0, ret_color.g+70.0/255.0, ret_color.b+70.0/255.0, 1.0);
     else
