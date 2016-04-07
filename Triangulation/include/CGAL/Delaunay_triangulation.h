ab9c7cf51f8f4ca254c339982178aa0136c91d6e
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-04-07 16:35:43 +0200

    Replace static with CGAL_STATIC_THREAD_LOCAL_VARIABLE

--- Triangulation/include/CGAL/Delaunay_triangulation.h
+++ Triangulation/include/CGAL/Delaunay_triangulation.h
@@ -20,6 +20,7 @@
 #ifndef CGAL_DELAUNAY_COMPLEX_H
 #define CGAL_DELAUNAY_COMPLEX_H
 
+#include <CGAL/tss.h>
 #include <CGAL/Triangulation.h>
 #include <CGAL/Dimension.h>
 #include <CGAL/Default.h>
@@ -779,10 +780,11 @@ typename Delaunay_triangulation<DCTraits, TDS>::Vertex_handle
 Delaunay_triangulation<DCTraits, TDS>
 ::insert_in_conflicting_cell(const Point & p, const Full_cell_handle s)
 {
+    // for storing conflicting full_cells.
     typedef std::vector<Full_cell_handle> Full_cell_h_vector;
-    static Full_cell_h_vector cs; // for storing conflicting full_cells.
+    CGAL_STATIC_THREAD_LOCAL_VARIABLE(Full_cell_h_vector,cs,0);
     cs.clear();
-    // cs.reserve(64);
+
     std::back_insert_iterator<Full_cell_h_vector> out(cs);
     Facet ft = compute_conflict_zone(p, s, out);
     return insert_in_hole(p, cs.begin(), cs.end(), ft);
