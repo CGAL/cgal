537bdc00deb26a4547d54a2282033afe4784e567
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-04-11 15:40:02 +0200

    Reintroduce Regular_triangulation_filtered_traits_2

---
+++ Triangulation_2/include/CGAL/Regular_triangulation_filtered_traits_2.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2004   INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// 
+//
+// Author(s)     : Sylvain Pion
+
+// kept for backward compatibility
