7f6550a48d3291a4056f254b5558813cf18edab8
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-04-12 17:38:25 +0200

    Remove the xy_3 traits classes; Deprecate Triangulation_euclidean_traits_2

--- Triangulation_2/doc/Triangulation_2/Concepts/DelaunayTriangulationTraits_2.h
+++ Triangulation_2/doc/Triangulation_2/Concepts/DelaunayTriangulationTraits_2.h
@@ -23,7 +23,6 @@ required if the  method `nearest_vertex()` is used.
 
 
 \cgalHasModel \cgal kernels 
-\cgalHasModel `CGAL::Triangulation_euclidean_traits_2<Rep>`. 
 \cgalHasModel `CGAL::Projection_traits_xy_3<K>` (not for dual Voronoi functions)
 \cgalHasModel `CGAL::Projection_traits_yz_3<K>` (not for dual Voronoi functions)
 \cgalHasModel `CGAL::Projection_traits_xz_3<K>` (not for dual Voronoi functions)
