ca284090b28f8243166e27ab4675bf9dde24f823
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-04-18 09:36:16 +0200

    move the using ::put to the put that VC++ has problems with

--- BGL/include/CGAL/boost/graph/properties_Polyhedron_3.h
+++ BGL/include/CGAL/boost/graph/properties_Polyhedron_3.h
@@ -150,12 +150,6 @@ struct Polyhedron_property_map {};
 
 namespace CGAL {
 
-  // Add this as VC did find the "generalized put" 
-  // when a bare pointer is used in jet_estimate_normals
-  using ::put;
-  using ::get;
-
-
 // generalized 2-ary get functions
 template<class Gt, class I, CGAL_HDS_PARAM_, class A, class PropertyTag>
 typename boost::property_map< CGAL::Polyhedron_3<Gt,I,HDS,A>, PropertyTag >::const_type
@@ -177,6 +171,13 @@ template<class Gt, class I, CGAL_HDS_PARAM_, class A, class PropertyTag, class K
 typename boost::property_traits< typename boost::property_map<CGAL::Polyhedron_3<Gt,I,HDS,A>, PropertyTag >::const_type >::reference
 get(PropertyTag p, CGAL::Polyhedron_3<Gt,I,HDS,A> const& g, const Key& key)
 { return get(get(p, g), key); }
+
+
+  
+// Add this as VC did find the "generalized put" below 
+// when a bare pointer is used in jet_estimate_normals
+using ::put;
+using ::get;
 
 // generalized put
 template<class Gt, class I, CGAL_HDS_PARAM_, class A, class PropertyTag, class Key,class Value>
