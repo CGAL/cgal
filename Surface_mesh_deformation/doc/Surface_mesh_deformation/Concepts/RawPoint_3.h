325fded10d7550a72811efb165e93522f6776c36
Sebastien Loriot <sloriot.ml@gmail.com>
2016-04-19 09:57:54 +0200

    Fix RawPoint_3 concept

--- Surface_mesh_deformation/doc/Surface_mesh_deformation/Concepts/RawPoint_3.h
+++ Surface_mesh_deformation/doc/Surface_mesh_deformation/Concepts/RawPoint_3.h
@@ -1,5 +1,6 @@
 /// \ingroup PkgSurfaceMeshDeformationConcepts
 /// \cgalConcept
+/// \cgalRefines `DefaultConstructible` and `Assignable`
 ///
 /// Concept describing the set of requirements of a simple point type.
 ///
@@ -8,14 +9,11 @@ class RawPoint_3
 public:
 /// \name Creation
 /// @{
-  RawPoint_3();
   RawPoint_3(double x, double y, double z);
 /// @}
 
 /// \name Coordinates Accessors
 /// @{
-  /// `i<=0 && i<3`
-  double& operator[](int i);
   /// `i<=0 && i<3`
   double  operator[](int i) const;
 /// @}
