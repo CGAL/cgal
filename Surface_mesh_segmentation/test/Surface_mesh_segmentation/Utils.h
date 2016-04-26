b92364132182e4df2a49ed3ef0d5297c79f1dae3
Maxime Gimeno <maxime.gimeno@gmail.com>
2016-04-26 11:04:38 +0200

    Clean-up

--- Surface_mesh_segmentation/test/Surface_mesh_segmentation/Utils.h
+++ Surface_mesh_segmentation/test/Surface_mesh_segmentation/Utils.h
@@ -5,15 +5,8 @@ template<class Polyhedron>
 bool read_to_polyhedron(const char* file_name, Polyhedron& mesh)
 {
   std::ifstream input(file_name);
-    
-  bool ok = true;
-  if(!input)
-   ok = false;
-  else if(!(input>>mesh))
-   ok = false;
-  else if(mesh.empty())
-   ok = false;
-  if ( !ok ){
+
+  if ( !input || !(input >> mesh) || mesh.empty() ){
     std::cerr << "Problem occured while reading off file";
     return false;
   }
