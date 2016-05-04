632c55adb971f163de685ba8a35dff0cba79ec0c
Simon Giraudot <simon.giraudot@geometryfactory.com>
2016-05-04 11:12:40 +0200

    Update test with new API and replace asserts by explicit tests

--- Point_set_processing_3/test/Point_set_processing_3/point_set_test.cpp
+++ Point_set_processing_3/test/Point_set_processing_3/point_set_test.cpp
@@ -1,4 +1,5 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
 #include <CGAL/Point_set_3.h>
 #include <CGAL/IO/read_xyz_points.h>
 #include <CGAL/IO/write_xyz_points.h>
@@ -16,13 +17,22 @@ typedef CGAL::Point_set_3<Kernel> Point_set;
 typedef CGAL::cpp11::array<unsigned char, 3> Color;
 
 
+void test (bool expr, const char* msg)
+{
+  static std::size_t nb_test = 0;
+  ++ nb_test;
+  if (!expr)
+    std::cerr << "Error on test " << nb_test << ": " << msg << std::endl;
+}
+
+
 int main (int, char**)
 {
   Point_set point_set;
 
-  assert (!(point_set.has_normals()));
+  test (!(point_set.has_normals()), "point set shouldn't have normals.");
   point_set.add_normal_property();
-  assert (point_set.has_normals());
+  test (point_set.has_normals(), "point set should have normals.");
 
   std::vector<Point_set::Item> indices;
   std::ifstream f ("data/oni.pwn");
@@ -34,17 +44,22 @@ int main (int, char**)
   f.close ();
 
   Point_set::iterator
-    first_selected = CGAL::grid_simplify_point_set (point_set.begin (), point_set.end (),
-                                                    &(point_set[0]),
-                                                    0.1);
+    first_to_remove = CGAL::grid_simplify_point_set (point_set.begin(),
+                                                     point_set.end(),
+                                                     &(point_set[0]),
+                                                     0.1);
 
-  assert (!(point_set.are_indices_up_to_date()));
-  point_set.erase (first_selected, point_set.end());
-  assert (point_set.are_indices_up_to_date());
-
-  assert (!(point_set.has_property<Color> ("color")));
+  std::size_t size = point_set.size ();
+  point_set.remove_from (first_to_remove);
+  test ((point_set.size() + point_set.removed_size() == size), "sizes before and after removal do not match.");
+  
+  test (point_set.has_garbage(), "point set should have garbage.");
+  point_set.collect_garbage();
+  test (!(point_set.has_garbage()), "point set shouldn't have garbage.");
+  
+  test (!(point_set.has_property<Color> ("color")), "point set shouldn't have colors.");
   point_set.add_property<Color> ("color");
-  assert (point_set.has_property<Color> ("color"));
+  test (point_set.has_property<Color> ("color"), "point set should have colors.");
 
 
   for (std::size_t i = 0; i < point_set.size(); ++ i)
@@ -53,15 +68,15 @@ int main (int, char**)
                    static_cast<unsigned char>(rand() % 255),
                    static_cast<unsigned char>(rand() % 255) }};
       point_set.property<Color> ("color", i) = c;
-      assert (point_set.property<Color> ("color", i) == c);
+      test ((point_set.property<Color> ("color", i) == c), "recovered color is incorrect.");
     }
   
   point_set.remove_normal_property ();
-  assert (!(point_set.has_normals()));
+  test (!(point_set.has_normals()), "point set shouldn't have normals.");
   
-  assert (point_set.has_property<Color> ("color"));
+  test (point_set.has_property<Color> ("color"), "point set should have colors.");
   point_set.remove_property<Color> ("color");
-  assert (!(point_set.has_property<Color> ("color")));
+  test (!(point_set.has_property<Color> ("color")), "point set shouldn't have colors.");
  
   return 0;
 }
