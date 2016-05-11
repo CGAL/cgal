a473a4ead525ce40e78d3f6ff8d68e7508f3b43d
Simon Giraudot <simon.giraudot@geometryfactory.com>
2016-05-11 15:34:04 +0200

    Color segmentation attribute with user-selected color

--- Data_classification/include/CGAL/Data_classification/Segmentation_attribute_color.h
+++ Data_classification/include/CGAL/Data_classification/Segmentation_attribute_color.h
@@ -16,6 +16,7 @@ class Segmentation_attribute_color : public Segmentation_attribute
   typedef typename Data_classification::HSV_Color HSV_Color;
   
   std::vector<double> color_attribute;
+  double mean_h, mean_s, mean_v, sd_h, sd_s, sd_v;
   
 public:
   double weight;
@@ -23,16 +24,25 @@ public:
   double max;
 
   Segmentation_attribute_color (PSC& M,
-                                double weight) : weight (weight)
+                                double weight,
+                                double mean_h = 156., double mean_s = 5., double mean_v = 76.,
+                                double sd_h = 70., double sd_s = 12., double sd_v = 8.4)
+    : mean_h (mean_h)
+    , mean_s (mean_s)
+    , mean_v (mean_v)
+    , sd_h (sd_h)
+    , sd_s (sd_s)
+    , sd_v (sd_v)
+    , weight (weight)
   {
 
-    std::cerr << "Using colors" << std::endl;
+    std::cerr << "Using colors " << mean_h << " " << mean_s << " " << mean_v << std::endl;
     for(std::size_t i=0; i < M.HPS.size();i++)
       {
         HSV_Color c = Data_classification::rgb_to_hsv (M.HPS[i].color);
-        color_attribute.push_back (std::exp (-(c[0] - 156.) * (c[0] - 156.) / (2. * 81. * 81.))
-                                   * std::exp (-(c[1] - 5.) * (c[1] - 5.) / (2. * 4. * 4.))
-                                   * std::exp (-(c[2] - 76.) * (c[2] - 76.) / (2. * 6.8 * 6.8)));
+        color_attribute.push_back (std::exp (-(c[0] - mean_h) * (c[0] - mean_h) / (2. * sd_h * sd_h))
+                                   * std::exp (-(c[1] - mean_s) * (c[1] - mean_s) / (2. * sd_s * sd_s))
+                                   * std::exp (-(c[2] - mean_v) * (c[2] - mean_v) / (2. * sd_v * sd_v)));
       }
     this->compute_mean_max (color_attribute, mean, max);
   }
