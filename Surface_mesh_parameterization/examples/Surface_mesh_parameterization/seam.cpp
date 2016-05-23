f0f6575c76f2b40a848578f9a96c47947af58844
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-05-23 14:29:08 +0200

    WIP on the doc

--- Surface_mesh_parameterization/examples/Surface_mesh_parameterization/seam.cpp
+++ Surface_mesh_parameterization/examples/Surface_mesh_parameterization/seam.cpp
@@ -1,93 +1,95 @@
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Surface_mesh.h>
+#include <CGAL/boost/graph/Seam_mesh.h>
 
-#include <string>
+#include <CGAL/parameterize.h>
+#include <boost/foreach.hpp>
 #include <iostream>
 #include <fstream>
-#include <boost/graph/dijkstra_shortest_paths.hpp>
-#include <boost/foreach.hpp>
 
-typedef CGAL::Simple_cartesian<double>                       Kernel;
-typedef Kernel::Point_3                                      Point;
-typedef CGAL::Surface_mesh<Point>                            Mesh;
+
+typedef CGAL::Simple_cartesian<double>      Kernel;
+typedef Kernel::Point_2                     Point_2;
+typedef Kernel::Point_3                     Point_3;
+typedef CGAL::Surface_mesh<Kernel::Point_3> SurfaceMesh;
+typedef CGAL::Seam_mesh<SurfaceMesh>        Mesh;
+
+typedef boost::graph_traits<SurfaceMesh>::edge_descriptor SM_edge_descriptor;
+typedef boost::graph_traits<SurfaceMesh>::halfedge_descriptor SM_halfedge_descriptor;
+typedef boost::graph_traits<SurfaceMesh>::vertex_descriptor SM_vertex_descriptor;
 
 typedef boost::graph_traits<Mesh>::vertex_descriptor vertex_descriptor;
+typedef boost::graph_traits<Mesh>::halfedge_descriptor halfedge_descriptor;
+typedef boost::graph_traits<Mesh>::face_descriptor face_descriptor;
 
 
-template <typename PolygonMesh>
-vertex_descriptor closest_vertex(const Point& p, const PolygonMesh& mesh)
-{  
-  typedef typename boost::property_map<PolygonMesh, CGAL::vertex_point_t>::const_type PPmap;
-  PPmap ppmap = get(CGAL::vertex_point, mesh);
-  vertex_descriptor res;
-  double dist = (std::numeric_limits<double>::max)();
-  //std::cerr <<dist << std::endl;
-  BOOST_FOREACH(vertex_descriptor vd, vertices(mesh)){
-    //std::cerr << "vd = " << vd << std::endl;
-    Point vp = get(ppmap,vd);
-    //std::cerr << "vp = " << vp << std::endl;
-    double d = CGAL::squared_distance(p,vp);
-    //std::cerr << "d = " << d << std::endl;
-    if(d < dist){
-      res = vd;
-      dist = d;
-      //std::cerr <<res << std::endl;
-      //std::cerr <<dist << std::endl;
-    } 
+SM_vertex_descriptor find(const Point_3& p, const SurfaceMesh& sm)
+{
+  BOOST_FOREACH(SM_vertex_descriptor vd, vertices(sm)){
+    if(squared_distance(p, sm.point(vd)) < 0.0001){
+      return vd;
+    }
   }
-  return res;
+  std::cerr << "epsilon is too small" << std::endl;
+  return SM_vertex_descriptor();
 }
 
 
-int main(int, char* argv[]) 
+int main(int argc, char * argv[])
 {
-  Mesh mesh;
-
-  std::string base(argv[1]);
-  unsigned found = base.find_last_of(".");
-  if(found !=  std::string::npos){
-    base = base.substr(0,found);
+  SurfaceMesh sm;
+  std::vector<SM_edge_descriptor> seam; 
+  {
+    std::ifstream in((argc>1)?argv[1]:"data/blob.off");
+    in >> sm;
   }
-  std::ifstream in(base + ".off");
-  std::ifstream pin(base + ".pts");
-  std::ofstream poly(base + ".poly");
-  std::ofstream seam(base + ".seam");
 
-  in >> mesh;
+  {
+    std::ifstream in((argc>2)?argv[2]:"data/blob.polylines.txt");
+    std::vector<Point_3> points;
+    int n;
+    in >> n;
+    Point_3 p;
+    in >> p;
+    SM_vertex_descriptor v = find(p,sm);
 
-  typedef boost::property_map<Mesh, CGAL::vertex_point_t>::const_type PPmap;
-  PPmap ppmap = get(CGAL::vertex_point, mesh);
-
-  Point sp, tp;
-
-  
-  while(pin >> sp >>  tp){
-    
-    vertex_descriptor sv, tv;
-    sv = closest_vertex(sp, mesh);
-    tv = closest_vertex(tp, mesh);
-    
-    
-    Mesh::Property_map<vertex_descriptor,vertex_descriptor> predecessor;
-    predecessor = mesh.add_property_map<vertex_descriptor,vertex_descriptor>("v:predecessor").first;
-    
-    boost::dijkstra_shortest_paths(mesh, sv, predecessor_map(predecessor));
-    
-    std::vector<Point> points;
-    
-    do {
-      seam << std::size_t(tv) << " " << std::size_t(predecessor[tv]) <<  std::endl;
-      points.push_back(get(ppmap,tv));
-      tv = predecessor[tv];
-    } while(predecessor[tv]!=tv);
-    
-    points.push_back(get(ppmap,sv));
-    
-    poly << points.size() << std::endl;
-    BOOST_FOREACH(Point p, points){
-      poly << p << std::endl;
+    for(int i=1; i < n; i++){
+      in >> p;
+      SM_vertex_descriptor w = find(p,sm);
+      seam.push_back(edge(v,w,sm).first);
+      v = w;
     }
   }
+
+  SM_halfedge_descriptor smhd = halfedge(seam.front(),sm);    
+  Mesh mesh(sm, seam);
+ 
+  // The 2D points of the uv parametrisation will be written into this map
+  // Note that this is a halfedge property map, and that the uv
+  // is only stored for the canonical halfedges representing a vertex  
+  SurfaceMesh::Property_map<SM_halfedge_descriptor,Point_2> uvpm;
+  bool created;
+  boost::tie(uvpm, created) = sm.add_property_map<SM_halfedge_descriptor,Point_2>("h:uv");
+  assert(created);
+ 
+  halfedge_descriptor bhd(smhd);
+
+  bhd = opposite(bhd,mesh); // a halfedge on the virtual border
+
+  CGAL::parameterize(mesh, bhd, uvpm);
+ 
+  BOOST_FOREACH(face_descriptor fd, faces(mesh)){  
+    halfedge_descriptor hd = halfedge(fd,mesh); 
   
+    std::cout << "4 " << uvpm[target(hd,mesh)].x() << " " << uvpm[target(hd,mesh)].y() << " 0 ";
+    
+    hd = next(hd,mesh);
+    BOOST_FOREACH(vertex_descriptor vd, vertices_around_face(hd,mesh)){
+      std::cout << uvpm[vd].x() << " " << uvpm[vd].y() << " 0 ";
+    }
+    std::cout << std::endl;
+  
+  }
+
   return 0;
 }
