39816543283f67292ee9ddd86958d45bde70809e
Sébastien Loriot <sebastien.loriot@cgal.org>
2016-05-24 11:32:20 +0200

    add data_set that is failing (intersection polyline inside a patch)

--- Operations_on_polyhedra/test/Operations_on_polyhedra/test_corefinement_on_polyhedra.cmd
+++ Operations_on_polyhedra/test/Operations_on_polyhedra/test_corefinement_on_polyhedra.cmd
@@ -19,6 +19,7 @@ data-coref/elephant_split_1.off data-coref/elephant_split_2.off
 data-coref/cube.off data-coref/cube_on_cube_edge.off
 data-coref/cube.off data-coref/cube_on_cube_corner.off
 data-coref/cow.off data-coref/cross.off
+data-coref/sphere.off data-coref/sphere_trans_cut.off
 data-coref/cube.off data-coref/coplanar_with_cube3.off
 data-coref/cube.off data-coref/coplanar_with_cube4.off
 data-coref/square1.off data-coref/square_pair.off
