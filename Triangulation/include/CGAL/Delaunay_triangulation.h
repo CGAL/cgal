ec16393b39dcd82c9b6fc474b4520b2dd9ce1ce2
Clement Jamin <clement.jamin@inria.fr>
2016-06-01 14:42:17 +0200

    Fix Linux compilation error

--- Triangulation/include/CGAL/Delaunay_triangulation.h
+++ Triangulation/include/CGAL/Delaunay_triangulation.h
@@ -881,8 +881,8 @@ Delaunay_triangulation<DCTraits, TDS>
   int dim = current_dimension();
   if (dim == maximal_dimension())
   {
-    for (Finite_full_cell_const_iterator cit = finite_full_cells_begin() ;
-         cit != finite_full_cells_end() ; ++cit )
+    for (Finite_full_cell_const_iterator cit = this->finite_full_cells_begin() ;
+         cit != this->finite_full_cells_end() ; ++cit )
     {
       Full_cell_const_handle ch = cit.base();
       for(int i = 0; i < dim+1 ; ++i )
