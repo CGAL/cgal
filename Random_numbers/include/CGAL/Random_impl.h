bb42e3a468f1320d6455b9ada4a3461c658a3147
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2016-06-03 09:06:34 +0200

    Readd the debug mechanism for default random, but only in non header only mode.

--- Random_numbers/include/CGAL/Random_impl.h
+++ Random_numbers/include/CGAL/Random_impl.h
@@ -46,15 +46,11 @@ Random( )
     std::time_t s;
     std::time( &s);
     seed = (unsigned int)s;
-#if defined( CGAL_TEST_SUITE ) || defined( CGAL_PRINT_SEED )
-    /* TODO Re-add this mechanism later
-       Test if this is the guilty code which generate __gnu_cxx::recursive_init_error
-       in header only mode.
-       
+#if (defined( CGAL_TEST_SUITE ) || defined( CGAL_PRINT_SEED )) && !defined(CGAL_HEADER_ONLY)
+    // In header only, the following line  generates __gnu_cxx::recursive_init_error
     if(this == & get_default_random()){
       std::cerr << "CGAL::get_default_random()::get_seed() = " << seed << std::endl;
     }
-    */
 #endif
     // initialize random numbers generator
     rng.seed(static_cast<boost::int32_t>(seed));
