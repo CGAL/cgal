37153ff9f1f7403bcf47802dba24457094d392de
Simon Giraudot <simon.giraudot@geometryfactory.com>
2016-07-08 15:13:33 +0200

    Update example with future API

--- Data_classification/examples/Data_classification/example_point_set_classification.cpp
+++ Data_classification/examples/Data_classification/example_point_set_classification.cpp
@@ -10,13 +10,10 @@
 typedef CGAL::Simple_cartesian<double> Kernel;
 typedef CGAL::Point_set_classification<Kernel> PSC;
 
-typedef CGAL::Scatter_segmentation_attribute<Kernel> Scatter;
-typedef CGAL::Elevation_segmentation_attribute<Kernel> Elevation;
-typedef CGAL::Distance_to_plane_segmentation_attribute<Kernel> Planarity;
-
-typedef CGAL::Vegetation_classification_type<Kernel> Vegetation;
-typedef CGAL::Ground_classification_type<Kernel> Ground;
-typedef CGAL::Roof_classification_type<Kernel> Roof;
+typedef CGAL::Segmentation_attribute_scatter<Kernel> Scatter;
+typedef CGAL::Segmentation_attribute_elevation<Kernel> Elevation;
+typedef CGAL::Segmentation_attribute_horizontality<Kernel> Horizontality;
+typedef CGAL::Segmentation_attribute_nonplanarity<Kernel> NonPlanarity;
 
 typedef Kernel::Point_3 Point;
 
@@ -35,23 +32,68 @@ int main (int argc, char** argv)
     }
 
   std::cerr << "Initializing" << std::endl;
-  PSC psc (pts.begin (), pts.end(), 1.5);
+  PSC psc (pts.begin (), pts.end(), 0.8);
   psc.initialization();
 
   std::cerr << "Computing attributes" << std::endl;
-  Scatter scat (psc, 0.1);
-  Planarity plan (psc, 3.5);
-  Elevation elev (psc, 16);
+  // Attributes with user-defined weights
+  Scatter scat (psc, 0.26);
+  Elevation elev (psc, 0.08);
+  Horizontality horiz (psc, 0.13);
+  NonPlanarity plan (psc, 0.72);
 
-  Vegetation vege (scat, plan, elev);
-  Ground ground (scat, plan, elev);
-  Roof roof (scat, plan, elev);
+  // Add attributes to PSC
+  psc.add_segmentation_attribute (&scat);
+  psc.add_segmentation_attribute (&elev);
+  psc.add_segmentation_attribute (&horiz);
+  psc.add_segmentation_attribute (&plan);
 
-  psc.segmentation_classes.push_back (&vege);
-  psc.segmentation_classes.push_back (&ground);
-  psc.segmentation_classes.push_back (&roof);
+  // Create classification type and define how attributes affect them
+  CGAL::Classification_type ground ("ground");
+  ground.change_attribute_effect (&scat, CGAL::Classification_type::PENALIZED_ATT);
+  ground.change_attribute_effect (&elev, CGAL::Classification_type::PENALIZED_ATT);
+  ground.change_attribute_effect (&horiz, CGAL::Classification_type::PENALIZED_ATT);
+  ground.change_attribute_effect (&plan, CGAL::Classification_type::PENALIZED_ATT);
 
-  psc.point_cloud_classification(2);
+  CGAL::Classification_type vege ("vegetation");
+  vege.change_attribute_effect (&scat, CGAL::Classification_type::FAVORED_ATT);
+  vege.change_attribute_effect (&elev, CGAL::Classification_type::NEUTRAL_ATT);
+  vege.change_attribute_effect (&horiz, CGAL::Classification_type::NEUTRAL_ATT);
+  vege.change_attribute_effect (&plan, CGAL::Classification_type::PENALIZED_ATT);
+  
+  CGAL::Classification_type roof ("roof");
+  roof.change_attribute_effect (&scat, CGAL::Classification_type::NEUTRAL_ATT);
+  roof.change_attribute_effect (&elev, CGAL::Classification_type::FAVORED_ATT);
+  roof.change_attribute_effect (&horiz, CGAL::Classification_type::NEUTRAL_ATT);
+  roof.change_attribute_effect (&plan, CGAL::Classification_type::NEUTRAL_ATT);
+
+  // Add types to PSC
+  psc.add_classification_type (&vege);
+  psc.add_classification_type (&ground);
+  psc.add_classification_type (&roof);
+
+  psc.classify (1); // Run with method=1 (global regularization with graphcut)
+
+  // Recover output
+  std::vector<Point> pts_ground, pts_vege, pts_roof;
+  for (std::size_t i = 0; i < pts.size(); ++ i)
+    {
+      Classification_type* type = psc.classification_type_of (i);
+      switch (type)
+        {
+        case &ground:
+          pts_ground.push_back (i);
+          break;
+        case &vege:
+          pts_vege.push_back (i);
+          break;
+        case &roof:
+          pts_roof.push_back (i);
+          break;
+        default:
+          std::cerr << "Error: unknown classification type" << std::endl;
+        }
+    }
   
   std::cerr << "All done" << std::endl;
   return EXIT_SUCCESS;
