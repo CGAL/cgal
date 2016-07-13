c65d3abe17bb3e677b8077996cdaf8672f9c4c6f
Laurent Rineau <laurent.rineau@cgal.org>
2016-07-13 17:48:40 +0200

    Add support for CTest

--- Installation/cmake/modules/CGAL_CreateSingleSourceCGALProgram.cmake
+++ Installation/cmake/modules/CGAL_CreateSingleSourceCGALProgram.cmake
@@ -18,6 +18,18 @@ function(create_single_source_cgal_program firstfile )
 
     add_executable(${exe_name} ${all})
 
+    if(BUILD_TESTING)
+      if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${exe_name}.cin")
+        set(ARGS "${CMAKE_CURRENT_SOURCE_DIR}/${exe_name}.cin")
+      elseif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${exe_name}.cmd")
+        file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/${exe_name}.cmd" ARGS LIMIT_COUNT 1)
+      endif()
+      message(STATUS "add test for ${exe_name}")
+      add_test(NAME ${exe_name}
+        COMMAND ${exe_name} ${ARGS}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    endif(BUILD_TESTING)
+
     add_to_cached_list( CGAL_EXECUTABLE_TARGETS ${exe_name} )
 
     # Link the executable to CGAL and third-party libraries
