8e9ac7740cd64727380e6b648ea45cda73b16c79
Efi Fogel <efifogel@gmail.com>
2016-08-03 12:08:46 +0300

    Enhanced the doc

--- Casting_2/doc/Casting_2/CGAL/single_mold_translational_casting_2.h
+++ Casting_2/doc/Casting_2/CGAL/single_mold_translational_casting_2.h
@@ -2,6 +2,14 @@ namespace CGAL {
 
 /*!
  * \ingroup PkgCasting2Funcs
+ * This function computes all top edges of a simple polygon.
+ * \param pgn[in] the input polygon.
+ * \param oi[out] the output iterator. Its value type is a pair.
+ *        a closed range of pull-out directions represented as a pair of the
+ *        extreme directions in the
+ * \return the past-the-end iterator of the output container.
+ * \pre the polygon must be non-degenerate (has at least 3 vertices), simple,
+ *      and does not have three consecutive collinear vertices.
  */
 template <typename Kernel, typename OutputIterator>
 OutputIterator
