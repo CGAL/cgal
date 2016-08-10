ba1812642b6706be6954dfdc75a4d9fdfa359b9d
Simon Giraudot <simon.giraudot@geometryfactory.com>
2016-08-10 16:12:36 +0200

    Update example

--- Point_set_processing_3/examples/Point_set_processing_3/scale_estimation_example.cpp
+++ Point_set_processing_3/examples/Point_set_processing_3/scale_estimation_example.cpp
@@ -1,6 +1,8 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/estimate_scale.h>
 #include <CGAL/IO/read_xyz_points.h>
+#include <CGAL/Timer.h>
+#include <CGAL/Memory_sizer.h>
 
 #include <vector>
 #include <fstream>
@@ -15,59 +17,74 @@ typedef Kernel::Point_2 Point_2;
 int main (int argc, char** argv)
 {
 
-  // 3D CASE
-  {
-    const char* fname = (argc>1)?argv[1]:"data/sphere_20k.xyz";
-    // Reads a .xyz point set file in points.
-    // As the point is the second element of the tuple (that is with index 1)
-    // we use a property map that accesses the 1st element of the tuple.
+  const char* fname = (argc>1)?argv[1]:"data/sphere_20k.xyz";
   
-    std::vector<Point_3> points;
-    std::ifstream stream(fname);
-    if (!stream ||
-        !CGAL::read_xyz_points(
-                               stream, std::back_inserter(points)))
-      {
-        std::cerr << "Error: cannot read file " << fname << std::endl;
-        return EXIT_FAILURE;
-      }
+  CGAL::Timer task_timer; 
+  
+  std::ifstream stream(fname);
+  if (stream)
+    {
+      std::vector<Point_3> points;
+      std::cerr << "TRYING 3D CASE" << std::endl;
+      if (CGAL::read_xyz_points(stream, std::back_inserter(points)))
+        {
+          task_timer.start();
+          
+          // estimate global scale
+          std::size_t k_scale = CGAL::estimate_global_k_neighbor_scale (points.begin(), points.end());
+          FT range_scale = CGAL::estimate_global_range_scale (points.begin(), points.end());
 
-    // estimate global scale
-    std::size_t k_scale = CGAL::estimate_global_k_neighbor_scale (points.begin(), points.end(),
-                                                                  CGAL::Identity_property_map<Point_3>(),
-                                                                  Kernel());
-    std::cout << "Global K scale: " << k_scale << std::endl;
+          std::size_t memory = CGAL::Memory_sizer().virtual_size();
+          double time = task_timer.time();
 
-    FT range_scale = CGAL::estimate_global_range_scale (points.begin(), points.end(),
-                                                        CGAL::Identity_property_map<Point_3>(),
-                                                        Kernel());
-    std::cout << "Global range scale: " << range_scale << std::endl;
-  }
+          std::cout << "Scales computed in " << time << " second(s) using "
+                    << (memory>>20) << " MiB of memory:" << std::endl;
+          std::cout << " * Global K scale: " << k_scale << std::endl;
+          std::cout << " * Global range scale: " << range_scale << std::endl;
+        }
+      else
+        {
+          stream.seekg(0);
+          std::cerr << "TRYING 2D CASE" << std::endl;
+          std::vector<Point_2> points_2;
 
-  // 2D CASE
-  {
-    std::vector<Point_2> points;
-    for (std::size_t i = 0; i < 5000; ++ i)
-      {
-        double angle = CGAL_PI * 2. * (rand() / (double)RAND_MAX);
-        double radius = 1. + 0.1 * (rand() / (double)RAND_MAX);
-        points.push_back (Point_2 (radius * std::cos (angle),
-                                   radius * std::sin (angle)));
-      }
+          // Read ASCII 2D point set file
+          std::string str;
+          while (getline (stream, str))
+            {
+              std::istringstream iss (str);
+              double x = 0., y = 0.;
+              iss >> x >> y;
+              points_2.push_back (Point_2 (x, y));
+            }
 
-        
-    // estimate global scale
-    std::size_t k_scale = CGAL::estimate_global_k_neighbor_scale (points.begin(), points.end(),
-                                                                  CGAL::Identity_property_map<Point_2>(),
-                                                                  Kernel());
-    std::cout << "Global K scale: " << k_scale << std::endl;
+          if (points_2.empty())
+            {
+              std::cerr << "Error: cannot read file " << fname << std::endl;
+              return EXIT_FAILURE;
+            }
+          
+          task_timer.start();
 
-    FT range_scale = CGAL::estimate_global_range_scale (points.begin(), points.end(),
-                                                        CGAL::Identity_property_map<Point_2>(),
-                                                        Kernel());
-    std::cout << "Global range scale: " << range_scale << std::endl;
+          // estimate global scale
+          std::size_t k_scale = CGAL::estimate_global_k_neighbor_scale (points_2.begin(), points_2.end());
+          FT range_scale = CGAL::estimate_global_range_scale (points_2.begin(), points_2.end());
 
-  }
+          std::size_t memory = CGAL::Memory_sizer().virtual_size();
+          double time = task_timer.time();
+
+          std::cout << "Scales computed in " << time << " second(s) using "
+                    << (memory>>20) << " MiB of memory:" << std::endl;
+          std::cout << " * Global K scale: " << k_scale << std::endl;
+          std::cout << " * Global range scale: " << range_scale << std::endl;
+        }
+    }
+  else
+    {
+      std::cerr << "Error: cannot read file " << fname << std::endl;
+      return EXIT_FAILURE;
+    }
+
   return EXIT_SUCCESS;
 }
 
