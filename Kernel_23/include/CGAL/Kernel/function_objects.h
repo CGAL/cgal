9841caa7548d6943eb87f99ec526b8c196858a06
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-08-17 10:14:01 +0200

    fix typo;  remove #includes of files which are now in the kernel

--- Kernel_23/include/CGAL/Kernel/function_objects.h
+++ Kernel_23/include/CGAL/Kernel/function_objects.h
@@ -35,7 +35,7 @@
 #include <CGAL/Kernel/Return_base_tag.h>
 #include <CGAL/Kernel/global_functions_3.h>
 #include <CGAL/Kernel/nearest_point_segment_3.h>
-#include <CGAL/Krnel/nearest_point_triangle_3.h>
+#include <CGAL/Kernel/nearest_point_triangle_3.h>
 
 namespace CGAL {
 
