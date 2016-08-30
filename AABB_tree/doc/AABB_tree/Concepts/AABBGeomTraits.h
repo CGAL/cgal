a412d93c347005364b9c799793d3a26c9d9693fa
Sébastien Loriot <sebastien.loriot@cgal.org>
2016-08-30 16:58:00 +0200

    do not add a nested typedef that make some free function calls ambiguous

--- AABB_tree/doc/AABB_tree/Concepts/AABBGeomTraits.h
+++ AABB_tree/doc/AABB_tree/Concepts/AABBGeomTraits.h
@@ -58,6 +58,7 @@ A functor object to construct the sphere centered at one point and passing throu
 typedef unspecified_type Construct_sphere_3; 
 
 /*!
+\todo This is not correct! that is not used!
 A functor object to compute the point on a geometric primitive which is closest from a query. Provides the operator: 
 `Point_3 operator()(const Point_3& p, const Type_2& type_2);` where `Type_2` is any type among `Segment_3` and `Triangle_3`. The operator returns the point on `type_2` which is closest to `p`. 
 */ 
