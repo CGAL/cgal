3d36fe05b3ca13d11b2b46afd1c89d64a9f0a32e
Laurent Rineau <laurent.rineau@cgal.org>
2016-09-07 16:14:00 +0200

    WIP: the Arr_2 testsuite is now working!!

--- Installation/cmake/modules/CGAL_CreateSingleSourceCGALProgram.cmake
+++ Installation/cmake/modules/CGAL_CreateSingleSourceCGALProgram.cmake
@@ -60,40 +60,9 @@ function(create_single_source_cgal_program firstfile )
 
     add_executable(${exe_name} ${all})
 
-    if(BUILD_TESTING)
-      set(cin_file "${CGAL_CURRENT_SOURCE_DIR}/${exe_name}.cin")
-      if(EXISTS ${cin_file})
-	add_test(NAME ${exe_name}
-          COMMAND ${CMAKE_COMMAND}
-	    -DCMD:STRING=$<TARGET_FILE:${exe_name}>
-	    -DCIN:STRING=${cin_file}
-	    -P "${CGAL_MODULES_DIR}/run_test_with_cin.cmake")
-#	message(STATUS "add test: ${exe_name} < ${cin_file}")
-      else()
-	# TODO: deal with shell globbing; if the `cmd` file contains
-	# a `*`, then interprete the command using bash.
-	set(cmd_file "${CGAL_CURRENT_SOURCE_DIR}/${exe_name}.cmd")
-	if(EXISTS ${cmd_file})
-          file(STRINGS "${cmd_file}" CMD_LINES)
-	  set(ARGS)
-#	  message(STATUS "DEBUG test ${exe_name}")
-	  foreach(CMD_LINE ${CMD_LINES})
-#	    message(STATUS "  command line: ${CMD_LINE}")
-	    separate_arguments(CMD_LINE_ARGS UNIX_COMMAND ${CMD_LINE})
-#	    message(STATUS "  args: ${CMD_LINE_ARGS}")
-	    list(APPEND ARGS ${CMD_LINE_ARGS})
-	  endforeach()
-	  expand_list_with_globbing(ARGS)
-	endif()
-#	message(STATUS "add test: ${exe_name} ${ARGS}")
-	add_test(NAME ${exe_name} COMMAND ${exe_name} ${ARGS})
-      endif()
-      set_property(TEST "${exe_name}"
-        APPEND PROPERTY LABELS "${PROJECT_NAME}")
-#      message(STATUS "  workding dir: ${CGAL_CURRENT_SOURCE_DIR}")
-      set_property(TEST "${exe_name}"
-        PROPERTY WORKING_DIRECTORY ${CGAL_CURRENT_SOURCE_DIR})
-    endif(BUILD_TESTING)
+    if(NOT NO_TESTING)
+      cgal_add_test("${exe_name}")
+    endif(NOT NO_TESTING)
 
     add_to_cached_list( CGAL_EXECUTABLE_TARGETS ${exe_name} )
 
