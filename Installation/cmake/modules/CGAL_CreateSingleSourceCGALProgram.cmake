a3ddaa132fb17cbda662d814d6df3e8966e76ccc
Laurent Rineau <laurent.rineau@cgal.org>
2016-09-09 11:44:26 +0200

    Define and use a new CMake API for CGAL
    For the moment, the new API is used only in:
    
    - `examples/Mesh_2/CMakeLists.txt`
    - `demo/Triangulation_2/CMakeLists.txt`

--- Installation/cmake/modules/CGAL_CreateSingleSourceCGALProgram.cmake
+++ Installation/cmake/modules/CGAL_CreateSingleSourceCGALProgram.cmake
@@ -63,16 +63,7 @@ function(create_single_source_cgal_program firstfile )
 
     add_to_cached_list( CGAL_EXECUTABLE_TARGETS ${exe_name} )
 
-    # Link the executable to CGAL and third-party libraries
-    if ( CGAL_AUTO_LINK_ENABLED )
-
-      target_link_libraries(${exe_name} ${CGAL_3RD_PARTY_LIBRARIES} )
-
-    else()
-
-      target_link_libraries(${exe_name} ${CGAL_LIBRARIES} ${CGAL_3RD_PARTY_LIBRARIES} )
-
-    endif()
+    target_link_libraries(${exe_name} CGAL::CGAL )
   else()
     message(AUTHOR_WARNING "The executable ${exe_name} will not be created because the source file ${firstfile} does not exist.")
   endif()
