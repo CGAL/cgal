ca8cfba5ad50227d4e276736fe450b7300bd18cb
Sébastien Loriot <sebastien.loriot@cgal.org>
2016-09-13 14:22:49 +0200

    -ansi implies c++98 -> update script requirements of boost libraries

--- Installation/cmake/modules/CGAL_SetupBoost.cmake
+++ Installation/cmake/modules/CGAL_SetupBoost.cmake
@@ -13,7 +13,8 @@ if ( NOT CGAL_Boost_Setup )
           AND CMAKE_CXX_FLAGS MATCHES "\\-std=(c|gnu)\\+\\+[01][14yxz]")
         #GCC 6.0+ without c++03 on
         OR ( NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.0
-             AND NOT CMAKE_CXX_FLAGS MATCHES "\\-std=(c|gnu)\\+\\+[90][83]")
+             AND NOT CMAKE_CXX_FLAGS MATCHES "\\-std=(c|gnu)\\+\\+[90][83]"
+             AND NOT CMAKE_CXX_FLAGS MATCHES "\\-ansi")
       ) )
     set ( CGAL_requires_Boost_libs FALSE )
   endif()
