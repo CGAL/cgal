d8b564e003fe4c5364ed8fbd44c2bdc459845be7
Laurent Rineau <laurent.rineau@cgal.org>
2016-09-20 10:31:58 +0200

    Use CGAL_add_test.cmake

--- Installation/cmake/modules/CGAL_CreateSingleSourceCGALProgram.cmake
+++ Installation/cmake/modules/CGAL_CreateSingleSourceCGALProgram.cmake
@@ -1,44 +1,4 @@
-
-# Process a list, and replace items contains a file pattern (like
-# `*.off`) by the sublist that corresponds to the globbing of the
-# pattern in the directory `${CGAL_CURRENT_SOURCE_DIR}`.
-#
-#
-# For example: the `file
-# test/Poisson_surface_reconstruction_3/poisson_reconstruction_test.cmd`
-# contains:
-#
-#     data/*.off data/*.xyz data/*.pwn
-#
-# For that file, the list `ARGS` computed in the function
-# `create_single_source_cgal_program` (see below) is the list:
-#
-#     data/*.off;data/*.xyz;data/*.pwn
-#
-# A call to `expand_list_with_globbing(ARGS)` replaces the list by:
-#
-#     data/ChineseDragon-10kv.off;data/robocat_deci.off;data/sphere_20k.xyz;data/oni.pwn"
-#
-function(expand_list_with_globbing list_name)
-  set(input_list ${${list_name}})
-#  message(STATUS "expand_list_with_globbing(${list_name}), ${list_name} is: ${input_list}")
-  list(LENGTH input_list list_lenght)
-  math(EXPR list_last_n "${list_lenght} - 1")
-  set(output_list)
-  foreach(n RANGE ${list_last_n})
-#    message(STATUS "n=${n}")
-    list(GET input_list ${n} item_n)
-#    message(STATUS "argument ${n} is ${item_n}")
-    if(item_n MATCHES ".*\\*.*")
-      file(GLOB files RELATIVE ${CGAL_CURRENT_SOURCE_DIR} ${item_n})
-      list(APPEND output_list ${files})
-    else()
-      list(APPEND output_list ${item_n})
-    endif()
-#    message(STATUS "  new value of the output list: ${output_list}")
-  endforeach()
-  set(${list_name} ${output_list} PARENT_SCOPE)
-endfunction()
+include(CGAL_add_test)
 
 function(create_single_source_cgal_program firstfile )
 
