9ba4132a38b6587ab38618a66a30eddf708e6566
Laurent Rineau <laurent.rineau@cgal.org>
2016-10-04 16:15:17 +0200

    No helper.update_restricted_facets() with dim 2!

--- Mesh_3/include/CGAL/make_mesh_3.h
+++ Mesh_3/include/CGAL/make_mesh_3.h
@@ -264,11 +264,16 @@ struct C3t3_initializer < C3T3, MD, MC, true, CGAL::Tag_true >
       // If c3t3 initialization is not sufficient (may happen if there is only
       // a planar curve as feature for example), add some surface points
 
-      CGAL::Mesh_3::C3T3_helpers<C3T3, MD> helper(c3t3, domain);
-      helper.update_restricted_facets();
+      bool need_more_init = c3t3.triangulation().dimension() != 3;
+      if(!need_more_init) {
+        CGAL::Mesh_3::C3T3_helpers<C3T3, MD> helper(c3t3, domain);
+        helper.update_restricted_facets();
 
-      if ( c3t3.triangulation().dimension() != 3
-        || c3t3.number_of_facets() == 0) {
+        if (c3t3.number_of_facets() == 0) {
+          need_more_init = true;
+        }
+      }
+      if(need_more_init) {
         init_c3t3(c3t3, domain, criteria, nb_initial_points);
       }
     }
