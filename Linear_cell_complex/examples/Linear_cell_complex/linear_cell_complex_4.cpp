535c2caff9e5f26768023eaa6b564051aa25af6b
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2016-10-06 14:46:02 -0400

    Update LCC examples

--- Linear_cell_complex/examples/Linear_cell_complex/linear_cell_complex_4.cpp
+++ Linear_cell_complex/examples/Linear_cell_complex/linear_cell_complex_4.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 #include <vector>
 
-typedef CGAL::Linear_cell_complex_for_combinatorial_map<4,5> LCC_4;
-typedef LCC_4::Dart_handle             Dart_handle;
-typedef LCC_4::Point                   Point;
-typedef LCC_4::Vector                  Vector;
-typedef LCC_4::FT                      FT;
+typedef CGAL::Linear_cell_complex_for_generalized_map<4,5> LCC_4;
+typedef LCC_4::Dart_handle                                 Dart_handle;
+typedef LCC_4::Point                                       Point;
+typedef LCC_4::Vector                                      Vector;
+typedef LCC_4::FT                                          FT;
 
 int main()
 {
@@ -49,7 +49,7 @@ int main()
   std::cout<<", valid="<<lcc.is_valid()<<std::endl;
 
   // Add one edge to cut the face containing dart d3 in two.
-  Dart_handle d4 = lcc.insert_cell_1_in_cell_2(d3,lcc.beta(d1, 0));
+  Dart_handle d4 = lcc.insert_cell_1_in_cell_2(d3,lcc.alpha(d1, 0, 1));
   CGAL_assertion( lcc.is_valid() );
   
   lcc.display_characteristics(std::cout);
