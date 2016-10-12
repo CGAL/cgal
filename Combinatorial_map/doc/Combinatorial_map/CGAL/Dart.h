268036a08807ebe60649ebd4bc4defdb73fd369f
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2016-10-12 14:24:47 -0400

    Keep old names in the doc, marked deprecated.

---
+++ Combinatorial_map/doc/Combinatorial_map/CGAL/Dart.h
@@ -0,0 +1,15 @@
+
+namespace CGAL {
+
+/*!
+\ingroup PkgCombinatorialMapsClasses
+
+The class `CGAL::Dart` represents a <I>d</I>D dart in a combinatorial map.
+
+\deprecated This class is deprecated since CGAL 4.9 and has been renamed to `Combinatorial_map_dart`.
+
+*/
+template< typename d, typename CMap >
+class Dart {
+}; /* end Dart */
+} /* end namespace CGAL */
---
+++ Linear_cell_complex/doc/Linear_cell_complex/CGAL/Linear_cell_complex_min_items.h
@@ -0,0 +1,20 @@
+
+namespace CGAL {
+
+/*!
+\ingroup PkgLinearCellComplexClasses
+
+The class `Linear_cell_complex_min_items` defines the type of darts, which is a `Combinatorial_map_dart<d,LCC>`, and the attributes used. In this class, 0-attributes are enabled and associated with `Cell_attribute_with_point`.
+
+\deprecated This class is deprecated since CGAL 4.9 and has been renamed to `Linear_cell_complex_for_combinatorial_map_min_items`.
+
+*/
+template< typename d >
+class Linear_cell_complex_min_items {
+public:
+
+/// @}
+
+}; /* end Linear_cell_complex_min_items */
+
+} /* end namespace CGAL */
