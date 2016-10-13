678f9572f7fbb6a9fa52639fd81a99af6db0695a
Marc Glisse <marc.glisse@inria.fr>
2016-10-13 23:11:26 +0200

    More ugly workarounds for expression templates.

--- Bounding_volumes/include/CGAL/Min_sphere_of_spheres_d/Min_sphere_of_spheres_d_support_set_impl.h
+++ Bounding_volumes/include/CGAL/Min_sphere_of_spheres_d/Min_sphere_of_spheres_d_support_set_impl.h
@@ -91,7 +91,7 @@ namespace CGAL_MINIBALL_NAMESPACE {
         for (int j=0; j<D; ++j) {
           eps[m]   -= u[m][j]*e[m-1][j];
           phi[m]   -= u[m][j]*f[m-1][j];
-          delta[m] += sqr(u[m][j]-d[m-1][j]);
+          delta[m] += sqr<FT>(u[m][j]-d[m-1][j]);
         }
         phi[m] = FT(2)*(phi[m] - t1);
         eps[m] = t1*t2+FT(2)*eps[m];
@@ -151,7 +151,7 @@ namespace CGAL_MINIBALL_NAMESPACE {
       if (m > 1) {
         // compute the coeffients beta[i] and the center:
         for(unsigned int i=1; i<m; ++i) {
-          beta[i] = (delta[i]+eps[i]+sol[m]*phi[i])/alpha[i];
+          beta[i] = (static_cast<FT>(delta[i]+eps[i])+sol[m]*phi[i])/alpha[i];
           for (int j=0; j<D; ++j)
             center[j] += beta[i]*u[i][j];
         }
