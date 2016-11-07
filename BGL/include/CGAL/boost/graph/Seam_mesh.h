c333aa34b181772a7a5514e3b8eec37286ecd521
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2016-11-07 15:58:27 +0100

    Fixed a bad static_cast in Seam_mesh.h

--- BGL/include/CGAL/boost/graph/Seam_mesh.h
+++ BGL/include/CGAL/boost/graph/Seam_mesh.h
@@ -565,7 +565,7 @@ public:
   /// Numbering
   vertices_size_type num_vertices() const
   {
-    if(number_of_vertices == static_cast<unsigned int>(-1)) {
+    if(number_of_vertices == static_cast<vertices_size_type>(-1)) {
       number_of_vertices = vertices().size();
     }
 
