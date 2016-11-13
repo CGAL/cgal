9e14322cec1e2ad297d6cd416462491aa9fdfc83
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-11-13 07:53:18 +0100

    add typename

--- Mesh_3/test/Mesh_3/test_meshing_polyhedron_with_features.cpp
+++ Mesh_3/test/Mesh_3/test_meshing_polyhedron_with_features.cpp
@@ -37,7 +37,7 @@ struct Polyhedron_with_features_tester : public Tester<K>
   void operator()() const
   {
     typedef CGAL::Mesh_3::Robust_intersection_traits_3<K> Gt;
-    typedef CGAL::Mesh_polyhedron_3<Gt>::type Polyhedron;
+    typedef typename CGAL::Mesh_polyhedron_3<Gt>::type Polyhedron;
     typedef CGAL::Polyhedral_mesh_domain_with_features_3<Gt> Mesh_domain;
     
     typedef typename CGAL::Mesh_triangulation_3<
