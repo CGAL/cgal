2620dcbaaccb261d45d44af11cf7b368181db981
Andreas Fabri <andreas.fabri@geometryfactory.com>
2016-11-16 14:00:29 +0100

    Add an example that shows that we need either RT_euclidean_traits or RT_vertex_base_with_info_3

---
+++ Triangulation_3/examples/Triangulation_3/regular_with_info.cpp
@@ -0,0 +1,32 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Regular_triangulation_3.h>
+#include <CGAL/Triangulation_vertex_base_with_info_3.h>
+#include <cassert>
+#include <vector>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+
+typedef K::FT                                          Weight;
+typedef K::Point_3                                     Point;
+typedef K::Weighted_point_3                            Weighted_point;
+
+typedef CGAL::Regular_triangulation_euclidean_traits_3<K> Traits;
+
+typedef CGAL::Triangulation_vertex_base_with_info_3<int, Traits> Vb;
+typedef CGAL::Regular_triangulation_cell_base_3<K> Cb;
+typedef CGAL::Triangulation_data_structure_3<Vb,Cb>                    Tds;
+
+typedef CGAL::Regular_triangulation_3<K, Tds>          Rt;
+
+int main()
+{
+  Weighted_point wp(CGAL::ORIGIN,1);
+
+  Rt rt;
+
+  rt.insert(wp);
+
+  assert( T.is_valid() );
+
+  return 0;
+}
