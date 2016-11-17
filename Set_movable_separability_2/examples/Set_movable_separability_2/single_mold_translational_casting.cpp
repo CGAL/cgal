864d456ed1e1f61443014addf3ca92653128bea5
Efi Fogel <efifogel@gmail.com>
2016-11-17 09:43:46 +0200

    Updated

--- Set_movable_separability_2/examples/Set_movable_separability_2/single_mold_translational_casting.cpp
+++ Set_movable_separability_2/examples/Set_movable_separability_2/single_mold_translational_casting.cpp
@@ -8,8 +8,6 @@
 typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
 typedef CGAL::Polygon_2<Kernel>                           Polygon_2;
 typedef Kernel::Direction_2                               Direction_2;
-typedef Kernel::Point_2                                   Point_2;
-
 typedef std::pair<Direction_2, Direction_2>               Direction_range;
 typedef std::pair<size_t, Direction_range>                Top_edge;
 
@@ -27,11 +25,9 @@ int main(int  argc, char* argv[])
     return -1;
   }
   input_file >> pgn;
+  input_file.close();
 
-  auto e_it = pgn.edges_begin();
-  size_t edge_index(0);
   auto poly_orientation = pgn.orientation();
-  ++edge_index;
   std::list<Top_edge> top_edges;
   SMS::single_mold_translational_casting_2(pgn, std::back_inserter(top_edges));
 
@@ -40,9 +36,9 @@ int main(int  argc, char* argv[])
   else {
     std::cout << "There are " << top_edges.size() << " top edges:" << std::endl;
     for (const auto& top_edge : top_edges) {
-      std::cout << "Edge number: "<<top_edge.first << std::endl
-                << "\tEdge: "<<pgn.edge(top_edge.first) << std::endl
-                << "\tPullout directions from: "<<top_edge.second.first
+      std::cout << "Edge number: " << top_edge.first << std::endl
+                << "\tEdge: "<< pgn.edge(top_edge.first) << std::endl
+                << "\tPullout directions from: "<< top_edge.second.first
                 << " to " << top_edge.second.second
                 << std::endl<< std::endl;
     }
