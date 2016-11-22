e31575e2edd82bac7a9a3a6935e5297057981aba
Simon Giraudot <simon.giraudot@geometryfactory.com>
2016-11-22 09:20:48 +0100

    Mesh simplification: add edge length stop predicate

--- Polyhedron/demo/Polyhedron/Plugins/Surface_mesh/Mesh_simplification_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/Surface_mesh/Mesh_simplification_plugin.cpp
@@ -12,6 +12,43 @@
 #include <CGAL/Surface_mesh_simplification/HalfedgeGraph_Polyhedron_3.h>
 #include <CGAL/Surface_mesh_simplification/edge_collapse.h>
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Count_stop_predicate.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Edge_length_stop_predicate.h>
+
+#include "ui_Mesh_simplification_dialog.h"
+
+class Custom_stop_predicate
+{
+  bool m_and;
+    CGAL::Surface_mesh_simplification::Count_stop_predicate<Polyhedron> m_count_stop;
+  CGAL::Surface_mesh_simplification::Edge_length_stop_predicate<double> m_length_stop;
+  
+public:
+
+  Custom_stop_predicate (bool use_and, std::size_t nb_edges, double edge_length)
+    : m_and (use_and), m_count_stop (nb_edges), m_length_stop (edge_length)
+  {
+    std::cerr << "Simplifying until:" << std::endl
+              << " * Number of edges = " << nb_edges << std::endl
+              << (use_and ? " AND " : " OR ") << std::endl
+              << " * Minimum edge length = " << edge_length << std::endl;
+
+  }
+
+  template <typename Profile>
+  bool operator() (const double& current_cost, const Profile& edge_profile,
+                   std::size_t initial_count, std::size_t current_count) const 
+  {
+    if (m_and)
+      return (m_count_stop(current_count, edge_profile, initial_count, current_count)
+              && m_length_stop(current_count, edge_profile, initial_count, current_count));
+    else
+      return (m_count_stop(current_count, edge_profile, initial_count, current_count)
+              || m_length_stop(current_count, edge_profile, initial_count, current_count));
+  }
+
+};
+
+
 using namespace CGAL::Three;
 class Polyhedron_demo_mesh_simplification_plugin : 
   public QObject,
@@ -63,20 +100,26 @@ void Polyhedron_demo_mesh_simplification_plugin::on_actionSimplify_triggered()
   {
     Polyhedron* pMesh = item->polyhedron();
 
-    // get option (#edges)
-    bool ok;
-  
-    const int nb_edges = 
-    QInputDialog::getInt(mw, tr("Stop condition"),
-      tr("Number of edges:"),
-      (int)(pMesh->size_of_halfedges () / 4), // default value: current #edges / 2 
-      3, // min = one triangle
-      (int)pMesh->size_of_halfedges(), // max #edges
-      1, // step for the spinbox
-      &ok);
+    // get option
+    QDialog dialog(mw);
+    Ui::Mesh_simplification_dialog ui;
+    ui.setupUi(&dialog);
+    connect(ui.buttonBox, SIGNAL(accepted()),
+            &dialog, SLOT(accept()));
+    connect(ui.buttonBox, SIGNAL(rejected()),
+            &dialog, SLOT(reject()));
+
+    Scene_interface::Bbox bbox = item->bbox();
+    double diago_length = CGAL::sqrt((bbox.xmax()-bbox.xmin())*(bbox.xmax()-bbox.xmin())
+                                     + (bbox.ymax()-bbox.ymin())*(bbox.ymax()-bbox.ymin()) +
+                                     (bbox.zmax()-bbox.zmin())*(bbox.zmax()-bbox.zmin()));
+    
+    ui.m_nb_edges->setValue ((int)(pMesh->size_of_halfedges () / 4));
+    ui.m_nb_edges->setMaximum ((int)(pMesh->size_of_halfedges ()));
+    ui.m_edge_length->setValue (diago_length * 0.05);
 
     // check user cancellation
-    if(!ok)
+    if(dialog.exec() == QDialog::Rejected)
       return;
 
     // simplify
@@ -85,9 +128,17 @@ void Polyhedron_demo_mesh_simplification_plugin::on_actionSimplify_triggered()
     std::cout << "Simplify...";
     QApplication::setOverrideCursor(Qt::WaitCursor);
     QApplication::processEvents();
-    namespace SMS = CGAL::Surface_mesh_simplification;
-    SMS::Count_stop_predicate< Polyhedron > stop(nb_edges); // target #edges
-    SMS::edge_collapse( *pMesh, stop,
+    Custom_stop_predicate stop ((ui.m_combinatorial->currentIndex() == 0)
+                                && !(ui.m_use_nb_edges->isChecked())
+                                && !(ui.m_use_edge_length->isChecked()),
+                                (ui.m_use_nb_edges->isChecked()
+                                 ? ui.m_nb_edges->value()
+                                 : 0),
+                                (ui.m_use_edge_length->isChecked()
+                                 ? ui.m_edge_length->value()
+                                 : std::numeric_limits<double>::max()));
+    
+    CGAL::Surface_mesh_simplification::edge_collapse( *pMesh, stop,
                         CGAL::parameters::vertex_index_map(get(CGAL::vertex_external_index,*pMesh))
                                          .halfedge_index_map(get(CGAL::halfedge_external_index,*pMesh)));
     std::cout << "ok (" << time.elapsed() << " ms, " 
