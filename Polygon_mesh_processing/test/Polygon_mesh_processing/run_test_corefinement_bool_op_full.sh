834879e50eb711a8f66f509e9998a3e7482038dd
Sébastien Loriot <sebastien.loriot@cgal.org>
2016-12-01 17:08:51 +0100

    add bash script to run locally a corefinement testsuite

---
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/run_test_corefinement_bool_op_full.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+GREEN="\\033[1;32m"
+NORMAL="\\033[0;39m"
+RED="\\033[1;31m"
+
+METHOD="SM"
+
+if [ ! -e $1 ""]; then
+  METHOD=$1
+fi
+
+echo "Testing with $METHOD"
+
+k=`wc -l test_corefinement_bool_op_full.cmd | awk '{print $1}'`
+
+for i in `seq 1 $k`; do
+  files=`head -n $i test_corefinement_bool_op_full.cmd | tail -n 1`
+  f1=`echo $files | awk '{print $1}'`
+  f2=`echo $files | awk '{print $2}'`
+  echo -n "==== " $f1 $f2 " "
+
+  if (./test_corefinement_bool_op $f1 $f2  $METHOD || false ) > /dev/null 2>&1; then
+    echo -e " ==> $GREEN SUCCEED"
+    echo -e -n "$NORMAL"
+  else
+    echo -e " ==> $RED FAILED"
+    echo -e -n "$NORMAL"
+  fi
+done
