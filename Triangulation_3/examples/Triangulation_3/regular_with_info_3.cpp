4b84aca9a862e14256bb99670b850b969b2f6737
Jane Tournois <jane.tournois@geometryfactory.com>
2016-12-09 12:24:16 +0100

    clean indentation of typedefs in example

--- Triangulation_3/examples/Triangulation_3/regular_with_info_3.cpp
+++ Triangulation_3/examples/Triangulation_3/regular_with_info_3.cpp
@@ -6,17 +6,17 @@
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 
-typedef K::FT                                          Weight;
-typedef K::Point_3                                     Point;
-typedef K::Weighted_point_3                            Weighted_point;
+typedef K::FT                                               Weight;
+typedef K::Point_3                                          Point;
+typedef K::Weighted_point_3                                 Weighted_point;
 
-typedef CGAL::Regular_triangulation_vertex_base_3<K> Vb0;
+typedef CGAL::Regular_triangulation_vertex_base_3<K>        Vb0;
 
 typedef CGAL::Triangulation_vertex_base_with_info_3<int, K, Vb0> Vb;
-typedef CGAL::Regular_triangulation_cell_base_3<K> Cb;
-typedef CGAL::Triangulation_data_structure_3<Vb,Cb>                    Tds;
+typedef CGAL::Regular_triangulation_cell_base_3<K>          Cb;
+typedef CGAL::Triangulation_data_structure_3<Vb,Cb>         Tds;
 
-typedef CGAL::Regular_triangulation_3<K, Tds>          Rt;
+typedef CGAL::Regular_triangulation_3<K, Tds>               Rt;
 
 int main()
 {
