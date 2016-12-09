eff63d69943f860ec821a78161da5d5682efc3cf
Jane Tournois <jane.tournois@geometryfactory.com>
2016-12-09 11:58:23 +0100

    add missing "regular"

--- Triangulation_3/doc/Triangulation_3/CGAL/Regular_triangulation_vertex_base_3.h
+++ Triangulation_3/doc/Triangulation_3/CGAL/Regular_triangulation_vertex_base_3.h
@@ -5,8 +5,8 @@ namespace CGAL {
 \ingroup PkgTriangulation3VertexCellClasses
 
 The class `Regular_triangulation_vertex_base_3` is a model of the concept 
-`TriangulationVertexBase_3`, the base vertex of a 3D-triangulation. 
-This class stores a point. 
+`RegularTriangulationVertexBase_3`, the base vertex of a 3D regular triangulation. 
+This class stores a weighted point.
 
 This class can be used directly or can serve as a base to derive other classes 
 with some additional attributes (a color for example) tuned for a specific 
@@ -14,18 +14,18 @@ application.
 
 
 \tparam TriangulationTraits_3 is the geometric traits class 
- which provides the point type `Point_3`. 
+ which provides the point type `Weighted_point_3`. 
 Users of the geometric triangulations are strongly advised to use the same 
 geometric traits class `TriangulationTraits_3` as the one used for 
 `Triangulation_3`. This way, the point type defined by the base vertex is 
 the same as the point type defined by the geometric traits class. 
 
 \tparam TriangulationDSVertexBase_3_ is a combinatorial vertex base class from which 
-`Triangulation_vertex_base_3` derives.
+`RegularTriangulation_vertex_base_3` derives.
 It must be a model of the `TriangulationDSVertexBase_3` concept.
 It has the default value `Triangulation_ds_vertex_base_3<TDS>`.
 
-\cgalModels `TriangulationVertexBase_3`
+\cgalModels `RegularTriangulationVertexBase_3`
 
 \sa `CGAL::Triangulation_cell_base_3` 
 \sa `CGAL::Triangulation_ds_vertex_base_3` 
