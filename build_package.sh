3a64d0275bf1023ccddf78b8f7b15fd5f0ee35fe
Maxime Gimeno <maxime.gimeno@gmail.com>
2017-01-10 16:51:32 +0100

    Make one job per package that builds the example and the tests.

---
+++ build_package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+EXAMPLES="$1/examples/$1"
+TEST="$1/test/$1"
+#DEMO="$1/demo/$1"
+ROOT="$PWD"
+cd $ROOT/$EXAMPLES
+mkdir -p build
+cd build
+cmake -DCGAL_DIR="$ROOT/build" ..
+make
+cd $ROOT/$TEST
+mkdir -p build
+cd build
+cmake -DCGAL_DIR="$ROOT/build" ..
+make
+#if [ "$1" != Polyhedron ]
+#then
+#  cd $ROOT/$DEMO
+#  mkdir -p build
+#  cd build
+#  cmake -DCGAL_DIR="$ROOT/build" ..
+#  make
+#fi
