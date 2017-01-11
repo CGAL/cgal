dc578928d051be4444c8f0ebc3ef9bbff71b4146
Maxime Gimeno <maxime.gimeno@gmail.com>
2017-01-11 09:46:09 +0100

    Add a test of existence of the examples and tests before trying to build.

--- build_package.sh
+++ build_package.sh
@@ -2,18 +2,24 @@
 
 EXAMPLES="$1/examples/$1"
 TEST="$1/test/$1"
-#DEMO="$1/demo/$1"
+DEMO="$1/demo/$1"
 ROOT="$PWD"
-cd $ROOT/$EXAMPLES
-mkdir -p build
-cd build
-cmake -DCGAL_DIR="$ROOT/build" ..
-make
-cd $ROOT/$TEST
-mkdir -p build
-cd build
-cmake -DCGAL_DIR="$ROOT/build" ..
-make
+if [ -d "$ROOT/$EXAMPLES" ]
+then
+  cd $ROOT/$EXAMPLES
+  mkdir -p build
+  cd build
+  cmake -DCGAL_DIR="$ROOT/build" ..
+  make
+fi
+if [ -d "$ROOT/$TEST" ]
+then
+  cd $ROOT/$TEST
+  mkdir -p build
+  cd build
+  cmake -DCGAL_DIR="$ROOT/build" ..
+  make
+fi
 #if [ "$1" != Polyhedron ]
 #then
 #  cd $ROOT/$DEMO
