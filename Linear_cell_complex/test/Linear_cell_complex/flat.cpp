3f5a0bd27ae63b792dc8a03d968f86ff4c7eeffb
Andreas Fabri <andreas.fabri@geometryfactory.com>
2017-01-13 10:57:21 +0100

    Linear_cell_complex_demo:  Reorder includes to work around a bug in boost
    Reordering includes of CGAL headers changes the inclusion order
    for boost headers.  The test file  flat.cpp should reveal the bug

---
+++ Linear_cell_complex/test/Linear_cell_complex/flat.cpp
@@ -0,0 +1,12 @@
+
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+#include <boost/container/flat_set.hpp>
+
+int main()
+{
+  boost::container::flat_set<int> fs;
+
+  return 0;
+}
