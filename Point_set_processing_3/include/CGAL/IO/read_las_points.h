d827e9d65d752d0020ab91975869c3db417c2878
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-01-19 15:13:03 +0100

    First version of LAS reader

--- Point_set_processing_3/include/CGAL/IO/read_las_points.h
+++ Point_set_processing_3/include/CGAL/IO/read_las_points.h
@@ -25,12 +25,21 @@
 #include <CGAL/point_set_processing_assertions.h>
 #include <CGAL/Kernel_traits.h>
 
+#include <pdal/PointTable.hpp>
+#include <pdal/PointView.hpp>
+#include <pdal/io/LasReader.hpp>
+#include <pdal/io/LasHeader.hpp>
+#include <pdal/Options.hpp>
+
 #include <boost/version.hpp>
 #include <boost/cstdint.hpp>
 
 #include <iostream>
 #include <sstream>
 #include <string>
+
+
+#include <lasreader_las.hpp>
 
 
 namespace CGAL {
@@ -48,18 +57,184 @@ namespace CGAL {
 // float      single-precision float    4
 // double     double-precision float    8
 
-/// \cond SKIP_IN_MANUAL
 
+namespace Las
+{
+
+  template <typename T>
+  struct Property
+  {
+    typedef T type;
+    const char* name;
+    Property (const char* name) : name (name) { }
+  };
+
+  Property<double> X() { return Property<double>("X"); }
+  Property<double> Y() { return Property<double>("Y"); }
+  Property<double> Z() { return Property<double>("Z"); }
+  
+/// \cond SKIP_IN_MANUAL
+  
 namespace internal {
 
+  template <class T>
+  void get_value(const LASpoint& r, T& v, Las::Property<T>& wrapper)
+  {
+    if (wrapper.name == "X")
+      v = r.get_x();
+    else if (wrapper.name == "Y")
+      v = r.get_y();
+    else if (wrapper.name == "Z")
+      v = r.get_z();
+  }
+
+  
+  template <std::size_t N>
+  struct Filler
+  {
+    template <class Value_tuple, class Las_property_tuple>
+    static void fill(const LASpoint& r, Value_tuple& values, Las_property_tuple wrappers)
+    {
+      get_value(r, std::get<N>(values), std::get<N+2>(wrappers));
+      Filler<N-1>::fill(r, values, wrappers);
+    }
+  };
+
+  template<int ...>
+  struct seq { };
+
+  template<int N, int ...S>
+  struct gens : gens<N-1, N-1, S...> { };
+
+  template<int ...S>
+  struct gens<0, S...> {
+    typedef seq<S...> type;
+  };
+
+  template<class ValueType, class Functor, class Tuple, int ...S>
+  ValueType call_functor(Functor f, Tuple t, seq<S...>) {
+    return f(std::get<S>(t) ...);
+  }
+
+  template <class ValueType, class Functor, typename ... T>
+  ValueType call_functor(Functor f, std::tuple<T...>& t)
+  {
+    return call_functor<ValueType>(f, t, typename gens<sizeof...(T)>::type());
+  }
+
+  template<>
+  struct Filler<0>
+  {
+    template <class Value_tuple, class Las_property_tuple>
+    static void fill(const LASpoint& r, Value_tuple& values, Las_property_tuple wrappers)
+    {
+      get_value(r, std::get<0>(values), std::get<2>(wrappers));
+    }
+  };
+  
+  template <typename OutputValueType,
+            typename PropertyMap,
+            typename Constructor,
+            typename ... T>
+  void process_properties (const LASpoint& reader, OutputValueType& new_element,
+                           std::tuple<PropertyMap, Constructor, Las::Property<T>...>& current)
+  {
+    typedef typename PropertyMap::value_type PmapValueType;
+    std::tuple<T...> values;
+    Filler<sizeof...(T)-1>::fill(reader, values, current);
+    PmapValueType new_value = call_functor<PmapValueType>(std::get<1>(current), values);
+    put (std::get<0>(current), new_element, new_value);
+  }
+  
+  template <typename OutputValueType,
+            typename PropertyMap,
+            typename Constructor,
+            typename ... T,
+            typename NextPropertyBinder,
+            typename ... PropertyMapBinders>
+  void process_properties (const LASpoint& reader, OutputValueType& new_element,
+                           std::tuple<PropertyMap, Constructor, Las::Property<T>...>& current,
+                           NextPropertyBinder& next,
+                           PropertyMapBinders&& ... properties)
+  {
+    typedef typename PropertyMap::value_type PmapValueType;
+    std::tuple<T...> values;
+    Filler<sizeof...(T)-1>::fill(reader, values, current);
+    PmapValueType new_value = call_functor<PmapValueType>(std::get<1>(current), values);
+    put (std::get<0>(current), new_element, new_value);
+  
+    process_properties (reader, new_element, next, properties...);
+  }
+
+
+  template <typename OutputValueType, typename PropertyMap, typename T>
+  void process_properties (const LASpoint& reader, OutputValueType& new_element,
+                           std::pair<PropertyMap, Las::Property<T> >& current)
+  {
+    T new_value = T();
+    // TODO 
+    put (current.first, new_element, new_value);
+  }
+
+  template <typename OutputValueType, typename PropertyMap, typename T,
+            typename NextPropertyBinder, typename ... PropertyMapBinders>
+  void process_properties (const LASpoint& reader, OutputValueType& new_element,
+                           std::pair<PropertyMap, Las::Property<T> >& current,
+                           NextPropertyBinder& next,
+                           PropertyMapBinders&& ... properties)
+  {
+    T new_value = T();
+    // TODO
+    put (current.first, new_element, new_value);
+    process_properties (reader, new_element, next, properties...);
+  }
   
 } // namespace internal
   
 
 /// \endcond
 
+}
 
+template <typename OutputIteratorValueType,
+          typename OutputIterator,
+          typename ... PropertyHandler>
+bool read_las_points_with_properties (std::istream& stream,
+                                      OutputIterator output,
+                                      PropertyHandler&& ... properties)
+{
+  typedef OutputIteratorValueType Enriched_point;
 
+  LASreaderLAS lasreader;
+  lasreader.open(stream);
+
+  while (lasreader.read_point())
+    {
+      const LASpoint& laspoint = lasreader.point;
+      Enriched_point new_point;
+
+      Las::internal::process_properties (laspoint, new_point, properties...);
+
+      *(output ++) = new_point;
+    }
+
+  lasreader.close();
+  
+  return true;
+
+}
+
+template <typename OutputIterator,
+          typename ... PropertyHandler>
+bool read_las_points_with_properties (std::istream& stream,
+                                      OutputIterator output,
+                                      PropertyHandler&& ... properties)
+{
+  typedef typename value_type_traits<OutputIterator>::type OutputValueType;
+
+  return read_las_points_with_properties<OutputValueType>
+    (stream, output, properties...);
+}
 //===================================================================================
 /// \ingroup PkgPointSetProcessing
 /// Reads points (positions + normals, if available) from a .las
@@ -90,7 +265,29 @@ bool read_las_points_and_normals(std::istream& stream, ///< input stream.
                                  NormalPMap normal_pmap, ///< property map: value_type of OutputIterator -> Vector_3.
                                  const Kernel& kernel) ///< geometric traits.
 {
+  typedef typename Kernel::FT FT;
+  typedef typename Kernel::Point_3 Point;
+  typedef typename Kernel::Vector_3 Vector;
+  typedef OutputIteratorValueType Enriched_point;
 
+  LASreaderLAS lasreader;
+  lasreader.open(stream);
+
+  while (lasreader.read_point())
+    {
+      const LASpoint& laspoint = lasreader.point;
+      FT x = FT(laspoint.get_x());
+      FT y = FT(laspoint.get_y());
+      FT z = FT(laspoint.get_z());
+
+      Enriched_point new_point;
+      Point p (x, y, z);
+      put (point_pmap, new_point, p);
+      *(output ++) = new_point;
+    }
+
+  lasreader.close();
+  
   return true;
 }
 
