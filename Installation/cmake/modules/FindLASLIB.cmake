a79d7be088eed5c33d373c0d63c835a0f4a34449
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-01-19 15:13:34 +0100

    New example for LAS reader

---
+++ Installation/cmake/modules/FindLASLIB.cmake
@@ -0,0 +1,32 @@
+# - Try to find LASLIB
+# Once done this will define
+#
+#  LASLIB_FOUND = LASLIB_FOUND - TRUE
+#  LASLIB_INCLUDE_DIR - include directory for LASlib
+#  LASLIB_LASZIP_INCLUDE_DIR - include directory for LASzip
+#  LASLIB_LIBRARIES   - the libraries (as targets)
+
+# first look in user defined locations
+find_path(LASLIB_INCLUDE_DIR
+          NAMES lasreader.hpp
+          PATHS /usr/local/include/LASlib/inc
+         )
+
+find_path(LASLIB_LASZIP_INCLUDE_DIR
+          NAMES mydefs.hpp
+          PATHS /usr/local/include/LASzip/src
+                ${LASLIB_INCLUDE_DIR}/../../LASzip/src
+         )
+
+find_library(LASLIB_LIBRARIES
+             NAMES LASlib
+             PATHS ENV LD_LIBRARY_PATH
+                   ENV LIBRARY_PATH
+                   /usr/local/lib
+                   ${LASLIB_INCLUDE_DIR}/../lib
+            )
+
+if(LASLIB_LIBRARIES)
+  set(LASLIB_FOUND TRUE)
+endif()
+
---
+++ Point_set_processing_3/examples/Point_set_processing_3/read_las_example.cpp
@@ -0,0 +1,41 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/property_map.h>
+#include <CGAL/IO/read_las_points.h>
+
+#include <utility>
+#include <vector>
+#include <fstream>
+
+// types
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::FT FT;
+typedef Kernel::Point_3 Point;
+
+int main(int argc, char*argv[])
+{
+  const char* fname = (argc>1) ? argv[1] : "data/pig_points.las";
+  // Reads a .ply point set file with normal vectors and colors
+
+  std::vector<Point> points; // store points
+  std::ifstream in(fname);
+
+  if (!in ||
+      !CGAL::read_las_points_with_properties
+      (in,
+       std::back_inserter (points),
+       CGAL::cpp11::make_tuple
+       (CGAL::Identity_property_map<Point>(),
+        Kernel::Construct_point_3(),
+        CGAL::Las::X(),
+        CGAL::Las::Y(),
+        CGAL::Las::Z())))
+    {
+      std::cerr << "Error: cannot read file " << fname << std::endl;
+      return EXIT_FAILURE;
+    }
+
+  for (std::size_t i = 0; i < points.size(); ++ i)
+    std::cout << points[i] << std::endl;
+  
+  return EXIT_SUCCESS;
+}
