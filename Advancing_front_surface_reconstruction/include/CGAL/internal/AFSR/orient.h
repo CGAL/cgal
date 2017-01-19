e28842cf763b0de46fab2fe3f5fa6e2df24a684b
Sébastien Loriot <sebastien.loriot@cgal.org>
2017-01-19 13:29:19 +0100

    add missing include guards

--- Advancing_front_surface_reconstruction/include/CGAL/internal/AFSR/orient.h
+++ Advancing_front_surface_reconstruction/include/CGAL/internal/AFSR/orient.h
@@ -17,7 +17,8 @@
 //
 // Author(s)     : Frank Da, David Cohen-Steiner, Andreas Fabri
 
-#ifndef CGAL_AFSR_ORIENT
+#ifndef CGAL_AFSR_ORIENT_H
+#define CGAL_AFSR_ORIENT_H
 
 namespace CGAL {
   namespace AFSR {
@@ -136,4 +137,4 @@ namespace CGAL {
   } // namespace AFSR
 } // namespace CGAL
 
-#endif //CGAL_AFSR_ORIENT
+#endif //CGAL_AFSR_ORIENT_H
