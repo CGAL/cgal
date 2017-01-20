11ee9056504ae5fc0f382cde1c069e6ee7675fd9
Sébastien Loriot <sebastien.loriot@cgal.org>
2017-01-20 16:32:59 +0100

    remove useless test files

--- Installation/test/Installation/license_check.cpp
+++
@@ -1,8 +0,0 @@
-// #define CGAL_LGPL_COMMERCIAL_LICENSE 0
-// #define CGAL_LICENSE_WARNING
-// #define CGAL_LICENSE_ERROR
-// #define CGAL_LGPL_LICENSE_WARNING
-// #define CGAL_LGPL_LICENSE_ERROR
-#include <CGAL/config.h>
-
-int main() {}
--- Installation/test/Installation/license_check_gpl.cpp
+++
@@ -1,6 +0,0 @@
-// #define CGAL_AABB_TREE_COMMERCIAL_LICENSE 0
-// #define CGAL_LICENSE_WARNING
-// #define CGAL_LICENSE_ERROR
-#include <CGAL/AABB_tree.h>
-
-int main() {}
