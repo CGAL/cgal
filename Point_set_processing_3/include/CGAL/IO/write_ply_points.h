5663a6836fdff8fc10f69cd8be05a3b040fef3fb
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-01-23 15:36:52 +0100

    Change API of PLY writer similarly to PLY reader

--- Point_set_processing_3/include/CGAL/IO/write_ply_points.h
+++ Point_set_processing_3/include/CGAL/IO/write_ply_points.h
@@ -25,6 +25,7 @@
 
 #include <CGAL/property_map.h>
 #include <CGAL/point_set_processing_assertions.h>
+#include <CGAL/IO/read_ply_points.h>
 
 #include <boost/version.hpp>
 
@@ -32,6 +33,257 @@
 #include <iterator>
 
 namespace CGAL {
+
+namespace Ply {
+
+  /**
+     \ingroup PkgPointSetProcessing
+     
+     Generates a PLY property handler to write 3D points. Points are
+     written as 3 PLY properties of type `double` and named `x`, `y`
+     and `z`.
+
+     \sa `write_ply_points_with_properties()`
+
+     \tparam PointMap the property map used to store points.
+  */
+  template <typename PointMap>
+  cpp11::tuple<PointMap, Property<double>, Property<double>, Property<double> >
+  point_writer(PointMap point_map)
+  {
+    return cpp11::make_tuple (point_map, Property<double>("x"), Property<double>("y"), Property<double>("z"));
+  }
+
+  /**
+     \ingroup PkgPointSetProcessing
+     
+     Generates a PLY property handler to write 3D normal
+     vectors. Vectors are written as 3 PLY properties of type `double`
+     and named `nx`, `ny` and `nz`.
+
+     \sa `write_ply_points_with_properties()`
+
+     \tparam VectorMap the property map used to store vectors.
+  */
+  template <typename VectorMap>
+  cpp11::tuple<VectorMap, Property<double>, Property<double>, Property<double> >
+  normal_writer(VectorMap normal_map)
+  {
+    return cpp11::make_tuple (normal_map, Property<double>("nx"), Property<double>("ny"), Property<double>("nz"));
+  }
+
+
+namespace internal {
+
+  template <typename T> void property_header_type (std::ostream& stream) { stream << "unknown"; }
+  template <> void property_header_type<double> (std::ostream& stream) { stream << "double"; }
+  template <> void property_header_type<int> (std::ostream& stream) { stream << "int"; }
+  template <> void property_header_type<unsigned char> (std::ostream& stream) { stream << "uchar"; }
+  
+  template <typename T>
+  void property_header (std::ostream& stream, const Ply::Property<T>& prop)
+  {
+    stream << "property ";
+    property_header_type<T>(stream);
+    stream << " " << prop.name << std::endl;
+  }
+
+  
+  template <std::size_t N>
+  struct Properties_header
+  {
+    template <class Ply_property_tuple>
+    static void write(std::ostream& stream, Ply_property_tuple& wrappers)
+    {
+      Properties_header<N-1>::write(stream, wrappers);
+      property_header (stream, std::get<N+1>(wrappers));
+    }
+  };
+  template <>
+  struct Properties_header<0>
+  {
+    template <class Ply_property_tuple>
+    static void write(std::ostream& stream, Ply_property_tuple& wrappers)
+    {
+      property_header (stream, std::get<1>(wrappers));
+    }
+  };
+
+  template <typename PropertyMap,
+            typename ... T>
+  void output_property_header (std::ostream& stream,
+                               cpp11::tuple<PropertyMap, Ply::Property<T>... >& current)
+  {
+    Properties_header<sizeof...(T)-1>::write(stream, current); 
+  }
+
+
+  template <typename PropertyMap,
+            typename T>
+  void output_property_header (std::ostream& stream,
+                               std::pair<PropertyMap, Ply::Property<T> >& current)
+  {
+    property_header (stream, current.second);
+  }
+
+  template <typename PropertyMap,
+            typename T,
+            typename NextPropertyHandler,
+            typename ... PropertyHandler>
+  void output_property_header (std::ostream& stream,
+                               std::pair<PropertyMap, Ply::Property<T> >& current,
+                               NextPropertyHandler& next,
+                               PropertyHandler&& ... properties)
+  {
+    property_header (stream, current.second);
+    output_property_header (stream, next, properties...);
+  }
+  template <typename PropertyMap,
+            typename ... T,
+            typename NextPropertyHandler,
+            typename ... PropertyHandler>
+  void output_property_header (std::ostream& stream,
+                               cpp11::tuple<PropertyMap, Ply::Property<T>... >& current,
+                               NextPropertyHandler& next,
+                               PropertyHandler&& ... properties)
+  {
+    Properties_header<sizeof...(T)-1>::write(stream, current); 
+    output_property_header (stream, next, properties...);
+  }
+
+
+  template <bool Binary,
+            typename ForwardIterator,
+            typename PropertyMap>
+  void property_write (std::ostream& stream, ForwardIterator it, PropertyMap map)
+  {
+    if (Binary)
+      {
+        typedef typename PropertyMap::value_type Value_type;
+        Value_type out = get(map, *it);
+        stream.write (reinterpret_cast<char*>(&out), sizeof (Value_type));
+      }
+    else
+      stream << get (map, *it);
+  }
+  
+  template <bool Binary,
+            typename ForwardIterator,
+            typename PropertyMap,
+            typename ... T>
+  void output_properties (std::ostream& stream,
+                          ForwardIterator it,
+                          cpp11::tuple<PropertyMap, Ply::Property<T>... >& current)
+  {
+    property_write<Binary> (stream, it, std::get<0>(current));
+    if (!Binary)
+      stream << std::endl;
+  }
+
+
+  template <bool Binary,
+            typename ForwardIterator,
+            typename PropertyMap,
+            typename T>
+  void output_properties (std::ostream& stream,
+                          ForwardIterator it,
+                          std::pair<PropertyMap, Ply::Property<T> >& current)
+  {
+    property_write<Binary> (stream, it, current.first);
+    if (!Binary)
+      stream << std::endl;
+  }
+
+  template <bool Binary,
+            typename ForwardIterator,
+            typename PropertyMap,
+            typename T,
+            typename NextPropertyHandler,
+            typename ... PropertyHandler>
+  void output_properties (std::ostream& stream,
+                          ForwardIterator it,
+                          std::pair<PropertyMap, Ply::Property<T> >& current,
+                          NextPropertyHandler& next,
+                          PropertyHandler&& ... properties)
+  {
+    property_write<Binary> (stream, it, current.first);
+    if (!Binary)
+      stream << " ";
+    output_properties<Binary> (stream, it, next, properties...);
+  }
+  
+  template <bool Binary,
+            typename ForwardIterator,
+            typename PropertyMap,
+            typename ... T,
+            typename NextPropertyHandler,
+            typename ... PropertyHandler>
+  void output_properties (std::ostream& stream,
+                          ForwardIterator it,
+                          cpp11::tuple<PropertyMap, Ply::Property<T>... >& current,
+                          NextPropertyHandler& next,
+                          PropertyHandler&& ... properties)
+  {
+    property_write<Binary> (stream, it, std::get<0>(current));
+    if (!Binary)
+      stream << " ";
+    output_properties<Binary> (stream, it, next, properties...);
+  }
+}
+  
+}    
+
+template < bool Binary,
+           typename ForwardIterator,
+           typename ... PropertyHandler>
+bool
+write_ply_points_with_properties(
+  std::ostream& stream, ///< output stream.
+  ForwardIterator first,  ///< iterator over the first input point.
+  ForwardIterator beyond, ///< past-the-end iterator over the input points.
+  PropertyHandler&& ... properties)
+{
+  CGAL_point_set_processing_precondition(first != beyond);
+
+  if(!stream)
+  {
+    std::cerr << "Error: cannot open file" << std::endl;
+    return false;
+  }
+
+  // Write header
+  stream << "ply" << std::endl
+         << (Binary ? "format binary_little_endian 1.0" : "format ascii 1.0") << std::endl
+         << "comment Generated by the CGAL library" << std::endl
+         << "element vertex " << std::distance (first, beyond) << std::endl;
+  
+  Ply::internal::output_property_header (stream, properties...);
+  
+  stream << "end_header" << std::endl;
+  
+
+  // Write positions + normals
+  for(ForwardIterator it = first; it != beyond; it++)
+  {
+    Ply::internal::output_properties<Binary> (stream, it, properties...);
+  }
+
+  return ! stream.fail();
+
+}
+
+template < typename ForwardIterator,
+           typename ... PropertyHandler>
+bool
+write_ply_points_with_properties(
+  std::ostream& stream, ///< output stream.
+  ForwardIterator first,  ///< iterator over the first input point.
+  ForwardIterator beyond, ///< past-the-end iterator over the input points.
+  PropertyHandler&& ... properties)
+{
+  return write_ply_points_with_properties<true> (stream, first, beyond, properties...);
+}
+
 
 
 //===================================================================================
@@ -41,102 +293,71 @@ namespace CGAL {
 /// \pre normals must be unit vectors
 ///
 /// @tparam ForwardIterator iterator over input points.
-/// @tparam PointPMap is a model of `ReadablePropertyMap` with  value type `Point_3<Kernel>`.
+/// @tparam PointMap is a model of `ReadablePropertyMap` with  value type `Point_3<Kernel>`.
 ///        It can be omitted if the value type of `ForwardIterator` is convertible to `Point_3<Kernel>`.
-/// @tparam NormalPMap is a model of `ReadablePropertyMap` with a value type  `Vector_3<Kernel>`.
+/// @tparam VectorMap is a model of `ReadablePropertyMap` with a value type  `Vector_3<Kernel>`.
 /// @tparam Kernel Geometric traits class.
-///        It can be omitted and deduced automatically from the value type of `PointPMap`.
+///        It can be omitted and deduced automatically from the value type of `PointMap`.
 ///
 /// @return true on success.
 
 // This variant requires all parameters.
-template < typename ForwardIterator,
-           typename PointPMap,
-           typename NormalPMap,
-           typename Kernel >
+template < bool Binary,
+           typename ForwardIterator,
+           typename PointMap,
+           typename VectorMap >
 bool
 write_ply_points_and_normals(
   std::ostream& stream, ///< output stream.
   ForwardIterator first,  ///< iterator over the first input point.
   ForwardIterator beyond, ///< past-the-end iterator over the input points.
-  PointPMap point_pmap, ///< property map: value_type of ForwardIterator -> Point_3. 
-  NormalPMap normal_pmap, ///< property map: value_type of ForwardIterator -> Vector_3. 
-  const Kernel& /*kernel*/) ///< geometric traits.
+  PointMap point_map, ///< property map: value_type of ForwardIterator -> Point_3. 
+  VectorMap normal_map) ///< property map: value_type of ForwardIterator -> Vector_3. 
 {
-  CGAL_point_set_processing_precondition(first != beyond);
-
-  if(!stream)
-  {
-    std::cerr << "Error: cannot open file" << std::endl;
-    return false;
-  }
-
-  // Write header
-  stream << "ply" << std::endl
-         << "format ascii 1.0" << std::endl
-         << "comment Generated by the CGAL library" << std::endl
-         << "element vertex " << std::distance (first, beyond) << std::endl
-         << "property double x" << std::endl
-         << "property double y" << std::endl
-         << "property double z" << std::endl
-         << "property double nx" << std::endl
-         << "property double ny" << std::endl
-         << "property double nz" << std::endl
-         << "end_header" << std::endl;
-  
-
-  // Write positions + normals
-  for(ForwardIterator it = first; it != beyond; it++)
-  {
-    stream << get(point_pmap, *it) << " "
-           << get(normal_pmap, *it) << std::endl;
-  }
-
-  return ! stream.fail();
+  return write_ply_points_with_properties<Binary> (stream, first, beyond,
+                                                   Ply::point_writer (point_map),
+                                                   Ply::normal_writer (normal_map));
 }
 
 /// @cond SKIP_IN_MANUAL
 // This variant deduces the kernel from the point property map.
 template < typename ForwardIterator,
-           typename PointPMap,
-           typename NormalPMap >
+           typename PointMap,
+           typename VectorMap >
 bool
 write_ply_points_and_normals(
   std::ostream& stream, ///< output stream.
   ForwardIterator first, ///< first input point.
   ForwardIterator beyond, ///< past-the-end input point.
-  PointPMap point_pmap, ///< property map: value_type of OutputIterator -> Point_3.
-  NormalPMap normal_pmap) ///< property map: value_type of OutputIterator -> Vector_3.
+  PointMap point_map, ///< property map: value_type of OutputIterator -> Point_3.
+  VectorMap normal_map) ///< property map: value_type of OutputIterator -> Vector_3.
 {
-  typedef typename boost::property_traits<PointPMap>::value_type Point;
-  typedef typename Kernel_traits<Point>::Kernel Kernel;
-  return write_ply_points_and_normals(
+  return write_ply_points_and_normals<true>(
     stream,
     first, beyond,
-    point_pmap,
-    normal_pmap,
-    Kernel());
+    point_map,
+    normal_map);
 }
 /// @endcond
 
 /// @cond SKIP_IN_MANUAL
 // This variant creates a default point property map = Identity_property_map.
 template <typename ForwardIterator,
-          typename NormalPMap
+          typename VectorMap
 >
 bool
 write_ply_points_and_normals(
   std::ostream& stream, ///< output stream.
   ForwardIterator first, ///< first input point.
   ForwardIterator beyond, ///< past-the-end input point.
-  NormalPMap normal_pmap) ///< property map: value_type of OutputIterator -> Vector_3.
+  VectorMap normal_map) ///< property map: value_type of OutputIterator -> Vector_3.
 {
   return write_ply_points_and_normals(
     stream,
     first, beyond,
     make_identity_property_map(
     typename std::iterator_traits<ForwardIterator>::value_type()),
-    normal_pmap);
+    normal_map);
 }
 /// @endcond
 
@@ -146,74 +367,40 @@ write_ply_points_and_normals(
 /// Saves the [first, beyond) range of points (positions only) to a .ply ASCII stream.
 ///
 /// @tparam ForwardIterator iterator over input points.
-/// @tparam PointPMap is a model of `ReadablePropertyMap` with a value_type = `Point_3<Kernel>`.
+/// @tparam PointMap is a model of `ReadablePropertyMap` with a value_type = `Point_3<Kernel>`.
 ///        It can be omitted if the value type of `ForwardIterator` is convertible to `Point_3<Kernel>`.
 /// @tparam Kernel Geometric traits class.
-///        It can be omitted and deduced automatically from the value type of `PointPMap`.
+///        It can be omitted and deduced automatically from the value type of `PointMap`.
 ///
 /// @return true on success.
 
 // This variant requires all parameters.
-template < typename ForwardIterator,
-           typename PointPMap,
-           typename Kernel >
+template < bool Binary,
+           typename ForwardIterator,
+           typename PointMap >
 bool
 write_ply_points(
   std::ostream& stream, ///< output stream.
   ForwardIterator first,  ///< iterator over the first input point.
   ForwardIterator beyond, ///< past-the-end iterator over the input points.
-  PointPMap point_pmap, ///< property map: value_type of ForwardIterator -> Point_3.
-  const Kernel& ) ///< geometric traits.
+  PointMap point_map) ///< property map: value_type of ForwardIterator -> Point_3.
 {
-  // basic geometric types
-  typedef typename Kernel::Point_3 Point;
-
-  CGAL_point_set_processing_precondition(first != beyond);
-
-  if(!stream)
-  {
-    std::cerr << "Error: cannot open file" << std::endl;
-    return false;
-  }
-
-  // Write header
-  stream << "ply" << std::endl
-         << "format ascii 1.0" << std::endl
-         << "comment Generated by the CGAL library" << std::endl
-         << "element vertex " << std::distance (first, beyond) << std::endl
-         << "property double x" << std::endl
-         << "property double y" << std::endl
-         << "property double z" << std::endl
-         << "end_header" << std::endl;
-
-  // Write positions
-  for(ForwardIterator it = first; it != beyond; it++)
-  {
-    Point p = get(point_pmap, *it);
-    stream << p << std::endl;
-  }
-
-  return ! stream.fail();
+  return write_ply_points_with_properties<Binary> (stream, first, beyond,
+                                                   Ply::point_writer (point_map));
 }
 
 /// @cond SKIP_IN_MANUAL
 // This variant deduces the kernel from the point property map.
 template < typename ForwardIterator,
-           typename PointPMap >
+           typename PointMap >
 bool
 write_ply_points(
   std::ostream& stream, ///< output stream.
   ForwardIterator first, ///< first input point.
   ForwardIterator beyond, ///< past-the-end input point.
-  PointPMap point_pmap) ///< property map: value_type of OutputIterator -> Point_3.
+  PointMap point_map) ///< property map: value_type of OutputIterator -> Point_3.
 {
-  typedef typename boost::property_traits<PointPMap>::value_type Point;
-  typedef typename Kernel_traits<Point>::Kernel Kernel;
-  return write_ply_points(
-    stream,
-    first, beyond,
-    point_pmap,
-    Kernel());
+  return write_ply_points<true> (stream, first, beyond, point_map);
 }
 /// @endcond
 
