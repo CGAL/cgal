e3d0e11b1e37baf1249b60f888922da5284356ba
Maxime Gimeno <maxime.gimeno@gmail.com>
2017-02-01 11:03:52 +0100

    Fix headers

--- Surface_mesher/doc/Surface_mesher/CGAL/IO/Complex_2_in_triangulation_3_file_writer.h
+++ Surface_mesher/doc/Surface_mesher/CGAL/IO/Complex_2_in_triangulation_3_file_writer.h
@@ -29,5 +29,15 @@ bool output_surface_facets_to_off (std::ostream& os,
 				   int options = 
 				   Surface_mesher::IO_ORIENT_SURFACE);
 
+ namespace Surface_mesher {
+/*!
+\ingroup PkgSurfaceMesher3FunctionsIO
+ Options for function `output_surface_facets_to_off()`.
+*/
+    enum IO_option { NO_OPTION = 0,
+                     IO_ORIENT_SURFACE = 1,
+                     IO_VERBOSE = 2 };
+    
+  } /* namespace Surface_mesher */
 } /* namespace CGAL */
 
