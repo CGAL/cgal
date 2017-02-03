0de5ac7b11486c4c39c1872cd82def787b7b599f
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-02-03 18:12:50 +0100

    Fixed P3RT3 spatial sort
    Can't use the underlying Kernel anymore since ::Point is a Weighted_Point_3
    and it doesn't match Point_2/Point_3/Point_d in spatial_sort.h

--- Periodic_3_triangulation_3/include/CGAL/Periodic_3_regular_triangulation_3.h
+++ Periodic_3_triangulation_3/include/CGAL/Periodic_3_regular_triangulation_3.h
@@ -482,10 +482,7 @@ public:
           return number_of_vertices() - n;
       }
 
-    // Use Geom_traits::K for efficiency: spatial_sort creates a lot
-    // of copies of the traits but does not need the domain that is
-    // stored in it.
-    spatial_sort(pbegin, points.end(), typename Geom_traits::K());
+    spatial_sort(pbegin, points.end(), geom_traits());
 
     Conflict_tester tester(*pbegin, this);
     Point_hider hider(this);
