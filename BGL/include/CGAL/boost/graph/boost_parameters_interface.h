3d8aaf0a6eddcf95c1cd5241c5ee33e202c5c78a
Maxime Gimeno <maxime.gimeno@gmail.com>
2017-02-09 15:46:01 +0100

    Add license headers

--- BGL/include/CGAL/boost/graph/boost_parameters_interface.h
+++ BGL/include/CGAL/boost/graph/boost_parameters_interface.h
@@ -1,6 +1,22 @@
-#ifndef CGAL_add_pmp_parameter
-#  define CGAL_add_pmp_parameter(X, Y, Z)
-#endif
+// Copyright (c) 2017  GeometryFactory (France).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+//
+// Author(s)     : Maxime Gimeno
+
 
 CGAL_add_pmp_parameter(vertex_point_t, vertex_point_map, vertex_point_map)
 CGAL_add_pmp_parameter(face_index_t, face_index_map, face_index_map)
