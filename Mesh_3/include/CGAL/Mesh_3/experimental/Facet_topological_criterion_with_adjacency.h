09d09a4dc3670c4fd06d4939193ebecd92708c73
Jane Tournois <jane.tournois@geometryfactory.com>
2017-02-09 16:18:36 +0100

    update license preamble

--- Mesh_3/include/CGAL/Mesh_3/experimental/Facet_topological_criterion_with_adjacency.h
+++ Mesh_3/include/CGAL/Mesh_3/experimental/Facet_topological_criterion_with_adjacency.h
@@ -1,9 +1,10 @@
-// Copyright (c) 2012 GeometryFactory Sarl (France)
+// Copyright (c) 2012-2016 GeometryFactory Sarl (France).
 // All rights reserved.
 //
-// This file is part of CGAL (www.cgal.org); you may redistribute it under
-// the terms of the Q Public License version 1.0.
-// See the file LICENSE.QPL distributed with CGAL.
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
 //
 // Licensees holding a valid commercial license may use this file in
 // accordance with the commercial license agreement provided with the software.
@@ -11,8 +12,16 @@
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
+// $URL$
+// $Id$
 //
 // Author(s)     : Laurent Rineau
+//
+//******************************************************************************
+// File Description :
+//
+//
+//******************************************************************************
 
 #ifndef CGAL_MESH_3_FACET_TOPOLOGICAL_CRITERION_WITH_ADJACENCY_H
 #define CGAL_MESH_3_FACET_TOPOLOGICAL_CRITERION_WITH_ADJACENCY_H
