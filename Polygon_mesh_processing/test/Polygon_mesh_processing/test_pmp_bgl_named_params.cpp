7c51ddc60604ea0ac581a7ab65f1a4e69b1e2438
Sébastien Loriot <sebastien.loriot@cgal.org>
2017-02-13 10:28:27 +0100

    remove commented lines

--- Polygon_mesh_processing/test/Polygon_mesh_processing/test_pmp_bgl_named_params.cpp
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/test_pmp_bgl_named_params.cpp
@@ -21,19 +21,9 @@ void test_np(const NamedParameters& np)
 {
 // test values
   assert(   get_param(np,CGAL::internal_np::vertex_index).v == 0 );
-//  assert(   get_param(np,boost::halfedge_index).v == 1 );
   assert(   get_param(np,CGAL::internal_np::face_index).v == 2 );
   assert(   get_param(np,CGAL::internal_np::vertex_point).v == 3 );
-//  assert(   get_param(np,boost::edge_index).v == 5 );
-//  assert(   get_param(np,boost::graph_visitor).v == 6 );
-//  assert(   get_param(np,CGAL::set_cache_policy).v == 7 );
-//  assert(   get_param(np,CGAL::get_cost_policy).v == 8 );
-//  assert(   get_param(np,CGAL::get_cost_policy_params).v == 9 );
-//  assert(   get_param(np,CGAL::get_placement_policy).v == 10 );
-//  assert(   get_param(np,CGAL::get_placement_policy_params).v == 11 );
   assert(   get_param(np,CGAL::internal_np::edge_is_constrained).v == 12 );
-//  assert(   get_param(np,CGAL::edge_is_constrained_params).v == 13 );
-  //
   assert(   get_param(np,CGAL::internal_np::density_control_factor).v == 14 );
   assert(   get_param(np,CGAL::internal_np::use_delaunay_triangulation).v == 15 );
   assert(   get_param(np,CGAL::internal_np::fairing_continuity).v == 16 );
@@ -42,18 +32,9 @@ void test_np(const NamedParameters& np)
 
 //test types
   check_same_type<0>( get_param(np,CGAL::internal_np::vertex_index) );
-//  check_same_type<1>( get_param(np,boost::halfedge_index) );
   check_same_type<2>( get_param(np,CGAL::internal_np::face_index) );
   check_same_type<3>( get_param(np,CGAL::internal_np::vertex_point) );
-//  check_same_type<5>( get_param(np,boost::edge_index) );
-//  check_same_type<6>( get_param(np,boost::graph_visitor) );
-//  check_same_type<7>( get_param(np,CGAL::set_cache_policy) );
-//  check_same_type<8>( get_param(np,CGAL::get_cost_policy) );
-//  check_same_type<9>( get_param(np,CGAL::get_cost_policy_params) );
-//  check_same_type<10>( get_param(np,CGAL::get_placement_policy) );
-//  check_same_type<11>( get_param(np,CGAL::get_placement_policy_params) );
   check_same_type<12>( get_param(np,CGAL::internal_np::edge_is_constrained) );
-//  check_same_type<13>( get_param(np,CGAL::edge_is_constrained_params) );
   //
   check_same_type<14>( get_param(np,CGAL::internal_np::density_control_factor) );
   check_same_type<15>( get_param(np,CGAL::internal_np::use_delaunay_triangulation) );
@@ -68,16 +49,8 @@ int main()
   // start with inherited named params
     CGAL::Polygon_mesh_processing::parameters::
                       vertex_index_map(A<0>(0)).
-//                      halfedge_index_map(A<1>(1)).
                       face_index_map(A<2>(2)).
                       vertex_point_map(A<3>(3)).
-//                      edge_index_map(A<5>(5)).
-//                      visitor(A<6>(6)).
-//                      set_cache(A<7>(7)).
-//                      get_cost(A<8>(8)).
-//                      get_cost_params(A<9>(9)).
-//                      get_placement(A<10>(10)).
-//                      get_placement_params(A<11>(11)).
                       edge_is_constrained_map(A<12>(12)).
   // continue with PMP specific named params
                       density_control_factor(A<14>(14)).
