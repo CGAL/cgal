89b6c64cdfe11cb91f155734ff61373f2f165e9e
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-03-02 16:09:26 +0100

    Add missing concept

---
+++ Classification/doc/Classification/Concepts/NeighborQuery.h
@@ -0,0 +1,35 @@
+namespace CGAL
+{
+
+/*!
+\ingroup PkgClassificationConcepts
+\cgalConcept
+
+Concept describing a neighbor query used for classification.
+
+\cgalHasModel `CGAL::Classification::Point_set_neighborhood::K_neighbor_query`
+\cgalHasModel `CGAL::Classification::Point_set_neighborhood::Range_neighbor_query`
+
+*/
+class NeighborQuery
+{
+public:
+
+  /*!
+    \brief Type of the data that is classified.
+  */
+  typedef unspecified_type value_type;
+
+  /*!
+
+    \brief Puts in `output` the indices of the neighbors of `query`.
+
+    \tparam OutputIterator An output iterator accepting `std::size_t`
+    values.
+   */
+  template <typename OutputIterator>
+  OutputIterator operator() (const value_type& query, OutputIterator output) const;
+
+};
+
+} // namespace CGAL
