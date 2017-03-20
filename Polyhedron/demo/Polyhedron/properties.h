6cd546a4db87c80e5a7ca4464277d8d65c8a325e
Andreas Fabri <andreas.fabri@geometryfactory.com>
2017-03-20 16:36:00 +0100

    Add properties.h

---
+++ Polyhedron/demo/Polyhedron/properties.h
@@ -0,0 +1,11 @@
+#ifndef POLYHEDRON_DEMO_PROPERTIES_H
+#define POLYHEDRON_DEMO_PROPERTIES_H
+
+struct halfedge_is_feature_t {};
+struct face_patch_id_t {};
+struct vertex_selection_t {};
+struct face_selection_t {};
+struct edge_selection_t {};
+struct vertex_num_feature_edges_t {};
+#endif POLYHEDRON_DEMO_PROPERTIES_H
+
