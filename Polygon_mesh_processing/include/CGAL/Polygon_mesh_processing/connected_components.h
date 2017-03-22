d04b048aa986c6d8a0c415f833cabb80e110efbd
Andreas Fabri <andreas.fabri@geometryfactory.com>
2017-03-22 09:21:27 +0100

    License check for Connected Components

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/connected_components.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/connected_components.h
@@ -21,7 +21,7 @@
 #ifndef CGAL_POLYGON_MESH_PROCESSING_CONNECTED_COMPONENTS_H
 #define CGAL_POLYGON_MESH_PROCESSING_CONNECTED_COMPONENTS_H
 
-#include <CGAL/license/Polygon_mesh_processing.h>
+#include <CGAL/license/Polygon_mesh_processing/connected_components.h>
 
 
 #include<set>
