c4f5122409ccd61c997511f895bb3c0314a70f3c
Andreas Fabri <andreas.fabri@geometryfactory.com>
2017-03-22 14:54:12 +0100

    Undo the accidental switching on of CGAL_LICENSE_WARNING

--- Installation/include/CGAL/license.h
+++ Installation/include/CGAL/license.h
@@ -20,7 +20,7 @@
 #ifndef CGAL_LICENSE_CHECK_H
 #define CGAL_LICENSE_CHECK_H
 
-#define CGAL_LICENSE_WARNING  1   // in order to get a warning during compilation
+//#define CGAL_LICENSE_WARNING  1   // in order to get a warning during compilation
 //#define CGAL_LICENSE_ERROR    1   // in order to get an error during compilation
 
 // if no such define exists, the package is used under the GPL or LGPL
