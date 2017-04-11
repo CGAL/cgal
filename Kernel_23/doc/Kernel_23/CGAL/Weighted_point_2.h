e95d3d3f2b521039b82ed13694456eb24e9eb91d
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-04-11 17:03:08 +0200

    Make all conversions between Point and Weighted_point explicit
    Also doc changes

--- Kernel_23/doc/Kernel_23/CGAL/Weighted_point_2.h
+++ Kernel_23/doc/Kernel_23/CGAL/Weighted_point_2.h
@@ -47,6 +47,9 @@ public:
 
   /*!
   introduces a weighted point from point `p` and weight `0`.
+
+  \warning The `explicit` keyword is used to not have accidental implicit conversions
+           between Point_2 and Weighted_point_2.
   */
   Weighted_point_2(const Point_2<Kernel>& p);
 
