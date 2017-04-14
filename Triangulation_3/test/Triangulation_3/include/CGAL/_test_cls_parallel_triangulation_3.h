04c8e53c5ea3e328f747d5d5170c2ee8945be53c
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-04-14 11:58:54 +0200

    Fixed missing typename

--- Triangulation_3/test/Triangulation_3/include/CGAL/_test_cls_parallel_triangulation_3.h
+++ Triangulation_3/test/Triangulation_3/include/CGAL/_test_cls_parallel_triangulation_3.h
@@ -33,9 +33,10 @@ _test_cls_parallel_triangulation_3(const Parallel_triangulation &)
   const int NUM_INSERTED_POINTS = 5000;
 
   typedef Parallel_triangulation                                Cls;
+
   typedef typename Cls::Vertex_handle                           Vertex_handle;
- 
-  typedef Cls::Point Point;
+  typedef typename Cls::Point                                   Point;
+
   CGAL::Random_points_in_cube_3<Point> rnd(1.);
 
   // Construction from a vector of points
