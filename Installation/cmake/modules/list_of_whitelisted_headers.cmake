4d6149647d5be7a70336474a196eaeb5bf67abf5
Laurent Rineau <laurent.rineau@cgal.org>
2017-04-18 11:16:16 +0200

    <CGAL/CGAL_Ipelet_base.h> requires C++11, skip it
    That header needs C++11 at least, and that does not play well with the
    `check_headers` feature. So put it in the whitelist, so that it is not
    tested along with the other headers.

--- Installation/cmake/modules/list_of_whitelisted_headers.cmake
+++ Installation/cmake/modules/list_of_whitelisted_headers.cmake
@@ -1,5 +1,6 @@
 set(list_of_whitelisted_headers_txt [=[
-CGAL/Linear_cell_complex_constructors.h
+  CGAL/Linear_cell_complex_constructors.h
+  CGAL/CGAL_Ipelet_base.h
 ]=])
 
 separate_arguments(list_of_whitelisted_headers UNIX_COMMAND ${list_of_whitelisted_headers_txt})
