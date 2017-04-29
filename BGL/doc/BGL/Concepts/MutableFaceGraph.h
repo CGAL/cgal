5fbb7720a01ffc6de84409ea3f52a7bcbc11e4a9
Andreas Fabri <andreas.fabri@geometryfactory.com>
2017-04-29 11:29:28 +0200

    typos

--- BGL/doc/BGL/Concepts/MutableFaceGraph.h
+++ BGL/doc/BGL/Concepts/MutableFaceGraph.h
@@ -10,7 +10,7 @@ the requirement for operations to add faces and to modify face-halfedge relation
 \cgalHasModel `CGAL::Polyhedron_3`
 \cgalHasModel `CGAL::Surface_mesh`
 
-
+*/
 class MutableFaceGraph{};
 
 /*! \relates MutableFaceGraph
