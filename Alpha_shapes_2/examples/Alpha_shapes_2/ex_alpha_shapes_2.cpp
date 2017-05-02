0351d9061302ce161e88156146d9b2772af4e5d7
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-05-02 12:35:45 +0200

    Improved Alpha shapes 2 examples
    -- Added a weighted point data set
    -- Readability (indentation, typedefs, etc.)

--- Alpha_shapes_2/examples/Alpha_shapes_2/ex_alpha_shapes_2.cpp
+++ Alpha_shapes_2/examples/Alpha_shapes_2/ex_alpha_shapes_2.cpp
@@ -8,40 +8,36 @@
 #include <vector>
 #include <list>
 
+typedef CGAL::Exact_predicates_inexact_constructions_kernel  K;
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::FT                                                FT;
+typedef K::Point_2                                           Point;
+typedef K::Segment_2                                         Segment;
 
-typedef K::FT FT;
-typedef K::Point_2  Point;
-typedef K::Segment_2  Segment;
+typedef CGAL::Alpha_shape_vertex_base_2<K>                   Vb;
+typedef CGAL::Alpha_shape_face_base_2<K>                     Fb;
+typedef CGAL::Triangulation_data_structure_2<Vb,Fb>          Tds;
+typedef CGAL::Delaunay_triangulation_2<K,Tds>                Triangulation_2;
+typedef CGAL::Alpha_shape_2<Triangulation_2>                 Alpha_shape_2;
 
-typedef CGAL::Alpha_shape_vertex_base_2<K> Vb;
-typedef CGAL::Alpha_shape_face_base_2<K>  Fb;
-typedef CGAL::Triangulation_data_structure_2<Vb,Fb> Tds;
-typedef CGAL::Delaunay_triangulation_2<K,Tds> Triangulation_2;
-
-typedef CGAL::Alpha_shape_2<Triangulation_2>  Alpha_shape_2;
-typedef Alpha_shape_2::Alpha_shape_edges_iterator Alpha_shape_edges_iterator;
+typedef Alpha_shape_2::Alpha_shape_edges_iterator            Alpha_shape_edges_iterator;
 
 template <class OutputIterator>
-void
-alpha_edges( const Alpha_shape_2&  A,
-	     OutputIterator out)
+void alpha_edges( const Alpha_shape_2& A, OutputIterator out)
 {
-  for(Alpha_shape_edges_iterator it =  A.alpha_shape_edges_begin();
-      it != A.alpha_shape_edges_end();
-      ++it){
+  Alpha_shape_edges_iterator it = A.alpha_shape_edges_begin(),
+                             end = A.alpha_shape_edges_end();
+  for( ; it!=end; ++it)
     *out++ = A.segment(*it);
-  }
 }
 
 template <class OutputIterator>
-bool
-file_input(OutputIterator out)
+bool file_input(OutputIterator out)
 {
   std::ifstream is("./data/fin", std::ios::in);
 
-  if(is.fail()){
+  if(is.fail())
+  {
     std::cerr << "unable to open file for input" << std::endl;
     return false;
   }
@@ -54,22 +50,20 @@ file_input(OutputIterator out)
   return true;
 }
 
-
 // Reads a list of points and returns a list of segments
 // corresponding to the Alpha shape.
 int main()
 {
   std::list<Point> points;
-  if(! file_input(std::back_inserter(points))){
+  if(! file_input(std::back_inserter(points)))
     return -1;
-  }
 
   Alpha_shape_2 A(points.begin(), points.end(),
-		  FT(10000),
-		  Alpha_shape_2::GENERAL);
+                  FT(10000),
+                  Alpha_shape_2::GENERAL);
 
   std::vector<Segment> segments;
-  alpha_edges( A, std::back_inserter(segments));
+  alpha_edges(A, std::back_inserter(segments));
 
   std::cout << "Alpha Shape computed" << std::endl;
   std::cout << segments.size() << " alpha shape edges" << std::endl;
