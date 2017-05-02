9c02caf48b9fc416e6e392241c81afdd301e2263
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-05-02 15:54:20 +0200

    Fixed concept refinement relationships in Alpha_shapes_3 doc

--- Alpha_shapes_3/doc/Alpha_shapes_3/Concepts/FixedAlphaShapeVertex_3.h
+++ Alpha_shapes_3/doc/Alpha_shapes_3/Concepts/FixedAlphaShapeVertex_3.h
@@ -3,12 +3,13 @@
 \ingroup PkgAlphaShapes3Concepts
 \cgalConcept
 
-This concept describes the requirements for the base vertex of a alpha shape with a fixed value alpha. 
+The concept `FixedAlphaShapeVertex_3` describes the requirements for the base vertex of a alpha shape with a fixed value alpha.
 
-\cgalRefines `TriangulationVertexBase_3` 
+\cgalRefines `TriangulationVertexBase_3`, if the underlying triangulation of the alpha shape is a Delaunay triangulation.
+\cgalRefines `RegularTriangulationVertexBase_3`, if the underlying triangulation of the alpha shape is a regular triangulation.
+\cgalRefines `Periodic_3TriangulationDSVertexBase_3`, if the underlying triangulation of the alpha shape is a periodic triangulation.
 
-
-\cgalHasModel `CGAL::Fixed_alpha_shape_vertex_base_3`
+\cgalHasModel `CGAL::Fixed_alpha_shape_vertex_base_3` (templated with the appropriate triangulation vertex base class).
 */
 
 class FixedAlphaShapeVertex_3 {
