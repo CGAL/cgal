63611fe600fd07b9b3a3ebdfda320a79be8263cd
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-05-05 13:12:54 +0200

    Kinetic regular example must use the weighted points table

--- Kinetic_data_structures/examples/Kinetic_data_structures/Kinetic_regular_triangulation_3.cpp
+++ Kinetic_data_structures/examples/Kinetic_data_structures/Kinetic_regular_triangulation_3.cpp
@@ -7,24 +7,24 @@
 int main()
 {
     typedef CGAL::Kinetic::Regular_triangulation_exact_simulation_traits Traits;
-    typedef CGAL::Kinetic::Regular_triangulation_3<Traits> KDel;
+    typedef CGAL::Kinetic::Regular_triangulation_3<Traits>               KDel;
 
     Traits tr(0,100000.0);
     KDel kdel(tr);
 
-    Traits::Simulator::Handle sp= tr.simulator_handle();
+    Traits::Simulator::Handle sp = tr.simulator_handle();
 
     std::ifstream in("data/weighted_points_3");
     CGAL_assertion(in.good());
-    in >> *tr.active_points_3_table_handle();
+
+    in >> *tr.active_weighted_points_3_table_handle();
     CGAL_assertion(!in.fail());
 
-    std::cout << *tr.active_points_3_table_handle();
-
-    std::cout <<  *tr.active_points_3_table_handle() << std::endl;
+    std::cout << "weighted points: " << std::endl;
+    std::cout << *tr.active_weighted_points_3_table_handle() << std::endl;
 
     kdel.set_has_certificates(true);
-
     sp->set_current_event_number(10000);
+
     return EXIT_SUCCESS;
 }
