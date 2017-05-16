aaed0f6737364fe5e03bfb7a5dd69975dc07037a
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-05-16 17:49:32 +0200

    Fixed using default templated member functions

--- Triangulation_3/include/CGAL/Regular_triangulation_cell_base_3.h
+++ Triangulation_3/include/CGAL/Regular_triangulation_cell_base_3.h
@@ -96,6 +96,9 @@ public:
   void hide_point(const Point& p)
   { hide_point_internal<Memory_policy>(p); }
 
+  void unhide_point(const Point_iterator pit)
+  { unhide_point_internal<Memory_policy>(pit); }
+
   // Memory_policy is Tag_true -------------------------------------------------
   template<typename Tag>
   Point_iterator hidden_points_begin_internal(typename boost::enable_if_c<Tag::value>::type* = NULL)
@@ -114,8 +117,8 @@ public:
   template<typename Tag>
   void hide_point_internal(const Point& p, typename boost::enable_if_c<Tag::value>::type* = NULL)
   { _hidden.push_back(p); }
-  template<typename Tag = Memory_policy>
-  void unhide_point(const Point_iterator pit, typename boost::enable_if_c<Tag::value>::type* = NULL)
+  template<typename Tag>
+  void unhide_point_internal(const Point_iterator pit, typename boost::enable_if_c<Tag::value>::type* = NULL)
   { _hidden.erase(pit); }
 
   // Memory_policy is Tag_false ------------------------------------------------
@@ -137,8 +140,8 @@ public:
   template<typename Tag>
   void hide_point_internal(const Point&, typename boost::disable_if_c<Tag::value>::type* = NULL)
   { }
-  template<typename Tag = Memory_policy>
-  void unhide_point(const Point_iterator, typename boost::disable_if_c<Tag::value>::type* = NULL)
+  template<typename Tag>
+  void unhide_point_internal(const Point_iterator, typename boost::disable_if_c<Tag::value>::type* = NULL)
   { }
 
   //note this function is not requested by the RegularTriangulationCellBase_3
