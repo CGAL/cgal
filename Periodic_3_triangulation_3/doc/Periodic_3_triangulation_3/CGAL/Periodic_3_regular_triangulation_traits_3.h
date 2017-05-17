a1a6a1727120d9bebf87464f2709329e0a4cd4eb
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-05-17 15:07:12 +0200

    Improved Periodic_3 documentation

--- Periodic_3_triangulation_3/doc/Periodic_3_triangulation_3/CGAL/Periodic_3_regular_triangulation_traits_3.h
+++ Periodic_3_triangulation_3/doc/Periodic_3_triangulation_3/CGAL/Periodic_3_regular_triangulation_traits_3.h
@@ -8,19 +8,18 @@ The class `Periodic_3_regular_triangulation_traits_3` is designed as a default t
 class `Periodic_3_regular_triangulation_3<Periodic_3RegularTriangulationTraits_3,TriangulationDataStructure_3>`.
 
 \tparam Traits must be a model of the `RegularTriangulationTraits_3` concept.
-\tparam Periodic_3Offset_3 must be a model of the concept `Periodic_3Offset_3` and defaults to `Periodic_3_offset_3`.
-
-\cgalModels Periodic_3RegularTriangulationTraits_3
+\tparam Offset must be a model of the concept `Periodic_3Offset_3` and defaults to `Periodic_3_offset_3`.
 
 If `Traits` is a `CGAL::Filtered_kernel` (detected when `Traits::Has_filtered_predicates` exists
 and is `true`), this class automatically provides filtered predicates.
 By default, this holds for `CGAL::Exact_predicates_inexact_constructions_kernel` and
 `CGAL::Exact_predicates_exact_constructions_kernel`.
 
+\cgalModels Periodic_3RegularTriangulationTraits_3
 */
-template< typename Traits, typename Periodic_3Offset_3 >
+template< typename Traits, typename Offset >
 class Periodic_3_regular_triangulation_traits_3 :
-  public Periodic_3_triangulation_traits_base_3< Traits, Periodic_3Offset_3 > {
+  public Periodic_3_triangulation_traits_base_3< Traits, Offset > {
 public:
 }; /* end Periodic_3_regular_triangulation_traits_3 */
 } /* end namespace CGAL */
