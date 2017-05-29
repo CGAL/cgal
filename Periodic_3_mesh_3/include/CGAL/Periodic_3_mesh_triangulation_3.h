6d3a8be7fa676b32d454dd3f27b865233946086d
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-05-29 15:58:59 +0200

    Regular triangulations must use regular vertex/cell bases

--- Periodic_3_mesh_3/include/CGAL/Periodic_3_mesh_triangulation_3.h
+++ Periodic_3_mesh_3/include/CGAL/Periodic_3_mesh_triangulation_3.h
@@ -513,11 +513,10 @@ class Periodic_3_mesh_triangulation_3
 
   // Periodic vertex and cell bases
   typedef Periodic_3_triangulation_ds_vertex_base_3<> VbDS;
-  typedef Triangulation_vertex_base_3<Geom_traits, VbDS> PVb;
+  typedef Regular_triangulation_vertex_base_3<Geom_traits, VbDS> PVb;
 
   typedef Periodic_3_triangulation_ds_cell_base_3<> CbDS;
-  typedef Triangulation_cell_base_3<Geom_traits, CbDS> PTCb;
-  typedef Triangulation_cell_base_with_circumcenter_3<Geom_traits, PTCb> PCb;
+  typedef Regular_triangulation_cell_base_3<Geom_traits, CbDS> PCb;
 
   typedef Mesh_vertex_base_3<Geom_traits, MD, PVb> Default_Vb;
   typedef Mesh_cell_base_3<Geom_traits, MD, PCb> Default_Cb;
