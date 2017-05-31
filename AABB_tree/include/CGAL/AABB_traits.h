6f6985065db0ebcbdbc3d26c5781fc6ba4b48750
Andreas Fabri <andreas.fabri@geometryfactory.com>
2017-05-31 10:48:43 +0200

    fix doc

--- AABB_tree/include/CGAL/AABB_traits.h
+++ AABB_tree/include/CGAL/AABB_traits.h
@@ -188,7 +188,7 @@ struct AABB_traits_base_2<GeomTraits,true>{
 /// \tparam Primitive provide the type of primitives stored in the AABB_tree.
 ///   It is a model of the concept `AABBPrimitive` or `AABBPrimitiveWithSharedData`.
 ///
-/// \tparam BboxMap must be a property map that has as key type a primitive id,
+/// \tparam BboxMap must be a model of `ReadablePropertyMap` that has as key type a primitive id,
 ///                 and as value type a `Bounding_box`.
 ///                 If the type is `Default` the `Datum` must have the
 ///                 member function `bbox()` that returns the bounding box  of the primitive.
