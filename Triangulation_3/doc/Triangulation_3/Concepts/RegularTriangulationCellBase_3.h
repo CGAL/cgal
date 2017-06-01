c1693b9a75c591028f6153697ddccc382265d80e
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-06-01 11:23:04 +0200

    Adapted the documentation to the new concept RegTrCellBaseWithWeightedCircumcenter
    - MeshCellBase_3 loses "invalidate_circumcenter" and refines the new concept
    - Reg_tr_cell_base_with_weighted_circumcenter_3 is a model of the new concept

--- Triangulation_3/doc/Triangulation_3/Concepts/RegularTriangulationCellBase_3.h
+++ Triangulation_3/doc/Triangulation_3/Concepts/RegularTriangulationCellBase_3.h
@@ -36,7 +36,6 @@ and an operator to compute its weighted circumcenter.
 \cgalRefines `TriangulationCellBase_3`
 
 \cgalHasModel CGAL::Regular_triangulation_cell_base_3 
-\cgalHasModel CGAL::Regular_triangulation_cell_base_with_weighted_circumcenter_3
 
 \sa `RegularTriangulationTraits_3`
 
