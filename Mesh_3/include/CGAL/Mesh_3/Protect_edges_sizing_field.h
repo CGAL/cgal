44e524b0deab20a970a11e803e2dd0d87ae1a2ae
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-06-02 16:05:16 +0200

    Removed some useless modifications in Protect_edges_sizing_field
    (actually not useless if placing dummy points as corners, but this is disabled
    for now. When re-enabled, it would be better to set a size_() to not be 0 at
    corners)

--- Mesh_3/include/CGAL/Mesh_3/Protect_edges_sizing_field.h
+++ Mesh_3/include/CGAL/Mesh_3/Protect_edges_sizing_field.h
@@ -489,8 +489,7 @@ insert_corners()
     Index p_index = domain_.index_from_corner_index(cit->first);
     
     // Get weight (ball radius is given by size_ function)
-    // <PERIODIC>
-    FT w = CGAL::square(size_(p, 0, p_index));
+    FT w = CGAL::square(query_size(p, 0, p_index));
 
     // the following lines ensure that the weight w is small enough so that
     // corners balls do not intersect
@@ -802,8 +801,7 @@ smart_insert_point(const Bare_point& p, Weight w, int dim, const Index& index,
     }
   }
 
-  // <PERIODIC>
-  const FT w_max = CGAL::square(size_(p, dim, index));
+  const FT w_max = CGAL::square(query_size(p, dim, index));
 
   if(w > w_max) {
 #if CGAL_MESH_3_PROTECTION_DEBUG & 1
