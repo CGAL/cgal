f860f8c2c73f33e369b9b4825950500f4ed05f53
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-06-20 20:29:54 +0200

    Replaced pointer to licence with licence itself

--- Installation/cmake/modules/FindSuiteSparse.cmake
+++ Installation/cmake/modules/FindSuiteSparse.cmake
@@ -56,7 +56,36 @@
 ##
 ## Created by jesnault (jerome.esnault@inria.fr) 2014-01-15
 ## Updated by jesnault (jerome.esnault@inria.fr) 2014-01-21
-## Licensed under 3-Claused BSD License. See https://github.com/jlblancoc/suitesparse-metis-for-windows/blob/master/LICENSE.md
+
+## Copyright (c) 2012-2015, Jose Luis Blanco (Universidad de Almeria);
+##                          Jerome Esnault (INRIA)
+## All rights reserved.
+
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+## ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+## WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+## IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+## INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+## BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+## LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+## OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+## OF THE POSSIBILITY OF SUCH DAMAGE.
+
+## Redistribution and use in source and binary forms, with or without modification,
+## are permitted provided that the following conditions are met:
+
+## Redistributions of source code must retain the above copyright notice,
+## this list of conditions and the following disclaimer.
+
+## Redistributions in binary form must reproduce the above copyright notice,
+## this list of conditions and the following disclaimer in the documentation
+## and/or other materials provided with the distribution.
+
+## Neither the name of suitesparse-metis-for-windows nor the names of its contributors
+## may be used to endorse or promote products derived from this software
+## without specific prior written permission.
+
 
 ## check if global root SuiteSparse folder is set or not and cache it in order to let user fill it
 if(NOT SuiteSparse_DIR)
