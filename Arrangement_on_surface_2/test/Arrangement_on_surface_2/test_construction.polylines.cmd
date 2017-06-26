29ce8330aed2a252c9a8e0c432243258dba63688
Sébastien Loriot <sebastien.loriot@cgal.org>
2017-06-26 16:56:19 +0200

    add all test cases

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_construction.polylines.cmd
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_construction.polylines.cmd
@@ -1,3 +1,5 @@
 ./data/test_construction/polylines/test01.txt
 ./data/test_construction/polylines/test02.txt
 ./data/test_construction/polylines/test03.txt
+./data/test_construction/polylines/test04.txt
+./data/test_construction/polylines/test05.txt
