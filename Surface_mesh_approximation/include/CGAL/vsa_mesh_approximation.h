64528e534e44a839703e98e0842abd204d95eec3
Lingjie Zhu <lingjie.zhu@nlpr.ia.ac.cn>
2017-07-02 21:44:40 +0800

    rename api file

--- Surface_mesh_approximation/include/CGAL/vsa_mesh_approximation.h
+++ Surface_mesh_approximation/include/CGAL/vsa_mesh_approximation.h
@@ -1,7 +1,7 @@
-#ifndef CGAL_SURFACE_MESH_SEGMENTATION_VSA_MESH_SEGMENTATION_H
-#define CGAL_SURFACE_MESH_SEGMENTATION_VSA_MESH_SEGMENTATION_H
+#ifndef CGAL_SURFACE_MESH_APPROXIMATION_VSA_MESH_APPROXIMATION_H
+#define CGAL_SURFACE_MESH_APPROXIMATION_VSA_MESH_APPROXIMATION_H
 
-#include "VSA_segmentation.h"
+#include "VSA.h"
 #include <CGAL/property_map.h>
 
 namespace CGAL
@@ -13,16 +13,16 @@ template <typename TriangleMesh,
   = typename boost::property_map<TriangleMesh, boost::vertex_point_t>::type,
   typename GeomTraits
   = typename Kernel_traits<typename boost::property_traits<PointPropertyMap>::value>::Kernel>
-  void vsa_mesh_segmentation(const TriangleMesh &triangle_mesh,
+  void vsa_mesh_approximation(const TriangleMesh &triangle_mesh,
     const std::size_t number_of_segments,
     SegmentPropertyMap segment_ids,
     FittingPropertyMap fit_error_map = FittingPropertyMap(),
     PointPropertyMap ppmap = PointPropertyMap(),
     GeomTraits traits = GeomTraits()) {
-  internal::VSA_segmentation<TriangleMesh, PointPropertyMap, GeomTraits>
+  internal::VSA<TriangleMesh, PointPropertyMap, GeomTraits>
     algorithm(triangle_mesh, ppmap, traits);
   algorithm.partition(number_of_segments, segment_ids);
 }
 }
 
-#endif // CGAL_SURFACE_MESH_SEGMENTATION_VSA_MESH_SEGMENTATION_H
+#endif // CGAL_SURFACE_MESH_APPROXIMATION_VSA_MESH_APPROXIMATION_H
