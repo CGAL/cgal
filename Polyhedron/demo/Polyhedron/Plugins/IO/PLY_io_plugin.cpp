c70270a1d40bb2edc0301b97039ce7e52f73c348
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-07-05 15:16:29 +0200

    Add polygon soup reader to PLY plugin

--- Polyhedron/demo/Polyhedron/Plugins/IO/PLY_io_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/IO/PLY_io_plugin.cpp
@@ -1,7 +1,12 @@
+#include "Scene_polygon_soup_item.h"
 #include "Scene_points_with_normal_item.h"
+
 #include <CGAL/Three/Polyhedron_demo_io_plugin_interface.h>
 #include <QInputDialog>
 #include <fstream>
+
+#include <CGAL/IO/PLY_reader.h>
+#include <QMessageBox>
 
 class Polyhedron_demo_ply_plugin :
   public QObject,
@@ -13,7 +18,7 @@ class Polyhedron_demo_ply_plugin :
 
 public:
   QString name() const { return "ply_plugin"; }
-  QString nameFilters() const { return "PLY files as Point set (*.ply)"; }
+  QString nameFilters() const { return "PLY files (*.ply)"; }
   bool canLoad() const;
   CGAL::Three::Scene_item* load(QFileInfo fileinfo);
 
@@ -32,16 +37,67 @@ Polyhedron_demo_ply_plugin::load(QFileInfo fileinfo) {
   if(!in)
     std::cerr << "Error!\n";
 
-  Scene_points_with_normal_item* item;
-  item = new Scene_points_with_normal_item();
-  if(!item->read_ply_point_set(in))
+  // Test if input is mesh or point set
+  bool input_is_mesh = false;
+  std::string line;
+  std::istringstream iss;
+  while (getline (in,line))
+  {
+    iss.clear();
+    iss.str (line);
+    std::string keyword;
+    if (iss >> keyword)
     {
-      delete item;
-      return 0;
+      if (keyword == "element")
+      {
+        std::string type;
+        if (iss >> type)
+          if (type == "face")
+          {
+            input_is_mesh = true;
+            break;
+          }
+      }
+      else if (keyword == "end_header")
+        break;
+    }
+  }
+
+  in.seekg(0);
+
+  if (input_is_mesh) // Open polygon soup
+  {
+    Scene_polygon_soup_item* soup_item = new Scene_polygon_soup_item;
+    soup_item->setName(fileinfo.completeBaseName());
+
+    std::vector<Kernel::Point_3> points;
+    std::vector<std::vector<std::size_t> > polygons;
+
+    if (!(CGAL::read_PLY (in, points, polygons)))
+    {
+      delete soup_item;
+      return NULL;
     }
 
-  item->setName(fileinfo.completeBaseName());
-  return item;
+    soup_item->load (points, polygons);
+    
+    return soup_item;
+  }
+  else // Open point set
+  {
+    Scene_points_with_normal_item* item;
+    item = new Scene_points_with_normal_item();
+    if(!item->read_ply_point_set(in))
+    {
+      delete item;
+      return NULL;
+    }
+
+    item->setName(fileinfo.completeBaseName());
+    return item;
+  }
+  
+  return NULL;
 }
 
 bool Polyhedron_demo_ply_plugin::canSave(const CGAL::Three::Scene_item* item)
