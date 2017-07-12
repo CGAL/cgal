912227a6b4f880233fd35dd82bad45eda44b0649
Sébastien Loriot <sebastien.loriot@cgal.org>
2017-07-12 17:06:54 +0200

    add testsuite and testcases

---
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/test_autorefinement.cmd
@@ -0,0 +1,17 @@
+data-autoref/test_01.off 0 0 4 1 4
+data-autoref/test_02.off 1 5 13 1 13
+data-autoref/test_03.off 4 8 18 0 18
+data-autoref/test_04.off 1 5 17 1 17
+data-autoref/test_05.off 1 5 17 1 17
+data-autoref/test_06.off 3 55 141 1 141
+data-autoref/test_07.off 1 4 10 1 10
+data-autoref/test_08.off 1 31 87 1 87
+data-autoref/test_09.off 1 4 5 1 5
+data-autoref/test_10.off 1 3 13 0 13
+data-autoref/test_11.off 1 3 12 1 12
+data-autoref/test_12.off 2 2 11 1 11
+data-autoref/test_13.off 1 5 16 0 16
+data-autoref/test_14.off 1 5 16 1 16
+data-autoref/test_15.off 3 8 16 1 16
+data-autoref/test_16.off 1 2 6 1 6
+data-autoref/test_17.off 1 2 6 1 6
