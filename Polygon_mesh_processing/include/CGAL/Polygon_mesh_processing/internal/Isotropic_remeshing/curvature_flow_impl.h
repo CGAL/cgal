9d1f09921c32f6dd6438ead498b50a008202f308
konstantinos katrioplas <konst.katrioplas@gmail.com>
2017-07-14 13:03:21 +0300

    cotangent weights calculation

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/internal/Isotropic_remeshing/curvature_flow_impl.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/internal/Isotropic_remeshing/curvature_flow_impl.h
@@ -1,14 +1,13 @@
 #ifndef CURVATURE_FLOW_IMPL_H
 #define CURVATURE_FLOW_IMPL_H
 
-
+#include <CGAL/Polygon_mesh_processing/Weights.h>
 
 namespace CGAL {
 
 namespace Polygon_mesh_processing {
 
 namespace internal {
-
 
 
 
@@ -20,11 +19,18 @@ class Curvature_flow
 
 
 
+
+
+
+
+
 public:
 
 
-    Curvature_flow(PolygonMesh& pmesh, VertexPointMap& vpmap) : mesh_(pmesh), vpmap_(vpmap)
+    Curvature_flow(PolygonMesh& pmesh, VertexPointMap& vpmap) : mesh_(pmesh), vpmap_(vpmap),
+                                                                cot_calculator_(pmesh_, vpmap_)
     {}
+
 
 
 
@@ -32,14 +38,18 @@ public:
     // k = div n
 
 
-    // cot a = v1 * v2 / v1 x v2
 
 
 
 
 private:
-    PolygonMesh& mesh_;
+    PolygonMesh& pmesh_;
     VertexPointMap& vpmap_;
+
+    // Cotagent calculator class
+    CGAL::internal::Cotangent_value_Meyer<
+        PolygonMesh,
+        typename boost::property_map<PolygonMesh, CGAL::vertex_point_t>::type >   cot_calculator_;
 
 };
 
