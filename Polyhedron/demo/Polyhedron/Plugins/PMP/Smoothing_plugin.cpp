22562eec110816bafa3ca3145fd008815f13ad75
konstantinos katrioplas <konst.katrioplas@gmail.com>
2017-07-21 12:13:16 +0300

    plugin: dock widget design & support both compatible and curvature

--- Polyhedron/demo/Polyhedron/Plugins/PMP/Smoothing_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/PMP/Smoothing_plugin.cpp
@@ -4,12 +4,14 @@
 #include <QAction>
 #include <QMainWindow>
 #include <QApplication>
+#include <QDockWidget>
 #include <QString>
 #include <QInputDialog>
 #include <QtPlugin>
 #include <QMessageBox>
 
 #include <CGAL/Three/Polyhedron_demo_plugin_interface.h>
+#include <CGAL/Three/Polyhedron_demo_plugin_helper.h>
 
 #include "Scene_polyhedron_item.h"
 #include "Scene_polyhedron_selection_item.h"
@@ -30,7 +32,7 @@
 using namespace CGAL::Three;
 class Polyhedron_demo_smothing_plugin :
   public QObject,
-  public Polyhedron_demo_plugin_interface
+  public Polyhedron_demo_plugin_helper
 {
     Q_OBJECT
     Q_INTERFACES(CGAL::Three::Polyhedron_demo_plugin_interface)
@@ -38,24 +40,32 @@ class Polyhedron_demo_smothing_plugin :
 
 
 public:
-    void init(QMainWindow* mainWindow,
-              Scene_interface* scene_interface,
-              Messages_interface*)
+    void init(QMainWindow* mainWindow, Scene_interface* scene_interface, Messages_interface*)
     {
-      this->scene = scene_interface;
-      this->mw = mainWindow;
+        scene = scene_interface;
+        mw = mainWindow;
 
-      actionSmoothing_ = new QAction("Smoothing", mw);
-      actionSmoothing_->setProperty("subMenuName", "Polygon Mesh Processing");
-      if (actionSmoothing_) {
-        connect(actionSmoothing_, SIGNAL(triggered()),
-          this, SLOT(smooth()));
-      }
+        actionSmoothing_ = new QAction(tr("Smoothing"), mw);
+        actionSmoothing_->setProperty("subMenuName", "Polygon Mesh Processing");
+
+
+        connect(actionSmoothing_, SIGNAL(triggered()), this, SLOT(smoothing_action()));
+
+        dock_widget = new QDockWidget("Smoothing", mw);
+        dock_widget->setVisible(false);
+
+        ui_widget.setupUi(dock_widget);
+        addDockWidget(dock_widget);
+
+        connect(ui_widget.Compatible_button,  SIGNAL(clicked()), this, SLOT(on_Compatible_button_clicked()));
+        connect(ui_widget.Curvature_button,  SIGNAL(clicked()), this, SLOT(on_Curvature_button_clicked()));
+
     }
 
-    QList<QAction*> actions() const {
+    QList<QAction*> actions() const
+    {
         return QList<QAction*>() << actionSmoothing_;
-      }
+    }
 
     bool applicable(QAction*) const
     {
@@ -68,118 +78,49 @@ public:
         return false;
     }
 
-
-
-public Q_SLOTS:
-    void smooth()
+    virtual void closure()
     {
-        const Scene_interface::Item_id index = scene->mainSelectionIndex();
-        Scene_polyhedron_item* poly_item =
-          qobject_cast<Scene_polyhedron_item*>(scene->item(index));
-        Scene_polyhedron_selection_item* selection_item =
-          qobject_cast<Scene_polyhedron_selection_item*>(scene->item(index));
-
-        //Create dialog box
-        QDialog dialog(mw);
-        Ui::Smoothing_dialog ui
-          = smooth_dialog(&dialog, poly_item, selection_item);
-
-        //Get values
-        int i = dialog.exec();
-        if (i == QDialog::Rejected)
-        {
-          std::cout << "Smoothing aborted" << std::endl;
-          return;
-        }
-
-        bool weights_flag = ui.angle_weights_checkBox->isChecked();
-        double gd_precision = ui.gradient_descent_doubleSpinBox->value();
-        unsigned int nb_iterations = ui.iterations_spinBox->value();
-
-        // wait cursor
-        QApplication::setOverrideCursor(Qt::WaitCursor);
-        QTime time;
-        time.start();
-
-        std::cout << "Smoothing..." << std::endl;
-
-        if(poly_item || selection_item)
-        {
-            Polyhedron& pmesh = *poly_item->polyhedron();
-            CGAL::Polygon_mesh_processing::compatible_remeshing(pmesh,
-                                                                faces(pmesh),
-                                                                edges(pmesh),
-                                                                CGAL::Polygon_mesh_processing::parameters::use_weights(weights_flag)
-                                                                .number_of_iterations(nb_iterations)
-                                                                .gradient_descent_precision(gd_precision));
-
-            // to fix
-            if(poly_item)
-            {
-                poly_item->invalidateOpenGLBuffers();
-                Q_EMIT poly_item->itemChanged();
-            }
-            else
-            {
-                std::cerr<<"selection_item!"<<std::endl;
-            }
-            //
-
-        }
-        else
-        {
-            std::cerr << "No smoothing!" << std::endl;
-        }
-
-        std::cout << " ok (" << time.elapsed() << " ms)" << std::endl;
-
-        // default cursor
-         QApplication::restoreOverrideCursor();
+      dock_widget->hide();
     }
 
-    Ui::Smoothing_dialog smooth_dialog(QDialog* dialog, Scene_polyhedron_item* poly_item, Scene_polyhedron_selection_item* selection_item)
+public Q_SLOTS:
+    void smoothing_action()
     {
-        Ui::Smoothing_dialog ui;
-        ui.setupUi(dialog);
-        connect(ui.buttonBox, SIGNAL(accepted()), dialog, SLOT(accept()));
-        connect(ui.buttonBox, SIGNAL(rejected()), dialog, SLOT(reject()));
+        dock_widget->show();
+        dock_widget->raise();
+    }
 
-        //Set default parameters
-        Scene_interface::Bbox bbox = poly_item != NULL ? poly_item->bbox()
-          : (selection_item != NULL ? selection_item->bbox() : scene->bbox());
-        ui.objectName->setText(poly_item != NULL ? poly_item->name()
-          : (selection_item != NULL ? selection_item->name() : QString("Smoothing parameters")));
+    void on_Compatible_button_clicked()
+    {
+        const Scene_interface::Item_id index = scene->mainSelectionIndex();
+        Scene_polyhedron_item* poly_item = qobject_cast<Scene_polyhedron_item*>(scene->item(index));
 
-        ui.objectNameSize->setText(
-          tr("Object bbox size (w,h,d):  <b>%1</b>,  <b>%2</b>,  <b>%3</b>")
-          .arg(bbox.xmax() - bbox.xmin(), 0, 'g', 3)
-          .arg(bbox.ymax() - bbox.ymin(), 0, 'g', 3)
-          .arg(bbox.zmax() - bbox.zmin(), 0, 'g', 3));
+        Polyhedron& pmesh = *poly_item->polyhedron();
+        CGAL::Polygon_mesh_processing::compatible_remeshing(pmesh);
 
-        double diago_length = CGAL::sqrt((bbox.xmax() - bbox.xmin())*(bbox.xmax() - bbox.xmin())
-          + (bbox.ymax() - bbox.ymin())*(bbox.ymax() - bbox.ymin())
-          + (bbox.zmax() - bbox.zmin())*(bbox.zmax() - bbox.zmin()));
-        double log = std::log10(diago_length);
-        unsigned int nb_decimals = (log > 0) ? 3 : (std::ceil(-log) + 3);
+        poly_item->invalidateOpenGLBuffers();
+        Q_EMIT poly_item->itemChanged();
+        QApplication::restoreOverrideCursor();
+    }
 
-        // default values
-        ui.angle_weights_checkBox->setChecked(false);
+    void on_Curvature_button_clicked()
+    {
+        const Scene_interface::Item_id index = scene->mainSelectionIndex();
+        Scene_polyhedron_item* poly_item = qobject_cast<Scene_polyhedron_item*>(scene->item(index));
 
-        ui.gradient_descent_doubleSpinBox->setDecimals(nb_decimals);
-        ui.gradient_descent_doubleSpinBox->setSingleStep(1e-3);
-        ui.gradient_descent_doubleSpinBox->setValue(0.001);
+        Polyhedron& pmesh = *poly_item->polyhedron();
+        CGAL::Polygon_mesh_processing::curvature_flow(pmesh);
 
-        ui.iterations_spinBox->setValue(1);
-        ui.iterations_spinBox->setMinimum(1);
-
-        return ui;
+        poly_item->invalidateOpenGLBuffers();
+        Q_EMIT poly_item->itemChanged();
+        QApplication::restoreOverrideCursor();
     }
 
 
 private:
-    Scene_interface *scene;
-    QMainWindow* mw;
     QAction* actionSmoothing_;
+    QDockWidget* dock_widget;
+    Ui::Smoothing ui_widget;
 
 
 
