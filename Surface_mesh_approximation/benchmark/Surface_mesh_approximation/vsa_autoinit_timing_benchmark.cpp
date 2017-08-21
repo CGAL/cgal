8665e5d830272af44aa6dd344666fe6a0346c1bb
Lingjie Zhu <lingjie.zhu@nlpr.ia.ac.cn>
2017-08-21 21:06:14 +0800

    rename file

--- Surface_mesh_approximation/benchmark/Surface_mesh_approximation/vsa_autoinit_timing_benchmark.cpp
+++ Surface_mesh_approximation/benchmark/Surface_mesh_approximation/vsa_autoinit_timing_benchmark.cpp
@@ -6,7 +6,7 @@
 #include <CGAL/IO/Polyhedron_iostream.h>
 
 #include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
-#include <CGAL/vsa_mesh_approximation_traits.h>
+#include <CGAL/vsa_mesh_approximation_metrics.h>
 #include <CGAL/VSA_approximation.h>
 #include <CGAL/Timer.h>
 
