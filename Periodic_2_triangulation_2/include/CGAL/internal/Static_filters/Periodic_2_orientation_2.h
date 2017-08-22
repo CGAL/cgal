b5fd4dfa018b37522b6852186ee7f3999656cefa
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-08-22 17:34:05 +0200

    Removed useless include

--- Periodic_2_triangulation_2/include/CGAL/internal/Static_filters/Periodic_2_orientation_2.h
+++ Periodic_2_triangulation_2/include/CGAL/internal/Static_filters/Periodic_2_orientation_2.h
@@ -22,9 +22,7 @@
 
 #include <CGAL/license/Periodic_2_triangulation_2.h>
 
-
 #include <CGAL/Profile_counter.h>
-#include <CGAL/internal/Static_filters/Orientation_2.h>
 #include <CGAL/internal/Static_filters/Static_filter_error.h>
 #include <CGAL/internal/Static_filters/tools.h>
 
