5118f830b321656e015ea145eee408d0625bb5f2
Lingjie Zhu <lingjie.zhu@nlpr.ia.ac.cn>
2017-08-29 11:19:42 +0800

    add meshing manifold test

--- Surface_mesh_approximation/test/Surface_mesh_approximation/vsa_meshing_manifold_test.cpp
+++ Surface_mesh_approximation/test/Surface_mesh_approximation/vsa_meshing_manifold_test.cpp
@@ -17,7 +17,7 @@ typedef CGAL::VSA_approximation<Polyhedron, VertexPointMap> L21VSA;
 typedef L21VSA::ErrorMetric L21Metric;
 typedef L21VSA::ProxyFitting L21ProxyFitting;
 
-bool test_shape(const char *file_name, const std::size_t target_num_proxies)
+bool test_manifold(const char *file_name, const FT drop = FT(1e-8))
 {
   Polyhedron mesh;
   std::ifstream input(file_name);
@@ -35,33 +35,28 @@ bool test_shape(const char *file_name, const std::size_t target_num_proxies)
   vsa_l21.set_metric(l21_metric, l21_fitting);
 
   // approximation, init from error, drop to the target error incrementally
-  // should reach targeted number of proxies gradually
-  const FT drop(1e-8);
   const std::size_t num_iterations = 20;
-  const std::size_t inner_iterations = 10;
+  const std::size_t inner_iterations = 5;
   vsa_l21.seeding_by_error(L21VSA::Incremental, drop, inner_iterations);
   for (std::size_t i = 0; i < num_iterations; ++i)
     vsa_l21.run_one_step();
-  if (vsa_l21.get_proxies_size() != target_num_proxies) {
-    std::cout << "#targeted - #result "
-      << target_num_proxies << ' '
-      << vsa_l21.get_proxies_size() << std::endl;
-    std::cout << "incremental reaching failed" << std::endl;
-    return false;
-  }
+  std::cout << "#proxies " << vsa_l21.get_proxies_size() << std::endl;
 
-  return true;
+  // meshing
+  Polyhedron mesh_out;
+  return vsa_l21.meshing(mesh_out);
 }
 
 /**
- * This file tests the correctness of the algorithm.
- * Basically we input a cube mesh and see if it outputs a cube.
+ * This file tests the meshing of the algorithm.
+ * For now, we can only expect manifold output on simple geometric objects.
  */
 int main()
 {
+  std::cout << "Meshing manifold test." << std::endl;
   const char file0[] = "./data/cube_meshed.off";
   std::cout << "Testing file \"" << file0 << '\"' << std::endl;
-  if (!test_shape(file0, 6)) {
+  if (!test_manifold(file0)) {
     std::cout << "Failed." << std::endl;
     return EXIT_FAILURE;
   }
@@ -70,7 +65,16 @@ int main()
 
   const char file1[] = "./data/cube_meshed_open.off";
   std::cout << "Testing file \"" << file1 << '\"' << std::endl;
-  if (!test_shape(file1, 5)) {
+  if (!test_manifold(file1)) {
+    std::cout << "Failed." << std::endl;
+    return EXIT_FAILURE;
+  }
+  else
+    std::cout << "Succeeded." << std::endl;
+
+  const char file2[] = "./data/sphere_iso.off";
+  std::cout << "Testing file \"" << file2 << '\"' << std::endl;
+  if (!test_manifold(file2, FT(1e-2))) {
     std::cout << "Failed." << std::endl;
     return EXIT_FAILURE;
   }
