98fdfdd2b2cd68f109b3b98cb14c38d971f1cdf9
Maxime Gimeno <maxime.gimeno@gmail.com>
2017-08-29 12:20:43 +0200

    Fix doc warnings

--- Surface_mesher/doc/Surface_mesher/CGAL/IO/output_surface_facets_to_polyhedron.h
+++ Surface_mesher/doc/Surface_mesher/CGAL/IO/output_surface_facets_to_polyhedron.h
@@ -7,9 +7,11 @@ This variant exports the surface as a polyhedron.
 It requires the surface to be manifold. For this purpose,
 you may call make_surface_mesh() with Manifold_tag or Manifold_with_boundary_tag parameter.
 
-@commentheading Template Parameters:
-@param SurfaceMeshComplex_2InTriangulation_3 model of the SurfaceMeshComplex_2InTriangulation_3 concept.
-@param Polyhedron an instance of CGAL::Polyhedron_3<Traits>.
+@tparam SurfaceMeshComplex_2InTriangulation_3 model of the SurfaceMeshComplex_2InTriangulation_3 concept.
+@tparam Polyhedron an instance of CGAL::Polyhedron_3<Traits>.
+
+@param c2t3 the input.
+@param output_polyhedron the output.
 
 @return true if the surface is manifold and orientable.
 */
