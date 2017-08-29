59e725ffbc1df363cbbaa213657942ed4c3df03a
Lingjie Zhu <lingjie.zhu@nlpr.ia.ac.cn>
2017-08-29 17:34:11 +0800

    add placement check to teleportation test

--- Surface_mesh_approximation/test/Surface_mesh_approximation/vsa_teleportation_test.cpp
+++ Surface_mesh_approximation/test/Surface_mesh_approximation/vsa_teleportation_test.cpp
@@ -4,19 +4,30 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/IO/Polyhedron_iostream.h>
+#include <CGAL/Bbox_3.h>
 
 #include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
 #include <CGAL/VSA_approximation.h>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef Kernel::FT FT;
+typedef Kernel::Point_3 Point;
+typedef Kernel::Vector_3 Vector;
+
 typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+typedef Polyhedron::Facet_handle Facet_handle;
+typedef Polyhedron::Facet_iterator Facet_iterator;
+typedef Polyhedron::Halfedge_handle Halfedge_handle;
+
 typedef boost::property_map<Polyhedron, boost::vertex_point_t>::type VertexPointMap;
+typedef boost::associative_property_map<std::map<Facet_handle, std::size_t> > FacetProxyMap;
 
 typedef CGAL::VSA_approximation<Polyhedron, VertexPointMap> L21VSA;
 typedef L21VSA::ErrorMetric L21Metric;
 typedef L21VSA::ProxyFitting L21ProxyFitting;
-typedef L21VSA::Proxy Proxy;
+typedef L21VSA::Proxy L21Proxy;
+
+#define CGAL_VSA_TEST_TOLERANCE 1e-8
 
 bool check_strict_ordering(const std::vector<FT> &error)
 {
@@ -35,8 +46,7 @@ bool check_strict_ordering(const std::vector<FT> &error)
 /**
  * This file tests the teleportation functionality on a plane-sphere shape.
  * We initialize random first, then verify that teleporting all (most) planes
- * from planar parts to the spherical one and lower the error.
- * TODO: check if all planar parts to the spherical one
+ * from planar part to the spherical one and lower the error.
  */
 int main()
 {
@@ -46,6 +56,7 @@ int main()
     std::cerr << "Invalid off file." << std::endl;
     return EXIT_FAILURE;
   }
+  std::cout << "Teleportation test." << std::endl;
 
   // algorithm instance
   L21Metric l21_metric(mesh);
@@ -75,12 +86,63 @@ int main()
   }
   std::cout << "#teleportation " << count << std::endl;
 
-  if (check_strict_ordering(error)) {
-    std::cout << "Pass the teleportation decrease test." << std::endl;
-    return EXIT_SUCCESS;
-  }
-  else {
-    std::cout << "Failed the teleportation decrease test." << std::endl;
+  if (!check_strict_ordering(error)) {
+    std::cout << "Failed: teleportation error decrease inconsistent." << std::endl;
     return EXIT_FAILURE;
   }
+
+  std::map<Facet_handle, std::size_t> internal_fidxmap;
+  for (Facet_iterator fitr = mesh.facets_begin(); fitr != mesh.facets_end(); ++fitr)
+    internal_fidxmap[fitr] = 0;
+  FacetProxyMap fproxymap(internal_fidxmap);
+  l21_vsa.get_proxy_map(fproxymap);
+  std::vector<L21Proxy> proxies;
+  l21_vsa.get_proxies(std::back_inserter(proxies));
+
+  CGAL::Bbox_3 bbox = CGAL::bbox_3(mesh.points_begin(), mesh.points_end());
+  const FT ymin = bbox.ymin(), ymax = bbox.ymax(), yrange = ymax - ymin;
+  std::cout << "Range along y axis: [" << ymin << ", " << ymax << "]" << std::endl;
+
+  // test if all facets on the planar part are in the same proxy
+  std::size_t planar_pxidx = static_cast<std::size_t>(-1);
+  std::size_t num_planar_facets = 0;
+  bool first = true;
+  for (Facet_iterator fitr = mesh.facets_begin(); fitr != mesh.facets_end(); ++fitr) {
+    Halfedge_handle he = fitr->halfedge();
+    const Point &p0 = he->opposite()->vertex()->point();
+    const Point &p1 = he->vertex()->point();
+    const Point &p2 = he->next()->vertex()->point();
+    const Point fcenter = CGAL::centroid(p0, p1, p2);
+    Vector fnormal = CGAL::normal(p0, p1, p2);
+    fnormal = fnormal / FT(std::sqrt(CGAL::to_double(fnormal.squared_length())));
+
+    // check the facet center and normal to see if it is on the planar part of the geometry
+    double dis_var = std::abs(CGAL::to_double((fcenter.y() - ymin) / yrange));
+    double dir_var = std::abs(CGAL::to_double(fnormal.y()) - 1.0);
+    if (dis_var < CGAL_VSA_TEST_TOLERANCE && dir_var < CGAL_VSA_TEST_TOLERANCE) {
+      ++num_planar_facets;
+      const std::size_t pxidx = fproxymap[fitr];
+      if (first) {
+        first = false;
+        planar_pxidx = pxidx;
+      }
+
+      // only one proxy on the planar part of the geometry
+      if (pxidx != planar_pxidx) {
+        std::cout << "Failed: more than one proxy on the planar part of the model." << std::endl;
+        return EXIT_FAILURE;
+      }
+    }
+  }
+  std::cout << "#facets on planar part " << num_planar_facets << std::endl;
+
+  // proxy of the planar part should facing straight towards the y positive.
+  double px_dir_var = std::abs(CGAL::to_double(proxies[planar_pxidx].normal.y()) - 1.0);
+  if (px_dir_var > CGAL_VSA_TEST_TOLERANCE) {
+    std::cout << "Failed: the proxy of planar part is incorrect." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "Succeeded." << std::endl;
+  return EXIT_SUCCESS;
 }
