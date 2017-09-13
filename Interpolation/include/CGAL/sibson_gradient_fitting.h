a926aa22cb14fa07995a14e4fad885f3248de432
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-09-13 16:13:04 +0200

    Fixed weighted point issue in gradient fitting functions

--- Interpolation/include/CGAL/sibson_gradient_fitting.h
+++ Interpolation/include/CGAL/sibson_gradient_fitting.h
@@ -48,7 +48,9 @@ sibson_gradient_fitting(ForwardIterator first, ForwardIterator beyond,
   typedef typename Traits::Aff_transformation_d Aff_transformation;
   typedef typename Traits::FT                   Coord_type;
 
-  typename Functor::result_type fn =  function_value(p);
+  const typename Traits::Point_d& bare_p = traits.construct_point_d_object()(p);
+
+  typename Functor::result_type fn = function_value(bare_p);
   CGAL_assertion(fn.second); //function value of p is valid
 
   typename Traits::Vector_d pn =
@@ -56,16 +58,17 @@ sibson_gradient_fitting(ForwardIterator first, ForwardIterator beyond,
   Aff_transformation scaling, m,
       Hn(traits.construct_null_matrix_d_object()());
 
-  for(;first!=beyond; ++first){
-    Coord_type square_dist = traits.compute_squared_distance_d_object()
-                             (first->first, p);
+  for(;first!=beyond; ++first)
+  {
+    const typename Traits::Point_d& bare_f = traits.construct_point_d_object()(first->first);
+
+    Coord_type square_dist = traits.compute_squared_distance_d_object()(bare_f, bare_p);
     CGAL_assertion(square_dist != 0);
     Coord_type scale = first->second / (norm*square_dist);
-    typename Traits::Vector_d d =
-        traits.construct_vector_d_object()(p, first->first);
+    typename Traits::Vector_d d = traits.construct_vector_d_object()(bare_p, bare_f);
 
     //compute the vector pn:
-    typename Functor::result_type f = function_value(first->first);
+    typename Functor::result_type f = function_value(bare_f);
     CGAL_assertion(f.second);//function value of first->first is valid
     pn = pn + traits.construct_scaled_vector_d_object()
          (d,scale * (f.first - fn.first));
@@ -89,7 +92,7 @@ sibson_gradient_fitting(const Triangul& tr,
                         CoordFunctor compute_coordinates,
                         const Traits& traits)
 {
-  typedef typename Traits::Point_d                        Point;
+  typedef typename Triangul::Point                        Point;
   typedef typename Traits::FT                             Coord_type;
 
   std::vector< std::pair< Point, Coord_type > > coords;
@@ -101,7 +104,7 @@ sibson_gradient_fitting(const Triangul& tr,
     if (!tr.is_edge(vit, tr.infinite_vertex()))
     {
       norm = compute_coordinates(tr, vit, std::back_inserter(coords)).second;
-      *out++ = std::make_pair(vit->point(),
+      *out++ = std::make_pair(traits.construct_point_d_object()(vit->point()),
                               sibson_gradient_fitting(coords.begin(),
                                                       coords.end(),
                                                       norm, vit->point(),
@@ -145,8 +148,8 @@ sibson_gradient_fitting_rn_2(const Rt& rt,
 {
   typedef typename std::back_insert_iterator<
                      std::vector<
-                       std::pair< typename Traits::Point_d,
-                                  typename  Traits::FT > > >   CoordInserter;
+                       std::pair< typename Traits::Weighted_point_d,
+                                  typename Traits::FT > > >   CoordInserter;
 
   return sibson_gradient_fitting(rt, out, function_value,
                                  regular_neighbor_coordinates_2_object< Rt,
