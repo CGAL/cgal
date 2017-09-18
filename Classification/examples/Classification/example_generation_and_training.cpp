e3ba95d97d25320a4a14f4926d87805129320f91
Simon Giraudot <simon.giraudot@geometryfactory.com>
2017-09-18 15:11:00 +0200

    Fix binary input stream for Windows

--- Classification/examples/Classification/example_generation_and_training.cpp
+++ Classification/examples/Classification/example_generation_and_training.cpp
@@ -38,7 +38,7 @@ typedef Classification::Point_set_feature_generator<Kernel, Point_set, Pmap>    
 int main (int argc, char** argv)
 {
   std::string filename (argc > 1 ? argv[1] : "data/b9_training.ply");
-  std::ifstream in (filename.c_str());
+  std::ifstream in (filename.c_str(), std::ios::binary);
   Point_set pts;
 
   std::cerr << "Reading input" << std::endl;
