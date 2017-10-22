c4fddff2353dde959f08d05cd79de99e98e91e25
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-10-22 23:32:16 +0200

    Minor wording improvement

--- Periodic_3_mesh_3/include/CGAL/Periodic_3_mesh_triangulation_3.h
+++ Periodic_3_mesh_3/include/CGAL/Periodic_3_mesh_triangulation_3.h
@@ -602,13 +602,16 @@ public:
     Vertex_handle v = tds().insert_in_hole(cell_begin, cell_end, begin, i);
     v->set_point(canonicalize_point(p));
 
-    std::vector<Cell_handle> nbs;
-    incident_cells(v, std::back_inserter(nbs));
+    std::vector<Cell_handle> incident_cells_;
+    incident_cells_.reserve(64);
+    incident_cells(v, std::back_inserter(incident_cells_));
 
-    // For all neighbors of the newly added vertex v: fetch their offsets from
+    // For all cells incident to the newly added vertex v: fetch their offsets from
     // the tester and reset them in the triangulation data structure.
-    for (typename std::vector<Cell_handle>::iterator cit = nbs.begin();
-         cit != nbs.end(); cit++) {
+    typename std::vector<Cell_handle>::iterator cit = incident_cells_.begin(),
+                                                cend = incident_cells_.end();
+    for (; cit != cend; cit++)
+    {
       Offset off[4];
       for (int i=0 ; i<4 ; i++)
         off[i] = (*cit)->vertex(i)->offset();
