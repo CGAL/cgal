6779e2cb351fa5691ce11e5aeb3f0da9b4670531
Efi Fogel <efifogel@gmail.com>
2017-11-15 13:24:48 +0200

    Added error and informative messages

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_data_traits.cpp
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_data_traits.cpp
@@ -18,21 +18,40 @@ int main()
   Segment_2 s2 = Segment_2(p2, p3);
 
   Traits_2 traits;
-  X_monotone_curve_2 c1 = X_monotone_curve_2(s1, 1);
+  X_monotone_curve_2 c1 = X_monotone_curve_2(s1, 1234);
   X_monotone_curve_2 fc1 = traits.construct_opposite_2_object()(c1);
 
-  if (c1.data() !=  fc1.data()) return 1;
+  if (c1.data() !=  fc1.data()) {
+    std::cerr << "Construct opposite failed: "
+              << c1.data() << " != " << fc1.data() << std::endl;
+    return 1;
+  }
+  std::cout << "Construct opposite data: " << c1.data() << std::endl;
 
-  X_monotone_curve_2 c2 = X_monotone_curve_2(s2, 1);
+  X_monotone_curve_2 c2 = X_monotone_curve_2(s2, 1234);
   X_monotone_curve_2 mc;
   if (!traits.are_mergeable_2_object()(c1, c2)) return 1;
   traits.merge_2_object()(c1, c2, mc);
-  if (c1.data() !=  mc.data()) return 1;
+  if (c1.data() !=  mc.data()) {
+    std::cerr << "Merge failed: "
+              << c1.data() << " != " << mc.data() << std::endl;
+    return 1;
+  }
+  std::cout << "Merge data: " << c1.data() << std::endl;
 
   X_monotone_curve_2 sc1, sc2;
   traits.split_2_object()(mc, p2, sc1, sc2);
-  if (mc.data() !=  sc1.data()) return 1;
-  if (sc1.data() !=  sc2.data()) return 1;
+  if (mc.data() !=  sc1.data()) {
+    std::cerr << "Split failed (original curve and split curve data differ): "
+              << mc.data() << " != " << sc1.data() << std::endl;
+    return 1;
+  }
+  if (sc1.data() !=  sc2.data()) {
+    std::cerr << "Split failed (data of split curves differ): "
+              << sc1.data() << " != " << sc2.data() << std::endl;
+    return 1;
+  }
+  std::cout << "Split data: " << sc1.data() << std::endl;
 
   return 0;
 }
