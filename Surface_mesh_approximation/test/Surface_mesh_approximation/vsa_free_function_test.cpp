8e39b7ca8c69a9b694a3751b9fd50cc6bb164580
Pierre Alliez <pierre.alliez@inria.fr>
2017-11-19 12:35:56 +0100

    Test updated with anchor_vertices

--- Surface_mesh_approximation/test/Surface_mesh_approximation/vsa_free_function_test.cpp
+++ Surface_mesh_approximation/test/Surface_mesh_approximation/vsa_free_function_test.cpp
@@ -23,27 +23,28 @@ int main()
   }
 
   Polyhedron out_mesh;
-  std::vector<std::vector<std::size_t> > tris;
-  std::vector<Kernel::Point_3> anchor_pos;
-  std::list<Polyhedron::Vertex_handle> anchor_vtx;
+  std::vector<std::vector<std::size_t> > triangles;
+  std::vector<Kernel::Point_3> points;
+  std::list<Polyhedron::Vertex_handle> anchors;
   std::vector<CGAL::VSA::Plane_proxy<Kernel> > proxies;
   std::map<Polyhedron::Facet_handle, std::size_t> fidxmap;
   boost::associative_property_map<std::map<Polyhedron::Facet_handle, std::size_t> > fpxmap(fidxmap);
+
   CGAL::VSA::mesh_approximation(mesh,
-    std::back_inserter(anchor_pos),
-    std::back_inserter(tris),
+    std::back_inserter(points),
+    std::back_inserter(triangles),
     CGAL::VSA::parameters::max_nb_proxies(6).
       iterations(30).
       inner_iterations(5).
       mesh_chord_error(0.5).
       facet_proxy_map(fpxmap).
-      anchor_vertex(std::back_inserter(anchor_vtx)).
+      anchor_vertices(std::back_inserter(anchors)).
       proxies(std::back_inserter(proxies)).
       output_mesh(&out_mesh));
 
-  std::cout << "#tris " << tris.size() << std::endl;
-  std::cout << "#anchor_pos " << anchor_pos.size() << std::endl;
-  std::cout << "#anchor_vtx " << anchor_vtx.size() << std::endl;
+  std::cout << "#triangles " << triangles.size() << std::endl;
+  std::cout << "#vertices " << points.size() << std::endl;
+  std::cout << "#anchor_vertices " << anchors.size() << std::endl;
   std::cout << "#proxies " << proxies.size() << std::endl;
   std::cout << "#fpxmap " << fidxmap.size() << std::endl;
 
