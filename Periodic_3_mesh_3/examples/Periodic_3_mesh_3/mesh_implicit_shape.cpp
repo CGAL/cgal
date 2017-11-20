bc0b45a4a4c956f9936edc6bfbed82079e881a07
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-11-20 16:58:08 +0100

    Fixed include

--- Periodic_3_mesh_3/examples/Periodic_3_mesh_3/mesh_implicit_shape.cpp
+++ Periodic_3_mesh_3/examples/Periodic_3_mesh_3/mesh_implicit_shape.cpp
@@ -12,7 +12,7 @@
 #include <CGAL/IO/Medit_IO.h>
 #include <CGAL/Implicit_periodic_3_mesh_domain_3.h>
 #include <CGAL/make_periodic_3_mesh_3.h>
-#include <CGAL/optimize_periodic_mesh_3.h>
+#include <CGAL/optimize_periodic_3_mesh_3.h>
 #include <CGAL/Periodic_3_mesh_triangulation_3.h>
 
 #include <CGAL/Mesh_complex_3_in_triangulation_3.h>
