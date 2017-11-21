1affc98dd12b4e37c32a0beb36c08008535afbec
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2017-11-21 17:22:39 +0100

    Added class Timestamp_hash_function
    Provides hashes for Vertex_handle-like classes. The hash will be based on
    the timestamp member if the vertex has one, and on its memory address otherwise.

--- Mesh_3/include/CGAL/Mesh_3/Sliver_perturber.h
+++ Mesh_3/include/CGAL/Mesh_3/Sliver_perturber.h
@@ -50,6 +50,7 @@
 #include <CGAL/Mesh_3/Null_perturber_visitor.h>
 #include <CGAL/Mesh_3/sliver_criteria.h>
 #include <CGAL/Has_timestamp.h>
+#include <CGAL/internal/Mesh_3/Timestamp_hash_function.h>
 
 #include <CGAL/Mesh_3/Concurrent_mesher_config.h>
 #include <CGAL/Mesh_3/Worksharing_data_structures.h>
@@ -81,31 +82,7 @@
 
 namespace CGAL {
 
-namespace internal { namespace Mesh_3 {
-
-  // Hash function for boost::unordered_map<Vertex_handle,...>
-  template <typename Tr, bool HasTimestamp>
-  struct VHash
-  {
-    typedef typename Tr::Vertex_handle Vertex_handle;
-    std::size_t operator()(Vertex_handle vh) const
-    {
-      return vh->time_stamp();
-    }
-  };
-  template <typename Tr>
-  struct VHash<Tr, false>
-  {
-    typedef typename Tr::Vertex_handle Vertex_handle;
-    std::size_t operator()(Vertex_handle vh) const
-    {
-      return boost::hash_value(&*vh);
-    }
-  };
-}} // end internal::Mesh_3
-
 namespace Mesh_3 {
-
 
 /**
 * @class PVertex
@@ -1128,11 +1105,8 @@ build_priority_queue(const FT& sliver_bound, PQueue& pqueue) const
 
   int pqueue_size = 0;
 
-  typedef typename std::iterator_traits<Vertex_handle>::value_type Vertex;
-  typedef CGAL::internal::Has_timestamp<Vertex> Vertex_has_timestamp;
-  using CGAL::internal::Mesh_3::VHash;
-  typedef VHash<Tr, Vertex_has_timestamp::value> Hash_fct;
-  typedef boost::unordered_map<Vertex_handle,PVertex,Hash_fct> M;
+  typedef CGAL::internal::Mesh_3::Timestamp_hash_function<Vertex_handle> Hash_fct;
+  typedef boost::unordered_map<Vertex_handle, PVertex, Hash_fct>         M;
 
   M vpm;
   for ( typename Tr::Finite_cells_iterator cit = tr_.finite_cells_begin();
---
+++ Mesh_3/include/CGAL/internal/Mesh_3/Timestamp_hash_function.h
@@ -0,0 +1,61 @@
+// Copyright (c) 2014-2017 GeometryFactory Sarl (France)
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+//
+// Author(s)     : Laurent Rineau,
+//                 Mael Rouxel-Labbé
+
+#ifndef CGAL_TIMESTAMP_HASH_FUNCTION_H
+#define CGAL_TIMESTAMP_HASH_FUNCTION_H
+
+#include <CGAL/Has_timestamp.h>
+
+#include <boost/functional/hash.hpp>
+#include <boost/utility/enable_if.hpp>
+
+#include <cstddef>
+#include <iterator>
+
+namespace CGAL {
+namespace internal {
+namespace Mesh_3 {
+
+// Hash function for unordered sets/maps with keys of type Vertex_handle
+template <typename Vertex_handle>
+struct Timestamp_hash_function
+{
+  template <typename VH>
+  std::size_t operator()(VH vh,
+                         typename boost::enable_if_c<
+                           CGAL::internal::Has_timestamp<
+                             typename std::iterator_traits<VH>::value_type>::value>::type* = NULL) const
+  {
+    return vh->time_stamp();
+  }
+
+  template <typename VH>
+  std::size_t operator()(VH vh,
+                         typename boost::disable_if_c<
+                           CGAL::internal::Has_timestamp<
+                             typename std::iterator_traits<VH>::value_type>::value>::type* = NULL) const
+  {
+    return boost::hash_value(&*vh);
+  }
+};
+
+} // namespace internal
+} // namespace Mesh_3
+} // namespace CGAL
+
+#endif // CGAL_TIMESTAMP_HASH_FUNCTION_H
