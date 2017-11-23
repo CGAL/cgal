c0ad64426916e67a6c4eabf8159156e507c11de1
Laurent Rineau <laurent.rineau@cgal.org>
2017-11-23 16:41:21 +0100

    Correct syntax for target_compile_definitions

--- Installation/cmake/modules/CGAL_SetupCGAL_ImageIODependencies.cmake
+++ Installation/cmake/modules/CGAL_SetupCGAL_ImageIODependencies.cmake
@@ -62,7 +62,7 @@ function(CGAL_setup_CGAL_ImageIO_dependencies target)
     target_include_directories( CGAL_ImageIO SYSTEM ${keyword} ${ZLIB_INCLUDE_DIRS})
     target_link_libraries( CGAL_ImageIO ${keyword} ${ZLIB_LIBRARIES})
 
-    target_compile_definitions( CGAL_ImageIO ${keyword} "-DCGAL_USE_ZLIB")
+    target_compile_definitions( CGAL_ImageIO ${keyword} CGAL_USE_ZLIB=1)
     if(NOT ARGV1 STREQUAL INTERFACE)
       set_target_properties(CGAL_ImageIO PROPERTIES CGAL_TARGET_USES_ZLIB TRUE)
     endif()
