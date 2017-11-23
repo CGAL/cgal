6434be16d5219a647f0084ea8e2a02357f7c3249
Lingjie Zhu <lingjie.zhu@nlpr.ia.ac.cn>
2017-11-23 17:26:06 +0800

    doc: add another approximation example files

---
+++ Surface_mesh_approximation/examples/Surface_mesh_approximation/vsa_approximation_2_example.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polyhedron_3.h>
+#include <CGAL/vsa_mesh_approximation.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef CGAL::VSA::Plane_proxy<Kernel> Plane_proxy;
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+typedef boost::graph_traits<Polyhedron>::face_descriptor face_descriptor;
+typedef std::map<face_descriptor, std::size_t> Facet_index_map;
+typedef boost::associative_property_map<Facet_index_map> Facet_proxy_pmap;
+
+int main()
+{
+  // create polyhedral surface and read input surface triangle mesh 
+  Polyhedron input;
+  std::ifstream file("data/mask.off");
+  if (!file || !(file >> input) || input.empty()) {
+    std::cerr << "Invalid off file." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // output indexed triangle mesh
+  std::vector<Kernel::Point_3> points;
+  std::vector<std::vector<std::size_t> > triangles; // triplets of indices
+
+  // output facet proxy index property map
+  Facet_index_map fidx_map;
+  BOOST_FOREACH(face_descriptor f, faces(input))
+    fidx_map[f] = 0;
+  Facet_proxy_pmap fpxmap(fidx_map);
+
+  // output plane proxies;
+  std::vector<Plane_proxy> proxies;
+
+  // free function interface with named parameters
+  CGAL::VSA::mesh_approximation(input,
+    std::back_inserter(points),
+    std::back_inserter(triangles),
+    CGAL::VSA::parameters::min_error_drop(0.05). // seeding with minimum error drop
+    nb_of_iterations(40). // set number of clustering iterations after seeding
+    mesh_chord_error(0.3). // set chord approximation error threshold when meshing
+    facet_proxy_map(fpxmap). // get facet partition map
+    proxies(std::back_inserter(proxies))); // get proxies
+
+  return EXIT_SUCCESS;
+}
