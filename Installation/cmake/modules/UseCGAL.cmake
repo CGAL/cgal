249aed9fd1d59787b3da4f0bcabcefe5db9ff249
Laurent Rineau <laurent.rineau@cgal.org>
2017-11-23 17:06:21 +0100

    Hack for the CGAL test suite

--- Installation/cmake/modules/UseCGAL.cmake
+++ Installation/cmake/modules/UseCGAL.cmake
@@ -18,7 +18,11 @@ if(NOT USE_CGAL_FILE_INCLUDED)
   set(USE_CGAL_FILE_INCLUDED 1)
 
   include(${CMAKE_CURRENT_LIST_DIR}/CGAL_Common.cmake)
-  include(${CMAKE_CURRENT_LIST_DIR}/CGAL_display_flags.cmake)
+  if( RUNNING_CGAL_AUTO_TEST )
+    include(${CMAKE_CURRENT_LIST_DIR}/CGAL_SetupFlags.cmake)
+  else()
+    include(${CMAKE_CURRENT_LIST_DIR}/CGAL_display_flags.cmake)
+  endif()
   include(${CMAKE_CURRENT_LIST_DIR}/CGAL_GeneratorSpecificSettings.cmake)
   include(${CMAKE_CURRENT_LIST_DIR}/CGAL_TweakFindBoost.cmake)
 
