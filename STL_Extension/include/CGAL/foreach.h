ac465fa133b474c502dd4f2b9273453eb86f7d47
Sébastien Loriot <sebastien.loriot@cgal.org>
2017-11-27 16:45:24 +0100

    add CGAL_FOREACH macro using either c++11 range-based for loop or BOOST_FOREACH
    this is to work around a bug observed with MSVC 2015

---
+++ STL_Extension/include/CGAL/foreach.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2008  INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: LGPL-3.0+
+//
+// Author(s)     : Sébastien Loriot
+
+#ifndef CGAL_FOREACH_H
+#define CGAL_FOREACH_H
+
+#include <CGAL/config.h>
+
+#if BOOST_VERSION >= 105100 && !defined(BOOST_NO_CXX11_RANGE_BASED_FOR)
+#define CGAL_FOREACH(A,B) for(A : B)
+#else
+#include <boost/foreach.hpp>
+#define CGAL_FOREACH(A,B) BOOST_FOREACH(A, B)
+#endif
+
+#endif
