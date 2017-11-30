3f15cf7307f92d02510eb838a5b8968db3bd1541
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2017-11-30 20:27:29 +0100

    Add basic viewer for t2

---
+++ Triangulation_2/include/CGAL/Triangulation_2_viewer_qt.h
@@ -0,0 +1,136 @@
+// Copyright (c) 2017 CNRS and LIRIS' Establishments (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+// Author(s)     : Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
+
+#ifndef CGAL_T2_VIEWER_QT_H
+#define CGAL_T2_VIEWER_QT_H
+
+#include <CGAL/Qt/Basic_viewer_qt.h>
+#include <CGAL/Random.h>
+
+// Default color functor; user can change it to have its own face color
+struct DefaultColorFunctor
+{
+  template<typename T2>
+  static CGAL::Color run(const T2& at2,
+                         const typename T2::Finite_faces_iterator fh)
+  {
+    // Here dh is the smaller dart of its face
+    CGAL::Random random((unsigned long int)(&*fh));
+    return get_random_color(random);
+  }
+};
+
+// Viewer class for T2 
+template<class T2, class ColorFunctor>
+class SimpleTriangulation2ViewerQt : public Basic_viewer_qt
+{
+  typedef Basic_viewer_qt                     Base;
+  typedef typename T2::Vertex_handle          Vertex_const_handle;
+  typedef typename T2::Finite_edges_iterator Edge_const_handle;
+  typedef typename T2::Finite_faces_iterator Facet_const_handle;
+  typedef typename T2::Point                  Point;
+ 
+public:
+  /// Construct the viewer.
+  /// @param at2 the t2 to view
+  /// @param title the title of the window
+  /// @param anofaces if true, do not draw faces (faces are not computed; this can be
+  ///        usefull for very big object where this time could be long)
+  SimpleTriangulation2ViewerQt(const T2& at2, const char* title="", bool anofaces=false) :
+    Base(title),
+    t2(at2),
+    m_nofaces(anofaces)
+  {
+    compute_elements();
+  }
+
+protected:
+  void compute_face(Facet_const_handle fh)
+  {
+    CGAL::Color c=ColorFunctor::run(t2, fh);
+    face_begin(c);
+
+    add_point_in_face(fh->vertex(0)->point());
+    add_point_in_face(fh->vertex(1)->point());
+    add_point_in_face(fh->vertex(2)->point());
+    
+    face_end();
+  }
+
+  void compute_edge(Edge_const_handle eh)
+  {
+    add_segment(eh->first->vertex(eh->first->cw(eh->second))->point(),
+                eh->first->vertex(eh->first->ccw(eh->second))->point());
+  }
+
+  void compute_vertex(Vertex_const_handle vh)
+  { add_point(vh->point()); }
+
+  void compute_elements()
+  {
+    clear();
+
+    for (typename T2::Finite_faces_iterator it=t2.finite_faces_begin();
+         it!=t2.finite_faces_end(); ++it)
+    { compute_face(it); } 
+
+    for (typename T2::Finite_edges_iterator it=t2.finite_edges_begin();
+         it!=t2.finite_edges_end(); ++it)
+    { compute_edge(it); } 
+
+    for (typename T2::Finite_vertices_iterator it=t2.finite_vertices_begin();
+         it!=t2.finite_vertices_end(); ++it)
+    { compute_vertex(it); } 
+  }
+
+  virtual void keyPressEvent(QKeyEvent *e)
+  {
+    const Qt::KeyboardModifiers modifiers = e->modifiers();
+    Base::keyPressEvent(e);
+  }
+
+protected:
+  const T2& t2;
+  bool m_nofaces;
+};
+
+  
+template<class T2, class ColorFunctor>
+void display(const T2& at2,
+             const char* title="",
+             bool nofill=false)
+{
+  int argc=1;
+
+  const char* argv[2]={"T2 viewer","\0"};
+  QApplication app(argc,const_cast<char**>(argv));
+
+  SimpleTriangulation2ViewerQt<T2, ColorFunctor> mainwindow(at2, title, nofill);
+  mainwindow.show();
+
+  app.exec();
+}
+
+template<class T2>
+void display(const T2& at2,
+             const char* title="",
+             bool nofill=false)
+{ return display<T2, DefaultColorFunctor>(at2, title, nofill); }
+
+#endif // CGAL_T2_VIEWER_QT_H
