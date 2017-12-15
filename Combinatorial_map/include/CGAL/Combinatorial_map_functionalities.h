8439951b20191b9f007a9654d3fe00f433babbbd
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2017-12-15 10:36:12 +0100

    add the quadrangulation step

--- Combinatorial_map/include/CGAL/Combinatorial_map_functionalities.h
+++ Combinatorial_map/include/CGAL/Combinatorial_map_functionalities.h
@@ -75,12 +75,12 @@ namespace CGAL {
     {
       assert(dh!=NULL);
       assert(faces.find(dh)!=faces.end());
-      return uftrees.find(faces.find(dh));
+      return uftrees.find(faces.find(dh)->second);
     }
 
     UFTree_handle get_opposite_face(Dart_handle dh)
     {
-      assert(m_map.template is_free<2>(dh));
+      assert(!m_map.template is_free<2>(dh));
       return get_face(m_map.template beta<2>(dh));
     }
     
@@ -121,22 +121,22 @@ namespace CGAL {
             
             // We remove dangling edges and degree two edges.
             // The two first tests allow to keep isolated edges (case of spheres)
-            if ((m_map->template beta<0>(currentdart)!=m_map->template beta<2>(currentdart) ||
-                 m_map->template beta<1>(currentdart)!=m_map->template beta<2>(currentdart)) &&
+            if ((m_map.template beta<0>(currentdart)!=m_map.template beta<2>(currentdart) ||
+                 m_map.template beta<1>(currentdart)!=m_map.template beta<2>(currentdart)) &&
                 (dangling || get_face(currentdart)!=get_opposite_face(currentdart)))
             {
               // We push in the stack all the neighboors of the current
               // edge that will become dangling after the removal.
-              if (!m_map.template is_free<2>(m_map->template beta<0>(currentdart)) &&
-                  m_map->template beta<0,2>(currentdart)==m_map->template beta<2,1>(currentdart))
-              { to_treat.push(m_map->template beta<0>(currentdart)); }
-              if (!m_map.template is_free<2>(m_map->template beta<1>(currentdart)) &&
-                  m_map->template beta<1,2>(currentdart)==m_map->template beta<2,0>(currentdart))
-              { to_treat.push(m_map->template beta<1>(currentdart)); }
-
+              /*              if (!m_map.template is_free<2>(m_map.template beta<0>(currentdart)) &&
+                  m_map.template beta<0,2>(currentdart)==m_map.template beta<2,1>(currentdart))
+              { to_treat.push(m_map.template beta<0>(currentdart)); }
+              if (!m_map.template is_free<2>(m_map.template beta<1>(currentdart)) &&
+                  m_map.template beta<1,2>(currentdart)==m_map.template beta<2,0>(currentdart))
+              { to_treat.push(m_map.template beta<1>(currentdart)); }
+              */
               // TODO LATER (?) OPTIMIZE AND REPLACE THE REMOVE_CELL CALL BY THE MODIFICATION BY HAND
               // Moreover, we make the removal manually instead of calling
-              // remove_cell<2>(currentdart) for optimisation reasons.
+              // remove_cell<1>(currentdart) for optimisation reasons.
               // Now we update alpha2
               /*t1 = alpha(*itEdge, 1);
               if ( !isMarked(t1, toDelete1) )
@@ -156,9 +156,9 @@ namespace CGAL {
                 }*/
 
               if (!dangling)
-              { uftrees.merge(get_face(currentdart), get_opposite_face(currentdart)); }
+              { uftrees.unify_sets(get_face(currentdart), get_opposite_face(currentdart)); }
 
-              remove_cell<2>(currentdart);
+              m_map.remove_cell<1>(currentdart);
             }
           }
         }
@@ -169,6 +169,33 @@ namespace CGAL {
     
     void surface_simplification_in_one_vertex()
     {
+      for (typename Map::Dart_range::iterator it=m_map.darts().begin(), itend=m_map.darts().end();
+           it!=itend; ++it)
+      {
+        if (m_map.vertex_attribute(it)!=m_map.vertex_attribute(m_map.template beta<2>(it)))
+        { m_map.contract_cell<1>(it); }
+      }
+    }
+    
+    void surface_quadrangulate()
+    {
+      // Here the map has only one face and one vertex.
+      typename Map::size_type oldedges=m_map.get_new_mark();
+      m_map.negate_mark(oldedges); // now all edges are marked
+      
+      // 1) We insert a vertex in the face (note that all points have the same geometry).
+      //    New edges created by the operation are not marked.
+      m_map.insert_point_in_cell_2(m_map.darts().begin(), m_map.point(m_map.darts().begin()));
+
+      // 2) We remove all the old edges.
+      for (typename Map::Dart_range::iterator it=m_map.darts().begin(), itend=m_map.darts().end();
+           it!=itend; ++it)
+      {
+        if (m_map.is_marked(it, oldedges))
+        { m_map.remove_cell<1>(it); }
+      }
+
+      m_map.free_mark(oldedges);
     }
 
   protected:
