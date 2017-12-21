989264e32b1bd92a141c137c66c119e589fb64f4
Sébastien Loriot <sebastien.loriot@cgal.org>
2017-12-21 15:08:20 +0100

    only the counts must be tight
    lSize must be num_edges as we use an array for edge property
    we access using halfedge index properties

--- Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Detail/Edge_collapse_impl.h
+++ Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Detail/Edge_collapse_impl.h
@@ -111,11 +111,11 @@ namespace Surface_mesh_simplification
   
   Equal_3 equal_points = Traits().equal_3_object();
     
-  size_type lSize = static_cast<size_type>(
-                      std::distance( boost::begin(edges(mSurface)),
-                                     boost::end(edges(mSurface)) ) );
+  size_type lSize = num_edges(mSurface);
 
-  mInitialEdgeCount = mCurrentEdgeCount = lSize;
+  mInitialEdgeCount = mCurrentEdgeCount = static_cast<size_type>(
+                      std::distance( boost::begin(edges(mSurface)),
+                                     boost::end(edges(mSurface)) ) );;
   
   mEdgeDataArray.reset( new Edge_data[lSize] ) ;
   
