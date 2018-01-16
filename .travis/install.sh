161cf61906a2962b4d2b65c5773704a75daf0eff
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-01-16 13:48:10 +0100

    Add ppas manualy

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo -E apt-add-repository -y "ppa:ppsspp/cmake"
+sudo -E apt-add-repository -y "ppa:hedges/qt5.5"
+sudo apt-get update
 for pkg in clang-3.6 zsh cmake libboost1.55-dev libboost-system1.55-dev libboost-program-options1.55-dev libboost-thread1.55-dev libboost-iostreams1.55-dev libgmp-dev libmpfr-dev libmpfi-dev zlib1g-dev libeigen3-dev qt55base qt55script qt55svg qt55tools qt55graphicaleffects mesa-common-dev libglu1-mesa-dev
 do
   DONE=0
