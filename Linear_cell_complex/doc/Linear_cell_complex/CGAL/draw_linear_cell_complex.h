381d09104a658cfbd300f2a962ded8b8f4fb4fc0
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2018-01-25 17:09:50 +0100

    Small update of LCC doc, following Andreas remarks.

--- Linear_cell_complex/doc/Linear_cell_complex/CGAL/draw_linear_cell_complex.h
+++ Linear_cell_complex/doc/Linear_cell_complex/CGAL/draw_linear_cell_complex.h
@@ -5,6 +5,9 @@ namespace CGAL {
 
 \cgalModifBegin
 Open a new window and draw `alcc`, a model of the `LinearCellComplex` concept. The function is blocking, that is the program continues as soon as the user closes the window. This function requires CGAL_Qt5, and is only available if the flag CGAL_USE_BASIC_VIEWER is defined at compile time.
+\tparam LCC a model of the `LinearCellComplex` concept.
+\param alcc The linear cell complex to draw.
+
 \cgalModifEnd 
 */
 template<class LCC>
