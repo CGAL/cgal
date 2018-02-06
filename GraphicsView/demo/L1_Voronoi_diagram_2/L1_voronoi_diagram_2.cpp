c86701de81956a2b9df0a7bb11bd4f8ef33efdc4
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-02-06 15:29:32 +0100

    Add a filter for L1_voronoi_diagram_2 demo

--- GraphicsView/demo/L1_Voronoi_diagram_2/L1_voronoi_diagram_2.cpp
+++ GraphicsView/demo/L1_Voronoi_diagram_2/L1_voronoi_diagram_2.cpp
@@ -277,7 +277,9 @@ MainWindow::on_actionLoadPoints_triggered()
 {
   QString fileName = QFileDialog::getOpenFileName(this,
 						  tr("Open Points file"),
-						  ".");
+                                                  ".",
+                                                  tr("CGAL files (*.pts.cgal);;"
+                                                     "All files (*)"));
   if(! fileName.isEmpty()){
     open(fileName);
   }
@@ -311,7 +313,9 @@ MainWindow::on_actionSavePoints_triggered()
 {
   QString fileName = QFileDialog::getSaveFileName(this,
 						  tr("Save points"),
-						  ".");
+                                                  ".",
+                                                  tr("CGAL files (*.pts.cgal);;"
+                                                     "All files (*)"));
   if(! fileName.isEmpty()) {
     std::ofstream ofs(qPrintable(fileName));
     for(Points::iterator it = m_sites.begin();
