f15edafaafb86e87f0215659652901dbf8f4ba83
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-03-02 08:12:29 +0100

    Add missing traits

---
+++ Polygon_IO/include/CGAL/IO/traits_multipolygon.h
@@ -0,0 +1,19 @@
+#ifndef CGAL_TRAITS_MULTIPOLYGON_H
+#define CGAL_TRAITS_MULTIPOLYGON_H
+#include <CGAL/Geometry_container.h>
+#include <boost/geometry/geometry.hpp>
+
+
+namespace boost{
+namespace geometry{
+namespace traits{
+// WKT traits for MultiPolygon
+template< typename R >
+struct tag<CGAL::Geometry_container<R, multi_polygon_tag> >
+{ typedef multi_polygon_tag type; };
+
+}//end traits
+}//end geometry
+}//end boost
+
+#endif 
