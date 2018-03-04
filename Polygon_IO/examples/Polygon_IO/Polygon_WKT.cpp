71e59e7228f38ed3d5fdc55851635d497a45cc65
Andreas Fabri <andreas.fabri@geometryfactory.com>
2018-03-04 11:18:44 +0000

    fix example

--- Polygon_IO/examples/Polygon_IO/Polygon_WKT.cpp
+++ Polygon_IO/examples/Polygon_IO/Polygon_WKT.cpp
@@ -21,21 +21,27 @@ int main(int argc, char* argv[])
   typedef CGAL::Point_2<Kernel> Point;
   typedef CGAL::Polygon_with_holes_2<Kernel> Polygon;
   typedef CGAL::Geometry_container<std::vector<Polygon>, boost::geometry::multi_polygon_tag> MultiPolygon;
-  std::ifstream is((argc>1)?argv[1]:"data/polygons.wkt");
-  std::list<Polygon> polys;
-  do
+
   {
-    Polygon p;
-    CGAL::read_polygon_WKT(is, p);
-    if(!p.outer_boundary().is_empty())
-      polys.push_back(p);
-  }while(is.good() && !is.eof());
-  BOOST_FOREACH(Polygon p, polys)
+    std::ifstream is((argc>1)?argv[1]:"data/polygons.wkt");
+    std::list<Polygon> polys;
+    do
+      {
+        Polygon p;
+        CGAL::read_polygon_WKT(is, p);
+        if(!p.outer_boundary().is_empty())
+          polys.push_back(p);
+      }while(is.good() && !is.eof());
+    BOOST_FOREACH(Polygon p, polys)
       std::cout<<p<<std::endl;
+  }
   
-  is = std::ifstream((argc>2)?argv[2]:"data/multipolygon.wkt");
-  MultiPolygon mp;
-  CGAL::read_multipolygon_WKT(is, mp);
-  BOOST_FOREACH(Polygon p, mp)
-    std::cout<<p<<std::endl;
+  {
+    std::ifstream  is((argc>2)?argv[2]:"data/multipolygon.wkt");
+    MultiPolygon mp;
+    CGAL::read_multipolygon_WKT(is, mp);
+    BOOST_FOREACH(Polygon p, mp)
+      std::cout<<p<<std::endl;
+  }
+  return 0;
 }
