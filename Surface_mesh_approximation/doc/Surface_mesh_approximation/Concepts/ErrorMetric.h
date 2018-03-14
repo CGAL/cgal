d67cf338a02b67afd62e9ad8728417802e95b29b
Pierre Alliez <pierre.alliez@inria.fr>
2018-03-14 22:46:43 +0100

    Error metric user manual

--- Surface_mesh_approximation/doc/Surface_mesh_approximation/Concepts/ErrorMetric.h
+++ Surface_mesh_approximation/doc/Surface_mesh_approximation/Concepts/ErrorMetric.h
@@ -2,7 +2,8 @@
 \ingroup PkgTSMAConcepts
 \cgalConcept
 
-The concept `ErrorMetric` computes the fitting error of a face to a `Proxy`, used in CGAL::VSA_approximation.
+The concept `ErrorMetric` computes the fitting error from
+ a face to a `Proxy`, used in CGAL::VSA_approximation.
 
 \cgalHasModel `CGAL::L21_metric`
 \cgalHasModel `CGAL::L2_metric`
@@ -10,23 +11,23 @@ The concept `ErrorMetric` computes the fitting error of a face to a `Proxy`, use
 
 class ErrorMetric {
 public:
-  /// A number type model of `Field` and `RealEmbeddable`
+  /// Number type model of `Field` and `RealEmbeddable`
   typedef unspecified_type FT;
-  /// Triangle mesh face descriptor.
+  /// Triangle mesh face descriptor
   typedef unspecified_type face_descriptor;
-  /// Parameterized shape proxy.
+  /// Parameterized shape proxy
   typedef unspecified_type Proxy;
 
   /// @name Operations
   /// A model of this concept must provide:
   /// @{
 
-  /// returns the fitting error of a face f to the Proxy px.
+  /// returns the fitting error from face f to Proxy px.
   FT compute_error(const face_descriptor &f, const Proxy &px) const;
 
-  /// returns the fitted proxy from a range of facets.
+  /// returns the fitted proxy for a range of facets.
   template <typename FacetIterator>
-  Proxy fit_proxy(const FacetIterator &beg, const FacetIterator &end) const;
+  Proxy fit_proxy(const FacetIterator &begin, const FacetIterator &end) const;
 
   /// }
 
