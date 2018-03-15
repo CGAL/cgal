ab8719458374af079ae0a1ba34f174180617f925
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-03-15 14:15:13 +0100

    Makes the number of cores for make a variable.

--- Scripts/developer_scripts/Bundle_polyhedron_demo_with_appimage.sh
+++ Scripts/developer_scripts/Bundle_polyhedron_demo_with_appimage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$1" == '--help' ]; then
-  echo "Usage: $0 <path to CGAL> <path to output>"
+  echo "Usage: $0 <path to CGAL> <path to output> <number of cores to dedicate>"
   echo "Builds and packages the Polyhedron demo form the CGAL dir."
   exit 0
 fi
-docker run --rm -v "$2":/results:Z -v "$1":/cgal:ro docker.io/cgal/bundle-3d-demo '/scripts/build.sh -j6 && /scripts/deploy.sh'
+docker run --rm -v "$2":/results:Z -v "$1":/cgal:ro docker.io/cgal/bundle-3d-demo "/scripts/build.sh -j$3 && /scripts/deploy.sh"
 
