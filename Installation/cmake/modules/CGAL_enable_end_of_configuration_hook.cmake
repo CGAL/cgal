b262232f494e5d0a1ee51880a47965954b848fa9
Laurent Rineau <laurent.rineau@cgal.org>
2018-03-22 13:11:29 +0100

    Remove the warnings from the testsuite
    In the testsuite, with `RUNNING_CGAL_AUTO_TEST`, the message will end with:
    > Set CGAL_DO_NOT_WARN_ABOUT_CMAKE_BUILD_TYPE to TRUE if you want to
    > disable this notice.
    
    instead of
    
    > disable this warning.
    
    Because otherwise our test results are full of yellow 'w' (warnings).

--- Installation/cmake/modules/CGAL_enable_end_of_configuration_hook.cmake
+++ Installation/cmake/modules/CGAL_enable_end_of_configuration_hook.cmake
@@ -22,9 +22,11 @@ function(CGAL_run_at_the_end_of_configuration variable access value current_list
   # Warn when CMAKE_BUILD_TYPE is empty or Debug
   if(DEFINED CMAKE_BUILD_TYPE AND ( NOT CMAKE_BUILD_TYPE OR CMAKE_BUILD_TYPE STREQUAL "Debug") )
     set(keyword WARNING)
+    set(type warning)
     if(RUNNING_CGAL_AUTO_TEST)
       # No warning in the CMake test suite, but a status message
-      set(keyword )
+      set(keyword)
+      set(type notice)
     endif()
     if(NOT CGAL_DO_NOT_WARN_ABOUT_CMAKE_BUILD_TYPE)
       message(${keyword} "\
@@ -33,7 +35,7 @@ CGAL performance notice:\n\
 The variable CMAKE_BUILD_TYPE is set to \"${CMAKE_BUILD_TYPE}\". For \
 performance reasons, you should set CMAKE_BUILD_TYPE to \"Release\".\n\
 Set CGAL_DO_NOT_WARN_ABOUT_CMAKE_BUILD_TYPE to TRUE if you want to \
-disable this warning.\n\
+disable this ${type}.\n\
 =======================================================================\
 ")
     endif()
