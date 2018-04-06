c570c7ea7d209f17c31af4451226533fe8c21343
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-04-06 11:04:39 +0200

    Change text in licenzse headers tospecify this is a fork

--- GraphicsView/include/CGAL/Qt/frame_impl.h
+++ GraphicsView/include/CGAL/Qt/frame_impl.h
@@ -2,12 +2,12 @@
 
  Copyright (C) 2002-2014 Gilles Debunne. All rights reserved.
 
- This file is part of the QGLViewer library version 2.7.0.
+ This file is part of a frok of the QGLViewer library version 2.7.0.
 
  http://www.libqglviewer.com - contact@libqglviewer.com
 
  This file may be used under the terms of the GNU General Public License 
- versions 2.0 or 3.0 as published by the Free Software Foundation and
+ versions 3.0 as published by the Free Software Foundation and
  appearing in the LICENSE file included in the packaging of this file.
  In addition, as a special exception, Gilles Debunne gives you certain 
  additional rights, described in the file GPL_EXCEPTION in this package.
