89882de03960f5638da07ae327ea7da529e8524c
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-04-06 10:33:16 +0200

    Update licenses

--- GraphicsView/include/CGAL/Qt/viewer_actions.h
+++ GraphicsView/include/CGAL/Qt/viewer_actions.h
@@ -1,3 +1,24 @@
+/****************************************************************************
+
+ Copyright (C) 2002-2014 Gilles Debunne. All rights reserved.
+
+ This file is part of the QGLViewer library version 2.7.0.
+
+ http://www.libqglviewer.com - contact@libqglviewer.com
+
+ This file may be used under the terms of the GNU General Public License 
+ versions 2.0 or 3.0 as published by the Free Software Foundation and
+ appearing in the LICENSE file included in the packaging of this file.
+ In addition, as a special exception, Gilles Debunne gives you certain 
+ additional rights, described in the file GPL_EXCEPTION in this package.
+
+ libQGLViewer uses dual licensing. Commercial/proprietary software must
+ purchase a libQGLViewer Commercial License.
+
+ This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+*****************************************************************************/
 // SPDX-License-Identifier: (GPL-2.0 OR GPL-3.0)
 #ifndef VIEWER_ACTIONS_H
 #define VIEWER_ACTIONS_H
