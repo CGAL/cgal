f94a50646f1d18b79dd7e4d35a212855924aa40a
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-04-12 15:28:56 +0200

    Add license to new file

--- GraphicsView/include/CGAL/Qt/image_interface.h
+++ GraphicsView/include/CGAL/Qt/image_interface.h
@@ -1,3 +1,27 @@
+/****************************************************************************
+
+ Copyright (c) 2018  GeometryFactory Sarl (France).
+ Copyright (C) 2002-2014 Gilles Debunne. All rights reserved.
+
+ This file is part of a fork of the CGAL::QGLViewer library version 2.7.0.
+
+ http://www.libqglviewer.com - contact@libqglviewer.com
+
+ This file may be used under the terms of the GNU General Public License 
+ version 3.0 as published by the Free Software Foundation and
+ appearing in the LICENSE file included in the packaging of this file.
+
+ libCGAL::QGLViewer uses dual licensing. Commercial/proprietary software must
+ purchase a libCGAL::QGLViewer Commercial License.
+
+ This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+*****************************************************************************/
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0
+
 #ifndef CGAL_IMAGE_INTERFACE_H
 #define CGAL_IMAGE_INTERFACE_H
 #include <QDialog>
