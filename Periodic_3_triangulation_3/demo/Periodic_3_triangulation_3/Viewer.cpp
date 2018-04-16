dbfa5bf8ad3aa630ee9ac5b88176cca762c31339
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-04-16 16:59:17 +0200

    Fix remaining Viewer error in P3T3 demo

--- Periodic_3_triangulation_3/demo/Periodic_3_triangulation_3/Viewer.cpp
+++ Periodic_3_triangulation_3/demo/Periodic_3_triangulation_3/Viewer.cpp
@@ -2,7 +2,7 @@
  #include <CGAL/Qt/CreateOpenGLContext.h>
 
 Viewer::Viewer(QWidget *parent)
-: CGAL::QGLViewer(CGAL::Qt::createOpenGLContext(),parent)
+: CGAL::QGLViewer(parent)
 {}
 Viewer::~Viewer()
 {}
