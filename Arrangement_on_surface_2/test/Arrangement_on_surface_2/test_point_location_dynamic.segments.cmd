dd8e500654eaab1bb4164b2435bcb93194a748fa
Efi Fogel <efifogel@gmail.com>
2018-05-06 15:53:12 +0300

    Added a test case that inserts a bunch of segments and then deletes them before applying a point-location op

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_point_location_dynamic.segments.cmd
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_point_location_dynamic.segments.cmd
@@ -1,1 +1,2 @@
 ./data/empty.zero ./data/point_location/segments/xcurves/test10.txt ./data/empty.zero ./data/point_location/segments/ops/test10.txt ./data/point_location/segments/queries/test10.txt
+./data/empty.zero ./data/point_location/segments/xcurves/test11.txt ./data/empty.zero ./data/point_location/segments/ops/test11.txt ./data/point_location/segments/queries/test03.txt
