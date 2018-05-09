99723330df5a690824b03bd43abf21a85a199346
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-05-09 11:14:20 +0200

    changed inheritance of vertex base; forwarded methods from base class

--- Periodic_4_hyperbolic_triangulation_2/include/CGAL/Periodic_4_hyperbolic_triangulation_vertex_base_2.h
+++ Periodic_4_hyperbolic_triangulation_2/include/CGAL/Periodic_4_hyperbolic_triangulation_vertex_base_2.h
@@ -24,20 +24,21 @@
 #define CGAL_PERIODIC_4_HYPERBOLIC_TRIANGULATION_VERTEX_BASE_2_H
 
 #include <CGAL/basic.h>
-#include <CGAL/Dummy_tds_2.h>
+#include <CGAL/Triangulation_ds_vertex_base_2.h>
+#include <CGAL/Triangulation_vertex_base_2.h>
+
 
 namespace CGAL {
 
-template< class GT, class Vb = CGAL::Triangulation_vertex_base_2<GT> >
+template< class GT, class Vb = CGAL::Triangulation_vertex_base_2<GT, CGAL::Triangulation_ds_vertex_base_2<> > >
 class Periodic_4_hyperbolic_triangulation_vertex_base_2 : public Vb {
 public:
-  typedef Vb                                         Base;
-	typedef typename Vb::Triangulation_data_structure  Triangulation_data_structure;
-  typedef Triangulation_data_structure               TDS;
-	typedef typename TDS::Vertex_handle                Vertex_handle;
-	typedef typename TDS::Face_handle 		             Face_handle;
-	typedef typename GT::Point_2			                 Point;
-	typedef typename GT::Hyperbolic_translation 			                 Hyperbolic_translation;
+  typedef Vb                                          Base;
+	typedef typename Vb::Triangulation_data_structure   Triangulation_data_structure;
+	typedef typename Triangulation_data_structure::Face_handle 		             
+                                                      Face_handle;
+	typedef typename GT::Point_2			                  Point;
+	typedef typename GT::Hyperbolic_translation 			  Hyperbolic_translation;
 
 
 	template <typename TDS2>
@@ -47,74 +48,45 @@ public:
   	};
 
 private:
-	Face_handle _face;
-	Point 		_p;
 	Hyperbolic_translation 		_tr; 	// Used only during insert();
-	bool 		_stored_translation;
+	bool 		                  _stored_translation;
 
 public:
 	Periodic_4_hyperbolic_triangulation_vertex_base_2() : 
-  Base(), _face(), _stored_translation(false)
+  Base(), _stored_translation(false)
 	{}
 
 	Periodic_4_hyperbolic_triangulation_vertex_base_2(const Point & p) : 
-	Base(p), _face(), _p(p), _stored_translation(false) 
+	Base(p), _stored_translation(false) 
 	{}
 
   Periodic_4_hyperbolic_triangulation_vertex_base_2(const Point & p, Face_handle fh) : 
-  Base(p, fh), _face(fh), _p(p), _stored_translation(false) 
+  Base(p, fh), _stored_translation(false) 
   {}
 
 	Periodic_4_hyperbolic_triangulation_vertex_base_2(const Face_handle& fh) :
-	Base(), _face(fh), _stored_translation(false) 
+	Base(), _stored_translation(false) 
 	{}
 
-	const Face_handle& face() {
-		return _face;
-	} 
-
-	void set_face(const Face_handle& fh) {
-		_face = fh;
+	void set_translation(Hyperbolic_translation tr) {
+		if (!_stored_translation) {
+			_tr = tr;
+			_stored_translation = true;
+		}
 	}
 
-	void set_point(const Point & p) { _p = p; }
-  	
-  	const Point&  point() const { return _p; }
+	Hyperbolic_translation translation() const {
+		return _tr;
+	}
 
-  	// the non const version of point() is undocumented
-  	// but needed to make the point iterator work
-  	// using Lutz projection scheme
-  	Point&        point() { return _p; }
+	void clear_translation() {
+		_tr = Hyperbolic_translation();
+		_stored_translation = false;
+	}
 
-
-  	// For use by the Compact_container.
-  	void * for_compact_container() const { 
-  		return _face.for_compact_container(); 
-  	}
-  	
-  	void * & for_compact_container() { 
-  		return _face.for_compact_container(); 
-  	}
-
-  	void store_translation(Hyperbolic_translation tr) {
-  		if (!_stored_translation) {
-  			_tr = tr;
-  			_stored_translation = true;
-  		}
-  	}
-
-  	Hyperbolic_translation get_translation() {
-  		return _tr;
-  	}
-
-  	void remove_translation() {
-  		_tr = Hyperbolic_translation();
-  		_stored_translation = false;
-  	}
-
-  	bool stored_translation() {
-  		return _stored_translation;
-  	}
+	bool get_translation_flag() const {
+		return _stored_translation;
+	}
 
 };
 
@@ -132,20 +104,6 @@ operator<<(std::ostream &os, Periodic_4_hyperbolic_triangulation_vertex_base_2<T
 	return os;
 }
 
-
-// Specialization vor 'void'
-template<typename GT>
-class Periodic_4_hyperbolic_triangulation_vertex_base_2<GT, void> {
-public:
-	typedef Dummy_tds_2 									Triangulation_data_structure;
-	typedef Triangulation_data_structure::Vertex_handle		Vertex_handle;
-	typedef Triangulation_data_structure::Face_handle 		Face_handle;
-	struct  Point {};	
-	template<typename TDS2>
-	struct Rebind_TDS {
-    	typedef Periodic_4_hyperbolic_triangulation_vertex_base_2<GT, TDS2> Other;
-  	};
-};
 
 }  // namespace CGAL
 
