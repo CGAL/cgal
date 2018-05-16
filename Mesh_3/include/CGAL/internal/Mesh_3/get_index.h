6653dabc628ea9a175c3b58f35f7f24f8025c411
Laurent Rineau <laurent.rineau@cgal.org>
2018-05-16 11:07:13 +0200

    Add the license-checking #include

--- Mesh_3/include/CGAL/internal/Mesh_3/get_index.h
+++ Mesh_3/include/CGAL/internal/Mesh_3/get_index.h
@@ -26,6 +26,8 @@
 #ifndef CGAL_INTERNAL_MESH_3_GET_INDEX_3_H
 #define CGAL_INTERNAL_MESH_3_GET_INDEX_3_H
 
+#include <CGAL/license/Mesh_3.h>
+
 #include <CGAL/internal/Mesh_3/indices_management.h>
 
 #endif // CGAL_INTERNAL_MESH_3_GET_INDEX_3_H
