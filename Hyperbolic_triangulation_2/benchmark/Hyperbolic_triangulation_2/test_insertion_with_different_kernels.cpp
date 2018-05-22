560acac4bc778fab225f78ea1785363e238b8f33
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-05-22 10:55:44 +0200

    a few modifications to benchmarks; to examine code and run again

--- Hyperbolic_triangulation_2/benchmark/Hyperbolic_triangulation_2/test_insertion_with_different_kernels.cpp
+++ Hyperbolic_triangulation_2/benchmark/Hyperbolic_triangulation_2/test_insertion_with_different_kernels.cpp
@@ -8,28 +8,32 @@
 #include <CGAL/CORE_Expr.h>
 #include <CGAL/Cartesian.h>
 
+#include <CGAL/Delaunay_triangulation_2.h>
+
 #include <CGAL/Timer.h>
 
 
-typedef CORE::Expr                                                    NT1;
-typedef CGAL::Cartesian<NT1>                                          Kernel1;
-typedef CGAL::Hyperbolic_Delaunay_triangulation_traits_2<Kernel1>     Traits1;
-typedef CGAL::Hyperbolic_Delaunay_triangulation_2<Traits1>            Triangulation1;
-typedef Triangulation1::Point                                         P1;
-typedef Triangulation1::Face_handle                                   FH1;
-typedef Triangulation1::Voronoi_point                                 VP1;
+typedef CORE::Expr                                                     CORE_NumberType;
+typedef CGAL::Cartesian<CORE_NumberType>                               CORE_Kernel;
+typedef CGAL::Hyperbolic_Delaunay_triangulation_traits_2<CORE_Kernel>  CORE_Traits;
+typedef CGAL::Hyperbolic_Delaunay_triangulation_2<CORE_Traits>         CORE_Triangulation;
+typedef CORE_Triangulation::Point                                      CORE_Point;
 
-typedef CGAL::Exact_circular_kernel_2                                 Kernel2;
-typedef CGAL::Hyperbolic_Delaunay_triangulation_CK_traits_2<Kernel2>  Traits2;
-typedef CGAL::Hyperbolic_Delaunay_triangulation_2<Traits2>            Triangulation2;
-typedef Triangulation2::Point                                         P2;
-typedef Triangulation2::Face_handle                                   FH2;
-typedef Triangulation2::Voronoi_point                                 VP2;
+typedef CGAL::Exact_circular_kernel_2                                  CK_Kernel;
+typedef CGAL::Hyperbolic_Delaunay_triangulation_CK_traits_2<CK_Kernel> CK_Traits;
+typedef CGAL::Hyperbolic_Delaunay_triangulation_2<CK_Traits>           CK_Triangulation;
+typedef CK_Triangulation::Point                                        CK_Point;
 
 
-typedef CGAL::Cartesian<double>             Dummy_K;
-typedef Dummy_K::Point_2                    CP;
-typedef CGAL::Creator_uniform_2<double,CP>  Creator;
+typedef CGAL::Delaunay_triangulation_2<CORE_Kernel>                    CORE_Euclidean_Triangulation;
+//typedef CORE_Euclidean_Triangulation::Point_2                          CORE_Euclidean_Point;
+
+typedef CGAL::Delaunay_triangulation_2<CK_Kernel>                      CK_Euclidean_Triangulation;
+//typedef CK_Euclidean_Triangulation::Point_2                            CK_Euclidean_Point;
+
+typedef CGAL::Cartesian<double>                                        Dummy_Kernel;
+typedef Dummy_Kernel::Point_2                                          Double_Point;
+typedef CGAL::Creator_uniform_2<double,Double_Point>                   Creator;
 
 using std::cout;
 using std::endl;
@@ -48,65 +52,98 @@ int main(int argc, char** argv) {
         iterations = atoi(argv[2]);
     }
 
-    std::vector<double> extCK, extCORE;
+    std::vector<double> CK_Time, CORE_Time, CK_Euclidean_Time, CORE_Euclidean_Time;
 
     for (int iter = 0; iter < iterations; iter++) {
         std::cout << "--> generating random points..."; std::cout.flush();
-        std::vector<CP> pts;
-        CGAL::Random_points_in_disc_2<CP,Creator> g( 1.0 );
+        std::vector<Double_Point> pts;
+        CGAL::Random_points_in_disc_2<Double_Point,Creator> g( 1.0 );
         CGAL::cpp11::copy_n( g, N, std::back_inserter(pts));
         std::cout << "  DONE!" << std::endl;
 
         std::cout << "--> copying random points...   "; std::cout.flush();
-        std::vector<P1> pts1;
-        std::vector<P2> pts2;
+        std::vector<CORE_Point>           Vector_CORE;
+        std::vector<CK_Point>             Vector_CK;
+        //std::vector<CK_Euclidean_Point>   Vector_Euclidean_CK;
+        //std::vector<CORE_Euclidean_Point> Vector_Euclidean_CORE;
         for (int i = 0; i < pts.size(); i++) {
-            pts1.push_back( P1(pts[i].x(), pts[i].y()) );
-            pts2.push_back( P2(pts[i].x(), pts[i].y()) );
+            Vector_CK.push_back( CK_Point(pts[i].x(), pts[i].y()) );
+            Vector_CORE.push_back( CORE_Point(pts[i].x(), pts[i].y()) );
+            //Vector_Euclidean_CK.push_back( CK_Euclidean_Point(pts[i].x(), pts[i].y()) );
+            //Vector_Euclidean_CORE.push_back( CORE_Euclidean_Point(pts[i].x(), pts[i].y()) );
         }
         std::cout << "  DONE!" << std::endl;
 
         CGAL::Timer tmr;
-        tmr.reset();
-
-        Triangulation1 tr1;
-        std::cout << "--> inserting into triangulation with CK...        "; std::cout.flush();
-        tmr.start();
-        tr1.insert( pts1.begin(), pts1.end() );
-        tmr.stop();
-        std::cout << "  DONE! Triangulation vertices: " << tr1.number_of_vertices() << std::endl;
-        double t1 = tmr.time();
 
         tmr.reset();
-
-        Triangulation2 tr2;
-        std::cout << "--> inserting into triangulation with CORE::Expr..."; std::cout.flush();
+        CK_Euclidean_Triangulation euclidean_CK_triangulation;
+        std::cout << "--> inserting into Euclidean triangulation with CK...         "; std::cout.flush();
         tmr.start();
-        tr2.insert( pts2.begin(), pts2.end() );
+        euclidean_CK_triangulation.insert( Vector_CK.begin(), Vector_CK.end() );
         tmr.stop();
-        std::cout << "  DONE! Triangulation vertices: " << tr2.number_of_vertices() << std::endl;
-        double t2 = tmr.time();
+        std::cout << "  DONE! Triangulation vertices: " << euclidean_CK_triangulation.number_of_vertices() << std::endl;
+        double Time_Euclidean_CK = tmr.time();
+
+        tmr.reset();
+        CK_Triangulation CK_triangulation;
+        std::cout << "--> inserting into hyperbolic triangulation with CK...        "; std::cout.flush();
+        tmr.start();
+        CK_triangulation.insert( Vector_CK.begin(), Vector_CK.end() );
+        tmr.stop();
+        std::cout << "  DONE! Triangulation vertices: " << CK_triangulation.number_of_vertices() << std::endl;
+        double Time_CK = tmr.time();
+
+        tmr.reset();
+        CORE_Euclidean_Triangulation euclidean_CORE_triangulation;
+        std::cout << "--> inserting into Euclidean triangulation with CORE::Expr... "; std::cout.flush();
+        tmr.start();
+        euclidean_CORE_triangulation.insert( Vector_CORE.begin(), Vector_CORE.end() );
+        tmr.stop();
+        std::cout << "  DONE! Triangulation vertices: " << euclidean_CORE_triangulation.number_of_vertices() << std::endl;
+        double Time_Euclidean_CORE = tmr.time();
+
+        tmr.reset();
+        CORE_Triangulation CORE_triangulation;
+        std::cout << "--> inserting into hyperbolic triangulation with CORE::Expr..."; std::cout.flush();
+        tmr.start();
+        CORE_triangulation.insert( Vector_CORE.begin(), Vector_CORE.end() );
+        tmr.stop();
+        std::cout << "  DONE! Triangulation vertices: " << CORE_triangulation.number_of_vertices() << std::endl;
+        double Time_CORE = tmr.time();
 
         std::cout << std::endl;
-        std::cout << "Time for CK:         " << t1 << std::endl;
-        std::cout << "Time for CORE::Expr: " << t2 << std::endl;
+        std::cout << "Time for Euclidean CK:    " << Time_Euclidean_CK   << std::endl;
+        std::cout << "Time for hyperbolic CK:   " << Time_CK             << std::endl;
+        std::cout << "Time for Euclidean CORE:  " << Time_Euclidean_CORE << std::endl;
+        std::cout << "Time for hyperbolic CORE: " << Time_CORE           << std::endl;
         std::cout << std::endl << "===========================================================" << std::endl << std::endl;
-        extCK.push_back(t1);
-        extCORE.push_back(t2);
+        CK_Time.push_back(Time_CK);
+        CORE_Time.push_back(Time_CORE);
+        CK_Euclidean_Time.push_back(Time_Euclidean_CK);
+        CORE_Euclidean_Time.push_back(Time_Euclidean_CORE);
     }
 
     double avgCK = 0;
     double avgCORE = 0;
+    double avgECK = 0;
+    double avgECORE = 0;
     for (int i = 0; i < iterations; i++) {
-        avgCK += extCK[i];
-        avgCORE += extCORE[i];
+        avgCK += CK_Time[i];
+        avgCORE += CORE_Time[i];
+        avgECK += CK_Euclidean_Time[i];
+        avgECORE += CORE_Euclidean_Time[i];
     }
-    avgCK = avgCK/(double)iterations;
-    avgCORE = avgCORE/(double)iterations;
+    avgCK    = avgCK/(double)iterations;
+    avgCORE  = avgCORE/(double)iterations;
+    avgECK   = avgECK/(double)iterations;
+    avgECORE = avgECORE/(double)iterations;
 
     std::cout << "######################################################################" << std::endl << std::endl;
-    std::cout << "Average time for CK:   " << avgCK   << std::endl;
-    std::cout << "Average time for CORE: " << avgCORE << std::endl;
+    std::cout << "Average time for Euclidean with CK:    " << avgECK   << std::endl;
+    std::cout << "Average time for hyperbolic with CK:   " << avgCK    << std::endl;
+    std::cout << "Average time for Euclidean with CORE:  " << avgECORE << std::endl;
+    std::cout << "Average time for hyperbolic with CORE: " << avgCORE  << std::endl;
     std::cout << std::endl;
 
     return 0;
