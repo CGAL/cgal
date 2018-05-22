284c2d9d337593e973b815745cf56a6f1eb70812
Konstantinos Katrioplas <konst.katrioplas@gmail.com>
2018-05-22 10:34:32 +0200

    python script setting up path

--- Optimal_bounding_box/benchmark/Optimal_bounding_box/draw_benchmark_graph.py
+++ Optimal_bounding_box/benchmark/Optimal_bounding_box/draw_benchmark_graph.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.loadtxt("build/data/times.txt", skiprows = 1)
+#path-to-benchmarks
+benchmarkfile='data/times.txt'
+
+data = np.loadtxt(benchmarkfile, skiprows = 1)
 
 x = data[:, 0]
 y1 = data[:, 1]
 y2 = data[:, 2]
-
-
 
 plt.plot(x, y1, 'go--', label='with convex hull')
 plt.plot(x, y2, 'ro--', label='without convex hull')
---
+++ Optimal_bounding_box/benchmark/Optimal_bounding_box/readme.txt
@@ -0,0 +1,2 @@
+To draw a graph with the benchmark times set the path-to-the-measurments in draw_benchmark_times.py if necessary,
+and run the script with python 3.
