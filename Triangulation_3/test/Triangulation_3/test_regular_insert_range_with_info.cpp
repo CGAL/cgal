8ac412cb8a7c38c0b8f77d700c861e7de8a0cc9c
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2018-05-29 15:29:19 +0200

    Fixed missing includes

--- Triangulation_3/test/Triangulation_3/test_regular_insert_range_with_info.cpp
+++ Triangulation_3/test/Triangulation_3/test_regular_insert_range_with_info.cpp
@@ -6,6 +6,9 @@
 #include <CGAL/Regular_triangulation_3.h>
 #include <CGAL/Triangulation_vertex_base_with_info_3.h>
 
+#include <iostream>
+#include <functional>
+#include <utility>
 #include <vector>
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel            K;
