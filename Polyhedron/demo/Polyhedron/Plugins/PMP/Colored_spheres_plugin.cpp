5703a92d94c84ab2468f63f1a77d1a92c87e3adf
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-06-06 15:41:15 +0200

    POC finished.

--- Polyhedron/demo/Polyhedron/Plugins/PMP/Colored_spheres_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/PMP/Colored_spheres_plugin.cpp
@@ -5,7 +5,6 @@
 #include "SMesh_type.h"
 #include "Scene_surface_mesh_item.h"
 #include "Scene_spheres_item.h"
-#include "Scene_facegraph_item_k_ring_selection.h"
 
 #include <CGAL/compute_average_spacing.h>
 
@@ -47,17 +46,30 @@ public :
   Scene_item* clone() const Q_DECL_OVERRIDE {return 0;}
   QString toolTip() const Q_DECL_OVERRIDE {return QString();}
   QMenu* contextMenu() Q_DECL_OVERRIDE;
-  void select(double orig_x, double orig_y, double orig_z, 
-              double dir_x, double dir_y, double dir_z) Q_DECL_OVERRIDE;
+  Scene_surface_mesh_item* parent() const { return _parent; } 
+  bool eventFilter(QObject *obj, QEvent *event) Q_DECL_OVERRIDE;
   
+  std::size_t sphere_id(const SMesh::Vertex_index& vi) const { return vertex_sphere_map[std::size_t(vi)]; }
+  bool vertex_has_sphere(const SMesh::Vertex_index& vi) const { return vertex_has_sphere_map[std::size_t(vi)]; }
+  std::size_t vertex_degree(const SMesh::Vertex_index& vi) const { return vertex_deg_map[std::size_t(vi)]; }
+  
+  void set_sphere_id(const SMesh::Vertex_index& vi, std::size_t id) { vertex_sphere_map[std::size_t(vi)] = id; }
+  void set_vertex_has_sphere(const SMesh::Vertex_index& vi, bool b) { vertex_has_sphere_map[std::size_t(vi)] = b; }
+  void set_vertex_degree(const SMesh::Vertex_index& vi, std::size_t deg) { vertex_deg_map[std::size_t(vi)] = deg; }
   
 public Q_SLOTS:
   void resize_spheres();
+
+Q_SIGNALS:
+  void selected(const SMesh::Vertex_index &vi);
   
 private:
   QSlider* radius_slider;
-  Scene_surface_mesh_item* parent;
+  Scene_surface_mesh_item* _parent;
   double radius;
+  std::vector<std::size_t> vertex_sphere_map;
+  std::vector<bool> vertex_has_sphere_map;
+  std::vector<std::size_t> vertex_deg_map;
 };
 
 
@@ -95,28 +107,23 @@ public:
       return true;
     return false;
   }
-  void closure()
-  {
-    dock_widget->hide();
-  }
+  void closure(){ dock_widget->hide(); }
+  
 public Q_SLOTS:
   void pop();
   void invalidateDisplay(bool update_spheres = false);
-  void selected(const std::set<fg_vertex_descriptor> &m);
+  void selected(const SMesh::Vertex_index &vi);
   void changeColor();
+  void vertex_has_been_selected(void*);
 private:
-  QColor changeDegree(size_t vid, bool &ok);
+  QColor changeDegree(SMesh::Vertex_index vi, bool &ok);
   DockWidget* dock_widget;
   QAction* actionBubbles;
-  std::vector<std::size_t> vertex_sphere_map;
-  std::vector<bool> vertex_has_sphere_map;
-  fg_vertex_descriptor sel_v;
-  fg_vertex_descriptor last_v;
+  SMesh::Vertex_index sel_v;
+  SMesh::Vertex_index last_v;
   std::vector<QColor> colors;
   QColor selection_color;
   Colored_spheres_item* spheres;
-  Scene_facegraph_item_k_ring_selection k_ring_selector;
-  std::vector<std::size_t> vertex_deg_map;
 }; // end Colored_spheres_plugin
 
 //   P l u g i n - D e f i n i t i o n s
@@ -180,28 +187,28 @@ void Colored_spheres_plugin::invalidateDisplay(bool update_spheres)
   layout->setColumnStretch(1,1);
   if(update_spheres)
   {
-    SMesh& mesh = *k_ring_selector.poly_item->face_graph();
+    SMesh& mesh = *spheres->parent()->face_graph();
     BOOST_FOREACH(SMesh::Vertex_index vi, mesh.vertices())
     {
-      if(vertex_has_sphere_map[std::size_t(vi)])
+      if(spheres->vertex_has_sphere(vi))
       {
-        std::size_t deg = vertex_deg_map[std::size_t(vi)];
+        std::size_t deg = spheres->vertex_degree(vi);
         QColor c = colors[deg];
         typedef unsigned char UC;
         spheres->setColor(CGAL::Color(UC(c.red()), 
                                       UC(c.green()), 
                                       UC(c.blue())), 
-                          vertex_sphere_map[std::size_t(vi)]);
+                          spheres->sphere_id(vi));
       }
     }
-    if(std::size_t(sel_v) < vertex_has_sphere_map.size()
-       && vertex_has_sphere_map[std::size_t(sel_v)])
+    if(std::size_t(sel_v) < spheres->parent()->face_graph()->number_of_vertices()
+       && spheres->vertex_has_sphere(sel_v))
     {
       typedef unsigned char UC;
       spheres->setColor(CGAL::Color(UC(selection_color.red()), 
                                     UC(selection_color.green()), 
                                     UC(selection_color.blue())), 
-                        vertex_sphere_map[std::size_t(sel_v)]);
+                        spheres->sphere_id(sel_v));
     }   
     spheres->invalidateOpenGLBuffers();
     spheres->redraw();
@@ -211,7 +218,7 @@ void Colored_spheres_plugin::invalidateDisplay(bool update_spheres)
   QApplication::restoreOverrideCursor();
 }
 
-QColor Colored_spheres_plugin::changeDegree(std::size_t vid, bool& ok)
+QColor Colored_spheres_plugin::changeDegree(SMesh::Vertex_index vi, bool& ok)
 {
   int new_deg = QInputDialog::getInt(mw, "Add a new degree", "Degree:", 3, 1, 100, 1, &ok);
   if(!ok)
@@ -231,7 +238,7 @@ QColor Colored_spheres_plugin::changeDegree(std::size_t vid, bool& ok)
     colors[new_deg] = color;
     color = colors[new_deg];
   }
-  vertex_deg_map[vid]=new_deg;
+  spheres->set_vertex_degree(vi, new_deg);
   invalidateDisplay();
   ok = true;
   return color;
@@ -244,9 +251,6 @@ void Colored_spheres_plugin::pop()
   if(!smitem)
     return;
   SMesh& mesh = *smitem->face_graph();
-  vertex_sphere_map.resize(vertices(mesh).size());
-  vertex_deg_map.resize(vertex_sphere_map.size());
-  vertex_has_sphere_map.resize(vertex_sphere_map.size(), false);
   double radius = 0.3 * CGAL::compute_average_spacing<CGAL::Sequential_tag>(mesh.points(), 3);
   Colored_spheres_item* spheres = new Colored_spheres_item(smitem, radius);
   QColor c;
@@ -254,21 +258,21 @@ void Colored_spheres_plugin::pop()
   BOOST_FOREACH(SMesh::Vertex_index vi, mesh.vertices())
   {
     std::size_t deg = smitem->face_graph()->degree(vi);
-    vertex_deg_map[std::size_t(vi)] = deg;
+    spheres->set_vertex_degree(vi, deg);
     if(deg > max_deg)
       max_deg = deg;
   }
   colors.resize(max_deg + 1);
   BOOST_FOREACH(SMesh::Vertex_index vi, mesh.vertices())
   {
-    std::size_t deg = vertex_deg_map[std::size_t(vi)];
+    std::size_t deg = spheres->vertex_degree(vi);
     if(deg != 4)
     {
       c = spheres->color();
       c = QColor::fromHsv((c.hue()+ 55*deg)%360, (deg*25)%155+100,c.lightness(), c.alpha());
       colors[deg] = c;
-      vertex_sphere_map[std::size_t(vi)] = spheres->numberOfSpheres();
-      vertex_has_sphere_map[std::size_t(vi)] = true;
+      spheres->set_sphere_id(vi, spheres->numberOfSpheres());
+      spheres->set_vertex_has_sphere(vi, true);
       const CGAL::qglviewer::Vec v_offset = static_cast<CGAL::Three::Viewer_interface*>(CGAL::QGLViewer::QGLViewerPool().first())->offset();
       const EPICK::Vector_3 offset(v_offset.x, v_offset.y, v_offset.z);
       EPICK::Point_3 center(mesh.point(vi) + offset);
@@ -287,23 +291,25 @@ void Colored_spheres_plugin::pop()
   scene->addItem(spheres);
   this->spheres = spheres;
   
-  connect(&k_ring_selector, SIGNAL(selected(const std::set<fg_vertex_descriptor>&)), this,
-          SLOT(selected(const std::set<fg_vertex_descriptor>&)));
-  k_ring_selector.init(smitem, mw, Scene_facegraph_item_k_ring_selection::Active_handle::VERTEX, -1);
-  k_ring_selector.k_ring = 0;
+  connect(spheres, SIGNAL(selected(const SMesh::Vertex_index&)), this,
+          SLOT(selected(const SMesh::Vertex_index&)));
+  connect(smitem, SIGNAL(selected_vertex(void*)), this, SLOT(vertex_has_been_selected(void*)));
+  mw->installEventFilter(spheres);
+  CGAL::QGLViewer::QGLViewerPool().first()->installEventFilter(spheres);
+  
 }
 
-void Colored_spheres_plugin::selected(const std::set<fg_vertex_descriptor> &m)
+void Colored_spheres_plugin::selected(const SMesh::Vertex_index &vi)
 {
-  sel_v = *m.begin();
+  sel_v = vi;
   static bool is_one_currently_selected = false;
   static std::size_t id = INVALID_ID;
   if(!is_one_currently_selected)
   {
-    if(!vertex_has_sphere_map[std::size_t(sel_v)])
+    if(!spheres->vertex_has_sphere(sel_v))
       return;
     else
-      id = vertex_sphere_map[std::size_t(sel_v)];
+      id = spheres->sphere_id(sel_v);
     typedef unsigned char UC;
     spheres->setColor(CGAL::Color(UC(selection_color.red()), 
                                   UC(selection_color.green()), 
@@ -315,20 +321,20 @@ void Colored_spheres_plugin::selected(const std::set<fg_vertex_descriptor> &m)
   else
   {
     if(sel_v != last_v 
-       && vertex_has_sphere_map[std::size_t(sel_v)])
+       && spheres->vertex_has_sphere(sel_v))
     {
       QMessageBox::warning(mw, "Warning", "Cannot overwrite an existing sphere.");
       return;
     }
     bool ok;
-    QColor c = changeDegree(std::size_t(sel_v), ok);
+    QColor c = changeDegree(sel_v, ok);
     if(!ok)
       return;
-    spheres->setCenter(k_ring_selector.poly_item->polyhedron()->point(SMesh::Vertex_index(sel_v)), id);
-    vertex_sphere_map[std::size_t(last_v)] = INVALID_ID;
-    vertex_has_sphere_map[std::size_t(last_v)] = false;
-    vertex_sphere_map[std::size_t(sel_v)] = id;
-    vertex_has_sphere_map[std::size_t(sel_v)] = true;
+    spheres->setCenter(spheres->parent()->face_graph()->point(SMesh::Vertex_index(sel_v)), id);
+    spheres->set_sphere_id(last_v, INVALID_ID);
+    spheres->set_vertex_has_sphere(last_v, false);
+    spheres->set_sphere_id(sel_v, id);
+    spheres->set_vertex_has_sphere(sel_v, true);
     typedef unsigned char UC;
     spheres->setColor(CGAL::Color(UC(c.red()),UC(c.green()),UC(c.blue())), id);
     is_one_currently_selected = false;
@@ -337,6 +343,7 @@ void Colored_spheres_plugin::selected(const std::set<fg_vertex_descriptor> &m)
   spheres->invalidateOpenGLBuffers();
   spheres->redraw();
 }
+
 void Colored_spheres_plugin::changeColor()
 {
   QPushButton* sender_button = qobject_cast<QPushButton*>(sender());
@@ -382,16 +389,25 @@ void Colored_spheres_plugin::changeColor()
   invalidateDisplay(true);
 }
 
+void Colored_spheres_plugin::vertex_has_been_selected(void* void_ptr)
+{
+  SMesh::Vertex_index  vi= SMesh::Vertex_index(static_cast<std::size_t>(reinterpret_cast<std::size_t>(void_ptr)));
+  selected(vi);
+}
 //   I t e m - D e f i n i t i o n s
 Colored_spheres_item::Colored_spheres_item(Scene_surface_mesh_item *parent, double radius)
   :Scene_spheres_item(NULL),
-    parent(parent),
+    _parent(parent),
     radius(radius)
 {
   radius_slider = new QSlider(Qt::Horizontal);
   radius_slider->setMinimum(1);
   radius_slider->setValue(100);
   radius_slider->setMaximum(200);  
+  SMesh& mesh = *parent->face_graph();
+  vertex_sphere_map.resize(vertices(mesh).size());
+  vertex_deg_map.resize(vertex_sphere_map.size());
+  vertex_has_sphere_map.resize(vertex_sphere_map.size(), false);
 }
 
 QMenu* Colored_spheres_item::contextMenu()
@@ -431,8 +447,48 @@ void Colored_spheres_item::resize_spheres()
   }
 }
 
-void Colored_spheres_item::select(double orig_x, double orig_y, double orig_z, double dir_x, double dir_y, double dir_z)
+bool Colored_spheres_item::eventFilter(QObject *obj, QEvent *event)
 {
-  parent->select(orig_x, orig_y, orig_z, dir_x, dir_y, dir_z); 
+  if(!visible())
+    return false;
+  CGAL::Three::Viewer_interface* viewer = qobject_cast<CGAL::Three::Viewer_interface*>(obj);
+  if(!viewer)
+    return false;
+  if(event->type() == QEvent::MouseButtonPress)
+  {
+    QMouseEvent* e = static_cast<QMouseEvent*>(event);
+    if(e->modifiers() == Qt::ShiftModifier)
+    {
+      bool found = false;
+      viewer->makeCurrent();
+      Vec p = viewer->camera()->pointUnderPixel(e->pos(), found);
+      const Vec offset = viewer->offset();
+      if(!found)
+        return false;
+      Kernel::Point_3 picked_point(p.x - offset.x, p.y - offset.y , p.z - offset.z);
+      
+      double min_dist = std::numeric_limits<double>::max();
+      SMesh::Vertex_index closest_vertex;
+      BOOST_FOREACH(SMesh::Vertex_index vi, vertices(*_parent->face_graph()))
+      {
+        if(vertex_has_sphere_map[std::size_t(vi)])
+        {
+          double dist = CGAL::squared_distance(_parent->face_graph()->
+                                               point(vi), picked_point);
+          if(dist < min_dist)
+          {
+            min_dist = dist;
+            closest_vertex = vi;
+          }
+        }
+      }
+      if(min_dist > 1.0001 * radius * (radius_slider->value()/100.0)
+         || min_dist < 0.0099 * radius * (radius_slider->value()/100.0))
+        return false;
+      selected(closest_vertex);
+      return true;
+    }
+  }
+  return false;
 }
 #include "Colored_spheres_plugin.moc"
