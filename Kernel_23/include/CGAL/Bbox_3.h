87517ac68d56048c1f7f65a75c80267401c432c9
Laurent Rineau <laurent.rineau@cgal.org>
2018-07-13 17:01:10 +0200

    Fix Bbox_[23]::dilate
    `boost::math::float_advance` does not modify its argument, but returns a
    value! The previous version of the code was not modifying the bbox at all,
    and the point snapping was active.

--- Kernel_23/include/CGAL/Bbox_3.h
+++ Kernel_23/include/CGAL/Bbox_3.h
@@ -183,12 +183,12 @@ void
 Bbox_3::dilate(int dist)
 {
   using boost::math::float_advance;
-  float_advance(rep[0],-dist);
-  float_advance(rep[1],-dist);
-  float_advance(rep[2],-dist);
-  float_advance(rep[3],dist);
-  float_advance(rep[4],dist);
-  float_advance(rep[5],dist);
+  rep[0] = float_advance(rep[0],-dist);
+  rep[1] = float_advance(rep[1],-dist);
+  rep[2] = float_advance(rep[2],-dist);
+  rep[3] = float_advance(rep[3],dist);
+  rep[4] = float_advance(rep[4],dist);
+  rep[5] = float_advance(rep[5],dist);
 }
 
 
