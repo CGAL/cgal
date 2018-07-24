e24b6c4dbfc7bd5f6f4a26c59fc94f626975ee57
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2018-07-24 14:20:01 +0200

    Revert "remove examples using a non documented function"
    This reverts commit f2882073bb4feb7250e190807cda836a09a84071.
    
    + updates

---
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/remove_degeneracies_example.cpp
@@ -0,0 +1,34 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+#include <CGAL/Surface_mesh.h>
+
+#include <CGAL/Polygon_mesh_processing/repair.h>
+
+#include <iostream>
+#include <fstream>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+
+typedef CGAL::Surface_mesh<K::Point_3> Surface_mesh;
+
+int main(int argc, char* argv[])
+{
+  const char* filename = (argc > 1) ? argv[1] : "data/degtri_sliding.off";
+  std::ifstream input(filename);
+
+  Surface_mesh mesh;
+  if (!input || !(input >> mesh) || mesh.is_empty()) {
+    std::cerr << "Not a valid .off file." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::size_t nb = CGAL::Polygon_mesh_processing::remove_degenerate_faces(mesh);
+
+  std::cout << "There were " << nb << " degenerate faces in this mesh" << std::endl;
+
+  mesh.collect_garbage();
+  std::ofstream out("repaired.off");
+  out << mesh;
+
+  return EXIT_SUCCESS;
+}
---
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/remove_degeneracies_example_OM.cpp
@@ -0,0 +1,38 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
+
+#include <CGAL/boost/graph/graph_traits_PolyMesh_ArrayKernelT.h>
+#include <CGAL/Polygon_mesh_processing/repair.h>
+
+#include <iostream>
+#include <fstream>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+
+typedef OpenMesh::PolyMesh_ArrayKernelT< >                  Mesh;
+
+namespace PMP = CGAL::Polygon_mesh_processing;
+
+int main(int argc, char* argv[])
+{
+  const char* filename = (argc > 1) ? argv[1] : "data/degtri_sliding.off";
+
+  Mesh mesh;
+  if (!input || !(OpenMesh::IO::read_mesh(mesh, filename)) || mesh.is_empty()) {
+    std::cerr << "Not a valid .off file." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::size_t nb = PMP::remove_degenerate_faces(mesh,
+                                                CGAL::parameters::vertex_point_map(get(CGAL::vertex_point, mesh))
+                                                                 .geom_traits(K()));
+
+  std::cout << "There were " << nb << " degenerate faces in this mesh" << std::endl;
+
+  mesh.garbage_collection();
+  OpenMesh::IO::write_mesh(mesh, "repaired.off");
+
+  return EXIT_SUCCESS;
+}
