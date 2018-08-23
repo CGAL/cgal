2570c21b7017f8a73c940f60954a971c999a7eae
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-08-23 15:24:32 +0200

    WIP tests with c3t3 and meshdomain ID

--- Polyhedron/demo/Polyhedron/Plugins/IO/VTK_io_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/IO/VTK_io_plugin.cpp
@@ -83,6 +83,236 @@ typedef Scene_polyhedron_item Scene_facegraph_item;
 #endif
 typedef Scene_facegraph_item::Face_graph FaceGraph;
 typedef boost::property_traits<boost::property_map<FaceGraph, CGAL::vertex_point_t>::type>::value_type Point;
+
+
+
+// writes the appended data into the .vtu file
+template <class FT> 
+void
+write_vector(std::ostream& os, 
+	     const std::vector<FT>& vect) 
+{
+  const char* buffer = reinterpret_cast<const char*>(&(vect[0]));
+  std::size_t size = vect.size()*sizeof(FT);
+  
+  os.write(reinterpret_cast<const char *>(&size), sizeof(std::size_t)); // number of bytes encoded
+  os.write(buffer, vect.size()*sizeof(FT));                     // encoded data
+}
+
+// writes the cells tags before binary data is appended
+template <class C3T3>
+void 
+write_cells_tag(std::ostream& os,
+		const C3T3 & c3t3,
+		std::map<typename C3T3::Triangulation::Vertex_handle, std::size_t> & V,
+		bool binary,
+		std::size_t& offset)
+{
+  typedef typename C3T3::Cells_in_complex_iterator Cell_iterator;
+  std::string formatattribute =
+    binary ? " format=\"appended\"" : " format=\"ascii\"";
+
+  std::string typeattribute;
+  switch(sizeof(std::size_t)) {
+  case 8: typeattribute = " type=\"UInt64\""; break;
+  case 4: typeattribute = " type=\"UInt32\""; break;
+  default: CGAL_error_msg("Unknown size of std::size_t");
+  }
+
+  // Write connectivity table
+  os << "    <Cells>\n"
+     << "      <DataArray Name=\"connectivity\""
+     << formatattribute << typeattribute;
+  
+  if (binary) { // if binary output, just write the xml tag
+    os << " offset=\"" << offset << "\"/>\n";
+    offset += (4 * c3t3.number_of_cells() + 1) * sizeof(std::size_t);
+    // 4 indices (size_t) per cell + length of the encoded data (size_t)
+  }
+  else {
+    os << "\">\n";   
+    for( Cell_iterator cit = c3t3.cells_in_complex_begin() ;
+	 cit != c3t3.cells_in_complex_end() ;
+	 ++cit )
+      {
+	for (int i=0; i<4; i++)
+	  os << V[cit->vertex(i)] << " ";
+      }
+    os << "      </DataArray>\n";
+  }
+  
+  // Write offsets
+  os   << "      <DataArray Name=\"offsets\""
+       << formatattribute << typeattribute;
+  
+  if (binary) {  // if binary output, just write the xml tag
+    os << " offset=\"" << offset << "\"/>\n";
+    offset += (c3t3.number_of_cells() + 1) * sizeof(std::size_t);
+    // 1 offset (size_t) per cell + length of the encoded data (size_t)
+  }
+  else {
+    os << "\">\n";  
+    std::size_t cells_offset = 0;
+    for( Cell_iterator cit = c3t3.cells_in_complex_begin() ;
+	 cit != c3t3.cells_in_complex_end() ;
+	 ++cit )
+      {
+	cells_offset += 4;
+	os << cells_offset << " ";
+      }  
+    os << "      </DataArray>\n";
+  }
+
+  // Write cell type (tetrahedra == 10)
+  os   << "      <DataArray Name=\"types\""
+       << formatattribute << " type=\"UInt8\"";
+
+  if (binary) {
+    os << " offset=\"" << offset << "\"/>\n";
+    offset += c3t3.number_of_cells() + sizeof(std::size_t);
+    // 1 unsigned char per cell + length of the encoded data (size_t)
+  }
+  else {
+    os << "\">\n";  
+    for( Cell_iterator cit = c3t3.cells_in_complex_begin() ;
+	 cit != c3t3.cells_in_complex_end() ;
+	 ++cit )
+      os << "10 ";
+    os << "      </DataArray>\n";
+  }
+  os << "    </Cells>\n";
+}
+
+// writes the cells appended data at the end of the .vtu file 
+template <class C3T3>
+void
+write_cells(std::ostream& os,
+	    const C3T3 & c3t3,
+	    std::map<typename C3T3::Triangulation::Vertex_handle, std::size_t> & V,
+            std::vector<float>& mids)
+{
+  typedef typename C3T3::Cells_in_complex_iterator Cell_iterator;
+  std::vector<std::size_t> connectivity_table;
+  std::vector<std::size_t> offsets;
+  std::vector<unsigned char> cell_type(c3t3.number_of_cells(),10);  // tetrahedra == 10
+  
+  std::size_t off = 0;
+  for( Cell_iterator cit = c3t3.cells_in_complex_begin() ;
+       cit != c3t3.cells_in_complex_end() ;
+       ++cit )
+    {
+      off += 4;
+      offsets.push_back(off);
+      for (int i=0; i<4; i++)
+	connectivity_table.push_back(V[cit->vertex(i)]);
+      mids.push_back(cit->subdomain_index());
+    }
+  write_vector<std::size_t>(os,connectivity_table);
+  write_vector<std::size_t>(os,offsets);
+  write_vector<unsigned char>(os,cell_type);
+}
+
+
+// writes the points tags before binary data is appended
+template <class Tr>
+void 
+write_points_tag(std::ostream& os,
+		 const Tr & tr,
+		 std::map<typename Tr::Vertex_handle, std::size_t> & V,
+		 bool binary,
+		 std::size_t& offset) 
+{
+  typedef typename Tr::Finite_vertices_iterator Finite_vertices_iterator;
+  typedef typename Tr::Geom_traits Gt;
+  typedef typename Gt::FT FT;
+
+  std::size_t inum = 0;
+  std::string format = binary ? "appended" : "ascii";
+  std::string type = (sizeof(FT) == 8) ? "Float64" : "Float32";
+
+  os << "    <Points>\n"
+     << "      <DataArray type =\"" << type << "\" NumberOfComponents=\"3\" format=\"" << format; //todo: 3 for 3D, 2 for 2D
+
+  if (binary) {
+    os << "\" offset=\"" << offset << "\"/>\n";    
+    offset += 3 * tr.number_of_vertices() * sizeof(FT) + sizeof(std::size_t);
+    // 3 coords per points + length of the encoded data (size_t)
+  }
+  else {
+    os << "\">\n";  
+    for( Finite_vertices_iterator vit = tr.finite_vertices_begin();
+	 vit != tr.finite_vertices_end();
+	 ++vit)
+      {
+	V[vit] = inum++;
+	os << vit->point().x() << " " << vit->point().y() << " " << vit->point().z() << " ";
+      }
+    os << "      </DataArray>\n";
+  }
+  os << "    </Points>\n";
+}
+
+// writes the points appended data at the end of the .vtu file 
+template <class Tr>
+void
+write_points(std::ostream& os,
+	     const Tr & tr,
+	     std::map<typename Tr::Vertex_handle, std::size_t> & V)
+{
+  typedef typename Tr::Finite_vertices_iterator Finite_vertices_iterator;
+  typedef typename Tr::Geom_traits Gt;
+  typedef typename Gt::FT FT;
+
+  std::size_t inum = 0;
+  std::vector<FT> coordinates;
+  for( Finite_vertices_iterator vit = tr.finite_vertices_begin();
+       vit != tr.finite_vertices_end();
+       ++vit)
+    {
+      V[vit] = inum++;  // binary output => the map has not been filled yet
+      coordinates.push_back(vit->point().x());
+      coordinates.push_back(vit->point().y());
+      coordinates.push_back(vit->point().z());
+    }
+  write_vector<FT>(os,coordinates);
+}
+
+// writes the attribute tags before binary data is appended
+template <class T>
+void 
+write_attribute_tag(std::ostream& os,
+		    const std::string& attr_name,
+		    const std::vector<T>& attribute,
+		    bool binary,
+		    std::size_t& offset)
+{
+  std::string format = binary ? "appended" : "ascii";
+  std::string type = (sizeof(T) == 8) ? "Float64" : "Float32";
+  os << "      <DataArray type=\"" << type << "\" Name=\"" << attr_name << "\" format=\"" << format; 
+
+  if (binary) {
+    os << "\" offset=\"" << offset << "\"/>\n";    
+    offset += attribute.size() * sizeof(T) + sizeof(std::size_t);
+  }
+  else {
+    typedef typename std::vector<T>::const_iterator Iterator;
+    os << "\">\n";   
+    for (Iterator it = attribute.begin();
+	 it != attribute.end();
+	 ++it )
+      os << *it << " ";
+    os << "      </DataArray>\n";
+  }
+}
+
+// writes the attributes appended data at the end of the .vtu file 
+template <typename FT>
+void
+write_attributes(std::ostream& os,
+		 const std::vector<FT>& att)
+{
+  write_vector(os,att);
+}
 namespace CGAL{
 
   class ErrorObserverVtk : public vtkCommand
@@ -300,6 +530,8 @@ public:
     return (qobject_cast<const Scene_facegraph_item*>(item)
             || qobject_cast<const Scene_c3t3_item*>(item));
   }
+  
+  
   bool save(const CGAL::Three::Scene_item* item, QFileInfo fileinfo)
   {
     std::string extension = fileinfo.suffix().toLower().toStdString();
@@ -328,12 +560,51 @@ public:
           qobject_cast<const Scene_c3t3_item*>(item);
       if(!c3t3_item || extension != "vtu")
         return false;
-
-      vtkSmartPointer<vtkXMLUnstructuredGridWriter> writer =
-          vtkSmartPointer<vtkXMLUnstructuredGridWriter>::New();
-      writer->SetFileName( output_filename.data());
-      writer->SetInputData(CGAL::output_c3t3_to_vtk_unstructured_grid(c3t3_item->c3t3()));
-      writer->Write();
+      
+      typedef typename C3t3::Triangulation Tr;
+      typedef typename Tr::Vertex_handle Vertex_handle;
+      const C3t3& c3t3 = c3t3_item->c3t3();
+      const Tr& tr = c3t3.triangulation();
+      std::map<Vertex_handle, std::size_t> V;
+      std::ofstream os(output_filename.data());
+      os << std::setprecision(16);
+    //write header
+      os << "<?xml version=\"1.0\"?>\n"
+         << "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\"";
+    #ifdef CGAL_LITTLE_ENDIAN
+      os << " byte_order=\"LittleEndian\"";
+    #else // CGAL_BIG_ENDIAN
+      os << " byte_order=\"BigEndian\"";
+    #endif
+      
+      switch(sizeof(std::size_t)) {
+      case 4: os << " header_type=\"UInt32\""; break;
+      case 8: os << " header_type=\"UInt64\""; break;
+      default: CGAL_error_msg("Unknown size of std::size_t");
+      }
+      os << ">\n"
+         << "  <UnstructuredGrid>" << "\n";
+      
+      os << "  <Piece NumberOfPoints=\"" << tr.number_of_vertices() 
+         << "\" NumberOfCells=\"" << c3t3.number_of_cells() << "\">\n";
+      bool binary = true;
+      std::size_t offset = 0;
+      write_points_tag(os,tr,V,binary,offset);
+      write_cells_tag(os,c3t3,V,binary,offset);
+      os << "   <CellData Domain=\"MeshDomain";
+      os << "\">\n";
+      std::vector<float> mids;      
+      write_attribute_tag(os,"MeshDomain",mids,binary,offset);
+      os << "    </CellData>\n";
+      os << "   </Piece>\n"
+         << "  </UnstructuredGrid>\n";
+      if (binary) {
+        os << "<AppendedData encoding=\"raw\">\n_"; 
+        write_points(os,tr,V);  // write points before cells to fill the std::map V
+        write_cells(os,c3t3,V, mids);//todo mids should be filled by write_attribute_tag
+        write_attributes(os,mids);
+      }
+      os << "</VTKFile>\n";
     }
     return true;
   }
