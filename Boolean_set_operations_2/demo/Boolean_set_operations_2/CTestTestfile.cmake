6088a0475e0d4b4e227a59a64519ee2bf2411e72
apurva786 <response2apurva@yahoo.com>
2018-09-01 09:30:29 +0530

    2D Boolean operations demo

--- Boolean_set_operations_2/demo/Boolean_set_operations_2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/apurva/Desktop/cgal-public-dev-gsoc2018-bso_demo-apurva/Boolean_set_operations_2/demo/Boolean_set_operations_2
-# Build directory: /home/apurva/Desktop/cgal-public-dev-gsoc2018-bso_demo-apurva/Boolean_set_operations_2/demo/Boolean_set_operations_2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
