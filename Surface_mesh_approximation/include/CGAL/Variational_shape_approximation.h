15c4fed427ee755aff7d919bfa3fa918d4d835de
Sébastien Loriot <sebastien.loriot@cgal.org>
2018-09-14 12:00:47 +0200

    add license headers

--- Surface_mesh_approximation/include/CGAL/Variational_shape_approximation.h
+++ Surface_mesh_approximation/include/CGAL/Variational_shape_approximation.h
@@ -1,3 +1,25 @@
+// Copyright (c) 2017-2018 INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0+
+//
+//
+// Author(s)     : Pierre Alliez and Lingjie Zhu
+
+
 #ifndef CGAL_VARIATIONAL_SHAPE_APPROXIMATION_H
 #define CGAL_VARIATIONAL_SHAPE_APPROXIMATION_H
 
