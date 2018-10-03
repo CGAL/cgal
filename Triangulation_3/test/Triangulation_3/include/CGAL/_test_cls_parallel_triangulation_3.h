8a7b11f34411ca20cb2ba4fd18ef3ef3520f5021
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2018-10-03 16:05:18 +0200

    Use actual weighted points in parallel regular triangulation tests

--- Triangulation_3/test/Triangulation_3/include/CGAL/_test_cls_parallel_triangulation_3.h
+++ Triangulation_3/test/Triangulation_3/include/CGAL/_test_cls_parallel_triangulation_3.h
@@ -27,42 +27,82 @@
 
 #include <boost/mpl/if.hpp>
 
+template <typename Parallel_triangulation,
+          typename WeightedTag = typename Parallel_triangulation::Weighted_tag>
+struct PTR_random_pts_generator
+{
+  typedef typename Parallel_triangulation::Point       Point;
+
+  void operator()(const int num, CGAL::Random& rnd, std::vector<Point>& points) const
+  {
+    CGAL::Random_points_in_cube_3<Point> gen(1., rnd);
+
+    points.reserve(num);
+    for(int i=0; i!=num; ++i)
+      points.push_back(*gen++);
+  }
+};
+
+template <typename Parallel_triangulation>
+struct PTR_random_pts_generator<Parallel_triangulation, CGAL::Tag_true /*weighted triangulation*/>
+{
+  typedef typename Parallel_triangulation::Bare_point      Bare_point;
+  typedef typename Parallel_triangulation::Weighted_point  Weighted_point;
+
+  void operator()(const int num, CGAL::Random& rnd, std::vector<Weighted_point>& points) const
+  {
+    CGAL::Random_points_in_cube_3<Bare_point> gen(1., rnd);
+
+    points.reserve(num);
+    for(int i=0; i!=num; ++i)
+      points.push_back(Weighted_point(*gen++, rnd.get_double(-1., 1.)));
+  }
+};
+
 template <class Parallel_triangulation>
 void
 _test_cls_parallel_triangulation_3(const Parallel_triangulation &)
 {
-  const int NUM_INSERTED_POINTS = 5000;
-
   typedef Parallel_triangulation                                Cls;
 
   typedef typename Cls::Vertex_handle                           Vertex_handle;
   typedef typename Cls::Point                                   Point;
 
-  CGAL::Random_points_in_cube_3<Point> rnd(1.);
+  typedef std::vector<Point>                                    Point_container;
 
-  // Construction from a vector of points
-  std::vector<Point> points;
-  points.reserve(NUM_INSERTED_POINTS);
-  for (int i = 0; i != NUM_INSERTED_POINTS; ++i)
-    points.push_back(*rnd++);
-  
+  CGAL::Random rnd;
+  std::cout << "Seed: " << rnd.get_seed() << std::endl;
+
+  const int num_insert = 5000;
+  Point_container points;
+  PTR_random_pts_generator<Parallel_triangulation> points_gen;
+  points_gen(num_insert, rnd, points);
+
   // Construct the locking data-structure, using the bounding-box of the points
-  typename Cls::Lock_data_structure locking_ds(
-    CGAL::Bbox_3(-1., -1., -1., 1., 1., 1.), 50);
+  typename Cls::Lock_data_structure locking_ds(CGAL::Bbox_3(-1., -1., -1., 1., 1., 1.), 50);
+
   // Contruct the triangulation in parallel
   std::cout << "Construction and parallel insertion" << std::endl;
   Cls tr(points.begin(), points.end(), &locking_ds);
+  std::cout << "Triangulation has " << tr.number_of_vertices() << " vertices" << std::endl;
+  assert(tr.is_valid());
 
   std::cout << "Parallel removal" << std::endl;
-  // Remove the first 100,000 vertices
   std::vector<Vertex_handle> vertices_to_remove;
   typename Cls::Finite_vertices_iterator vit = tr.finite_vertices_begin();
-  for (int i = 0 ; i < NUM_INSERTED_POINTS/10 ; ++i)
+
+  const int num_remove = tr.number_of_vertices() / 10;
+  std::cout << "Removing " << num_remove << " from " << tr.number_of_vertices() << " vertices" << std::endl;
+
+  for(int i=0 ; i<num_remove; ++i) {
     vertices_to_remove.push_back(vit++);
+}
+
   // Parallel remove
   tr.remove(vertices_to_remove.begin(), vertices_to_remove.end());
-  
+  std::cout << "Now, " << tr.number_of_vertices() << " vertices are left" << std::endl;
   assert(tr.is_valid());
+
   tr.clear();
   assert(tr.is_valid());
   assert(tr.dimension()==-1);
