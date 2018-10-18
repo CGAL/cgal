2d385fc2f248f4c993c5dd3ea6733276deb5f897
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-10-18 14:30:07 +0200

    Finalize metadata plugins mechanism. Create the IO group.

--- Polyhedron/demo/Polyhedron/Plugins/PMP/Selection_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/PMP/Selection_plugin.cpp
@@ -11,6 +11,8 @@
 
 #include <CGAL/Three/Scene_interface.h>
 #include <CGAL/Three/Polyhedron_demo_plugin_helper.h>
+#include <CGAL/Three/Polyhedron_demo_io_plugin_interface.h>
+#include <CGAL/Three/Three.h>
 #include "ui_Selection_widget.h"
 
 #include <QAction>
@@ -66,12 +68,46 @@ struct Polyline_visitor
 using namespace CGAL::Three;
 class Polyhedron_demo_selection_plugin :
   public QObject,
-  public Polyhedron_demo_plugin_helper
+  public Polyhedron_demo_plugin_helper,
+  public Polyhedron_demo_io_plugin_interface
 {
   Q_OBJECT
-    Q_INTERFACES(CGAL::Three::Polyhedron_demo_plugin_interface)
-    Q_PLUGIN_METADATA(IID "com.geometryfactory.PolyhedronDemo.PluginInterface/1.0")
+    Q_INTERFACES(CGAL::Three::Polyhedron_demo_plugin_interface CGAL::Three::Polyhedron_demo_io_plugin_interface)
+    Q_PLUGIN_METADATA(IID "com.geometryfactory.PolyhedronDemo.PluginInterface/1.0" FILE "selection_plugin.json")
+    Q_PLUGIN_METADATA(IID "com.geometryfactory.PolyhedronDemo.IOPluginInterface/1.0")
 public:
+  QString nameFilters() const { return "Selection files(*.selection.txt)"; }
+  QString name() const { return "selection_sm_plugin"; }
+  
+  bool canLoad() const {
+  Scene_facegraph_item* sel_item = qobject_cast<Scene_facegraph_item*>(CGAL::Three::Three::scene()->item(
+        CGAL::Three::Three::scene()->mainSelectionIndex()));
+  if(sel_item)
+    return true;
+  return false;
+  }
+  CGAL::Three::Scene_item* load(QFileInfo fileinfo) {
+      if(fileinfo.suffix().toLower() != "txt") return 0;
+      // There will be no actual loading at this step.
+      Scene_polyhedron_selection_item* item = new Scene_polyhedron_selection_item();
+      if(!item->load(fileinfo.filePath().toStdString())) {
+          delete item;
+          return NULL;
+      }
+      item->setName(fileinfo.baseName());
+      return item;
+  }
+
+  bool canSave(const CGAL::Three::Scene_item* scene_item) {
+      return qobject_cast<const Scene_polyhedron_selection_item*>(scene_item);
+  }
+  bool save(const CGAL::Three::Scene_item* scene_item, QFileInfo fileinfo) {
+      const Scene_polyhedron_selection_item* item = qobject_cast<const Scene_polyhedron_selection_item*>(scene_item);
+      if(item == NULL) { return false; }
+
+      return item->save(fileinfo.filePath().toStdString());
+  }
+  
   bool applicable(QAction*) const { 
     return qobject_cast<Scene_face_graph_item*>(scene->item(scene->mainSelectionIndex()))
         || qobject_cast<Scene_polyhedron_selection_item*>(scene->item(scene->mainSelectionIndex())); 
