c2d0689a04190127e6e875038696fb241e913dcf
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2018-10-23 08:48:36 +0200

    Fixed typo

--- Kernel_23/doc/Kernel_23/Concepts/FunctionObjectConcepts.h
+++ Kernel_23/doc/Kernel_23/Concepts/FunctionObjectConcepts.h
@@ -3846,7 +3846,7 @@ public:
     direction as the sum of the normalized normal vectors of the two planes, and 
     passes through the intersection of `h1` and `h2`. 
     If `h1` and `h2` are parallel, then the bisector is defined as the 
-    plane which has the same oriented normal vector as `l1`, and which is at 
+    plane which has the same oriented normal vector as `h1`, and which is at
     the same distance from `h1` and `h2`. 
     This function requires that `Kernel::RT` supports the `sqrt()` 
     operation. 
