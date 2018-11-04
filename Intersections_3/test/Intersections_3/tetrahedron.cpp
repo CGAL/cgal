a76623e7e8ed7b33ba73ab512e5473051fac2eba
Andreas Fabri <andreas.fabri@geometryfactory.com>
2018-11-04 11:21:27 +0100

    Fix test

--- Intersections_3/test/Intersections_3/tetrahedron.cpp
+++ Intersections_3/test/Intersections_3/tetrahedron.cpp
@@ -5,6 +5,7 @@
 
 typedef CGAL::Cartesian<double> K;
 
+typedef K::Point_3         Point;
 typedef K::Tetrahedron_3   Tetrahedron;
 
 typedef K::Segment_3       Segment;
@@ -20,35 +21,37 @@ typedef CGAL::Bbox_3       Bbox;
 
 int main()
 {
-  Tetrahedron tet;
-  Sphere sp;
-
-  CGAL::do_intersect(tet,Triangle());
-  CGAL::do_intersect(tet,Segment());
-  CGAL::do_intersect(tet,Iso_cuboid());
-  CGAL::do_intersect(tet,Sphere());
-  CGAL::do_intersect(tet,Plane());
-  CGAL::do_intersect(tet,Line());
-  CGAL::do_intersect(tet,Ray());
+  Point p(0,0,0), q(10,0,0), r(10,10,0), s(0, 10,10);
+  Point p2(1,1,1), q2(20,20,20), r2(0,0,20);
+  Tetrahedron tet(p,q,r,s);
+  Sphere sp(p2,1.0);
+  
+  CGAL::do_intersect(tet,Triangle(p2,q2,r2));
+  CGAL::do_intersect(tet,Segment(p2,q2));
+  CGAL::do_intersect(tet,Iso_cuboid(p2,q2));
+  CGAL::do_intersect(tet,sp);
+  CGAL::do_intersect(tet,Plane(p2,q2,r2));
+  CGAL::do_intersect(tet,Line(p2,q2));
+  CGAL::do_intersect(tet,Ray(p2,q2));  
   CGAL::do_intersect(tet,tet);
-  CGAL::do_intersect(tet,Bbox());
-  CGAL::do_intersect(sp, Line());
-  CGAL::do_intersect(sp, Ray());
-  CGAL::do_intersect(sp, Segment());
-
-  CGAL::do_intersect(Triangle(), tet);
-  CGAL::do_intersect(Segment(), tet);
-  CGAL::do_intersect(Iso_cuboid(), tet);
-  CGAL::do_intersect(Sphere(), tet);
-  CGAL::do_intersect(Plane(), tet);
-  CGAL::do_intersect(Line(), tet);
-  CGAL::do_intersect(Ray(), tet);
-  CGAL::do_intersect(Bbox(), tet);
-
-  CGAL::do_intersect(Line(), sp);
-  CGAL::do_intersect(Ray(), sp);
-  CGAL::do_intersect(Segment(), sp);
-
+  CGAL::do_intersect(tet,sp.bbox());
+  CGAL::do_intersect(sp, Line(p2,q2));
+  CGAL::do_intersect(sp, Ray(p2,q2));
+  CGAL::do_intersect(sp, Segment(p2,q2));
+  
+  
+  CGAL::do_intersect(Triangle(p2,q2,r2), tet);
+  CGAL::do_intersect(Segment(p2,q2), tet);
+  CGAL::do_intersect(Iso_cuboid(p2,q2), tet);
+  CGAL::do_intersect(sp, tet);
+  CGAL::do_intersect(Plane(p2,q2,r2), tet);
+  CGAL::do_intersect(Line(p2,q2), tet);
+  CGAL::do_intersect(Ray(p2,q2), tet);  
+  CGAL::do_intersect(sp.bbox(), tet);
+  
+  CGAL::do_intersect(Line(p2,q2), sp);
+  CGAL::do_intersect(Ray(p2,q2), sp);
+  CGAL::do_intersect(Segment(p2,q2), sp);
 
   return 0;
 }
