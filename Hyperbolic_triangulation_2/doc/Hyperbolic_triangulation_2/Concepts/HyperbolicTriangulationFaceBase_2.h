b9785828032fdff5c30867cb300c373d420fb46f
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-11-14 18:48:36 +0100

    Removed unneeded typedefs and constructors

--- Hyperbolic_triangulation_2/doc/Hyperbolic_triangulation_2/Concepts/HyperbolicTriangulationFaceBase_2.h
+++ Hyperbolic_triangulation_2/doc/Hyperbolic_triangulation_2/Concepts/HyperbolicTriangulationFaceBase_2.h
@@ -12,12 +12,11 @@ face class of a hyperbolic triangulation data structure.
 
 \cgalModifBegin
 This concept provides an interface for the functionality needed in faces to compute 
-Delaunay triangulations in the hyperbolic plane. The functions `tds_data()` are used
+Delaunay triangulations in the hyperbolic plane. The function `tds_data()` is used
 internally by the triangulation class during the insertion of points in the triangulation.
 \cgalModifEnd
 
 \cgalHasModel CGAL::Hyperbolic_triangulation_face_base_2
-\cgalHasModel CGAL::Hyperbolic_triangulation_face_base_with_info_2
 
 \sa `TriangulationDataStructure_2`
 
@@ -27,39 +26,6 @@ internally by the triangulation class during the insertion of points in the tria
 class HyperbolicTriangulationFaceBase_2 {
 
 public:
-
-  /// \name Types
-  /// @{
-    typedef unspecified_type    Vertex_handle;
-    typedef unspecified_type    Face_handle;
-  // @}
-
-  /// \name Creation
-  /// @{
-
-    /*!
-    Default constructor.
-    */
-    Hyperbolic_triangulation_face_base_2();
-
-    /*!
-      Creates a face to which the vertices `v0, v1, v2` are incident.
-    */
-    Hyperbolic_triangulation_face_base_2(Vertex_handle v0, 
-  			                                 Vertex_handle v1, 
-  			                                 Vertex_handle v2);
-
-    /*!
-      Creates a face to which the vertices `v0, v1, v2` are incident, and the faces `n0, n1, n2` are neighbors.
-    */
-    Hyperbolic_triangulation_face_base_2(Vertex_handle v0, 
-                    			               Vertex_handle v1, 
-                    			               Vertex_handle v2,
-                    			               Face_handle n0, 
-                    			               Face_handle n1, 
-                    			               Face_handle n2);
-  /// @}
-
 
   /// \name Internal access functions
   /// \cgalAdvancedBegin
