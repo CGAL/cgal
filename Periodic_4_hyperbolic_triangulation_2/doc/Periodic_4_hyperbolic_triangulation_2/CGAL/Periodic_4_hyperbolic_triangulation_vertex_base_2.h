8b53ba70cf34603dc07237b9c1b90fd5e3405dbe
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-11-14 19:02:30 +0100

    Added better requirement for GT model; indication of how the class can be used

--- Periodic_4_hyperbolic_triangulation_2/doc/Periodic_4_hyperbolic_triangulation_2/CGAL/Periodic_4_hyperbolic_triangulation_vertex_base_2.h
+++ Periodic_4_hyperbolic_triangulation_2/doc/Periodic_4_hyperbolic_triangulation_2/CGAL/Periodic_4_hyperbolic_triangulation_vertex_base_2.h
@@ -10,10 +10,18 @@ namespace CGAL {
 The class `Periodic_4_hyperbolic_triangulation_vertex_base_2` is the default model for the 
 concept `Periodic_4HyperbolicTriangulationVertexBase_2`. It accepts two template parameters:
 
-\tparam GT 	Geometric Traits type. This should be a model of the concept `Periodic_4HyperbolicDelaunayTriangulationTraits_2`.
-			This template parameter has no default value.
+\tparam GT 	Geometric Traits type. This should be the same model of the concept 
+			`Periodic_4HyperbolicDelaunayTriangulationTraits_2` that is used in the class
+			`Periodic_4_hyperbolic_Delaunay_triangulation_2`. This template parameter has 
+			no default value.
 \tparam Vb 	Vertex Base type. Should be a model of the concept `TriangulationVertexBase_2`. 
-		   	The default value for this template parameter is `CGAL::Triangulation_vertex_base_2<>`
+		   	The default value for this template parameter is `CGAL::Triangulation_vertex_base_2<GT>`
+
+\cgalModifBegin
+`Periodic_4_hyperbolic_triangulation_vertex_base_2` can be simply plugged in the triangulation 
+data structure of a periodic hyperbolic triangulation, or used as a base class to derive other 
+base vertex classes tuned for specific applications.
+\cgalModifEnd
 
 \cgalModels Periodic_4HyperbolicTriangulationVertexBase_2
 
