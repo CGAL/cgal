a49c635135d58a3e53d7b3d0f35c9c2d65dafdc0
Iordan Iordanov <i.m.iordanov@gmail.com>
2018-11-15 13:46:19 +0100

    exposed derived members in traits class; changed phrase in main page of ref manual

--- Periodic_4_hyperbolic_triangulation_2/doc/Periodic_4_hyperbolic_triangulation_2/CGAL/Periodic_4_hyperbolic_Delaunay_triangulation_traits_2.h
+++ Periodic_4_hyperbolic_triangulation_2/doc/Periodic_4_hyperbolic_triangulation_2/CGAL/Periodic_4_hyperbolic_Delaunay_triangulation_traits_2.h
@@ -15,13 +15,22 @@ the default traits class for the class `Periodic_4_hyperbolic_Delaunay_triangula
 \sa `Periodic_4_hyperbolic_Delaunay_triangulation_2`
 */
 template< class Kernel = CGAL::Cartesian<CORE::Expr> >
-class Periodic_4_hyperbolic_Delaunay_triangulation_traits_2 
+class Periodic_4_hyperbolic_Delaunay_triangulation_traits_2 :
+	public Hyperbolic_Delaunay_triangulation_traits_2<Kernel>
 {
 public:
 	/// \name Types
 	/// @{
-		typedef typename Kernel::FT     				FT;
-		typedef Hyperbolic_octagon_translation<FT> 		Hyperbolic_translation;
+
+		typedef Hyperbolic_Delaunay_triangulation_traits_2<Kernel>  				Base;
+
+		typedef typename Base::FT     												FT;
+		typedef Hyperbolic_octagon_translation<FT> 									Hyperbolic_translation;
+
+		typedef typename Base::Hyperbolic_point_2     								Hyperbolic_point_2;
+		typedef Hyperbolic_point_2 													Hyperbolic_Voronoi_point_2;
+		typedef typename Base::Hyperbolic_segment_2  			 					Hyperbolic_segment_2;
+		
 	/// @}
 }; /* end Periodic_4_hyperbolic_Delaunay_triangulation_traits_2 */
 } /* end namespace CGAL */
