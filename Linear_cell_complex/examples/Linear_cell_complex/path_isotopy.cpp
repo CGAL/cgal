40265aa139370f042ee5198721f9982673cdcd13
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2018-11-21 15:04:54 +0100

    Add an option to make several tests in a same run.

--- Linear_cell_complex/examples/Linear_cell_complex/path_isotopy.cpp
+++ Linear_cell_complex/examples/Linear_cell_complex/path_isotopy.cpp
@@ -14,13 +14,16 @@ typedef CGAL::Linear_cell_complex_for_combinatorial_map<2,3> LCC_3_cmap;
 ///////////////////////////////////////////////////////////////////////////////
 [[ noreturn ]] void usage(int /*argc*/, char** argv)
 {
-  std::cout<<"usage: "<<argv[0]<<" file [-draw] [-seed S] [-nbfaces F] [-nbdefo D]"<<std::endl
-           <<"   Loadd the given off file, compute one random path, deform it into a second path and "
-           <<"   test that the two pathes are isotopic."<<std::endl
+  std::cout<<"usage: "<<argv[0]<<" file [-draw] [-seed S] [-nbfaces F] "
+           <<"[-nbdefo D]"<<std::endl
+           <<"   Load the given off file, compute one random path, deform it "
+           <<"into a second path and test that the two pathes are isotopic."
+           <<std::endl
            <<"   -draw: draw mesh and the two pathes." <<std::endl
            <<"   -seed S: uses S as seed of random generator. Otherwise use a different seed at each run (based on time)."<<std::endl
            <<"   -nbfaces F: use F connected random faces to generate the initial path (by default a random number beween 10 and 100)."<<std::endl
            <<"   -nbdefo D: use D deformations to generate the second path (by default a random number between 10 and 100)."<<std::endl
+           <<"   -nbtests N: do N tests of isotopy (using 2*N random pathes) (by default 1)."<<std::endl
            <<std::endl;
   exit(EXIT_FAILURE);
 }
@@ -36,7 +39,8 @@ void process_command_line(int argc, char** argv,
                           bool& draw,
                           unsigned int& F,
                           unsigned int& D,
-                          CGAL::Random& random)
+                          CGAL::Random& random,
+                          unsigned int& N)
 {
   std::string arg;
   for (int i=1; i<argc; ++i)
@@ -62,6 +66,12 @@ void process_command_line(int argc, char** argv,
      { error_command_line(argc, argv, "Error: no number after -nbdefo option."); }
      D=static_cast<unsigned int>(std::stoi(std::string(argv[++i])));
     }
+    else if (arg=="-nbtests")
+    {
+      if (i==argc-1)
+      { error_command_line(argc, argv, "Error: no number after -nbtests option."); }
+      N=static_cast<unsigned int>(std::stoi(std::string(argv[++i])));
+    }
     else if (arg=="-h" || arg=="--help" || arg=="-?")
     { usage(argc, argv); }
     else if (arg[0]=='-')
@@ -75,6 +85,8 @@ void process_command_line(int argc, char** argv,
 
   if (D==0)
   { D=static_cast<unsigned int>(random.get_int(10, 101)); }
+
+  if (N==0) { N=1; }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -84,9 +96,10 @@ int main(int argc, char** argv)
   bool draw=false;
   unsigned int F=0;
   unsigned int D=0;
+  unsigned int N=1;
   CGAL::Random random; // Used when user do not provide its own seed.
 
-  process_command_line(argc, argv, file, draw, F, D, random);
+  process_command_line(argc, argv, file, draw, F, D, random, N);
 
   LCC_3_cmap lcc;
   if (!CGAL::load_off(lcc, file.c_str()))
@@ -104,65 +117,47 @@ int main(int argc, char** argv)
   std::cout<<"Reduced map: ";
   cmt.get_map().display_characteristics(std::cout)
     << ", valid="<< cmt.get_map().is_valid() << std::endl;
-  
-  std::vector<const CGAL::Path_on_surface<LCC_3_cmap>* > paths;
-  std::vector<CGAL::Path_on_surface<LCC_3_cmap> > transformed_paths;
-  
-  CGAL::Path_on_surface<LCC_3_cmap> path1(lcc);
-  generate_random_closed_path(path1, F, random); // random path, length between 30 and 500
-  std::cout<<"Path1 size: "<<path1.length()<<" (from "<<F<<" faces); ";
-  paths.push_back(&path1);
-
-  CGAL::Path_on_surface<LCC_3_cmap> path2(path1);
-  update_path_randomly(path2, D, random);
-  std::cout<<"Path2 size: "<<path2.length()<<" (from "<<D<<" deformations): ";
-  paths.push_back(&path2);
-  std::cout<<std::flush;
-  
-  /* Test RLE encoding */
+   
+  for (unsigned int i=0; i<N; ++i)
   {
-/*    CGAL::Path_on_surface_with_rle<LCC_3_cmap> path1_rle(path1);
-    CGAL::Path_on_surface_with_rle<LCC_3_cmap> path2_rle(path2);
-    CGAL::Path_on_surface<LCC_3_cmap> path1_copy(path1_rle);
-    CGAL::Path_on_surface<LCC_3_cmap> path2_copy(path2_rle);
-    if (path1!=path1_copy)
+    std::vector<const CGAL::Path_on_surface<LCC_3_cmap>* > paths;
+    std::vector<CGAL::Path_on_surface<LCC_3_cmap> > transformed_paths;
+
+    CGAL::Path_on_surface<LCC_3_cmap> path1(lcc);
+    generate_random_closed_path(path1, F, random); // random path, length between 30 and 500
+    std::cout<<"Path1 size: "<<path1.length()<<" (from "<<F<<" faces); ";
+    paths.push_back(&path1);
+
+    CGAL::Path_on_surface<LCC_3_cmap> path2(path1);
+    update_path_randomly(path2, D, random);
+    std::cout<<"Path2 size: "<<path2.length()<<" (from "<<D<<" deformations): ";
+    paths.push_back(&path2);
+    std::cout<<std::flush;
+  
+    CGAL::Path_on_surface<LCC_3_cmap> transformed_path1=
+        cmt.transform_original_path_into_quad_surface(path1);
+
+    CGAL::Path_on_surface<LCC_3_cmap> transformed_path2=
+        cmt.transform_original_path_into_quad_surface(path2);
+  
+    cmt.canonize(transformed_path1);
+    cmt.canonize(transformed_path2);
+    
+    if (transformed_path1!=transformed_path2)
     {
-      std::cout<<"Error: path1!=path1_copy: "<<std::endl
-              <<"path1="<<path1<<"; "<<std::endl
-             <<"path1_rle="<<path1_rle<<std::endl
-            <<"path1_copy="<<path1_copy<<std::endl;
+      std::cout<<"ERROR: pathes are not isotopic while they should be. (";
+      transformed_path1.display();
+      std::cout<<") != (";
+      transformed_path2.display();
+      std::cout<<")"<<std::endl;
     }
     else
-    {
-      std::cout<<"Lengt normal="<<path2.length()
-              <<"; length rle="<<path2_rle.size_of_list()<<std::endl;
-      std::cout<<path2_rle<<std::endl;
-    }*/
-  }
-  /* End of test RLE encoding */
+    { std::cout<<"TEST OK: pathes are isotopic (path length="
+              <<transformed_path1.length()<<")."<<std::endl; }
 
-  CGAL::Path_on_surface<LCC_3_cmap> transformed_path1=
-    cmt.transform_original_path_into_quad_surface(path1);
-
-  CGAL::Path_on_surface<LCC_3_cmap> transformed_path2=
-    cmt.transform_original_path_into_quad_surface(path2);
-  
-  cmt.canonize(transformed_path1);
-  cmt.canonize(transformed_path2);
-    
-  if (transformed_path1!=transformed_path2)
-  {
-    std::cout<<"ERROR: pathes are not isotopic while they should be. (";
-    transformed_path1.display();
-    std::cout<<") != (";
-    transformed_path2.display();
-    std::cout<<")"<<std::endl;
+    if (draw)
+    { display(lcc, paths); }
   }
-  else
-  { std::cout<<"TEST OK: pathes are isotopic."<<std::endl; }
-  
-  if (draw)
-  { display(lcc, paths); }
 
   return EXIT_SUCCESS;
 }
