f636d331b248062bc89541e773f5dc1a6683c2a6
Andreas Fabri <andreas.fabri@geometryfactory.com>
2018-12-06 14:11:43 +0100

    (void) result to avoid warning

--- Box_intersection_d/test/Box_intersection_d/util.h
+++ Box_intersection_d/test/Box_intersection_d/util.h
@@ -27,14 +27,14 @@ struct Util {
       int numBoxes, numDim;
       int boxNum, dim;
     
-      std::fscanf(infile, "%d %d\n", &numBoxes, &numDim);
+      (void)std::fscanf(infile, "%d %d\n", &numBoxes, &numDim);
       std::vector< int > minc( numDim ), maxc( numDim );
       /* Read boxes */
       for(boxNum = 0; boxNum < numBoxes; boxNum++) {
           for(dim = 0; dim < numDim; dim++)
-              std::fscanf( infile, "[%d, %d) ", &minc[dim], &maxc[dim] );
+              (void)std::fscanf( infile, "[%d, %d) ", &minc[dim], &maxc[dim] );
           boxes.push_back( Box( &minc[0], &maxc[0] ) );
-          std::fscanf(infile, "\n");
+          (void)std::fscanf(infile, "\n");
       }
     }
     
