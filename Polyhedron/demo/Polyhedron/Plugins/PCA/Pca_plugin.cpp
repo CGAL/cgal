48965b2a24396c84e0072194256a1ba1d5ca5f31
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-12-21 10:33:48 +0100

    fix PCA plugin

--- Polyhedron/demo/Polyhedron/Plugins/PCA/Pca_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/PCA/Pca_plugin.cpp
@@ -95,71 +95,70 @@ private:
 void Polyhedron_demo_pca_plugin::on_actionFitPlane_triggered()
 {
   const CGAL::Three::Scene_interface::Item_id index = scene->mainSelectionIndex();
-
-    Scene_surface_mesh_item* sm_item =
-    qobject_cast<Scene_surface_mesh_item*>(scene->item(index));
-
+  
+  Scene_surface_mesh_item* sm_item =
+      qobject_cast<Scene_surface_mesh_item*>(scene->item(index));
+  
+  
   QApplication::setOverrideCursor(Qt::WaitCursor);
-
-  std::list<Triangle> triangles;
+  if(sm_item){
+    std::list<Triangle> triangles;
+    
+    SMesh* pMesh = sm_item->polyhedron();
+    ::triangles(*pMesh,std::back_inserter(triangles));
+    
+    if(! triangles.empty()){
+      QString item_name = sm_item->name();
+      // fit plane to triangles
+      Plane plane;
+      std::cout << "Fit plane...";
+      CGAL::linear_least_squares_fitting_3(triangles.begin(),triangles.end(),plane,CGAL::Dimension_tag<2>());
+      std::cout << "ok" << std::endl;
+      
+      // compute centroid
+      Point center_of_mass = CGAL::centroid(triangles.begin(),triangles.end());
+      Scene_plane_item* new_item = new Scene_plane_item(this->scene);
+      new_item->setPosition(center_of_mass.x(),
+                            center_of_mass.y(),
+                            center_of_mass.z());
+      const Vector& normal = plane.orthogonal_vector();
+      new_item->setNormal(normal.x(), normal.y(), normal.z());
+      new_item->setName(tr("%1 (plane fit)").arg(item_name));
+      new_item->setColor(Qt::magenta);
+      new_item->setRenderingMode(sm_item->renderingMode());
+      scene->addItem(new_item);
+      QApplication::restoreOverrideCursor();
+      return;
+    }
+  }
+  Scene_points_with_normal_item* item =
+      qobject_cast<Scene_points_with_normal_item*>(scene->item(index));
   
-  SMesh* pMesh = sm_item->polyhedron();
-  ::triangles(*pMesh,std::back_inserter(triangles));
-  
-  if(! triangles.empty()){
-    QString item_name = sm_item->name();
+  if (item)
+  {
+    Point_set* points = item->point_set();
+    
     // fit plane to triangles
     Plane plane;
+    Point center_of_mass;
     std::cout << "Fit plane...";
-    CGAL::linear_least_squares_fitting_3(triangles.begin(),triangles.end(),plane,CGAL::Dimension_tag<2>());
+    CGAL::linear_least_squares_fitting_3
+        (points->points().begin(),points->points().end(),plane, center_of_mass,
+         CGAL::Dimension_tag<0>());
     std::cout << "ok" << std::endl;
-
+    
     // compute centroid
-    Point center_of_mass = CGAL::centroid(triangles.begin(),triangles.end());
     Scene_plane_item* new_item = new Scene_plane_item(this->scene);
     new_item->setPosition(center_of_mass.x(),
                           center_of_mass.y(),
                           center_of_mass.z());
     const Vector& normal = plane.orthogonal_vector();
     new_item->setNormal(normal.x(), normal.y(), normal.z());
-    new_item->setName(tr("%1 (plane fit)").arg(item_name));
+    new_item->setName(tr("%1 (plane fit)").arg(item->name()));
     new_item->setColor(Qt::magenta);
-    new_item->setRenderingMode(sm_item->renderingMode());
+    new_item->setRenderingMode(item->renderingMode());
     scene->addItem(new_item);
-
   }
-  else
-    {
-      Scene_points_with_normal_item* item =
-        qobject_cast<Scene_points_with_normal_item*>(scene->item(index));
-
-      if (item)
-        {
-          Point_set* points = item->point_set();
-
-          // fit plane to triangles
-          Plane plane;
-          Point center_of_mass;
-          std::cout << "Fit plane...";
-         CGAL::linear_least_squares_fitting_3
-           (points->points().begin(),points->points().end(),plane, center_of_mass,
-            CGAL::Dimension_tag<0>());
-          std::cout << "ok" << std::endl;
-
-          // compute centroid
-          Scene_plane_item* new_item = new Scene_plane_item(this->scene);
-          new_item->setPosition(center_of_mass.x(),
-                                center_of_mass.y(),
-                                center_of_mass.z());
-          const Vector& normal = plane.orthogonal_vector();
-          new_item->setNormal(normal.x(), normal.y(), normal.z());
-          new_item->setName(tr("%1 (plane fit)").arg(item->name()));
-          new_item->setColor(Qt::magenta);
-          new_item->setRenderingMode(item->renderingMode());
-          scene->addItem(new_item);
-
-        }
-    }
   QApplication::restoreOverrideCursor();
 }
 
@@ -167,80 +166,87 @@ void Polyhedron_demo_pca_plugin::on_actionFitLine_triggered()
 {
   const CGAL::Three::Scene_interface::Item_id index = scene->mainSelectionIndex();
 
+  QApplication::setOverrideCursor(Qt::WaitCursor);
+
   Scene_surface_mesh_item* sm_item =
-    qobject_cast<Scene_surface_mesh_item*>(scene->item(index));
-
-    QApplication::setOverrideCursor(Qt::WaitCursor);
-
-  CGAL::Bbox_3 bb;
+      qobject_cast<Scene_surface_mesh_item*>(scene->item(index));
   
-  std::list<Triangle> triangles;
-  SMesh* pMesh = sm_item->polyhedron();
-  bb = ::triangles(*pMesh,std::back_inserter(triangles));
-
-  if(! triangles.empty()){
-    QString item_name = sm_item->name();
-    // fit line to triangles
-    Line line;
-    std::cout << "Fit line...";
-    CGAL::linear_least_squares_fitting_3(triangles.begin(),triangles.end(),line,CGAL::Dimension_tag<2>());
-    std::cout << "ok" << std::endl;
-
-    // compute centroid
-    Point center_of_mass = CGAL::centroid(triangles.begin(),triangles.end());
-
-    // compute bounding box diagonal
-    Iso_cuboid bbox(bb);
-
-    // compute scale for rendering using diagonal of bbox
-    Point cmin = (bbox.min)();
-    Point cmax = (bbox.max)();
-    FT diag = std::sqrt(CGAL::squared_distance(cmin,cmax));
-
-    // construct a 3D bar
-    Vector u = line.to_vector();
-    u = u / std::sqrt(u*u);
-
-    Point a = center_of_mass + u * diag;
-    Point b = center_of_mass - u * diag;
-
-    Plane plane_a = line.perpendicular_plane(a);
-
-    Vector u1 = plane_a.base1();
-    u1 = u1 / std::sqrt(u1*u1);
-    u1 = u1 * 0.01 * diag;
-    Vector u2 = plane_a.base2();
-    u2 = u2 / std::sqrt(u2*u2);
-    u2 = u2 * 0.01 * diag;
-
-    Point points[8];
-
-    points[0] = a + u1;
-    points[1] = a + u2;
-    points[2] = a - u1;
-    points[3] = a - u2;
-
-    points[4] = b + u1;
-    points[5] = b + u2;
-    points[6] = b - u1;
-    points[7] = b - u2;
-
-    // add best fit line as new polyhedron bar
-    SMesh* pFit = new SMesh;
-    CGAL::make_hexahedron(points[0],
-                          points[1],
-                          points[2],
-                          points[3],
-                          points[4],
-                          points[5],
-                          points[6],
-                          points[7],
-                          *pFit);
-    Scene_surface_mesh_item* new_item = new Scene_surface_mesh_item(pFit);
-    new_item->setName(tr("%1 (line fit)").arg(item_name));
-    new_item->setColor(Qt::magenta);
-    new_item->setRenderingMode( sm_item->renderingMode());
-    scene->addItem(new_item);
+  if(sm_item)
+  {
+    CGAL::Bbox_3 bb;
+    
+    SMesh* pMesh = sm_item->polyhedron();
+    std::list<Triangle> triangles;
+    
+    bb = ::triangles(*pMesh,std::back_inserter(triangles));
+    
+    if(! triangles.empty()){
+      QString item_name = sm_item->name();
+      // fit line to triangles
+      Line line;
+      std::cout << "Fit line...";
+      CGAL::linear_least_squares_fitting_3(triangles.begin(),triangles.end(),line,CGAL::Dimension_tag<2>());
+      std::cout << "ok" << std::endl;
+      
+      // compute centroid
+      Point center_of_mass = CGAL::centroid(triangles.begin(),triangles.end());
+      
+      // compute bounding box diagonal
+      Iso_cuboid bbox(bb);
+      
+      // compute scale for rendering using diagonal of bbox
+      Point cmin = (bbox.min)();
+      Point cmax = (bbox.max)();
+      FT diag = std::sqrt(CGAL::squared_distance(cmin,cmax));
+      
+      // construct a 3D bar
+      Vector u = line.to_vector();
+      u = u / std::sqrt(u*u);
+      
+      Point a = center_of_mass + u * diag;
+      Point b = center_of_mass - u * diag;
+      
+      Plane plane_a = line.perpendicular_plane(a);
+      
+      Vector u1 = plane_a.base1();
+      u1 = u1 / std::sqrt(u1*u1);
+      u1 = u1 * 0.01 * diag;
+      Vector u2 = plane_a.base2();
+      u2 = u2 / std::sqrt(u2*u2);
+      u2 = u2 * 0.01 * diag;
+      
+      Point points[8];
+      
+      points[0] = a + u1;
+      points[1] = a + u2;
+      points[2] = a - u1;
+      points[3] = a - u2;
+      
+      points[4] = b + u1;
+      points[5] = b + u2;
+      points[6] = b - u1;
+      points[7] = b - u2;
+      
+      // add best fit line as new polyhedron bar
+      SMesh* pFit = new SMesh;
+      CGAL::make_hexahedron(
+            points[0],
+          points[3],
+          points[2],
+          points[1],
+          points[5],
+          points[4],
+          points[7],
+          points[6],
+          *pFit);
+      Scene_surface_mesh_item* new_item = new Scene_surface_mesh_item(pFit);
+      new_item->setName(tr("%1 (line fit)").arg(item_name));
+      new_item->setColor(Qt::magenta);
+      new_item->setRenderingMode( sm_item->renderingMode());
+      scene->addItem(new_item);
+    }
+    QApplication::restoreOverrideCursor();
+    return;
   }
   else
   {
@@ -297,15 +303,16 @@ void Polyhedron_demo_pca_plugin::on_actionFitLine_triggered()
         points[7] = b - u2;
 
         SMesh* pFit = new SMesh;
-        CGAL::make_hexahedron(points[0],
-                              points[1],
-                              points[2],
-                              points[3],
-                              points[4],
-                              points[5],
-                              points[6],
-                              points[7],
-                              *pFit);
+        CGAL::make_hexahedron(
+              points[0],
+            points[3],
+            points[2],
+            points[1],
+            points[5],
+            points[4],
+            points[7],
+            points[6],
+            *pFit);
         Scene_surface_mesh_item* new_item = new Scene_surface_mesh_item(pFit);
         new_item->setName(tr("%1 (line fit)").arg(item->name()));
         new_item->setColor(Qt::magenta);
@@ -313,7 +320,6 @@ void Polyhedron_demo_pca_plugin::on_actionFitLine_triggered()
         scene->addItem(new_item);
       }
   }
-
   QApplication::restoreOverrideCursor();
 }
 
