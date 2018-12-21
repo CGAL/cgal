9e8bfa3dedc27eb824a607f61e677dce7ea2cd62
Maxime Gimeno <maxime.gimeno@gmail.com>
2018-12-21 09:18:03 +0100

    Fix code in ply_writer

--- Polyhedron_IO/include/CGAL/IO/PLY_writer.h
+++ Polyhedron_IO/include/CGAL/IO/PLY_writer.h
@@ -77,9 +77,7 @@ namespace CGAL{
     typedef typename boost::graph_traits<SurfaceMesh>::halfedge_descriptor halfedge_descriptor;
     typedef typename boost::graph_traits<SurfaceMesh>::vertex_descriptor vertex_descriptor;
     typedef typename boost::property_map<SurfaceMesh, boost::vertex_point_t>::type::value_type Point_3;
-    typedef typename SurfaceMesh::template Property_map<halfedge_descriptor,std::pair<float, float> > ;
-    typedef boost::property_map<SurfaceMesh, CGAL::dynamic_halfedge_property_t<std::pair<float, float> > >::type TrafficDensityMap;
-    TrafficDensityMap tdm = get(CGAL::dynamic_halfedge_property_t<double>(), mesh);
+    typedef typename SurfaceMesh::template Property_map<halfedge_descriptor,std::pair<float, float> > UV_map;
     UV_map h_uv;
     bool has_texture;
     boost::tie(h_uv, has_texture) = mesh.template property_map<halfedge_descriptor,std::pair<float, float> >("h:uv");
