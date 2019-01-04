33d17ac7a855a29f6847d270ab2e54de2b21198f
Maxime Gimeno <maxime.gimeno@gmail.com>
2019-01-04 12:56:40 +0100

    Add data and script for testing

---
+++ Polyhedron/demo/Polyhedron/testing/test_demo.js
@@ -0,0 +1,22 @@
+var testItem = "./testing/data/sphere.off";
+main_window.open(testItem, 'off_plugin');
+main_window.test_all_actions();
+testItem = "./testing/data/oni.pwn";
+main_window.open(testItem, 'xyz_plugin');
+main_window.test_all_actions();
+
+var itemA = "./testing/data/itema.off";
+var itemB = "./testing/data/itemb.off";
+main_window.open(itemA, 'off_to_nef_plugin');
+main_window.open(itemB, 'off_to_nef_plugin');
+scene.setItemA(0);
+scene.setItemB(1);
+main_window.test_all_actions();
+
+testItem = "./testing/data/poly.polylines.txt";
+main_window.open(testItem, 'polylines_io_plugin');
+main_window.test_all_actions();
+
+testItem = "./testing/data/sphere.mesh";
+main_window.open(testItem, 'C3t3_io_plugin');
+main_window.test_all_actions();
