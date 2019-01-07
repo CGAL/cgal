6c42f63d64d9f11586e181057447929032a4fac3
Simon Giraudot <simon.giraudot@geometryfactory.com>
2019-01-07 14:47:24 +0100

    Modify named parameters and begin ref manual

--- Point_set_processing_3/include/CGAL/OpenGR/register_point_sets.h
+++ Point_set_processing_3/include/CGAL/OpenGR/register_point_sets.h
@@ -71,9 +71,56 @@ register_point_sets(const PointRange1& range1,    PointRange2& range2,
 
 } // end of namespace internal
 
-/*!
- * TODO: document me
- */
+/**
+   \ingroup PkgPointSetProcessing3Algorithms
+   
+   Computes the registration of `point_set_2` with respect to `point_set_1` and
+   applies it.
+
+   \tparam PointRange1 is a model of `Range`. The value type of its iterator is
+   the key type of the named parameter `point_map` in `NamedParameters1`.
+   \tparam PointRange2 is a model of `Range`. The value type of its iterator is
+   the key type of the named parameter `point_map` in `NamedParameters2`.
+
+   \param point_set_1 input point range used as reference.
+   \param point_set_2 input point range whose registration w.r.t. `point_set_1` will be computed.
+   \param np1 optional sequence of \ref psp_namedparameters "Named Parameters" among the ones listed below.
+
+   \cgalNamedParamsBegin
+     \cgalParamBegin{point_map} a model of `ReadablePropertyMap` whose key type
+     is the value type of the iterator of `PointRange1` and whose value type is
+     `geom_traits::Point_3`.  If this parameter is omitted,
+     `CGAL::Identity_property_map<geom_traits::Point_3>` is used.\cgalParamEnd
+     \cgalParamBegin{normal_map} a model of `ReadablePropertyMap` whose key
+     type is the value type of the iterator of `PointRange1` and whose value
+     type `geom_traits::Vector_3`.\cgalParamEnd
+     \cgalParamBegin{number_of_samples} size of the subset of input points used
+     to compute registration.\cgalParamEnd
+     \cgalParamBegin{accuracy} registration accuracy expressed in scene
+     units.\cgalParamEnd
+     \cgalParamBegin{overlap} ratio of expected overlap between the two point
+     sets (between 0 for no overlap to 1 for a full overlap).\cgalParamEnd
+     \cgalParamBegin{maximum_running_time} number of seconds after which the
+     algorithm is forced to stop and returns the best solution found so
+     far.\cgalParamEnd
+     \cgalParamBegin{geom_traits} an instance of a geometric traits class,
+     model of `Kernel`\cgalParamEnd
+   \cgalNamedParamsEnd
+
+   \param np2 optional sequence of \ref psp_namedparameters "Named Parameters" among the ones listed below.
+
+   \cgalNamedParamsBegin
+     \cgalParamBegin{point_map} a model of `ReadablePropertyMap` whose key type
+     is the value type of the iterator of `PointRange2` and whose value type is
+     `geom_traits::Point_3`.  If this parameter is omitted,
+     `CGAL::Identity_property_map<geom_traits::Point_3>` is used.\cgalParamEnd
+     \cgalParamBegin{normal_map} a model of `ReadablePropertyMap` whose key
+     type is the value type of the iterator of `PointRange2` and whose value
+     type `geom_traits::Vector_3`.\cgalParamEnd
+   \cgalNamedParamsEnd
+
+   \return the registration score.
+*/
 template <class PointRange1, class PointRange2,
           class NamedParameters1, class NamedParameters2>
 double
@@ -105,7 +152,13 @@ register_point_sets (const PointRange1& point_set_1, PointRange2& point_set_2,
   PointMap1 point_map2 = choose_param(get_param(np2, internal_np::point_map), PointMap2());
   NormalMap2 normal_map2 = choose_param(get_param(np2, internal_np::normal_map), NormalMap2());
 
-  Options options = choose_param(get_param(np1, internal_np::opengr_options), Options());
+  Options options;
+  options.sample_size = choose_param(get_param(np1, internal_np::number_of_samples), 200);
+  options.delta = choose_param(get_param(np1, internal_np::accuracy), 5.00);
+  options.max_time_seconds = choose_param(get_param(np1, internal_np::maximum_running_time), 1000);
+  bool overlap_ok = options.configureOverlap (choose_param(get_param(np1, internal_np::overlap), 0.20));
+  CGAL_USE (overlap_ok);
+  CGAL_static_assertion_msg (overlap_ok, "Invalid overlap configuration.");
 
   return internal::register_point_sets<Kernel>(point_set_1, point_set_2,
                                                point_map1, point_map2,
