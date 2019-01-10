92b87281eae816efd083d84b6e8d6135e412c37c
Maxime Gimeno <maxime.gimeno@gmail.com>
2019-01-10 11:27:59 +0100

    Fix selection crash for non triangle meshes

--- Polyhedron/demo/Polyhedron/Plugins/PMP/Selection_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/PMP/Selection_plugin.cpp
@@ -777,7 +777,8 @@ public Q_SLOTS:
       bool is_valid = true;
       BOOST_FOREACH(boost::graph_traits<Face_graph>::face_descriptor fd, faces(*selection_item->polyhedron()))
       {
-        if (CGAL::Polygon_mesh_processing::is_degenerate_triangle_face(fd, *selection_item->polyhedron()))
+        if (is_triangle(halfedge(fd, *selection_item->polyhedron()), *selection_item->polyhedron())
+            && CGAL::Polygon_mesh_processing::is_degenerate_triangle_face(fd, *selection_item->polyhedron()))
         {
           is_valid = false;
           break;
