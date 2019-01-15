10acacaf0d2c9c5e6a23e583b54c63dca95c9b86
Andreas Fabri <andreas.fabri@geometryfactory.com>
2019-01-15 08:31:18 +0100

    Work around CGAL_CFG_MATCHING_BUG_6

--- Periodic_4_hyperbolic_triangulation_2/include/CGAL/Periodic_4_hyperbolic_Delaunay_triangulation_traits_2.h
+++ Periodic_4_hyperbolic_triangulation_2/include/CGAL/Periodic_4_hyperbolic_Delaunay_triangulation_traits_2.h
@@ -63,7 +63,15 @@ public:
 #ifndef CGAL_CFG_MATCHING_BUG_6
   using Predicate::operator();
 #else
+  result_type operator()(const Point& p0, const Point& p1, const Point& p2) const
+  {
+    return Predicate()(p0,p1,p2);
+  }
 
+  result_type operator()(const Point& p0, const Point& p1, const Point& p2, const Point& p3) const
+  {
+    return Predicate()(p0,p1,p2,p3);
+  }
 #endif
 
   Hyperbolic_traits_with_translations_2_adaptor(const Predicate_ pred = Predicate_()) : Predicate_(pred) {}
@@ -128,7 +136,10 @@ public:
 #ifndef CGAL_CFG_MATCHING_BUG_6
   using Construct_point_base::operator();
 #else
-
+  Point operator()(const NT& x, const NT& y) const
+  {
+    return Construct_point_base()(x,y);
+  }
 #endif
   
   Periodic_4_construct_hyperbolic_point_2() { }
