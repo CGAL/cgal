9e056880e83d4174bc7580ac4d0a5e9f028bc10f
Laurent Rineau <laurent.rineau@cgal.org>
2019-02-08 09:46:24 +0100

    Better warning fix: do not initialize, and use CGAL_assume
    Let the variable uninitialized, and instruct the compiler that the
    condition where `i` remains uninitialized is an error condition.

--- Triangulation_2/include/CGAL/Constrained_triangulation_plus_2.h
+++ Triangulation_2/include/CGAL/Constrained_triangulation_plus_2.h
@@ -649,10 +649,9 @@ public:
 	++it){
       if(! is_subconstraint(*it, *succ)){ // this checks whether other constraints pass
 	Face_handle fh;
-	int i = -1; // this will be set by is_edge() below. Without initialization we get a warning
-        CGAL_triangulation_assertion_code(bool b =)
-          Triangulation::is_edge(*it, *succ, fh, i);
-	CGAL_triangulation_assertion(b);
+	int i;
+	bool b = Triangulation::is_edge(*it, *succ, fh, i);
+	CGAL_assume(b);
 	Triangulation::remove_constrained_edge(fh,i, out); // this does also flipping if necessary.
       }
     }
