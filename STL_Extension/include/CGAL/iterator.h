b84f54a9e324104112c305a309a61015c36f4f66
Andreas Fabri <andreas.fabri@geometryfactory.com>
2019-02-08 09:46:18 +0100

    Do not define the macro CGAL_CFG_NO_CPP0X_DELETED_AND_DEFAULT_FUNCTIONS for VC2013 as suggested in Issue #3436

--- STL_Extension/include/CGAL/iterator.h
+++ STL_Extension/include/CGAL/iterator.h
@@ -1445,7 +1445,7 @@ public:
 
   Dispatch_or_drop_output_iterator(O... o) : Base(o...) {}
   
-#if( (! defined(CGAL_CFG_NO_CPP0X_DELETED_AND_DEFAULT_FUNCTIONS) ) && ( ( ! defined BOOST_MSVC) || (BOOST_MSVC > 1800) ) )
+#ifndef CGAL_CFG_NO_CPP0X_DELETED_AND_DEFAULT_FUNCTIONS
   Dispatch_or_drop_output_iterator(const Dispatch_or_drop_output_iterator&)=default;
   Dispatch_or_drop_output_iterator& operator=(const Dispatch_or_drop_output_iterator&)=default;
 #endif
