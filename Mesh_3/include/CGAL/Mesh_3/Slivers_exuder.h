616fb5dd5151e0ffad60e2bf8a68b3a154b63bd4
Andreas Fabri <andreas.fabri@geometryfactory.com>
2019-02-11 16:06:34 +0100

    Mesh_3: Disable parallel sliver exudation in VC++ debug in the testsuite

--- Mesh_3/include/CGAL/Mesh_3/Slivers_exuder.h
+++ Mesh_3/include/CGAL/Mesh_3/Slivers_exuder.h
@@ -195,7 +195,10 @@ private:
   Tet_priority_queue cells_queue_;
 };
 
-#ifdef CGAL_LINKED_WITH_TBB
+
+  //  The debug version of the VC++ testsuite gets a timeout when using TBB, so let's disable it
+#if defined( CGAL_LINKED_WITH_TBB ) && ( (! defined (BOOST_MSVC)) || ( defined (BOOST_MSVC)  && defined( _DEBUG ) && (! defined ( CGAL_TEST_SUITE ) ) ) )
+
 // Parallel
 template <typename Tr>
 class Slivers_exuder_base<Tr, Parallel_tag>
