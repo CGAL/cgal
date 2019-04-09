3fa3fc1ab066a63c1e94ad2b31c5ccd6e0808b42
Andreas Fabri <andreas.fabri@geometryfactory.com>
2019-04-09 14:12:40 +0200

    typos

--- Partition_2/doc/Partition_2/CGAL/Partition_traits_adapter_2.h
+++ Partition_2/doc/Partition_2/CGAL/Partition_traits_adapter_2.h
@@ -3,13 +3,13 @@ namespace CGAL {
 /*!
 \ingroup PkgPartition2Ref
 
-Given a property map associating a key to a point, the class `Partition_traits_adapter_2` induces a spatial 
-reorder of the keys instead of the points, the comparisons being done on the associated points. 
-In other words, the traits provides to a spatial sort algorithm a point type which is a key, 
-while the actual point type is `Base_traits::Point_2`. 
+Given a property map associating a key to a point, the class `Partition_traits_adapter_2`
+enables to compute a partition of a sequence of keys, performing the predicates of the base traits class
+on the points associated to the keys.
 
 
-\tparam Base_traits must be a model for `SpatialSortingTraits_2`. 
+
+\tparam Base_traits must be a model for `PartitionTraits_2`. 
 \tparam PointPropertyMap must be a model of `ReadablePropertyMap`
 with value type `Base_traits::Point_2`.
 
