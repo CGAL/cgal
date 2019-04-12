37226211cee94e487e705e9d30ab077e75b86c29
Maxime Gimeno <maxime.gimeno@gmail.com>
2019-04-12 15:16:24 +0200

    Add license header and namespace

--- Stream_support/include/CGAL/IO/has_data.h
+++ Stream_support/include/CGAL/IO/has_data.h
@@ -1,6 +1,29 @@
+// Copyright (c) 2019
+// GeometryFactory.  All rights reserved. 
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: LGPL-3.0+
+//
+//
+// Author(s)     : Maxime Gimeno
+
 #ifndef CGAL_HAS_DATA_H
 #define CGAL_HAS_DATA_H
 #include <CGAL/Has_member.h>
+namespace CGAL{
 CGAL_GENERATE_MEMBER_DETECTOR(read_data);
 CGAL_GENERATE_MEMBER_DETECTOR(write_data);
+}
 #endif // CGAL_HAS_DATA_H
