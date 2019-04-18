3990f75764d674cc7442d34f98165460a87ab68d
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2019-04-18 16:26:09 +0200

    Update ref manual for the two classes Surface_mesh_curve_topology and Path_on_surface.

--- Surface_mesh_topology/doc/Surface_mesh_topology/CGAL/Path_on_surface.h
+++ Surface_mesh_topology/doc/Surface_mesh_topology/CGAL/Path_on_surface.h
@@ -3,16 +3,21 @@ namespace CGAL {
   /*!
     \ingroup PkgSurfaceMeshTopologyClasses
     
-    The class `Path_on_surface` represents a walk in the vertex-edge graph of a `CombinatorialMap`. Each object in this class is constructed from an external combinatorial map on which the path should lie. A path is represented as a sequence of darts, each dart representing an oriented edge in the path. The class `Path_on_surface` behaves as a container for this sequence of darts. Darts are added one at a time to the path thanks to the `push_back()` method.
+    The class `Path_on_surface` represents a walk in a mesh which is either a 2D `CombinatorialMap` or a model of a `FaceGraph`. Each object in this class is constructed from an external mesh on which the path should lie. A path is represented as a sequence of darts or half-edges, each one representing an oriented edge in the path. The class `Path_on_surface` behaves as a container for this sequence of darts/half-edges. Elements are added in the path one at a time to the path thanks to the `push_back()` method.
     
-    \tparam CMap a model of  `CombinatorialMap`
+    \tparam Mesh a model of `CombinatorialMap` or of `FaceGraph`
   */
-  template<typename CMap_>
+  template<typename Mesh>
   class Path_on_surface
   {
   public:
-    /// Constructor. Creates an empty path object which should lie on amap.
-    Path_on_surface(const CMap& amap);
+    /*!
+      %Dart const handle type. An handle to Dart for combinatorial maps, or an half-edge descriptor for models of FaceGraph.
+    */
+    typedef unspecified_type Dart_const_handle;
+
+    /// Constructor. Creates an empty path object which lies on amesh.
+    Path_on_surface(const Mesh& aesh);
 
     /// @return `true` iff the path is empty
     bool is_empty() const;
@@ -30,22 +35,22 @@ namespace CGAL {
     /// @return `true` iff df can be added at the end of the path.
     bool can_be_pushed(Dart_const_handle dh) const;
   
-    /// Add the given dart at the end of this path.
+    /// Add the given dart/half-edge at the end of this path.
     /// @pre can_be_pushed(dh)
     void push_back(Dart_const_handle dh);
 
-    /// Add the dart with given index i at the end of this path.
+    /// Add the dart/half-edge with given index i at the end of this path.
     /// @pre can_be_pushed_by_index(i)
     void push_back_by_index(std::size_t i);
 
-    /// @return `true` iff the dart with index i can be added at the end of the path.
+    /// @return `true` iff the dart/half-edge with index i can be added at the end of the path.
     bool can_be_pushed_by_index(std::size_t i) const;
 
-    /// Add the dart obtained by turning nb times around the target vertex of the last dart in this path, in the positive circular order.
+    /// Add the dart/half-edge obtained by turning nb times around the target vertex of the last dart/half-edge in this path, in the positive circular order.
     /// @pre !is_empty()
     void extend_positive_turn(std::size_t nb); 
 
-    /// Add the dart obtained by turning nb times around the target vertex of the last dart in this path, in the negative circular order.
+    /// Add the dart/half-edge obtained by turning nb times around the target vertex of the last dart/half-edge in this path, in the negative circular order.
     /// @pre !is_empty()
     void extend_negative_turn(std::size_t nb); 
 
@@ -56,10 +61,10 @@ namespace CGAL {
     /// Reverse the path (i.e. negate its orientation).
     void reverse();
 
-    /// Creates a random open path with lenght darts.
+    /// Creates a random open path with lenght darts/half-edges.
     void generate_random_path(std::size_t lenght, CGAL::Random& random=CGAL::get_default_random());
 
-    /// Creates a random closed path with at least lenght darts.
+    /// Creates a random closed path with at least lenght darts/half-edges.
     void generate_random_closed_path(std::size_t length, CGAL::Random& random=CGAL::get_default_random());
     
    };
--- Surface_mesh_topology/doc/Surface_mesh_topology/CGAL/Surface_mesh_curve_topology.h
+++ Surface_mesh_topology/doc/Surface_mesh_topology/CGAL/Surface_mesh_curve_topology.h
@@ -3,32 +3,35 @@ namespace CGAL {
   /*!
     \ingroup PkgSurfaceMeshTopologyClasses
     
-    The class `Surface_mesh_curve_topology` represents a combinatorial map internally represented as a quadrangulation. Each object in this class is constructed from an external combinatorial map. It maintains a correspondence between this combinatorial map and its internal quadrangulation.
+    The class `Surface_mesh_curve_topology` represents a combinatorial map internally represented as a quadrangulation. Each object in this class is constructed from an external mesh, either a 2D combinatorial map or a model of a FaceGraph. It maintains a correspondence between this mesh and its internal quadrangulation.
     
-    \tparam CMap a model of `CombinatorialMap`
+    \tparam Mesh a model of `CombinatorialMap` or of `FaceGraph`
   */
-  template<typename CMap>
+  template<typename Mesh>
   class Surface_mesh_curve_topology
   {
   public:
 
-    /*! Constructor. Creates a Surface_mesh_curve_topology object using amap as input.
+    /*! Constructor. Creates a Surface_mesh_curve_topology object using amesh as input.
      */
-    Surface_mesh_curve_topology(const CMap& amap);
+    Surface_mesh_curve_topology(const Mesh& amesh);
 
     /*! Returns `true` if the closed paths p1 and p2 are freely homotopic.
+     *  @pre p1 and p2 must be two paths on amesh.
      */
-    bool are_freely_homotopic(const Path_on_surface<CMap>& p1,
-                              const Path_on_surface<CMap>& p2) const;
+    bool are_freely_homotopic(const Path_on_surface<Mesh>& p1,
+                              const Path_on_surface<Mesh>& p2) const;
 
     /*! Returns `true` if the paths p1 and p2 are homotopic with fixed endpoints. The paths p1 and p2 must have the same endpoints but must not be closed. Equivalent to `Surface_mesh_curve_topology::is_contractible` (q) where q is the concatenation of p1 and the reverse of p2.
+     *  @pre p1 and p2 must be two paths on amesh.
      */
-    bool are_base_point_homotopic(const Path_on_surface<CMap>& p1,
-                                  const Path_on_surface<CMap>& p2) const;
+    bool are_base_point_homotopic(const Path_on_surface<Mesh>& p1,
+                                  const Path_on_surface<Mesh>& p2) const;
 
     /*! Returns `true` if the closed path p is contractible.
+     *  @pre p must be two paths on amesh.
      */
-    bool is_contractible(const Path_on_surface<CMap>& p) const;    
+    bool is_contractible(const Path_on_surface<Mesh>& p) const;    
   };
 
 }
