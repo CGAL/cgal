f3076fa9333bdbb2ec580ab6cba8fa2d7cd3305e
Maxime Gimeno <maxime.gimeno@gmail.com>
2019-04-30 13:24:03 +0200

    Merge implicit_functions in Polyhedron_Demo

--- Testsuite/test/post_process_ctest_results.py
+++ Testsuite/test/post_process_ctest_results.py
@@ -49,6 +49,8 @@ with open(report_file, "rt") as test_report:
           name="libCGALimageIO_shared"
         elif name == "libCGAL_Qt5":
           name="libCGALQt5_shared"
+        elif name == "Mesh_3_implicit_functions":
+          name="Polyhedron_Demo"
         if name=="incomplete":
           is_writing=False
           is_ignored=False
