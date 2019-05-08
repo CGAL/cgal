c49f173d83f4988eeedd640f2b1a6afa1b17d729
Sébastien Loriot <sebastien.loriot@cgal.org>
2019-05-08 10:13:40 +0200

    polyline orientation depend on supporting mesh

--- Polygon_mesh_processing/test/Polygon_mesh_processing/polygon_mesh_slicer_test.cpp
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/polygon_mesh_slicer_test.cpp
@@ -10,6 +10,7 @@
 #include <CGAL/AABB_halfedge_graph_segment_primitive.h>
 
 #include <CGAL/Polygon_mesh_slicer.h>
+#include <CGAL/Polygon_mesh_processing/orientation.h>
 #include <CGAL/AABB_tree.h>
 #include <CGAL/AABB_traits.h>
 #include <CGAL/Polygon_2.h>
@@ -21,7 +22,7 @@ typedef CGAL::Exact_predicates_inexact_constructions_kernel Epic;
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Epec;
 
 template<class K, class Polyline_type>
-bool is_ccw(const typename K::Plane_3& plane,
+bool is_ccw(int xi, int yi,
     const Polyline_type& polyline)
 {
   CGAL::Polygon_2<K> polygon;
@@ -29,12 +30,10 @@ bool is_ccw(const typename K::Plane_3& plane,
   {
     BOOST_FOREACH(const typename K::Point_3& p, polyline)
     {
-      polygon.push_back(plane.to_2d(p));
+      polygon.push_back(typename K::Point_2(p[xi], p[yi]));
     }
     polygon.erase(polygon.vertices_end()-1);
-    if(polygon.size() > 2 &&
-       polygon.is_counterclockwise_oriented())
-      return polygon.is_counterclockwise_oriented();
+    return polygon.is_counterclockwise_oriented();
   }
   return true;
 }
@@ -46,100 +45,123 @@ int test_slicer()
 #else
   typedef CGAL::Polyhedron_3<K> Mesh;
 #endif
-  
+
   typedef CGAL::AABB_halfedge_graph_segment_primitive<Mesh> HGSP;
   typedef CGAL::AABB_traits<K, HGSP>    AABB_traits;
   typedef CGAL::AABB_tree<AABB_traits>  AABB_tree;
   typedef std::vector<typename K::Point_3> Polyline_type;
-  typedef std::list< Polyline_type > Polylines;
-  
+  typedef std::vector< Polyline_type > Polylines;
+
   //API test
   {
     std::ifstream input("data/U.off");
     Mesh m;
-    
+
     if (!input || !(input >> m)){
       std::cerr << "Error: can not read file.\n";
       return 1;
     }
-    
+
     AABB_tree tree(edges(m).first, edges(m).second, m);
-    
+
     CGAL::Polygon_mesh_slicer<Mesh, K> slicer(m, tree);
     Polylines polylines;
     slicer(typename K::Plane_3(1,1,0,0), std::back_inserter(polylines));
     assert(polylines.size()==1);
   }
-  
+
   std::ifstream input("data_slicer/open_cube_meshed.off");
   Mesh m;
-  
+
   if (!input || !(input >> m)){
     std::cerr << "Error: can not read file.\n";
     return 1;
   }
-  
+
   CGAL::Polygon_mesh_slicer<Mesh, K> slicer(m);
-  
+
   Polylines polylines;
-  
+
   // test isolated vertex
-  typename K::Plane_3 plane(0,1,0,0);
-  slicer(plane, std::back_inserter(polylines));
+  slicer(typename K::Plane_3(0,1,0,0), std::back_inserter(polylines));
   assert(polylines.size()==2); // two polylines
   assert( (polylines.front().size()==1) != (polylines.back().size()==1)); //only one isolated vertex
-  
-  BOOST_FOREACH(const Polyline_type& polyline, polylines)
-  {
-    bool ok = is_ccw<K,Polyline_type>( plane,polyline);
-    assert(ok);
-  }
+
+
   //test two nested polylines, one open and one closed
   polylines.clear();
-  plane = typename K::Plane_3(0,1,0,0.5); 
-  slicer(plane, std::back_inserter(polylines));
+  slicer(typename K::Plane_3(0,1,0,0.5), std::back_inserter(polylines));
   assert(polylines.size()==2);// two polylines
   assert( (polylines.front().front()==polylines.front().back()) !=
-      (polylines.back().front()==polylines.back().back()) ); //one open and one closed polyline
-  
-  
-  BOOST_FOREACH(const Polyline_type& polyline, polylines)
-  {
-    bool ok = is_ccw<K,Polyline_type>( plane,polyline);
-    assert(ok);
-  }
-  polylines.clear();
-  
+          (polylines.back().front()==polylines.back().back()) ); //one open and one closed polyline
+
   // test only coplanar edges
-  plane = typename K::Plane_3(0,0,1,1);
-  slicer(plane, std::back_inserter(polylines));
+  polylines.clear();
+  slicer(typename K::Plane_3(0,0,1,1), std::back_inserter(polylines));
   assert(polylines.size()==1); // one polyline
   assert(polylines.front().front()==polylines.front().back()); // that is closed
-  
-  
-  BOOST_FOREACH(const Polyline_type& polyline, polylines)
-  {
-    bool ok = is_ccw<K,Polyline_type>( plane,polyline);
-    assert(ok);
-  }
-  polylines.clear();
-  
+
+
   //test only coplanar border edges
-  plane = typename K::Plane_3(0,0,1,-1);
-  slicer(plane, std::back_inserter(polylines));
+  polylines.clear();
+  slicer(typename K::Plane_3(0,0,1,-1), std::back_inserter(polylines));
   assert(polylines.size()==1); // one polyline
   assert(polylines.front().front()!=polylines.front().back()); // that is closed
-  
-  BOOST_FOREACH(const Polyline_type& polyline, polylines)
-  {
-    bool ok = is_ccw<K,Polyline_type>( plane,polyline);
-    assert(ok);
-  }
-  polylines.clear();
+
   //test no intersection
+  polylines.clear();
   slicer(typename K::Plane_3(0,0,1,333), std::back_inserter(polylines));
   assert(polylines.empty());
-  
+
+// Now test the orientation of polylines
+  polylines.clear();
+  slicer(typename K::Plane_3(0,1,0,0.5), std::back_inserter(polylines));
+  assert(polylines.size()==2); // two polylines
+  int closed_id = polylines.front().front()==polylines.front().back() ? 0 : 1;
+  CGAL_assertion( is_ccw<K>(0, 2 , polylines[closed_id]) );
+
+  polylines.clear();
+  slicer(typename K::Plane_3(0,-1,0,-0.5), std::back_inserter(polylines));
+  assert(polylines.size()==2); // two polylines
+  closed_id = polylines.front().front()==polylines.front().back() ? 0 : 1;
+  CGAL_assertion( !is_ccw<K>(0, 2, polylines[closed_id]) );
+
+  polylines.clear();
+  slicer(typename K::Plane_3(0,0,1,1), std::back_inserter(polylines));
+  assert(polylines.size()==1); // one polyline
+  CGAL_assertion( is_ccw<K>(0, 1 , polylines[0]) );
+
+  polylines.clear();
+  slicer(typename K::Plane_3(0,0,-1,-1), std::back_inserter(polylines));
+  assert(polylines.size()==1); // one polyline
+  CGAL_assertion( !is_ccw<K>(0, 1 , polylines[0]) );
+
+  // reverse face orientation (no need to rebuild the tree)
+  CGAL::Polygon_mesh_processing::reverse_face_orientations(m);
+  polylines.clear();
+  slicer(typename K::Plane_3(0,1,0,0.5), std::back_inserter(polylines));
+  assert(polylines.size()==2); // two polylines
+  closed_id = polylines.front().front()==polylines.front().back() ? 0 : 1;
+  CGAL_assertion( !is_ccw<K>(0, 2 , polylines[closed_id]) );
+
+  polylines.clear();
+  slicer(typename K::Plane_3(0,-1,0,-0.5), std::back_inserter(polylines));
+  assert(polylines.size()==2); // two polylines
+  closed_id = polylines.front().front()==polylines.front().back() ? 0 : 1;
+  CGAL_assertion( is_ccw<K>(0, 2, polylines[closed_id]) );
+
+  polylines.clear();
+  slicer(typename K::Plane_3(0,0,1,1), std::back_inserter(polylines));
+  assert(polylines.size()==1); // one polyline
+  CGAL_assertion( !is_ccw<K>(0, 1 , polylines[0]) );
+
+  polylines.clear();
+  slicer(typename K::Plane_3(0,0,-1,-1), std::back_inserter(polylines));
+  assert(polylines.size()==1); // one polyline
+  CGAL_assertion( is_ccw<K>(0, 1 , polylines[0]) );
+
+
+
   return 0;
 }
 
@@ -148,6 +170,6 @@ int main()
 {
   assert(test_slicer<Epic>() == 0);
   assert(test_slicer<Epec>() == 0);
-  
+
   return 0;
 }
