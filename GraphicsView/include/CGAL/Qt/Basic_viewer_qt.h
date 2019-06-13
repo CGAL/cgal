a06f4f9aa2a8d40155e8f1f2869c9334f1082a31
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-06-12 23:38:38 -0700

    Add method add_line(point, vector) in Basic_viewer_qt to view rays.

--- GraphicsView/include/CGAL/Qt/Basic_viewer_qt.h
+++ GraphicsView/include/CGAL/Qt/Basic_viewer_qt.h
@@ -281,9 +281,33 @@ public:
                    const CGAL::Color& acolor)
   { m_buffer_for_colored_segments.add_segment(p1, p2, acolor); }
 
-  template<typename KPoint>
-  void add_line(const KPoint& p1, const KPoint& p2)
-  { m_buffer_for_mono_lines.add_line(p1, p2); }
+  template<typename KPoint, typename KVector>
+  void add_line(const KPoint& p, const KVector& v)
+  {       CGAL::Bbox_3 bb;
+      if (bb==bounding_box()) // Case of "empty" bounding box
+      {
+      bb=Local_point(CGAL::ORIGIN).bbox();
+          bb=bb + Local_point(200,200,200).bbox(); // To avoid a warning from Qglviewer
+      }
+      else
+      { bb=bounding_box(); }
+      this->camera()->setSceneBoundingBox(CGAL::qglviewer::Vec(bb.xmin(),
+                                                               bb.ymin(),
+                                                               bb.zmin()),
+                                          CGAL::qglviewer::Vec(bb.xmax(),
+                                                               bb.ymax(),
+                                                               bb.zmax()));
+      this->showEntireScene();
+      Local_kernel::Iso_rectangle_2 clipping_rect(bb.xmin(), bb.ymin(),
+                                                  bb.xmax(), bb.ymax());
+      Local_kernel::Ray_2 ray(p, v);
+      Object o = CGAL::intersection(ray, clipping_rect);
+      typedef Local_kernel::Segment_2 Segment_2;
+      typedef Local_kernel::Point_2 Point_2;
+      if(const Segment_2 *s = CGAL::object_cast<Segment_2>(&o)){
+          add_segment(s->source(), s->target());
+      }
+  }
 
   bool is_a_face_started() const
   {
--- Voronoi_diagram_2/include/CGAL/draw_voronoi_diagram_2.h
+++ Voronoi_diagram_2/include/CGAL/draw_voronoi_diagram_2.h
@@ -122,22 +122,7 @@ protected:
           } else {
               end_point = he->target()->point();
           }
-          Kernel::Ray_2 ray(end_point, direction);
-          CGAL::Bbox_3 bb;
-          if (bb==bounding_box()) // Case of "empty" bounding box
-          {   bb=Local_point(CGAL::ORIGIN).bbox();
-              bb=bb + Local_point(500,500,500).bbox(); // To avoid a warning from Qglviewer
-          }
-          else
-          { bb=bounding_box(); }
-          Kernel::Iso_rectangle_2 clipping_rect(bb.xmin(), bb.ymin(),
-                                                bb.xmax(), bb.ymax());
-          Object o = CGAL::intersection(ray, clipping_rect);
-          typedef Kernel::Segment_2 Segment_2;
-          typedef Kernel::Point_2 Point_2;
-          if(const Segment_2 *s = CGAL::object_cast<Segment_2>(&o)){
-              add_segment(s->source(), s->target());
-          }
+          add_line(end_point, direction);
       }
   }
 
