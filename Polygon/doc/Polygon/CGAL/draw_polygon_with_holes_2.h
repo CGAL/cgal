0ad3d5cd3bfe539504e5948537acbebc0ba0134c
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2019-06-19 15:27:11 +0200

    Update Polygon/doc/Polygon/CGAL/draw_polygon_with_holes_2.h
    Co-Authored-By: Sebastien Loriot <sloriot.ml@gmail.com>

--- Polygon/doc/Polygon/CGAL/draw_polygon_with_holes_2.h
+++ Polygon/doc/Polygon/CGAL/draw_polygon_with_holes_2.h
@@ -3,7 +3,7 @@ namespace CGAL {
 /*!
 \ingroup PkgDrawPolygonWithHoles2
 
-Open a new window and draw `aph`, an instance of the `CGAL::Polygon_with_holes_2` class. The function is blocking, that is the program continues as soon as the user closes the window. This function requires CGAL_Qt5, and is only available if the flag CGAL_USE_BASIC_VIEWER is defined at compile time.
+opens a new window and draws `aph`, an instance of the `CGAL::Polygon_with_holes_2` class. The function is blocking, that is the program continues as soon as the user closes the window. This function requires CGAL_Qt5, and is only available if the flag CGAL_USE_BASIC_VIEWER is defined at compile time.
 \tparam PH an instance of the `CGAL::Polygon_with_holes_2` class.
 \param aph the polygon with holes to draw.
 
