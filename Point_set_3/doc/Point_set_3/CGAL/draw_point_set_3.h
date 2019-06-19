37967344231d843b0fcc5ce68cc456bf04d06e59
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2019-06-19 15:25:44 +0200

    Update Point_set_3/doc/Point_set_3/CGAL/draw_point_set_3.h
    Co-Authored-By: Sebastien Loriot <sloriot.ml@gmail.com>

--- Point_set_3/doc/Point_set_3/CGAL/draw_point_set_3.h
+++ Point_set_3/doc/Point_set_3/CGAL/draw_point_set_3.h
@@ -3,7 +3,7 @@ namespace CGAL {
 /*!
 \ingroup PkgDrawPointSet3D
 
-Open a new window and draw `aps`, an instance of the `CGAL::Point_set_3` class. The function is blocking, that is the program continues as soon as the user closes the window. This function requires CGAL_Qt5, and is only available if the flag CGAL_USE_BASIC_VIEWER is defined at compile time.
+opens a new window and draws `aps`, an instance of the `CGAL::Point_set_3` class. A call to this function is blocking, that is the program continues as soon as the user closes the window. This function requires CGAL_Qt5, and is only available if the flag CGAL_USE_BASIC_VIEWER is defined at compile time.
 \tparam PS an instance of the `CGAL::Point_set_3` class.
 \param aps the point set to draw.
 
