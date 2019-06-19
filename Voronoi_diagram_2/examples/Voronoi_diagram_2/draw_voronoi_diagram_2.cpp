001fda493be1aeb76cfc628f4bd0497009e947cb
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-06-19 16:30:56 -0700

    Add documentation for voronoi diagram viewer.

--- Voronoi_diagram_2/examples/Voronoi_diagram_2/draw_voronoi_diagram_2.cpp
+++ Voronoi_diagram_2/examples/Voronoi_diagram_2/draw_voronoi_diagram_2.cpp
@@ -1,30 +1,13 @@
-// Copyright(c) 2019  Foundation for Research and Technology-Hellas (Greece).
-// All rights reserved.
-//
-// This file is part of CGAL (www.cgal.org).
-// You can redistribute it and/or modify it under the terms of the GNU
-// General Public License as published by the Free Software Foundation,
-// either version 3 of the License, or (at your option) any later version.
-//
-// Licensees holding a valid commercial license may use this file in
-// accordance with the commercial license agreement provided with the software.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-//
-// $URL$
-// $Id$
-// SPDX-License-Identifier: GPL-3.0+
-//
-// Author(s)     : Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
+// standard includes
+#include <fstream>
 
+// includes for drawing the Voronoi Diagram
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Voronoi_diagram_2.h>
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Delaunay_triangulation_adaptation_traits_2.h>
 #include <CGAL/Delaunay_triangulation_adaptation_policies_2.h>
 #include <CGAL/draw_voronoi_diagram_2.h>
-#include <fstream>
 
 // typedefs for defining the adaptor
 typedef CGAL::Exact_predicates_inexact_constructions_kernel                  K;
