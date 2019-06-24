e661cfb261c240cd1e121eb37d8ff12996f6c06f
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-06-24 16:33:33 -0700

    Update documentation.

--- Voronoi_diagram_2/doc/Voronoi_diagram_2/CGAL/draw_voronoi_diagram_2.h
+++ Voronoi_diagram_2/doc/Voronoi_diagram_2/CGAL/draw_voronoi_diagram_2.h
@@ -3,11 +3,11 @@ namespace CGAL {
 /*!
 \ingroup PkgDrawVoronoiDiagram2
 
-Open a new window and draw `av2`, the `Voronoi_diagram_2` constructed from a Delaunay Graph which is a model of `DelaunayGraph_2` concept.
+Opens a new window and draws `av2`, the `Voronoi_diagram_2` constructed from a Delaunay Graph which is a model of `DelaunayGraph_2` concept.
 The class `Voronoi_diagram_2` provides an adaptor to view a triangulated Delaunay graph as their dual subdivision, the 
-Voronoi diagram. The function is blocking, that is the program continues as soon as the user closes the window. 
+Voronoi diagram. A call to this function is blocking, that is the program continues as soon as the user closes the window. 
  This function requires CGAL_Qt5, and is only available if the flag CGAL_USE_BASIC_VIEWER is defined at compile time.
-\tparam V2 a model of the `Voronoi2` concept.
+\tparam V2 a model of the `AdaptationTraits_2` concept.
 \param av2 the voronoi diagram to draw.
 
 */
