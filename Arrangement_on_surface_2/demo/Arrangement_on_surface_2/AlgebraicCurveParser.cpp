53a7bdc78f4b5d9da1d6e8d5d0c776635bd11675
Saurabh Singh <ssingh@cs.iitr.ac.in>
2019-06-27 22:40:02 +0530

    Add basic code for new Parser

---
+++ Arrangement_on_surface_2/demo/Arrangement_on_surface_2/AlgebraicCurveParser.cpp
@@ -0,0 +1,185 @@
+//
+// Copyright (c) 2012  Tel-Aviv University (Israel).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+// Created by saurabh on 25/6/19.
+//
+
+#include "AlgebraicCurveParser.h"
+
+AlgebraicCurveParser::AlgebraicCurveParser(std::string &expression) : expression(expression) {};
+
+TermsArray AlgebraicCurveParser::extractTerms() {
+
+    TermsArray algebraicTerms;
+
+    //Preprocess the expression: trim and remove spaces
+    if (!this->validateExpression(expression)) return algebraicTerms;
+    this->expression.erase(remove_if(expression.begin(), expression.end(), isspace), this->expression.end());
+
+    long termIndex = 0;
+
+    //iterate through the string
+    for (auto iterator = expression.begin(); iterator != expression.end(); iterator++) {
+        auto next = iterator + 1;
+        if (*next == '+' || *next == '-' || next == expression.end()) {
+            //find the term between the +/- and the previous +/-
+            std::string subExpression = std::string(expression.begin() + termIndex, iterator);
+            AlgebraicTerm term = extractCoefficientAndExponent(subExpression);
+            algebraicTerms.push_back(term);
+            termIndex = iterator - expression.begin();
+        }
+    }
+}
+
+AlgebraicTerm AlgebraicCurveParser::extractCoefficientAndExponent(std::string &subExpression) {
+    AlgebraicTerm algebraicTerm{};
+
+    algebraicTerm.positiveSign = extractSign(subExpression);
+    algebraicTerm.coefficient = extractCoefficient(subExpression);
+    algebraicTerm.xExponent = extractXExponent(subExpression);
+    algebraicTerm.yExponent = extractYExponent(subExpression);
+    return algebraicTerm;
+
+}
+
+int AlgebraicCurveParser::extractXExponent(std::string &subExpression) {
+    bool extractingExponent = false;
+    int exponentValue;
+    std::string exponentString;
+
+    for (auto iterator = subExpression.begin(); iterator != subExpression.end(); iterator++) {
+        if (extractingExponent) {
+            if (*iterator == '^') {
+                continue;
+            } else if (*iterator == 'y' || *iterator == '*') {
+                extractingExponent = false;
+                break;
+            } else {
+                char charAtPosition = *iterator;
+                exponentString.append(charAtPosition, 1);
+            }
+        } else {
+            if (*iterator == 'x') {
+                if (iterator == subExpression.end() - 1) {
+                    return 1;
+                } else if (*(iterator + 1) != '^') {
+                    return 1;
+                } else if (*(iterator + 1) == '^') {
+                    extractingExponent = true;
+                    continue;
+                }
+            }
+        }
+    }
+
+    std::string::size_type sz;
+    exponentValue = std::stoi(exponentString, &sz);
+
+    return exponentValue;
+}
+
+int AlgebraicCurveParser::extractYExponent(std::string &subExpression) {
+    bool extractingExponent = false;
+    int exponentValue;
+    std::string exponentString;
+
+    for (auto iterator = subExpression.begin(); iterator != subExpression.end(); iterator++) {
+        if (extractingExponent) {
+            if (*iterator == '^') {
+                continue;
+            } else if (*iterator == 'x' || *iterator == '*') {
+                extractingExponent = false;
+                break;
+            } else {
+                const char charAtPosition = *iterator;
+                exponentString.append(charAtPosition, 1);
+            }
+        } else {
+            if (*iterator == 'y') {
+                if (iterator == subExpression.end() - 1) {
+                    return 1;
+                } else if (*(iterator + 1) != '^') {
+                    return 1;
+                } else if (*(iterator + 1) == '^') {
+                    extractingExponent = true;
+                    continue;
+                }
+            }
+        }
+    }
+
+    std::string::size_type sz;
+    exponentValue = std::stoi(exponentString, &sz);
+
+    return exponentValue;
+}
+
+int AlgebraicCurveParser::extractCoefficient(std::string &subExpression) {
+    bool signPresent = this->signPresent(subExpression);
+    std::string coefficientString;
+    int coefficientValue;
+
+    std::string::iterator iteratorStart;
+    if (signPresent) iteratorStart = subExpression.begin() + 1;
+    else iteratorStart = subExpression.begin();
+
+    for (auto iterator = iteratorStart; iterator != subExpression.end(); iterator++) {
+        //determine coefficient
+        if (*iterator == 'x' || *iterator == 'y' || *iterator == '*') {
+            break;
+        }
+        const char value = *iterator;
+        coefficientString.append(&value);
+    }
+
+    std::string::size_type sz;
+    coefficientValue = std::stoi(coefficientString, &sz);
+
+    return coefficientValue;
+}
+
+bool AlgebraicCurveParser::extractSign(std::string &subExpression) {
+    bool positiveSign;
+    switch (*subExpression.begin()) {
+        case '+':
+            positiveSign = true;
+            break;
+        case '-':
+            positiveSign = false;
+            break;
+        default:
+            positiveSign = true;
+    }
+    return positiveSign;
+}
+
+bool AlgebraicCurveParser::signPresent(std::string &subExpression) {
+
+    bool signPresent;
+    switch (*subExpression.begin()) {
+        case '+':
+            signPresent = true;
+            break;
+        case '-':
+            signPresent = true;
+            break;
+        default:
+            signPresent = false;
+    }
+    return signPresent;
+}
+
+bool AlgebraicCurveParser::validateExpression(const std::string &expression) {
+    return false;
+}
