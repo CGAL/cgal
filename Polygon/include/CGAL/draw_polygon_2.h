14a59deda6e559017b3b08668a8b16225ad8d6c5
Sébastien Loriot <sebastien.loriot@cgal.org>
2019-06-28 10:03:10 +0200

    lazy doc fix

--- Polygon/include/CGAL/draw_polygon_2.h
+++ Polygon/include/CGAL/draw_polygon_2.h
@@ -28,6 +28,24 @@
 
 #include <CGAL/Qt/Basic_viewer_qt.h>
 
+#ifdef DOXYGEN_RUNNING
+namespace CGAL {
+
+/*!
+\ingroup PkgDrawPolygon2
+
+opens a new window and draws `ap`, an instance of the `CGAL::Polygon_2` class. A call to this function is blocking, that is the program continues as soon as the user closes the window. This function requires CGAL_Qt5, and is only available if the flag CGAL_USE_BASIC_VIEWER is defined at compile time.
+\tparam P an instance of the `CGAL::Polygon_2` class.
+\param ap the polygon to draw.
+
+*/
+template<class P>
+void draw(const P& ap);
+
+} /* namespace CGAL */
+
+#endif
+
 #ifdef CGAL_USE_BASIC_VIEWER
 
 #include <CGAL/Polygon_2.h>
