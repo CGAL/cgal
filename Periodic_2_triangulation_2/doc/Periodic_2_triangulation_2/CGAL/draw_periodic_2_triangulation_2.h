35008aeeea40261931e12c99f67975fbc1713a0e
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-07-03 15:14:57 -0700

    Update draw_periodic_2_triangulation_2.h

--- Periodic_2_triangulation_2/doc/Periodic_2_triangulation_2/CGAL/draw_periodic_2_triangulation_2.h
+++ Periodic_2_triangulation_2/doc/Periodic_2_triangulation_2/CGAL/draw_periodic_2_triangulation_2.h
@@ -3,7 +3,7 @@ namespace CGAL {
 /*!
 \ingroup PkgDrawPeriodic2Triangulation2
 
-Opens a new window and draws `ap2t2`, the `Periodic_2_Triangulation_2`. A call to this function is blocking, that is the program continues as soon as the user closes the window. 
+opens a new window and draws `ap2t2`, the `Periodic_2_Triangulation_2`. A call to this function is blocking, that is the program continues as soon as the user closes the window. 
  This function requires CGAL_Qt5, and is only available if the flag CGAL_USE_BASIC_VIEWER is defined at compile time.
 \tparam P2T2 a model of the `Periodic_2TriangulationTraits_2` concept.
 \param ap2t2 the 2D periodic trinagulation to draw.
