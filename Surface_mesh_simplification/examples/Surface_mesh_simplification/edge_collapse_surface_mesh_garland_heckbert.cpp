1b7c1c341720288a886b3cd797f67962b2fd7beb
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2019-07-03 11:37:23 +0200

    Fix default value so that the comment makes sense

--- Surface_mesh_simplification/examples/Surface_mesh_simplification/edge_collapse_surface_mesh_garland_heckbert.cpp
+++ Surface_mesh_simplification/examples/Surface_mesh_simplification/edge_collapse_surface_mesh_garland_heckbert.cpp
@@ -82,7 +82,7 @@ int main(int argc, char** argv)
 
   // In this example, the simplification stops when the number of undirected edges
   // drops below 10% of the initial count
-  double threshold = (argc > 2) ? std::atof(argv[2]) : 0.01;
+  double threshold = (argc > 2) ? std::atof(argv[2]) : 0.1;
 //  SMS::Count_ratio_stop_predicate<Surface_mesh> stop(threshold);
   SMS::GarlandHeckbert_cost_stop_predicate<double> stop(threshold);
 
