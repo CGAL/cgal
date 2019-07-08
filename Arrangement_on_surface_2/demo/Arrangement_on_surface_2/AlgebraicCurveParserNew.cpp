c4704b6eb57571ac340de6e076c8248cb7004f92
Saurabh Singh <ssingh@cs.iitr.ac.in>
2019-07-08 18:33:48 +0530

    New parser using boost::spirit::qi

---
+++ Arrangement_on_surface_2/demo/Arrangement_on_surface_2/AlgebraicCurveParserNew.cpp
@@ -0,0 +1,76 @@
+//
+// Created by saurabh on 8/7/19.
+//
+
+#include "AlgebraicCurveParserNew.h"
+
+AlgebraicCurveParserNew::AlgebraicCurveParserNew(std::string &expression) : expression(expression) {};
+
+bool AlgebraicCurveParserNew::validateExpression(const std::string &expression) {
+    std::string expressionMutable = expression;
+    for (auto iterator = expressionMutable.begin(); iterator != expressionMutable.end(); iterator++) {
+        if (*iterator == 'x' || *iterator == 'y') continue;
+        if (*iterator == '+' || *iterator == '-' || *iterator == '*' || *iterator == '^') continue;
+        if (isdigit(*iterator) || isspace(*iterator)) continue;
+        else return false;
+    }
+    return true;
+}
+
+Terms AlgebraicCurveParserNew::extractTerms() {
+    Terms algebraicTerms;
+
+    //Preprocess the expression: trim and remove spaces
+    if (!this->validateExpression(expression)) return algebraicTerms;
+    this->expression.erase(remove_if(expression.begin(), expression.end(), isspace), this->expression.end());
+
+    long termIndex = 0;
+
+    //iterate through the string
+    for (auto iterator = expression.begin(); iterator != expression.end(); iterator++) {
+        auto next = iterator + 1;
+        if (*next == '+' || *next == '-' || next == expression.end()) {
+            //find the term between the +/- and the previous +/-
+            std::string subExpression = std::string(expression.begin() + termIndex, next);
+            AlgebraicCurveTerm term;
+            bool result = parseTerm(subExpression.begin(), subExpression.end(), term);
+            algebraicTerms.push_back(term);
+            termIndex = next - expression.begin();
+        }
+    }
+}
+
+template<typename Iterator>
+bool AlgebraicCurveParserNew::parseTerm(Iterator first, Iterator last, AlgebraicCurveTerm &term) {
+    using boost::spirit::qi::long_long;
+    using boost::spirit::qi::phrase_parse;
+    using boost::phoenix::ref;
+    long long xExponent = 0;
+    long long yExponent = 0;
+    long long coefficient = 1;
+    phrase_parse(
+            first,
+            last,
+
+            //Begin parser
+            (
+                    long_long[ref(coefficient)] = boost::spirit::qi::_1 >>
+                                                                        -("x^" >> long_long[ref(
+                                                                                xExponent)] = boost::spirit::qi::_0) >>
+                                                                        -("y^" >> long_long[ref(
+                                                                                yExponent)] = boost::spirit::qi::_0)
+                    |
+                            "x^" >> long_long[ref(
+                                    xExponent)] = boost::spirit::qi::_0 >>
+                                                                         -("y^" >> long_long[ref(
+                                                                                 yExponent)] = boost::spirit::qi::_0)
+                    |
+                            "y^" >> long_long[ref(
+                                    yExponent)] = boost::spirit::qi::_0 >>
+                                                                        -("x^" >> long_long[ref(
+                                                                                xExponent)] = boost::spirit::qi::_0)
+            ),
+            boost::spirit::ascii::space
+    );
+    return false;
+}
---
+++ Arrangement_on_surface_2/demo/Arrangement_on_surface_2/AlgebraicCurveParserNew.h
@@ -0,0 +1,29 @@
+//
+// Created by saurabh on 8/7/19.
+//
+#include <vector>
+#include <string>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#ifndef ARRANGEMENT_ON_SURFACE_2_DEMO_ALGEBRAICCURVEPARSERNEW_H
+#define ARRANGEMENT_ON_SURFACE_2_DEMO_ALGEBRAICCURVEPARSERNEW_H
+
+typedef std::vector<struct AlgebraicCurveTerm> Terms;
+struct AlgebraicCurveTerm {
+    long long xExponent, yExponent, coefficient;
+    bool isPositive;
+};
+class AlgebraicCurveParserNew {
+public:
+    explicit AlgebraicCurveParserNew(std::string& expression);
+    bool validateExpression(const std::string &expression);
+    Terms extractTerms();
+    std::string expression;
+private:
+    template <typename Iterator>
+    bool parseTerm(Iterator first, Iterator last, AlgebraicCurveTerm& term);
+};
+
+
+#endif //ARRANGEMENT_ON_SURFACE_2_DEMO_ALGEBRAICCURVEPARSERNEW_H
