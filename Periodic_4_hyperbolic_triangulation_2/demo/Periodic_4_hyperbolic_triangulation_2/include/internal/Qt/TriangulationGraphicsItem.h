d21709d0bd4c54cb5331218b8daf858e9e430c9d
Maxime Gimeno <maxime.gimeno@gmail.com>
2019-07-10 11:58:13 +0200

    More ->matrix() replaced

--- Periodic_4_hyperbolic_triangulation_2/demo/Periodic_4_hyperbolic_triangulation_2/include/internal/Qt/TriangulationGraphicsItem.h
+++ Periodic_4_hyperbolic_triangulation_2/demo/Periodic_4_hyperbolic_triangulation_2/include/internal/Qt/TriangulationGraphicsItem.h
@@ -365,10 +365,10 @@ TriangulationGraphicsItem<T>::paintVertices(QPainter *painter)
     Converter<Geom_traits> convert;
 
     painter->setPen(verticesPen());
-    QMatrix matrix = painter->matrix();
+    QTransform matrix = painter->worldTransform();
     //QMatrix tr(1, 0, 0, -1, 0, 0);
     //matrix = tr*matrix;
-    painter->resetMatrix();
+    painter->resetTransform();
 
     QPen temp = painter->pen();
     QPen old = temp;
@@ -455,10 +455,10 @@ TriangulationGraphicsItem<T>::paintOneVertex(const typename T::Point& point)
   Converter<Geom_traits> convert;
 
   m_painter->setPen(this->verticesPen());
-  QMatrix matrix = m_painter->matrix();
-  m_painter->resetMatrix();
+  QTransform matrix = m_painter->worldTransform();
+  m_painter->resetTransform();
   m_painter->drawPoint(matrix.map(convert(point)));
-  m_painter->setMatrix(matrix);
+  m_painter->setWorldTransform(matrix);
 }
 
 template <typename T>
@@ -468,10 +468,10 @@ TriangulationGraphicsItem<T>::paintVertex(typename T::Vertex_handle vh)
   Converter<Geom_traits> convert;
   m_painter->setPen(this->verticesPen());
   
-  QMatrix matrix = m_painter->matrix();
-  m_painter->resetMatrix();
+  QTransform matrix = m_painter->worldTransform();
+  m_painter->resetTransform();
   m_painter->drawPoint(matrix.map(convert(vh->point())));
-  m_painter->setMatrix(matrix);
+  m_painter->setWorldTransform(matrix);
 }
 
 template <typename T>
