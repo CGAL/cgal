0332d9f00f02a275cf7378904287126e28e4d1b2
Martin Skrodzki <skrodzki.martin@gmail.com>
2019-07-12 18:55:55 +0200

    Enable reading of real meshes and perturbation of them for distance computation.

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/hausdorff_bounded_error_distance_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/hausdorff_bounded_error_distance_example.cpp
@@ -2,6 +2,7 @@
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/Polygon_mesh_processing/distance.h>
 #include <CGAL/Polygon_mesh_processing/remesh.h>
+#include <CGAL/Polygon_mesh_processing/random_perturbation.h>
 
 #if defined(CGAL_LINKED_WITH_TBB)
 #define TAG CGAL::Parallel_tag
@@ -17,14 +18,11 @@ typedef Mesh::Vertex_index Vertex_index;
 
 namespace PMP = CGAL::Polygon_mesh_processing;
 
-int main()
+int main(int argc, char** argv)
 {
   Mesh tm1, tm2;
-  // TODO Read real meshes and try method with it.
-  //      std::ofstream(argv[1]) >> tm1;
-  //      http://www.pmp-book.org/download/meshes/iphi_fullres.zip
-  //      https://doc.cgal.org/latest/Polygon_mesh_processing/group__PMP__meshing__grp.html#ga028a80dc84395650f67714fa7618ec53
 /*
+  // Easy Example
   CGAL::make_tetrahedron(Point_3(.0,.0,.0),
                          Point_3(2,.0,.0),
                          Point_3(1,1,1),
@@ -33,6 +31,10 @@ int main()
   tm2=tm1;
   CGAL::Polygon_mesh_processing::isotropic_remeshing(tm2.faces(),.05, tm2);
 */
+
+/*
+  // Example with point realizing the Hausdorff distance strictly lying in the
+  // interior of a triangle
   tm1 = Mesh();
   tm1.add_vertex( Point_3(-1.,1.,1.) );
   tm1.add_vertex( Point_3(0.,-1.,1.) );
@@ -53,6 +55,19 @@ int main()
   tm2.add_face( w2, w5, w3 );
 
   std::cout << "TM2 is valid: " << (tm2.is_valid() ? "true" : "false") << std::endl;
+*/
+
+  // Read a real mesh given by the user
+  std::ifstream input( argv[1] );
+  input >> tm1;
+  std::cout << "Read a mesh with " << tm1.number_of_faces() << " triangles." << std::endl;
+  // Copy the mesh and perturb it slightly
+  tm2 = tm1;
+  bool do_project = false;
+  CGAL::Polygon_mesh_processing::random_perturbation( tm2.vertices(), tm2, 0.001, do_project );
+  std::cout << "Perturbed the input mesh, now computing the Hausdorff distance." << std::endl;
+
+//      https://doc.cgal.org/latest/Polygon_mesh_processing/group__PMP__meshing__grp.html#ga028a80dc84395650f67714fa7618ec53
 
   std::cout << "Approximated Hausdorff distance: "
             << CGAL::Polygon_mesh_processing::bounded_error_Hausdorff_distance
