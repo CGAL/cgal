56354bcf8b39fa9adf666b44172a345052f90ce9
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2019-08-02 12:14:25 +0200

    Use approximate sqrt because compute_area_3 does not work for some kernels

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/measure.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/measure.h
@@ -338,9 +338,9 @@ face_area(typename boost::graph_traits<TriangleMesh>::face_descriptor f,
   typedef typename GetGeomTraits<TriangleMesh, CGAL_PMP_NP_CLASS>::type GT;
   GT traits = choose_param(get_param(np, internal_np::geom_traits), GT());
 
-  return traits.compute_area_3_object()(get(vpm, source(hd, tmesh)),
-                                        get(vpm, target(hd, tmesh)),
-                                        get(vpm, target(nhd, tmesh)));
+  return approximate_sqrt(traits.compute_squared_area_3_object()(get(vpm, source(hd, tmesh)),
+                                                                 get(vpm, target(hd, tmesh)),
+                                                                 get(vpm, target(nhd, tmesh))));
 }
 
 template<typename TriangleMesh>
