dc085d19b510ea9cbb5284a93674b9abe9c03255
Saurabh Singh <ssingh@cs.iitr.ac.in>
2019-08-03 11:11:42 +0530

    Changes the painter files

--- Arrangement_on_surface_2/demo/Arrangement_on_surface_2/ArrangementPainterOstream.cpp
+++ Arrangement_on_surface_2/demo/Arrangement_on_surface_2/ArrangementPainterOstream.cpp
@@ -261,6 +261,153 @@ operator<<( const Point_2& p )
   return *this;
 }
 
+// Instantiation of Arr_Bezier_traits_2
+
+template < typename RatKernel, class AlgKernel, class NtTraits >
+ArrangementPainterOstream<CGAL::Arr_Bezier_curve_traits_2<RatKernel, AlgKernel,
+                                                   NtTraits > >&
+ArrangementPainterOstream<CGAL::Arr_Bezier_curve_traits_2<RatKernel, AlgKernel,
+                                                   NtTraits > >::
+operator<<( const X_monotone_curve_2& curve )
+{
+  // std::cout << "In ArrangementPainterOstream& operator curve" << std::endl;
+
+  CGAL::Bbox_2 bb = curve.bbox( );
+  QRectF qbb = this->convert( bb );
+  // quick cull
+
+  // bool is_horizontal = (qbb.top() == qbb.bottom());
+  // bool is_vertical = (qbb.left() == qbb.right());
+
+  if (this->clippingRect.isValid() && ! this->clippingRect.intersects(qbb)) {
+    //std::cout << "quick culled curve" << std::endl;
+    return *this;
+  }
+
+  if ( this->clippingRect.isValid( ) )
+  {
+    std::vector< X_monotone_curve_2 > visibleParts;
+    if ( this->clippingRect.contains( qbb ) )
+    {
+      visibleParts.push_back( curve );
+    }
+    else
+    {
+      visibleParts = this->visibleParts( curve );
+    }
+
+    for ( unsigned int i = 0; i < visibleParts.size( ); ++i )
+    {
+      X_monotone_curve_2 subcurve = visibleParts[ i ];
+      int n;
+      if ( this->scene == NULL )
+      {
+        n = 100; // TODO: get an adaptive approximation
+      }
+      else
+      {
+        QGraphicsView* view = this->scene->views( ).first( );
+        int xmin, xmax;
+        xmin = view->mapFromScene( bb.xmin( ), bb.ymin( ) ).x( );
+        xmax = view->mapFromScene( bb.xmax( ), bb.ymin( ) ).x( );
+        n = xmax - xmin;
+      }
+      if ( n == 0 )
+      {
+        return *this;
+      }
+
+      std::pair<double, double>* app_pts =
+        new std::pair<double, double>[n + 1];
+      std::pair<double, double>* end_pts =
+        subcurve.polyline_approximation(n, app_pts);
+      std::pair<double, double>* p_curr = app_pts;
+      std::pair<double, double>* p_next = p_curr + 1;
+      int count = 0;
+      do
+      {
+        QPointF p1( p_curr->first, p_curr->second );
+        QPointF p2( p_next->first, p_next->second );
+#if 0
+        Segment_2 seg( p1, p2 );
+        this->painterOstream << seg;
+#endif
+        this->qp->drawLine( p1, p2 );
+        p_curr++;
+        p_next++;
+        ++count;
+      }
+      while ( p_next != end_pts );
+    }
+  }
+  else
+  { // draw the whole curve
+    int n;
+    if ( this->scene == NULL )
+    {
+      n = 100; // TODO: get an adaptive approximation
+    }
+    else
+    {
+      QGraphicsView* view = this->scene->views( ).first( );
+      int xmin, xmax;
+      xmin = view->mapFromScene( bb.xmin( ), bb.ymin( ) ).x( );
+      xmax = view->mapFromScene( bb.xmax( ), bb.ymin( ) ).x( );
+      n = xmax - xmin;
+    }
+    if ( n == 0 )
+    {
+      return *this;
+    }
+
+    std::pair<double, double>* app_pts = new std::pair<double, double>[n + 1];
+    std::pair<double, double>* end_pts =
+      curve.polyline_approximation(n, app_pts);
+
+    std::pair<double, double>* p_curr = app_pts;
+    std::pair<double, double>* p_next = p_curr + 1;
+    int count = 0;
+    do
+    {
+      QPointF p1( p_curr->first, p_curr->second );
+      QPointF p2( p_next->first, p_next->second );
+#if 0
+      Segment_2 seg( p1, p2 );
+      this->painterOstream << seg;
+#endif
+      this->qp->drawLine( p1, p2 );
+      p_curr++;
+      p_next++;
+      ++count;
+    }
+    while ( p_next != end_pts );
+    //std::cout << count << " approximation points" << std::endl;
+  }
+  return *this;
+}
+
+template < typename RatKernel, class AlgKernel, class NtTraits >
+ArrangementPainterOstream<CGAL::Arr_Bezier_curve_traits_2<RatKernel, AlgKernel,
+                                                   NtTraits > >&
+ArrangementPainterOstream<CGAL::Arr_Bezier_curve_traits_2<RatKernel, AlgKernel,
+                                                   NtTraits > >::
+operator<<( const Point_2& p )
+  {
+    // std::cout<< "In ArrangementPainterOstream& operator Point_2"<<std::endl;
+
+    QPointF qpt = this->convert( p );
+    QPen savePen = this->qp->pen( );
+    this->qp->setBrush( QBrush( savePen.color( ) ) );
+    double radius = savePen.width( ) / 2.0;
+    radius /= this->scale;
+
+    this->qp->drawEllipse( qpt, radius, radius );
+
+    this->qp->setBrush( QBrush( ) );
+    this->qp->setPen( savePen );
+    return *this;
+  }
+
 // Instantiation of Arr_linear_traits_2
 
 template < typename Kernel_ >
