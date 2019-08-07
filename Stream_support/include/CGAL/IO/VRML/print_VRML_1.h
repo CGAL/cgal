0a38d237646ed4a2543f351d84a4b9f289a4299f
Maxime Gimeno <maxime.gimeno@gmail.com>
2019-08-07 15:12:50 +0200

    Add all missing files. Enable the STL_reader to read multiple "solid"s in a same soup.

---
+++ Stream_support/include/CGAL/IO/VRML/print_VRML_1.h
@@ -0,0 +1,53 @@
+// Copyright (c) 1997  ETH Zurich (Switzerland).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0+
+// 
+//
+// Author(s)     : Lutz Kettner  <kettner@mpi-sb.mpg.de>
+
+#ifndef CGAL_IO_PRINT_VRML_1_H
+#define CGAL_IO_PRINT_VRML_1_H 1
+
+#include <CGAL/license/Polyhedron.h>
+
+
+#include <CGAL/IO/Polyhedron_VRML_1_ostream.h>
+
+namespace CGAL {
+
+template <class Polyhedron>
+void print_polyhedron_VRML_1( std::ostream& out, const Polyhedron& P) {
+    VRML_1_ostream os( out);
+    os << P;
+}
+
+// Deprecated global functions, replaced with functions above
+
+template < class Traits,
+           class Items,
+           template < class T, class I, class A>
+           class HDS, class Alloc>
+void
+print_VRML_1( std::ostream& out, 
+              const Polyhedron_3<Traits,Items,HDS,Alloc>& P) {
+    VRML_1_ostream os( out);
+    os << P;
+}
+
+} //namespace CGAL
+#endif // CGAL_IO_PRINT_VRML_1_H //
+// EOF //
