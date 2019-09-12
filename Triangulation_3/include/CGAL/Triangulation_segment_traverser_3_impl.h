c78d4b66a9a1291b5a6e319cc1a334a48532d0ed
Laurent Rineau <laurent.rineau@cgal.org>
2019-09-12 10:01:20 +0200

    Add SPDX license headers

--- Triangulation_3/include/CGAL/Triangulation_segment_traverser_3_impl.h
+++ Triangulation_3/include/CGAL/Triangulation_segment_traverser_3_impl.h
@@ -1,18 +1,20 @@
 //A class that follows a straight line through a Delaunay triangulation structure.
 //Copyright (C) 2012  Utrecht University
 //
-//This program is free software: you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
+// This file is part of CGAL (www.cgal.org).
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0+
 //
 // Author(s): Thijs van Lankveld
 
