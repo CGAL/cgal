faf55abc7391238559a56bab4ecdd3280afb447a
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2019-10-01 14:35:02 +0200

    Doc improvements

--- Triangulation_2/doc/Triangulation_2/CGAL/Constrained_triangulation_2.h
+++ Triangulation_2/doc/Triangulation_2/CGAL/Constrained_triangulation_2.h
@@ -5,11 +5,11 @@ namespace CGAL {
 /*!
 \ingroup PkgTriangulation2TriangulationClasses
 
+Intersection tag for constrained triangulations, when input constraints do not intersect.
+
 \deprecated This class is deprecated since \cgal 5.1 as it was ambiguous. Users should instead
 use the tags `No_constraint_intersection_tag` and `No_constraint_intersection_requiring_constructions_tag`,
 depending on their needs.
-
-Intersection tag for constrained triangulations, when input constraints do not intersect.
 */
 struct No_intersection_tag{};
 
@@ -26,6 +26,11 @@ struct No_constraint_intersection_tag{};
 
 Intersection tag for constrained triangulations, when input constraints are not allowed to intersect
 except if the intersection does not require any new point construction.
+
+This for example allows configurations such as two segments intersecting in a 'T', or overlapping (and
+even identical) segments.
+
+This is the default tag.
 */
 struct No_constraint_intersection_requiring_constructions_tag{};
 
@@ -100,7 +105,6 @@ as it avoids the cascading of intersection computations.
 \image html constraints.png
 \image latex constraints.png
 
-
 \tparam Traits is a geometric traits class and must be a model 
 of the concept `TriangulationTraits_2`. 
 When intersection of input constraints are supported, 
@@ -110,48 +114,32 @@ to compute the intersection of two segments.
 It has then to be a model of the concept 
 `ConstrainedTriangulationTraits_2`. 
 
+\tparam Tds must be a model of the concept `TriangulationDataStructure_2` or `Default`.
+The information about constrained edges is stored in the faces of the triangulation. Thus the nested `Face`
+type of a constrained triangulation offers additional functionalities to deal with this information.
+These additional functionalities induce additional requirements on the face base class
+plugged into the triangulation data structure of a constrained Delaunay triangulation.
+The face base of a constrained Delaunay triangulation has to be a model of the concept
+`ConstrainedTriangulationFaceBase_2`.
 
-\tparam Tds must be a model 
-of the concept `TriangulationDataStructure_2` or `Default`.
-
-\tparam Itag is the intersection tag 
-which serves to choose between the different 
-strategies to deal with constraints intersections. 
-\cgal provides three valid types for this parameter: 
+\tparam Itag is the intersection tag
+which serves to choose between the different
+strategies to deal with constraints intersections.
+\cgal provides three valid types for this parameter:
 - `No_constraint_intersection_tag` disallows intersections of input constraints
-except for the case of a single common extremity,
-- `No_constraint_intersection_requiring_constructions_tag` disallows intersections of input constraints
-except for configurations where the intersection can be represented without requiring the construction
-of a new point (e.g. overlapping constraints),
-- `Exact_predicates_tag` is to be used when the traits 
-class 
-provides exact predicates but approximate constructions of the 
-intersection points. 
-- `Exact_intersections_tag` is to be used in conjunction 
-with an exact arithmetic type. 
+except for the case of a single common extremity;
+- `No_constraint_intersection_requiring_constructions_tag` (default value) disallows intersections
+of input constraints except for configurations where the intersection can be represented
+without requiring the construction of a new point such as overlapping constraints;
+- `Exact_predicates_tag` is to be used when the traits class
+provides exact predicates but approximate constructions of the
+intersection points;
+- `Exact_intersections_tag` is to be used in conjunction
+with an exact arithmetic type.
 
-The information about constrained edges is stored in the 
-faces of the triangulation. Thus the nested `Face` 
-type of a constrained triangulation offers 
-additional functionalities to deal with this information. 
-These additional functionalities 
-induce additional requirements on the face base class 
-plugged into the triangulation data structure of 
-a constrained Delaunay triangulation. 
-The face base of a constrained Delaunay triangulation 
-has to be a model of the concept 
-`ConstrainedTriangulationFaceBase_2`. 
-
-\cgal provides default instantiations for the template parameters 
-`Tds` and `Itag`, and for the `ConstrainedTriangulationFaceBase_2`. 
-If `Gt` is the geometric traits class 
-parameter, 
-the default for 
-`ConstrainedTriangulationFaceBase_2` is the class 
-`Constrained_triangulation_face_base_2<Gt>` 
-and the default for the 
-triangulation data structure parameter is the class 
-`Triangulation_data_structure_2 < Triangulation_vertex_base_2<Gt>, Constrained_triangulation_face_base_2<Gt> >`. 
+\cgal provides default instantiations for the template parameters `Tds` and `Itag`.
+If `Gt` is the geometric traits class parameter, the default triangulation data structure
+is the class is the class `Triangulation_data_structure_2<Triangulation_vertex_base_2<Gt>, Constrained_triangulation_face_base_2<Gt> >`.
 The default intersection tag is `No_constraint_intersection_requiring_constructions_tag`.
 
 \sa `CGAL::Triangulation_2<Traits,Tds>`
