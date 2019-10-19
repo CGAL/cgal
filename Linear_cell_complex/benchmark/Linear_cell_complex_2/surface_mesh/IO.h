b13e116919035d5f0deac513380e66b308c0c3fd
Sébastien Loriot <sebastien.loriot@cgal.org>
2019-10-19 17:00:08 +0200

    Manual removal of license notice + fix SPDX flags

--- Linear_cell_complex/benchmark/Linear_cell_complex_2/surface_mesh/IO.h
+++ Linear_cell_complex/benchmark/Linear_cell_complex_2/surface_mesh/IO.h
@@ -2,20 +2,7 @@
 // Copyright (C) 2001-2005 by Computer Graphics Group, RWTH Aachen
 // Copyright (C) 2011 by Graphics & Geometry Group, Bielefeld University
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Library General Public License
-// as published by the Free Software Foundation, version 2.
-//
-// This library is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Library General Public License for more details.
-//
-// You should have received a copy of the GNU Library General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//
-// SPDX-License-Identifier: LGPL-2.0-only
+// SPDX-License-Identifier: GPL-2.0-only
 //
 //=============================================================================
 
