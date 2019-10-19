43166732b6f49195427edf0b76b5193fb1debdfc
Sébastien Loriot <sebastien.loriot@cgal.org>
2019-10-19 16:39:32 +0200

    remove license notice from kernel tests

--- Kernel_23/test/Kernel_23/include/CGAL/_test_cls_weighted_point_3.h
+++ Kernel_23/test/Kernel_23/include/CGAL/_test_cls_weighted_point_3.h
@@ -5,13 +5,7 @@
 // Max-Planck-Institute Saarbruecken (Germany),
 // and Tel-Aviv University (Israel).  All rights reserved.
 //
-// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; either version 3 of the License,
-// or (at your option) any later version.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+// This file is part of CGAL (www.cgal.org)
 //
 // $URL$
 // $Id$
