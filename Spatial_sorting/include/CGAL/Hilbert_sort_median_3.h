82009f60fd6e03f44dd5045f60a7d6bff90ff0ca
Andreas Fabri <andreas.fabri@geometryfactory.com>
2019-10-30 15:40:27 +0100

    Spatial_sorting: Parallelize 3D  case

--- Spatial_sorting/include/CGAL/Hilbert_sort_median_3.h
+++ Spatial_sorting/include/CGAL/Hilbert_sort_median_3.h
@@ -17,6 +17,10 @@
 #include <cstddef>
 #include <CGAL/Hilbert_sort_base.h>
 
+#ifdef CGAL_LINKED_WITH_TBB
+#include <tbb/task_group.h>
+#endif
+
 namespace CGAL {
 
 namespace internal {
@@ -83,6 +87,8 @@ template <class K>
 class Hilbert_sort_median_3
 {
 public:
+
+  typedef Hilbert_sort_median_3<K> Self;
     typedef K Kernel;
     typedef typename Kernel::Point_3 Point;
 
@@ -98,8 +104,9 @@ public:
         : _k(k), _limit (limit)
     {}
 
+  
     template <int x, bool upx, bool upy, bool upz, class RandomAccessIterator>
-    void sort (RandomAccessIterator begin, RandomAccessIterator end) const
+    void recursive_sort (RandomAccessIterator begin, RandomAccessIterator end) const
     {
         const int y = (x + 1) % 3, z = (x + 2) % 3;
         if (end - begin <= _limit) return;
@@ -114,16 +121,96 @@ public:
         RandomAccessIterator m5 = internal::hilbert_split (m4, m6, Cmp< z,  upz> (_k));
         RandomAccessIterator m7 = internal::hilbert_split (m6, m8, Cmp< z, !upz> (_k));
 
-        sort<z, upz, upx, upy> (m0, m1);
-        sort<y, upy, upz, upx> (m1, m2);
-        sort<y, upy, upz, upx> (m2, m3);
-        sort<x, upx,!upy,!upz> (m3, m4);
-        sort<x, upx,!upy,!upz> (m4, m5);
-        sort<y,!upy, upz,!upx> (m5, m6);
-        sort<y,!upy, upz,!upx> (m6, m7);
-        sort<z,!upz,!upx, upy> (m7, m8);
+        recursive_sort<z, upz, upx, upy> (m0, m1);
+        recursive_sort<y, upy, upz, upx> (m1, m2);
+        recursive_sort<y, upy, upz, upx> (m2, m3);
+        recursive_sort<x, upx,!upy,!upz> (m3, m4);
+        recursive_sort<x, upx,!upy,!upz> (m4, m5);
+        recursive_sort<y,!upy, upz,!upx> (m5, m6);
+        recursive_sort<y,!upy, upz,!upx> (m6, m7);
+        recursive_sort<z,!upz,!upx, upy> (m7, m8);
     }
 
+  template <int x, bool upx, bool upy, bool upz, class RandomAccessIterator>
+  struct Recursive_sort {
+    const Self& hs;
+    RandomAccessIterator begin,end;
+    
+    Recursive_sort(const Self& hs, RandomAccessIterator begin, RandomAccessIterator end)
+      : hs(hs), begin(begin), end(end)
+    {}
+    
+    void operator()() const
+    {
+      hs.recursive_sort<x,upx,upy,upz>(begin,end);
+    }
+  };
+  
+  template <class RandomAccessIterator, class Comp>
+  struct Hilbert_split
+  {
+    RandomAccessIterator& res;
+    RandomAccessIterator begin, end;
+    const Comp& comp;
+    
+    Hilbert_split(RandomAccessIterator& res, RandomAccessIterator begin, RandomAccessIterator end, const Comp comp)
+      : res(res), begin(begin), end(end), comp(comp)
+    {}
+    
+    void operator()() const
+    {
+      res = internal::hilbert_split(begin, end, comp);
+    }
+  };
+  
+    
+  template <int x, bool upx, bool upy, bool upz, class RandomAccessIterator>
+  void sort (RandomAccessIterator begin, RandomAccessIterator end) const
+  {
+    const int y = (x + 1) % 3, z = (x + 2) % 3;
+    if ((end - begin) <= _limit) return;
+
+    RandomAccessIterator m0 = begin, m8 = end;
+#if defined( CGAL_LINKED_WITH_TBB) && ( ! defined (CGAL_NO_PARALLEL_SPATIAL_SORT) )
+    if((end - begin) > 1024){
+      RandomAccessIterator m1, m2, m3, m4, m5, m6, m7;
+      m4 = internal::hilbert_split(m0, m8, Cmp<x, upx>(_k));
+      {
+        tbb::task_group g;
+        g.run(Hilbert_split<RandomAccessIterator,Cmp<y, upy> >(m2,m0,m4,Cmp<y,  upy>(_k)));
+        g.run(Hilbert_split<RandomAccessIterator,Cmp<y, !upy> >(m6,m4,m8,Cmp<y,  !upy>(_k)));
+        g.wait();
+      }
+      
+      {
+        tbb::task_group g;
+        g.run(Hilbert_split<RandomAccessIterator,Cmp<z, upz> >(m1,m0,m2,Cmp<z,  upz>(_k)));
+        g.run(Hilbert_split<RandomAccessIterator,Cmp<z, !upz> >(m3,m2,m4,Cmp<z, !upz>(_k)));
+        g.run(Hilbert_split<RandomAccessIterator,Cmp<z, upz> >(m5,m4,m6,Cmp<z,  upz>(_k)));
+        g.run(Hilbert_split<RandomAccessIterator,Cmp<z, !upz> >(m7,m6,m8,Cmp<z, !upz>(_k)));
+        g.wait();
+      }
+      
+      {
+        tbb::task_group g;
+        g.run(Recursive_sort<z, upz, upx, upy, RandomAccessIterator>(*this, m0, m1));
+        g.run(Recursive_sort<y, upy, upz, upx, RandomAccessIterator>(*this, m1, m2));
+        g.run(Recursive_sort<y, upy, upz, upx, RandomAccessIterator>(*this, m2, m3));
+        g.run(Recursive_sort<x, upx, !upy, !upz, RandomAccessIterator>(*this, m3, m4));
+        g.run(Recursive_sort<x, upx, !upy, !upz, RandomAccessIterator>(*this, m4, m5));
+        g.run(Recursive_sort<y, !upy, upz, !upx, RandomAccessIterator>(*this, m5, m6));
+        g.run(Recursive_sort<y, !upy, upz, !upx, RandomAccessIterator>(*this, m6, m7));
+        g.run(Recursive_sort<z, !upz, !upx, upy, RandomAccessIterator>(*this, m7, m8));
+        g.wait();
+      }
+    } else {
+      recursive_sort<0, false, false, false>(begin, end);
+    }
+#else
+    recursive_sort<0, false, false, false>(begin, end);
+#endif        
+  }
+  
     template <class RandomAccessIterator>
     void operator() (RandomAccessIterator begin, RandomAccessIterator end) const
     {
