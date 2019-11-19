47c0ce254ed408b71ebba92df1b907ded5f2c266
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2019-11-19 18:40:34 +0100

    Enrich example

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/self_intersections_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/self_intersections_example.cpp
@@ -1,6 +1,7 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Surface_mesh.h>
 
+#include <CGAL/Real_timer.h>
 #include <CGAL/Polygon_mesh_processing/self_intersections.h>
 #include <CGAL/tags.h>
 
@@ -24,14 +25,22 @@ int main(int argc, char* argv[])
     return EXIT_FAILURE;
   }
 
+  CGAL::Real_timer timer;
+  timer.start();
+
+  std::cout << "Using parallel mode? " << std::is_same<CGAL::Parallel_if_available_tag, CGAL::Parallel_tag>::value << std::endl;
+
   bool intersecting = PMP::does_self_intersect<CGAL::Parallel_if_available_tag>(
                         mesh, CGAL::parameters::vertex_point_map(get(CGAL::vertex_point, mesh)));
-
   std::cout << (intersecting ? "There are self-intersections." : "There is no self-intersection.") << std::endl;
 
   std::vector<std::pair<face_descriptor, face_descriptor> > intersected_tris;
-  PMP::self_intersections<CGAL::Parallel_if_available_tag>(faces(mesh), mesh, std::back_inserter(intersected_tris));
+  PMP::self_intersections<CGAL::Sequential_tag>(faces(mesh), mesh, std::back_inserter(intersected_tris));
   std::cout << intersected_tris.size() << " pairs of triangles intersect." << std::endl;
+
+  timer.stop();
+  double t = timer.time();
+  std::cout << "Time: " << t << std::endl;
 
   return EXIT_SUCCESS;
 }
