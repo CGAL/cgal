00b1f51b241e00a52386a5c3b039cf493906f206
Simon Giraudot <simon.giraudot@geometryfactory.com>
2019-11-28 13:31:55 +0100

    Move find_package() to function to avoid polluting global cmake variables

--- Installation/cmake/modules/CGAL_target_use_Boost_IOStreams.cmake
+++ Installation/cmake/modules/CGAL_target_use_Boost_IOStreams.cmake
@@ -3,16 +3,16 @@ if (CGAL_target_use_Boost_IOStreams_included)
 endif()
 set(CGAL_target_use_Boost_IOStreams_included TRUE)
 
-if( WIN32 )
-# to avoid a warning with old cmake 
-  set(_Boost_BZIP2_HEADERS             "boost/iostreams/filter/bzip2.hpp")
-  set(_Boost_ZLIB_HEADERS              "boost/iostreams/filter/zlib.hpp")
-  find_package( Boost OPTIONAL_COMPONENTS bzip2 zlib)
-else()
-  find_package(ZLIB QUIET)
-endif()
-
 function(CGAL_target_use_Boost_IOStreams target)
+
+  if( WIN32 )
+    # to avoid a warning with old cmake 
+    set(_Boost_BZIP2_HEADERS             "boost/iostreams/filter/bzip2.hpp")
+    set(_Boost_ZLIB_HEADERS              "boost/iostreams/filter/zlib.hpp")
+    find_package( Boost OPTIONAL_COMPONENTS bzip2 zlib)
+  else()
+    find_package(ZLIB QUIET)
+  endif()
 
   if(TARGET Boost::iostreams)
     target_link_libraries(${target} PUBLIC Boost::iostreams)
