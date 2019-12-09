da726d1c3c83aec9f9e02a2e09801aaa104aa97c
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2019-12-09 12:27:27 +0100

    Add a basic example

---
+++ Optimal_bounding_box/examples/Optimal_bounding_box/obb_example.cpp
@@ -0,0 +1,29 @@
+#define CGAL_OPTIMAL_BOUNDING_BOX_DEBUG
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh.h>
+
+#include <CGAL/Optimal_bounding_box/optimal_bounding_box.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel    K;
+typedef K::Point_3                                             Point;
+typedef CGAL::Surface_mesh<Point>                              Surface_mesh;
+
+int main(int argc, char** argv)
+{
+  std::ifstream input(argv[1]);
+  Surface_mesh sm;
+  if (!input || !(input >> sm) || sm.is_empty())
+  {
+    std::cerr << argv[1] << " is not a valid off file.\n";
+    return EXIT_FAILURE;
+  }
+
+  // test one
+  Surface_mesh obb_sm;
+  CGAL::optimal_bounding_box(sm, obb_sm);
+
+  std::ofstream("obb.off") << obb_sm;
+
+  return EXIT_SUCCESS;
+}
