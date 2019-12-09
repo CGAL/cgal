ba1c21f3bd9138ff6e11262adfbbf4e3553f4f9b
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2019-12-09 18:48:46 +0100

    Enhance CGAL::Eigen_matrix

--- Solver_interface/include/CGAL/Eigen_matrix.h
+++ Solver_interface/include/CGAL/Eigen_matrix.h
@@ -28,36 +28,64 @@ The class `Eigen_matrix` is a wrapper around `Eigen` matrix type
 \cgalModels `SvdTraits::Matrix` 
 
 \tparam T Number type.
+\tparam D1 Number of rows, or Dynamic
+\tparam D2 Number of columns, or Dynamic
 
 \sa `CGAL::Eigen_vector<T>`
 \sa `CGAL::Eigen_sparse_matrix<T>`
 \sa `CGAL::Eigen_sparse_symmetric_matrix<T>`
 */
-template <class FT>
+template <class FT, int D1 = ::Eigen::Dynamic, int D2 = ::Eigen::Dynamic>
 struct Eigen_matrix
-  : public ::Eigen::Matrix<FT, ::Eigen::Dynamic, ::Eigen::Dynamic>
+  : public ::Eigen::Matrix<FT, D1, D2>
 {
   /// The internal matrix type from \ref thirdpartyEigen "Eigen".
-  typedef ::Eigen::Matrix<FT, ::Eigen::Dynamic, ::Eigen::Dynamic> EigenType;
+  typedef ::Eigen::Matrix<FT, D1, D2> EigenType;
 
-  /// Construct a matrix with `nr` rows and `nc` columns.
+  /// Constructs a null matrix.
+  Eigen_matrix() { }
+
+  /// Constructs an uninitialized matrix with `nr` rows and `nc` columns.
+  /// This is useful for dynamic-size matrices.
+  /// For fixed-size matrices, it is redundant to pass these parameters.
   Eigen_matrix(std::size_t nr, std::size_t nc) : EigenType(nr, nc) { }
 
-  /// Return the matrix number of rows.
+  /// Constructs a matrix from an Eigen matrix.
+  Eigen_matrix(const EigenType& b) : EigenType(b) { }
+
+  /// Returns the matrix number of rows.
   std::size_t number_of_rows() const { return this->rows(); }
-  /// Return the matrix number of columns.
+  /// Returns the matrix number of columns.
   std::size_t number_of_columns() const { return this->cols(); }
 
-  /// Return the value of the matrix at position (i,j).
+  /// Returns the value of the matrix at position (i,j).
   FT operator()( std::size_t i , std::size_t j ) const { return EigenType::operator()(i,j); }
 
-  /// Write access to a matrix coefficient: `a_ij` <- `val`.
+  /// Writes access to a matrix coefficient: `a_ij` <- `val`.
   void set(std::size_t i, std::size_t j, FT value) { this->coeffRef(i,j) = value; }
 
-  /// Return the internal matrix, with type `EigenType`.
+  /// Returns the internal matrix, with type `EigenType`.
   const EigenType& eigen_object() const { return static_cast<const EigenType&>(*this); }
+
+public:
+  /// \cond SKIP_IN_MANUAL
+  friend Eigen_matrix operator*(const FT c, const Eigen_matrix& M)
+  {
+    return Eigen_matrix(c * M.eigen_object());
+  }
+
+  friend Eigen_matrix operator*(const Eigen_matrix& M0, const Eigen_matrix& M1)
+  {
+    return Eigen_matrix(M0.eigen_object() * M1.eigen_object());
+  }
+
+  friend Eigen_matrix operator+(const Eigen_matrix& M0, const Eigen_matrix& M1)
+  {
+    return Eigen_matrix(M0.eigen_object() + M1.eigen_object());
+  }
+  /// \endcond
 };
 
 } //namespace CGAL
 
-#endif // CGAL_EIGEN_MATRIX_H
+#endif // CGAL_SOLVER_INTERFACE_EIGEN_MATRIX_H
