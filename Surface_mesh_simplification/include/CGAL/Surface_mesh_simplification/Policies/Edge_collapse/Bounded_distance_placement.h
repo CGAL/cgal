32e78834eb6d6a41abbda1d4da5753b73ab80bea
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-01-02 15:21:52 +0100

    Clean header

--- Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Bounded_distance_placement.h
+++ Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Bounded_distance_placement.h
@@ -1,19 +1,10 @@
 // Copyright (c) 2019  GeometryFactory (France). All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org).
-// You can redistribute it and/or modify it under the terms of the GNU
-// General Public License as published by the Free Software Foundation,
-// either version 3 of the License, or (at your option) any later version.
-//
-// Licensees holding a valid commercial license may use this file in
-// accordance with the commercial license agreement provided with the software.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
 // $URL$
 // $Id$
-// SPDX-License-Identifier: GPL-3.0+
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 //
 // Author(s)     : Maxime Gimeno
 //
@@ -21,74 +12,68 @@
 #define CGAL_SURFACE_MESH_SIMPLIFICATION_POLICIES_EDGE_COLLAPSE_BOUNDED_DISTANCE_PLACEMENT_H
 
 #include <CGAL/license/Surface_mesh_simplification.h>
+
+#include <CGAL/intersections.h>
+
 #include <boost/optional.hpp>
-#include<CGAL/intersections.h>
 
 namespace CGAL {
-
-namespace Surface_mesh_simplification
-{
+namespace Surface_mesh_simplification {
 
 template<class Placement, class Tree>
 class Bounded_distance_placement
 {
 public:
-
-  typedef typename Placement::TM TM ;
+  typedef typename Placement::TM                          TM;
 
 public:
-
   Bounded_distance_placement(const double dist,
-                              const Tree& tree,
-                             const Placement& placement = Placement() )
-    : mPlacement(placement), tree(tree), threshold_dist(dist)
-  {
-  }
+                             const Tree& tree,
+                             const Placement& placement = Placement())
+    : m_get_placement(placement), m_tree(tree), m_(dist)
+  { }
 
   template <typename Profile>
   boost::optional<typename Profile::Point>
-  operator()( Profile const& aProfile) const
+  operator()(const Profile& profile) const
   {
-    boost::optional<typename Profile::Point> op = mPlacement(aProfile);
-    typedef typename Profile::Point Point;
+    typedef typename Profile::Point                       Point;
     typedef typename CGAL::Kernel_traits< Point >::Kernel Kernel;
-    if(op){
 
+    boost::optional<typename Profile::Point> op = m_get_placement(profile);
+    if(op)
+    {
       CGAL_assertion(!tree.empty());
 
-      const Point* p = boost::get<Point>(&op);
+      const Point& p = *op;
 
       tree.accelerate_distance_queries();
-      Point cp = tree.best_hint(*p).first; // requires accelerate distance query to be called.
-                                    // We could do better by having access to the internal kd-tree
-                                    // and call search_any_point with a fuzzy_sphere.
+      const Point& cp = tree.best_hint(*p).first; // requires accelerate distance query to be called.
 
-      const double sqtd = threshold_dist*threshold_dist;
+      // We could do better by having access to the internal kd-tree
+      // and call search_any_point with a fuzzy_sphere.
+      const double sqtd = CGAL::square(m_threshold_dist);
 
       // if no input vertex is closer than the threshold, then
       // any face closer than the threshold is intersected by
       // the sphere (avoid the inclusion of the mesh into the threshold sphere)
-      if( CGAL::compare_squared_distance(*p,cp, sqtd)!=LARGER ||
-          tree.do_intersect(CGAL::Sphere_3<Kernel>(*p, sqtd)))
-      {
+      if(CGAL::compare_squared_distance(p, cp, sqtd) != LARGER ||
+         tree.do_intersect(CGAL::Sphere_3<Kernel>(p, sqtd)))
         return op;
-      }
-      return boost::none;
+
+      return boost::optional<Point>();
     }
+
     return op;
   }
 
-
 private:
-
-  Placement  mPlacement ;
-  const Tree& tree;
-  double threshold_dist;
-
+  const Placement m_get_placement;
+  const Tree& m_tree;
+  const double m_threshold_dist;
 };
-}
-}
+
+} // namespace Surface_mesh_simplification
+} // namespace CGAL
 
 #endif // CGAL_SURFACE_MESH_SIMPLIFICATION_POLICIES_EDGE_COLLAPSE_BOUNDED_DISTANCE_PLACEMENT_H
-
-
