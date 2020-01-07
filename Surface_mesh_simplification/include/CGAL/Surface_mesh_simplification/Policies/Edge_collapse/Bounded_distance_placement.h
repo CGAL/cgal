46e7a8f930f34b79bbb086a72183ffcb54434d78
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-01-07 13:54:45 +0100

    Make it optional to pass the AABB tree

--- Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Bounded_distance_placement.h
+++ Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Bounded_distance_placement.h
@@ -6,49 +6,105 @@
 // $Id$
 // SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 //
-// Author(s)     : Maxime Gimeno
+// Author(s)     : Maxime Gimeno,
+//                 Mael Rouxel-Labbé
 //
 #ifndef CGAL_SURFACE_MESH_SIMPLIFICATION_POLICIES_EDGE_COLLAPSE_BOUNDED_DISTANCE_PLACEMENT_H
 #define CGAL_SURFACE_MESH_SIMPLIFICATION_POLICIES_EDGE_COLLAPSE_BOUNDED_DISTANCE_PLACEMENT_H
 
 #include <CGAL/license/Surface_mesh_simplification.h>
 
+#include <CGAL/AABB_tree.h>
+#include <CGAL/AABB_traits.h>
+#include <CGAL/AABB_triangle_primitive.h>
+#include <CGAL/assertions.h>
+#include <CGAL/Default.h>
 #include <CGAL/intersections.h>
+#include <CGAL/boost/graph/named_params_helper.h>
 
 #include <boost/optional.hpp>
 
 namespace CGAL {
 namespace Surface_mesh_simplification {
 
-template<class Placement, class Tree>
+// An AABB tree can also be passed to the placement instead, see undocumented specialization below
+template<typename BasePlacement, typename GeomTraits>
 class Bounded_distance_placement
 {
-public:
-  typedef typename Placement::TM                          TM;
+  typedef typename GeomTraits::Triangle_3                                     Triangle;
+  typedef std::vector<Triangle>                                               Triangle_container;
+  typedef typename Triangle_container::iterator                               TC_iterator;
+  typedef CGAL::AABB_triangle_primitive<GeomTraits, TC_iterator>              Primitive;
+  typedef CGAL::AABB_traits<GeomTraits, Primitive>                            Traits;
+  typedef CGAL::AABB_tree<Traits>                                             AABB_tree;
+
+private:
+  template <typename Profile>
+  void initialize_tree(const Profile& profile) const
+  {
+    typedef typename Profile::Geom_traits                                     Geom_traits;
+    typedef typename Geom_traits::Triangle_3                                  Triangle;
+
+    typedef typename Profile::Triangle_mesh                                   Triangle_mesh;
+    typedef typename boost::graph_traits<Triangle_mesh>::halfedge_descriptor  halfedge_descriptor;
+    typedef typename boost::graph_traits<Triangle_mesh>::face_descriptor      face_descriptor;
+
+    CGAL_precondition(m_tree_ptr == nullptr);
+
+    const Triangle_mesh& tm = profile.surface_mesh();
+    const Geom_traits& gt = profile.geom_traits();
+    std::vector<Triangle> input_triangles;
+
+    for(face_descriptor f : faces(profile.surface_mesh()))
+    {
+      halfedge_descriptor h = halfedge(f, tm);
+      CGAL_assertion(!is_border(h, tm));
+
+      input_triangles.push_back(gt.construct_triangle_3_object()(
+                                  get(profile.vertex_point_map(), source(h, tm)),
+                                  get(profile.vertex_point_map(), target(h, tm)),
+                                  get(profile.vertex_point_map(), target(next(h, tm), tm))));
+    }
+
+    m_tree_ptr = new AABB_tree(input_triangles.begin(), input_triangles.end());
+    const_cast<AABB_tree*>(m_tree_ptr)->build();
+  }
 
 public:
   Bounded_distance_placement(const double dist,
-                             const Tree& tree,
-                             const Placement& placement = Placement())
-    : m_get_placement(placement), m_tree(tree), m_sq_threshold_dist(CGAL::square(dist))
+                             const BasePlacement& placement = BasePlacement())
+    :
+      m_sq_threshold_dist(CGAL::square(dist)),
+      m_tree_ptr(nullptr),
+      m_base_placement(placement)
   { }
+
+  ~Bounded_distance_placement()
+  {
+    if(m_tree_ptr != nullptr)
+      delete m_tree_ptr;
+  }
 
   template <typename Profile>
   boost::optional<typename Profile::Point>
   operator()(const Profile& profile) const
   {
-    typedef typename Profile::Point                       Point;
-    typedef typename CGAL::Kernel_traits< Point >::Kernel Kernel;
+    typedef typename Profile::Geom_traits                                     Geom_traits;
+    typedef typename Profile::Point                                           Point;
 
-    boost::optional<typename Profile::Point> op = m_get_placement(profile);
+    boost::optional<typename Profile::Point> op = m_base_placement(profile);
     if(op)
     {
-      CGAL_assertion(!m_tree.empty());
+      if(m_tree_ptr == nullptr)
+        initialize_tree(profile);
+
+      CGAL_assertion(m_tree_ptr != nullptr);
+      CGAL_assertion(!m_tree_ptr->empty());
 
       const Point& p = *op;
 
-      m_tree.accelerate_distance_queries();
-      const Point& cp = m_tree.best_hint(p).first; // requires accelerate distance query to be called.
+      m_tree_ptr->accelerate_distance_queries();
+      const Point& cp = m_tree_ptr->best_hint(p).first; // requires accelerate distance query to be called.
 
       // We could do better by having access to the internal kd-tree
       // and call search_any_point with a fuzzy_sphere.
@@ -57,7 +113,7 @@ public:
       // any face closer than the threshold is intersected by
       // the sphere (avoid the inclusion of the mesh into the threshold sphere)
       if(CGAL::compare_squared_distance(p, cp, m_sq_threshold_dist) != LARGER ||
-         m_tree.do_intersect(CGAL::Sphere_3<Kernel>(p, m_sq_threshold_dist)))
+         m_tree_ptr->do_intersect(CGAL::Sphere_3<Geom_traits>(p, m_sq_threshold_dist)))
         return op;
 
       return boost::optional<Point>();
@@ -67,9 +123,61 @@ public:
   }
 
 private:
-  const Placement m_get_placement;
-  const Tree& m_tree;
   const double m_sq_threshold_dist;
+  mutable const AABB_tree* m_tree_ptr;
+
+  const BasePlacement m_base_placement;
+};
+
+// Undocumented specizalization where an _already built_ AABB tree is passed
+template<typename BasePlacement, typename AABBTraits>
+class Bounded_distance_placement<BasePlacement, CGAL::AABB_tree<AABBTraits> >
+{
+  typedef CGAL::AABB_tree<AABBTraits>                                       AABB_tree;
+
+public:
+  Bounded_distance_placement(const double dist,
+                             const AABB_tree& tree,
+                             const BasePlacement& placement = BasePlacement())
+    :
+      m_sq_threshold_dist(CGAL::square(dist)),
+      m_tree_ptr(&tree),
+      m_base_placement(placement)
+  { }
+
+  template <typename Profile>
+  boost::optional<typename Profile::Point>
+  operator()(const Profile& profile) const
+  {
+    typedef typename Profile::Geom_traits                                     Geom_traits;
+    typedef typename Profile::Point                                           Point;
+
+    boost::optional<typename Profile::Point> op = m_base_placement(profile);
+    if(op)
+    {
+      CGAL_assertion(m_tree_ptr != nullptr);
+      CGAL_assertion(!m_tree_ptr->empty());
+
+      const Point& p = *op;
+
+      m_tree_ptr->accelerate_distance_queries();
+      const Point& cp = m_tree_ptr->best_hint(p).first; // requires accelerate distance query to be called.
+
+      if(CGAL::compare_squared_distance(p, cp, m_sq_threshold_dist) != LARGER ||
+         m_tree_ptr->do_intersect(CGAL::Sphere_3<Geom_traits>(p, m_sq_threshold_dist)))
+        return op;
+
+      return boost::optional<Point>();
+    }
+
+    return op;
+  }
+
+private:
+  const double m_sq_threshold_dist;
+  mutable const AABB_tree* m_tree_ptr;
+
+  const BasePlacement m_base_placement;
 };
 
 } // namespace Surface_mesh_simplification
