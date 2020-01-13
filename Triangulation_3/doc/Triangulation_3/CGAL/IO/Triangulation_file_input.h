796402859032673b31f5b6eb8bb8abf142c7826b
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-01-13 15:48:38 +0100

    More doc

--- Triangulation_3/doc/Triangulation_3/CGAL/IO/Triangulation_file_input.h
+++ Triangulation_3/doc/Triangulation_3/CGAL/IO/Triangulation_file_input.h
@@ -1,30 +1,33 @@
 namespace CGAL {
 
 /*!
-The triangulation streamed in `is` is written into `tr`. As the vertex and cell
+\ingroup PkgIOTriangulation3
+
+The triangulation streamed in `is`, of original type `Tr_src`,  is written into `tr`, of type `Tr_tgt`. As the vertex and cell
  types might be different and incompatible, the creation of new cells and vertices 
 is made thanks to the functors `convert_vertex` and `convert_cell`, that convert 
 vertex and cell types. For each vertex `v_src` in `in`, the corresponding 
 vertex `v_tgt` in `tr` is a copy of the vertex returned by `convert_vertex(v_src)`. 
 The same operations are done for cells with the functor convert_cell, except cells
-int tr are created using the default constructor, and then filled with the data
+in `tr` are created using the default constructor, and then filled with the data
 contained in the stream.
 
 If `v != Tr_src::Vertex_handle()`, a handle to the vertex created in `tr` 
 that is the copy of `v` is returned, otherwise `Tr_tgt::Vertex_handle()` is returned.
 
- - A model of `ConvertVertex` must provide two operator()'s that are responsible
+ - A model of `ConvertVertex` must provide two `operator()`s that are responsible
  for converting the source vertex `v_src` into the target vertex:
   - `Tr_tgt::Vertex operator()(const Tr_src::Vertex& v_src) const;` This operator is 
 used to create the vertex from `v_src`.
   - `void operator()(const Tr_src::Vertex& v_src, Tr_tgt::Vertex& v_tgt) const;` This
  operator is meant to be used in case heavy data should be transferred to `v_tgt`.
- - A model of ConvertCell must provide an operator()'s that are responsible for 
+ - A model of ConvertCell must provide an `operator()` that is responsible for 
 converting the source cell `c_src` into the target cell:
   - `void operator()(const Tr_src::Cell& c_src, Tr_tgt::Cell& c_tgt) const;` This operator
  is meant to be used in case heavy data should be transferred to `c_tgt`.
 
-\pre The optional argument `v` is a vertex of `tr_src` or is `Vertex_handle()`.
+\note It is required to know the type `Tr_src`, or one that is compatible with it, to call this function.
+\note The triangulation contained in `is` can be obtained with the `operator>>` of a `Triangulation_3`.
 */
 template <typename Tr_src, 
           typename Tr_tgt,
--- Triangulation_3/doc/Triangulation_3/CGAL/Triangulation_3.h
+++ Triangulation_3/doc/Triangulation_3/CGAL/Triangulation_3.h
@@ -1553,8 +1553,42 @@ istream& operator>> (istream& is, Triangulation_3 &t);
 Writes the triangulation `t` into `os`. 
 */ 
 ostream& operator<< (ostream& os, const Triangulation_3 &t); 
-
 /// @}
+/// 
+/*!
+\ingroup PkgIOTriangulation3
+The triangulation streamed in `is`, of original type `Tr_src`, is written into the triangulation. As the vertex and cell
+ types might be different and incompatible, the creation of new cells and vertices 
+is made thanks to the functors `convert_vertex` and `convert_cell`, that convert 
+vertex and cell types. For each vertex `v_src` in `in`, the corresponding 
+vertex `v_tgt` in the triangulation is a copy of the vertex returned by `convert_vertex(v_src)`. 
+The same operations are done for cells with the functor convert_cell, except cells
+in the triangulation are created using the default constructor, and then filled with the data
+contained in the stream.
+
+If `v != Tr_src::Vertex_handle()`, a handle to the vertex created in `tr` 
+that is the copy of `v` is returned, otherwise `Vertex_handle()` is returned.
+
+ - A model of `ConvertVertex` must provide two `operator()`s that are responsible
+ for converting the source vertex `v_src` into the target vertex:
+  - `Vertex operator()(const Tr_src::Vertex& v_src) const;` This operator is 
+used to create the vertex from `v_src`.
+  - `void operator()(const Tr_src::Vertex& v_src, Vertex& v_tgt) const;` This
+ operator is meant to be used in case heavy data should be transferred to `v_tgt`.
+ - A model of ConvertCell must provide an operator() that is responsible for 
+converting the source cell `c_src` into the target cell:
+  - `void operator()(const Tr_src::Cell& c_src, Cell& c_tgt) const;` This operator
+ is meant to be used in case heavy data should be transferred to `c_tgt`.
+
+\note It is required to know the type `Tr_src`, or one that is compatible with it, to call this function.
+\note The triangulation contained in `is` can be obtained with the `operator>>` of a `Triangulation_3`.
+*/
+template <typename Tr_src, 
+          typename ConvertVertex,
+          typename ConvertCell>
+std::istream& file_input(std::istream& is,
+                         ConvertVertex convert_vertex = ConvertVertex(),
+                         ConvertCell convert_cell = ConvertCell());
 
 /// \name Concurrency 
 /// @{
