06c66599817e5b9347823f4cc899e8ec3a816a9c
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-01-17 17:02:14 +0100

    Add STL.h as a convenience header regrouping STL I/O headers

---
+++ Stream_support/include/CGAL/IO/STL.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2018  GeometryFactory Sarl (France).
+// All rights reserved.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is part of CGAL (www.cgal.org)
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : Maxime Gimeno
+
+#ifndef CGAL_IO_STL_H
+#define CGAL_IO_STL_H
+
+#include <CGAL/IO/STL/STL_reader.h>
+#include <CGAL/IO/STL/STL_writer.h>
+
+#endif // CGAL_IO_STL_H
