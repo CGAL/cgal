6f10eb15997dfcd4c355239719ee957518d8e56e
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-01-17 17:00:09 +0100

    Regroup 3MF

---
+++ Stream_support/include/CGAL/IO/3MF.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2015 GeometryFactory
+//
+// This file is part of CGAL (www.cgal.org);
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : Maxime Gimeno
+
+#ifndef CGAL_IO_3MF_H
+#define CGAL_IO_3MF_H
+
+#include <CGAL/IO/3MF/read_3mf.h>
+#include <CGAL/IO/3MF/write_3mf.h>
+
+#endif // CGAL_IO_3MF_H
