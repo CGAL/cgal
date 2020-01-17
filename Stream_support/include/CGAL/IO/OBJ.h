93968e685eb9c1e1633595167cd822be4fb89282
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-01-17 17:10:18 +0100

    Add a convenience header for OBJ I/O headers

---
+++ Stream_support/include/CGAL/IO/OBJ.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2015  Geometry Factory
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org)
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s) : Lutz Kettner
+//             Andreas Fabri
+//             Maxime Gimeno
+
+#ifndef CGAL_IO_OBJ_H
+#define CGAL_IO_OBJ_H
+
+#include <CGAL/IO/OBJ/File_writer_wavefront.h>
+#include <CGAL/IO/OBJ/OBJ_reader.h>
+
+#endif // CGAL_IO_OBJ_H
