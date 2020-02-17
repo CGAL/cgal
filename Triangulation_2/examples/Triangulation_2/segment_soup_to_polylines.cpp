b546a7078fd04de9ef3e0a9c6d0e467584b4196e
Simon Giraudot <simon.giraudot@geometryfactory.com>
2020-02-17 13:59:26 +0100

    Improve example

--- Triangulation_2/examples/Triangulation_2/segment_soup_to_polylines.cpp
+++ Triangulation_2/examples/Triangulation_2/segment_soup_to_polylines.cpp
@@ -34,17 +34,21 @@ int main()
   cdtp.insert_constraint(Point(100,100), Point(200,200));
 
   // Segment soup of 8 segments as input
-  std::cout << "Input CDT+ has "
-            << std::distance (cdtp.constraints_begin(), cdtp.constraints_end())
-            << " constraints/subconstraints" << std::endl;
+  std::cout << "Input CDT+ has " << cdtp.number_of_constraints() << " constraints/subconstraints" << std::endl;
 
   std::cout << "Splitting subconstraints graph into constraints" << std::endl;
   cdtp.split_subconstraint_graph_into_constraints();
 
   // 5 polylines as output
-  std::cout << "Output CDT+ has "
-            << std::distance (cdtp.constraints_begin(), cdtp.constraints_end())
-            << " constraints" << std::endl;
+  std::cout << "Output CDT+ has " << cdtp.number_of_constraints() << " constraints:" << std::endl;
+
+  for (CDTP::Constraint_id cid : cdtp.constraints())
+  {
+    std::cout << " *";
+    for (CDTP::Vertex_handle vh : cdtp.vertices_in_constraint(cid))
+      std::cout << " (" << vh->point() << ")";
+    std::cout << std::endl;
+  }
 
   return EXIT_SUCCESS;
 }
