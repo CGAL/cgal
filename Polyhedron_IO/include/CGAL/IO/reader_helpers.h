3bbc07ebf5dd8d3dc0db265b274421c89511a6b6
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-02-20 11:15:26 +0100

    Regroup some helpers to call reserve/resize on containers that do not offer it
    Useful because sometimes models of the range concepts we demand
    do not offer reserve or resize, yet we still want to call it
    when it's available.

--- Polyhedron_IO/include/CGAL/IO/reader_helpers.h
+++ Polyhedron_IO/include/CGAL/IO/reader_helpers.h
@@ -13,47 +13,10 @@
 
 #include <CGAL/array.h>
 #include <CGAL/assertions.h>
-#include <CGAL/Has_member.h>
-
-#include <boost/mpl/logical.hpp>
-#include <boost/utility/enable_if.hpp>
 
 namespace CGAL{
-
 namespace IO {
-
 namespace internal {
-
-CGAL_GENERATE_MEMBER_DETECTOR(size);
-CGAL_GENERATE_MEMBER_DETECTOR(resize);
-
-// Typical container
-template <class Container>
-void resize(Container& c, std::size_t size,
-            typename boost::enable_if_c<has_resize<Container>::value>::type* = nullptr)
-{
-  c.resize(size);
-}
-
-// Container without a resize() function, but with a size() function (e.g. an array)
-template <class Container>
-void resize(Container& CGAL_assertion_code(array), std::size_t CGAL_assertion_code(size),
-            typename boost::enable_if<
-              boost::mpl::and_<
-                boost::mpl::not_<has_resize<Container> >,
-                                 has_size<Container> > >::type* = nullptr)
-{
-  CGAL_assertion(array.size() == size);
-}
-
-// A class with neither resize() nor size(), can't enforce size (it better be correct!)
-template <class Container>
-void resize(Container&, std::size_t,
-            typename boost::disable_if<
-              boost::mpl::or_<has_resize<Container>,
-                              has_size<Container> > >::type* = nullptr)
-{
-}
 
 // Ideally this should be a std::is_constructible(double, double, double) but boost::is_constructible
 // is not safe to use without CXX11
@@ -73,9 +36,7 @@ void fill_point(const double x, const double y, const double z, Point_3& pt)
 }
 
 } // end namespace internal
-
 } // end namespace IO
-
 } // namespace CGAL
 
 #endif // CGAL_IO_READER_HELPERS_H
