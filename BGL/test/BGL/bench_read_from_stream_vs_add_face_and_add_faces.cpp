9bfc0e8170c8e603a9363ad98f974eca40d96dde
Sébastien Loriot <sebastien.loriot@cgal.org>
2020-03-04 10:57:34 +0100

    add benchmark + hack of read_OFF

---
+++ BGL/test/BGL/bench_read_from_stream_vs_add_face_and_add_faces.cpp
@@ -0,0 +1,74 @@
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Surface_mesh.h>
+#include <iostream>
+#include <fstream>
+#include <CGAL/IO/OFF_reader.h>
+#include <CGAL/boost/graph/Euler_operations.h>
+
+#include <CGAL/Real_timer.h>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef CGAL::Surface_mesh<Kernel::Point_3> Mesh;
+
+int main(int, char** argv)
+{
+  {
+  std::cout << "Reading from stream\n";
+  Mesh m;
+  CGAL::Real_timer timer;
+  timer.start();
+  std::ifstream(argv[1]) >> m;
+  timer.stop();
+  std::cout << "  is_valid? " << CGAL::is_valid_polygon_mesh(m) << "\n";
+  std::cout << "Total time: " << timer.time() << std::endl << std::endl;
+  }
+////////////////////////////////
+  {
+  std::cout << "Reading from soup + iterative add_face\n";
+  Mesh m;
+  CGAL::Real_timer timer;
+  timer.start();
+  std::ifstream in(argv[1]);
+  std::vector<Kernel::Point_3> points;
+  std::vector<std::array<Mesh::Vertex_index, 3> > triangles;
+  CGAL::read_OFF(in, points, triangles);
+  std::cout << "  Read soup: " << timer.time() << std::endl;
+  m.reserve(points.size(), 3*triangles.size()/2, triangles.size());
+  for (const Kernel::Point_3& pt : points)
+    m.add_vertex(pt);
+  CGAL::Real_timer subtimer;
+  subtimer.start();
+  for (const std::array<Mesh::Vertex_index, 3>& t : triangles)
+    CGAL::Euler::add_face(t, m);
+  subtimer.stop();
+  timer.stop();
+  std::cout << "  is_valid? " << CGAL::is_valid_polygon_mesh(m) << "\n";
+  std::cout << "  time for iterative add_face: " << subtimer.time() << std::endl;
+  std::cout << "Total time: " << timer.time() << std::endl << std::endl;
+  }
+////////////////////////////////
+  {
+  std::cout << "Reading from soup + add_faces\n";
+  Mesh m;
+  CGAL::Real_timer timer;
+  timer.start();
+  std::ifstream in(argv[1]);
+  std::vector<Kernel::Point_3> points;
+  std::vector<std::array<Mesh::Vertex_index, 3> > triangles;
+  CGAL::read_OFF(in, points, triangles);
+  std::cout << "  Read soup: " << timer.time() << std::endl;
+  m.reserve(points.size(), 3*triangles.size()/2, triangles.size());
+  for (const Kernel::Point_3& pt : points)
+    m.add_vertex(pt);
+  CGAL::Real_timer subtimer;
+  subtimer.start();
+  CGAL::Euler::add_faces(triangles, m);
+  subtimer.stop();
+  timer.stop();
+  std::cout << "  is_valid? " << CGAL::is_valid_polygon_mesh(m) << "\n";
+  std::cout << "  time for add_faces: " << subtimer.time() << std::endl;
+  std::cout << "Total time: " << timer.time() << std::endl;
+  }
+
+  return 0;
+}
