461e3fab00538649b23e10ff54ca3fb8703c6706
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-03-04 12:57:52 +0100

    Improve index pmap tests / Add tests with the new get_initiliazed_ipmap()

--- BGL/test/BGL/test_Properties.cpp
+++ BGL/test/BGL/test_Properties.cpp
@@ -2,16 +2,17 @@
 
 #include <boost/unordered_set.hpp>
 
-template< typename G,
-          typename ForwardRange,
-          typename IndexPropertyMap
-          >
-void index_uniqueness(const G&, 
+template<typename G,
+         typename ForwardRange,
+         typename IndexPropertyMap>
+void index_uniqueness(const G&,
                       ForwardRange range,
                       IndexPropertyMap pm)
 {
+  std::cout << "index_uniqueness" << std::endl;
+
   typename boost::range_iterator<ForwardRange>::type 
-    begin = boost::begin(range), 
+    begin = boost::begin(range),
     begin2 = boost::begin(range),
     end = boost::end(range);
 
@@ -28,13 +29,29 @@ void index_uniqueness(const G&,
   assert(std::distance(begin2, end) == static_cast<std::ptrdiff_t>(m.size()));
 }
 
+template <typename Graph>
+void index_uniqueness(const Graph& g)
+{
+  std::cout << "Graph has:" << std::endl
+            << "\t" << num_vertices(g) << " vertices" << std::endl
+            << "\t" << num_halfedges(g) << " halfedges" << std::endl
+            << "\t" << num_edges(g) << " edges" << std::endl
+            << "\t" << num_faces(g) << " faces" << std::endl;
+
+  index_uniqueness(g, vertices(g),  get(boost::vertex_index, g));
+  index_uniqueness(g, halfedges(g), get(boost::halfedge_index, g));
+  index_uniqueness(g, edges(g) ,    get(boost::edge_index, g));
+  index_uniqueness(g, faces(g),     get(boost::face_index, g));
+
+  index_uniqueness(g, vertices(g),  CGAL::get_initialized_vertex_index_map(g));
+  index_uniqueness(g, halfedges(g), CGAL::get_initialized_halfedge_index_map(g));
+  index_uniqueness(g, edges(g) ,    CGAL::get_initialized_edge_index_map(g));
+  index_uniqueness(g, faces(g),     CGAL::get_initialized_face_index_map(g));
+}
 
 void index_uniqueness_poly(const Polyhedron& g)
 {
-  index_uniqueness(g, edges(g) ,    get(boost::edge_index, g));
-  index_uniqueness(g, vertices(g),  get(boost::vertex_index, g));
-  index_uniqueness(g, faces(g),     get(boost::face_index, g));
-  index_uniqueness(g, halfedges(g), get(boost::halfedge_index, g));
+  index_uniqueness(g);
 
   index_uniqueness(g, edges(g) ,    get(boost::edge_external_index, g));
   index_uniqueness(g, vertices(g),  get(boost::vertex_external_index, g));
@@ -42,73 +59,39 @@ void index_uniqueness_poly(const Polyhedron& g)
   index_uniqueness(g, halfedges(g), get(boost::halfedge_external_index, g));
 }
 
-void index_uniqueness_lcc(const LCC& g)
-{
-  index_uniqueness(g, edges(g) ,    get(boost::edge_index, g));
-  index_uniqueness(g, vertices(g),  get(boost::vertex_index, g));
-  index_uniqueness(g, faces(g),     get(boost::face_index, g));
-  index_uniqueness(g, halfedges(g), get(boost::halfedge_index, g));
-}
-
-void index_uniqueness_sm(const SM& g)
-{
-  index_uniqueness(g, edges(g) ,    get(boost::edge_index, g));
-  index_uniqueness(g, vertices(g),  get(boost::vertex_index, g));
-  index_uniqueness(g, faces(g),     get(boost::face_index, g));
-  index_uniqueness(g, halfedges(g), get(boost::halfedge_index, g));
-}
-
-#if defined(CGAL_USE_OPENMESH)
-void index_uniqueness_omesh(const OMesh& g)
-{
-  index_uniqueness(g, edges(g) ,    get(boost::edge_index, g));
-  index_uniqueness(g, vertices(g),  get(boost::vertex_index, g));
-  index_uniqueness(g, faces(g),     get(boost::face_index, g));
-  index_uniqueness(g, halfedges(g), get(boost::halfedge_index, g));
-}
-#endif
-
-template <typename Triangulation>
-void index_uniqueness_tr(const Triangulation& g)
-{
-  index_uniqueness(g, edges(g) ,    get(boost::edge_index, g));
-  index_uniqueness(g, vertices(g),  get(boost::vertex_index, g));
-  index_uniqueness(g, faces(g),     get(boost::face_index, g));
-  index_uniqueness(g, halfedges(g), get(boost::halfedge_index, g));
-}
-
-int main()
+// @todo test non-const
+int main(int, char**)
 {
   std::cout << "testing Polyhedron\n";
-  std::vector<Polyhedron> polys = poly_data();
-  for(Polyhedron p : polys)
+  std::vector<Polyhedron> polys = poly_data(); // @tmp
+  for(const Polyhedron& p : polys)
     index_uniqueness_poly(p);
 
   std::cout << "testing Linear_cell_complex\n";
   std::vector<LCC> lccs = lcc_data();
-  for(LCC p : lccs)
-    index_uniqueness_lcc(p);
+  for(const LCC& p : lccs)
+    index_uniqueness(p);
 
   std::cout << "testing Surface_mesh\n";
   std::vector<SM> sms = sm_data();
-  for(SM p : sms)
-    index_uniqueness_sm(p);
+  for(const SM& p : sms)
+    index_uniqueness(p);
 
 #if defined(CGAL_USE_OPENMESH)
   std::cout << "testing OpenMesh\n";
   std::vector<OMesh> omeshs = omesh_data();
-  for(OMesh p : omeshs)
-    index_uniqueness_omesh(p);
+  for(const OMesh& p : omeshs)
+    index_uniqueness(p);
 #endif
 
   std::cout << "testing Triangulations\n";
-  index_uniqueness_tr(t2_data());
-  index_uniqueness_tr(dt2_data());
-  index_uniqueness_tr(rt2_data());
-  index_uniqueness_tr(ct2_data());
-  index_uniqueness_tr(cdt2_data());
-  index_uniqueness_tr(cdtp2_data());
-  index_uniqueness_tr(t2h_data());
+  index_uniqueness(t2_data());
+  index_uniqueness(dt2_data());
+  index_uniqueness(rt2_data());
+  index_uniqueness(ct2_data());
+  index_uniqueness(cdt2_data());
+  index_uniqueness(cdtp2_data());
+  index_uniqueness(t2h_data());
 
   std::cerr << "done\n";
   return 0;
