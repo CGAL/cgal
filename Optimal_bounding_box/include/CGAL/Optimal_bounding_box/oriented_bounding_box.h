8e00ffff21d997e5e72a611ec2a997f53e377b41
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-03-12 22:21:45 +0100

    Enable passing a point_map in complement to the range

--- Optimal_bounding_box/include/CGAL/Optimal_bounding_box/oriented_bounding_box.h
+++ Optimal_bounding_box/include/CGAL/Optimal_bounding_box/oriented_bounding_box.h
@@ -28,6 +28,7 @@
 #include <CGAL/convex_hull_3.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Iso_cuboid_3.h>
+#include <CGAL/Iterator_range.h>
 #include <CGAL/Kernel_traits.h>
 #include <CGAL/Random.h>
 #include <CGAL/Simple_cartesian.h>
@@ -35,6 +36,10 @@
 #ifdef CGAL_OPTIMAL_BOUNDING_BOX_BENCHMARKS
 #include <CGAL/Real_timer.h>
 #endif
+
+#include <boost/iterator/transform_iterator.hpp>
+#include <boost/range/value_type.hpp>
+#include <boost/utility/enable_if.hpp>
 
 #include <array>
 #include <iostream>
@@ -52,10 +57,10 @@ namespace Optimal_bounding_box {
 namespace internal {
 
 template <typename PointRange, typename Traits>
-void construct_oriented_bounding_box(std::array<typename Traits::Point_3, 8>& obb_points,
+void construct_oriented_bounding_box(const PointRange& points,
                                      const typename Traits::Aff_transformation_3& transformation,
                                      const typename Traits::Aff_transformation_3& inverse_transformation,
-                                     const PointRange& points,
+                                     std::array<typename Traits::Point_3, 8>& obb_points,
                                      const Traits& traits)
 {
   typedef typename Traits::Point_3                                   Point;
@@ -84,9 +89,9 @@ void construct_oriented_bounding_box(std::array<typename Traits::Point_3, 8>& ob
 }
 
 template <typename PointRange, typename Traits>
-void compute_best_transformation(typename Traits::Aff_transformation_3& transformation,
+void compute_best_transformation(const PointRange& points,
+                                 typename Traits::Aff_transformation_3& transformation,
                                  typename Traits::Aff_transformation_3& inverse_transformation,
-                                 const PointRange& points,
                                  CGAL::Random& rng,
                                  const Traits& traits)
 {
@@ -126,32 +131,32 @@ void compute_best_transformation(typename Traits::Aff_transformation_3& transfor
 
 // Following two functions are overload to dispatch depending on return type
 template <typename PointRange, typename Traits>
-void construct_oriented_bounding_box(typename Traits::Aff_transformation_3& transformation,
-                                     const PointRange& points,
+void construct_oriented_bounding_box(const PointRange& points,
+                                     typename Traits::Aff_transformation_3& transformation,
                                      CGAL::Random& rng,
                                      const Traits& traits)
 {
   typename Traits::Aff_transformation_3 inverse_transformation;
-  compute_best_transformation(transformation, inverse_transformation, points, rng, traits);
+  compute_best_transformation(points, transformation, inverse_transformation, rng, traits);
 }
 
 template <typename PointRange, typename Traits>
-void construct_oriented_bounding_box(std::array<typename Traits::Point_3, 8>& obb_points,
-                                     const PointRange& points,
+void construct_oriented_bounding_box(const PointRange& points,
+                                     std::array<typename Traits::Point_3, 8>& obb_points,
                                      CGAL::Random& rng,
                                      const Traits& traits)
 {
   typename Traits::Aff_transformation_3 transformation, inverse_transformation;
-  compute_best_transformation(transformation, inverse_transformation, points, rng, traits);
+  compute_best_transformation(points, transformation, inverse_transformation, rng, traits);
 
-  construct_oriented_bounding_box(obb_points, transformation, inverse_transformation, points, traits);
+  construct_oriented_bounding_box(points, transformation, inverse_transformation, obb_points, traits);
 }
 
 // Entry point, decide whether to compute the CH_3 or not
-template <typename Output, typename PointRange, typename Traits>
-void construct_oriented_bounding_box(Output& output,
+template <typename PointRange, typename Output, typename Traits>
+void construct_oriented_bounding_box(const PointRange& points,
                                      const bool use_ch,
-                                     const PointRange& points,
+                                     Output& output,
                                      CGAL::Random& rng,
                                      const Traits& traits)
 {
@@ -164,11 +169,11 @@ void construct_oriented_bounding_box(Output& output,
   {
     std::vector<Point> ch_points;
     extreme_points_3(points, std::back_inserter(ch_points));
-    return construct_oriented_bounding_box(output, ch_points, rng, traits);
+    return construct_oriented_bounding_box(ch_points, output, rng, traits);
   }
   else
   {
-    return construct_oriented_bounding_box(output, points, rng, traits);
+    return construct_oriented_bounding_box(points, output, rng, traits);
   }
 }
 
@@ -257,8 +262,10 @@ void oriented_bounding_box(const PointRange& points,
   using CGAL::parameters::choose_parameter;
   using CGAL::parameters::get_parameter;
 
+  typedef typename Point_set_processing_3::GetPointMap<PointRange, CGAL_BGL_NP_CLASS>::type PointMap;
+
 #if defined(CGAL_EIGEN3_ENABLED)
-  typedef typename boost::range_value<PointRange>::type                                 Point;
+  typedef typename boost::property_traits<PointMap>::value_type                         Point;
   typedef typename CGAL::Kernel_traits<Point>::type                                     K;
   typedef Oriented_bounding_box_traits_3<K>                                             Default_traits;
 #else
@@ -267,12 +274,13 @@ void oriented_bounding_box(const PointRange& points,
 
   typedef typename internal_np::Lookup_named_param_def<internal_np::geom_traits_t,
                                                        CGAL_BGL_NP_CLASS,
-                                                       Default_traits>::type        Geom_traits;
+                                                       Default_traits>::type            Geom_traits;
 
   CGAL_static_assertion_msg(!(std::is_same<Geom_traits, void>::value),
                             "You must provide a traits class or have Eigen enabled!");
 
-  Geom_traits traits = choose_parameter(get_parameter(np, internal_np::geom_traits), Geom_traits());
+  Geom_traits traits = choose_parameter<Geom_traits>(get_parameter(np, internal_np::geom_traits));
+  PointMap point_map = choose_parameter<PointMap>(get_parameter(np, internal_np::point_map));
 
   const bool use_ch = choose_parameter(get_parameter(np, internal_np::use_convex_hull), true);
   const unsigned int seed = choose_parameter(get_parameter(np, internal_np::random_seed), 0); // undocumented
@@ -287,7 +295,11 @@ void oriented_bounding_box(const PointRange& points,
     return;
   }
 
-  return Optimal_bounding_box::internal::construct_oriented_bounding_box(out, use_ch, points, rng, traits);
+  return Optimal_bounding_box::internal::construct_oriented_bounding_box(
+           CGAL::make_range(
+             boost::make_transform_iterator(points.begin(), CGAL::Property_map_to_unary_function<PointMap>(point_map)),
+             boost::make_transform_iterator(points.end(), CGAL::Property_map_to_unary_function<PointMap>(point_map))),
+           use_ch, out, rng, traits);
 }
 
 /// \ingroup PkgOptimalBoundingBox_Oriented_bounding_box
@@ -307,7 +319,7 @@ void oriented_bounding_box(const PointRange& points,
 ///   \cgalParamBegin{vertex_point_map}
 ///     the property map with the points associated to the vertices of `pmesh`.
 ///     If this parameter is omitted, an internal property map for
-///     `CGAL::vertex_point_t` must be available in `PolygonMesh`
+///     `CGAL::vertex_point_t` must be available in `PolygonMesh`.
 ///   \cgalParamEnd
 ///   \cgalParamBegin{geom_traits}
 ///     a geometric traits class instance, model of the concept `OrientedBoundingBoxTraits_3`.
@@ -341,19 +353,12 @@ void oriented_bounding_box(const PolygonMesh& pmesh,
 
   typedef typename boost::graph_traits<PolygonMesh>::vertex_descriptor                  vertex_descriptor;
 
-  typedef typename PMP::GetVertexPointMap<PolygonMesh, CGAL_BGL_NP_CLASS>::const_type   VPM;
-  typedef typename boost::property_traits<VPM>::value_type                              Point;
+  typedef typename CGAL::GetVertexPointMap<PolygonMesh, CGAL_BGL_NP_CLASS>::const_type  VPM;
 
   VPM vpm = choose_parameter(get_parameter(np, internal_np::vertex_point),
                              get_const_property_map(vertex_point, pmesh));
 
-  std::vector<Point> points;
-  points.reserve(num_vertices(pmesh));
-
-  for(vertex_descriptor v : vertices(pmesh))
-    points.push_back(get(vpm, v));
-
-  oriented_bounding_box(points, out, np);
+  oriented_bounding_box(vertices(pmesh), out, np.point_map(vpm));
 }
 
 /// \cond SKIP_IN_MANUAL
