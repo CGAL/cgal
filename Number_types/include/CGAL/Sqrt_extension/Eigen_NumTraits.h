82926482d651d43d85720724e82f275eb6fd43f9
Simon Giraudot <simon.giraudot@geometryfactory.com>
2020-03-16 09:02:31 +0100

    Fix eigen numtraits

--- Number_types/include/CGAL/Sqrt_extension/Eigen_NumTraits.h
+++ Number_types/include/CGAL/Sqrt_extension/Eigen_NumTraits.h
@@ -14,23 +14,11 @@
 #ifndef CGAL_SQRT_EXTENSION_EIGEN_NUMTRAITS_H
 #define CGAL_SQRT_EXTENSION_EIGEN_NUMTRAITS_H
 
-#ifdef CGAL_EIGEN3_ENABLED
-
-#include <Eigen/src/Core/NumTraits.h>
-
 namespace Eigen {
   template<class> struct NumTraits;
   template <class NT,class ROOT, class ACDE_TAG, class FP_TAG>
   struct NumTraits<CGAL::Sqrt_extension<NT, ROOT, ACDE_TAG, FP_TAG> >
-    : GenericNumTraits<CGAL::Sqrt_extension<NT, ROOT, ACDE_TAG, FP_TAG> >
   {
-    typedef CGAL::Sqrt_extension<NT, ROOT, ACDE_TAG, FP_TAG> Real;
-    typedef Real NonInteger;
-    typedef Real Nested;
-    typedef Real Literal;
-
-    static inline Real epsilon() { return NumTraits<NT>::epsilon(); }
-
     enum {
       IsInteger = 0,
       IsSigned = 1,
@@ -40,9 +28,20 @@ namespace Eigen {
       AddCost = 2*NumTraits<NT>::AddCost+NumTraits<ROOT>::ReadCost,
       MulCost = 5*NumTraits<NT>::MulCost+2*NumTraits<NT>::AddCost
     };
+    
+    typedef CGAL::Sqrt_extension<NT, ROOT, ACDE_TAG, FP_TAG> Real;
+    typedef Real NonInteger;
+    typedef Real Nested;
+    typedef Real Literal;
+
+    static inline Real epsilon() { return NumTraits<NT>::epsilon(); }
+    static inline int digits10() { return NumTraits<NT>::digits10(); }
+    static inline Real dummy_precision() { return NumTraits<NT>::dummy_precision(); }
+    static inline Real highest() { return NumTraits<NT>::highest(); }
+    static inline Real lowest() { return NumTraits<NT>::lowest(); }
+    static inline Real infinity() { return NumTraits<NT>::infinity(); }
+    static inline Real quiet_NaN() { return NumTraits<NT>::quiet_NaN(); }
   };
 }
-
-#endif // CGAL_EIGEN3_ENABLED
 
 #endif
