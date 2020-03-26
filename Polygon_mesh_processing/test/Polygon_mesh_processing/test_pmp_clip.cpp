e6e9e23fdd9665d31eac780d645962e0ddc76435
Sébastien Loriot <sebastien.loriot@cgal.org>
2020-03-26 19:40:08 +0100

    extra run of the script to remove tabs and trailing whitespaces

--- Polygon_mesh_processing/test/Polygon_mesh_processing/test_pmp_clip.cpp
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/test_pmp_clip.cpp
@@ -645,19 +645,19 @@ int main()
 
   std::cout << "Polyhedron" << std::endl;
   test<Polyhedron>();
-  
+
   std::cout << "running test_split with Surface_mesh\n";
   test_split<Surface_mesh>();
-  
+
   std::cout << "running test_iso_cuboid with Surface_mesh\n";
   test_isocuboid<Surface_mesh>();
 
   std::cout << "running test_split_plane with Surface_mesh\n";
   test_split_plane<Surface_mesh>();
-  
+
   std::cout << "running test_split with Polyhedron\n";
   test_split<Polyhedron>();
-  
+
   std::cout << "running test_split_plane with Polyhedron\n";
   test_split_plane<Polyhedron>();
 
