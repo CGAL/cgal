0040436ae3e7eab54ecb618db78cd97ac71e3c36
Andreas Fabri <andreas.fabri@geometryfactory.com>
2020-03-27 10:57:48 +0100

    Add non-recursive determinant for 7x7 matrix

---
+++ Kernel_23/test/Kernel_23/determinant.cpp
@@ -0,0 +1,15 @@
+#define CGAL_CHECK_DETERMINANT
+
+#include <CGAL/Simple_cartesian.h>
+
+int main()
+{
+  assert(CGAL::determinant<int>(4, 5, 1, 4, 6, 3, 1,
+                                4, 3, 6, 4, 2, 7, 3,
+                                6, 3, 3, 6, 2, 4, 5,
+                                1, 4, 3, 5, 5, 6 ,1,
+                                1, 3, 2, 7, 9, 6, 1,
+                                7, 6, 5, 4, 6, 2, 2, 
+                                2, 3, 5, 7, 4, 3, 3) == 763);
+  return 0;
+}
