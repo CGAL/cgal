7e8fc7f72a17871e09eeaf89f977da615df21b13
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2020-03-31 09:20:34 +0200

    Add header

--- Surface_mesh_topology/include/CGAL/Surface_mesh_topology/internal/Generic_map_selector.h
+++ Surface_mesh_topology/include/CGAL/Surface_mesh_topology/internal/Generic_map_selector.h
@@ -1,3 +1,14 @@
+// Copyright (c) 2020 CNRS and LIRIS' Establishments (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : Thien Hoang <thienvhoang99@gmail.com>
+//
 #ifndef CGAL_GENERIC_MAP_SELECTOR_H
 #define CGAL_GENERIC_MAP_SELECTOR_H
 
