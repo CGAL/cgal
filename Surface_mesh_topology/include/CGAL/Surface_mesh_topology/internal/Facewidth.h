87306ce852a663980366f968a8b17d645d19769a
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2020-04-01 15:44:46 +0200

    Start small improvements of Facewidth

--- Surface_mesh_topology/include/CGAL/Surface_mesh_topology/internal/Facewidth.h
+++ Surface_mesh_topology/include/CGAL/Surface_mesh_topology/internal/Facewidth.h
@@ -27,106 +27,100 @@ template <class Mesh_>
 class Facewidth
 {
 public:
-  using Mesh_original=Mesh_;
+  using Self=Facewidth<Mesh_>;
+  using Mesh=Mesh_;
 
-  using Gmap                       = typename Generic_map_selector<Mesh_original>::Generic_map;
-  using Gmap_wrapper               = Generic_map_selector<Mesh_original>;
-  using Dart_handle_original       = typename Gmap_wrapper::Dart_handle_original;
-  using Dart_handle                = typename Gmap::Dart_handle;
-  using Dart_const_handle          = typename Gmap::Dart_const_handle;
-  using size_type                  = typename Gmap::size_type;
-  using Path                       = CGAL::Surface_mesh_topology::Path_on_surface<Mesh_original>;
-  using SNC                        = Shortest_noncontractible_cycle<Gmap>;
+  using Original_map_wrapper=internal::Generic_map_selector<Mesh>;
+  using Original_dart_const_handle=typename Original_map_wrapper::Dart_handle_original; // TODO SOLVE const problem with copy
+
+  using Local_map        =typename Original_map_wrapper::Generic_map;
+  using Dart_handle      =typename Local_map::Dart_handle;
+  using Dart_const_handle=typename Local_map::Dart_const_handle;
+  using size_type        = typename Local_map::size_type;
+
+  using Path             = CGAL::Surface_mesh_topology::Path_on_surface<Mesh>;
+  using SNC              = Shortest_noncontractible_cycle<Local_map>;
   
-  Facewidth(const Mesh_original& gmap, bool display_time=false)
+  Facewidth(const Mesh& amesh, bool display_time=false)
   {
     CGAL::Timer t;
     if (display_time)
     { t.start(); }
     
-    typename Gmap_wrapper::Origin_to_copy_map origin_to_radial;
-    Gmap_wrapper::copy(m_radial_map, const_cast<Mesh_original&>(gmap),
-                       origin_to_radial, m_copy_to_origin);
+    typename Original_map_wrapper::Origin_to_copy_map origin_to_radial;
+    Original_map_wrapper::copy(m_radial_map, const_cast<Mesh&>(amesh),
+                               origin_to_radial, m_copy_to_origin, Local_map::NB_MARKS);
     m_copy_to_origin.clear();
-    Gmap_wrapper::copy(m_gmap, const_cast<Mesh_original&>(gmap),
-                       m_origin_to_copy, m_copy_to_origin);
+    Original_map_wrapper::copy(m_gmap, const_cast<Mesh&>(amesh),
+                               m_origin_to_copy, m_copy_to_origin, Local_map::NB_MARKS); // TODO BETTER
     // Initialize 0-attributes for m_gmap
-    for (auto it = m_gmap.darts().begin(), itend = m_gmap.darts().end(); it != itend; ++it)
+    int counter = 0;
+    for (auto it=m_gmap.darts().begin(), itend=m_gmap.darts().end(); it!=itend; ++it)
     {
       if (m_gmap.template attribute<0>(it)==NULL)
-      { m_gmap.template set_attribute<0>(it, m_gmap.template create_attribute<0>()); }
-    }
-    // Assign values
-    int counter = 0;
-    for (auto it=m_gmap.template attributes<0>().begin(),
-           itend=m_gmap.template attributes<0>().end(); it!=itend; ++it)
-    {
-      m_vertex_list.push_back(m_gmap.template dart_of_attribute<0>(it));
-      m_gmap.template info_of_attribute<0>(it)=counter++;
+      {
+        m_gmap.template set_attribute<0>(it, m_gmap.template create_attribute<0>(counter++));
+        m_vertex_list.push_back(it);
+      }
     }
 
     // m_face_list contains dart handles of m_gmap
-    for (auto it = m_gmap.template one_dart_per_cell<2>().begin(),
-              itend = m_gmap.template one_dart_per_cell<2>().end(); it != itend; ++it)
+    for (auto it=m_gmap.template one_dart_per_cell<2>().begin(),
+         itend=m_gmap.template one_dart_per_cell<2>().end(); it!=itend; ++it)
     { m_face_list.push_back(it); }
 
     // Create edge_list
     std::vector<Dart_handle> edge_list;
-    for (auto it = m_radial_map.template one_dart_per_cell<1>().begin(),
-              itend = m_radial_map.template one_dart_per_cell<1>().end(); it != itend; ++it)
+    for (auto it=m_radial_map.template one_dart_per_cell<1>().begin(),
+         itend=m_radial_map.template one_dart_per_cell<1>().end(); it!=itend; ++it)
     { edge_list.push_back(it); }
 
     // m_radial_map hasn't been changed so far
 
     // face_list contains dart handles of m_radial_map
     std::vector<Dart_handle> face_list;
-    for (auto it = m_radial_map.template one_dart_per_cell<2>().begin(),
-              itend = m_radial_map.template one_dart_per_cell<2>().end(); it != itend; ++it)
+    for (auto it=m_radial_map.template one_dart_per_cell<2>().begin(),
+         itend=m_radial_map.template one_dart_per_cell<2>().end(); it!=itend; ++it)
     { face_list.push_back(it); }
     
     // Adding "centroids"
     std::vector<Dart_handle> centroids;
     for (auto it : face_list) // face_list contains dart handles of m_radial_map
     {
-      auto new_vertex = m_radial_map.insert_cell_0_in_cell_2(it);
+      auto new_vertex=m_radial_map.insert_cell_0_in_cell_2(it);
       centroids.push_back(new_vertex);
     }
 
     // Initialize 1-attributes of m_radial_map
-    for (auto it = m_radial_map.darts().begin(), itend = m_radial_map.darts().end(); it != itend; ++it)
+    for (auto it=m_radial_map.darts().begin(), itend=m_radial_map.darts().end(); it!=itend; ++it)
     {
       if (m_radial_map.template attribute<1>(it)==NULL)
       { m_radial_map.template set_attribute<1>(it, m_radial_map.template create_attribute<1>()); }
     }
 
     // Assign values
-    for (int i = 0; i < centroids.size(); ++i)
+    for (int i=0; i<centroids.size(); ++i)
     {
-      auto u = centroids[i];
-      bool first_run = true;
-      for (Dart_handle it = u; first_run || it != u; it = m_radial_map.next(m_radial_map.opposite2(it)))
+      auto u=centroids[i];
+      bool first_run=true;
+      for (Dart_handle it=u; first_run || it!=u; it=m_radial_map.next(m_radial_map.opposite2(it)))
       {
-        first_run = false;
-        m_radial_map.template info<1>(it) = i;
+        first_run=false;
+        m_radial_map.template info<1>(it)=i;
       }
     }
 
     // Initialize 0-attributes of m_radial_map
-    for (auto it = m_radial_map.darts().begin(), itend = m_radial_map.darts().end(); it != itend; ++it)
+    for (auto it=m_radial_map.darts().begin(), itend=m_radial_map.darts().end(); it!=itend; ++it)
     {
       if (m_radial_map.template attribute<0>(it)==NULL)
-      { m_radial_map.template set_attribute<0>(it, m_radial_map.template create_attribute<0>()); }
+      { m_radial_map.template set_attribute<0>(it, m_radial_map.template create_attribute<0>(-1)); }
     }
-    for (auto it = m_radial_map.darts().begin(), itend = m_radial_map.darts().end(); it != itend; ++it)
-    {
-      m_radial_map.template info<0>(it) = -1;
-    }
-    typedef typename Gmap::template Attribute_handle<0>::type Attribute_handle_0;
-    for (Attribute_handle_0 att_it = m_gmap.template attributes<0>().begin(), 
-                            att_itend = m_gmap.template attributes<0>().end(); att_it != att_itend; ++att_it)
+    for (auto att_it=m_gmap.template attributes<0>().begin(),
+         att_itend = m_gmap.template attributes<0>().end(); att_it != att_itend; ++att_it)
     {
       auto it_radial = origin_to_radial[m_copy_to_origin[att_it->dart()]];
-      m_radial_map.template info<0>(it_radial) = att_it->info();
+      m_radial_map.template info<0>(it_radial)=att_it->info();
     }
 
     // Remove the marked edges of m_radial_map
@@ -139,11 +133,11 @@ public:
     if (display_time)
     {
       t.stop();
-      std::cout<<" [TIME] Facewidth constructor: "<<t.time()<<" seconds."<<std::endl;
+      std::cout<<"[TIME] Facewidth constructor: "<<t.time()<<" seconds."<<std::endl;
     }
   }
 
-  std::vector<Dart_handle_original> compute_facewidth(bool display_time=false)
+  std::vector<Original_dart_const_handle> compute_facewidth(bool display_time=false)
   {
     CGAL::Timer t;
     if (display_time)
@@ -151,10 +145,11 @@ public:
 
     m_cycle.clear();
     // Find edgewidth of the radial map
-    SNC snc_to_find_facewidth(m_radial_map);
-    Path_on_surface<Gmap> edgewidth_of_radial_map=snc_to_find_facewidth.compute_edgewidth();
+    SNC snc_to_find_facewidth(m_radial_map, display_time);
+    Path_on_surface<Local_map> edgewidth_of_radial_map=
+        snc_to_find_facewidth.compute_edgewidth(display_time);
 
-    int last_vertex_index=-1, first_vertex_index=-1;
+    int last_vertex_index=-1;
     int last_face_index=-1;
     for (int i=0, n=edgewidth_of_radial_map.length(); i<=n; i++)
     {
@@ -178,18 +173,18 @@ public:
     if (display_time)
     {
       t.stop();
-      std::cout<<" [TIME] compute_facewidth: "<<t.time()<<" seconds."<<std::endl;
+      std::cout<<"[TIME] compute_facewidth: "<<t.time()<<" seconds."<<std::endl;
     }
     
     return m_cycle;
   }
 
 protected:
-  Gmap m_gmap, m_radial_map;
+  Local_map m_gmap, m_radial_map;
   std::vector<Dart_handle> m_vertex_list, m_face_list;
-  std::vector<Dart_handle_original> m_cycle;
-  typename Gmap_wrapper::Origin_to_copy_map m_origin_to_copy;
-  typename Gmap_wrapper::Copy_to_origin_map m_copy_to_origin;
+  std::vector<Original_dart_const_handle> m_cycle;
+  typename Original_map_wrapper::Origin_to_copy_map m_origin_to_copy;
+  typename Original_map_wrapper::Copy_to_origin_map m_copy_to_origin;
 };
 
 } // namespace internal
