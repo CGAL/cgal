5da5a0de277f28ed071071cbf41ccad14f7d6d66
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-04-09 11:50:15 +0200

    Re-organize DT3 benchmarks

--- Triangulation_3/benchmark/Triangulation_3/DT3_benchmark_with_external_data.cpp
+++ Triangulation_3/benchmark/Triangulation_3/DT3_benchmark_with_external_data.cpp
@@ -1,44 +1,46 @@
 //#define CGAL_DO_NOT_USE_BOOST_MP
+
+// #define CGAL_LAZY_FILTERED_RATIONAL_KERNEL
 #define CGAL_USE_FILTERED_RATIONAL_KERNEL
 
-//#define CGAL_PROFILE
+#define CGAL_PROFILE
 //#define CGAL_USE_SSE2_FABS
 //#define CGAL_USE_SSE2_MAX
 //#define CGAL_MSVC_USE_STD_FABS  // use this one with precise
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
+#include <CGAL/bounding_box.h>
 #include <CGAL/Delaunay_triangulation_3.h>
 #include <CGAL/Delaunay_triangulation_cell_base_3.h>
 #include <CGAL/Triangulation_vertex_base_with_info_3.h>
 #include <CGAL/Timer.h>
 #include <CGAL/Real_timer.h>
-#include <CGAL/bounding_box.h>
 #include <CGAL/Random.h>
 
+#include <chrono>
 #include <iostream>
 #include <fstream>
 
-#include <chrono>
+// typedef CGAL::Exact_predicates_inexact_constructions_kernel       K;
+typedef CGAL::Exact_predicates_exact_constructions_kernel       K;
+typedef K::Point_3                                              Point_3;
 
+typedef CGAL::Delaunay_triangulation_3<K>                       DT;
 
-typedef CGAL::Exact_predicates_exact_constructions_kernel  K;
-typedef K::Point_3 Point_3;
+typedef CGAL::Triangulation_data_structure_3<
+          CGAL::Triangulation_vertex_base_3<K>,
+          CGAL::Delaunay_triangulation_cell_base_3<K>,
+          CGAL::Parallel_tag>                                   PTds;
+typedef CGAL::Delaunay_triangulation_3<K, PTds>                 PDT;
 
+typedef CGAL::Timer                                             Timer;
+typedef CGAL::Real_timer                                        Real_timer;
 
-typedef CGAL::Delaunay_triangulation_3<K> DT;
-/*
-typedef CGAL::Triangulation_data_structure_3<CGAL::Triangulation_vertex_base_with_info_3<X,K>,
-                                             CGAL::Delaunay_triangulation_cell_base_3<K>,
-                                             CGAL::Parallel_tag> PTds;
-typedef CGAL::Delaunay_triangulation_3<K, PTds> PDT;
-*/
-typedef CGAL::Timer Timer;
-typedef CGAL::Real_timer Real_timer;
-
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-#if 0
+#if 1
   std::cout.precision(17);
   Point_3 p(0.4324235, 0.3256236, 0.6532346), q(45634.3564, 3256.34577, 43633.34678);
 
@@ -58,15 +60,17 @@ int main(int argc, char* argv[])
   double x,y,z;
   std::vector<Point_3> points;
   Point_3 p;
+
   std::ifstream in(argv[1]);
   in >> n;
   points.reserve(n);
-  while(n--){
+  while(n--)
+  {
     in >> x >> y >> z;
-    points.push_back(Point_3(x,y,z));
+    points.emplace_back(x,y,z));
   }
 
-  int V,C;
+  int V, C;
   Timer timer;
   Real_timer rtimer;
   timer.start();
@@ -74,22 +78,23 @@ int main(int argc, char* argv[])
 
   std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
 
-  if(true){
+  if(true)
+  {
     std::cout << "Sequential" << std::endl;
     DT dt(points.begin(), points.end());
     V = dt.number_of_vertices();
     C = dt.number_of_cells();
-  } else {
-    /*
+  }
+  else
+  {
     std::cout << "Parallel" << std::endl;
-        CGAL::Bbox_3 bb  = CGAL::bounding_box(points.begin(), points.end()).bbox();
+    CGAL::Bbox_3 bb  = CGAL::bounding_box(points.begin(), points.end()).bbox();
 
     PDT::Lock_data_structure locking_ds(bb, 50);
 
     PDT pdt(points.begin(), points.end(), &locking_ds);
     V = pdt.number_of_vertices();
     C = pdt.number_of_cells();
-    */
   }
   timer.stop();
   rtimer.stop();
