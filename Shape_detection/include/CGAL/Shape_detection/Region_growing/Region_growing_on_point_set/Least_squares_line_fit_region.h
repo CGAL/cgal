9a8b48c8eacff1c53b91ff09d95eee87c89fd53d
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2020-04-10 17:13:26 +0200

    Remove trailing whitespace

--- Shape_detection/include/CGAL/Shape_detection/Region_growing/Region_growing_on_point_set/Least_squares_line_fit_region.h
+++ Shape_detection/include/CGAL/Shape_detection/Region_growing/Region_growing_on_point_set/Least_squares_line_fit_region.h
@@ -244,7 +244,6 @@ namespace Point_set {
 
       CGAL_precondition(region.size() > 0);
       if (region.size() == 1) { // create new reference line and normal
-          
         CGAL_precondition(region[0] < m_input_range.size());
 
         // The best fit line will be a line through this point with
