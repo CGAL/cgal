c372c04a6bf2e0e1a89439898413e7e82511ba1d
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-04-23 14:24:14 +0200

    Change script for use zith new dicker image.

--- Scripts/developer_scripts/Bundle_polyhedron_demo_with_appimage.sh
+++ Scripts/developer_scripts/Bundle_polyhedron_demo_with_appimage.sh
@@ -4,5 +4,5 @@ if [ "$1" == '--help' ]; then
   echo "Builds and packages the Polyhedron demo form the CGAL dir."
   exit 0
 fi
-docker run --rm -v "$2":/results:Z -v "$1":/cgal:ro docker.io/cgal/bundle-3d-demo "/scripts/build.sh -j$3 && /scripts/deploy.sh"
+docker run --rm -v "$2":/results:Z -v "$1":/cgal:ro -e "NUMBER_OF_DEDICATED_CORES=$3" docker.io/cgal/bundle-3d-demo
 
