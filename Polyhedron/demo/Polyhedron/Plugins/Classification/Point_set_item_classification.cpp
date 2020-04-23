8da84d4748867b508ac453074cde6ccb3b29e436
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-04-23 09:27:26 +0200

    More errors and warnigns

--- Polyhedron/demo/Polyhedron/Plugins/Classification/Point_set_item_classification.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/Classification/Point_set_item_classification.cpp
@@ -429,7 +429,7 @@ void Point_set_item_classification::change_color (int index, float* vmin, float*
           for (Point_set::const_iterator it = m_points->point_set()->begin();
                it != m_points->point_set()->first_selected(); ++ it)
           {
-            float v = std::max (0.f, (std::min)(1.f, m_label_probabilities[corrected_index][*it]));
+            float v = (std::max)(0.f, (std::min)(1.f, m_label_probabilities[corrected_index][*it]));
             m_points->point_set()->set_color(*it, ramp.r(v) * 255, ramp.g(v) * 255, ramp.b(v) * 255);
           }
         }
