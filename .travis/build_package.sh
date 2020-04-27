4e3465c36c3aa5816b21baf9a9ff5a3c25acd7f0
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-04-27 11:20:11 +0200

    Add a fake windows.h for travis to detect the most common mistakes.

--- .travis/build_package.sh
+++ .travis/build_package.sh
@@ -11,7 +11,7 @@ function mytime {
 function build_examples {
   mkdir -p build-travis
   cd build-travis
-  mytime cmake -DCGAL_DIR="/usr/local/lib/cmake/CGAL" -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DCGAL_BUILD_THREE_DOC=TRUE ..
+  mytime cmake -DCGAL_DIR="/usr/local/lib/cmake/CGAL" -DCMAKE_CXX_FLAGS="${CXX_FLAGS} -I/home/travis/build/CGAL/cgal/.travis/" -DCGAL_BUILD_THREE_DOC=TRUE ..
   mytime make -j2 VERBOSE=1
 }
 
---
+++ .travis/windows.h
@@ -0,0 +1,12 @@
+#define MAX(a,b)            (((a) > (b)) ? (a) : (b))
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+
+#define MIN(a,b)            (((a) < (b)) ? (a) : (b))
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+
+
+#define FAR #error named reserved in windows.h
+#define far #error named reserved in windows.h
+
+#define Polyline #error named reserved in windows.h
+#define Polygon #error named reserved in windows.h
