b37d116a112c5d8141a031feaf59c52267a26a06
Jackson Campolattaro <jackcamp@vt.edu>
2020-06-10 12:02:22 -0400

    Test program now constructs an octree instance

--- Octree/test/Octree/octree_test.cpp
+++ Octree/test/Octree/octree_test.cpp
@@ -3,6 +3,25 @@
 
 #include <CGAL/Octree.h>
 
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Point_set_3.h>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_3 Point;
+typedef CGAL::Point_set_3<Point> Point_set;
+typedef CGAL::Octree
+        <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
+        Octree;
+
 int main(void) {
+
+  Point_set points;
+  points.insert({0, 0, 1});
+
+  auto point_map = points.point_map();
+  auto normal_map = points.normal_map();
+
+  Octree octree(points, point_map, normal_map);
+
   return 0;
 }
