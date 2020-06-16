fd72d53d5f037b5fa43816bd462ab88aa56c47de
Sébastien Loriot <sebastien.loriot@cgal.org>
2020-06-16 16:15:09 +0200

    test do_not_modify

--- Polygon_mesh_processing/test/Polygon_mesh_processing/test_corefine.cpp
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/test_corefine.cpp
@@ -71,11 +71,71 @@ void test(const char* f1, const char* f2)
   assert(P.is_valid());
   assert(Q.is_valid());
 }
+
+void test_no_modifications(const char* f1, const char* f2)
+{
+  std::cout << "Corefining " << f1
+            << " and " << f2 << "\n";
+
+  std::cout << "  with Surface_mesh\n";
+  Surface_mesh sm1, sm2;
+  std::ifstream input(f1);
+  assert(input);
+  input >> sm1;
+  input.close();
+  input.open(f2);
+  assert(input);
+  input >> sm2;
+  input.close();
+  My_visitor<Surface_mesh> sm_v;
+
+  std::size_t nb_v_before1 = vertices(sm1).size();
+  std::size_t nb_v_before2 = vertices(sm2).size();
+ 
+  CGAL::Polygon_mesh_processing::corefine(sm1, sm2,
+    CGAL::parameters::visitor(sm_v),
+    CGAL::parameters::do_not_modify(true));
+
+  std::size_t nb_v_after1 = vertices(sm1).size();
+  std::size_t nb_v_after2 = vertices(sm2).size();
+
+  assert(sm1.is_valid());
+  assert(sm2.is_valid());
+  assert(nb_v_after2==nb_v_before2);
+  
+  assert((*(sm_v.i) != 0) == (nb_v_before1!=nb_v_after1));
+
+  std::cout << "  with Polyhedron_3\n";
+  Polyhedron_3 P, Q;
+  input.open(f1);
+  assert(input);
+  input >> P;
+  input.close();
+  input.open(f2);
+  assert(input);
+  input >> Q;
+  My_visitor<Polyhedron_3> sm_p;
+
+  nb_v_before1 = vertices(P).size();
+  nb_v_before2 = vertices(Q).size();
+  CGAL::Polygon_mesh_processing::corefine(P, Q,
+    CGAL::parameters::visitor(sm_p).do_not_modify(true));
+  nb_v_after1 = vertices(P).size();
+  nb_v_after2 = vertices(Q).size();
+
+  assert(nb_v_after1==nb_v_before1);
+  assert((*(sm_p.i) != 0)  == (nb_v_before2!=nb_v_after2));
+
+  assert(P.is_valid());
+  assert(Q.is_valid());
+}
+
 int main(int argc, char** argv)
 {
   for(int i=0; i< (argc-1)/2;++i)
   {
     test(argv[2*i+1], argv[2*(i+1)]);
     test(argv[2*(i+1)], argv[2*i+1]);
+    test_no_modifications(argv[2*(i+1)], argv[2*i+1]);
   }
 }
