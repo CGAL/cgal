5298ff114e07b5812fdfdcf3c6ee49338cfb61d5
Efi Fogel <efifogel@gmail.com>
2020-07-02 19:10:29 +0300

    Cleaned up

--- Arrangement_on_surface_2/include/CGAL/Arr_line_arc_traits_2.h
+++ Arrangement_on_surface_2/include/CGAL/Arr_line_arc_traits_2.h
@@ -125,9 +125,8 @@ public:
     template <typename OutputIterator>
     OutputIterator operator()(const Curve_2& line, OutputIterator oi) const
     {
-      typedef boost::variant<Point_2, X_monotone_curve_2>
-        Make_x_monotone_2_result;
-      *oi++ = Make_x_monotone_2_result(line);
+      typedef boost::variant<Point_2, X_monotone_curve_2> Make_x_monotone_result;
+      *oi++ = Make_x_monotone_result(line);
       return oi;
     }
   };
