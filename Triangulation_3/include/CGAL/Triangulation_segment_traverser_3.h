4eb10d8323fb3fc32a8ddcacb7a6d3076671ac17
Jane Tournois <jane.tournois@geometryfactory.com>
2020-07-06 06:44:00 +0200

    fix license header

--- Triangulation_3/include/CGAL/Triangulation_segment_traverser_3.h
+++ Triangulation_3/include/CGAL/Triangulation_segment_traverser_3.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2012  Utrecht University
+// Copyright (c) 2012  INRIA Sophia-Antipolis (France).
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org).
@@ -7,13 +7,11 @@
 // $Id$
 // SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 //
-//
 // Author(s): Thijs van Lankveld, Jane Tournois
 
 
 #ifndef CGAL_TRIANGULATION_SEGMENT_TRAVERSER_3_H
 #define CGAL_TRIANGULATION_SEGMENT_TRAVERSER_3_H
-
 
 #include <CGAL/basic.h>
 
