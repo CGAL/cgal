8f62f239e43304d6fc908086be0bbf5c4a7f9353
Jackson Campolattaro <jackcamp@vt.edu>
2020-07-10 11:51:30 -0400

    Add a larger test of locate()

--- Octree/test/Octree/test_octree_locate.cpp
+++ Octree/test/Octree/test_octree_locate.cpp
@@ -75,10 +75,53 @@ void test_8_points() {
 
 }
 
+void test_10_points() {
+
+  // Define the dataset
+  Point_set points;
+  points.insert({-1, -1, -1});
+  points.insert({1, -1, -1});
+  points.insert({-1, 1, -1});
+  points.insert({1, 1, -1});
+  points.insert({-1, -1, 1});
+  points.insert({1, -1, 1});
+  points.insert({-1, 1, 1});
+  points.insert({1, 1, 1});
+  points.insert({0.875, 1, -1});
+  points.insert({-1, -0.75, 1});
+  auto point_map = points.point_map();
+
+  // Create the octree
+  Octree octree(points, point_map);
+  octree.refine(10, 1);
+
+  // Existing points should end up in the same place
+  assert(octree.root()[0] == octree.locate({-1, -1, -1}));
+  assert(octree.root()[1] == octree.locate({1, -1, -1}));
+  assert(octree.root()[2] == octree.locate({-1, 1, -1}));
+  assert(octree.root()[3][3][3][7][3][7][7][7][7][7] == octree.locate({1, 1, -1}));
+  assert(octree.root()[4][4][4] == octree.locate({-1, -1, 1}));
+  assert(octree.root()[5] == octree.locate({1, -1, 1}));
+  assert(octree.root()[6] == octree.locate({-1, 1, 1}));
+  assert(octree.root()[7] == octree.locate({1, 1, 1}));
+
+  // Points adjacent to the existing points might end up in different places
+  assert(octree.root()[0] == octree.locate({-1.1, -1.1, -1.1}));
+  assert(octree.root()[1] == octree.locate({1.1, -1.1, -1.1}));
+  assert(octree.root()[2] == octree.locate({-1.1, 1.1, -1.1}));
+  assert(octree.root()[3][3][3][3] == octree.locate({1.1, 1.1, -1.1}));
+  assert(octree.root()[4][4][4] == octree.locate({-1.1, -1.1, 1.1}));
+  assert(octree.root()[5] == octree.locate({1.1, -1.1, 1.1}));
+  assert(octree.root()[6] == octree.locate({-1.1, 1.1, 1.1}));
+  assert(octree.root()[7] == octree.locate({1.1, 1.1, 1.1}));
+
+}
+
 int main(void) {
 
   test_1_point();
   test_8_points();
+  test_10_points();
 
   return EXIT_SUCCESS;
 }
