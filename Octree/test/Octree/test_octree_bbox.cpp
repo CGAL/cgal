255903cef265f2f56ee4695198507737602dc2ae
Jackson Campolattaro <jackcamp@vt.edu>
2020-07-15 14:17:37 -0400

    Add root node tests

--- Octree/test/Octree/test_octree_bbox.cpp
+++ Octree/test/Octree/test_octree_bbox.cpp
@@ -17,14 +17,39 @@ typedef CGAL::Octree::Octree
 
 void test_1_node() {
 
+  // Define the dataset
+  Point_set points;
+  points.insert({-1, -1, -1});
+  auto point_map = points.point_map();
+
+  // Create the octree
+  Octree octree(points, point_map);
+  octree.refine(10, 1);
+
+  // Compare the top (only) node
+  assert(octree.bbox(octree.root()) == CGAL::Bbox_3(-1, -1, -1, -1, -1, -1));
 }
 
 void test_9_nodes() {
 
+  // Define the dataset
+  Point_set points;
+  points.insert({-1, -1, -1});
+  points.insert({1, 1, 1});
+  auto point_map = points.point_map();
+
+  // Create the octree
+  Octree octree(points, point_map, 1.1);
+  octree.refine(10, 1);
+
+  // Compare the top node
+  assert(octree.bbox(octree.root()) == CGAL::Bbox_3(-1.1, -1.1, -1.1, 1.1, 1.1, 1.1));
 }
 
 int main(void) {
 
+  test_1_node();
+  test_9_nodes();
 
   return EXIT_SUCCESS;
 }
