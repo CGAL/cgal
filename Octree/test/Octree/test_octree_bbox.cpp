f8b65e251d6870674920fa0ea17bbdbdbc9ac9df
Jackson Campolattaro <jackcamp@vt.edu>
2020-07-15 12:28:35 -0400

    Outline test of bbox function

---
+++ Octree/test/Octree/test_octree_bbox.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <CGAL/Octree.h>
+#include <CGAL/Octree/IO.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Point_set_3.h>
+
+#include <cassert>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_3 Point;
+typedef Kernel::FT FT;
+typedef CGAL::Point_set_3<Point> Point_set;
+typedef CGAL::Octree::Octree
+        <Point_set, typename Point_set::Point_map>
+        Octree;
+
+void test_1_node() {
+
+}
+
+void test_9_nodes() {
+
+}
+
+int main(void) {
+
+
+  return EXIT_SUCCESS;
+}
