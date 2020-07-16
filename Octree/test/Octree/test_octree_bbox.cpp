0005c08ec2464e56f332cc9a01b335d5061058a7
Jackson Campolattaro <jackcamp@vt.edu>
2020-07-15 21:45:57 -0400

    Add tests to catch the error just fixed

--- Octree/test/Octree/test_octree_bbox.cpp
+++ Octree/test/Octree/test_octree_bbox.cpp
@@ -94,16 +94,14 @@ void test_25_nodes() {
   assert(octree.bbox(octree.root()[0][7]) == CGAL::Bbox_3(-0.75, -0.75, -0.75, 0, 0, 0));
 
   // Compare children of the last child
-  std::cout << octree.bbox(octree.root()[7][0]) << std::endl;
-  // FIXME: I've found my error! root()[7][0] produces the same box as root()[0][0]
-//  assert(octree.bbox(octree.root()[7][0]) == CGAL::Bbox_3(-1.5, -1.5, -1.5, -0.75, -0.75, -0.75));
-//  assert(octree.bbox(octree.root()[7][1]) == CGAL::Bbox_3(-0.75, -1.5, -1.5, 0, -0.75, -0.75));
-//  assert(octree.bbox(octree.root()[7][2]) == CGAL::Bbox_3(-1.5, -0.75, -1.5, -0.75, 0, -0.75));
-//  assert(octree.bbox(octree.root()[7][3]) == CGAL::Bbox_3(-0.75, -0.75, -1.5, 0, 0, -0.75));
-//  assert(octree.bbox(octree.root()[7][4]) == CGAL::Bbox_3(-1.5, -1.5, -0.75, -0.75, -0.75, 0));
-//  assert(octree.bbox(octree.root()[7][5]) == CGAL::Bbox_3(-0.75, -1.5, -0.75, 0, -0.75, 0));
-//  assert(octree.bbox(octree.root()[7][6]) == CGAL::Bbox_3(-1.5, -0.75, -0.75, -0.75, 0, 0));
-//  assert(octree.bbox(octree.root()[7][7]) == CGAL::Bbox_3(-0.75, -0.75, -0.75, 0, 0, 0));
+  assert(octree.bbox(octree.root()[7][0]) == CGAL::Bbox_3(0, 0, 0, 0.75, 0.75, 0.75));
+  assert(octree.bbox(octree.root()[7][1]) == CGAL::Bbox_3(0.75, 0, 0, 1.5, 0.75, 0.75));
+  assert(octree.bbox(octree.root()[7][2]) == CGAL::Bbox_3(0, 0.75, 0, 0.75, 1.5, 0.75));
+  assert(octree.bbox(octree.root()[7][3]) == CGAL::Bbox_3(0.75, 0.75, 0, 1.5, 1.5, 0.75));
+  assert(octree.bbox(octree.root()[7][4]) == CGAL::Bbox_3(0, 0, 0.75, 0.75, 0.75, 1.5));
+  assert(octree.bbox(octree.root()[7][5]) == CGAL::Bbox_3(0.75, 0, 0.75, 1.5, 0.75, 1.5));
+  assert(octree.bbox(octree.root()[7][6]) == CGAL::Bbox_3(0, 0.75, 0.75, 0.75, 1.5, 1.5));
+  assert(octree.bbox(octree.root()[7][7]) == CGAL::Bbox_3(0.75, 0.75, 0.75, 1.5, 1.5, 1.5));
 
   std::cout << octree << std::endl;
 }
