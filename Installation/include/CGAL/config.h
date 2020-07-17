ebdf7b2c041e6b9cb4c2d41922cab247ceea3779
Laurent Rineau <laurent.rineau@cgal.org>
2020-07-17 09:53:07 +0200

    Fix a compiler with the no_unique_address attribute, in C++03

--- Installation/include/CGAL/config.h
+++ Installation/include/CGAL/config.h
@@ -555,7 +555,7 @@ using std::max;
 #endif
 
 // Macro to specify [[no_unique_address]] if supported
-#if __has_cpp_attribute(no_unique_address)
+#if CGAL_CXX11 && __has_cpp_attribute(no_unique_address)
 #  define CGAL_NO_UNIQUE_ADDRESS [[no_unique_address]]
 #else
 #  define CGAL_NO_UNIQUE_ADDRESS
