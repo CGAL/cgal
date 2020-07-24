2f7f37983b59802d2aee1cabee2ad9d5827f347a
Dmitry Anisimov <danston@ymail.com>
2020-07-24 11:26:28 +0200

    periodic 3 and generator fixes + the issue #3083 is addressed

--- STL_Extension/include/CGAL/result_of.h
+++ STL_Extension/include/CGAL/result_of.h
@@ -20,41 +20,50 @@
 
 #include <CGAL/config.h>
 #include <CGAL/disable_warnings.h>
-#include <type_traits>
 
-// Address the warning C4003: not enough actual parameters for macro 'BOOST_PP_SEQ_DETAIL_IS_NOT_EMPTY'
-// result_of.hpp includes files from boost/preprocessor
-// This concerns boost 1_65_1
-#if defined(BOOST_MSVC)
-#  pragma warning(push)
-#  pragma warning(disable: 4003)
-#endif
-#include <boost/utility/result_of.hpp>
-#if defined(BOOST_MSVC)
-#  pragma warning(pop)
-#endif
+#if CGAL_CXX11
 
-#include <boost/version.hpp>
+  #include <type_traits>
 
-namespace CGAL{
+#else // C++<11
 
-namespace cpp11{
+  // Address the warning C4003: not enough actual parameters for macro 'BOOST_PP_SEQ_DETAIL_IS_NOT_EMPTY'
+  // result_of.hpp includes files from boost/preprocessor
+  // This concerns boost 1_65_1
+  #if defined(BOOST_MSVC)
+    #pragma warning(push)
+    #pragma warning(disable: 4003)
+  #endif
+    #include <boost/utility/result_of.hpp>
+  #if defined(BOOST_MSVC)
+    #pragma warning(pop)
+  #endif
+  #include <boost/version.hpp>
 
-// When we switch to C++17, we should use the std::invoke_result instead.
-// #if __cplusplus >= 201703L
-//   template<typename F>
-//   struct result_of : public std::invoke_result<F>
-//   {
+#endif // end C++<11
 
-//   };
-// #else
+namespace CGAL {
+namespace cpp11 {
+
+#if CGAL_CXX20
+
+  template<typename Signature> class result_of;
+  template<typename F, typename... Args>
+  class result_of<F(Args...)> : public std::invoke_result<F, Args...> { };
+
+#elif CGAL_CXX11
+
   using std::result_of;
-// #endif
 
-}
+#else // C++<11
 
-}
+  using boost::result_of;
+
+#endif
+
+} // end cpp11
+} // end CGAL
 
 #include <CGAL/enable_warnings.h>
 
-#endif //CGAL_RESULT_OF_H
+#endif // CGAL_RESULT_OF_H
