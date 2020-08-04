dac614bd02aec1eb3cae166e504445c757805c1f
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-08-04 10:12:58 +0200

    Update to bionic and install and use clang-10

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 
 while [ $DONE = 0 ]
 do
-  DONE=1 && sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install clang zsh \
+  DONE=1 && sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install clang-10 zsh \
 flex bison cmake graphviz libgmp-dev libmpfr-dev libmpfi-dev zlib1g-dev libeigen3-dev  \
 qtbase5-dev libqt5sql5-sqlite libqt5opengl5-dev qtscript5-dev libqt5svg5-dev qttools5-dev qttools5-dev-tools qml-module-qtgraphicaleffects libopencv-dev mesa-common-dev libmetis-dev libglu1-mesa-dev \
 libboost1.73-dev || DONE=0 && sudo apt-get update
