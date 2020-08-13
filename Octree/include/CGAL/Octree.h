14562878a5b4f9f39d0ff53c9bea3c3b52607de7
Jackson Campolattaro <jackcamp@vt.edu>
2020-08-13 16:16:38 -0400

    Remove old greater_or_equal_neighbor method

--- Octree/include/CGAL/Octree.h
+++ Octree/include/CGAL/Octree.h
@@ -711,47 +711,6 @@ private: // functions :
     return neighbors_to_split;
   }
 
-  Node *greater_or_equal_neighbor(Node *node, std::size_t direction) {
-
-    // A root node doesn't have neighbors
-    if (node->is_root())
-      return nullptr;
-
-    unsigned int dir_axis = direction & 1;  // 0, 1, 0, 1, 0, 1
-    unsigned int bit_axis = direction >> 1; // 0, 0, 1, 1, 2, 2
-    unsigned int dir_idx_offset = 1;  // -1, 1, -2, 2, -4, 4
-    dir_idx_offset <<= bit_axis;
-    if (!dir_axis)
-      dir_idx_offset = -dir_idx_offset;
-
-    // Find each sibling with the same parent
-    for (unsigned int child_id = 0; child_id < 8; child_id++) {
-
-      // If that sibling isn't this node and it's in the right direction
-      if (((child_id >> bit_axis) & 1) != dir_axis && (*node->parent())[child_id] == *node) {
-
-        // Return that sibling
-        return &(node->parent()[child_id + dir_idx_offset]);
-      }
-    }
-
-    // Find the parent's sibling in that direction (recursively)
-    Node *parent_neighbor = greater_or_equal_neighbor(node->parent(), direction);
-
-    // If that node doesn't have children, return it
-    if (parent_neighbor == nullptr || parent_neighbor->is_leaf())
-      return parent_neighbor;
-
-    // Otherwise, find the closest child of that parent
-    for (int child_id = 0; child_id < 8; child_id++) {
-      if (((child_id >> bit_axis) & 1) == dir_axis && (*node->parent())[child_id] == *node) {
-        return *parent_neighbor[child_id - dir_idx_offset];
-      }
-    }
-
-    return nullptr;
-  }
-
 }; // end class Octree
 
 } // namespace Octree
