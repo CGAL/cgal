05ac9f7e1c48dbe610fec991548a0fc1ebf8c733
Jackson Campolattaro <jackcamp@vt.edu>
2020-08-18 22:45:45 -0400

    Implement Split_criterion concept

--- Octree/doc/Octree/Concepts/Split_criterion.h
+++ Octree/doc/Octree/Concepts/Split_criterion.h
@@ -1,4 +1,27 @@
-#ifndef DOCUMENTATION_SPLIT_CRITERION_H
-#define DOCUMENTATION_SPLIT_CRITERION_H
 
-#endif //DOCUMENTATION_SPLIT_CRITERION_H
+/*!
+ * \ingroup PkgOctreeConcepts
+ * \cgalConcept
+ *
+ * \brief A Split_criterion is a functor which determines whether an octree node needs to be split during refinement.
+ *
+ * This can also be done by a function pointer, or a lambda.
+ */
+struct Split_criterion {
+
+
+  /// \name Operators
+  /// @{
+
+  /*!
+   * \brief determines whether a node needs to be split based on a const reference to the node
+   *
+   * \tparam Node
+   * \param n const reference to the node
+   * \return whether or not the node needs to be split (true for yes)
+   */
+  template<class Node>
+  bool operator()(const Node &n) const {}
+
+  /// @}
+};
