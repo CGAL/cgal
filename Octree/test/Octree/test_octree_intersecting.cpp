71e7705ea1985e7c042c35e3631abe0ba613dc51
Jackson Campolattaro <jackcamp@vt.edu>
2020-08-20 11:58:06 -0400

    Add scoped sections for each test

--- Octree/test/Octree/test_octree_intersecting.cpp
+++ Octree/test/Octree/test_octree_intersecting.cpp
@@ -16,6 +16,53 @@ typedef CGAL::Octree::Octree<Point_vector> Octree;
 
 int main(void) {
 
+  // Fill a vector with points
+  Point_vector points;
+  points.emplace_back(1, 1, 1);
+  points.emplace_back(-1, 1, 1);
+  points.emplace_back(1, -1, 1);
+  points.emplace_back(-1, -1, 1);
+  points.emplace_back(1, 1, -1);
+  points.emplace_back(-1, 1, -1);
+  points.emplace_back(1, -1, -1);
+  points.emplace_back(-1, -1, -1);
+  points.emplace_back(0.9, -1, -1);
+  points.emplace_back(0.9, -0.9, -1);
+  points.emplace_back(0.9, -0.95, -1);
+  points.emplace_back(0.9, -0.9, -0.9);
+  points.emplace_back(0.9, -0.95, -0.9);
+  points.emplace_back(0.9, -1, -1);
+  points.emplace_back(-0.9, -1, -1);
+
+  // Create an octree from the vector
+  Octree octree(points);
+
+  // Build the octree
+  octree.refine(10, 2);
+
+  // Intersection with a point (not particularly useful)
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  {
+    std::vector<const Octree::Node *> nodes{};
+
+  }
+
+  // Intersection with a sphere
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  {
+    std::vector<const Octree::Node *> nodes{};
+
+  }
+
+  // Intersection with a ray
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  {
+    std::vector<const Octree::Node *> nodes{};
+
+  }
+
+  // Print out the tree
+  std::cout << octree;
 
   return EXIT_SUCCESS;
 }
