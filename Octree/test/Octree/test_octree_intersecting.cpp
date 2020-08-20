6e87e999e1ea87b52b686725172bffb630c75f6d
Jackson Campolattaro <jackcamp@vt.edu>
2020-08-20 11:18:35 -0400

    Add empty intersection test

---
+++ Octree/test/Octree/test_octree_intersecting.cpp
@@ -0,0 +1,20 @@
+#define CGAL_TRACE_STREAM std::cerr
+
+#include <iostream>
+#include <CGAL/Octree.h>
+#include <CGAL/Octree/IO.h>
+#include <CGAL/Octree/Traversal.h>
+#include <CGAL/Simple_cartesian.h>
+
+#include <cassert>
+#include <CGAL/point_generators_3.h>
+
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_3 Point;
+typedef std::vector<Point> Point_vector;
+typedef CGAL::Octree::Octree<Point_vector> Octree;
+
+int main(void) {
+
+  return EXIT_SUCCESS;
+}
