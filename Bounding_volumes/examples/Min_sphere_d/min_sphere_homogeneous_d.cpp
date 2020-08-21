7e04a030a56782505e4cdc4ef4711ab7e98a31cc
Andreas Fabri <andreas.fabri@geometryfactory.com>
2020-08-21 09:22:49 +0100

    turn homogeneous example 3D

--- Bounding_volumes/examples/Min_sphere_d/min_sphere_homogeneous_d.cpp
+++ Bounding_volumes/examples/Min_sphere_d/min_sphere_homogeneous_d.cpp
@@ -1,28 +1,26 @@
-#include <CGAL/Cartesian_d.h>
-#include <iostream>
-#include <cstdlib>
+#include <CGAL/Exact_integer.h>
+#include <CGAL/Homogenous.h>
 #include <CGAL/Random.h>
 #include <CGAL/Min_sphere_annulus_d_traits_d.h>
 #include <CGAL/Min_sphere_d.h>
 
-typedef CGAL::Cartesian_d<double>              K;
-typedef CGAL::Min_sphere_annulus_d_traits_d<K> Traits;
-typedef CGAL::Min_sphere_d<Traits>             Min_sphere;
-typedef K::Point_d                             Point;
+#include <iostream>
+#include <cstdlib>
+
+typedef CGAL::Homogeneous<CGAL::Exact_integer>   K;
+typedef CGAL::Min_sphere_annulus_d_traits_d<K>   Traits;
+typedef CGAL::Min_sphere_d<Traits>               Min_sphere;
+typedef K::Point_3                               Point;
 
 const int n = 10;                        // number of points
-const int d = 5;                         // dimension of points
 
 int main ()
 {
     Point         P[n];                  // n points
-    double        coord[d];              // d coordinates
     CGAL::Random  r;                     // random number generator
 
     for (int i=0; i<n; ++i) {
-        for (int j=0; j<d; ++j)
-            coord[j] = r.get_double();
-        P[i] = Point(d, coord, coord+d); // random point
+      P[i] = Point(r.get_int(0, 1000),r.get_int(0, 1000), r.get_int(0, 1000), 1 );
     }
 
     Min_sphere  ms (P, P+n);             // smallest enclosing sphere
