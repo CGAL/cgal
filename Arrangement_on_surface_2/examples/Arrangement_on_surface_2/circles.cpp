9095e21985f5118fc5d768cf80141f4d82940411
Efi Fogel <efifogel@gmail.com>
2020-08-24 18:09:54 +0300

    Updated

--- Arrangement_on_surface_2/examples/Arrangement_on_surface_2/circles.cpp
+++ Arrangement_on_surface_2/examples/Arrangement_on_surface_2/circles.cpp
@@ -14,14 +14,13 @@ int main()
   insert(arr, Curve(Circle(Rational_point(7, 7), Number_type(25))));
 
   // Create a circle centered at (4,-0.5) with radius 3.5 (= 7/2) (C3).
-  Rational_point c3 = Rational_point(4, Number_type(-1) / Number_type(2));
+  Rational_point c3(4, Number_type(-1) / Number_type(2));
   insert(arr, Curve(Circle(c3, Number_type(49) / Number_type(4))));
 
   // Locate the vertex with maximum degree.
   Arrangement_2::Vertex_const_handle v_max;
-  for (auto vit = arr.vertices_begin(); vit != arr.vertices_end(); ++vit) {
+  for (auto vit = arr.vertices_begin(); vit != arr.vertices_end(); ++vit)
     if (vit->degree() > max_degree) v_max = vit;
-  }
 
   std::cout << "The vertex with maximal degree in the arrangement is: "
             << "v_max = (" << v_max->point() << ") "
--- Arrangement_on_surface_2/examples/Arrangement_on_surface_2/circular_arcs.cpp
+++ Arrangement_on_surface_2/examples/Arrangement_on_surface_2/circular_arcs.cpp
@@ -29,30 +29,30 @@ int main()
   // Create a circular arc (C5) that is the upper half of the circle centered at
   // (1, 1) with squared radius 3. Create the circle with clockwise orientation,
   // so the arc is directed from (1 - sqrt(3), 1) to (1 + sqrt(3), 1).
-  Rational_point c5 = Rational_point(1, 1);
-  Circle circ5 = Circle(c5, 3, CGAL::CLOCKWISE);
-  CoordNT one_minus_sqrt_3 = CoordNT(1, -1, 3);
-  CoordNT one_plus_sqrt_3 = CoordNT(1, 1, 3);
-  Point s5 = Point(one_minus_sqrt_3, CoordNT(1));
-  Point t5 = Point(one_plus_sqrt_3, CoordNT(1));
+  Rational_point c5(1, 1);
+  Circle circ5(c5, 3, CGAL::CLOCKWISE);
+  CoordNT one_minus_sqrt_3(1, -1, 3);
+  CoordNT one_plus_sqrt_3(1, 1, 3);
+  Point s5(one_minus_sqrt_3, CoordNT(1));
+  Point t5(one_plus_sqrt_3, CoordNT(1));
   curves.push_back(Curve(circ5, s5, t5));
 
   // Create an arc (C6) of the unit circle, directed clockwise from
   // (-1/2, sqrt(3)/2) to (1/2, sqrt(3)/2).
   // The supporting circle is oriented accordingly.
-  Rational_point c6 = Rational_point(0, 0);
+  Rational_point c6(0, 0);
   Number_type half = Number_type(1) / Number_type(2);
-  CoordNT sqrt_3_div_2 = CoordNT(Number_type(0), half, 3);
-  Point s6 = Point(-half, sqrt_3_div_2);
-  Point t6 = Point(half, sqrt_3_div_2);
+  CoordNT sqrt_3_div_2(Number_type(0), half, 3);
+  Point s6(-half, sqrt_3_div_2);
+  Point t6(half, sqrt_3_div_2);
   curves.push_back(Curve(c6, 1, CGAL::CLOCKWISE, s6, t6));
 
   // Create a circular arc (C7) defined by two endpoints and a midpoint,
   // all having rational coordinates. This arc is the upper right
   // quarter of a circle centered at the origin with radius 5.
-  Rational_point s7 = Rational_point(0, 5);
-  Rational_point mid7 = Rational_point(3, 4);
-  Rational_point t7 = Rational_point(5, 0);
+  Rational_point s7(0, 5);
+  Rational_point mid7(3, 4);
+  Rational_point t7(5, 0);
   curves.push_back(Curve(s7, mid7, t7));
 
   // Construct the arrangement of the curves and print its size.
