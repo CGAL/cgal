b9135c3d2cba2c32853c5f06b96142b13c3a7326
Efi Fogel <efifogel@gmail.com>
2020-08-25 15:18:49 +0300

    Updated

--- Arrangement_on_surface_2/examples/Arrangement_on_surface_2/rational_functions.cpp
+++ Arrangement_on_surface_2/examples/Arrangement_on_surface_2/rational_functions.cpp
@@ -19,15 +19,15 @@ int main()
   std::vector<Traits::X_monotone_curve_2> arcs;
 
   // Create an arc (C1) supported by the polynomial y = x^4 - 6x^2 + 8,
-  // defined over the interval [-2.1, 2.1].
-  Polynomial P1 = x*x*x*x - 6*x*x + 8;
+  // defined over the (approximate) interval [-2.1, 2.1].
+  Polynomial P1 = CGAL::ipower(x,4) - 6*x*x + 8;
   Alg_real l(Bound(-2.1)), r(Bound(2.1));
   arcs.push_back(construct(P1, l, r));
 
   // Create an arc (C2) supported by the function y = x / (1 + x^2),
   // defined over the interval [-3, 3].
   Polynomial P2 = x;
-  Polynomial Q2 = 1+x*x;
+  Polynomial Q2 = 1 + x*x;
   arcs.push_back(construct(P2, Q2, Alg_real(-3), Alg_real(3)));
 
   // Create an arc (C3) supported by the parbola y = 8 - x^2,
