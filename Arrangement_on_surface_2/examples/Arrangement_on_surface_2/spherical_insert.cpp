2ba5b641f65a360146748d694488d8a5500d8544
Efi Fogel <efifogel@gmail.com>
2020-08-31 10:36:33 +0300

    Updated

--- Arrangement_on_surface_2/examples/Arrangement_on_surface_2/spherical_insert.cpp
+++ Arrangement_on_surface_2/examples/Arrangement_on_surface_2/spherical_insert.cpp
@@ -1,7 +1,6 @@
 //! \file examples/Arrangement_on_surface_2/spherical_insert.cpp
 // Constructing an arrangement of arcs of great circles.
 
-#include <vector>
 #include <list>
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
@@ -9,9 +8,10 @@
 #include <CGAL/Arr_geodesic_arc_on_sphere_traits_2.h>
 #include <CGAL/Arr_spherical_topology_traits_2.h>
 
+#include "arr_print.h"
+
 typedef CGAL::Exact_predicates_exact_constructions_kernel    Kernel;
-typedef CGAL::Arr_geodesic_arc_on_sphere_traits_2<Kernel>
-// typedef CGAL::Arr_geodesic_arc_on_sphere_traits_2<Kernel, -11, 7>
+typedef CGAL::Arr_geodesic_arc_on_sphere_traits_2<Kernel, -11, 7>
                                                              Geom_traits_2;
 typedef Geom_traits_2::Point_2                               Point_2;
 typedef Geom_traits_2::Curve_2                               Curve_2;
@@ -25,10 +25,8 @@ int main()
   Geom_traits_2 traits;
   Arrangement_2 arr(&traits);
 
-  Geom_traits_2::Construct_point_2 ctr_p = traits.construct_point_2_object();
-  Geom_traits_2::Construct_curve_2 ctr_cv = traits.construct_curve_2_object();
-
-  std::vector<Point_2> points;
+  auto ctr_p = traits.construct_point_2_object();
+  auto ctr_cv = traits.construct_curve_2_object();
 
   // Observe that the identification curve is a meridian that contains the
   // point (-11, 7, 0). The curve (-1,0,0),(0,1,0) intersects the identification
@@ -49,25 +47,9 @@ int main()
   arcs.push_back(ctr_cv(ctr_p(-1, 0, 0), ctr_p(0, 1, 0)));
   arcs.push_back(ctr_cv(ctr_p(-1, 0, 0), ctr_p(0, -1, 0)));
 
-  // CGAL::insert(arr, arcs.begin(), arcs.end());
-  for (const auto& arc : arcs) CGAL::insert(arr, arc);
-
-  // Print the size of the arrangement.
-  std::cout << "The arrangement size:" << std::endl
-            << "   V = " << arr.number_of_vertices()
-            << ",  E = " << arr.number_of_edges()
-            << ",  F = " << arr.number_of_faces() << std::endl;
-
-  for (auto it = arr.halfedges_begin(); it != arr.halfedges_end(); ++ it)
-    std::cout << it->source()->point() << " => "
-              << it->target()->point() << ", "
-              << it->direction() << std::endl;
-
-  std::cout << "# outer ccbs: " << arr.faces_begin()->number_of_outer_ccbs()
-            << std::endl;
-  std::cout << "# inner ccbs: " << arr.faces_begin()->number_of_inner_ccbs()
-            << std::endl;
-
+  CGAL::insert(arr, arcs.begin(), arcs.end());
+  print_arrangement_size(arr);
+  print_arrangement(arr);
 
   return 0;
 }
