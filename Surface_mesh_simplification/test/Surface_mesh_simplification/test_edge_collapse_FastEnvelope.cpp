f31b7fe4e136e0bd5c3b0fc72f94c62edc175ad7
Andreas Fabri <andreas.fabri@geometryfactory.com>
2020-09-09 17:02:03 +0100

    SMS: Use FastEnvelope to bound the error

---
+++ Surface_mesh_simplification/test/Surface_mesh_simplification/test_edge_collapse_FastEnvelope.cpp
@@ -0,0 +1,72 @@
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Surface_mesh.h>
+
+// Simplification function
+#include <CGAL/Surface_mesh_simplification/edge_collapse.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/FastEnvelope_placement.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Count_stop_predicate.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/LindstromTurk_cost.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/LindstromTurk_placement.h>
+
+
+//bbox
+#include <CGAL/Polygon_mesh_processing/bbox.h>
+
+#include <iostream>
+#include <fstream>
+
+namespace SMS = CGAL::Surface_mesh_simplification;
+
+typedef CGAL::Simple_cartesian<double>                        Kernel;
+
+typedef Kernel::Point_3                                       Point_3;
+typedef CGAL::Surface_mesh<Point_3>                           Surface;
+
+typedef SMS::LindstromTurk_cost<Surface>                      Cost;
+typedef SMS::LindstromTurk_placement<Surface>                 Placement;
+
+
+typedef SMS::FastEnvelope_placement<Placement, Kernel>    Filtered_placement;
+
+int main(int argc, char** argv)
+{
+  Surface ref_mesh;
+  std::ifstream is(argc > 1 ? argv[1] : "data/helmet.off");
+  is >> ref_mesh;
+
+  SMS::Count_stop_predicate<Surface> stop(10);
+
+  std::cout << "input has " << num_vertices(ref_mesh) << " vertices." << std::endl;
+  CGAL::Iso_cuboid_3<Kernel> bbox(CGAL::Polygon_mesh_processing::bbox(ref_mesh));
+
+  Point_3 cmin = (bbox.min)();
+  Point_3 cmax = (bbox.max)();
+  const double diag = CGAL::approximate_sqrt(CGAL::squared_distance(cmin, cmax));
+
+  Surface mesh_cpy = ref_mesh; // need a copy to keep the AABB tree valid
+  Surface small_mesh = ref_mesh;
+  Surface big_mesh = ref_mesh;
+
+  Placement placement_ref;
+  SMS::edge_collapse(ref_mesh, stop, CGAL::parameters::get_cost(Cost()).get_placement(placement_ref));
+
+  Filtered_placement placement_small(0.00005*diag, placement_ref);
+  SMS::edge_collapse(small_mesh, stop, CGAL::parameters::get_cost(Cost()).get_placement(placement_small));
+
+  Filtered_placement placement_big(0.1, placement_ref); // lazily builds the AABB tree
+  SMS::edge_collapse(big_mesh, stop, CGAL::parameters::get_cost(Cost()).get_placement(placement_big));
+
+  std::ofstream out("out.off");
+  out << big_mesh << std::endl;
+  out.close();
+
+  std::cout << "no filtering: " << vertices(ref_mesh).size() << " vertices left" << std::endl;
+  std::cout << "large filtering distance: " << vertices(big_mesh).size() << " vertices left" << std::endl;
+  std::cout << "small filtering distance: " << vertices(small_mesh).size() << " vertices left" << std::endl;
+
+  assert(vertices(ref_mesh).size() < vertices(small_mesh).size());
+  assert(vertices(big_mesh).size() < vertices(small_mesh).size());
+  assert(vertices(ref_mesh).size() < vertices(big_mesh).size());
+
+  return EXIT_SUCCESS;
+}
