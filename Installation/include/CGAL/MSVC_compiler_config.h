5c8df6672ad398df65d48967de0146df80117da7
Jane Tournois <jane.tournois@geometryfactory.com>
2020-09-25 14:25:14 +0200

    remove CGAL_CFG_MATCHING_BUG_ variants
    the "matching bug" of msvc has been fixed since at least vs 2015

--- Installation/include/CGAL/MSVC_compiler_config.h
+++ Installation/include/CGAL/MSVC_compiler_config.h
@@ -12,16 +12,6 @@
 #ifndef CGAL_MSVC_COMPILER_CONFIG_H
 #define CGAL_MSVC_COMPILER_CONFIG_H
 
-// For all known version of MSVC. Actually we do not really have a
-// test for that bug.
-#define CGAL_CFG_MATCHING_BUG_6 1
-
-// Fixed since MSVC 2015
-#define CGAL_CFG_MATCHING_BUG_7 1
-
-// for all known version of MSVC
-#define CGAL_CFG_MATCHING_BUG_8 1
-
 // Should be only for MSVC 2012 and 2013 in Release
 #define CGAL_CFG_FPU_ROUNDING_MODE_UNWINDING_VC_BUG 1
 
