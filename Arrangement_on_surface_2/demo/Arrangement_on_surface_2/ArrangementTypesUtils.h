f13532f27f8346f3d4d0e7506d373127a46324d8
Ahmed Essam <theartful.ae@gmail.com>
2020-09-29 15:33:16 +0200

    Fix issue with constexpr functions having multiple returns in msvc2015

--- Arrangement_on_surface_2/demo/Arrangement_on_surface_2/ArrangementTypesUtils.h
+++ Arrangement_on_surface_2/demo/Arrangement_on_surface_2/ArrangementTypesUtils.h
@@ -13,6 +13,7 @@
 #define ARRANGEMENT_DEMO_TYPE_UTILS
 
 #include "ArrangementTypes.h"
+#include <CGAL/assertions.h>
 #include <type_traits>
 
 namespace demo_types
@@ -38,21 +39,61 @@ struct TypeHolder
   using type = T;
 };
 
+namespace details
+{
+template <typename T>
+struct EnumFromTraits
+{
+  static constexpr TraitsType value = TraitsType::NONE;
+};
+template <typename Kernel_>
+struct EnumFromTraits<CGAL::Arr_segment_traits_2<Kernel_>>
+{
+  static constexpr TraitsType value = TraitsType::SEGMENT_TRAITS;
+};
+template <typename SegmentTraits_2_>
+struct EnumFromTraits<CGAL::Arr_polyline_traits_2<SegmentTraits_2_>>
+{
+  static constexpr TraitsType value = TraitsType::POLYLINE_TRAITS;
+};
+template <typename Kernel_>
+struct EnumFromTraits<CGAL::Arr_linear_traits_2<Kernel_>>
+{
+  static constexpr TraitsType value = TraitsType::LINEAR_TRAITS;
+};
+#ifdef CGAL_USE_CORE
+template <class Rat_kernel_, class Alg_kernel_, class Nt_traits_>
+struct EnumFromTraits<
+  CGAL::Arr_conic_traits_2<Rat_kernel_, Alg_kernel_, Nt_traits_>>
+{
+  static constexpr TraitsType value = TraitsType::CONIC_TRAITS;
+};
+template <
+  typename RatKernel_, typename AlgKernel_, typename NtTraits_,
+  typename BoundingTraits_>
+struct EnumFromTraits<CGAL::Arr_Bezier_curve_traits_2<
+  RatKernel_, AlgKernel_, NtTraits_, BoundingTraits_>>
+{
+  static constexpr TraitsType value = TraitsType::BEZIER_TRAITS;
+};
+template <typename Coefficient_>
+struct EnumFromTraits<CGAL::Arr_algebraic_segment_traits_2<Coefficient_>>
+{
+  static constexpr TraitsType value = TraitsType::ALGEBRAIC_TRAITS;
+};
+template <typename AlgebraicKernel_d_1_>
+struct EnumFromTraits<
+  CGAL::Arr_rational_function_traits_2<AlgebraicKernel_d_1_>>
+{
+  static constexpr TraitsType value = TraitsType::RATIONAL_FUNCTION_TRAITS;
+};
+#endif // CGAL_USE_CORE
+}
+
 template <typename T>
 static constexpr TraitsType enumFromArrType()
 {
-  using namespace std;
-
-  if (is_same<T, Seg_arr>::value) return TraitsType::SEGMENT_TRAITS;
-  else if (is_same<T, Pol_arr>::value) return TraitsType::POLYLINE_TRAITS;
-  else if (is_same<T, Lin_arr>::value) return TraitsType::LINEAR_TRAITS;
-#ifdef CGAL_USE_CORE
-  else if (is_same<T, Conic_arr>::value) return TraitsType::CONIC_TRAITS;
-  else if (is_same<T, Alg_seg_arr>::value) return TraitsType::ALGEBRAIC_TRAITS;
-  else if (is_same<T, Bezier_arr>::value) return TraitsType::BEZIER_TRAITS;
-  else if (is_same<T, Rational_arr>::value) return TraitsType::RATIONAL_FUNCTION_TRAITS;
-#endif
-  else return TraitsType::NONE;
+  return details::EnumFromTraits<typename T::Geometry_traits_2>::value;
 }
 
 template <class Lambda>
@@ -60,7 +101,6 @@ static void visitArrangementType(TraitsType tt, Lambda lambda)
 {
   switch (tt)
   {
-  default:
   case TraitsType::SEGMENT_TRAITS:
     lambda(TypeHolder<Seg_arr>{});
     break;
@@ -84,6 +124,8 @@ static void visitArrangementType(TraitsType tt, Lambda lambda)
     lambda(TypeHolder<Rational_arr>{});
     break;
 #endif
+  default:
+    CGAL_error();
   }
 }
 
