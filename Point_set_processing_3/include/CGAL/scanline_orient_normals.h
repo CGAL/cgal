45248526cadfe4012b03d5c8c6c0b96a4f95974f
Simon Giraudot <simon.giraudot@geometryfactory.com>
2020-10-08 15:09:01 +0200

    Better orientation of scanlines using PCA

--- Point_set_processing_3/include/CGAL/scanline_orient_normals.h
+++ Point_set_processing_3/include/CGAL/scanline_orient_normals.h
@@ -19,7 +19,11 @@
 #include <CGAL/boost/graph/Named_function_parameters.h>
 #include <CGAL/boost/graph/named_params_helper.h>
 
-#include <queue>
+#include <CGAL/linear_least_squares_fitting_3.h>
+
+#include <boost/iterator/transform_iterator.hpp>
+
+//#define CGAL_SCANLINE_ORIENT_VERBOSE
 
 namespace CGAL
 {
@@ -30,6 +34,13 @@ namespace Point_set_processing_3
 
 namespace internal
 {
+
+template <typename Vector_3>
+const Vector_3& vertical_vector()
+{
+  static Vector_3 v(0, 0, 1);
+  return v;
+}
 
 template <typename Iterator, typename PointMap,
           typename ScanDirectionFlagMap>
@@ -73,118 +84,237 @@ bool is_end_of_scanline (Iterator scanline_begin, Iterator it,
 }
 
 template <typename Iterator, typename PointMap>
-typename Kernel_traits<typename boost::property_traits
-                       <PointMap>::value_type>::Kernel::Vector_3
-scanline_direction (Iterator begin, Iterator end,
-                    PointMap point_map)
+std::pair<typename Kernel_traits<typename boost::property_traits
+                                 <PointMap>::value_type>::Kernel::Vector_3,
+          typename Kernel_traits<typename boost::property_traits
+                                 <PointMap>::value_type>::Kernel::Vector_3>
+scanline_base (Iterator begin, Iterator end,
+               PointMap point_map)
 {
-  using Vector_3 = typename Kernel_traits<typename boost::property_traits
-                                          <PointMap>::value_type>::Kernel::Vector_3;
+  using Point_3 = typename boost::property_traits<PointMap>::value_type;
+  using Kernel = typename Kernel_traits<Point_3>::Kernel;
+  using Vector_3 = typename Kernel::Vector_3;
+
+  using Line_3 = typename Kernel::Line_3;
+  using Plane_3 = typename Kernel::Plane_3;
+
+  const double limit = CGAL_PI * 30. / 180.;
+
+  Line_3 pca2;
+  linear_least_squares_fitting_3
+    (boost::make_transform_iterator
+     (begin, Property_map_to_unary_function<PointMap>(point_map)),
+      boost::make_transform_iterator
+     (end, Property_map_to_unary_function<PointMap>(point_map)),
+      pca2, Dimension_tag<0>());
+
+  Vector_3 pca_direction = pca2.to_vector();
+  pca_direction = Vector_3 (pca_direction.x(), pca_direction.y(), 0);
+  pca_direction = pca_direction / CGAL::approximate_sqrt(pca_direction.squared_length());
+
+  Plane_3 pca3;
+  linear_least_squares_fitting_3
+    (boost::make_transform_iterator
+     (begin, Property_map_to_unary_function<PointMap>(point_map)),
+     boost::make_transform_iterator
+     (end, Property_map_to_unary_function<PointMap>(point_map)),
+     pca3, Dimension_tag<0>());
+
+  Vector_3 orthogonal = pca3.orthogonal_vector();
+  Vector_3 vertical = CGAL::cross_product (pca_direction, orthogonal);
+  if (vertical * vertical_vector<Vector_3>() < 0)
+    vertical = -vertical;
+
+  vertical = vertical / CGAL::approximate_sqrt(vertical.squared_length());
+
+  if (std::acos(vertical * vertical_vector<Vector_3>()) < limit)
+    return std::make_pair (pca_direction, vertical);
+
+  // if plane diverges from the vertical more than 30 degrees, then
+  // fallback to 0 0 1 vertical vector
+
+  // Dummy begin->end vector version
   Iterator last = end; -- last;
-  Vector_3 direction (get (point_map, *begin),
-                      get (point_map, *last));
-  direction = Vector_3 (direction.x(), direction.y(), 0);
+  const Point_3& pbegin = get (point_map, *begin);
+  const Point_3& plast = get (point_map, *last);
+  Vector_3 direction (Point_3 (pbegin.x(), pbegin.y(), 0),
+                      Point_3 (plast.x(), plast.y(), 0));
   direction = direction / CGAL::approximate_sqrt (direction.squared_length());
-  return direction;
+
+  return std::make_pair (direction, vertical_vector<Vector_3>());
 }
 
-template <typename Iterator, typename PointMap, typename NormalMap,
-          typename ScanDirectionFlagMap, typename ScanAngleMap>
-void orient_scanline (Iterator begin, Iterator end,
-                      PointMap point_map,
-                      NormalMap normal_map,
-                      ScanDirectionFlagMap scan_direction_flag,
-                      ScanAngleMap scan_angle_map,
-                      const Tag_false&, // no fallback direction flag
-                      const Tag_false&) // no fallback scan angle
+template <typename Vector_3>
+bool normal_along_scanline_is_inverted
+(const Vector_3& normal, const Vector_3& line_of_sight)
 {
-  using Vector_3 = typename boost::property_traits<NormalMap>::value_type;
-
-  static const Vector_3 vertical (0, 0, 1);
-
-  Vector_3 direction
-    = Point_set_processing_3::internal::scanline_direction
-    (begin, end, point_map);
-  if (get (scan_direction_flag, *begin) == 1)
-    direction = -direction;
-
-  for (Iterator it = begin; it != end; ++ it)
-  {
-    double angle = CGAL_PI * double(get (scan_angle_map, *it)) / 180.;
-    Vector_3 line_of_sight
-      = direction * std::sin(angle) + vertical * std::cos(angle);
-    const Vector_3& normal = get (normal_map, *it);
-    if (line_of_sight * normal < 0)
-      put (normal_map, *it, -normal);
-  }
+  return (line_of_sight * normal < 0);
 }
 
+template <typename Iterator, typename PointMap, typename Vector_3>
+std::pair<typename boost::property_traits<PointMap>::value_type, bool>
+estimate_scan_position (Iterator begin, Iterator end, PointMap point_map,
+                        const std::vector<Vector_3>& lines_of_sight)
+{
+  using Point_3 = typename boost::property_traits<PointMap>::value_type;
+
+  typedef Eigen::Matrix3d Matrix;
+  typedef Eigen::Vector3d Vector;
+  typedef Eigen::ConjugateGradient<Matrix> Solver;
+
+  Matrix R;
+  R << 0, 0, 0, 0, 0, 0, 0, 0, 0;
+  Vector Q;
+  Q << 0, 0, 0;
+
+  std::size_t idx = 0;
+  for (Iterator it = begin; it != end; ++ it, ++ idx)
+  {
+    const Point_3& p = get (point_map, *it);
+    const Vector_3& v = lines_of_sight[idx];
+
+    Vector n;
+    n << v.x(), v.y(), v.z();
+
+    Matrix I_nnt = Matrix::Identity() - n * n.transpose();
+    Vector a;
+    a << p.x(), p.y(), p.z();
+
+    R += I_nnt;
+    Q += I_nnt * a;
+  }
+
+  Solver solver;
+  solver.compute(R);
+  if (solver.info() != Eigen::Success)
+    return std::make_pair (ORIGIN, false);
+
+  Vector p = solver.solve(Q);
+  if (solver.info() != Eigen::Success)
+    return std::make_pair (ORIGIN, false);
+
+  return std::make_pair (Point_3(p(0), p(1), p(2)), true);
+}
+
+
 template <typename Iterator, typename PointMap, typename NormalMap,
-          typename ScanDirectionFlagMap, typename ScanAngleMap>
+          typename ScanAngleMap>
 void orient_scanline (Iterator begin, Iterator end,
                       PointMap point_map,
                       NormalMap normal_map,
-                      ScanDirectionFlagMap,
                       ScanAngleMap scan_angle_map,
-                      const Tag_true&, // no fallback direction flag
-                      const Tag_false&) // fallback scan angle
+                      const Tag_false&) // no fallback scan angle
 {
   using Point_3 = typename boost::property_traits<PointMap>::value_type;
   using Vector_3 = typename boost::property_traits<NormalMap>::value_type;
 
-  // Estimate scanner position:
-  // above point with minimum scan angle
-  float min_scan_angle = (std::numeric_limits<float>::max)();
-  Iterator chosen = begin;
-  double min_z = (std::numeric_limits<double>::max)();
-  double max_z = -(std::numeric_limits<double>::max)();
+  Vector_3 direction;
+  Vector_3 vertical;
+  std::tie (direction, vertical)
+    = scanline_base (begin, end, point_map);
 
+  std::vector<Vector_3> lines_of_sight;
+  lines_of_sight.reserve (std::distance (begin, end));
+
+  double mean_z = 0;
   for (Iterator it = begin; it != end; ++ it)
   {
-    const Point_3& p = get (point_map, *it);
-    min_z = (std::min(min_z, p.z()));
-    max_z = (std::max(max_z, p.z()));
-    float scan_angle = get (scan_angle_map, *it);
-    if (scan_angle < min_scan_angle)
+    double angle = CGAL_PI * double(get (scan_angle_map, *it)) / 180.;
+    Vector_3 los = direction * std::sin(angle) + vertical * std::cos(angle);
+    lines_of_sight.push_back (los);
+    mean_z += get (point_map, *it).z();
+  }
+  mean_z /= std::distance (begin, end);
+
+#ifdef CGAL_SCANORIENT_DUMP_RANDOM_SCANLINES
+  if (rand() % 1000 == 0 && std::distance(begin, end) > 10)
+  {
+    std::ofstream ofile ("scanline.polylines.txt");
+    ofile.precision(18);
+    ofile << std::distance (begin, end);
+    for (Iterator it = begin; it != end; ++ it)
+      ofile << " " << get(point_map, *it);
+    ofile << std::endl;
+
+    std::ofstream ofile2 ("base.polylines.txt");
+    Point_3 orig = get (point_map, *(begin + std::distance(begin, end) / 2));
+    double dist = CGAL::approximate_sqrt (CGAL::squared_distance
+                                          (get(point_map, *begin), orig));
+
+    ofile2.precision(18);
+    ofile2 << "2 " << orig << " " << orig + direction * dist << std::endl
+           << "2 " << orig << " " << orig + vertical * dist << std::endl;
+  }
+#endif
+
+  Point_3 scan_position;
+  bool solver_success;
+  std::tie (scan_position, solver_success)
+    = estimate_scan_position (begin, end, point_map, lines_of_sight);
+
+  // If solver failed OR if scan position is detected under the
+  // scanline (obviously wrong)
+  if (!solver_success || scan_position.z() < mean_z)
+  {
+#ifdef CGAL_SCANLINE_ORIENT_VERBOSE
+    if (!solver_success)
+      std::cerr << "Inverting because olver failed: ";
+    else
+      std::cerr << "Inverting because scanner under scanline: ";
+#endif
+
+    direction = -direction;
+    std::size_t idx = 0;
+    for (Iterator it = begin; it != end; ++ it, ++ idx)
     {
-      min_scan_angle = scan_angle;
-      chosen = it;
+      double angle = CGAL_PI * double(get (scan_angle_map, *it)) / 180.;
+      Vector_3 los = direction * std::sin(angle) + vertical * std::cos(angle);
+      lines_of_sight[idx] = los;
     }
+
+    std::tie (scan_position, solver_success)
+      = estimate_scan_position (begin, end, point_map, lines_of_sight);
+
+#ifdef CGAL_SCANLINE_ORIENT_VERBOSE
+    if (solver_success && scan_position.z() > mean_z)
+      std::cerr << "SOLVED" << std::endl;
+    else if (!solver_success)
+      std::cerr << "FAILED, solver failure" << std::endl;
+    else
+      std::cerr << "FAILED, scanner under scanline" << std::endl;
+#endif
   }
 
-  const Point_3& chosen_point = get (point_map, *chosen);
-  Point_3 scan_position (chosen_point.x(),
-                         chosen_point.y(),
-                         min_z + 10 * (max_z - min_z));
-
-  for (Iterator it = begin; it != end; ++ it)
+  std::size_t idx = 0;
+  for (Iterator it = begin; it != end; ++ it, ++ idx)
   {
-    Vector_3 line_of_sight (get(point_map, *it), scan_position);
+    const Vector_3 los = lines_of_sight[idx];
     const Vector_3& normal = get (normal_map, *it);
-    if (line_of_sight * normal < 0)
+    if (normal_along_scanline_is_inverted (normal, los))
       put (normal_map, *it, -normal);
   }
 }
 
 template <typename Iterator, typename PointMap, typename NormalMap,
-          typename ScanDirectionFlagMap, typename ScanAngleMap,
-          typename FallbackFlag>
+          typename ScanAngleMap>
 void orient_scanline (Iterator begin, Iterator end,
                       PointMap point_map,
                       NormalMap normal_map,
-                      ScanDirectionFlagMap,
                       ScanAngleMap,
-                      const FallbackFlag&, // either fallback direction flag or not
                       const Tag_true&) // fallback scan angle
 {
   using Point_3 = typename boost::property_traits<PointMap>::value_type;
   using Vector_3 = typename boost::property_traits<NormalMap>::value_type;
 
+  Vector_3 direction;
+  Vector_3 vertical;
+  std::tie (direction, vertical)
+    = scanline_base (begin, end, point_map);
+
   // Estimate scanner position:
   // average XY-projected point, located above
   double mean_x = 0.;
   double mean_y = 0.;
-  double min_z = (std::numeric_limits<double>::max)();
   double max_z = -(std::numeric_limits<double>::max)();
   std::size_t nb = 0;
 
@@ -193,19 +323,23 @@ void orient_scanline (Iterator begin, Iterator end,
     const Point_3& p = get (point_map, *it);
     mean_x += p.x();
     mean_y += p.y();
-    min_z = (std::min(min_z, p.z()));
     max_z = (std::max(max_z, p.z()));
     ++ nb;
   }
 
+  Iterator last = end; -- last;
+  double length = CGAL::approximate_sqrt (CGAL::squared_distance
+                                          (get (point_map, *begin),
+                                           get (point_map, *last)));
+
   Point_3 scan_position (mean_x / nb, mean_y / nb,
-                         min_z + 10 * (max_z - min_z));
+                         max_z + length * 2);
 
   for (Iterator it = begin; it != end; ++ it)
   {
     Vector_3 line_of_sight (get(point_map, *it), scan_position);
     const Vector_3& normal = get (normal_map, *it);
-    if (line_of_sight * normal < 0)
+    if (normal_along_scanline_is_inverted (normal, line_of_sight))
       put (normal_map, *it, -normal);
   }
 }
@@ -353,17 +487,48 @@ void scanline_orient_normals (PointRange& points, const NamedParameters& np)
 
   std::size_t nb_scanlines = 1;
 
+#ifdef CGAL_SCANORIENT_DUMP_RANDOM_SCANLINES
+  std::ofstream ofile ("scanlines.polylines.txt");
+  ofile.precision(18);
+#endif
+
+  CGAL::Bbox_3 bbox = CGAL::bbox_3
+    (boost::make_transform_iterator
+     (points.begin(), Property_map_to_unary_function<PointMap>(point_map)),
+     boost::make_transform_iterator
+     (points.end(), Property_map_to_unary_function<PointMap>(point_map)));
+
+  double bbox_diagonal
+    = CGAL::approximate_sqrt((bbox.xmax() - bbox.xmin()) * (bbox.xmax() - bbox.xmin())
+                             + (bbox.ymax() - bbox.ymin()) * (bbox.ymax() - bbox.ymin())
+                             + (bbox.zmax() - bbox.zmin()) * (bbox.zmax() - bbox.zmin()));
+  double limit = 0.05 * bbox_diagonal;
+
   Iterator scanline_begin = points.begin();
   for (Iterator it = points.begin(); it != points.end(); ++ it)
   {
+    bool force_cut = false;
+    if (it != points.begin())
+    {
+      Iterator prev = it; -- prev;
+      force_cut = (CGAL::squared_distance (get (point_map, *prev),
+                                           get (point_map, *it)) > limit * limit);
+    }
+
     if (Point_set_processing_3::internal::is_end_of_scanline
         (scanline_begin, it, point_map, scan_direction_flag_map,
-         Fallback_scan_direction_flag()))
+         Fallback_scan_direction_flag()) || force_cut)
     {
       Point_set_processing_3::internal::orient_scanline
         (scanline_begin, it, point_map, normal_map,
-         scan_direction_flag_map, scan_angle_map,
-         Fallback_scan_direction_flag(), Fallback_scan_angle());
+         scan_angle_map, Fallback_scan_angle());
+
+#ifdef CGAL_SCANORIENT_DUMP_RANDOM_SCANLINES
+      ofile << std::distance (scanline_begin, it);
+      for (Iterator it2 = scanline_begin; it2 != it; ++ it2)
+        ofile << " " << get (point_map, *it2);
+      ofile << std::endl;
+#endif
 
       scanline_begin = it;
       ++ nb_scanlines;
@@ -372,12 +537,13 @@ void scanline_orient_normals (PointRange& points, const NamedParameters& np)
 
   Point_set_processing_3::internal::orient_scanline
     (scanline_begin, points.end(), point_map, normal_map,
-     scan_direction_flag_map, scan_angle_map,
-     Fallback_scan_direction_flag(), Fallback_scan_angle());
+     scan_angle_map, Fallback_scan_angle());
 
+#ifdef CGAL_SCANLINE_ORIENT_VERBOSE
   std::cerr << nb_scanlines << " scanline(s) identified (mean length = "
             << std::size_t(points.size() / double(nb_scanlines))
             << " point(s))" << std::endl;
+#endif
 }
 
 template <typename PointRange>
