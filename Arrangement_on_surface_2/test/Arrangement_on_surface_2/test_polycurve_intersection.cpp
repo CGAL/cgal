9fb9efc7eb6ee8a39d84723f825a06fd28447215
Simon Giraudot <simon.giraudot@geometryfactory.com>
2020-10-19 15:36:12 +0200

    Update test

--- Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_polycurve_intersection.cpp
+++ Arrangement_on_surface_2/test/Arrangement_on_surface_2/test_polycurve_intersection.cpp
@@ -20,16 +20,30 @@ typedef Geom_traits_2::X_monotone_subcurve_2              X_monotone_subcurve;
 
 struct Test_functor
 {
+  const X_monotone_polyline* reference;
+
+  Test_functor (const X_monotone_polyline& reference)
+    : reference (&reference) { }
+
   void operator() (const CGAL::Object& obj) const
   {
-    const X_monotone_polyline* poly
+     const X_monotone_polyline* poly
       = CGAL::object_cast<X_monotone_polyline>(&obj);
     CGAL_assertion_msg (poly != nullptr, "Intersection is not a polyline");
-    std::cerr << " * Intersection = " << *poly << std::endl;
+
+    typename X_monotone_polyline::Point_const_iterator
+      itref = reference->points_begin(),
+      itpoly = poly->points_begin();
+
+    for (; itref != reference->points_end()
+           && itpoly != poly->points_end();
+         ++ itref, ++ itpoly)
+      CGAL_assertion (*itref == *itpoly);
   }
 };
 
-void test (const X_monotone_polyline& a, const X_monotone_polyline& b)
+void test (const X_monotone_polyline& a, const X_monotone_polyline& b,
+           const X_monotone_polyline& reference)
 {
   Geom_traits_2 traits;
   Geom_traits_2::Intersect_2 intersect_2 =
@@ -39,7 +53,7 @@ void test (const X_monotone_polyline& a, const X_monotone_polyline& b)
             << " * Polyline B = " << b << std::endl;
 
   intersect_2
-    (a, b, boost::make_function_output_iterator (Test_functor()));
+    (a, b, boost::make_function_output_iterator (Test_functor(reference)));
 }
 
 int main(int argc, char* argv[])
@@ -69,15 +83,14 @@ int main(int argc, char* argv[])
   X_monotone_polyline p1r2l
     = x_mono_polyline_construct (r2l.begin(), r2l.end());
 
-
   std::cerr << "Testing intersection left-to-right / left-to-right" << std::endl;
-  test (p0l2r, p1l2r);
+  test (p0l2r, p1l2r, p0l2r);
   std::cerr << "Testing intersection left-to-right / right-to-left" << std::endl;
-  test (p0l2r, p1r2l);
+  test (p0l2r, p1r2l, p0l2r);
   std::cerr << "Testing intersection right-to-left / left-to-right" << std::endl;
-  test (p0r2l, p1l2r);
+  test (p0r2l, p1l2r, p0l2r);
   std::cerr << "Testing intersection right-to-left / right-to-left" << std::endl;
-  test (p0r2l, p1r2l);
+  test (p0r2l, p1r2l, p0r2l);
 
   return EXIT_SUCCESS;
 }
