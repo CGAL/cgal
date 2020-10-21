09974cf643faaae629f7e1444cc9a5d9b410e6bb
Michael Hemmer <hemmer@google.com>
2020-10-21 15:27:28 +0200

    Update Refine_edges_with_clusters.h
    Use CGAL_NTS macro instead of CGAL::to_double

--- Mesh_2/include/CGAL/Mesh_2/Refine_edges_with_clusters.h
+++ Mesh_2/include/CGAL/Mesh_2/Refine_edges_with_clusters.h
@@ -274,8 +274,8 @@ private:
 
         typename Geom_traits::Vector_2 v = vector(a,m);
         v = scaled_vector(
-            v, CGAL_NTS sqrt(CGAL::to_double(c.minimum_squared_length /
-                                             squared_distance(a, b))));
+            v, CGAL_NTS sqrt(CGAL_NTS to_double(c.minimum_squared_length /
+                                                squared_distance(a, b))));
 
 
         Point i = translate(a,v), i2(i);
