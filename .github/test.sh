aaeed9fedc585a3c55d8036d796c791181e8f39c
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-11-02 09:28:02 +0100

    Ignore 3mf-plugin

--- .github/test.sh
+++ .github/test.sh
@@ -4,7 +4,8 @@ set -ex
 cd Polyhedron/demo
 LIST_OF_PLUGINS=$(for f in $(find ./Polyhedron/Plugins -iname "CMakeLists.txt" |egrep -v Three_examples); do\
   egrep "polyhedron_demo_plugin" $f |cut -d\( -f2 | cut -d" " -f1|sort -u|egrep -v "partition_plugin"\
- |egrep -v "basic_item_plugin"|egrep -v "vtk_plugin" |egrep -v "register_point_sets_plugin"| egrep -v "classification_plugin" | egrep -v "io_image_plugin" ||true; done)
+ |egrep -v "basic_item_plugin"|egrep -v "vtk_plugin" |egrep -v "register_point_sets_plugin"| egrep -v "classification_plugin" \
+ | egrep -v "io_image_plugin"| egrep -v "3mf_io_plugin" ||true; done)
 PLUGINS_ARRAY=(${LIST_OF_PLUGINS});
 NB_OF_PLUGINS=${#PLUGINS_ARRAY[@]}
 DEL=$(($NB_OF_PLUGINS / 4))
