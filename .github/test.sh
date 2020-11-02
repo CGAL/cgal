b421fde9a1bb3a4008956872704401ca3a979ed3
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-11-02 09:53:31 +0100

    use a far better way to list the plugins

--- .github/test.sh
+++ .github/test.sh
@@ -3,10 +3,7 @@ cmake --version
 FACTOR=$1
 set -ex
 cd Polyhedron/demo
-LIST_OF_PLUGINS=$(for f in $(find ./Polyhedron/Plugins -iname "CMakeLists.txt" |egrep -v Three_examples); do\
-  egrep "polyhedron_demo_plugin" $f |cut -d\( -f2 | cut -d" " -f1|sort -u|egrep -v "partition_plugin"\
- |egrep -v "basic_item_plugin"|egrep -v "vtk_plugin" |egrep -v "register_point_sets_plugin"| egrep -v "classification_plugin" \
- | egrep -v "io_image_plugin"| egrep -v "3mf_io_plugin" ||true; done)
+LIST_OF_PLUGINS=$(cmake --build . -t help | egrep 'plugin$' |& cut -d\  -f2)
 PLUGINS_ARRAY=(${LIST_OF_PLUGINS});
 NB_OF_PLUGINS=${#PLUGINS_ARRAY[@]}
 DEL=$(($NB_OF_PLUGINS / 4))
