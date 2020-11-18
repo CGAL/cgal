1bfa6525f794eed1680b84e408bed42405a83bdf
Giles Bathgate <giles.bathgate@gmail.com>
2020-11-18 07:43:12 +0000

    Fix uninitialized member in Filtered_predicate.h

--- Filtered_kernel/include/CGAL/Filtered_predicate.h
+++ Filtered_kernel/include/CGAL/Filtered_predicate.h
@@ -59,6 +59,7 @@ public:
   // AP::result_type must be convertible to EP::result_type.
 
   Filtered_predicate()
+    : c2e{}, c2a{}
   {}
 
   // These constructors are used for constructive predicates.
