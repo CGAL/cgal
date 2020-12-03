0edbd988983a05056f4ed16b0971076bd046ba3b
Sébastien Loriot <sebastien.loriot@cgal.org>
2020-12-03 14:36:43 +0100

    update example + add it in the user manual

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/polyhedral_envelope_mesh_containment.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/polyhedral_envelope_mesh_containment.cpp
@@ -1,39 +1,46 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polyhedral_envelope.h>
+#include <CGAL/Polygon_mesh_processing/remesh.h>
 #include <CGAL/Surface_mesh.h>
 
 #include <fstream>
+
+namespace PMP = CGAL::Polygon_mesh_processing;
 
 int main(int argc, char* argv[])
 {
   typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
   typedef Kernel::Point_3 Point_3;
   typedef CGAL::Surface_mesh<Point_3> Surface_mesh;
-  typedef boost::graph_traits<Surface_mesh>::halfedge_descriptor halfedge_descriptor;
 
   typedef CGAL::Polyhedral_envelope<Kernel> Envelope;
 
-  std::ifstream in((argc>1) ? argv[1] : "data/patch.off");
-  std::ifstream in2((argc>2) ? argv[2] : "data/tentative.off");
-  Surface_mesh tmesh, test;
-
+  std::ifstream in((argc>1) ? argv[1] : "data/blobby.off");
+  Surface_mesh tmesh;
   in >> tmesh;
-  in2 >> test;
 
-  double eps = (argc>3) ? std::stod(std::string(argv[3])) : 0.2;
+  // remesh the input using the longest edge size as target edge length
+  Surface_mesh query = tmesh;
+  Surface_mesh::Edge_iterator longest_edge_it =
+    std::max_element(edges(query).begin(), edges(query).end(),
+                     [&query](Surface_mesh::Edge_index e1, Surface_mesh::Edge_index e2)
+                     {
+                        return PMP::edge_length(halfedge(e1, query), query) <
+                               PMP::edge_length(halfedge(e2, query), query);
+                     });
+  PMP::isotropic_remeshing(faces(tmesh), PMP::edge_length(halfedge(*longest_edge_it, query), query), query);
 
-  Envelope envelope;
-  envelope = Envelope(tmesh, eps);
+  // construct the polyhedral envelope
+  const double eps = (argc>2) ? std::stod(std::string(argv[2])) : 0.01;
+  Envelope envelope(tmesh, eps);
 
-  for(auto fd : faces(test)){
-    halfedge_descriptor hd = halfedge(fd, test);
-    Point_3 p = test.point(target(hd,test));
-    Point_3 q = test.point(target(next(hd,test),test));
-    Point_3 r = test.point(source(hd,test));
-    if(envelope(p,q,r)){
-      std::cout << "inside polyhedral envelope" << std::endl;
-    }
-  }
-  std::cout << "done" << std::endl;
+  // check is the remeshed version is inside the polyhedral envelope of the input mesh
+  if ( envelope(query) )
+    std::cout << "Remeshing is inside the polyhedral envelope\n";
+  else
+    std::cout << "Remeshing is not inside the polyhedral envelope\n";
+
+  std::ofstream("remeshed.off") << query;
+
   return 0;
 }
