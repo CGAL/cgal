fa22cd972b660b54894c5907c19fdf06cff38d8e
Maxime Gimeno <maxime.gimeno@gmail.com>
2021-01-14 13:58:15 +0100

    Change libpointmatcher_support so it won't fail when old boost has been used to build pointmatcher.

--- Installation/cmake/modules/CGAL_pointmatcher_support.cmake
+++ Installation/cmake/modules/CGAL_pointmatcher_support.cmake
@@ -1,7 +1,6 @@
 if(libpointmatcher_FOUND AND NOT TARGET CGAL::pointmatcher_support)
   add_library(CGAL::pointmatcher_support INTERFACE IMPORTED)
-  set_target_properties(CGAL::pointmatcher_support PROPERTIES
-    INTERFACE_COMPILE_DEFINITIONS "CGAL_LINKED_WITH_POINTMATCHER"
-    INTERFACE_INCLUDE_DIRECTORIES "${libpointmatcher_INCLUDE_DIR}"
-    INTERFACE_LINK_LIBRARIES "${libpointmatcher_LIBRARIES}")
+  target_compile_definitions(CGAL::pointmatcher_support INTERFACE "CGAL_LINKED_WITH_POINTMATCHER")
+  target_include_directories(CGAL::pointmatcher_support INTERFACE "${libpointmatcher_INCLUDE_DIR}")
+  target_link_libraries(CGAL::pointmatcher_support INTERFACE "${libpointmatcher_LIBRARIES}")
 endif()
