99a31e07fe31d26133eea6d1bcbd2aac5f24a122
Maxime Gimeno <maxime.gimeno@gmail.com>
2021-01-15 14:03:12 +0100

    Move the Boost components in the support file and condition it to windows.

--- Installation/cmake/modules/CGAL_pointmatcher_support.cmake
+++ Installation/cmake/modules/CGAL_pointmatcher_support.cmake
@@ -1,6 +1,19 @@
 if(libpointmatcher_FOUND AND NOT TARGET CGAL::pointmatcher_support)
-  add_library(CGAL::pointmatcher_support INTERFACE IMPORTED)
-  target_compile_definitions(CGAL::pointmatcher_support INTERFACE "CGAL_LINKED_WITH_POINTMATCHER")
-  target_include_directories(CGAL::pointmatcher_support INTERFACE "${libpointmatcher_INCLUDE_DIR}")
-  target_link_libraries(CGAL::pointmatcher_support INTERFACE "${libpointmatcher_LIBRARIES}")
+  if(WIN32 OR CMAKE_SYSTEM_NAME STREQUAL Windows)
+    find_package(Boost COMPONENTS thread filesystem system program_options date_time chrono)
+  endif()
+  if(NOT (WIN32 OR CMAKE_SYSTEM_NAME STREQUAL Windows)
+      OR ( Boost_chrono_FOUND
+      AND Boost_thread_FOUND
+      AND Boost_filesystem_FOUND
+      AND Boost_system_FOUND
+      AND Boost_program_options_FOUND
+      AND Boost_date_time_FOUND) )
+    add_library(CGAL::pointmatcher_support INTERFACE IMPORTED)
+    target_compile_definitions(CGAL::pointmatcher_support INTERFACE "CGAL_LINKED_WITH_POINTMATCHER")
+    target_include_directories(CGAL::pointmatcher_support INTERFACE "${libpointmatcher_INCLUDE_DIR}")
+    target_link_libraries(CGAL::pointmatcher_support INTERFACE "${libpointmatcher_LIBRARIES}")
+  else()
+    message(STATUS "NOTICE : the libpointmatcher library requires the following boost components: thread filesystem system program_options date_time chrono.")
+  endif()
 endif()
