dc8f15271a7272dddc9476f36464856730e0d16a
Sébastien Loriot <sebastien.loriot@cgal.org>
2021-01-22 13:19:36 +0100

    expand *

--- Point_set_processing_3/test/Point_set_processing_3/analysis_test.cmd
+++ Point_set_processing_3/test/Point_set_processing_3/analysis_test.cmd
@@ -1,1 +1,4 @@
-data/*.xyz
+data/before_upsample.xyz
+data/fin90_with_PCA_normals.xyz
+data/sphere_1k.xyz
+data/sphere_20k.xyz
--- Poisson_surface_reconstruction_3/test/Poisson_surface_reconstruction_3/poisson_reconstruction_test.cmd
+++ Poisson_surface_reconstruction_3/test/Poisson_surface_reconstruction_3/poisson_reconstruction_test.cmd
@@ -1,1 +1,1 @@
-data/*.off data/*.xyz data/*.pwn
+data/ChineseDragon-10kv.off  data/oni.pwn  data/robocat_deci.off  data/sphere_20k.xyz
