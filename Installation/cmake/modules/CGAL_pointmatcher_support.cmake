4ed3b0129c3894758ac921cd7baa6468b521950c
Maxime Gimeno <maxime.gimeno@gmail.com>
2021-01-25 15:35:27 +0100

    make the dependencies to boost general, not just for windows

--- Installation/cmake/modules/CGAL_pointmatcher_support.cmake
+++ Installation/cmake/modules/CGAL_pointmatcher_support.cmake
@@ -1,14 +1,11 @@
 if(libpointmatcher_FOUND AND NOT TARGET CGAL::pointmatcher_support)
-  if(WIN32 OR CMAKE_SYSTEM_NAME STREQUAL Windows)
-    find_package(Boost COMPONENTS thread filesystem system program_options date_time chrono)
-  endif()
-  if(NOT (WIN32 OR CMAKE_SYSTEM_NAME STREQUAL Windows)
-      OR ( Boost_chrono_FOUND
+  find_package(Boost COMPONENTS thread filesystem system program_options date_time chrono)
+  if(Boost_chrono_FOUND
       AND Boost_thread_FOUND
       AND Boost_filesystem_FOUND
       AND Boost_system_FOUND
       AND Boost_program_options_FOUND
-      AND Boost_date_time_FOUND) )
+      AND Boost_date_time_FOUND)
     add_library(CGAL::pointmatcher_support INTERFACE IMPORTED)
     target_compile_definitions(CGAL::pointmatcher_support INTERFACE "CGAL_LINKED_WITH_POINTMATCHER")
     target_include_directories(CGAL::pointmatcher_support INTERFACE "${libpointmatcher_INCLUDE_DIR}")
