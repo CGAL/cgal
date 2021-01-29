e6dcea8d7f811498211b9eb5a2726084afedf451
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2021-01-29 12:11:29 +0100

    Start cleaning TOS2 demo

--- Triangulation_on_sphere_2/demo/Triangulation_on_sphere_2/main.cpp
+++ Triangulation_on_sphere_2/demo/Triangulation_on_sphere_2/main.cpp
@@ -1,55 +1,63 @@
-//Author: Sebastien Loriot sebastien.loriot@sophia.inria.fr
-#include <boost/iterator/transform_iterator.hpp>
+// Author: Sebastien Loriot sebastien.loriot@sophia.inria.fr
+
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Exact_spherical_kernel_3.h>
+
 #include <CGAL/Delaunay_triangulation_sphere_traits_2.h>
 #include <CGAL/Projection_sphere_traits_3.h>
 #include <CGAL/Delaunay_triangulation_on_sphere_2.h>
+
 #include <CGAL/utility.h>
 #include <CGAL/Qt/DemosMainWindow.h>
+
 #include <QMainWindow>
 #include <QFileDialog>
+
+#include <boost/iterator/transform_iterator.hpp>
+
 #include <list>
 #include <fstream>
 
 #include <qapplication.h>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel         Kernel;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel          Kernel;
+typedef Kernel::Point_3                                              Point_3;
 
 #include "simpleViewer.h"
 #include "ui_Mainwindow.h"
 
+typedef CGAL::Projection_sphere_traits_3<Kernel>                     Projection_traits;
+typedef CGAL::Delaunay_triangulation_on_sphere_2<Projection_traits>  Projected_DToS2;
 
-typedef CGAL::Projection_sphere_traits_3<Kernel>          Projection_traits;
-typedef CGAL::Delaunay_triangulation_on_sphere_2<Projection_traits>                 Projected_DToS2;
-
-
-struct Cell_info{
-  Kernel::Point_3* pt;
-  Cell_info():pt(NULL){}
-  ~Cell_info(){if (pt!=NULL) delete pt;}
+struct Cell_info
+{
+  Point_3* pt;
+  Cell_info() : pt(nullptr) { }
+  ~Cell_info() { if(pt != nullptr) delete pt; }
 };
 
-typedef Kernel::Point_3                             Point_3;
 Projection_traits traits(Kernel::Point_3(0,0,0));
 Projected_DToS2 dtos(traits);
 
 template <class Output_iterator>
-void read_points(const char* file_path,Output_iterator out){
+void read_points(const char* file_path,Output_iterator out)
+{
   int nb;
   double x,y,z;
   std::ifstream input(file_path);
-  if (!input){
+  if(!input)
+  {
     std::cerr << "Error while reading " << file_path << std::endl;
-    exit(EXIT_FAILURE);
+    std::exit(EXIT_FAILURE);
   }
-  
+
   input >> nb;
-  
-  for (int i=0;i<nb;++i){
-    #warning tmp : must handle the sphere on which are points (parameter of the traits?)
+
+  for(int i=0; i<nb; ++i)
+  {
+    // @warning tmp : must handle the sphere on which are points (parameter of the traits?)
     input >> x >> y >> z;
-    *out++=Point_3(x/100.,y/100.,z/100.);
+    *out++ = Point_3(x/100., y/100., z/100.);
   }
 }
 
@@ -59,42 +67,38 @@ class MainWindow :
 {
   Q_OBJECT
 public:
-  MainWindow() {
+  MainWindow()
+  {
     setupUi(this);
   }
-public slots:
-  void open(QString filename) {
 
+public slots:
+  void open(QString filename)
+  {
     std::vector<Point_3> lst_pt;
-    read_points(filename.toUtf8().data(),
-                std::back_inserter(lst_pt));
-  
-        
-  Projection_traits::Construct_projected_point_3 cst =
-  traits.construct_projected_point_3_object();
-  dtos.insert(
-    boost::make_transform_iterator(lst_pt.begin(), cst),
-    boost::make_transform_iterator(lst_pt.end(), cst)
-   );
-    
-       
+    read_points(filename.toUtf8().data(), std::back_inserter(lst_pt));
+
+    Projection_traits::Construct_projected_point_3 cst = traits.construct_point_on_sphere_2_object();
+    dtos.insert(boost::make_transform_iterator(lst_pt.begin(), cst),
+                boost::make_transform_iterator(lst_pt.end(), cst));
 
     Point_3 center(0,0,0);
-    double scale=1;
-  
+    double scale = 1;
+
     MainWindow mainWindow;
     mainWindow.show();
-  
-    // Instantiate the viewer.
-    viewer->open(lst_pt.begin(),lst_pt.end(),dtos,center,scale);
-    
-      }
 
-  void on_action_Quit_triggered() {
+    // Instantiate the viewer.
+    viewer->open(lst_pt.begin(), lst_pt.end(), dtos, center, scale);
+  }
+
+  void on_action_Quit_triggered()
+  {
     close();
   }
 
-  void on_action_Open_triggered() {
+  void on_action_Open_triggered()
+  {
     QString filename = QFileDialog::getOpenFileName(this);
     if(!filename.isNull())
       open(filename);
@@ -111,9 +115,8 @@ int main(int argc, char** argv)
 
   MainWindow mainWindow;
   if(!args.empty())
-  {
     mainWindow.open(args[0]);
-  }  
+
   mainWindow.show();
 
   // Run main loop.
