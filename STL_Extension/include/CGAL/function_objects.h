55e23ae840b833f931752980069e73119540021a
Andreas Fabri <andreas.fabri@geometryfactory.com>
2021-01-29 10:37:41 +0000

    Remove types from Binary_compose_2 (which is not documented

--- STL_Extension/include/CGAL/function_objects.h
+++ STL_Extension/include/CGAL/function_objects.h
@@ -426,18 +426,13 @@ compose1_2(const Op1& op1, const Op2& op2)
 
 template < class Op1, class Op2, class Op3 >
 class Binary_compose_2
-  : public CGAL::cpp98::binary_function< typename Op2::argument_type,
-                                         typename Op3::argument_type,
-                                         typename Op1::result_type >
+
 {
 protected:
   Op1 op1;
   Op2 op2;
   Op3 op3;
 public:
-  typedef typename Op2::argument_type  first_argument_type;
-  typedef typename Op3::argument_type  second_argument_type;
-  typedef typename Op1::result_type    result_type;
 
   Binary_compose_2(const Op1& x, const Op2& y, const Op3& z)
   : op1(x), op2(y), op3(z) {}
@@ -455,7 +450,8 @@ public:
     return *this;
   }
 
-  result_type
+  template <typename first_argument_type, typename second_argument_type>
+  decltype(auto)
   operator()(const first_argument_type& x,
              const second_argument_type& y) const
   { return op1(op2(x), op3(y)); }
