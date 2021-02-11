d39744380e1747f9677444cca2f1bdbec40f940f
Maxime Gimeno <maxime.gimeno@gmail.com>
2021-02-11 15:05:10 +0100

    Fix script

--- Scripts/developer_scripts/cgal_demo_copy_all_dlls_cygwin.sh
+++ Scripts/developer_scripts/cgal_demo_copy_all_dlls_cygwin.sh
@@ -12,9 +12,9 @@ fi
 
 copy_dll()
 {
-  local dll_full_path="`cygpath --unix --absolute $1`"
-  echo "copy " $dll_full_path " to " $2
-  cp $dll_full_path $2
+  local dll_full_path=$(cygpath --unix --absolute "$1")
+  echo "copy $dll_full_path to $2"
+  cp "$dll_full_path" "$2"
 }
 
 
@@ -25,12 +25,12 @@ files+=(Plugins/*/$config/*.dll)
 for file in "${files[@]}"; do
 
   # copy exe or dll
-  copy_dll $file $target_directory
+  copy_dll "$file" "$target_directory"
 
   # list and copy dependencies
-  cygcheck $file | while read -r dll ; do
+  cygcheck "$file" | while read -r dll ; do
   
-    copy_dll $dll $target_directory
+    copy_dll "$dll" "$target_directory"
   
   done; #check dependencies
     
