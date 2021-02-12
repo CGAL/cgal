cd37d33a3073b6c2ff39f0f0d0df69734f127f98
Dmitry Anisimov <danston@ymail.com>
2021-02-12 15:07:01 +0100

    hilbert sort, added a visibility macro with the hidden option

--- Spatial_sorting/include/CGAL/Hilbert_sort_median_3.h
+++ Spatial_sorting/include/CGAL/Hilbert_sort_median_3.h
@@ -83,8 +83,12 @@ struct Hilbert_cmp_3<K,2,false>
 
 } // namespace internal
 
+#ifdef __clang__
+#define CGAL_VISIBILITY_MACRO __attribute__ ((visibility ("hidden")))
+#endif
+
 template <class K, class ConcurrencyTag>
-class Hilbert_sort_median_3
+class CGAL_VISIBILITY_MACRO Hilbert_sort_median_3
 {
 public:
   typedef Hilbert_sort_median_3<K, ConcurrencyTag> Self;
