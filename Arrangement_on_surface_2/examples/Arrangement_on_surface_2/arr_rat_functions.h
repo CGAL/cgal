839ee6ecd211ab00a315b50a010d86c8a6839542
Efi Fogel <efifogel@gmail.com>
2021-03-02 15:10:29 +0200

    Removed trailing spaces

--- Arrangement_on_surface_2/examples/Arrangement_on_surface_2/arr_rat_functions.h
+++ Arrangement_on_surface_2/examples/Arrangement_on_surface_2/arr_rat_functions.h
@@ -8,8 +8,8 @@
 #include <CGAL/Arrangement_2.h>
 
 typedef CORE::BigInt                               Number_type;
-typedef CGAL::Algebraic_kernel_d_1<Number_type>    AK1; 
-typedef CGAL::Arr_rational_function_traits_2<AK1>  Traits; 
+typedef CGAL::Algebraic_kernel_d_1<Number_type>    AK1;
+typedef CGAL::Arr_rational_function_traits_2<AK1>  Traits;
 
 typedef Traits::Polynomial_1                       Polynomial;
 typedef Traits::Algebraic_real_1                   Alg_real;
