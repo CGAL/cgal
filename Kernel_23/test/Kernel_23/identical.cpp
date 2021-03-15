5aecb82f7b5764c7d94eb36eed0fb9a6c81a1ba6
Andreas Fabri <andreas.fabri@geometryfactory.com>
2021-03-15 19:00:45 +0100

    Construct points where inf !=sup for the intervals

--- Kernel_23/test/Kernel_23/identical.cpp
+++ Kernel_23/test/Kernel_23/identical.cpp
@@ -1,4 +1,4 @@
-#define CGAL_IDENTICAL
+#define TEST_IDENTICAL
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Timer.h>
@@ -8,6 +8,7 @@
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel K;
 typedef K::Point_3 Point_3;
+typedef K::Vector_3 Vector_3;
 
 
 int main(){
@@ -16,12 +17,17 @@ int main(){
 
   std::vector<Point_3> points(N);
 
+  int count = 0;
   for(int i = 0; i < N; i+=10){
-    points[i] = Point_3((N-i) * CGAL_PI , 0, 0);
+    points[i] = Point_3((N-i) * CGAL_PI , 0, 0) + Vector_3(i * (CGAL_PI/2.0), 0, 0);
+    if(points[i].approx().x().inf() != points[i].approx().x().sup()){
+      ++count;
+    }
     for(int j = 1; j < 10; j++){
       points[i+j] =  points[i];
     }
   }
+  std::cout << count << " points approx().x().inf() != approx().x().sup()" << std::endl;
   CGAL::Timer t;
   t.start();
   std::sort(points.begin(), points.end());
