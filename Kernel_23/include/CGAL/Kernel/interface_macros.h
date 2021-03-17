b32250242a40b9acfb7eb82788387d2d30be6e39
Laurent Rineau <laurent.rineau@cgal.org>
2021-03-17 11:30:21 +0100

    CGAL_Kernel_pred_RT for Do_intersect_2 and Do_intersect_3
    The code is not ready for that. A lot of overloads of
    `Do_interesect_[23]` are implemented by calling the equivalent
    overloads from `Intersect_23` (that requires division of coordinates).

--- Kernel_23/include/CGAL/Kernel/interface_macros.h
+++ Kernel_23/include/CGAL/Kernel/interface_macros.h
@@ -494,10 +494,10 @@ CGAL_Kernel_pred_RT(Coplanar_3,
                  coplanar_3_object)
 CGAL_Kernel_pred(Counterclockwise_in_between_2,
                  counterclockwise_in_between_2_object)
-CGAL_Kernel_pred(Do_intersect_2,
-                 do_intersect_2_object)
-CGAL_Kernel_pred(Do_intersect_3,
-                 do_intersect_3_object)
+CGAL_Kernel_pred_RT(Do_intersect_2,
+                    do_intersect_2_object)
+CGAL_Kernel_pred_RT(Do_intersect_3,
+                    do_intersect_3_object)
 CGAL_Kernel_pred(Equal_xy_3,
                  equal_xy_3_object)
 CGAL_Kernel_pred(Equal_x_2,
