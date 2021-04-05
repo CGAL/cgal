f70bec6526c68e2fb5e7e73c7846e5a4f1a56ee2
Giles Bathgate <giles.bathgate@gmail.com>
2021-04-05 18:21:06 +0100

    Robin hood hashmap in Object_index

--- Nef_2/include/CGAL/Nef_2/Object_index.h
+++ Nef_2/include/CGAL/Nef_2/Object_index.h
@@ -17,7 +17,9 @@
 
 
 #include <CGAL/basic.h>
-#include <CGAL/Unique_hash_map.h>
+#include <CGAL/Handle_hash_function.h>
+#include <CGAL/Tools/robin_hood.h>
+#include <CGAL/circulator.h>
 #include <string>
 #include <sstream>
 
@@ -26,24 +28,29 @@ namespace CGAL {
 template <typename I>
 class Object_index {
   char _prefix;
-  CGAL::Unique_hash_map<I,int> _index;
+  robin_hood::unordered_map<I,int,Handle_hash_function> _index;
 public:
-  Object_index() : _prefix('\0'), _index(-1) {}
-  Object_index(I first, I beyond, char c=' ') : _prefix(c), _index(-1)
-  { for(int i=0 ; first!=beyond; ++i,++first) _index[first]=i; }
-  int operator[](const I& it) const { return _index[it]; }
+  Object_index() : _prefix('\0'), _index() {}
+  Object_index(I first, I beyond, char c=' ') : _prefix(c), _index()
+  { index(first,beyond,c); }
+  int operator[](const I& it) const { return _index.at(it); }
   int& operator[](const I& it) { return _index[it]; }
 
   void index(I first, I beyond, char c=' ')
   { _prefix=c;
-    for(int i=0 ; first!=beyond; ++i,++first) _index[first]=i;
+    _index.reserve(iterator_distance(first,beyond));
+    for(int i=0; first!=beyond; ++i,++first)
+        _index.emplace(first,i);
   }
+
   std::string operator()(const I& it, bool verbose=true) const
-  { if (verbose && _index[it]==-1) return "nil";
-    if (verbose && _index[it]==-2) return "end";
+  { auto v = _index.find(it);
+    int i = (v == _index.end()) ? -1 : v->second;
+    if(verbose && i==-1) return "nil";
+    if(verbose && i==-2) return "end";
     std::ostringstream os;
     if (verbose) os << _prefix;
-    os << _index[it];
+    os << i;
     return os.str();
   }
 };
