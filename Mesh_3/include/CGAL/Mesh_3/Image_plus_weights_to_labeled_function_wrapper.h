71b85f4b85c95a7e58b5d42c86c2a10b2d3e69d8
Laurent Rineau <laurent.rineau@cgal.org>
2021-04-08 18:44:19 +0200

    Fix license

--- Mesh_3/include/CGAL/Mesh_3/Image_plus_weights_to_labeled_function_wrapper.h
+++ Mesh_3/include/CGAL/Mesh_3/Image_plus_weights_to_labeled_function_wrapper.h
@@ -1,19 +1,11 @@
-// Copyright (c) 2016  GeometryFactory Sarl (France).
+// Copyright (c) 2016,2021  GeometryFactory Sarl (France).
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org).
-// You can redistribute it and/or modify it under the terms of the GNU
-// General Public License as published by the Free Software Foundation,
-// either version 3 of the License, or (at your option) any later version.
-//
-// Licensees holding a valid commercial license may use this file in
-// accordance with the commercial license agreement provided with the software.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
 // $URL$
 // $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 //
 //
 // Author(s)     : Laurent Rineau
@@ -21,6 +13,8 @@
 
 #ifndef CGAL_MESH_3_IMAGE_PLUS_WEIGHTS_TO_LABELED_FUNCTION_WRAPPER_H
 #define CGAL_MESH_3_IMAGE_PLUS_WEIGHTS_TO_LABELED_FUNCTION_WRAPPER_H
+
+#include <CGAL/license/Mesh_3.h>
 
 #include <CGAL/function_objects.h>
 
