fe3da4cc9fc4fc3bee6ca0c1c800c48b411a130d
Simon Giraudot <simon.giraudot@geometryfactory.com>
2021-04-14 09:45:06 +0200

    Fix conversion warnings AGAIN

--- Principal_component_analysis/include/CGAL/linear_least_squares_fitting_circles_2.h
+++ Principal_component_analysis/include/CGAL/linear_least_squares_fitting_circles_2.h
@@ -135,7 +135,7 @@ linear_least_squares_fitting_2(InputIterator first,
     // isotropic case (infinite number of directions)
     // by default: assemble a line that goes through
     // the centroid and with a default horizontal vector.
-    line = Line(c, Vector(1.0, 0.0));
+    line = Line(c, Vector(FT(1), FT(0)));
     return (FT)0.0;
   }
 
