670cd96e4553cbe33388d731a7298f7248efe6c3
kabirkedia <55885241+kabirkedia@users.noreply.github.com>
2021-04-23 15:02:12 +0530

    add detect_sharp_corners to examples

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/detect_features_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/detect_features_example.cpp
@@ -27,15 +27,25 @@ int main(int argc, char* argv[])
   typedef boost::property_map<Mesh, CGAL::edge_is_feature_t>::type EIFMap;
   typedef boost::property_map<Mesh, CGAL::face_patch_id_t<int> >::type PIMap;
   typedef boost::property_map<Mesh, CGAL::vertex_incident_patches_t<int> >::type VIMap;
+  typedef boost::property_map<Mesh, CGAL::vertex_is_constrained_map>::type VCMap
 
   EIFMap eif = get(CGAL::edge_is_feature, mesh);
   PIMap pid = get(CGAL::face_patch_id_t<int>(), mesh);
   VIMap vip = get(CGAL::vertex_incident_patches_t<int>(), mesh);
+  VCMap vcmap =get(CGAL::vertex_is_constrained_map,meshj);
 
   std::size_t number_of_patches
     = PMP::sharp_edges_segmentation(mesh, 90, eif, pid,
                                     PMP::parameters::vertex_incident_patches_map(vip));
 
+  PMP::detect_sharp_corners(mesh, 60, vcmap);
+  std::size_t sharp_corners_counter = 0;
+  for(boost::graph_traits<Mesh>::vertex_descriptor v : vertices(mesh))
+  {
+    if(get(vcmap, v))
+      ++sharp_corners_counter
+  }
+      
   std::size_t nb_sharp_edges = 0;
   for(boost::graph_traits<Mesh>::edge_descriptor e : edges(mesh))
   {
@@ -44,6 +54,7 @@ int main(int argc, char* argv[])
   }
 
   std::cout<<"This mesh contains "<<nb_sharp_edges<<" sharp edges"<<std::endl;
+  std::cout<<","<<sharp_corner_counter<<" sharp corners"<<std::endl;
   std::cout<<" and "<<number_of_patches<<" surface patches."<<std::endl;
 
   return 0;
