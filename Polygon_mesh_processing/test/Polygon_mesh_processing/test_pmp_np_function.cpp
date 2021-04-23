9cc1e1c5ab319f8ed41546df173dbead35849943
Maxime Gimeno <maxime.gimeno@gmail.com>
2021-04-23 13:26:44 +0200

    Add a boolean np

--- Polygon_mesh_processing/test/Polygon_mesh_processing/test_pmp_np_function.cpp
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/test_pmp_np_function.cpp
@@ -10,35 +10,47 @@
 namespace CGAL {
 template <class PolygonMesh,
           class NamedParameters = parameters::Default_named_parameters >
-void np_function(PolygonMesh& mesh, const NamedParameters& np = parameters::use_default_values())
+void my_function_with_named_parameters(PolygonMesh& mesh, const NamedParameters& np = parameters::use_default_values())
 {
-
+  //The class containing all the geometric definitions for the PolygonMesh
   typedef typename GetGeomTraits<PolygonMesh, NamedParameters>::type  Traits;
+  //A vertex-index-map that is either taken from the NPs, either an already initialized map for vertex-indices.
+  //Also exists for Faces, Edges and Halfedges
   typedef typename CGAL::GetInitializedVertexIndexMap<PolygonMesh, NamedParameters>::type VertexIndexMap;
+  //A vertex-point-map either taken from the NPs, either a specified default map.
   typedef typename GetVertexPointMap < PolygonMesh, NamedParameters>::type VPM;
+  //A face-normal-map either taken from the NPs, either a specified default map.
   typedef typename GetFaceNormalMap < PolygonMesh, NamedParameters>::type FNM;
 
   typedef typename Traits::Point_3 Point_3;
   typedef typename Traits::Vector_3 Vector_3;
-
+  //The class defining all boost-graph types for the PolygonMesh, like vertex_descriptor and so.
   typedef boost::graph_traits<PolygonMesh> Graph_traits;
   typedef typename Graph_traits::vertex_descriptor vertex_descriptor;
   typedef typename Graph_traits::edge_descriptor edge_descriptor;
   typedef typename Graph_traits::halfedge_descriptor halfedge_descriptor;
+
   using parameters::choose_parameter;
   using parameters::get_parameter;
   using parameters::is_default_parameter;
 
-
+  //default face-normal-map if the NPs don't provide it.
   FNM default_fvmap;
+  //If the NPs provide a vertex-index-map, returns it. Else, returns an initialized vertex-index map.
   VertexIndexMap vim = CGAL::get_initialized_vertex_index_map(mesh, np);
+  //If the NPs provide a vertex-point-map, returns it. Else, returns the default boost-graph vpm.
   VPM vpm = choose_parameter(get_parameter(np, internal_np::vertex_point),
                              get_property_map(vertex_point, mesh));
+  //If the NPs provide a face-normal-map, returns it. Else, returns default_fvmap.
   FNM fnm = choose_parameter(get_parameter(np, internal_np::face_normal),
                              default_fvmap);
-
+  //is true if a face-normal-map is provided by he NPs
   bool has_fnm = !is_default_parameter(get_parameter(np, internal_np::face_normal));
+  //boolean NP example. Default value is `false`
+  bool do_project = choose_parameter(get_parameter(np, internal_np::do_project), false);
 
+
+  //demonstrates usage for those values.
   if(has_fnm)
     for(auto f : faces(mesh))
     {
@@ -49,7 +61,12 @@ void np_function(PolygonMesh& mesh, const NamedParameters& np = parameters::use_
     std::cout<<"vertex #"<<get(vim, v)<<" : "<<get(vpm, v)<<std::endl;
   }
 
+  if(do_project)
+    std::cout<<"do project"<<std::endl;
+  else
+      std::cout<<"don't project"<<std::endl;
 }
+
 }
 
 int main()
@@ -67,13 +84,15 @@ int main()
                          "3  0 3 2\n"
                          "3  0 2 1\n");
   poly >> sm;
+  //Define a custom face-normal-map
   typedef std::map<boost::graph_traits<SMesh>::face_descriptor,CGAL::Epick::Vector_3> FNmap;
   FNmap fnm;
   for(auto f : faces(sm))
     fnm[f] = {0,0,1};
   typedef boost::associative_property_map<FNmap> Face_normal_pmap;
   Face_normal_pmap fn_pmap(fnm);
-  CGAL::np_function(sm);
-  CGAL::np_function(sm, CGAL::parameters::face_normal_map(fn_pmap));
+  CGAL::my_function_with_named_parameters(sm);
+  CGAL::my_function_with_named_parameters(sm, CGAL::parameters::face_normal_map(fn_pmap)
+                                          .do_project(true));
   return 0;
 }
