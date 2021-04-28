87174f30072b368c3bb2405525e71630b568f299
Sébastien Loriot <sebastien.loriot@cgal.org>
2021-04-28 08:37:08 +0200

    remove useless check (go further is updated with primitives not bbox)

--- AABB_tree/include/CGAL/AABB_tree.h
+++ AABB_tree/include/CGAL/AABB_tree.h
@@ -553,7 +553,7 @@ public:
         traits.intersection(query, singleton_data());
         break;
       default: // if(size() >= 2)
-        if ( traits.do_intersect(query, *root_node()) && traits.go_further() )
+        if ( traits.do_intersect(query, *root_node()) )
           root_node()->template traversal_with_priority_and_group_traversal(m_primitives, query, traits, m_primitives.size(), 0, group_traversal_bound);
       }
     }
