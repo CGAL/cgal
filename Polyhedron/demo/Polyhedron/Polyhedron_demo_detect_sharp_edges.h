ef33f0eec467b204349d901337531639c5f46856
kabirkedia <55885241+kabirkedia@users.noreply.github.com>
2021-05-06 10:46:18 +0530

    add the detect_corners functionality

--- Polyhedron/demo/Polyhedron/Polyhedron_demo_detect_sharp_edges.h
+++ Polyhedron/demo/Polyhedron/Polyhedron_demo_detect_sharp_edges.h
@@ -26,6 +26,26 @@ namespace CGAL
         get(CGAL::edge_is_feature, *pMesh);
     CGAL::Polygon_mesh_processing::detect_sharp_edges(*pMesh, angle, eif);
   }
+  template<typename Polyhedron>
+  void reset_sharp_corners(Polyhedron* pMesh)
+  {
+    typename boost::property_map<Polyhedron,vertex_is_feature_t>::type if_pm =
+        get(CGAL::vertex_is_feature, *pMesh);
+    for(typename boost::graph_traits<Polyhedron>::vertex_descriptor vd : vertices(*pMesh))
+    {
+      put(if_pm,vd,false);
+    }
+  }
+  template<typename Polyhedron>
+  void detect_sharp_corners(Polyhedron* pMesh, const double angle)
+  {
+    reset_sharp_edges(pMesh);
+
+    // Detect corners in current polyhedron
+    typename boost::property_map<Polyhedron,vertex_is_feature_t>::type vif =
+        get(CGAL::vertex_is_feature, *pMesh);
+    CGAL::Polygon_mesh_processing::detect_sharp_corners(angle, vif,*pmesh);
+  }
 
 }//end namespace CGAL
 
