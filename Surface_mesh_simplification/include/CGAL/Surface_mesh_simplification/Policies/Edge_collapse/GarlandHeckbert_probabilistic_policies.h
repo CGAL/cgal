8fb02b2765fe4a007c4b57208ad6b01d83d9e388
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2021-06-30 10:32:36 +0200

    Initialize 'dm'

--- Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/GarlandHeckbert_probabilistic_policies.h
+++ Surface_mesh_simplification/include/CGAL/Surface_mesh_simplification/Policies/Edge_collapse/GarlandHeckbert_probabilistic_policies.h
@@ -96,7 +96,7 @@ public:
                                            FT dm,
                                            FT sdn,
                                            FT sdp)
-      : sdev_n_2(square(sdn)), sdev_p_2(square(sdp))
+      : Cost_base(dm), sdev_n_2(square(sdn)), sdev_p_2(square(sdp))
     {
       // initialize the private variable vcm so it's lifetime is bound to that of the policy's
       vcm_ = get(Cost_property(), tmesh);
