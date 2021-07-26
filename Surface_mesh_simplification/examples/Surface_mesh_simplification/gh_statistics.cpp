e2b2ad8acc09d7d641a6be7b1ff05721b2d6a678
Julian Komaromy <julian.komaromy@gmail.com>
2021-07-26 15:22:25 +0200

    add benchmarking functions

--- Surface_mesh_simplification/examples/Surface_mesh_simplification/gh_statistics.cpp
+++ Surface_mesh_simplification/examples/Surface_mesh_simplification/gh_statistics.cpp
@@ -18,6 +18,8 @@
 #include <boost/format.hpp>
 #include <CGAL/Polygon_mesh_processing/measure.h>
 
+#include <boost/filesystem.hpp>
+
 typedef CGAL::Simple_cartesian<double>                          Kernel;
 typedef Kernel::FT                                              FT;
 typedef Kernel::Point_3                                         Point_3;
@@ -28,9 +30,126 @@ typedef typename boost::graph_traits<Surface_mesh>::face_descriptor face_descrip
 
 namespace SMS = CGAL::Surface_mesh_simplification;
 namespace PMP = CGAL::Polygon_mesh_processing;
+namespace fs = boost::filesystem;
 
 using hist = boost::histogram::histogram<std::tuple<boost::histogram::axis::regular<double, 
       boost::use_default, boost::use_default, boost::use_default>>>; 
+
+constexpr int n_burns = 1;
+constexpr int n_samples = 10;
+
+template<typename Policy>
+Surface_mesh edge_collapse(Surface_mesh& mesh, double ratio = 0.2)
+{
+  typedef typename Policy::Get_cost Cost; 
+  typedef typename Policy::Get_placement Placement;
+  
+  typedef SMS::Bounded_normal_change_placement<Placement> Bounded_placement;
+  
+  const Policy p {mesh, 100};
+  
+  const Cost& cost = p.get_cost();
+  const Placement& unbounded_placement = p.get_placement();
+
+  Bounded_placement placement(unbounded_placement);
+
+  SMS::Count_ratio_stop_predicate<Surface_mesh> stop(ratio);
+  
+  // collapse edges ignoring result code
+  SMS::edge_collapse(mesh, stop,
+      CGAL::parameters::get_cost(cost).get_placement(placement));
+  
+  return mesh;
+}
+
+template<typename Policy>
+void time_all_vector(const std::vector<Surface_mesh>& meshes, const fs::path& output_file)
+{
+  namespace time = std::chrono;
+  
+  fs::ofstream out_stream(output_file);
+  
+  // always decimate meshes in this vector to avoid timing the
+  // copying of the meshes
+  std::vector<Surface_mesh> temp_meshes = meshes;
+  
+  for (int i = 0; i < n_burns; ++i)
+  {
+    for (Surface_mesh& mesh : temp_meshes)
+    {
+      edge_collapse<Policy>(mesh);
+    }
+  }
+  temp_meshes = meshes;
+
+  // measure each run 
+  for (int i = 0; i < n_samples; ++i)
+  {
+    // measure time taken by the edge_collapse function over all meshes
+    time::time_point start_time = time::steady_clock::now();
+  
+    for (Surface_mesh& mesh : temp_meshes)
+    {
+      edge_collapse<Policy>(mesh);
+    }
+    
+    time::time_point end_time = time::steady_clock::now();
+    
+    // get elapsed time in nanoseconds
+    unsigned long elapsed_ns = time::duration_cast<time::nanoseconds>
+      (end_time - start_time).count();
+
+    // add the elapsed time as data point
+    out_stream << std::to_string(elapsed_ns) << '\n';   
+
+    temp_meshes = meshes;
+  }
+}
+
+template<typename Policy>
+void time_policy(const fs::path& dir, const fs::path& output_file)
+{
+
+  std::vector<Surface_mesh> meshes;
+
+  // read all meshes in the given directory into the vector
+  if (fs::is_directory(dir))
+  {
+    fs::directory_iterator end_iter;
+    Surface_mesh mesh;
+    
+    for (fs::directory_iterator dir_iter(dir); dir_iter != end_iter; ++dir_iter)
+    {
+      if (fs::is_regular_file(dir_iter->status())
+          && dir_iter->path().extension() == ".off")
+      {
+        const fs::path& path = dir_iter->path();
+
+        //full path (relative to directory)
+        fs::path full_path = dir.relative_path();
+        full_path += path;
+
+        // input stream from the file
+        fs::ifstream is(full_path);
+
+        // try to read the file into the surface mesh
+        if (!is || !(is >> mesh))
+        {
+          std::cerr << "Failed to read input mesh " << full_path 
+            << " in directory " << dir << std::endl;
+
+          return;
+        }
+        
+        // we can move this mesh since we don't need it anymore - the next one will be read into 
+        // the same variable
+        meshes.push_back(std::move(mesh));
+      }
+    }
+  }
+
+  time_all_vector<Policy>(meshes, output_file);
+}
 
 // taken from a boost example
 template<typename histo>
@@ -75,49 +194,10 @@ hist generate_face_statistics(Surface_mesh mesh, hist histo, std::function<FT(fa
   return histo;
 }
 
-template<typename Policy>
-Surface_mesh edge_collapse(Surface_mesh mesh)
-{
-  typedef typename Policy::Get_cost Cost; 
-  typedef typename Policy::Get_placement Placement;
-  
-  typedef SMS::Bounded_normal_change_placement<Placement> Bounded_placement;
-  
-  Policy p {mesh, 100};
-  
-  const Cost& cost = p.get_cost();
-  const Placement& unbounded_placement = p.get_placement();
 
-  Bounded_placement placement(unbounded_placement);
-
-  //TODO don't hardcode ratio 
-  const double ratio = 0.2;
-  SMS::Count_ratio_stop_predicate<Surface_mesh> stop(ratio);
-  
-  // collapse edges ignoring result code
-  SMS::edge_collapse(mesh, stop,
-      CGAL::parameters::get_cost(cost).get_placement(placement));
-  
-  return mesh;
-}
 
 int main(int argc, char** argv)
 {
-  Surface_mesh surface_mesh;
-  const char* filename = (argc > 1) ? argv[1] : "data/cube-meshed.off";
-  std::ifstream is(filename);
-  if(!is || !(is >> surface_mesh))
-  {
-    std::cerr << "Failed to read input mesh: " << filename << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  if(!CGAL::is_triangle_mesh(surface_mesh))
-  {
-    std::cerr << "Input geometry is not triangulated." << std::endl;
-    return EXIT_FAILURE;
-  }
-
   using namespace boost::histogram;
   hist edge_histo = make_histogram(axis::regular<>(14, 0.1, 4.0, "length"));
   
@@ -129,6 +209,21 @@ int main(int argc, char** argv)
     return PMP::face_aspect_ratio(face, mesh);
   };
   
+  typedef SMS::GarlandHeckbert_probabilistic_policies<Surface_mesh, Kernel> Prob_policies;
+  typedef SMS::GarlandHeckbert_policies<Surface_mesh, Kernel> Classic_policies;
+  typedef SMS::GarlandHeckbert_probabilistic_tri_policies<Surface_mesh, Kernel> Prob_tri_policies;
+  typedef SMS::GarlandHeckbert_triangle_policies<Surface_mesh, Kernel> Classic_tri_policies;
+  
+  const fs::path output {"prob_timings"};
+
+  if (fs::exists(output))
+  {
+    // clear file
+    fs::resize_file(output, 0); 
+  }
+  
+  time_policy<Classic_tri_policies>("../data/", output);
+  /*
   Surface_mesh probabilistic = edge_collapse
     <SMS::GarlandHeckbert_probabilistic_policies<Surface_mesh, Kernel>>(surface_mesh);
   
@@ -173,6 +268,7 @@ int main(int argc, char** argv)
   
   std::cout << "Probabilistic tri aspect ratio:\n";
   print_hist(generate_face_statistics(probabilistic_tri, face_histo, g));
+  */
   
   return EXIT_SUCCESS;
 }
