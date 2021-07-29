2e3ebb19164666980734d76c6600423d676c1d08
Maxime Gimeno <maxime.gimeno@gmail.com>
2021-07-29 10:13:05 +0200

    Add a visitor to the example

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/orient_polygon_soup_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/orient_polygon_soup_example.cpp
@@ -16,6 +16,31 @@
 typedef CGAL::Exact_predicates_inexact_constructions_kernel          K;
 typedef CGAL::Polyhedron_3<K, CGAL::Polyhedron_items_with_id_3>      Polyhedron;
 
+//Optional visitor for orientation to demonstrate usage
+struct Visitor : public CGAL::Polygon_mesh_processing::internal::Polygon_soup_orientation_visitor
+{
+  void non_manifold_edge(const std::size_t& id1, const std::size_t& id2) final
+  {
+    std::cout<<"The edge "<<id1<<", "<<id2<<" is not manifold."<<std::endl;
+  }
+  void duplicated_vertex(const std::size_t& v1, const std::size_t& v2) final
+  {
+    std::cout<<v1<<" has been duplicated, the new id is "<<v2<<"."<<std::endl;
+  }
+  void non_manifold_vertex(const std::size_t&v) final
+  {
+    std::cout<<"The vertex "<<v<<" is not manifold."<<std::endl;
+  }
+  void point_id_in_polygon_updated(const std::size_t& p_id, const std::size_t& i1, const std::size_t& i2) final
+  {
+    std::cout<<"In the polygon "<<p_id<<"< the index "<<i1<<" has been replaced by "<<i2<<"."<<std::endl;
+  }
+  void polygon_orientation_reversed(const std::size_t& p_id) final
+  {
+    std::cout<<"The polygon "<<p_id<<" has been reversed."<<std::endl;
+  }
+  };
+
 int main(int argc, char* argv[])
 {
   const char* filename = (argc > 1) ? argv[1] : "data/tet-shuffled.off";
@@ -29,7 +54,8 @@ int main(int argc, char* argv[])
     return EXIT_FAILURE;
   }
 
-  CGAL::Polygon_mesh_processing::orient_polygon_soup(points, polygons);
+  Visitor visitor;
+  CGAL::Polygon_mesh_processing::orient_polygon_soup(points, polygons, CGAL::parameters::visitor(visitor));
 
   Polyhedron mesh;
   CGAL::Polygon_mesh_processing::polygon_soup_to_polygon_mesh(points, polygons, mesh);
