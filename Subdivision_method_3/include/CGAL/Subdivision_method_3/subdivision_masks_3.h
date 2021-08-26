8013590cc499db0802cd0b76b6ad2af0f3003ed5
Sebastien Loriot <sloriot.ml@gmail.com>
2021-08-26 15:49:59 +0200

    Update Subdivision_method_3/include/CGAL/Subdivision_method_3/subdivision_masks_3.h
    Co-authored-by: Laurent Rineau <Laurent.Rineau@cgal.org>

--- Subdivision_method_3/include/CGAL/Subdivision_method_3/subdivision_masks_3.h
+++ Subdivision_method_3/include/CGAL/Subdivision_method_3/subdivision_masks_3.h
@@ -331,7 +331,7 @@ public:
   void vertex_node(vertex_descriptor vertex, Point& pt) {
     Halfedge_around_vertex_circulator vcir(vertex, *(this->pmesh));
     size_t n = circulator_size(vcir);
-    CGAL_assertion(n > 0);
+    CGAL_assume(n > 0);
 
     FT R[] = {0.0, 0.0, 0.0};
     Point_ref S = get(this->vpmap,vertex);
