bcce76c0620fd863f89997045b9d57917ca75a8e
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2021-08-26 11:08:16 +0200

    Fix test & warnings

--- Triangulation_2/include/CGAL/Triangulation_2_projection_traits_3.h
+++ Triangulation_2/include/CGAL/Triangulation_2_projection_traits_3.h
@@ -10,31 +10,30 @@
 //
 // Author(s)     : Laurent Rineau
 
-
 #ifndef CGAL_TRIANGULATION_2_PROJECTION_TRAITS_3_H
 #define CGAL_TRIANGULATION_2_PROJECTION_TRAITS_3_H
 
 #include <CGAL/license/Triangulation_2.h>
 
-
 #define CGAL_DEPRECATED_HEADER "<CGAL/Triangulation_2_projection_traits_3.h>"
 #define CGAL_REPLACEMENT_HEADER "<CGAL/Projection_traits_3.h>"
 #include <CGAL/internal/deprecation_warning.h>
 
-#include <CGAL/internal/Filtered_projection_traits_3.h>
 #ifndef CGAL_NO_DEPRECATED_CODE
-namespace CGAL{
+
+#include <CGAL/internal/Filtered_projection_traits_3.h>
+
+namespace CGAL {
 
 template < class Filtered_kernel >
 class Filtered_projection_traits_3;
 
-template <class Kernel, bool Has_filtered_predicates=Kernel::Has_filtered_predicates>
+template <class Kernel, bool Has_filtered_predicates = Kernel::Has_filtered_predicates>
 class Triangulation_2_projection_traits_3
   : public Projection_traits_base_3<Kernel>
 {
 public:
-  explicit
-  Triangulation_2_projection_traits_3(const typename Kernel::Vector_3& n_)
+  explicit Triangulation_2_projection_traits_3(const typename Kernel::Vector_3& n_)
     : Projection_traits_base_3<Kernel>(n_)
   {}
 };
@@ -44,12 +43,13 @@ class Triangulation_2_projection_traits_3<Kernel, true>
   : public Filtered_projection_traits_3<Kernel>
 {
 public:
-  explicit
-  Triangulation_2_projection_traits_3(const typename Kernel::Vector_3& n_)
+  explicit Triangulation_2_projection_traits_3(const typename Kernel::Vector_3& n_)
     : Filtered_projection_traits_3<Kernel>(n_)
   {}
 };
 
 } // end namespace CGAL
+
 #endif //CGAL_NO_DEPRECATED_CODE
+
 #endif // CGAL_TRIANGULATION_2_PROJECTION_TRAITS_3_H
