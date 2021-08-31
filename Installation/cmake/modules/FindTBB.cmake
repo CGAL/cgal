0229fb113b2dab6f549e83612a7b9033243a64ae
Charrière Maxime <51918753+maximecharriere@users.noreply.github.com>
2021-08-31 17:19:55 +0200

    FindTBB is no longer compatible with oneTBB 2021.1.1
    In `FindTBB.cmake` it open the `/tbb/tbb_stddef.h` file.
    
    https://github.com/CGAL/cgal/blob/ebb719c8a4a096f4b26b28c3bd0c45147b6196a8/Installation/cmake/modules/FindTBB.cmake#L419
    
    Since oneTBB 2021.1.1, `/tbb/tbb_stddef.h` no longer exists.  
    The version can be found in [/oneapi/tbb/version.h](https://github.com/oneapi-src/oneTBB/blob/master/include/oneapi/tbb/version.h)

--- Installation/cmake/modules/FindTBB.cmake
+++ Installation/cmake/modules/FindTBB.cmake
@@ -416,7 +416,7 @@ if(NOT TBB_VERSION)
 
  #only read the start of the file
  file(STRINGS
-      "${TBB_INCLUDE_DIR}/tbb/tbb_stddef.h"
+      "${TBB_INCLUDE_DIR}/oneapi/tbb/version.h"
       TBB_VERSION_CONTENTS
       REGEX "VERSION")
 
