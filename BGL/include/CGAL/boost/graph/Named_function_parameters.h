0e730466ef27e449ef4c424341084f3e54b36d6f
Sébastien Loriot <sebastien.loriot@cgal.org>
2021-09-01 10:13:15 +0200

    forgot temporary default

--- BGL/include/CGAL/boost/graph/Named_function_parameters.h
+++ BGL/include/CGAL/boost/graph/Named_function_parameters.h
@@ -308,6 +308,12 @@ D& choose_parameter(const internal_np::Param_not_found&, D& d)
   return d;
 }
 
+template <typename D>
+const D& choose_parameter(const internal_np::Param_not_found&, const D& d)
+{
+  return d;
+}
+
 template <typename T, typename D>
 T& choose_parameter(T& t, D&)
 {
