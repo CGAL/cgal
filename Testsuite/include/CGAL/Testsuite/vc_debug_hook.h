3a8caae5fd1b396e50d584fb9a4e9f4f8ffbdf26
Andreas Fabri <andreas.fabri@geometryfactory.com>
2021-09-15 15:58:13 +0100

    fprintf -> std::cerr <<

--- Testsuite/include/CGAL/Testsuite/vc_debug_hook.h
+++ Testsuite/include/CGAL/Testsuite/vc_debug_hook.h
@@ -19,6 +19,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <crtdbg.h>
+#include <iostream>
 
 
 namespace
@@ -38,11 +39,11 @@ namespace
   {
     switch(n)
     {
-      case SIGSEGV: std::fprintf(stderr,"In CGAL_handle_signal, Program received signal SIGSEGV: Segmentation Fault."); break ;
-      case SIGFPE : std::fprintf(stderr,"In CGAL_handle_signal, Program received signal SIGFPE: Floating Point Execption."); break ;
-      case SIGILL : std::fprintf(stderr,"In CGAL_handle_signal, Program received signal SIGILL: Illegal Instruction."); break ;
+      case SIGSEGV: std::cerr << "In CGAL_handle_signal, Program received signal SIGSEGV: Segmentation Fault." << std::endl; break ;
+      case SIGFPE : std::cerr << "In CGAL_handle_signal, Program received signal SIGFPE: Floating Point Execption." << std::endl; break ;
+      case SIGILL : std::cerr << "In CGAL_handle_signal, Program received signal SIGILL: Illegal Instruction." << std::endl; break ;
       default:
-        std::fprintf(stderr,"In CGAL_handle_signal, Program received signal %d", n); break ;
+        std::cerr << "In CGAL_handle_signal, Program received signal " << n << std::endl; break ;
     }
 
     std::exit(128+n);
