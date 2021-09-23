9b19c5faff8f3d1151318e96a475b55b39045cc2
Sébastien Loriot <sebastien.loriot@cgal.org>
2021-09-23 15:11:18 +0200

    rename enum

--- Intersections_2/include/CGAL/Intersections_2/Line_2_Ray_2.h
+++ Intersections_2/include/CGAL/Intersections_2/Line_2_Ray_2.h
@@ -35,11 +35,11 @@ namespace internal {
 template <class K>
 class Ray_2_Line_2_pair {
 public:
-    enum Intersection_results {NOT_COMPUTED_YET, NO_INTERSECTION, POINT, RAY};
+    enum Intersection_results {NO_INTERSECTION, POINT, RAY, UNKNOWN};
     typedef typename K::FT FT;
     Ray_2_Line_2_pair(typename K::Ray_2 const *ray,
                       typename K::Line_2 const *line)
-      : _ray(ray), _line(line), _result(NOT_COMPUTED_YET),
+      : _ray(ray), _line(line),
         _intersection_point(K().construct_point_2_object()(ORIGIN))
     {}
 
@@ -50,7 +50,7 @@ public:
 protected:
     typename K::Ray_2 const *   _ray;
     typename K::Line_2 const *  _line;
-    mutable Intersection_results    _result;
+    mutable Intersection_results    _result = UNKNOWN;
     mutable typename K::Point_2         _intersection_point;
 };
 
@@ -117,7 +117,7 @@ template <class K>
 typename Ray_2_Line_2_pair<K>::Intersection_results
 Ray_2_Line_2_pair<K>::intersection_type() const
 {
-    if (_result != NOT_COMPUTED_YET)
+    if (_result != UNKNOWN)
         return _result;
     // The non const this pointer is used to cast away const.
     const typename K::Line_2 &l1 = _ray->supporting_line();
@@ -144,7 +144,7 @@ template <class K>
 typename K::Point_2
 Ray_2_Line_2_pair<K>::intersection_point() const
 {
-    if (_result == NOT_COMPUTED_YET)
+    if (_result == UNKNOWN)
         intersection_type();
     CGAL_kernel_assertion(_result == POINT);
     return _intersection_point;
@@ -154,7 +154,7 @@ template <class K>
 typename K::Ray_2
 Ray_2_Line_2_pair<K>::intersection_ray() const
 {
-    if (_result == NOT_COMPUTED_YET)
+    if (_result == UNKNOWN)
         intersection_type();
     CGAL_kernel_assertion(_result == RAY);
     return *_ray;
