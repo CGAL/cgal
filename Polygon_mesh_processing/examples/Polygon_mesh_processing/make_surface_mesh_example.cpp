bf2b30d9ce1df4707d34856496d8011e5ba259a2
Jane Tournois <jane.tournois@gmail.com>
2021-09-30 14:53:22 +0200

    fix parameter name in example

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/make_surface_mesh_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/make_surface_mesh_example.cpp
@@ -31,7 +31,7 @@ int main(int argc, char* argv[])
   Mesh outmesh;
   PMP::make_surface_mesh(mesh, outmesh,
     PMP::parameters::protect_constraints(true)
-    .edge_size(0.025));
+    .mesh_edge_size(0.025));
 
   std::cout << "Remeshing done." << std::endl;
 
