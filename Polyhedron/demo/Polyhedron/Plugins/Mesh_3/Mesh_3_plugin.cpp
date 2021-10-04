9beac5e0398719e3f25320d213e0221c53aafbcf
Jane Tournois <jane.tournois@gmail.com>
2021-10-04 16:08:24 +0200

    hide menu when the input is not a labeled image

--- Polyhedron/demo/Polyhedron/Plugins/Mesh_3/Mesh_3_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/Mesh_3/Mesh_3_plugin.cpp
@@ -594,22 +594,29 @@ void Mesh_3_plugin::mesh_3(const Mesh_type mesh_type,
       }
     }
   }
-  //weighted image
-#ifdef CGAL_USE_ITK
+
+  //Labeled (weighted) image
   connect(ui.useWeights_checkbox, SIGNAL(toggled(bool)),
-          ui.weightsSigma,        SLOT(setEnabled(bool)));
+          ui.weightsSigma, SLOT(setEnabled(bool)));
   connect(ui.useWeights_checkbox, SIGNAL(toggled(bool)),
-          ui.weightsSigma_label,  SLOT(setEnabled(bool)));
+          ui.weightsSigma_label, SLOT(setEnabled(bool)));
   ui.weightsSigma->setValue(1.);
-#else
-  ui.labeledImgGroup->setDisabled(true);
-  ui.labeledImgGroup->setToolTip(
-    QString("The use of weighted images is disabled "
-            "because the Insight Toolkit (ITK) is not available."));
-  ui.useWeights_checkbox->setDisabled(true);
-  ui.weightsSigma_label->setDisabled(true);
-  ui.weightsSigma->setDisabled(true);
+  bool input_is_labeled_img = (image_item != nullptr && !image_item->isGray());
+  ui.labeledImgGroup->setVisible(input_is_labeled_img);
+
+#ifndef CGAL_USE_ITK
+  if (input_is_labeled_img)
+  {
+    ui.labeledImgGroup->setDisabled(true);
+    ui.labeledImgGroup->setToolTip(
+      QString("The use of weighted images is disabled "
+        "because the Insight Toolkit (ITK) is not available."));
+    ui.useWeights_checkbox->setDisabled(true);
+    ui.weightsSigma_label->setDisabled(true);
+    ui.weightsSigma->setDisabled(true);
+  }
 #endif
+
   // -----------------------------------
   // Get values
   // -----------------------------------
