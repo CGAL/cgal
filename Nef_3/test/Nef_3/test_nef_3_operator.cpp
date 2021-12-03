a1a289461bcb16fc5967039298a2e0f41ff18cff
Andreas Fabri <andreas.fabri@geometryfactory.com>
2021-12-03 09:58:13 +0000

    Remove unused variables

--- Nef_3/test/Nef_3/test_nef_3_operator.cpp
+++ Nef_3/test/Nef_3/test_nef_3_operator.cpp
@@ -11,12 +11,12 @@ typedef CGAL::Surface_mesh<K::Point_3> Polygon_mesh;
 
 typedef CGAL::Nef_polyhedron_3<K> Nef_polyhedron;
 
-int main(int argc, char* argv[])
+int main(int /* argc */, char* /* argv[] */)
 {
   Point_3 p0(1,1,1), p1(2,1,1), p2(2,2,1), p3(1,2,1), p4(1,2,2), p5(1,1,2), p6(2,1,2), p7(2,2,2);
   Point_3 q0(0,0,0), q1(3,0,0), q2(3,3,0), q3(0,3,0), q4(0,3,3), q5(0,0,3), q6(3,0,3), q7(3,3,3);
 
-    Polygon_mesh A1, A2, A3, A4, A5, A6;
+    Polygon_mesh A1, A2;
 
     make_hexahedron(p0, p1, p2, p3, p4, p5, p6, p7, A1);
     make_hexahedron(q0, q1, q2, q3, q4, q5, q6, q7, A2);
