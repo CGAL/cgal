a76e3bb93f6fbbf62ff4b013f7108d1e5c35059c
Jane Tournois <jane.tournois@gmail.com>
2022-01-04 17:31:08 +0100

    move cell and vertex base headers to TMDS_3
    to avoid dependency on Mesh_3 for tet remeshing

--- TMDS_3/include/CGAL/Compact_mesh_cell_base_3.h
+++ TMDS_3/include/CGAL/Compact_mesh_cell_base_3.h
@@ -709,6 +709,32 @@ public:
 
 };  // end class Compact_mesh_cell_3
 
+/*!
+\ingroup PkgMesh3MeshClasses
+
+The class `Compact_mesh_cell_base_3<GT, MD>` is a model of the concept `MeshCellBase_3`.
+It is designed to serve as cell base class for the 3D triangulation
+used in the 3D mesh generation process. It is more compact in memory than
+`Mesh_cell_base_3`.
+
+\tparam GT is the geometric traits class.
+It has to be a model of the concept `MeshTriangulationTraits_3`.
+
+\tparam MD provides the types of indices used to identify
+the faces of the input complex. It has to be a model
+of the concept `MeshDomain_3`.
+
+\tparam TDS is the triangulation data structure class to which cells
+belong. That parameter is only used by the rebind mechanism (see
+`::TriangulationDSCellBase_3::Rebind_TDS`). Users should always use the
+default parameter value `void`.
+
+\cgalModels `MeshCellBase_3`
+
+\sa `CGAL::Mesh_complex_3_in_triangulation_3<Tr,CornerIndex,CurveIndex>`
+\sa `CGAL::Mesh_cell_base_3<GT, MD, Cb>`
+
+*/
 template< class GT,
           class MD,
           class TDS = void >
