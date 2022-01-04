ab38865f7c8aa42605eb2d03137eb3c3d8679074
Sébastien Loriot <sebastien.loriot@cgal.org>
2022-01-04 11:33:50 +0100

    deprecated function

--- Polygon_mesh_processing/include/CGAL/polygon_mesh_processing.h
+++ Polygon_mesh_processing/include/CGAL/polygon_mesh_processing.h
@@ -56,9 +56,8 @@ namespace CGAL {
 namespace Polygon_mesh_processing {
 namespace parameters {
 
-/// TODO: remove this one and deprecate it
-
 /*! \ingroup namedparameters
+\deprecated This function is deprecated and `use_default_values()` should be used instead.
 This function can be used to indicate that all optional named parameters
 to be used are the default ones. This is particularly useful when a function
 requires more than one sequence of named parameters and default values is
