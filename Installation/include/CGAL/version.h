c58ac97e93c838ebfb1e8adaf23ff4fd185dc8e4
Laurent Rineau <laurent.rineau@cgal.org>
2022-01-27 12:12:21 +0100

    Prepare 5.4.x-branch for the release of version 5.4

--- Installation/include/CGAL/version.h
+++ Installation/include/CGAL/version.h
@@ -17,12 +17,12 @@
 #define CGAL_VERSION_H
 
 #ifndef SWIG
-#define CGAL_VERSION 5.4-beta2
+#define CGAL_VERSION 5.4
 #define CGAL_GIT_HASH abcdef
 #endif
 #define CGAL_VERSION_NR 1050400920
 #define CGAL_SVN_REVISION 99999
-#define CGAL_RELEASE_DATE 20211215
+#define CGAL_RELEASE_DATE 20220128
 
 #include <CGAL/version_macros.h>
 
