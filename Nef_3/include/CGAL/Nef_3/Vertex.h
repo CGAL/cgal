2d4a857c81f6766735a056714c36bdc94cf5bd79
Giles Bathgate <giles.bathgate@gmail.com>
2022-03-12 12:06:17 +0000

    Make boundary_entry_objects_ private and use accessor method

--- Nef_3/include/CGAL/Nef_3/Vertex.h
+++ Nef_3/include/CGAL/Nef_3/Vertex.h
@@ -261,8 +261,7 @@ class Vertex_base {
           fend = sfaces_end();
         while (fit != fend) {
           SFace_iterator fdel = fit++;
-          CGAL_assertion(&fdel->boundary_entry_objects_ == &fdel->boundary_entry_objects());
-          sncp()->reset_sm_object_list(fdel->boundary_entry_objects_);
+          sncp()->reset_sm_object_list(fdel->boundary_entry_objects());
           sncp()->delete_sface_only(fdel);
         }
         sfaces_begin_ = sfaces_last_ = sncp()->sfaces_end();
