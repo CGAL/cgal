211cb75dd34cad6a07d3ea2ed18446c5fb36879a
Andreas Fabri <andreas.fabri@geometryfactory.com>
2022-03-18 15:42:17 +0100

    Nef_3: Avoid geometric test

--- Nef_3/include/CGAL/Nef_3/SNC_point_locator.h
+++ Nef_3/include/CGAL/Nef_3/SNC_point_locator.h
@@ -234,7 +234,7 @@ public:
       CGAL_for_each( o, candidates) {
         if( CGAL::assign( v, *o) && ((mask&1) != 0)) {
           _CGAL_NEF_TRACEN("trying vertex on "<<v->point());
-          if( ray.source() != v->point() && ray.has_on(v->point())) {
+          if( (ray_source_vertex != v) && (ray.source() != v->point()) && ray.has_on(v->point())) {
             _CGAL_NEF_TRACEN("the ray intersects the vertex");
             _CGAL_NEF_TRACEN("prev. intersection? "<<hit);
             CGAL_assertion_code
