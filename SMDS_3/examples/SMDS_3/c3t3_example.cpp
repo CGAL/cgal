8c5fe5fd0e072f3e9668ccd74bd1449060baafbe
Jane Tournois <jane.tournois@gmail.com>
2022-03-24 14:17:58 +0100

    use "simplicial" types in example

--- SMDS_3/examples/SMDS_3/c3t3_example.cpp
+++ SMDS_3/examples/SMDS_3/c3t3_example.cpp
@@ -1,27 +1,46 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
-#include <CGAL/Tetrahedral_remeshing/Remeshing_triangulation_3.h>
-
+#include <CGAL/Triangulation_3.h>
+#include <CGAL/Triangulation_data_structure_3.h>
+#include <CGAL/Simplicial_mesh_cell_base_3.h>
+#include <CGAL/Simplicial_mesh_vertex_base_3.h>
 #include <CGAL/Mesh_complex_3_in_triangulation_3.h>
 
-#include <CGAL/IO/File_medit.h>
+#include <CGAL/Tetrahedral_remeshing.h>
 
+#include <CGAL/tags.h>
+
+#include <CGAL/IO/File_medit.h>
 #include <fstream>
+
 
 using K = CGAL::Exact_predicates_inexact_constructions_kernel;
 
-using Remeshing_triangulation = CGAL::Tetrahedral_remeshing::Remeshing_triangulation_3<K>;
+using Subdomain_index = int;
+using Surface_patch_index = unsigned char;
+using Curve_index = char;
+using Corner_index = short;
+
+using Cb = CGAL::Simplicial_mesh_cell_base_3<K, Subdomain_index, Surface_patch_index>;
+using Vb = CGAL::Simplicial_mesh_vertex_base_3<K, Subdomain_index, Surface_patch_index,
+                                                  Curve_index, Corner_index>;
+
+using Tds = CGAL::Triangulation_data_structure_3<Vb, Cb, CGAL::Sequential_tag>;
+using Triangulation = CGAL::Triangulation_3<K, Tds>;
+
+using C3t3 = CGAL::Mesh_complex_3_in_triangulation_3<Triangulation>;
+
 
 int main(int argc, char* argv[])
 {
   const char* filename = (argc > 1) ? argv[1] : "data/sphere.mesh";
 
-  Remeshing_triangulation tr;
+  Triangulation tr;
 
   std::ifstream is(filename, std::ios_base::in);
   CGAL::IO::read_MEDIT(is, tr);
 
-  // [call to a remeshing algorithm]
+  // [call a remeshing algorithm]
 
   std::ofstream os("after_remeshing.mesh");
   CGAL::IO::write_MEDIT(os, tr);
