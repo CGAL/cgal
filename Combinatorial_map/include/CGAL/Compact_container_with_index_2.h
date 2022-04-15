f1199c2fda9dc4f4c60af400de9e8360a5f01048
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2022-04-15 11:11:26 +0200

    spdx

--- Combinatorial_map/include/CGAL/Compact_container_with_index_2.h
+++ Combinatorial_map/include/CGAL/Compact_container_with_index_2.h
@@ -1,22 +1,14 @@
-// Copyright (c) 2003,2004,2007-2010  INRIA Sophia-Antipolis (France).
+// Copyright (c) 2010-2011 CNRS and LIRIS' Establishments (France).
 // All rights reserved.
 //
-// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; either version 3 of the License,
-// or (at your option) any later version.
-//
-// Licensees holding a valid commercial license may use this file in
-// accordance with the commercial license agreement provided with the software.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+// This file is part of CGAL (www.cgal.org)
 //
 // $URL$
 // $Id$
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
 //
-// Author(s)     : Sylvain Pion
-
+// Author(s)     : Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
+//
 #ifndef CGAL_COMPACT_CONTAINER_WITH_INDEX_2_H
 #define CGAL_COMPACT_CONTAINER_WITH_INDEX_2_H
 
