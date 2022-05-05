a131617c29c7f1b030dcd9c65d171e3bc7b3aaae
Sébastien Loriot <sebastien.loriot@cgal.org>
2022-05-05 08:51:38 +0200

    fix include directive

--- Convex_hull_3/include/CGAL/Convex_hull_3/predicates.h
+++ Convex_hull_3/include/CGAL/Convex_hull_3/predicates.h
@@ -17,7 +17,7 @@
 #include <CGAL/license/Convex_hull_3.h>
 
 #include <CGAL/Named_function_parameters.h>
-#include <CGAL/Polygon_mesh_processing/internal/named_params_helper.h>
+#include <CGAL/boost/graph/named_params_helper.h>
 
 #include <vector>
 
