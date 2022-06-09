56b21dde1606378b7a4f6c66a5f7e8bc806f70f2
Sven Oesau <sven.oesau@geometryfactory.com>
2022-06-09 10:39:17 +0200

    removed BSL-1.0 license

--- Shape_detection/include/CGAL/Shape_detection/Region_growing/internal/cylinder_fitting.h
+++ Shape_detection/include/CGAL/Shape_detection/Region_growing/internal/cylinder_fitting.h
@@ -5,10 +5,11 @@
 //
 // $URL$
 // $Id$
-// SPDX-License-Identifier: ( GPL-3.0-or-later OR LicenseRef-Commercial ) AND BSL-1.0
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 //
 // Author(s)     : Sven Oesau
-//
+
+// Header of the original source
 // This file incorporates work covered by the following copyright and
 // permission notice:
 //
