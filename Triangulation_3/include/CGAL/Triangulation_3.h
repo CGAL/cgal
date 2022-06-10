14b582f83de2bb73a743742724f8ded08682f033
Laurent Rineau <laurent.rineau@cgal.org>
2022-06-10 09:15:11 +0200

    More use our free of `outer_map`

--- Triangulation_3/include/CGAL/Triangulation_3.h
+++ Triangulation_3/include/CGAL/Triangulation_3.h
@@ -5367,6 +5367,7 @@ remove_3D(Vertex_handle v, VertexRemover& remover,
     }
 
     typename Vertex_triple_Facet_map::value_type o_vt_f_pair = *oit;
+    outer_map.erase(oit);
     Cell_handle o_ch = o_vt_f_pair.second.first;
     unsigned int o_i = o_vt_f_pair.second.second;
 
@@ -5413,7 +5414,6 @@ remove_3D(Vertex_handle v, VertexRemover& remover,
         }
       }
     }
-    outer_map.erase(oit);
   }
   tds().delete_vertex(v);
   tds().delete_cells(inc_cells.begin(), inc_cells.end());
@@ -5670,6 +5670,7 @@ remove_3D(Vertex_handle v, VertexRemover& remover, OutputItCells fit)
     }
 
     typename Vertex_triple_Facet_map::value_type o_vt_f_pair = *oit;
+    outer_map.erase(oit);
     Cell_handle o_ch = o_vt_f_pair.second.first;
     unsigned int o_i = o_vt_f_pair.second.second;
 
@@ -5717,7 +5718,6 @@ remove_3D(Vertex_handle v, VertexRemover& remover, OutputItCells fit)
         }
       }
     }
-    outer_map.erase(oit);
   }
   tds().delete_vertex(v);
   tds().delete_cells(hole.begin(), hole.end());
@@ -6851,6 +6851,7 @@ _remove_cluster_3D(InputIterator first, InputIterator beyond, VertexRemover& rem
       }
 
       typename Vertex_triple_Facet_map::value_type o_vt_f_pair = *oit;
+      outer_map.erase(oit);
       Cell_handle o_ch = o_vt_f_pair.second.first;
       unsigned int o_i = o_vt_f_pair.second.second;
 
@@ -6900,7 +6901,6 @@ _remove_cluster_3D(InputIterator first, InputIterator beyond, VertexRemover& rem
         }
       }
 
-      outer_map.erase(oit);
     }
 
     this->tds().delete_cells(hole.begin(), hole.end());
