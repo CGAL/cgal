10903485c5a116c50d16438f371a19ae5d78de75
Sébastien Loriot <sebastien.loriot@cgal.org>
2022-06-30 09:19:31 +0200

    rename typedef

--- Straight_skeleton_2/examples/Straight_skeleton_2/exterior_offset_of_multiple_polygons_with_holes.cpp
+++ Straight_skeleton_2/examples/Straight_skeleton_2/exterior_offset_of_multiple_polygons_with_holes.cpp
@@ -14,11 +14,11 @@
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K ;
 
 typedef K::Point_2                    Point ;
-typedef CGAL::Polygon_2<K>            Polygon ;
+typedef CGAL::Polygon_2<K>            Polygon_2 ;
 typedef CGAL::Polygon_with_holes_2<K> PolygonWithHoles ;
 
 typedef boost::shared_ptr<PolygonWithHoles> PolygonWithHolesPtr ;
-typedef boost::shared_ptr<Polygon> PolygonPtr ;
+typedef boost::shared_ptr<Polygon_2> PolygonPtr ;
 
 typedef std::vector<PolygonWithHolesPtr> PolygonWithHolesPtrVector;
 typedef std::vector<PolygonPtr> PolygonPtrVector;
@@ -45,13 +45,13 @@ exterior_offset_of_disjoint_polygons_with_holes(double lOffset, const std::vecto
     double fymin = bbox.ymin() - lm ;
     double fymax = bbox.ymax() + lm ;
 
-    Polygon frame ;
+    Polygon_2 frame ;
     frame.push_back( Point(fxmin,fymin) );
     frame.push_back( Point(fxmax,fymin) );
     frame.push_back( Point(fxmax,fymax) );
     frame.push_back( Point(fxmin,fymax) );
 
-    std::vector<Polygon> outer_as_holes;
+    std::vector<Polygon_2> outer_as_holes;
     outer_as_holes.reserve(pwhs.size());
     for (const PolygonWithHoles& pwh : pwhs)
       outer_as_holes.emplace_back(pwh.outer_boundary().container().rbegin(),
@@ -86,7 +86,7 @@ exterior_offset_of_disjoint_polygons_with_holes(double lOffset, const std::vecto
                                                  hit!=pwh.holes_end();
                                                  ++hit)
       {
-        Polygon h = *hit;
+        Polygon_2 h = *hit;
         h.reverse_orientation();
         PolygonPtrVector off_hole = CGAL::create_interior_skeleton_and_offset_polygons_2(lOffset,h);
         off_polys.insert(off_polys.end(), off_hole.begin(), off_hole.end());
@@ -105,14 +105,14 @@ int main()
 
   for (int i=0; i<4; ++i)
   {
-    Polygon outer;
+    Polygon_2 outer;
     outer.push_back( Point(i+0+i*10, 0) );
     outer.push_back( Point(i+0+(i+1)*10, 0) );
     outer.push_back( Point(i+0+(i+1)*10, 10) );
     outer.push_back( Point(i+0+i*10, 10) );
     pwhs.emplace_back(outer);
 
-    Polygon hole;
+    Polygon_2 hole;
     hole.push_back( Point(i+3+i*10,3) ) ;
     hole.push_back( Point(i+6+i*10,3) ) ;
     hole.push_back( Point(i+6+i*10,6) ) ;
