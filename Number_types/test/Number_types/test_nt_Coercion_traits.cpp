c1afb483f58a22a6880e053abf063f8fb42def8b
Sébastien Loriot <sebastien.loriot@cgal.org>
2022-07-19 09:04:19 +0200

    licence -> license

--- Number_types/test/Number_types/test_nt_Coercion_traits.cpp
+++ Number_types/test/Number_types/test_nt_Coercion_traits.cpp
@@ -1,17 +1,3 @@
-// TODO: Add licence
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-//
-// $URL$
-// $Id$
-//
-//
-// Author(s)     : Michael Hemmer <mhemmer@uni-mainz.de>
-//
-// ============================================================================
-
-
 #include <CGAL/use.h>
 #include <cassert>
 #include <CGAL/Arithmetic_kernel.h>
