c3541b2c6f963c4c107026c4867cb3f400baa67b
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2022-07-20 10:06:10 +0200

    Use the new version.

--- Linear_cell_complex/examples/Linear_cell_complex/draw_linear_cell_complex.cpp
+++ Linear_cell_complex/examples/Linear_cell_complex/draw_linear_cell_complex.cpp
@@ -1,5 +1,7 @@
 #include <CGAL/Linear_cell_complex_for_combinatorial_map.h>
-#include <CGAL/draw_linear_cell_complex.h>
+// NOTE: deprecated, would remove
+// #include <CGAL/draw_linear_cell_complex.h>
+#include <CGAL/draw_linear_cell_complex_function.h>
 
 typedef CGAL::Linear_cell_complex_for_combinatorial_map<3> LCC;
 typedef LCC::Dart_handle Dart_handle;
