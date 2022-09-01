421c88d3633a75b539a62d7867a8716fe5ea4763
Sven Oesau <sven.oesau@geometryfactory.com>
2022-09-01 19:30:09 +0200

    using m_region_map instead of m_visited resolves failed test on Archlinux-clang-release

--- Shape_detection/include/CGAL/Shape_detection/Region_growing/Region_growing.h
+++ Shape_detection/include/CGAL/Shape_detection/Region_growing/Region_growing.h
@@ -290,7 +290,7 @@ namespace Shape_detection {
 
       for (auto it = input_range.begin(); it != input_range.end(); it++) {
         Item i = get(item_map_,it);
-        if (!get(m_visited, i))
+        if (get(m_region_map, i) == std::size_t(-1))
           *(output++) = i;
       }
       return output;
