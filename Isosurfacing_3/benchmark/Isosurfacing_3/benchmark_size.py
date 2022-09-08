bc08299304adcb1d755c173d11a66b0011da44ec
Julian Stahl <julianstahl.dev@gmail.com>
2022-09-08 14:57:30 +0200

    Fix benchmarks

--- Isosurfacing_3/benchmark/Isosurfacing_3/benchmark_size.py
+++ Isosurfacing_3/benchmark/Isosurfacing_3/benchmark_size.py
@@ -3,12 +3,13 @@ from benchmark_util import *
 scenario = "SCENARIO_GRID_SPHERE"
 kernel = "KERNEL_SIMPLE_CARTESIAN_FLOAT"
 algorithm = "ALGO_DUAL_CONTOURING"
+tag = "TAG_SEQUENTIAL"
 threads = 1
-exponent = 2
-min_cells = 10
-max_cells = 1000
+exponent = 1.2
+min_cells = 1000000
+max_cells = 100000000
 
-build(scenario, kernel, algorithm)
+build(scenario, kernel, algorithm, tag)
 
 data = []
 
@@ -17,10 +18,10 @@ while c < max_cells:
 	n = int(c ** (1.0 / 3.0))
 
 	time = execute(n)
-	data.append([scenario, kernel, algorithm, threads, c, time])
+	data.append([scenario, kernel, algorithm, tag, threads, int(c), time])
 
 	c *= exponent
 
-df = pd.DataFrame(data, comulmns=["scenario", "kernel", "algorithm", "threads", "cells", "time"])
+df = pd.DataFrame(data, columns=["scenario", "kernel", "algorithm", "tag", "threads", "cells", "time"])
 
 df.to_csv("benchmark_size.csv")
--- Isosurfacing_3/benchmark/Isosurfacing_3/benchmark_threads.py
+++ Isosurfacing_3/benchmark/Isosurfacing_3/benchmark_threads.py
@@ -2,20 +2,21 @@ from benchmark_util import *
 
 scenario = "SCENARIO_GRID_SPHERE"
 kernel = "KERNEL_SIMPLE_CARTESIAN_FLOAT"
-algorithm = "ALGO_DUAL_CONTOURING"
+algorithm = "ALGO_MARCHING_CUBES"
+tag = "TAG_PARALLEL"
 min_threads = 1
 max_threads = 8
-cells = 1000000
+cells = 500
 
-build(scenario, kernel, algorithm)
+build(scenario, kernel, algorithm, tag)
 
 data = []
 
 for t in range(min_threads, max_threads):
 	time = execute(cells)
 
-	data.append([scenario, kernel, algorithm, t, cells, 0])
+	data.append([scenario, kernel, algorithm, tag, t, cells, time])
 
-df = pd.DataFrame(data, comulmns=["scenario", "kernel", "algorithm", "threads", "cells", "time"])
+df = pd.DataFrame(data, columns=["scenario", "kernel", "algorithm", "tag", "threads", "cells", "time"])
 
 df.to_csv("benchmark_threads.csv")
