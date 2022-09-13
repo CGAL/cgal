07059805366240b627aa102f8fb3978f3134e606
Julian Stahl <julianstahl.dev@gmail.com>
2022-09-13 17:05:41 +0200

    Improve benchmarks

--- Isosurfacing_3/benchmark/Isosurfacing_3/benchmark_size.py
+++ Isosurfacing_3/benchmark/Isosurfacing_3/benchmark_size.py
@@ -1,12 +1,12 @@
 from benchmark_util import *
 
-scenario = "SCENARIO_GRID_SPHERE"
-kernel = "KERNEL_SIMPLE_CARTESIAN_FLOAT"
-algorithm = "ALGO_DUAL_CONTOURING"
+scenario = "SCENARIO_IMPLICIT_IWP"
+kernel = "KERNEL_SIMPLE_CARTESIAN_DOUBLE"
+algorithm = "ALGO_MARCHING_CUBES"
 tag = "TAG_SEQUENTIAL"
 threads = 1
 exponent = 1.2
-min_cells = 1000000
+min_cells = 100000
 max_cells = 100000000
 
 build(scenario, kernel, algorithm, tag)
@@ -17,11 +17,11 @@ c = min_cells
 while c < max_cells:
 	n = int(c ** (1.0 / 3.0))
 
-	time = execute(n, threads)
-	data.append([scenario, kernel, algorithm, tag, threads, int(c), time])
+	res = execute(n, threads, 5)
+	data.append([scenario, kernel, algorithm, tag, threads, int(c), res["time"], res["polygons"], res["points"]])
 
 	c *= exponent
 
-df = pd.DataFrame(data, columns=["scenario", "kernel", "algorithm", "tag", "threads", "cells", "time"])
+df = pd.DataFrame(data, columns=["scenario", "kernel", "algorithm", "tag", "threads", "cells", "time", "polygons", "points"])
 
 df.to_csv("benchmark_size.csv")
--- Isosurfacing_3/benchmark/Isosurfacing_3/benchmark_threads.py
+++ Isosurfacing_3/benchmark/Isosurfacing_3/benchmark_threads.py
@@ -1,12 +1,12 @@
 from benchmark_util import *
 
-scenario = "SCENARIO_IMPLICIT_SPHERE"
+scenario = "SCENARIO_IMPLICIT_IWP"
 kernel = "KERNEL_SIMPLE_CARTESIAN_DOUBLE"
 algorithm = "ALGO_MARCHING_CUBES"
 tag = "TAG_PARALLEL"
 min_threads = 1
-max_threads = 12
-n = 300
+max_threads = 26
+n = 500
 cells = n ** 3
 
 build(scenario, kernel, algorithm, tag)
@@ -14,10 +14,10 @@ build(scenario, kernel, algorithm, tag)
 data = []
 
 for t in range(min_threads, max_threads):
-	time = execute(n, t)
+	res = execute(n, t, times=5)
 
-	data.append([scenario, kernel, algorithm, tag, t, cells, time])
+	data.append([scenario, kernel, algorithm, tag, t, cells, res["time"], res["bandwidth"]])
 
-df = pd.DataFrame(data, columns=["scenario", "kernel", "algorithm", "tag", "threads", "cells", "time"])
+df = pd.DataFrame(data, columns=["scenario", "kernel", "algorithm", "tag", "threads", "cells", "time", "bandwidth"])
 
 df.to_csv("benchmark_threads.csv")
