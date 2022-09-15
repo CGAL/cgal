953b01db1f2e9f5b9b970b2fffc86767e5691179
Sébastien Loriot <sebastien.loriot@cgal.org>
2022-09-15 15:10:12 +0200

    regroup code for named parameters

--- Mesh_3/include/CGAL/refine_mesh_3.h
+++ Mesh_3/include/CGAL/refine_mesh_3.h
@@ -28,19 +28,9 @@
 #include <CGAL/optimize_mesh_3.h>
 #include <CGAL/SMDS_3/Dump_c3t3.h>
 #include <CGAL/Named_function_parameters.h>
-
+#include <CGAL/Mesh_3/parameters.h>
 
 #include <atomic>
-
-#define CGAL_BOOLEAN_PARAMETER(Class, function_true, function_false)     \
-  struct Class : public Base<bool> { Class(bool b) : Base<bool>(b){} };       \
-  inline Named_function_parameters<Class, internal_np::reset_options_param_t> function_true() { \
-  typedef Named_function_parameters<Class, internal_np::reset_options_param_t> Param;           \
-  return Param(Class(true)); }                        \
-  inline Named_function_parameters<Class, internal_np::reset_options_param_t> function_false() {\
-  typedef Named_function_parameters<Class, internal_np::reset_options_param_t> Param;           \
-  return Param(Class(false)); }
-
 
 namespace CGAL {
 
@@ -109,174 +99,6 @@ private:
 };
 
 } // namespace details
-
-namespace parameters {
-
-namespace internal {
-
-const int undef_parameter = -1;
-
-// Helpers
-struct Optimization_options_base
-{
-  Optimization_options_base(bool b)
-  : b_(b), time_limit_(undef_parameter), bound_(undef_parameter) {}
-
-  operator bool() const { return b_; }
-
-  bool is_time_limit_set() const { return time_limit_ != undef_parameter; }
-  void set_time_limit(double d) { time_limit_ = d; }
-  double time_limit() const { return time_limit_; }
-
-  bool is_bound_set() const { return bound_ != undef_parameter; }
-  void set_bound(double d) { bound_ = d; }
-  double bound() const { return bound_; }
-
-private:
-  bool b_;
-  double time_limit_;
-  double bound_;
-};
-
-struct Global_optimization_options_base
-{
-  Global_optimization_options_base()
-  : convergence_(undef_parameter), max_it_nb_(undef_parameter) {}
-
-  bool is_convergence_set() const { return convergence_ != undef_parameter; }
-  void set_convergence(double d) { convergence_ = d; }
-  double convergence() const { return convergence_; }
-
-  bool is_max_iteration_number_set() const { return max_it_nb_ != undef_parameter; }
-  void set_max_iteration_number(int i) { max_it_nb_ = i; }
-  int max_iteration_number() const { return max_it_nb_; }
-
-private:
-  double convergence_;
-  int max_it_nb_;
-};
-
-// Perturb
-struct Perturb_options : public Optimization_options_base
-{
-  Perturb_options(bool b) : Optimization_options_base(b) {}
-};
-
-// Exude
-struct Exude_options : public Optimization_options_base
-{
-  Exude_options(bool b) : Optimization_options_base(b) {}
-};
-
-// Odt
-struct Odt_options : public Optimization_options_base
-, public Global_optimization_options_base
-{
-  Odt_options(bool b) : Optimization_options_base(b)
-  , Global_optimization_options_base() {}
-};
-
-// Lloyd
-struct Lloyd_options : public Optimization_options_base
-, public Global_optimization_options_base
-{
-  Lloyd_options(bool b) : Optimization_options_base(b)
-  , Global_optimization_options_base() {}
-};
-
-// Manifold
-struct Manifold_options {
-  enum {
-    NON_MANIFOLD = 0,
-    MANIFOLD_WITH_BOUNDARY = 8,
-    NO_BOUNDARY = 16,
-    MANIFOLD = 24
-  };
-
-  Manifold_options(const int topology)
-    : mesh_topology(topology)
-  {}
-  Manifold_options()
-    : mesh_topology(NON_MANIFOLD)
-  {}
-
-  int mesh_topology;
-};
-
-// Various Mesh_3 option
-struct Mesh_3_options {
-#ifndef CGAL_NO_ATOMIC
-      typedef std::atomic<bool>* Pointer_to_stop_atomic_boolean_t;
-#else
-      typedef bool* Pointer_to_stop_atomic_boolean_t;
-#endif
-  Mesh_3_options(bool nonlinear = false)
-    // This parameter `nonlinear` adds a compatibility with previous
-    // API of the constructor of `C3t3_initializer`.
-    // -- Laurent Rineau, 2019/05/03
-    : dump_after_init_prefix()
-    , dump_after_refine_surface_prefix()
-    , dump_after_refine_prefix()
-    , dump_after_glob_opt_prefix()
-    , dump_after_perturb_prefix()
-    , dump_after_exude_prefix()
-    , number_of_initial_points(-1)
-    , nonlinear_growth_of_balls(nonlinear)
-    , maximal_number_of_vertices(0)
-    , pointer_to_error_code(0)
-#ifndef CGAL_NO_ATOMIC
-    , pointer_to_stop_atomic_boolean(0)
-#endif
-  {}
-
-  std::string dump_after_init_prefix;
-  std::string dump_after_refine_surface_prefix;
-  std::string dump_after_refine_prefix;
-  std::string dump_after_glob_opt_prefix;
-  std::string dump_after_perturb_prefix;
-  std::string dump_after_exude_prefix;
-  int number_of_initial_points;
-  bool nonlinear_growth_of_balls;
-  std::size_t maximal_number_of_vertices;
-  Mesh_error_code* pointer_to_error_code;
-#ifndef CGAL_NO_ATOMIC
-  Pointer_to_stop_atomic_boolean_t pointer_to_stop_atomic_boolean;
-#endif
-
-}; // end struct Mesh_3_options
-
-} // end namespace internal
-
-// Undocumented Boost parameter for refine_mesh_3 and make_mesh_3.
-// Default Mesh_3_options: dump at every stage of the mesh generation.
-inline internal::Mesh_3_options mesh_3_dump()
-{
-  internal::Mesh_3_options options;
-
-  options.dump_after_init_prefix = "mesh_dump_after_init";
-  options.dump_after_refine_surface_prefix = "mesh_dump_after_refine_surface";
-  options.dump_after_refine_prefix = "mesh_dump_after_refine";
-  options.dump_after_glob_opt_prefix = "mesh_dump_after_glob_opt";
-  options.dump_after_perturb_prefix = "mesh_dump_after_perturb";
-  options.dump_after_exude_prefix = "mesh_dump_after_exude";
-
-  return options;
-}
-        template <typename T>
-        struct Base
-        {
-            Base(T t) : t_(t) {}
-            T operator()() const { return t_; }
-        private:
-            T t_;
-        };
-
-// -----------------------------------
-// Reset_c3t3 (undocumented)
-// -----------------------------------
-  CGAL_BOOLEAN_PARAMETER(Reset,reset_c3t3,no_reset_c3t3)
-
-} // end namespace parameters
 
 /*!
 \ingroup PkgMesh3Functions
@@ -602,7 +424,5 @@ void refine_mesh_3_impl(C3T3& c3t3,
 }
 #endif // DOXYGEN_RUNNING
 } // end namespace CGAL
-
-#include <CGAL/Mesh_3/parameters.h>
 
 #endif // CGAL_REFINE_MESH_3_H
