1f10fb93d7692da50013a274c30430d6e2370e8c
Julian Stahl <julianstahl.dev@gmail.com>
2022-09-15 11:35:37 +0200

    Change triangle creation

--- Isosurfacing_3/include/CGAL/TC_marching_cubes_3.h
+++ Isosurfacing_3/include/CGAL/TC_marching_cubes_3.h
@@ -1,3 +1,14 @@
+// Copyright (c) 2022 INRIA Sophia-Antipolis (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : Julian Stahl
+
 #ifndef CGAL_TMC_3_H
 #define CGAL_TMC_3_H
 
