5bac2a264d9f18637fb5689c017c66015b216a75
Julian Stahl <julianstahl.dev@gmail.com>
2022-09-21 01:32:22 +0200

    Add parenthesis around min / max

--- Isosurfacing_3/include/CGAL/Isosurfacing_3/internal/Cell_type.h
+++ Isosurfacing_3/include/CGAL/Isosurfacing_3/internal/Cell_type.h
@@ -13,6 +13,7 @@
 #define CGAL_DOMAIN_CELL_TYPE
 
 #include <CGAL/license/Isosurfacing_3.h>
+
 #include <limits>
 
 namespace CGAL {
@@ -20,7 +21,7 @@ namespace Isosurfacing {
 
 typedef std::size_t Cell_type;
 
-static constexpr Cell_type ANY_CELL = std::numeric_limits<Cell_type>::max();
+static constexpr Cell_type ANY_CELL = (std::numeric_limits<Cell_type>::max)();
 
 static constexpr Cell_type POLYHERDAL_CELL = ((Cell_type)1) << 0;
 static constexpr Cell_type TETRAHEDRAL_CELL = ((Cell_type)1) << 1;
