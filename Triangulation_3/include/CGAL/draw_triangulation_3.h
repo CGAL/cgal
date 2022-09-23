cf51780c0ddc6adda9eabac32b937eb6af8946a8
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2022-09-23 19:48:07 +0200

    small modifs

--- Triangulation_3/include/CGAL/draw_triangulation_3.h
+++ Triangulation_3/include/CGAL/draw_triangulation_3.h
@@ -34,13 +34,10 @@ void compute_face(typename T3::Finite_facets_iterator fh,
                   const DrawingFunctor &drawing_functor,
                   CGAL::GraphicBuffer<BufferType> &graphic_buffer, const T3 *t3)
 {
-  // DONE: TEST draw_face
   if(!drawing_functor.draw_face(*t3, fh))
-  {
-    return;
-  }
+  { return; }
 
-  if(drawing_functor.face_color)
+  if(drawing_functor.colored_face(*t3, fh) && drawing_functor.face_color)
   {
     CGAL::IO::Color c = drawing_functor.face_color(*t3, fh);
     graphic_buffer.face_begin(c);
@@ -60,13 +57,13 @@ void compute_face(typename T3::Finite_facets_iterator fh,
 
 template <typename BufferType = float, class T3, class DrawingFunctor>
 void compute_edge(typename T3::Finite_edges_iterator eh,
-                  CGAL::GraphicBuffer<BufferType> &graphic_buffer, const DrawingFunctor &drawing_functor, const T3 *t3) {
-  // DONE: TEST draw_edge + edge color
-  if(!drawing_functor.draw_edge(*t3, eh)) {
-      return;
-  }
-  if(drawing_functor.edge_color)  {
+                  CGAL::GraphicBuffer<BufferType> &graphic_buffer, const DrawingFunctor &drawing_functor, const T3 *t3)
+{
+  if(!drawing_functor.draw_edge(*t3, eh))
+  { return; }
 
+  if(drawing_functor.colored_edge(*t3, eh) && drawing_functor.edge_color)
+  {
     CGAL::IO::Color c = drawing_functor.edge_color(*t3, eh);
     graphic_buffer.add_segment(eh->first->vertex(eh->second)->point(),
                              eh->first->vertex(eh->third)->point(), c);
@@ -79,35 +76,28 @@ void compute_edge(typename T3::Finite_edges_iterator eh,
 
 template <typename BufferType = float, class T3, class DrawingFunctor>
 void compute_vertex(typename T3::Vertex_handle vh,
-                    CGAL::GraphicBuffer<BufferType> &graphic_buffer, const DrawingFunctor &drawing_functor, const T3 *t3) {
-
-  // DONE TEST draw_vertex  + vertex color
+                    CGAL::GraphicBuffer<BufferType> &graphic_buffer, const DrawingFunctor &drawing_functor, const T3 *t3)
+{
   if(!drawing_functor.draw_vertex(*t3, vh))
-  {
-    return;
-  }
+  { return; }
 
-  if(drawing_functor.vertex_color)
+  if(drawing_functor.colored_vertex(*t3, vh) && drawing_functor.vertex_color)
   {
     CGAL::IO::Color c = drawing_functor.vertex_color(*t3, vh);
     graphic_buffer.add_point(vh->point(), c);
   }
   else
   { graphic_buffer.add_point(vh->point()); }
-
 }
 
 template <typename BufferType = float, class T3, class DrawingFunctor>
 void compute_elements(CGAL::GraphicBuffer<BufferType> &graphic_buffer, const T3 *t3,
-                      const DrawingFunctor &drawing_functor,
-                      bool m_nofaces = false) {
-
-  if (!m_nofaces) {
-    for (typename T3::Finite_facets_iterator it = t3->finite_facets_begin();
-         it != t3->finite_facets_end(); ++it)
-    {
-      compute_face(it, drawing_functor, graphic_buffer, t3);
-    }
+                      const DrawingFunctor &drawing_functor)
+{
+  for (typename T3::Finite_facets_iterator it = t3->finite_facets_begin();
+       it != t3->finite_facets_end(); ++it)
+  {
+    compute_face(it, drawing_functor, graphic_buffer, t3);
   }
 
   for (typename T3::Finite_edges_iterator it = t3->finite_edges_begin();
@@ -122,7 +112,7 @@ void compute_elements(CGAL::GraphicBuffer<BufferType> &graphic_buffer, const T3 
     compute_vertex(it, graphic_buffer, drawing_functor, t3);
   }
 }
-
+  
 } // namespace draw_function_for_t3
 
 template <typename BufferType = float, class T3, class DrawingFunctor>
@@ -158,7 +148,12 @@ void draw(const CGAL_T3_TYPE &at3, const char *title = "T3 Basic Viewer")
                        typename CGAL_T3_TYPE::Finite_facets_iterator>
     drawingfunctor;
 
-  drawingfunctor.face_color =
+  drawingfunctor.colored_face =
+    [](const CGAL_T3_TYPE &at3, const typename CGAL_T3_TYPE::Finite_facets_iterator fh)
+    -> bool
+    { return true; };
+
+      drawingfunctor.face_color =
     [](const CGAL_T3_TYPE &at3, const typename CGAL_T3_TYPE::Finite_facets_iterator fh)
     -> CGAL::IO::Color
     {
