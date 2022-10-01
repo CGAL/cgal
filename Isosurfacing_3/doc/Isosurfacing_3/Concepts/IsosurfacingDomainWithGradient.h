e645be122aeaa69de7216be5efa94c85dae764de
Julian Stahl <julianstahl.dev@gmail.com>
2022-10-01 21:08:07 +0200

    Specialize domain with gradient in doc

---
+++ Isosurfacing_3/doc/Isosurfacing_3/Concepts/IsosurfacingDomainWithGradient.h
@@ -0,0 +1,36 @@
+/*!
+\ingroup PkgIsosurfacing3Concepts
+\cgalConcept
+
+The concept `IsosurfacingDomainWithGradient` describes the set of requirements to be
+fulfilled by any class used as input data for some isosurfacing algorithms.
+
+\cgalHasModel `CGAL::Isosurfacing::Explicit_cartesian_grid_domain`
+\cgalHasModel `CGAL::Isosurfacing::Implicit_cartesian_grid_domain`
+\cgalHasModel `CGAL::Isosurfacing::Implicit_octree_domain`
+
+*/
+
+class IsosurfacingDomainWithGradient : public IsosurfacingDomain {
+public:
+    /// \name Types
+    /// @{
+
+    /*!
+    The vector type.
+    */
+    typedef unspecified_type Vector;
+
+    /// @}
+
+    /// \name Operations
+    /// The following member functions must exist.
+    /// @{
+
+    /*!
+    Returns the gradient at the position p
+    */
+    Vector gradient(const Point& p) const;
+
+    /// @}
+};
