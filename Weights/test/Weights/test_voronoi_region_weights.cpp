f4f6229c428e182fa59efb11e8a59901cab3cbc3
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2022-10-20 17:34:02 +0200

    Weights examples/tests improvements

--- Weights/test/Weights/test_voronoi_region_weights.cpp
+++ Weights/test/Weights/test_voronoi_region_weights.cpp
@@ -1,25 +1,26 @@
+#include "include/utils.h"
+#include "include/wrappers.h"
+
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 
-#include "include/utils.h"
-#include "include/wrappers.h"
-
-// Typedefs.
 using SCKER = CGAL::Simple_cartesian<double>;
 using EPICK = CGAL::Exact_predicates_inexact_constructions_kernel;
 using EPECK = CGAL::Exact_predicates_exact_constructions_kernel;
 
 template<typename Kernel>
-bool test_kernel() {
+void test_kernel()
+{
   const wrappers::Voronoi_region_wrapper<Kernel> vor;
-  return tests::test_region_weight<Kernel>(vor);
+  tests::test_region_weight<Kernel>(vor);
 }
 
-int main() {
-  assert(test_kernel<SCKER>());
-  assert(test_kernel<EPICK>());
-  assert(test_kernel<EPECK>());
+int main(int, char**)
+{
+  test_kernel<SCKER>();
+  test_kernel<EPICK>();
+  test_kernel<EPECK>();
   std::cout << "* test_voronoi_region_weights: SUCCESS" << std::endl;
   return EXIT_SUCCESS;
 }
