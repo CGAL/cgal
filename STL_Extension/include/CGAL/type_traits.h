51a1179d3ba37d295206d3e804a3d18f34eba748
Laurent Rineau <laurent.rineau@cgal.org>
2022-10-27 21:42:03 +0200

    Add c++20 std::remove_cvref, for convenience
    @mglisse Next time you need it... it is now here.

--- STL_Extension/include/CGAL/type_traits.h
+++ STL_Extension/include/CGAL/type_traits.h
@@ -27,6 +27,14 @@ struct is_same_or_derived :
   >::type
 {};
 
+template< class T >
+struct remove_cvref {
+    typedef std::remove_cv_t<std::remove_reference_t<T>> type;
+};
+
+template< class T >
+using remove_cvref_t = typename remove_cvref<T>::type;
+
 }
 
 #endif // CGAL_TYPE_TRAITS_H
