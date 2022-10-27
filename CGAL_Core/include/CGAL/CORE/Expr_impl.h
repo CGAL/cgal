27ca0dfb038711df6992017a0192e7a9970cc6fa
Andreas Fabri <andreas.fabri@geometryfactory.com>
2022-10-27 09:35:47 +0100

    BigInt and BigRep are no longer real CORE storage

--- CGAL_Core/include/CGAL/CORE/Expr_impl.h
+++ CGAL_Core/include/CGAL/CORE/Expr_impl.h
@@ -1215,8 +1215,6 @@ void BinOpRep::debugTree(int level, int indent, int depthLimit) const {
   second->debugTree(level, indent + 2, depthLimit - 1);
 }
 
-// AF CORE_MEMORY_IMPL(BigIntRep)
-// AF  CORE_MEMORY_IMPL(BigRatRep)
 CORE_MEMORY_IMPL(ConstDoubleRep)
 CORE_MEMORY_IMPL(ConstRealRep)
 
