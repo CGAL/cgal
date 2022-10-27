1c5454dcbe2dd0bedbca36f488b84185d59999d2
Laurent Rineau <laurent.rineau@cgal.org>
2022-10-27 11:42:13 +0200

    Add Converting_construction to Simple_precision_epick
    The difficulty is to detect/define which constructions are "trivial"
    and must not be wrapped into `Converting_construction`. Otherwise the
    use of `Cartesian_converter`, that uses trivial constructions itself,
    can lead to loops and infinite call stacks.

--- Kernel_23/include/CGAL/Exact_predicates_inexact_constructions_kernel.h
+++ Kernel_23/include/CGAL/Exact_predicates_inexact_constructions_kernel.h
@@ -19,34 +19,80 @@
 
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Filtered_kernel.h>
+#include <CGAL/Converting_construction.h>
 #include <CGAL/Triangulation_structural_filtering_traits.h>
 
 namespace CGAL {
 
-// The following is equivalent to Filtered_kernel< Simple_cartesian<double> >,
-// but it's shorter in terms of template name length (for error messages, mangling...).
+constexpr bool epick_use_static_filter =
+#ifdef CGAL_NO_STATIC_FILTERS
+    false;
+#else
+    true;
+#endif
 
 #ifndef CGAL_EPICK_SINGLE_PRECISION
-using Epick_number_type = double;
-#else
-using Epick_number_type = float;
-#endif
+// Here Epick is a class, and Double_precision_epick an alias to it.
+class Epick;
+class Single_precision_epick;
+using Double_precision_epick = Epick;
+#else // CGAL_EPICK_SINGLE_PRECISION
+// Here, Epick is an alias to Single_precision_epick.
+class Single_precision_epick;
+class Double_precision_epick;
+using Epick = Single_precision_epick;
+#endif // CGAL_EPICK_SINGLE_PRECISION
 
-class Epick
-  : public Filtered_kernel_adaptor<
-               Type_equality_wrapper< Simple_cartesian<Epick_number_type>::Base<Epick>::Type, Epick >,
-#ifdef CGAL_NO_STATIC_FILTERS
-               false >
-#else
-               true >
-#endif
+namespace internal {
+
+  // Basic objects, constructions, and predicates, using `double` for `FT`.
+  using Double_precision_epick_base =
+      Simple_cartesian<double>::Base<Double_precision_epick>::Type;
+
+  // Add the type equality property, by changing the objects types
+  using Double_precision_epick_base_with_type_equality =
+      Type_equality_wrapper<Double_precision_epick_base, Double_precision_epick>;
+
+  // Change the predicates to filtered predicates (with static filters or not)
+  using Double_precision_epick_with_filtered_predicates =
+      Filtered_kernel_adaptor<Double_precision_epick_base_with_type_equality, epick_use_static_filter>;
+
+  // Same, but with objects using `float` for FT
+  using Single_precision_epick_base =
+      Simple_cartesian<float>::Base<Single_precision_epick>::Type;
+  using Single_precision_epick_base_with_type_equality =
+      Type_equality_wrapper<Single_precision_epick_base, Single_precision_epick>;
+  using Single_precision_epick_with_filtered_predicates =
+      Filtered_kernel_adaptor<Single_precision_epick_base_with_type_equality, epick_use_static_filter>;
+};
+
+#ifndef CGAL_EPICK_SINGLE_PRECISION
+// The following is equivalent to Filtered_kernel< Simple_cartesian<double> >,
+// but it's shorter in terms of template name length (for error messages, mangling...).
+class Epick : public internal::Double_precision_epick_with_filtered_predicates
 {};
-
-typedef Epick Exact_predicates_inexact_constructions_kernel;
+#else // CGAL_EPICK_SINGLE_PRECISION
+class Double_precision_epick : public internal::Double_precision_epick_with_filtered_predicates
+{};
+#endif // CGAL_EPICK_SINGLE_PRECISION
 
 template <>
-struct Triangulation_structural_filtering_traits<Epick> {
-  typedef Tag_true Use_structural_filtering_tag;
+struct Triangulation_structural_filtering_traits<Double_precision_epick> {
+  using Use_structural_filtering_tag = Tag_true;
+};
+
+using Exact_predicates_inexact_constructions_kernel = Epick;
+
+
+// This kernel is Epick with the difference that its `FT` is `float`.
+class Single_precision_epick
+    : public Converting_constructions_kernel_adaptor<internal::Single_precision_epick_with_filtered_predicates,
+                                                     Double_precision_epick>
+{};
+
+template <>
+struct Triangulation_structural_filtering_traits<Single_precision_epick> {
+  using Use_structural_filtering_tag = Tag_true;
 };
 
 } //namespace CGAL
