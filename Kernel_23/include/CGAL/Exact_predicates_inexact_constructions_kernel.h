cb0ad7f5095f10325492f81944f83f1ba4e7a54a
Laurent Rineau <laurent.rineau@cgal.org>
2022-11-08 11:04:38 +0100

    Use template aliases to factorize code

--- Kernel_23/include/CGAL/Exact_predicates_inexact_constructions_kernel.h
+++ Kernel_23/include/CGAL/Exact_predicates_inexact_constructions_kernel.h
@@ -46,33 +46,28 @@ using Epick = Single_precision_epick;
 namespace internal {
 
   // Basic objects, constructions, and predicates, using `double` for `FT`.
-  using Double_precision_epick_base =
-      Simple_cartesian<double>::Base<Double_precision_epick>::Type;
+  template <typename NT, typename Kernel>
+  using Epick_base =
+      typename Simple_cartesian<NT>::template Base<Kernel>::Type;
 
   // Add the type equality property, by changing the objects types
-  using Double_precision_epick_base_with_type_equality =
-      Type_equality_wrapper<Double_precision_epick_base, Double_precision_epick>;
+  template <typename NT, typename Kernel>
+  using Epick_base_with_type_equality =
+      Type_equality_wrapper<Epick_base<NT, Kernel>, Kernel>;
 
   // Change the predicates to filtered predicates (with static filters or not)
-  using Double_precision_epick_with_filtered_predicates =
-      Filtered_kernel_adaptor<Double_precision_epick_base_with_type_equality, epick_use_static_filter>;
-
-  // Same, but with objects using `float` for FT
-  using Single_precision_epick_base =
-      Simple_cartesian<float>::Base<Single_precision_epick>::Type;
-  using Single_precision_epick_base_with_type_equality =
-      Type_equality_wrapper<Single_precision_epick_base, Single_precision_epick>;
-  using Single_precision_epick_with_filtered_predicates =
-      Filtered_kernel_adaptor<Single_precision_epick_base_with_type_equality, epick_use_static_filter>;
+  template <typename NT, typename Kernel>
+  using Epick_with_filtered_predicates =
+      Filtered_kernel_adaptor<Epick_base_with_type_equality<NT, Kernel>, epick_use_static_filter>;
 };
 
 #ifndef CGAL_EPICK_SINGLE_PRECISION
 // The following is equivalent to Filtered_kernel< Simple_cartesian<double> >,
 // but it's shorter in terms of template name length (for error messages, mangling...).
-class Epick : public internal::Double_precision_epick_with_filtered_predicates
+class Epick : public internal::Epick_with_filtered_predicates<double, Epick>
 {};
 #else // CGAL_EPICK_SINGLE_PRECISION
-class Double_precision_epick : public internal::Double_precision_epick_with_filtered_predicates
+class Double_precision_epick : public internal::Epick_with_filtered_predicates<double, Double_precision_epick>
 {};
 #endif // CGAL_EPICK_SINGLE_PRECISION
 
@@ -86,8 +81,9 @@ using Exact_predicates_inexact_constructions_kernel = Epick;
 
 // This kernel is Epick with the difference that its `FT` is `float`.
 class Single_precision_epick
-    : public Converting_constructions_kernel_adaptor<internal::Single_precision_epick_with_filtered_predicates,
-                                                     Double_precision_epick>
+    : public Converting_constructions_kernel_adaptor<
+          internal::Epick_with_filtered_predicates<float, Single_precision_epick>,
+          Double_precision_epick>
 {};
 
 template <>
