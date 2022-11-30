2a2e319061fee867e76ee10ca6f676a08120db62
Andreas Fabri <andreas.fabri@geometryfactory.com>
2022-11-30 08:16:57 +0000

    Don't use deprecated code in demo

--- Polyhedron/demo/Polyhedron/Plugins/PMP/Hole_filling_plugin.cpp
+++ Polyhedron/demo/Polyhedron/Plugins/PMP/Hole_filling_plugin.cpp
@@ -706,9 +706,10 @@ bool Polyhedron_demo_hole_filling_plugin::fill
              CGAL::parameters::use_delaunay_triangulation(use_DT));
   }
   else if(action_index == 1) {
-    CGAL::Polygon_mesh_processing::triangulate_and_refine_hole(poly,
-             it, std::back_inserter(patch), CGAL::Emptyset_iterator(),
-             CGAL::parameters::density_control_factor(alpha).
+    CGAL::Polygon_mesh_processing::triangulate_and_refine_hole(poly, it,
+             CGAL::parameters::
+             face_output_iterator(std::back_inserter(patch)).
+             density_control_factor(alpha).
              use_delaunay_triangulation(use_DT));
   }
   else {
@@ -716,9 +717,9 @@ bool Polyhedron_demo_hole_filling_plugin::fill
 
     bool success;
     if(weight_index == 0) {
-      success = std::get<0>(CGAL::Polygon_mesh_processing::triangulate_refine_and_fair_hole(poly,
-              it, std::back_inserter(patch), CGAL::Emptyset_iterator(),
+      success = std::get<0>(CGAL::Polygon_mesh_processing::triangulate_refine_and_fair_hole(poly, it,
               CGAL::parameters::
+              face_output_iterator(std::back_inserter(patch)).
               weight_calculator(CGAL::Weights::Uniform_weight<Face_graph>()).
               density_control_factor(alpha).
               fairing_continuity(continuity).
@@ -726,9 +727,9 @@ bool Polyhedron_demo_hole_filling_plugin::fill
     }
     else {
       auto pmap = get_property_map(CGAL::vertex_point, poly);
-      success = std::get<0>(CGAL::Polygon_mesh_processing::triangulate_refine_and_fair_hole(poly,
-              it, std::back_inserter(patch), CGAL::Emptyset_iterator(),
+      success = std::get<0>(CGAL::Polygon_mesh_processing::triangulate_refine_and_fair_hole(poly, it,
               CGAL::parameters::
+              face_output_iterator(std::back_inserter(patch)).
               weight_calculator(CGAL::Weights::Secure_cotangent_weight_with_voronoi_area<Face_graph, decltype(pmap)>(poly, pmap)).
               density_control_factor(alpha).
               fairing_continuity(continuity).
