aa7ef5c16de06df942a4cf87cdf0720530ff1eab
Laurent Rineau <laurent.rineau@cgal.org>
2022-12-15 15:38:38 +0100

    Better testsuite
        ctest --test-dir build/RelWithDebInfo -L CDT_3 -j4

--- Triangulation_3/test/Triangulation_3/cdt_test_insert_constrained_edge_from_EDG_file.cpp
+++ Triangulation_3/test/Triangulation_3/cdt_test_insert_constrained_edge_from_EDG_file.cpp
@@ -52,17 +52,21 @@ int main()
     int li, lj;
     assert(!cdt.is_edge(vertices[0], vertices[1], ch, li, lj));
     cdt.insert_constrained_edge(vertices[0], vertices[1]);
-    // cdt.insert_constrained_edge(vertices[5], vertices[1]);
+    cdt.insert_constrained_edge(vertices[5], vertices[1]);
     cdt.insert_constrained_edge(vertices[5], vertices[11]);
     return cdt.is_conforming() ? 0 : 1;
   };
-  CGAL_USE(test1);
 
-  auto test2 = []() {
-    //    CGAL::Conforming_Delaunay_triangulation_3<Delaunay> cdt;
+  enum Dim { DIM_2D, DIM_3D };
+
+  auto test2 = [](std::string filename, Dim dim = DIM_3D) {
+#ifndef CGAL_TEST_CDT_3_USE_CDT
+    CGAL::Conforming_Delaunay_triangulation_3<Delaunay> cdt;
+#else
     CGAL::Constrained_Delaunay_triangulation_3<Delaunay> cdt;
+#endif
 
-    std::ifstream input("clusters2.edg");
+    std::ifstream input(filename);
     if(!input) return 1;
     int n;
     input >> n;
@@ -70,38 +74,43 @@ int main()
     while(n-- > 0) {
       double x, y;
       input >> x >> y;
-      auto v1 = cdt.insert({x, y, 0});
+      auto v1 = cdt.insert({x, y, 0}), v3 = v1;
       CGAL_assertion(cdt.is_conforming());
-      auto v3 = cdt.insert({x, y, 1});
-      CGAL_assertion(cdt.is_conforming());
+      if(dim == DIM_3D) {
+        v3 = cdt.insert({x, y, 1});
+        CGAL_assertion(cdt.is_conforming());
+      }
       input >> x >> y;
-      auto v2 = cdt.insert({x, y, 0});
+      auto v2 = cdt.insert({x, y, 0}), v4 = v1;
       CGAL_assertion(cdt.is_conforming());
-      auto v4 = cdt.insert({x, y, 1});
-      CGAL_assertion(cdt.is_conforming());
+      if(dim == DIM_3D) {
+        v4 = cdt.insert({x, y, 1});
+        CGAL_assertion(cdt.is_conforming());
+      }
       cdt.insert_constrained_edge(v1, v2);
       CGAL_assertion(cdt.is_conforming());
-      cdt.insert_constrained_edge(v3, v4);
-      CGAL_assertion(cdt.is_conforming());
+      if(dim == DIM_3D) {
+        cdt.insert_constrained_edge(v3, v4);
+        CGAL_assertion(cdt.is_conforming());
+      }
     }
     cdt.insert({11, 28, 0});
     CGAL_assertion(cdt.is_conforming());
     cdt.insert({11, 33, 0});
     CGAL_assertion(cdt.is_conforming());
 
-    cdt.insert_constrained_polygon(
-        std::array{Point_3{1000, 0, 0}, Point_3{1000, 100, 0},
-                   Point_3{1100, 100, 0}, Point_3{1010, 90, 0}});
-
-    // assert(cdt.is_edge(vertices[0], vertices[1], ch, li, lj));
     for (auto v : cdt.finite_vertex_handles()) {
       std::cout << "Point ( " << v->point() << " )\n";
       std::cout << "  on " << v->nb_of_incident_constraints
                 << " constraint(s): " << v->c_id << "\n";
     }
-    CGAL::draw(static_cast<Delaunay&>(cdt), "CDT_3", true);
     return cdt.is_conforming() ? 0 : 1;
   };
 
-  return test1() + test2();
+  return test1() + test2("clusters.edg") + test2("clusters2.edg")
+  //  // For the moment, Triangulation_segment_traverser_3 does not work in 1D:
+  //  //     Expr: _tr->dimension() >= 2
+  //  //     File: .../CGAL/Triangulation_3/internal/Triangulation_segment_traverser_3_impl.h
+  //  + test2("clusters.edg", DIM_2D) + test2("clusters2.edg", DIM_2D)
+   ;
 }
--- Triangulation_3/test/Triangulation_3/cdt_test_insert_constrained_edge_from_OFF_file.cpp
+++ Triangulation_3/test/Triangulation_3/cdt_test_insert_constrained_edge_from_OFF_file.cpp
@@ -3,6 +3,7 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_3.h>
 #include <CGAL/Random.h>
+#include <CGAL/Conforming_Delaunay_triangulation_3.h>
 #include <CGAL/Constrained_Delaunay_triangulation_3.h>
 
 #include <CGAL/Surface_mesh.h>
@@ -39,27 +40,27 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  CGAL::Constrained_Delaunay_triangulation_3<Delaunay> cdt;
 
-  auto pmap = get(CGAL::vertex_point, mesh);
-  for(auto face_descriptor: faces(mesh)) {
-    std::vector<Point_3> polygon;
-    const auto he = halfedge(face_descriptor, mesh);
-    for(auto vertex_it: CGAL::vertices_around_face(he, mesh)) {
-      polygon.push_back(get(pmap, vertex_it));
-    }
-    std::cerr << "NEW POLYGON\n";
-    cdt.insert_constrained_polygon(polygon);
-    std::ofstream dump("dump.binary.cgal");
-    CGAL::Mesh_3::save_binary_file(dump, cdt);
+#ifndef CGAL_TEST_CDT_3_USE_CDT
+  CGAL::Conforming_Delaunay_triangulation_3<Delaunay> cdt_edge;
+#else
+  CGAL::Constrained_Delaunay_triangulation_3<Delaunay> cdt_edge;
+#endif
+  auto point_map = get(CGAL::vertex_point, mesh);
+  auto dt_vertex_handle_map =
+      get(CGAL::dynamic_vertex_property_t<Delaunay::Vertex_handle>(), mesh);
+  for(auto vertex_descriptor: vertices(mesh)) {
+    auto vertex_handle = cdt_edge.insert(get(point_map, vertex_descriptor));
+    put(dt_vertex_handle_map, vertex_descriptor, vertex_handle);
   }
-  {
-    std::ofstream missing_faces("missing_faces.polylines.txt");
-    cdt.write_missing_subfaces_file(missing_faces);
+
+  for(auto edge_descriptor: edges(mesh)) {
+    auto s = source(edge_descriptor, mesh);
+    auto t = target(edge_descriptor, mesh);
+    cdt_edge.insert_constrained_edge(get(dt_vertex_handle_map, s),
+                                     get(dt_vertex_handle_map, t));
   }
-  {
-    std::ofstream missing_edges("missing_segments.polylines.txt");
-    cdt.write_missing_segments_file(missing_edges);
-  }
-  assert(cdt.is_conforming());
+  assert(cdt_edge.is_conforming());
+
+  return EXIT_SUCCESS;
 }
