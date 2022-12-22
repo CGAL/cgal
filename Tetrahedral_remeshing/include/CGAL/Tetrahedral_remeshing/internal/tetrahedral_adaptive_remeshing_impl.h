b001146a9456283e1c0037cb80bd041b787b03cb
Jane Tournois <janetournois@users.noreply.github.com>
2022-12-22 17:52:20 +0100

    use C3T3 cells_in_complex() API

--- Tetrahedral_remeshing/include/CGAL/Tetrahedral_remeshing/internal/tetrahedral_adaptive_remeshing_impl.h
+++ Tetrahedral_remeshing/include/CGAL/Tetrahedral_remeshing/internal/tetrahedral_adaptive_remeshing_impl.h
@@ -297,7 +297,20 @@ public:
 #ifdef CGAL_DUMP_REMESHING_STEPS
     CGAL::Tetrahedral_remeshing::debug::dump_c3t3(m_c3t3, "99-postprocess");
 #endif
+#ifdef CGAL_TETRAHEDRAL_REMESHING_VERBOSE
+    mindh = 180.;
+    for (Cell_handle cit : tr().finite_cell_handles())
+    {
+      if (m_c3t3.is_in_complex(cit))
+      {
+        const double dh = min_dihedral_angle(tr(), cit);
+        mindh = (std::min)(dh, mindh);
+      }
+    }
+    std::cout << "Peeling done (removed " << nb_peeled << " slivers, "
+      << "min dihedral angle = " << mindh << ")." << std::endl;
 
+#endif
     return nb_peeled;
   }
 
