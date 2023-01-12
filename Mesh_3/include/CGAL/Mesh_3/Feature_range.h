f1f480d95cf37496e4c3f93da6e94b79c1361248
Jane Tournois <janetournois@users.noreply.github.com>
2023-01-12 14:48:38 +0100

    add feature_range() as input for detect_features

---
+++ Mesh_3/include/CGAL/Mesh_3/Feature_range.h
@@ -0,0 +1,74 @@
+// Copyright (c) 2023 GeometryFactory (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
+// Author(s)     : Jane Tournois
+//
+//******************************************************************************
+//
+//******************************************************************************
+
+#ifndef CGAL_MESH_3_FEATURE_RANGE_H
+#define CGAL_MESH_3_FEATURE_RANGE_H
+
+#include <CGAL/license/Mesh_3.h>
+
+#include <CGAL/Mesh_3/polylines_to_protect.h>
+
+#include <vector>
+
+namespace CGAL
+{
+namespace Mesh_3
+{
+/*!
+* \ingroup PkgMesh3FeatureDetection
+*
+* Functor for feature addition
+*/
+template<typename PolylineRange>
+struct Feature_range
+{
+private:
+  PolylineRange polylines_;
+
+public:
+  Feature_range(const PolylineRange polylines)
+    : polylines_(polylines)
+  {}
+
+public:
+  /*!
+  *
+  * \tparam Mesh_domain class model of `MeshDomainWithFeatures_3`
+  * \param domain the mesh domain to be enriched with polyline features
+  */
+  template<typename Img, typename Mesh_domain>
+  void operator()(const Img&, Mesh_domain& domain)
+  {
+    using Point = typename Mesh_domain::Point_3;
+
+    std::vector<std::vector<Point>> polyline_graph;
+    CGAL::polylines_to_protect(polyline_graph, std::begin(polylines_), std::end(polylines_));
+
+    domain.add_features(polyline_graph.begin(), polyline_graph.end());
+  }
+};
+
+template<typename PolylineRange>
+auto feature_range(const PolylineRange range)
+{
+  return Feature_range<PolylineRange>(range);
+}
+
+}//end namespace Mesh_3
+}//end namespace CGAL
+
+
+#endif //CGAL_MESH_3_FEATURE_RANGE_H
