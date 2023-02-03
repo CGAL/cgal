0eb6f8062aec4b47f92211b5e1762dae9a19167d
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2023-02-03 16:46:44 +0100

    Minor existing SLS doc fix

--- Straight_skeleton_2/doc/Straight_skeleton_2/Concepts/PolygonOffsetBuilderTraits_2.h
+++ Straight_skeleton_2/doc/Straight_skeleton_2/Concepts/PolygonOffsetBuilderTraits_2.h
@@ -26,25 +26,25 @@ is the Euclidean distance at which the <I>offset lines</I> intersect in a single
 The source of such offset lines is given by the three <I>oriented</I> lines defined by the edge-triple `et`.
 
 \pre `et` must be an edge-triple corresponding to an event that actually exist (that is,
-there must exist an offset distance `t > 0` at which the offset lines do intersect at a single point.
+there must exist an offset distance `t > 0` at which the offset lines do intersect at a single point).
 */
 typedef unspecified_type Compare_offset_against_event_time_2;
 
 /*!
 A construction object type.
 
-Must provide `boost::optional<Point_2> operator()(const FT& t, const Segment_2& e0, Segment_2_with_ID const& e1, const Trisegment_2_ptr& et) const`,
+Must provide `boost::optional<Point_2> operator()(const FT& t, const Segment_2& e0, const Segment_2& e1, const Trisegment_2_ptr& et) const`,
 which constructs the point of intersection of the lines obtained by offsetting
 the oriented lines given by `e0` and `e0` an Euclidean distance `t`.
 
-If `e0` and `e1` are collinear, if `seed` is not specified (`nullptr`), then the midpoint should be returned,
+If `e0` and `e1` are collinear, if `et` is not specified (`nullptr`), then the midpoint should be returned,
 otherwise, the event point of `et` should be returned.
 
 If the point cannot be computed, not even approximately (because of overflow for instance),
 an empty optional must be returned.
 
 \pre `et` must be an edge-triple corresponding to an event that actually exist (that is,
-there must exist an offset distance `t > 0` at which the offset lines do intersect at a single point.
+there must exist an offset distance `t > 0` at which the offset lines do intersect at a single point).
 */
 typedef unspecified_type Construct_offset_point_2;
 
