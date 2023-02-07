789f2c3b6053e7a3bf607e4c3c764bbcdef93e5a
Andreas Fabri <andreas.fabri@geometryfactory.com>
2023-02-07 06:58:56 +0000

    Polygon is already defined in a windows.h

--- Polygon/test/Polygon/issue7228.cpp
+++ Polygon/test/Polygon/issue7228.cpp
@@ -7,13 +7,13 @@
 
 typedef CGAL::Simple_cartesian<double> K;
 typedef K::Point_2 Point;
-typedef CGAL::Polygon_2<K> Polygon;
+typedef CGAL::Polygon_2<K> Polygon_2;
 typedef Polygon::Vertex_circulator Vertex_circulator;
 
 int main()
 {
   std::array<Point,4> points =  { Point(0,0), Point(1,0), Point(1,1), Point(0,1) };
-  Polygon poly(points.begin(), points.end());
+  Polygon_2 poly(points.begin(), points.end());
 
   Vertex_circulator vc = poly.vertices_circulator();
 
