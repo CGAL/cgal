db9b5898622523b4e099f47cbc60d389c74891a7
Sven Oesau <sven.oesau@geometryfactory.com>
2023-03-08 17:08:21 +0100

    fixed path

--- Kinetic_shape_reconstruction/examples/Kinetic_shape_reconstruction/kinetic_precomputed_shapes.cpp
+++ Kinetic_shape_reconstruction/examples/Kinetic_shape_reconstruction/kinetic_precomputed_shapes.cpp
@@ -27,7 +27,7 @@ int main(const int argc, const char** argv) {
 
   // Reading polygons from file
   const auto kernel_name = boost::typeindex::type_id<Kernel>().pretty_name();
-  std::string input_filename = (argc > 1 ? argv[1] : "../data/test-4-rnd-polygons-4-6.off");
+  std::string input_filename = (argc > 1 ? argv[1] : "data/test-4-rnd-polygons-4-6.off");
   std::ifstream input_file_off(input_filename);
   std::ifstream input_file_ply(input_filename);
 
