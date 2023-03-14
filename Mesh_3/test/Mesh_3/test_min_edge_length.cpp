9f7eaca7af750a3f6a7485423b884c8d3445bf8e
Jane Tournois <janetournois@users.noreply.github.com>
2023-03-14 16:39:08 +0100

    improve test

--- Mesh_3/test/Mesh_3/test_min_edge_length.cpp
+++ Mesh_3/test/Mesh_3/test_min_edge_length.cpp
@@ -8,21 +8,26 @@
 #include <CGAL/Polyhedral_mesh_domain_with_features_3.h>
 #include <CGAL/make_mesh_3.h>
 
-template <typename K, typename Polyhedron>
-struct Tester {
+#include <CGAL/Mesh_3/experimental/Sizing_field_with_aabb_tree.h>
+
+
+template <typename K,
+          typename Polyhedron,
+          typename Concurrency_tag>
+struct Tester
+{
   // Domain
   typedef CGAL::Polyhedral_mesh_domain_with_features_3<K, Polyhedron> Mesh_domain;
 
-
   // Triangulation
-  typedef typename CGAL::Mesh_triangulation_3<Mesh_domain>::type Tr;
+  typedef typename CGAL::Mesh_triangulation_3<Mesh_domain, K, Concurrency_tag>::type Tr;
 
   typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
 
   // Criteria
   typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
 
-  int operator()(const std::string fname, const std::string out_fname)
+  void operator()(const std::string fname, const std::string out_fname)
   {
     std::ifstream input(fname);
     using namespace CGAL::parameters;
@@ -31,12 +36,12 @@ struct Tester {
     input >> polyhedron;
     if (input.fail()) {
       std::cerr << "Error: Cannot read file " << fname << std::endl;
-      return EXIT_FAILURE;
+      return;
     }
 
     if (!CGAL::is_triangle_mesh(polyhedron)) {
       std::cerr << "Input geometry is not triangulated." << std::endl;
-      return EXIT_FAILURE;
+      return;
     }
 
     // Create domain
@@ -44,8 +49,10 @@ struct Tester {
 
     domain.detect_features(40);
 
+    Sizing_field_with_aabb_tree<K, Mesh_domain> sf(0.074, domain.aabb_tree(), domain);
+
     // Mesh criteria
-    Mesh_criteria criteria(edge_size = 0.074,
+    Mesh_criteria criteria(edge_size = sf,
                            edge_min_size = 0.1,
                            facet_distance = 0.0074,
                            facet_angle = 25,
@@ -58,8 +65,6 @@ struct Tester {
 
     // Output
     CGAL::dump_c3t3(c3t3, out_fname);
-
-    return EXIT_SUCCESS;
   }
 };
 
@@ -70,5 +75,9 @@ int main(int argc, char* argv[])
   typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
   typedef CGAL::Surface_mesh<K::Point_3>                      Surface_mesh;
 
-  return Tester<K, Surface_mesh>()(fname, "out-dragknob");
+  Tester<K, Surface_mesh, CGAL::Sequential_tag>()(fname, "out-dragknob-sequential");
+
+  Tester<K, Surface_mesh, CGAL::Parallel_tag>()(fname, "out-dragknob-parallel");
+
+  return EXIT_SUCCESS;
 }
