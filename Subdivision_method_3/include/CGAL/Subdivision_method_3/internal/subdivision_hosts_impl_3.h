56937801d52f8a235e6007c938948cde49caaf34
Andreas Fabri <andreas.fabri@geometryfactory.com>
2023-03-17 14:46:49 +0100

    Just copy the input points

--- Subdivision_method_3/include/CGAL/Subdivision_method_3/internal/subdivision_hosts_impl_3.h
+++ Subdivision_method_3/include/CGAL/Subdivision_method_3/internal/subdivision_hosts_impl_3.h
@@ -81,7 +81,7 @@ void PQQ_1step(Poly& p, VertexPointMap vpm, Mask mask) {
   int i=0;
   std::unordered_map<vertex_descriptor,int> v_index;
   for(vertex_descriptor vh : p_vertices){
-      mask.vertex_node(vh,vertex_point_buffer[i]);
+    vertex_point_buffer[i] = get(vpm, vh);
     v_index[vh]= i++;
   }
 
