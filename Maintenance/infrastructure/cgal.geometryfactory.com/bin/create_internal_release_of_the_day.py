33ae0b5fd4229dca120202b5886c4b8b3997cff7
Laurent Rineau <laurent.rineau@cgal.org>
2023-04-06 12:13:51 +0200

    Fix pylint warnings

--- Maintenance/infrastructure/cgal.geometryfactory.com/bin/create_internal_release_of_the_day.py
+++ Maintenance/infrastructure/cgal.geometryfactory.com/bin/create_internal_release_of_the_day.py
@@ -1,16 +1,13 @@
 #! /bin/python3
 
-'''This script is called by a cron job every day.
+"""This script is called by a cron job every day.
    It creates and publish a release tarball.
-'''
+"""
 
-import sys
 import os
 import datetime
 import locale
 import argparse
-from pathlib import Path
-sys.path.append(Path(__file__))
 from cgal_release import release, integration, master, beta_release_from_master
 
 # Define a dictionary that maps day of the week to an action
@@ -21,30 +18,35 @@ actions = {
     "Thursday": integration,
     "Friday": release("5.5"),
     "Saturday": release("5.4"),
-    "Sunday": beta_release_from_master(1)
+    "Sunday": beta_release_from_master(1),
 }
 
+
 def main():
+    """Entry point of the script."""
     # Get the current day of the week, or get it from the command line
-    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
     day_help = f"Day of the week (default: {datetime.datetime.now().strftime('%A')})"
-    day_help += " possible values: ({})".format(', '.join(actions.keys()))
+    day_help += f" possible values: ({', '.join(actions.keys())})"
     parser = argparse.ArgumentParser()
-    parser.add_argument('day', help = day_help, nargs='?', default = datetime.datetime.now().strftime("%A"))
-    parser.add_argument('-n', '--dry-run', action='store_true')
+    parser.add_argument(
+        "day", help=day_help, nargs="?", default=datetime.datetime.now().strftime("%A")
+    )
+    parser.add_argument("-n", "--dry-run", action="store_true")
     args = parser.parse_args()
 
     # Look up the action for the current day of the week in the dictionary
     create_release = actions[args.day]
 
-    if(args.dry_run):
+    if args.dry_run:
         print(create_release)
         return
-    else:
-        # Then create the release tarball
-        if os.system(create_release.command()) != 0 :
-            raise RuntimeError(f"ERROR while creating release tarball")
 
-if __name__ == '__main__':
+    # Then create the release tarball
+    if os.system(create_release.command()) != 0:
+        raise RuntimeError("ERROR while creating release tarball")
+
+
+if __name__ == "__main__":
     main()
