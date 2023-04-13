7889f8aa6b943d2dc0d9e25651608080dd183a3a
Sébastien Loriot <sebastien.loriot@cgal.org>
2023-04-13 09:02:59 +0200

    remove commented line

--- Installation/lib/cmake/CGAL/CGALConfig.cmake
+++ Installation/lib/cmake/CGAL/CGALConfig.cmake
@@ -204,7 +204,6 @@ endif()
 #warning: the order in this list has to match the enum in Exact_type_selector
 set(CGAL_CMAKE_EXACT_NT_BACKEND_OPTIONS GMP_BACKEND GMPXX_BACKEND BOOST_GMP_BACKEND BOOST_BACKEND LEDA_BACKEND MP_FLOAT_BACKEND Default)
 set(CGAL_CMAKE_EXACT_NT_BACKEND "Default" CACHE STRING "Setting for advanced users that what to change the default number types used in filtered kernels. Some options might not be working depending on how you configured your build.")
-#~ set_property(CACHE CGAL_CMAKE_EXACT_NT_BACKEND PROPERTY STRINGS GMP_BACKEND GMPXX_BACKEND BOOST_GMP_BACKEND BOOST_BACKEND LEDA_BACKEND MP_FLOAT_BACKEND)
 set_property(CACHE CGAL_CMAKE_EXACT_NT_BACKEND PROPERTY STRINGS ${CGAL_CMAKE_EXACT_NT_BACKEND_OPTIONS})
 
 if ( NOT "${CGAL_CMAKE_EXACT_NT_BACKEND}" STREQUAL "Default" )
