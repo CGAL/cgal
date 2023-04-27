48cfa7ad61f1336550b6fd65e2f3cd05d104849c
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-04-27 18:30:41 +0200

    add default value in the doc

--- Basic_viewer/doc/Basic_viewer/CGAL/Drawing_functor.h
+++ Basic_viewer/doc/Basic_viewer/CGAL/Drawing_functor.h
@@ -20,33 +20,43 @@ template <typename DS,
 struct Drawing_functor
 {
   /// std::function that returns true if the given vertex must be drawn, false otherwise.
+  /// Returns true by default.
   std::function<bool(const DS &, Vertex_descriptor)> draw_vertex;
 
   /// std::function that returns true if the given edge must be drawn, false otherwise.
+  /// Returns true by default.
   std::function<bool(const DS &, Edge_descriptor)>   draw_edge;
 
   /// std::function that returns true if the given face must be drawn, false otherwise.
+  /// Returns true by default.
   std::function<bool(const DS &, Face_descriptor)>   draw_face;
 
   /// std::function that returns true if the given vertex is colored, false otherwise.
+  /// Returns false by default.
   std::function<bool(const DS &, Vertex_descriptor)> colored_vertex;
 
   /// std::function that returns true if the given edge is colored, false otherwise.
+  /// Returns false by default.
   std::function<bool(const DS &, Edge_descriptor)>   colored_edge;
 
   /// std::function that returns true if the given face is colored, false otherwise.
+  /// Returns false by default.
   std::function<bool(const DS &, Face_descriptor)>   colored_face;
 
   /// std::function that returns true if the given face is drawn in wireframe, false otherwise.
+  /// Returns false by default.
   std::function<bool(const DS &, Face_descriptor)> face_wireframe;
 
   /// std::function that returns the color of the given vertex.
+  /// nullptr by default.
   std::function<CGAL::IO::Color(const DS &, Vertex_descriptor)> vertex_color;
 
   /// std::function that returns the color of the given edge.
+  /// nullptr by default.
   std::function<CGAL::IO::Color(const DS &, Edge_descriptor)>   edge_color;
 
   /// std::function that returns the color of the given face.
+  /// nullptr by default.
   std::function<CGAL::IO::Color(const DS &, Face_descriptor)>   face_color;
 
   /// Disable the drawing of vertices.
@@ -93,16 +103,20 @@ struct Drawing_functor_with_volume :
     public Drawing_functor<DS, Vertex_descriptor, Edge_descriptor, Face_descriptor>
 {
   /// std::function that returns true if the given volume must be drawn, false otherwise.
-  std::function<bool(const DS &, volume_descriptor)> draw_volume;
+  /// Returns true by default.
+  std::function<bool(const DS &, Volume_descriptor)> draw_volume;
 
   /// std::function that returns the color of the given volume.
-  std::function<bool(const DS &, volume_descriptor)> colored_volume;
+  /// Returns false by default.
+  std::function<bool(const DS &, Volume_descriptor)> colored_volume;
 
   /// std::function that returns true if the given volume is drawn in wireframe, false otherwise.
-  std::function<bool(const DS &, volume_descriptor)> volume_wireframe;
+  /// Returns false by default.
+  std::function<bool(const DS &, Volume_descriptor)> volume_wireframe;
 
   /// std::function that returns the color of the given volume.
-  std::function<CGAL::IO::Color(const DS &, volume_descriptor)> volume_color;
+  /// nullptr by default.
+  std::function<CGAL::IO::Color(const DS &, Volume_descriptor)> volume_color;
 
   /// Disable the drawing of volumes.
   void disable_volumes();
