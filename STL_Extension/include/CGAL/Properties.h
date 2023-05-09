e3201869d1af4d578a697c505d6681bd31cdc896
JacksonCampolattaro <jackcamp@vt.edu>
2023-05-09 13:07:49 +0200

    Add support & tests for the obviously necessary features from Surface_mesh's property system

--- STL_Extension/include/CGAL/Properties.h
+++ STL_Extension/include/CGAL/Properties.h
@@ -9,18 +9,18 @@
 
 namespace CGAL::Properties {
 
-template <char... chars>
-using String_literal_type = std::integer_sequence<char, chars...>;
-
-template <typename T, T... chars>
-constexpr String_literal_type<chars...> operator ""_param() { return {}; }
-
 class Property_array_base {
 public:
 
   virtual ~Property_array_base() = default;
 
-  // todo: Declare virtual functions here for things which need to be done within the Property container
+  // todo: Declare virtual functions here, for things which need to be done within the Property container
+
+  virtual void reserve(std::size_t n) = 0;
+
+  virtual void swap(std::size_t a, std::size_t b) = 0;
+
+  virtual void reset(std::size_t i) = 0;
 
 };
 
@@ -34,11 +34,47 @@ class Property_array : public Property_array_base {
 
   std::vector<T> m_data;
   const std::vector<bool>& m_active_indices;
+  T m_default_value;
 
 public:
 
   Property_array(const std::vector<bool>& active_indices, const T& default_value) :
-    m_active_indices(active_indices), m_data() {}
+    m_data(), m_active_indices(active_indices), m_default_value(default_value) {
+
+    m_data.reserve(active_indices.capacity());
+    m_data.resize(active_indices.size(), m_default_value);
+  }
+
+  virtual void reserve(std::size_t n) override {
+    CGAL_precondition(m_active_indices.size() == n);
+    m_data.resize(n, m_default_value);
+  };
+
+  virtual void swap(std::size_t a, std::size_t b) override {
+    CGAL_precondition(a < m_data.size() && b < m_data.size());
+    std::iter_swap(m_data.begin() + a, m_data.begin() + b);
+  };
+
+  virtual void reset(std::size_t i) override {
+    CGAL_precondition(i < m_data.size());
+    m_data[i] = m_default_value;
+  };
+
+  std::size_t capacity() const { return m_data.size(); }
+
+public:
+
+  const T& operator[](std::size_t i) const {
+    CGAL_precondition(i < m_data.size());
+    return m_data[i];
+  }
+
+  T& operator[](std::size_t i) {
+    CGAL_precondition(i < m_data.size());
+    return m_data[i];
+  }
+
+public:
 
   bool operator==(const Property_array<T>& other) const {
     return &other == this;
@@ -50,55 +86,92 @@ public:
 
 class Property_container {
 
-
   std::map<std::string, std::shared_ptr<Property_array_base>> m_property_arrays;
   std::vector<bool> m_active_indices;
 
 public:
 
-  /*
   template <typename T>
-  Property_array<T>& add(const std::string& name, const T default_value = T()) {
-    return dynamic_cast<Property_array<T>&>(*(*m_property_arrays.emplace(name, std::make_shared<Property_array<T>>(
-      m_active_indices,
-      default_value)
-    ).first).second);
+  std::pair<std::reference_wrapper<Property_array<T>>, bool>
+  add(const std::string& name, const T default_value = T()) {
+    auto [it, created] = m_property_arrays.emplace(
+      name,
+      std::make_shared<Property_array<T>>(
+        m_active_indices,
+        default_value
+      )
+    );
+    auto [key, array] = *it;
+    auto& typed_array = dynamic_cast<Property_array<T>&>(*array);
+    return {{typed_array}, !created};
+  }
+
+  template <typename T>
+  const Property_array<T>& get(const std::string& name) const {
+    CGAL_precondition(m_property_arrays.count(name) != 0);
+    return dynamic_cast<const Property_array<T>&>(*m_property_arrays.at(name));
   }
 
   template <typename T>
   Property_array<T>& get(const std::string& name) {
     CGAL_precondition(m_property_arrays.count(name) != 0);
-    return dynamic_cast<Property_array<T>&>(*m_property_arrays[name]);
-  }
-  */
-
-  template <typename T, typename Name>
-  Property_array<T>& get() {
-
-    static Property_array<T> array{m_active_indices, T{}};
-    return array;
-
+    return dynamic_cast<Property_array<T>&>(*m_property_arrays.at(name));
   }
 
-  template <typename T, typename Name>
-  std::pair<std::reference_wrapper<Property_array<T>>, std::unique_lock<std::mutex>> threadsafe_get() {
-    static std::mutex m{};
-    return {std::reference_wrapper<Property_array<T>>{get<T, Name>()}, std::unique_lock<std::mutex>{m}};
+  /*!
+   * Removes a property array from the container
+   *
+   * @param name
+   * @return True if a container with this name existed, false otherwise
+   */
+  bool remove(const std::string& name) { return m_property_arrays.erase(name) == 1; }
+
+  std::size_t n_properties() { return m_property_arrays.size(); }
+
+public:
+
+  void reserve(std::size_t n) {
+    m_active_indices.resize(n);
+    for (auto [name, array]: m_property_arrays)
+      array->reserve(n);
   }
 
+  std::size_t emplace() {
+    // todo: should emplacing an element also reset it to default values?
 
-private:
+    // If there are empty slots, return the index of one of them and mark it as full
+    auto first_unused = std::find_if(m_active_indices.begin(), m_active_indices.end(), [](bool used) { return !used; });
+    if (first_unused != m_active_indices.end()) {
+      *first_unused = true;
+      return std::distance(m_active_indices.begin(), first_unused);
+    }
 
-  static constexpr std::size_t next_type_index() {
-    static std::atomic<std::size_t> value{0};
-    return value++;
+    // Otherwise, expand the storage and return the last element
+    reserve(size() + 1);
+    m_active_indices.back() = true;
+    return size() - 1;
+
   }
 
-  template <typename T, typename Name>
-  static constexpr std::size_t type_index() {
-    static std::size_t value{next_type_index()};
-    return value;
+  void swap(std::size_t a, std::size_t b) {
+    for (auto [name, array]: m_property_arrays)
+      array->swap(a, b);
   }
+
+  void reset(std::size_t i) {
+    for (auto [name, array]: m_property_arrays)
+      array->reset(i);
+  }
+
+  void erase(std::size_t i) {
+    m_active_indices[i] = false;
+    for (auto [name, array]: m_property_arrays)
+      array->reset(i);
+  }
+
+  std::size_t size() const { return std::count(m_active_indices.begin(), m_active_indices.end(), true); }
+
+  std::size_t capacity() const { return m_active_indices.size(); }
 
 };
 
--- STL_Extension/test/STL_Extension/test_Properties.cpp
+++ STL_Extension/test/STL_Extension/test_Properties.cpp
@@ -3,22 +3,105 @@
 
 using namespace CGAL::Properties;
 
-int main() {
+void test_property_creation() {
 
   Property_container properties;
 
-  auto &a = properties.get<int, decltype("A"_param)>();
-  auto &b = properties.get<int, decltype("B"_param)>();
-  auto &c = properties.get<float, decltype("C"_param)>();
+  // Should return an integer array which didn't previously exist
+  auto [integers, preexisting] = properties.add("integer", 5);
+  static_assert(std::is_same_v<decltype(integers), std::reference_wrapper<Property_array<int>>>);
+  assert(!preexisting);
+  assert(properties.n_properties() == 1);
 
-  assert((std::is_same<decltype("A"_param), decltype("A"_param)>::value));
+  auto [floats, _] = properties.add<float>("float");
+  static_assert(std::is_same_v<decltype(floats), std::reference_wrapper<Property_array<float>>>);
+  assert(properties.n_properties() == 2);
 
-  assert(a != b);
+  // get() should retreive the same arrays
+  assert(integers.get() == properties.get<int>("integer"));
+  assert(floats.get() == properties.get<float>("float"));
 
-  auto &a2 = properties.get<int, decltype("A"_param)>();
-  assert(a == a2);
+  // remove() should delete a property array & return if it existed
+  assert(!properties.remove("not-a-real-property"));
+  auto removed = properties.remove("integer");
+  assert(removed);
+  assert(properties.n_properties() == 1);
 
-  properties.threadsafe_get<float, decltype("C"_param)>();
+}
+
+void test_property_array() {
+
+  Property_container properties;
+
+  auto [integers, integers_existed] = properties.add("integers", 5);
+
+  // Reserve space for 100 elements
+  properties.reserve(100);
+  assert(properties.capacity() == 100);
+  assert(properties.size() == 0);
+
+  // Newly emplaced elements should go at the front
+  assert(properties.emplace() == 0);
+  assert(properties.emplace() == 1);
+  assert(properties.emplace() == 2);
+  assert(properties.size() == 3);
+
+  // Make sure that the new elements are equal to the default value
+  assert(integers.get()[0] == 5);
+  assert(integers.get()[1] == 5);
+  assert(integers.get()[2] == 5);
+
+  // Add a new property
+  auto [floats, floats_existed] = properties.add("floats", 6.0f);
+
+  // The new property array should already be of the right size
+  assert(floats.get().capacity() == 100);
+  assert(properties.size() == 3);
+
+  // Pre-existing elements should contain the default value
+  assert(floats.get()[0] == 6.0f);
+  assert(floats.get()[1] == 6.0f);
+  assert(floats.get()[2] == 6.0f);
+
+  // Update values for a few elements
+  floats.get()[0] = 1.0f;
+  floats.get()[1] = 2.0f;
+  floats.get()[2] = 3.0f;
+  integers.get()[2] = -2;
+  assert(floats.get()[0] == 1.0f);
+  assert(floats.get()[1] == 2.0f);
+  assert(floats.get()[2] == 3.0f);
+  assert(integers.get()[2] == -2);
+
+  // Reset an element, and all of its properties should revert to the defaults
+  properties.reset(2);
+  assert(floats.get()[2] == 6.0f);
+  assert(integers.get()[2] == 5);
+
+  // Erase an element, and the size should be reduced
+  properties.erase(1);
+  assert(properties.size() == 2);
+  assert(properties.capacity() == 100);
+
+  // A newly emplaced element should take the empty slot
+  assert(properties.emplace() == 1);
+  assert(properties.size() == 3);
+  // todo: should the new element have default properties?
+  assert(properties.emplace() == 3);
+  assert(properties.size() == 4);
+
+  // Swapping a pair of elements swaps all of their properties
+  properties.swap(0, 3);
+  assert(integers.get()[0] == 5);
+  assert(floats.get()[0] == 6.0f);
+  assert(integers.get()[3] == 5);
+  assert(floats.get()[3] == 1.0f);
+}
+
+int main() {
+
+  test_property_creation();
+  test_property_array();
 
   return 0;
 }
