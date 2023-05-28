f247756bfbc21eaa5126453d85fe45c8a000c434
Efi Fogel <efifogel@gmail.com>
2023-05-28 17:26:04 +0300

    Enhanced test

--- Surface_sweep_2/test/Surface_sweep_2/Compare_curves.h
+++ Surface_sweep_2/test/Surface_sweep_2/Compare_curves.h
@@ -1,47 +1,44 @@
-#ifndef _COMPARE_CURVE_LIST_H
-#define _COMPARE_CURVE_LIST_H
+#ifndef _COMPARE_CURVES_H
+#define _COMPARE_CURVES_H
 
 #include <list>
 #include <algorithm>
 
-
-template <class Traits>
-class Equal_pred
-{
+template <typename Traits>
+class Equal_pred {
 public:
-  typedef typename Traits::Point_2                Point_2;
-  typedef typename Traits::X_monotone_curve_2     X_monotone_curve_2;
+  using Point_2 = typename Traits::Point_2;
+  using X_monotone_curve_2 = typename Traits::X_monotone_curve_2;
+
+  Equal_pred(const Traits& traits) : m_traits(traits) {}
 
   bool operator()(const Point_2& p1, const Point_2& p2)
-  {
-    return(Traits().equal_2_object()(p1, p2));
-  }
+  { return(m_traits.equal_2_object()(p1, p2)); }
 
   bool operator()(const X_monotone_curve_2& c1, const X_monotone_curve_2& c2)
-  {
-    return(Traits().equal_2_object()(c1, c2));
-  }
+  { return(m_traits.equal_2_object()(c1, c2)); }
+
+private:
+  const Traits& m_traits;
 };
 
 
-template <class List, class Traits>
-  bool compare_lists(const List& list1, const List& list2, Traits& /*tr*/)
-{
-  typedef typename List::const_iterator  Iter;
-  Iter begin1 = list1.begin();
-  Iter end1 = list1.end();
-
-  Iter begin2 = list2.begin();
-
-  if(! (list1.size() == list2.size()))
-  {
-    std::cout << "The lists are not of the same lengths ("
+template <typename List, typename Traits>
+bool compare_lists(const List& list1, const List& list2, Traits& traits) {
+  if(! (list1.size() == list2.size())) {
+    std::cerr << "Error: The lists are not of the same lengths ("
               << list1.size() << "," << list2.size() << ")\n";
     return false;
   }
 
-  Equal_pred<Traits> eq;
-  return std::equal(begin1, end1, begin2, eq);
+  Equal_pred<Traits> eq(traits);
+  auto rc = std::equal(list1.begin(), list1.end(), list2.begin(), eq);
+  if (! rc) {
+    std::cerr << "Error: The curves do not match\n";
+    return false;
+  }
+
+  return true;
 }
 
 
