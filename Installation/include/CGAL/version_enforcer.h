5a7f0252ac2b13481f6d6a0db9dc2150e3a463c5
Jane Tournois <janetournois@users.noreply.github.com>
2023-06-15 16:27:08 +0200

    fix error message

--- Installation/include/CGAL/version_enforcer.h
+++ Installation/include/CGAL/version_enforcer.h
@@ -17,7 +17,7 @@
 // All files including this header are meant to work with a given version of CGAL
 // When using forked headers, set the 4 following macros to the version of CGAL
 // you want to use.
-#define CGAL_AUTHORIZED_VERSION CGAL_VERSION_STR
+#define CGAL_AUTHORIZED_VERSION_STR CGAL_VERSION_STR
 #define CGAL_AUTHORIZED_VERSION_MAJOR CGAL_VERSION_MAJOR
 #define CGAL_AUTHORIZED_VERSION_MINOR CGAL_VERSION_MINOR
 #define CGAL_AUTHORIZED_VERSION_PATCH CGAL_VERSION_PATCH
@@ -25,17 +25,17 @@
 // Check that the version of CGAL used is the one expected
 #if (CGAL_VERSION_MAJOR != CGAL_AUTHORIZED_VERSION_MAJOR)
 #pragma message "You are using CGAL version: " CGAL_STR(CGAL_VERSION) "."
-#error This header is meant to be with used with CGAL 5.5 only.
+#error This header is meant to be with used with CGAL "CGAL_AUTHORIZED_VERSION_STR" only."
 #endif
 
 #if (CGAL_VERSION_MINOR != CGAL_AUTHORIZED_VERSION_MINOR)
 #pragma message "You are using CGAL version: " CGAL_STR(CGAL_VERSION) "."
-#error This header is meant to be with used with CGAL 5.5 only.
+#error This header is meant to be with used with CGAL "CGAL_AUTHORIZED_VERSION_STR" only.
 #endif
 
 #if (CGAL_VERSION_PATCH != CGAL_AUTHORIZED_VERSION_PATCH)
 #pragma message "You are using CGAL version: " CGAL_STR(CGAL_VERSION) "."
-#error This header is meant to be with used with CGAL 5.5 only.
+#error This header is meant to be with used with CGAL "CGAL_AUTHORIZED_VERSION_STR" only."
 #endif
 
 #endif // CGAL_VERSION_ENFORCER_H
