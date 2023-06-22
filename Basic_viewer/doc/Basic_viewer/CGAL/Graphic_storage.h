10a87486cf0e38832be8c4237b208112d742ff3c
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-06-22 16:08:35 +0200

    Basic viewer doc (cont)

--- Basic_viewer/doc/Basic_viewer/CGAL/Graphic_storage.h
+++ Basic_viewer/doc/Basic_viewer/CGAL/Graphic_storage.h
@@ -4,45 +4,45 @@ namespace CGAL {
 /*!
  \ingroup PkgBasicViewerClasses
 
-The class `Graphic_storage` store points, segments and triangles. Elements can be added, possibly with associated colors. Non triangular faces can be directly added, and are triangulated internally.
+The class `Graphic_storage` store points, segments, triangles, rays and lines. Elements can be added, possibly with associated colors. Non triangular faces can be directly added, and are triangulated internally.
 
 \tparam BufferType the type used for point coordinates: float by default.
 
-*/   
+*/
 template <typename BufferType=float>
 class Graphic_storage {
 public:
-  /// Add the given point in this storage.
+  /// add the given point in the storage.
   template <typename KPoint>
   void add_point(const KPoint &p);
 
-  /// Add the given colored point in this storage.
+  /// add the given colored point in the storage.
   template <typename KPoint>
   void add_point(const KPoint &p, const CGAL::IO::Color &acolor);
 
-  /// Add the given segment in this storage.
+  /// add the given segment in the storage.
   template <typename KPoint>
   void add_segment(const KPoint &p1, const KPoint &p2);
 
-  /// Add the given colored segment in this storage.
+  /// add the given colored segment in the storage.
   template <typename KPoint>
   void add_segment(const KPoint &p1, const KPoint &p2,
                    const CGAL::IO::Color &acolor);
 
-  /// Add the given ray in the storage: an half line starting from p and having v as direction.
+  /// add the given ray in the storage: an half line starting from p and having v as direction.
   template <typename KPoint, typename KVector>
   void add_ray(const KPoint &p, const KVector &v);
 
-  /// Add the given colored ray in the storage: an half line starting from p and having v as direction.
+  /// add the given colored ray in the storage: an half line starting from p and having v as direction.
   template <typename KPoint, typename KVector>
   void add_ray(const KPoint &p, const KVector &v,
                const CGAL::IO::Color &acolor);
 
-  /// Add the given line in the storage, defined by p and v as direction.
+  /// add the given line in the storage, defined by p and v as direction.
   template <typename KPoint, typename KVector>
   void add_line(const KPoint &p, const KVector &v);
 
-  /// Add the given colored line in the storage, defined by p and v as direction.
+  /// add the given colored line in the storage, defined by p and v as direction.
   template <typename KPoint, typename KVector>
   void add_line(const KPoint &p, const KVector &v,
                 const CGAL::IO::Color &acolor);
@@ -66,12 +66,6 @@ public:
   /// end the current face.
   void face_end();
 
-  /// @return `true` iff the storage has no element.
-  bool is_empty() const;
-
-  /// clear the storage, i.e. remove all points, segments, triangles and text.
-  void clear();
-
   /// add the given text at the given position in the storage.
   template <typename KPoint>
   void add_text(const KPoint &kp, const char *txt);
@@ -80,8 +74,11 @@ public:
   template <typename KPoint>
   void add_text(const KPoint &kp, const std::string &txt);
 
-  /// clear all the texts.
-  void m_texts_clear();
+  /// @return `true` iff the storage has no element.
+  bool is_empty() const;
+
+  /// clear the storage, i.e. remove all points, segments, triangles and text.
+  void clear();
 };
 
 } // namespace CGAL
