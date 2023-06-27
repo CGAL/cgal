c7eb4f9fdd4f9dcc199430685fb5b0c05e01396c
Andreas Fabri <andreas.fabri@geometryfactory.com>
2023-06-27 13:33:48 +0100

    Deal with prev() for graph_traits<T2>::iterator

--- Triangulation_2/include/CGAL/boost/graph/internal/graph_traits_2D_triangulation_helper.h
+++ Triangulation_2/include/CGAL/boost/graph/internal/graph_traits_2D_triangulation_helper.h
@@ -156,7 +156,7 @@ struct Dereference_to_handle_enforcer
       Dereference_to_handle_enforcer<Tr, Iterator, Handle>,
       Iterator /*base*/,
       Handle /*value*/,
-      boost::use_default,
+      typename std::iterator_traits<Iterator>::iterator_category,
       Handle /*reference*/
     >
 {
@@ -166,7 +166,8 @@ public:
 private:
   typedef Dereference_to_handle_enforcer<Tr, Iterator, Handle>                         Self;
   typedef Iterator                                                                     I;
-  typedef boost::iterator_adaptor<Self, I, value_type, boost::use_default, value_type> Base;
+  typedef typename std::iterator_traits<I>::iterator_category                          Category;
+  typedef boost::iterator_adaptor<Self, I, value_type, Category, value_type>           Base;
 
 public:
   Dereference_to_handle_enforcer() { }
