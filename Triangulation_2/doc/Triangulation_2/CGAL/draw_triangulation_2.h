367b2d3815ea2d8f74f2387e701f7720bd0de662
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-06-27 16:04:18 +0200

    Update doc

--- Triangulation_2/doc/Triangulation_2/CGAL/draw_triangulation_2.h
+++ Triangulation_2/doc/Triangulation_2/CGAL/draw_triangulation_2.h
@@ -3,41 +3,40 @@ namespace CGAL {
 /*!
 \ingroup PkgDrawTriangulation2
 
-opens a new window and draws a triangulation.  If the triangulation
-has constraints they are drawn.  If the face type has a member
-function `bool is_in_domain()` the faces inside and outside of the
-domain are drawn in different colors.
+opens a new window and draws a triangulation. Parameters of the drawing are taken from the optional drawing functor parameter.
 
 A call to this function is blocking, that is the program continues as soon as the user closes the window. This function requires `CGAL_Qt5`, and is only available if the macro `CGAL_USE_BASIC_VIEWER` is defined.
 Linking with the cmake target `CGAL::CGAL_Basic_viewer` will link with
 `CGAL_Qt5` and add the definition `CGAL_USE_BASIC_VIEWER`.
 
-\tparam T2 a triangulation class derived from `Triangulation_2`  or `Constrained_triangulation_2`
+\tparam T2 a triangulation class derived from `Triangulation_2`
 \param at2 the triangulation to draw.
+\param df the drawing functor.
 
 */
 template<class T2>
-void draw(const T2& at2);
+void draw(const T2& at2, const CGAL::Drawing_functor<T2,
+                  typename T2::Vertex_handle,
+                  typename T2::Finite_edges_iterator,
+                  typename T2::Finite_faces_iterator>& df=default);
 
-  /*!
+/*!
 \ingroup PkgDrawTriangulation2
 
-opens a new window and draws a constrained triangulation. If the triangulation
-has constraints they are drawn.  The faces inside and outside of the
-domain, based on the property map, are drawn in different colors.
+adds the vertices, edges and faces of at2 into the given graphic storage gs. Parameters of the cells are taken from the optional drawing functor parameter. Note that gs is not clear before to be filled (to enable to draw several data structures in a same basic viewer).
 
-A call to this function is blocking, that is the program continues as soon as the user closes the window. This function requires `CGAL_Qt5`, and is only available if the macro `CGAL_USE_BASIC_VIEWER` is defined.
-Linking with the cmake target `CGAL::CGAL_Basic_viewer` will link with `CGAL_Qt5` and add the definition `CGAL_USE_BASIC_VIEWER`.
-
-\tparam CT2 a triangulation class derived from `Constrained_triangulation_2`
-\tparam InDomainPMap a class model of `ReadWritePropertyMap`  with
-`CT::Face_handle`  as key type and  `bool`  as value type.
-
-\param act2 the constrained triangulation to draw.
-\param ipm the property map defining the faces which are in the domain.
+\tparam T2 a triangulation class derived from `Triangulation_2`
+\param at2 the triangulation to draw.
+\param gs the graphic storage to fill.
+\param df the drawing functor.
 
 */
-  template<class CT2, class InDomainPMap>
-  void draw(const CT2& act2, InDomainPMap ipm);
+template<class T2>
+void add_in_graphic_storage(const T2& at2,
+                            CGAL::Graphic_storage<BufferType>& gs,
+                            const CGAL::Drawing_functor<T2,
+                            typename T2::Vertex_handle,
+                            typename T2::Finite_edges_iterator,
+                            typename T2::Finite_faces_iterator>& df=default);
 
 } /* namespace CGAL */
