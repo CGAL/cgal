f1faa51541e6ef84a3639f6bb7188e8136138d24
denizdiktas <denizdiktas@gmail.com>
2023-08-12 16:49:02 +0300

    added copyright notices

--- Arrangement_on_surface_2/demo/earth/Kml_reader.h
+++ Arrangement_on_surface_2/demo/earth/Kml_reader.h
@@ -1,3 +1,11 @@
+// Copyright(c) 2012, 2020  Tel - Aviv University(Israel).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s): Engin Deniz Diktas <denizdiktas@gmail.com>
 
 #ifndef KML_READER_H
 #define KML_READER_H
--- Arrangement_on_surface_2/demo/earth/Shapefile.cpp
+++ Arrangement_on_surface_2/demo/earth/Shapefile.cpp
@@ -1,3 +1,12 @@
+// Copyright(c) 2012, 2020  Tel - Aviv University(Israel).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s): Engin Deniz Diktas <denizdiktas@gmail.com>
+
 //
 //#include "Shapefile.h"
 //
