6db7abbb6c8d0403444a53e080950e7055ae2707
Ken Arroyo Ohori <k.ohori@tudelft.nl>
2023-08-21 11:36:56 +0200

    put wkt directly in example

--- Polygon_repair_2/examples/Polygon_repair_2/draw_multipolygon.cpp
+++ Polygon_repair_2/examples/Polygon_repair_2/draw_multipolygon.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream>
+#include <sstream>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polygon_repair_2/draw_multipolygon_with_holes_2.h>
@@ -9,9 +9,10 @@ using Kernel = CGAL::Exact_predicates_inexact_constructions_kernel;
 using Multipolygon_with_holes_2 = CGAL::Multipolygon_with_holes_2<Kernel>;
 
 int main(int argc, char* argv[]) {
-  std::ifstream in("data/nesting.wkt");
+  std::string wkt = "MULTIPOLYGON(((0 0,1 0,1 1,0 1,0 0),(0.1 0.1,0.1 0.9,0.9 0.9,0.9 0.1,0.1 0.1)),((0.2 0.2,0.8 0.2,0.8 0.8,0.2 0.8,0.2 0.2),(0.3 0.3,0.3 0.7,0.7 0.7,0.7 0.3,0.3 0.3)))";
+  std::istringstream iss(wkt);
   Multipolygon_with_holes_2 mp;
-  CGAL::IO::read_multi_polygon_WKT(in, mp);
+  CGAL::IO::read_multi_polygon_WKT(iss, mp);
   CGAL::draw(mp);
 
   return 0;
