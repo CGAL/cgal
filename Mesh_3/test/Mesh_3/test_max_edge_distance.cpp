7136ce6499dcc64d8f4255fbb8f5c2636a645cdc
ange-clement <ange.clement@etu.umontpellier.fr>
2023-09-05 12:05:11 +0200

    More precise testing test if edges generated with a max distance are smaller.

--- Mesh_3/test/Mesh_3/test_max_edge_distance.cpp
+++ Mesh_3/test/Mesh_3/test_max_edge_distance.cpp
@@ -76,6 +76,26 @@ public:
     this->verify_c3t3(c3t3, domain, Polyhedral_tag(), expected_nb_vertices * 0.95, expected_nb_vertices * 1.05, expected_nb_triangles * 0.95, expected_nb_triangles * 1.05);
 
     // verify that there are more vertices than without criteria
+    // and that the edges are smaller (minimum size and average size)
+
+    C3t3::Edges_in_complex_iterator edge_begin = c3t3.edges_in_complex_begin();
+    K::FT min_edge_size_with_distance = CGAL::sqrt(
+        CGAL::squared_distance(edge_begin->first->vertex( edge_begin->second )->point(), edge_begin->first->vertex( edge_begin->third )->point())
+        );
+    K::FT avg_edge_size_with_distance = min_edge_size_with_distance;
+    edge_begin++;
+    int nbEdges = 1;
+    for (C3t3::Edges_in_complex_iterator eit = edge_begin ; eit != c3t3.edges_in_complex_end(); ++eit ) {
+      K::FT dist = CGAL::sqrt(
+          CGAL::squared_distance(eit->first->vertex( eit->second )->point(), eit->first->vertex( eit->third )->point())
+          );
+      if (dist < min_edge_size_with_distance) {
+          min_edge_size_with_distance = dist;
+      }
+      avg_edge_size_with_distance += dist;
+      nbEdges++;
+    }
+    avg_edge_size_with_distance /= nbEdges;
 
     Mesh_criteria criteria_without(edge_size = 0.074,
         facet_distance = 0.0074,
@@ -86,8 +106,32 @@ public:
 
     // Mesh generation
     c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria_without, no_perturb(), no_exude());
+
+    // Get edge min and average length
+    C3t3::Edges_in_complex_iterator edge_begin_ = c3t3.edges_in_complex_begin();
+    K::FT min_edge_size_without_distance = CGAL::sqrt(
+        CGAL::squared_distance(edge_begin_->first->vertex( edge_begin_->second )->point(), edge_begin_->first->vertex( edge_begin_->third )->point())
+        );
+    K::FT avg_edge_size_without_distance = min_edge_size_without_distance;
+    edge_begin_++;
+    nbEdges = 1;
+    for (C3t3::Edges_in_complex_iterator eit = edge_begin_ ; eit != c3t3.edges_in_complex_end(); ++eit ) {
+      K::FT dist = CGAL::sqrt(
+          CGAL::squared_distance(eit->first->vertex( eit->second )->point(), eit->first->vertex( eit->third )->point())
+          );
+      if (dist < min_edge_size_without_distance) {
+          min_edge_size_without_distance = dist;
+      }
+      avg_edge_size_without_distance += dist;
+      nbEdges++;
+    }
+    avg_edge_size_without_distance /= nbEdges;
+
     assert(nb_vertices  > c3t3.triangulation().number_of_vertices());
     assert(nb_triangles > c3t3.number_of_facets_in_complex());
+
+    assert(min_edge_size_with_distance < min_edge_size_without_distance);
+    assert(avg_edge_size_with_distance < avg_edge_size_without_distance);
   }
 };
 
