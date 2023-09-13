d796c06cae5b5ada36241040ee8222185ec24f80
Sébastien Loriot <sebastien.loriot@cgal.org>
2023-09-13 12:02:46 +0200

    add test

--- Polygon_mesh_processing/test/Polygon_mesh_processing/test_corefinement_nm_bo.cpp
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/test_corefinement_nm_bo.cpp
@@ -3,6 +3,7 @@
 
 #include <CGAL/Polygon_mesh_processing/corefinement.h>
 #include <CGAL/Polygon_mesh_processing/IO/polygon_mesh_io.h>
+#include <CGAL/Polygon_mesh_processing/measure.h>
 
 #include <iostream>
 #include <string>
@@ -12,16 +13,14 @@ typedef CGAL::Surface_mesh<K::Point_3>                        Mesh;
 
 namespace PMP = CGAL::Polygon_mesh_processing;
 
-int main(int argc, char* argv[])
+void test(std::string filename1, std::string filename2)
 {
-  const std::string filename1 = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/blobby.off");
-  const std::string filename2 = (argc > 2) ? argv[2] : CGAL::data_file_path("meshes/eight.off");
-
+  std::cout << "Running test with " << filename1 << " " << filename2 << "\n";
   Mesh mesh1, mesh2;
   if(!PMP::IO::read_polygon_mesh(filename1, mesh1) || !PMP::IO::read_polygon_mesh(filename2, mesh2))
   {
     std::cerr << "Invalid input." << std::endl;
-    return 1;
+    exit(1);
   }
 
 
@@ -32,40 +31,56 @@ int main(int argc, char* argv[])
 
   std::array<bool,4> res = PMP::corefine_and_compute_boolean_operations(mesh1, mesh2, output, CGAL::parameters::visitor(visitor));
 
-  // UNION
+  std::array<std::string, 4> out_name_prefixes = {"union", "intersection", "tm1_minus_tm2", "tm2_minus_tm1"};
+
+  for (int k=0; k<4; ++k)
   {
-  std::vector<K::Point_3> points;
-  std::vector< std::array<std::size_t, 3> > polygons;
-  visitor.extract_union(points, polygons);
-  CGAL::IO::write_polygon_soup("union.off", points, polygons, CGAL::parameters::stream_precision(17));
-  assert(res[PMP::Corefinement::UNION] == PMP::is_polygon_soup_a_polygon_mesh(polygons));
+    std::cout << "  checking " << out_name_prefixes[k] << "\n";
+    std::vector<K::Point_3> points;
+    std::vector< std::array<std::size_t, 3> > polygons;
+    switch(k)
+    {
+      case 0: visitor.extract_union(points, polygons); break;
+      case 1: visitor.extract_intersection(points, polygons); break;
+      case 2: visitor.extract_tm1_minus_tm2(points, polygons); break;
+      case 3: visitor.extract_tm2_minus_tm1(points, polygons); break;
+    }
+    CGAL::IO::write_polygon_soup(out_name_prefixes[k]+".off", points, polygons, CGAL::parameters::stream_precision(17));
+    assert(res[k] == PMP::is_polygon_soup_a_polygon_mesh(polygons));
+    if (res[k])
+    {
+      Mesh tmp;
+      PMP::polygon_soup_to_polygon_mesh(points, polygons, tmp);
+      std::size_t i=0;
+      PMP::match_faces(tmp, *(output[k].value()), CGAL::Emptyset_iterator(),
+                       CGAL::Counting_output_iterator(&i),
+                       CGAL::Counting_output_iterator(&i));
+      assert(i==0); // might be breaking with coplanar patches
+    }
   }
-  // INTERSECTION
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc<3)
   {
-  std::vector<K::Point_3> points;
-  std::vector< std::array<std::size_t, 3> > polygons;
-  visitor.extract_intersection(points, polygons);
-  CGAL::IO::write_polygon_soup("inter.off", points, polygons, CGAL::parameters::stream_precision(17));
-  assert(res[PMP::Corefinement::INTERSECTION] == PMP::is_polygon_soup_a_polygon_mesh(polygons));
+    test("data-coref/nm/tm1.off", "data-coref/nm/tm_union.off");
+    test("data-coref/nm/tm1.off", "data-coref/nm/tm_inter.off");
+    test("data-coref/nm/tm1.off", "data-coref/nm/tm_minus.off");
+    test("data-coref/nm/tm_minus.off", "data-coref/nm/tm1.off");
+    test("data-coref/nm/tm_1.off", "data-coref/nm/tm_multi.off");
+    test("data-coref/nm/tm_multi.off", "data-coref/nm/tm1.off");
+    test("data-coref/sphere.off", "data-coref/elephant.off");
   }
-  // TM1_MINUS_TM2
+  else
   {
-  std::vector<K::Point_3> points;
-  std::vector< std::array<std::size_t, 3> > polygons;
-  visitor.extract_tm1_minus_tm2(points, polygons);
-  CGAL::IO::write_polygon_soup("tm1_minus_tm2.off", points, polygons, CGAL::parameters::stream_precision(17));
-  assert(res[PMP::Corefinement::TM1_MINUS_TM2] == PMP::is_polygon_soup_a_polygon_mesh(polygons));
-  }
-  // TM2_MINUS_TM1
-  {
-  std::vector<K::Point_3> points;
-  std::vector< std::array<std::size_t, 3> > polygons;
-  visitor.extract_tm2_minus_tm1(points, polygons);
-  CGAL::IO::write_polygon_soup("tm2_minus_tm1.off", points, polygons, CGAL::parameters::stream_precision(17));
-  assert(res[PMP::Corefinement::TM2_MINUS_TM1] == PMP::is_polygon_soup_a_polygon_mesh(polygons));
+    const std::string filename1 = argv[1];
+    const std::string filename2 = argv[2];
+
+    test(filename1, filename2);
   }
 
-  //TODO common faces
+
 
   return 0;
 }
