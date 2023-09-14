3fca44f9492f5e5e70de0bd560dffba54603eb0d
Efi Fogel <efifogel@gmail.com>
2023-09-14 16:04:23 +0300

    Left \ingroup statements in separe lines

--- Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/Arrangement_2.h
+++ Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/Arrangement_2.h
@@ -160,12 +160,12 @@ public:
 
 namespace CGAL {
 
-/*! \ingroup PkgArrangementOnSurface2Insert insert The function `%insert`
- * inserts one or more curves or \f$ x\f$-monotone curves into a given
- * arrangement, where no restrictions are imposed on the inserted curves. If an
- * inserted curve is not \f$ x\f$-monotone curve, it is subdivided into \f$
- * x\f$-monotone subcurves (and perhaps isolated points), which are inserted
- * into the arrangement.
+/*! \ingroup PkgArrangementOnSurface2Insert
+ * The function `%insert` inserts one or more curves or \f$ x\f$-monotone curves
+ * into a given arrangement, where no restrictions are imposed on the inserted
+ * curves. If an inserted curve is not \f$ x\f$-monotone curve, it is subdivided
+ * into \f$ x\f$-monotone subcurves (and perhaps isolated points), which are
+ * inserted into the arrangement.
  *
  * \cgalHeading{Requirements}
  *
--- Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/Arrangement_on_surface_2.h
+++ Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/Arrangement_on_surface_2.h
@@ -1014,7 +1014,7 @@ public:
 
 namespace CGAL {
 
-/*! \ingroup PkgArrangementOnSurface2Insert insert
+/*! \ingroup PkgArrangementOnSurface2Insert
  * The function `%insert` inserts one or more curves or \f$ x\f$-monotone
  * curves into a given arrangement, where no restrictions are imposed on the
  * inserted curves. If an inserted curve is not \f$ x\f$-monotone curve, it is
