0d344cdc2727d6b9ac208dd35d919eebe7d9a859
Andreas Fabri <andreas.fabri@geometryfactory.com>
2023-09-20 15:58:13 +0100

    Bounding_volumes: Add issue7730.cpp

---
+++ Bounding_volumes/test/Bounding_volumes/issue7730.cpp
@@ -0,0 +1,31 @@
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/rectangular_p_center_2.h>
+#include <CGAL/algorithm.h>
+#include <CGAL/iterator.h>
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+
+typedef double                                      FT;
+
+typedef CGAL::Simple_cartesian<FT>                  Kernel;
+
+typedef Kernel::Point_2                             Point;
+typedef std::array<Point,4>                          Cont;
+
+int main()
+{
+  int n = 10;
+  int p = 3;
+
+  Cont points = { Point(0, -1), Point(1, -2),  Point(3,2),  Point(4,1) };
+  
+  FT p_radius;
+
+  CGAL::rectangular_p_center_2(
+    points.begin(), points.end(), CGAL::Emptyset_iterator(), p_radius, p);
+  std::cout << "\n\n" << p << "-radius = " << p_radius << std::endl;
+
+  return 0;
+}
