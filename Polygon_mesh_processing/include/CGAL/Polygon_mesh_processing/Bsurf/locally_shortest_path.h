fcf4c2030bba04d2b533e8213541cbda3ecf399a
Sébastien Loriot <sebastien.loriot@cgal.org>
2023-09-25 11:22:34 +0200

    add initial structure for bsurf import into CGAL

---
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/Bsurf/locally_shortest_path.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2023 University of Genova (Italy).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
+// Author(s)     : Claudio Mancinelli
+
+#ifndef CGAL_POLYGON_MESH_PROCESSING_BSURF_LOCALLY_SHORTEST_PATH_H
+#define CGAL_POLYGON_MESH_PROCESSING_BSURF_LOCALLY_SHORTEST_PATH_H
+
+// #include <CGAL/license/Polygon_mesh_processing/bsurf.h>
+
+#include <CGAL/Named_function_parameters.h>
+#include <CGAL/boost/graph/named_params_helper.h>
+
+#include <boost/graph/graph_traits.hpp>
+
+
+
+namespace CGAL{
+namespace Polygon_mesh_processing {
+
+} } // CGAL::Polygon_mesh_processing
