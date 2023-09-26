5b1248f910401c35617df1ff8f5ae2dc025cb624
Laurent Rineau <laurent.rineau@cgal.org>
2023-09-26 12:43:46 +0200

    unless CGAL_TRIANGULATION_CHECK_EXPENSIVE

--- Triangulation_3/test/Triangulation_3/cdt_3_from_off.cpp
+++ Triangulation_3/test/Triangulation_3/cdt_3_from_off.cpp
@@ -1,7 +1,6 @@
 //#define CGAL_CDT_2_DEBUG_INTERSECTIONS 1
 #define NO_TRY_CATCH 1
 // #define CGAL_DEBUG_CDT_3 1
-#define CGAL_TRIANGULATION_CHECK_EXPENSIVE 1
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_3.h>
 #include <CGAL/Random.h>
