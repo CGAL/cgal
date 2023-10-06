93183fc2b7a2b5ad40438599392ba7c63272db86
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-10-06 07:52:15 +0200

    document draw and add_in_graphics_scene for lcc (for validation)

--- Linear_cell_complex/doc/Linear_cell_complex/CGAL/draw_linear_cell_complex.h
+++ Linear_cell_complex/doc/Linear_cell_complex/CGAL/draw_linear_cell_complex.h
@@ -6,17 +6,23 @@ namespace CGAL {
 opens a new window and draws `alcc`, a model of the `LinearCellComplex` concept. A call to this function is blocking, that is the program continues as soon as the user closes the window. This function requires `CGAL_Qt5`, and is only available if the macro `CGAL_USE_BASIC_VIEWER` is defined.
 Linking with the cmake target `CGAL::CGAL_Basic_viewer` will link with `CGAL_Qt5` and add the definition `CGAL_USE_BASIC_VIEWER`.
 \tparam LCC a model of the `LinearCellComplex` concept.
+\tparam GSOptions a class having the same methods than `Graphics_scene_options`: `Graphics_scene_options` by default.
+
 \param alcc the linear cell complex to draw.
 \param gs_options graphics scene options.
 
 */
   template<class LCC, class GSOptions>
-  void draw(const LCC& alcc, const GSOptions& gs_options);
+  void draw(const LCC& alcc, const GSOptions& gs_options=GSOptions());
 
 /*!
 \ingroup PkgDrawLinearCellComplex
 
 Add in the given graphics scene the elements of alcc.
+
+\tparam LCC a model of the `LinearCellComplex` concept.
+\tparam BufferType the number type used for point coordinates: `float` by default.
+\tparam GSOptions a class having the same methods than `Graphics_scene_options`: `Graphics_scene_options` by default.
 
 \param alcc the linear cell complex to draw.
 \param graphics_scene the graphics scene.
@@ -25,7 +31,7 @@ Add in the given graphics scene the elements of alcc.
 */
   template<class LCC, class BufferType, class GSOptions>
   void add_in_graphics_scene(const LCC& alcc, CGAL::Graphics_scene<BufferType>& graphics_scene,
-                              const GSOptions& gs_options);
+                             const GSOptions& gs_options=GSOptions());
 
 } /* namespace CGAL */
 
