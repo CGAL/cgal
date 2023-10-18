91d66d0bfc76572e8bc1e1f06595405f5596b9f0
ange-clement <ange.clement@etu.umontpellier.fr>
2023-10-18 11:01:50 +0200

    Renamed Construct_initial_points_from_labeled_image.h

--- Mesh_3/include/CGAL/Mesh_3/Construct_initial_points_labeled_image.h
+++ Mesh_3/include/CGAL/Mesh_3/Construct_initial_points_labeled_image.h
@@ -8,7 +8,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 //
 //
-// Author(s)     : Ange Clement
+// Author(s)     : Laurent Rineau and Ange Clement
 
 #ifndef CGAL_MESH_3_CONSTRUCT_INITIAL_POINTS_FROM_LABELED_IMAGE_H
 #define CGAL_MESH_3_CONSTRUCT_INITIAL_POINTS_FROM_LABELED_IMAGE_H
