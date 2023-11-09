75689f325af3e7ffa3e37b6219a63346bb82f968
Laurent Rineau <laurent.rineau@cgal.org>
2023-11-09 19:36:53 +0100

    use std::invoke (C++17) for IILE
    *Immediately Invoked Lambda Expression*
    
    Now that CGAL can use C++17 , switch to `std::invoke`
    that is more readable that the two small characters `()`
    hidden somewhere a few lines below.
    
    With `std::invoke`, we understand straight away that the lambda is
    called immediately.
    
    Ref:
    
    https://www.cppstories.com/2016/11/iife-for-complex-initialization/

--- Triangulation_3/include/CGAL/Triangulation_3/internal/Triangulation_segment_traverser_3_impl.h
+++ Triangulation_3/include/CGAL/Triangulation_3/internal/Triangulation_segment_traverser_3_impl.h
@@ -597,7 +597,7 @@ Triangulation_segment_cell_iterator_3<Tr,Inc>::walk_to_next_3(const Simplex& pre
   // The target lies inside this cell.
   CGAL_assertion( incell );
   return {
-    [&]() -> Simplex {
+    std::invoke([&]() -> Simplex {
       switch( op[0] + op[1] + op[2] + op[3] ) {
       case 4:
         CGAL_assertion( pos == 6 );
@@ -625,7 +625,7 @@ Triangulation_segment_cell_iterator_3<Tr,Inc>::walk_to_next_3(const Simplex& pre
       default:
         CGAL_unreachable();
       }
-    }(),
+    }),
     { Cell_handle() }
   };
 }
