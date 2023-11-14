30bd53c25cea4aff6d8fe3a61d7cdb58f45d7935
Sven Oesau <sven.oesau@geometryfactory.com>
2023-11-14 10:43:08 +0100

    pass on documentation [skip ci]

---
+++ Kinetic_shape_reconstruction/doc/Kinetic_shape_reconstruction/Concepts/KineticLCCItems.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2023 GeometryFactory Sarl (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
+// Author(s)     : Sven Oesau, Florent Lafarge, Dmitry Anisimov, Simon Giraudot
+
+/*!
+\ingroup PkgKineticShapePartitionConcepts
+\cgalConcept
+
+The concept `KineticLCCItems` refines the concept of `LinearCellComplexItems` by adding 2-attributes and 3-attributes to store information from the kinetic partition.
+
+The third type in Attributes tuple must be a model of the `KineticLCCFaceAttribute` concept.
+The fourth type in Attributes tuple must be a model of the `KineticLCCVolumeAttribute` concept.
+
+\sa `CGAL::Kinetic_shape_partition_3`
+\sa `CGAL::KineticLCCFaceAttribute`
+\sa `CGAL::KineticLCCVolumeAttribute`
+\sa `LinearCellComplexItems`
+*/
+
+struct KineticLCCItems {
+/// \name Types
+/// @{
+  /// Using the index-based version of the `Linear_Cell_Complex`.
+  typedef CGAL::Tag_true Use_index;
+  typedef std::uint32_t Index_type;
+/// @}
+};
--- Kinetic_shape_reconstruction/doc/Kinetic_shape_reconstruction/Concepts/KineticLCCVolumeAttribute.h
+++ Kinetic_shape_reconstruction/doc/Kinetic_shape_reconstruction/Concepts/KineticLCCVolumeAttribute.h
@@ -14,19 +14,22 @@
 \ingroup PkgKineticShapePartitionConcepts
 \cgalConcept
 
-The concept `KineticLCCVolumeProperty` is part of the item properties of the `Linear_Cell_Complex` used to store the resulting partition of `CGAL::Kinetic_shape_partition_3`.
+The concept `KineticLCCVolumeAttribute` refines `CellAttribute` to store the barycenter and an id.
 
 \cgalHasModelsBegin
-\cgalHasModelsBare{`CGAL::Kinetic_shape_partition_3::LCC_Base_Properties::Volume_property`}
+\cgalHasModelsBare{`CellAttribute<LCC, CGAL::Kinetic_shape_partition_3::LCC_Base_Attribute::Volume_attribute>`}
 \cgalHasModelsEnd
 
 \sa `CGAL::Kinetic_shape_partition_3`
 \sa `LinearCellComplexItems`
 */
 
-struct KineticLCCVolumeProperty {
+struct KineticLCCVolumeAttribute {
+/// \name Access Members
+/// @{
   /// Contains the bary_cernter of the volume.
   typename Intersection_kernel::Point_3 barycenter;
   /// 0-based volume id.
   std::size_t volume_id;
+/// @}
 };
