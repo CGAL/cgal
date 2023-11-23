d9bdf5cfc5eacb36c0c012d2c77651bf7b5dbbcd
Sébastien Loriot <sebastien.loriot@cgal.org>
2023-11-23 09:38:28 +0100

    remove extra typename

--- Triangulation_2/include/CGAL/Regular_triangulation_2.h
+++ Triangulation_2/include/CGAL/Regular_triangulation_2.h
@@ -528,8 +528,8 @@ public:
   insert(boost::zip_iterator< boost::tuple<InputIterator_1,InputIterator_2> > first,
           boost::zip_iterator< boost::tuple<InputIterator_1,InputIterator_2> > last,
           std::enable_if_t<
-              typename std::is_convertible_v< typename std::iterator_traits<InputIterator_1>::value_type, Weighted_point > &&
-              typename std::is_convertible_v< typename std::iterator_traits<InputIterator_2>::value_type, typename internal::Info_check<typename Triangulation_data_structure::Vertex>::type >
+              std::is_convertible_v< typename std::iterator_traits<InputIterator_1>::value_type, Weighted_point > &&
+              std::is_convertible_v< typename std::iterator_traits<InputIterator_2>::value_type, typename internal::Info_check<typename Triangulation_data_structure::Vertex>::type >
           >* =nullptr
 )
   {return insert_with_info< boost::tuple<Weighted_point,typename internal::Info_check<typename Triangulation_data_structure::Vertex>::type> >(first,last);}
