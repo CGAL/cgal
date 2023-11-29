cde35d5179cbeba3dccd8533d85f1b358df48a08
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-11-29 16:29:04 +0100

    SMT: draw face width

--- Surface_mesh_topology/examples/Surface_mesh_topology/draw_facewidth.h
+++ Surface_mesh_topology/examples/Surface_mesh_topology/draw_facewidth.h
@@ -9,84 +9,27 @@
 #include <CGAL/Graphics_scene_options.h>
 
 template<typename ALCC>
-struct Facewidth_graphics_scene_options
+struct Facewidth_graphics_scene_options:
+  public CGAL::Graphics_scene_options<ALCC,
+                                      typename ALCC::Dart_const_handle,
+                                      typename ALCC::Dart_const_handle,
+                                      typename ALCC::Dart_const_handle,
+                                      typename ALCC::Dart_const_handle>
 {
   Facewidth_graphics_scene_options(typename ALCC::size_type amark1, typename ALCC::size_type amark2) :
     m_vertex_mark(amark1), m_face_mark(amark2)
-  {}
+  {
+    this->colored_vertex=[this](const ALCC& alcc, typename ALCC::Dart_const_handle dh)->bool
+    { return alcc.is_marked(dh, m_vertex_mark); };
+    this->vertex_color=[](const ALCC&, typename ALCC::Dart_const_handle)->CGAL::IO::Color
+    { return CGAL::IO::Color(0, 255, 0); };
 
-  template<typename LCC>
-  bool colored_vertex(const LCC& alcc, typename LCC::Dart_const_descriptor dh) const
-  { return alcc.is_marked(dh, m_vertex_mark); }
-
-  template<typename LCC>
-  CGAL::IO::Color vertex_color(const LCC& /* alcc */,
-                           typename LCC::Dart_const_descriptor /* dh */) const
-  { return CGAL::IO::Color(0, 255, 0); }
-
-  template<typename LCC>
-  bool colored_edge(const LCC& /*alcc*/, typename LCC::Dart_const_descriptor /*dh*/) const
-  { return false; }
-
-  template<typename LCC>
-  CGAL::IO::Color edge_color(const LCC& /* alcc*/,
-                         typename LCC::Dart_const_descriptor /* dh */) const
-  { return CGAL::IO::Color(0, 0, 255); }
-
-  template<typename LCC>
-  bool colored_face(const LCC& /* alcc */,
-                    typename LCC::Dart_const_descriptor /* dh */) const
-  {return true;}
-
-  template<typename LCC>
-  CGAL::IO::Color face_color(const LCC& alcc, typename LCC::Dart_const_descriptor dh) const
-  { return alcc.is_marked(dh, m_face_mark)?CGAL::IO::Color(255, 0, 0)
-                                          :CGAL::IO::Color(211, 211, 211); }
-
-  template<typename LCC>
-  bool colored_volume(const LCC& /* alcc */,
-                      typename LCC::Dart_const_descriptor /* dh */) const
-  { return false; }
-
-  template<typename LCC>
-  bool draw_volume (const LCC& /* alcc */,
-                      typename LCC::Dart_const_handle /* dh */) const
-  { return false; }
-
-  template<typename LCC>
-  bool draw_face (const LCC& /* alcc */,
-                      typename LCC::Dart_const_handle /* dh */) const
-  { return true; }
-
-  template<typename LCC>
-  bool draw_edge (const LCC& /* alcc */,
-                      typename LCC::Dart_const_handle /* dh */) const
-  { return true; }
-
-  template<typename LCC>
-  bool volume_wireframe (const LCC& /* alcc */,
-                      typename LCC::Dart_const_handle /* dh */) const
-  { return false; }
-
-  template<typename LCC>
-  bool face_wireframe (const LCC& /* alcc */,
-                      typename LCC::Dart_const_handle /* dh */) const
-  { return false; }
-
-  template<typename LCC>
-  bool draw_vertex (const LCC& /* alcc */,
-                      typename LCC::Dart_const_handle /* dh */) const
-  { return true; }
-
-  template<typename LCC>
-  CGAL::IO::Color volume_color(const LCC& /* alcc */,
-                         typename LCC::Dart_const_handle /* dh */) const {
-    return CGAL::IO::Color(20, 10, 30);
+    this->colored_face=[this](const ALCC&, typename ALCC::Dart_const_handle)->bool
+    { return true; };
+    this->face_color=[this](const ALCC& alcc, typename ALCC::Dart_const_handle dh)->CGAL::IO::Color
+    { return alcc.is_marked(dh, m_face_mark)?CGAL::IO::Color(255, 0, 0)
+        :CGAL::IO::Color(211, 211, 211); };
   }
-
-  bool are_edges_enabled() const { return true; }
-  bool are_vertices_enabled() const { return true; }
-  bool are_faces_enabled() const { return true; }
 
   typename ALCC::size_type m_vertex_mark, m_face_mark;
 };
@@ -99,10 +42,10 @@ void draw_facewidth(const LCC& lcc,
   typename LCC::size_type face_mark = lcc.get_new_mark();
   for (std::size_t i=0; i<cycle.size(); ++i)
   {
-    // Color the vertex
+    // Color the vertices
     if (!lcc.is_marked(cycle[i], vertex_mark))
     { lcc.template mark_cell<0>(cycle[i], vertex_mark); }
-    // Color the face
+    // Color the faces
     if (!lcc.is_marked(cycle[i], face_mark))
     { lcc.template mark_cell<2>(cycle[i], face_mark); }
   }
@@ -112,15 +55,6 @@ void draw_facewidth(const LCC& lcc,
 
   lcc.free_mark(vertex_mark);
   lcc.free_mark(face_mark);
-}
-
-#else  // CGAL_USE_BASIC_VIEWER
-
-template<typename LCC>
-void draw_facewidth(const LCC&,
-                    const std::vector<typename LCC::Dart_const_descriptor>&)
-{
-  std::cerr<<"Impossible to draw, CGAL_USE_BASIC_VIEWER is not defined."<<std::endl;
 }
 
 #endif // CGAL_USE_BASIC_VIEWER
