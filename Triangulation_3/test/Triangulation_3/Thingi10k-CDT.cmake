75a08ce7461c3a1a5f15e6f2ef7898ce72a4fc97
Laurent Rineau <laurent.rineau@cgal.org>
2023-11-30 12:44:24 +0100

    Thingi10k: add a way to run a test case with merge_facets only
    Now CTest displays:
    ```
        Start 5393: execution   of  cdt_3_from_off --merge-facets Thingi10K_106789
        Start 5392: execution   of  cdt_3_from_off Thingi10K_106789
    4/6 Test #5392: execution   of  cdt_3_from_off Thingi10K_106789 ..................***Not Run (Disabled)   0.00 sec
    5/6 Test #5393: execution   of  cdt_3_from_off --merge-facets Thingi10K_106789 ...   Passed    0.10 sec
    ```

--- Triangulation_3/test/Triangulation_3/Thingi10k-CDT.cmake
+++ Triangulation_3/test/Triangulation_3/Thingi10k-CDT.cmake
@@ -9,8 +9,14 @@ find_path(THINGI10K_DATA_DIR NAME 132423.stl
   )
 include(./thingi10k_max_10k_solid.cmake)
 
+set(thingi10k_BLACKLIST_WITHOUT_MERGE_FACETS
+    106789.stl
+    # At point 4.0163683382116631 -2.094120689431076 0
+    # There is a edge of the input mesh that is infinitely close to a vertex.
+    )
+
 function(CGAL_add_cdt3_test_from_Thingi10k data_name data_filename)
-  set(options "")
+  set(options "ONLY_MERGE_FACETS")
   set(oneValueArgs TIMEOUT)
   set(multiValueArgs LABELS)
   cmake_parse_arguments(PARSE_ARGV 2 "MY" "${options}" "${oneValueArgs}"
@@ -21,13 +27,21 @@ function(CGAL_add_cdt3_test_from_Thingi10k data_name data_filename)
   if(MY_TIMEOUT)
     set(MY_TIMEOUT_KEYWORD TIMEOUT)
   endif()
+  if(MY_ONLY_MERGE_FACETS)
+    set(MY_ONLY_MERGE_FACETS ONLY_MERGE_FACETS)
+  endif()
   CGAL_add_cdt3_from_off_test_aux(${data_name} ${THINGI10K_DATA_DIR} DATA_FILENAME ${data_filename}
      LABELS Thingi10K ${MY_LABELS}
      ${MY_TIMEOUT_KEYWORD} ${MY_TIMEOUT}
+     ${MY_ONLY_MERGE_FACETS}
      )
 endfunction()
 
 foreach(thingi_file_name ${thingi10k_max_10k_solid})
+  if(thingi_file_name IN_LIST thingi10k_BLACKLIST_WITHOUT_MERGE_FACETS)
+    set(MY_ONLY_MERGE_FACETS ONLY_MERGE_FACETS)
+  endif()
   get_filename_component(thingi_ID "${thingi_file_name}" NAME_WE)
-  CGAL_add_cdt3_test_from_Thingi10k(Thingi10K_${thingi_ID} ${thingi_file_name} TIMEOUT 600 LABELS Thing10K_max_10k_solid)
+  CGAL_add_cdt3_test_from_Thingi10k(Thingi10K_${thingi_ID} ${thingi_file_name}
+      TIMEOUT 600 LABELS Thing10K_max_10k_solid ${MY_ONLY_MERGE_FACETS})
 endforeach()
