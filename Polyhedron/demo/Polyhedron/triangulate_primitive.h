9115847ed4c7f95b9cf727cd1af896cc74759747
Sven Oesau <sven.oesau@geometryfactory.com>
2023-11-30 16:03:28 +0100

    added handling for rendering of non-triangular faces with ghost edges ghost edges are not added to cdt in FacetTriangulator mark_domain_in_triangulation is used to mark holes as external

--- Polyhedron/demo/Polyhedron/triangulate_primitive.h
+++ Polyhedron/demo/Polyhedron/triangulate_primitive.h
@@ -23,13 +23,15 @@
 // @todo just use PMP::triangulate_face()...?
 // or at least mark_faces_in_domain()
 
-template<class Mesh, typename Kernel, typename Index_type>
+template<class Mesh, typename Kernel, typename Index_type, typename CDT_ITag = CGAL::Exact_predicates_tag>
 class FacetTriangulator
 {
 public:
   using Traits = Kernel;
   using Point = typename Kernel::Point_3;
   using Vector = typename Kernel::Vector_3;
+
+  using Index = Index_type;
 
   using P_traits = CGAL::Projection_traits_3<Traits>;
 
@@ -46,23 +48,14 @@ public:
   using Fbb = CGAL::Triangulation_face_base_with_info_2<Face_info, P_traits>;
   using Fb = CGAL::Constrained_triangulation_face_base_2<P_traits, Fbb>;
   using TDS = CGAL::Triangulation_data_structure_2<Vb, Fb>;
-  using Itag = CGAL::Exact_predicates_tag;//Exact_intersections_tag
-  using CDT = CGAL::Constrained_Delaunay_triangulation_2<P_traits, TDS, Itag>;
+  using CDT = CGAL::Constrained_Delaunay_triangulation_2<P_traits, TDS, CDT_ITag>;
 
   using Vertex_handle = typename CDT::Vertex_handle;
   using Face_handle = typename CDT::Face_handle;
 
-  struct PointAndId
-  {
-    Point point;
-    Index_type id;
-    PointAndId() = default;
-    PointAndId(const Point& point, const Index_type id) : point(point), id(id) { }
-  };
-
 public:
   CDT* cdt;
-  CGAL::Unique_hash_map<Vertex_handle, Index_type> v2v;
+  CGAL::Unique_hash_map<Vertex_handle, Index> v2v;
 
 public:
   // Constructor
@@ -71,10 +64,10 @@ public:
                     Mesh* poly,
                     Vector offset = Vector(0,0,0))
   {
-    std::vector<PointAndId> idPoints;
+    std::vector<std::pair<typename Kernel::Point_3, Index> > idPoints;
     for(halfedge_descriptor he_circ : halfedges_around_face(halfedge(fd, *poly), *poly))
-      idPoints.emplace_back(get(CGAL::vertex_point, *poly, source(he_circ, *poly)) + offset,
-                            source(he_circ, *poly));
+      idPoints.emplace_back(std::make_pair(get(CGAL::vertex_point, *poly, source(he_circ, *poly)) + offset,
+                            source(he_circ, *poly)));
 
     if(!triangulate(idPoints, normal))
       std::cerr << "Facet not displayed" << std::endl;
@@ -86,22 +79,22 @@ public:
                     Mesh* poly,
                     Vector offset = Vector(0,0,0))
   {
-    std::vector<PointAndId> idPoints;
+    std::vector<std::pair<typename Kernel::Point_3, Index> > idPoints;
     for(halfedge_descriptor he_circ : halfedges_around_face(halfedge(fd, *poly), *poly))
-      idPoints.emplace_back(get(CGAL::vertex_point, *poly, source(he_circ, *poly)) + offset,
-                            source(he_circ, *poly));
+      idPoints.emplace_back(std::make_pair(get(CGAL::vertex_point, *poly, source(he_circ, *poly)) + offset,
+                            source(he_circ, *poly)));
 
     if(!triangulate_with_points(idPoints, more_points, normal))
       std::cerr << "Facet not displayed" << std::endl;
   }
 
-  FacetTriangulator(std::vector<PointAndId>& idPoints,
+  FacetTriangulator(std::vector<std::pair<typename Kernel::Point_3, Index> >& idPoints,
                     const Vector& normal)
   {
     if(!triangulate(idPoints, normal))
       std::cerr << "Facet not displayed" << std::endl;
   }
-  FacetTriangulator(std::vector<PointAndId>& idPoints,
+  FacetTriangulator(std::vector < std::pair<typename Kernel::Point_3, Index> >& idPoints,
                     const std::vector<Point>& more_points,
                     const Vector& normal)
   {
@@ -115,8 +108,14 @@ public:
       delete cdt;
   }
 
+  template<class Func>
+  void per_face(const Func& f) {
+    for (typename CDT::Finite_faces_iterator fit = cdt->finite_faces_begin(); fit != cdt->finite_faces_end(); ++fit)
+      f(*fit, v2v);
+  }
+
 private:
-  bool triangulate(std::vector<PointAndId>& idPoints,
+  bool triangulate(std::vector<std::pair<typename Kernel::Point_3, Index> >& idPoints,
                    const Vector& normal)
   {
     P_traits cdt_traits(normal);
@@ -124,28 +123,24 @@ private:
 
     std::map<std::pair<Point_3, Point_3>, std::size_t> edge_map;
     std::vector<bool> skip(idPoints.size(), false);
-    bool has_ghost_edges = false;
 
-/*
     for (std::size_t i = 0; i < idPoints.size(); i++) {
       int prev = (i - 1 + idPoints.size()) % idPoints.size();
-      if (idPoints[i].point < idPoints[prev].point) {
-        auto it = edge_map.emplace(std::make_pair(idPoints[i].point, idPoints[prev].point), i);
+      if (idPoints[i].first < idPoints[prev].first) {
+        auto it = edge_map.emplace(std::make_pair(idPoints[i].first, idPoints[prev].first), i);
         if (!it.second) {
           skip[i] = true;
           skip[it.first->second] = true;
-          has_ghost_edges = true;
         }
       }
       else {
-        auto it = edge_map.emplace(std::make_pair(idPoints[prev].point, idPoints[i].point), i);
+        auto it = edge_map.emplace(std::make_pair(idPoints[prev].first, idPoints[i].first), i);
         if (!it.second) {
           skip[i] = true;
           skip[it.first->second] = true;
-          has_ghost_edges = true;
         }
       }
-    }*/
+    }
 
     Vertex_handle previous, first, last_inserted;
     std::size_t previd, firstid, lastid;
@@ -155,20 +150,20 @@ private:
 
     for(std::size_t i = 0;i<idPoints.size();i++)
     {
-      const PointAndId& idPoint = idPoints[i];
-      x += idPoint.point.x();
-      y += idPoint.point.y();
-      z += idPoint.point.z();
+      const std::pair<typename Kernel::Point_3, Index>& idPoint = idPoints[i];
+      x += idPoint.first.x();
+      y += idPoint.first.y();
+      z += idPoint.first.z();
 
       Vertex_handle vh;
       // Always insert the first point, then only insert if the distance with the previous is reasonable.
-      if(first == Vertex_handle() || idPoint.point != previous->point())
+      if(first == Vertex_handle() || idPoint.first != previous->point())
       {
-        vh = cdt->insert(idPoint.point);
-        v2v[vh] = idPoint.id;
+        vh = cdt->insert(idPoint.first);
+        v2v[vh] = idPoint.second;
         if (first == Vertex_handle()) {
           first = vh;
-          firstid = idPoint.id;
+          firstid = idPoint.second;
         }
 
         if(previous != nullptr && previous != vh)
@@ -179,7 +174,7 @@ private:
           lastid = previd;
         }
         previous = vh;
-        previd = idPoint.id;
+        previd = idPoint.second;
       }
     }
 
@@ -189,76 +184,66 @@ private:
     if(previous != first && !skip[0])
       cdt->insert_constraint(previous, first);
 
-/*
-    std::vector<Point_3> pts;
-    std::ofstream vout("test.polylines.txt");
-    for (auto& e : cdt->constrained_edges()) {
-      // edge is a pair of Face_handle and index
-      auto v1 = e.first->vertex((e.second + 1) % 3);
-      auto v2 = e.first->vertex((e.second + 2) % 3);
-      pts.push_back(v1->point());
-      pts.push_back(v2->point());
-      vout << "2 " << v1->point() << " " << v2->point() << std::endl;
-    }
-    vout.close();*/
-
     // sets mark is_external
     for(Face_handle f2 : cdt->all_face_handles())
       f2->info().is_external = false;
 
-     std::unordered_map<Face_handle, bool> in_domain_map;
-     boost::associative_property_map< std::unordered_map<Face_handle, bool> > in_domain(in_domain_map);
+    std::unordered_map<Face_handle, bool> in_domain_map;
+    boost::associative_property_map< std::unordered_map<Face_handle, bool> > in_domain(in_domain_map);
     CGAL::mark_domain_in_triangulation<CDT>(*cdt, in_domain);
 
-    // check if the facet is external or internal
-/*
-    std::queue<typename CDT::Face_handle> face_queue;
-    face_queue.push(cdt->infinite_vertex()->face());
-    while(! face_queue.empty())
-    {
-      typename CDT::Face_handle fh = face_queue.front();
-      face_queue.pop();
-      if(fh->info().is_external)
-        continue;
-
-      fh->info().is_external = true;
-      for(int i = 0; i <3; ++i)
-      {
-        if(!cdt->is_constrained(std::make_pair(fh, i)))
-          face_queue.push(fh->neighbor(i));
-      }
-    }*/
-
-    for (const Face_handle& fh : cdt->all_face_handles()) {
+    for (const Face_handle& fh : cdt->all_face_handles())
       fh->info().is_external = !get(in_domain, fh);
-    }
 
     return true;
   }
 
-  bool triangulate_with_points(std::vector<PointAndId >& idPoints,
+  bool triangulate_with_points(std::vector<std::pair<typename Kernel::Point_3, Index> >& idPoints,
                                const std::vector<Point>& more_points,
                                const Vector& normal)
   {
     P_traits cdt_traits(normal);
     cdt = new CDT(cdt_traits);
 
+    std::map<std::pair<Point_3, Point_3>, std::size_t> edge_map;
+    std::vector<bool> skip(idPoints.size(), false);
+
+    for (std::size_t i = 0; i < idPoints.size(); i++) {
+      int prev = (i - 1 + idPoints.size()) % idPoints.size();
+      if (idPoints[i].first < idPoints[prev].first) {
+        auto it = edge_map.emplace(std::make_pair(idPoints[i].first, idPoints[prev].first), i);
+        if (!it.second) {
+          skip[i] = true;
+          skip[it.first->second] = true;
+        }
+      }
+      else {
+        auto it = edge_map.emplace(std::make_pair(idPoints[prev].first, idPoints[i].first), i);
+        if (!it.second) {
+          skip[i] = true;
+          skip[it.first->second] = true;
+        }
+      }
+    }
+
     // Iterate the points of the facet and decide if they must be inserted in the CDT
     Vertex_handle previous, first, last_inserted;
-    for(const PointAndId& idPoint : idPoints)
+    for (std::size_t i = 0; i < idPoints.size(); i++)
     {
+      const std::pair<typename Kernel::Point_3, Index>& idPoint = idPoints[i];
       Vertex_handle vh;
       // Always insert the first point, then only insert if the distance with the previous is reasonable.
-      if(first == Vertex_handle() || idPoint.point != previous->point())
+      if(first == Vertex_handle() || idPoint.first != previous->point())
       {
-        vh = cdt->insert(idPoint.point);
-        v2v[vh] = idPoint.id;
+        vh = cdt->insert(idPoint.first);
+        v2v[vh] = idPoint.second;
         if(first == Vertex_handle())
           first = vh;
 
         if(previous != nullptr && previous != vh)
         {
-          cdt->insert_constraint(previous, vh);
+          if (!skip[i])
+            cdt->insert_constraint(previous, vh);
           last_inserted = previous;
         }
         previous = vh;
@@ -268,7 +253,9 @@ private:
     if(last_inserted == Vertex_handle())
       return false;
 
+    if (previous != first && !skip[0])
     cdt->insert_constraint(previous, first);
+
     for(const Point& point : more_points)
       cdt->insert(point);
 
@@ -276,22 +263,12 @@ private:
     for(Face_handle f2 : cdt->all_face_handles())
       f2->info().is_external = false;
 
-    // check if the facet is external or internal
-    std::queue<typename CDT::Face_handle> face_queue;
-    face_queue.push(cdt->infinite_vertex()->face());
-    while(!face_queue.empty())
-    {
-      typename CDT::Face_handle fh = face_queue.front();
-      face_queue.pop();
-      if(fh->info().is_external)
-        continue;
-      fh->info().is_external = true;
-      for(int i = 0; i <3; ++i)
-      {
-        if(!cdt->is_constrained(std::make_pair(fh, i)))
-          face_queue.push(fh->neighbor(i));
-      }
-    }
+    std::unordered_map<Face_handle, bool> in_domain_map;
+    boost::associative_property_map< std::unordered_map<Face_handle, bool> > in_domain(in_domain_map);
+    CGAL::mark_domain_in_triangulation<CDT>(*cdt, in_domain);
+
+    for (const Face_handle& fh : cdt->all_face_handles())
+      fh->info().is_external = !get(in_domain, fh);
 
     return true;
   }
