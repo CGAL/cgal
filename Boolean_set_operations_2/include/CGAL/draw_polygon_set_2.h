d97ff7beb6cb437181c0297e54559f1f669ff831
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-12-04 18:25:56 +0100

    remove duplicate doc

--- Boolean_set_operations_2/include/CGAL/draw_polygon_set_2.h
+++ Boolean_set_operations_2/include/CGAL/draw_polygon_set_2.h
@@ -20,28 +20,6 @@
 #include <CGAL/Qt/Basic_viewer.h>
 #include <CGAL/Graphics_scene.h>
 #include <CGAL/Graphics_scene_options.h>
-
-#ifdef DOXYGEN_RUNNING
-namespace CGAL {
-
-/*!
- * \ingroup PkgDrawPolygonSet2
- *
- * opens a new window and draws `aps`, an instance of the `CGAL::Polygon_set_2`
- * class. A call to this function is blocking, that is the program continues as
- * soon as the user closes the window. This function requires `CGAL_Qt6`, and is
- * only available if the macro `CGAL_USE_BASIC_VIEWER` is defined.  Linking with
- * the cmake target `CGAL::CGAL_Basic_viewer` will link with `CGAL_Qt6` and add
- * the definition `CGAL_USE_BASIC_VIEWER`.
- * \tparam PS an instance of the `CGAL::Polygon_set_2` class.
- * \param aps the polygon set to draw.
- */
-template<class PS>
-void draw(const PS& aps);
-
-} /* namespace CGAL */
-#endif
-
 #include <CGAL/Polygon_set_2.h>
 
 namespace CGAL {
