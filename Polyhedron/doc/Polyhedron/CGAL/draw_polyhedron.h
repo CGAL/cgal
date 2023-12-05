269f5829c237b338d320bcda87d78c0c01e99fc0
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-12-05 13:51:45 +0100

    try to move advanced

--- Polyhedron/doc/Polyhedron/CGAL/draw_polyhedron.h
+++ Polyhedron/doc/Polyhedron/CGAL/draw_polyhedron.h
@@ -8,6 +8,12 @@ opens a new window and draws a polyhedron. Parameters of the drawing are taken f
 A call to this function blocks the execution of the program until the drawing window is closed. This function requires `CGAL_Qt6`, and is only available if the macro `CGAL_USE_BASIC_VIEWER` is defined.
 Linking with the cmake target `CGAL::CGAL_Basic_viewer` will link with `CGAL_Qt6` and add the definition `CGAL_USE_BASIC_VIEWER`.
 
+\tparam P which must be an instanciation of a `CGAL::Polyhedron_3<...>`.
+\tparam GSOptions a model of `GraphicsSceneOptions` concept.
+
+\param p the polyhedron to draw.
+\param gso the graphics scene options parameter.
+
 \cgalAdvancedBegin
 The real declaration of this function template is:
 
@@ -18,11 +24,6 @@ The real declaration of this function template is:
 </code>
 \cgalAdvancedEnd
 
-\tparam P which must be an instanciation of a `CGAL::Polyhedron_3<...>`.
-\tparam GSOptions a model of `GraphicsSceneOptions` concept.
-
-\param p the polyhedron to draw.
-\param gso the graphics scene options parameter.
 */
 template<class P, class GSOptions>
 void draw(const P& p, const GSOptions& gso);
@@ -40,6 +41,13 @@ A shortcut to `CGAL::draw(p, Graphics_scene_options{})`.
 
 adds the vertices, edges and faces of `p` into the given graphic scene `gs`. Parameters of the cells are taken from the optional graphics scene options parameter `gso` . Note that `gs` is not cleared before being filled (to enable to draw several data structures in the same basic viewer).
 
+\tparam P which must be an instanciation of a `CGAL::Polyhedron_3<...>`.
+\tparam GSOptions a model of `GraphicsSceneOptions` concept.
+
+\param p the polyhedron to draw.
+\param gs the graphic scene to fill.
+\param gso the graphics scene options parameter.
+
 \cgalAdvancedBegin
 The real declaration of this function template is:
 
@@ -49,13 +57,6 @@ The real declaration of this function template is:
  void CGAL::add_to_graphics_scene(const CGAL::Polyhedron_3<PT, PI, HDS, Alloc>& p, CGAL::Graphics_scene& gs, const GSOptions& gso);
 </code>
 \cgalAdvancedEnd
-
-\tparam P which must be an instanciation of a `CGAL::Polyhedron_3<...>`.
-\tparam GSOptions a model of `GraphicsSceneOptions` concept.
-
-\param p the polyhedron to draw.
-\param gs the graphic scene to fill.
-\param gso the graphics scene options parameter.
 */
 template<class P, class GSOptions>
 void add_to_graphics_scene(const P& p,
