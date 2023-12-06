7037df170ea7b9ccb61dbbd409f96f00684f8f42
Laurent Rineau <laurent.rineau@cgal.org>
2023-12-06 14:58:08 +0100

    backup this script change

--- Triangulation_3/test/Triangulation_3/select_data_files_from_Thingi10K_using_json.sh
+++ Triangulation_3/test/Triangulation_3/select_data_files_from_Thingi10K_using_json.sh
@@ -4,7 +4,7 @@ NUM_MAX=${1:-10000}
 
 
 IDS=$(jq -c "[input_filename, .num_vertices < $NUM_MAX and .solid == 1]" json/* | awk -F'[/.,]' '/,true/ { print $2 }')
-pushd raw_meshes >/dev/null
+pushd solid-max_10k_vertices > /dev/null
 for i in $IDS; do
-    echo $i.*
+    ln -s ../raw_meshes/$i.* ./
 done
