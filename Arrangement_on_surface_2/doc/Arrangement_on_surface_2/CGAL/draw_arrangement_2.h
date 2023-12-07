a3b69241220cc8c1705e9500956273ac0255f4ad
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-12-07 17:35:48 +0100

    doc draw arrangement

--- Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/draw_arrangement_2.h
+++ Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/draw_arrangement_2.h
@@ -24,33 +24,30 @@
 namespace CGAL {
 
 /*! \ingroup PkgArrangementOnSurface2Draw
- *
- * opens a new window and draws `arr`, an instance of the `CGAL::Arrangement_2`
- * class template. Parameters of the drawing are taken from the optional graphics scene options parameter.
+
+  opens a new window and draws `arr`, an instance of the `CGAL::Arrangement_2` class template. Parameters of the drawing are taken from the optional graphics scene options parameter.
 
 A call to this function blocks the execution of the program until the drawing window is closed. This function requires `CGAL_Qt6`, and is only available if the macro `CGAL_USE_BASIC_VIEWER` is defined.
 Linking with the cmake target `CGAL::CGAL_Basic_viewer` will link with `CGAL_Qt6` and add the definition `CGAL_USE_BASIC_VIEWER`.
 
- *
- * \tparam GeometryTraits_2 a geometry traits type, a model of a 2D arrangement
- * traits concept. At this point it must be an instance of either
- * `CGAL::Arr_segment_traits_2` or `CGAL::Arr_conic_traits_2`.
- *
- * \tparam Dcel the \dcel type, a model of the `ArrangementDcel` concept.
- *
- * \param arr the 2D arrangement to draw.
- * \param title the window title.
- *
- * \sa `ArrangementDcel`
- * \sa `ArrangementTraits_2`
- */
+
+\tparam GeometryTraits_2 a geometry traits type, a model of a 2D arrangement traits concept. At this point it must be an instance of either `CGAL::Arr_segment_traits_2` or `CGAL::Arr_conic_traits_2`.
+\tparam Dcel the \dcel type, a model of the `ArrangementDcel` concept.
+\tparam GSOptions a model of `GraphicsSceneOptions` concept.
+
+\param arr the 2D arrangement to draw.
+\param gso the graphics scene options parameter.
+
+\sa `ArrangementDcel`
+\sa `ArrangementTraits_2`
+*/
 template <typename GeometryTraits_2, typename Dcel, typename GSOptions>
 void draw(const Arrangement_2<GeometryTraits_2, Dcel>& arr, const GSOptions& gso);
 
 /*! \ingroup PkgArrangementOnSurface2Draw
 
   A shortcut to `CGAL::draw(arr, Graphics_scene_options{})`.
- */
+*/
 template <typename GeometryTraits_2, typename Dcel>
 void draw(const Arrangement_2<GeometryTraits_2, Dcel>& arr);
 
