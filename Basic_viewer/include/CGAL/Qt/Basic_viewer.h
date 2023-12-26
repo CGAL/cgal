6af548095ad3ea1214ab653eb7c6e76a811a292f
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-12-26 10:07:52 +0100

    add missing include and function when basic viewer is disable

--- Basic_viewer/include/CGAL/Qt/Basic_viewer.h
+++ Basic_viewer/include/CGAL/Qt/Basic_viewer.h
@@ -1644,6 +1644,13 @@ namespace CGAL
     std::cerr<<"Impossible to draw, CGAL_USE_BASIC_VIEWER is not defined."<<std::endl;
   }
 
+  inline
+  void draw_graphics_scene(const Graphics_scene&,
+                           const char *="CGAL Basic Viewer")
+  {
+    std::cerr<<"Impossible to draw, CGAL_USE_BASIC_VIEWER is not defined."<<std::endl;
+  }
+
 } // End namespace CGAL
 
 #endif // CGAL_USE_BASIC_VIEWER
