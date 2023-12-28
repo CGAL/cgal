48562c9c338229c09899e300d2ff3fb56c5f8dbd
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2023-12-28 09:01:02 +0100

    do not use strncpy

--- Basic_viewer/include/CGAL/Qt/Basic_viewer.h
+++ Basic_viewer/include/CGAL/Qt/Basic_viewer.h
@@ -14,8 +14,6 @@
 #ifndef CGAL_BASIC_VIEWER_H
 #define CGAL_BASIC_VIEWER_H
 
-#define _CRT_SECURE_NO_WARNINGS
-
 // TODO #include <CGAL/license/GraphicsView.h>
 #include <cstring>
 #include <iostream>
@@ -1553,7 +1551,7 @@ public:
     m_argc(1)
   {
     m_argv[0]=new char[strlen(title)+1];
-    strncpy(m_argv[0], title, strlen(title));
+    memcpy(m_argv[0], title, strlen(title)+1);
     m_argv[1]=nullptr;
 
 #if defined(CGAL_TEST_SUITE)
@@ -1656,7 +1654,5 @@ namespace CGAL
 } // End namespace CGAL
 
 #endif // CGAL_USE_BASIC_VIEWER
-
-#undef _CRT_SECURE_NO_WARNINGS
 
 #endif // CGAL_BASIC_VIEWER_H
