8242a407b2427cb433acb69f414d476a62d4e059
Sébastien Loriot <sebastien.loriot@cgal.org>
2024-01-04 16:34:06 +0100

    remove useless anchor

--- Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/Arr_observer.h
+++ Arrangement_on_surface_2/doc/Arrangement_on_surface_2/CGAL/Arr_observer.h
@@ -2,8 +2,6 @@ namespace CGAL {
 
 /*! \ingroup PkgArrangementOnSurface2Ref
  *
- * \anchor arr_refarr_obs
- *
  * `Arr_observer<Arrangement_2>` is an alias for
  * Aos_observer<Arrangement_on_surface_2>`,
  * where `Arrangement_2` derives from `Arrangement_on_surface_2` and the latter
