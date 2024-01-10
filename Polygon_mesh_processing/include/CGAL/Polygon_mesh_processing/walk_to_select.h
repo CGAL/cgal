2ac824f0971d2f74e8456312a303a25ec10440f2
Sébastien Loriot <sebastien.loriot@cgal.org>
2024-01-10 11:55:43 +0100

    fix headers

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/walk_to_select.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/walk_to_select.h
@@ -1,22 +1,13 @@
-// Copyright (c) 2014-2019  GeometryFactory (France). All rights reserved.
+// Copyright (c) 2014 GeometryFactory (France).
+// All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org).
-// You can redistribute it and/or modify it under the terms of the GNU
-// General Public License as published by the Free Software Foundation,
-// either version 3 of the License, or (at your option) any later version.
-//
-// Licensees holding a valid commercial license may use this file in
-// accordance with the commercial license agreement provided with the software.
-//
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 //
 // $URL$
 // $Id$
-// SPDX-License-Identifier: GPL-3.0+
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 //
-// Author(s)     : Maxime Gimeno
-//
+// Author(s)     : Maxime Gimero and Sébastien Loriot
 
 #ifndef CGAL_POLYGON_MESH_PROCESSING_WALK_TO_SELECT_H
 #define CGAL_POLYGON_MESH_PROCESSING_WALK_TO_SELECT_H
