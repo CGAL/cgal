cdea8cf8bb7f9b63b6377a7d0a7085777aa74bca
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-01-18 12:39:46 +0100

    Update Orthtree/include/CGAL/Orthtree_traits_face_graph.h

--- Orthtree/include/CGAL/Orthtree_traits_face_graph.h
+++ Orthtree/include/CGAL/Orthtree_traits_face_graph.h
@@ -125,7 +125,7 @@ struct Orthtree_traits_face_graph : public Orthtree_traits_base_for_dimension<
   /// @}
 
   /// Recommended split predicate to pass to `Orthtree::refine()` function so
-  /// that the octree is refined until a node is either empty or has an extend
+  /// that the octree is refined until a node is either empty or has an extent
   /// that would be smaller after split than the value provided to the constructor.
   class Split_predicate_node_min_extent {
 
