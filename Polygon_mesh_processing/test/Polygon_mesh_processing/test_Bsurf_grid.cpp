5bff11f8723d1061a7d9f825b41cdf03a8df889c
Sébastien Loriot <sebastien.loriot@cgal.org>
2024-02-01 10:18:46 +0100

    add simplification function showing that we need exact predicates

--- Polygon_mesh_processing/test/Polygon_mesh_processing/test_Bsurf_grid.cpp
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/test_Bsurf_grid.cpp
@@ -15,15 +15,82 @@ typedef PMP::Face_location<Mesh, double>                      Face_location;
 typedef PMP::Edge_location<Mesh, double>                      Edge_location;
 
 
+//TODO: move to header and add doc
+template <class TriangleMesh, class FT>
+void simplify_sequence(std::vector<PMP::Edge_location<TriangleMesh, FT>>& edge_locations, const TriangleMesh& tm)
+{
+  std::cout << "  simplify_sequence:\n";
+  std::cout << "  input.size() = " << edge_locations.size() << "\n";
+  typedef boost::graph_traits<TriangleMesh> BGT;
+  typedef typename BGT::vertex_descriptor vertex_descriptor;
+
+  auto print_seq = [&]()
+  {
+    std::cout << "  ";
+    typedef typename BGT::halfedge_descriptor halfedge_descriptor;
+    for (auto el : edge_locations)
+    {
+      auto var = PMP::get_descriptor_from_location(el, tm);
+      if (const vertex_descriptor* vd_ptr = std::get_if<vertex_descriptor>(&var))
+        std::cout << "  " << *vd_ptr;
+      else
+      {
+        if (const halfedge_descriptor* ed_ptr = std::get_if<halfedge_descriptor>(&var))
+          std::cout << "  " << *ed_ptr;
+        else
+          std::cout << "  ERROR";
+      }
+    }
+    std::cout << "\n";
+  };
+  print_seq();
+
+
+  std::vector<PMP::Edge_location<TriangleMesh, FT>> filtered_locations;
+  std::size_t end=edge_locations.size();
+  filtered_locations.reserve(edge_locations.size());
+  for(std::size_t curr=0; curr<end;)
+  {
+    filtered_locations.push_back(edge_locations[curr]);
+    auto var = PMP::get_descriptor_from_location(edge_locations[curr], tm);
+    if (const vertex_descriptor* vd_ptr = std::get_if<vertex_descriptor>(&var))
+    {
+      do{
+        vertex_descriptor vd = *vd_ptr;
+        ++curr;
+        if (curr!=end)
+        {
+          var = PMP::get_descriptor_from_location(edge_locations[curr], tm);
+          vd_ptr = std::get_if<vertex_descriptor>(&var);
+          if (vd_ptr==nullptr || *vd_ptr!=vd)
+            break;
+        }
+        else
+          break;
+      }
+      while(true);
+    }
+    else
+      ++curr;
+  }
+
+  if (filtered_locations.size()!=edge_locations.size())
+    filtered_locations.swap(edge_locations);
+
+  std::cout << "  output.size() = " << edge_locations.size() << "\n";
+  print_seq();
+}
+
 void test_vertex_pair(unsigned int vid1, unsigned int vid2, const Mesh& mesh)
 {
   Mesh::Vertex_index v1(vid1), v2(vid2);
   std::cout << "running " << v1 << " " << v2 << "\n";
   static int rid=-1;
   std::string fname="grid_shortest_path"+std::to_string(++rid)+".polylines.txt";
-  std::cout << "writing in " << fname << "\n";
+  std::cout << "  writing in " << fname << "\n";
 
   std::ofstream out(fname);
+  out << std::setprecision(17);
 
   Mesh::Face_index f1=face(halfedge(v1, mesh), mesh), f2 = face(halfedge(v2, mesh), mesh);
 
@@ -52,6 +119,8 @@ void test_vertex_pair(unsigned int vid1, unsigned int vid2, const Mesh& mesh)
   PMP::locally_shortest_path<double>(src, tgt, mesh, edge_locations);
   assert(get<Mesh::Vertex_index>(PMP::get_descriptor_from_location(src,mesh))==v1);
   assert(get<Mesh::Vertex_index>(PMP::get_descriptor_from_location(tgt,mesh))==v2);
+
+  simplify_sequence(edge_locations, mesh);
 
   out << edge_locations.size()+2;
   out << " " << PMP::construct_point(src, mesh);
