84362a2b1eb98898b99b56a422467002dd5a07fb
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-03-04 15:42:49 +0000

    Add Construct_projected_point_2::operator()(Segment_2,Point_2)

---
+++ AABB_tree/examples/AABB_tree/AABB_segment_2_example.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <list>
+
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/AABB_tree.h>
+#include <CGAL/AABB_traits_2.h>
+#include <CGAL/AABB_segment_primitive_2.h>
+
+
+typedef CGAL::Simple_cartesian<double> K;
+
+typedef K::FT FT;
+typedef K::Segment_2 Segment;
+typedef K::Point_2 Point;
+
+typedef std::list<Segment>::iterator Iterator;
+typedef CGAL::AABB_segment_primitive_2<K, Iterator> Primitive;
+typedef CGAL::AABB_traits_2<K, Primitive> Traits;
+typedef CGAL::AABB_tree<Traits> Tree;
+
+
+int main()
+{
+        Point a(0.0, 0.0);
+        Point b(2.0, 1.0);
+        Point c(3.0, 4.0);
+        Point d(1.0, 6.0);
+        Point e(-1.0, 3.0);
+
+        std::list<Segment> segments;
+        segments.push_back(Segment(a, b));
+        segments.push_back(Segment(b, c));
+        segments.push_back(Segment(c, d));
+        segments.push_back(Segment(d, e));
+        segments.push_back(Segment(e, a));
+
+        //   // constructs the AABB tree and the internal search tree for
+        //   // efficient distance computations.
+        Tree tree(segments.begin(), segments.end());
+      //  tree.accelerate_distance_queries();
+
+        //   // counts #intersections with a segment query
+        Segment segment_query(Point(1.0, 0.0), Point(0.0, 7.0));
+        std::cout << tree.number_of_intersected_primitives(segment_query)
+                << " intersections(s) with segment" << std::endl;
+
+
+        // computes the closest point from a point query
+        Point point_query(1.5, 3.0);
+        Point closest = tree.closest_point(point_query);
+
+        //std::cerr << "closest point is: " << closest << std::endl;
+        return EXIT_SUCCESS;
+}
--- Cartesian_kernel/include/CGAL/Cartesian/function_objects.h
+++ Cartesian_kernel/include/CGAL/Cartesian/function_objects.h
@@ -3278,6 +3278,7 @@ namespace CartesianKernelFunctors {
   {
     typedef typename K::Point_2    Point_2;
     typedef typename K::Line_2     Line_2;
+    typedef typename K::Segment_2  Segment_2;
   public:
     typedef Point_2                result_type;
 
@@ -3289,6 +3290,15 @@ namespace CartesianKernelFunctors {
       line_project_pointC2(l.a(), l.b(), l.c(), p.x(), p.y(), x, y);
       return construct_point_2(x, y);
     }
+
+    Point_2
+    operator()( const Segment_2& s, const Point_2& p ) const
+    {
+      assert(false);
+      return p;
+      // CommonKernelFunctors::Construct_projected_point_3<K>()(s,p,K());
+    }
+
   };
 
 
