fcbf327b16a459735bfab7811c6b4a79c1ad207d
Sébastien Loriot <sebastien.loriot@cgal.org>
2024-03-13 17:12:24 +0100

    workaround absence of <filesystem>

--- Polygon_repair/test/Polygon_repair/repair_polygon_2_test.cpp
+++ Polygon_repair/test/Polygon_repair/repair_polygon_2_test.cpp
@@ -3,6 +3,19 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
 #include <iostream>
+
+// work around for old compilers (Apple clang < 11 for example)
+#define HAS_FILESYSTEM 1
+#if (__has_include)
+#if !__has_include(<filesystem>)
+#undef HAS_FILESYSTEM
+#define HAS_FILESYSTEM 0
+#endif
+#endif
+
+
+#if HAS_FILESYSTEM
+
 #include <fstream>
 #include <sstream>
 #include <filesystem>
@@ -80,3 +93,13 @@ int main() {
 
   return 0;
 }
+
+#else
+
+int main()
+{
+  std::cout << "Warning: filesystem feature is not present on the system, nothing will be tested\n";
+  return 0;
+}
+
+#endif
