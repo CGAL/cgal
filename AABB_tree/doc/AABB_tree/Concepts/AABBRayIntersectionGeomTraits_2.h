6e4092a37e4c9fe3d773dbd6da017064a8201076
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-03-13 14:32:47 +0100

    replace _3 with _2

--- AABB_tree/doc/AABB_tree/Concepts/AABBRayIntersectionGeomTraits_2.h
+++ AABB_tree/doc/AABB_tree/Concepts/AABBRayIntersectionGeomTraits_2.h
@@ -1,10 +1,11 @@
+3
 /*!
 \ingroup PkgAABBTreeConcepts
 \cgalConcept
 
-The concept `AABBRayIntersectionGeomTraits_3` is a refinement of the
-concept `AABBGeomTraits`. In addition to the types required by
-`AABBGeomTraits` it also requires types and functors necessary to
+The concept `AABBRayIntersectionGeomTraits_2` is a refinement of the
+concept `AABBGeomTraits_2`. In addition to the types required by
+`AABBGeomTraits_2` it also requires types and functors necessary to
 define the Intersection_distance functor.
 
 \cgalRefines{AABBGeomTraits}
@@ -13,54 +14,54 @@ define the Intersection_distance functor.
 \cgalHasModelsBare{All models of the concept `Kernel`}
 \cgalHasModelsEnd
 
-\sa `CGAL::AABB_traits<AABBGeomTraits,AABBPrimitive>`
+\sa `CGAL::AABB_traits_<AABBGeomTraits_2,AABBPrimitive>`
 \sa `CGAL::AABB_tree<AABBTraits>`
 \sa `AABBPrimitive`
 
 */
-class AABBRayIntersectionGeomTraits_3 {
+class AABBRayIntersectionGeomTraits_2 {
 public:
   /*!
     Type of a 3D ray.
   */
-  typedef unspecified_type Ray_3;
+  typedef unspecified_type Ray_2;
 
   /*!
     Type of a 3D vector.
   */
-  typedef unspecified_type Vector_3;
+  typedef unspecified_type Vector_2;
 
   /*!
     A functor object to construct the source point of a ray. Provides the operator:
-    `Point_3 operator()(const Ray_3&);`
+    `Point_2 operator()(const Ray_2&);`
    */
-  typedef unspecified_type Construct_source_3;
+  typedef unspecified_type Construct_source_2;
 
   /*!
    */
-  Construct_source_3 construct_source_3_object();
+  Construct_source_2 construct_source_2_object();
 
   /*!
     A model of `CartesianConstIterator3`.
    */
-  typedef unspecified_type Cartesian_const_iterator_3;
+  typedef unspecified_type Cartesian_const_iterator_2;
 
   /*!
     A model of `ConstructCartesianConstIterator3`.
    */
-  typedef unspecified_type  Construct_cartesian_const_iterator_3;
+  typedef unspecified_type  Construct_cartesian_const_iterator_2;
 
   /*!
    */
-  Construct_source_3 construct_cartesian_const_iterator_3_object();
+  Construct_source_2 construct_cartesian_const_iterator_2_object();
 
   /*!
     A functor object to construct a vector giving the direction of a ray. Provides the operator:
-    `Vector_3 operator()(const Ray_3&);`
+    `Vector_2 operator()(const Ray_2&);`
    */
-  typedef unspecified_type Construct_vector_3;
+  typedef unspecified_type Construct_vector_2;
 
   /*!
    */
-  Construct_source_3 construct_vector_3_object();
+  Construct_source_2 construct_vector_2_object();
 };
