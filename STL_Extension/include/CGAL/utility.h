488668b54b631e7da6531b582311cc6f8bcb8fd7
Laurent Rineau <laurent.rineau@cgal.org>
2024-04-10 10:24:11 +0200

    remove less_cpp14

--- STL_Extension/include/CGAL/utility.h
+++ STL_Extension/include/CGAL/utility.h
@@ -295,22 +295,8 @@ struct Default_using_type
   };
 };
 
-template <class T_ = Default_using_type>
-struct less_cpp14
-{
-  template <class T1, class T2>
-  bool operator() (T1&& t1, T2&& t2) const
-  {
-    typedef typename Default_using_type::Get<
-      T_,
-      typename std::common_type<typename std::decay<T1>::type,
-                                typename std::decay<T2>::type> >::type T;
-    return std::less<T>()(t1,t2);
-  }
-};
-
 template <class T = Default_using_type,
-          class Compare = less_cpp14<T>,
+          class Compare = std::less<>,
           class T1, class T2,
           class A = typename Default_using_type::Get<T,
             typename std::common_type<
