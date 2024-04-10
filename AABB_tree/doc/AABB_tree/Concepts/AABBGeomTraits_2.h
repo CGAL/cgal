4015b2b5c593e5bc6200c8d8c5598e4f2b3cd9c2
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-04-10 11:52:33 +0100

    fixes after review

--- AABB_tree/doc/AABB_tree/Concepts/AABBGeomTraits_2.h
+++ AABB_tree/doc/AABB_tree/Concepts/AABBGeomTraits_2.h
@@ -78,7 +78,7 @@ A functor object to compare the distance of two points wrt a third one. Provides
 
 `CGAL::Comparison_result operator()(const Point_2& p1, const Point_2& p2, const Point_2& p3)`,
 
-which compares the distance between `p1` and `p2`, and between `p2` and `p3`.
+which compares the distance between `p1` and `p2`, to the squared distance between `p2` and `p3`.
 */
 typedef unspecified_type Compare_distance_2;
 
