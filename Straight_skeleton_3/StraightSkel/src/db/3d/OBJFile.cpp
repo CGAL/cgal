87f2c6bbd850b0d49b55160f24c2700d5cdf1f95
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2024-04-25 11:55:31 +0200

    Triangulate faces before writing them

--- Straight_skeleton_3/StraightSkel/src/db/3d/OBJFile.cpp
+++ Straight_skeleton_3/StraightSkel/src/db/3d/OBJFile.cpp
@@ -25,6 +25,12 @@
 #include "data/3d/KernelFactory.h"
 #include "util/StringFuncs.h"
 #include "util/Configuration.h"
+
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <CGAL/Projection_traits_3.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <CGAL/mark_domain_in_triangulation.h>
+
 #include <cmath>
 #include <exception>
 #include <fstream>
@@ -188,9 +194,9 @@ bool OBJFile::save(const std::string& filename, PolyhedronSPtr polyhedron) {
             VertexSPtr vertex = *it_v++;
             vertex_id++;
             vertex->setID(vertex_id);
-            ofs << "v " << vertex->getX() << " "
-                        << vertex->getY() << " "
-                        << vertex->getZ() << "\n";
+            ofs << "v " << CGAL::to_double(vertex->getX()) << " "
+                        << CGAL::to_double(vertex->getY()) << " "
+                        << CGAL::to_double(vertex->getZ()) << "\n";
         }
         unsigned int facet_id = 0;
         std::list<FacetSPtr>::iterator it_f = polyhedron->facets().begin();
@@ -199,27 +205,105 @@ bool OBJFile::save(const std::string& filename, PolyhedronSPtr polyhedron) {
             facet->makeFirstConvex();
             facet_id++;
             facet->setID(facet_id);
-            ofs << "f ";
-            unsigned int num_edges = 0;
-            EdgeSPtr first = EdgeSPtr();
-            EdgeSPtr edge = facet->edges().front();
-            while (first != edge) {
-                if (!first) {
-                    first = edge;
-                } else {
-                    ofs << " ";
-                }
-                VertexSPtr vertex = edge->src(facet);
-                ofs << vertex->getID();
-                edge = edge->next(facet);
-                num_edges++;
-            }
-            if (num_edges != facet->edges().size()) {
-                DEBUG_VAL("Warning: Facet does not consist of connected edges only.");
-                DEBUG_VAL("Warning: It is impossible for an obj file to store holes inside a facet.");
+
+            using Itag = CGAL::No_constraint_intersection_requiring_constructions_tag;
+            using PK = CGAL::Projection_traits_3<CGAL::K>;
+            using PVbb = CGAL::Triangulation_vertex_base_with_info_2<VertexSPtr, PK>;
+            using PVb = CGAL::Triangulation_vertex_base_2<PK, PVbb>;
+            using PFb = CGAL::Constrained_triangulation_face_base_2<PK>;
+            using PTDS = CGAL::Triangulation_data_structure_2<PVb,PFb>;
+            using PCDT = CGAL::Constrained_Delaunay_triangulation_2<PK, PTDS, Itag>;
+            using PCDT_VH = PCDT::Vertex_handle;
+            using PCDT_FH = PCDT::Face_handle;
+
+            auto eit = facet->edges().begin();
+            const VertexSPtr p0 = (*eit++)->src(facet);
+            const VertexSPtr p1 = (*eit++)->src(facet);
+            const VertexSPtr p2 = (*eit)->src(facet);
+
+            const auto n = CGAL::cross_product(*(p1->getPoint()) - *(p0->getPoint()),
+                                               *(p2->getPoint()) - *(p0->getPoint()));
+            PK traits(n);
+            PCDT pcdt(traits);
+
+            std::map<VertexSPtr, PCDT_VH> face_vhs;
+
+            auto ins = [&pcdt, &face_vhs] (const VertexSPtr v) -> PCDT_VH
+            {
+              auto res = face_vhs.emplace(v, PCDT_VH());
+              if(res.second) // first time seeing this point
+              {
+                PCDT_VH vh = pcdt.insert(*(v->getPoint()));
+                vh->info() = v;
+                res.first->second = vh;
+              }
+              return res.first->second;
+            };
+
+            bool use_default = false;
+
+            for(auto e : facet->edges_)
+            {
+              PCDT_VH vh0 = ins(e->src(facet));
+              PCDT_VH vh1 = ins(e->dst(facet));
+
+              try
+              {
+                pcdt.insert_constraint(vh0, vh1);
+              }
+              catch(const typename PCDT::Intersection_of_constraints_exception&)
+              {
+                std::cerr << "Warning: Failed to triangulate face" << std::endl;
                 DEBUG_VAR(facet->toString());
+                use_default = true;
+              }
             }
-            ofs << "\n";
+
+            if(!use_default)
+            {
+              std::unordered_map<PCDT_FH, bool> in_domain_map;
+              boost::associative_property_map< std::unordered_map<PCDT_FH, bool> > in_domain(in_domain_map);
+
+              CGAL::mark_domain_in_triangulation(pcdt, in_domain);
+
+              unsigned int num_f = 0;
+              for(auto fh : pcdt.finite_face_handles())
+              {
+                if(!get(in_domain, fh))
+                  continue;
+
+                ofs << "f " << fh->vertex(0)->info()->getID() << " "
+                            << fh->vertex(1)->info()->getID() << " "
+                            << fh->vertex(2)->info()->getID() << "\n";
+
+                ++num_f;
+              }
+              // std::cout << "face split into " << num_f << " faces" << std::endl;
+            }
+            else
+            {
+              ofs << "f ";
+              unsigned int num_edges = 0;
+              EdgeSPtr first = EdgeSPtr();
+              EdgeSPtr edge = facet->edges().front();
+              while (first != edge) {
+                  if (!first) {
+                      first = edge;
+                  } else {
+                      ofs << " ";
+                  }
+                  VertexSPtr vertex = edge->src(facet);
+                  ofs << vertex->getID();
+                  edge = edge->next(facet);
+                  num_edges++;
+              }
+              if (num_edges != facet->edges().size()) {
+                  DEBUG_VAL("Warning: Facet does not consist of connected edges only.");
+                  DEBUG_VAL("Warning: It is impossible for an obj file to store holes inside a facet.");
+                  DEBUG_VAR(facet->toString());
+              }
+              ofs << "\n";
+            }
         }
         result = (polyhedron->vertices().size() == vertex_id &&
                   polyhedron->facets().size() == facet_id);
