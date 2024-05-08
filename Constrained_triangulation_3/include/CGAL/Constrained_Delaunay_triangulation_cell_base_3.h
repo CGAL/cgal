11ce70c26a6443c5f2269c6ed710c4571fde0e75
Laurent Rineau <laurent.rineau@cgal.org>
2024-05-08 16:04:34 +0200

    wip on the compatibility with Tet remeshing

--- Constrained_triangulation_3/include/CGAL/Constrained_Delaunay_triangulation_cell_base_3.h
+++ Constrained_triangulation_3/include/CGAL/Constrained_Delaunay_triangulation_cell_base_3.h
@@ -26,30 +26,25 @@
 namespace CGAL {
 
 /**
- * @brief The Constrained_Delaunay_triangulation_cell_base_3 class is a cell base class for the
- *        Constrained Delaunay Triangulation in 3D.
+ * @brief Cell base class for the 3D Constrained Delaunay Triangulation.
  *
  * This class is derived from the `Triangulation_cell_base_3` class and provides additional functionality
  * required by `Constrained_Delaunay_triangulation_3`.
  *
- * @tparam Gt The geometric traits class, model of `DelaunayTriangulationTraits_3`.
- *         It must be the same as the geometric traits class of the triangulation.
- * @tparam Cb The base class for the cell. It must be a model of `TriangulationCellBase_3`.
+ * @tparam Gt The geometric traits class, which must be a model of `ConstrainedDelaunayTriangulationTraits_3`.
+ *         It should be the same as the geometric traits class of the triangulation.
+ * @tparam Cb The base class for the cell, which must be a model of `TriangulationCellBase_3`.
  *
- * @cgalModels{ConstrainedDelaunayTriangulationCellBase_3}
+ * @cgalModels{ConstrainedDelaunayTriangulationCellBase_3, SimplicialMeshCellBase_3, RemeshingCellBase_3}
+ *
+ * \note This cell base class also models the `SimplicialMeshCellBase_3` and `RemeshingCellBase_3` concepts, allowing the use of functionality from \ref Chapter_Tetrahedral_Remeshing "Tetrahedral Remeshing" and \ref Chapter_3D_Simplicial_Mesh_Data_Structure "3D Simplicial Mesh Data Structures", if the corresponding vertex base also models the right concepts.
+ * \todo After discussion with Jane. The note above is wrong. There should be a second pair of Vb/Cb, designed to model the concepts of simplicial mesh and remeshing.
  *
  * \sa `CGAL::Constrained_Delaunay_triangulation_vertex_base_3`
  */
 template < typename Gt, typename Cb = Triangulation_cell_base_3<Gt> >
 class Constrained_Delaunay_triangulation_cell_base_3 : public Cb {
 public:
-        // Add your custom member functions and variables here
-
-protected:
-        // Add your protected member functions and variables here
-
-private:
-        // Add your private member functions and variables here
 };
 
 } // namespace CGAL
