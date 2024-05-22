9c2224620ddf9b619b78f7b164c5ca0c8788db0e
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-05-22 14:27:43 +0100

    Surface_mesh:  Add exact_num_faces(const SM&)

--- Surface_mesh/include/CGAL/Surface_mesh/Surface_mesh.h
+++ Surface_mesh/include/CGAL/Surface_mesh/Surface_mesh.h
@@ -26,6 +26,7 @@
 #include <CGAL/boost/graph/Euler_operations.h>
 #include <CGAL/boost/graph/iterator.h>
 #include <CGAL/boost/graph/named_params_helper.h>
+#include <CGAL/boost/graph/internal/helpers.h>
 #include <CGAL/Named_function_parameters.h>
 #include <CGAL/circulator.h>
 #include <CGAL/Handle_hash_function.h>
@@ -2802,6 +2803,16 @@ namespace internal{
   }
 }
 
+namespace internal {
+
+template <typename P>
+std::size_t
+exact_num_faces(const CGAL::Surface_mesh<P>& sm)
+{
+ return sm.number_of_faces();
+}
+
+} // namespace internal
 } // namespace CGAL
 
 #ifndef DOXYGEN_RUNNING
