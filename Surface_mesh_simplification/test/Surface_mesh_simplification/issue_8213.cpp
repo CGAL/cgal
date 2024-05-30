b54a2eab1a056451a3ae6545d2824163623822e1
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-05-30 14:59:22 +0100

    Add a filter

--- Surface_mesh_simplification/test/Surface_mesh_simplification/issue_8213.cpp
+++ Surface_mesh_simplification/test/Surface_mesh_simplification/issue_8213.cpp
@@ -15,6 +15,7 @@ void Surface_simplification_external_trace(const std::string& s)
 
 #include <CGAL/Surface_mesh_simplification/edge_collapse.h>
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Bounded_normal_change_filter.h>
+#include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Bounding_box_filter.h>
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/Face_count_stop_predicate.h>
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/LindstromTurk_cost.h>
 #include <CGAL/Surface_mesh_simplification/Policies/Edge_collapse/LindstromTurk_placement.h>
@@ -52,12 +53,16 @@ int main(int argc, char** argv)
   std::cout << "Input mesh has " << num_vertices(sm) << " vertices" << std::endl;
   std::cout << "Input mesh has " << num_faces(sm) << " faces" << std::endl;
 
-  SMS::Face_count_stop_predicate<Surface_mesh> stop(1);
-  SMS::edge_collapse(sm, stop,
-                     CGAL::parameters::filter(SMS::Bounded_normal_change_filter<>())
-                                      .get_cost(SMS::LindstromTurk_cost<Surface_mesh>())
-                                      .get_placement(SMS::LindstromTurk_placement<Surface_mesh>()));
 
+  SMS::Face_count_stop_predicate<Surface_mesh> stop(1);
+   SMS::edge_collapse(
+    surface_mesh,
+    stop,
+    CGAL::parameters::
+       filter(SMS::Bounded_normal_change_filter<SMS::Bounding_box_filter<>>(SMS::Bounding_box_filter<>()))
+      .get_cost(SMS::LindstromTurk_cost<Surface_mesh>())
+      .get_placement(SMS::LindstromTurk_placement<Surface_mesh>())
+    );
   CGAL::IO::write_OFF(std::cout, sm, CGAL::parameters::stream_precision(17));
 
   for(auto v : vertices(sm))
