b352f28627c43eb33b7ae14eb4c85102252f70fa
Sébastien Loriot <sebastien.loriot@cgal.org>
2024-06-03 18:06:21 +0200

    deprecate Surface_mesher package

--- Surface_mesher/include/CGAL/Surface_mesher_generator.h
+++ Surface_mesher/include/CGAL/Surface_mesher_generator.h
@@ -15,6 +15,10 @@
 
 #include <CGAL/license/Surface_mesher.h>
 
+#define CGAL_DEPRECATED_HEADER "<CGAL/Surface_mesher_generator.h>"
+#define CGAL_DEPRECATED_MESSAGE_DETAILS \
+  "The 3D Mesh Generation package (see https://doc.cgal.org/latest/Mesh_3/) should be used instead."
+#include <CGAL/Installation/internal/deprecation_warning.h>
 
 #include <CGAL/Surface_mesher/Surface_mesher.h>
 #include <CGAL/Surface_mesher/Surface_mesher_manifold.h>
--- Surface_mesher/include/CGAL/make_surface_mesh.h
+++ Surface_mesher/include/CGAL/make_surface_mesh.h
@@ -14,6 +14,10 @@
 
 #include <CGAL/license/Surface_mesher.h>
 
+#define CGAL_DEPRECATED_HEADER "<CGAL/make_surface_mesh.h>"
+#define CGAL_DEPRECATED_MESSAGE_DETAILS \
+  "The 3D Mesh Generation package (see https://doc.cgal.org/latest/Mesh_3/) should be used instead."
+#include <CGAL/Installation/internal/deprecation_warning.h>
 
 #include <CGAL/Surface_mesher_generator.h>
 
