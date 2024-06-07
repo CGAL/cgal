ef44ef27eb2f409f184f5eb12bb9eb5c33d37787
Sébastien Loriot <sebastien.loriot@cgal.org>
2024-06-07 13:50:37 +0200

    use new macro

--- Barycentric_coordinates_3/doc/Barycentric_coordinates_3/Concepts/BarycentricTraits_3.h
+++ Barycentric_coordinates_3/doc/Barycentric_coordinates_3/Concepts/BarycentricTraits_3.h
@@ -7,12 +7,14 @@ namespace Barycentric_coordinates {
 
 A concept that describes the set of requirements of the template parameter
 `GeomTraits` used to parameterize all classes and functions with 3D barycentric
-coordinates from the namespace `CGAL::Barycentric_coordinates`. The concept `BarycentricTraits_3` 
+coordinates from the namespace `CGAL::Barycentric_coordinates`. The concept `BarycentricTraits_3`
 extends the concept `BarycentricTraits_2` and adds the requirements for 3D objects.
 \cgalGeneralizes `BarycentricTraits_2`
 
-\cgalHasModel
-- All models of `Kernel`
+\cgalHasModelsBegin
+\cgalHasModelsBare{All models of the \cgal concept `Kernel`}
+\cgalHasModelsEnd
+
 */
 class BarycentricTraits_3 {
 
@@ -50,7 +52,7 @@ typedef unspecified_type Vector_3;
   A construction object that must provide the function operator:
 
   `FT operator(const Point_3& p0, const Point_3& p1, const Point_3& p2, const Point_3& p3)`
-  
+
   that returns the signed volume of the tetrahedron defined by the four points `p0`, `p1`, `p2`, and `p3`.
 */
 typedef unspecified_type Compute_volume_3;
@@ -59,7 +61,7 @@ typedef unspecified_type Compute_volume_3;
   A construction object that must provide the function operator:
 
   `FT operator(const Vector_3& u, const Vector_3& v)`
-  
+
   that returns an approximation of the angle between `u` and `v`.
   The angle is given in degrees.
 */
@@ -69,7 +71,7 @@ typedef unspecified_type Compute_approximate_angle_3;
   A construction object that must provide the function operator:
 
   `FT operator(const Vector_3& v, const Vector_3& w)`
-  
+
   that returns the scalar (inner) product of the two vectors `v` and `w`.
 */
 typedef unspecified_type Compute_scalar_product_3;
@@ -78,7 +80,7 @@ typedef unspecified_type Compute_scalar_product_3;
   A construction object that must provide the function operator:
 
   `FT operator(const Vector_3& u, const Vector_3& v, const Vector_3& w)`
-  
+
   that returns the determinant of the three vectors `u`, `v` and `w`.
 */
 typedef unspecified_type Compute_determinant_3;
@@ -87,7 +89,7 @@ typedef unspecified_type Compute_determinant_3;
   A construction object that must provide the function operator:
 
   `Vector_3 operator(const Point_3& p, const Point_3& q)`
-  
+
   that returns the vector `q` - `p`.
 */
 typedef unspecified_type Construct_vector_3;
@@ -96,7 +98,7 @@ typedef unspecified_type Construct_vector_3;
   A construction object that must provide the function operator:
 
   `Vector_3 operator(const Vector_3& u, const Vector_3& v)`
-  
+
   that returns the cross product between `u` and `v`.
 */
 typedef unspecified_type Construct_cross_product_vector_3;
