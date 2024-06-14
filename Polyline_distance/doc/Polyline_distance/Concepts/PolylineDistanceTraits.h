fc176f31b6ecce0dbcf5d6f983a5de679fd73e81
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-06-14 14:57:25 +0100

    Add concept

---
+++ Polyline_distance/doc/Polyline_distance/Concepts/PolylineDistanceTraits.h
@@ -0,0 +1,36 @@
+/*!
+\ingroup PkgPolylineDistanceConcepts
+\cgalConcept
+
+The concept `PolylineDistanceTraits` defines the requirements ...
+
+
+\cgalHasModelsBegin
+\cgalHasModels{CGAL::Polyline_distance_traits_2}
+\cgalHasModels{CGAL::Polyline_distance_traits_3}
+\cgalHasModels{CGAL::Polyline_distance_traits_d}
+\cgalHasModelsEnd
+*/
+
+class PolylineDistanceTraits {
+
+    public:
+/*!  2 or 3 */
+    const int dimension;
+
+/// \name Types
+/// @{
+
+/*! The kernel type. If this type has a nested type `Has_filtered_predicates_tag`  with `value == true`,
+    it must have a nested type `Exact_kernel`, and a nested type `C2E` with an  `operator()` that converts
+    a point of `Kernel` to a point of `Exact_kernel`. Otherwise, it must have a nested type `FT` for
+    which an overload of `to_double()` exists.
+*/
+using Kernel = unspecified;
+
+/*! The point type of `Kernel` corresponding to `dimension`
+*/
+using Point = unspecified_type;
+
+/// @}
+};
