9b169010c2212b09c9eccb54a13c6964a59bae5c
Laurent Rineau <laurent.rineau@cgal.org>
2024-06-20 15:50:15 +0200

    [WIP] add an example with tet remeshing (does not work, yet)
    [skip ci]

---
+++ Constrained_triangulation_3/examples/Constrained_triangulation_3/remesh_constrained_Delaunay_triangulation_3.cpp
@@ -0,0 +1,39 @@
+#define CGAL_TETRAHEDRAL_REMESHING_VERBOSE 1
+#define CGAL_TETRAHEDRAL_REMESHING_DEBUG 1
+
+#include <CGAL/make_constrained_Delaunay_triangulation_3.h>
+
+#include <CGAL/Tetrahedral_remeshing/Remeshing_cell_base_3.h>
+#include <CGAL/Tetrahedral_remeshing/Remeshing_vertex_base_3.h>
+#include <CGAL/tetrahedral_remeshing.h>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/draw_triangulation_3.h>
+
+using K = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Vb = CGAL::Constrained_Delaunay_triangulation_vertex_base_3<K, CGAL::Tetrahedral_remeshing::Remeshing_vertex_base_3<K>>;
+using Cb = CGAL::Constrained_Delaunay_triangulation_cell_base_3<K, CGAL::Tetrahedral_remeshing::Remeshing_cell_base_3<K>>;
+using Tds = CGAL::Triangulation_data_structure_3<Vb, Cb>;
+using Tr = CGAL::Triangulation_3<K, Tds>;
+using CDt = CGAL::Constrained_Delaunay_triangulation_3<K, Tr>;
+
+int main(int argc, char* argv[])
+{
+  auto filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/mpi.off");
+  std::ifstream in(filename);
+
+  CGAL::Surface_mesh<K::Point_3> mesh;
+  if(!in || !CGAL::IO::read_OFF(in, mesh)) {
+    std::cerr << "Error: cannot read file " << filename << std::endl;
+    return EXIT_FAILURE;
+  }
+  Tr tr = CGAL::make_constrained_Delaunay_triangulation_3<Tr>(mesh);
+  std::cout << "Number of vertices in tr: "
+            << tr.number_of_vertices() << std::endl;
+  CGAL::tetrahedral_isotropic_remeshing(tr, 0.1,
+    CGAL::parameters::number_of_iterations(3));
+
+  std::cout << "Number of vertices in tr: "
+            << tr.number_of_vertices() << std::endl;
+  // CGAL::draw(cdt.triangulation());
+}
