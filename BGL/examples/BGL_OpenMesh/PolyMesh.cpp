0e25e333a1028716ccd164f125e87ce86e3aec16
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-08-20 16:20:33 +0100

    put code in header file

--- BGL/examples/BGL_OpenMesh/PolyMesh.cpp
+++ BGL/examples/BGL_OpenMesh/PolyMesh.cpp
@@ -1,98 +1,53 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
 #include <OpenMesh/Core/IO/MeshIO.hh>
-#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
 
 #include <CGAL/boost/graph/graph_traits_PolyMesh_ArrayKernelT.h>
 #include <CGAL/boost/graph/iterator.h>
-#include <CGAL/boost/graph/Euler_operations.h>
 #include <CGAL/IO/polygon_mesh_io.h>
-#include <CGAL/mesh_segmentation.h>
 #include <CGAL/property_map.h>
 #include <CGAL/Surface_mesh.h>
 #include <iostream>
 #include <fstream>
 
-namespace CGAL {
-namespace IO {
-
-template <typename SM, typename VSelectionPM, typename EFeaturePM>
-bool read_OM(std::string fname, SM& sm, VSelectionPM vspm, EFeaturePM efpm)
-{
-  typedef OpenMesh::PolyMesh_ArrayKernelT<> Mesh;
-  Mesh mesh;
-  OpenMesh::IO::Options options  = OpenMesh::IO::Options::Status;
-  bool ok = OpenMesh::IO::read_mesh(mesh, fname, options);
-  if(! ok){
-    return false;
-  }
-
-  std::map<vertex_descriptor,sm_vertex_descriptor> v2v;
-  auto v2vpmap = boost::make_assoc_property_map(v2v);
-
-  std::map<halfedge_descriptor,sm_halfedge_descriptor> h2h;
-  auto h2hpmap = boost::make_assoc_property_map(h2h);
-
-  CGAL::copy_face_graph<Mesh,SM>(mesh, sm, CGAL::parameters::vertex_to_vertex_map(v2vpmap).halfedge_to_halfedge_map(h2hpmap));
-
-  if(options.vertex_has_status()){
-    for(auto v : vertices(mesh)){
-        put(vspm, v2v[v], mesh.status(v).selected());
-    }
-  }else{
-    std::cout << "no vertex status" << std::endl;
-  }
-
-  if(options.edge_has_status()){
-    for(auto e : edges(mesh)){
-        auto sme = edge(h2h[halfedge(e,mesh)], sm);
-        put(efpm, sme , mesh.status(e).feature());
-    }
-  }else{
-    std::cout << "no edge status" << std::endl;
-  }
-  return true;
-}
-
-} // namespace IO
-} // namespace CGAL
+#include "read_OM.h"
 
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 
-typedef OpenMesh::PolyMesh_ArrayKernelT</* MyTraits*/> Mesh;
+typedef OpenMesh::PolyMesh_ArrayKernelT</* MyTraits*/> OMesh;
 typedef CGAL::Surface_mesh<Kernel::Point_3> SM;
-typedef boost::graph_traits<Mesh>::vertex_descriptor vertex_descriptor;
+typedef boost::graph_traits<OMesh>::vertex_descriptor vertex_descriptor;
+typedef boost::graph_traits<OMesh>::halfedge_descriptor halfedge_descriptor;
+
 typedef boost::graph_traits<SM>::vertex_descriptor sm_vertex_descriptor;
-typedef boost::graph_traits<Mesh>::face_descriptor face_descriptor;
-typedef boost::graph_traits<Mesh>::halfedge_descriptor halfedge_descriptor;
-typedef boost::graph_traits<SM>::halfedge_descriptor sm_halfedge_descriptor;
 typedef boost::graph_traits<SM>::edge_descriptor sm_edge_descriptor;
 
 int main(int argc, char** argv )
 {
-  Mesh mesh;
+  OMesh omesh;
 
   const std::string filename = (argc>1)?argv[1]:CGAL::data_file_path("meshes/in.off");
   const char* outname= (argc>2)?argv[2]:"out.om";
-  CGAL::IO::read_polygon_mesh(filename, mesh);
+  CGAL::IO::read_polygon_mesh(filename, omesh);
 
-  mesh.request_vertex_status();
-  mesh.request_edge_status();
+  omesh.request_vertex_status();
+  omesh.request_edge_status();
 
   int i = 0;
-  for(auto v : vertices(mesh)){
-        mesh.status(v).set_selected((i%2) == 0);
+  for(auto v : vertices(omesh)){
+        omesh.status(v).set_selected((i%2) == 0);
         ++i;
   }
 
   i = 0;
-  for(auto e : edges(mesh)){
-        mesh.status(e).set_feature(i > 2);
+  for(auto e : edges(omesh)){
+        omesh.status(e).set_feature(i > 2);
         ++i;
   }
 
-  OpenMesh::IO::write_mesh(mesh, outname, OpenMesh::IO::Options::Status);
+  OpenMesh::IO::write_mesh(omesh, outname, OpenMesh::IO::Options::Status);
 
   SM sm;
 
