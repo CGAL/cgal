d2b5e4979ce80899087738367bc8cbe89218e668
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-11-29 11:25:15 +0000

    Add data for 2D example

--- Frechet_distance/examples/Frechet_distance/Frechet_distance_2.cpp
+++ Frechet_distance/examples/Frechet_distance/Frechet_distance_2.cpp
@@ -1,8 +1,10 @@
 #include <CGAL/Frechet_distance.h>
 #include <CGAL/Frechet_distance_traits_2.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/IO/WKT.h>
 
 #include <ostream>
+#include <fstream>
 
 using Kernel = CGAL::Exact_predicates_inexact_constructions_kernel;
 using Traits = CGAL::Frechet_distance_traits_2<Kernel>;
@@ -11,6 +13,14 @@ using Point = Traits::Point_d;
 int main(int argc, char* argv[])
 {
     std::vector<Point> A, B;
+    {
+      std::ifstream in(CGAL::data_file_path("wkt/LetterA.wkt"));
+      CGAL::IO::read_linestring_WKT(in, A);
+    }
+    {
+      std::ifstream in(CGAL::data_file_path("wkt/LetterAbis.wkt"));
+      CGAL::IO::read_linestring_WKT(in, B);
+    }
     bool res = CGAL::is_Frechet_distance_larger<Traits>(A, B, 0.001);
     std::cout << std::boolalpha << res << std::endl;
     return 0;
