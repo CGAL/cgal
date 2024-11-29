23af1048bad592694949384bfe12356a41c00d06
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-11-29 12:35:17 +0000

    Add data for 3D example

--- Frechet_distance/examples/Frechet_distance/Frechet_distance_3.cpp
+++ Frechet_distance/examples/Frechet_distance/Frechet_distance_3.cpp
@@ -1,8 +1,10 @@
 #include <CGAL/Frechet_distance.h>
 #include <CGAL/Frechet_distance_traits_3.h>
 #include <CGAL/Simple_cartesian.h>
+#include <CGAL/IO/WKT.h>
 
 #include <ostream>
+#include <fstream>
 
 using Kernel = CGAL::Simple_cartesian<double>;
 using Traits = CGAL::Frechet_distance_traits_3<Kernel>;
@@ -11,6 +13,14 @@ using Point = Traits::Point_d;
 int main(int argc, char* argv[])
 {
     std::vector<Point> A, B;
+    {
+      std::ifstream in(CGAL::data_file_path("wkt/moebius.wkt"));
+      CGAL::IO::read_linestring_WKT(in, A);
+    }
+    {
+      std::ifstream in(CGAL::data_file_path("wkt/moebius2.wkt"));
+      CGAL::IO::read_linestring_WKT(in, B);
+    }
     std::pair<double, double> res = CGAL::approximate_Frechet_distance<Traits>(A, B, 0.000001);
     std::cout << "The Frechet distance between the polylines is between " <<  res.first << " and " << res.second << std::endl;
     return 0;
