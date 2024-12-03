2a41e1dc99a1a627db47f7eeea666ecedfe6a492
Andreas Fabri <andreas.fabri@geometryfactory.com>
2024-12-03 07:16:34 +0000

    change parameters for shorter debug cycle.  To undo befoe merging

--- Periodic_3_mesh_3/test/Periodic_3_mesh_3/test_triply_periodic_minimal_surfaces.cpp
+++ Periodic_3_mesh_3/test/Periodic_3_mesh_3/test_triply_periodic_minimal_surfaces.cpp
@@ -237,10 +237,10 @@ C3t3 make_mesh(const Labeling_function& labeling_function, const Iso_cuboid& can
   Periodic_mesh_domain domain(labeling_function, canonical_cube);
 
   Periodic_mesh_criteria criteria(facet_angle = 30.,
-                                  facet_size = 0.03 * 2 /*domain's edge length*/,
-                                  facet_distance = 0.03 * 2 /*domain's edge length*/,
+                                  facet_size = 0.1 * 2 /*domain's edge length*/,
+                                  facet_distance = 0.1 * 2 /*domain's edge length*/,
                                   cell_radius_edge_ratio = 2.,
-                                  cell_size = 0.05);
+                                  cell_size = 0.5);
 
   return CGAL::make_periodic_3_mesh_3<C3t3>(domain, criteria);
 }
