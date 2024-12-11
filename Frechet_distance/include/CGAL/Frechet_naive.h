354786b56972d7cddb801dede30fb6267986fc64
André Nusser <andre.nusser@inria.fr>
2024-12-11 16:11:54 +0100

    WIP: High-dimensional test data creation.

--- Frechet_distance/include/CGAL/Frechet_naive.h
+++ Frechet_distance/include/CGAL/Frechet_naive.h
@@ -1,3 +1,18 @@
+// Copyright (c) 2024 Max-Planck-Institute Saarbruecken (Germany), CNRS (France), GeometryFactory (France)
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : André Nusser <anusser@mpi-inf.mpg.de>
+//                 Marvin Künnemann <marvin@mpi-inf.mpg.de>
+//                 Karl Bringmann <kbringma@mpi-inf.mpg.de>
+//                 Andreas Fabri
+// =============================================================================
+
 #pragma once
 #include <CGAL/license/Frechet_distance.h>
 #include <CGAL/Frechet_distance/internal/certificate.h>
@@ -8,6 +23,7 @@
 #include <CGAL/Frechet_distance/internal/id.h>
 #include <CGAL/Frechet_distance/internal/high_level_predicates.h>
 
+#include <optional>
 #include <vector>
 #include <limits>
 
@@ -22,6 +38,7 @@ namespace internal {
   template <typename C>
 class FrechetNaive
 {
+    // TODO: clean up
     using Curve = C;
     using K = typename Curve::K;
     using Point = typename C::Point;
@@ -43,6 +60,9 @@ class FrechetNaive
     using Lambda = CGAL::Frechet_distance_::internal::Lambda<C>;
     using CurvePair = std::array<Curve const*, 2>;
 
+    // this we actually need???
+    using Interval = Interval<C>;
+
 public:
     FrechetNaive() = default;
 
@@ -56,62 +76,62 @@ template <typename C>
 bool FrechetNaive<C>::lessThan(distance_t const& distance, Curve const& curve1,
                                Curve const& curve2)
 {
+    using OptLambda = std::optional<Lambda>;
+
 	assert(curve1.size() >= 2);
 	assert(curve2.size() >= 2);
 	distance_t dist_sqr = distance * distance;
 
-	if (curve1[0].dist_sqr(curve2[0]) > dist_sqr || curve1.back().dist_sqr(curve2.back()) > dist_sqr) { return false; }
+	if (CGAL::squared_distance(curve1[0],curve2[0]) > dist_sqr || CGAL::squared_distance(curve1.back(), curve2.back()) > dist_sqr) { return false; }
 
-	distance_t infty = std::numeric_limits<distance_t>::max();
-
-	std::vector<std::vector<distance_t>> reachable1(curve1.size()-1, std::vector<distance_t>(curve2.size(), infty));
-	std::vector<std::vector<distance_t>> reachable2(curve1.size(), std::vector<distance_t>(curve2.size()-1, infty));
+	std::vector<std::vector<OptLambda>> reachable1(curve1.size()-1, std::vector<OptLambda>(curve2.size(), std::nullopt));
+	std::vector<std::vector<OptLambda>> reachable2(curve1.size(), std::vector<OptLambda>(curve2.size()-1, std::nullopt));
 	for (size_t i = 0; i < curve1.size() - 1; ++i) {
 		reachable1[i][0] = 0.;
-		if (curve2[0].dist_sqr(curve1[i+1]) > dist_sqr) { break; }
+		if (CGAL::squared_distance(curve2[0], curve1[i+1]) > dist_sqr) { break; }
 	}
 	for (size_t j = 0; j < curve2.size() - 1; ++j) {
 		reachable2[0][j] = 0.;
-		if (curve1[0].dist_sqr(curve2[j+1]) > dist_sqr) { break; }
+		if (CGAL::squared_distance(curve1[0], curve2[j+1]) > dist_sqr) { break; }
 	}
 
 	for (size_t i = 0; i < curve1.size(); ++i) {
 		for (size_t j = 0; j < curve2.size(); ++j) {
 			if (i < curve1.size() - 1 && j > 0) {
-				Interval free_int = HLPred::intersection_interval(curve2[j], distance, curve1[i], curve1[i+1]);
+				Interval free_int = HLPred::intersection_interval(curve2, j, curve1, i, distance);
 				if (!free_int.is_empty()) {
-					if (reachable2[i][j-1] != infty) {
-						reachable1[i][j] = free_int.begin;
-					}
-					else if (reachable1[i][j-1] <= free_int.end) {
-						reachable1[i][j] = CGAL::max(free_int.begin, reachable1[i][j-1]);
-					}
+                    if (reachable2[i][j-1]) {
+                        reachable1[i][j] = free_int.begin;
+                    }
+                    else if (reachable1[i][j-1] && reachable1[i][j-1] <= free_int.end) {
+                        reachable1[i][j] = CGAL::max(free_int.begin, reachable1[i][j-1].value());
+                    }
 				}
 			}
 			if (j < curve2.size() - 1 && i > 0) {
-				Interval free_int = HLPred::intersection_interval(curve1[i], distance, curve2[j], curve2[j+1]);
+				Interval free_int = HLPred::intersection_interval(curve1, i, curve2, j, distance);
 				if (!free_int.is_empty()) {
-					if (reachable1[i-1][j] != infty) {
-						reachable2[i][j] = free_int.begin;
-					}
-					else if (reachable2[i-1][j] <= free_int.end) {
-						reachable2[i][j] = CGAL::max(free_int.begin, reachable2[i-1][j]);
-					}
+                    if (reachable1[i-1][j]) {
+                        reachable2[i][j] = free_int.begin;
+                    }
+                    else if (reachable2[i-1][j] && reachable2[i-1][j] <= free_int.end) {
+					    reachable2[i][j] = CGAL::max(free_int.begin, reachable2[i-1][j].value());
+                    }
 				}
 			}
 		}
 	}
 
-	assert((reachable1.back().back() < infty) == (reachable2.back().back() < infty));
+	assert((bool)reachable1.back().back() == (bool)reachable2.back().back());
 
-	return reachable1.back().back() < infty;
+	return (bool)reachable1.back().back();
 }
 
 template <typename C>
 std::pair<double,double> FrechetNaive<C>::calcDistance(Curve const& curve1, Curve const& curve2, double epsilon)
 {
-	distance_t min = 0.;
-	distance_t max = curve1.getUpperBoundDistance(curve2);
+	double min = 0.;
+	double max = curve1.getUpperBoundDistance(curve2).sup();
 
 	while (max - min >= epsilon) {
 		auto split = (max + min)/2.;
@@ -123,7 +143,7 @@ std::pair<double,double> FrechetNaive<C>::calcDistance(Curve const& curve1, Curv
 		}
 	}
 
-	return {min, max};
+	return std::make_pair(min, max);
 }
 
 } // namespace internal
---
+++ Frechet_distance/test/Frechet_distance/data/scripts/create_curves.py
@@ -0,0 +1,76 @@
+import random
+import numpy as np
+import numpy.linalg as la
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+# dimensionality of the curves
+D = 3
+# approximate average distance between two consecutive vertices
+verbose = False
+plot = False # plots only the first two dimensions
+# number of curves and how many different start and end points
+num_curves = 10
+num_start_end = 3
+# random seed
+seed = 42
+# curve directory
+curve_name_prefix = "../curves/generated_3d/"
+
+rng = np.random.default_rng(seed)
+
+def norm(x):
+    return la.norm(x, np.inf)
+
+def create_random_point(mean=np.zeros(D)):
+    return rng.normal(mean, 1.2)
+
+def create_curve(start, end):
+    curve = [start]
+    while norm(end - curve[-1]) > np.sqrt(D):
+        step = curve[-1] + (end - curve[-1])/norm(end - curve[-1])
+        noisy_step = create_random_point(step)
+        curve.append(noisy_step)
+    curve.append(end)
+    return curve
+
+def create_curves(starts=None, ends=None, num_curves=1):
+    curves = []
+    for i in range(num_curves):
+        start = random.choice(starts)
+        end = random.choice(ends)
+        curves.append(create_curve(start, end))
+    return curves
+
+def write_to_file(curves, prefix = "curve"):
+    for i in range(len(curves)):
+        f = open(prefix + str(i) + ".txt", "w")
+        for p in curves[i]:
+            for x in p:
+                f.write(str(x))
+                f.write(" ")
+            f.write("\n")
+
+starts = []
+ends = []
+for i in range(num_start_end):
+    starts.append(create_random_point(np.array(D*[0.])))
+    ends.append(create_random_point(np.array(D*[100.])))
+
+curves = create_curves(starts, ends, num_curves)
+
+# print
+if verbose:
+    print(curves)
+
+# plot
+if plot:
+    for curve in curves:
+        X = [p[0] for p in curve]
+        Y = [p[1] for p in curve]
+        plt.plot(X, Y)
+    plt.show()
+
+# export
+write_to_file(curves, curve_name_prefix)
