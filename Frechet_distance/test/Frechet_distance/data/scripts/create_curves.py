1b82dd3f0f6629a99ff355ae414d7e8956af7a54
André Nusser <andre.nusser@inria.fr>
2024-12-12 11:10:13 +0100

    Create goal directed test curves in 3d and 100d.

--- Frechet_distance/test/Frechet_distance/data/scripts/create_curves.py
+++ Frechet_distance/test/Frechet_distance/data/scripts/create_curves.py
@@ -1,3 +1,7 @@
+#
+# Small helper script to create goal directed curves in arbitrary dimensions.
+#
+
 import random
 import numpy as np
 import numpy.linalg as la
@@ -6,17 +10,19 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 # dimensionality of the curves
-D = 3
+D = 100
 # approximate average distance between two consecutive vertices
 verbose = False
 plot = False # plots only the first two dimensions
 # number of curves and how many different start and end points
 num_curves = 10
 num_start_end = 3
+start_mean = np.array(D*[0.])
+end_mean = np.array(D*[20.])
 # random seed
 seed = 42
 # curve directory
-curve_name_prefix = "../curves/generated_3d/"
+curve_name_prefix = "../curves/generated_100d/"
 
 rng = np.random.default_rng(seed)
 
@@ -24,7 +30,7 @@ def norm(x):
     return la.norm(x, np.inf)
 
 def create_random_point(mean=np.zeros(D)):
-    return rng.normal(mean, 1.2)
+    return rng.normal(mean, 1.3)
 
 def create_curve(start, end):
     curve = [start]
@@ -55,8 +61,8 @@ def write_to_file(curves, prefix = "curve"):
 starts = []
 ends = []
 for i in range(num_start_end):
-    starts.append(create_random_point(np.array(D*[0.])))
-    ends.append(create_random_point(np.array(D*[100.])))
+    starts.append(create_random_point(start_mean))
+    ends.append(create_random_point(end_mean))
 
 curves = create_curves(starts, ends, num_curves)
 
