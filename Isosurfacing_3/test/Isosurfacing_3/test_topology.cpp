59af6a81a39505eee6eca4962d283492df63856e
Julian Stahl <julian.stahl@fau.de>
2024-12-19 19:01:06 +0100

    Add topology verifier

--- Isosurfacing_3/test/Isosurfacing_3/test_topology.cpp
+++ Isosurfacing_3/test/Isosurfacing_3/test_topology.cpp
@@ -27,33 +27,6 @@
 
 namespace IS = CGAL::Isosurfacing;
 
-template <typename PolygonMesh>
-std::size_t connected_components(PolygonMesh& mesh)
-{
-  using face_descriptor = typename boost::graph_traits<PolygonMesh>::face_descriptor;
-
-  auto fccmap = mesh.template add_property_map<face_descriptor, std::size_t>("f:CC").first;
-
-  return CGAL::Polygon_mesh_processing::connected_components(mesh, fccmap);
-}
-
-template <typename PolygonMesh>
-std::size_t euler_characteristic(const PolygonMesh& mesh)
-{
-  return mesh.number_of_vertices() - mesh.number_of_edges() + mesh.number_of_faces();
-}
-
-template <typename PolygonMesh>
-std::size_t boundary_components(const PolygonMesh& mesh)
-{
-  using halfedge_descriptor = typename boost::graph_traits<PolygonMesh>::halfedge_descriptor;
-  std::vector<halfedge_descriptor> border_cycles;
-
-  CGAL::Polygon_mesh_processing::extract_boundary_cycles(mesh, std::back_inserter(border_cycles));
-
-  return border_cycles.size();
-}
-
 template <typename Grid>
 bool check_closed_not_empty(const Grid& grid, const IS::Interpolated_discrete_values_3<Grid>& values, const typename Grid::Geom_traits::FT iso = 0)
 {
@@ -439,29 +412,6 @@ FT generate_predefined_plane(const PlaneCase topology_case, std::array<FT, 8>& v
   }
 
   return iso;
-}
-
-template<typename Domain>
-void write_debug_grid(const Domain& domain, const std::string& filename) {
-  using Point = typename Domain::Geom_traits::Point_3;
-  using Mesh = CGAL::Surface_mesh<Point>;
-
-  Mesh debug_grid;
-  auto debug_grid_creator = [&](const typename Domain::cell_descriptor& c)
-  {
-    std::vector<typename Mesh::Vertex_index> cell_vertices;
-    for (const auto& v : domain.cell_vertices(c)) {
-      cell_vertices.push_back(debug_grid.add_vertex(domain.point(v)));
-    }
-    debug_grid.add_face(cell_vertices[6], cell_vertices[2], cell_vertices[0], cell_vertices[4]);
-    debug_grid.add_face(cell_vertices[1], cell_vertices[3], cell_vertices[7], cell_vertices[5]);
-    debug_grid.add_face(cell_vertices[0], cell_vertices[1], cell_vertices[5], cell_vertices[4]);
-    debug_grid.add_face(cell_vertices[6], cell_vertices[7], cell_vertices[3], cell_vertices[2]);
-    debug_grid.add_face(cell_vertices[2], cell_vertices[3], cell_vertices[1], cell_vertices[0]);
-    debug_grid.add_face(cell_vertices[4], cell_vertices[5], cell_vertices[7], cell_vertices[6]);
-  };
-  domain.template for_each_cell<CGAL::Sequential_tag>(debug_grid_creator);
-  CGAL::IO::write_OFF(filename, debug_grid);
 }
 
 template<typename KERNEL>
--- Isosurfacing_3/test/Isosurfacing_3/test_util.h
+++ Isosurfacing_3/test/Isosurfacing_3/test_util.h
@@ -7,6 +7,7 @@
 #include <CGAL/Polygon_mesh_processing/repair.h>
 #include <CGAL/Polygon_mesh_processing/repair_degeneracies.h>
 #include <CGAL/Polygon_mesh_processing/repair_polygon_soup.h>
+#include <CGAL/Surface_mesh.h>
 
 #include <CGAL/Named_function_parameters.h>
 #include <CGAL/boost/graph/named_params_helper.h>
@@ -59,6 +60,48 @@ bool has_degenerate_faces(PolygonMesh& pmesh)
   return !dfs.empty();
 }
 
+template <typename PolygonMesh>
+int connected_components(PolygonMesh& mesh)
+{
+  using face_descriptor = typename boost::graph_traits<PolygonMesh>::face_descriptor;
+
+  auto fccmap = mesh.template add_property_map<face_descriptor, std::size_t>("f:CC").first;
+
+  return CGAL::Polygon_mesh_processing::connected_components(mesh, fccmap);
+}
+
+template <typename PolygonMesh>
+int euler_characteristic(const PolygonMesh& mesh)
+{
+  return mesh.number_of_vertices() - mesh.number_of_edges() + mesh.number_of_faces();
+}
+
+template <typename PolygonMesh>
+int boundary_components(const PolygonMesh& mesh)
+{
+  using halfedge_descriptor = typename boost::graph_traits<PolygonMesh>::halfedge_descriptor;
+  std::vector<halfedge_descriptor> border_cycles;
+
+  CGAL::Polygon_mesh_processing::extract_boundary_cycles(mesh, std::back_inserter(border_cycles));
+
+  return border_cycles.size();
+}
+
+template <typename PolygonMesh>
+int betti_0(PolygonMesh& mesh) {
+    return connected_components(mesh);
+}
+
+template <typename PolygonMesh>
+int betti_2(PolygonMesh& mesh) {
+    return connected_components(mesh); // only for closed surfaces
+}
+
+template <typename PolygonMesh>
+int betti_1(PolygonMesh& mesh) {
+    return betti_0(mesh) + betti_2(mesh) - euler_characteristic(mesh);
+}
+
 // template <typename PolygonMesh>
 // bool check_mesh_distance(const PolygonMesh& m0, const PolygonMesh& m1)
 // {
@@ -67,5 +110,28 @@ bool has_degenerate_faces(PolygonMesh& pmesh)
 //   std::cout << dist << std::endl;
 //   return true;
 // }
+
+template<typename Domain>
+void write_debug_grid(const Domain& domain, const std::string& filename) {
+  using Point = typename Domain::Geom_traits::Point_3;
+  using Mesh = CGAL::Surface_mesh<Point>;
+
+  Mesh debug_grid;
+  auto debug_grid_creator = [&](const typename Domain::cell_descriptor& c)
+  {
+    std::vector<typename Mesh::Vertex_index> cell_vertices;
+    for (const auto& v : domain.cell_vertices(c)) {
+      cell_vertices.push_back(debug_grid.add_vertex(domain.point(v)));
+    }
+    debug_grid.add_face(cell_vertices[6], cell_vertices[2], cell_vertices[0], cell_vertices[4]);
+    debug_grid.add_face(cell_vertices[1], cell_vertices[3], cell_vertices[7], cell_vertices[5]);
+    debug_grid.add_face(cell_vertices[0], cell_vertices[1], cell_vertices[5], cell_vertices[4]);
+    debug_grid.add_face(cell_vertices[6], cell_vertices[7], cell_vertices[3], cell_vertices[2]);
+    debug_grid.add_face(cell_vertices[2], cell_vertices[3], cell_vertices[1], cell_vertices[0]);
+    debug_grid.add_face(cell_vertices[4], cell_vertices[5], cell_vertices[7], cell_vertices[6]);
+  };
+  domain.template for_each_cell<CGAL::Sequential_tag>(debug_grid_creator);
+  CGAL::IO::write_OFF(filename, debug_grid);
+}
 
 #endif // CGAL_ISOSURFACING_TEST_UTIL_H
