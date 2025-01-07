e0572b9946fef55804c905063240accb756be999
Sébastien Loriot <sebastien.loriot@cgal.org>
2025-01-07 10:28:49 +0100

    fix license and copyright

--- Vector_graphics_on_surfaces/include/CGAL/Polygon_mesh_processing/Bsurf/locally_shortest_path.h
+++ Vector_graphics_on_surfaces/include/CGAL/Polygon_mesh_processing/Bsurf/locally_shortest_path.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 University of Genova (Italy).
+// Copyright (c) 2023 GeometryFactory and Claudio Mancinelli.
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org).
@@ -13,7 +13,7 @@
 #ifndef CGAL_POLYGON_MESH_PROCESSING_BSURF_LOCALLY_SHORTEST_PATH_H
 #define CGAL_POLYGON_MESH_PROCESSING_BSURF_LOCALLY_SHORTEST_PATH_H
 
-// #include <CGAL/license/Polygon_mesh_processing/bsurf.h>
+#include <CGAL/license/Vector_graphics_on_surfaces.h>
 
 #include <CGAL/Named_function_parameters.h>
 #include <CGAL/Polygon_mesh_processing/locate.h>
