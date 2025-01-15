46ab7efe83b0ff59440771b15b79191d504ddbe5
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2025-01-15 01:02:25 +0100

    Add missing default

--- Straight_skeleton_2/include/CGAL/create_straight_skeleton_from_polygon_with_holes_2.h
+++ Straight_skeleton_2/include/CGAL/create_straight_skeleton_from_polygon_with_holes_2.h
@@ -26,11 +26,12 @@
 
 namespace CGAL {
 
-template<class K, class Polygon>
+template<class Polygon,
+         class K = Exact_predicates_inexact_constructions_kernel>
 boost::shared_ptr< Straight_skeleton_2<K> >
 inline
-create_interior_straight_skeleton_2 ( Polygon const& aPolyWithHoles,
-                                      K const& k,
+create_interior_straight_skeleton_2 ( const Polygon& aPolyWithHoles,
+                                      const K& k = K(),
                                       std::enable_if_t<
                                         CGAL_SS_i::has_Hole_const_iterator<Polygon>::value>* = nullptr)
 {
