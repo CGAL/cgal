a509669a8a71b274d69e830edeef4088272c0c0a
Anirudh Lakhanpal <anirudhlakhanpal123@gmail.com>
2025-01-26 19:22:11 +0530

    Moved << and >> overloads from Min_circle_2_impl.h to Min_circle_2.h

--- Bounding_volumes/include/CGAL/Min_circle_2.h
+++ Bounding_volumes/include/CGAL/Min_circle_2.h
@@ -570,15 +570,84 @@ class Min_circle_2 {
 // ---
 template < class Traits_ >
 std::ostream&
-operator << ( std::ostream& os, const Min_circle_2<Traits_>& mc);
+operator << ( std::ostream& os,
+              const Min_circle_2<Traits_>& min_circle)
+{
+    using namespace std;
+
+    typedef  typename Min_circle_2<Traits_>::Point  Point;
+    typedef  ostream_iterator<Point>       Os_it;
+
+    switch ( CGAL::IO::get_mode( os)) {
+
+      case CGAL::IO::PRETTY:
+        os << endl;
+        os << "CGAL::Min_circle_2( |P| = " << min_circle.number_of_points()
+           << ", |S| = " << min_circle.number_of_support_points() << endl;
+        os << "  P = {" << endl;
+        os << "    ";
+        copy( min_circle.points_begin(), min_circle.points_end(),
+              Os_it( os, ",\n    "));
+        os << "}" << endl;
+        os << "  S = {" << endl;
+        os << "    ";
+        copy( min_circle.support_points_begin(),
+              min_circle.support_points_end(),
+              Os_it( os, ",\n    "));
+        os << "}" << endl;
+        os << "  circle = " << min_circle.circle() << endl;
+        os << ")" << endl;
+        break;
+
+      case CGAL::IO::ASCII:
+        copy( min_circle.points_begin(), min_circle.points_end(),
+              Os_it( os, "\n"));
+        break;
+
+      case CGAL::IO::BINARY:
+        copy( min_circle.points_begin(), min_circle.points_end(),
+              Os_it( os));
+        break;
+
+      default:
+        CGAL_assertion_msg( false,
+                                         "CGAL::IO::get_mode( os) invalid!");
+        break; }
+
+    return( os);
+}
 
 template < class Traits_ >
 std::istream&
-operator >> ( std::istream& is,       Min_circle_2<Traits_>& mc);
+operator >> ( std::istream& is, CGAL::Min_circle_2<Traits_>& min_circle)
+{
+    using namespace std;
+
+    switch ( CGAL::IO::get_mode( is)) {
+
+      case CGAL::IO::PRETTY:
+        cerr << endl;
+        cerr << "Stream must be in ASCII or binary mode" << endl;
+        break;
+
+      case CGAL::IO::ASCII:
+      case CGAL::IO::BINARY:
+        typedef  typename CGAL::Min_circle_2<Traits_>::Point  Point;
+        typedef  istream_iterator<Point>            Is_it;
+        min_circle.clear();
+        min_circle.insert( Is_it( is), Is_it());
+        break;
+
+      default:
+        CGAL_assertion_msg( false, "CGAL::IO::mode invalid!");
+        break; }
+
+    return( is);
+}
 
 } //namespace CGAL
 
-#include <CGAL/Min_circle_2/Min_circle_2_impl.h>
+// #include <CGAL/Min_circle_2/Min_circle_2_impl.h>
 
 #endif // CGAL_MIN_CIRCLE_2_H
 
--- Bounding_volumes/include/CGAL/Min_circle_2/Min_circle_2_impl.h
+++ Bounding_volumes/include/CGAL/Min_circle_2/Min_circle_2_impl.h
@@ -1,107 +1,107 @@
-// Copyright (c) 1997-2001
-// ETH Zurich (Switzerland).  All rights reserved.
-//
-// This file is part of CGAL (www.cgal.org).
-//
-// $URL$
-// $Id$
-// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
-//
-//
-// Author(s)     : Sven Schoenherr <sven@inf.ethz.ch>, Bernd Gaertner
+// // Copyright (c) 1997-2001
+// // ETH Zurich (Switzerland).  All rights reserved.
+// //
+// // This file is part of CGAL (www.cgal.org).
+// //
+// // $URL$
+// // $Id$
+// // SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+// //
+// //
+// // Author(s)     : Sven Schoenherr <sven@inf.ethz.ch>, Bernd Gaertner
 
-#ifndef CGAL_MIN_CIRCLE_2_MIN_CIRCLE_2_IMPL_H
-#define CGAL_MIN_CIRCLE_2_MIN_CIRCLE_2_IMPL_H
+// #ifndef CGAL_MIN_CIRCLE_2_MIN_CIRCLE_2_IMPL_H
+// #define CGAL_MIN_CIRCLE_2_MIN_CIRCLE_2_IMPL_H
 
-#include <CGAL/license/Bounding_volumes.h>
+// #include <CGAL/license/Bounding_volumes.h>
 
-#include <iterator>
+// #include <iterator>
 
-namespace CGAL {
+// namespace CGAL {
 
-// Class implementation (continued)
-// ================================
-// I/O
-// ---
-template < class Traits_ >
-std::ostream&
-operator << ( std::ostream& os,
-              const Min_circle_2<Traits_>& min_circle)
-{
-    using namespace std;
+// // Class implementation (continued)
+// // ================================
+// // I/O
+// // ---
+// template < class Traits_ >
+// std::ostream&
+// operator << ( std::ostream& os,
+//               const Min_circle_2<Traits_>& min_circle)
+// {
+//     using namespace std;
 
-    typedef  typename Min_circle_2<Traits_>::Point  Point;
-    typedef  ostream_iterator<Point>       Os_it;
+//     typedef  typename Min_circle_2<Traits_>::Point  Point;
+//     typedef  ostream_iterator<Point>       Os_it;
 
-    switch ( CGAL::IO::get_mode( os)) {
+//     switch ( CGAL::IO::get_mode( os)) {
 
-      case CGAL::IO::PRETTY:
-        os << endl;
-        os << "CGAL::Min_circle_2( |P| = " << min_circle.number_of_points()
-           << ", |S| = " << min_circle.number_of_support_points() << endl;
-        os << "  P = {" << endl;
-        os << "    ";
-        copy( min_circle.points_begin(), min_circle.points_end(),
-              Os_it( os, ",\n    "));
-        os << "}" << endl;
-        os << "  S = {" << endl;
-        os << "    ";
-        copy( min_circle.support_points_begin(),
-              min_circle.support_points_end(),
-              Os_it( os, ",\n    "));
-        os << "}" << endl;
-        os << "  circle = " << min_circle.circle() << endl;
-        os << ")" << endl;
-        break;
+//       case CGAL::IO::PRETTY:
+//         os << endl;
+//         os << "CGAL::Min_circle_2( |P| = " << min_circle.number_of_points()
+//            << ", |S| = " << min_circle.number_of_support_points() << endl;
+//         os << "  P = {" << endl;
+//         os << "    ";
+//         copy( min_circle.points_begin(), min_circle.points_end(),
+//               Os_it( os, ",\n    "));
+//         os << "}" << endl;
+//         os << "  S = {" << endl;
+//         os << "    ";
+//         copy( min_circle.support_points_begin(),
+//               min_circle.support_points_end(),
+//               Os_it( os, ",\n    "));
+//         os << "}" << endl;
+//         os << "  circle = " << min_circle.circle() << endl;
+//         os << ")" << endl;
+//         break;
 
-      case CGAL::IO::ASCII:
-        copy( min_circle.points_begin(), min_circle.points_end(),
-              Os_it( os, "\n"));
-        break;
+//       case CGAL::IO::ASCII:
+//         copy( min_circle.points_begin(), min_circle.points_end(),
+//               Os_it( os, "\n"));
+//         break;
 
-      case CGAL::IO::BINARY:
-        copy( min_circle.points_begin(), min_circle.points_end(),
-              Os_it( os));
-        break;
+//       case CGAL::IO::BINARY:
+//         copy( min_circle.points_begin(), min_circle.points_end(),
+//               Os_it( os));
+//         break;
 
-      default:
-        CGAL_assertion_msg( false,
-                                         "CGAL::IO::get_mode( os) invalid!");
-        break; }
+//       default:
+//         CGAL_assertion_msg( false,
+//                                          "CGAL::IO::get_mode( os) invalid!");
+//         break; }
 
-    return( os);
-}
+//     return( os);
+// }
 
-template < class Traits_ >
-std::istream&
-operator >> ( std::istream& is, CGAL::Min_circle_2<Traits_>& min_circle)
-{
-    using namespace std;
+// template < class Traits_ >
+// std::istream&
+// operator >> ( std::istream& is, CGAL::Min_circle_2<Traits_>& min_circle)
+// {
+//     using namespace std;
 
-    switch ( CGAL::IO::get_mode( is)) {
+//     switch ( CGAL::IO::get_mode( is)) {
 
-      case CGAL::IO::PRETTY:
-        cerr << endl;
-        cerr << "Stream must be in ASCII or binary mode" << endl;
-        break;
+//       case CGAL::IO::PRETTY:
+//         cerr << endl;
+//         cerr << "Stream must be in ASCII or binary mode" << endl;
+//         break;
 
-      case CGAL::IO::ASCII:
-      case CGAL::IO::BINARY:
-        typedef  typename CGAL::Min_circle_2<Traits_>::Point  Point;
-        typedef  istream_iterator<Point>            Is_it;
-        min_circle.clear();
-        min_circle.insert( Is_it( is), Is_it());
-        break;
+//       case CGAL::IO::ASCII:
+//       case CGAL::IO::BINARY:
+//         typedef  typename CGAL::Min_circle_2<Traits_>::Point  Point;
+//         typedef  istream_iterator<Point>            Is_it;
+//         min_circle.clear();
+//         min_circle.insert( Is_it( is), Is_it());
+//         break;
 
-      default:
-        CGAL_assertion_msg( false, "CGAL::IO::mode invalid!");
-        break; }
+//       default:
+//         CGAL_assertion_msg( false, "CGAL::IO::mode invalid!");
+//         break; }
 
-    return( is);
-}
+//     return( is);
+// }
 
-} //namespace CGAL
+// } //namespace CGAL
 
-// ===== EOF ==================================================================
+// // ===== EOF ==================================================================
 
-#endif // CGAL_MIN_CIRCLE_2_MIN_CIRCLE_2_IMPL_H
+// #endif // CGAL_MIN_CIRCLE_2_MIN_CIRCLE_2_IMPL_H
