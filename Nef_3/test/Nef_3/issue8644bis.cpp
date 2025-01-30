ca17f813b6102230ec6c4c73d4740e2a86349153
Sébastien Loriot <sebastien.loriot@cgal.org>
2025-01-30 18:24:06 +0100

    fix debug

--- Nef_3/test/Nef_3/issue8644bis.cpp
+++ Nef_3/test/Nef_3/issue8644bis.cpp
@@ -5,14 +5,14 @@
 using Kernel = CGAL::Extended_cartesian<CGAL::Gmpq>;
 using Nef = CGAL::Nef_polyhedron_3<Kernel>;
 
-int main(int argc, char ** argv)
+int main()
 {
   Nef hspace_1(Nef::Plane_3(1.0, 0.0, 0.0, 0.0), Nef::INCLUDED);
   Nef hspace_2(Nef::Plane_3(1.0, 0.0, 0.0, 1.0), Nef::INCLUDED);
   Nef hspace_3(Nef::Plane_3(0.0, 0.0, 1.0, 1.0), Nef::INCLUDED);
 
-  Nef intersection_1 = hspace_1*hspace_2; // Line 14. Works fine.
-  Nef intersection_2 = hspace_2*hspace_3; // Line 15. Assertion failure.
-  
+  Nef intersection_1 = hspace_1*hspace_2;
+  Nef intersection_2 = hspace_2*hspace_3;
+
   return 0;
 }
