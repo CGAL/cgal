30a8e52c512b11d41e014715c0ed130d89f01483
Léo Valque <leo.valque@geometryfactory.com>
2025-02-07 16:19:29 +0100

    Replace Sqrt with KthRoot and modify description of longlong int

--- Number_types/doc/Number_types/CGAL/long_double.h
+++ Number_types/doc/Number_types/CGAL/long_double.h
@@ -5,10 +5,10 @@
 
 This header provides all necessary functions so the fundamental type
 `long double` is a model of the concepts `RealEmbeddable` and
-`FieldWithSqrt`. Due to rounding errors and overflow `long double` is
+`FieldWithKthRoot`. Due to rounding errors and overflow `long double` is
 considered as not exact.
 
-\cgalModels{FieldWithSqrt,RealEmbeddable}
+\cgalModels{FieldWithKthRoot,RealEmbeddable}
 */
 
 namespace CGAL {
