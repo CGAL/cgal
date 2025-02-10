bfc8f2ca8079b2cb8c3a828271d809277a879ee0
Léo Valque <leo.valque@geometryfactory.com>
2025-02-10 10:30:31 +0100

    Replace four almost identical sentences in int.h by one

--- Number_types/doc/Number_types/CGAL/int.h
+++ Number_types/doc/Number_types/CGAL/int.h
@@ -3,11 +3,8 @@
 \ingroup nt_builtin
 
 This header provides all necessary functions so the fundamental types
-`int`, `long int`, and `short int` become models of their respective algebraic concepts.
-
-- `int` is a model of `RealEmbeddable` and `EuclideanRing`,
-- `long int` is a model of `RealEmbeddable` and `EuclideanRing`
-- `short int` is a model of `RealEmbeddable` and `EuclideanRing`
+`int`, `long int`, and `short int` are models of the concepts `RealEmbeddable` and
+`EuclideanRing`.
 
 Due to overflow neither of those types is considered as exact.
 
