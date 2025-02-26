e42e85aaf4b6fb6d38e92329eb27388db1a4e4d1
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-02-26 15:48:22 +0000

    Adress some remarks of Pierre's review

--- Frechet_distance/examples/Frechet_distance/Frechet_distance_3.cpp
+++ Frechet_distance/examples/Frechet_distance/Frechet_distance_3.cpp
@@ -10,17 +10,17 @@ using Point = Kernel::Point_3;
 
 int main(int argc, char* argv[])
 {
-    std::vector<Point> A, B;
+    std::vector<Point> polylineA, polylineB;
     {
       std::ifstream in((argc > 1) ? argv[1] : CGAL::data_file_path("wkt/moebius.wkt"));
-      CGAL::IO::read_linestring_WKT(in, A);
+      CGAL::IO::read_linestring_WKT(in, polylineA);
     }
     {
       std::ifstream in((argc > 2) ? argv[2] : CGAL::data_file_path("wkt/moebius2.wkt"));
-      CGAL::IO::read_linestring_WKT(in, B);
+      CGAL::IO::read_linestring_WKT(in, polylineB);
     }
 
-    std::pair<double, double> res = CGAL::bounded_error_Frechet_distance(A, B, 0.000001);
+    std::pair<double, double> res = CGAL::bounded_error_Frechet_distance(polylineA, polylineB, 0.000001);
     std::cout << "The Frechet distance between the polylines is between " <<  res.first << " and " << res.second << std::endl;
     return 0;
 }
