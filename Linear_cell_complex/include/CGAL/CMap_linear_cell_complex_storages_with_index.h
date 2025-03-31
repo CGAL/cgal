e1762f476d59d4f4f1bdc8971e5f4084de3fbbaf
Sébastien Loriot <sebastien.loriot@cgal.org>
2025-03-31 10:25:17 +0200

    bad merge

--- Linear_cell_complex/include/CGAL/CMap_linear_cell_complex_storages_with_index.h
+++ Linear_cell_complex/include/CGAL/CMap_linear_cell_complex_storages_with_index.h
@@ -190,7 +190,6 @@ namespace CGAL {
     {
       // Allocate a dart for null_dart_descriptor
       CGAL_assertion(mdarts.empty()); // the compact container is empty
-      Dart_index local_null_dart_descriptor = mdarts.emplace();
       CGAL_assertion_code( Dart_index local_null_dart_descriptor = ) mdarts.emplace();
       CGAL_assertion_msg(local_null_dart_descriptor==0,
                           "[ERROR] fatal in CMap_linear_cell_complex_storage_with_index::init_storage");
