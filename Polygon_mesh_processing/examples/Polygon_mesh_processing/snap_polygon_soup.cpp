80265d0e54b6adc379262472ca7045e12c1f153c
Léo Valque <leo.valque@geometryfactory.com>
2025-04-03 11:42:31 +0200

    factorize snap_polygon_soup

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/snap_polygon_soup.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/snap_polygon_soup.cpp
@@ -1,4 +1,4 @@
-#define PMP_ROUNDING_VERTICES_IN_POLYGON_SOUP_VERBOSE
+// #define PMP_ROUNDING_VERTICES_IN_POLYGON_SOUP_VERBOSE
 // #define CGAL_PMP_AUTOREFINE_USE_DEFAULT_VERBOSE
 
 #include <CGAL/Simple_cartesian.h>
@@ -37,61 +37,17 @@ struct Track_visitor
   inline void delete_triangle(std::size_t /*src_id*/) {}
 };
 
-int main(int argc, char** argv)
-{
-  const std::string filename = argc == 1 ? CGAL::data_file_path("meshes/elephant.off")
-                                         : std::string(argv[1]);
-
-  // const std::string out_file = argc <= 2 ? "rounded_soup.off"
-  //                                        : std::string(argv[2]);
-  const std::string out_file = "rounded_soup.off";
-
-  const int grid_size = argc <= 2 ? 23
-                                  : std::stoi(std::string(argv[2]));
-
-  const bool epeck = argc <= 3 ? false
-                               : std::string(argv[3])=="EPECK";
-
-  if(epeck)
-  {
-    std::vector<EPECK::Point_3> input_points;
+template<typename Kernel>
+struct Example{
+  void operator()(const std::string& filename, int grid_size){
+    const std::string out_file = "rounded_soup.off";
+    std::vector<typename Kernel::Point_3> input_points;
     std::vector<boost::container::small_vector<std::size_t, 3>> input_triangles;
-
-    std::cout << "Snap rounding apply on " << filename << " with EPECK\n";
+    std::cout << "Snap rounding on " << filename << " with " << typeid(Kernel).name() << "\n";
     if (!CGAL::IO::read_polygon_soup(filename, input_points, input_triangles))
     {
       std::cerr << "Cannot read " << filename << "\n";
-      return 1;
-    }
-    std::cout << "#points = " << input_points.size() << " and #triangles = " << input_triangles.size() << std::endl;
-    std::cout << "Is 2-manifold: " << PMP::is_polygon_soup_a_polygon_mesh(input_triangles) << std::endl;
-
-    PMP::repair_polygon_soup(input_points, input_triangles);
-    PMP::triangulate_polygons(input_points, input_triangles);
-
-    CGAL::Real_timer t;
-    t.start();
-    PMP::autorefine_triangle_soup(input_points, input_triangles, CGAL::parameters::apply_iterative_snap_rounding(true).erase_all_duplicates(true).concurrency_tag(CGAL::Parallel_if_available_tag()).snap_grid_size(grid_size));
-    t.stop();
-    std::cout << "#points = " << input_points.size() << " and #triangles = " << input_triangles.size() << " in " << t.time() << " sec." << std::endl;
-    std::cout << "Does self-intersect: " << PMP::does_triangle_soup_self_intersect(input_points, input_triangles) << std::endl;
-    std::cout << "Is 2-manifold: " << PMP::orient_polygon_soup(input_points, input_triangles) << "\n\n"  << std::endl;
-
-    std::vector<Cartesian::Point_3> output_points;
-    for(auto &p: input_points)
-      output_points.emplace_back(CGAL::to_double(p.x()),CGAL::to_double(p.y()),CGAL::to_double(p.z()));
-
-    CGAL::IO::write_polygon_soup(out_file, output_points, input_triangles, CGAL::parameters::stream_precision(17));
-  }
-  else
-  {
-    std::vector<EPICK::Point_3> input_points;
-    std::vector<boost::container::small_vector<std::size_t, 3>> input_triangles;
-    std::cout << "Snap rounding on " << filename << " with EPICK\n";
-    if (!CGAL::IO::read_polygon_soup(filename, input_points, input_triangles))
-    {
-      std::cerr << "Cannot read " << filename << "\n";
-      return 1;
+      return;
     }
     std::cout << "#points = " << input_points.size() << " and #triangles = " << input_triangles.size() << std::endl;
     std::cout << "Is 2-manifold: " << PMP::is_polygon_soup_a_polygon_mesh(input_triangles) << std::endl;
@@ -118,8 +74,32 @@ int main(int argc, char** argv)
       std::cout << "Does self-intersect: " << PMP::does_triangle_soup_self_intersect<CGAL::Parallel_if_available_tag>(input_points, input_triangles) << std::endl;
     else
       std::cout << "ROUNDING FAILED" << std::endl;
-    CGAL::IO::write_polygon_soup(out_file, input_points, input_triangles, CGAL::parameters::stream_precision(17));
+
+    std::vector<Cartesian::Point_3> output_points;
+    for(auto &p: input_points)
+      output_points.emplace_back(CGAL::to_double(p.x()),CGAL::to_double(p.y()),CGAL::to_double(p.z()));
+    CGAL::IO::write_polygon_soup(out_file, output_points, input_triangles, CGAL::parameters::stream_precision(17));
     std::cout << "Is 2-manifold: " << PMP::orient_polygon_soup(input_points, input_triangles) << "\n\n"  << std::endl;
   }
+};
+
+int main(int argc, char** argv)
+{
+  const std::string filename = argc == 1 ? CGAL::data_file_path("meshes/elephant.off")
+                                         : std::string(argv[1]);
+
+  // const std::string out_file = argc <= 2 ? "rounded_soup.off"
+  //                                        : std::string(argv[2]);
+
+  const int grid_size = argc <= 2 ? 23
+                                  : std::stoi(std::string(argv[2]));
+
+  const bool epeck = argc <= 3 ? false
+                               : std::string(argv[3])=="EPECK";
+
+  if(epeck)
+    Example<EPECK>()(filename, grid_size);
+  else
+    Example<EPICK>()(filename, grid_size);
   return 0;
 }
