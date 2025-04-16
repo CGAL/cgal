c523eef0d00b0c1ef0c7e61677fb5246571f4d24
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-04-16 08:25:24 +0100

    Sphere Packing (new package)

---
+++ Sphere_packing/include/CGAL/Sphere_packing.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2025 Universitaet Bremen
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
+// Author(s) : Rene Weller
+//
+
+#ifndef CGAL_SPHEERE_PACKINGH_H_
+#define CGAL_SPHEERE_PACKINGH_H_
+
+#include <CGAL/license/Sphere_packing.h>
+
+#include <CGAL/disable_warnings.h>
+
+#include <CGAL/Bbox_2.h>
+
+
+namespace CGAL {
+
+/// \ingroup PkgSpherePackingRef
+
+
+
+/**
+ * \brief Sphere packing class.
+ *
+ * This class is a placeholder for the Sphere Packing package.
+ * It is not implemented yet.
+ *
+ * \tparam GeomTraits a geometric traits class, model of `Kernel`
+ *
+ */
+
+
+template<typename GeomTraits>
+class Sphere_packing
+{
+};
+
+
+
+}  // end namespace CGAL
+
+#include <CGAL/enable_warnings.h>
+
+#endif // CGAL_SPHEERE_PACKINGH_H_
