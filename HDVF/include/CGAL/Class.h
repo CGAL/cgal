c487c1ef792ce5af44eb49a93107ceaea9264f5c
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-04-17 12:37:45 +0100

    Add package_info

--- HDVF/include/CGAL/Class.h
+++ HDVF/include/CGAL/Class.h
@@ -1,3 +1,19 @@
+// Copyright (c) 2025 Marseille University (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : Alexandra Bac
+
+#ifndef CGAL_CLASSIFICATION_H
+#define CGAL_CLASSIFICATION_H
+
+#include <CGAL/license/HDVF.h>
+
 
 namespace CGAL {
 
