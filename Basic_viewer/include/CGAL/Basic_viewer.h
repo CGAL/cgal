9a5adb37e7142db1bacc033a3eb753196e5195ed
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2025-04-17 17:45:34 +0200

    Add file Basic_viewer.h

---
+++ Basic_viewer/include/CGAL/Basic_viewer.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2018  GeometryFactory Sarl (France).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
+// Author(s)     : Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
+
+#ifndef CGAL_BASIC_VIEWER_H
+#define CGAL_BASIC_VIEWER_H
+
+#include <CGAL/Graphics_scene.h>
+
+#if defined(CGAL_USE_BASIC_VIEWER_QT) 
+#include <CGAL/Qt/Basic_viewer.h>
+// #elif defined(CGAL_USE_BASIC_VIEWER_GLFW)
+// #include <CGAL/GLFW/Basic_viewer.h>
+#else
+namespace CGAL
+{
+  inline
+  void draw_graphics_scene(const Graphics_scene&,
+                           const char* ="CGAL Basic Viewer")
+  {
+    std::cerr<<"Impossible to draw, CGAL_USE_BASIC_VIEWER_QT is not defined."<<std::endl;
+  }
+} // End namespace CGAL
+#endif
+
+#endif // CGAL_BASIC_VIEWER_H
