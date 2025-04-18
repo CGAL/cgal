1cfb509a0572397c05e0997ae9c484f18ba3d9b9
Sébastien Loriot <sebastien.loriot@cgal.org>
2025-04-18 21:28:44 +0200

    handle some particular cases

--- Scripts/developer_scripts/add_checks_for_named_parameters.py
+++ Scripts/developer_scripts/add_checks_for_named_parameters.py
@@ -9,18 +9,38 @@ np_name="np"
 
 modified=False
 
+
+def is_in_comment_section(line):
+  global in_comment_section
+
+  if re.search("^\s*"+re.escape("//"), line):
+    return True
+  if re.search("^\s*"+re.escape("/*"), line):
+    in_comment_section=True
+    return True
+  if not in_comment_section:
+    return False
+  if re.search(re.escape("*/"), line):
+    in_comment_section=False;
+  return True
+
+in_comment_section = False
+
 while True:
   line = f.readline()
   if not line:
     break;
   res+=line
 
+  if not is_in_comment_section(line):
+    continue
+
   #possible np name (TODO: does not work if not on the same line)
   if re.search("bgl_namedparameters", line):
     m = re.search("[@\\\]param\s+([^ ]+)\s", line)
     if m:
       np_name=m.group(1)
-      print("found "+np_name)
+      #print("found "+np_name)
 
   if re.search("cgalNamedParamsBegin", line):
     params=[]
@@ -30,6 +50,7 @@ while True:
         break
       res+=line
       if re.search("cgalNamedParamsEnd", line):
+        is_in_comment_section(line)
         break
       m = re.search(r"cgalParamNBegin{\s*([^ ]+)\s*}", line)
       if m:
@@ -42,14 +63,28 @@ while True:
         break
       res+=line
       if re.search("cgalNamedParamsBegin", line):
-        stderr.write("Function with several nps! Not handled yet\n")
+        stderr.write("Function with several nps! Not handled yet ("+argv[1]+")\n")
         exit(1)
+      if is_in_comment_section(line):
+        continue
+
       m = re.search("^(\s*).*{", line)
       if m:
         s=m.group(1)+"  CGAL_CHECK_AUTHORIZED_NAMED_PARAMETERS("+np_name
         for p in params:
           s+=", "+p+"_t"
         s+=");"
+
+        mb = re.search("(^\s+{)(.*)}$", line)
+        if mb:
+          res+=mb.group(1)+"\n";
+          res+=s+"\n"
+          if mb.group(2).strip():
+            res+=m.group(1)+"  "+mb.group(2)+"\n"
+          res+=m.group(1)+"}\n"
+          modified=True
+          break
+
 
         add_macro=True
 
@@ -60,6 +95,7 @@ while True:
             stderr.write("Error parsing the function\n")
             exit(1)
           if re.search("CGAL_CHECK_AUTHORIZED_NAMED_PARAMETERS", line):
+            #TODO: in check mode, we should check that there is a match between code and `params`
             add_macro=False
             res+=line
             break
