eef2bea7b1ad389465cd6f1829fa15b0499649b2
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-04-24 08:11:39 +0100

    Add doc for storage policies

---
+++ Triangulation/doc/Triangulation/CGAL/TDS_full_cell_default_storage_policy.h
@@ -0,0 +1,10 @@
+
+namespace CGAL {
+
+  /*!
+  \ingroup PkgTriangulationsRef
+  A tag class to indicate that mirror indices are not stored in full cells.
+  */
+class TDS_full_cell_mirror_storage_policy
+{};
+}
