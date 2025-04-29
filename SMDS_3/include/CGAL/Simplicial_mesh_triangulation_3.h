90c3791221d45650faa7f80c80fe0162ab5535dd
Jane Tournois <jane.tournois@geometryfactory.com>
2025-04-29 09:14:31 +0200

    add class `Simplicial_mesh_triangulation_3` to ease the use of C3t3

---
+++ SMDS_3/include/CGAL/Simplicial_mesh_triangulation_3.h
@@ -0,0 +1,51 @@
+// Copyright (c) 2006-2007  INRIA Sophia-Antipolis (France).
+// Copyright (c) 2008,2011 GeometryFactory Sarl (France)
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : Laurent Rineau, Stephane Tayeb, Andreas Fabri, Jane Tournois
+
+#ifndef CGAL_SIMPLICIAL_MESH_TRIANGULATION_3_H
+#define CGAL_SIMPLICIAL_MESH_TRIANGULATION_3_H
+
+#include <CGAL/license/SMDS_3.h>
+
+#include <CGAL/Simplicial_mesh_vertex_base_3.h>
+#include <CGAL/Simplicial_mesh_cell_base_3.h>
+#include <CGAL/Triangulation_data_structure_3.h>
+#include <CGAL/Triangulation_3.h>
+
+namespace CGAL
+{
+  /**
+  *\ingroup PkgSMDS3Classes
+  * `Simplicial_mesh_triangulation_3`
+  * @todo
+  */
+  template<typename K,
+           typename SubdomainIndex = int,
+           typename SurfacePatchIndex = int,
+           typename CurveIndex = int,
+           typename CornerIndex = int>
+  class Simplicial_mesh_triangulation_3
+    : public CGAL::Triangulation_3<K,
+        CGAL::Triangulation_data_structure_3<
+          CGAL::Simplicial_mesh_vertex_base_3<K,
+                                              SubdomainIndex,
+                                              SurfacePatchIndex,
+                                              CurveIndex,
+                                              CornerIndex>,
+          CGAL::Simplicial_mesh_cell_base_3<K, SubdomainIndex, SurfacePatchIndex>,
+          CGAL::Sequential_tag>
+          >
+  {};
+};
+
+#endif // CGAL_SIMPLICIAL_MESH_TRIANGULATION_3_H
+
+
