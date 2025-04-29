c41a0c7b9f8a8298e1f78cece2f98aed3a66c815
Léo Valque <leo.valque@geometryfactory.com>
2025-04-29 14:36:54 +0200

    correct Indexes_range constructor

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/internal/triangle_soup_snap_rounding.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/internal/triangle_soup_snap_rounding.h
@@ -76,9 +76,8 @@ public:
   typedef typename Range::const_iterator const_iterator;
   typedef typename Range::iterator iterator;
 
-  Indexes_range(){}
+  Indexes_range() = default;
   Indexes_range(const std::initializer_list<std::size_t>  &l): Range(l), m_id(0), modified(true){}
-  Indexes_range(const Indexes_range<Range> &ir): Range(ir), m_id(ir.id()), modified(ir.was_modified()){}
   Indexes_range(Range &p): Range(p), modified(true){}
   Indexes_range(Range &p, size_t id): Range(p), m_id(id),modified(false){}
 
