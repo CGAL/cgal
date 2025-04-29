1e30aab6bccdf16a858867d175b1fa3b991a0c75
Léo Valque <leo.valque@geometryfactory.com>
2025-04-29 18:19:31 +0200

    Correct namepaspace of Polygon_types

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/internal/triangle_soup_snap_rounding.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/internal/triangle_soup_snap_rounding.h
@@ -92,7 +92,7 @@ private:
 
 // Repair_polygon_soup without remove_pinched_polygons since our polygon are triangles
 template <typename PointRange, typename PolygonRange,
-          typename Polygon = typename internal::Polygon_types<PointRange, PolygonRange>::Polygon_3>
+          typename Polygon = typename CGAL::internal::Polygon_types<PointRange, PolygonRange>::Polygon_3>
 struct Triangle_soup_fixer
 {
   template <typename NamedParameters>
