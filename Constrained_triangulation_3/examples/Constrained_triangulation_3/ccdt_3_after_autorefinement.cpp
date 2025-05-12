ca1a5982b66f9968a87a5835f27ae6dd71ecdb1e
Jane Tournois <jane.tournois@geometryfactory.com>
2025-05-12 12:12:32 +0200

    add an example with self-intersections and autorefinement

---
+++ Constrained_triangulation_3/examples/Constrained_triangulation_3/ccdt_3_after_autorefinement.cpp
@@ -0,0 +1,48 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+#include <CGAL/Polygon_mesh_processing/autorefinement.h>
+#include <CGAL/Polygon_mesh_processing/polygon_mesh_to_polygon_soup.h>
+#include <CGAL/Polygon_mesh_processing/self_intersections.h>
+#include <CGAL/Surface_mesh/Surface_mesh.h>
+#include <CGAL/make_conforming_constrained_Delaunay_triangulation_3.h>
+
+#include <CGAL/draw_constrained_triangulation_3.h>
+
+using K = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Point = K::Point_3;
+using Surface_mesh = CGAL::Surface_mesh<Point>;
+
+namespace PMP = CGAL::Polygon_mesh_processing;
+
+int main(int argc, char* argv[])
+{
+  const auto filename = (argc > 1) ? argv[1]
+                       : CGAL::data_file_path("meshes/spheres_intersecting.off");
+
+  CGAL::Surface_mesh<K::Point_3> mesh;
+  std::ifstream in(filename);
+  if(!in || !(in >> mesh)) {
+    std::cerr << "Error: cannot read file " << filename << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "Number of facets in " << filename << ": "
+    << mesh.number_of_faces() << "\n";
+
+  if(PMP::does_self_intersect(mesh))
+  {
+    PMP::autorefine(mesh);
+  }
+
+  std::cout << "Number of facets after autorefine: "
+    << mesh.number_of_faces() << "\n";
+
+  auto ccdt = CGAL::make_conforming_constrained_Delaunay_triangulation_3(mesh);
+
+  std::cout << "Number of constrained facets in the CDT: "
+            << ccdt.number_of_constrained_facets() << '\n';
+
+  CGAL::draw(ccdt);
+
+  return EXIT_SUCCESS;
+}
