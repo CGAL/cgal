51a28c4d0a177e4e69cbf530f829a253667485f4
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-05-12 07:54:39 +0100

    cleanup

--- Frechet_distance/include/CGAL/Frechet_distance/Neighbor_search.h
+++ Frechet_distance/include/CGAL/Frechet_distance/Neighbor_search.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Max-Planck-Institute Saarbruecken (Germany), CNRS (France), GeometryFactory (France)
+// Copyright (c) 2024 Max-Planck-Institute Saarbruecken (Germany), GeometryFactory (France)
 // All rights reserved.
 //
 // This file is part of CGAL (www.cgal.org).
