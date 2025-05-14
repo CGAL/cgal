607ada33ea714e67f50487876319dbafb953f700
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-05-14 09:48:44 +0100

    Polygon: prefix functions with CGAL::IO

--- Polygon/include/CGAL/Polygon_with_holes_2.h
+++ Polygon/include/CGAL/Polygon_with_holes_2.h
@@ -111,8 +111,8 @@ public:
 This operator exports a polygon with holes to the output stream `os`.
 
 An \ascii and a binary format exist. The format can be selected with
-the \cgal modifiers for streams, `set_ascii_mode()` and `set_binary_mode()`,
-respectively. The modifier `set_pretty_mode()` can be used to allow for (a
+the \cgal modifiers for streams, `CGAL::IO::set_ascii_mode()` and `GAL::IO::set_binary_mode()`,
+respectively. The modifier `GAL::IO::set_pretty_mode()` can be used to allow for (a
 few) structuring comments in the output. Otherwise, the output would
 be free of comments. The default for writing is \ascii without comments.
 
