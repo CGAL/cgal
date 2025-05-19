5db49a1b890b9ec46cead44a1e66ce90ffadbda2
Laurent Rineau <laurent.rineau@cgal.org>
2025-05-19 17:14:57 +0200

    review Mael

--- Constrained_triangulation_3/doc/Constrained_triangulation_3/Concepts/ConformingConstrainedDelaunayTriangulationVertexBase_3.h
+++ Constrained_triangulation_3/doc/Constrained_triangulation_3/Concepts/ConformingConstrainedDelaunayTriangulationVertexBase_3.h
@@ -3,9 +3,8 @@
 \cgalConcept
 
 The concept `ConformingConstrainedDelaunayTriangulationVertexBase_3` refines the concept
-`TriangulationVertexBase_3` and is the base vertex class for
-the triangulation returned by
-`CGAL::make_conforming_constrained_Delaunay_triangulation_3()` function template.
+`TriangulationVertexBase_3` and describes the requirements for a base vertex class of
+the `CGAL::Conforming_constrained_Delaunay_triangulation_3` class.
 
 \cgalRefines{TriangulationVertexBase_3, BaseWithTimeStamp}
 
@@ -24,7 +23,7 @@ public:
   /// The following functions return a reference to an object of type
   /// `CGAL::Conforming_constrained_Delaunay_triangulation_vertex_data_3`, that contains
   /// the per-vertex data required by the implementation of the
-  /// `CGAL::make_conforming_constrained_Delaunay_triangulation_3()` function template.
+  /// `CGAL::Conforming_constrained_Delaunay_triangulation_3` class.
   /// @{
   CGAL::Conforming_constrained_Delaunay_triangulation_vertex_data_3& ccdt_3_data();
   const CGAL::Conforming_constrained_Delaunay_triangulation_vertex_data_3& ccdt_3_data() const;
