bb611272543fce8c6b21735c383c954421bf130c
Laurent Rineau <laurent.rineau@cgal.org>
2025-05-21 15:34:35 +0200

    always use read_polygon_mesh and write_MEDIT

--- Constrained_triangulation_3/examples/Constrained_triangulation_3/ccdt_3_preprocessing.cpp
+++ Constrained_triangulation_3/examples/Constrained_triangulation_3/ccdt_3_preprocessing.cpp
@@ -1,5 +1,6 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
+#include <CGAL/IO/write_MEDIT.h>
 #include <CGAL/Surface_mesh/Surface_mesh.h>
 #include <CGAL/Polygon_mesh_processing/self_intersections.h>
 #include <CGAL/Polygon_mesh_processing/autorefinement.h>
@@ -22,8 +23,7 @@ int main(int argc, char* argv[])
                        : CGAL::data_file_path("meshes/mpi_and_sphere.off");
 
   CGAL::Surface_mesh<K::Point_3> mesh;
-  std::ifstream in(filename);
-  if(!in || !(in >> mesh)) {
+  if(!CGAL::IO::read_polygon_mesh(filename, mesh)) {
     std::cerr << "Error: cannot read file " << filename << std::endl;
     return EXIT_FAILURE;
   }
@@ -52,6 +52,7 @@ int main(int argc, char* argv[])
     }
   }
 
+  CGAL::Conforming_constrained_Delaunay_triangulation_3<K> ccdt;
   if(triangle_mesh && PMP::does_self_intersect(mesh))
   {
     std::cout << "Mesh is a self-intersecting triangle mesh, perform autorefinement...\n";
@@ -64,25 +65,25 @@ int main(int argc, char* argv[])
     std::cout << "Number of facets after preprocessing: "
               << polygons.size() << "\n";
 
-    auto ccdt = CGAL::make_conforming_constrained_Delaunay_triangulation_3(points, polygons);
+    ccdt = CGAL::make_conforming_constrained_Delaunay_triangulation_3(points, polygons);
 
-    std::cout << "Number of constrained facets in the CDT: "
-              << ccdt.number_of_constrained_facets() << '\n';
-
-    CGAL::draw(ccdt);
   }
   else
   {
     std::cout << "Number of facets after preprocessing: "
               << mesh.number_of_faces() << "\n";
 
-    auto ccdt = CGAL::make_conforming_constrained_Delaunay_triangulation_3(mesh);
-
-    std::cout << "Number of constrained facets in the CDT: "
-              << ccdt.number_of_constrained_facets() << '\n';
-
-    CGAL::draw(ccdt);
+    ccdt = CGAL::make_conforming_constrained_Delaunay_triangulation_3(mesh);
   }
+
+  std::cout << "Number of constrained facets in the CDT: "
+            << ccdt.number_of_constrained_facets() << '\n';
+
+  std::ofstream ofs(argc > 2 ? argv[2] : "out.mesh");
+  ofs.precision(17);
+  CGAL::IO::write_MEDIT(ofs, ccdt);
+
+  CGAL::draw(ccdt);
 
   return EXIT_SUCCESS;
 }
--- Constrained_triangulation_3/examples/Constrained_triangulation_3/conforming_constrained_Delaunay_triangulation_3_from_soup.cpp
+++ Constrained_triangulation_3/examples/Constrained_triangulation_3/conforming_constrained_Delaunay_triangulation_3_from_soup.cpp
@@ -1,5 +1,6 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/IO/polygon_soup_io.h>
+#include <CGAL/IO/write_MEDIT.h>
 #include <CGAL/draw_constrained_triangulation_3.h>
 #include <CGAL/make_conforming_constrained_Delaunay_triangulation_3.h>
 
@@ -9,10 +10,10 @@ using K = CGAL::Exact_predicates_inexact_constructions_kernel;
 
 int main(int argc, char* argv[])
 {
+  auto filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/cubes.off");
+
   std::vector<K::Point_3> points;
   std::vector<std::vector<std::size_t>> polygons;
-
-  auto filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/cubes.off");
   if(!CGAL::IO::read_polygon_soup(filename, points, polygons)) {
     std::cerr << "Error: cannot read file " << filename << std::endl;
     return EXIT_FAILURE;
@@ -27,6 +28,10 @@ int main(int argc, char* argv[])
             << ccdt.triangulation().number_of_vertices() << '\n'
             << "Number of constrained facets in the CDT: "
             << ccdt.number_of_constrained_facets() << '\n';
+
+  std::ofstream ofs(argc > 2 ? argv[2] : "out.mesh");
+  ofs.precision(17);
+  CGAL::IO::write_MEDIT(ofs, ccdt);
 
   CGAL::draw(ccdt);
 
