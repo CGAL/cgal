169383bdadde0d14e19ef257a23f7b2aced8fe18
hofwe <soichiro19998@gmail.com>
2025-06-16 11:41:43 +0900

    resolution for the Debug mode compilation problem

--- Linear_cell_complex/examples/Linear_cell_complex/query_replace/cmap_copy.h
+++ Linear_cell_complex/examples/Linear_cell_complex/query_replace/cmap_copy.h
@@ -72,7 +72,8 @@ std::size_t copy_cell(CMap1& amap1, typename CMap1::Dart_handle dh,
     }
   }
 
-  unsigned int min_dim=std::min(amap1.dimension, amap2.dimension);
+  // unsigned int min_dim=std::min(amap1.dimension, amap2.dimension);
+  unsigned int min_dim=3;
 
   typename std::unordered_map<typename CMap1::Dart_handle,
       typename CMap2::Dart_handle>::iterator
