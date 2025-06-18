d169a542a0ade44ca94372b1aed5ab30b9f6dde7
Laurent Rineau <laurent.rineau@cgal.org>
2025-06-18 12:17:16 +0200

    bench simple_bench_DT3 with ou without indices
    So far, indices uses less memory but more time:
      15.4949 sec (for 100 runs)
    instead of
      14.1458 sec

--- Triangulation_3/benchmark/Triangulation_3/simple_bench_DT3.cpp
+++ Triangulation_3/benchmark/Triangulation_3/simple_bench_DT3.cpp
@@ -2,6 +2,7 @@
 //#define CGAL_USE_SSE2_FABS
 //#define CGAL_USE_SSE2_MAX
 //#define CGAL_MSVC_USE_STD_FABS  // use this one with precise
+//#define INDEX_STORAGE 1
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
@@ -13,20 +14,28 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <clocale>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel  K;
+#if INDEX_STORAGE
 typedef CGAL::Triangulation_data_structure_3<CGAL::VertexWithPoint<K>, CGAL::Cell4Delaunay<K>, CGAL::Sequential_tag, CGAL::Index_tag> Tds;
-typedef CGAL::Delaunay_triangulation_3<K,Tds, CGAL::Compact_location>                 DT;
+typedef CGAL::Delaunay_triangulation_3<K,Tds>                DT;
+#else
+typedef CGAL::Delaunay_triangulation_3<K>                    DT;
+#endif
 typedef DT::Point                                            Point_3;
 typedef CGAL::Timer                                          Timer;
 typedef CGAL::Memory_sizer                                   Memory_sizer;
 
 int main(int argc, char* argv[])
 {
-
+  std::locale loc = std::locale()
+      .combine<std::numpunct<char>>(std::locale("en_US.UTF8"));
+  std::cout.imbue(loc);
   Memory_sizer memory_sizer;
+  auto res_mem_at_start = memory_sizer.resident_size();
   std::cout << "Memory usage right at start:\n" << memory_sizer.virtual_size() << " bytes (virtual), "
-            << memory_sizer.resident_size() << " bytes (resident)" << std::endl;
+            << res_mem_at_start << " bytes (resident)" << std::endl;
 
   const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("points_3/ocean_r.xyz");
   std::ifstream in(filename.c_str());
@@ -41,21 +50,27 @@ int main(int argc, char* argv[])
 
   Timer timer;
   timer.start();
-  int M = 1; // Number of times to compute the triangulation
+  int M = 100; // Number of times to compute the triangulation
   std::cout << "Compute triangulation "<< M << " times" << std::endl;
   for(int i = 0; i < M; i++){
     DT dt;
     dt.insert(points.begin(), points.end());
     std::cout << "Number of cells: " << dt.number_of_cells() << std::endl;
-    std::cout << "Memory usage after construction of the triangulaiton:\n" << memory_sizer.virtual_size() << " bytes (virtual), "
-            << memory_sizer.resident_size() << " bytes (resident)" << std::endl;
+    auto res_mem = memory_sizer.resident_size();
+    std::cout << "Memory usage after construction of the triangulation:\n" << memory_sizer.virtual_size() << " bytes (virtual), "
+            << res_mem << " bytes (resident)" << std::endl;
+    std::cout << "Diff in resident memory: "
+              << res_mem - res_mem_at_start << " bytes" << std::endl;
   }
 
   timer.stop();
   std::cout << "Time elapsed: " << timer.time() << " sec" << std::endl;
 
-std::cout << "Memory usage after deallocation of the triangulation:\n" << memory_sizer.virtual_size() << " bytes (virtual), "
-            << memory_sizer.resident_size() << " bytes (resident)" << std::endl;
+  auto res_mem = memory_sizer.resident_size();
+  std::cout << "Memory usage after deallocation of the triangulation:\n" << memory_sizer.virtual_size() << " bytes (virtual), "
+            << res_mem << " bytes (resident)" << std::endl;
+  std::cout << "Diff in resident memory: "
+            << res_mem - res_mem_at_start << " bytes" << std::endl;
 
   return 0;
 }
