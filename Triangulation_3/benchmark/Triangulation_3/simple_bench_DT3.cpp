067e0636e3040480c24f1193dd0e78ac7c6dc9a8
Laurent Rineau <laurent.rineau@cgal.org>
2025-06-19 16:07:38 +0200

    add parallel tag to simple bench

--- Triangulation_3/benchmark/Triangulation_3/simple_bench_DT3.cpp
+++ Triangulation_3/benchmark/Triangulation_3/simple_bench_DT3.cpp
@@ -4,6 +4,9 @@
 //#define CGAL_MSVC_USE_STD_FABS  // use this one with precise
 //#define INDEX_STORAGE 1
 
+#include "CGAL/Bbox_3.h"
+#include "CGAL/Triangulation_data_structure_3.h"
+#include "CGAL/Triangulation_vertex_base_3.h"
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
 
@@ -16,13 +19,23 @@
 #include <fstream>
 #include <clocale>
 
+#if PARALLEL
+using Concurrent_tag = CGAL::Parallel_tag;
+#else
+using Concurrent_tag = CGAL::Sequential_tag;
+#endif
 typedef CGAL::Exact_predicates_inexact_constructions_kernel  K;
 #if INDEX_STORAGE
-typedef CGAL::Triangulation_data_structure_3<CGAL::VertexWithPoint<K>, CGAL::Cell4Delaunay<K>, CGAL::Sequential_tag, CGAL::Index_tag> Tds;
-typedef CGAL::Delaunay_triangulation_3<K,Tds>                DT;
+typedef CGAL::VertexWithPoint<K> Vb;
+typedef CGAL::Cell4Delaunay<K> Cb;
+typedef CGAL::Index_tag Tds_type_tag;
 #else
-typedef CGAL::Delaunay_triangulation_3<K>                    DT;
+typedef CGAL::Triangulation_vertex_base_3<K> Vb;
+typedef CGAL::Delaunay_triangulation_cell_base_3<K> Cb;
+typedef CGAL::Handle_tag Tds_type_tag;
 #endif
+typedef CGAL::Triangulation_data_structure_3<Vb, Cb, Concurrent_tag, Tds_type_tag> Tds;
+typedef CGAL::Delaunay_triangulation_3<K,Tds>                DT;
 typedef DT::Point                                            Point_3;
 typedef CGAL::Timer                                          Timer;
 typedef CGAL::Memory_sizer                                   Memory_sizer;
@@ -53,7 +66,12 @@ int main(int argc, char* argv[])
   int M = 100; // Number of times to compute the triangulation
   std::cout << "Compute triangulation "<< M << " times" << std::endl;
   for(int i = 0; i < M; i++){
+#if PARALLEL
+    DT::Lock_data_structure lock_data_structure{CGAL::bbox_3(points.begin(), points.end()), 50};
+    DT dt(&lock_data_structure);
+#else
     DT dt;
+#endif
     dt.insert(points.begin(), points.end());
     std::cout << "Number of cells: " << dt.number_of_cells() << std::endl;
     auto res_mem = memory_sizer.resident_size();
