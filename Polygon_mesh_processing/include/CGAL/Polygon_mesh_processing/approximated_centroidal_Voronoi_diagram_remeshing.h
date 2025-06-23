6905ab2b21cf7676f025a2858e0cd4c44887e3f1
Sébastien Loriot <sebastien.loriot@cgal.org>
2025-06-23 12:01:05 +0200

    backticks

--- Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/approximated_centroidal_Voronoi_diagram_remeshing.h
+++ Polygon_mesh_processing/include/CGAL/Polygon_mesh_processing/approximated_centroidal_Voronoi_diagram_remeshing.h
@@ -1391,7 +1391,7 @@ acvd_impl(TriangleMesh& tmesh,
 * \cgalNamedParamsEnd
 *
 *
-* @pre tmesh is a triangulated surface mesh and has exactly one connected component.
+* @pre `tmesh` is a triangulated surface mesh and has exactly one connected component.
 *
 * @return `true` if `nb_vertices` was sufficiently large for remeshing the input, and `false` if more points were used
 *
