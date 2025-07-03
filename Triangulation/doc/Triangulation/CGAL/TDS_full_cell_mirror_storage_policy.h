ce87535e11040eac86897c657a72c56c0fdecaf2
Sébastien Loriot <sebastien.loriot@cgal.org>
2025-07-03 14:05:46 +0200

    fix doxygen command

--- Triangulation/doc/Triangulation/CGAL/TDS_full_cell_mirror_storage_policy.h
+++ Triangulation/doc/Triangulation/CGAL/TDS_full_cell_mirror_storage_policy.h
@@ -4,7 +4,7 @@ namespace CGAL {
   /*!
   \ingroup PkgTriangulationsRef
   A tag class to indicate that mirror indices are stored in full cells.
-  \precondition The dimension is not larger than 127.
+  \pre The dimension is not larger than 127.
   */
 class TDS_full_cell_mirror_storage_policy
 {};
