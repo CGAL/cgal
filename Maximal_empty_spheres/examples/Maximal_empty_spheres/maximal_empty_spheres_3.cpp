448323bc358b4c90c56a9efe80db893f39553d0a
Max Kohlbrenner <kohlbrenner@tu-berlin.de>
2025-07-10 16:26:14 +0200

    Init Maximal_empty_spheres dir structure

---
+++ Maximal_empty_spheres/examples/Maximal_empty_spheres/maximal_empty_spheres_3.cpp
@@ -0,0 +1,6 @@
+#include <CGAL/Maximal_empty_spheres/maximal_empty_spheres.h>
+
+int main(){
+    CGAL::maximal_empty_spheres_dummy();
+    return 0;
+}
---
+++ Maximal_empty_spheres/include/CGAL/Maximal_empty_spheres/maximal_empty_spheres.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2025  TU Berlin
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+// Author(s)     : Max Kohlbrenner
+
+#ifndef CGAL_MAXIMAL_EMPTY_SPHERES_MAXIMAL_EMPTY_SPHERES_H
+#define CGAL_MAXIMAL_EMPTY_SPHERES_MAXIMAL_EMPTY_SPHERES_H
+
+// #include <CGAL/license/Maximal_empth_spheres.h>
+#include <iostream>
+#include <Eigen/Core>
+
+namespace CGAL {
+    void maximal_empty_spheres_dummy(){
+        Eigen::MatrixXd X;
+        std::cout << "Maximal Empty Spheres Hello" << std::endl;
+    }
+}
+
+#endif
