ac660b2c8a4feb2dfbc657ede8cc1233d2ac46a1
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-07-11 09:26:37 +0100

    Changes so that manual builds

--- Maximal_empty_spheres/include/CGAL/Maximal_empty_spheres/maximal_empty_spheres.h
+++ Maximal_empty_spheres/include/CGAL/Maximal_empty_spheres/maximal_empty_spheres.h
@@ -17,10 +17,18 @@
 #include <Eigen/Core>
 
 namespace CGAL {
-    void maximal_empty_spheres_dummy(){
+
+    /*!
+     * \ingroup PkgMaximalEmptySpheresFunctions
+     * \brief A dummy function to demonstrate the structure of the library.
+     *
+     * This function is a placeholder and does not perform any operations.
+     * It is intended to be replaced with actual functionality in the future.
+     */
+    void maximal_empty_spheres(){
         Eigen::MatrixXd X;
         std::cout << "Maximal Empty Spheres Hello" << std::endl;
     }
-}
+} // namespace CGAL
 
-#endif
+#endif // GAL_MAXIMAL_EMPTY_SPHERES_MAXIMAL_EMPTY_SPHERES_H
