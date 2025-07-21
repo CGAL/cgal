4e93ea15391982d721f9874e4cecd631f0b331cd
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-07-21 17:04:33 +0100

    work in progress

--- Maximal_empty_spheres/examples/Maximal_empty_spheres/maximal_empty_spheres_3.cpp
+++ Maximal_empty_spheres/examples/Maximal_empty_spheres/maximal_empty_spheres_3.cpp
@@ -1,6 +1,22 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Maximal_empty_spheres/maximal_empty_spheres.h>
+#include <iostream>
+#include <vector>
+
+using Sphere_3 = CGAL::Exact_predicates_inexact_constructions_kernel::Sphere_3;
+using Point_3 = CGAL::Exact_predicates_inexact_constructions_kernel::Point_3;
 
 int main(){
-    CGAL::maximal_empty_spheres_dummy();
+    std::vector<Sphere_3> spheres, result;
+    std::ifstream in("data/3D/spheres.csv");
+    double x, y, z, r;
+    while(in >> x){
+
+        in.ignore(10,','); in >> y;  in.ignore(10,','); in >> z; in.ignore(10,','); in >> r;
+        spheres.emplace_back(Point_3(x, y, z), r);
+        std::cout << "Sphere: " << spheres.back() << std::endl;
+
+    }
+    CGAL::maximal_empty_spheres_3(spheres, std::back_inserter(result));
     return 0;
 }
--- Maximal_empty_spheres/include/CGAL/Maximal_empty_spheres/maximal_empty_spheres.h
+++ Maximal_empty_spheres/include/CGAL/Maximal_empty_spheres/maximal_empty_spheres.h
@@ -12,11 +12,187 @@
 #ifndef CGAL_MAXIMAL_EMPTY_SPHERES_MAXIMAL_EMPTY_SPHERES_H
 #define CGAL_MAXIMAL_EMPTY_SPHERES_MAXIMAL_EMPTY_SPHERES_H
 
-// #include <CGAL/license/Maximal_empth_spheres.h>
+// #include <CGAL/license/Maximal_empty_spheres.h>
+
+#include <CGAL/Maximal_empty_spheres/internal/Lie_geometry.h>
+#include <CGAL/Maximal_empty_spheres/internal/rotation_from_to.h>
+
 #include <iostream>
 #include <Eigen/Core>
 
+#include <CGAL/Epick_d.h>
+#include <CGAL/Triangulation.h>
+#include <CGAL/algorithm.h>
+
 namespace CGAL {
+
+template<typename Dimension>
+void maximal_empty_spheres(const Eigen::MatrixXd &G, Eigen::MatrixXd &result, double atol=1e-8, int debug_level=0) {
+
+    bool full_simplices_only=true;
+    const int D = Dimension::value;
+    assert( D == G.cols()-1 && "Dimension does not match the number of columns in G");
+
+    typedef CGAL::Epick_d< CGAL::Dimension_tag<D+2> >  Kernel;
+    typedef CGAL::Triangulation_vertex<Kernel, std::ptrdiff_t>                                      Vertex;
+    typedef CGAL::Triangulation_ds_full_cell<void,CGAL::TDS_full_cell_mirror_storage_policy>        DS_full_cell;
+    typedef CGAL::Triangulation_full_cell<Kernel, int, DS_full_cell>                                Full_cell;
+    typedef CGAL::Triangulation_data_structure<CGAL::Dimension_tag<D+2>,
+                                               Vertex,
+                                               Full_cell>                                          TDS;
+    typedef CGAL::Triangulation<Kernel,TDS>                                                         Triangulation;
+    typedef Triangulation::Vertex_handle                                                            Vertex_handle;
+
+    using namespace CGAL::Maximal_empty_spheres::internal;
+
+    std::cout << "Loaded SDF values of shape (" << G.rows() << ", " << G.cols() << ")"  << std::endl;
+
+    Eigen::MatrixXd SE = G;
+    SE.col(D) = G.col(D).array().abs();
+
+    // std::cout << "SE: " << std::endl;
+    // std::cout << SE << std::endl;
+
+    std::cout << "Convert to Lie representation" << std::endl;
+    Eigen::MatrixXd H;
+    lie_ip_matrix(D,H);
+
+    Eigen::MatrixXd SL;
+    spheres_to_lie(SE,SL);
+    // std::cout << "Lie Repr: " << std::endl << SL << std::endl;
+
+    Eigen::MatrixXd NC = SL * H;
+
+    // std::cout << "... on quadric? ";
+    // std::cout << std::boolalpha << ( ((NC.array() * SL.array()).rowwise().sum()).matrix().norm() < atol) << std::endl;
+
+    std::cout << std::boolalpha;
+
+    Eigen::VectorXd s0  = Eigen::VectorXd::Zero(D+3);
+    s0(D+2) = -1.;
+    s0(D)   = -1.;
+    Eigen::VectorXd dst = Eigen::VectorXd::Zero(D+3);
+    dst[D+2] = 1.;
+    Eigen::MatrixXd R = rotation_from_to(s0, dst);
+
+    std::cout << "All normals in the same halfspace? : " <<  (((NC*s0).transpose()).array() >= atol).all() << std::endl;
+
+    Eigen::MatrixXd NCR = NC * R.transpose(); // rotate
+    std::cout << "Last component greater than zero?  : "  << (NCR.col(D+2).array()    >= atol).all() << std::endl;
+    NCR.array().colwise() /= NCR.array().col(D+2);
+    Eigen::MatrixXd Np = NCR.block(0,0,NCR.rows(),D+2);
+
+    // ------------------------ CONVEX HULL COMPUTATION -------------------------
+
+    // --> convert to point list --
+    std::vector<Triangulation::Point> points;
+    points.reserve(int(Np.rows()));
+    std::vector<Kernel::FT> p_;
+    p_.reserve(D+2);
+    for (int i=0; i<Np.rows(); i++){
+        p_.clear();
+        for (int d=0; d<D+2; d++){
+            p_.push_back(Np(i,d));
+        }
+        points.push_back(Triangulation::Point(p_.begin(),p_.end()));
+    }
+
+    Triangulation t(D+2);
+    t.insert_and_index(points.begin(), points.end());
+    // --> recover the infinite cells (contain the convex hull)
+    std::vector<Triangulation::Full_cell_handle> infinite_cells;
+    for(auto it = t.full_cells_begin(); it != t.full_cells_end(); ++it) {
+        if(t.is_infinite(it)) {
+            infinite_cells.push_back(it);
+        }
+    }
+
+    // --> get a unique index per cell
+    int ci=0;
+    for(auto ch : infinite_cells) ch->data() = ci++;
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd;
+    Eigen::MatrixXd Ks(infinite_cells.size(),D+3);
+    Eigen::Vector<bool,Eigen::Dynamic> full_simplices(infinite_cells.size());
+
+    Eigen::MatrixXd simplex(D+2,D+3);
+    Eigen::RowVectorXd simplex_row(D+3);
+    int ki=0;
+    for(auto ch : infinite_cells) {
+
+        int si=0;
+        for (int i=0; i<D+3; i++) {
+             if(ch->vertex(i) != t.infinite_vertex()) {
+                 // std::cout << point_indices[ch->vertex(i)] << std::endl;
+                 simplex.row(si++) = NC.row(ch->vertex(i)->data());
+             }
+        }
+        // std::cout << "simplex: " << std::endl << simplex << std::endl;
+        svd.compute(simplex, Eigen::ComputeFullV);
+        Ks.row(ki)         = svd.matrixV().col(D+2).transpose();
+        full_simplices(ki) = svd.singularValues().array().abs().minCoeff() > atol;
+        ki++;
+
+        if (svd.singularValues().array().abs().minCoeff() <= atol) {
+            std::cout << "Not a full simplex" << std::endl;
+        }
+    }
+
+    // std::cout << "Ks.shape: (" << Ks.rows() << ", " << Ks.cols() << ")" << std::endl;
+    // std::cout << "Ks: " << std::endl << Ks << std::endl;
+    // std::cout << "Ks * NC.T" << std::endl << Ks * NC.transpose() << std::endl;
+
+    int n_check_planes = (NC.rows()<=1000)? NC.rows(): 1000;
+    // std::cout << "n_check_planes: " << n_check_planes << std::endl;
+    std::cout << "NC.shape(): " << NC.rows() << ", " << NC.cols() << std::endl;
+    Eigen::Vector<bool,Eigen::Dynamic> inv = ((Ks * NC.block(0,0,n_check_planes,D+3).transpose()).rowwise().maxCoeff().array() >= atol); // .rowwise().maxCoeff().array() >= 1e-5) << std::endl;
+    for (int i=0; i<Ks.rows(); i++) if (inv(i)) Ks.row(i) *= -1;
+
+    if (debug_level > 3) {
+        // costly and not possible for large sets of spheres
+        std::cout << "Debugging, remove later (matrices too large), all Ks in the cone?: ";
+        std::cout << ((NC * Ks.transpose()).rowwise().maxCoeff().array() <= atol).array().all() << std::endl;
+    }
+
+    std::vector<Eigen::RowVectorXd> solutions_;
+    for(auto ch : infinite_cells) {
+        int ci = ch->data();
+        for (int i=0; i<D+3; i++) {
+             if(ch->vertex(i) != t.infinite_vertex()) {
+                int cj =  ch->neighbor(i)->data();
+                if ((ci < cj) &&    // only treat each edge once
+                    (!full_simplices_only || (full_simplices(ci) && full_simplices(cj)))  // only consider edges between full simplices
+                    ) {
+                    double l1,l2;
+                    line_quadric_intersection(Ks.row(ci), Ks.row(cj), H, l1,l2);
+                    if ((0.<=l1) && (l1<=1.))solutions_.push_back((1-l1)*Ks.row(ci)+l1*Ks.row(cj));
+                    if ((0.<=l2) && (l2<=1.))solutions_.push_back((1-l2)*Ks.row(ci)+l2*Ks.row(cj));
+                }
+             }
+        }
+    }
+
+    Eigen::MatrixXd solutions(solutions_.size(),D+3);
+    for (int i=0; i<solutions_.size(); i++) solutions.row(i) = solutions_[i];
+    std::cout << "Solutions.size: " << solutions.rows() << ", " << solutions.cols() << std::endl;
+
+    if (debug_level > 3) {
+        std::cout << "Debugging, remove later (matrices too large), all solutions in the cone?: ";
+        std::cout << ((NC * solutions.transpose()).rowwise().maxCoeff().array() <= atol).array().all() << std::endl;
+    }
+    // ------- Post selection ------------
+    Eigen::Vector<bool,Eigen::Dynamic> neg_convention = (solutions.col(D+2).array() < 0.);
+    Eigen::Vector<bool,Eigen::Dynamic> neg_radius     = (solutions.col(D+1).array().sign() != solutions.col(D+2).array().sign());
+
+    std::vector<Eigen::RowVectorXd> solutions_filtered_;
+    for (int i=0; i<solutions.rows(); i++) if (neg_convention(i) && neg_radius(i)) solutions_filtered_.push_back(solutions.row(i));
+
+    Eigen::MatrixXd solutions_filtered(solutions_filtered_.size(),D+3);
+    for (int i=0; i<solutions_filtered_.size(); i++) solutions_filtered.row(i) = solutions_filtered_[i];
+    std::cout << "solutions_filtered.shape: (" << solutions_filtered.rows() << ", " << solutions_filtered.cols() << ")"  << std::endl;
+
+    lie_to_spheres(solutions_filtered, result);
+    }
 
     /*!
      * \ingroup PkgMaximalEmptySpheresFunctions
@@ -25,9 +201,17 @@ namespace CGAL {
      * This function is a placeholder and does not perform any operations.
      * It is intended to be replaced with actual functionality in the future.
      */
-    void maximal_empty_spheres(){
-        Eigen::MatrixXd X;
-        std::cout << "Maximal Empty Spheres Hello" << std::endl;
+    template<typename SphereRange, typename OutputIterator>
+    void maximal_empty_spheres_3(const SphereRange& input, OutputIterator result) {
+        Eigen::MatrixXd G(input.size(),4), res;
+        for(auto it = input.begin(); it != input.end(); ++it) {
+            G.row(it - input.begin()) = Eigen::RowVector4d(it->center().x(), it->center().y(), it->center().z(), it->squared_radius());
+        }
+        maximal_empty_spheres<Dimension_tag<3>>(G, res);
+        const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ", ", "\n");
+        // write soution in lie representation:
+        std::ofstream pointfile("solution_lie.csv");
+        pointfile << res.format(CSVFormat);
     }
 } // namespace CGAL
 
