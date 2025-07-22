39cabf4a67f273fe321a7178380919143a2f4588
Max Kohlbrenner <kohlbrenner@tu-berlin.de>
2025-07-22 19:01:36 +0200

    Maximal_empty_spheres: continue reconstruction notebook (unfinished)

--- Maximal_empty_spheres/examples/Maximal_empty_spheres/empty_spheres_reconstruction_3.cpp
+++ Maximal_empty_spheres/examples/Maximal_empty_spheres/empty_spheres_reconstruction_3.cpp
@@ -14,6 +14,8 @@ int main(){
     while(in >> x){
 
         in.ignore(10,','); in >> y;  in.ignore(10,','); in >> z; in.ignore(10,','); in >> r;
+        // I only filter out the positive spheres here as a first demo. The proper version uses positive / negative spheres separately.
+        // TODO: implement the proper version
         if (r >= 0) input_spheres.emplace_back(Eigen::RowVector4d(x,y,z,fabs(r)));
         // std::cout << "Sphere: " << x << y << z << r << std::endl;
     }
@@ -27,22 +29,35 @@ int main(){
     Eigen::MatrixXd res;
     CGAL::maximal_empty_spheres<CGAL::Dimension_tag<3>>(G, res, &contact_indices);
 
-    std::cout << "Res.shape:             " << res.rows() << ", " << res.cols() << std::endl;
-    std::cout << "contact_indices.shape: " << contact_indices.rows() << ", " << contact_indices.cols() << std::endl;
-    std::cout << contact_indices << std::endl;
+    // std::cout << res << std::endl;
+    // std::cout << "contact_indices:" << std::endl;
+    // std::cout << contact_indices << std::endl;
 
-    /*
-    std::vector<Sphere_3> spheres, result;
-    std::ifstream in("data/3D/spheres.csv");
-    double x, y, z, r;
-    while(in >> x){
-
-        in.ignore(10,','); in >> y;  in.ignore(10,','); in >> z; in.ignore(10,','); in >> r;
-        spheres.emplace_back(Point_3(x, y, z), r*r);
-        std::cout << "Sphere: " << spheres.back() << std::endl;
-
+    // contact_indices contains the indices G and contains the spheres that each spheres of res is adjacent to.
+    // calculate the contact sphere with largest (absolute) radius for each sphere in G:
+    // (careful: results spheres all have negative radius)
+    // TODO: filter out the contact spheres outside of a bounding box
+    Eigen::VectorXi contact_point_indices = Eigen::VectorXi::Constant(G.rows(),-1);
+    Eigen::VectorXd contact_point_radii   = Eigen::VectorXd::Constant(G.rows(),-1.);
+    for (int i=0; i<contact_indices.rows(); i++){
+        double r= fabs(res(i,3)); // contact spheres have negative radius, use abs value here
+        for (int j=0; j<contact_indices.cols(); j++){
+            int n = contact_indices(i,j);
+            if ( (contact_point_indices(n) < 0) || ((contact_point_indices(n) >= 0) && (contact_point_radii(n) < r)) ){
+                contact_point_indices(n) = i;
+                contact_point_radii(n)   = r;
+            } 
+        }
     }
-    CGAL::maximal_empty_spheres(spheres, std::back_inserter(result));
-    */
+
+    std::cout << "CP Indices: " << std::endl;
+    std::cout << contact_point_indices.transpose() << std::endl;
+
+    std::cout << "CP Radii: " << std::endl;
+    std::cout << contact_point_radii.transpose() << std::endl;
+
+    // TODO: calculate the contact points and normals
+    // TODO: integrate Poisson Reconstruction
+
     return 0;
 }
