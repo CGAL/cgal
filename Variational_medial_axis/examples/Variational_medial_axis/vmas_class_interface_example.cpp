4dc31c87a97436269f7dcbf00b62250e66ee57a3
huang qijia <qijiahuang1@gmail.com>
2025-07-29 19:24:05 +0200

    Add example of free function and rename the examples files

--- Variational_medial_axis/examples/Variational_medial_axis/vmas_class_interface_example.cpp
+++ Variational_medial_axis/examples/Variational_medial_axis/vmas_class_interface_example.cpp
@@ -5,28 +5,9 @@
 using K = CGAL::Exact_predicates_inexact_constructions_kernel;
 using Mesh = CGAL::Surface_mesh<K::Point_3>;
 
-void normalize_mesh(Mesh& mesh) {
-  CGAL::Bbox_3 bbox;
-  for(auto v : mesh.vertices())
-    bbox = bbox + mesh.point(v).bbox();
-
-  double cx = (bbox.xmin() + bbox.xmax()) / 2.0;
-  double cy = (bbox.ymin() + bbox.ymax()) / 2.0;
-  double cz = (bbox.zmin() + bbox.zmax()) / 2.0;
-  double max_dim = std::max({bbox.xmax() - bbox.xmin(), bbox.ymax() - bbox.ymin(), bbox.zmax() - bbox.zmin()});
-
-  for(auto v : mesh.vertices()) {
-    K::Point_3 p = mesh.point(v);
-    double nx = (p.x() - bbox.xmin()) / max_dim;
-    double ny = (p.y() - bbox.ymin()) / max_dim;
-    double nz = (p.z() - bbox.zmin()) / max_dim;
-    mesh.point(v) = K::Point_3(nx, ny, nz);
-  }
-}
-
 int main(int argc, char** argv)
 { 
-  const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/elephant.off");
+  const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/chair.off");
 
   Mesh mesh;
   if(!CGAL::IO::read_polygon_mesh(filename, mesh))
@@ -34,16 +15,22 @@ int main(int argc, char** argv)
     std::cerr << "Invalid input." << std::endl;
     return 1;
   }
-  normalize_mesh(mesh);
   
   CGAL::Variational_medial_axis<Mesh> vmas(mesh);
   vmas.init();
   // Compute medial axis with custom parameters
   //vmas.compute_variational_medial_axis_sampling(CGAL::parameters::number_of_iterations(2000).number_of_spheres(1000).lambda(1.0).concurrency_tag(CGAL::Parallel_tag{}));
   vmas.compute_variational_medial_axis_sampling(
-      CGAL::parameters::number_of_iterations(1000).number_of_spheres(200).lambda(0.2).concurrency_tag(
-      CGAL::Parallel_tag{}));
-  // Export skeleton
+      CGAL::parameters::number_of_iterations(1000)// number of max iterations
+      .number_of_spheres(200) // target number of spheres
+      .lambda(0.2) // lambda parameter for the optimization
+      .concurrency_tag(CGAL::Parallel_tag{})); // use parallel execution if available
+
+  // add additional 3 sphere, this function will update the medial skeleton automatically 
+  vmas.add_sphere(30);
+  // update the medial skeleton with 20 iterations
+  vmas.update(20);
+
   std::cout << "Exporting skeleton..." << std::endl;
   auto skeleton = vmas.export_skeleton();
 
---
+++ Variational_medial_axis/examples/Variational_medial_axis/vmas_free_function_example.cpp
@@ -0,0 +1,29 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/extract_variational_medial_skeleton.h>
+
+using K = CGAL::Exact_predicates_inexact_constructions_kernel;
+using Mesh = CGAL::Surface_mesh<K::Point_3>;
+using Medial_Skeleton = CGAL::Medial_Skeleton<Mesh>;
+
+int main(int argc, char** argv)
+{ 
+  const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/elephant.off");
+
+  Mesh mesh;
+  if(!CGAL::IO::read_polygon_mesh(filename, mesh))
+  {
+    std::cerr << "Invalid input." << std::endl;
+    return 1;
+  }
+
+  Medial_Skeleton skeleton = extract_variational_medial_skeleton(
+      mesh,
+      CGAL::parameters::number_of_iterations(1000).number_of_spheres(200).lambda(0.2).concurrency_tag(
+      CGAL::Parallel_tag{}));
+ 
+  // Write skeleton to PLY file
+  std::string output_filename = "skeleton.ply";
+  skeleton.write_to_ply_file(output_filename);
+
+}
