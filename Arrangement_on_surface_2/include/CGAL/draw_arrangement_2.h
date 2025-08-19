beee32e945d2853c31f39a0e7dcc8635d5381684
Efi Fogel <efifogel@gmail.com>
2025-08-19 17:38:37 +0300

    Guard for unbounded faces

--- Arrangement_on_surface_2/include/CGAL/draw_arrangement_2.h
+++ Arrangement_on_surface_2/include/CGAL/draw_arrangement_2.h
@@ -65,11 +65,13 @@ namespace draw_function_for_arrangement_2
            it != face->inner_ccbs_end(); ++it)
       { add_ccb(*it); }
 
-      for (Outer_ccb_const_iterator it = face->outer_ccbs_begin();
-           it != face->outer_ccbs_end(); ++it)
-      {
-        add_ccb(*it);
-        draw_region(*it);
+      if (! face->is_unbounded()) {
+        for (Outer_ccb_const_iterator it = face->outer_ccbs_begin();
+             it != face->outer_ccbs_end(); ++it)
+        {
+          add_ccb(*it);
+          draw_region(*it);
+        }
       }
     }
 
