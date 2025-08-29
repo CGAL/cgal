bf133b6de90c754c12f05d0dc85c004db697eeaa
lvalque <leo.valque@gmail.com>
2025-08-29 16:10:01 +0200

    float_snap_rounding example

--- Snap_rounding_2/examples/Snap_rounding_2/float_snap_rounding.cpp
+++ Snap_rounding_2/examples/Snap_rounding_2/float_snap_rounding.cpp
@@ -1,46 +1,68 @@
-// #define DOUBLE_2D_SNAP_VERBOSE
-
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Float_snap_rounding_2.h>
 #include <CGAL/Arr_segment_traits_2.h>
+#include <CGAL/Random.h>
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel       Kernel;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel     Epick;
 typedef CGAL::Arr_segment_traits_2<Kernel>                      Traits_2;
 typedef Traits_2::Curve_2                                       Segment_2;
 typedef Kernel::Point_2                          Point_2;
 typedef Kernel::FT                               FT;
 typedef std::vector<Point_2 > Polyline_2;
 
-int main()
+int main(int argc, char *argv[])
 {
-  std::vector<Point_2> pts;
   std::vector< Segment_2 > segs;
 
-  FT e(std::pow(2, -60));
+  if(argc>1){
+    std::cout << "Read segments in " << argv[1] << std::endl;
+    std::ifstream in(argv[1]);
 
-  segs.emplace_back(Point_2(1-e, 1), Point_2(-1-e, -1+2*e));
-  segs.emplace_back(Point_2(e/2, e/2), Point_2(1, -1));
-  segs.emplace_back(Point_2(0, 2-e/2), Point_2(2, 0));
-  segs.emplace_back(Point_2(0, 2-e/2), Point_2(-2+e, -4));
-  segs.emplace_back(Point_2(-2, 2), Point_2(2, 2));
-  segs.emplace_back(Point_2(7, 7), Point_2(7+e, 7+e));
-  segs.emplace_back(Point_2(5, 7-e), Point_2(9, 7-e));
+    int n;
+    in >> n;
+    segs.reserve(n);
+    for (int i=0; i<n; ++i)
+    {
+      double x1,x2,y1,y2;
+      in >> x1 >> y1 >> x2 >> y2;
+      if(Point_2(x1,y1)!=Point_2(x2,y2))
+        segs.emplace_back(Point_2(x1, y1), Point_2(x2, y2));
+    }
+  } else {
+    //Example with a non-trivial rounding
+    FT e(std::pow(2, -60));
 
-  std::cout << "Input" << std::endl;
-  for(auto &seg: segs){
-    std::cout << seg.source() << " -> " << seg.target() << std::endl;
+    segs.emplace_back(Point_2(1-e, 1), Point_2(-1-e, -1+2*e));
+    segs.emplace_back(Point_2(e/2, e/2), Point_2(1, -1));
+    segs.emplace_back(Point_2(0, 2-e/2), Point_2(2, 0));
+    segs.emplace_back(Point_2(0, 2-e/2), Point_2(-2+e, -4));
+    segs.emplace_back(Point_2(-2, 2), Point_2(2, 2));
+    segs.emplace_back(Point_2(7, 7), Point_2(7+e, 7+e));
+    segs.emplace_back(Point_2(5, 7-e), Point_2(9, 7-e));
   }
-  std::cout << "\n\n" << std::endl;
 
-  std::vector< Polyline_2 > out;
-  CGAL::double_snap_rounding_2(segs.begin(), segs.end(), out);
+  std::cout << "Computes the intersections and snaps the segments" << std::endl;
+  std::vector< Segment_2> out;
+  CGAL::compute_snapped_subcurves_2<CGAL::Parallel_if_available_tag>(segs.begin(), segs.end(), out);
+  std::cout << "Does the output intersect: " << CGAL::do_curves_intersect(out.begin(), out.end()) << std::endl;
+  std::cout << "Size of the output: " << out.size() << std::endl;
 
-  std::cout << "Output" << std::endl;
-  for(auto &poly: out){
-    std::cout << poly[0];
-    for(size_t i=1; i!=poly.size(); ++i)
-        std::cout << " -> " << poly[i];
-    std::cout << std::endl;
+  std::string out_path=(argc>2)?argv[2]:"out.segs";
+  std::cout << "Write the outputs in " << out_path << std::endl;
+  std::ofstream outf(out_path);
+  CGAL::Random r;
+
+  outf << std::setprecision(17);
+  outf << out.size() << std::endl;
+  for (auto &seg: out)
+  {
+    double x1 = CGAL::to_double(seg.source().x());
+    double y1 = CGAL::to_double(seg.source().y());
+    double x2 = CGAL::to_double(seg.target().x());
+    double y2 = CGAL::to_double(seg.target().y());
+    outf << x1 << " " << y1 << " " << x2 << " " << y2 << std::endl;
   }
 
   return 0;
