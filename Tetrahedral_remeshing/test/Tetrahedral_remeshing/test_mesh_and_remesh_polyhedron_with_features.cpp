5f6cba7e981e12019c131bf02eabdabd1796018a
Jane Tournois <jane.tournois@geometryfactory.com>
2025-09-18 12:58:07 +0200

    add stuff in the test

--- Tetrahedral_remeshing/test/Tetrahedral_remeshing/test_mesh_and_remesh_polyhedron_with_features.cpp
+++ Tetrahedral_remeshing/test/Tetrahedral_remeshing/test_mesh_and_remesh_polyhedron_with_features.cpp
@@ -7,8 +7,13 @@
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/Polyhedral_mesh_domain_with_features_3.h>
 #include <CGAL/make_mesh_3.h>
+#include <CGAL/property_map.h>
 
 #include <CGAL/tetrahedral_remeshing.h>
+
+#include <unordered_set>
+
+#include <CGAL/Timer.h>
 
 // Domain
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
@@ -21,6 +26,12 @@ using namespace CGAL::parameters;
 template <typename Concurrency_tag>
 void mesh(const Surface_mesh& mesh)
 {
+  if(std::is_convertible<Concurrency_tag, CGAL::Parallel_tag>::value)
+    std::cout << "* Concurrent Remeshing *" << std::endl;
+  else
+    std::cout << "* Sequential Remeshing *" << std::endl;
+  CGAL::Real_timer timer;
+
   // Triangulation
   typedef CGAL::Mesh_triangulation_3<Mesh_domain, CGAL::Default, Concurrency_tag>::type Tr;
   typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr, Mesh_domain::Corner_index, Mesh_domain::Curve_index> C3t3;
@@ -40,19 +51,55 @@ void mesh(const Surface_mesh& mesh)
     cell_radius_edge_ratio = 3, cell_size = 0.05);
 
   // Mesh generation
-  C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria);
+  timer.start();
+  C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria, no_perturb().no_exude());
+  timer.stop();
+  std::cout << "\tmeshing time     : " << timer.time() << " seconds" << std::endl;
 
-  auto t3 = CGAL::convert_to_triangulation_3(std::move(c3t3));
+  // Property maps of constraints
+  using Vertex_handle = typename Tr::Vertex_handle;
+  using Vertex_pair = std::pair<Vertex_handle, Vertex_handle>;
+  using Constraints_set = std::unordered_set<Vertex_pair, boost::hash<Vertex_pair>>;
+  using Constraints_pmap = CGAL::Boolean_property_map<Constraints_set>;
 
-  double target_edge_length = 0.1;//coarsen the mesh
+  using Corners_set = std::unordered_set<Vertex_handle, boost::hash<Vertex_handle>>;
+  using Corners_pmap = CGAL::Boolean_property_map<Corners_set>;
+
+  Constraints_set constraints;
+  Constraints_pmap constraints_pmap(constraints);
+
+  Corners_set corners;
+  Corners_pmap corners_pmap(corners);
+
+  auto t3 = CGAL::convert_to_triangulation_3(std::move(c3t3),
+                                             edge_is_constrained_map(constraints_pmap)
+                                            .vertex_is_constrained_map(corners_pmap));
+
+  double target_edge_length = 0.2;//coarsen the mesh
+  timer.reset();
+  timer.start();
   CGAL::tetrahedral_isotropic_remeshing(t3, target_edge_length,
-    number_of_iterations(3).remesh_boundaries(false));
+     number_of_iterations(3)
+    .remesh_boundaries(false)
+    .edge_is_constrained_map(constraints_pmap)
+    .vertex_is_constrained_map(corners_pmap));
+  timer.stop();
+  std::cout << "\tremeshing time 1 : " << timer.time() << " seconds" << std::endl;
 
   assert(t3.is_valid());
 
-  target_edge_length = 0.04;//re-refine the mesh
+  target_edge_length = 0.02;//re-refine the mesh
+  timer.reset();
+  timer.start();
+
   CGAL::tetrahedral_isotropic_remeshing(t3, target_edge_length,
-    number_of_iterations(1).remesh_boundaries(true));
+     number_of_iterations(1)
+    .remesh_boundaries(true)
+    .edge_is_constrained_map(constraints_pmap)
+    .vertex_is_constrained_map(corners_pmap));
+
+  timer.stop();
+  std::cout << "\tremeshing time 2 : " << timer.time() << " seconds " << std::endl;
 
   assert(t3.is_valid());
 }
@@ -76,7 +123,7 @@ int main(int argc, char* argv[])
   std::cout << "Running test with the CGAL::Sequential_tag tag." << std::endl;
   mesh<CGAL::Sequential_tag>(m);
 
-#ifdef CGAL_LINKED_WITH_TBB
+#if defined CGAL_ACTIVATE_CONCURRENT_MESHING && defined CGAL_LINKED_WITH_TBB
   std::cout << std::endl;
   std::cout << "Running test with the CGAL::Parallel_tag tag." << std::endl;
   mesh<CGAL::Parallel_tag>(m);
