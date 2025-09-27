9cdaacb7d0d80d16e6dcf7714cf258ab0f57b149
Sebastien Loriot <sloriot.ml@gmail.com>
2025-09-27 08:32:50 +0200

    warnings

--- Surface_mesh/test/Surface_mesh/issue_9086.cpp
+++ Surface_mesh/test/Surface_mesh/issue_9086.cpp
@@ -8,15 +8,15 @@ int main()
 {
 CGAL::Surface_mesh<CGAL::Epick::Point_3> mesh;
   auto f0 = mesh.add_face();
-  auto f1 = mesh.add_face();  // If this line is commented out, it will not crash.
+  mesh.add_face();
   mesh.remove_face(f0);
 
   auto v0 = mesh.add_vertex();
-  auto v1 = mesh.add_vertex();  // If this line is commented out, it will not crash.
+  mesh.add_vertex();
   mesh.remove_vertex(v0);
 
   auto e0 = mesh.add_edge();
-  auto e1 = mesh.add_edge();  // If this line is commented out, it will not crash.
+  mesh.add_edge();
   mesh.remove_edge(edge(e0,mesh));
 
 
