0cb0b3854aeef3259f1579b3577b6896f9098b90
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2025-09-30 21:05:20 +0200

    Remove \link

--- Kernel_d/doc/Kernel_d/Concepts/Kernel--Component_accessor_d.h
+++ Kernel_d/doc/Kernel_d/Concepts/Kernel--Component_accessor_d.h
@@ -19,17 +19,17 @@ the dimension of \f$ p\f$.
 int dimension(const Kernel_d::Point_d& p);
 
 /*!
-returns the ith homogeneous coordinate of \f$ p\f$.
+returns the i-th homogeneous coordinate of \f$ p\f$.
 
-\pre `0 <= i <=` \link Kernel_d::Component_accessor_d::dimension `dimension`\endlink(`p`).
+\pre `0 <= i <= dimension(p)`.
 */
 Kernel_d::RT homogeneous(const Kernel_d::Point_d& p,
 int i);
 
 /*!
-returns the ith %Cartesian coordinate of \f$ p\f$.
+returns the i-th %Cartesian coordinate of \f$ p\f$.
 
-\pre `0 <= i <` \link Kernel_d::Component_accessor_d::dimension `dimension`\endlink(`p`).
+\pre `0 <= i < dimension(p)`.
 */
 Kernel_d::FT cartesian(const Kernel_d::Point_d& p, int
 i);
