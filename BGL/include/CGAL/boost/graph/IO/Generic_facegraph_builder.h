f697aa5237691b24826af9fc76a0984fc2414f4e
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2025-10-02 13:35:48 +0200

    Also dump can_add_face output when a face fails to be added

--- BGL/include/CGAL/boost/graph/IO/Generic_facegraph_builder.h
+++ BGL/include/CGAL/boost/graph/IO/Generic_facegraph_builder.h
@@ -145,7 +145,13 @@ public:
 
       face_descriptor f = CGAL::Euler::add_face(face, g);
       if(f == boost::graph_traits<Graph>::null_face()) {
-        if (verbose) std::cerr << "Error: Failed to add face [" << i << "]\n";
+        if (verbose) {
+          std::cerr << "Error: Failed to add face [" << i << "]\n";
+          std::cerr << "Diagnostic:" << std::endl;
+          CGAL_assertion_code(bool cannot_add = )
+            CGAL::Euler::can_add_face(face, g, true /*verbose*/);
+          CGAL_assertion(!cannot_add);
+        }
         return false;
       }
 
