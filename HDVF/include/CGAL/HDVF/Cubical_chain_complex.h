538a3b3e8f77a0cf03e1f9e57eefdba544077d7d
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-10-13 14:28:28 +0100

    Add tparam

--- HDVF/include/CGAL/HDVF/Cubical_chain_complex.h
+++ HDVF/include/CGAL/HDVF/Cubical_chain_complex.h
@@ -74,6 +74,8 @@ namespace Homological_discrete_vector_field {
  \cgalModels{GeometricChainComplex}
 
  \tparam CoefficientRing a model of the `IntegralDomainWithoutDivision` concept.
+
+ \tparam Traits a geometric traits class model of the `HDVFTraits` concept.
  */
 
 
