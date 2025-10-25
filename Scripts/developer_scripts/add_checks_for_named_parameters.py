7475b9e657c0a93de2ebd5a6c962d270826961a5
Sébastien Loriot <sebastien.loriot@cgal.org>
2025-10-25 22:17:17 +0200

    handle several nps

--- Scripts/developer_scripts/add_checks_for_named_parameters.py
+++ Scripts/developer_scripts/add_checks_for_named_parameters.py
@@ -26,57 +26,28 @@ def is_in_comment_section(line):
 
 in_comment_section = False
 
+params_stack={}
+np_names=[]
 while True:
   line = f.readline()
   if not line:
     break;
   res+=line
 
-  if not is_in_comment_section(line):
-    continue
-
-  #possible np name (TODO: does not work if not on the same line)
-  if re.search("bgl_namedparameters", line):
-    m = re.search("[@\\\]param\s+([^ ]+)\s", line)
-    if m:
-      np_name=m.group(1)
-      #print("found "+np_name)
-
-  if re.search("cgalNamedParamsBegin", line):
-    params=[]
-    while True:
-      line = f.readline()
-      if not line:
-        break
-      res+=line
-      if re.search("cgalNamedParamsEnd", line):
-        is_in_comment_section(line)
-        break
-      m = re.search(r"cgalParamNBegin{\s*([^ ]+)\s*}", line)
-      if m:
-        params.append(m.group(1).strip())
-    if not line:
-      break
-    while True:
-      line = f.readline()
-      if not line:
-        break
-      res+=line
-      if re.search("cgalNamedParamsBegin", line):
-        stderr.write("Function with several nps! Not handled yet ("+argv[1]+")\n")
-        exit(1)
-      if is_in_comment_section(line):
-        continue
-
+  if bool(params_stack):
+    if not is_in_comment_section(line):
       m = re.search("^(\s*).*{", line)
       if m:
-        if len(params)==0:
-          stderr.write("Cannot parse documented named parameters ("+argv[1]+")\n")
-          exit(1)
-        s=m.group(1)+"  CGAL_CHECK_AUTHORIZED_NAMED_PARAMETERS("+np_name
-        for p in params:
-          s+=", "+p+"_t"
-        s+=");"
+        s=""
+        for np_name, params in params_stack.items():
+          if len(params)==0:
+            stderr.write("Cannot parse documented named parameters ("+argv[1]+")\n")
+            exit(1)
+          s+=m.group(1)+"  CGAL_CHECK_AUTHORIZED_NAMED_PARAMETERS("+np_name
+          for p in params:
+            s+=", "+p+"_t"
+          s+=");\n"
+        params_stack={}
 
         mb = re.search("(^\s+{)(.*)}$", line)
         if mb:
@@ -99,20 +70,51 @@ while True:
             stderr.write("Error parsing the function\n")
             exit(1)
           if re.search("CGAL_CHECK_AUTHORIZED_NAMED_PARAMETERS", line):
-            macro_already_here=True
-            break
+            continue # in case there are more than one macro
           else:
-            if line=="\n":
-              res+=line
-            else:
+            #~ if line=="\n":
+              #~ res+=line
+            #~ else:
               break # do we want to support comments?
 
-        if add_macro:
-          res+=s+"\n"
-          if not macro_already_here:
-            res+="\n"+line
-          modified=True
-        break;
+        res+=s+"\n"
+        if line != "\n":
+          res+=line
+        modified=True
+        continue;
+  else:
+    if not is_in_comment_section(line):
+      continue
+
+  #possible np name (TODO: does not work if not on the same line)
+  if re.search("bgl_namedparameters", line):
+    m = re.search("[@\\\]param\s+([^ ]+)\s", line)
+    if m:
+      np_name=m.group(1)
+      np_names.append(np_name)
+
+  if re.search("cgalNamedParamsBegin", line):
+    params=[]
+    while True:
+      line = f.readline()
+      if not line:
+        break
+      res+=line
+      if re.search("cgalNamedParamsEnd", line):
+        is_in_comment_section(line)
+        for np_name in np_names:
+          params_stack[np_name]=params
+        np_names=[]
+        break
+      m = re.search(r"cgalParamNBegin{\s*([^ ]+)\s*}", line)
+      if m:
+        params.append(m.group(1).strip())
+    if not line:
+      stderr.write("ERROR: don't remember why I put that ("+argv[1]+")\n")
+      break
+
+
+
 
 f.close()
 if modified:
