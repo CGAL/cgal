04cb44bea142f1a513f59063c7463aa64469efa8
Laurent Rineau <laurent.rineau@cgal.org>
2025-10-31 15:43:57 +0100

    fix license: LGPL

--- STL_Extension/include/CGAL/bisect_failures.h
+++ STL_Extension/include/CGAL/bisect_failures.h
@@ -5,7 +5,7 @@
 //
 // $URL$
 // $Id$
-// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
 //
 //
 // Author(s)     : Laurent Rineau
