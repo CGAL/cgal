2c4855c4389fbe20d99dfd51723d831a47c7bcad
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-11-04 13:37:46 +0000

    Use Exact_predicates_inexact_constructions_kernel

--- HDVF/examples/HDVF/dual_hdvf_simplicial_cgal.cpp
+++ HDVF/examples/HDVF/dual_hdvf_simplicial_cgal.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <functional>
-#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/HDVF/Hdvf_traits_3.h>
 #include <CGAL/HDVF/Surface_mesh_io.h>
 #include <CGAL/HDVF/Simplicial_chain_complex.h>
@@ -15,7 +15,8 @@
 
 namespace HDVF = CGAL::Homological_discrete_vector_field;
 
-typedef CGAL::Simple_cartesian<double> Kernel;
+// typedef CGAL::Simple_cartesian<double> Kernel;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef HDVF::Hdvf_traits_3<Kernel> Traits;
 
 //typedef HDVF::Zp<5,int,true> Coefficient_ring;
@@ -32,36 +33,36 @@ int main(int argc, char **argv)
     if (argc > 2) std::cout << "usage: dual_hdvf_simplicial off_file" << std::endl;
     else if (argc == 1) filename  = "data/data_simplicial/two_rings.off";
     else filename = argv[1];
-    
+
     // Load simplicial object into Surface_mesh
     Surface_mesh sm;
     CGAL::IO::read_polygon_mesh(filename, sm);
-    
+
     if (sm.number_of_vertices() == 0)
     {
         std::cerr << "Emtpy mesh read" << std::endl;
         throw("Emtpy mesh read");
     }
-    
+
 //    std::cout << sm;
-    
+
     // Build K and L
     typename Tools_type::Complex_duality_data t(Tools_type::simplicial_chain_complex_bb(sm, 1.5, 2));
     std::cout << "--- Triangulation built" << std::endl ;
     Complex& L(t.L);
     Sub_chain_complex& K(t.K);
     std::cout << "--- K,L built" << std::endl ;
-    
+
     std::cout << "----> complex informations" << std::endl ;
     std::cout << "------> complex L" << std::endl ;
     std::cout << L;
     std::cout << "------> subcomplex K" << std::endl ;
     std::cout << K << std::endl ;
-    
+
     // Create and compute a perfect HDVF
     HDVF_type hdvf(L, K, HDVF::OPT_FULL);
     hdvf.compute_perfect_hdvf();
-    
+
     // Export K HDVF
     hdvf.set_mask_K();
     CGAL::IO::write_VTK(hdvf, L, "tmp/res_complex_K", false) ;
