15095827f3d893dc09a86f11b9de118bee596c49
lvalque <leo.valque@gmail.com>
2025-11-19 18:04:02 +0100

    Add test without rounding before computing the kernel

--- Polygon_mesh_processing/test/Polygon_mesh_processing/test_mesh_kernel.cpp
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/test_mesh_kernel.cpp
@@ -13,6 +13,45 @@ using EMesh=CGAL::Surface_mesh<EK::Point_3>;
 
 namespace PMP=CGAL::Polygon_mesh_processing;
 
+template<class Mesh>
+void round_mesh(Mesh &m){
+  auto exp = [](const double v)
+  {
+    int n;
+    frexp(v, &n);
+    return n;
+  };
+  auto pow_2 = [](const int k)
+  {
+      return (k>=0)?std::pow(2,k):1./std::pow(2,-k);
+  };
+
+  CGAL::Bbox_3 bb;
+  for(auto vh : m.vertices()){
+    bb += m.point(vh).bbox();
+  }
+  size_t grid_size=26;
+  std::array<double, 3> max_abs{(std::max)(-bb.xmin(), bb.xmax()),
+                                (std::max)(-bb.ymin(), bb.ymax()),
+                                (std::max)(-bb.zmin(), bb.zmax())};
+  // Compute scale so that the exponent of max absolute value are 52-1.
+  std::array<double, 3> scale{pow_2(grid_size - exp(max_abs[0]) - 1),
+                              pow_2(grid_size - exp(max_abs[1]) - 1),
+                              pow_2(grid_size - exp(max_abs[2]) - 1)};
+
+  // auto round=[&](Point_3 &p){
+  auto round=[&](typename Mesh::Point &p){
+    return typename Mesh::Point(std::ceil((CGAL::to_double(p.x()) * scale[0]) - 0.5) / scale[0],
+                                std::ceil((CGAL::to_double(p.y()) * scale[1]) - 0.5) / scale[1],
+                                std::ceil((CGAL::to_double(p.z()) * scale[2]) - 0.5) / scale[2]);
+  };
+
+  for(auto vh : m.vertices()){
+    auto &p = m.point(vh);
+    p = round(p);
+  }
+}
+
 void test_traits()
 {
   Mesh m;
@@ -44,6 +83,24 @@ void test_kernel(std::string fname)
   std::cout << "test_kernel done in " << timer.time() << "\n";
 }
 
+void test_kernel_with_rounding(std::string fname)
+{
+  CGAL::Real_timer timer;
+  Mesh m;
+  if (!CGAL::IO::read_polygon_mesh(fname, m)|| is_empty(m))
+  {
+    std::cerr << "ERROR: cannot read " << fname << "\n";
+    exit(1);
+  }
+  round_mesh(m);
+  timer.start();
+  Mesh kernel = PMP::experimental::kernel(m);
+  timer.stop();
+
+  std::ofstream("kernel.off") << kernel;
+  std::cout << "test_kernel done in " << timer.time() << "\n";
+}
+
 void test_exact_kernel(std::string fname)
 {
   CGAL::Real_timer timer;
@@ -53,6 +110,24 @@ void test_exact_kernel(std::string fname)
     std::cerr << "ERROR: cannot read " << fname << "\n";
     exit(1);
   }
+  timer.start();
+  EMesh kernel = PMP::experimental::kernel(m);
+  timer.stop();
+
+  std::ofstream("ekernel.off") << kernel;
+  std::cout << "test_exact_kernel done in " << timer.time() << "\n";
+}
+
+void test_exact_kernel_with_rounding(std::string fname)
+{
+  CGAL::Real_timer timer;
+  EMesh m;
+  if (!CGAL::IO::read_polygon_mesh(fname, m)|| is_empty(m))
+  {
+    std::cerr << "ERROR: cannot read " << fname << "\n";
+    exit(1);
+  }
+  round_mesh(m);
   timer.start();
   EMesh kernel = PMP::experimental::kernel(m);
   timer.stop();
@@ -98,10 +173,12 @@ void test_kernel_with_chull_and_constructions(std::string fname)
 
 int main(int argc, char** argv)
 {
-  test_traits();
+  // test_traits();
   const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/blobby.off");
-  test_kernel(filename);
-  test_exact_kernel(filename);
-  test_kernel_with_chull(filename);
-  test_kernel_with_chull_and_constructions(filename);
+  // test_kernel(filename);
+  // test_kernel_with_rounding(filename);
+  // test_exact_kernel(filename);
+  test_exact_kernel_with_rounding(filename);
+  // test_kernel_with_chull(filename);
+  // test_kernel_with_chull_and_constructions(filename);
 }
