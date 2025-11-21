3354de25e50fd8ee9945201e06fd54c0a2de7b37
Camille Lanuel <camille.lanuel@inria.fr>
2025-11-21 11:40:27 +0100

    update demo

--- Triangulation_on_hyperbolic_surface_2/demo/Triangulation_on_hyperbolic_surface_2/Delaunay_triangulation_on_hyperbolic_surface_2_demo.cpp
+++ Triangulation_on_hyperbolic_surface_2/demo/Triangulation_on_hyperbolic_surface_2/Delaunay_triangulation_on_hyperbolic_surface_2_demo.cpp
@@ -23,50 +23,52 @@ typedef CGAL::Hyperbolic_fundamental_domain_factory_2<Traits>                   
 typedef CGAL::Delaunay_triangulation_on_hyperbolic_surface_2<Traits>                                            Delaunay_triangulation;
 typedef typename Delaunay_triangulation::Anchor                                                                 Anchor;
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+  HOW TO USE THIS DEMO
+  ./Delaunay_triangulation_on_hyperbolic_surface_2_demo [epsilon] [surface seed] [precision]
+  With no arguments, uses the default values defined below.
+*/
 
-double eps = 0.25;
+// DEFAULT VALUES
+double epsilon = 0.25;
 int seed = time(NULL);
 int p = 1;
 
 int main(int argc, char *argv[])
 {
+  // 1. Parse args and generate the input
   if (argc > 1) {
-    eps = std::stod(argv[1]);
+    epsilon = std::stod(argv[1]);
   }
 
-  if (argc > 2) {
-    p = atoi(argv[2]);
-  }
-
-  // 1. GENERATE THE INPUT
   Domain domain;
-  if (argc <= 3) {
+  if (argc <= 2) {
     std::cout << "Using random seed " << seed << std::endl;
   } else {
-    seed = atoi(argv[3]);
+    seed = atoi(argv[2]);
   }
-
   Factory factory;
   std::cout << "Generating surface with seed " << seed << "..." << std::endl;
   domain = factory.make_hyperbolic_fundamental_domain_g2(seed);
   Delaunay_triangulation dt = Delaunay_triangulation(domain);
 
-  // 2. GET A VERTEX
+  if (argc > 3) {
+    p = atoi(argv[3]);
+  }
+
+  // 2. Get a vertex
   // So that if you run the demo on a same surface but with different values of epsilon,
   // the drawing will be centered at the same vertex and it will look similar.
   Point v0 = dt.anchor().vertices[0];
 
-  // 3. COMPUTE EPSILON-NET and display useful info
+  // 3. Compute epsilon-net and display useful info
   if constexpr(!std::is_same<NumberType, CGAL::Gmpq>::value) {
     std::cout << "WARNING: Not using the CGAL::Gmpq number type. Precision will be ignored and to_double approximation will be used instead." << std::endl;
   }
-  std::cout << "Computing a " << eps << "-net with floating-point precision " << p*53 << "..." << std::endl;
+  std::cout << "Computing a " << epsilon << "-net with floating-point precision " << p*53 << "..." << std::endl;
   CGAL::Timer timer;
   timer.start();
-  std::cout << "Is epsilon-net? " << dt.epsilon_net(eps, p) << std::endl;
+  std::cout << "Is epsilon-net? " << dt.epsilon_net(epsilon, p) << std::endl;
   timer.stop();
   std::cout << "Done in " << timer.time() << " seconds." << std::endl;
   dt.combinatorial_map().display_characteristics(std::cout) << std::endl;
