215d30bc88782b669977265bf5d0fc7634b41c11
Camille Lanuel <camille.lanuel@inria.fr>
2025-11-28 15:30:14 +0100

    used @param

--- Triangulation_on_hyperbolic_surface_2/doc/Triangulation_on_hyperbolic_surface_2/CGAL/Hyperbolic_Dirichlet_domain_2.h
+++ Triangulation_on_hyperbolic_surface_2/doc/Triangulation_on_hyperbolic_surface_2/CGAL/Hyperbolic_Dirichlet_domain_2.h
@@ -4,7 +4,7 @@ namespace CGAL {
 
   \return a vector with the vertices of a Dirichlet domain whose base point is translated to the origin of the Poincaré disk.
 
-  `domain` must be a fundamental domain whose vertices represent a same point on the corresponding hyperbolic surface.
+  @param domain must be a fundamental domain whose vertices represent a same point on the corresponding hyperbolic surface.
 */
 std::vector<typename Traits::Hyperbolic_Voronoi_point_2> Dirichlet_vertices(Hyperbolic_fundamental_domain_2<Traits> & domain);
 
