179f2dfaf2bc162bc8c15e9942f143fe50d847d5
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-12-02 16:08:21 +0000

    typos

--- HDVF/doc/HDVF/Concepts/HDVFTraits.h
+++ HDVF/doc/HDVF/Concepts/HDVFTraits.h
@@ -5,14 +5,25 @@
 The concept `HDVFTraits` describes the requirements for geometric traits classes used in this package.
 
 \cgalHasModelsBegin
+\cgalHasModelsBare{`CGAL::Homological_discrete_vector_field::Hdvf_traits_2<K>`}
 \cgalHasModelsBare{`CGAL::Homological_discrete_vector_field::Hdvf_traits_3<K>`}
+\cgalHasModelsBare{`CGAL::Homological_discrete_vector_field::Hdvf_traits_d<K>`}
 \cgalHasModelsEnd
+
+@todo add requirements on Point, Vector, FT, Bbox
 */
 class HDVFTraits
 {
 public:
+    /* \brief Type tag encoding the dimension of the data. Must be a `CGAL::Dimension_tag<>` */
+    typedef unspecified_type Dimension ;
+
+    /* \brief Geometric kernel type. Must be a model of the `Kernel` concept. */
+    typedef unspecified_type Kernel ;
     typedef unspecified_type Point ;
     typedef unspecified_type Vector ;
-    typedef unspecified_type FT ;
+    typedef typename Kernel::FT FT ;
+
+    /* \brief Type of bounding boxes in the data dimension. */
     typedef unspecified_type Bbox ;
 };
