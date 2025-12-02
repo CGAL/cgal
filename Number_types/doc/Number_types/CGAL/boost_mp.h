52e4bb845e3e4f319115038a2ebbec634881977a
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-12-02 21:35:18 +0000

    Number_types: Document boost::mp::cpp_int

---
+++ Number_types/doc/Number_types/CGAL/boost_mp.h
@@ -0,0 +1,40 @@
+
+
+
+/*!
+\ingroup nt_gmp
+
+The class `mpq_class` is an exact multiprecision rational number type,
+provided by \gmp.
+CGAL provides the necessary functions to make it compliant to the number type
+concept.
+
+\cgalModels{Field,RealEmbeddable,Fraction}
+
+See the \gmp documentation for additional details.
+
+*/
+
+class mpq_class {
+}; /* end mpq_class */
+
+
+
+
+/*!
+\ingroup nt_gmp
+
+The class `mpz_class` is an exact multiprecision integer number type,
+provided by \gmp.
+CGAL provides the necessary functions to make it compliant to the number type
+concept.
+
+\cgalModels{EuclideanRing,RealEmbeddable}
+
+See the \gmp documentation for additional details.
+
+*/
+
+class mpz_class {
+}; /* end mpz_class */
+
