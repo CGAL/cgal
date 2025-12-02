ee41a0cbadeac5a025f6deaaabfdede9a9af3b3a
Andreas Fabri <andreas.fabri@geometryfactory.com>
2025-12-02 22:01:00 +0000

    Add gmp_int

--- Number_types/doc/Number_types/CGAL/boost_mp.h
+++ Number_types/doc/Number_types/CGAL/boost_mp.h
@@ -6,7 +6,7 @@ namespace boost { namespace multiprecision {
 
 The class `cpp_rational` is an exact multiprecision rational number type,
 provided by \boost.
-CGAL provides the necessary functions to make it compliant to the number type
+This header provides the necessary functions to make it compliant to the number type
 concept.
 
 \cgalModels{Field,RealEmbeddable,Fraction}
@@ -20,6 +20,23 @@ class cpp_rational {
 };
 
 
+/*!
+\ingroup nt_boost
+
+The class `gmp_rational` is an exact multiprecision rational number type,
+provided by \boost.
+This header provides the necessary functions to make it compliant to the number type
+concept.
+
+\cgalModels{Field,RealEmbeddable,Fraction}
+
+See the \boost documentation for additional details on
+<a href="https://www.boost.org/doc/libs/latest/libs/multiprecision/doc/html/boost_multiprecision/tut/rational.html">Boost Multiprecision Rational Types</a>
+
+*/
+
+class gmp_rational {
+};
 
 
 /*!
@@ -27,7 +44,7 @@ class cpp_rational {
 
 The class `cpp_int` is an exact multiprecision integer number type,
 provided by \boost.
-CGAL provides the necessary functions to make it compliant to the number type
+This header provides the necessary functions to make it compliant to the number type
 concept.
 
 \cgalModels{EuclideanRing,RealEmbeddable}
@@ -38,6 +55,24 @@ See the \boost documentation for additional details on
 */
 
 class cpp_int {
+};
+
+/*!
+\ingroup nt_boost
+
+The class `gmp_int` is an exact multiprecision integer number type,
+provided by \boost.
+This header provides the necessary functions to make it compliant to the number type
+concept.
+
+\cgalModels{EuclideanRing,RealEmbeddable}
+
+See the \boost documentation for additional details on
+<a href="https://www.boost.org/doc/libs/latest/libs/multiprecision/doc/html/boost_multiprecision/tut/ints.html">Boost Multiprecision Integer Types</a>
+
+*/
+
+class gmp_int {
 };
 
 }} //namespace boost::multiprecision
