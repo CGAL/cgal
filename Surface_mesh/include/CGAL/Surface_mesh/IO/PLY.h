c782f20cb5c84e08a4d33f2113dd5481109184f2
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2025-12-08 22:45:25 +0100

    Remove types which cannot be read from a PLY file

--- Surface_mesh/include/CGAL/Surface_mesh/IO/PLY.h
+++ Surface_mesh/include/CGAL/Surface_mesh/IO/PLY.h
@@ -236,7 +236,6 @@ public:
     typedef std::tuple<std::int8_t, std::uint8_t,
                        std::int16_t , std::uint16_t,
                        std::int32_t , std::uint32_t,
-                       std::int64_t, std:: uint64_t,
                        float, double> Type_tuple;
 
     for(std::size_t j = 0; j < element.number_of_properties(); ++ j)
