d1527d86f1ea25774555a59cebb9db97d7a0de73
SOUHARDYA TOLA <souhardyatola6297@gmail.com>
2026-01-06 22:18:04 +0530

    Update Arrangement_on_surface_2/include/CGAL/Arr_point_location/Arr_batched_point_location_traits_2.h
    Co-authored-by: albert-github <albert.tests@gmail.com>

--- Arrangement_on_surface_2/include/CGAL/Arr_point_location/Arr_batched_point_location_traits_2.h
+++ Arrangement_on_surface_2/include/CGAL/Arr_point_location/Arr_batched_point_location_traits_2.h
@@ -323,7 +323,7 @@ public:
   Compare_y_at_x_2 compare_y_at_x_2_object() const
   { return (Compare_y_at_x_2(m_base_traits->compare_y_at_x_2_object())); }
 
-  /*! A functor that compares  the y-coordinates of two x-monotone
+  /*! A functor that compares the y-coordinates of two x-monotone
    * curves immediately to the right of their intersection point.
    */
   class Compare_y_at_x_right_2 {
