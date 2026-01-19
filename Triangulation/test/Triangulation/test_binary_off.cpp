abcea8a644b6734b00404a798100ec20db2c63fc
ST3-NET <souhardyatola6297@gmail.com>
2026-01-19 15:43:53 +0530

    Add test case for binary OFF output (Issue verification)

---
+++ Triangulation/test/Triangulation/test_binary_off.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Triangulation_3.h>
+#include <CGAL/IO/Triangulation_off_ostream.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Triangulation_3<K> Triangulation;
+typedef K::Point_3 Point;
+
+int main()
+{
+  Triangulation T;
+  T.insert(Point(0,0,0));
+  T.insert(Point(1,0,0));
+  T.insert(Point(0,1,0));
+  T.insert(Point(0,0,1));
+
+  // The maintainer explicitly requested std::ios::binary
+  const char* filename = "test_binary_output.off";
+  std::ofstream out(filename, std::ios::binary); 
+  
+  if(!out) {
+      std::cerr << "Error opening file!" << std::endl;
+      return 1;
+  }
+
+  // This calls the operator<< you fixed
+  out << T;
+  
+  out.close();
+
+  return 0;
+}
