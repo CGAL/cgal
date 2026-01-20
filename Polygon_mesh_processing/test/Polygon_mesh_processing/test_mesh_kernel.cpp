e0b04d2b332a945a9b2e1110e2fd9f4749018749
Valque Léo <leo.valque@geometryfactory.com>
2026-01-20 17:45:19 +0100

    Add more test in test_mesh_kernel

--- Polygon_mesh_processing/test/Polygon_mesh_processing/test_mesh_kernel.cpp
+++ Polygon_mesh_processing/test/Polygon_mesh_processing/test_mesh_kernel.cpp
@@ -1,5 +1,3 @@
-// #define TEST_MESH_KERNEL_VERBOSE
-
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/Polygon_mesh_processing/transform.h>
 #include <CGAL/Polygon_mesh_processing/kernel.h>
@@ -36,8 +34,6 @@ void test_kernel_on_mesh(const Mesh &input, std::size_t expected_nb_vertices, st
 
   Mesh kernel = PMP::kernel(input, CGAL::parameters::allow_non_manifold_non_watertight_input(true).use_bounding_box_filtering(false).shuffle_planes(false).starting_cube(PMP::bbox(input)));
 #ifdef TEST_MESH_KERNEL_VERBOSE
-  std::ofstream("input.off") << input;
-  std::ofstream("test.off") << kernel;
   std::cout << "nb of vertices: " << vertices(kernel).size() << " ( " << expected_nb_vertices << " expected)" << std::endl
             << "nb of edges: " << edges(kernel).size() << " ( " << expected_nb_edges << " expected)" << std::endl
             << "nb of faces: " << faces(kernel).size() << " ( " << expected_nb_faces << " expected)" << std::endl;
@@ -52,14 +48,23 @@ void test_kernel_on_mesh(const Mesh &input, std::size_t expected_nb_vertices, st
 #endif
     assert(PMP::volume(kernel) > expected_volume * 0.99 && PMP::volume(kernel) < expected_volume * 1.01);
   }
-  clear(kernel);
 
+  clear(kernel);
   kernel = PMP::kernel(input, CGAL::parameters::allow_non_manifold_non_watertight_input(true));
+  std::ofstream("input.off") << input; std::ofstream("output.off") << kernel;
+#ifdef TEST_MESH_KERNEL_VERBOSE
+  std::cout << "nb of vertices: " << vertices(kernel).size() << " ( " << expected_nb_vertices << " expected)" << std::endl
+            << "nb of edges: " << edges(kernel).size() << " ( " << expected_nb_edges << " expected)" << std::endl
+            << "nb of faces: " << faces(kernel).size() << " ( " << expected_nb_faces << " expected)" << std::endl;
+#endif
   assert(vertices(kernel).size() == expected_nb_vertices);
   assert(edges(kernel).size()    == expected_nb_edges);
   assert(faces(kernel).size()    == expected_nb_faces);
   if(expected_volume != 0){
     PMP::triangulate_faces(kernel);
+#ifdef TEST_MESH_KERNEL_VERBOSE
+    std::cout << "volume: " << PMP::volume(kernel) << " ( " << expected_volume << " expected)" << std::endl;
+#endif
     assert(PMP::volume(kernel) > expected_volume * 0.99 && PMP::volume(kernel) < expected_volume * 1.01);
   }
 }
@@ -70,6 +75,7 @@ void tests(){
   Mesh m;
 
 #ifdef TEST_MESH_KERNEL_VERBOSE
+  std::cout << "\n Test " << typeid(Mesh).name() << "\n" << std::endl;
   std::cout << "Test cube" << std::endl;
 #endif
   make_hexahedron(P(0,0,0).bbox()+P(1,1,1).bbox(), m);
@@ -145,45 +151,51 @@ void tests(){
   test_kernel_on_mesh(m, 4, 4, 1, 0);
   clear(m);
 
+#ifdef TEST_MESH_KERNEL_VERBOSE
+  std::cout << "Test degenerated to a face 2" << std::endl;
+#endif
+  make_hexahedron(P(0,0,0).bbox()+P(2,2,2).bbox(), m);
+  make_hexahedron(P(0,1,1).bbox()+P(-2,3,3).bbox(), m);
+  test_kernel_on_mesh(m, 4, 4, 1, 0);
+  clear(m);
 
-  // Degenerate to a segment
-  // make_hexahedron(P(0,0,0).bbox()+P(1,1,1).bbox(), m);
-  // kernel = PMP::internal::kernel(m, faces(m), CGAL::parameters::allow_non_manifold_non_watertight_input(true).use_bounding_box_filtering(false));
-  // assert(kernel.vertices().size()==8);
+#ifdef TEST_MESH_KERNEL_VERBOSE
+  std::cout << "Test degenerated to a segment 1" << std::endl;
+#endif
+  make_hexahedron(P(0,0,0).bbox()+P(1,1,1).bbox(), m);
+  make_hexahedron(P(0,0,0).bbox()+P(-1,-1,1).bbox(), m);
+  test_kernel_on_mesh(m, 2, 0, 0, 0);
+  clear(m);
 
-  // Degenerate to a face
-  // make_hexahedron(P(0,0,0).bbox()+P(1,1,1).bbox(), m);
-  // make_hexahedron(P(0,0,0).bbox()+P(-1,1,1).bbox(), m);
-  // kernel = PMP::internal::kernel(m, faces(m), CGAL::parameters::allow_non_manifold_non_watertight_input(true));
-  // assert(kernel.vertices().size()==4);
+#ifdef TEST_MESH_KERNEL_VERBOSE
+  std::cout << "Test degenerated to a segment 2" << std::endl;
+#endif
+  make_hexahedron(P(0,0,0).bbox()+P(2,2,2).bbox(), m);
+  make_hexahedron(P(0,0,1).bbox()+P(-2,-2,3).bbox(), m);
+  test_kernel_on_mesh(m, 2, 0, 0, 0);
+  clear(m);
 
-  // Degenerate to a segment
-  // m.clear();
-  // kernel.clear();
-  // make_hexahedron(P(0,0,0).bbox()+P(1,1,1).bbox(), m);
-  // make_hexahedron(P(0,0,-0.5).bbox()+P(-1,-1,0.5).bbox(), m);
-  // kernel = PMP::internal::kernel(m, faces(m), CGAL::parameters::allow_non_manifold_non_watertight_input(true).use_bounding_box_filtering(false));
-  // assert(kernel.vertices().size()==2);
-  // P a = kernel.point(*kernel.vertices().begin());
-  // P b = kernel.point(*(++kernel.vertices().begin()));
-  // assert((a==P(0,0,0) && b==P(0,0,0.5)) || (b==P(0,0,0) && a==P(0,0,0.5)));
+#ifdef TEST_MESH_KERNEL_VERBOSE
+  std::cout << "Test degenerated to a vertex" << std::endl;
+#endif
+  make_hexahedron(P(0,0,0).bbox()+P(2,2,2).bbox(), m);
+  make_hexahedron(P(0,0,0).bbox()+P(-2,-2,-2).bbox(), m);
+  test_kernel_on_mesh(m, 1, 0, 0, 0);
+  clear(m);
 
-  // Degenerate to a point
-  // m.clear();
-  // kernel.clear();
-  // make_hexahedron(P(0,0,0).bbox()+P(1,1,1).bbox(), m);
-  // make_hexahedron(P(0,0,0).bbox()+P(-1,-1,-1).bbox(), m);
-  // kernel = PMP::internal::kernel(m, faces(m), CGAL::parameters::allow_non_manifold_non_watertight_input(true).use_bounding_box_filtering(false));
-  // assert(kernel.vertices().size()==1);
-  // a = kernel.point(*kernel.vertices().begin());
-  // assert((a==P(0,0,0)));
-
+#ifdef TEST_MESH_KERNEL_VERBOSE
+  std::cout << "Test empty" << std::endl;
+#endif
+  make_hexahedron(P(1,1,1).bbox()+P(3,3,3).bbox(), m);
+  make_hexahedron(P(0,0,0).bbox()+P(-2,-2,-2).bbox(), m);
+  test_kernel_on_mesh(m, 0, 0, 0, 0);
+  clear(m);
 }
 
 int main(/*int argc, char** argv*/)
 {
   tests<SM, K>();
   tests<ESM, EK>();
-  // tests<CGAL::Polyhedron_3<K>,K>();
-  // tests<CGAL::Polyhedron_3<EK>, EK>();
+  tests<CGAL::Polyhedron_3<K>,K>();
+  tests<CGAL::Polyhedron_3<EK>, EK>();
 }
