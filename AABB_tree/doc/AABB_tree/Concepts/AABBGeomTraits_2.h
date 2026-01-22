5eeabbd96a93d8b59c5553ba09dce76c545e03b5
Sven Oesau <sven.oesau@geometryfactory.com>
2026-01-22 10:38:42 +0100

    added Boolean type to AABBGeomTraits_2 and AABBGeomTraits_3 [skip ci]

--- AABB_tree/doc/AABB_tree/Concepts/AABBGeomTraits_2.h
+++ AABB_tree/doc/AABB_tree/Concepts/AABBGeomTraits_2.h
@@ -28,6 +28,11 @@ public:
 /// @{
 
 /*!
+  `bool` or `Uncertain<bool>`
+ */
+typedef unspecified_type Boolean;
+
+/*!
 A functor object to detect intersections between two geometric objects.
 Provides the following operators:
 
