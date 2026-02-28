84f989018de6d474c19709bfb80b5b3d28205de8
aminkhalsi <aminkhalsi@gmail.com>
2026-02-28 16:01:32 +0100

    Formatted GEOMETRY_SOURCE_EDGE

--- Basic_viewer/include/CGAL/Basic_shaders.h
+++ Basic_viewer/include/CGAL/Basic_shaders.h
@@ -892,48 +892,52 @@ noperspective out vec3 GEdgeDistance;
 uniform mat4 u_Vp; // Viewport matrix
 void main()
 {
-// Transform each vertex into viewport space
-vec3 p0 = vec3(u_Vp * (gl_in[0].gl_Position /
-gl_in[0].gl_Position.w));
-vec3 p1 = vec3(u_Vp * (gl_in[1].gl_Position /
-gl_in[1].gl_Position.w));
-vec3 p2 = vec3(u_Vp * (gl_in[2].gl_Position /
-gl_in[2].gl_Position.w));
-// Find the altitudes (ha, hb and hc)
-float a = length(p1 - p2);
-float b = length(p2 - p0);
-float c = length(p1 - p0);
-float alpha = acos( (b*b + c*c - a*a) / (2.0*b*c) );
-float beta = acos( (a*a + c*c - b*b) / (2.0*a*c) );
-float ha = abs( c * sin( beta ) );
-float hb = abs( c * sin( alpha ) );
-float hc = abs( b * sin( alpha ) );
-// Send the triangle along with the edge distances
-GEdgeDistance = vec3( ha, 0, 0 );
-fN= gN[0];
-vs_fP = g_vs_fP[0];
-ls_fP = g_ls_fP[0];
-fColor = gColor[0];
-fEdgeSize = gEdgeSize[0];
-gl_Position = gl_in[0].gl_Position;
-EmitVertex();
-GEdgeDistance = vec3( 0, hb, 0 );
-fN= gN[1];
-vs_fP = g_vs_fP[1];
-ls_fP = g_ls_fP[1];
-fColor = gColor[1];
-fEdgeSize = gEdgeSize[1];
-gl_Position = gl_in[1].gl_Position;
-EmitVertex();
-GEdgeDistance = vec3( 0, 0, hc );
-fN= gN[2];
-vs_fP = g_vs_fP[2];
-ls_fP = g_ls_fP[2];
-fColor = gColor[2];
-fEdgeSize = gEdgeSize[2];
-gl_Position = gl_in[2].gl_Position;
-EmitVertex();
-EndPrimitive();
+  // Transform each vertex into viewport space
+  vec3 p0 = vec3(u_Vp * (gl_in[0].gl_Position /
+  gl_in[0].gl_Position.w));
+  vec3 p1 = vec3(u_Vp * (gl_in[1].gl_Position /
+  gl_in[1].gl_Position.w));
+  vec3 p2 = vec3(u_Vp * (gl_in[2].gl_Position /
+  gl_in[2].gl_Position.w));
+  // Find the altitudes (ha, hb and hc)
+  float a = length(p1 - p2);
+  float b = length(p2 - p0);
+  float c = length(p1 - p0);
+  float alpha = acos( (b*b + c*c - a*a) / (2.0*b*c) );
+  float beta = acos( (a*a + c*c - b*b) / (2.0*a*c) );
+  float ha = abs( c * sin( beta ) );
+  float hb = abs( c * sin( alpha ) );
+  float hc = abs( b * sin( alpha ) );
+
+  // Send the triangle along with the edge distances
+  GEdgeDistance = vec3( ha, 0, 0 );
+  fN= gN[0];
+  vs_fP = g_vs_fP[0];
+  ls_fP = g_ls_fP[0];
+  fColor = gColor[0];
+  fEdgeSize = gEdgeSize[0];
+  gl_Position = gl_in[0].gl_Position;
+  EmitVertex();
+
+  GEdgeDistance = vec3( 0, hb, 0 );
+  fN= gN[1];
+  vs_fP = g_vs_fP[1];
+  ls_fP = g_ls_fP[1];
+  fColor = gColor[1];
+  fEdgeSize = gEdgeSize[1];
+  gl_Position = gl_in[1].gl_Position;
+  EmitVertex();
+
+  GEdgeDistance = vec3( 0, 0, hc );
+  fN= gN[2];
+  vs_fP = g_vs_fP[2];
+  ls_fP = g_ls_fP[2];
+  fColor = gColor[2];
+  fEdgeSize = gEdgeSize[2];
+  gl_Position = gl_in[2].gl_Position;
+  EmitVertex();
+
+  EndPrimitive();
 }
 )DELIM";
 
