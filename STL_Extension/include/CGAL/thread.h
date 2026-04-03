3998ef28fac1cd59376f6bd751f7942939a5d266
Mael Rouxel-Labbé <mael.rouxel.labbe@geometryfactory.com>
2026-04-03 09:05:55 +0200

    Fix some issues with deprecated header messages

--- STL_Extension/include/CGAL/thread.h
+++ STL_Extension/include/CGAL/thread.h
@@ -14,25 +14,20 @@
 
 #define CGAL_DEPRECATED_HEADER "<CGAL/thread.h>"
 #define CGAL_REPLACEMENT_HEADER "<CGAL/config.h>"
+#include <CGAL/Installation/internal/deprecation_warning.h>
 
 #include <CGAL/config.h>
 
 /*
   This file defines the following:
-  - CGAL::cpp11::thread
-  - CGAL::cpp11::atomic
   - CGAL::cpp11::sleep_for
 */
 
 #include <thread>
 #include <chrono>
-#include <CGAL/atomic.h> // for CGAL::cpp11::atomic
 
 namespace CGAL {
-
 namespace cpp11 {
-
-  using std::thread;
 
   inline void sleep_for (double seconds)
   {
