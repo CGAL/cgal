e82278db5bfb1b167701b9afff41637649960051
Sébastien Loriot <sebastien.loriot@cgal.org>
2026-04-28 09:56:33 +0200

    handle packaging issue of tinyGLTF when STB image is not installed automatically

--- Installation/cmake/modules/CGAL_tinygltf_support.cmake
+++ Installation/cmake/modules/CGAL_tinygltf_support.cmake
@@ -1,5 +1,8 @@
 if(TINYGLTF_FOUND AND NOT TARGET CGAL::tinygltf_support)
   add_library(CGAL::tinygltf_support INTERFACE IMPORTED)
+  #definition only needed for v2 series, off by default in v3
+  target_compile_definitions(CGAL::tinygltf_support INTERFACE TINYGLTF_NO_STB_IMAGE)
+  target_compile_definitions(CGAL::tinygltf_support INTERFACE TINYGLTF_NO_STB_IMAGE_WRITE)
   set_target_properties(CGAL::tinygltf_support PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${TINYGLTF_INCLUDE_DIR}")
 endif()
