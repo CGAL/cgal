2398a4522749e5e3e09329a803111141cceec868
Sébastien Loriot <sebastien.loriot@cgal.org>
2026-04-28 10:37:39 +0200

    add missing header

--- Stream_support/include/CGAL/IO/GLTF/tiny_gltf_impl.cpp
+++ Stream_support/include/CGAL/IO/GLTF/tiny_gltf_impl.cpp
@@ -1,3 +1,19 @@
+// Copyright (c) 2025
+// Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland),
+// INRIA Sophia-Antipolis (France),
+// Max-Planck-Institute Saarbruecken (Germany),
+// and Tel-Aviv University (Israel).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org)
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
+// Author(s)     : Dema Nasrawt and Aidan Pawlak
+
 #define TINYGLTF_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
--- Stream_support/include/CGAL/IO/GLTF/write_gltf.h
+++ Stream_support/include/CGAL/IO/GLTF/write_gltf.h
@@ -1,3 +1,17 @@
+// Copyright (c) 2025
+// Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland),
+// INRIA Sophia-Antipolis (France),
+// Max-Planck-Institute Saarbruecken (Germany),
+// and Tel-Aviv University (Israel).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org)
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: LGPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
 // Author(s)     : Dema Nasrawt and Aidan Pawlak
 
 #ifndef CGAL_IO_GLTF_WRITE_GLTF_H
