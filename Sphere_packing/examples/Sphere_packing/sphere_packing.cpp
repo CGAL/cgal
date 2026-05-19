294924da38af3dc1f257a248ed3c73cf40d9d640
Sven Oesau <sven.oesau@geometryfactory.com>
2026-05-19 17:18:20 +0200

    added user manual

--- Sphere_packing/examples/Sphere_packing/sphere_packing.cpp
+++ Sphere_packing/examples/Sphere_packing/sphere_packing.cpp
@@ -6,12 +6,12 @@
 using Kernel = CGAL::Simple_cartesian<double>;
 using Point = Kernel::Point_3;
 using Mesh = CGAL::Surface_mesh<Point>;
+using Sphere_3 = CGAL::Sphere_3<Kernel>;
 
 namespace PMP = CGAL::Polygon_mesh_processing;
 
-int main(int argc, const char **argv)
-{
-  const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/bear.off");
+int main(int argc, const char **argv) {
+  const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/dino.off");
 
   Mesh mesh;
   if (!PMP::IO::read_polygon_mesh(filename, mesh))
@@ -19,5 +19,17 @@ int main(int argc, const char **argv)
     std::cerr << "Invalid input." << std::endl;
     return 1;
   }
+
+  std::vector<Sphere_3> spheres;
+    spheres.clear();
+    pack_spheres(mesh, std::back_inserter(spheres),
+      CGAL::parameters::number_of_spheres(1000).target_coverage(0.98f));
+
+  std::ofstream out("dino.spheres");
+  for (const auto& s : spheres)
+    out << s.center() << " " << CGAL::sqrt(s.squared_radius()) << std::endl;
+
+  out.close();
+
   return 0;
 }
