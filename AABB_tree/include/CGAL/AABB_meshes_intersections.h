11100ee40545441ac14e9c5f464262688648226d
Valque Léo <leo.valque@geometryfactory.com>
2026-05-29 14:37:31 +0200

    add AABB_meshes_intersections of two different meshes

--- AABB_tree/include/CGAL/AABB_meshes_intersections.h
+++ AABB_tree/include/CGAL/AABB_meshes_intersections.h
@@ -114,6 +114,102 @@ void AABB_self_intersections(const TriangleMesh &tm, OutputIterator out, const N
   }
 }
 
+template< typename TriangleMesh1,
+          typename TriangleMesh2,
+          typename OutputIterator,
+          typename NamedParameters1 = parameters::Default_named_parameters,
+          typename NamedParameters2 = parameters::Default_named_parameters>
+void AABB_meshes_intersections(const TriangleMesh1 &tm1,
+                               const TriangleMesh2 &tm2,
+                               OutputIterator out,
+                               const NamedParameters1& np1 = parameters::default_values(),
+                               const NamedParameters2& np2 = parameters::default_values())
+{
+  using CGAL::parameters::choose_parameter;
+  using CGAL::parameters::get_parameter;
+
+  using face_descriptor_1 = typename boost::graph_traits<TriangleMesh1>::face_descriptor;
+  using halfedge_descriptor_1 = typename boost::graph_traits<TriangleMesh1>::halfedge_descriptor;
+
+  using face_descriptor_2 = typename boost::graph_traits<TriangleMesh1>::face_descriptor;
+  using halfedge_descriptor_2 = typename boost::graph_traits<TriangleMesh1>::halfedge_descriptor;
+
+  using Concurrency_tag = typename internal_np::Lookup_named_param_def <
+                                                internal_np::concurrency_tag_t,
+                                                NamedParameters1,
+                                                Sequential_tag
+                                              > ::type;
+  using GT = typename GetGeomTraits<TriangleMesh1, NamedParameters1>::type;
+  GT gt = choose_parameter<GT>(get_parameter(np1, internal_np::geom_traits));
+
+  using Primitive = AABB_face_graph_triangle_primitive<TriangleMesh2>;
+  using Face_bbox_tag = typename CGAL::dynamic_face_property_t<Bbox_3>  ;
+  using Bbox_pmap = typename boost::property_map<TriangleMesh2, Face_bbox_tag>::const_type;
+  using Traits = AABB_traits_3<GT, Primitive, Bbox_pmap>;
+  using Tree = AABB_tree<Traits>;
+
+  auto vpm1 = choose_parameter(get_parameter(np1, internal_np::vertex_point),
+                               get_const_property_map(vertex_point, tm1));
+  auto vpm2 = choose_parameter(get_parameter(np2, internal_np::vertex_point),
+                               get_const_property_map(vertex_point, tm2));
+
+  auto triangle = [&](face_descriptor_1 fd){
+    halfedge_descriptor_1 hd = halfedge(fd,tm1);
+    auto a = get(vpm1, source(hd,tm1));
+    auto b = get(vpm1, target(hd,tm1));
+    auto c = get(vpm1, target(next(hd,tm1),tm1));
+    return typename GT::Triangle_3(a, b, c);
+  };
+
+  auto bbox = [&](face_descriptor_2 fd){
+    halfedge_descriptor_2 hd = halfedge(fd,tm2);
+    Bbox_3 res = get(vpm2, source(hd,tm2)).bbox();
+    res += get(vpm2, target(hd,tm2)).bbox();
+    res += get(vpm2, target(next(hd,tm2),tm2)).bbox();
+    return res;
+  };
+
+  Bbox_pmap bb = get(Face_bbox_tag(), tm2);
+  for(face_descriptor_2 fd : faces(tm2))
+    put(bb, fd, bbox(fd));
+
+  Traits traits(bb);
+  Tree tree(traits);
+  tree.insert(faces(tm2).first, faces(tm2).second, tm2);
+  tree.template build<Concurrency_tag>();
+
+#ifdef CGAL_LINKED_WITH_TBB
+  if constexpr(std::is_same_v<Concurrency_tag, Parallel_tag>)
+  {
+    CGAL_MUTEX mutex;
+    tbb::concurrent_vector<std::pair<face_descriptor_1, face_descriptor_2>> inter;
+    std::vector<face_descriptor_1> face_vec(faces(tm1).begin(), faces(tm1).end());
+    oneapi::tbb::parallel_for(
+      oneapi::tbb::blocked_range<size_t>(0, face_vec.size()),
+      [&](const oneapi::tbb::blocked_range<size_t>& r) {
+        for (size_t i = r.begin(); i != r.end(); ++i) {
+          face_descriptor_1 f_1 = face_vec[i];
+          std::vector<face_descriptor_2> inter;
+          tree.all_intersected_primitives(triangle(f_1), std::back_inserter(inter));
+          CGAL_SCOPED_LOCK(mutex);
+          for(face_descriptor_2 f_2: inter)
+            *out ++ = std::make_pair(f_1, f_2);
+        }
+      }
+    );
+  }
+  else
+#endif
+  {
+    for (face_descriptor_1 f_1: faces(tm1)){
+      std::vector<face_descriptor_2> inter;
+      tree.all_intersected_primitives(triangle(f_1), std::back_inserter(inter));
+      for(face_descriptor_2 f_2: inter)
+        *out ++ = std::make_pair(f_1, f_2);
+    }
+  }
+}
+
 }
 } // end namespace CGAL
 
