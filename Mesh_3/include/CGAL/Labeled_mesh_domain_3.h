cf6045876cde8e3d12b3f86774d8149152314989
ange-clement <ange.clement@etu.umontpellier.fr>
2026-06-18 09:35:32 +0200

    Mesh_3: Changed doc of value outside in create_labeled_image_mesh_domain

--- Mesh_3/include/CGAL/Labeled_mesh_domain_3.h
+++ Mesh_3/include/CGAL/Labeled_mesh_domain_3.h
@@ -631,8 +631,7 @@ public:
    *   \cgalParamNEnd
    *   \cgalParamNBegin{value_outside}
    *     \cgalParamDescription{the value attached to voxels
-   *                           outside of the domain to be meshed. It should be lower than
-   *                           `iso_value`.}
+   *                           outside of the domain to be meshed.}
    *     \cgalParamDefault{0}
    *   \cgalParamNEnd
    *
