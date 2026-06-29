3fee2b3709496fc2f0d87c7bb9e4cade31f91182
Andreas Fabri <andreas.fabri@geometryfactory.com>
2026-06-29 06:09:28 +0100

    Filtered_kernel: Optimisation of side_of_orienyed_sphere_3

---
+++ Filtered_kernel/benchmark/Filtered_kernel/side_of_oriented_sphere_3.cpp
@@ -0,0 +1,77 @@
+// #define CGAL_SMALL_UNORDERED_MAP_STATS
+// #define CGAL_PROFILE
+//#define CGAL_USE_SSE2_FABS
+//#define CGAL_USE_SSE2_MAX
+//#define CGAL_MSVC_USE_STD_FABS  // use this one with precise
+#define CGAL_NDEBUG 1
+#define NDEBUG 1
+
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
+
+#include <CGAL/Timer.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <locale>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel  K;
+
+typedef K::Point_3                                           Point_3;
+typedef CGAL::Timer                                          Timer;
+
+
+int main(int argc, char* argv[])
+{
+  std::locale loc = std::locale()
+      .combine<std::numpunct<char>>(std::locale("en_US.UTF8"));
+  std::cout.imbue(loc);
+
+  int M = 10; // outer loop counter
+  int Q = 10000; // number of consecutive queries
+
+
+  const std::string filename = (argc > 1) ? argv[1] : CGAL::data_file_path("points_3/ocean_r.xyz");
+  if(argc > 2) {
+    auto M_ = std::atoi(argv[2]);
+    if(M_ <= 0) {
+      std::cerr << "Invalid number of iterations: " << M_ << ". Using default value of " << M << std::endl;
+    } else {
+      M = M_;
+    }
+  }
+  if(argc > 3) {
+    auto Q_ = std::atoi(argv[3]);
+    if(Q <= 0) {
+      std::cerr << "Invalid number of iterations: " << Q_ << ". Using default value of " << Q << std::endl;
+    } else {
+      Q = Q_;
+    }
+  }
+  std::ifstream in(filename.c_str());
+  std::vector<Point_3> points;
+  Point_3 p, q;
+
+  while(in >> p ){
+    points.push_back(p);
+  }
+
+  std::cout << points.size() << " points read\n";
+
+  std::cout << "We run " << M << " times: All four consecutive points combined with the next " << Q << " points as query" << std::endl;
+  Timer timer;
+  timer.start();
+  std::size_t inside = 0;
+  std::size_t bound = (std::min)(points.size(), std::size_t(Q));
+  for(int k = 0; k < M; k++)
+    for(int i = 0; i < points.size()-6; ++i)
+      for(int j = i; j < bound ; ++j )
+        if(side_of_oriented_sphere(points[i],points[i+1], points[i+2], points[i+3], points[j]) == CGAL::ON_NEGATIVE_SIDE) ++inside;
+
+  std ::cout << inside << std::endl;
+
+  timer.stop();
+  std::cout << "Time elapsed: " << timer.time() << " sec" << std::endl;
+  return 0;
+}
