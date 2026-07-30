30db3098fc8a19605ed6c950cf72f1c8b515ae45
Jane Tournois <jane.tournois@geometryfactory.com>
2026-07-30 12:53:23 +0200

    change template parameter to C3t3

--- SMDS_3/include/CGAL/to_triangulation_3_complex.h
+++ SMDS_3/include/CGAL/to_triangulation_3_complex.h
@@ -18,7 +18,8 @@
 #include <CGAL/Mesh_complex_3_in_triangulation_3.h>
 #include <CGAL/Triangulation_3.h>
 
-namespace CGAL {
+namespace CGAL
+{
 
 /*!
  * \ingroup PkgSMDS3Functions
@@ -26,34 +27,35 @@ namespace CGAL {
  * to a `Mesh_complex_3_in_triangulation_3` with a `Triangulation_3` as
  * underlying triangulation.
  *
- * @tparam Tr is the underlying triangulation of the input `Mesh_complex_3_in_triangulation_3`
+ * @tparam C3t3 model of `MeshComplex_3InTriangulation_3`
  * @tparam CornerIndex is the type of the corner indices
  * @tparam CurveIndex is the type of the curve indices
  * @tparam NamedParameters a sequence of \ref bgl_namedparameters "Named Parameters"
  *
  * @todo
  */
-template <typename Tr,
-          typename CornerIndex,
-          typename CurveIndex,
+template <typename C3t3,
           typename NamedParameters = parameters::Default_named_parameters>
 CGAL::Mesh_complex_3_in_triangulation_3<
-  CGAL::Triangulation_3<typename Tr::Geom_traits,
-                        typename Tr::Triangulation_data_structure>>
+  CGAL::Triangulation_3<typename C3t3::Triangulation::Geom_traits,
+                        typename C3t3::Triangulation::Triangulation_data_structure>>
 to_triangulation_3_complex(
-  CGAL::Mesh_complex_3_in_triangulation_3<Tr, CornerIndex, CurveIndex> c3t3,
+  C3t3 c3t3,
   const NamedParameters& np = parameters::default_values())
 {
-  using GT = typename Tr::Geom_traits;
-  using TDS = typename Tr::Triangulation_data_structure;
+  using GT = typename C3t3::Triangulation::Geom_traits;
+  using TDS = typename C3t3::Triangulation::Triangulation_data_structure;
+  using Corner_index = typename C3t3::Corner_index;
+  using Curve_index = typename C3t3::Curve_index;
+
   using T3 = CGAL::Triangulation_3<GT, TDS>;
-  using C3t3 = CGAL::Mesh_complex_3_in_triangulation_3<T3, CornerIndex, CurveIndex>;
+  using C3t3_new = CGAL::Mesh_complex_3_in_triangulation_3<T3, Corner_index, Curve_index>;
 
   T3 tr;
   tr.swap(c3t3.triangulation());
-  C3t3 c3t3_new;
+  C3t3_new c3t3_new;
   c3t3_new.triangulation().swap(tr);
-  
+
   // todo : port metadata from input c3t3 to the new one
 
   return c3t3_new;
