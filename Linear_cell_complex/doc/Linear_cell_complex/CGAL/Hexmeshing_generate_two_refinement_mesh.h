4116791c97b8c35b615d8af2dce324a4fefc791d
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2026-07-31 11:50:16 +0200

    Doc hexmeshing

--- Linear_cell_complex/doc/Linear_cell_complex/CGAL/Hexmeshing_generate_two_refinement_mesh.h
+++ Linear_cell_complex/doc/Linear_cell_complex/CGAL/Hexmeshing_generate_two_refinement_mesh.h
@@ -4,13 +4,13 @@ namespace CGAL {
 \ingroup PkgLinearCellComplexConstructions
 
 generates a pure hexahedral mesh from a triangle mesh using the two refinement algorithm described in \cgalCite{cgal:owen2017template-based}.
-Starts to create a regular grid of `cube_cells_per_dim`\f$ ^3\f$ voxels. Then refine voxels intersected by the surface `nb_levels` times, while creating transitions between refined and non-refined voxels.
+Starts to create a regular grid of `cube_cells_per_dim`\f$ ^3\f$ voxels. Then refines voxels intersected by the surface `nb_levels` times, while creating transitions between refined and non-refined voxels.
 
 \tparam TriangleMesh a model of `FaceListGraph`
 \param tmesh a triangle mesh
 \param cube_cells_per_dim number of grid cells, per dimension
 \param nb_levels How many times to perform refinement
-\param trim `true` to apply trimming, i.e.\ remove volumes that are entirely outside of the surface mesh
+\param trim `true` to apply trimming, i.e., remove volumes that are entirely outside of the surface mesh
 \param smooth `true` to smooth the hexahedral mesh, using a 3D laplacian smoothing method
 
 \sa `CGAL::Linear_cell_complex_for_combinatorial_map`
