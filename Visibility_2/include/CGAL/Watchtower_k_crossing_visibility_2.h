5577c69d0fdbc829653bf7fb38b6a46e97c3c42c
Teodor Cirstoiu <teodor.cirstoiu@outlook.com>
2026-08-02 12:21:00 -0400

    feat: create skeleton code for watchtower impl.

---
+++ Visibility_2/include/CGAL/Watchtower_k_crossing_visibility_2.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2026 Toronto Metropolitan Unversity (Canada).
+// All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org).
+//
+// $URL$
+// $Id$
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
+//
+//
+// Author(s):  Yeganeh Bahoo Torudi <bahoo@torontomu.ca>
+//             Teodor Cirstoiu <tcirstoiu@torontomu.ca>
+//
+
+#ifndef CGAL_WATCHTOWER_K_CROSSING_VISIBILITY_2_H
+#define CGAL_WATCHTOWER_K_CROSSING_VISIBILITY_2_H
+
+#include <CGAL/license/Visibility_2.h>
+
+#include <CGAL/tags.h>
+
+namespace CGAL {
+
+template <class Arrangement_2_, class RegularizationCategory = CGAL::Tag_true>
+class Watchtower_k_crossing_visibility_2 {
+};
+
+} // namespace CGAL
+
+#endif // CGAL_WATCHTOWER_K_CROSSING_VISIBILITY_2_H
