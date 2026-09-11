02a76b239d5d1859219c73986b22f950f2418655
Andreas Fabri <andreas.fabri@geometryfactory.com>
2026-09-11 10:42:09 +0100

    Add )

--- Installation/cmake/modules/CGAL_FastFloat_support.cmake
+++ Installation/cmake/modules/CGAL_FastFloat_support.cmake
@@ -2,5 +2,5 @@ if(FastFloat_FOUND AND NOT TARGET CGAL::FastFloat_support)
   add_library(CGAL::FastFloat_support INTERFACE IMPORTED)
   set_target_properties(CGAL::FastFloat_support PROPERTIES
     INTERFACE_COMPILE_DEFINITIONS "CGAL_USE_FastFloat"
-    INTERFACE_INCLUDE_DIRECTORIES "${FastFloat_INCLUDE_DIR}"
+    INTERFACE_INCLUDE_DIRECTORIES "${FastFloat_INCLUDE_DIR}" )
 endif()
