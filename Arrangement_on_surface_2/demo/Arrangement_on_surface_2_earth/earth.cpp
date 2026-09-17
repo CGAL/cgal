dd6a22b689403678fc73806555ecfa4172f8da22
Andreas Fabri <andreas.fabri@geometryfactory.com>
2026-09-17 10:09:42 +0200

    Update Arrangement_on_surface_2/demo/Arrangement_on_surface_2_earth/earth.cpp
    Co-authored-by: Laurent Rineau <Laurent.Rineau@cgal.org>

--- Arrangement_on_surface_2/demo/Arrangement_on_surface_2_earth/earth.cpp
+++ Arrangement_on_surface_2/demo/Arrangement_on_surface_2_earth/earth.cpp
@@ -44,7 +44,7 @@ int main(int argc, char* argv[]) {
   app.setApplicationVersion("0.1");
   try {
 #ifndef QT_NO_OPENGL
-    Main_widget widget(QString(file_name.string().c_str()));
+    Main_widget widget(file_name..c_str());
     widget.show();
 #else
     QLabel note("OpenGL Support required");
