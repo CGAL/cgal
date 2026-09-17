f2eaf2c85235722e5da81a7d59f0b6a5793fedee
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2026-09-17 16:41:19 +0200

    Update real hexmeshing test

--- Linear_cell_complex/test/Linear_cell_complex/hexmeshing_real_mesh_test.cpp
+++ Linear_cell_complex/test/Linear_cell_complex/hexmeshing_real_mesh_test.cpp
@@ -3,37 +3,76 @@
 #include <CGAL/config.h>
 #include <string>
 
-
-void render_two_refinement(const std::string& file, int cube_cells_per_dim, int nb_levels = 1)
+bool render_two_refinement(const std::string& file, int cube_cells_per_dim,
+                           int nb_levels, bool trim, bool smooth)
 {
   CGAL::Polyhedron_3<CGAL::Exact_predicates_inexact_constructions_kernel> poly;
   CGAL::IO::read_polygon_mesh(CGAL::data_file_path("meshes/" + file), poly);
 
-  CGAL::generate_hexahedral_mesh_using_two_refinement
-    (poly, cube_cells_per_dim, nb_levels,  CGAL::parameters::use_trimming(true));
+  auto lcc=CGAL::generate_hexahedral_mesh_using_two_refinement
+    (poly, cube_cells_per_dim, nb_levels,
+     CGAL::parameters::use_trimming(trim).use_smoothing(smooth));
 
-  // CGAL::render_two_refinement_result(hdata);
-  std::cout << "file: " << file << std::endl;
-  std::cout << "nb_levels: " << nb_levels << std::endl;
-  std::cout << "test passed\n" << std::endl;
+  bool res=lcc.is_valid();
+  if(!res)
+  { std::cout<<"[ERROR] lcc is not valid."<<std::endl; }
+
+  std::size_t nb_non_hex=0;
+  for(auto it=lcc.one_dart_per_cell<3>().begin(),
+        itend=lcc.one_dart_per_cell<3>().end(); it!=itend; ++it)
+  {
+    if(!lcc.is_volume_combinatorial_hexahedron(it))
+    { ++nb_non_hex; }
+  }
+
+  if(nb_non_hex>0)
+  {
+    std::cout<<"[ERROR] "<<nb_non_hex<<" volumes are not hexahedron."<<std::endl;
+    res=false;
+  }
+
+  return res;
 }
 
 int main()
 {
-  // render_two_refinement("anc101.off", 20, 0);
-  render_two_refinement("bunny00.off", 16, 0);
-  render_two_refinement("dragon_res3.off", 16, 0);
+  bool res=true;
 
-  // render_two_refinement("anc101.off", 20, 1);
-  render_two_refinement("bunny00.off", 10, 1);
-  render_two_refinement("dragon_res3.off", 10, 1);
-  // std::string s = "mesh2.off";
-  // render_two_refinement(s, 20, 0);
-  // render_two_refinement(s, 20, 1);
+  if(!render_two_refinement("bunny00.off", 16, 0, true, true))
+  {
+    std::cout<<"[ERROR] in hexmeshing_real_mesh_test: test1."<<std::endl;
+    res=false;
+  }
 
-  // render_two_refinement("anc101.off", 20, 2);
-  render_two_refinement("bunny00.off", 6, 2);
-  render_two_refinement("dragon_res2.off", 6, 2);
+  if(!render_two_refinement("dragon_res3.off", 16, 0, false, true))
+  {
+    std::cout<<"[ERROR] in hexmeshing_real_mesh_test: test2."<<std::endl;
+    res=false;
+  }
+
+  if(!render_two_refinement("bunny00.off", 10, 1, false, false))
+  {
+    std::cout<<"[ERROR] in hexmeshing_real_mesh_test: test3."<<std::endl;
+    res=false;
+  }
+
+  if(!render_two_refinement("dragon_res3.off", 10, 1, true, false))
+  {
+    std::cout<<"[ERROR] in hexmeshing_real_mesh_test: test4."<<std::endl;
+    res=false;
+  }
+
+  if(!render_two_refinement("bunny00.off", 6, 2, true, true))
+  {
+    std::cout<<"[ERROR] in hexmeshing_real_mesh_test: test5."<<std::endl;
+    res=false;
+  }
+
+  if(!render_two_refinement("dragon_res2.off", 6, 2, false, false))
+  {
+    std::cout<<"[ERROR] in hexmeshing_real_mesh_test: test6."<<std::endl;
+    res=false;
+  }
 
   return EXIT_SUCCESS;
 }
