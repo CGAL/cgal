f8771e43c884b35d932c0b10fdff9af58ca558ad
Andreas Fabri <andreas.fabri@geometryfactory.com>
2026-09-24 12:29:29 +0100

    Input_rep<float> now also uses from_chars()

--- Stream_support/include/CGAL/IO/io.h
+++ Stream_support/include/CGAL/IO/io.h
@@ -440,8 +440,10 @@ public:
   }
 };
 
+
 template <>
 class Input_rep<float>
+  : public IO_rep_is_specialized
 {
   float& t;
 
@@ -451,64 +453,52 @@ public:
 
   std::istream& operator()( std::istream& is) const
   {
-    typedef std::istream istream;
-    typedef istream::char_type char_type;
-    typedef istream::int_type int_type;
-    typedef istream::traits_type traits_type;
+    using Traits = std::char_traits<char>;
 
-    std::string buffer;
-    buffer.reserve(32);
+    std::streambuf* sb = is.rdbuf();
 
-    char_type c;
-    do
+    auto c = sb->sgetc();
+
+    while (!Traits::eq_int_type(c, Traits::eof()) &&
+           std::isspace(static_cast<unsigned char>(
+             Traits::to_char_type(c))))
     {
-      const int_type i = is.get();
-      if(i == traits_type::eof())
+      c = sb->snextc();
+    }
+
+    if (Traits::eq_int_type(c, Traits::eof())){
+      is.setstate(std::ios_base::failbit);
+      return is;
+    }
+
+    char buffer[256]; // far too big as doubles have only precision 17, but to be on the safe side
+    char* p = buffer;
+    char* const end = buffer + sizeof(buffer);
+
+    while (!Traits::eq_int_type(c, Traits::eof()) &&
+           !std::isspace(static_cast<unsigned char>(
+              Traits::to_char_type(c))))
+    {
+      if (p == end){
+        is.setstate(std::ios_base::failbit);
         return is;
-
-      c = static_cast<char_type>(i);
-    }
-    while (std::isspace(c));
-
-    if(c == '-')
-    {
-      buffer += '-';
-    }
-    else if(c != '+')
-    {
-      is.unget();
-    }
-
-    for(;;)
-    {
-      const int_type i = is.get();
-      if(i == traits_type::eof())
-      {
-        is.clear(is.rdstate() & ~std::ios_base::failbit);
-        break;
       }
 
-      c = static_cast<char_type>(i);
-      if(std::isdigit(c) || (c =='.') || (c =='E') || (c =='e') || (c =='+') || (c =='-'))
-      {
-        buffer += c;
-      }
-      else
-      {
-        is.unget();
-        break;
-      }
+      *p++ = Traits::to_char_type(c);
+      c = sb->snextc();
     }
 
-    if(sscanf_s(buffer.c_str(), "%f", &t) != 1)
-    {
-      // if a 'buffer' does not contain a double, set the fail bit.
+    auto [ptr, ec] = CGAL_FROM_CHARS_NAMESPACE::from_chars(buffer, p, t);
+
+
+    if (ec != std::errc{} || ptr != p){
       is.setstate(std::ios_base::failbit);
     }
 
     return is;
   }
 };
+
 #endif
 
 /*! \relates Input_rep
